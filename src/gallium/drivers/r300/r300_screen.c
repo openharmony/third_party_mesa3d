openharmony/third_party_mesa3d

commit 32cae8d4fe8a79e4dc07eec5d1a1b125b71f7dc9

r300g: do not advertise color_buffer_float on r300 and r400
It seems they can't do unclamped vertex colors. Tested on RV350.

src/gallium/drivers/r300/r300_screen.c
@@ -114,7 +114,7 @@ static int r300_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
         case PIPE_CAP_BLEND_EQUATION_SEPARATE:
         case PIPE_CAP_VERTEX_ELEMENT_INSTANCE_DIVISOR:
         case PIPE_CAP_FRAGMENT_COLOR_CLAMP_CONTROL:
-            return 1;
+            return is_r500 ? 1 : 0;
         case PIPE_CAP_TEXTURE_SWIZZLE:
             return util_format_s3tc_enabled ? r300screen->caps.dxtc_swizzle : 1;
         case PIPE_CAP_MIXED_COLORBUFFER_FORMATS:

commit 18dbab939415b8ea8fd129420fc011094740087c

r300g: fix exposing caps on r300-r400
Broken with 32cae8d4fe8a79e4dc07eec5d1a1b125b71f7dc9.

src/gallium/drivers/r300/r300_screen.c
@@ -113,10 +113,10 @@ static int r300_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
         case PIPE_CAP_TEXTURE_MIRROR_REPEAT:
         case PIPE_CAP_BLEND_EQUATION_SEPARATE:
         case PIPE_CAP_VERTEX_ELEMENT_INSTANCE_DIVISOR:
-        case PIPE_CAP_FRAGMENT_COLOR_CLAMP_CONTROL:
-            return is_r500 ? 1 : 0;
+            return 1;
         case PIPE_CAP_TEXTURE_SWIZZLE:
             return util_format_s3tc_enabled ? r300screen->caps.dxtc_swizzle : 1;
+        case PIPE_CAP_FRAGMENT_COLOR_CLAMP_CONTROL:
         case PIPE_CAP_MIXED_COLORBUFFER_FORMATS:
             return is_r500 ? 1 : 0;
 