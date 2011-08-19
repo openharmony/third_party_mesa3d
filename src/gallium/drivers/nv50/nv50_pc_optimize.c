openharmony/third_party_mesa3d

commit 5e4516dccd805cacccdafd0c0a9b967d99275200

nv50: don't drop flags definition when merging SAT with ADD/MAD

src/gallium/drivers/nv50/nv50_pc_optimize.c
@@ -462,6 +462,8 @@ nv_pass_lower_mods(struct nv_pass *ctx, struct nv_basic_block *b)
          mi->saturate = 1;
          mi->def[0] = nvi->def[0];
          mi->def[0]->insn = mi;
+         mi->flags_def = nvi->flags_def;
+         mi->flags_def->insn = mi;
          nv_nvi_delete(nvi);
       }
    }

commit 18734af0bbe4676908e078a2e5ee87b4ac5eb161

nv50: fix potential null deref in insn modifer optimization pass
Bug introduced in 5e4516dccd805cacccdafd0c0a9b967d99275200.

src/gallium/drivers/nv50/nv50_pc_optimize.c
@@ -462,8 +462,10 @@ nv_pass_lower_mods(struct nv_pass *ctx, struct nv_basic_block *b)
          mi->saturate = 1;
          mi->def[0] = nvi->def[0];
          mi->def[0]->insn = mi;
-         mi->flags_def = nvi->flags_def;
-         mi->flags_def->insn = mi;
+         if (nvi->flags_def) {
+            mi->flags_def = nvi->flags_def;
+            mi->flags_def->insn = mi;
+         }
          nv_nvi_delete(nvi);
       }
    }