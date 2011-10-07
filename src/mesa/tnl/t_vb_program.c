openharmony/third_party_mesa3d

commit 73847e593efafe4a44b1f49fb943f82bb651c188

tnl: Delay results allocation until we actually need them.
Decreases i965 peak memory allocation for a trivial shader program
from 23,483,048B to 21,932,128B, since we never actually use tnl for
rendering.

src/mesa/tnl/t_vb_program.c
@@ -336,6 +336,11 @@ run_vp( struct gl_context *ctx, struct tnl_pipeline_stage *stage )
    for (i = 0; i < VERT_RESULT_MAX; i++) {
       if (program->Base.OutputsWritten & BITFIELD64_BIT(i)) {
          outputs[numOutputs++] = i;
+
+	 if (!store->results[i].data) {
+	    _mesa_vector4f_alloc( &store->results[i], 0, VB->Size, 32 );
+	    store->results[i].size = 4;
+	 }
       }
    }
 
@@ -504,18 +509,11 @@ init_vp(struct gl_context *ctx, struct tnl_pipeline_stage *stage)
    struct vertex_buffer *VB = &(tnl->vb);
    struct vp_stage_data *store;
    const GLuint size = VB->Size;
-   GLuint i;
 
    stage->privatePtr = CALLOC(sizeof(*store));
    store = VP_STAGE_DATA(stage);
    if (!store)
       return GL_FALSE;
-
-   /* Allocate arrays of vertex output values */
-   for (i = 0; i < VERT_RESULT_MAX; i++) {
-      _mesa_vector4f_alloc( &store->results[i], 0, size, 32 );
-      store->results[i].size = 4;
-   }
 
    /* a few other misc allocations */
    _mesa_vector4f_alloc( &store->ndcCoords, 0, size, 32 );

commit dc507b29eef98a934eb592d2a488c5149cee9aa5

tnl: fix result vector allocation regression
We need to allocate all the output vectors.
Fixes a regression from commit 73847e593efafe4a44b1f49fb943f82bb651c188
Fixes fd.o bugs 41441 and 41492.

src/mesa/tnl/t_vb_program.c
@@ -336,11 +336,17 @@ run_vp( struct gl_context *ctx, struct tnl_pipeline_stage *stage )
    for (i = 0; i < VERT_RESULT_MAX; i++) {
       if (program->Base.OutputsWritten & BITFIELD64_BIT(i)) {
          outputs[numOutputs++] = i;
+      }
+   }
 
-	 if (!store->results[i].data) {
-	    _mesa_vector4f_alloc( &store->results[i], 0, VB->Size, 32 );
-	    store->results[i].size = 4;
-	 }
+   /* Allocate result vectors.  We delay this until now to avoid allocating
+    * memory that would never be used if we don't run the software tnl pipeline.
+    */
+   if (!store->results[0].storage) {
+      for (i = 0; i < VERT_RESULT_MAX; i++) {
+         assert(!store->results[i].storage);
+         _mesa_vector4f_alloc( &store->results[i], 0, VB->Size, 32 );
+         store->results[i].size = 4;
       }
    }
 