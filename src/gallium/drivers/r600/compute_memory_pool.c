openharmony/third_party_mesa3d

commit a2297fae66ea7ee097865fd0cc31e5ff06e28656

r600g: Use a texture as the underlying resource for compute_memory_pool
This the first step towards being able to use evergreen_cb to bind RATs.

src/gallium/drivers/r600/compute_memory_pool.c
@@ -41,6 +41,32 @@
 #include "compute_memory_pool.h"
 #include "evergreen_compute_internal.h"
 
+static struct r600_resource_texture * create_pool_texture(struct r600_screen * screen,
+		unsigned size_in_dw)
+{
+
+	struct pipe_resource templ;
+	struct r600_resource_texture * tex;
+
+	memset(&templ, 0, sizeof(templ));
+	templ.target = PIPE_TEXTURE_1D;
+	templ.format = PIPE_FORMAT_R32_UNORM;
+	templ.bind = PIPE_BIND_CUSTOM;
+	templ.usage = PIPE_USAGE_IMMUTABLE;
+	templ.flags = 0;
+	templ.width0 = size_in_dw;
+	templ.height0 = 1;
+	templ.depth0 = 1;
+	templ.array_size = 1;
+
+	tex = (struct r600_resource_texture *)r600_texture_create(
+						&screen->screen, &templ);
+	/* XXX: Propagate this error */
+	assert(tex && "Out of memory");
+	tex->is_rat = 1;
+	return tex;
+}
+
 /**
  * Creates a new pool
  */
@@ -54,8 +80,8 @@ struct compute_memory_pool* compute_memory_pool_new(
 	pool->next_id = 1;
 	pool->size_in_dw = initial_size_in_dw;
 	pool->screen = rscreen;
-	pool->bo = (struct r600_resource*)r600_compute_buffer_alloc_vram(
-					pool->screen, pool->size_in_dw*4);
+	pool->bo = (struct r600_resource*)create_pool_texture(pool->screen,
+							pool->size_in_dw);
 	pool->shadow = (uint32_t*)CALLOC(4, pool->size_in_dw);
 
 	return pool;
@@ -147,8 +173,8 @@ void compute_memory_grow_pool(struct compute_memory_pool* pool,
 	pool->screen->screen.resource_destroy(
 		(struct pipe_screen *)pool->screen,
 		(struct pipe_resource *)pool->bo);
-	pool->bo = r600_compute_buffer_alloc_vram(pool->screen,
-						pool->size_in_dw*4);
+	pool->bo = (struct r600_resource*)create_pool_texture(pool->screen,
+							pool->size_in_dw);
 	compute_memory_shadow(pool, pipe, 0);
 }
 

src/gallium/drivers/r600/evergreen_compute_internal.c
@@ -298,20 +298,13 @@ void evergreen_set_rat(
 		evergreen_compute_get_gpu_format(&fmt, bo);
 	}
 
-	if (linear) {
-		evergreen_reg_set(res,
-			R_028C70_CB_COLOR0_INFO, S_028C70_RAT(1)
-			| S_028C70_ARRAY_MODE(V_028C70_ARRAY_LINEAR_ALIGNED)
-			| S_028C70_FORMAT(fmt.format)
-			| S_028C70_NUMBER_TYPE(fmt.number_type)
-		);
-		evergreen_emit_force_reloc(res);
-	} else {
-		assert(0 && "TODO");
-		///TODO
-//	 evergreen_reg_set(res, R_028C70_CB_COLOR0_INFO, S_028C70_RAT(1) | S_028C70_ARRAY_MODE(????));
-//	 evergreen_emit_force_reloc(res);
-	}
+	evergreen_reg_set(res,
+		R_028C70_CB_COLOR0_INFO, S_028C70_RAT(1)
+		| S_028C70_ARRAY_MODE(V_028C70_ARRAY_LINEAR_ALIGNED)
+		| S_028C70_FORMAT(fmt.format)
+		| S_028C70_NUMBER_TYPE(fmt.number_type)
+	);
+	evergreen_emit_force_reloc(res);
 
 	evergreen_reg_set(res, R_028C74_CB_COLOR0_ATTRIB, S_028C74_NON_DISP_TILING_ORDER(1));
 	evergreen_emit_force_reloc(res);

commit 5904bc3fd05559855c3206782fc1d80036de8860

r600g: Don't create a texture for the memory_pool during screen init
This fixes a segfault in r600_screen_create() introduced by
a2297fae66ea7ee097865fd0cc31e5ff06e286

Reported by tilman on irc.

src/gallium/drivers/r600/compute_memory_pool.c
@@ -48,6 +48,9 @@ static struct r600_resource_texture * create_pool_texture(struct r600_screen * s
 	struct pipe_resource templ;
 	struct r600_resource_texture * tex;
 
+	if (size_in_dw == 0) {
+		return NULL;
+	}
 	memset(&templ, 0, sizeof(templ));
 	templ.target = PIPE_TEXTURE_1D;
 	templ.format = PIPE_FORMAT_R32_UINT;
@@ -93,8 +96,10 @@ struct compute_memory_pool* compute_memory_pool_new(
 void compute_memory_pool_delete(struct compute_memory_pool* pool)
 {
 	free(pool->shadow);
-	pool->screen->screen.resource_destroy((struct pipe_screen *)
+	if (pool->bo) {
+		pool->screen->screen.resource_destroy((struct pipe_screen *)
 			pool->screen, (struct pipe_resource *)pool->bo);
+	}
 	free(pool);
 }
 
@@ -167,15 +172,24 @@ void compute_memory_grow_pool(struct compute_memory_pool* pool,
 
 	new_size_in_dw += 1024 - (new_size_in_dw % 1024);
 
-	compute_memory_shadow(pool, pipe, 1);
+	if (pool->bo) {
+		compute_memory_shadow(pool, pipe, 1);
+	}
 	pool->shadow = (uint32_t*)realloc(pool->shadow, new_size_in_dw*4);
 	pool->size_in_dw = new_size_in_dw;
-	pool->screen->screen.resource_destroy(
-		(struct pipe_screen *)pool->screen,
-		(struct pipe_resource *)pool->bo);
-	pool->bo = (struct r600_resource*)create_pool_texture(pool->screen,
+	if (pool->bo) {
+		pool->screen->screen.resource_destroy(
+			(struct pipe_screen *)pool->screen,
+			(struct pipe_resource *)pool->bo);
+		pool->bo = (struct r600_resource*)create_pool_texture(
+							pool->screen,
 							pool->size_in_dw);
-	compute_memory_shadow(pool, pipe, 0);
+		compute_memory_shadow(pool, pipe, 0);
+	} else {
+		pool->bo = (struct r600_resource*)create_pool_texture(
+							pool->screen,
+							pool->size_in_dw);
+	}
 }
 
 /**
@@ -382,6 +396,8 @@ void compute_memory_transfer(
 
 	struct pipe_transfer *xfer;
 	uint32_t *map;
+
+	assert(gart);
 
 	if (device_to_host)
 	{

src/gallium/drivers/r600/r600_pipe.c
@@ -952,7 +952,7 @@ struct pipe_screen *r600_screen_create(struct radeon_winsys *ws)
 	rscreen->use_surface_alloc = debug_get_bool_option("R600_SURF", TRUE);
 	rscreen->glsl_feature_level = debug_get_bool_option("R600_GLSL130", TRUE) ? 130 : 120;
 
-	rscreen->global_pool = compute_memory_pool_new(1024*16, rscreen);
+	rscreen->global_pool = compute_memory_pool_new(0, rscreen);
 
 	return &rscreen->screen;
 }