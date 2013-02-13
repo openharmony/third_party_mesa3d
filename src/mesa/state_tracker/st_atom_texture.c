openharmony/third_party_mesa3d

commit ff9c243551ae21cf9623cd2bf9d135d2ca295bad

st/mesa: simplify the update_single_texture() function
In particular, rework the sRGB/linear format selection code.
There's no reason to mess with the Mesa format.
Just do everything in terms of the gallium pipe_format.

Reviewed-by: Marek Olšák <maraeo@gmail.com>

src/mesa/state_tracker/st_atom_texture.c
@@ -214,7 +214,7 @@ update_single_texture(struct st_context *st,
    const struct gl_sampler_object *samp;
    struct gl_texture_object *texObj;
    struct st_texture_object *stObj;
-   enum pipe_format st_view_format;
+   enum pipe_format view_format;
    GLboolean retval;
 
    samp = _mesa_get_samplerobj(ctx, texUnit);
@@ -234,32 +234,11 @@ update_single_texture(struct st_context *st,
    }
 
    /* Determine the format of the texture sampler view */
-   st_view_format = stObj->pt->format;
+   view_format = stObj->pt->format;
 
-   {
-      gl_format texFormat;
-      enum pipe_format firstImageFormat;
-
-      if (texObj->Target == GL_TEXTURE_BUFFER) {
-         texFormat = stObj->base._BufferObjectFormat;
-      } else {
-         const struct st_texture_image *firstImage =
-            st_texture_image(stObj->base.Image[0][stObj->base.BaseLevel]);
-         texFormat = firstImage->base.TexFormat;
-      }
-      firstImageFormat = st_mesa_format_to_pipe_format(texFormat);
-      if ((samp->sRGBDecode == GL_SKIP_DECODE_EXT) &&
-	  (_mesa_get_format_color_encoding(texFormat) == GL_SRGB)) {
-         /* Don't do sRGB->RGB conversion.  Interpret the texture data as
-          * linear values.
-          */
-	 const gl_format linearFormat =
-	    _mesa_get_srgb_format_linear(texFormat);
-	 firstImageFormat = st_mesa_format_to_pipe_format(linearFormat);
-      }
-
-      if (firstImageFormat != stObj->pt->format)
-	 st_view_format = firstImageFormat;
+   /* If sRGB decoding is off, use the linear format */
+   if (samp->sRGBDecode == GL_SKIP_DECODE_EXT) {
+      view_format = util_format_linear(view_format);
    }
 
    /* if sampler view has changed dereference it */
@@ -267,7 +246,7 @@ update_single_texture(struct st_context *st,
       if (check_sampler_swizzle(stObj->sampler_view,
 				stObj->base._Swizzle,
 				stObj->base.DepthMode) ||
-	  (st_view_format != stObj->sampler_view->format) ||
+	  (view_format != stObj->sampler_view->format) ||
 	  stObj->base.BaseLevel != stObj->sampler_view->u.tex.first_level) {
 	 pipe_sampler_view_reference(&stObj->sampler_view, NULL);
       }
@@ -275,7 +254,7 @@ update_single_texture(struct st_context *st,
 
    *sampler_view = st_get_texture_sampler_view_from_stobj(stObj, pipe,
 							  samp,
-							  st_view_format);
+							  view_format);
    return GL_TRUE;
 }
 

commit f79a8ddd0f8db2cbeddd5c1acb84e735e0fff97e

st/mesa: fix texture buffer objects
Broken by ff9c243551ae21cf9623cd2bf9d135d2ca295bad.

Reviewed-by: Brian Paul <brianp@vmware.com>

src/mesa/state_tracker/st_atom_texture.c
@@ -234,11 +234,17 @@ update_single_texture(struct st_context *st,
    }
 
    /* Determine the format of the texture sampler view */
-   view_format = stObj->pt->format;
+   if (texObj->Target == GL_TEXTURE_BUFFER) {
+      view_format =
+         st_mesa_format_to_pipe_format(stObj->base._BufferObjectFormat);
+   }
+   else {
+      view_format = stObj->pt->format;
 
-   /* If sRGB decoding is off, use the linear format */
-   if (samp->sRGBDecode == GL_SKIP_DECODE_EXT) {
-      view_format = util_format_linear(view_format);
+      /* If sRGB decoding is off, use the linear format */
+      if (samp->sRGBDecode == GL_SKIP_DECODE_EXT) {
+         view_format = util_format_linear(view_format);
+      }
    }
 
    /* if sampler view has changed dereference it */