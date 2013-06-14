openharmony/third_party_mesa3d

commit 133a7b804b439433c697a9a829827a0494b1cbc4

ilo: access bo size directly
buf->bo_size is readily avaiable, no need to go via buf->bo->get_size().

src/gallium/drivers/ilo/ilo_gpe_gen6.c
@@ -767,7 +767,7 @@ gen6_emit_3DSTATE_VERTEX_BUFFERS(const struct ilo_dev_info *dev,
       if (vb->buffer && vb->stride <= 2048) {
          const struct ilo_buffer *buf = ilo_buffer(vb->buffer);
          const uint32_t start_offset = vb->buffer_offset;
-         const uint32_t end_offset = buf->bo->get_size(buf->bo) - 1;
+         const uint32_t end_offset = buf->bo_size - 1;
 
          dw |= vb->stride << BRW_VB0_PITCH_SHIFT;
 
@@ -1040,7 +1040,7 @@ gen6_emit_3DSTATE_INDEX_BUFFER(const struct ilo_dev_info *dev,
    }
 
    /* end_offset must also be aligned */
-   end_offset = buf->bo->get_size(buf->bo);
+   end_offset = buf->bo_size;
    end_offset -= (end_offset % ib->index_size);
    /* it is inclusive */
    end_offset -= 1;

commit 3b8367963b6eb1ed8fbde570eecefabaa4783e7b

ilo: fix for half-float vertex arrays
Commit 133a7b804b439433c697a9a829827a0494b1cbc4 broke half-float vertex
arrays.  This reverts a part of that commit, and explains why.

src/gallium/drivers/ilo/ilo_gpe_gen6.c
@@ -767,7 +767,20 @@ gen6_emit_3DSTATE_VERTEX_BUFFERS(const struct ilo_dev_info *dev,
       if (vb->buffer && vb->stride <= 2048) {
          const struct ilo_buffer *buf = ilo_buffer(vb->buffer);
          const uint32_t start_offset = vb->buffer_offset;
-         const uint32_t end_offset = buf->bo_size - 1;
+         /*
+          * As noted in ilo_translate_format(), we treat some 3-component
+          * formats as 4-component formats to work around hardware
+          * limitations.  Imagine the case where the vertex buffer holds a
+          * single PIPE_FORMAT_R16G16B16_FLOAT vertex, and buf->bo_size is 6.
+          * The hardware would not be able to fetch it because the vertex
+          * buffer is expected to hold a PIPE_FORMAT_R16G16B16A16_FLOAT vertex
+          * and that takes at least 8 bytes.
+          *
+          * For the workaround to work, we query the physical size, which is
+          * page aligned, to calculate end_offset so that the last vertex has
+          * a better chance to be fetched.
+          */
+         const uint32_t end_offset = intel_bo_get_size(buf->bo) - 1;
 
          dw |= vb->stride << BRW_VB0_PITCH_SHIFT;
 