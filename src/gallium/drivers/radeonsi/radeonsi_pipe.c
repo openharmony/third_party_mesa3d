openharmony/third_party_mesa3d

commit d486c19ba56ede8e265055732e79489fb23c1dd5

radeonsi: require LLVM 3.4 for MSAA

src/gallium/drivers/radeonsi/radeonsi_pipe.c
@@ -371,8 +371,9 @@ static int r600_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
         case PIPE_CAP_PREFER_BLIT_BASED_TEXTURE_TRANSFER:
 	case PIPE_CAP_TGSI_INSTANCEID:
 	case PIPE_CAP_COMPUTE:
+
 	case PIPE_CAP_TEXTURE_MULTISAMPLE:
-		return 1;
+		return HAVE_LLVM >= 0x0304 && rscreen->chip_class == SI;
 
 	case PIPE_CAP_TGSI_TEXCOORD:
 		return 0;

src/gallium/drivers/radeonsi/si_state.c
@@ -1689,7 +1689,7 @@ boolean si_is_format_supported(struct pipe_screen *screen,
 		return FALSE;
 
 	if (sample_count > 1) {
-		if (rscreen->chip_class >= CIK)
+		if (HAVE_LLVM < 0x0304 || rscreen->chip_class != SI)
 			return FALSE;
 
 		switch (sample_count) {

commit 7b6de1b528f820792f5e49f1165a41916270962a

radeonsi: fix feature support reporting
broken by d486c19ba56ede8e265055732e79489fb23c1dd5

src/gallium/drivers/radeonsi/radeonsi_pipe.c
@@ -371,6 +371,7 @@ static int r600_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
         case PIPE_CAP_PREFER_BLIT_BASED_TEXTURE_TRANSFER:
 	case PIPE_CAP_TGSI_INSTANCEID:
 	case PIPE_CAP_COMPUTE:
+		return 1;
 
 	case PIPE_CAP_TEXTURE_MULTISAMPLE:
 		return HAVE_LLVM >= 0x0304 && rscreen->chip_class == SI;