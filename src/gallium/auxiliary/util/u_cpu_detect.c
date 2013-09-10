openharmony/third_party_mesa3d

commit d1ac908739332c109aed665a6cc1a09b670ac4b3

util: don't use _fxsave() with MSVC 2010 or older
And update _MSC_VER comments in p_config.h

Reviewed-by: Roland Scheidegger <sroland@vmware.com>

src/gallium/auxiliary/util/u_cpu_detect.c
@@ -283,8 +283,11 @@ static INLINE boolean sse2_has_daz(void)
    fxarea.mxcsr_mask = 0;
 #if (defined(PIPE_CC_GCC) || defined(PIPE_CC_SUNPRO))
    __asm __volatile ("fxsave %0" : "+m" (fxarea));
-#elif (defined(PIPE_CC_MSVC) || defined(PIPE_CC_ICL))
+#elif (defined(PIPE_CC_MSVC) && _MSC_VER >= 1700)) || defined(PIPE_CC_ICL)
+   /* 1700 = Visual Studio 2012 */
    _fxsave(&fxarea);
+#else
+   fxarea.mxcsr_mask = 0;
 #endif
    return !!(fxarea.mxcsr_mask & (1 << 6));
 }

src/gallium/include/pipe/p_config.h
@@ -58,6 +58,10 @@
 
 /*
  * Meaning of _MSC_VER value:
+ * - 1800: Visual Studio 2013
+ * - 1700: Visual Studio 2012
+ * - 1600: Visual Studio 2010
+ * - 1500: Visual Studio 2008
  * - 1400: Visual C++ 2005
  * - 1310: Visual C++ .NET 2003
  * - 1300: Visual C++ .NET 2002

commit a4138a9f8dcca23c865db15626d105c5803b0448

util: Fix unmatched parenthesis.
Fixes MSVC build error introduced with commit
d1ac908739332c109aed665a6cc1a09b670ac4b3.

src\gallium\auxiliary\util\u_cpu_detect.c(286) : fatal error C1012: unmatched parenthesis : missing '('

Signed-off-by: Vinson Lee <vlee@freedesktop.org>

src/gallium/auxiliary/util/u_cpu_detect.c
@@ -283,7 +283,7 @@ static INLINE boolean sse2_has_daz(void)
    fxarea.mxcsr_mask = 0;
 #if (defined(PIPE_CC_GCC) || defined(PIPE_CC_SUNPRO))
    __asm __volatile ("fxsave %0" : "+m" (fxarea));
-#elif (defined(PIPE_CC_MSVC) && _MSC_VER >= 1700)) || defined(PIPE_CC_ICL)
+#elif (defined(PIPE_CC_MSVC) && _MSC_VER >= 1700) || defined(PIPE_CC_ICL)
    /* 1700 = Visual Studio 2012 */
    _fxsave(&fxarea);
 #else