openharmony/third_party_mesa3d

commit 6e0440a2be6e702c10e73152b0935e7f9a02f57e

mesa: Shrink the size of the enum string lookup struct.
Since it's only used for debug information, we can misalign the struct and
save the disk space.  Another 19k on a 64-bit build.

v2: Make a compiler.h macro to only use the attribute if we know we can.

Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Reviewed-by: Ian Romanick <ian.d.romanick@.intel.com>

src/mapi/glapi/gen/gl_enums.py
@@ -47,8 +47,8 @@ class PrintGlEnums(gl_XML.gl_print_base):
         print '#include "main/imports.h"'
         print '#include "main/mtypes.h"'
         print ''
-        print 'typedef struct {'
-        print '   size_t offset;'
+        print 'typedef struct PACKED {'
+        print '   uint16_t offset;'
         print '   int n;'
         print '} enum_elt;'
         print ''
@@ -78,6 +78,8 @@ static char token_tmp[20];
 
 const char *_mesa_lookup_enum_by_nr( int nr )
 {
+   STATIC_ASSERT(sizeof(enum_string_table) < (1 << 16));
+
    enum_elt *elt = _mesa_bsearch(& nr, enum_string_table_offsets,
                                  Elements(enum_string_table_offsets),
                                  sizeof(enum_string_table_offsets[0]),

src/mesa/main/compiler.h
@@ -270,6 +270,15 @@ static INLINE GLuint CPU_TO_LE32(GLuint x)
 #define NULL 0
 #endif
 
+/* Used to optionally mark structures with misaligned elements or size as
+ * packed, to trade off performance for space.
+ */
+#if (__GNUC__ >= 3)
+#define PACKED __attribute__((__packed__))
+#else
+#define PACKED
+#endif
+
 
 /**
  * LONGSTRING macro

commit 6085d6cbfbe798512cbe19e5d0b4e670f3a84802

glapi: Move declaration before code.
This patch fixes the MSVC build error introduced by commit
6e0440a2be6e702c10e73152b0935e7f9a02f57e.

enums.c
mesa\main\enums.c(3776) : error C2143: syntax error : missing ';' before 'type'
mesa\main\enums.c(3781) : error C2065: 'elt' : undeclared identifier
mesa\main\enums.c(3781) : warning C4047: '!=' : 'int' differs in levels of indirection from 'void *'
mesa\main\enums.c(3782) : error C2065: 'elt' : undeclared identifier
mesa\main\enums.c(3782) : error C2223: left of '->offset' must point to struct/union
mesa\main\enums.c(3782) : warning C4033: '_mesa_lookup_enum_by_nr' must return a value

Signed-off-by: Vinson Lee <vlee@freedesktop.org>

src/mapi/glapi/gen/gl_enums.py
@@ -78,12 +78,14 @@ static char token_tmp[20];
 
 const char *_mesa_lookup_enum_by_nr( int nr )
 {
+   enum_elt *elt;
+
    STATIC_ASSERT(sizeof(enum_string_table) < (1 << 16));
 
-   enum_elt *elt = _mesa_bsearch(& nr, enum_string_table_offsets,
-                                 Elements(enum_string_table_offsets),
-                                 sizeof(enum_string_table_offsets[0]),
-                                 (cfunc) compar_nr);
+   elt = _mesa_bsearch(& nr, enum_string_table_offsets,
+                       Elements(enum_string_table_offsets),
+                       sizeof(enum_string_table_offsets[0]),
+                       (cfunc) compar_nr);
 
    if (elt != NULL) {
       return &enum_string_table[elt->offset];