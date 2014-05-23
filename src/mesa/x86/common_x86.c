openharmony/third_party_mesa3d

commit bf69168fb31b7f328f1b25e0081b56631ebc2176

mesa/x86: add SSE4.1 runtime detection.
Add a bit to _mesa_x86_features for SSE 4.1, along with macros to query.

Reviewed-by: Matt Turner <mattst88@gmail.com>

src/mesa/x86/common_x86.c
@@ -241,6 +241,7 @@ _mesa_get_x86_features(void)
 
        /* get cpu features */
        cpu_features = _mesa_x86_cpuid_edx(1);
+       cpu_features_ecx = _mesa_x86_cpuid_ecx(1);
 
        if (cpu_features & X86_CPU_FPU)
 	   _mesa_x86_cpu_features |= X86_FEATURE_FPU;
@@ -257,6 +258,8 @@ _mesa_get_x86_features(void)
 	   _mesa_x86_cpu_features |= X86_FEATURE_XMM;
        if (cpu_features & X86_CPU_XMM2)
 	   _mesa_x86_cpu_features |= X86_FEATURE_XMM2;
+       if (cpu_features & x86_CPU_SSE4_1)
+	   _mesa_x86_features |= X86_FEATURE_SSE4_1;
 #endif
 
        /* query extended cpu features */
@@ -341,6 +344,9 @@ _mesa_get_x86_features(void)
    _mesa_x86_cpu_features |= X86_FEATURE_XMM | X86_FEATURE_XMM2;
 
    __get_cpuid(1, &eax, &ebx, &ecx, &edx);
+
+   if (ecx & bit_SSE4_1)
+      _mesa_x86_cpu_features |= X86_FEATURE_SSE4_1;
 #endif /* USE_X86_ASM */
 
    (void) detection_debug;

src/mesa/x86/common_x86_features.h
@@ -43,6 +43,7 @@
 #define X86_FEATURE_XMM2	(1<<6)
 #define X86_FEATURE_3DNOWEXT	(1<<7)
 #define X86_FEATURE_3DNOW	(1<<8)
+#define X86_FEATURE_SSE4_1	(1<<9)
 
 /* standard X86 CPU features */
 #define X86_CPU_FPU		(1<<0)
@@ -50,6 +51,8 @@
 #define X86_CPU_MMX		(1<<23)
 #define X86_CPU_XMM		(1<<25)
 #define X86_CPU_XMM2		(1<<26)
+/* ECX. */
+#define X86_CPU_SSE4_1		(1<<19)
 
 /* extended X86 CPU features */
 #define X86_CPUEXT_MMX_EXT	(1<<22)
@@ -62,6 +65,7 @@
 #define cpu_has_xmm2		(_mesa_x86_cpu_features & X86_FEATURE_XMM2)
 #define cpu_has_3dnow		(_mesa_x86_cpu_features & X86_FEATURE_3DNOW)
 #define cpu_has_3dnowext	(_mesa_x86_cpu_features & X86_FEATURE_3DNOWEXT)
+#define cpu_has_sse4_1		(_mesa_x86_cpu_features & X86_FEATURE_SSE4_1)
 
 #endif
 

commit 4db823a2cb0754dfe95df0483b2a3e621f1b3ad4

mesa/x86: fix a typos in SSE4.1 detection
Commit bf69168fb introduced 32-bit code for SSE4.1. Fix compilation, and
make sure to check ecx for the SSE4.1 bit.

[imirkin: switch sse4.1 to look at ecx]
Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>

src/mesa/x86/common_x86.c
@@ -258,8 +258,8 @@ _mesa_get_x86_features(void)
 	   _mesa_x86_cpu_features |= X86_FEATURE_XMM;
        if (cpu_features & X86_CPU_XMM2)
 	   _mesa_x86_cpu_features |= X86_FEATURE_XMM2;
-       if (cpu_features & x86_CPU_SSE4_1)
-	   _mesa_x86_features |= X86_FEATURE_SSE4_1;
+       if (cpu_features_ecx & X86_CPU_SSE4_1)
+	   _mesa_x86_cpu_features |= X86_FEATURE_SSE4_1;
 #endif
 
        /* query extended cpu features */