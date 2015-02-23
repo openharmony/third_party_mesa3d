openharmony/third_party_mesa3d

commit e5fde08fd2b0ba80a1d5983b3537715fdf927c54

radeonsi: small fix in SPI state
Cc: 10.5 10.4 <mesa-stable@lists.freedesktop.org>
Reviewed-by: Michel Dänzer <michel.daenzer@amd.com>

src/gallium/drivers/radeonsi/si_state_shaders.c
@@ -662,8 +662,10 @@ bcolor:
 		}
 
 		if (j == vsinfo->num_outputs) {
-			/* No corresponding output found, load defaults into input */
-			tmp |= S_028644_OFFSET(0x20);
+			/* No corresponding output found, load defaults into input.
+			 * Don't set any other bits.
+			 * (FLAT_SHADE=1 completely changes behavior) */
+			tmp = S_028644_OFFSET(0x20);
 		}
 
 		si_pm4_set_reg(pm4,

commit 022dd1ea1e4e70e6002bd9a93a327e46c340c88c

radeonsi: fix point sprites
Broken by e5fde08fd2b0ba80a1d5983b3537715fdf927c54.

This fix is critical and should be ported to stable ASAP.

Cc: 10.5 10.4 <mesa-stable@lists.freedesktop.org>

src/gallium/drivers/radeonsi/si_state_shaders.c
@@ -661,7 +661,7 @@ bcolor:
 			}
 		}
 
-		if (j == vsinfo->num_outputs) {
+		if (j == vsinfo->num_outputs & !G_028644_PT_SPRITE_TEX(tmp)) {
 			/* No corresponding output found, load defaults into input.
 			 * Don't set any other bits.
 			 * (FLAT_SHADE=1 completely changes behavior) */