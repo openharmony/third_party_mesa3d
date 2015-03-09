openharmony/third_party_mesa3d

commit 34ccb8c97692bb128cf6d78e2c7e953b62b691c0

freedreno: use util_copy_framebuffer_state()
Signed-off-by: Rob Clark <robclark@freedesktop.org>

src/gallium/drivers/freedreno/freedreno_state.c
@@ -117,28 +117,17 @@ fd_set_framebuffer_state(struct pipe_context *pctx,
 {
 	struct fd_context *ctx = fd_context(pctx);
 	struct pipe_framebuffer_state *cso = &ctx->framebuffer;
-	unsigned i;
 
 	DBG("%d: cbufs[0]=%p, zsbuf=%p", ctx->needs_flush,
 			framebuffer->cbufs[0], framebuffer->zsbuf);
 
 	fd_context_render(pctx);
 
-	for (i = 0; i < framebuffer->nr_cbufs; i++)
-		pipe_surface_reference(&cso->cbufs[i], framebuffer->cbufs[i]);
-	for (; i < ctx->framebuffer.nr_cbufs; i++)
-		pipe_surface_reference(&cso->cbufs[i], NULL);
-
-	cso->nr_cbufs = framebuffer->nr_cbufs;
+	util_copy_framebuffer_state(cso, framebuffer);
 
 	if ((cso->width != framebuffer->width) ||
 			(cso->height != framebuffer->height))
 		ctx->needs_rb_fbd = true;
-
-	cso->width = framebuffer->width;
-	cso->height = framebuffer->height;
-
-	pipe_surface_reference(&cso->zsbuf, framebuffer->zsbuf);
 
 	ctx->dirty |= FD_DIRTY_FRAMEBUFFER;
 

commit a9d3b9e5594e817721c92d903c783f41a2750619

freedreno: move fb state copy after checking for size change
Fixes: 34ccb8c9 ("freedreno: use util_copy_framebuffer_state()")
Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
Reviewed-by: Rob Clark <robclark@freedesktop.org>
Cc: "10.4 10.5" <mesa-stable@lists.freedesktop.org>

src/gallium/drivers/freedreno/freedreno_state.c
@@ -123,11 +123,11 @@ fd_set_framebuffer_state(struct pipe_context *pctx,
 
 	fd_context_render(pctx);
 
-	util_copy_framebuffer_state(cso, framebuffer);
-
 	if ((cso->width != framebuffer->width) ||
 			(cso->height != framebuffer->height))
 		ctx->needs_rb_fbd = true;
+
+	util_copy_framebuffer_state(cso, framebuffer);
 
 	ctx->dirty |= FD_DIRTY_FRAMEBUFFER;
 