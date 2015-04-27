openharmony/third_party_mesa3d

commit c3b0134e167845937c1c3bb2db8153ec211c9a23

glsl: support packing of arrays of arrays
Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>

src/glsl/lower_packed_varyings.cpp
@@ -645,14 +645,8 @@ lower_packed_varyings_visitor::needs_lowering(ir_variable *var)
    if (var->data.explicit_location)
       return false;
 
-   const glsl_type *type = var->type;
-   if (this->gs_input_vertices != 0) {
-      assert(type->is_array());
-      type = type->element_type();
-   }
-   if (type->is_array())
-      type = type->fields.array;
-   if (type->vector_elements == 4 && !type->is_double())
+   if (var->type->without_array()->vector_elements == 4 &&
+       !var->type->is_double())
       return false;
    return true;
 }

commit 0548c948cfefe448eb3c4638adb6fea940087388

glsl: fix packing support for arrays of doubles
Broke in commit c3b0134e167845937c1c3bb2db8153ec211c9a23 when
adding support for multidimensional arrays

Reviewed-by: Ilia Mirkin <imirkin at alum.mit.edu>

src/glsl/lower_packed_varyings.cpp
@@ -645,8 +645,8 @@ lower_packed_varyings_visitor::needs_lowering(ir_variable *var)
    if (var->data.explicit_location)
       return false;
 
-   if (var->type->without_array()->vector_elements == 4 &&
-       !var->type->is_double())
+   const glsl_type *type = var->type->without_array();
+   if (type->vector_elements == 4 && !type->is_double())
       return false;
    return true;
 }