openharmony/third_party_mesa3d

commit 857aecef21e96c462ed80536a8dfaff900909881

nvc0/ir: fix predicated PFETCH emission
src1 would contain the predicate, which would get emitted as a register
source by an undiscerning srcId helper. Work around this in the same way
as in emitTEX.

Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
Cc: mesa-stable@lists.freedesktop.org

src/gallium/drivers/nouveau/codegen/nv50_ir_emit_gk110.cpp
@@ -1333,8 +1333,10 @@ CodeEmitterGK110::emitPFETCH(const Instruction *i)
 
    emitPredicate(i);
 
+   const int src1 = (i->predSrc == 1) ? 2 : 1; // if predSrc == 1, !srcExists(2)
+
    defId(i->def(0), 2);
-   srcId(i->src(1), 10);
+   srcId(i, 1, 10);
 }
 
 void

src/gallium/drivers/nouveau/codegen/nv50_ir_emit_nvc0.cpp
@@ -1495,8 +1495,10 @@ CodeEmitterNVC0::emitPFETCH(const Instruction *i)
 
    emitPredicate(i);
 
+   const int src1 = (i->predSrc == 1) ? 2 : 1; // if predSrc == 1, !srcExists(2)
+
    defId(i->def(0), 14);
-   srcId(i->src(1), 20);
+   srcId(i, 1, 20);
 }
 
 void

commit 055a2ef67392d3b67b87f446fe748ba737caf2ed

nvc0/ir: fix predicated PFETCH for real
Commit 857aecef2 accidentally didn't make use of the new src1 variable.
Use it.

Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
Cc: mesa-stable@lists.freedesktop.org

src/gallium/drivers/nouveau/codegen/nv50_ir_emit_gk110.cpp
@@ -1336,7 +1336,7 @@ CodeEmitterGK110::emitPFETCH(const Instruction *i)
    const int src1 = (i->predSrc == 1) ? 2 : 1; // if predSrc == 1, !srcExists(2)
 
    defId(i->def(0), 2);
-   srcId(i, 1, 10);
+   srcId(i, src1, 10);
 }
 
 void

src/gallium/drivers/nouveau/codegen/nv50_ir_emit_nvc0.cpp
@@ -1498,7 +1498,7 @@ CodeEmitterNVC0::emitPFETCH(const Instruction *i)
    const int src1 = (i->predSrc == 1) ? 2 : 1; // if predSrc == 1, !srcExists(2)
 
    defId(i->def(0), 14);
-   srcId(i, 1, 20);
+   srcId(i, src1, 20);
 }
 
 void