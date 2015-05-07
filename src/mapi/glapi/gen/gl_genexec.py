openharmony/third_party_mesa3d

commit 16f31245d1708f1960711cbf5b5cfda1cd9c3c43

glapi/es3.1: Add support for GLES versions > 3.0
Make the checks in the Python script and the generated code more generic
to support arbitrary GLES versions >= 2.0.

The updated dispatch_sanity.cpp test discovered this problem.  Without
this, the next patch would erroneously enable GLES 3.1 functions in GLES
2.0 and GLES 3.0.

Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>

src/mapi/glapi/gen/gl_genexec.py
@@ -183,8 +183,8 @@ class PrintCode(gl_XML.gl_print_base):
             if 'es1' in f.api_map:
                 condition_parts.append('ctx->API == API_OPENGLES')
             if 'es2' in f.api_map:
-                if f.api_map['es2'] == 3:
-                    condition_parts.append('_mesa_is_gles3(ctx)')
+                if f.api_map['es2'] > 2.0:
+                    condition_parts.append('(ctx->API == API_OPENGLES2 && ctx->Version >= {})'.format(int(f.api_map['es2'] * 10)))
                 else:
                     condition_parts.append('ctx->API == API_OPENGLES2')
             if not condition_parts:

commit 29ed60babb0586119b00d3a1c3be690769d00d9b

glapi: Add positional argument specifier.
Fix build error introduced with commit 16f3124 "glapi/es3.1: Add support
for GLES versions > 3.0" with Python < 2.7.

  File "src/mapi/glapi/gen/gl_genexec.py", line 230, in <module>
    printer.Print(api)
  File "src/mapi/glapi/gen/gl_XML.py", line 120, in Print
    self.printBody(api)
  File "src/mapi/glapi/gen/gl_genexec.py", line 187, in printBody
    condition_parts.append('(ctx->API == API_OPENGLES2 && ctx->Version >= {})'.format(int(f.api_map['es2'] * 10)))
ValueError: zero length field name in format

Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

src/mapi/glapi/gen/gl_genexec.py
@@ -184,7 +184,7 @@ class PrintCode(gl_XML.gl_print_base):
                 condition_parts.append('ctx->API == API_OPENGLES')
             if 'es2' in f.api_map:
                 if f.api_map['es2'] > 2.0:
-                    condition_parts.append('(ctx->API == API_OPENGLES2 && ctx->Version >= {})'.format(int(f.api_map['es2'] * 10)))
+                    condition_parts.append('(ctx->API == API_OPENGLES2 && ctx->Version >= {0})'.format(int(f.api_map['es2'] * 10)))
                 else:
                     condition_parts.append('ctx->API == API_OPENGLES2')
             if not condition_parts: