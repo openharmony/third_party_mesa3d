Igalia/mesa

glsl: Identify active uniform blocks that are buffer blocks as such.

src/glsl/link_uniform_block_active_visitor.cpp
@@ -44,6 +44,7 @@ process_block(void *mem_ctx, struct hash_table *ht, ir_variable *var)
 
       b->type = block_type;
       b->has_instance_name = var->is_interface_instance();
+      b->is_buffer = var->data.mode == ir_var_buffer;
 
       if (var->data.explicit_binding) {
          b->has_binding = true;

src/glsl/link_uniform_blocks.cpp
@@ -293,6 +293,8 @@ link_uniform_blocks(void *mem_ctx,
             blocks[i].NumUniforms =
                (unsigned)(ptrdiff_t)(&variables[parcel.index] - blocks[i].Uniforms);
 
+            blocks[i].IsBuffer = b->is_buffer;
+
             i++;
          }
       } else {
@@ -310,6 +312,8 @@ link_uniform_blocks(void *mem_ctx,
 
          blocks[i].NumUniforms =
             (unsigned)(ptrdiff_t)(&variables[parcel.index] - blocks[i].Uniforms);
+
+         blocks[i].IsBuffer = b->is_buffer;
 
          i++;
       }

openharmony/third_party_mesa3d

Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>

src/glsl/link_uniform_block_active_visitor.cpp
@@ -44,6 +44,7 @@ process_block(void *mem_ctx, struct hash_table *ht, ir_variable *var)
 
       b->type = block_type;
       b->has_instance_name = var->is_interface_instance();
+      b->is_shader_storage = var->data.mode == ir_var_shader_storage;
 
       if (var->data.explicit_binding) {
          b->has_binding = true;

src/glsl/link_uniform_blocks.cpp
@@ -293,6 +293,8 @@ link_uniform_blocks(void *mem_ctx,
             blocks[i].NumUniforms =
                (unsigned)(ptrdiff_t)(&variables[parcel.index] - blocks[i].Uniforms);
 
+            blocks[i].IsShaderStorage = b->is_shader_storage;
+
             i++;
          }
       } else {
@@ -310,6 +312,8 @@ link_uniform_blocks(void *mem_ctx,
 
          blocks[i].NumUniforms =
             (unsigned)(ptrdiff_t)(&variables[parcel.index] - blocks[i].Uniforms);
+
+         blocks[i].IsShaderStorage = b->is_shader_storage;
 
          i++;
       }