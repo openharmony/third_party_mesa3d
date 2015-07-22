ChristophHaag/mesa-mesa

st/mesa: lower gl_TessLevel from float[] to vecn

src/mesa/state_tracker/st_extensions.c
@@ -251,6 +251,7 @@ void st_init_limits(struct pipe_screen *screen,
          options->MaxUnrollIterations = MIN2(screen->get_shader_param(screen, sh, PIPE_SHADER_CAP_MAX_INSTRUCTIONS), 65536);
 
       options->LowerClipDistance = true;
+      options->LowerTessLevel = true;
    }
 
    c->MaxCombinedTextureImageUnits =

openharmony/third_party_mesa3d

src/mesa/state_tracker/st_extensions.c
@@ -253,6 +253,8 @@ void st_init_limits(struct pipe_screen *screen,
       options->LowerClipDistance = true;
    }
 
+   c->LowerTessLevel = true;
+
    c->MaxCombinedTextureImageUnits =
          _min(c->Program[MESA_SHADER_VERTEX].MaxTextureImageUnits +
               c->Program[MESA_SHADER_TESS_CTRL].MaxTextureImageUnits +