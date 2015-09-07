openharmony/third_party_mesa3d

commit cae42b51de89f13431cb6eaddd5e636e14b1cb3b

util: always include sha1 into the build
SHA1 is now used in all builds when HAVE_SHA1 is defined. Adjust src to
do the same thing, rather than predicating on shader cache.

Fixes: 27bd3aaa995 ("mesa: change 'SHADER_SUBST' facility to work with env variables")
Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
Reviewed-by: Dave Airlie <airlied@gmail.com>

src/util/mesa-sha1.c
@@ -290,6 +290,8 @@ _mesa_sha1_final(struct mesa_sha1 *ctx, unsigned char result[20])
 
 #endif
 
+#ifdef HAVE_SHA1
+
 void
 _mesa_sha1_compute(const void *data, size_t size, unsigned char result[20])
 {
@@ -314,3 +316,5 @@ _mesa_sha1_format(char *buf, const unsigned char *sha1)
 
    return buf;
 }
+
+#endif

src/util/Makefile.am
@@ -44,11 +44,7 @@ libmesautil_la_SOURCES = \
 	$(MESA_UTIL_FILES) \
 	$(MESA_UTIL_GENERATED_FILES)
 
-if ENABLE_SHADER_CACHE
-libmesautil_la_SOURCES += $(MESA_UTIL_SHADER_CACHE_FILES)
-
 libmesautil_la_LIBADD = $(SHA1_LIBS)
-endif
 
 roundeven_test_LDADD = -lm
 

src/util/Makefile.sources
@@ -1,7 +1,3 @@
-MESA_UTIL_SHADER_CACHE_FILES := \
-	mesa-sha1.c \
-	mesa-sha1.h
-
 MESA_UTIL_FILES :=	\
 	bitset.h \
 	format_srgb.h \
@@ -9,6 +5,8 @@ MESA_UTIL_FILES :=	\
 	hash_table.h \
 	list.h \
 	macros.h \
+	mesa-sha1.c \
+	mesa-sha1.h \
 	ralloc.c \
 	ralloc.h \
 	register_allocate.c \

commit 913ec3201bb76a3a4d350b5a961eeacc22d30b44

util: make mesa-sha1.c completely empty when there are no SHA1 impls
My earlier attempt to fix this missed the fact that there was a #else
clause that assumes that you have openssh. This moves the whole thing
under #ifdef HAVE_SHA1 which should avoid this issue.

Fixes: cae42b51d (util: always include sha1 into the build)
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=91898
Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
Reviewed-by: Dave Airlie <airlied@gmail.com>

src/util/mesa-sha1.c
@@ -26,6 +26,8 @@
 
 #include "mesa-sha1.h"
 
+#ifdef HAVE_SHA1
+
 #if defined(HAVE_SHA1_IN_LIBMD)  /* Use libmd for SHA1 */ \
 	|| defined(HAVE_SHA1_IN_LIBC)   /* Use libc for SHA1 */
 
@@ -289,8 +291,6 @@ _mesa_sha1_final(struct mesa_sha1 *ctx, unsigned char result[20])
 }
 
 #endif
-
-#ifdef HAVE_SHA1
 
 void
 _mesa_sha1_compute(const void *data, size_t size, unsigned char result[20])