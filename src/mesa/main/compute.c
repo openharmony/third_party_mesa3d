openharmony/third_party_mesa3d

commit 5beabf26ca166a4ce392b393b89789d18f9adb4a

mesa/cs: Add MESA_VERBOSE=api support in DispatchCompute*
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Kristian Høgsberg <krh@bitplanet.net>

src/mesa/main/compute.c
@@ -34,6 +34,10 @@ _mesa_DispatchCompute(GLuint num_groups_x,
    GET_CURRENT_CONTEXT(ctx);
    const GLuint num_groups[3] = { num_groups_x, num_groups_y, num_groups_z };
 
+   if (MESA_VERBOSE & VERBOSE_API)
+      _mesa_debug(ctx, "glDispatchCompute(%d, %d, %d)\n",
+                  num_groups_x, num_groups_y, num_groups_z);
+
    if (!_mesa_validate_DispatchCompute(ctx, num_groups))
       return;
 
@@ -44,6 +48,9 @@ extern void GLAPIENTRY
 _mesa_DispatchComputeIndirect(GLintptr indirect)
 {
    GET_CURRENT_CONTEXT(ctx);
+
+   if (MESA_VERBOSE & VERBOSE_API)
+      _mesa_debug(ctx, "glDispatchComputeIndirect(%d)\n", indirect);
 
    if (!_mesa_validate_DispatchComputeIndirect(ctx, indirect))
       return;

commit d0d2bb24351ebf4a7f03c6dc11a75e9bc9301ddc

mesa: Fix format specifier warning in mesa_DispatchComputeIndirect()
Commit 5beabf26ca166a4ce392b393b89789d18f9adb4a introduced an incorrect
format specifier that operates on GLintptr indirect within the function
_mesa_DispatchComputeIndirect().

This patch mitigates the introduced GCC warning:

src/mesa/main/compute.c: In function '_mesa_DispatchComputeIndirect':
src/mesa/main/compute.c:53:7: warning: format '%d' expects argument of type 'int', but argument 3 has type 'GLintptr' [-Wformat=]
       _mesa_debug(ctx, "glDispatchComputeIndirect(%d)\n", indirect);
           ^

v2: Amend for Boyan Ding <boyan.j.ding@gmail.com> feedback.

Signed-off-by: Rhys Kidd <rhyskidd@gmail.com>
Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>

src/mesa/main/compute.c
@@ -50,7 +50,7 @@ _mesa_DispatchComputeIndirect(GLintptr indirect)
    GET_CURRENT_CONTEXT(ctx);
 
    if (MESA_VERBOSE & VERBOSE_API)
-      _mesa_debug(ctx, "glDispatchComputeIndirect(%d)\n", indirect);
+      _mesa_debug(ctx, "glDispatchComputeIndirect(%ld)\n", (long) indirect);
 
    if (!_mesa_validate_DispatchComputeIndirect(ctx, indirect))
       return;