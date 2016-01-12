openharmony/third_party_mesa3d

commit 23fd0c8533c9cb6921f94af92c8e36a2e899de3e

nir/spirv: Allow creating local/global variables from interface types
Not sure if this is actually allowed, but it's not that hard to just strip
the interface information from the type.

src/glsl/nir/spirv/spirv_to_nir.c
@@ -1740,9 +1740,11 @@ vtn_handle_variables(struct vtn_builder *b, SpvOp opcode,
          break;
       case SpvStorageClassPrivate:
          var->data.mode = nir_var_global;
+         var->interface_type = NULL;
          break;
       case SpvStorageClassFunction:
          var->data.mode = nir_var_local;
+         var->interface_type = NULL;
          break;
       case SpvStorageClassWorkgroup:
       case SpvStorageClassCrossWorkgroup:
@@ -1798,7 +1800,7 @@ vtn_handle_variables(struct vtn_builder *b, SpvOp opcode,
       /* Interface block variables aren't actually going to be referenced
        * by the generated NIR, so we don't put them in the list
        */
-      if (interface_type && glsl_type_is_struct(interface_type->type))
+      if (var->interface_type)
          break;
 
       if (var->data.mode == nir_var_local) {

commit 777bfbdee04b2c296063b548f269404fe81fee05

nir/spirv: Allow non-block variables with interface types in lists
The original objective was to disallow UBO and SSBO variables from the
variable lists.  This was accidentally broken in 23fd0c853 when fixing some
other interface issues.

src/glsl/nir/spirv/spirv_to_nir.c
@@ -1816,7 +1816,7 @@ vtn_handle_variables(struct vtn_builder *b, SpvOp opcode,
       /* Interface block variables aren't actually going to be referenced
        * by the generated NIR, so we don't put them in the list
        */
-      if (var->interface_type)
+      if (var->interface_type && glsl_type_is_struct(var->interface_type))
          break;
 
       if (var->data.mode == nir_var_local) {