ifzz/mesa

nir: add support for d2f and f2d

src/glsl/nir/nir_opcodes.py
@@ -171,6 +171,9 @@ unop_convert("i2b", tint32, tbool, "src0 != 0")
 unop_convert("b2i", tbool, tint32, "src0 ? 1 : 0") # Boolean-to-int conversion
 unop_convert("u2f", tuint32, tfloat, "src0") #Unsigned-to-float conversion.
 
+unop_convert("d2f", tfloat64, tfloat32, "src0") # Single to double precision
+unop_convert("f2d", tfloat32, tfloat64, "src0") # Double to single precision
+
 unop_reduce("bany", 1, tbool, tbool, "{src}", "{src0} || {src1}", "{src}")
 unop_reduce("ball", 1, tbool, tbool, "{src}", "{src0} && {src1}", "{src}")
 unop_reduce("fany", 1, tfloat32, tfloat32, "{src} != 0.0f", "{src0} || {src1}",

openharmony/third_party_mesa3d

Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

src/compiler/nir/nir_opcodes.py
@@ -170,6 +170,9 @@ unop_convert("b2f", tfloat32, tbool, "src0 ? 1.0f : 0.0f")
 unop_convert("i2b", tbool, tint32, "src0 != 0")
 unop_convert("b2i", tint32, tbool, "src0 ? 1 : 0") # Boolean-to-int conversion
 unop_convert("u2f", tfloat32, tuint32, "src0") # Unsigned-to-float conversion.
+# double-to-float conversion
+unop_convert("d2f", tfloat32, tfloat64, "src0") # Single to double precision
+unop_convert("f2d", tfloat64, tfloat32, "src0") # Double to single precision
 
 # Unary floating-point rounding operations.
 