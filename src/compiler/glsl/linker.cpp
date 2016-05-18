openharmony/third_party_mesa3d

commit f364612424d4a8c238f633e834545be14a111669

glsl/linker: Include the interface name for input and output blocks
On my oes_shader_io_blocks branch, this fixes 71
dEQP-GLES31.functional.program_interface_query.* tests.

Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Cc: mesa-stable@lists.freedesktop.org

src/compiler/glsl/linker.cpp
@@ -3654,6 +3654,21 @@ add_shader_variable(struct gl_shader_program *shProg, unsigned stage_mask,
    }
 
    default: {
+      /* Issue #16 of the ARB_program_interface_query spec says:
+       *
+       * "* If a variable is a member of an interface block without an
+       *    instance name, it is enumerated using just the variable name.
+       *
+       *  * If a variable is a member of an interface block with an instance
+       *    name, it is enumerated as "BlockName.Member", where "BlockName" is
+       *    the name of the interface block (not the instance name) and
+       *    "Member" is the name of the variable."
+       */
+      const char *prefixed_name = var->data.from_named_ifc_block
+         ? ralloc_asprintf(shProg, "%s.%s", var->get_interface_type()->name,
+                           name)
+         : name;
+
       /* The ARB_program_interface_query spec says:
        *
        *     "For an active variable declared as a single instance of a basic
@@ -3661,7 +3676,7 @@ add_shader_variable(struct gl_shader_program *shProg, unsigned stage_mask,
        *     from the shader source."
        */
       gl_shader_variable *sha_v =
-         create_shader_variable(shProg, var, name, type,
+         create_shader_variable(shProg, var, prefixed_name, type,
                                 use_implicit_location, location);
       if (!sha_v)
          return false;

commit 1e7904e5e630fdb0b68ffe9f85acbd1514d89ea3

glsl/linker: Don't include interface name for built-in blocks
Commit f364612 introduced a regression in some piglit tests (e.g.,
arb_program_interface_query-resource-query).  I did not notice this
regression because other (unrelated) problems caused failed assertions
in those same tests on my system... so they crashed before getting to
the new failure.

v2: Use is_gl_identifier.  Suggested by Tim.

Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
Reviewed-by: Timothy Arceri <timothy.arceri@collabora.com>
Cc: mesa-stable@lists.freedesktop.org

src/compiler/glsl/linker.cpp
@@ -3664,7 +3664,8 @@ add_shader_variable(struct gl_shader_program *shProg, unsigned stage_mask,
        *    the name of the interface block (not the instance name) and
        *    "Member" is the name of the variable."
        */
-      const char *prefixed_name = var->data.from_named_ifc_block
+      const char *prefixed_name = (var->data.from_named_ifc_block &&
+                                   !is_gl_identifier(var->name))
          ? ralloc_asprintf(shProg, "%s.%s", var->get_interface_type()->name,
                            name)
          : name;