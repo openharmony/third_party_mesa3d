openharmony/third_party_mesa3d

commit 25a0f3c05623ce6847ddd8458894a20b0fa88ed1

util: s/unsigned/enum pipe_resource_usage/ for buffer usage variables
Reviewed-by: Marek Olšák <marek.olsak@amd.com>

Reviewed-by: Roland Scheidegger <sroland@vmware.com>

src/gallium/auxiliary/util/u_upload_mgr.h
@@ -48,7 +48,7 @@ struct pipe_resource;
  */
 struct u_upload_mgr *
 u_upload_create(struct pipe_context *pipe, unsigned default_size,
-                unsigned bind, unsigned usage);
+                unsigned bind, enum pipe_resource_usage usage);
 
 /**
  * Destroy the upload manager.

src/gallium/auxiliary/util/u_debug.c
@@ -550,7 +550,7 @@ debug_print_bind_flags(const char *msg, unsigned usage)
  * Print PIPE_USAGE_x enum values with a message.
  */
 void
-debug_print_usage_enum(const char *msg, unsigned usage)
+debug_print_usage_enum(const char *msg, enum pipe_resource_usage usage)
 {
    static const struct debug_named_value names[] = {
       DEBUG_NAMED_VALUE(PIPE_USAGE_DEFAULT),

src/gallium/auxiliary/util/u_debug.h
@@ -473,7 +473,7 @@ void
 debug_print_bind_flags(const char *msg, unsigned usage);
 
 void
-debug_print_usage_enum(const char *msg, unsigned usage);
+debug_print_usage_enum(const char *msg, enum pipe_resource_usage usage);
 
 
 #ifdef	__cplusplus

src/gallium/auxiliary/util/u_inlines.h
@@ -230,12 +230,12 @@ pipe_surface_equal(struct pipe_surface *s1, struct pipe_surface *s2)
 /**
  * Create a new resource.
  * \param bind  bitmask of PIPE_BIND_x flags
- * \param usage  bitmask of PIPE_USAGE_x flags
+ * \param usage  a PIPE_USAGE_x value
  */
 static inline struct pipe_resource *
 pipe_buffer_create( struct pipe_screen *screen,
 		    unsigned bind,
-		    unsigned usage,
+		    enum pipe_resource_usage usage,
 		    unsigned size )
 {
    struct pipe_resource buffer;
@@ -395,7 +395,7 @@ pipe_buffer_write_nooverlap(struct pipe_context *pipe,
 static inline struct pipe_resource *
 pipe_buffer_create_with_data(struct pipe_context *pipe,
                              unsigned bind,
-                             unsigned usage,
+                             enum pipe_resource_usage usage,
                              unsigned size,
                              const void *ptr)
 {

src/gallium/auxiliary/util/u_staging.c
@@ -56,7 +56,7 @@ util_staging_resource_template(struct pipe_resource *pt, unsigned width,
 struct util_staging_transfer *
 util_staging_transfer_init(struct pipe_context *pipe,
                            struct pipe_resource *pt,
-                           unsigned level, unsigned usage,
+                           unsigned level, enum pipe_resource_usage usage,
                            const struct pipe_box *box,
                            boolean direct, struct util_staging_transfer *tx)
 {

src/gallium/auxiliary/util/u_staging.h
@@ -56,7 +56,7 @@ struct util_staging_transfer {
 struct util_staging_transfer *
 util_staging_transfer_init(struct pipe_context *pipe,
                            struct pipe_resource *pt,
-                           unsigned level, unsigned usage,
+                           unsigned level, enum pipe_resource_usage usage,
                            const struct pipe_box *box,
                            boolean direct, struct util_staging_transfer *tx);
 

src/gallium/auxiliary/util/u_suballoc.c
@@ -43,7 +43,7 @@ struct u_suballocator {
    unsigned size;          /* Size of the whole buffer, in bytes. */
    unsigned alignment;     /* Alignment of each sub-allocation. */
    unsigned bind;          /* Bitmask of PIPE_BIND_* flags. */
-   unsigned usage;         /* One of PIPE_USAGE_* flags. */
+   enum pipe_resource_usage usage;
    boolean zero_buffer_memory; /* If the buffer contents should be zeroed. */
 
    struct pipe_resource *buffer;   /* The buffer we suballocate from. */
@@ -59,7 +59,8 @@ struct u_suballocator {
  */
 struct u_suballocator *
 u_suballocator_create(struct pipe_context *pipe, unsigned size,
-                      unsigned alignment, unsigned bind, unsigned usage,
+                      unsigned alignment, unsigned bind,
+                      enum pipe_resource_usage usage,
 		      boolean zero_buffer_memory)
 {
    struct u_suballocator *allocator = CALLOC_STRUCT(u_suballocator);

src/gallium/auxiliary/util/u_suballoc.h
@@ -35,7 +35,8 @@ struct u_suballocator;
 
 struct u_suballocator *
 u_suballocator_create(struct pipe_context *pipe, unsigned size,
-                      unsigned alignment, unsigned bind, unsigned usage,
+                      unsigned alignment, unsigned bind,
+                      enum pipe_resource_usage usage,
 		      boolean zero_buffer_memory);
 
 void

commit 355d22c8d6ff32d4867cf62e282b2621b5982fcd

gallium/util: fix build break
Missing #include caused build breaks after 25a0f3c05.

Signed-off-by: Rob Clark <robclark@freedesktop.org>

src/gallium/auxiliary/util/u_upload_mgr.h
@@ -33,6 +33,7 @@
 #define U_UPLOAD_MGR_H
 
 #include "pipe/p_compiler.h"
+#include "pipe/p_defines.h"
 
 struct pipe_context;
 struct pipe_resource;