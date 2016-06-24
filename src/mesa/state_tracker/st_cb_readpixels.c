openharmony/third_party_mesa3d

commit 9d89b0520511256d37046f9dff6593e9b86e432e

st/mesa: use a single memcpy in st_ReadPixels when possible
This avoids costly address recomputations, function overhead, and may trigger
large copy optimizations.

Reviewed-by: Brian Paul <brianp@vmware.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>

src/mesa/state_tracker/st_cb_readpixels.c
@@ -520,14 +520,21 @@ st_ReadPixels(struct gl_context *ctx, GLint x, GLint y,
    /* memcpy data into a user buffer */
    {
       const uint bytesPerRow = width * util_format_get_blocksize(dst_format);
-      GLuint row;
+      const uint destStride = _mesa_image_row_stride(pack, width, format, type);
+      char *dest = _mesa_image_address2d(pack, pixels,
+                                         width, height, format,
+                                         type, 0, 0);
 
-      for (row = 0; row < (unsigned) height; row++) {
-         void *dest = _mesa_image_address2d(pack, pixels,
-                                              width, height, format,
-                                              type, row, 0);
-         memcpy(dest, map, bytesPerRow);
-         map += tex_xfer->stride;
+      if (tex_xfer->stride == bytesPerRow && destStride == bytesPerRow) {
+         memcpy(dest, map, bytesPerRow * height);
+      } else {
+         GLuint row;
+
+         for (row = 0; row < (unsigned) height; row++) {
+            memcpy(dest, map, bytesPerRow);
+            map += tex_xfer->stride;
+            dest += destStride;
+         }
       }
    }
 

commit 7f6c4c9b7c172b9fdedddbaf425c067c31574740

st/mesa: fix readpixels regression with MESA_pack_invert
Fixes an error introduced in commit 9d89b0520511256d37046f9dff6593e9b86e432e.

Reported-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Brian Paul <brianp@vmware.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>

src/mesa/state_tracker/st_cb_readpixels.c
@@ -520,7 +520,7 @@ st_ReadPixels(struct gl_context *ctx, GLint x, GLint y,
    /* memcpy data into a user buffer */
    {
       const uint bytesPerRow = width * util_format_get_blocksize(dst_format);
-      const uint destStride = _mesa_image_row_stride(pack, width, format, type);
+      const int destStride = _mesa_image_row_stride(pack, width, format, type);
       char *dest = _mesa_image_address2d(pack, pixels,
                                          width, height, format,
                                          type, 0, 0);