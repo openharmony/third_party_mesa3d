openharmony/third_party_mesa3d

commit 8280168efa1e77f741696978941b984ecb67d812

radeonsi: catch a potential state tracker error with non-MSAA FBs
At least st/mesa ensures this, so I'd rather not handle deviations in radeonsi.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>

src/gallium/drivers/radeonsi/si_state.c
@@ -3193,6 +3193,12 @@ static void si_emit_sample_mask(struct si_context *sctx, struct r600_atom *atom)
 	struct radeon_winsys_cs *cs = sctx->b.gfx.cs;
 	unsigned mask = sctx->sample_mask.sample_mask;
 
+	/* Needed for line and polygon smoothing as well as for the Polaris
+	 * small primitive filter. We expect the state tracker to take care of
+	 * this for us.
+	 */
+	assert(mask == 0xffff || sctx->framebuffer.nr_samples > 1);
+
 	radeon_set_context_reg_seq(cs, R_028C38_PA_SC_AA_MASK_X0Y0_X1Y0, 2);
 	radeon_emit(cs, mask | (mask << 16));
 	radeon_emit(cs, mask | (mask << 16));

commit d6e7621f247527c3afcefa8aa6a083c84787fd53

radeonsi: fix bad assertion in si_emit_sample_mask
The blitter sets mask == 1, which is fine since it doesn't use smoothing.
Fixes a regression introduced in commit 8280168e.

Reviewed-by: Edward O'Callaghan <funfunctor@folklore1984.net>

src/gallium/drivers/radeonsi/si_state.c
@@ -3197,7 +3197,8 @@ static void si_emit_sample_mask(struct si_context *sctx, struct r600_atom *atom)
 	 * small primitive filter. We expect the state tracker to take care of
 	 * this for us.
 	 */
-	assert(mask == 0xffff || sctx->framebuffer.nr_samples > 1);
+	assert(mask == 0xffff || sctx->framebuffer.nr_samples > 1 ||
+	       (mask & 1 && sctx->blitter->running));
 
 	radeon_set_context_reg_seq(cs, R_028C38_PA_SC_AA_MASK_X0Y0_X1Y0, 2);
 	radeon_emit(cs, mask | (mask << 16));