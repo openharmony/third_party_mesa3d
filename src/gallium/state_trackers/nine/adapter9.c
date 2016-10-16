openharmony/third_party_mesa3d

commit 95b0d13be60e5e48557154cc24e308bf75ecb1e9

st/nine: Add additional error handling in CheckDeviceMultiSampleType
Return one supported quality level in error cases.
Return error on invalid multisample count.

Fixes failing wine tests.

Signed-off-by: Patrick Rudolph <siro@das-labor.org>
Reviewed-by: Axel Davy <axel.davy@ens.fr>

src/gallium/state_trackers/nine/adapter9.c
@@ -385,6 +385,12 @@ NineAdapter9_CheckDeviceMultiSampleType( struct NineAdapter9 *This,
         d3dformat_to_string(SurfaceFormat), Windowed, MultiSampleType,
         pQualityLevels);
 
+    if (pQualityLevels) {
+        /* In error cases return only 1 quality level supported */
+        *pQualityLevels = 1;
+    }
+    user_assert(MultiSampleType < D3DMULTISAMPLE_16_SAMPLES, D3DERR_INVALIDCALL);
+
     hr = NineAdapter9_GetScreen(This, DeviceType, &screen);
     if (FAILED(hr))
         return hr;

commit 62c99b9d0ff357e4eed13a6e92ee57753803a74f

st/nine: Fix multisample limit check
Fixes regression introduced by
95b0d13be60e5e48557154cc24e308bf75ecb1e9

The regression prevents some apps to start.

Signed-off-by: Axel Davy <axel.davy@ens.fr>

src/gallium/state_trackers/nine/adapter9.c
@@ -393,7 +393,7 @@ NineAdapter9_CheckDeviceMultiSampleType( struct NineAdapter9 *This,
         /* In error cases return only 1 quality level supported */
         *pQualityLevels = 1;
     }
-    user_assert(MultiSampleType < D3DMULTISAMPLE_16_SAMPLES, D3DERR_INVALIDCALL);
+    user_assert(MultiSampleType <= D3DMULTISAMPLE_16_SAMPLES, D3DERR_INVALIDCALL);
 
     hr = NineAdapter9_GetScreen(This, DeviceType, &screen);
     if (FAILED(hr))