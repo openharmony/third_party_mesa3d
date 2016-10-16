openharmony/third_party_mesa3d

commit 82ba8088146ed5d0b4d6c31723ee4547f5a9558d

egl/surfaceless: tweak surfaceless_add_configs_for_visuals()
Analogous to previous commit.

v2: Use correct comparison in loop conditional (Eric)
    Use valid C initializer (Gurchetan)

Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
Reviewed-by: Eric Engestrom <eric@engestrom.ch>

src/egl/drivers/dri2/platform_surfaceless.c
@@ -189,25 +189,26 @@ surfaceless_add_configs_for_visuals(_EGLDriver *drv, _EGLDisplay *dpy)
       { "RGB888",   { 0xff0000, 0xff00, 0xff, 0x0 } },
       { "RGB565",   { 0x00f800, 0x07e0, 0x1f, 0x0 } },
    };
+   unsigned int format_count[ARRAY_SIZE(visuals)] = { 0 };
    unsigned int count, i, j;
 
    count = 0;
-   for (i = 0; i < ARRAY_SIZE(visuals); i++) {
-      int format_count = 0;
-
-      for (j = 0; dri2_dpy->driver_configs[j]; j++) {
+   for (i = 0; i < dri2_dpy->driver_configs[i]; i++) {
+      for (j = 0; j < ARRAY_SIZE(visuals); j++) {
          struct dri2_egl_config *dri2_conf;
 
-         dri2_conf = dri2_add_config(dpy, dri2_dpy->driver_configs[j],
+         dri2_conf = dri2_add_config(dpy, dri2_dpy->driver_configs[i],
                count + 1, EGL_PBUFFER_BIT, NULL, visuals[i].rgba_masks);
 
          if (dri2_conf) {
             count++;
-            format_count++;
+            format_count[j]++;
          }
       }
+   }
 
-      if (!format_count) {
+   for (i = 0; i < ARRAY_SIZE(format_count); i++) {
+      if (!format_count[i]) {
          _eglLog(_EGL_DEBUG, "No DRI config supports native format %s",
                visuals[i].format_name);
       }

commit bd953ded610f5a67d94ee13f5f5779aba17d2e94

egl/surfaceless: use correct index when accesing the visual
i is used for the driver_configs, while j is for the visuals.

Fixes: 82ba8088146 ("egl/surfaceless: tweak surfaceless_add_configs_for_visuals()")
Reported-by: Chad Versace <chadversary@chromium.org>
Tested-by: Chad Versace <chadversary@chromium.org>
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
Reviewed-by: Eric Engestrom <eric@engestrom.ch>

src/egl/drivers/dri2/platform_surfaceless.c
@@ -198,7 +198,7 @@ surfaceless_add_configs_for_visuals(_EGLDriver *drv, _EGLDisplay *dpy)
          struct dri2_egl_config *dri2_conf;
 
          dri2_conf = dri2_add_config(dpy, dri2_dpy->driver_configs[i],
-               count + 1, EGL_PBUFFER_BIT, NULL, visuals[i].rgba_masks);
+               count + 1, EGL_PBUFFER_BIT, NULL, visuals[j].rgba_masks);
 
          if (dri2_conf) {
             count++;