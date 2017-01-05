openharmony/third_party_mesa3d

commit 5056b3b9672509b555f62cad31cefd7659991acc

st/va: error handling
handle the cases when vl_compositor_set_csc_matrix(),
vl_compositor_init_state() and vl_compositor_init() fail

Signed-off-by: Nayan Deshmukh <nayan26deshmukh@gmail.com>
Reviewed-by: Christian König <christian.koenig@amd.com>

src/gallium/state_trackers/va/context.c
@@ -155,11 +155,14 @@ VA_DRIVER_INIT_FUNC(VADriverContextP ctx)
    if (!drv->htab)
       goto error_htab;
 
-   vl_compositor_init(&drv->compositor, drv->pipe);
-   vl_compositor_init_state(&drv->cstate, drv->pipe);
+   if (!vl_compositor_init(&drv->compositor, drv->pipe))
+      goto error_compositor;
+   if (!vl_compositor_init_state(&drv->cstate, drv->pipe))
+      goto error_compositor_state;
 
    vl_csc_get_matrix(VL_CSC_COLOR_STANDARD_BT_601, NULL, true, &drv->csc);
-   vl_compositor_set_csc_matrix(&drv->cstate, (const vl_csc_matrix *)&drv->csc, 1.0f, 0.0f);
+   if (!vl_compositor_set_csc_matrix(&drv->cstate, (const vl_csc_matrix *)&drv->csc, 1.0f, 0.0f))
+      goto error_csc_matrix;
    pipe_mutex_init(drv->mutex);
 
    ctx->pDriverData = (void *)drv;
@@ -176,6 +179,15 @@ VA_DRIVER_INIT_FUNC(VADriverContextP ctx)
    ctx->str_vendor = "mesa gallium vaapi";
 
    return VA_STATUS_SUCCESS;
+
+error_csc_matrix:
+   vl_compositor_cleanup_state(&drv->cstate);
+
+error_compositor_state:
+   vl_compositor_cleanup(&drv->cstate);
+
+error_compositor:
+   handle_table_destroy(drv->htab);
 
 error_htab:
    drv->pipe->destroy(drv->pipe);

commit 1d364a9f7fba5620bf0aa17b1080ef49531ae817

st/va: fix incorrect argument in vl_compositor_cleanup
This fixes the mistake introduced in commit
5056b3b9672509b555f62cad31cefd7659991acc

Signed-off-by: Nayan Deshmukh <nayan26deshmukh@gmail.com>
Reviewed-by: Christian König <christian.koenig@amd.com>

src/gallium/state_trackers/va/context.c
@@ -184,7 +184,7 @@ error_csc_matrix:
    vl_compositor_cleanup_state(&drv->cstate);
 
 error_compositor_state:
-   vl_compositor_cleanup(&drv->cstate);
+   vl_compositor_cleanup(&drv->compositor);
 
 error_compositor:
    handle_table_destroy(drv->htab);