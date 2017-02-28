openharmony/third_party_mesa3d

commit eac6451bd14bc8a811a04f7ad1e0bcce05ce90fa

vulkan/util: Add generator for enum_to_str functions
This adds a python generator to produce enum_to_str functions for
Vulkan from the vk.xml API description. It supports extensions as well
as core API features, and the generator works with both python2 and
python3.

Signed-off-by: Dylan Baker <dylanx.c.baker@intel.com>
Acked-by: Matt Turner <mattst88@gmail.com>
Acked-by: Jason Ekstrand <jason@jlekstrand.net>

src/vulkan/util/gen_enum_to_str.py
@@ -1,0 +1,172 @@
+# encoding=utf-8
+# Copyright © 2017 Intel Corporation
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Create enum to string functions for vulking using vk.xml."""
+
+from __future__ import print_function
+import os
+import textwrap
+import xml.etree.cElementTree as et
+
+from mako.template import Template
+
+VK_XML = os.path.join(os.path.dirname(__file__), '..', 'registry', 'vk.xml')
+
+COPYRIGHT = textwrap.dedent(u"""\
+    * Copyright © 2017 Intel Corporation
+    *
+    * Permission is hereby granted, free of charge, to any person obtaining a copy
+    * of this software and associated documentation files (the "Software"), to deal
+    * in the Software without restriction, including without limitation the rights
+    * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    * copies of the Software, and to permit persons to whom the Software is
+    * furnished to do so, subject to the following conditions:
+    *
+    * The above copyright notice and this permission notice shall be included in
+    * all copies or substantial portions of the Software.
+    *
+    * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    * SOFTWARE.""")
+
+C_TEMPLATE = Template(textwrap.dedent(u"""\
+    /* Autogenerated file -- do not edit
+     * generated by ${file}
+     *
+     ${copyright}
+     */
+
+    #include <vulkan/vulkan.h>
+    #include "util/macros.h"
+    #include "vk_enum_to_str.h"
+
+    % for enum in enums:
+
+        const char *
+        vk_${enum.name[2:]}_to_str(${enum.name} input)
+        {
+            switch(input) {
+            % for v in enum.values:
+                case ${v}:
+                    return "${v}";
+            % endfor
+            default:
+                unreachable("Undefined enum value.");
+            }
+        }
+    %endfor"""),
+    output_encoding='utf-8')
+
+H_TEMPLATE = Template(textwrap.dedent(u"""\
+    /* Autogenerated file -- do not edit
+     * generated by ${file}
+     *
+     ${copyright}
+     */
+
+    #ifndef MESA_VK_ENUM_TO_STR_H
+    #define MESA_VK_ENUM_TO_STR_H
+
+    #include <vulkan/vulkan.h>
+
+    % for enum in enums:
+        const char * vk_${enum.name[2:]}_to_str(${enum.name} input);
+    % endfor
+
+    #endif"""),
+    output_encoding='utf-8')
+
+
+class EnumFactory(object):
+    """Factory for creating enums."""
+
+    def __init__(self, type_):
+        self.registry = {}
+        self.type = type_
+
+    def __call__(self, name):
+        try:
+            return self.registry[name]
+        except KeyError:
+            n = self.registry[name] = self.type(name)
+        return n
+
+
+class VkEnum(object):
+    """Simple struct-like class representing a single Vulkan Enum."""
+
+    def __init__(self, name, values=None):
+        self.name = name
+        self.values = values or []
+
+
+def xml_parser(filename):
+    """Parse the XML file and return parsed data.
+
+    This parser is a memory efficient iterative XML parser that returns a list
+    of VkEnum objects.
+    """
+    efactory = EnumFactory(VkEnum)
+
+    with open(filename, 'rb') as f:
+        context = iter(et.iterparse(f, events=('start', 'end')))
+
+        # This gives the root element, since goal is to iterate over the
+        # elements without building a tree, this allows the root to be cleared
+        # (erase the elements) after the children have been processed.
+        _, root = next(context)
+
+        for event, elem in context:
+            if event == 'end' and elem.tag == 'enums':
+                type_ = elem.attrib.get('type')
+                if type_ == 'enum':
+                    enum = efactory(elem.attrib['name'])
+                    enum.values.extend([e.attrib['name'] for e in elem
+                                        if e.tag == 'enum'])
+            elif event == 'end' and elem.tag == 'extension':
+                if elem.attrib['supported'] != 'vulkan':
+                    continue
+                for e in elem.findall('.//enum[@extends][@offset]'):
+                    enum = efactory(e.attrib['extends'])
+                    enum.values.append(e.attrib['name'])
+
+            root.clear()
+
+    return efactory.registry.values()
+
+
+def main():
+    enums = xml_parser(VK_XML)
+    for template, file_ in [(C_TEMPLATE, 'vk_enum_to_str.c'),
+                            (H_TEMPLATE, 'vk_enum_to_str.h')]:
+        with open(file_, 'wb') as f:
+            f.write(template.render(
+                file=os.path.basename(__file__),
+                enums=enums,
+                copyright=COPYRIGHT))
+
+
+if __name__ == '__main__':
+    main()

src/Makefile.am
@@ -117,7 +117,7 @@ SUBDIRS += intel/tools
 endif
 
 if HAVE_VULKAN_COMMON
-SUBDIRS += vulkan/wsi
+SUBDIRS += vulkan/util vulkan/wsi
 endif
 EXTRA_DIST += vulkan/registry/vk.xml
 

src/intel/vulkan/Makefile.am
@@ -49,6 +49,7 @@ AM_CPPFLAGS = \
 	-I$(top_builddir)/src \
 	-I$(top_srcdir)/src \
 	-I$(top_srcdir)/src/vulkan/wsi \
+	-I$(top_builddir)/src/vulkan/util \
 	-I$(top_builddir)/src/compiler \
 	-I$(top_srcdir)/src/compiler \
 	-I$(top_builddir)/src/compiler/nir \
@@ -126,6 +127,7 @@ libvulkan_common_la_SOURCES = $(VULKAN_SOURCES)
 
 VULKAN_LIB_DEPS += \
 	libvulkan_common.la \
+	$(top_builddir)/src/vulkan/util/libvulkan_util.la \
 	$(top_builddir)/src/vulkan/wsi/libvulkan_wsi.la \
 	$(top_builddir)/src/mesa/drivers/dri/i965/libi965_compiler.la \
 	$(top_builddir)/src/compiler/nir/libnir.la \

src/vulkan/util/Makefile.am
@@ -1,0 +1,22 @@
+vulkan_api_xml = $(top_srcdir)/src/vulkan/registry/vk.xml
+
+AM_CPPFLAGS = \
+	-I$(top_srcdir)/include \
+	-I$(top_srcdir)/src
+
+EXTRA_DIST = \
+	gen_enum_to_str.py
+
+BUILT_SOURCES = \
+	vk_enum_to_str.c \
+	vk_enum_to_str.h
+
+vk_enum_to_str.c vk_enum_to_str.h: gen_enum_to_str.py $(vulkan_api_xml)
+	$(AM_V_GEN)$(PYTHON2) $(srcdir)/gen_enum_to_str.py
+
+noinst_LTLIBRARIES = libvulkan_util.la
+
+libvulkan_util_la_SOURCES = \
+	vk_enum_to_str.c \
+	vk_enum_to_str.h
+

configure.ac
@@ -2691,6 +2691,7 @@ AC_CONFIG_FILES([Makefile
 		src/mesa/main/tests/Makefile
 		src/util/Makefile
 		src/util/tests/hash_table/Makefile
+		src/vulkan/util/Makefile
 		src/vulkan/wsi/Makefile])
 
 AC_OUTPUT

src/intel/vulkan/anv_util.c
@@ -29,6 +29,7 @@
 #include <assert.h>
 
 #include "anv_private.h"
+#include "vk_enum_to_str.h"
 
 /** Log an error message.  */
 void anv_printflike(1, 2)
@@ -69,40 +70,7 @@ __vk_errorf(VkResult error, const char *file, int line, const char *format, ...)
    va_list ap;
    char buffer[256];
 
-#define ERROR_CASE(error) case error: error_str = #error; break;
-
-   const char *error_str;
-   switch ((int32_t)error) {
-
-   /* Core errors */
-   ERROR_CASE(VK_ERROR_OUT_OF_HOST_MEMORY)
-   ERROR_CASE(VK_ERROR_OUT_OF_DEVICE_MEMORY)
-   ERROR_CASE(VK_ERROR_INITIALIZATION_FAILED)
-   ERROR_CASE(VK_ERROR_DEVICE_LOST)
-   ERROR_CASE(VK_ERROR_MEMORY_MAP_FAILED)
-   ERROR_CASE(VK_ERROR_LAYER_NOT_PRESENT)
-   ERROR_CASE(VK_ERROR_EXTENSION_NOT_PRESENT)
-   ERROR_CASE(VK_ERROR_FEATURE_NOT_PRESENT)
-   ERROR_CASE(VK_ERROR_INCOMPATIBLE_DRIVER)
-   ERROR_CASE(VK_ERROR_TOO_MANY_OBJECTS)
-   ERROR_CASE(VK_ERROR_FORMAT_NOT_SUPPORTED)
-   ERROR_CASE(VK_ERROR_FRAGMENTED_POOL)
-
-   /* Extension errors */
-   ERROR_CASE(VK_ERROR_SURFACE_LOST_KHR)
-   ERROR_CASE(VK_ERROR_NATIVE_WINDOW_IN_USE_KHR)
-   ERROR_CASE(VK_ERROR_OUT_OF_DATE_KHR)
-   ERROR_CASE(VK_ERROR_INCOMPATIBLE_DISPLAY_KHR)
-   ERROR_CASE(VK_ERROR_VALIDATION_FAILED_EXT)
-   ERROR_CASE(VK_ERROR_INVALID_SHADER_NV)
-   ERROR_CASE(VK_ERROR_OUT_OF_POOL_MEMORY_KHR)
-
-   default:
-      assert(!"Unknown error");
-      error_str = "unknown error";
-   }
-
-#undef ERROR_CASE
+   const char *error_str = vk_Result_to_str(error);
 
    if (format) {
       va_start(ap, format);

src/vulkan/util/.gitignore
@@ -1,0 +1,1 @@
+vk_enum_to_str.*

commit 52dd902faf7f13d8ee94769ef2e633ea1e809c15

android: vulkan: add support for libmesa_vulkan_util
The following changes are implemented:

Add src/vulkan/Android.mk to build libmesa_vulkan_util
Android.mk: add src/vulkan to SUBDIR to build new module
intel/vulkan: fix libmesa_vulkan_util,vk_enum_to_str.h dependencies
Add -o OUTPUT_PATH option in src/vulkan/util/gen_enum_to_str.py script
Use -o OUTPUT_PATH option in automake generation rules for vk_enum_to_str.{c,h}

Fixes: eac6451 "vulkan/util: Add generator for enum_to_str functions"
Fixes: 2dcd7a2 "vulkan: Combine wsi and util makefiles"
Reviewed-by: Dylan Baker <dylan@pnwbakers.com>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

[Emil Velikov]
 - Move parser within main()
 - Use --outdir instead of -o
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

Android.mk
@@ -97,7 +97,8 @@ SUBDIRS := \
 	src/egl \
 	src/amd \
 	src/intel \
-	src/mesa/drivers/dri
+	src/mesa/drivers/dri \
+	src/vulkan
 
 INC_DIRS := $(call all-named-subdir-makefiles,$(SUBDIRS))
 

src/intel/vulkan/Android.mk
@@ -74,7 +74,8 @@ include $(BUILD_STATIC_LIBRARY)
 ANV_INCLUDES := \
 	$(VULKAN_COMMON_INCLUDES) \
 	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_anv_entrypoints,,) \
-	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_nir,,)/nir
+	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_nir,,)/nir \
+	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_vulkan_util,,)/util
 
 #
 # libanv for gen7
@@ -172,7 +173,10 @@ LOCAL_C_INCLUDES := \
 	$(ANV_INCLUDES) \
 	$(MESA_TOP)/src/compiler
 
-LOCAL_WHOLE_STATIC_LIBRARIES := libmesa_anv_entrypoints libmesa_genxml
+LOCAL_WHOLE_STATIC_LIBRARIES := \
+	libmesa_anv_entrypoints \
+	libmesa_genxml \
+	libmesa_vulkan_util
 
 LOCAL_GENERATED_SOURCES += $(intermediates)/anv_entrypoints.c
 

src/vulkan/Android.mk
@@ -1,0 +1,55 @@
+# Copyright © 2017 Mauro Rossi <issor.oruam@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice (including the next
+# paragraph) shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+LOCAL_PATH := $(call my-dir)
+
+# Import variables
+include $(LOCAL_PATH)/Makefile.sources
+
+#
+# libmesa_vulkan_util
+#
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmesa_vulkan_util
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+
+intermediates := $(call local-generated-sources-dir)
+
+LOCAL_C_INCLUDES := \
+	$(MESA_TOP)/include/vulkan
+
+LOCAL_GENERATED_SOURCES := \
+	$(intermediates)/util/vk_enum_to_str.c \
+	$(intermediates)/util/vk_enum_to_str.h
+
+vulkan_api_xml = $(MESA_TOP)/src/vulkan/registry/vk.xml
+
+$(LOCAL_GENERATED_SOURCES): $(MESA_TOP)/src/vulkan/util/gen_enum_to_str.py $(vulkan_api_xml)
+	@echo "target Generated: $(PRIVATE_MODULE) <= $(notdir $(@))"
+	@mkdir -p $(dir $@)
+	$(hide) $(MESA_PYTHON2) $(MESA_TOP)/src/vulkan/util/gen_enum_to_str.py --outdir $(intermediates)/util
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := \
+        $(intermediates)
+
+include $(MESA_COMMON_MK)
+include $(BUILD_STATIC_LIBRARY)

src/vulkan/Makefile.am
@@ -17,7 +17,7 @@ BUILT_SOURCES = \
 
 util/vk_enum_to_str.c util/vk_enum_to_str.h: util/gen_enum_to_str.py $(vulkan_api_xml)
 	$(MKDIR_GEN)
-	$(PYTHON_GEN) $(srcdir)/util/gen_enum_to_str.py
+	$(PYTHON_GEN) $(srcdir)/util/gen_enum_to_str.py --outdir $(top_builddir)/src/vulkan/util
 
 libvulkan_util_la_SOURCES = $(VULKAN_UTIL_FILES)
 

src/vulkan/util/gen_enum_to_str.py
@@ -22,6 +22,7 @@
 """Create enum to string functions for vulking using vk.xml."""
 
 from __future__ import print_function
+import argparse
 import os
 import textwrap
 import xml.etree.cElementTree as et
@@ -158,9 +159,16 @@ def xml_parser(filename):
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--outdir',
+                        help='Directory to put the generated files in',
+                        required=True)
+
+    args = parser.parse_args()
+
     enums = xml_parser(VK_XML)
-    for template, file_ in [(C_TEMPLATE, 'util/vk_enum_to_str.c'),
-                            (H_TEMPLATE, 'util/vk_enum_to_str.h')]:
+    for template, file_ in [(C_TEMPLATE, os.path.join(args.outdir, 'vk_enum_to_str.c')),
+                            (H_TEMPLATE, os.path.join(args.outdir, 'vk_enum_to_str.h'))]:
         with open(file_, 'wb') as f:
             f.write(template.render(
                 file=os.path.basename(__file__),