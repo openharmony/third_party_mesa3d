openharmony/third_party_mesa3d

commit 3d17ee3e18415871347f829208fae123d676b19e

util/disk_cache: support caches for multiple architectures
Previously we were deleting the entire cache if a user switched
between 32 and 64 bit applications.

V2: make the check more generic, it should now work with any
platform we are likely to support.

V3: Use suggestion from Emil to make even more generic/fix issue
with __ILP32__ not being declared on gcc for regular 32-bit builds.

Tested-by: Grazvydas Ignotas <notasas@gmail.com>
Tested-by: Dieter Nützel <Dieter@nuetzel-hh.de>

src/util/disk_cache.c
@@ -74,6 +74,23 @@ struct disk_cache {
    uint64_t max_size;
 };
 
+static const char *
+get_arch_bitness_str(void)
+{
+    if (sizeof(void *) == 4)
+#ifdef __ILP32__
+        return "ilp-32";
+#else
+        return "32";
+#endif
+    if (sizeof(void *) == 8)
+        return "64";
+
+    /* paranoia check which will be dropped by the optimiser */
+    assert(!"unknown_arch");
+    return "unknown_arch";
+}
+
 /* Create a directory named 'path' if it does not already exist.
  *
  * Returns: 0 if path already exists as a directory or if created.
@@ -175,6 +192,15 @@ create_mesa_cache_dir(void *mem_ctx, const char *path, const char *timestamp,
                       const char *gpu_name)
 {
    char *new_path = concatenate_and_mkdir(mem_ctx, path, "mesa");
+   if (new_path == NULL)
+      return NULL;
+
+   /* Create a parent architecture directory so that we don't remove cache
+    * files for other architectures. In theory we could share the cache
+    * between architectures but we have no way of knowing if they were created
+    * by a compatible Mesa version.
+    */
+   new_path = concatenate_and_mkdir(mem_ctx, new_path, get_arch_bitness_str());
    if (new_path == NULL)
       return NULL;
 

commit 82cbfc0c29110db1e1d0d708834c08ed17466c99

util/disk_cache: fix make check
Fixes make check after 3d17ee3e18415871347 which caused disk cache
to create an additional directory.

src/util/disk_cache.c
@@ -74,23 +74,6 @@ struct disk_cache {
    uint64_t max_size;
 };
 
-static const char *
-get_arch_bitness_str(void)
-{
-    if (sizeof(void *) == 4)
-#ifdef __ILP32__
-        return "ilp-32";
-#else
-        return "32";
-#endif
-    if (sizeof(void *) == 8)
-        return "64";
-
-    /* paranoia check which will be dropped by the optimiser */
-    assert(!"unknown_arch");
-    return "unknown_arch";
-}
-
 /* Create a directory named 'path' if it does not already exist.
  *
  * Returns: 0 if path already exists as a directory or if created.

src/util/disk_cache.h
@@ -27,6 +27,7 @@
 #ifdef ENABLE_SHADER_CACHE
 #include <dlfcn.h>
 #endif
+#include <assert.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <sys/stat.h>
@@ -41,6 +42,23 @@ extern "C" {
 typedef uint8_t cache_key[CACHE_KEY_SIZE];
 
 struct disk_cache;
+
+static inline const char *
+get_arch_bitness_str(void)
+{
+    if (sizeof(void *) == 4)
+#ifdef __ILP32__
+        return "ilp-32";
+#else
+        return "32";
+#endif
+    if (sizeof(void *) == 8)
+        return "64";
+
+    /* paranoia check which will be dropped by the optimiser */
+    assert(!"unknown_arch");
+    return "unknown_arch";
+}
 
 static inline bool
 disk_cache_get_function_timestamp(void *ptr, uint32_t* timestamp)

src/compiler/glsl/tests/cache_test.c
@@ -127,7 +127,7 @@ rmrf_local(const char *path)
 }
 
 static void
-check_timestamp_and_gpu_id_directories_created(const char *cache_dir)
+check_timestamp_and_gpu_id_directories_created(char *cache_dir)
 {
    bool sub_dirs_created = false;
 
@@ -180,13 +180,16 @@ test_disk_cache_create(void)
    expect_null(cache, "disk_cache_create with XDG_CACHE_HOME set with"
                "a non-existing parent directory");
 
+   /* Create string with expected directory hierarchy */
+   char expected_dir_h[255];
+   sprintf(expected_dir_h, "%s%s%s", CACHE_TEST_TMP "/xdg-cache-home/mesa/",
+           get_arch_bitness_str(), "/make_check/test");
+
    mkdir(CACHE_TEST_TMP, 0755);
    cache = disk_cache_create("test", "make_check");
    expect_non_null(cache, "disk_cache_create with XDG_CACHE_HOME set");
 
-   check_timestamp_and_gpu_id_directories_created(CACHE_TEST_TMP
-                                                  "/xdg-cache-home"
-                                                  "/mesa/make_check/test");
+   check_timestamp_and_gpu_id_directories_created(expected_dir_h);
 
    disk_cache_destroy(cache);
 
@@ -199,13 +202,15 @@ test_disk_cache_create(void)
    expect_null(cache, "disk_cache_create with MESA_GLSL_CACHE_DIR set with"
                "a non-existing parent directory");
 
+   sprintf(expected_dir_h, "%s%s%s", CACHE_TEST_TMP
+           "/mesa-glsl-cache-dir/mesa/", get_arch_bitness_str(),
+           "/make_check/test");
+
    mkdir(CACHE_TEST_TMP, 0755);
    cache = disk_cache_create("test", "make_check");
    expect_non_null(cache, "disk_cache_create with MESA_GLSL_CACHE_DIR set");
 
-   check_timestamp_and_gpu_id_directories_created(CACHE_TEST_TMP
-                                                  "/mesa-glsl-cache-dir"
-                                                  "/mesa/make_check/test");
+   check_timestamp_and_gpu_id_directories_created(expected_dir_h);
 
    disk_cache_destroy(cache);
 }