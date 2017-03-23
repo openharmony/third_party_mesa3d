openharmony/third_party_mesa3d

commit e032bfaf1b5546bf1f115a2ff92a7be4321b8f52

anv: Generate anv_entrypoints header and code in one command
This produces the header and the code in one command, saving the need to
call the same script twice, which parses the same XML file.

Signed-off-by: Dylan Baker <dylanx.c.baker@intel.com>

src/intel/Android.vulkan.mk
@@ -179,8 +179,11 @@ LOCAL_WHOLE_STATIC_LIBRARIES := \
 
 LOCAL_GENERATED_SOURCES += $(intermediates)/vulkan/anv_entrypoints.c
 
-$(intermediates)/vulkan/anv_entrypoints.c:
-	$(VK_ENTRYPOINTS_SCRIPT) code $@ --xml $(MESA_TOP)/src/vulkan/registry/vk.xml
+$(intermediates)/vulknan/anv_entrypoints.c:
+	$(VK_ENTRYPOINTS_SCRIPT) \
+		--xml $(MESA_TOP)/src/vulkan/registry/vk.xml \
+		--outdir $(intermediates)/vulkan
+$(intermediates)/vulkan/anv_entrypoints.h: $(intermediates)/vulkan/anv_entrypoints.c
 
 LOCAL_SHARED_LIBRARIES := libdrm_intel
 

src/intel/Makefile.vulkan.am
@@ -24,11 +24,10 @@
 # out and we'll fail at `make dist'
 vulkan_api_xml = $(top_srcdir)/src/vulkan/registry/vk.xml
 
-vulkan/anv_entrypoints.h : vulkan/anv_entrypoints_gen.py $(vulkan_api_xml)
-	$(AM_V_GEN)$(PYTHON2) $(srcdir)/vulkan/anv_entrypoints_gen.py header $@ --xml $(vulkan_api_xml)
-
-vulkan/anv_entrypoints.c : vulkan/anv_entrypoints_gen.py $(vulkan_api_xml)
-	$(AM_V_GEN)$(PYTHON2) $(srcdir)/vulkan/anv_entrypoints_gen.py code $@ --xml $(vulkan_api_xml)
+vulkan/anv_entrypoints.c: vulkan/anv_entrypoints_gen.py $(vulkan_api_xml)
+	$(AM_V_GEN)$(PYTHON2) $(srcdir)/vulkan/anv_entrypoints_gen.py \
+		--xml $(vulkan_api_xml) --outdir $(builddir)/vulkan
+vulkan/anv_entrypoints.h: vulkan/anv_entrypoints.c
 
 BUILT_SOURCES += $(VULKAN_GENERATED_FILES)
 CLEANFILES += \

src/intel/vulkan/anv_entrypoints_gen.py
@@ -354,10 +354,9 @@ def gen_code(entrypoints):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('target', choices=['header', 'code'],
-                        help='Which file to generate.')
-    parser.add_argument('file', help='Where to write the file.')
-    parser.add_argument('--xml', help='Vulkan API XML file.')
+    parser.add_argument('--outdir', help='Where to write the files.',
+                        required=True)
+    parser.add_argument('--xml', help='Vulkan API XML file.', required=True)
     args = parser.parse_args()
 
     doc = et.parse(args.xml)
@@ -375,13 +374,11 @@ def main():
 
     # For outputting entrypoints.h we generate a anv_EntryPoint() prototype
     # per entry point.
-    if args.target == 'header':
-        with open(args.file, 'wb') as f:
-            f.write(TEMPLATE_H.render(entrypoints=entrypoints,
-                                      filename=os.path.basename(__file__)))
-    else:
-        with open(args.file, 'wb') as f:
-            f.write(gen_code(entrypoints))
+    with open(os.path.join(args.outdir, 'anv_entrypoints.h'), 'wb') as f:
+        f.write(TEMPLATE_H.render(entrypoints=entrypoints,
+                                  filename=os.path.basename(__file__)))
+    with open(os.path.join(args.outdir, 'anv_entrypoints.c'), 'wb') as f:
+        f.write(gen_code(entrypoints))
 
 
 if __name__ == '__main__':

commit 6ccac5c7e0822ee63274fedb70d2586b2457f47c

android: fix vulkan build issues with anv_entrypoints
Patch fixes entrypoint generation for libmesa_anv_entrypoints that
still used old style of calling generator script.

Also small fixes to libmesa_vulkan_common where there was a typo
in target name (vulknan) and files were generated to wrong folder.

Fixes: e032bfaf ("anv: Generate anv_entrypoints header and code in one command")
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Acked-by: Emil Velikov <emil.velikov@collabora.com>

src/intel/Android.vulkan.mk
@@ -58,9 +58,10 @@ $(intermediates)/vulkan/dummy.c:
 	@echo "Gen Dummy: $(PRIVATE_MODULE) <= $(notdir $(@))"
 	$(hide) touch $@
 
-$(intermediates)/vulkan/anv_entrypoints.h:
-	$(VK_ENTRYPOINTS_SCRIPT) header $@ --xml $(MESA_TOP)/src/vulkan/registry/vk.xml
-
+$(intermediates)/vulkan/anv_entrypoints.h: $(intermediates)/vulkan/dummy.c
+	$(VK_ENTRYPOINTS_SCRIPT) \
+		--outdir $(dir $@) \
+		--xml $(MESA_TOP)/src/vulkan/registry/vk.xml
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := \
         $(intermediates)
@@ -177,13 +178,18 @@ LOCAL_WHOLE_STATIC_LIBRARIES := \
 	libmesa_genxml \
 	libmesa_vulkan_util
 
+# The rule generates both C and H files, but due to some strange
+# reason generating the files once leads to link-time issues.
+# Work around create them here as well - we're safe from race
+# conditions since they are stored in another location.
+
 LOCAL_GENERATED_SOURCES += $(intermediates)/vulkan/anv_entrypoints.c
 
-$(intermediates)/vulknan/anv_entrypoints.c:
+$(intermediates)/vulkan/anv_entrypoints.c:
+	@mkdir -p $(dir $@)
 	$(VK_ENTRYPOINTS_SCRIPT) \
 		--xml $(MESA_TOP)/src/vulkan/registry/vk.xml \
-		--outdir $(intermediates)/vulkan
-$(intermediates)/vulkan/anv_entrypoints.h: $(intermediates)/vulkan/anv_entrypoints.c
+		--outdir $(dir $@)
 
 LOCAL_SHARED_LIBRARIES := libdrm_intel
 