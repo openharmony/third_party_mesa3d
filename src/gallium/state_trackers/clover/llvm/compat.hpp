openharmony/third_party_mesa3d

commit 72f464836f0d6f4cd9025ba105affb59fce73087

clover: Fix build against clang SVN >= r299965
clang::LangAS::Offset is gone, the behaviour is as if it was 0.

v2: Introduce and use clover::llvm::compat::lang_as_offset (Francisco
    Jerez)

Reviewed-by: Francisco Jerez <currojerez@riseup.net>

src/gallium/state_trackers/clover/llvm/compat.hpp
@@ -67,6 +67,12 @@ namespace clover {
          typedef ::llvm::TargetLibraryInfo target_library_info;
 #endif
 
+#if HAVE_LLVM >= 0x0500
+         const auto lang_as_offset = 0;
+#else
+         const auto lang_as_offset = clang::LangAS::Offset;
+#endif
+
          inline void
          set_lang_defaults(clang::CompilerInvocation &inv,
                            clang::LangOptions &lopts, clang::InputKind ik,

src/gallium/state_trackers/clover/llvm/codegen/common.cpp
@@ -129,7 +129,7 @@ namespace {
                   cast<::llvm::PointerType>(actual_type)->getAddressSpace();
 
                if (address_space == address_spaces[clang::LangAS::opencl_local
-                                                   - clang::LangAS::Offset]) {
+                                                   - compat::lang_as_offset]) {
                   args.emplace_back(module::argument::local, arg_api_size,
                                     target_size, target_align,
                                     module::argument::zero_ext);

commit 4ee1e14a38537c1a945c79e4d44c3028d9cff006

clover: Add missing include to compat header
Fixes build failure with LLVM 4

Fixes: 72f464836f0d6f4cd9025ba105affb59fce73087
	(clover: Fix build against clang SVN >= r299965)

Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
Reviewed-by: Francisco Jerez <currojerez@riseup.net>

src/gallium/state_trackers/clover/llvm/compat.hpp
@@ -55,6 +55,7 @@
 #include <llvm/Support/FormattedStream.h>
 #endif
 
+#include <clang/Basic/TargetInfo.h>
 #include <clang/Frontend/CodeGenOptions.h>
 #include <clang/Frontend/CompilerInstance.h>
 