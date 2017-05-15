openharmony/third_party_mesa3d

commit c3ce9f9e1ed84ebc557d14e99508366d6dba9211

egl: simplify the Android logger
Drop the unsupported pre-JellyBean macros and use a simple egl2android
mapping. With this we loose the explicit abort() provided by LOG_FATAL,
although Mesa already already calls exit(1) in case of a fatal errors.

Suggested-by: Rob Herring <robh@kernel.org>
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
Reviewed-by: Rob Herring <robh@kernel.org>

src/egl/main/egllog.c
@@ -49,17 +49,6 @@
 #define LOG_TAG "EGL-MAIN"
 #include <cutils/log.h>
 
-/* support versions < JellyBean */
-#ifndef ALOGW
-#define ALOGW LOGW
-#endif
-#ifndef ALOGD
-#define ALOGD LOGD
-#endif
-#ifndef ALOGI
-#define ALOGI LOGI
-#endif
-
 #endif /* HAVE_ANDROID_PLATFORM */
 
 #define MAXSTRING 1000
@@ -92,22 +81,13 @@ static void
 _eglDefaultLogger(EGLint level, const char *msg)
 {
 #ifdef HAVE_ANDROID_PLATFORM
-   switch (level) {
-   case _EGL_DEBUG:
-      ALOGD("%s", msg);
-      break;
-   case _EGL_INFO:
-      ALOGI("%s", msg);
-      break;
-   case _EGL_WARNING:
-      ALOGW("%s", msg);
-      break;
-   case _EGL_FATAL:
-      LOG_FATAL("%s", msg);
-      break;
-   default:
-      break;
-   }
+   static const int egl2alog[] = {
+      [_EGL_FATAL] = LOG_ERROR,
+      [_EGL_WARNING]  = LOG_WARN,
+      [_EGL_INFO] = LOG_INFO,
+      [_EGL_DEBUG] = LOG_DEBUG,
+   };
+   ALOG(egl2alog[level], LOG_TAG, "%s", msg);
 #else
    fprintf(stderr, "libEGL %s: %s\n", level_strings[level], msg);
 #endif /* HAVE_ANDROID_PLATFORM */

commit d894083b1239922100cf34bde69bfeedaec1db9d

egl: fix android logger compilation
c3ce9f9 broken compilation since LOG_ERROR is not defined and also
macro expansion won't work as planned (expands to 'ANDROID_egl2alog[level]')

v2: append 'ANDROID' to egl2alog table and use LOG_PRI
    (suggested by Chih-Wei Huang)

Fixes: c3ce9f9 ("egl: simplify the Android logger")
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

src/egl/main/egllog.c
@@ -82,12 +82,12 @@ _eglDefaultLogger(EGLint level, const char *msg)
 {
 #ifdef HAVE_ANDROID_PLATFORM
    static const int egl2alog[] = {
-      [_EGL_FATAL] = LOG_ERROR,
-      [_EGL_WARNING]  = LOG_WARN,
-      [_EGL_INFO] = LOG_INFO,
-      [_EGL_DEBUG] = LOG_DEBUG,
+      [_EGL_FATAL] = ANDROID_LOG_ERROR,
+      [_EGL_WARNING]  = ANDROID_LOG_WARN,
+      [_EGL_INFO] = ANDROID_LOG_INFO,
+      [_EGL_DEBUG] = ANDROID_LOG_DEBUG,
    };
-   ALOG(egl2alog[level], LOG_TAG, "%s", msg);
+   LOG_PRI(egl2alog[level], LOG_TAG, "%s", msg);
 #else
    fprintf(stderr, "libEGL %s: %s\n", level_strings[level], msg);
 #endif /* HAVE_ANDROID_PLATFORM */