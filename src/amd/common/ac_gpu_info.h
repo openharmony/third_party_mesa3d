openharmony/third_party_mesa3d

commit 3ac38ab75b665f621b8231f5f7f48fae614e0948

ac: remove amdgpu.h dependency
Add a couple of forward declarations and drop the amdgpu.h requirement.

With this we can build the r300 and r600 drivers without the need for
amdgpu.

v2:
 - Add amdgpu.h include in the C file (Marek)
 - Add a comment about pre C11 typedef redeclaration warning (Eric)

Cc: Nicolai Hähnle <nicolai.haehnle@amd.com>
Cc: Marek Olšák <marek.olsak@amd.com>
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=101189
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

src/amd/common/ac_gpu_info.h
@@ -28,11 +28,13 @@
 
 #include "amd_family.h"
 
-#include <amdgpu.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/* Prior to C11 the following may trigger a typedef redeclaration warning */
+typedef void * amdgpu_device_handle;
+struct amdgpu_gpu_info;
 
 struct radeon_info {
 	/* PCI info: domain:bus:dev:func */

src/amd/common/ac_gpu_info.c
@@ -34,6 +34,8 @@
 #include <xf86drm.h>
 #include <amdgpu_drm.h>
 
+#include <amdgpu.h>
+
 #define CIK_TILE_MODE_COLOR_2D			14
 
 #define CIK__GB_TILE_MODE__PIPE_CONFIG(x)        (((x) >> 6) & 0x1f)

commit 46440a8a5295693fbd182256aa86f749f619badc

ac: resolve conflicts introduced with "ac: remove amdgpu.h dependency"
The commit did not add the relevant includes - in particular
stdint.h and stdbool.h for the respective standard types.

At the same time, the amdgpu_device_handle typedef redeclaration was
off.

Fixes: 3ac38ab75b6 ("ac: remove amdgpu.h dependency")
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=101471
Cc: Mark Janes <mark.a.janes@intel.com>
Cc: Gregor Münch <gr.muench@gmail.com>
Reported-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Reported-by: Mark Janes <mark.a.janes@intel.com>
Reported-by: Gregor Münch <gr.muench@gmail.com>
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Tested-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

src/amd/common/ac_gpu_info.h
@@ -26,6 +26,8 @@
 #ifndef AC_GPU_INFO_H
 #define AC_GPU_INFO_H
 
+#include <stdint.h>
+#include <stdbool.h>
 #include "amd_family.h"
 
 #ifdef __cplusplus
@@ -33,7 +35,7 @@ extern "C" {
 #endif
 
 /* Prior to C11 the following may trigger a typedef redeclaration warning */
-typedef void * amdgpu_device_handle;
+typedef struct amdgpu_device *amdgpu_device_handle;
 struct amdgpu_gpu_info;
 
 struct radeon_info {