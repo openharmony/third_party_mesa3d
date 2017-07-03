openharmony/third_party_mesa3d

commit 029787be17196f35320aa7fc328aacf71b2aef8e

radv: Enable Float64 support.
Signed-off-by: Bas Nieuwenhuizen <basni@google.com>
Reviewed-by: Dave Airlie <airlied@redhat.com>

src/amd/vulkan/radv_pipeline.c
@@ -207,6 +207,7 @@ radv_shader_compile_to_nir(struct radv_device *device,
 			}
 		}
 		const struct nir_spirv_supported_extensions supported_ext = {
+			.float64 = true
 		};
 		entry_point = spirv_to_nir(spirv, module->size / 4,
 					   spec_entries, num_spec_entries,

src/amd/vulkan/radv_device.c
@@ -463,7 +463,7 @@ void radv_GetPhysicalDeviceFeatures(
 		.shaderStorageImageWriteWithoutFormat     = false,
 		.shaderClipDistance                       = true,
 		.shaderCullDistance                       = true,
-		.shaderFloat64                            = false,
+		.shaderFloat64                            = true,
 		.shaderInt64                              = false,
 		.shaderInt16                              = false,
 		.alphaToOne                               = true,

commit e8e72cece6153feba49ce63aaa114cab83fe66ea

ac/nir: implement 64-bit packing and unpacking
We implement the split opcodes, and tell NIR to lower the original ones.
The lowering to LLVM is a little more complicated, but NIR can optimize
the split ones a little better, and some NIR lowering passes that we
might want to use (particularly for doubles) emit the split ones.

This should fix pack/unpackDouble2x32, which seems like a bug since when
we enabled the Float64 capability. It will also fix pack/unpackInt2x32
when we enable the Int64 capability.

Fixes: 029787be ("radv: Enable Float64 support.")
Signed-off-by: Connor Abbott <cwabbott0@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

src/amd/common/ac_nir_to_llvm.c
@@ -1866,6 +1866,37 @@ static void visit_alu(struct nir_to_llvm_context *ctx, const nir_alu_instr *inst
 	case nir_op_fddy_coarse:
 		result = emit_ddxy(ctx, instr->op, src[0]);
 		break;
+
+	case nir_op_unpack_64_2x32_split_x: {
+		assert(instr->src[0].src.ssa->num_components == 1);
+		LLVMValueRef tmp = LLVMBuildBitCast(ctx->builder, src[0],
+						    LLVMVectorType(ctx->i32, 2),
+						    "");
+		result = LLVMBuildExtractElement(ctx->builder, tmp,
+						 ctx->i32zero, "");
+		break;
+	}
+
+	case nir_op_unpack_64_2x32_split_y: {
+		assert(instr->src[0].src.ssa->num_components == 1);
+		LLVMValueRef tmp = LLVMBuildBitCast(ctx->builder, src[0],
+						    LLVMVectorType(ctx->i32, 2),
+						    "");
+		result = LLVMBuildExtractElement(ctx->builder, tmp,
+						 ctx->i32one, "");
+		break;
+	}
+
+	case nir_op_pack_64_2x32_split: {
+		LLVMValueRef tmp = LLVMGetUndef(LLVMVectorType(ctx->i32, 2));
+		tmp = LLVMBuildInsertElement(ctx->builder, tmp,
+					     src[0], ctx->i32zero, "");
+		tmp = LLVMBuildInsertElement(ctx->builder, tmp,
+					     src[1], ctx->i32one, "");
+		result = LLVMBuildBitCast(ctx->builder, tmp, ctx->i64, "");
+		break;
+	}
+
 	default:
 		fprintf(stderr, "Unknown NIR alu instr: ");
 		nir_print_instr(&instr->instr, stderr);

src/amd/vulkan/radv_pipeline.c
@@ -151,6 +151,7 @@ radv_optimize_nir(struct nir_shader *shader)
                 progress = false;
 
                 NIR_PASS_V(shader, nir_lower_vars_to_ssa);
+		NIR_PASS_V(shader, nir_lower_64bit_pack);
                 NIR_PASS_V(shader, nir_lower_alu_to_scalar);
                 NIR_PASS_V(shader, nir_lower_phis_to_scalar);
 