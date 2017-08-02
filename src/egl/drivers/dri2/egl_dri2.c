openharmony/third_party_mesa3d

commit f1f29bd50e1ebd6e2b5e21b235774679cc4cd72b

egl: make platform's SwapInterval() optional
Signed-off-by: Eric Engestrom <eric.engestrom@imgtec.com>
Reviewed-by: Daniel Stone <daniels@collabora.com>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>

src/egl/main/eglapi.c
@@ -1205,7 +1205,7 @@ eglSwapInterval(EGLDisplay dpy, EGLint interval)
                     surf->Config->MinSwapInterval,
                     surf->Config->MaxSwapInterval);
 
-   if (surf->SwapInterval != interval)
+   if (surf->SwapInterval != interval && drv->API.SwapInterval)
       ret = drv->API.SwapInterval(drv, disp, surf, interval);
    else
       ret = EGL_TRUE;

src/egl/drivers/dri2/platform_wayland.c
@@ -1139,15 +1139,6 @@ static const struct wl_registry_listener registry_listener_drm = {
    .global_remove = registry_handle_global_remove
 };
 
-static EGLBoolean
-dri2_wl_swap_interval(_EGLDriver *drv,
-                   _EGLDisplay *disp,
-                   _EGLSurface *surf,
-                   EGLint interval)
-{
-   return EGL_TRUE;
-}
-
 static void
 dri2_wl_setup_swap_interval(struct dri2_egl_display *dri2_dpy)
 {
@@ -1193,7 +1184,6 @@ static const struct dri2_egl_display_vtbl dri2_wl_display_vtbl = {
    .create_pbuffer_surface = dri2_fallback_create_pbuffer_surface,
    .destroy_surface = dri2_wl_destroy_surface,
    .create_image = dri2_create_image_khr,
-   .swap_interval = dri2_wl_swap_interval,
    .swap_buffers = dri2_wl_swap_buffers,
    .swap_buffers_with_damage = dri2_wl_swap_buffers_with_damage,
    .swap_buffers_region = dri2_fallback_swap_buffers_region,
@@ -1894,7 +1884,6 @@ static const struct dri2_egl_display_vtbl dri2_wl_swrast_display_vtbl = {
    .create_pbuffer_surface = dri2_fallback_create_pbuffer_surface,
    .destroy_surface = dri2_wl_destroy_surface,
    .create_image = dri2_fallback_create_image_khr,
-   .swap_interval = dri2_wl_swap_interval,
    .swap_buffers = dri2_wl_swrast_swap_buffers,
    .swap_buffers_with_damage = dri2_fallback_swap_buffers_with_damage,
    .swap_buffers_region = dri2_fallback_swap_buffers_region,

commit 486f32b815a83df7d8ae6a66dcc2510c6775a8f8

egl: check the correct function pointer
`.swap_interval` != `.SwapInterval`...

Fixes: f1f29bd50e1ebd6e2b5e "egl: make platform's SwapInterval() optional"
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=102015
Cc: Cedric Sodhi <manday@openmail.cc>
Signed-off-by: Eric Engestrom <eric.engestrom@imgtec.com>
Tested-by: Cedric Sodhi <manday@openmail.cc>

src/egl/drivers/dri2/egl_dri2.c
@@ -1455,6 +1455,8 @@ dri2_swap_interval(_EGLDriver *drv, _EGLDisplay *dpy, _EGLSurface *surf,
                    EGLint interval)
 {
    struct dri2_egl_display *dri2_dpy = dri2_egl_display(dpy);
+   if (!dri2_dpy->vtbl->swap_interval)
+      return EGL_TRUE;
    return dri2_dpy->vtbl->swap_interval(drv, dpy, surf, interval);
 }
 

src/egl/main/eglapi.c
@@ -1205,7 +1205,7 @@ eglSwapInterval(EGLDisplay dpy, EGLint interval)
                     surf->Config->MinSwapInterval,
                     surf->Config->MaxSwapInterval);
 
-   if (surf->SwapInterval != interval && drv->API.SwapInterval)
+   if (surf->SwapInterval != interval)
       ret = drv->API.SwapInterval(drv, disp, surf, interval);
    else
       ret = EGL_TRUE;