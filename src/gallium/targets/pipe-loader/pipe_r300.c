openharmony/third_party_mesa3d

commit ef11c75e556628b971c51c5de07b65cdcee0b4ad

gallium: add pipe_screen_config to screen_create functions
This allows a more generic mechanism for passing user configurations
into drivers by accessing the dri options directly.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>

src/gallium/targets/pipe-loader/pipe_r300.c
@@ -5,7 +5,7 @@
 #include "r300/r300_public.h"
 
 static struct pipe_screen *
-create_screen(int fd, unsigned flags)
+create_screen(int fd, const struct pipe_screen_config *config)
 {
    struct radeon_winsys *sws;
 

src/gallium/targets/pipe-loader/pipe_r600.c
@@ -5,7 +5,7 @@
 #include "r600/r600_public.h"
 
 static struct pipe_screen *
-create_screen(int fd, unsigned flags)
+create_screen(int fd, const struct pipe_screen_config *config)
 {
    struct radeon_winsys *rw;
 

src/gallium/targets/pipe-loader/pipe_radeonsi.c
@@ -6,7 +6,7 @@
 #include "radeonsi/si_public.h"
 
 static struct pipe_screen *
-create_screen(int fd, unsigned flags)
+create_screen(int fd, const struct pipe_screen_config *config)
 {
    struct radeon_winsys *rw;
 

src/gallium/auxiliary/pipe-loader/pipe_loader.c
@@ -74,9 +74,10 @@ pipe_loader_configuration(struct pipe_loader_device *dev,
 }
 
 struct pipe_screen *
-pipe_loader_create_screen(struct pipe_loader_device *dev, unsigned flags)
+pipe_loader_create_screen(struct pipe_loader_device *dev,
+                          struct pipe_screen_config *config)
 {
-   return dev->ops->create_screen(dev, flags);
+   return dev->ops->create_screen(dev, config);
 }
 
 struct util_dl_library *

src/gallium/auxiliary/pipe-loader/pipe_loader.h
@@ -82,9 +82,12 @@ pipe_loader_probe(struct pipe_loader_device **devs, int ndev);
  * Create a pipe_screen for the specified device.
  *
  * \param dev Device the screen will be created for.
+ * \param config Configuration options. The lifetime of this structure and its
+ *               elements may be limited to the duration of this call.
  */
 struct pipe_screen *
-pipe_loader_create_screen(struct pipe_loader_device *dev, unsigned flags);
+pipe_loader_create_screen(struct pipe_loader_device *dev,
+                          struct pipe_screen_config *config);
 
 /**
  * Query the configuration parameters for the specified device.

src/gallium/auxiliary/pipe-loader/pipe_loader_drm.c
@@ -281,11 +281,12 @@ pipe_loader_drm_configuration(struct pipe_loader_device *dev,
 }
 
 static struct pipe_screen *
-pipe_loader_drm_create_screen(struct pipe_loader_device *dev, unsigned flags)
+pipe_loader_drm_create_screen(struct pipe_loader_device *dev,
+                              const struct pipe_screen_config *config)
 {
    struct pipe_loader_drm_device *ddev = pipe_loader_drm_device(dev);
 
-   return ddev->dd->create_screen(ddev->fd, flags);
+   return ddev->dd->create_screen(ddev->fd, config);
 }
 
 static const struct pipe_loader_ops pipe_loader_drm_ops = {

src/gallium/auxiliary/pipe-loader/pipe_loader_priv.h
@@ -32,7 +32,7 @@
 
 struct pipe_loader_ops {
    struct pipe_screen *(*create_screen)(struct pipe_loader_device *dev,
-                                        unsigned flags);
+                                        const struct pipe_screen_config *config);
 
    const struct drm_conf_ret *(*configuration)(struct pipe_loader_device *dev,
                                                enum drm_conf conf);

src/gallium/auxiliary/pipe-loader/pipe_loader_sw.c
@@ -295,7 +295,7 @@ pipe_loader_sw_configuration(struct pipe_loader_device *dev,
 
 static struct pipe_screen *
 pipe_loader_sw_create_screen(struct pipe_loader_device *dev,
-                             unsigned flags)
+                             const struct pipe_screen_config *config)
 {
    struct pipe_loader_sw_device *sdev = pipe_loader_sw_device(dev);
    struct pipe_screen *screen;

commit a69b4f0a0485c54d3ffb85d6782a7000228603eb

pipe-loader: fix build of dynamic pipe-drivers
v2: add libxmlconfig.la to the dynamic pipe_radeonsi driver
v3: add libxmlconfig.la to targets/opencl build
v4: add EXPAT_LIBS to opencl build
    (note: for only-opencl builds, Emil's configure.ac changes
     are also needed)

Fixes: ef11c75e556 ("gallium: add pipe_screen_config to screen_create functions")
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=102014
Tested-by: Andy Furniss <adf.lists@gmail.com>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com> (v1)

src/gallium/targets/opencl/Makefile.am
@@ -19,6 +19,8 @@ lib@OPENCL_LIBNAME@_la_LIBADD = \
 	$(top_builddir)/src/gallium/state_trackers/clover/libclover.la \
 	$(top_builddir)/src/gallium/auxiliary/libgallium.la \
 	$(top_builddir)/src/util/libmesautil.la \
+	$(top_builddir)/src/util/libxmlconfig.la \
+	$(EXPAT_LIBS) \
 	$(LIBELF_LIBS) \
 	$(DLOPEN_LIBS) \
 	-lclangCodeGen \

src/gallium/targets/pipe-loader/Makefile.am
@@ -146,6 +146,7 @@ pipe_radeonsi_la_LIBADD = \
 	$(top_builddir)/src/gallium/drivers/radeon/libradeon.la \
 	$(top_builddir)/src/gallium/drivers/radeonsi/libradeonsi.la \
 	$(top_builddir)/src/amd/common/libamd_common.la \
+	$(top_builddir)/src/util/libxmlconfig.la \
 	$(LIBDRM_LIBS) \
 	$(RADEON_LIBS) \
 	$(AMDGPU_LIBS)

src/gallium/targets/pipe-loader/pipe_r300.c
@@ -9,7 +9,7 @@ create_screen(int fd, const struct pipe_screen_config *config)
 {
    struct radeon_winsys *sws;
 
-   sws = radeon_drm_winsys_create(fd, flags, r300_screen_create);
+   sws = radeon_drm_winsys_create(fd, config, r300_screen_create);
    return sws ? debug_screen_wrap(sws->screen) : NULL;
 }
 

src/gallium/targets/pipe-loader/pipe_r600.c
@@ -9,7 +9,7 @@ create_screen(int fd, const struct pipe_screen_config *config)
 {
    struct radeon_winsys *rw;
 
-   rw = radeon_drm_winsys_create(fd, flags, r600_screen_create);
+   rw = radeon_drm_winsys_create(fd, config, r600_screen_create);
    return rw ? debug_screen_wrap(rw->screen) : NULL;
 }
 

src/gallium/targets/pipe-loader/pipe_radeonsi.c
@@ -12,10 +12,10 @@ create_screen(int fd, const struct pipe_screen_config *config)
    struct radeon_winsys *rw;
 
    /* First, try amdgpu. */
-   rw = amdgpu_winsys_create(fd, flags, radeonsi_screen_create);
+   rw = amdgpu_winsys_create(fd, config, radeonsi_screen_create);
 
    if (!rw)
-      rw = radeon_drm_winsys_create(fd, flags, radeonsi_screen_create);
+      rw = radeon_drm_winsys_create(fd, config, radeonsi_screen_create);
 
    return rw ? debug_screen_wrap(rw->screen) : NULL;
 }