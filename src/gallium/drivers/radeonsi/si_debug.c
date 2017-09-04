openharmony/third_party_mesa3d

commit 585e4d61c09892d401357c4242ddaff70ffdbfe3

radeonsi: log draw and compute state into log context
Also add missing trace emits and CS logging for compute launches.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>

src/gallium/drivers/radeonsi/si_debug.c
@@ -1067,34 +1067,38 @@ static void si_dump_debug_state(struct pipe_context *ctx, FILE *f,
 			si_dump_command("Wave information", "umr -O bits -wa", f);
 		}
 	}
+}
 
-	struct u_log_context log;
-	u_log_context_init(&log);
+void si_log_draw_state(struct si_context *sctx, struct u_log_context *log)
+{
+	if (!log)
+		return;
 
-	if (flags & PIPE_DUMP_CURRENT_STATES)
-		si_dump_framebuffer(sctx, &log);
+	si_dump_framebuffer(sctx, log);
 
-	if (flags & PIPE_DUMP_CURRENT_SHADERS) {
-		si_dump_gfx_shader(sctx, &sctx->vs_shader, &log);
-		si_dump_gfx_shader(sctx, &sctx->tcs_shader, &log);
-		si_dump_gfx_shader(sctx, &sctx->tes_shader, &log);
-		si_dump_gfx_shader(sctx, &sctx->gs_shader, &log);
-		si_dump_gfx_shader(sctx, &sctx->ps_shader, &log);
-		si_dump_compute_shader(&sctx->cs_shader_state, &log);
+	si_dump_gfx_shader(sctx, &sctx->vs_shader, log);
+	si_dump_gfx_shader(sctx, &sctx->tcs_shader, log);
+	si_dump_gfx_shader(sctx, &sctx->tes_shader, log);
+	si_dump_gfx_shader(sctx, &sctx->gs_shader, log);
+	si_dump_gfx_shader(sctx, &sctx->ps_shader, log);
 
-		si_dump_descriptor_list(&sctx->descriptors[SI_DESCS_RW_BUFFERS],
-					"", "RW buffers", 4, SI_NUM_RW_BUFFERS,
-					si_identity, &log);
-		si_dump_gfx_descriptors(sctx, &sctx->vs_shader, &log);
-		si_dump_gfx_descriptors(sctx, &sctx->tcs_shader, &log);
-		si_dump_gfx_descriptors(sctx, &sctx->tes_shader, &log);
-		si_dump_gfx_descriptors(sctx, &sctx->gs_shader, &log);
-		si_dump_gfx_descriptors(sctx, &sctx->ps_shader, &log);
-		si_dump_compute_descriptors(sctx, &log);
-	}
+	si_dump_descriptor_list(&sctx->descriptors[SI_DESCS_RW_BUFFERS],
+				"", "RW buffers", 4, SI_NUM_RW_BUFFERS,
+				si_identity, log);
+	si_dump_gfx_descriptors(sctx, &sctx->vs_shader, log);
+	si_dump_gfx_descriptors(sctx, &sctx->tcs_shader, log);
+	si_dump_gfx_descriptors(sctx, &sctx->tes_shader, log);
+	si_dump_gfx_descriptors(sctx, &sctx->gs_shader, log);
+	si_dump_gfx_descriptors(sctx, &sctx->ps_shader, log);
+}
 
-	u_log_new_page_print(&log, f);
-	u_log_context_destroy(&log);
+void si_log_compute_state(struct si_context *sctx, struct u_log_context *log)
+{
+	if (!log)
+		return;
+
+	si_dump_compute_shader(&sctx->cs_shader_state, log);
+	si_dump_compute_descriptors(sctx, log);
 }
 
 static void si_dump_dma(struct si_context *sctx,
@@ -1246,13 +1250,17 @@ void si_check_vm_faults(struct r600_common_context *ctx,
 			sctx->apitrace_call_number);
 
 	switch (ring) {
-	case RING_GFX:
-		si_dump_debug_state(&sctx->b.b, f,
-				    PIPE_DUMP_CURRENT_STATES |
-				    PIPE_DUMP_CURRENT_SHADERS |
-				    PIPE_DUMP_LAST_COMMAND_BUFFER);
-		break;
+	case RING_GFX: {
+		struct u_log_context log;
+		u_log_context_init(&log);
 
+		si_log_draw_state(sctx, &log);
+		si_log_compute_state(sctx, &log);
+
+		u_log_new_page_print(&log, f);
+		u_log_context_destroy(&log);
+		break;
+	}
 	case RING_DMA:
 		si_dump_dma(sctx, saved, f);
 		break;

src/gallium/drivers/radeonsi/si_compute.c
@@ -801,6 +801,9 @@ static void si_launch_grid(
 
 	si_need_cs_space(sctx);
 
+	if (sctx->b.log)
+		si_log_compute_state(sctx, sctx->b.log);
+
 	if (!sctx->cs_shader_state.initialized)
 		si_initialize_compute(sctx);
 
@@ -846,6 +849,9 @@ static void si_launch_grid(
 	si_emit_dispatch_packets(sctx, info);
 
 	si_ce_post_draw_synchronization(sctx);
+
+	if (unlikely(sctx->current_saved_cs))
+		si_trace_emit(sctx);
 
 	sctx->compute_is_busy = true;
 	sctx->b.num_compute_calls++;

src/gallium/drivers/radeonsi/si_pipe.h
@@ -508,6 +508,8 @@ void si_init_cp_dma_functions(struct si_context *sctx);
 /* si_debug.c */
 void si_auto_log_cs(void *data, struct u_log_context *log);
 void si_log_hw_flush(struct si_context *sctx);
+void si_log_draw_state(struct si_context *sctx, struct u_log_context *log);
+void si_log_compute_state(struct si_context *sctx, struct u_log_context *log);
 void si_init_debug_functions(struct si_context *sctx);
 void si_check_vm_faults(struct r600_common_context *ctx,
 			struct radeon_saved_cs *saved, enum ring_type ring);

src/gallium/drivers/radeonsi/si_state_draw.c
@@ -1361,6 +1361,9 @@ void si_draw_vbo(struct pipe_context *ctx, const struct pipe_draw_info *info)
 
 	si_need_cs_space(sctx);
 
+	if (unlikely(sctx->b.log))
+		si_log_draw_state(sctx, sctx->b.log);
+
 	/* Since we've called r600_context_add_resource_size for vertex buffers,
 	 * this must be called after si_need_cs_space, because we must let
 	 * need_cs_space flush before we add buffers to the buffer list.

commit 7c3f9feb572f076cfca07c6167120c20379144a4

radeonsi: fix compute shader state dumping
Fixes: 585e4d61c098 ("radeonsi: log draw and compute state into log context")
Reviewed-by: Marek Olšák <marek.olsak@amd.com>

src/gallium/drivers/radeonsi/si_debug.c
@@ -60,6 +60,7 @@ struct si_log_chunk_shader {
 	 */
 	struct si_context *ctx;
 	struct si_shader *shader;
+	enum pipe_shader_type processor;
 
 	/* For keep-alive reference counts */
 	struct si_shader_selector *sel;
@@ -80,7 +81,7 @@ si_log_chunk_shader_print(void *data, FILE *f)
 {
 	struct si_log_chunk_shader *chunk = data;
 	struct si_screen *sscreen = chunk->ctx->screen;
-	si_dump_shader(sscreen, chunk->shader->selector->info.processor,
+	si_dump_shader(sscreen, chunk->processor,
 		       chunk->shader, f);
 }
 
@@ -100,18 +101,23 @@ static void si_dump_gfx_shader(struct si_context *ctx,
 
 	struct si_log_chunk_shader *chunk = CALLOC_STRUCT(si_log_chunk_shader);
 	chunk->ctx = ctx;
+	chunk->processor = state->cso->info.processor;
 	chunk->shader = current;
 	si_shader_selector_reference(ctx, &chunk->sel, current->selector);
 	u_log_chunk(log, &si_log_chunk_type_shader, chunk);
 }
 
-static void si_dump_compute_shader(const struct si_cs_shader_state *state,
+static void si_dump_compute_shader(struct si_context *ctx,
 				   struct u_log_context *log)
 {
-	if (!state->program || state->program != state->emitted_program)
+	const struct si_cs_shader_state *state = &ctx->cs_shader_state;
+
+	if (!state->program)
 		return;
 
 	struct si_log_chunk_shader *chunk = CALLOC_STRUCT(si_log_chunk_shader);
+	chunk->ctx = ctx;
+	chunk->processor = PIPE_SHADER_COMPUTE;
 	chunk->shader = &state->program->shader;
 	si_compute_reference(&chunk->program, state->program);
 	u_log_chunk(log, &si_log_chunk_type_shader, chunk);
@@ -745,8 +751,7 @@ static void si_dump_gfx_descriptors(struct si_context *sctx,
 static void si_dump_compute_descriptors(struct si_context *sctx,
 					struct u_log_context *log)
 {
-	if (!sctx->cs_shader_state.program ||
-	    sctx->cs_shader_state.program != sctx->cs_shader_state.emitted_program)
+	if (!sctx->cs_shader_state.program)
 		return;
 
 	si_dump_descriptors(sctx, PIPE_SHADER_COMPUTE, NULL, log);
@@ -1068,7 +1073,7 @@ void si_log_compute_state(struct si_context *sctx, struct u_log_context *log)
 	if (!log)
 		return;
 
-	si_dump_compute_shader(&sctx->cs_shader_state, log);
+	si_dump_compute_shader(sctx, log);
 	si_dump_compute_descriptors(sctx, log);
 }
 