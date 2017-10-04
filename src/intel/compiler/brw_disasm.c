openharmony/third_party_mesa3d

commit de1839b2781b74a525e1911a99f2aa0ba09ede48

i965: Add support for disassembling 64-bit integer immediates
After the last patch converted things into enums, I helpfully got a
compiler warning about these missing from the switch statement.

Reviewed-by: Scott D Phillips <scott.d.phillips@intel.com>

src/intel/compiler/brw_disasm.c
@@ -1029,6 +1029,12 @@ imm(FILE *file, const struct gen_device_info *devinfo, enum hw_imm_type type,
     const brw_inst *inst)
 {
    switch (type) {
+   case GEN8_HW_IMM_TYPE_UQ:
+      format(file, "0x%16lxUD", brw_inst_imm_uq(devinfo, inst));
+      break;
+   case GEN8_HW_IMM_TYPE_Q:
+      format(file, "%ldD", brw_inst_imm_uq(devinfo, inst));
+      break;
    case BRW_HW_IMM_TYPE_UD:
       format(file, "0x%08xUD", brw_inst_imm_ud(devinfo, inst));
       break;

src/intel/compiler/brw_inst.h
@@ -569,6 +569,13 @@ brw_inst_imm_ud(const struct gen_device_info *devinfo, const brw_inst *insn)
    return brw_inst_bits(insn, 127, 96);
 }
 
+static inline uint64_t
+brw_inst_imm_uq(const struct gen_device_info *devinfo, const brw_inst *insn)
+{
+   assert(devinfo->gen >= 8);
+   return brw_inst_bits(insn, 127, 64);
+}
+
 static inline float
 brw_inst_imm_f(const struct gen_device_info *devinfo, const brw_inst *insn)
 {

commit 6b561a2d512e42b48d5a893638c16fa561074177

i965: Fix support for disassembling 64-bit integer immediates
The type suffixes were wrong, and the 16 was missing the 0 prefix.

Fixes: de1839b2781b ("i965: Add support for disassembling 64-bit integer immediates")
Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>

src/intel/compiler/brw_disasm.c
@@ -1012,10 +1012,10 @@ imm(FILE *file, const struct gen_device_info *devinfo, enum brw_reg_type type,
 {
    switch (type) {
    case BRW_REGISTER_TYPE_UQ:
-      format(file, "0x%16lxUD", brw_inst_imm_uq(devinfo, inst));
+      format(file, "0x%016lxUQ", brw_inst_imm_uq(devinfo, inst));
       break;
    case BRW_REGISTER_TYPE_Q:
-      format(file, "%ldD", brw_inst_imm_uq(devinfo, inst));
+      format(file, "%ldQ", brw_inst_imm_uq(devinfo, inst));
       break;
    case BRW_REGISTER_TYPE_UD:
       format(file, "0x%08xUD", brw_inst_imm_ud(devinfo, inst));