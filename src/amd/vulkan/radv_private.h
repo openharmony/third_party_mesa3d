BNieuwenhuizen/mesa

radv: Add multiple shader cache store & load functions.

src/amd/vulkan/radv_private.h
@@ -317,6 +317,20 @@ radv_pipeline_cache_insert_shader(struct radv_pipeline_cache *cache,
 				  struct radv_shader_variant *variant,
 				  const void *code, unsigned code_size);
 
+bool
+radv_create_shader_variants_from_pipeline_cache(struct radv_device *device,
+					        struct radv_pipeline_cache *cache,
+					        const unsigned char *sha1,
+					        struct radv_shader_variant **variants);
+
+void
+radv_pipeline_cache_insert_shaders(struct radv_pipeline_cache *cache,
+				   const unsigned char *sha1,
+				   struct radv_shader_variant **variants,
+				   const void *const *codes,
+				   const unsigned *code_sizes);
+
+
 void radv_shader_variant_destroy(struct radv_device *device,
 				 struct radv_shader_variant *variant);
 

openharmony/third_party_mesa3d

Signed-off-by: Timothy Arceri <tarceri@itsqueeze.com>
Acked-by: Dave Airlie <airlied@redhat.com>

src/amd/vulkan/radv_private.h
@@ -336,6 +336,20 @@ radv_pipeline_cache_insert_shader(struct radv_device *device,
 				  struct radv_shader_variant *variant,
 				  const void *code, unsigned code_size);
 
+bool
+radv_create_shader_variants_from_pipeline_cache(struct radv_device *device,
+					        struct radv_pipeline_cache *cache,
+					        const unsigned char *sha1,
+					        struct radv_shader_variant **variants);
+
+void
+radv_pipeline_cache_insert_shaders(struct radv_device *device,
+				   struct radv_pipeline_cache *cache,
+				   const unsigned char *sha1,
+				   struct radv_shader_variant **variants,
+				   const void *const *codes,
+				   const unsigned *code_sizes);
+
 struct radv_meta_state {
 	VkAllocationCallbacks alloc;
 