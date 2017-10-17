openharmony/third_party_mesa3d

commit 3448eda7a17383a037e5c2d55f811d349a9266b8

radv: Expose VK_KHX_multiview.
Reviewed-by: Dave Airlie <airlied@redhat.com>

src/amd/vulkan/radv_device.c
@@ -178,6 +178,10 @@ static const VkExtensionProperties ext_sema_device_extensions[] = {
 		.extensionName = VK_KHR_EXTERNAL_SEMAPHORE_FD_EXTENSION_NAME,
 		.specVersion = 1,
 	},
+	{
+		.extensionName = VK_KHX_MULTIVIEW_EXTENSION_NAME,
+		.specVersion = 1,
+	},
 };
 
 static VkResult
@@ -628,6 +632,13 @@ void radv_GetPhysicalDeviceFeatures2KHR(
 			features->variablePointers = false;
 			break;
 		}
+		case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_MULTIVIEW_FEATURES_KHX: {
+			VkPhysicalDeviceMultiviewFeaturesKHX *features = (VkPhysicalDeviceMultiviewFeaturesKHX*)ext;
+			features->multiview = true;
+			features->multiviewGeometryShader = true;
+			features->multiviewTessellationShader = true;
+			break;
+		}
 		default:
 			break;
 		}
@@ -802,6 +813,12 @@ void radv_GetPhysicalDeviceProperties2KHR(
 			memcpy(properties->driverUUID, pdevice->driver_uuid, VK_UUID_SIZE);
 			memcpy(properties->deviceUUID, pdevice->device_uuid, VK_UUID_SIZE);
 			properties->deviceLUIDValid = false;
+			break;
+		}
+		case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_MULTIVIEW_PROPERTIES_KHX: {
+			VkPhysicalDeviceMultiviewPropertiesKHX *properties = (VkPhysicalDeviceMultiviewPropertiesKHX*)ext;
+			properties->maxMultiviewViewCount = MAX_VIEWS;
+			properties->maxMultiviewInstanceIndex = INT_MAX;
 			break;
 		}
 		default:

src/amd/vulkan/radv_pipeline.c
@@ -230,6 +230,7 @@ radv_shader_compile_to_nir(struct radv_device *device,
 			.image_write_without_format = true,
 			.tessellation = true,
 			.int64 = true,
+			.multiview = true,
 			.variable_pointers = true,
 		};
 		entry_point = spirv_to_nir(spirv, module->size / 4,

commit bb786e807bb8b49f4c2f613ad072992aecdd414e

radv: enable VK_KHX_multiview always.
This was in the wrong place.

Fixes: 3448eda7a (radv: Expose VK_KHX_multiview.)
Acked-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Signed-off-by: Dave Airlie <airlied@redhat.com>

src/amd/vulkan/radv_device.c
@@ -186,6 +186,10 @@ static const VkExtensionProperties common_device_extensions[] = {
 		.extensionName = VK_KHR_RELAXED_BLOCK_LAYOUT_EXTENSION_NAME,
 		.specVersion = 1,
 	},
+	{
+		.extensionName = VK_KHX_MULTIVIEW_EXTENSION_NAME,
+		.specVersion = 1,
+	},
 };
 
 static const VkExtensionProperties rasterization_order_extension[] ={
@@ -202,10 +206,6 @@ static const VkExtensionProperties ext_sema_device_extensions[] = {
 	},
 	{
 		.extensionName = VK_KHR_EXTERNAL_SEMAPHORE_FD_EXTENSION_NAME,
-		.specVersion = 1,
-	},
-	{
-		.extensionName = VK_KHX_MULTIVIEW_EXTENSION_NAME,
 		.specVersion = 1,
 	},
 };