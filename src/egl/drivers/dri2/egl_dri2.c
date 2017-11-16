openharmony/third_party_mesa3d

commit 1329ca66c559bd42a989eaaeaf40b57f2954e0f6

egl/drm: move teardown code to the platform file
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
Reviewed-by: Eric Engestrom <eric.engestrom@imgtec.com>

src/egl/drivers/dri2/egl_dri2.c
@@ -991,13 +991,9 @@ dri2_display_destroy(_EGLDisplay *disp)
    case _EGL_PLATFORM_X11:
       dri2_teardown_x11(dri2_dpy);
       break;
-#ifdef HAVE_DRM_PLATFORM
    case _EGL_PLATFORM_DRM:
-      if (dri2_dpy->own_device) {
-         gbm_device_destroy(&dri2_dpy->gbm_dri->base);
-      }
+      dri2_teardown_drm(disp);
       break;
-#endif
 #ifdef HAVE_WAYLAND_PLATFORM
    case _EGL_PLATFORM_WAYLAND:
       if (dri2_dpy->wl_drm)

src/egl/drivers/dri2/egl_dri2.h
@@ -418,12 +418,16 @@ dri2_teardown_x11(struct dri2_egl_display *dri2_dpy) {}
 #ifdef HAVE_DRM_PLATFORM
 EGLBoolean
 dri2_initialize_drm(_EGLDriver *drv, _EGLDisplay *disp);
+void
+dri2_teardown_drm(struct dri2_egl_display *dri2_dpy);
 #else
 static inline EGLBoolean
 dri2_initialize_drm(_EGLDriver *drv, _EGLDisplay *disp)
 {
    return _eglError(EGL_NOT_INITIALIZED, "GBM/DRM platform not built");
 }
+static inline void
+dri2_teardown_drm(struct dri2_egl_display *dri2_dpy) {}
 #endif
 
 #ifdef HAVE_WAYLAND_PLATFORM

src/egl/drivers/dri2/platform_drm.c
@@ -741,3 +741,10 @@ cleanup:
    dri2_display_destroy(disp);
    return _eglError(EGL_NOT_INITIALIZED, err);
 }
+
+void
+dri2_teardown_drm(struct dri2_egl_display *dri2_dpy)
+{
+   if (dri2_dpy->own_device)
+      gbm_device_destroy(&dri2_dpy->gbm_dri->base);
+}

commit 7c0f031addb4c8136e26f8de8eddcb7d5c776eb1

egl: pass the dri2_dpy to the $plat_teardown functions
Cc: Mark Janes <mark.a.janes@intel.com>
Fixes: 1329ca66c55 ("egl/drm: move teardown code to the platform file")
Fixes: 5fa24f9a865 ("egl/wayland: move teardown code to the platform file")
Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
Tested-by: Dylan Baker <dylan@pnwbakers.com>
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=103784

src/egl/drivers/dri2/egl_dri2.c
@@ -992,10 +992,10 @@ dri2_display_destroy(_EGLDisplay *disp)
       dri2_teardown_x11(dri2_dpy);
       break;
    case _EGL_PLATFORM_DRM:
-      dri2_teardown_drm(disp);
+      dri2_teardown_drm(dri2_dpy);
       break;
    case _EGL_PLATFORM_WAYLAND:
-      dri2_teardown_wayland(disp);
+      dri2_teardown_wayland(dri2_dpy);
       break;
    default:
       /* TODO: add teardown for other platforms */