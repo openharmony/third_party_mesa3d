openharmony/third_party_mesa3d

commit 1ddb576c7739db7b1541e21bb0c1cd3addd3b7ee

util: Assume little endian in the absence of platform-specific handling

src/util/u_endian.h
@@ -67,4 +67,7 @@
 
 #endif
 
+#warn Unknown Endianness for this platform. Assuming little endian
+#define PIPE_ARCH_LITTLE_ENDIAN
+
 #endif

commit b4b1988866171ccdb56ac6e9cd9fb26d0c3aa581

util: Use preprocessor correctly
Fixes: 1ddb576c7739 ("util: Assume little endian in the absence of
                      platform-specific handling")

src/util/u_endian.h
@@ -67,7 +67,7 @@
 
 #endif
 
-#warn Unknown Endianness for this platform. Assuming little endian
+#warning Unknown Endianness for this platform. Assuming little endian
 #define PIPE_ARCH_LITTLE_ENDIAN
 
 #endif