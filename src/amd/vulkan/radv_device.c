openharmony/third_party_mesa3d

commit 27357f80c492945b03f8d410b43ec45431ba00a9

radv: set some dcc parameters depending on if texture will be sampled
This is ported from amdvlk which sets the independent 64b blocks
only for image which will sample dcc.

I'm not sure how to port this to radeonsi.

Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Signed-off-by: Dave Airlie <airlied@redhat.com>

src/amd/vulkan/radv_device.c
@@ -3163,6 +3163,8 @@ radv_initialise_color_surface(struct radv_device *device,
 	if (device->physical_device->rad_info.chip_class >= VI) {
 		unsigned max_uncompressed_block_size = V_028C78_MAX_BLOCK_SIZE_256B;
 		unsigned min_compressed_block_size = V_028C78_MIN_BLOCK_SIZE_32B;
+		unsigned independent_64b_blocks = 0;
+		unsigned max_compressed_block_size;
 
 		/* amdvlk: [min-compressed-block-size] should be set to 32 for dGPU and
 		   64 for APU because all of our APUs to date use DIMMs which have
@@ -3178,9 +3180,16 @@ radv_initialise_color_surface(struct radv_device *device,
 				max_uncompressed_block_size = V_028C78_MAX_BLOCK_SIZE_128B;
 		}
 
+		if (iview->image->usage & (VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT)) {
+			independent_64b_blocks = 1;
+			max_compressed_block_size = V_028C78_MAX_BLOCK_SIZE_64B;
+		} else
+			max_compressed_block_size = max_uncompressed_block_size;
+
 		cb->cb_dcc_control = S_028C78_MAX_UNCOMPRESSED_BLOCK_SIZE(max_uncompressed_block_size) |
+			S_028C78_MAX_COMPRESSED_BLOCK_SIZE(max_compressed_block_size) |
 			S_028C78_MIN_COMPRESSED_BLOCK_SIZE(min_compressed_block_size) |
-			S_028C78_INDEPENDENT_64B_BLOCKS(1);
+			S_028C78_INDEPENDENT_64B_BLOCKS(independent_64b_blocks);
 	}
 
 	/* This must be set for fast clear to work without FMASK. */

commit 289b988373161523c90293e8ea4fbd7b9755b994

radv: Also set DCC params for sampling for input attachment usage.
Those are implemented as texture sampling, so we need to make the
texture TC-compatible too.

Fixes: 27357f80c49 "radv: set some dcc parameters depending on if texture will be sampled"
Reviewed-by: Fredrik Höglund <fredrik@kde.org>

src/amd/vulkan/radv_device.c
@@ -3181,7 +3181,8 @@ radv_initialise_color_surface(struct radv_device *device,
 				max_uncompressed_block_size = V_028C78_MAX_BLOCK_SIZE_128B;
 		}
 
-		if (iview->image->usage & (VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT)) {
+		if (iview->image->usage & (VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
+		                           VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT)) {
 			independent_64b_blocks = 1;
 			max_compressed_block_size = V_028C78_MAX_BLOCK_SIZE_64B;
 		} else