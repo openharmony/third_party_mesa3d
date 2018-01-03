openharmony/third_party_mesa3d

commit 8a9b44a20e5db847bb49f4aa80b760a0d487b31d

radv: Drop the default viewport when 0 viewports are given.
Signed-off-by: Bas Nieuwenhuizen <basni@google.com>
Reviewed-by: Dave Airlie <airlied@redhat.com>

src/amd/vulkan/si_cmd_buffer.c
@@ -506,21 +506,7 @@ si_write_viewport(struct radeon_winsys_cs *cs, int first_vp,
 {
 	int i;
 
-	if (count == 0) {
-		radeon_set_context_reg_seq(cs, R_02843C_PA_CL_VPORT_XSCALE, 6);
-		radeon_emit(cs, fui(1.0));
-		radeon_emit(cs, fui(0.0));
-		radeon_emit(cs, fui(1.0));
-		radeon_emit(cs, fui(0.0));
-		radeon_emit(cs, fui(1.0));
-		radeon_emit(cs, fui(0.0));
-
-		radeon_set_context_reg_seq(cs, R_0282D0_PA_SC_VPORT_ZMIN_0, 2);
-		radeon_emit(cs, fui(0.0));
-		radeon_emit(cs, fui(1.0));
-
-		return;
-	}
+	assert(count);
 	radeon_set_context_reg_seq(cs, R_02843C_PA_CL_VPORT_XSCALE +
 				   first_vp * 4 * 6, count * 6);
 
@@ -552,8 +538,7 @@ si_write_scissors(struct radeon_winsys_cs *cs, int first,
                   int count, const VkRect2D *scissors)
 {
 	int i;
-	if (count == 0)
-		return;
+	assert(count);
 
 	radeon_set_context_reg_seq(cs, R_028250_PA_SC_VPORT_SCISSOR_0_TL + first * 4 * 2, count * 2);
 	for (i = 0; i < count; i++) {

commit 5d8ee263dfbb3b774fb20579394dae9a7cd6bd24

radv: Allow writing 0 scissors.
When rasterization is disabled we can have that few.

Fixes: 8a9b44a20e5 "radv: Drop the default viewport when 0 viewports are given."
Reviewed-by: Dave Airlie <airlied@redhat.com>

src/amd/vulkan/si_cmd_buffer.c
@@ -673,7 +673,8 @@ si_write_scissors(struct radeon_winsys_cs *cs, int first,
 	int i;
 	float scale[3], translate[3], guardband_x = INFINITY, guardband_y = INFINITY;
 	const float max_range = 32767.0f;
-	assert(count);
+	if (!count)
+		return;
 
 	radeon_set_context_reg_seq(cs, R_028250_PA_SC_VPORT_SCISSOR_0_TL + first * 4 * 2, count * 2);
 	for (i = 0; i < count; i++) {