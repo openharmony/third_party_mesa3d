aras-p/glsl-optimizer

commit 4e0d99a63588c67a955f797733da32d04e6f4ee6

r100: Use shared debug code.
Converted r100 to use shared debug code with sed and fast compile check. New
code has compability layer so old debugging code doesn't have to be changed
all immidiatly.

src/mesa/drivers/dri/radeon/radeon_context.c
@@ -137,25 +137,6 @@ static const struct tnl_pipeline_stage *radeon_pipeline[] = {
    NULL,
 };
 
-static const struct dri_debug_control debug_control[] =
-{
-    { "fall",  DEBUG_FALLBACKS },
-    { "tex",   DEBUG_TEXTURE },
-    { "ioctl", DEBUG_IOCTL },
-    { "prim",  DEBUG_PRIMS },
-    { "vert",  DEBUG_VERTS },
-    { "state", DEBUG_STATE },
-    { "code",  DEBUG_CODEGEN },
-    { "vfmt",  DEBUG_VFMT },
-    { "vtxf",  DEBUG_VFMT },
-    { "verb",  DEBUG_VERBOSE },
-    { "dri",   DEBUG_DRI },
-    { "dma",   DEBUG_DMA },
-    { "san",   DEBUG_SANITY },
-    { "sync",  DEBUG_SYNC },
-    { NULL,    0 }
-};
-
 static void r100_get_lock(radeonContextPtr radeon)
 {
    r100ContextPtr rmesa = (r100ContextPtr)radeon;

src/mesa/drivers/dri/radeon/Makefile
@@ -15,17 +15,18 @@ CS_SOURCES = radeon_cs_space_drm.c
 endif
 
 RADEON_COMMON_SOURCES = \
-	radeon_texture.c \
+	radeon_bo_legacy.c \
 	radeon_common_context.c \
 	radeon_common.c \
-	radeon_dma.c \
-	radeon_lock.c \
-	radeon_bo_legacy.c \
 	radeon_cs_legacy.c \
-	radeon_mipmap_tree.c \
-	radeon_span.c \
+	radeon_dma.c \
+	radeon_debug.c \
 	radeon_fbo.c \
-	radeon_queryobj.c
+	radeon_lock.c \
+	radeon_mipmap_tree.c \
+	radeon_queryobj.c \
+	radeon_span.c \
+	radeon_texture.c
 
 DRIVER_SOURCES = \
 	radeon_context.c \

src/mesa/drivers/dri/radeon/radeon_bo_legacy.c
@@ -640,7 +640,7 @@ static int bo_vram_validate(struct radeon_bo *bo,
 						      &tex,
 						      sizeof(drm_radeon_texture_t));
 			    if (ret) {
-				    if (RADEON_DEBUG & DEBUG_IOCTL)
+				    if (RADEON_DEBUG & RADEON_IOCTL)
 					    fprintf(stderr, "DRM_RADEON_TEXTURE:  again!\n");
 				    usleep(1);
 			    }
@@ -677,7 +677,7 @@ static int bo_vram_validate(struct radeon_bo *bo,
 						      &tex,
 						      sizeof(drm_radeon_texture_t));
 			    if (ret) {
-				    if (RADEON_DEBUG & DEBUG_IOCTL)
+				    if (RADEON_DEBUG & RADEON_IOCTL)
 					    fprintf(stderr, "DRM_RADEON_TEXTURE:  again!\n");
 				    usleep(1);
 			    }

src/mesa/drivers/dri/radeon/radeon_common.c
@@ -91,7 +91,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * 1 most output
  * 2 also print state alues
  */
-#define DEBUG_CMDBUF         0
+#define RADEON_CMDBUF         0
 
 /* =============================================================
  * Scissoring
@@ -521,7 +521,7 @@ void radeonCopyBuffer( __DRIdrawablePrivate *dPriv,
 
 	rfb = dPriv->driverPrivate;
 
-	if ( RADEON_DEBUG & DEBUG_IOCTL ) {
+	if ( RADEON_DEBUG & RADEON_IOCTL ) {
 		fprintf( stderr, "\n%s( %p )\n\n", __FUNCTION__, (void *) rmesa->glCtx );
 	}
 
@@ -615,7 +615,7 @@ static GLboolean radeonPageFlip( __DRIdrawablePrivate *dPriv )
 
 	LOCK_HARDWARE(radeon);
 
-	if ( RADEON_DEBUG & DEBUG_IOCTL ) {
+	if ( RADEON_DEBUG & RADEON_IOCTL ) {
 		fprintf(stderr, "%s: pfCurrentPage: %d %d\n", __FUNCTION__,
 			radeon->sarea->pfCurrentPage, radeon->sarea->pfState);
 	}
@@ -875,7 +875,7 @@ void radeon_draw_buffer(GLcontext *ctx, struct gl_framebuffer *fb)
  */
 void radeonDrawBuffer( GLcontext *ctx, GLenum mode )
 {
-	if (RADEON_DEBUG & DEBUG_DRI)
+	if (RADEON_DEBUG & RADEON_DRI)
 		fprintf(stderr, "%s %s\n", __FUNCTION__,
 			_mesa_lookup_enum_by_nr( mode ));
 
@@ -981,7 +981,7 @@ static void radeon_print_state_atom_prekmm(radeonContextPtr radeon, struct radeo
 
 	fprintf(stderr, "  emit %s %d/%d\n", state->name, dwords, state->cmd_size);
 
-	if (DEBUG_CMDBUF > 1 && RADEON_DEBUG & DEBUG_VERBOSE) {
+	if (radeon_is_debug_enabled(RADEON_STATE, RADEON_TRACE)) {
 		if (dwords > state->cmd_size)
 			dwords = state->cmd_size;
 
@@ -1006,7 +1006,7 @@ static void radeon_print_state_atom(radeonContextPtr radeon, struct radeon_state
 	int i, j, reg, count;
 	int dwords;
 	uint32_t packet0;
-	if (! (DEBUG_CMDBUF && RADEON_DEBUG & DEBUG_STATE) )
+	if (!radeon_is_debug_enabled(RADEON_STATE, RADEON_VERBOSE) )
 		return;
 
 	if (!radeon->radeonScreen->kernel_mm) {
@@ -1018,7 +1018,7 @@ static void radeon_print_state_atom(radeonContextPtr radeon, struct radeon_state
 
 	fprintf(stderr, "  emit %s %d/%d\n", state->name, dwords, state->cmd_size);
 
-	if (DEBUG_CMDBUF > 1 && RADEON_DEBUG & DEBUG_VERBOSE) {
+	if (radeon_is_debug_enabled(RADEON_STATE, RADEON_TRACE)) {
 		if (dwords > state->cmd_size)
 			dwords = state->cmd_size;
 		for (i = 0; i < dwords;) {
@@ -1046,17 +1046,15 @@ GLuint radeonCountStateEmitSize(radeonContextPtr radeon)
 	struct radeon_state_atom *atom;
 	GLuint dwords = 0;
 	/* check if we are going to emit full state */
-	if (DEBUG_CMDBUF && RADEON_DEBUG & DEBUG_VERBOSE)
-		fprintf(stderr, "%s\n", __func__);
 
 	if (radeon->cmdbuf.cs->cdw && !radeon->hw.all_dirty) {
 		if (!radeon->hw.is_dirty)
-			return dwords;
+			goto out;
 		foreach(atom, &radeon->hw.atomlist) {
 			if (atom->dirty) {
 				const GLuint atom_size = atom->check(radeon->glCtx, atom);
 				dwords += atom_size;
-				if (DEBUG_CMDBUF && atom_size) {
+				if (RADEON_CMDBUF && atom_size) {
 					radeon_print_state_atom(radeon, atom);
 				}
 			}
@@ -1065,12 +1063,14 @@ GLuint radeonCountStateEmitSize(radeonContextPtr radeon)
 		foreach(atom, &radeon->hw.atomlist) {
 			const GLuint atom_size = atom->check(radeon->glCtx, atom);
 			dwords += atom_size;
-			if (DEBUG_CMDBUF && atom_size) {
+			if (RADEON_CMDBUF && atom_size) {
 				radeon_print_state_atom(radeon, atom);
 			}
 
 		}
 	}
+out:
+	radeon_print(RADEON_STATE, RADEON_NORMAL, "%s %u\n", __func__, dwords);
 	return dwords;
 }
 
@@ -1092,10 +1092,7 @@ static INLINE void radeon_emit_atom(radeonContextPtr radeon, struct radeon_state
 			END_BATCH();
 		}
 	} else {
-		if (DEBUG_CMDBUF && RADEON_DEBUG & DEBUG_STATE) {
-			fprintf(stderr, "  skip state %s\n",
-					atom->name);
-		}
+		radeon_print(RADEON_STATE, RADEON_VERBOSE, "  skip state %s\n", atom->name);
 	}
 	atom->dirty = GL_FALSE;
 
@@ -1135,8 +1132,7 @@ static GLboolean radeon_revalidate_bos(GLcontext *ctx)
 
 void radeonEmitState(radeonContextPtr radeon)
 {
-	if (RADEON_DEBUG & (DEBUG_STATE|DEBUG_PRIMS))
-		fprintf(stderr, "%s\n", __FUNCTION__);
+	radeon_print(RADEON_STATE, RADEON_NORMAL, "%s\n", __FUNCTION__);
 
 	if (radeon->vtbl.pre_emit_state)
 		radeon->vtbl.pre_emit_state(radeon);
@@ -1146,13 +1142,13 @@ void radeonEmitState(radeonContextPtr radeon)
 		return;
 
 	if (!radeon->cmdbuf.cs->cdw) {
-		if (RADEON_DEBUG & DEBUG_STATE)
+		if (RADEON_DEBUG & RADEON_STATE)
 			fprintf(stderr, "Begin reemit state\n");
 
 		radeonEmitAtoms(radeon, GL_TRUE);
 	} else {
 
-		if (RADEON_DEBUG & DEBUG_STATE)
+		if (RADEON_DEBUG & RADEON_STATE)
 			fprintf(stderr, "Begin dirty state\n");
 
 		radeonEmitAtoms(radeon, GL_FALSE);
@@ -1166,7 +1162,7 @@ void radeonEmitState(radeonContextPtr radeon)
 void radeonFlush(GLcontext *ctx)
 {
 	radeonContextPtr radeon = RADEON_CONTEXT(ctx);
-	if (RADEON_DEBUG & DEBUG_IOCTL)
+	if (RADEON_DEBUG & RADEON_IOCTL)
 		fprintf(stderr, "%s %d\n", __FUNCTION__, radeon->cmdbuf.cs->cdw);
 
 	/* okay if we have no cmds in the buffer &&
@@ -1258,7 +1254,7 @@ int rcommonFlushCmdBufLocked(radeonContextPtr rmesa, const char *caller)
 	}
 	rmesa->cmdbuf.flushing = 1;
 
-	if (RADEON_DEBUG & DEBUG_IOCTL) {
+	if (RADEON_DEBUG & RADEON_IOCTL) {
 		fprintf(stderr, "%s from %s - %i cliprects\n",
 			__FUNCTION__, caller, rmesa->numClipRects);
 	}
@@ -1327,15 +1323,13 @@ void rcommonInitCmdBuf(radeonContextPtr rmesa)
 	if (size > 64 * 256)
 		size = 64 * 256;
 
-	if (RADEON_DEBUG & (DEBUG_IOCTL | DEBUG_DMA)) {
-		fprintf(stderr, "sizeof(drm_r300_cmd_header_t)=%zd\n",
-			sizeof(drm_r300_cmd_header_t));
-		fprintf(stderr, "sizeof(drm_radeon_cmd_buffer_t)=%zd\n",
-			sizeof(drm_radeon_cmd_buffer_t));
-		fprintf(stderr,
+	radeon_print(RADEON_CS, RADEON_VERBOSE,
+			"sizeof(drm_r300_cmd_header_t)=%zd\n", sizeof(drm_r300_cmd_header_t));
+	radeon_print(RADEON_CS, RADEON_VERBOSE,
+			"sizeof(drm_radeon_cmd_buffer_t)=%zd\n", sizeof(drm_radeon_cmd_buffer_t));
+	radeon_print(RADEON_CS, RADEON_VERBOSE,
 			"Allocating %d bytes command buffer (max state is %d bytes)\n",
 			size * 4, rmesa->hw.max_state_size * 4);
-	}
 
 	if (rmesa->radeonScreen->kernel_mm) {
 		int fd = rmesa->radeonScreen->driScreen->fd;
@@ -1388,14 +1382,13 @@ void rcommonBeginBatch(radeonContextPtr rmesa, int n,
 		       int line)
 {
 	if (!rmesa->cmdbuf.cs->cdw && dostate) {
-		if (RADEON_DEBUG & DEBUG_IOCTL)
-			fprintf(stderr, "Reemit state after flush (from %s)\n", function);
+		radeon_print(RADEON_STATE, RADEON_NORMAL,
+				"Reemit state after flush (from %s)\n", function);
 		radeonEmitState(rmesa);
 	}
 	radeon_cs_begin(rmesa->cmdbuf.cs, n, file, function, line);
 
-        if (DEBUG_CMDBUF && RADEON_DEBUG & DEBUG_IOCTL)
-                fprintf(stderr, "BEGIN_BATCH(%d) at %d, from %s:%i\n",
+    radeon_print(RADEON_CS, RADEON_VERBOSE, "BEGIN_BATCH(%d) at %d, from %s:%i\n",
                         n, rmesa->cmdbuf.cs->cdw, function, line);
 
 }

src/mesa/drivers/dri/radeon/radeon_common_context.c
@@ -363,7 +363,7 @@ GLboolean radeonUnbindContext(__DRIcontextPrivate * driContextPriv)
 {
 	radeonContextPtr radeon = (radeonContextPtr) driContextPriv->driverPrivate;
 
-	if (RADEON_DEBUG & DEBUG_DRI)
+	if (RADEON_DEBUG & RADEON_DRI)
 		fprintf(stderr, "%s ctx %p\n", __FUNCTION__,
 			radeon->glCtx);
 
@@ -527,7 +527,7 @@ radeon_update_renderbuffers(__DRIcontext *context, __DRIdrawable *drawable)
 	char *regname;
 	struct radeon_bo *depth_bo = NULL, *bo;
 
-	if (RADEON_DEBUG & DEBUG_DRI)
+	if (RADEON_DEBUG & RADEON_DRI)
 	    fprintf(stderr, "enter %s, drawable %p\n", __func__, drawable);
 
 	draw = drawable->driverPrivate;
@@ -654,7 +654,7 @@ radeon_update_renderbuffers(__DRIcontext *context, __DRIdrawable *drawable)
 				continue;
 		}
 
-		if (RADEON_DEBUG & DEBUG_DRI)
+		if (RADEON_DEBUG & RADEON_DRI)
 			fprintf(stderr,
 				"attaching buffer %s, %d, at %d, cpp %d, pitch %d\n",
 				regname, buffers[i].name, buffers[i].attachment,
@@ -667,7 +667,7 @@ radeon_update_renderbuffers(__DRIcontext *context, __DRIdrawable *drawable)
 		rb->has_surface = 0;
 
 		if (buffers[i].attachment == __DRI_BUFFER_STENCIL && depth_bo) {
-			if (RADEON_DEBUG & DEBUG_DRI)
+			if (RADEON_DEBUG & RADEON_DRI)
 				fprintf(stderr, "(reusing depth buffer as stencil)\n");
 			bo = depth_bo;
 			radeon_bo_ref(bo);
@@ -740,7 +740,7 @@ GLboolean radeonMakeCurrent(__DRIcontextPrivate * driContextPriv,
 	struct gl_framebuffer *readfb;
 
 	if (!driContextPriv) {
-		if (RADEON_DEBUG & DEBUG_DRI)
+		if (RADEON_DEBUG & RADEON_DRI)
 			fprintf(stderr, "%s ctx is null\n", __FUNCTION__);
 		_mesa_make_current(NULL, NULL, NULL);
 		return GL_TRUE;
@@ -762,7 +762,7 @@ GLboolean radeonMakeCurrent(__DRIcontextPrivate * driContextPriv,
 		radeon_make_renderbuffer_current(radeon, drfb);
 	}
 
-	if (RADEON_DEBUG & DEBUG_DRI)
+	if (RADEON_DEBUG & RADEON_DRI)
 	     fprintf(stderr, "%s ctx %p dfb %p rfb %p\n", __FUNCTION__, radeon->glCtx, drfb, readfb);
 
 	driUpdateFramebufferSize(radeon->glCtx, driDrawPriv);
@@ -797,7 +797,7 @@ GLboolean radeonMakeCurrent(__DRIcontextPrivate * driContextPriv,
 	}
 
 
-	if (RADEON_DEBUG & DEBUG_DRI)
+	if (RADEON_DEBUG & RADEON_DRI)
 		fprintf(stderr, "End %s\n", __FUNCTION__);
 
 	return GL_TRUE;

src/mesa/drivers/dri/radeon/radeon_common_context.h
@@ -8,6 +8,7 @@
 #include "tnl/t_context.h"
 #include "main/colormac.h"
 
+#include "radeon_debug.h"
 #include "radeon_screen.h"
 #include "radeon_drm.h"
 #include "dri_util.h"
@@ -17,22 +18,6 @@
 struct radeon_context;
 
 #include "radeon_bocs_wrapper.h"
-
-/* From http://gcc. gnu.org/onlinedocs/gcc-3.2.3/gcc/Variadic-Macros.html .
-   I suppose we could inline this and use macro to fetch out __LINE__ and stuff in case we run into trouble
-   with other compilers ... GLUE!
-*/
-#define WARN_ONCE(a, ...)	{ \
-	static int warn##__LINE__=1; \
-	if(warn##__LINE__){ \
-		fprintf(stderr, "*********************************WARN_ONCE*********************************\n"); \
-		fprintf(stderr, "File %s function %s line %d\n", \
-			__FILE__, __FUNCTION__, __LINE__); \
-		fprintf(stderr,  a, ## __VA_ARGS__);\
-		fprintf(stderr, "***************************************************************************\n"); \
-		warn##__LINE__=0;\
-		} \
-	}
 
 /* This union is used to avoid warnings/miscompilation
    with float to uint32_t casts due to strict-aliasing */
@@ -401,23 +386,6 @@ struct radeon_dri_mirror {
 	int drmMinor;
 };
 
-#define DEBUG_TEXTURE	0x001
-#define DEBUG_STATE	0x002
-#define DEBUG_IOCTL	0x004
-#define DEBUG_PRIMS	0x008
-#define DEBUG_VERTS	0x010
-#define DEBUG_FALLBACKS	0x020
-#define DEBUG_VFMT	0x040
-#define DEBUG_CODEGEN	0x080
-#define DEBUG_VERBOSE	0x100
-#define DEBUG_DRI       0x200
-#define DEBUG_DMA       0x400
-#define DEBUG_SANITY    0x800
-#define DEBUG_SYNC      0x1000
-#define DEBUG_PIXEL     0x2000
-#define DEBUG_MEMORY    0x4000
-
-
 typedef void (*radeon_tri_func) (radeonContextPtr,
 				 radeonVertex *,
 				 radeonVertex *, radeonVertex *);
@@ -620,16 +588,5 @@ GLboolean radeonMakeCurrent(__DRIcontextPrivate * driContextPriv,
 			    __DRIdrawablePrivate * driDrawPriv,
 			    __DRIdrawablePrivate * driReadPriv);
 extern void radeonDestroyContext(__DRIcontextPrivate * driContextPriv);
-
-/* ================================================================
- * Debugging:
- */
-#define DO_DEBUG		1
-
-#if DO_DEBUG
-extern int RADEON_DEBUG;
-#else
-#define RADEON_DEBUG		0
-#endif
 
 #endif

src/mesa/drivers/dri/radeon/radeon_debug.c
@@ -35,6 +35,7 @@ static const struct dri_debug_control debug_control[] = {
 	{"fall", RADEON_FALLBACKS},
 	{"tex", RADEON_TEXTURE},
 	{"ioctl", RADEON_IOCTL},
+	{"verts", RADEON_RENDER},
 	{"render", RADEON_RENDER},
 	{"swrender", RADEON_SWRENDER},
 	{"state", RADEON_STATE},

src/mesa/drivers/dri/radeon/radeon_debug.h
@@ -49,22 +49,23 @@ typedef enum radeon_debug_levels {
 #endif
 
 typedef enum radeon_debug_types {
-	RADEON_TEXTURE   = 0x0001,
-	RADEON_STATE     = 0x0002,
-	RADEON_IOCTL     = 0x0004,
-	RADEON_RENDER    = 0x0008,
-	RADEON_SWRENDER  = 0x0010,
-	RADEON_FALLBACKS = 0x0020,
-	RADEON_VFMT      = 0x0040,
-	RADEON_SHADER    = 0x0080,
-	RADEON_CS        = 0x0100,
-	RADEON_DRI       = 0x0200,
-	RADEON_DMA       = 0x0400,
-	RADEON_SANITY    = 0x0800,
-	RADEON_SYNC      = 0x1000,
-	RADEON_PIXEL     = 0x2000,
-	RADEON_MEMORY    = 0x4000,
-	RADEON_GENERAL   = 0x8000   /* Used for errors and warnings */
+	RADEON_TEXTURE   = 0x00001,
+	RADEON_STATE     = 0x00002,
+	RADEON_IOCTL     = 0x00004,
+	RADEON_RENDER    = 0x00008,
+	RADEON_SWRENDER  = 0x00010,
+	RADEON_FALLBACKS = 0x00020,
+	RADEON_VFMT      = 0x00040,
+	RADEON_SHADER    = 0x00080,
+	RADEON_CS        = 0x00100,
+	RADEON_DRI       = 0x00200,
+	RADEON_DMA       = 0x00400,
+	RADEON_SANITY    = 0x00800,
+	RADEON_SYNC      = 0x01000,
+	RADEON_PIXEL     = 0x02000,
+	RADEON_MEMORY    = 0x04000,
+	RADEON_VERTS     = 0x08000,
+	RADEON_GENERAL   = 0x10000   /* Used for errors and warnings */
 } radeon_debug_type_t;
 
 extern radeon_debug_type_t radeon_enabled_debug_types;

openharmony/third_party_mesa3d

commit 86480356d4dee0f09644da886414ed87e35a41f0

radeon: remove left over dead code
Fixes: 4e0d99a63588c67a955f "r100: Use shared debug code"
Cc: Pauli Nieminen <suokkos@gmail.com>
Signed-off-by: Eric Engestrom <eric.engestrom@imgtec.com>
Reviewed-by: Eric Anholt <eric@anholt.net>

src/mesa/drivers/dri/radeon/radeon_context.c
@@ -330,12 +330,6 @@ r100CreateContext( gl_api api,
 
    rmesa->radeon.do_usleeps = (fthrottle_mode == DRI_CONF_FTHROTTLE_USLEEPS);
 
-
-#if DO_DEBUG
-   RADEON_DEBUG = parse_debug_string( getenv( "RADEON_DEBUG" ),
-                                      debug_control );
-#endif
-
    tcl_mode = driQueryOptioni(&rmesa->radeon.optionCache, "tcl_mode");
    if (driQueryOptionb(&rmesa->radeon.optionCache, "no_rast")) {
       fprintf(stderr, "disabling 3D acceleration\n");