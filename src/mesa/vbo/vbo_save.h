openharmony/third_party_mesa3d

commit a154a836ccd2a703588e361dada638c17a413b67

vbo: optimize some display list drawing (v2)
The vbo_save_vertex_list structure records one or more glBegin/End
primitives which all have the same vertex format.

To draw these primitives, we setup the vertex array state, then
issue the drawing command.  Before, the 'start' vertex was typically
zero and we used the vertex array pointer to indicate where the
vertex data starts.

This patch checks if the vertex buffer offset is an exact multiple of
the vertex size.  If so, that means we can use zero-based vertex array
pointers and use the draw's start value to indicate where the vertex
data starts.

This means a series of display list drawing commands may have
identical vertex array state.  This will get filtered out by the
Gallium CSO module so we can issue a tight series of drawing commands
without state changes to the device.

Note that this also works for a series of glCallList commands (not
just one list that contains multiple glBegin/End pairs).

No Piglit or conform changes.

v2: minor fixes suggested by Ian.

Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

src/mesa/vbo/vbo_save.h
@@ -86,6 +86,20 @@ struct vbo_save_vertex_list {
    struct vbo_save_primitive_store *prim_store;
 };
 
+
+/**
+ * Is the vertex lists's buffer offset an exact multiple of the
+ * vertex size (in bytes)?  This is used to check for a vertex array /
+ * drawing optimization.
+ */
+static inline bool
+aligned_vertex_buffer_offset(const struct vbo_save_vertex_list *node)
+{
+   unsigned vertex_size = node->vertex_size * sizeof(GLfloat); /* in bytes */
+   return vertex_size != 0 && node->buffer_offset % vertex_size == 0;
+}
+
+
 /* These buffers should be a reasonable size to support upload to
  * hardware.  Current vbo implementation will re-upload on any
  * changes, so don't make too big or apps which dynamically create

src/mesa/vbo/vbo_save_api.c
@@ -546,6 +546,20 @@ compile_vertex_list(struct gl_context *ctx)
       save->prim_store = alloc_prim_store();
    }
 
+   /*
+    * If the vertex buffer offset is a multiple of the vertex size,
+    * we can use the _mesa_prim::start value to indicate where the
+    * vertices starts, instead of the buffer offset.  Also see the
+    * bind_vertex_list() function.
+    */
+   if (aligned_vertex_buffer_offset(node)) {
+      const unsigned start_offset =
+         node->buffer_offset / (node->vertex_size * sizeof(GLfloat));
+      for (unsigned i = 0; i < save->prim_count; i++) {
+         save->prims[i].start += start_offset;
+      }
+   }
+
    /* Reset our structures for the next run of vertices:
     */
    reset_counters(ctx);

src/mesa/vbo/vbo_save_draw.c
@@ -146,6 +146,18 @@ bind_vertex_list(struct gl_context *ctx,
    memcpy(node_attrsz, node->attrsz, sizeof(node->attrsz));
    memcpy(node_attrtype, node->attrtype, sizeof(node->attrtype));
 
+   if (aligned_vertex_buffer_offset(node)) {
+      /* The vertex size is an exact multiple of the buffer offset.
+       * This means that we can use zero-based vertex attribute pointers
+       * and specify the start of the primitive with the _mesa_prim::start
+       * field.  This results in issuing several draw calls with identical
+       * vertex attribute information.  This can result in fewer state
+       * changes in drivers.  In particular, the Gallium CSO module will
+       * filter out redundant vertex buffer changes.
+       */
+      buffer_offset = 0;
+   }
+
    /* Install the default (ie Current) attributes first, then overlay
     * all active ones.
     */

commit 30c0a67fedda201860e09efedabe41836378d780

vbo: fix incorrect min/max_index values in display list draw call
This fixes another regression from commit a154a836ccd2 ("vbo: optimize
some display list drawing").  The problem was the min_index, max_index
values passed to the vbo drawing function were not computed to compensate
for the biased prim::start values.

https://bugs.freedesktop.org/show_bug.cgi?id=104746
https://bugs.freedesktop.org/show_bug.cgi?id=104742
https://bugs.freedesktop.org/show_bug.cgi?id=104690
Tested-by: Clayton Craft <clayton.a.craft@intel.com>
Fixes: a154a836ccd2 ("vbo: optimize some display list drawing")
Reviewed-by: Emil Velikov <emil.velikov@collabora.co.uk>

src/mesa/vbo/vbo_save.h
@@ -74,7 +74,8 @@ struct vbo_save_vertex_list {
    GLuint current_size;
 
    GLuint buffer_offset;        /**< in bytes */
-   GLuint vertex_count;
+   GLuint start_vertex;         /**< first vertex used by any primitive */
+   GLuint vertex_count;         /**< number of vertices in this list */
    GLuint wrap_count;		/* number of copied vertices at start */
    GLboolean dangling_attr_ref;	/* current attr implicitly referenced
                                    outside the list */

src/mesa/vbo/vbo_save_api.c
@@ -563,6 +563,9 @@ compile_vertex_list(struct gl_context *ctx)
       for (unsigned i = 0; i < save->prim_count; i++) {
          save->prims[i].start += start_offset;
       }
+      node->start_vertex = start_offset;
+   } else {
+      node->start_vertex = 0;
    }
 
    /* Reset our structures for the next run of vertices:

src/mesa/vbo/vbo_save_draw.c
@@ -325,13 +325,14 @@ vbo_save_playback_vertex_list(struct gl_context *ctx, void *data)
          _mesa_update_state(ctx);
 
       if (node->vertex_count > 0) {
+         GLuint min_index = node->start_vertex;
+         GLuint max_index = min_index + node->vertex_count - 1;
          vbo_context(ctx)->draw_prims(ctx,
                                       node->prims,
                                       node->prim_count,
                                       NULL,
                                       GL_TRUE,
-                                      0,    /* Node is a VBO, so this is ok */
-                                      node->vertex_count - 1,
+                                      min_index, max_index,
                                       NULL, 0, NULL);
       }
    }