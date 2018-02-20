openharmony/third_party_mesa3d

commit 1032d031f71a4ed3362fda13e4f0789f81855d5c

svga: Relax the format checks for copy_region_vgpu10 somewhat
The new generic checks were actually more restrictive than the previous svga-
specific tests and not vice versa. So bypass the common format checks for
copy_region_vgpu10.

Signed-off-by: Thomas Hellstrom <thellstrom@vmware.com>
Reviewed-by: Neha Bhende <bhenden@vmware.com>

src/gallium/drivers/svga/svga_pipe_blit.c
@@ -275,8 +275,17 @@ static bool
 can_blit_via_svga_copy_region(struct svga_context *svga,
                               const struct pipe_blit_info *blit_info)
 {
-   if (!util_can_blit_via_copy_region(blit_info, FALSE) &&
-       !util_can_blit_via_copy_region(blit_info, TRUE))
+   struct pipe_blit_info local_blit = *blit_info;
+
+   /* First basic checks to catch incompatibilities in new or locally unchecked
+    * struct pipe_blit_info members but bypass the format check here.
+    * Also since util_can_blit_via_copy_region() requires a dimension match,
+    * PIPE_FILTER_LINEAR should be equal to PIPE_FILTER_NEAREST.
+    */
+   local_blit.dst.format = local_blit.src.format;
+   if (local_blit.filter == PIPE_TEX_FILTER_LINEAR)
+      local_blit.filter = PIPE_TEX_FILTER_NEAREST;
+   if (!util_can_blit_via_copy_region(&local_blit, TRUE))
       return false;
 
    /* For depth+stencil formats, copy with mask != PIPE_MASK_ZS is not
@@ -330,6 +339,21 @@ can_blit_via_surface_copy(struct svga_context *svga,
                           const struct pipe_blit_info *blit_info)
 {
    struct svga_texture *dtex, *stex;
+
+   /* Mimic the format tests in util_can_blit_via_copy_region(), but
+    * skip the other tests that have already been performed.
+    */
+   if (blit_info->src.format != blit_info->dst.format) {
+      const struct util_format_description *src_desc, *dst_desc;
+
+      src_desc = util_format_description(blit_info->src.resource->format);
+      dst_desc = util_format_description(blit_info->dst.resource->format);
+
+      if (blit_info->src.resource->format != blit_info->src.format ||
+          blit_info->dst.resource->format != blit_info->dst.format ||
+          !util_is_format_compatible(src_desc, dst_desc));
+      return FALSE;
+   }
 
    if (svga->render_condition && blit_info->render_condition_enable)
       return false;

commit bfa52388dda086749625c672be2af3ef494c1ab4

svga: Fix a leftover debug hack
Fix what appears to be a leftover debug hack.
The hack would force the driver to take a different blit path; possibly,
although unverified, reverting to software blits.

Tested using piglit tests/quick. No related regressions.

Cc: "17.2 17.3 18.0" <mesa-stable@lists.freedesktop.org>
Fixes: 1032d031f7 (svga: Relax the format checks for copy_region_vgpu10 somewhat)
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=104625
Reported-by: Grazvydas Ignotas <notasas@gmail.com>
Signed-off-by: Thomas Hellstrom <thellstrom@vmware.com>
Reviewed-by: Brian Paul <brianp@vmware.com>

src/gallium/drivers/svga/svga_pipe_blit.c
@@ -351,8 +351,8 @@ can_blit_via_surface_copy(struct svga_context *svga,
 
       if (blit_info->src.resource->format != blit_info->src.format ||
           blit_info->dst.resource->format != blit_info->dst.format ||
-          !util_is_format_compatible(src_desc, dst_desc));
-      return FALSE;
+          !util_is_format_compatible(src_desc, dst_desc))
+         return false;
    }
 
    if (svga->render_condition && blit_info->render_condition_enable)