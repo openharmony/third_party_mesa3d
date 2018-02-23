openharmony/third_party_mesa3d

commit 3bd3563812514030b5271228e45aa8612d8147a7

gbm/dri: Add RGBA masks to GBM format table
Eventually, we can replace the visuals list inside GBM EGL driver with
this one.

Signed-off-by: Daniel Stone <daniels@collabora.com>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
Tested-by: Ilia Mirkin <imirkin@alum.mit.edu>

src/gbm/backends/dri/gbm_dri.c
@@ -546,16 +546,44 @@ dri_screen_create_sw(struct gbm_dri_device *dri)
 static const struct {
    uint32_t gbm_format;
    int dri_image_format;
+   uint32_t rgba_masks[4];
 } gbm_to_dri_image_formats[] = {
-   { GBM_FORMAT_R8,          __DRI_IMAGE_FORMAT_R8          },
-   { GBM_FORMAT_GR88,        __DRI_IMAGE_FORMAT_GR88        },
-   { GBM_FORMAT_RGB565,      __DRI_IMAGE_FORMAT_RGB565      },
-   { GBM_FORMAT_XRGB8888,    __DRI_IMAGE_FORMAT_XRGB8888    },
-   { GBM_FORMAT_ARGB8888,    __DRI_IMAGE_FORMAT_ARGB8888    },
-   { GBM_FORMAT_XBGR8888,    __DRI_IMAGE_FORMAT_XBGR8888    },
-   { GBM_FORMAT_ABGR8888,    __DRI_IMAGE_FORMAT_ABGR8888    },
-   { GBM_FORMAT_XRGB2101010, __DRI_IMAGE_FORMAT_XRGB2101010 },
-   { GBM_FORMAT_ARGB2101010, __DRI_IMAGE_FORMAT_ARGB2101010 },
+   {
+     GBM_FORMAT_R8, __DRI_IMAGE_FORMAT_R8,
+     { 0x000000ff, 0x00000000, 0x00000000, 0x00000000 },
+   },
+   {
+     GBM_FORMAT_GR88, __DRI_IMAGE_FORMAT_GR88,
+     { 0x000000ff, 0x0000ff00, 0x00000000, 0x00000000 },
+   },
+   {
+     GBM_FORMAT_RGB565, __DRI_IMAGE_FORMAT_RGB565,
+     { 0x0000f800, 0x000007e0, 0x0000001f, 0x00000000 },
+   },
+   {
+     GBM_FORMAT_XRGB8888, __DRI_IMAGE_FORMAT_XRGB8888,
+     { 0x00ff0000, 0x0000ff00, 0x000000ff, 0x00000000 },
+   },
+   {
+     GBM_FORMAT_ARGB8888, __DRI_IMAGE_FORMAT_ARGB8888,
+     { 0x00ff0000, 0x0000ff00, 0x000000ff, 0x000000ff },
+   },
+   {
+     GBM_FORMAT_XBGR8888, __DRI_IMAGE_FORMAT_XBGR8888,
+     { 0x000000ff, 0x0000ff00, 0x00ff0000, 0x00000000 },
+   },
+   {
+     GBM_FORMAT_ABGR8888, __DRI_IMAGE_FORMAT_ABGR8888,
+     { 0x000000ff, 0x0000ff00, 0x00ff0000, 0x000000ff },
+   },
+   {
+     GBM_FORMAT_XRGB2101010, __DRI_IMAGE_FORMAT_XRGB2101010,
+     { 0x3ff00000, 0x000ffc00, 0x000003ff, 0x00000000 },
+   },
+   {
+     GBM_FORMAT_ARGB2101010, __DRI_IMAGE_FORMAT_ARGB2101010,
+     { 0x3ff00000, 0x000ffc00, 0x000003ff, 0xc0000000 },
+   },
 };
 
 /* The two GBM_BO_FORMAT_[XA]RGB8888 formats alias the GBM_FORMAT_*

commit a5b9c848125daf458f5ee89dbb5f1301640b5572

gbm: Fix the alpha masks in the GBM format table.
Once GBM started looking at the values of the alpha masks, ARGB/ABGR
wouldn't match any more because we had both A and R in the low bits.

Fixes: 3bd356381251 ("gbm/dri: Add RGBA masks to GBM format table")
Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>
Reviewed-by: Daniel Stone <daniels@collabora.com>

src/gbm/backends/dri/gbm_dri.c
@@ -562,7 +562,7 @@ static const struct gbm_dri_visual gbm_dri_visuals_table[] = {
    },
    {
      GBM_FORMAT_ARGB8888, __DRI_IMAGE_FORMAT_ARGB8888,
-     { 0x00ff0000, 0x0000ff00, 0x000000ff, 0x000000ff },
+     { 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000 },
    },
    {
      GBM_FORMAT_XBGR8888, __DRI_IMAGE_FORMAT_XBGR8888,
@@ -570,7 +570,7 @@ static const struct gbm_dri_visual gbm_dri_visuals_table[] = {
    },
    {
      GBM_FORMAT_ABGR8888, __DRI_IMAGE_FORMAT_ABGR8888,
-     { 0x000000ff, 0x0000ff00, 0x00ff0000, 0x000000ff },
+     { 0x000000ff, 0x0000ff00, 0x00ff0000, 0xff000000 },
    },
    {
      GBM_FORMAT_XRGB2101010, __DRI_IMAGE_FORMAT_XRGB2101010,