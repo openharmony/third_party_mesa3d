openharmony/third_party_mesa3d

commit e784f4bdb923576ec92bc8bd2a52143f4c2d3367

radeon/vce: move destroy command before feedback command
VCE processing IBs starts from session and task info at first level,
other commands processed subsequently. The task info for destroy is
embedded to destroy command, resulting that feedback command is not
properly procoessed. This is causing kernel spin VM fault messages on
Polaris and Vega10 card when running ends at encode application.

The fix is also verified on VCE physical mode card.

Signed-off-by: Leo Liu <leo.liu@amd.com>
Cc: mesa-stable@lists.freedesktop.org
Acked-by: Christian König <christian.koenig@amd.com>

src/gallium/drivers/radeon/radeon_vce.c
@@ -246,8 +246,8 @@ static void rvce_destroy(struct pipe_video_codec *encoder)
 		si_vid_create_buffer(enc->screen, &fb, 512, PIPE_USAGE_STAGING);
 		enc->fb = &fb;
 		enc->session(enc);
-		enc->feedback(enc);
 		enc->destroy(enc);
+		enc->feedback(enc);
 		flush(enc);
 		si_vid_destroy_buffer(&fb);
 	}

commit 18227ac3dba39aa5b0593e17f448f841ddb9f47e

radeon/vce: move feedback command inside of destroy function
On the CI family, firmware requires the destory command have to be the
last command in the IB, moving feedback command after destroy is causing
issues on CI cards, so we have to keep the previous logic that moves
destroy back to the last command.

But as the original issue fixed previously, with the newer family like Vega10,
feedback command have to be included inside of the task info command along
with destroy command.

Fixes: e784f4bd("radeon/vce: move destroy command before feedback command")

Signed-off-by: Leo Liu <leo.liu@amd.com>
Acked-by: Christian König <christian.koenig@amd.com>
Cc: mesa-stable@lists.freedesktop.org

src/gallium/drivers/radeon/radeon_vce.c
@@ -247,7 +247,6 @@ static void rvce_destroy(struct pipe_video_codec *encoder)
 		enc->fb = &fb;
 		enc->session(enc);
 		enc->destroy(enc);
-		enc->feedback(enc);
 		flush(enc);
 		si_vid_destroy_buffer(&fb);
 	}

src/gallium/drivers/radeon/radeon_vce_40_2_2.c
@@ -421,6 +421,8 @@ static void destroy(struct rvce_encoder *enc)
 {
 	enc->task_info(enc, 0x00000001, 0, 0, 0);
 
+	feedback(enc);
+
 	RVCE_BEGIN(0x02000001); // destroy
 	RVCE_END();
 }

src/gallium/drivers/radeon/radeon_vce_52.c
@@ -458,19 +458,21 @@ static void config_extension(struct rvce_encoder *enc)
 	RVCE_END();
 }
 
-static void destroy(struct rvce_encoder *enc)
-{
-	enc->task_info(enc, 0x00000001, 0, 0, 0);
-
-	RVCE_BEGIN(0x02000001); // destroy
-	RVCE_END();
-}
-
 static void feedback(struct rvce_encoder *enc)
 {
 	RVCE_BEGIN(0x05000005); // feedback buffer
 	RVCE_WRITE(enc->fb->res->buf, enc->fb->res->domains, 0x0); // feedbackRingAddressHi/Lo
 	RVCE_CS(enc->enc_pic.fb.feedback_ring_size);
+	RVCE_END();
+}
+
+static void destroy(struct rvce_encoder *enc)
+{
+	enc->task_info(enc, 0x00000001, 0, 0, 0);
+
+	feedback(enc);
+
+	RVCE_BEGIN(0x02000001); // destroy
 	RVCE_END();
 }
 