openharmony/third_party_mesa3d

commit 0a01eb621d83854ba228e249ee8cac667b2e6d19

freedreno/ir3: eliminate unused false-deps
Previously false-dependencies would get flagged as used, even if the
only "use" was a false dep to (for example) prevent a load from being
scheduled after a store.

In addition to being pointless instructions, in some cases they can
cause problems.  For example, ldg (and similar instructions) depend on
an immed arg getting CP'd into the instruction, but this doesn't happen
if an instruction is otherwise unused.  Which can result in undefined
results (overwriting unintended registers).

Signed-off-by: Rob Clark <robdclark@gmail.com>

src/gallium/drivers/freedreno/ir3/ir3_depth.c
@@ -133,13 +133,18 @@ ir3_insert_by_depth(struct ir3_instruction *instr, struct list_head *list)
 }
 
 static void
-ir3_instr_depth(struct ir3_instruction *instr, unsigned boost)
+ir3_instr_depth(struct ir3_instruction *instr, unsigned boost, bool falsedep)
 {
 	struct ir3_instruction *src;
 
 	/* if we've already visited this instruction, bail now: */
-	if (ir3_instr_check_mark(instr))
+	if (falsedep) {
+		/* don't mark falsedep's as used, but process them normally: */
+		if (instr->flags & IR3_INSTR_MARK)
+			return;
+	} else if (ir3_instr_check_mark(instr)) {
 		return;
+	}
 
 	instr->depth = 0;
 
@@ -147,7 +152,7 @@ ir3_instr_depth(struct ir3_instruction *instr, unsigned boost)
 		unsigned sd;
 
 		/* visit child to compute it's depth: */
-		ir3_instr_depth(src, boost);
+		ir3_instr_depth(src, boost, __is_false_dep(instr, i));
 
 		/* for array writes, no need to delay on previous write: */
 		if (i == 0)
@@ -165,9 +170,10 @@ ir3_instr_depth(struct ir3_instruction *instr, unsigned boost)
 	ir3_insert_by_depth(instr, &instr->block->instr_list);
 }
 
-static void
+static bool
 remove_unused_by_block(struct ir3_block *block)
 {
+	bool progress = false;
 	list_for_each_entry_safe (struct ir3_instruction, instr, &block->instr_list, node) {
 		if (!ir3_instr_check_mark(instr)) {
 			if (instr->opc == OPC_END)
@@ -178,32 +184,35 @@ remove_unused_by_block(struct ir3_block *block)
 			instr->flags |= IR3_INSTR_UNUSED;
 			/* and remove from instruction list: */
 			list_delinit(&instr->node);
+			progress = true;
 		}
 	}
+	return progress;
 }
 
-void
-ir3_depth(struct ir3 *ir)
+static bool
+compute_depth_and_remove_unused(struct ir3 *ir)
 {
 	unsigned i;
+	bool progress = false;
 
 	ir3_clear_mark(ir);
 	for (i = 0; i < ir->noutputs; i++)
 		if (ir->outputs[i])
-			ir3_instr_depth(ir->outputs[i], 0);
+			ir3_instr_depth(ir->outputs[i], 0, false);
 
 	list_for_each_entry (struct ir3_block, block, &ir->block_list, node) {
 		for (i = 0; i < block->keeps_count; i++)
-			ir3_instr_depth(block->keeps[i], 0);
+			ir3_instr_depth(block->keeps[i], 0, false);
 
 		/* We also need to account for if-condition: */
 		if (block->condition)
-			ir3_instr_depth(block->condition, 6);
+			ir3_instr_depth(block->condition, 6, false);
 	}
 
 	/* mark un-used instructions: */
 	list_for_each_entry (struct ir3_block, block, &ir->block_list, node) {
-		remove_unused_by_block(block);
+		progress |= remove_unused_by_block(block);
 	}
 
 	/* note that we can end up with unused indirects, but we should
@@ -221,4 +230,15 @@ ir3_depth(struct ir3 *ir)
 		if (in && (in->flags & IR3_INSTR_UNUSED))
 			ir->inputs[i] = NULL;
 	}
+
+	return progress;
+}
+
+void
+ir3_depth(struct ir3 *ir)
+{
+	bool progress;
+	do {
+		progress = compute_depth_and_remove_unused(ir);
+	} while (progress);
 }

src/gallium/drivers/freedreno/ir3/ir3_cp.c
@@ -607,7 +607,7 @@ ir3_cp(struct ir3 *ir, struct ir3_shader_variant *so)
 		list_for_each_entry (struct ir3_instruction, instr, &block->instr_list, node) {
 			struct ir3_instruction *src;
 
-			/* by the way, we don't acount for false-dep's, so the CP
+			/* by the way, we don't account for false-dep's, so the CP
 			 * pass should always happen before false-dep's are inserted
 			 */
 			debug_assert(instr->deps_count == 0);

commit c09981ba23bc03661c5a1c454ad165573a6a11d4

freedreno/ir3: fix fallout of unused false-depth elimination
Since we were MARK flag for both preventing loops, and tracking whether
instructions were used, we could end up in an infinite loop due to
0a01eb621d.  Instead invert the logic.. mark all instructions UNUSED
up front and clear the flag as we visit them.

Fixes: 0a01eb621d freedreno/ir3: eliminate unused false-deps
Signed-off-by: Rob Clark <robdclark@gmail.com>

src/gallium/drivers/freedreno/ir3/ir3.h
@@ -762,7 +762,6 @@ static inline bool writes_pred(struct ir3_instruction *instr)
 static inline struct ir3_instruction *ssa(struct ir3_register *reg)
 {
 	if (reg->flags & (IR3_REG_SSA | IR3_REG_ARRAY)) {
-		debug_assert(!(reg->instr && (reg->instr->flags & IR3_INSTR_UNUSED)));
 		return reg->instr;
 	}
 	return NULL;

src/gallium/drivers/freedreno/ir3/ir3_depth.c
@@ -137,14 +137,12 @@ ir3_instr_depth(struct ir3_instruction *instr, unsigned boost, bool falsedep)
 {
 	struct ir3_instruction *src;
 
-	/* if we've already visited this instruction, bail now: */
-	if (falsedep) {
-		/* don't mark falsedep's as used, but process them normally: */
-		if (instr->flags & IR3_INSTR_MARK)
-			return;
-	} else if (ir3_instr_check_mark(instr)) {
+	/* don't mark falsedep's as used, but otherwise process them normally: */
+	if (!falsedep)
+		instr->flags &= ~IR3_INSTR_UNUSED;
+
+	if (ir3_instr_check_mark(instr))
 		return;
-	}
 
 	instr->depth = 0;
 
@@ -175,14 +173,9 @@ remove_unused_by_block(struct ir3_block *block)
 {
 	bool progress = false;
 	list_for_each_entry_safe (struct ir3_instruction, instr, &block->instr_list, node) {
-		if (!ir3_instr_check_mark(instr)) {
-			if (instr->opc == OPC_END)
-				continue;
-			/* mark it, in case it is input, so we can
-			 * remove unused inputs:
-			 */
-			instr->flags |= IR3_INSTR_UNUSED;
-			/* and remove from instruction list: */
+		if (instr->opc == OPC_END)
+			continue;
+		if (instr->flags & IR3_INSTR_UNUSED) {
 			list_delinit(&instr->node);
 			progress = true;
 		}
@@ -197,6 +190,16 @@ compute_depth_and_remove_unused(struct ir3 *ir)
 	bool progress = false;
 
 	ir3_clear_mark(ir);
+
+	/* initially mark everything as unused, we'll clear the flag as we
+	 * visit the instructions:
+	 */
+	list_for_each_entry (struct ir3_block, block, &ir->block_list, node) {
+		list_for_each_entry (struct ir3_instruction, instr, &block->instr_list, node) {
+			instr->flags |= IR3_INSTR_UNUSED;
+		}
+	}
+
 	for (i = 0; i < ir->noutputs; i++)
 		if (ir->outputs[i])
 			ir3_instr_depth(ir->outputs[i], 0, false);
@@ -220,7 +223,7 @@ compute_depth_and_remove_unused(struct ir3 *ir)
 	 */
 	for (i = 0; i < ir->indirects_count; i++) {
 		struct ir3_instruction *instr = ir->indirects[i];
-		if (instr->flags & IR3_INSTR_UNUSED)
+		if (instr && (instr->flags & IR3_INSTR_UNUSED))
 			ir->indirects[i] = NULL;
 	}
 