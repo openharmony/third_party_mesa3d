openharmony/third_party_mesa3d

commit ed75afc4fa786e8982f9aacdc5d38de8eed48524

meson: extend install_megadrivers script to handle symmlinking
Which is required for the gallium media state trackers.

v2: - Make symlinks local instead of absolute

Signed-off-by: Dylan Baker <dylanx.c.baker@intel.com>
Reviewed-by: Eric Engestrom <eric.engestrom@imgtec.com>

bin/install_megadrivers.py
@@ -44,10 +44,24 @@ def main():
 
     for each in args.drivers:
         driver = os.path.join(to, each)
+
         if os.path.exists(driver):
             os.unlink(driver)
         print('installing {} to {}'.format(args.megadriver, driver))
         os.link(master, driver)
+
+        try:
+            ret = os.getcwd()
+            os.chdir(to)
+
+            name, ext = os.path.splitext(each)
+            while ext != '.so':
+                if os.path.exists(name):
+                    os.unlink(name)
+                os.symlink(driver, name)
+                name, ext = os.path.splitext(name)
+        finally:
+            os.chdir(ret)
     os.unlink(master)
 
 

commit 6412ef63ef6c2b9325ee348796d730a7764392ab

meson: fix megadriver symlinking
Which should be relative instead of absolute.

Fixes: ed75afc4fa786e8982f9aacdc5d38de8eed48524
       ("meson: extend install_megadrivers script to handle symmlinking")
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=105567
Signed-off-by: Dylan Baker <dylan.c.baker@intel.com>
Reviewed-and-Tested-by: Eric Engestrom <eric.engestrom@imgtec.com>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

bin/install_megadrivers.py
@@ -58,7 +58,7 @@ def main():
             while ext != '.so':
                 if os.path.exists(name):
                     os.unlink(name)
-                os.symlink(driver, name)
+                os.symlink(each, name)
                 name, ext = os.path.splitext(name)
         finally:
             os.chdir(ret)