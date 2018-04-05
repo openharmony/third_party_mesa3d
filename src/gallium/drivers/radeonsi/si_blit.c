openharmony/third_party_mesa3d

commit 53475a5525ed5bd4e0fac0fa2b01e1c541e86a9f

radeonsi: skip DCC render feedback checking if color writes are disabled

src/gallium/drivers/radeonsi/si_blit.c
@@ -713,6 +713,11 @@ static void si_check_render_feedback_resident_images(struct si_context *sctx)
 
 static void si_check_render_feedback(struct si_context *sctx)
 {
+	/* There is no render feedback if color writes are disabled.
+	 * (e.g. a pixel shader with image stores)
+	 */
+	if (!si_get_total_colormask(sctx))
+		return;
 
 	if (!sctx->need_check_render_feedback)
 		return;

src/gallium/drivers/radeonsi/si_pipe.h
@@ -940,4 +940,21 @@ static inline unsigned si_get_ps_iter_samples(struct si_context *sctx)
 	return sctx->ps_iter_samples;
 }
 
+static inline unsigned si_get_total_colormask(struct si_context *sctx)
+{
+	if (sctx->queued.named.rasterizer->rasterizer_discard)
+		return 0;
+
+	struct si_shader_selector *ps = sctx->ps_shader.cso;
+	unsigned colormask = sctx->framebuffer.colorbuf_enabled_4bit &
+			     sctx->queued.named.blend->cb_target_mask;
+
+	if (!ps->info.properties[TGSI_PROPERTY_FS_COLOR0_WRITES_ALL_CBUFS])
+		colormask &= ps->colors_written_4bit;
+	else if (!ps->colors_written_4bit)
+		colormask = 0; /* color0 writes all cbufs, but it's not written */
+
+	return colormask;
+}
+
 #endif

src/gallium/drivers/radeonsi/si_state_shaders.c
@@ -1215,11 +1215,7 @@ static void si_shader_selector_key_hw_vs(struct si_context *sctx,
 				      ps->info.writes_samplemask ||
 				      alpha_to_coverage ||
 				      si_get_alpha_test_func(sctx) != PIPE_FUNC_ALWAYS;
-
-		unsigned ps_colormask = sctx->framebuffer.colorbuf_enabled_4bit &
-					sctx->queued.named.blend->cb_target_mask;
-		if (!ps->info.properties[TGSI_PROPERTY_FS_COLOR0_WRITES_ALL_CBUFS])
-			ps_colormask &= ps->colors_written_4bit;
+		unsigned ps_colormask = si_get_total_colormask(sctx);
 
 		ps_disabled = sctx->queued.named.rasterizer->rasterizer_discard ||
 			      (!ps_colormask &&

commit ef59be2a6b9390a44a0b964e407a3f582e9c3b1c

radeonsi: Reorder checks in si_check_render_feedback
si_get_total_colormask accesses NULL pointer on compute shaders
Fixes crashes on clover
Fixes: 53475a5525ed5bd4e0fac0fa2b01e1c541e86a9f ("radeonsi: skip DCC render feedback checking if color writes are disabled")
CC: Marek Olšák <marek.olsak@amd.com>
Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>

src/gallium/drivers/radeonsi/si_blit.c
@@ -710,13 +710,13 @@ static void si_check_render_feedback_resident_images(struct si_context *sctx)
 
 static void si_check_render_feedback(struct si_context *sctx)
 {
+	if (!sctx->need_check_render_feedback)
+		return;
+
 	/* There is no render feedback if color writes are disabled.
 	 * (e.g. a pixel shader with image stores)
 	 */
 	if (!si_get_total_colormask(sctx))
-		return;
-
-	if (!sctx->need_check_render_feedback)
 		return;
 
 	for (int i = 0; i < SI_NUM_SHADERS; ++i) {