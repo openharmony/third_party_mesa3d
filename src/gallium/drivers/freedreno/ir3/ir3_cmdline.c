openharmony/third_party_mesa3d

commit 611908dfacd551903e6ea75829743cddf84c192c

nir: move GL specific passes to src/compiler/glsl
With this we should have no passes in src/compiler/nir with any
dependencies on headers from core GL Mesa.

Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>

src/gallium/drivers/freedreno/ir3/ir3_cmdline.c
@@ -162,7 +162,7 @@ load_glsl(unsigned num_files, char* const* files, gl_shader_stage stage)
 
 	NIR_PASS_V(nir, nir_lower_system_values);
 	NIR_PASS_V(nir, nir_lower_io, nir_var_all, ir3_glsl_type_size, 0);
-	NIR_PASS_V(nir, nir_lower_samplers, prog);
+	NIR_PASS_V(nir, gl_nir_lower_samplers, prog);
 
 	return nir;
 }

src/compiler/Makefile.sources
@@ -25,6 +25,10 @@ LIBGLSL_FILES = \
 	glsl/builtin_types.cpp \
 	glsl/builtin_variables.cpp \
 	glsl/generate_ir.cpp \
+	glsl/gl_nir_lower_atomics.c \
+	glsl/gl_nir_lower_samplers.c \
+	glsl/gl_nir_lower_samplers_as_deref.c \
+	glsl/gl_nir.h \
 	glsl/glsl_parser_extras.cpp \
 	glsl/glsl_parser_extras.h \
 	glsl/glsl_symbol_table.cpp \
@@ -211,7 +215,6 @@ NIR_FILES = \
 	nir/nir_lower_64bit_packing.c \
 	nir/nir_lower_alpha_test.c \
 	nir/nir_lower_alu_to_scalar.c \
-	nir/nir_lower_atomics.c \
 	nir/nir_lower_atomics_to_ssbo.c \
 	nir/nir_lower_bitmap.c \
 	nir/nir_lower_clamp_color_outputs.c \
@@ -237,8 +240,6 @@ NIR_FILES = \
 	nir/nir_lower_phis_to_scalar.c \
 	nir/nir_lower_regs_to_ssa.c \
 	nir/nir_lower_returns.c \
-	nir/nir_lower_samplers.c \
-	nir/nir_lower_samplers_as_deref.c \
 	nir/nir_lower_subgroups.c \
 	nir/nir_lower_system_values.c \
 	nir/nir_lower_tex.c \

src/compiler/glsl/gl_nir.h
@@ -1,0 +1,47 @@
+/*
+ * Copyright © 2018 Timothy Arceri
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef GL_NIR_H
+#define GL_NIR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct nir_shader;
+struct gl_shader_program;
+
+bool gl_nir_lower_atomics(nir_shader *shader,
+                          const struct gl_shader_program *shader_program,
+                          bool use_binding_as_idx);
+
+bool gl_nir_lower_samplers(nir_shader *shader,
+                           const struct gl_shader_program *shader_program);
+bool gl_nir_lower_samplers_as_deref(nir_shader *shader,
+                                    const struct gl_shader_program *shader_program);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GL_NIR_H */

src/compiler/glsl/gl_nir_lower_atomics.c
@@ -25,8 +25,10 @@
  *
  */
 
-#include "compiler/glsl/ir_uniform.h"
-#include "nir.h"
+#include "compiler/nir/nir.h"
+#include "gl_nir.h"
+#include "ir_uniform.h"
+
 #include "main/config.h"
 #include "main/mtypes.h"
 #include <assert.h>
@@ -177,9 +179,9 @@ lower_instr(nir_intrinsic_instr *instr,
 }
 
 bool
-nir_lower_atomics(nir_shader *shader,
-                  const struct gl_shader_program *shader_program,
-                  bool use_binding_as_idx)
+gl_nir_lower_atomics(nir_shader *shader,
+                     const struct gl_shader_program *shader_program,
+                     bool use_binding_as_idx)
 {
    bool progress = false;
 

src/compiler/glsl/gl_nir_lower_samplers.c
@@ -23,9 +23,10 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-#include "nir.h"
-#include "nir_builder.h"
-#include "compiler/glsl/ir_uniform.h"
+#include "compiler/nir/nir.h"
+#include "compiler/nir/nir_builder.h"
+#include "gl_nir.h"
+#include "ir_uniform.h"
 
 #include "main/compiler.h"
 #include "main/mtypes.h"
@@ -148,8 +149,8 @@ lower_impl(nir_function_impl *impl, const struct gl_shader_program *shader_progr
 }
 
 bool
-nir_lower_samplers(nir_shader *shader,
-                   const struct gl_shader_program *shader_program)
+gl_nir_lower_samplers(nir_shader *shader,
+                      const struct gl_shader_program *shader_program)
 {
    bool progress = false;
 

src/compiler/glsl/gl_nir_lower_samplers_as_deref.c
@@ -55,9 +55,10 @@
  * the opaque uniform mapping.
  */
 
-#include "nir.h"
-#include "nir_builder.h"
-#include "compiler/glsl/ir_uniform.h"
+#include "compiler/nir/nir.h"
+#include "compiler/nir/nir_builder.h"
+#include "gl_nir.h"
+#include "ir_uniform.h"
 
 #include "main/compiler.h"
 #include "main/mtypes.h"
@@ -226,8 +227,8 @@ lower_impl(nir_function_impl *impl, struct lower_samplers_as_deref_state *state)
 }
 
 bool
-nir_lower_samplers_as_deref(nir_shader *shader,
-                            const struct gl_shader_program *shader_program)
+gl_nir_lower_samplers_as_deref(nir_shader *shader,
+                               const struct gl_shader_program *shader_program)
 {
    bool progress = false;
    struct lower_samplers_as_deref_state state;

src/compiler/glsl/meson.build
@@ -66,6 +66,10 @@ files_libglsl = files(
   'builtin_types.cpp',
   'builtin_variables.cpp',
   'generate_ir.cpp',
+  'gl_nir_lower_atomics.c',
+  'gl_nir_lower_samplers.c',
+  'gl_nir_lower_samplers_as_deref.c',
+  'gl_nir.h',
   'glsl_parser_extras.cpp',
   'glsl_parser_extras.h',
   'glsl_symbol_table.cpp',

src/compiler/nir/meson.build
@@ -106,7 +106,6 @@ files_libnir = files(
   'nir_lower_64bit_packing.c',
   'nir_lower_alu_to_scalar.c',
   'nir_lower_alpha_test.c',
-  'nir_lower_atomics.c',
   'nir_lower_atomics_to_ssbo.c',
   'nir_lower_bitmap.c',
   'nir_lower_clamp_color_outputs.c',
@@ -132,8 +131,6 @@ files_libnir = files(
   'nir_lower_phis_to_scalar.c',
   'nir_lower_regs_to_ssa.c',
   'nir_lower_returns.c',
-  'nir_lower_samplers.c',
-  'nir_lower_samplers_as_deref.c',
   'nir_lower_subgroups.c',
   'nir_lower_system_values.c',
   'nir_lower_tex.c',

commit 1f7e0034214ba2a1818e9e7374cd3e3c95905f01

freedreno: Fix ir3_cmdline.c build.
Fixes: 611908dfacd5 ("nir: move GL specific passes to src/compiler/glsl")
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Reviewed-by: Rob Clark <robdclark@gmail.com>

src/gallium/drivers/freedreno/ir3/ir3_cmdline.c
@@ -46,6 +46,7 @@
 
 #include "compiler/glsl/standalone.h"
 #include "compiler/glsl/glsl_to_nir.h"
+#include "compiler/glsl/gl_nir.h"
 #include "compiler/nir_types.h"
 #include "compiler/spirv/nir_spirv.h"
 