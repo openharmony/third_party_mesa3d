openharmony/third_party_mesa3d

commit ce52356f1299055d73eb588ef3c01d1b31a6fb0e

mesa: retain gl_shader_programs after glDeleteProgram if they are in use
Fixes regressions from 54702b1506571af3fa7adccb9a61110746c39ba5.

Switching from using gl_shader_program to gl_program for the pipline
objects CurrentProgram array meant we were freeing gl_shader_programs
immediately after glDeleteProgram was called, but the spec states
the program should only get deleted once it is no longer in use.

To work around this we add a new ReferencedPrograms array to track
gl_shader_programs in use.

Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

src/mesa/main/shaderapi.c
@@ -1128,7 +1128,7 @@ _mesa_link_program(struct gl_context *ctx, struct gl_shader_program *shProg)
          if (shProg->_LinkedShaders[stage])
             prog = shProg->_LinkedShaders[stage]->Program;
 
-         _mesa_use_program(ctx, stage, prog, ctx->_Shader);
+         _mesa_use_program(ctx, stage, shProg, prog, ctx->_Shader);
       }
    }
 
@@ -1243,7 +1243,8 @@ _mesa_active_program(struct gl_context *ctx, struct gl_shader_program *shProg,
 
 static void
 use_program(struct gl_context *ctx, gl_shader_stage stage,
-            struct gl_program *new_prog, struct gl_pipeline_object *shTarget)
+            struct gl_shader_program *shProg, struct gl_program *new_prog,
+            struct gl_pipeline_object *shTarget)
 {
    struct gl_program **target;
 
@@ -1279,6 +1280,9 @@ use_program(struct gl_context *ctx, gl_shader_stage stage,
 	 break;
       }
 
+      _mesa_reference_shader_program(ctx,
+                                     &shTarget->ReferencedPrograms[stage],
+                                     shProg);
       _mesa_reference_program(ctx, target, new_prog);
       return;
    }
@@ -1296,7 +1300,7 @@ _mesa_use_shader_program(struct gl_context *ctx,
       struct gl_program *new_prog = NULL;
       if (shProg && shProg->_LinkedShaders[i])
          new_prog = shProg->_LinkedShaders[i]->Program;
-      use_program(ctx, i, new_prog, &ctx->Shader);
+      use_program(ctx, i, shProg, new_prog, &ctx->Shader);
    }
    _mesa_active_program(ctx, shProg, "glUseProgram");
 }
@@ -2180,10 +2184,10 @@ invalid_value:
 
 void
 _mesa_use_program(struct gl_context *ctx, gl_shader_stage stage,
-                  struct gl_program *prog,
+                  struct gl_shader_program *shProg, struct gl_program *prog,
                   struct gl_pipeline_object *shTarget)
 {
-   use_program(ctx, stage, prog, shTarget);
+   use_program(ctx, stage, shProg, prog, shTarget);
 }
 
 

src/mesa/drivers/common/meta.c
@@ -962,7 +962,7 @@ _mesa_meta_end(struct gl_context *ctx)
           * program object must be NULL.  _mesa_use_program is a no-op
           * in that case.
           */
-         _mesa_use_program(ctx, i, save->Program[i],  &ctx->Shader);
+         _mesa_use_program(ctx, i, NULL, save->Program[i],  &ctx->Shader);
 
          /* Do this *before* killing the reference. :)
           */

src/mesa/main/mtypes.h
@@ -2875,6 +2875,8 @@ struct gl_pipeline_object
     */
    struct gl_program *CurrentProgram[MESA_SHADER_STAGES];
 
+   struct gl_shader_program *ReferencedPrograms[MESA_SHADER_STAGES];
+
    struct gl_program *_CurrentFragmentProgram;
 
    /**

src/mesa/main/pipelineobj.c
@@ -60,8 +60,10 @@ _mesa_delete_pipeline_object(struct gl_context *ctx,
 
    _mesa_reference_program(ctx, &obj->_CurrentFragmentProgram, NULL);
 
-   for (i = 0; i < MESA_SHADER_STAGES; i++)
+   for (i = 0; i < MESA_SHADER_STAGES; i++) {
       _mesa_reference_program(ctx, &obj->CurrentProgram[i], NULL);
+      _mesa_reference_shader_program(ctx, &obj->ReferencedPrograms[i], NULL);
+   }
 
    _mesa_reference_shader_program(ctx, &obj->ActiveProgram, NULL);
    mtx_destroy(&obj->Mutex);
@@ -227,7 +229,7 @@ use_program_stage(struct gl_context *ctx, GLenum type,
    if (shProg && shProg->_LinkedShaders[stage])
       prog = shProg->_LinkedShaders[stage]->Program;
 
-   _mesa_use_program(ctx, stage, prog, pipe);
+   _mesa_use_program(ctx, stage, shProg, prog, pipe);
 }
 
 /**

src/mesa/main/shaderapi.h
@@ -215,7 +215,7 @@ _mesa_ProgramParameteri(GLuint program, GLenum pname, GLint value);
 
 void
 _mesa_use_program(struct gl_context *ctx, gl_shader_stage stage,
-                  struct gl_program *prog,
+                  struct gl_shader_program *shProg, struct gl_program *prog,
                   struct gl_pipeline_object *shTarget);
 
 extern void

commit 0cd1bf3e975b25227383eb232c5f936dc267f23d

mesa: do not leak ctx->Shader.ReferencedProgram references
When glUseProgram is used, references to the included shaders are
added in ctx->Shader.ReferencedProgram. But those references are not
decreased when the shader data is deallocated. Thus, those shaders
are leaked.

Explicitely remove the pending references to these shaders.

Fixes: ce52356f129 ("mesa: retain gl_shader_programs after glDeleteProgram if they are in use")
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

src/mesa/main/shaderapi.c
@@ -159,6 +159,9 @@ _mesa_free_shader_state(struct gl_context *ctx)
 {
    for (int i = 0; i < MESA_SHADER_STAGES; i++) {
       _mesa_reference_program(ctx, &ctx->Shader.CurrentProgram[i], NULL);
+      _mesa_reference_shader_program(ctx,
+                                     &ctx->Shader.ReferencedPrograms[i],
+                                     NULL);
    }
    _mesa_reference_shader_program(ctx, &ctx->Shader.ActiveProgram, NULL);
 