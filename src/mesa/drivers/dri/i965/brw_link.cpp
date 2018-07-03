Igalia/mesa

i965: Use the new nir atomic counter linker for SPIR-V shaders

src/mesa/drivers/dri/i965/brw_link.cpp
@@ -263,6 +263,8 @@ brw_link_shader(struct gl_context *ctx, struct gl_shader_program *shProg)
    /* SPIR-V programs use a NIR linker */
    if (shProg->data->spirv) {
       nir_link_uniforms(ctx, shProg);
+
+      nir_link_assign_atomic_counter_resources(ctx, shProg);
    }
 
    /* Determine first and last stage. */

openharmony/third_party_mesa3d

Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

src/mesa/drivers/dri/i965/brw_link.cpp
@@ -265,6 +265,8 @@ brw_link_shader(struct gl_context *ctx, struct gl_shader_program *shProg)
    if (shProg->data->spirv) {
       if (!gl_nir_link_uniforms(ctx, shProg))
          return false;
+
+      gl_nir_link_assign_atomic_counter_resources(ctx, shProg);
    }
 
    /* Determine first and last stage. */