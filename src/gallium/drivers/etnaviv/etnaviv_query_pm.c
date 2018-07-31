openharmony/third_party_mesa3d

commit ed4f95c04c8f8b261777e6331dd7a35f6b195585

etnaviv: support HI performance counters
Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Tested-by: Chris Healy <cphealy@gmail.com>

src/gallium/drivers/etnaviv/etnaviv_query_pm.c
@@ -46,6 +46,41 @@ struct etna_perfmon_config
 };
 
 static const struct etna_perfmon_config query_config[] = {
+   {
+      .name = "hi-total-cyles",
+      .type = ETNA_QUERY_HI_TOTAL_CYCLES,
+      .source = (const struct etna_perfmon_source[]) {
+         { "HI", "TOTAL_CYCLES" }
+      }
+   },
+   {
+      .name = "hi-idle-cyles",
+      .type = ETNA_QUERY_HI_IDLE_CYCLES,
+      .source = (const struct etna_perfmon_source[]) {
+         { "HI", "IDLE_CYCLES" }
+      }
+   },
+   {
+      .name = "hi-axi-cycles-read-request-stalled",
+      .type = ETNA_QUERY_HI_AXI_CYCLES_READ_REQUEST_STALLED,
+      .source = (const struct etna_perfmon_source[]) {
+         { "HI", "AXI_CYCLES_READ_REQUEST_STALLED" }
+      }
+   },
+   {
+      .name = "hi-axi-cycles-write-request-stalled",
+      .type = ETNA_QUERY_HI_AXI_CYCLES_WRITE_REQUEST_STALLED,
+      .source = (const struct etna_perfmon_source[]) {
+         { "HI", "AXI_CYCLES_WRITE_REQUEST_STALLED" }
+      }
+   },
+   {
+      .name = "hi-axi-cycles-write-data-stalled",
+      .type = ETNA_QUERY_HI_AXI_CYCLES_WRITE_DATA_STALLED,
+      .source = (const struct etna_perfmon_source[]) {
+         { "HI", "AXI_CYCLES_WRITE_DATA_STALLED" }
+      }
+   }
 };
 
 static const struct etna_perfmon_config *

src/gallium/drivers/etnaviv/etnaviv_query_pm.h
@@ -33,6 +33,12 @@
 
 struct etna_screen;
 
+#define ETNA_QUERY_HI_TOTAL_CYCLES                       (ETNA_PM_QUERY_BASE + 0)
+#define ETNA_QUERY_HI_IDLE_CYCLES                        (ETNA_PM_QUERY_BASE + 1)
+#define ETNA_QUERY_HI_AXI_CYCLES_READ_REQUEST_STALLED    (ETNA_PM_QUERY_BASE + 2)
+#define ETNA_QUERY_HI_AXI_CYCLES_WRITE_REQUEST_STALLED   (ETNA_PM_QUERY_BASE + 3)
+#define ETNA_QUERY_HI_AXI_CYCLES_WRITE_DATA_STALLED      (ETNA_PM_QUERY_BASE + 4)
+
 struct etna_pm_query {
    struct etna_query base;
    struct etna_perfmon_signal *signal;

commit bb6a67fde2daf18ea1a163c6a1720adc7185ef8b

etnaviv: fix typo in query names
Fixes: ed4f95c04c8f ("etnaviv: support HI performance counters")
Cc: mesa-stable@lists.freedesktop.org
Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Reviewed-by: Chris Healy <cphealy@gmail.com>

src/gallium/drivers/etnaviv/etnaviv_query_pm.c
@@ -59,7 +59,7 @@ static const char *group_names[] = {
 
 static const struct etna_perfmon_config query_config[] = {
    {
-      .name = "hi-total-cyles",
+      .name = "hi-total-cycles",
       .type = ETNA_QUERY_HI_TOTAL_CYCLES,
       .group_id = ETNA_QUERY_HI_GROUP_ID,
       .source = (const struct etna_perfmon_source[]) {
@@ -67,7 +67,7 @@ static const struct etna_perfmon_config query_config[] = {
       }
    },
    {
-      .name = "hi-idle-cyles",
+      .name = "hi-idle-cycles",
       .type = ETNA_QUERY_HI_IDLE_CYCLES,
       .group_id = ETNA_QUERY_HI_GROUP_ID,
       .source = (const struct etna_perfmon_source[]) {