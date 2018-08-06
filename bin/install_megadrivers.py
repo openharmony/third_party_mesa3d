openharmony/third_party_mesa3d

commit ed75afc4fa786e8982f9aacdc5d38de8eed48524

meson: extend install_megadrivers script to handle symmlinking
Which is required for the gallium media state trackers.

v2: - Make symlinks local instead of absolute

Signed-off-by: Dylan Baker <dylanx.c.baker@intel.com>
Reviewed-by: Eric Engestrom <eric.engestrom@imgtec.com>

bin/install_megadrivers.py
@@ -44,10 +44,24 @@ def main():
 
     for each in args.drivers:
         driver = os.path.join(to, each)
+
         if os.path.exists(driver):
             os.unlink(driver)
         print('installing {} to {}'.format(args.megadriver, driver))
         os.link(master, driver)
+
+        try:
+            ret = os.getcwd()
+            os.chdir(to)
+
+            name, ext = os.path.splitext(each)
+            while ext != '.so':
+                if os.path.exists(name):
+                    os.unlink(name)
+                os.symlink(driver, name)
+                name, ext = os.path.splitext(name)
+        finally:
+            os.chdir(ret)
     os.unlink(master)
 
 

commit 97b9b893d1541e23863163bb5a93cb8aef0220b5

meson, install_megadrivers: Also remove stale symlinks
os.path.exists doesn't return True for stale symlinks, but they are in
the way later, when a link/file with the same name is to be created.
For instance it is conceivable that the pointed to file is replaced by
a file with a new name, and then the symlink is dead.

To handle this check specifically for all existing symlinks to be
removed. (This bugged me for some time with a link libXvMCr600.so
always being in the way of installing this file)

v2: use only os.lexist and replace all instances of os.exist (Dylan Baker)

v3: handle directory check correctly (Eric Engestrom)

Fixes: ed75afc4fa786e8982f9aacdc5d38de8eed48524
       ("meson: extend install_megadrivers script to handle symmlinking")

Reviewed-by: Eric Engestrom <eric.engestrom@intel.com>(v2 minus dir check)
Reviewed-by: Dylan Baker <dylan@pnwbakers.com>
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

bin/install_megadrivers.py
@@ -43,13 +43,15 @@ def main():
     master = os.path.join(to, os.path.basename(args.megadriver))
 
     if not os.path.exists(to):
+        if os.path.lexists(to):
+            os.unlink(to)
         os.makedirs(to)
     shutil.copy(args.megadriver, master)
 
     for driver in args.drivers:
         abs_driver = os.path.join(to, driver)
 
-        if os.path.exists(abs_driver):
+        if os.path.lexists(abs_driver):
             os.unlink(abs_driver)
         print('installing {} to {}'.format(args.megadriver, abs_driver))
         os.link(master, abs_driver)
@@ -60,7 +62,7 @@ def main():
 
             name, ext = os.path.splitext(driver)
             while ext != '.so':
-                if os.path.exists(name):
+                if os.path.lexists(name):
                     os.unlink(name)
                 os.symlink(driver, name)
                 name, ext = os.path.splitext(name)