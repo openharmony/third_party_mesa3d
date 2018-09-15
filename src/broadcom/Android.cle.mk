openharmony/third_party_mesa3d

commit 76c8828a9c132414fb11212558d035bb4205f250

broadcom/genxml: Introduce a V3D packet/struct decoder.
This is copied from Intel's XML decoder, modified to handle V3D's
byte-oriented packets.

v2: Squash in robher's fixes for Android

src/broadcom/Android.cle.mk
@@ -1,4 +1,5 @@
 # Copyright © 2016 Intel Corporation
+# Copyright © 2016 Mauro Rossi <issor.oruam@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -7,36 +8,28 @@
 # and/or sell copies of the Software, and to permit persons to whom the
 # Software is furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice (including the next
-# paragraph) shall be included in all copies or substantial portions of the
-# Software.
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
 
-BUILT_SOURCES += $(BROADCOM_GENXML_GENERATED_FILES)
+include $(CLEAR_VARS)
 
-EXTRA_DIST += $(BROADCOM_GENXML_XML_FILES)
-EXTRA_DIST += $(BROADCOM_GENXML_GENERATED_FILES)
+LOCAL_MODULE := libmesa_broadcom_cle
 
-SUFFIXES = _pack.h .xml
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
 
-$(BROADCOM_GENXML_GENERATED_FILES): cle/gen_pack_header.py
+LOCAL_SRC_FILES := $(BROADCOM_DECODER_FILES)
 
-.xml_pack.h:
-	$(MKDIR_GEN)
-	$(PYTHON_GEN) $(srcdir)/cle/gen_pack_header.py $< > $@ || ($(RM) $@; false)
+LOCAL_STATIC_LIBRARIES := libmesa_broadcom_genxml
 
-GEN_ZIPPED = $(srcdir)/../intel/genxml/gen_zipped_file.py
-cle/v3d_xml.h: $(GEN_ZIPPED) $(BROADCOM_GENXML_XML_FILES)
-	$(MKDIR_GEN)
-	$(PYTHON_GEN) $(GEN_ZIPPED) $(BROADCOM_GENXML_XML_FILES:%=$(srcdir)/%) > $@ || ($(RM) $@; false)
+LOCAL_SHARED_LIBRARIES := libexpat libz
 
-EXTRA_DIST += \
-	cle/gen_pack_header.py \
-	$()
+include $(MESA_COMMON_MK)
+include $(BUILD_STATIC_LIBRARY)

src/broadcom/Android.genxml.mk
@@ -50,8 +50,14 @@ $(intermediates)/broadcom/cle/v3d_packet_v21_pack.h: PRIVATE_XML := $(LOCAL_PATH
 $(intermediates)/broadcom/cle/v3d_packet_v21_pack.h: $(LOCAL_PATH)/cle/v3d_packet_v21.xml $(LOCAL_PATH)/cle/gen_pack_header.py
 	$(call header-gen)
 
+$(intermediates)/broadcom/cle/v3d_xml.h: $(addprefix $(MESA_TOP)/src/broadcom/,$(BROADCOM_GENXML_XML_FILES)) $(MESA_TOP)/src/intel/genxml/gen_zipped_file.py
+	@mkdir -p $(dir $@)
+	@echo "Gen Header: $(PRIVATE_MODULE) <= $(notdir $(@))"
+	$(hide) $(MESA_PYTHON2) $(MESA_TOP)/src/intel/genxml/gen_zipped_file.py $(addprefix $(MESA_TOP)/src/broadcom/,$(BROADCOM_GENXML_XML_FILES)) > $@ || (rm -f $@; false)
+
 LOCAL_EXPORT_C_INCLUDE_DIRS := \
 	$(MESA_TOP)/src/broadcom/cle \
+	$(intermediates)/broadcom/cle \
 	$(intermediates)
 
 include $(MESA_COMMON_MK)

src/broadcom/Android.mk
@@ -26,3 +26,4 @@ LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/Makefile.sources
 
 include $(LOCAL_PATH)/Android.genxml.mk
+include $(LOCAL_PATH)/Android.cle.mk

src/broadcom/Makefile.am
@@ -20,6 +20,13 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
+AM_CPPFLAGS = \
+	-I$(top_srcdir)/include \
+	-I$(top_builddir)/src \
+	-I$(top_srcdir)/src \
+	$(VALGRIND_CFLAGS) \
+	$(DEFINES)
+
 include Makefile.sources
 
 lib_LTLIBRARIES =
@@ -38,5 +45,6 @@ MKDIR_GEN = $(AM_V_at)$(MKDIR_P) $(@D)
 PYTHON_GEN = $(AM_V_GEN)$(PYTHON2) $(PYTHON_FLAGS)
 
 include Makefile.genxml.am
+include Makefile.cle.am
 
 CLEANFILES += $(BUILT_SOURCES)

src/broadcom/Makefile.cle.am
@@ -1,0 +1,4 @@
+noinst_LTLIBRARIES += cle/libbroadcom_cle.la
+
+cle_libbroadcom_cle_la_CFLAGS = $(AM_CFLAGS)
+cle_libbroadcom_cle_la_SOURCES = $(BROADCOM_DECODER_FILES)

src/broadcom/Makefile.sources
@@ -1,5 +1,6 @@
 BROADCOM_GENXML_GENERATED_FILES = \
 	cle/v3d_packet_v21_pack.h \
+	cle/v3d_xml.h \
 	$()
 
 BROADCOM_GENXML_XML_FILES = \
@@ -9,5 +10,10 @@ BROADCOM_GENXML_XML_FILES = \
 BROADCOM_FILES = \
 	cle/v3d_packet_helpers.h \
 	common/v3d_device_info.h \
+	$()
+
+BROADCOM_DECODER_FILES = \
+	cle/v3d_decoder.c \
+	cle/v3d_decoder.h \
 	$()
 

src/broadcom/cle/v3d_decoder.h
@@ -1,0 +1,146 @@
+/*
+ * Copyright © 2016 Intel Corporation
+ * Copyright © 2017 Broadcom
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef V3D_DECODER_H
+#define V3D_DECODER_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "broadcom/common/v3d_device_info.h"
+
+struct v3d_spec;
+struct v3d_group;
+struct v3d_field;
+
+struct v3d_group *v3d_spec_find_struct(struct v3d_spec *spec, const char *name);
+struct v3d_spec *v3d_spec_load(const struct v3d_device_info *devinfo);
+struct v3d_group *v3d_spec_find_instruction(struct v3d_spec *spec, const uint8_t *p);
+struct v3d_group *v3d_spec_find_register(struct v3d_spec *spec, uint32_t offset);
+struct v3d_group *v3d_spec_find_register_by_name(struct v3d_spec *spec, const char *name);
+int v3d_group_get_length(struct v3d_group *group);
+const char *v3d_group_get_name(struct v3d_group *group);
+uint8_t v3d_group_get_opcode(struct v3d_group *group);
+struct v3d_enum *v3d_spec_find_enum(struct v3d_spec *spec, const char *name);
+
+struct v3d_field_iterator {
+        struct v3d_group *group;
+        char name[128];
+        char value[128];
+        struct v3d_group *struct_desc;
+        const uint8_t *p;
+        int offset; /**< current field starts at &p[offset] */
+
+        int field_iter;
+        int group_iter;
+
+        struct v3d_field *field;
+        bool print_colors;
+};
+
+struct v3d_group {
+        struct v3d_spec *spec;
+        char *name;
+
+        struct v3d_field **fields;
+        uint32_t nfields;
+        uint32_t fields_size;
+
+        uint32_t group_offset, group_count;
+        uint32_t group_size;
+        bool variable;
+
+        struct v3d_group *parent;
+        struct v3d_group *next;
+
+        uint8_t opcode;
+
+        /* Register specific */
+        uint32_t register_offset;
+};
+
+struct v3d_value {
+        char *name;
+        uint64_t value;
+};
+
+struct v3d_enum {
+        char *name;
+        int nvalues;
+        struct v3d_value **values;
+};
+
+struct v3d_type {
+        enum {
+                V3D_TYPE_UNKNOWN,
+                V3D_TYPE_INT,
+                V3D_TYPE_UINT,
+                V3D_TYPE_BOOL,
+                V3D_TYPE_FLOAT,
+                V3D_TYPE_ADDRESS,
+                V3D_TYPE_OFFSET,
+                V3D_TYPE_STRUCT,
+                V3D_TYPE_UFIXED,
+                V3D_TYPE_SFIXED,
+                V3D_TYPE_MBO,
+                V3D_TYPE_ENUM
+        } kind;
+
+        /* Struct definition for V3D_TYPE_STRUCT */
+        union {
+                struct v3d_group *v3d_struct;
+                struct v3d_enum *v3d_enum;
+                struct {
+                        /* Integer and fractional sizes for V3D_TYPE_UFIXED and
+                         * V3D_TYPE_SFIXED
+                         */
+                        int i, f;
+                };
+        };
+};
+
+struct v3d_field {
+        char *name;
+        int start, end;
+        struct v3d_type type;
+        bool has_default;
+        uint32_t default_value;
+
+        struct v3d_enum inline_enum;
+};
+
+void v3d_field_iterator_init(struct v3d_field_iterator *iter,
+                             struct v3d_group *group,
+                             const uint8_t *p,
+                             bool print_colors);
+
+bool v3d_field_iterator_next(struct v3d_field_iterator *iter);
+
+void v3d_print_group(FILE *out,
+                     struct v3d_group *group,
+                     uint64_t offset, const uint8_t *p,
+                     bool color);
+
+#endif /* V3D_DECODER_H */

commit 17d028d299e3db74caf3e43ccaf1de09864bda78

android: broadcom/cle: export the broadcom top level path headers
Fixes the following building error in vc4 build:

In file included from external/mesa/src/gallium/drivers/vc4/kernel/vc4_render_cl.c:34:
In file included from external/mesa/src/gallium/drivers/vc4/kernel/vc4_drv.h:27:
In file included from external/mesa/src/gallium/drivers/vc4/vc4_simulator_validate.h:34:
In file included from external/mesa/src/gallium/drivers/vc4/vc4_context.h:39:
In file included from external/mesa/src/gallium/drivers/vc4/vc4_cl.h:56:
gen/STATIC_LIBRARIES/libmesa_broadcom_genxml_intermediates/broadcom/cle/v3d_packet_v21_pack.h:12:10:
fatal error: 'cle/v3d_packet_helpers.h' file not found
         ^~~~~~~~~~~~~~~~~~~~~~~~~~
1 error generated.

Fixes: 76c8828a9c ("broadcom/genxml: Introduce a V3D packet/struct decoder.")
Cc: "18.2" <mesa-stable@lists.freedesktop.org>
Acked-by: Eric Anholt <eric@anholt.net>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>

src/broadcom/Android.cle.mk
@@ -31,6 +31,8 @@ LOCAL_STATIC_LIBRARIES := libmesa_broadcom_genxml
 
 LOCAL_C_INCLUDES += $(MESA_TOP)/src/gallium/include
 
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+
 LOCAL_SHARED_LIBRARIES := libexpat libz
 
 include $(MESA_COMMON_MK)