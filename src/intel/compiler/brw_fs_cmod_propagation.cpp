openharmony/third_party_mesa3d

commit bf89f59e88bf3f50deb3ffd9f073cd8df5d401a9

i965/fs: Refactor propagation of conditional modifiers from compares to adds
Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>

src/intel/compiler/brw_fs_cmod_propagation.cpp
@@ -49,6 +49,77 @@
  */
 
 static bool
+cmod_propagate_cmp_to_add(const gen_device_info *devinfo, bblock_t *block,
+                          fs_inst *inst)
+{
+   bool read_flag = false;
+
+   foreach_inst_in_block_reverse_starting_from(fs_inst, scan_inst, inst) {
+      /* The extra scope is to prevent compiler errors for gotos crossing a
+       * variable initializer (cond, below).
+       */
+      {
+         bool negate;
+
+         if (scan_inst->opcode != BRW_OPCODE_ADD)
+            goto not_match;
+
+         /* A CMP is basically a subtraction.  The result of the
+          * subtraction must be the same as the result of the addition.
+          * This means that one of the operands must be negated.  So (a +
+          * b) vs (a == -b) or (a + -b) vs (a == b).
+          */
+         if ((inst->src[0].equals(scan_inst->src[0]) &&
+              inst->src[1].negative_equals(scan_inst->src[1])) ||
+             (inst->src[0].equals(scan_inst->src[1]) &&
+              inst->src[1].negative_equals(scan_inst->src[0]))) {
+            negate = false;
+         } else if ((inst->src[0].negative_equals(scan_inst->src[0]) &&
+                     inst->src[1].equals(scan_inst->src[1])) ||
+                    (inst->src[0].negative_equals(scan_inst->src[1]) &&
+                     inst->src[1].equals(scan_inst->src[0]))) {
+            negate = true;
+         } else {
+            goto not_match;
+         }
+
+         if (scan_inst->is_partial_write() ||
+             scan_inst->exec_size != inst->exec_size)
+            goto not_match;
+
+         /* From the Sky Lake PRM Vol. 7 "Assigning Conditional Mods":
+          *
+          *    * Note that the [post condition signal] bits generated at
+          *      the output of a compute are before the .sat.
+          *
+          * So we don't have to bail if scan_inst has saturate.
+          */
+         /* Otherwise, try propagating the conditional. */
+         const enum brw_conditional_mod cond =
+            negate ? brw_swap_cmod(inst->conditional_mod)
+            : inst->conditional_mod;
+
+         if (scan_inst->can_do_cmod() &&
+             ((!read_flag && scan_inst->conditional_mod == BRW_CONDITIONAL_NONE) ||
+              scan_inst->conditional_mod == cond)) {
+            scan_inst->conditional_mod = cond;
+            inst->remove(block);
+            return true;
+         }
+         break;
+      }
+
+   not_match:
+      if (scan_inst->flags_written())
+         break;
+
+      read_flag = read_flag || scan_inst->flags_read(devinfo);
+   }
+
+   return false;
+}
+
+static bool
 opt_cmod_propagation_local(const gen_device_info *devinfo, bblock_t *block)
 {
    bool progress = false;
@@ -90,64 +161,17 @@ opt_cmod_propagation_local(const gen_device_info *devinfo, bblock_t *block)
           inst->conditional_mod != BRW_CONDITIONAL_NZ)
          continue;
 
+      /* A CMP with a second source of zero can match with anything.  A CMP
+       * with a second source that is not zero can only match with an ADD
+       * instruction.
+       */
+      if (inst->opcode == BRW_OPCODE_CMP && !inst->src[1].is_zero()) {
+         progress = cmod_propagate_cmp_to_add(devinfo, block, inst) || progress;
+         continue;
+      }
+
       bool read_flag = false;
       foreach_inst_in_block_reverse_starting_from(fs_inst, scan_inst, inst) {
-         /* A CMP with a second source of zero can match with anything.  A CMP
-          * with a second source that is not zero can only match with an ADD
-          * instruction.
-          */
-         if (inst->opcode == BRW_OPCODE_CMP && !inst->src[1].is_zero()) {
-            bool negate;
-
-            if (scan_inst->opcode != BRW_OPCODE_ADD)
-               goto not_match;
-
-            /* A CMP is basically a subtraction.  The result of the
-             * subtraction must be the same as the result of the addition.
-             * This means that one of the operands must be negated.  So (a +
-             * b) vs (a == -b) or (a + -b) vs (a == b).
-             */
-            if ((inst->src[0].equals(scan_inst->src[0]) &&
-                 inst->src[1].negative_equals(scan_inst->src[1])) ||
-                (inst->src[0].equals(scan_inst->src[1]) &&
-                 inst->src[1].negative_equals(scan_inst->src[0]))) {
-               negate = false;
-            } else if ((inst->src[0].negative_equals(scan_inst->src[0]) &&
-                        inst->src[1].equals(scan_inst->src[1])) ||
-                       (inst->src[0].negative_equals(scan_inst->src[1]) &&
-                        inst->src[1].equals(scan_inst->src[0]))) {
-               negate = true;
-            } else {
-               goto not_match;
-            }
-
-            if (scan_inst->is_partial_write() ||
-                scan_inst->exec_size != inst->exec_size)
-               goto not_match;
-
-            /* From the Sky Lake PRM Vol. 7 "Assigning Conditional Mods":
-             *
-             *    * Note that the [post condition signal] bits generated at
-             *      the output of a compute are before the .sat.
-             *
-             * So we don't have to bail if scan_inst has saturate.
-             */
-
-            /* Otherwise, try propagating the conditional. */
-            const enum brw_conditional_mod cond =
-               negate ? brw_swap_cmod(inst->conditional_mod)
-                      : inst->conditional_mod;
-
-            if (scan_inst->can_do_cmod() &&
-                ((!read_flag && scan_inst->conditional_mod == BRW_CONDITIONAL_NONE) ||
-                 scan_inst->conditional_mod == cond)) {
-               scan_inst->conditional_mod = cond;
-               inst->remove(block);
-               progress = true;
-            }
-            break;
-         }
-
          if (regions_overlap(scan_inst->dst, scan_inst->size_written,
                              inst->src[0], inst->size_read(0))) {
             if (scan_inst->is_partial_write() ||
@@ -242,7 +266,6 @@ opt_cmod_propagation_local(const gen_device_info *devinfo, bblock_t *block)
             break;
          }
 
-      not_match:
          if (scan_inst->flags_written())
             break;
 

commit 87bc3d62c9d0813ae23e6a811e6fc8eab2b28c65

i965/fs: Don't propagate conditional modifiers from integer compares to adds
No shader-db changes on any Intel platform... which probably explains
why no bugs have been bisected to this problem since it landed in Mesa
18.1. :( The commit mentioned below is in 18.2, so 18.1 would need a
slightly different fix (due to code refactoring).

Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
Fixes: bf89f59e88b "i965/fs: Refactor propagation of conditional modifiers from compares to adds"
Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com> (reviewed the original patch)
Cc: Matt Turner <mattst88@gmail.com> (reviewed the original patch)

src/intel/compiler/brw_fs_cmod_propagation.cpp
@@ -211,9 +211,17 @@ opt_cmod_propagation_local(const gen_device_info *devinfo, bblock_t *block)
       /* A CMP with a second source of zero can match with anything.  A CMP
        * with a second source that is not zero can only match with an ADD
        * instruction.
+       *
+       * Only apply this optimization to float-point sources.  It can fail for
+       * integers.  For inputs a = 0x80000000, b = 4, int(0x80000000) < 4, but
+       * int(0x80000000) - 4 overflows and results in 0x7ffffffc.  that's not
+       * less than zero, so the flags get set differently than for (a < b).
        */
       if (inst->opcode == BRW_OPCODE_CMP && !inst->src[1].is_zero()) {
-         progress = cmod_propagate_cmp_to_add(devinfo, block, inst) || progress;
+         if (brw_reg_type_is_floating_point(inst->src[0].type) &&
+             cmod_propagate_cmp_to_add(devinfo, block, inst))
+            progress = true;
+
          continue;
       }
 