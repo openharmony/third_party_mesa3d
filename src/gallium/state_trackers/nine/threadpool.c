openharmony/third_party_mesa3d

commit 086e8afeee1557e11c6056406fa4d961bb43cd45

st/nine: Use WINE thread for threadpool
Use present interface 1.2 function ID3DPresent_CreateThread
to create the thread for threadpool.
Creating the thread with WINE prevents some rarely occuring crashes.

Signed-off-by: Patrick Rudolph <siro@das-labor.org>
Reviewed-by: Axel Davy <axel.davy@ens.fr>

src/gallium/state_trackers/nine/threadpool.c
@@ -77,7 +77,7 @@ threadpool_worker(void *data)
 }
 
 struct threadpool *
-_mesa_threadpool_create(void)
+_mesa_threadpool_create(struct NineSwapChain9 *swapchain)
 {
     struct threadpool *pool = calloc(1, sizeof(*pool));
 
@@ -87,13 +87,16 @@ _mesa_threadpool_create(void)
     pthread_mutex_init(&pool->m, NULL);
     pthread_cond_init(&pool->new_work, NULL);
 
-    pthread_create(&pool->thread, NULL, threadpool_worker, pool);
-
+    pool->wthread = NineSwapChain9_CreateThread(swapchain, threadpool_worker, pool);
+    if (!pool->wthread) {
+        /* using pthread as fallback */
+        pthread_create(&pool->pthread, NULL, threadpool_worker, pool);
+    }
     return pool;
 }
 
 void
-_mesa_threadpool_destroy(struct threadpool *pool)
+_mesa_threadpool_destroy(struct NineSwapChain9 *swapchain, struct threadpool *pool)
 {
     if (!pool)
         return;
@@ -103,7 +106,11 @@ _mesa_threadpool_destroy(struct threadpool *pool)
     pthread_cond_broadcast(&pool->new_work);
     pthread_mutex_unlock(&pool->m);
 
-    pthread_join(pool->thread, NULL);
+    if (pool->wthread) {
+        NineSwapChain9_WaitForThread(swapchain, pool->wthread);
+    } else {
+        pthread_join(pool->pthread, NULL);
+    }
 
     pthread_cond_destroy(&pool->new_work);
     pthread_mutex_destroy(&pool->m);

src/gallium/state_trackers/nine/swapchain9.c
@@ -241,12 +241,12 @@ NineSwapChain9_Resize( struct NineSwapChain9 *This,
     desc.Height = pParams->BackBufferHeight;
 
     if (This->pool) {
-        _mesa_threadpool_destroy(This->pool);
+        _mesa_threadpool_destroy(This, This->pool);
         This->pool = NULL;
     }
     This->enable_threadpool = This->actx->thread_submit && (pParams->SwapEffect != D3DSWAPEFFECT_COPY);
     if (This->enable_threadpool)
-        This->pool = _mesa_threadpool_create();
+        This->pool = _mesa_threadpool_create(This);
     if (!This->pool)
         This->enable_threadpool = FALSE;
 
@@ -504,7 +504,7 @@ NineSwapChain9_dtor( struct NineSwapChain9 *This )
     DBG("This=%p\n", This);
 
     if (This->pool)
-        _mesa_threadpool_destroy(This->pool);
+        _mesa_threadpool_destroy(This, This->pool);
 
     if (This->buffers) {
         for (i = 0; i < This->params.BackBufferCount; i++) {
@@ -1027,4 +1027,25 @@ NineSwapChain9_ResolutionMismatch( struct NineSwapChain9 *This )
     }
 
     return FALSE;
+}
+
+HANDLE
+NineSwapChain9_CreateThread( struct NineSwapChain9 *This,
+                                 void *pFuncAddress,
+                                 void *pParam )
+{
+    if (This->base.device->minor_version_num > 1) {
+        return ID3DPresent_CreateThread(This->present, pFuncAddress, pParam);
+    }
+
+    return NULL;
+}
+
+void
+NineSwapChain9_WaitForThread( struct NineSwapChain9 *This,
+                                  HANDLE thread )
+{
+    if (This->base.device->minor_version_num > 1) {
+        (void) ID3DPresent_WaitForThread(This->present, thread);
+    }
 }

src/gallium/state_trackers/nine/swapchain9.h
@@ -145,4 +145,13 @@ NineSwapChain9_GetOccluded( struct NineSwapChain9 *This );
 BOOL
 NineSwapChain9_ResolutionMismatch( struct NineSwapChain9 *This );
 
+HANDLE
+NineSwapChain9_CreateThread( struct NineSwapChain9 *This,
+                                 void *pFuncAddress,
+                                 void *pParam );
+
+void
+NineSwapChain9_WaitForThread( struct NineSwapChain9 *This,
+                                  HANDLE thread );
+
 #endif /* _NINE_SWAPCHAIN9_H_ */

src/gallium/state_trackers/nine/threadpool.h
@@ -24,13 +24,16 @@
 #ifndef _THREADPOOL_H_
 #define _THREADPOOL_H_
 
+struct NineSwapChain9;
+
 #define MAXTHREADS 1
 
 struct threadpool {
     pthread_mutex_t m;
     pthread_cond_t new_work;
 
-    pthread_t thread;
+    HANDLE wthread;
+    pthread_t pthread;
     struct threadpool_task *workqueue;
     BOOL shutdown;
 };
@@ -45,8 +48,8 @@ struct threadpool_task {
     BOOL finished;
 };
 
-struct threadpool *_mesa_threadpool_create(void);
-void _mesa_threadpool_destroy(struct threadpool *pool);
+struct threadpool *_mesa_threadpool_create(struct NineSwapChain9 *swapchain);
+void _mesa_threadpool_destroy(struct NineSwapChain9 *swapchain, struct threadpool *pool);
 struct threadpool_task *_mesa_threadpool_queue_task(struct threadpool *pool,
                                                     threadpool_task_func func,
                                                     void *data);

commit 1b9d00697ee332ee015fbf92bfee9a43f588df7f

st/nine: fix stack corruption due to ABI mismatch
This fixes various crashes and hangs when using nine's 'thread_submit'
feature.

On 64bit, the thread function's data argument would just be NULL.
On 32bit, the data argument would be garbage depending on the compiler
flags (in my case -march>=core2).

Fixes: 086e8afeee1557 ("st/nine: Use WINE thread for threadpool")
Cc: mesa-stable@lists.freedesktop.org
Signed-off-by: Andre Heider <a.heider@gmail.com>
Reviewed-by: Axel Davy <davyaxel0@gmail.com>

src/gallium/state_trackers/nine/threadpool.c
@@ -37,6 +37,7 @@
 #include "os/os_thread.h"
 #include "threadpool.h"
 
+/* POSIX thread function */
 static void *
 threadpool_worker(void *data)
 {
@@ -76,6 +77,15 @@ threadpool_worker(void *data)
     return NULL;
 }
 
+/* Windows thread function */
+static DWORD NINE_WINAPI
+wthreadpool_worker(void *data)
+{
+    threadpool_worker(data);
+
+    return 0;
+}
+
 struct threadpool *
 _mesa_threadpool_create(struct NineSwapChain9 *swapchain)
 {
@@ -87,7 +97,9 @@ _mesa_threadpool_create(struct NineSwapChain9 *swapchain)
     pthread_mutex_init(&pool->m, NULL);
     pthread_cond_init(&pool->new_work, NULL);
 
-    pool->wthread = NineSwapChain9_CreateThread(swapchain, threadpool_worker, pool);
+    /* This uses WINE's CreateThread, so the thread function needs to use
+     * the Windows ABI */
+    pool->wthread = NineSwapChain9_CreateThread(swapchain, wthreadpool_worker, pool);
     if (!pool->wthread) {
         /* using pthread as fallback */
         pthread_create(&pool->pthread, NULL, threadpool_worker, pool);