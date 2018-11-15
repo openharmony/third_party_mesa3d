openharmony/third_party_mesa3d

commit 7baded8670d26cca0d825caafa5b68fea2c2c6a1

egl: add missing glvnd entrypoint for EGL_ANDROID_blob_cache
Fixes dEQP-EGL.functional.get_proc_address.extension.egl_android_blob_cache
on builds with glvnd enabled.

Fixes: 63373fab0724a4547a466 "egl: add support for EGL_ANDROID_blob_cache"
Signed-off-by: Eric Engestrom <eric.engestrom@intel.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

src/egl/generate/eglFunctionList.py
@@ -196,6 +196,9 @@ EGL_FUNCTIONS = (
     # EGL_ANDROID_native_fence_sync
     _eglFunc("eglDupNativeFenceFDANDROID",           "display"),
 
+    # EGL_ANDROID_blob_cache
+    ("eglSetBlobCacheFuncsANDROID",          "display"),
+
     # EGL_EXT_image_dma_buf_import_modifiers
     _eglFunc("eglQueryDmaBufFormatsEXT",             "display"),
     _eglFunc("eglQueryDmaBufModifiersEXT",           "display"),

commit c6c0c30dd40a5fcde61c5e8ebfa6dcdc15305444

egl: fix bad rebase
I screwed up a rebase over a refactor and didn't notice locally because
the uncommitted refactor hid the issue.

Fixes: 7baded8670d26cca0d82 "egl: add missing glvnd entrypoint for EGL_ANDROID_blob_cache"
Signed-off-by: Eric Engestrom <eric.engestrom@intel.com>

src/egl/generate/eglFunctionList.py
@@ -197,7 +197,7 @@ EGL_FUNCTIONS = (
     _eglFunc("eglDupNativeFenceFDANDROID",           "display"),
 
     # EGL_ANDROID_blob_cache
-    ("eglSetBlobCacheFuncsANDROID",          "display"),
+    _eglFunc("eglSetBlobCacheFuncsANDROID",          "display"),
 
     # EGL_EXT_image_dma_buf_import_modifiers
     _eglFunc("eglQueryDmaBufFormatsEXT",             "display"),