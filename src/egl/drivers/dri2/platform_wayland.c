openharmony/third_party_mesa3d

commit ec89e41e1aae98770db0fefb213cc665c553f1d2

egl/wayland: plug memory leak in drm_handle_device()
As we fail to open the node, we leak the node/device name.

v2: Log and then free() (Eric)

Cc: mesa-stable@lists.freedesktop.org
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
Reviewed-by: Eric Engestrom <eric.engestrom@intel.com>

src/egl/drivers/dri2/platform_wayland.c
@@ -1127,6 +1127,8 @@ drm_handle_device(void *data, struct wl_drm *drm, const char *device)
    if (dri2_dpy->fd == -1) {
       _eglLog(_EGL_WARNING, "wayland-egl: could not open %s (%s)",
               dri2_dpy->device_name, strerror(errno));
+      free(dri2_dpy->device_name);
+      dri2_dpy->device_name = NULL:
       return;
    }
 

commit b8a372103c8c37ec09c8d275b513a714d2fcfa84

egl/wayland: rather obvious build fix
Fixes: ec89e41e1aae ("egl/wayland: plug memory leak in drm_handle_device()")
Fixes: 61d55100efe0 ("egl/wayland: bail out when drmGetMagic fails")

src/egl/drivers/dri2/platform_wayland.c
@@ -1128,7 +1128,7 @@ drm_handle_device(void *data, struct wl_drm *drm, const char *device)
       _eglLog(_EGL_WARNING, "wayland-egl: could not open %s (%s)",
               dri2_dpy->device_name, strerror(errno));
       free(dri2_dpy->device_name);
-      dri2_dpy->device_name = NULL:
+      dri2_dpy->device_name = NULL;
       return;
    }
 
@@ -1139,7 +1139,7 @@ drm_handle_device(void *data, struct wl_drm *drm, const char *device)
          close(dri2_dpy->fd);
          dri2_dpy->fd = -1;
          free(dri2_dpy->device_name);
-         dri2_dpy->device_name = NULL:
+         dri2_dpy->device_name = NULL;
          _eglLog(_EGL_WARNING, "wayland-egl: drmGetMagic failed");
          return;
       }