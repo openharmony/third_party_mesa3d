openharmony/third_party_mesa3d

commit bdbeca227ad4bb6a1779679e775110e385a53ee3

iris: add Android build
Note that at least following additional libs/components require changes
since they refer to BOARD_GPU_DRIVERS variable which is used to select
the driver:

  - mixins
  - minigbm
  - libdrm
  - drm_gralloc

v2: (feedback by Gustaw Smolarczyk) Fix trailing \ in a few cases

Signed-off-by: Tapani Pälli <tapani.palli@intel.com>

src/intel/Android.isl.mk
@@ -198,6 +198,19 @@ LOCAL_WHOLE_STATIC_LIBRARIES := libmesa_genxml
 include $(MESA_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
 
+
+# ---------------------------------------
+# Build libmesa_isl_tiled_memcpy
+# ---------------------------------------
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libmesa_isl_tiled_memcpy
+
+LOCAL_SRC_FILES := isl/isl_tiled_memcpy.c
+
+include $(MESA_COMMON_MK)
+include $(BUILD_STATIC_LIBRARY)
+
 # ---------------------------------------
 # Build libmesa_isl_tiled_memcpy
 # ---------------------------------------

Android.mk
@@ -24,7 +24,7 @@
 # BOARD_GPU_DRIVERS should be defined.  The valid values are
 #
 #   classic drivers: i915 i965
-#   gallium drivers: swrast freedreno i915g nouveau kmsro r300g r600g radeonsi vc4 virgl vmwgfx etnaviv
+#   gallium drivers: swrast freedreno i915g nouveau kmsro r300g r600g radeonsi vc4 virgl vmwgfx etnaviv iris
 #
 # The main target is libGLES_mesa.  For each classic driver enabled, a DRI
 # module will also be built.  DRI modules will be loaded by libGLES_mesa.
@@ -59,7 +59,8 @@ gallium_drivers := \
 	vmwgfx.HAVE_GALLIUM_VMWGFX \
 	vc4.HAVE_GALLIUM_VC4 \
 	virgl.HAVE_GALLIUM_VIRGL \
-	etnaviv.HAVE_GALLIUM_ETNAVIV
+	etnaviv.HAVE_GALLIUM_ETNAVIV \
+	iris.HAVE_GALLIUM_IRIS
 
 ifeq ($(BOARD_GPU_DRIVERS),all)
 MESA_BUILD_CLASSIC := $(filter HAVE_%, $(subst ., , $(classic_drivers)))

src/gallium/Android.mk
@@ -47,6 +47,7 @@ SUBDIRS += winsys/virgl/drm winsys/virgl/vtest drivers/virgl
 SUBDIRS += winsys/svga/drm drivers/svga
 SUBDIRS += winsys/etnaviv/drm drivers/etnaviv drivers/renderonly
 SUBDIRS += state_trackers/dri
+SUBDIRS += winsys/iris/drm drivers/iris
 
 # sort to eliminate any duplicates
 INC_DIRS := $(call all-named-subdir-makefiles,$(sort $(SUBDIRS)))

src/gallium/drivers/iris/Android.mk
@@ -1,0 +1,140 @@
+# Mesa 3-D graphics library
+#
+# Copyright (C) 2018 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+LOCAL_PATH := $(call my-dir)
+
+# get C_SOURCES
+include $(LOCAL_PATH)/Makefile.sources
+
+include $(CLEAR_VARS)
+
+LIBIRIS_SRC_FILES := \
+	iris_blorp.c \
+	iris_state.c
+
+LIBIRIS_STATIC_LIBS := \
+	libmesa_nir
+
+IRIS_COMMON_INCLUDES := \
+	$(MESA_TOP)/src/mapi \
+	$(MESA_TOP)/src/mesa \
+	$(MESA_TOP)/src/gallium/include \
+	$(MESA_TOP)/src/gallium/auxiliary
+
+#
+# libiris for gen9
+#
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmesa_iris_gen9
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+
+LOCAL_SRC_FILES := $(LIBIRIS_SRC_FILES)
+LOCAL_CFLAGS := -DGEN_VERSIONx10=90
+
+LOCAL_C_INCLUDES := $(IRIS_COMMON_INCLUDES)
+
+LOCAL_STATIC_LIBRARIES := $(LIBIRIS_STATIC_LIBS)
+
+LOCAL_WHOLE_STATIC_LIBRARIES := libmesa_genxml
+
+include $(MESA_COMMON_MK)
+include $(BUILD_STATIC_LIBRARY)
+
+#
+# libiris for gen10
+#
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmesa_iris_gen10
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+
+LOCAL_SRC_FILES := $(LIBIRIS_SRC_FILES)
+LOCAL_CFLAGS := -DGEN_VERSIONx10=100
+
+LOCAL_C_INCLUDES := $(IRIS_COMMON_INCLUDES)
+
+LOCAL_STATIC_LIBRARIES := $(LIBIRIS_STATIC_LIBS)
+
+LOCAL_WHOLE_STATIC_LIBRARIES := libmesa_genxml
+
+include $(MESA_COMMON_MK)
+include $(BUILD_STATIC_LIBRARY)
+
+#
+# libiris for gen11
+#
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmesa_iris_gen11
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+
+LOCAL_SRC_FILES := $(LIBIRIS_SRC_FILES)
+LOCAL_CFLAGS := -DGEN_VERSIONx10=110
+
+LOCAL_C_INCLUDES := $(IRIS_COMMON_INCLUDES)
+
+LOCAL_STATIC_LIBRARIES := $(LIBIRIS_STATIC_LIBS)
+
+LOCAL_WHOLE_STATIC_LIBRARIES := libmesa_genxml
+
+include $(MESA_COMMON_MK)
+include $(BUILD_STATIC_LIBRARY)
+
+
+###########################################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libmesa_pipe_iris
+
+LOCAL_SRC_FILES := \
+	$(IRIS_C_SOURCES)
+
+LOCAL_C_INCLUDES := \
+	$(MESA_TOP)/src/mapi \
+	$(MESA_TOP)/src/mesa \
+	$(MESA_TOP)/include/drm-uapi \
+	$(MESA_TOP)/src/gallium/include
+
+LOCAL_SHARED_LIBRARIES := libdrm_intel
+
+LOCAL_STATIC_LIBRARIES := \
+	libmesa_intel_common \
+	libmesa_nir
+
+LOCAL_WHOLE_STATIC_LIBRARIES := \
+	libmesa_genxml \
+	libmesa_blorp \
+	libmesa_intel_common \
+	libmesa_intel_compiler \
+	libmesa_iris_gen9 \
+	libmesa_iris_gen10 \
+	libmesa_iris_gen11
+
+include $(GALLIUM_COMMON_MK)
+include $(BUILD_STATIC_LIBRARY)
+
+ifneq ($(HAVE_GALLIUM_IRIS),)
+GALLIUM_TARGET_DRIVERS += iris
+$(eval GALLIUM_LIBS += $(LOCAL_MODULE) libmesa_winsys_iris)
+$(eval GALLIUM_SHARED_LIBS += $(LOCAL_SHARED_LIBRARIES))
+endif

src/gallium/drivers/iris/Makefile.sources
@@ -1,0 +1,48 @@
+# Mesa 3-D graphics library
+#
+# Copyright (C) 2018 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+IRIS_C_SOURCES = \
+	iris_batch.c \
+	iris_batch.h \
+	iris_binder.c \
+	iris_binder.h \
+	iris_blit.c \
+	iris_border_color.c \
+	iris_bufmgr.c \
+	iris_bufmgr.h \
+	iris_clear.c \
+	iris_context.c \
+	iris_context.h \
+	iris_draw.c \
+	iris_fence.c \
+	iris_fence.h \
+	iris_formats.c \
+	iris_pipe.h \
+	iris_pipe_control.c \
+	iris_program.c \
+	iris_program_cache.c \
+	iris_query.c \
+	iris_resolve.c \
+	iris_resource.c \
+	iris_resource.h \
+	iris_screen.c \
+	iris_screen.h

src/gallium/winsys/iris/drm/Android.mk
@@ -1,0 +1,40 @@
+# Mesa 3-D graphics library
+#
+# Copyright (C) 2018 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+LOCAL_PATH := $(call my-dir)
+
+# get C_SOURCES
+include $(LOCAL_PATH)/Makefile.sources
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(C_SOURCES)
+
+LOCAL_WHOLE_STATIC_LIBRARIES := \
+	libmesa_isl \
+	libmesa_intel_dev
+
+LOCAL_SHARED_LIBRARIES := libdrm_intel
+LOCAL_MODULE := libmesa_winsys_iris
+
+include $(GALLIUM_COMMON_MK)
+include $(BUILD_STATIC_LIBRARY)

src/gallium/winsys/iris/drm/Makefile.sources
@@ -1,0 +1,3 @@
+C_SOURCES := \
+	iris_drm_public.h \
+	iris_drm_winsys.c

commit 28781a4046e251113ab1258709de8270174b6260

android: intel/isl: remove redundant building rules
Fixes the following building error:

including ./external/mesa/Android.mk ...
build/core/base_rules.mk:183: *** external/mesa/src/intel:
MODULE.TARGET.STATIC_LIBRARIES.libmesa_isl_tiled_memcpy already defined by external/mesa/src/intel.
make: *** [build/core/ninja.mk:164: out/build-android_x86_64.ninja] Error 1

ISL_TILED_MEMCPY_FILES is isl/isl_tiled_memcpy_normal.c
and that source file includes isl_tiled_memcpy.c source

Fixes: bdbeca2 ("iris: add Android build")
Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>

src/intel/Android.isl.mk
@@ -198,19 +198,6 @@ LOCAL_WHOLE_STATIC_LIBRARIES := libmesa_genxml
 include $(MESA_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
 
-
-# ---------------------------------------
-# Build libmesa_isl_tiled_memcpy
-# ---------------------------------------
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := libmesa_isl_tiled_memcpy
-
-LOCAL_SRC_FILES := isl/isl_tiled_memcpy.c
-
-include $(MESA_COMMON_MK)
-include $(BUILD_STATIC_LIBRARY)
-
 # ---------------------------------------
 # Build libmesa_isl_tiled_memcpy
 # ---------------------------------------