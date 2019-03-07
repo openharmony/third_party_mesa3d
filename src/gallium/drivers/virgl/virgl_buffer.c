openharmony/third_party_mesa3d

commit f0499bebcb7b2f3932062e1ceea944a338f5f484

virgl: don't mark unclean after a flush
The guest memory is still clean until host GL touches it,
which we should track elsewhere.

Reviewed-by: Gert Wollny <gert.wollny@collabora.com>

src/gallium/drivers/virgl/virgl_buffer.c
@@ -120,7 +120,6 @@ static void virgl_buffer_transfer_flush_region(struct pipe_context *ctx,
     * We'll end up flushing 25 --> 70.
     */
    util_range_add(&trans->range, box->x, box->x + box->width);
-   vbuf->clean[0] = FALSE;
 }
 
 static const struct u_resource_vtbl virgl_buffer_vtbl =

commit 7de299bed7c8be6d9a02498449db85af9af83da7

virgl: remove unused variable
This variable is now unused, so let's remove it.

Fixes: f0499bebcb7 (virgl: don't mark unclean after a flush)
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>

src/gallium/drivers/virgl/virgl_buffer.c
@@ -100,7 +100,6 @@ static void virgl_buffer_transfer_flush_region(struct pipe_context *ctx,
                                                struct pipe_transfer *transfer,
                                                const struct pipe_box *box)
 {
-   struct virgl_resource *vbuf = virgl_resource(transfer->resource);
    struct virgl_transfer *trans = virgl_transfer(transfer);
 
    /*