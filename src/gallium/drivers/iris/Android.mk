openharmony/third_party_mesa3d

commit a06bec5d17651474f571116aafb8a712d037f7f4

iris: Add mechanism for iris-specific driconf options
Based on Nicolai's 7fef8d6fea457c696340829af292ad51b1d63e6f.

Reviewed-by: Dylan Baker <dylan@pnwbakers.com>

src/gallium/drivers/iris/Android.mk
@@ -125,6 +125,24 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libmesa_pipe_iris
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+
+intermediates := $(call local-generated-sources-dir)
+
+LOCAL_GENERATED_SOURCES := $(addprefix $(intermediates)/iris/,$(GENERATED_SOURCES))
+
+GEN_DRIINFO_INPUTS := \
+        $(MESA_TOP)/src/gallium/auxiliary/pipe-loader/driinfo_gallium.h \
+        $(LOCAL_PATH)/driinfo_iris.h
+
+MERGE_DRIINFO := $(MESA_TOP)/src/util/merge_driinfo.py
+
+$(intermediates)/iris/si_driinfo.h: $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS)
+	@mkdir -p $(dir $@)
+	@echo "Gen Header: $(PRIVATE_MODULE) <= $(notdir $(@))"
+	$(hide) $(MESA_PYTHON2) $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS) > $@ || ($(RM) $@; false)
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(intermediates)
 
 LOCAL_SRC_FILES := \
 	$(IRIS_C_SOURCES)

src/gallium/auxiliary/pipe-loader/pipe_loader_drm.c
@@ -74,7 +74,7 @@ static const struct drm_driver_descriptor driver_descriptors[] = {
     {
         .driver_name = "iris",
         .create_screen = pipe_iris_create_screen,
-        .configuration = pipe_default_configuration_query,
+        .configuration = pipe_iris_configuration_query,
     },
     {
         .driver_name = "nouveau",

src/gallium/auxiliary/target-helpers/drm_helper.h
@@ -68,8 +68,26 @@ pipe_iris_create_screen(int fd, const struct pipe_screen_config *config)
 {
    struct pipe_screen *screen;
 
-   screen = iris_drm_screen_create(fd);
+   screen = iris_drm_screen_create(fd, config);
    return screen ? debug_screen_wrap(screen) : NULL;
+}
+
+const struct drm_conf_ret *
+pipe_iris_configuration_query(enum drm_conf conf)
+{
+   static const struct drm_conf_ret xml_options_ret = {
+      .type = DRM_CONF_POINTER,
+      .val.val_pointer =
+#include "iris/iris_driinfo.h"
+   };
+
+   switch (conf) {
+   case DRM_CONF_XML_OPTIONS:
+      return &xml_options_ret;
+   default:
+      break;
+   }
+   return pipe_default_configuration_query(conf);
 }
 
 #else
@@ -78,6 +96,12 @@ struct pipe_screen *
 pipe_iris_create_screen(int fd, const struct pipe_screen_config *config)
 {
    fprintf(stderr, "iris: driver missing\n");
+   return NULL;
+}
+
+const struct drm_conf_ret *
+pipe_iris_configuration_query(enum drm_conf conf)
+{
    return NULL;
 }
 

src/gallium/auxiliary/target-helpers/drm_helper_public.h
@@ -12,6 +12,8 @@ pipe_i915_create_screen(int fd, const struct pipe_screen_config *config);
 
 struct pipe_screen *
 pipe_iris_create_screen(int fd, const struct pipe_screen_config *config);
+const struct drm_conf_ret *
+pipe_iris_configuration_query(enum drm_conf conf);
 
 struct pipe_screen *
 pipe_nouveau_create_screen(int fd, const struct pipe_screen_config *config);

src/gallium/drivers/iris/Makefile.sources
@@ -20,7 +20,12 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+GENERATED_SOURCES := \
+	iris_driinfo.h
+
 IRIS_C_SOURCES = \
+	$(GENERATED_SOURCES) \
+	driinfo_iris.h \
 	iris_batch.c \
 	iris_batch.h \
 	iris_binder.c \

src/gallium/drivers/iris/driinfo_iris.h
@@ -1,0 +1,1 @@
+// iris specific driconf options

src/gallium/drivers/iris/iris_screen.c
@@ -552,7 +552,7 @@ iris_shader_perf_log(void *data, const char *fmt, ...)
 }
 
 struct pipe_screen *
-iris_screen_create(int fd)
+iris_screen_create(int fd, const struct pipe_screen_config *config)
 {
    struct iris_screen *screen = rzalloc(NULL, struct iris_screen);
    if (!screen)

src/gallium/drivers/iris/iris_screen.h
@@ -74,7 +74,8 @@ struct iris_screen {
    struct iris_bo *workaround_bo;
 };
 
-struct pipe_screen *iris_screen_create(int fd);
+struct pipe_screen *
+iris_screen_create(int fd, const struct pipe_screen_config *config);
 
 boolean
 iris_is_format_supported(struct pipe_screen *pscreen,

commit 7ae647bbe11cff6310fc92ca4542a478ceabfc4b

android/iris: fix driinfo header filename
Fixes iris driver Android build.

Fixes: a06bec5d176 "iris: Add mechanism for iris-specific driconf options"
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

src/gallium/drivers/iris/Android.mk
@@ -137,7 +137,7 @@ GEN_DRIINFO_INPUTS := \
 
 MERGE_DRIINFO := $(MESA_TOP)/src/util/merge_driinfo.py
 
-$(intermediates)/iris/si_driinfo.h: $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS)
+$(intermediates)/iris/iris_driinfo.h: $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS)
 	@mkdir -p $(dir $@)
 	@echo "Gen Header: $(PRIVATE_MODULE) <= $(notdir $(@))"
 	$(hide) $(MESA_PYTHON2) $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS) > $@ || ($(RM) $@; false)