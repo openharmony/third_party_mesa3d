openharmony/third_party_mesa3d

commit 04a53c64ebe830abc6b2c29a16974f230051281d

anv: release memory allocated by bo_heap when descriptor pool is destroyed
Fixes following leak:

   ==21853== 32 bytes in 1 blocks are definitely lost in loss record 2 of 20
   ==21853==    at 0x483AB1A: calloc (vg_replace_malloc.c:762)
   ==21853==    by 0x4C4DD7F: util_vma_heap_free (vma.c:221)
   ==21853==    by 0x4C4D647: util_vma_heap_init (vma.c:46)
   ==21853==    by 0x4957B9F: anv_CreateDescriptorPool (anv_descriptor_set.c:578)

Fixes: 7ae3ea7566b5 ("anv: Add a concept of a descriptor buffer")
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

src/intel/vulkan/anv_descriptor_set.c
@@ -614,6 +614,8 @@ void anv_DestroyDescriptorPool(
       anv_descriptor_set_destroy(device, pool, set);
    }
 
+   util_vma_heap_finish(&pool->bo_heap);
+
    vk_free2(&device->alloc, pAllocator, pool);
 }
 

commit 572248e1e9b9c1458989c35dd5baec613ee68f83

anv/descriptor_set: Only vma_heap_finish if we have a descriptor buffer
Fixes: 04a53c64ebe "anv: release memory allocated by bo_heap when..."
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

src/intel/vulkan/anv_descriptor_set.c
@@ -754,10 +754,9 @@ void anv_DestroyDescriptorPool(
       anv_gem_munmap(pool->bo.map, pool->bo.size);
       anv_vma_free(device, &pool->bo);
       anv_gem_close(device, pool->bo.gem_handle);
+      util_vma_heap_finish(&pool->bo_heap);
    }
    anv_state_stream_finish(&pool->surface_state_stream);
-
-   util_vma_heap_finish(&pool->bo_heap);
 
    vk_free2(&device->alloc, pAllocator, pool);
 }