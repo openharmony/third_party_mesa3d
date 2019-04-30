aras-p/glsl-optimizer

commit fe5d67d95f3a5fc84c5421d409a6464642aaf2cb

Implement EXT_fog_coord and EXT_secondary_color.
EXT_secondary_color is disabled until we get some dispatch offsets from SGI.

src/mesa/main/dlist.c
@@ -1,4 +1,4 @@
-/* $Id: dlist.c,v 1.46 2000/09/26 20:53:53 brianp Exp $ */
+/* $Id: dlist.c,v 1.47 2000/10/27 16:44:40 keithw Exp $ */
 
 /*
  * Mesa 3-D graphics library
@@ -5076,6 +5076,10 @@ _mesa_init_dlist_table( struct _glapi_table *table, GLuint tableSize )
    table->FeedbackBuffer = _mesa_FeedbackBuffer;
    table->Finish = _mesa_Finish;
    table->Flush = _mesa_Flush;
+   table->FogCoordfEXT = _mesa_FogCoordfEXT;
+   table->FogCoordfvEXT = _mesa_FogCoordfvEXT;
+   table->FogCoorddEXT = _mesa_FogCoorddEXT;
+   table->FogCoorddvEXT = _mesa_FogCoorddvEXT;
    table->Fogf = save_Fogf;
    table->Fogfv = save_Fogfv;
    table->Fogi = save_Fogi;
@@ -5226,6 +5230,26 @@ _mesa_init_dlist_table( struct _glapi_table *table, GLuint tableSize )
    table->Scaled = save_Scaled;
    table->Scalef = save_Scalef;
    table->Scissor = save_Scissor;
+
+#if 0
+   table->SecondaryColor3fEXT = _mesa_SecondaryColor3bEXT;
+   table->SecondaryColor3fvEXT = _mesa_SecondaryColor3bvEXT;
+   table->SecondaryColor3dEXT = _mesa_SecondaryColor3sEXT;
+   table->SecondaryColordvEXT = _mesa_SecondaryColor3svEXT;
+   table->SecondaryColor3fEXT = _mesa_SecondaryColor3iEXT;
+   table->SecondaryColor3fvEXT = _mesa_SecondaryColor3ivEXT;
+   table->SecondaryColor3dEXT = _mesa_SecondaryColor3fEXT;
+   table->SecondaryColordvEXT = _mesa_SecondaryColor3fvEXT;
+   table->SecondaryColor3fEXT = _mesa_SecondaryColor3dEXT;
+   table->SecondaryColor3fvEXT = _mesa_SecondaryColor3dvEXT;
+   table->SecondaryColor3dEXT = _mesa_SecondaryColor3ubEXT;
+   table->SecondaryColordvEXT = _mesa_SecondaryColor3ubvEXT;
+   table->SecondaryColor3fEXT = _mesa_SecondaryColor3usEXT;
+   table->SecondaryColor3fvEXT = _mesa_SecondaryColor3usvEXT;
+   table->SecondaryColor3dEXT = _mesa_SecondaryColor3uiEXT;
+   table->SecondaryColordvEXT = _mesa_SecondaryColor3uivEXT;
+#endif
+
    table->SelectBuffer = _mesa_SelectBuffer;
    table->ShadeModel = save_ShadeModel;
    table->StencilFunc = save_StencilFunc;

src/mesa/drivers/glide/fxdd.c
@@ -886,6 +886,7 @@ void fxDDInitExtensions( GLcontext *ctx )
    gl_extensions_disable(ctx, "GL_EXT_blend_minmax");
    gl_extensions_disable(ctx, "GL_EXT_blend_subtract");
    gl_extensions_disable(ctx, "GL_EXT_blend_color");
+   gl_extensions_disable(ctx, "GL_EXT_fog_coord");
 
    gl_extensions_add(ctx, DEFAULT_ON, "3DFX_set_global_palette", 0);
    

src/mesa/main/clip.c
@@ -1,4 +1,4 @@
-/* $Id: clip.c,v 1.9 2000/09/26 20:53:53 brianp Exp $ */
+/* $Id: clip.c,v 1.10 2000/10/27 16:44:40 keithw Exp $ */
 
 /*
  * Mesa 3-D graphics library
@@ -396,7 +396,7 @@ void gl_update_clipmask( GLcontext *ctx )
 	 mask |= CLIP_INDEX1;
    }
 
-   if (ctx->FogMode == FOG_FRAGMENT && (ctx->TriangleCaps & DD_CLIP_FOG_COORD))
+   if (ctx->Fog.Enabled)
       mask |= CLIP_FOG_COORD;
    
    ctx->ClipInterpFunc = clip_interp_tab[mask];

src/mesa/main/context.c
@@ -1,4 +1,4 @@
-/* $Id: context.c,v 1.95 2000/10/23 00:16:28 gareth Exp $ */
+/* $Id: context.c,v 1.96 2000/10/27 16:44:40 keithw Exp $ */
 
 /*
  * Mesa 3-D graphics library
@@ -698,6 +698,22 @@ init_fallback_arrays( GLcontext *ctx )
    cl->Ptr = (void *) ctx->Current.ByteColor;
    cl->Enabled = 1;
 
+   cl = &ctx->Fallback.SecondaryColor;
+   cl->Size = 3;
+   cl->Type = GL_UNSIGNED_BYTE;
+   cl->Stride = 0;
+   cl->StrideB = 0;
+   cl->Ptr = (void *) ctx->Current.SecondaryColor;
+   cl->Enabled = 1;
+
+   cl = &ctx->Fallback.FogCoord;
+   cl->Size = 1;
+   cl->Type = GL_FLOAT;
+   cl->Stride = 0;
+   cl->StrideB = 0;
+   cl->Ptr = (void *) &ctx->Current.FogCoord;
+   cl->Enabled = 1;
+
    cl = &ctx->Fallback.Index;
    cl->Size = 1;
    cl->Type = GL_UNSIGNED_INT;
@@ -887,6 +903,8 @@ init_attrib_groups( GLcontext *ctx )
    ctx->Current.Flag = (VERT_NORM |
                         VERT_INDEX |
                         VERT_RGBA |
+                        VERT_SPEC_RGB |
+                        VERT_FOG_COORD |
                         VERT_EDGE |
                         VERT_TEX0_1 |
                         VERT_TEX1_1 |
@@ -969,6 +987,8 @@ init_attrib_groups( GLcontext *ctx )
    ctx->Fog.Density = 1.0;
    ctx->Fog.Start = 0.0;
    ctx->Fog.End = 1.0;
+   ctx->Fog.ColorSumEnabled = GL_FALSE;
+   ctx->Fog.FogCoordinateSource = GL_FRAGMENT_DEPTH_EXT;
 
    /* Hint group */
    ctx->Hint.PerspectiveCorrection = GL_DONT_CARE;

src/mesa/main/drawpix.c
@@ -1,4 +1,4 @@
-/* $Id: drawpix.c,v 1.38 2000/10/17 00:42:02 brianp Exp $ */
+/* $Id: drawpix.c,v 1.39 2000/10/27 16:44:40 keithw Exp $ */
 
 /*
  * Mesa 3-D graphics library
@@ -239,7 +239,7 @@ fast_draw_pixels(GLcontext *ctx, GLint x, GLint y,
                GLint row;
                for (row=0; row<drawHeight; row++) {
                   gl_write_zoomed_rgba_span(ctx, drawWidth, destX, destY,
-                                            zSpan, (void *) src, zoomY0);
+                                            zSpan, 0, (void *) src, zoomY0);
                   src += rowLength * 4;
                   destY++;
                }
@@ -276,7 +276,7 @@ fast_draw_pixels(GLcontext *ctx, GLint x, GLint y,
                GLint row;
                for (row=0; row<drawHeight; row++) {
                   gl_write_zoomed_rgb_span(ctx, drawWidth, destX, destY,
-                                           zSpan, (void *) src, zoomY0);
+                                           zSpan, 0, (void *) src, zoomY0);
                   src += rowLength * 3;
                   destY++;
                }
@@ -335,7 +335,7 @@ fast_draw_pixels(GLcontext *ctx, GLint x, GLint y,
                      rgb[i][2] = src[i];
 		  }
                   gl_write_zoomed_rgb_span(ctx, drawWidth, destX, destY,
-                                           zSpan, (void *) rgb, zoomY0);
+                                           zSpan, 0, (void *) rgb, zoomY0);
                   src += rowLength;
                   destY++;
                }
@@ -400,7 +400,7 @@ fast_draw_pixels(GLcontext *ctx, GLint x, GLint y,
                      rgba[i][3] = *ptr++;
 		  }
                   gl_write_zoomed_rgba_span(ctx, drawWidth, destX, destY,
-                                            zSpan, (void *) rgba, zoomY0);
+                                            zSpan, 0, (void *) rgba, zoomY0);
                   src += rowLength*2;
                   destY++;
                }
@@ -447,7 +447,7 @@ fast_draw_pixels(GLcontext *ctx, GLint x, GLint y,
                   ASSERT(drawWidth < MAX_WIDTH);
                   _mesa_map_ci8_to_rgba(ctx, drawWidth, src, rgba);
                   gl_write_zoomed_rgba_span(ctx, drawWidth, destX, destY,
-                                            zSpan, (void *) rgba, zoomY0);
+                                            zSpan, 0, (void *) rgba, zoomY0);
                   src += rowLength;
                   destY++;
                }
@@ -520,10 +520,10 @@ draw_index_pixels( GLcontext *ctx, GLint x, GLint y,
                               type, source, &ctx->Unpack,
                               ctx->ImageTransferState);
       if (zoom) {
-         gl_write_zoomed_index_span(ctx, drawWidth, x, y, zspan, indexes, desty);
+         gl_write_zoomed_index_span(ctx, drawWidth, x, y, zspan, 0, indexes, desty);
       }
       else {
-         gl_write_index_span(ctx, drawWidth, x, y, zspan, indexes, GL_BITMAP);
+         gl_write_index_span(ctx, drawWidth, x, y, zspan, 0, indexes, GL_BITMAP);
       }
    }
 }
@@ -643,7 +643,7 @@ draw_depth_pixels( GLcontext *ctx, GLint x, GLint y,
          GLint i;
          for (i = 0; i < width; i++)
             zspan[i] = zptr[i];
-         gl_write_rgba_span( ctx, width, x, y, zspan, rgba, GL_BITMAP );
+         gl_write_rgba_span( ctx, width, x, y, zspan, 0, rgba, GL_BITMAP );
       }
    }
    else if (type==GL_UNSIGNED_INT && ctx->Visual.DepthBits == 32
@@ -653,7 +653,7 @@ draw_depth_pixels( GLcontext *ctx, GLint x, GLint y,
       for (row = 0; row < height; row++, y++) {
          const GLuint *zptr = _mesa_image_address(&ctx->Unpack,
                 pixels, width, height, GL_DEPTH_COMPONENT, type, 0, row, 0);
-         gl_write_rgba_span( ctx, width, x, y, zptr, rgba, GL_BITMAP );
+         gl_write_rgba_span( ctx, width, x, y, zptr, 0, rgba, GL_BITMAP );
       }
    }
    else {
@@ -667,20 +667,21 @@ draw_depth_pixels( GLcontext *ctx, GLint x, GLint y,
                                   &ctx->Unpack, ctx->ImageTransferState );
          if (ctx->Visual.RGBAflag) {
             if (zoom) {
-               gl_write_zoomed_rgba_span(ctx, width, x, y, zspan,
+               gl_write_zoomed_rgba_span(ctx, width, x, y, zspan, 0,
                                          (const GLubyte (*)[4])rgba, desty);
             }
             else {
-               gl_write_rgba_span(ctx, width, x, y, zspan, rgba, GL_BITMAP);
+               gl_write_rgba_span(ctx, width, x, y, zspan, 0, rgba, GL_BITMAP);
             }
          }
          else {
             if (zoom) {
-               gl_write_zoomed_index_span(ctx, width, x, y, zspan,
+               gl_write_zoomed_index_span(ctx, width, x, y, zspan, 0,
                                           ispan, GL_BITMAP);
             }
             else {
-               gl_write_index_span(ctx, width, x, y, zspan, ispan, GL_BITMAP);
+               gl_write_index_span(ctx, width, x, y, zspan, 0,
+				   ispan, GL_BITMAP);
             }
          }
 
@@ -827,11 +828,12 @@ draw_rgba_pixels( GLcontext *ctx, GLint x, GLint y,
                                           (CONST GLubyte (*)[]) rgba, NULL);
          }
          else if (zoom) {
-            gl_write_zoomed_rgba_span( ctx, width, x, y, zspan, 
+            gl_write_zoomed_rgba_span( ctx, width, x, y, zspan, 0,
 				       (CONST GLubyte (*)[]) rgba, desty );
          }
          else {
-            gl_write_rgba_span( ctx, (GLuint) width, x, y, zspan, rgba, GL_BITMAP);
+            gl_write_rgba_span( ctx, (GLuint) width, x, y, zspan, 0,
+				rgba, GL_BITMAP);
          }
       }
    }

src/mesa/main/enable.c
@@ -1,4 +1,4 @@
-/* $Id: enable.c,v 1.26 2000/10/21 01:29:12 brianp Exp $ */
+/* $Id: enable.c,v 1.27 2000/10/27 16:44:40 keithw Exp $ */
 
 /*
  * Mesa 3-D graphics library
@@ -535,6 +535,16 @@ void _mesa_set_enable( GLcontext *ctx, GLenum cap, GLboolean state )
          }
 	 break;
 
+      /* GL_EXT_secondary_color */
+      case GL_COLOR_SUM_EXT:
+         ctx->Fog.ColorSumEnabled = state;
+	 if (state)
+	    SET_BITS(ctx->TriangleCaps, DD_SEPERATE_SPECULAR);
+	 else if (ctx->Light.Model.ColorControl == GL_SINGLE_COLOR)
+	    CLEAR_BITS(ctx->TriangleCaps, DD_SEPERATE_SPECULAR);
+	 ctx->NewState |= NEW_RASTER_OPS;
+         break;
+
       default:
          gl_error(ctx, GL_INVALID_ENUM, state ? "glEnable" : "glDisable");
          return;
@@ -814,6 +824,12 @@ client_state( GLcontext *ctx, GLenum cap, GLboolean state )
          break;
       case GL_EDGE_FLAG_ARRAY:
          ctx->Array.EdgeFlag.Enabled = state;
+         break;
+      case GL_FOG_COORDINATE_ARRAY_EXT:
+         ctx->Array.FogCoord.Enabled = state;
+         break;
+      case GL_SECONDARY_COLOR_ARRAY_EXT:
+         ctx->Array.SecondaryColor.Enabled = state;
          break;
       default:
          gl_error( ctx, GL_INVALID_ENUM, "glEnable/DisableClientState" );

src/mesa/main/extensions.c
@@ -1,4 +1,4 @@
-/* $Id: extensions.c,v 1.36 2000/09/25 16:03:59 brianp Exp $ */
+/* $Id: extensions.c,v 1.37 2000/10/27 16:44:40 keithw Exp $ */
 
 /*
  * Mesa 3-D graphics library
@@ -92,7 +92,12 @@ static struct { int enabled; const char *name; } default_extensions[] = {
    { DEFAULT_ON,     "GL_SGIS_pixel_texture" },
    { DEFAULT_ON,     "GL_SGIS_texture_edge_clamp" },
    { DEFAULT_ON,     "GL_SGIX_pixel_texture" },
-   { DEFAULT_OFF,    "GL_3DFX_texture_compression_FXT1" }
+   { DEFAULT_OFF,    "GL_3DFX_texture_compression_FXT1" },
+
+   { DEFAULT_OFF,    "GL_EXT_secondary_color" }, 
+   { DEFAULT_ON,     "GL_EXT_fog_coord" },
+   { DEFAULT_OFF,    "GL_EXT_bgra" },
+   { DEFAULT_OFF,    "GL_EXT_cull_vertex" },
 };
 
 

src/mesa/main/fog.c
@@ -1,4 +1,4 @@
-/* $Id: fog.c,v 1.20 2000/09/26 20:53:53 brianp Exp $ */
+/* $Id: fog.c,v 1.21 2000/10/27 16:44:40 keithw Exp $ */
 
 /*
  * Mesa 3-D graphics library
@@ -64,6 +64,7 @@ _mesa_Fogiv(GLenum pname, const GLint *params )
       case GL_FOG_START:
       case GL_FOG_END:
       case GL_FOG_INDEX:
+      case GL_FOG_COORDINATE_SOURCE_EXT:
 	 p[0] = (GLfloat) *params;
 	 break;
       case GL_FOG_COLOR:
@@ -123,6 +124,14 @@ _mesa_Fogfv( GLenum pname, const GLfloat *params )
 	 ctx->Fog.Color[2] = params[2];
 	 ctx->Fog.Color[3] = params[3];
          break;
+      case GL_FOG_COORDINATE_SOURCE_EXT: {
+	 GLenum p = (GLenum)(GLint) *params;
+	 if (p == GL_FOG_COORDINATE_EXT || p == GL_FRAGMENT_DEPTH_EXT)
+	    ctx->Fog.FogCoordinateSource = p;
+	 else
+	    gl_error( ctx, GL_INVALID_ENUM, "glFog" );
+	 break;
+      }
       default:
          gl_error( ctx, GL_INVALID_ENUM, "glFog" );
          return;
@@ -136,218 +145,136 @@ _mesa_Fogfv( GLenum pname, const GLfloat *params )
 }
 
 
-typedef void (*fog_func)( struct vertex_buffer *VB, GLuint side, 
-			  GLubyte flag );
-
-typedef void (*fog_coord_func)( struct vertex_buffer *VB, 
-				const GLvector4f *from,
-				GLubyte flag );
-
-static fog_func fog_ci_tab[2];
-static fog_func fog_rgba_tab[2];
-static fog_coord_func make_fog_coord_tab[2];
-
-/*
- * Compute the fogged color for an array of vertices.
- * Input:  n - number of vertices
- *         v - array of vertices
- *         color - the original vertex colors
- * Output:  color - the fogged colors
- * 
- */
-#define TAG(x) x##_raw
-#define CULLCHECK
-#define IDX 0
-#include "fog_tmp.h"
-
-#define TAG(x) x##_masked
-#define CULLCHECK if (cullmask[i]&flag)
-#define IDX 1
-#include "fog_tmp.h"
 
 
 void
 _mesa_init_fog( void )
 {
-   init_fog_tab_masked();
-   init_fog_tab_raw();
 }
 
-
-/*
- * Compute fog for the vertices in the vertex buffer.
- */
-void
-_mesa_fog_vertices( struct vertex_buffer *VB )
+static GLvector1f *get_fogcoord_ptr( GLcontext *ctx, GLvector1f *tmp )
 {
-   GLcontext *ctx = VB->ctx;
-   GLuint i = VB->CullMode & 1;
+   struct vertex_buffer *VB = ctx->VB;
 
-   if (ctx->Visual.RGBAflag) {
-      /* Fog RGB colors */
-      if (ctx->TriangleCaps & DD_TRI_LIGHT_TWOSIDE) {
-	 fog_rgba_tab[i]( VB, 0, VERT_FACE_FRONT );
-	 fog_rgba_tab[i]( VB, 1, VERT_FACE_REAR );
-      } else {
-	 fog_rgba_tab[i]( VB, 0, VERT_FACE_FRONT|VERT_FACE_REAR );
+   if (ctx->Fog.FogCoordinateSource == GL_FRAGMENT_DEPTH_EXT) {      
+      if (!ctx->NeedEyeCoords) {
+	 GLfloat *m = ctx->ModelView.m;
+	 GLfloat plane[4];
+
+	 plane[0] = m[2];
+	 plane[1] = m[6];
+	 plane[2] = m[10];
+	 plane[3] = m[14];
+
+	 /* Full eye coords weren't required, just calculate the
+	  * eye Z values.  
+	  */
+	 gl_dotprod_tab[0][VB->ObjPtr->size](&VB->Eye, 2,
+					     VB->ObjPtr, plane, 0 );
+
+	 tmp->data = &(VB->Eye.data[0][2]);
+	 tmp->start = VB->Eye.start+2;
+	 tmp->stride = VB->Eye.stride;
+	 return tmp;
       }
-   }
-   else {
-      /* Fog color indexes */
-      if (ctx->TriangleCaps & DD_TRI_LIGHT_TWOSIDE) {
-	 fog_ci_tab[i]( VB, 0, VERT_FACE_FRONT );
-         fog_ci_tab[i]( VB, 1, VERT_FACE_REAR );
-      } else {
-	 fog_ci_tab[i]( VB, 0, VERT_FACE_FRONT|VERT_FACE_REAR );
+      else 
+      {
+	 if (VB->EyePtr->size < 2)
+	    gl_vector4f_clean_elem( &VB->Eye, VB->Count, 2 );
+
+	 tmp->data = &(VB->EyePtr->data[0][2]);
+	 tmp->start = VB->EyePtr->start+2;
+	 tmp->stride = VB->EyePtr->stride;
+	 return tmp;
       }
-   }
+   } else
+      return VB->FogCoordPtr;
 }
 
 
-static void check_fog_coords( GLcontext *ctx, struct gl_pipeline_stage *d )
-{
-   d->type = 0;
-
-   if (ctx->FogMode==FOG_FRAGMENT)
-   {
-      d->type = PIPE_IMMEDIATE|PIPE_PRECALC;
-      d->inputs = VERT_OBJ_ANY;
-      d->outputs = VERT_FOG_COORD;
-   }
-}
-
-
-static void gl_make_fog_coords( struct vertex_buffer *VB )
-{
-   GLcontext *ctx = VB->ctx;
-
-   /* If full eye coords weren't required, just calculate the eye Z
-    * values.  
-    */
-   if (!ctx->NeedEyeCoords) {
-      GLfloat *m = ctx->ModelView.m;
-      GLfloat plane[4];
-
-      plane[0] = m[2];
-      plane[1] = m[6];
-      plane[2] = m[10];
-      plane[3] = m[14];
-
-      gl_dotprod_tab[0][VB->ObjPtr->size](&VB->Eye,
-					  2, /* fill z coordinates */
-					  VB->ObjPtr,
-					  plane,
-					  0 );
-
-      make_fog_coord_tab[0]( VB, &VB->Eye, 0 );
-   }
-   else
-   {
-      make_fog_coord_tab[0]( VB, VB->EyePtr, 0 );
-   }
-}
-
-
-/* Drivers that want fog coordinates in VB->Spec[0] alpha, can substitute this
- * stage for the default PIPE_OP_FOG pipeline stage.
+/* Use lookup table & interpolation?
  */
-struct gl_pipeline_stage gl_fog_coord_stage = {
-   "build fog coordinates",
-   PIPE_OP_FOG,
-   PIPE_PRECALC|PIPE_IMMEDIATE,
-   0,
-   NEW_FOG,
-   NEW_LIGHTING|NEW_RASTER_OPS|NEW_FOG|NEW_MODELVIEW,
-   0, 0,
-   0, 0, 0,
-   check_fog_coords,
-   gl_make_fog_coords 
-};
+static void 
+make_win_fog_coords( struct vertex_buffer *VB,
+		     GLvector1f *fogcoord)
+{
+   const GLcontext *ctx = VB->ctx;
+   GLfloat end  = ctx->Fog.End;
+   GLfloat *v = fogcoord->start;
+   GLuint stride = fogcoord->stride;
+   GLuint n = VB->Count - VB->Start;
+   GLfloat *out;		
+   GLfloat d;
+   GLuint i;
+
+   VB->FogCoordPtr = VB->store.FogCoord;
+   out = VB->FogCoordPtr->data + VB->Start;
+
+   switch (ctx->Fog.Mode) {
+   case GL_LINEAR:
+      d = 1.0F / (ctx->Fog.End - ctx->Fog.Start);
+      for ( i = 0 ; i < n ; i++, STRIDE_F(v, stride)) {
+	 out[i] = (end - ABSF(*v)) * d;
+	 if (0) fprintf(stderr, "z %f out %f\n", *v, out[i]);
+      }
+      break;
+   case GL_EXP:
+      d = -ctx->Fog.Density;
+      for ( i = 0 ; i < n ; i++, STRIDE_F(v,stride)) {
+	 out[i] = exp( d*ABSF(*v) );
+	 if (0) fprintf(stderr, "z %f out %f\n", *v, out[i]);
+      }
+      break;
+   case GL_EXP2:
+      d = -(ctx->Fog.Density*ctx->Fog.Density);
+      for ( i = 0 ; i < n ; i++, STRIDE_F(v, stride)) {
+	 GLfloat z = *v;
+	 out[i] = exp( d*z*z ); 
+	 if (0) fprintf(stderr, "z %f out %f\n", *v, out[i]);
+      }
+      break;
+   default:
+      gl_problem(ctx, "Bad fog mode in make_fog_coord");
+      return;
+   }
+}
 
 
+void 
+_mesa_make_win_fog_coords( struct vertex_buffer *VB )
+{
+   GLvector1f tmp;
+
+   make_win_fog_coords( VB, get_fogcoord_ptr( VB->ctx, &tmp ) );
+}
 
 
 
 /*
  * Apply fog to an array of RGBA pixels.
  * Input:  n - number of pixels
- *         z - array of integer depth values
+ *         fog - array of interpolated screen-space fog coordinates in [0..1]
  *         red, green, blue, alpha - pixel colors
  * Output:  red, green, blue, alpha - fogged pixel colors
  */
 void
 _mesa_fog_rgba_pixels( const GLcontext *ctx,
-                       GLuint n, const GLdepth z[], GLubyte rgba[][4] )
+                       GLuint n, 
+		       const GLfixed fog[], 
+		       GLubyte rgba[][4] )
 {
-   GLfloat c = ctx->ProjectionMatrix.m[10];
-   GLfloat d = ctx->ProjectionMatrix.m[14];
+   GLfixed rFog = ctx->Fog.Color[0] * 255.0;
+   GLfixed gFog = ctx->Fog.Color[1] * 255.0;
+   GLfixed bFog = ctx->Fog.Color[2] * 255.0;
    GLuint i;
 
-   GLfloat rFog = ctx->Fog.Color[0] * 255.0F;
-   GLfloat gFog = ctx->Fog.Color[1] * 255.0F;
-   GLfloat bFog = ctx->Fog.Color[2] * 255.0F;
-
-   GLfloat tz = ctx->Viewport.WindowMap.m[MAT_TZ];
-   GLfloat szInv = 1.0F / ctx->Viewport.WindowMap.m[MAT_SZ];
-
-   switch (ctx->Fog.Mode) {
-      case GL_LINEAR:
-         {
-            GLfloat fogEnd = ctx->Fog.End;
-            GLfloat fogScale = 1.0F / (ctx->Fog.End - ctx->Fog.Start);
-            for (i=0;i<n;i++) {
-               GLfloat ndcz = ((GLfloat) z[i] - tz) * szInv;
-               GLfloat eyez = -d / (c+ndcz);
-               GLfloat f, g;
-               if (eyez < 0.0)  eyez = -eyez;
-               f = (fogEnd - eyez) * fogScale;
-               f = CLAMP( f, 0.0F, 1.0F );
-               g = 1.0F - f;
-               rgba[i][RCOMP] = (GLint) (f * rgba[i][RCOMP] + g * rFog);
-               rgba[i][GCOMP] = (GLint) (f * rgba[i][GCOMP] + g * gFog);
-               rgba[i][BCOMP] = (GLint) (f * rgba[i][BCOMP] + g * bFog);
-            }
-         }
-	 break;
-      case GL_EXP:
-	 for (i=0;i<n;i++) {
-	    GLfloat ndcz = ((GLfloat) z[i] - tz) * szInv;
-	    GLfloat eyez = d / (c+ndcz);
-            GLfloat f, g;
-	    if (eyez < 0.0)
-               eyez = -eyez;
-	    f = exp( -ctx->Fog.Density * eyez );
-            g = 1.0F - f;
-            rgba[i][RCOMP] = (GLint) (f * rgba[i][RCOMP] + g * rFog);
-            rgba[i][GCOMP] = (GLint) (f * rgba[i][GCOMP] + g * gFog);
-            rgba[i][BCOMP] = (GLint) (f * rgba[i][BCOMP] + g * bFog);
-	 }
-	 break;
-      case GL_EXP2:
-         {
-            GLfloat negDensitySquared = -ctx->Fog.Density * ctx->Fog.Density;
-            for (i=0;i<n;i++) {
-               GLfloat ndcz = ((GLfloat) z[i] - tz) * szInv;
-               GLfloat eyez = d / (c+ndcz);
-               GLfloat f, g;
-               GLfloat tmp = negDensitySquared * eyez * eyez;
-#ifdef __alpha__
-               /* XXX this underflow check may be needed for other systems */
-               if (tmp < FLT_MIN_10_EXP)
-                  f = exp( FLT_MIN_10_EXP );
-               else
-#endif
-                  f = exp( tmp );
-               g = 1.0F - f;
-               rgba[i][RCOMP] = (GLint) (f * rgba[i][RCOMP] + g * rFog);
-               rgba[i][GCOMP] = (GLint) (f * rgba[i][GCOMP] + g * gFog);
-               rgba[i][BCOMP] = (GLint) (f * rgba[i][BCOMP] + g * bFog);
-            }
-         }
-	 break;
-      default:
-         gl_problem(ctx, "Bad fog mode in _mesa_fog_rgba_pixels");
-         return;
+   for (i=0;i<n;i++) {
+      GLfixed f = CLAMP(fog[i], 0, FIXED_ONE);
+      GLfixed g = FIXED_ONE - f;
+/*        fprintf(stderr, "f %d/%f g %d ONE %d\n", f, f/(float)FIXED_ONE, g, FIXED_ONE); */
+      rgba[i][0] = (f*rgba[i][0] + g*rFog) >> FIXED_SHIFT;
+      rgba[i][1] = (f*rgba[i][1] + g*gFog) >> FIXED_SHIFT;
+      rgba[i][2] = (f*rgba[i][2] + g*bFog) >> FIXED_SHIFT;
    }
 }
 
@@ -363,7 +290,33 @@ _mesa_fog_rgba_pixels( const GLcontext *ctx,
  */
 void
 _mesa_fog_ci_pixels( const GLcontext *ctx,
-                     GLuint n, const GLdepth z[], GLuint index[] )
+                     GLuint n, const GLfixed fog[], GLuint index[] )
+{
+   GLuint idx = ctx->Fog.Index;
+   GLuint i;
+
+   for (i=0;i<n;i++) {
+      GLfixed f = FixedToFloat(CLAMP(fog[i], 0, FIXED_ONE));
+      index[i] = (GLuint) ((GLfloat) index[i] + (1.0F-f) * idx);
+   }
+}
+
+
+
+/*
+ * Calculate fog coords from window z values 
+ * Input:  n - number of pixels
+ *         z - array of integer depth values
+ *         red, green, blue, alpha - pixel colors
+ * Output:  red, green, blue, alpha - fogged pixel colors
+ *
+ * Use lookup table & interpolation? 
+ */
+void
+_mesa_win_fog_coords_from_z( const GLcontext *ctx,
+			     GLuint n, 
+			     const GLdepth z[], 
+			     GLfixed fogcoord[] )
 {
    GLfloat c = ctx->ProjectionMatrix.m[10];
    GLfloat d = ctx->ProjectionMatrix.m[14];
@@ -380,24 +333,17 @@ _mesa_fog_ci_pixels( const GLcontext *ctx,
             for (i=0;i<n;i++) {
                GLfloat ndcz = ((GLfloat) z[i] - tz) * szInv;
                GLfloat eyez = -d / (c+ndcz);
-               GLfloat f;
-               if (eyez < 0.0)  eyez = -eyez;
-               f = (fogEnd - eyez) * fogScale;
-               f = CLAMP( f, 0.0F, 1.0F );
-               index[i] = (GLuint) ((GLfloat) index[i] + (1.0F-f) * ctx->Fog.Index);
+	       if (eyez < 0.0)  eyez = -eyez;
+               fogcoord[i] = (fogEnd - eyez) * fogScale;
             }
-	 }
+         }
 	 break;
       case GL_EXP:
-         for (i=0;i<n;i++) {
+	 for (i=0;i<n;i++) {
 	    GLfloat ndcz = ((GLfloat) z[i] - tz) * szInv;
-	    GLfloat eyez = -d / (c+ndcz);
-            GLfloat f;
-	    if (eyez < 0.0)
-               eyez = -eyez;
-	    f = exp( -ctx->Fog.Density * eyez );
-	    f = CLAMP( f, 0.0F, 1.0F );
-	    index[i] = (GLuint) ((GLfloat) index[i] + (1.0F-f) * ctx->Fog.Index);
+	    GLfloat eyez = d / (c+ndcz);
+	    if (eyez < 0.0) eyez = -eyez;
+	    fogcoord[i] = exp( -ctx->Fog.Density * eyez );
 	 }
 	 break;
       case GL_EXP2:
@@ -405,25 +351,53 @@ _mesa_fog_ci_pixels( const GLcontext *ctx,
             GLfloat negDensitySquared = -ctx->Fog.Density * ctx->Fog.Density;
             for (i=0;i<n;i++) {
                GLfloat ndcz = ((GLfloat) z[i] - tz) * szInv;
-               GLfloat eyez = -d / (c+ndcz);
-               GLfloat tmp, f;
-               if (eyez < 0.0)
-                  eyez = -eyez;
-               tmp = negDensitySquared * eyez * eyez;
+               GLfloat eyez = d / (c+ndcz);
+               GLfloat tmp = negDensitySquared * eyez * eyez;
 #ifdef __alpha__
-               /* XXX this underflow check may be needed for other systems */
                if (tmp < FLT_MIN_10_EXP)
-                  f = exp( FLT_MIN_10_EXP );
-               else
+		  tmp = FLT_MIN_10_EXP;
 #endif
-               f = exp( tmp );
-               f = CLAMP( f, 0.0F, 1.0F );
-               index[i] = (GLuint) ((GLfloat) index[i] + (1.0F-f) * ctx->Fog.Index);
+	       fogcoord[i] = exp( tmp );
             }
-	 }
+         }
 	 break;
       default:
-         gl_problem(ctx, "Bad fog mode in _mesa_fog_ci_pixels");
+         gl_problem(ctx, "Bad fog mode in _mesa_win_fog_coords_from_z");
          return;
    }
 }
+
+
+/*
+ * Apply fog to an array of RGBA pixels.
+ * Input:  n - number of pixels
+ *         z - array of integer depth values
+ *         red, green, blue, alpha - pixel colors
+ * Output:  red, green, blue, alpha - fogged pixel colors
+ */
+void
+_mesa_depth_fog_rgba_pixels( const GLcontext *ctx,
+			     GLuint n, const GLdepth z[], GLubyte rgba[][4] )
+{
+   GLfixed fog[MAX_WIDTH];
+   _mesa_win_fog_coords_from_z( ctx, n, z, fog );
+   _mesa_fog_rgba_pixels( ctx, n, fog, rgba );
+}
+
+
+/*
+ * Apply fog to an array of color index pixels.
+ * Input:  n - number of pixels
+ *         z - array of integer depth values
+ *         index - pixel color indexes
+ * Output:  index - fogged pixel color indexes
+ */
+void
+_mesa_depth_fog_ci_pixels( const GLcontext *ctx,
+                     GLuint n, const GLdepth z[], GLuint index[] )
+{
+   GLfixed fog[MAX_WIDTH];
+   _mesa_win_fog_coords_from_z( ctx, n, z, fog );
+   _mesa_fog_ci_pixels( ctx, n, fog, index );
+}
+

openharmony/third_party_mesa3d

commit 025d2e032eb73456e0e337d8756838a39a66c1e9

mesa: Add missing display list support for GL_FOG_COORDINATE_SOURCE
Fixes: fe5d67d95f3 ("Implement EXT_fog_coord and EXT_secondary_color.")
Reviewed-by: Mathias Fröhlich <Mathias.Froehlich@web.de>
Cc: Brian Paul <brianp@vmware.com>

src/mesa/main/dlist.c
@@ -2755,6 +2755,7 @@ save_Fogiv(GLenum pname, const GLint *params)
    case GL_FOG_START:
    case GL_FOG_END:
    case GL_FOG_INDEX:
+   case GL_FOG_COORDINATE_SOURCE:
       p[0] = (GLfloat) *params;
       p[1] = 0.0f;
       p[2] = 0.0f;