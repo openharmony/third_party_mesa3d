openharmony/third_party_mesa3d

commit 367d7e9818422c8f7cac4922f9c8a9bc1e5b65e7

iris: Do not advertise multisampled image load/store.
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

src/gallium/drivers/iris/iris_formats.c
@@ -454,6 +454,11 @@ iris_is_format_supported(struct pipe_screen *pscreen,
    }
 
    if (usage & PIPE_BIND_SHADER_IMAGE) {
+      /* Dataport doesn't support compression, and we can't resolve an MCS
+       * compressed surface.
+       */
+      supported &= sample_count == 1;
+
       // XXX: allow untyped reads
       supported &= isl_format_supports_typed_reads(devinfo, format) &&
                    isl_format_supports_typed_writes(devinfo, format);

commit 1e1214bf55154f584e636ad0f01dfaca4d51f50e

iris: Fix imageBuffer and PBO download.
Recently we added checks to try and deny multisampled shader images.
Unfortunately, this messed up imageBuffers, which have sample_count = 0,
which are also used in PBO download, causing us hit CPU map fallbacks.

Fixes: 367d7e98184 iris: Do not advertise multisampled image load/store.
Reviewed-by: Rafael Antognolli <rafael.antognolli@intel.com>

src/gallium/drivers/iris/iris_formats.c
@@ -455,9 +455,9 @@ iris_is_format_supported(struct pipe_screen *pscreen,
 
    if (usage & PIPE_BIND_SHADER_IMAGE) {
       /* Dataport doesn't support compression, and we can't resolve an MCS
-       * compressed surface.
+       * compressed surface.  (Buffer images may have sample count of 0.)
        */
-      supported &= sample_count == 1;
+      supported &= sample_count <= 1;
 
       // XXX: allow untyped reads
       supported &= isl_format_supports_typed_reads(devinfo, format) &&