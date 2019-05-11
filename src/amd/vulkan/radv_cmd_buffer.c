openharmony/third_party_mesa3d

commit a397c3630bf4443c2709126c1a48620a05da0494

radv: bail out when binding the same vertex buffers
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

src/amd/vulkan/radv_cmd_buffer.c
@@ -2259,14 +2259,28 @@ void radv_CmdBindVertexBuffers(
 {
 	RADV_FROM_HANDLE(radv_cmd_buffer, cmd_buffer, commandBuffer);
 	struct radv_vertex_binding *vb = cmd_buffer->state.vertex_bindings;
+	bool changed = false;
 
 	/* We have to defer setting up vertex buffer since we need the buffer
 	 * stride from the pipeline. */
 
 	assert(firstBinding + bindingCount <= MAX_VBS);
 	for (uint32_t i = 0; i < bindingCount; i++) {
-		vb[firstBinding + i].buffer = radv_buffer_from_handle(pBuffers[i]);
-		vb[firstBinding + i].offset = pOffsets[i];
+		uint32_t idx = firstBinding + i;
+
+		if (!changed &&
+		    (vb[idx].buffer != radv_buffer_from_handle(pBuffers[i]) ||
+		     vb[idx].offset != pOffsets[i])) {
+			changed = true;
+		}
+
+		vb[idx].buffer = radv_buffer_from_handle(pBuffers[i]);
+		vb[idx].offset = pOffsets[i];
+	}
+
+	if (!changed) {
+		/* No state changes. */
+		return;
 	}
 
 	cmd_buffer->state.vb_dirty = true;

commit 46884cd18e24ec8de1ebe7c9610f3a6266565f41

radv: clear vertex bindings while resetting command buffer
Only vertex inputs accessed by vertex shader must have valid buffers
bound.

Signed-off-by: Józef Kucia <joseph.kucia@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Fixes: a397c3630bf "radv: bail out when binding the same vertex buffers"

src/amd/vulkan/radv_cmd_buffer.c
@@ -301,7 +301,6 @@ radv_cmd_buffer_destroy(struct radv_cmd_buffer *cmd_buffer)
 static VkResult
 radv_reset_cmd_buffer(struct radv_cmd_buffer *cmd_buffer)
 {
-
 	cmd_buffer->device->ws->cs_reset(cmd_buffer->cs);
 
 	list_for_each_entry_safe(struct radv_cmd_buffer_upload, up,
@@ -325,6 +324,8 @@ radv_reset_cmd_buffer(struct radv_cmd_buffer *cmd_buffer)
 	cmd_buffer->upload.offset = 0;
 
 	cmd_buffer->record_result = VK_SUCCESS;
+
+	memset(cmd_buffer->vertex_bindings, 0, sizeof(cmd_buffer->vertex_bindings));
 
 	for (unsigned i = 0; i < VK_PIPELINE_BIND_POINT_RANGE_SIZE; i++) {
 		cmd_buffer->descriptors[i].dirty = 0;