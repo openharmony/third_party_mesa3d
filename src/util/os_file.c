openharmony/third_party_mesa3d

commit 7f74ea63578510668b650071a94f9e3c69a864d5

util: add os_read_file() helper
readN() taken from igt.
os_read_file() inspired by igt_sysfs_get()

Signed-off-by: Eric Engestrom <eric.engestrom@intel.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

src/util/os_file.c
@@ -1,0 +1,129 @@
+/*
+ * Copyright 2019 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "os_file.h"
+
+#include <errno.h>
+
+#if defined(__linux__)
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+static ssize_t
+readN(int fd, char *buf, size_t len)
+{
+   int err = -ENODATA;
+   size_t total = 0;
+   do {
+      ssize_t ret = read(fd, buf + total, len - total);
+
+      if (ret < 0)
+         ret = -errno;
+
+      if (ret == -EINTR || ret == -EAGAIN)
+         continue;
+
+      if (ret <= 0)
+         break;
+
+      total += ret;
+   } while (total != len);
+
+   return total ? total : err;
+}
+
+static char *
+read_grow(int fd)
+{
+   size_t len = 64;
+
+   char *buf = malloc(len);
+   if (!buf) {
+      close(fd);
+      errno = -ENOMEM;
+      return NULL;
+   }
+
+   ssize_t read;
+   size_t offset = 0, remaining = len - 1;
+   while ((read = readN(fd, buf + offset, remaining)) == remaining) {
+      char *newbuf = realloc(buf, 2 * len);
+      if (!newbuf) {
+         free(buf);
+         close(fd);
+         errno = -ENOMEM;
+         return NULL;
+      }
+
+      buf = newbuf;
+      len *= 2;
+      offset += read;
+      remaining = len - offset - 1;
+   }
+
+   close(fd);
+
+   if (read > 0)
+      offset += read;
+
+   buf[offset] = '\0';
+
+   return buf;
+}
+
+char *
+os_read_file(const char *filename)
+{
+   size_t len = 0;
+
+   int fd = open(filename, O_RDONLY);
+   if (fd == -1) {
+      /* errno set by open() */
+      return NULL;
+   }
+
+   struct stat stat;
+   if (fstat(fd, &stat) == 0)
+      len = stat.st_size;
+
+   if (!len)
+      return read_grow(fd);
+
+   /* add NULL terminator */
+   len++;
+
+   char *buf = malloc(len);
+   if (!buf) {
+      close(fd);
+      errno = -ENOMEM;
+      return NULL;
+   }
+
+   ssize_t read = readN(fd, buf, len - 1);
+
+   close(fd);
+
+   if (read == -1)
+      return NULL;
+
+   buf[read] = '\0';
+
+   return buf;
+}
+
+#else
+
+char *
+os_read_file(const char *filename)
+{
+   errno = -ENOSYS;
+   return NULL;
+}
+
+#endif

src/util/Makefile.sources
@@ -29,6 +29,8 @@ MESA_UTIL_FILES := \
 	mesa-sha1.h \
 	os_time.c \
 	os_time.h \
+	os_file.c \
+	os_file.h \
 	os_misc.c \
 	os_misc.h \
 	u_process.c \

src/util/meson.build
@@ -52,6 +52,7 @@ files_mesa_util = files(
   'mesa-sha1.h',
   'os_time.c',
   'os_time.h',
+  'os_file.c',
   'os_misc.c',
   'os_misc.h',
   'u_process.c',

src/util/os_file.h
@@ -1,0 +1,26 @@
+/*
+ * Copyright 2019 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ *
+ * File operations helpers
+ */
+
+#ifndef _OS_FILE_H_
+#define _OS_FILE_H_
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Read a file.
+ * Returns a char* that the caller must free(), or NULL and sets errno.
+ */
+char *
+os_read_file(const char *filename);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _OS_FILE_H_ */

commit bd114daf11ccbbfd816f0e0449aebd4105a89375

util/os_file: always use the 'grow' mechanism
Use fstat() only to pre-allocate a big enough buffer.

This fixes a race where if the file grows between fstat() and read()
we would be missing the end of the file, and if the file slims down
read() would just fail.

Fixes: 7f74ea63578510668b65 "util: add os_read_file() helper"
Reported-by: Jason Ekstrand <jason@jlekstrand.net>
Signed-off-by: Eric Engestrom <eric.engestrom@intel.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

src/util/os_file.c
@@ -38,10 +38,28 @@ readN(int fd, char *buf, size_t len)
    return total ? total : err;
 }
 
-static char *
-read_grow(int fd)
+char *
+os_read_file(const char *filename)
 {
+   /* Note that this also serves as a slight margin to avoid a 2x grow when
+    * the file is just a few bytes larger when we read it than when we
+    * fstat'ed it.
+    * The string's NULL terminator is also included in here.
+    */
    size_t len = 64;
+
+   int fd = open(filename, O_RDONLY);
+   if (fd == -1) {
+      /* errno set by open() */
+      return NULL;
+   }
+
+   /* Pre-allocate a buffer at least the size of the file if we can read
+    * that information.
+    */
+   struct stat stat;
+   if (fstat(fd, &stat) == 0)
+      len += stat.st_size;
 
    char *buf = malloc(len);
    if (!buf) {
@@ -73,46 +91,6 @@ read_grow(int fd)
       offset += read;
 
    buf[offset] = '\0';
-
-   return buf;
-}
-
-char *
-os_read_file(const char *filename)
-{
-   size_t len = 0;
-
-   int fd = open(filename, O_RDONLY);
-   if (fd == -1) {
-      /* errno set by open() */
-      return NULL;
-   }
-
-   struct stat stat;
-   if (fstat(fd, &stat) == 0)
-      len = stat.st_size;
-
-   if (!len)
-      return read_grow(fd);
-
-   /* add NULL terminator */
-   len++;
-
-   char *buf = malloc(len);
-   if (!buf) {
-      close(fd);
-      errno = -ENOMEM;
-      return NULL;
-   }
-
-   ssize_t read = readN(fd, buf, len - 1);
-
-   close(fd);
-
-   if (read == -1)
-      return NULL;
-
-   buf[read] = '\0';
 
    return buf;
 }