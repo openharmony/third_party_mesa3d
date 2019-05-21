openharmony/third_party_mesa3d

commit a32a1433ebf348ae41d02482ca213f280f6b2032

iris: Serialize the NIR to a blob we can use for shader cache purposes.
We will use a hash of the serialized NIR together with brw_prog_*_key
(for NOS) as the disk cache key, where the disk cache contains actual
assembly shaders.

Reviewed-by: Dylan Baker <dylan@pnwbakers.com>

src/gallium/drivers/iris/iris_context.h
@@ -260,6 +260,10 @@ struct iris_uncompiled_shader {
 
    struct pipe_stream_output_info stream_output;
 
+   /* The serialized NIR (for the disk cache) and size in bytes. */
+   void *ir_cache_binary;
+   uint32_t ir_cache_binary_size;
+
    unsigned program_id;
 
    /** Bitfield of (1 << IRIS_NOS_*) flags. */

src/gallium/drivers/iris/iris_program.c
@@ -38,6 +38,7 @@
 #include "util/u_atomic.h"
 #include "compiler/nir/nir.h"
 #include "compiler/nir/nir_builder.h"
+#include "compiler/nir/nir_serialize.h"
 #include "intel/compiler/brw_compiler.h"
 #include "intel/compiler/brw_nir.h"
 #include "iris_context.h"
@@ -1456,6 +1457,26 @@ iris_create_uncompiled_shader(struct pipe_context *ctx,
    if (so_info) {
       memcpy(&ish->stream_output, so_info, sizeof(*so_info));
       update_so_info(&ish->stream_output, nir->info.outputs_written);
+   }
+
+   if (screen->disk_cache) {
+      /* Serialize the NIR to a binary blob that we can hash for the disk
+       * cache.  First, drop unnecessary information (like variable names)
+       * so the serialized NIR is smaller, and also to let us detect more
+       * isomorphic shaders when hashing, increasing cache hits.
+       *
+       * We skip this step when not using the disk cache, as variable names
+       * are useful for inspecting and debugging shaders.
+       */
+      nir_strip(nir);
+
+      struct blob blob;
+      blob_init(&blob);
+      nir_serialize(&blob, ish->nir);
+      ish->ir_cache_binary = malloc(blob.size);
+      ish->ir_cache_binary_size = blob.size;
+      memcpy(ish->ir_cache_binary, blob.data, blob.size);
+      blob_finish(&blob);
    }
 
    return ish;

commit a57d9569d37fd52e320e6cc6aae7a501410fc102

iris: Fix ALT mode regressions from shader cache
We were checking this based on nir->info.name, but with the shader
cache enabled, nir_strip throws out the name, causing us to use IEEE
mode for ARB programs.

gl-1.0-spot-light regressed because it wants ALT mode for 0^0 behavior.

Fixes: a32a1433ebf iris: Serialize the NIR to a blob we can use for shader cache purposes.

src/gallium/drivers/iris/iris_context.h
@@ -271,6 +271,9 @@ struct iris_uncompiled_shader {
 
    /** Have any shader variants been compiled yet? */
    bool compiled_once;
+
+   /** Should we use ALT mode for math?  Useful for ARB programs. */
+   bool use_alt_mode;
 };
 
 /**

src/gallium/drivers/iris/iris_program.c
@@ -543,8 +543,7 @@ iris_compile_vs(struct iris_context *ice,
       nir_shader_gather_info(nir, impl);
    }
 
-   if (nir->info.name && strncmp(nir->info.name, "ARB", 3) == 0)
-      prog_data->use_alt_mode = true;
+   prog_data->use_alt_mode = ish->use_alt_mode;
 
    iris_setup_uniforms(compiler, mem_ctx, nir, prog_data, &system_values,
                        &num_system_values, &num_cbufs);
@@ -1061,8 +1060,7 @@ iris_compile_fs(struct iris_context *ice,
 
    nir_shader *nir = nir_shader_clone(mem_ctx, ish->nir);
 
-   if (nir->info.name && strncmp(nir->info.name, "ARB", 3) == 0)
-      prog_data->use_alt_mode = true;
+   prog_data->use_alt_mode = ish->use_alt_mode;
 
    iris_setup_uniforms(compiler, mem_ctx, nir, prog_data, &system_values,
                        &num_system_values, &num_cbufs);
@@ -1489,6 +1487,10 @@ iris_create_uncompiled_shader(struct pipe_context *ctx,
       memcpy(&ish->stream_output, so_info, sizeof(*so_info));
       update_so_info(&ish->stream_output, nir->info.outputs_written);
    }
+
+   /* Save this now before potentially dropping nir->info.name */
+   if (nir->info.name && strncmp(nir->info.name, "ARB", 3) == 0)
+      ish->use_alt_mode = true;
 
    if (screen->disk_cache) {
       /* Serialize the NIR to a binary blob that we can hash for the disk