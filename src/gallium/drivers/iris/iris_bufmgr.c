openharmony/third_party_mesa3d

commit 0f80df38b49bff103f8bdea1b401832c9fa475d8

iris: Dig through the cache to find a BO in the right memzone
This saves some util_vma thrash when the first entry in the cache
happens to be in a different memory zone, but one just a tiny bit
ahead is already there and instantly reusable.  Hopefully the cost
of a little extra searching won't break the bank - if it does, we
can consider having separate list heads or keeping a separate VMA
cache.

Improves OglDrvRes performance by 22%, restoring a regression from
deleting the bucket allocators in a03b40ed0886722d02ded1c4c6f29fe0.

Thanks to Clayton Craft for alerting me to the regression.

Reviewed-by: Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>

src/gallium/drivers/iris/iris_bufmgr.c
@@ -364,18 +364,22 @@ static struct iris_bo *
 alloc_bo_from_cache(struct iris_bufmgr *bufmgr,
                     struct bo_cache_bucket *bucket,
                     enum iris_memory_zone memzone,
-                    unsigned flags)
+                    unsigned flags,
+                    bool match_zone)
 {
    if (!bucket)
       return NULL;
 
    struct iris_bo *bo = NULL;
 
-   while (!list_empty(&bucket->head)) {
-      struct iris_bo *cur = LIST_ENTRY(struct iris_bo, bucket->head.next, head);
+   list_for_each_entry_safe(struct iris_bo, cur, &bucket->head, head) {
+      /* Try a little harder to find one that's already in the right memzone */
+      if (match_zone && memzone != iris_memzone_for_address(cur->gtt_offset))
+         continue;
 
-      /* If the last BO in the cache is busy, there are no idle BOs.
-       * Fall back to allocating a fresh buffer.
+      /* If the last BO in the cache is busy, there are no idle BOs.  Bail,
+       * either falling back to a non-matching memzone, or if that fails,
+       * allocating a fresh buffer.
        */
       if (iris_bo_busy(cur))
          return NULL;
@@ -471,8 +475,14 @@ bo_alloc_internal(struct iris_bufmgr *bufmgr,
 
    mtx_lock(&bufmgr->lock);
 
-   /* Get a buffer out of the cache if available */
-   bo = alloc_bo_from_cache(bufmgr, bucket, memzone, flags);
+   /* Get a buffer out of the cache if available.  First, we try to find
+    * one with a matching memory zone so we can avoid reallocating VMA.
+    */
+   bo = alloc_bo_from_cache(bufmgr, bucket, memzone, flags, true);
+
+   /* If that fails, we try for any cached BO, without matching memzone. */
+   if (!bo)
+      bo = alloc_bo_from_cache(bufmgr, bucket, memzone, flags, false);
 
    if (!bo) {
       alloc_pages = true;

commit 49a3b81bf4795c6ce6b9ca8ec64c25ea52b4a70c

iris: Be lazy about cleaning up purged BOs in the cache.
Mathias Fröhlich reported that commit 0f80df38b49bff103f8bde crashes.
list_for_each_entry_safe is safe against removing the current entry,
but iris_bo_cache_purge_bucket was potentially removing next entries
too, which broke our saved next pointer.

To fix this, don't bother with the iris_bo_cache_purge_bucket step.
We just detected a single entry where the kernel has purged the BO's
memory, and so it isn't a usable entry for our cache.  We're about to
continue the search with the next BO.  If that one's purged, we'll
clean it up too.  And so on.

We may miss cleaning up purged BOs that are further down the list
after non-purged BOs...but that's probably fine.  We still have the
time-based cleaner (cleanup_bo_cache) which will take care of them
eventually, and the kernel's already freed their memory, so it's not
that harmful to have a few kicking around a little longer.

Fixes: 0f80df38b49 iris: Dig through the cache to find a BO in the right memzone
Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>

src/gallium/drivers/iris/iris_bufmgr.c
@@ -336,20 +336,6 @@ iris_bo_madvise(struct iris_bo *bo, int state)
    return madv.retained;
 }
 
-/* drop the oldest entries that have been purged by the kernel */
-static void
-iris_bo_cache_purge_bucket(struct iris_bufmgr *bufmgr,
-                          struct bo_cache_bucket *bucket)
-{
-   list_for_each_entry_safe(struct iris_bo, bo, &bucket->head, head) {
-      if (iris_bo_madvise(bo, I915_MADV_DONTNEED))
-         break;
-
-      list_del(&bo->head);
-      bo_free(bo);
-   }
-}
-
 static struct iris_bo *
 bo_calloc(void)
 {
@@ -392,10 +378,8 @@ alloc_bo_from_cache(struct iris_bufmgr *bufmgr,
          break;
       }
 
-      /* This BO was purged, clean up any others and retry */
+      /* This BO was purged, throw it out and keep looking. */
       bo_free(cur);
-
-      iris_bo_cache_purge_bucket(bufmgr, bucket);
    }
 
    if (!bo)