openharmony/third_party_mesa3d

commit 61c535692252d4336dfb49a70a93692e12f372f5

amd/common: use generated register header

src/amd/Android.common.mk
@@ -47,6 +47,9 @@ $(LOCAL_GENERATED_SOURCES): PRIVATE_CUSTOM_TOOL = $(PRIVATE_PYTHON) $^ > $@
 $(intermediates)/common/sid_tables.h: $(LOCAL_PATH)/common/sid_tables.py $(LOCAL_PATH)/common/sid.h $(LOCAL_PATH)/registers/amdgfxregs.json $(LOCAL_PATH)/registers/pkt3.json
 	$(transform-generated-source)
 
+$(intermediates)/common/amdgfxregs.h: $(LOCAL_PATH)/registers/makeregheader.py $(LOCAL_PATH)/registers/amdgfxregs.json $(LOCAL_PATH)/registers/pkt3.json
+	$(transform-generated-source) --sort address --guard AMDGFXREGS_H
+
 LOCAL_C_INCLUDES := \
 	$(MESA_TOP)/include \
 	$(MESA_TOP)/src \

src/amd/Makefile.sources
@@ -1,5 +1,4 @@
 COMMON_HEADER_FILES = \
-	common/gfx9d.h \
 	common/sid.h \
 	common/amd_family.h \
 	common/amd_kernel_code_t.h
@@ -64,4 +63,5 @@ AMD_DEBUG_FILES = \
 	common/ac_debug.h
 
 AMD_GENERATED_FILES = \
+	common/amdgfxregs.h \
 	common/sid_tables.h

src/amd/common/ac_debug.c
@@ -34,7 +34,6 @@
 #include <inttypes.h>
 
 #include "sid.h"
-#include "gfx9d.h"
 #include "sid_tables.h"
 #include "util/u_math.h"
 #include "util/u_memory.h"

src/amd/common/ac_gpu_info.c
@@ -25,7 +25,6 @@
 
 #include "ac_gpu_info.h"
 #include "sid.h"
-#include "gfx9d.h"
 
 #include "util/u_math.h"
 

src/amd/common/meson.build
@@ -26,6 +26,14 @@ sid_tables_h = custom_target(
   capture : true,
 )
 
+amdgfxregs_h = custom_target(
+  'amdgfxregs_h',
+  input : ['../registers/makeregheader.py', '../registers/amdgfxregs.json', '../registers/pkt3.json'],
+  output : 'amdgfxregs.h',
+  command : [prog_python, '@INPUT@', '--sort', 'address', '--guard', 'AMDGFXREGS_H'],
+  capture : true,
+)
+
 amd_common_files = files(
   'ac_binary.c',
   'ac_binary.h',
@@ -52,7 +60,7 @@ amd_common_files = files(
 
 libamd_common = static_library(
   'amd_common',
-  [amd_common_files, sid_tables_h],
+  [amd_common_files, sid_tables_h, amdgfxregs_h],
   include_directories : [
     inc_common, inc_compiler, inc_mesa, inc_mapi, inc_amd,
   ],
@@ -63,3 +71,5 @@ libamd_common = static_library(
   c_args : [c_vis_args],
   cpp_args : [cpp_vis_args],
 )
+
+idep_amdgfxregs_h = declare_dependency(sources : [amdgfxregs_h])

src/amd/vulkan/meson.build
@@ -142,7 +142,7 @@ libvulkan_radeon = shared_library(
   dependencies : [
     dep_llvm, dep_libdrm_amdgpu, dep_thread, dep_elf, dep_dl, dep_m,
     dep_valgrind, radv_deps,
-    idep_nir, idep_vulkan_util,
+    idep_nir, idep_vulkan_util, idep_amdgfxregs_h,
   ],
   c_args : [c_vis_args, no_override_init_args, radv_flags],
   cpp_args : [cpp_vis_args, radv_flags],

commit b12e392c9127dca9a19144447af380e5dd811f6f

android: winsys/amdgpu,radv: fix generated amdgfxregs.h header dependecies
Fix android building errors in winsys/amdgpu and radv
due to 'amdgfxregs.h' not found.

Changelog:
amd/common - generated $(intermediated)/common path is added to exports
winsys/amdgpu - libmesa_amd_common static dependency is added
radv - correct generated $(intermediated)/common path is added to includes

Fixes: 61c5356 ("amd/common: use generated register header")
Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>
Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>

src/amd/Android.common.mk
@@ -62,7 +62,8 @@ LOCAL_C_INCLUDES := \
 	$(intermediates)/common
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := \
-	$(LOCAL_PATH)/common
+	$(LOCAL_PATH)/common \
+	$(intermediates)/common
 
 LOCAL_SHARED_LIBRARIES := \
 	libdrm_amdgpu

src/amd/vulkan/Android.mk
@@ -68,7 +68,7 @@ $(call mesa-build-with-llvm)
 
 LOCAL_C_INCLUDES := \
 	$(RADV_COMMON_INCLUDES) \
-	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_amd_common,,) \
+	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_amd_common,,)/common \
 	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_nir,,)/nir \
 	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_radv_common,,) \
 	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_vulkan_util,,)/util \

src/gallium/winsys/amdgpu/drm/Android.mk
@@ -32,7 +32,7 @@ LOCAL_SRC_FILES := $(C_SOURCES)
 
 LOCAL_CFLAGS := $(AMDGPU_CFLAGS)
 
-LOCAL_STATIC_LIBRARIES := libmesa_amdgpu_addrlib
+LOCAL_STATIC_LIBRARIES := libmesa_amdgpu_addrlib libmesa_amd_common
 
 LOCAL_SHARED_LIBRARIES := libdrm_amdgpu
 LOCAL_MODULE := libmesa_winsys_amdgpu