openharmony/third_party_mesa3d

commit 9591511bceedae2a34fc423856ac4ccce12aeea9

virgl: Add driinfo file and tie it into the build
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>

src/gallium/drivers/virgl/Android.mk
@@ -33,6 +33,17 @@ LOCAL_MODULE := libmesa_pipe_virgl
 include $(GALLIUM_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
 
+GEN_DRIINFO_INPUTS := \
+	$(MESA_TOP)/src/gallium/auxiliary/pipe-loader/driinfo_gallium.h \
+	$(LOCAL_PATH)/virgl_driinfo.h.in
+
+MERGE_DRIINFO := $(MESA_TOP)/src/util/merge_driinfo.py
+
+$(intermediates)/virgl/virgl_driinfo.h: $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS)
+	@mkdir -p $(dir $@)
+	@echo "Gen Header: $(PRIVATE_MODULE) <= $(notdir $(@))"
+	$(hide) $(MESA_PYTHON2) $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS) > $@ || ($(RM) $@; false)
+
 ifneq ($(HAVE_GALLIUM_VIRGL),)
 GALLIUM_TARGET_DRIVERS += virtio_gpu
 $(eval GALLIUM_LIBS += $(LOCAL_MODULE) libmesa_winsys_virgl libmesa_winsys_virgl_vtest)

src/gallium/drivers/virgl/meson.build
@@ -31,15 +31,27 @@ files_libvirgl = files(
   'virgl_tgsi.c',
 )
 
+virgl_driinfo_h = custom_target(
+  'virgl_driinfo.h',
+  input : files(
+    '../../../util/merge_driinfo.py',
+    '../../auxiliary/pipe-loader/driinfo_gallium.h', 'virgl_driinfo.h.in'
+  ),
+  output : 'virgl_driinfo.h',
+  command : [prog_python, '@INPUT@'],
+  capture : true,
+)
+
 libvirgl = static_library(
   'virgl',
-  files_libvirgl,
+  [ files_libvirgl, virgl_driinfo_h ],
   c_args : c_vis_args,
   include_directories : inc_common,
-  dependencies : dep_libdrm,
+  dependencies : dep_libdrm
 )
 
 driver_virgl = declare_dependency(
+  sources : virgl_driinfo_h,
   compile_args : '-DGALLIUM_VIRGL',
   link_with : [libvirgl, libvirgldrm, libvirglvtest],
 )

src/gallium/drivers/virgl/virgl_driinfo.h.in
@@ -1,0 +1,11 @@
+// DriConf options specific to virgl
+// To add a new tweak:
+//   1. Define the tweak in util/xmlpool/t_options.h and also add
+//      the tweak name as define to virgl_protocol.h
+//   2. Add the tweak here with the appropriated default value
+//   3. Add the code to query the tweak in virgl_create_screen
+//   4. Add the code to send the tweek to the host in virgl_send_tweaks
+//   5. Implement the tweak in virglrenderer
+DRI_CONF_SECTION_MISCELLANEOUS
+DRI_CONF_SECTION_END
+

commit c5bb2de0b50828e77231970c6e2f970c9504c228

android: virgl: fix generated virgl_driinfo.h building rules
Changelog in Android makefile:
- Add LOCAL_MODULE_CLASS, intermediates and LOCAL_GENERATED_SOURCES
- Use LOCAL_EXPORT_C_INCLUDE_DIRS to export $(intermediates) path
- Move generated header rules before 'include $(BUILD_STATIC_LIBRARY)'

Fixes the following building error:

In file included from external/mesa/src/gallium/targets/dri/target.c:1:
external/mesa/src/gallium/auxiliary/target-helpers/drm_helper.h:257:16:
fatal error: 'virgl/virgl_driinfo.h' file not found
      #include "virgl/virgl_driinfo.h"
               ^~~~~~~~~~~~~~~~~~~~~~~
1 error generated.

Fixes: 9591511bc ("virgl: Add driinfo file and tie it into the build")
Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>
Review-by: Chih-Wei Huang <cwhuang@linux.org.tw>

src/gallium/drivers/virgl/Android.mk
@@ -30,8 +30,9 @@ LOCAL_SRC_FILES := \
 
 LOCAL_MODULE := libmesa_pipe_virgl
 
-include $(GALLIUM_COMMON_MK)
-include $(BUILD_STATIC_LIBRARY)
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+intermediates := $(call local-generated-sources-dir)
+LOCAL_GENERATED_SOURCES := $(intermediates)/virgl/virgl_driinfo.h
 
 GEN_DRIINFO_INPUTS := \
 	$(MESA_TOP)/src/gallium/auxiliary/pipe-loader/driinfo_gallium.h \
@@ -43,6 +44,11 @@ $(intermediates)/virgl/virgl_driinfo.h: $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS)
 	@mkdir -p $(dir $@)
 	@echo "Gen Header: $(PRIVATE_MODULE) <= $(notdir $(@))"
 	$(hide) $(MESA_PYTHON2) $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS) > $@ || ($(RM) $@; false)
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(intermediates)
+
+include $(GALLIUM_COMMON_MK)
+include $(BUILD_STATIC_LIBRARY)
 
 ifneq ($(HAVE_GALLIUM_VIRGL),)
 GALLIUM_TARGET_DRIVERS += virtio_gpu