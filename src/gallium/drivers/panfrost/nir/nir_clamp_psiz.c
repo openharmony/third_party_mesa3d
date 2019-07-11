openharmony/third_party_mesa3d

commit 16d7e861bbb5015046616ec1f31a1aa0c6ee3b0c

panfrost: Clamp point size
It's not clear the hardware really has a maximum which confuses dEQP;
clamp to whatever we report as our maximum.

Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>

src/gallium/drivers/panfrost/nir/nir_clamp_psiz.c
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2019 Collabora, Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/**
+ * @file
+ *
+ * Clamps writes to VARYING_SLOT_PSIZ to a given limit.
+ */
+
+#include "compiler/nir/nir.h"
+#include "compiler/nir/nir_builder.h"
+
+void
+nir_clamp_psiz(nir_shader *shader, float min_size, float max_size);
+
+void
+nir_clamp_psiz(nir_shader *shader, float min_size, float max_size)
+{
+   nir_foreach_function(func, shader) {
+      nir_foreach_block(block, func->impl) {
+         nir_foreach_instr_safe(instr, block) {
+            if (instr->type != nir_instr_type_intrinsic)
+               continue;
+
+            nir_intrinsic_instr *intr = nir_instr_as_intrinsic(instr);
+            if (intr->intrinsic != nir_intrinsic_store_deref)
+               continue;
+
+            nir_variable *var = nir_intrinsic_get_var(intr, 0);
+            if (var->data.location != VARYING_SLOT_PSIZ)
+               continue;
+
+            nir_builder b;
+            nir_builder_init(&b, func->impl);
+            b.cursor = nir_before_instr(instr);
+
+            nir_ssa_def *in_size = nir_ssa_for_src(&b, intr->src[1], 1);
+
+            nir_ssa_def *clamped =
+               nir_fmin(&b,
+                        nir_fmax(&b, in_size, nir_imm_float(&b, min_size)),
+                        nir_imm_float(&b, max_size));
+
+            nir_instr_rewrite_src(instr, &intr->src[1],
+                                  nir_src_for_ssa(clamped));
+
+         }
+      }
+
+      nir_metadata_preserve(func->impl, nir_metadata_block_index |
+                            nir_metadata_dominance);
+   }
+}
+

src/gallium/drivers/panfrost/meson.build
@@ -29,6 +29,7 @@ files_panfrost = files(
   'nir/nir_undef_to_zero.c',
   'nir/nir_lower_blend.c',
   'nir/nir_lower_framebuffer.c',
+  'nir/nir_clamp_psiz.c',
 
   'pan_context.c',
   'pan_afbc.c',

src/gallium/drivers/panfrost/pan_screen.c
@@ -335,7 +335,7 @@ panfrost_get_paramf(struct pipe_screen *screen, enum pipe_capf param)
 
         /* fall-through */
         case PIPE_CAPF_MAX_POINT_WIDTH_AA:
-                return 255.0; /* arbitrary */
+                return 1024.0;
 
         case PIPE_CAPF_MAX_TEXTURE_ANISOTROPY:
                 return 16.0;

src/panfrost/midgard/compiler.h
@@ -448,9 +448,12 @@ void emit_binary_bundle(
         struct util_dynarray *emission,
         int next_tag);
 
-/* NIR stuff */
+/* NIR stuff. TODO: Move? Share? Something? */
 
 bool
 nir_undef_to_zero(nir_shader *shader);
+
+void
+nir_clamp_psiz(nir_shader *shader, float min_size, float max_size);
 
 #endif

src/panfrost/midgard/midgard_compile.c
@@ -2596,8 +2596,10 @@ midgard_compile_shader_nir(nir_shader *nir, midgard_program *program, bool is_bl
 
         NIR_PASS_V(nir, nir_lower_vars_to_ssa);
 
-        if (ctx->stage == MESA_SHADER_VERTEX)
+        if (ctx->stage == MESA_SHADER_VERTEX) {
                 NIR_PASS_V(nir, nir_lower_viewport_transform);
+                NIR_PASS_V(nir, nir_clamp_psiz, 1.0, 1024.0);
+        }
 
         NIR_PASS_V(nir, nir_lower_var_copies);
         NIR_PASS_V(nir, nir_lower_vars_to_ssa);

commit 80d75e9e8da5904d46a763f5ac7efb444535cfa2

pan/midgard: Correct component count clamping PSIZ
Kind of a funky corner case that does not (as far as I know) apply to
organic shaders from GLES but does pop up in generated shaders from the
fixed-function desktop pipeline.

Fixes: 16d7e861bbb5 ("panfrost: Clamp point size")

Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>

src/gallium/drivers/panfrost/nir/nir_clamp_psiz.c
@@ -54,7 +54,8 @@ nir_clamp_psiz(nir_shader *shader, float min_size, float max_size)
             nir_builder_init(&b, func->impl);
             b.cursor = nir_before_instr(instr);
 
-            nir_ssa_def *in_size = nir_ssa_for_src(&b, intr->src[1], 1);
+            nir_ssa_def *in_size = nir_ssa_for_src(&b, intr->src[1],
+                  intr->num_components);
 
             nir_ssa_def *clamped =
                nir_fmin(&b,