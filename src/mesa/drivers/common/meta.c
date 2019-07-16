openharmony/third_party_mesa3d

commit 64aaa5de21946f48c5ae130b59fe584da456b930

meta: Don't pollute the buffer object namespace in _mesa_meta_setup_vertex_objects
tl;dr: For many types of GL object, we can *NEVER* use the Gen function.

In OpenGL ES (all versions!) and OpenGL compatibility profile,
applications don't have to call Gen functions.  The GL spec is very
clear about how you can mix-and-match generated names and non-generated
names: you can use any name you want for a particular object type until
you call the Gen function for that object type.

Here's the problem scenario:

 - Application calls a meta function that generates a name.  The first
   Gen will probably return 1.

 - Application decides to use the same name for an object of the same
   type without calling Gen.  Many demo programs use names 1, 2, 3,
   etc. without calling Gen.

 - Application calls the meta function again, and the meta function
   replaces the data.  The application's data is lost, and the app
   fails.  Have fun debugging that.

Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=92363
Reviewed-by: Anuj Phogat <anuj.phogat@gmail.com>

src/mesa/drivers/common/meta.c
@@ -339,8 +339,6 @@ _mesa_meta_setup_vertex_objects(struct gl_context *ctx,
                                 unsigned vertex_size, unsigned texcoord_size,
                                 unsigned color_size)
 {
-   GLuint VBO;
-
    if (*VAO == 0) {
       struct gl_vertex_array_object *array_obj;
       assert(*buf_obj == NULL);
@@ -353,21 +351,12 @@ _mesa_meta_setup_vertex_objects(struct gl_context *ctx,
       assert(array_obj != NULL);
 
       /* create vertex array buffer */
-      _mesa_CreateBuffers(1, &VBO);
-      *buf_obj = _mesa_lookup_bufferobj(ctx, VBO);
-
-      /* _mesa_lookup_bufferobj only returns NULL if name is 0.  If the object
-       * does not yet exist (i.e., hasn't been bound) it will return a dummy
-       * object that you can't do anything with.  _mesa_CreateBuffers also
-       * makes the object exist.
-       */
-      assert(*buf_obj != NULL && (*buf_obj)->Name == VBO);
-      assert(*buf_obj != ctx->Array.ArrayBufferObj);
+      *buf_obj = ctx->Driver.NewBufferObject(ctx, 0xDEADBEEF);
+      if (*buf_obj == NULL)
+         return;
 
       _mesa_buffer_data(ctx, *buf_obj, GL_NONE, 4 * sizeof(struct vertex), NULL,
                         GL_DYNAMIC_DRAW, __func__);
-
-      assert((*buf_obj)->Size == 4 * sizeof(struct vertex));
 
       /* setup vertex arrays */
       if (use_generic_attributes) {

commit 1d826ab6151dfae3c3c8e1b3feed9deb14af5d9a

meta: memory leak of CopyPixels usage
Meta of CopyPixel generates a buffer object
but does not free it on cleanup.

Fixes: 64aaa5de2194 (meta: Don't pollute the buffer object namespace in _mesa_meta_setup_vertex_objects)
Signed-off-by: Sergii Romantsov <sergii.romantsov@globallogic.com>
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

src/mesa/drivers/common/meta.c
@@ -104,6 +104,8 @@ static void cleanup_temp_texture(struct gl_context *ctx,
                                  struct temp_texture *tex);
 static void meta_glsl_clear_cleanup(struct gl_context *ctx,
                                     struct clear_state *clear);
+static void meta_copypix_cleanup(struct gl_context *ctx,
+                                    struct copypix_state *copypix);
 static void meta_decompress_cleanup(struct gl_context *ctx,
                                     struct decompress_state *decompress);
 static void meta_drawpix_cleanup(struct gl_context *ctx,
@@ -422,6 +424,7 @@ _mesa_meta_free(struct gl_context *ctx)
    _mesa_make_current(ctx, NULL, NULL);
    _mesa_meta_glsl_blit_cleanup(ctx, &ctx->Meta->Blit);
    meta_glsl_clear_cleanup(ctx, &ctx->Meta->Clear);
+   meta_copypix_cleanup(ctx, &ctx->Meta->CopyPix);
    _mesa_meta_glsl_generate_mipmap_cleanup(ctx, &ctx->Meta->Mipmap);
    cleanup_temp_texture(ctx, &ctx->Meta->TempTex);
    meta_decompress_cleanup(ctx, &ctx->Meta->Decompress);
@@ -1594,6 +1597,17 @@ meta_glsl_clear_cleanup(struct gl_context *ctx, struct clear_state *clear)
       _mesa_reference_shader_program(ctx, &clear->IntegerShaderProg, NULL);
    }
 }
+
+static void
+meta_copypix_cleanup(struct gl_context *ctx, struct copypix_state *copypix)
+{
+   if (copypix->VAO == 0)
+      return;
+   _mesa_DeleteVertexArrays(1, &copypix->VAO);
+   copypix->VAO = 0;
+   _mesa_reference_buffer_object(ctx, &copypix->buf_obj, NULL);
+}
+
 
 /**
  * Given a bitfield of BUFFER_BIT_x draw buffers, call glDrawBuffers to