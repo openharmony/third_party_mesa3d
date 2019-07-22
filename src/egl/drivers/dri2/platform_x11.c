openharmony/third_party_mesa3d

commit 95d16aa4c0f82527455bc361af0ac649c0b157dc

egl: Add a 565 pbuffer-only EGL config under X11.
The CTS requires a 565-no-depth-no-stencil (meaning d/s not-required, not
not-present) config for ES 3.0, but at depth 24 of X11 we wouldn't do so.
We can satisfy that bad requirement using a pbuffer-only visual with
whatever other buffers the driver happens to have given us.

I've tried to raise this as an absurd requirement with Khronos and made no
progress.

v2: Make sure it's single sample, no depth, no stencil.  Comment typo fix

Reviewed-by: Adam Jackson <ajax@redhat.com>

src/egl/drivers/dri2/platform_x11.c
@@ -766,6 +766,18 @@ dri2_x11_authenticate(_EGLDisplay *disp, uint32_t id)
    return dri2_x11_do_authenticate(dri2_dpy, id);
 }
 
+static bool
+dri2_x11_config_match_attrib(struct dri2_egl_display *dri2_dpy,
+                             const __DRIconfig *config,
+                             unsigned int attrib,
+                             unsigned int value)
+{
+   uint32_t config_val;
+   if (!dri2_dpy->core->getConfigAttrib(config, attrib, &config_val))
+      return false;
+   return config_val == value;
+}
+
 static EGLBoolean
 dri2_x11_add_configs_for_visuals(struct dri2_egl_display *dri2_dpy,
                                  _EGLDisplay *disp, bool supports_preserved)
@@ -842,6 +854,41 @@ dri2_x11_add_configs_for_visuals(struct dri2_egl_display *dri2_dpy,
       }
 
       xcb_depth_next(&d);
+   }
+
+   /* Add a 565-no-depth-no-stencil pbuffer-only config.  If X11 is depth 24,
+    * we wouldn't have 565 available, which the CTS demands.
+    */
+   for (int j = 0; dri2_dpy->driver_configs[j]; j++) {
+      const __DRIconfig *config = dri2_dpy->driver_configs[j];
+      const EGLint config_attrs[] = {
+         EGL_NATIVE_VISUAL_ID,    0,
+         EGL_NATIVE_VISUAL_TYPE,  EGL_NONE,
+         EGL_NONE
+      };
+      EGLint surface_type = EGL_PBUFFER_BIT;
+      unsigned int rgba_masks[4] = {
+         0x1f << 11,
+         0x3f << 5,
+         0x1f << 0,
+         0,
+      };
+
+      /* Check that we've found single-sample, no depth, no stencil. */
+      if (!dri2_x11_config_match_attrib(dri2_dpy, config,
+                                        __DRI_ATTRIB_DEPTH_SIZE, 0) ||
+          !dri2_x11_config_match_attrib(dri2_dpy, config,
+                                        __DRI_ATTRIB_STENCIL_SIZE, 0) ||
+          !dri2_x11_config_match_attrib(dri2_dpy, config,
+                                        __DRI_ATTRIB_SAMPLES, 0)) {
+         continue;
+      }
+
+      if (dri2_add_config(disp, config, config_count + 1, surface_type,
+                          config_attrs, rgba_masks)) {
+         config_count++;
+         break;
+      }
    }
 
    if (!config_count) {

commit 366558b2ce531c2f7c1abf1d6ebb9a0db7da9b30

egl: Make the 565 pbuffer-only config single buffered.
In commit 95d16aa4c0f82527455bc361af0ac649c0b157dc, Eric found the first
matching 565 pbuffer config, and stopped.  Our double-buffered configs
come first in the list, so we added that, making a pbuffer-only config
that claimed to be double buffered.  This doesn't make sense, since
pixmaps/pbuffers are fundamentally not double buffered.

When using that config, every call to eglCreatePbufferSurface would fail
with EGL_BAD_MATCH.  The call chain looks like this:

   - eglCreatePbufferSurface
   - dri3_create_pbuffer_surface
   - dri3_create_surface
   - dri2_get_dri_config

which eventually does:

   const bool double_buffer = surface_type == EGL_WINDOW_BIT;

and then fails to find a matching config, because it ends up looking
for a single-buffered config - and there aren't any.

To fix this, make the 565 pbuffer config single-buffered.  This fixes
at least 51 dEQP-EGL.* tests.

Fixes: 95d16aa4c0f ("egl: Add a 565 pbuffer-only EGL config under X11.")
Reviewed-by: Eric Engestrom <eric.engestrom@intel.com>
Reviewed-by: Eric Anholt <eric@anholt.net>

src/egl/drivers/dri2/platform_x11.c
@@ -862,13 +862,17 @@ dri2_x11_add_configs_for_visuals(struct dri2_egl_display *dri2_dpy,
          0,
       };
 
-      /* Check that we've found single-sample, no depth, no stencil. */
+      /* Check that we've found single-sample, no depth, no stencil,
+       * and single-buffered.
+       */
       if (!dri2_x11_config_match_attrib(dri2_dpy, config,
                                         __DRI_ATTRIB_DEPTH_SIZE, 0) ||
           !dri2_x11_config_match_attrib(dri2_dpy, config,
                                         __DRI_ATTRIB_STENCIL_SIZE, 0) ||
           !dri2_x11_config_match_attrib(dri2_dpy, config,
-                                        __DRI_ATTRIB_SAMPLES, 0)) {
+                                        __DRI_ATTRIB_SAMPLES, 0) ||
+          !dri2_x11_config_match_attrib(dri2_dpy, config,
+                                        __DRI_ATTRIB_DOUBLE_BUFFER, 0)) {
          continue;
       }
 