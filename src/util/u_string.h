openharmony/third_party_mesa3d

commit 3163976b4a15f0ed037eb670f0694ce607235a0c

util: add asprintf() wrapper for MSVC
Fixes: 5d07abdfb9b8013fe409 ("mesa/st: add sampler uniforms")
Signed-off-by: Eric Engestrom <eric.engestrom@intel.com>
Reviewed-by: Eric Anholt <eric@anholt.net>

src/util/u_string.h
@@ -98,6 +98,18 @@ util_vasprintf(char **ret, const char *format, va_list ap)
    return vsnprintf(*ret, r + 1, format, ap);
 }
 
+#define asprintf util_asprintf
+static inline char *
+util_asprintf(const char *fmt, ...)
+{
+   char *ptr;
+   va_list args;
+   va_start(args, fmt);
+   if (vasprintf(&ptr, fmt, args) == -1)
+      ptr = NULL;
+   va_end(args);
+   return ptr;
+}
 
 #define strcasecmp stricmp
 #define strdup _strdup

commit 2991fe782224cc79948a45eb77e4e5f72f28b947

util: fix asprintf() fallback
Fixes: 3163976b4a15f0ed037e ("util: add asprintf() wrapper for MSVC")
Signed-off-by: Eric Engestrom <eric.engestrom@intel.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>

src/util/u_string.h
@@ -99,16 +99,15 @@ util_vasprintf(char **ret, const char *format, va_list ap)
 }
 
 #define asprintf util_asprintf
-static inline char *
-util_asprintf(const char *fmt, ...)
+static inline int
+util_asprintf(char **str, const char *fmt, ...)
 {
-   char *ptr;
+   int ret;
    va_list args;
    va_start(args, fmt);
-   if (vasprintf(&ptr, fmt, args) == -1)
-      ptr = NULL;
+   ret = vasprintf(str, fmt, args);
    va_end(args);
-   return ptr;
+   return ret;
 }
 
 #define strcasecmp stricmp