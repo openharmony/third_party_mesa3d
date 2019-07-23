openharmony/third_party_mesa3d

commit 3d3842bfa0f7ad9f9803eed0a0ce97b363d02908

radv: dont store disasm string unless keep_shader_info flag set
This fixes the memory use regression from bug 111107.

Fixes: 591883ccc4e ("radv: Add the concept of radv shader binaries.")

Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=111107

src/amd/vulkan/radv_shader.c
@@ -1011,18 +1011,20 @@ radv_shader_variant_create(struct radv_device *device,
 			return NULL;
 		}
 
-		const char *disasm_data;
-		size_t disasm_size;
-		if (!ac_rtld_get_section_by_name(&rtld_binary, ".AMDGPU.disasm", &disasm_data, &disasm_size)) {
-			radv_shader_variant_destroy(device, variant);
-			ac_rtld_close(&rtld_binary);
-			return NULL;
-		}
+		if (device->keep_shader_info) {
+			const char *disasm_data;
+			size_t disasm_size;
+			if (!ac_rtld_get_section_by_name(&rtld_binary, ".AMDGPU.disasm", &disasm_data, &disasm_size)) {
+				radv_shader_variant_destroy(device, variant);
+				ac_rtld_close(&rtld_binary);
+				return NULL;
+			}
 
-		variant->llvm_ir_string = bin->llvm_ir_size ? strdup((const char*)(bin->data + bin->elf_size)) : NULL;
-		variant->disasm_string = malloc(disasm_size + 1);
-		memcpy(variant->disasm_string, disasm_data, disasm_size);
-		variant->disasm_string[disasm_size] = 0;
+			variant->llvm_ir_string = bin->llvm_ir_size ? strdup((const char*)(bin->data + bin->elf_size)) : NULL;
+			variant->disasm_string = malloc(disasm_size + 1);
+			memcpy(variant->disasm_string, disasm_data, disasm_size);
+			variant->disasm_string[disasm_size] = 0;
+		}
 
 		ac_rtld_close(&rtld_binary);
 	} else {

commit 7f8665b26dc6e26d16395db4b959e2d253e0fbf9

radv: fix dumping disassembly with RADV_DEBUG=shaders
Fixes: 3d3842bfa0f ("radv: dont store disasm string unless keep_shader_info flag set")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

src/amd/vulkan/radv_shader.c
@@ -1013,7 +1013,8 @@ radv_shader_variant_create(struct radv_device *device,
 			return NULL;
 		}
 
-		if (device->keep_shader_info) {
+		if (device->keep_shader_info ||
+		    (device->instance->debug_flags & RADV_DEBUG_DUMP_SHADERS)) {
 			const char *disasm_data;
 			size_t disasm_size;
 			if (!ac_rtld_get_section_by_name(&rtld_binary, ".AMDGPU.disasm", &disasm_data, &disasm_size)) {