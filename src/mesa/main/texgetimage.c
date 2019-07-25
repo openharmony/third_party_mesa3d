openharmony/third_party_mesa3d

commit f818db8a67a1b3924cd394c320015aeb5d479b53

mesa: refactor get_texture_image to remove duplicate code
Move shared code in a new function (_get_texture_image) and use it instead
of duplicating the same lines.
Will be also used by the EXT_dsa functions (GetTextureImageEXT and GetMultiTexImageEXT).

Reviewed-by: Marek Olšák <marek.olsak@amd.com>

src/mesa/main/texgetimage.c
@@ -1445,23 +1445,28 @@ get_texture_image(struct gl_context *ctx,
    _mesa_unlock_texture(ctx, texObj);
 }
 
-
-void GLAPIENTRY
-_mesa_GetnTexImageARB(GLenum target, GLint level, GLenum format, GLenum type,
-                      GLsizei bufSize, GLvoid *pixels)
+static void
+_get_texture_image(struct gl_context *ctx,
+                  struct gl_texture_object *texObj,
+                  GLenum target, GLint level,
+                  GLenum format, GLenum type,
+                  GLsizei bufSize, GLvoid *pixels,
+                  const char *caller)
 {
-   GET_CURRENT_CONTEXT(ctx);
-   static const char *caller = "glGetnTexImageARB";
    GLsizei width, height, depth;
-   struct gl_texture_object *texObj;
-
-   if (!legal_getteximage_target(ctx, target, false)) {
+   /* EXT/ARB direct_state_access variants don't call _get_texture_image
+    * with a NULL texObj */
+   bool is_dsa = texObj != NULL;
+   if (!legal_getteximage_target(ctx, target, is_dsa)) {
       _mesa_error(ctx, GL_INVALID_ENUM, "%s", caller);
       return;
    }
 
-   texObj = _mesa_get_current_tex_object(ctx, target);
-   assert(texObj);
+   if (!is_dsa) {
+      texObj = _mesa_get_current_tex_object(ctx, target);
+      assert(texObj);
+   }
+
 
    get_texture_image_dims(texObj, target, level, &width, &height, &depth);
 
@@ -1474,6 +1479,18 @@ _mesa_GetnTexImageARB(GLenum target, GLint level, GLenum format, GLenum type,
    get_texture_image(ctx, texObj, target, level,
                      0, 0, 0, width, height, depth,
                      format, type, pixels, caller);
+}
+
+
+void GLAPIENTRY
+_mesa_GetnTexImageARB(GLenum target, GLint level, GLenum format, GLenum type,
+                      GLsizei bufSize, GLvoid *pixels)
+{
+   GET_CURRENT_CONTEXT(ctx);
+   static const char *caller = "glGetnTexImageARB";
+
+   _get_texture_image(ctx, NULL, target, level, format, type,
+                      bufSize, pixels, caller);
 }
 
 
@@ -1483,28 +1500,9 @@ _mesa_GetTexImage(GLenum target, GLint level, GLenum format, GLenum type,
 {
    GET_CURRENT_CONTEXT(ctx);
    static const char *caller = "glGetTexImage";
-   GLsizei width, height, depth;
-   struct gl_texture_object *texObj;
 
-   if (!legal_getteximage_target(ctx, target, false)) {
-      _mesa_error(ctx, GL_INVALID_ENUM, "%s", caller);
-      return;
-   }
-
-   texObj = _mesa_get_current_tex_object(ctx, target);
-   assert(texObj);
-
-   get_texture_image_dims(texObj, target, level, &width, &height, &depth);
-
-   if (getteximage_error_check(ctx, texObj, target, level,
-                               width, height, depth,
-                               format, type, INT_MAX, pixels, caller)) {
-      return;
-   }
-
-   get_texture_image(ctx, texObj, target, level,
-                     0, 0, 0, width, height, depth,
-                     format, type, pixels, caller);
+   _get_texture_image(ctx, NULL, target, level, format, type,
+                      INT_MAX, pixels, caller);
 }
 
 
@@ -1513,7 +1511,6 @@ _mesa_GetTextureImage(GLuint texture, GLint level, GLenum format, GLenum type,
                       GLsizei bufSize, GLvoid *pixels)
 {
    GET_CURRENT_CONTEXT(ctx);
-   GLsizei width, height, depth;
    static const char *caller = "glGetTextureImage";
    struct gl_texture_object *texObj =
       _mesa_lookup_texture_err(ctx, texture, caller);
@@ -1522,23 +1519,8 @@ _mesa_GetTextureImage(GLuint texture, GLint level, GLenum format, GLenum type,
       return;
    }
 
-   if (!legal_getteximage_target(ctx, texObj->Target, true)) {
-      _mesa_error(ctx, GL_INVALID_OPERATION, "%s", caller);
-      return;
-   }
-
-   get_texture_image_dims(texObj, texObj->Target, level,
-                          &width, &height, &depth);
-
-   if (getteximage_error_check(ctx, texObj, texObj->Target, level,
-                               width, height, depth,
-                               format, type, bufSize, pixels, caller)) {
-      return;
-   }
-
-   get_texture_image(ctx, texObj, texObj->Target, level,
-                     0, 0, 0, width, height, depth,
-                     format, type, pixels, caller);
+   _get_texture_image(ctx, texObj, texObj->Target, level, format, type,
+                      bufSize, pixels, caller);
 }
 
 

commit c3f8ca977d8cdfe5556e25f186b0652a2df28d21

mesa: Fix GetTextureImage error reporting, again
Iago Toral Quiroga fixed this in commit 04dc5dfb6fe0222b6717ca6c1ccec,
but it recently regressed in f818db8a67a1b3924cd394c320015aeb5d479b53.
Quoting Iago's original commit message for the fix:

GetTex*Image should return INVALID_ENUM if target is not valid, however,
GetTextureImage does not receive a target, and instead should return
INVALID_OPERATION if the effective target is not valid.  From the
OpenGL 4.6 core profile spec, section 8.11 Texture Queries:

   "An INVALID_OPERATION error is generated by GetTextureImage if the
    effective target is not one of TEXTURE_1D, TEXTURE_2D, TEXTURE_3D,
    TEXTURE_1D_ARRAY, TEXTURE_2D_ARRAY, TEXTURE_CUBE_MAP_ARRAY,
    TEXTURE_RECTANGLE, or TEXTURE_CUBE_MAP (for GetTextureImage only)."

Note that this differs from the original ARB_direct_state_access spec.

However, the EXT_direct_state_access version does take a target
parameter, so it should continue reporting INVALID_ENUM.

Fixes KHR-GL45.direct_state_access.textures_image_query_errors.

Fixes: f818db8a67a ("mesa: refactor get_texture_image to remove duplicate code")
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

src/mesa/main/texgetimage.c
@@ -1457,10 +1457,6 @@ _get_texture_image(struct gl_context *ctx,
    /* EXT/ARB direct_state_access variants don't call _get_texture_image
     * with a NULL texObj */
    bool is_dsa = texObj != NULL;
-   if (!legal_getteximage_target(ctx, target, is_dsa)) {
-      _mesa_error(ctx, GL_INVALID_ENUM, "%s", caller);
-      return;
-   }
 
    if (!is_dsa) {
       texObj = _mesa_get_current_tex_object(ctx, target);
@@ -1489,6 +1485,11 @@ _mesa_GetnTexImageARB(GLenum target, GLint level, GLenum format, GLenum type,
    GET_CURRENT_CONTEXT(ctx);
    static const char *caller = "glGetnTexImageARB";
 
+   if (!legal_getteximage_target(ctx, target, false)) {
+      _mesa_error(ctx, GL_INVALID_ENUM, "%s", caller);
+      return;
+   }
+
    _get_texture_image(ctx, NULL, target, level, format, type,
                       bufSize, pixels, caller);
 }
@@ -1500,6 +1501,11 @@ _mesa_GetTexImage(GLenum target, GLint level, GLenum format, GLenum type,
 {
    GET_CURRENT_CONTEXT(ctx);
    static const char *caller = "glGetTexImage";
+
+   if (!legal_getteximage_target(ctx, target, false)) {
+      _mesa_error(ctx, GL_INVALID_ENUM, "%s", caller);
+      return;
+   }
 
    _get_texture_image(ctx, NULL, target, level, format, type,
                       INT_MAX, pixels, caller);
@@ -1519,6 +1525,11 @@ _mesa_GetTextureImage(GLuint texture, GLint level, GLenum format, GLenum type,
       return;
    }
 
+   if (!legal_getteximage_target(ctx, texObj->Target, true)) {
+      _mesa_error(ctx, GL_INVALID_OPERATION, "%s", caller);
+      return;
+   }
+
    _get_texture_image(ctx, texObj, texObj->Target, level, format, type,
                       bufSize, pixels, caller);
 }
@@ -1535,6 +1546,11 @@ _mesa_GetTextureImageEXT(GLuint texture, GLenum target, GLint level,
                                      false, true, caller);
 
    if (!texObj) {
+      return;
+   }
+
+   if (!legal_getteximage_target(ctx, target, true)) {
+      _mesa_error(ctx, GL_INVALID_ENUM, "%s", caller);
       return;
    }
 