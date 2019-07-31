openharmony/third_party_mesa3d

commit 53188c5602a92c117d0d6a58a579b9ca4102894a

scons: Test for random_r()
Suggested-by: Eric Engestrom <eric.engestrom@intel.com>

scons/gallium.py
@@ -368,6 +368,9 @@ def generate(env):
         if check_functions(env, ['strtod_l', 'strtof_l']):
             cppdefines += ['HAVE_STRTOD_L']
 
+        if check_functions(env, 'random_r'):
+            cppdefines += ['HAVE_RANDOM_R']
+
         if check_functions(env, ['timespec_get']):
             cppdefines += ['HAVE_TIMESPEC_GET']
 

commit 0a308be68aa9d73293775c20c70311fc5fb5e8cb

scons: Fix random_r check.
Fixes: 53188c5602a9 ("scons: Test for random_r()")
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Eric Engestrom <eric@engestrom.ch>
Reviewed-by: Matt Turner <mattst88@gmail.com>

scons/gallium.py
@@ -369,7 +369,7 @@ def generate(env):
         if check_functions(env, ['strtod_l', 'strtof_l']):
             cppdefines += ['HAVE_STRTOD_L']
 
-        if check_functions(env, 'random_r'):
+        if check_functions(env, ['random_r']):
             cppdefines += ['HAVE_RANDOM_R']
 
         if check_functions(env, ['timespec_get']):