openharmony/third_party_mesa3d

commit d4cf69c7c9453ccecc7e70ee6a79f11ae1dc16f9

pan/midgard: Break, not return, in disassembler
We'll want to dump some stats after the shader, and I refuse to use one
teensy little goto.

Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>

src/panfrost/midgard/disassemble.c
@@ -1338,9 +1338,6 @@ disassemble_midgard(uint8_t *code, size_t size)
                 case midgard_word_type_alu:
                         print_alu_word(&words[i], num_quad_words, tabs);
 
-                        if (prefetch_flag)
-                                return;
-
                         /* Reset word static analysis state */
                         is_embedded_constant_half = false;
                         is_embedded_constant_int = false;
@@ -1355,6 +1352,9 @@ disassemble_midgard(uint8_t *code, size_t size)
                         break;
                 }
 
+                if (prefetch_flag && midgard_word_types[tag] == midgard_word_type_alu)
+                        break;
+
                 printf("\n");
 
                 unsigned next = (words[i] & 0xF0) >> 4;
@@ -1367,7 +1367,7 @@ disassemble_midgard(uint8_t *code, size_t size)
                         prefetch_flag = true;
 
                         if (midgard_word_types[words[i] & 0xF] != midgard_word_type_alu)
-                                return;
+                                break;
                 }
         }
 

commit 9c4001966737cbf0a843ab88a2ee0a2996c550d9

pan/midgard: Fix disassembly termination condition
Fixes: d4cf69c7c94 ("pan/midgard: Break, not return, in disassembler")

Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>

src/panfrost/midgard/disassemble.c
@@ -1387,8 +1387,6 @@ disassemble_midgard(uint8_t *code, size_t size, bool stats, unsigned nr_register
 
                 unsigned next = (words[i] & 0xF0) >> 4;
 
-                i += 4 * num_quad_words;
-
                 /* We are parsing per bundle anyway */
                 nr_bundles++;
                 nr_quadwords += num_quad_words;
@@ -1401,6 +1399,8 @@ disassemble_midgard(uint8_t *code, size_t size, bool stats, unsigned nr_register
                         if (midgard_word_types[words[i] & 0xF] != midgard_word_type_alu)
                                 break;
                 }
+
+                i += 4 * num_quad_words;
         }
 
         if (stats) {