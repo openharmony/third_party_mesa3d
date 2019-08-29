openharmony/third_party_mesa3d

commit 40212a365e01582c847f41fc7357916382f4dd05

nir/algebraic: Use value range analysis to eliminate tautological compares
It's only one application on one platform (Haswell) that's affected,
but spills and fills increase quite dramatically. :(

All Gen8+ platforms had similar results. (Ice Lake shown)
total instructions in shared programs: 16320850 -> 16297320 (-0.14%)
instructions in affected programs: 448012 -> 424482 (-5.25%)
helped: 1938
HURT: 0
helped stats (abs) min: 2 max: 264 x̄: 12.14 x̃: 10
helped stats (rel) min: 0.35% max: 43.75% x̄: 5.85% x̃: 5.38%
95% mean confidence interval for instructions value: -12.80 -11.48
95% mean confidence interval for instructions %-change: -5.99% -5.72%
Instructions are helped.

total cycles in shared programs: 367496943 -> 367221624 (-0.07%)
cycles in affected programs: 8557232 -> 8281913 (-3.22%)
helped: 1907
HURT: 26
helped stats (abs) min: 4 max: 12802 x̄: 147.21 x̃: 48
helped stats (rel) min: 0.03% max: 75.85% x̄: 5.55% x̃: 3.94%
HURT stats (abs)   min: 4 max: 1870 x̄: 208.23 x̃: 20
HURT stats (rel)   min: 0.16% max: 32.11% x̄: 8.31% x̃: 0.79%
95% mean confidence interval for cycles value: -165.38 -119.48
95% mean confidence interval for cycles %-change: -5.68% -5.04%
Cycles are helped.

LOST:   1
GAINED: 0

Haswell
total instructions in shared programs: 13374211 -> 13353925 (-0.15%)
instructions in affected programs: 349868 -> 329582 (-5.80%)
helped: 1669
HURT: 1
helped stats (abs) min: 1 max: 264 x̄: 12.57 x̃: 10
helped stats (rel) min: 0.12% max: 46.81% x̄: 6.86% x̃: 6.49%
HURT stats (abs)   min: 700 max: 700 x̄: 700.00 x̃: 700
HURT stats (rel)   min: 64.34% max: 64.34% x̄: 64.34% x̃: 64.34%
95% mean confidence interval for instructions value: -13.25 -11.04
95% mean confidence interval for instructions %-change: -7.01% -6.63%
Instructions are helped.

total cycles in shared programs: 375763544 -> 375535444 (-0.06%)
cycles in affected programs: 6932686 -> 6704586 (-3.29%)
helped: 1622
HURT: 48
helped stats (abs) min: 2 max: 12229 x̄: 148.31 x̃: 68

src/compiler/nir/nir_opt_algebraic.py
@@ -798,33 +798,33 @@ optimizations = [
    (('fmax', ('fadd', ('fmul', ('fsat', a), ('fneg', ('fsat', a))), 1.0), 0.0), ('fadd', ('fmul', ('fsat', a), ('fneg', ('fsat', a))), 1.0)),
    (('fmax', ('fadd', ('fneg', ('fmul', ('fsat', a), ('fsat', a))), 1.0), 0.0), ('fadd', ('fneg', ('fmul', ('fsat', a), ('fsat', a))), 1.0)),
 
-   (('fne(is_not_used_by_if)', 'a(is_not_zero)', 0.0), True),
-   (('feq(is_not_used_by_if)', 'a(is_not_zero)', 0.0), False),
+   (('fne', 'a(is_not_zero)', 0.0), True),
+   (('feq', 'a(is_not_zero)', 0.0), False),
 
-   (('fge(is_not_used_by_if)', 'a(is_not_negative)', 'b(is_not_positive)'), True),
-   (('fge(is_not_used_by_if)', 'b(is_not_positive)', 'a(is_gt_zero)'),      False),
-   (('fge(is_not_used_by_if)', 'a(is_lt_zero)',      'b(is_not_negative)'), False),
-   (('fge(is_not_used_by_if)', 'b(is_not_negative)', 'a(is_not_positive)'), True),
+   (('fge', 'a(is_not_negative)', 'b(is_not_positive)'), True),
+   (('fge', 'b(is_not_positive)', 'a(is_gt_zero)'),      False),
+   (('fge', 'a(is_lt_zero)',      'b(is_not_negative)'), False),
+   (('fge', 'b(is_not_negative)', 'a(is_not_positive)'), True),
 
-   (('flt(is_not_used_by_if)', 'a(is_not_negative)', 'b(is_not_positive)'), False),
-   (('flt(is_not_used_by_if)', 'b(is_not_positive)', 'a(is_gt_zero)'),      True),
-   (('flt(is_not_used_by_if)', 'a(is_lt_zero)',      'b(is_not_negative)'), True),
-   (('flt(is_not_used_by_if)', 'b(is_not_negative)', 'a(is_not_positive)'), False),
+   (('flt', 'a(is_not_negative)', 'b(is_not_positive)'), False),
+   (('flt', 'b(is_not_positive)', 'a(is_gt_zero)'),      True),
+   (('flt', 'a(is_lt_zero)',      'b(is_not_negative)'), True),
+   (('flt', 'b(is_not_negative)', 'a(is_not_positive)'), False),
 
-   (('ine(is_not_used_by_if)', 'a(is_not_zero)', 0), True),
-   (('ieq(is_not_used_by_if)', 'a(is_not_zero)', 0), False),
+   (('ine', 'a(is_not_zero)', 0), True),
+   (('ieq', 'a(is_not_zero)', 0), False),
 
-   (('ige(is_not_used_by_if)', 'a(is_not_negative)', 'b(is_not_positive)'), True),
-   (('ige(is_not_used_by_if)', 'b(is_not_positive)', 'a(is_gt_zero)'),      False),
-   (('ige(is_not_used_by_if)', 'a(is_lt_zero)',      'b(is_not_negative)'), False),
-   (('ige(is_not_used_by_if)', 'b(is_not_negative)', 'a(is_not_positive)'), True),
+   (('ige', 'a(is_not_negative)', 'b(is_not_positive)'), True),
+   (('ige', 'b(is_not_positive)', 'a(is_gt_zero)'),      False),
+   (('ige', 'a(is_lt_zero)',      'b(is_not_negative)'), False),
+   (('ige', 'b(is_not_negative)', 'a(is_not_positive)'), True),
 
-   (('ilt(is_not_used_by_if)', 'a(is_not_negative)', 'b(is_not_positive)'), False),
-   (('ilt(is_not_used_by_if)', 'b(is_not_positive)', 'a(is_gt_zero)'),      True),
-   (('ilt(is_not_used_by_if)', 'a(is_lt_zero)',      'b(is_not_negative)'), True),
-   (('ilt(is_not_used_by_if)', 'b(is_not_negative)', 'a(is_not_positive)'), False),
+   (('ilt', 'a(is_not_negative)', 'b(is_not_positive)'), False),
+   (('ilt', 'b(is_not_positive)', 'a(is_gt_zero)'),      True),
+   (('ilt', 'a(is_lt_zero)',      'b(is_not_negative)'), True),
+   (('ilt', 'b(is_not_negative)', 'a(is_not_positive)'), False),
 
-   (('ult(is_not_used_by_if)', 0, 'a(is_gt_zero)'), True),
+   (('ult', 0, 'a(is_gt_zero)'), True),
 
    # Packing and then unpacking does nothing
    (('unpack_64_2x32_split_x', ('pack_64_2x32_split', a, b)), a),

commit 3236cdb4c44aa4c7e5c8762e9eb6fcdc34d65d98

nir/algebraic: Mark some value range analysis-based optimizations imprecise
This didn't fix bug #111308, but it was found will trying to find the
actual cause of that bug.

Fixes piglit tests (new in piglit!110):

    - fs-fract-of-NaN.shader_test
    - fs-lt-nan-tautology.shader_test
    - fs-ge-nan-tautology.shader_test

No shader-db changes on any Intel platform.

Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=111308
Fixes: 40212a365e0 ("nir/algebraic: Use value range analysis to eliminate tautological compares")
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Reviewed-by: Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>

src/compiler/nir/nir_opt_algebraic.py
@@ -807,7 +807,8 @@ optimizations.extend([
    (('ffloor', 'a(is_integral)'), a),
    (('fceil', 'a(is_integral)'), a),
    (('ftrunc', 'a(is_integral)'), a),
-   (('ffract', 'a(is_integral)'), 0.0),
+   # fract(x) = x - floor(x), so fract(NaN) = NaN
+   (('~ffract', 'a(is_integral)'), 0.0),
    (('fabs', 'a(is_not_negative)'), a),
    (('iabs', 'a(is_not_negative)'), a),
    (('fsat', 'a(is_not_positive)'), 0.0),
@@ -837,15 +838,18 @@ optimizations.extend([
    (('fne', 'a(is_not_zero)', 0.0), True),
    (('feq', 'a(is_not_zero)', 0.0), False),
 
-   (('fge', 'a(is_not_negative)', 'b(is_not_positive)'), True),
-   (('fge', 'b(is_not_positive)', 'a(is_gt_zero)'),      False),
-   (('fge', 'a(is_lt_zero)',      'b(is_not_negative)'), False),
-   (('fge', 'b(is_not_negative)', 'a(is_not_positive)'), True),
+   # The results expecting true, must be marked imprecise.  The results
+   # expecting false are fine because NaN compared >= or < anything is false.
 
-   (('flt', 'a(is_not_negative)', 'b(is_not_positive)'), False),
-   (('flt', 'b(is_not_positive)', 'a(is_gt_zero)'),      True),
-   (('flt', 'a(is_lt_zero)',      'b(is_not_negative)'), True),
-   (('flt', 'b(is_not_negative)', 'a(is_not_positive)'), False),
+   (('~fge', 'a(is_not_negative)', 'b(is_not_positive)'), True),
+   (('fge',  'b(is_not_positive)', 'a(is_gt_zero)'),      False),
+   (('fge',  'a(is_lt_zero)',      'b(is_not_negative)'), False),
+   (('~fge', 'b(is_not_negative)', 'a(is_not_positive)'), True),
+
+   (('flt',  'a(is_not_negative)', 'b(is_not_positive)'), False),
+   (('~flt', 'b(is_not_positive)', 'a(is_gt_zero)'),      True),
+   (('~flt', 'a(is_lt_zero)',      'b(is_not_negative)'), True),
+   (('flt',  'b(is_not_negative)', 'a(is_not_positive)'), False),
 
    (('ine', 'a(is_not_zero)', 0), True),
    (('ieq', 'a(is_not_zero)', 0), False),