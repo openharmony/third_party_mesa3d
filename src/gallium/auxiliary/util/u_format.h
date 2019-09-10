openharmony/third_party_mesa3d

commit 996cc2fa9e8a327253062b6845a7fa4f07aba333

gallium/auxiliary: Add util_format_get_depth_only() helper.
This will be used by u_transfer_helper.c shortly, in order to split
packed depth-stencil into separate resources.

Reviewed-by: Eric Anholt <eric@anholt.net>

src/gallium/auxiliary/util/u_format.h
@@ -557,6 +557,27 @@ util_format_is_depth_and_stencil(enum pipe_format format)
           util_format_has_stencil(desc);
 }
 
+/**
+ * For depth-stencil formats, return the equivalent depth-only format.
+ */
+static inline boolean
+util_format_get_depth_only(enum pipe_format format)
+{
+   switch (format) {
+   case PIPE_FORMAT_Z24_UNORM_S8_UINT:
+      return PIPE_FORMAT_Z24X8_UNORM;
+
+   case PIPE_FORMAT_S8_UINT_Z24_UNORM:
+      return PIPE_FORMAT_X8Z24_UNORM;
+
+   case PIPE_FORMAT_Z32_FLOAT_S8X24_UINT:
+      return PIPE_FORMAT_Z32_FLOAT;
+
+   default:
+      return format;
+   }
+}
+
 static inline boolean
 util_format_is_yuv(enum pipe_format format)
 {

commit 041bba9e8691c309c7f82f188ea2a3182e6a1adf

gallium: Fix util_format_get_depth_only
This is a pipe format, not a boolean.

Fixes: 996cc2fa9e8 ("gallium/auxiliary: Add util_format_get_depth_only() helper.")
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>

src/gallium/auxiliary/util/u_format.h
@@ -573,7 +573,7 @@ util_format_is_depth_and_stencil(enum pipe_format format)
 /**
  * For depth-stencil formats, return the equivalent depth-only format.
  */
-static inline boolean
+static inline enum pipe_format
 util_format_get_depth_only(enum pipe_format format)
 {
    switch (format) {