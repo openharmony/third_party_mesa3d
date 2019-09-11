openharmony/third_party_mesa3d

commit cc1f1c795d3760224fef27cb3df73d5b7ebf5e95

glsl/nir: Add and use a gl_nir_link() function
Perform all the NIR linking steps in order.  Change iris and i965 to
use it.  Suggested by Alejandro.

v2: Add gl_nir_linker_options struct.

Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com> [v1]

src/mesa/state_tracker/st_glsl_to_nir.cpp
@@ -687,14 +687,11 @@ st_link_nir(struct gl_context *ctx,
     * st_nir_preprocess.
     */
    if (shader_program->data->spirv) {
-      if (!gl_nir_link_uniform_blocks(ctx, shader_program))
+      static const gl_nir_linker_options opts = {
+         .fill_parameters = true,
+      };
+      if (!gl_nir_link(ctx, shader_program, &opts))
          return GL_FALSE;
-
-      if (!gl_nir_link_uniforms(ctx, shader_program, /* fill_parameters */ true))
-         return GL_FALSE;
-
-      gl_nir_link_assign_atomic_counter_resources(ctx, shader_program);
-      gl_nir_link_assign_xfb_resources(ctx, shader_program);
 
       nir_build_program_resource_list(ctx, shader_program);
 

src/compiler/glsl/gl_nir_linker.c
@@ -253,3 +253,19 @@ nir_build_program_resource_list(struct gl_context *ctx,
 
    _mesa_set_destroy(resource_set, NULL);
 }
+
+bool
+gl_nir_link(struct gl_context *ctx, struct gl_shader_program *prog,
+            const struct gl_nir_linker_options *options)
+{
+   if (!gl_nir_link_uniform_blocks(ctx, prog))
+      return false;
+
+   if (!gl_nir_link_uniforms(ctx, prog, options->fill_parameters))
+      return false;
+
+   gl_nir_link_assign_atomic_counter_resources(ctx, prog);
+   gl_nir_link_assign_xfb_resources(ctx, prog);
+
+   return true;
+}

src/compiler/glsl/gl_nir_linker.h
@@ -31,6 +31,14 @@ extern "C" {
 struct gl_context;
 struct gl_shader_program;
 
+struct gl_nir_linker_options {
+   bool fill_parameters;
+};
+
+bool gl_nir_link(struct gl_context *ctx,
+                 struct gl_shader_program *prog,
+                 const struct gl_nir_linker_options *options);
+
 bool gl_nir_link_uniforms(struct gl_context *ctx,
                           struct gl_shader_program *prog,
                           bool fill_parameters);

src/mesa/drivers/dri/i965/brw_link.cpp
@@ -269,15 +269,11 @@ brw_link_shader(struct gl_context *ctx, struct gl_shader_program *shProg)
 
    /* SPIR-V programs use a NIR linker */
    if (shProg->data->spirv) {
-      if (!gl_nir_link_uniform_blocks(ctx, shProg)) {
+      static const gl_nir_linker_options opts = {
+         .fill_parameters = false,
+      };
+      if (!gl_nir_link(ctx, shProg, &opts))
          return GL_FALSE;
-      }
-
-      if (!gl_nir_link_uniforms(ctx, shProg, /* fill_parameters */ false))
-         return GL_FALSE;
-
-      gl_nir_link_assign_atomic_counter_resources(ctx, shProg);
-      gl_nir_link_assign_xfb_resources(ctx, shProg);
    }
 
    for (stage = 0; stage < ARRAY_SIZE(shProg->_LinkedShaders); stage++) {

commit 21161ab9c8b3ed32b3f34f4433719f60bb1b70ef

st/nir: fix illegal designated initializer in st_glsl_to_nir.cpp
IIRC, designated initializers are not legal C++.
Fixes the MSVC build.

Fixes: cc1f1c79 ("glsl/nir: Add and use a gl_nir_link() function")

Reviewed-by: Neha Bhende <bhenden@vmware.com>

src/mesa/state_tracker/st_glsl_to_nir.cpp
@@ -688,7 +688,7 @@ st_link_nir(struct gl_context *ctx,
     */
    if (shader_program->data->spirv) {
       static const gl_nir_linker_options opts = {
-         .fill_parameters = true,
+         true /*fill_parameters */
       };
       if (!gl_nir_link(ctx, shader_program, &opts))
          return GL_FALSE;