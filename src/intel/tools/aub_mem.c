openharmony/third_party_mesa3d

commit 6b009957daf437178d0c7e8f0b724104f7f0d640

util/rb_tree: Reverse the order of comparison functions
The new order matches that of the comparison functions accepted by the C
standard library qsort() functions.  Being consistent with qsort will
hopefully help avoid developer confusion.

The only current user of the red-black tree is aub_mem.c which is pretty
easy to fix up.

Reviewed-by: Lionel Landwerlin <lionel.g.lndwerlin@intel.com>

src/intel/tools/aub_mem.c
@@ -87,9 +87,9 @@ static inline int
 cmp_uint64(uint64_t a, uint64_t b)
 {
    if (a < b)
-      return -1;
-   if (a > b)
       return 1;
+   if (a > b)
+      return -1;
    return 0;
 }
 

src/util/rb_tree.h
@@ -127,7 +127,7 @@ rb_tree_insert(struct rb_tree *T, struct rb_node *node,
     bool left = false;
     while (x != NULL) {
         y = x;
-        left = cmp(node, x) < 0;
+        left = cmp(x, node) < 0;
         if (left)
             x = x->left;
         else
@@ -167,9 +167,9 @@ rb_tree_search(struct rb_tree *T, const void *key,
     while (x != NULL) {
         int c = cmp(x, key);
         if (c < 0)
-            x = x->right;
-        else if (c > 0)
             x = x->left;
+        else if (c > 0)
+            x = x->right;
         else
             return x;
     }
@@ -205,9 +205,9 @@ rb_tree_search_sloppy(struct rb_tree *T, const void *key,
         y = x;
         int c = cmp(x, key);
         if (c < 0)
-            x = x->right;
-        else if (c > 0)
             x = x->left;
+        else if (c > 0)
+            x = x->right;
         else
             return x;
     }

src/util/rb_tree_test.c
@@ -56,7 +56,7 @@ static int
 rb_test_node_cmp_void(const struct rb_node *n, const void *v)
 {
     struct rb_test_node *tn = rb_node_data(struct rb_test_node, n, node);
-    return tn->key - *(int *)v;
+    return *(int *)v - tn->key;
 }
 
 static int
@@ -65,7 +65,7 @@ rb_test_node_cmp(const struct rb_node *a, const struct rb_node *b)
     struct rb_test_node *ta = rb_node_data(struct rb_test_node, a, node);
     struct rb_test_node *tb = rb_node_data(struct rb_test_node, b, node);
 
-    return ta->key - tb->key;
+    return tb->key - ta->key;
 }
 
 static void

commit 49835cf633ed7de05f00206f413c2523f35d470f

intel/tools: Fix aubinator usage of rb_tree.
The order of comparison has changed, so we need to invert the logic of
"insert_left" when using rb_tree_insert_at().

Fixes: 6b009957daf (util/rb_tree: Reverse the order of comparison
                    functions).
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

src/intel/tools/aub_mem.c
@@ -109,7 +109,7 @@ ensure_ggtt_entry(struct aub_mem *mem, uint64_t virt_addr)
    if (!node || (cmp = cmp_ggtt_entry(node, &virt_addr))) {
       struct ggtt_entry *new_entry = calloc(1, sizeof(*new_entry));
       new_entry->virt_addr = virt_addr;
-      rb_tree_insert_at(&mem->ggtt, node, &new_entry->node, cmp > 0);
+      rb_tree_insert_at(&mem->ggtt, node, &new_entry->node, cmp < 0);
       node = &new_entry->node;
    }
 
@@ -153,7 +153,7 @@ ensure_phys_mem(struct aub_mem *mem, uint64_t phys_addr)
                            mem->mem_fd, new_mem->fd_offset);
       assert(new_mem->data != MAP_FAILED);
 
-      rb_tree_insert_at(&mem->mem, node, &new_mem->node, cmp > 0);
+      rb_tree_insert_at(&mem->mem, node, &new_mem->node, cmp < 0);
       node = &new_mem->node;
    }
 