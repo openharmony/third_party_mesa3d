openharmony/third_party_mesa3d

commit 2141377490b8258f76ee2f242af8fe8bcf0d3f67

gallivm: add coroutine pass manager support
coroutines require a proper pass manager, so add the passes
to the correct places

Reviewed-by: Roland Scheidegger <sroland@vmware.com>

src/gallium/auxiliary/gallivm/lp_bld_init.c
@@ -44,7 +44,9 @@
 #include <llvm-c/Transforms/Utils.h>
 #endif
 #include <llvm-c/BitWriter.h>
-
+#if GALLIVM_HAVE_CORO
+#include <llvm-c/Transforms/Coroutines.h>
+#endif
 
 /* Only MCJIT is available as of LLVM SVN r216982 */
 #if HAVE_LLVM >= 0x0306
@@ -125,6 +127,10 @@ create_pass_manager(struct gallivm_state *gallivm)
    gallivm->passmgr = LLVMCreateFunctionPassManagerForModule(gallivm->module);
    if (!gallivm->passmgr)
       return FALSE;
+
+#if GALLIVM_HAVE_CORO
+   gallivm->cgpassmgr = LLVMCreatePassManager();
+#endif
    /*
     * TODO: some per module pass manager with IPO passes might be helpful -
     * the generated texture functions may benefit from inlining if they are
@@ -143,6 +149,12 @@ create_pass_manager(struct gallivm_state *gallivm)
       LLVMSetDataLayout(gallivm->module, td_str);
       free(td_str);
    }
+
+#if GALLIVM_HAVE_CORO
+   LLVMAddCoroEarlyPass(gallivm->cgpassmgr);
+   LLVMAddCoroSplitPass(gallivm->cgpassmgr);
+   LLVMAddCoroElidePass(gallivm->cgpassmgr);
+#endif
 
    if ((gallivm_perf & GALLIVM_PERF_NO_OPT) == 0) {
       /*
@@ -170,6 +182,9 @@ create_pass_manager(struct gallivm_state *gallivm)
       LLVMAddConstantPropagationPass(gallivm->passmgr);
       LLVMAddInstructionCombiningPass(gallivm->passmgr);
       LLVMAddGVNPass(gallivm->passmgr);
+#if GALLIVM_HAVE_CORO
+      LLVMAddCoroCleanupPass(gallivm->passmgr);
+#endif
    }
    else {
       /* We need at least this pass to prevent the backends to fail in
@@ -192,6 +207,12 @@ gallivm_free_ir(struct gallivm_state *gallivm)
    if (gallivm->passmgr) {
       LLVMDisposePassManager(gallivm->passmgr);
    }
+
+#if GALLIVM_HAVE_CORO
+   if (gallivm->cgpassmgr) {
+      LLVMDisposePassManager(gallivm->cgpassmgr);
+   }
+#endif
 
    if (gallivm->engine) {
       /* This will already destroy any associated module */
@@ -219,6 +240,7 @@ gallivm_free_ir(struct gallivm_state *gallivm)
    gallivm->target = NULL;
    gallivm->module = NULL;
    gallivm->module_name = NULL;
+   gallivm->cgpassmgr = NULL;
    gallivm->passmgr = NULL;
    gallivm->context = NULL;
    gallivm->builder = NULL;
@@ -610,6 +632,9 @@ gallivm_compile_module(struct gallivm_state *gallivm)
    if (gallivm_debug & GALLIVM_DEBUG_PERF)
       time_begin = os_time_get();
 
+#if GALLIVM_HAVE_CORO
+   LLVMRunPassManager(gallivm->cgpassmgr, gallivm->module);
+#endif
    /* Run optimization passes */
    LLVMInitializeFunctionPassManager(gallivm->passmgr);
    func = LLVMGetFirstFunction(gallivm->module);

src/gallium/auxiliary/gallivm/lp_bld.h
@@ -95,6 +95,11 @@ typedef void *LLVMMCJITMemoryManagerRef;
 #define LLVMInsertBasicBlock ILLEGAL_LLVM_FUNCTION
 #define LLVMCreateBuilder ILLEGAL_LLVM_FUNCTION
 
+#if HAVE_LLVM >= 0x0800
+#define GALLIVM_HAVE_CORO 1
+#else
+#define GALLIVM_HAVE_CORO 0
+#endif
 
 /*
  * Before LLVM 3.4 LLVMSetAlignment only supported GlobalValue, not

src/gallium/auxiliary/gallivm/lp_bld_init.h
@@ -46,6 +46,7 @@ struct gallivm_state
    LLVMExecutionEngineRef engine;
    LLVMTargetDataRef target;
    LLVMPassManagerRef passmgr;
+   LLVMPassManagerRef cgpassmgr;
    LLVMContextRef context;
    LLVMBuilderRef builder;
    LLVMMCJITMemoryManagerRef memorymgr;

commit 533679dea16f3dab8ae431ab7fd0f9a32ad87b57

gallivm: fix coroutines on aarch64 with llvm 8
The coroutine split pass is missing a dependency before LLVM 9.0,
and fails to initialise properly if the CallGraphWrapperPass hasn't
be initialised earlier (x86 does it due to some of it's passes
requiring it).

This is a workaround for llvm 8 (coroutines are only supported in 8
and higher). It adds another pass that has a dependency on the pass
the coroutines split requires. This pass shouldn't have any raal
effects.

Fixes: 2141377490b (gallivm: add coroutine pass manager support)
Reviewed-by: Roland Scheidegger <sroland@vmware.com>

src/gallium/auxiliary/gallivm/lp_bld_init.c
@@ -46,6 +46,9 @@
 #endif
 #include <llvm-c/BitWriter.h>
 #if GALLIVM_HAVE_CORO
+#if LLVM_VERSION_MAJOR <= 8 && defined(PIPE_ARCH_AARCH64)
+#include <llvm-c/Transforms/IPO.h>
+#endif
 #include <llvm-c/Transforms/Coroutines.h>
 #endif
 
@@ -134,6 +137,9 @@ create_pass_manager(struct gallivm_state *gallivm)
    }
 
 #if GALLIVM_HAVE_CORO
+#if LLVM_VERSION_MAJOR <= 8 && defined(PIPE_ARCH_AARCH64)
+   LLVMAddFunctionAttrsPass(gallivm->cgpassmgr);
+#endif
    LLVMAddCoroEarlyPass(gallivm->cgpassmgr);
    LLVMAddCoroSplitPass(gallivm->cgpassmgr);
    LLVMAddCoroElidePass(gallivm->cgpassmgr);