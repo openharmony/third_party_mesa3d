openharmony/third_party_mesa3d

commit 10d1f35cf9565918a2c735ac8e26dc8c321a0049

amd: Move all amd/common code that depends on LLVM to amd/llvm.
This commit is a step towards the goal of being able to build RADV
without LLVM. In the future we would like to offer the option to
use RADV solely with ACO. There is still a need for the common AMD
code located in amd/common but the LLVM specific parts need to be
separated.

Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Acked-by: Marek Olšák <marek.olsak@amd.com>
Acked-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

src/amd/Android.common.mk
@@ -30,9 +30,8 @@ LOCAL_MODULE := libmesa_amd_common
 
 LOCAL_SRC_FILES := \
 	$(AMD_COMMON_FILES) \
-	$(AMD_COMPILER_FILES) \
-	$(AMD_DEBUG_FILES) \
-	$(AMD_NIR_FILES)
+	$(AMD_COMMON_LLVM_FILES) \
+	$(AMD_DEBUG_FILES)
 
 LOCAL_CFLAGS += -DFORCE_BUILD_AMDGPU   # instructs LLVM to declare LLVMInitializeAMDGPU* functions
 
@@ -72,6 +71,7 @@ LOCAL_C_INCLUDES := \
 	$(MESA_TOP)/include \
 	$(MESA_TOP)/src \
 	$(MESA_TOP)/src/amd/common \
+	$(MESA_TOP)/src/amd/llvm \
 	$(MESA_TOP)/src/compiler \
 	$(call generated-sources-dir-for,STATIC_LIBRARIES,libmesa_nir,,)/nir \
 	$(MESA_TOP)/src/gallium/include \

src/amd/Makefile.sources
@@ -36,33 +36,30 @@ ADDRLIB_FILES = \
 	addrlib/src/r800/siaddrlib.cpp \
 	addrlib/src/r800/siaddrlib.h
 
-AMD_COMPILER_FILES = \
+AMD_COMMON_FILES = \
 	common/ac_binary.c \
 	common/ac_binary.h \
 	common/ac_exp_param.h \
-	common/ac_llvm_build.c \
-	common/ac_llvm_build.h \
-	common/ac_llvm_cull.c \
-	common/ac_llvm_cull.h \
-	common/ac_llvm_helper.cpp \
-	common/ac_llvm_util.c \
-	common/ac_llvm_util.h \
-	common/ac_rtld.c \
-	common/ac_rtld.h \
-	common/ac_shader_abi.h \
-	common/ac_shader_util.c \
-	common/ac_shader_util.h
-
-
-AMD_NIR_FILES = \
-	common/ac_nir_to_llvm.c \
-	common/ac_nir_to_llvm.h
-
-AMD_COMMON_FILES = \
 	common/ac_gpu_info.c \
 	common/ac_gpu_info.h \
 	common/ac_surface.c \
-	common/ac_surface.h
+	common/ac_surface.h \
+	common/ac_rtld.c \
+	common/ac_rtld.h \
+	common/ac_shader_util.c \
+	common/ac_shader_util.h
+
+AMD_COMMON_LLVM_FILES = \
+	llvm/ac_llvm_build.c \
+	llvm/ac_llvm_build.h \
+	llvm/ac_llvm_cull.c \
+	llvm/ac_llvm_cull.h \
+	llvm/ac_llvm_helper.cpp \
+	llvm/ac_llvm_util.c \
+	llvm/ac_llvm_util.h \
+	llvm/ac_shader_abi.h \
+	llvm/ac_nir_to_llvm.c \
+	llvm/ac_nir_to_llvm.h
 
 AMD_DEBUG_FILES = \
 	common/ac_debug.c \

src/amd/common/meson.build
@@ -40,18 +40,8 @@ amd_common_files = files(
   'ac_binary.c',
   'ac_binary.h',
   'ac_exp_param.h',
-  'ac_llvm_build.c',
-  'ac_llvm_build.h',
-  'ac_llvm_cull.c',
-  'ac_llvm_cull.h',
-  'ac_llvm_helper.cpp',
-  'ac_llvm_util.c',
-  'ac_llvm_util.h',
-  'ac_shader_abi.h',
   'ac_shader_util.c',
   'ac_shader_util.h',
-  'ac_nir_to_llvm.c',
-  'ac_nir_to_llvm.h',
   'ac_gpu_info.c',
   'ac_gpu_info.h',
   'ac_rtld.c',
@@ -69,7 +59,7 @@ libamd_common = static_library(
     inc_common, inc_compiler, inc_mesa, inc_mapi, inc_amd,
   ],
   dependencies : [
-    dep_llvm, dep_thread, dep_elf, dep_libdrm_amdgpu, dep_valgrind,
+    dep_thread, dep_elf, dep_libdrm_amdgpu, dep_valgrind,
     idep_nir_headers,
   ],
   c_args : [c_vis_args],

src/amd/compiler/meson.build
@@ -85,7 +85,7 @@ _libaco = static_library(
   'aco',
   [libaco_files, aco_opcodes_c, aco_opcodes_h, aco_builder_h],
   include_directories : [
-   inc_common, inc_compiler, inc_mesa, inc_mapi, inc_amd, inc_amd_common,
+   inc_common, inc_compiler, inc_mesa, inc_mapi, inc_amd, inc_amd_common, inc_amd_common_llvm,
   ],
   dependencies : [
     dep_llvm, dep_thread, dep_elf, dep_libdrm_amdgpu, dep_valgrind,

commit e1a09b7f72390f5a70a1108eae8a2422c30cf262

android: amd/common: export amd/llvm headers
Fixes the following building error:

external/mesa/src/gallium/winsys/amdgpu/drm/amdgpu_winsys.c:42:10:
fatal error: 'ac_llvm_util.h' file not found
         ^~~~~~~~~~~~~~~~
1 error generated.

Fixes: 10d1f35 ("amd: Move all amd/common code that depends on LLVM to amd/llvm.")
Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

src/amd/Android.common.mk
@@ -81,6 +81,7 @@ LOCAL_C_INCLUDES := \
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := \
 	$(LOCAL_PATH)/common \
+	$(LOCAL_PATH)/llvm \
 	$(intermediates)/common
 
 LOCAL_SHARED_LIBRARIES := \