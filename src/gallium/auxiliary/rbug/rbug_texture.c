openharmony/third_party_mesa3d

commit 731a277cab92c1bbaa9f80d9fefc26818abc71fb

rbug: fix transmitted texture sizes
The rbug wire format defines the texture size parameters to be uint32_t sized
and uses memcpy to move the function parameters to the message structure.
This caused totally wrong transmitted texture sizes since the height and depth
paramterds have been changed to uint16_t in the gallium API. Fix this by doing
an explicit conversion to the correct representation before packing into the
wire message.

Fixes: 80cf4cdeef51 (gallium: decrease the size of pipe_resource - 64 -> 48 bytes)
Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

src/gallium/auxiliary/rbug/rbug_texture.c
@@ -283,9 +283,9 @@ int rbug_send_texture_info_reply(struct rbug_connection *__con,
                                  uint32_t format,
                                  uint32_t *width,
                                  uint32_t width_len,
-                                 uint16_t *height,
+                                 uint16_t *h16,
                                  uint32_t height_len,
-                                 uint16_t *depth,
+                                 uint16_t *d16,
                                  uint32_t depth_len,
                                  uint32_t blockw,
                                  uint32_t blockh,
@@ -299,6 +299,8 @@ int rbug_send_texture_info_reply(struct rbug_connection *__con,
 	uint32_t __pos = 0;
 	uint8_t *__data = NULL;
 	int __ret = 0;
+	uint32_t *height = alloca(sizeof(uint32_t) * height_len);
+	uint32_t *depth = alloca(sizeof(uint32_t) * height_len);
 
 	LEN(8); /* header */
 	LEN(4); /* serial */
@@ -320,6 +322,11 @@ int rbug_send_texture_info_reply(struct rbug_connection *__con,
 	__data = (uint8_t*)MALLOC(__len);
 	if (!__data)
 		return -ENOMEM;
+
+	for (int i = 0; i < height_len; i++)
+		height[i] = h16[i];
+	for (int i = 0; i < depth_len; i++)
+		depth[i] = d16[i];
 
 	WRITE(4, int32_t, ((int32_t)RBUG_OP_TEXTURE_INFO_REPLY));
 	WRITE(4, uint32_t, ((uint32_t)(__len / 4)));

commit fcfa9d60ea0940b1accc8240c02a3abbf9dcf4a5

rbug: Fix use of alloca() without #include "c99_alloca.h"
[12/60] Compiling C object 'src/gallium/auxiliary/eb820e8@@gallium@sta/rbug_rbug_texture.c.o'.
FAILED: src/gallium/auxiliary/eb820e8@@gallium@sta/rbug_rbug_texture.c.o
[...]
../src/gallium/auxiliary/rbug/rbug_texture.c: In function 'rbug_send_texture_info_reply':
../src/gallium/auxiliary/rbug/rbug_texture.c:302:21: error: implicit declaration of function 'alloca'; did you mean 'malloc'? [-Werror=implicit-function-declaration]
  uint32_t *height = alloca(sizeof(uint32_t) * height_len);
                     ^~~~~~
                     malloc
../src/gallium/auxiliary/rbug/rbug_texture.c:302:21: warning: initialization makes pointer from integer without a cast [-Wint-conversion]
../src/gallium/auxiliary/rbug/rbug_texture.c:303:20: warning: initialization makes pointer from integer without a cast [-Wint-conversion]
  uint32_t *depth = alloca(sizeof(uint32_t) * height_len);
                    ^~~~~~
cc1: some warnings being treated as errors

Include c99_alloca.h to portably make the alloca() prototype available.

See also: bdb48211, 1171f538, 9a42b061

Fixes: 731a277c ("rbug: fix transmitted texture sizes")
Reviewed-by: Eric Engestrom <eric@engestrom.ch>
Reviewed-by: Christian Gmeiner <christian.gmeiner@gmail.com>

src/gallium/auxiliary/rbug/rbug_texture.c
@@ -34,6 +34,8 @@
  * Functions ending with _reply are replies to requests.
  */
 
+#include "c99_alloca.h"
+
 #include "rbug_internal.h"
 #include "rbug_texture.h"
 