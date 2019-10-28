openharmony/third_party_mesa3d

commit 9141bc81814debfdc4b031785f04f538f2b5c323

iris/resource: Fall back to no aux if creation fails
No surface requires an auxiliary surface to operate correctly. Fall back
to an uncompressed surface if mesa fails to create and allocate an
auxiliary surface. This enables adding more restrictions to ISL without
having to update iris.

Reviewed-by: Rafael Antognolli <rafael.antognolli@intel.com>

src/gallium/drivers/iris/iris_resource.c
@@ -351,6 +351,7 @@ iris_resource_alloc_aux(struct iris_screen *screen, struct iris_resource *res)
    switch (res->aux.usage) {
    case ISL_AUX_USAGE_NONE:
       res->aux.surf.size_B = 0;
+      ok = true;
       break;
    case ISL_AUX_USAGE_HIZ:
       initial_state = ISL_AUX_STATE_AUX_INVALID;
@@ -392,12 +393,13 @@ iris_resource_alloc_aux(struct iris_screen *screen, struct iris_resource *res)
       break;
    }
 
+   /* We should have a valid aux_surf. */
+   if (!ok)
+      return false;
+
    /* No work is needed for a zero-sized auxiliary buffer. */
    if (res->aux.surf.size_B == 0)
       return true;
-
-   /* Assert that ISL gave us a valid aux surf */
-   assert(ok);
 
    /* Create the aux_state for the auxiliary buffer. */
    res->aux.state = create_aux_state_map(res, initial_state);
@@ -684,7 +686,7 @@ iris_resource_create_with_modifiers(struct pipe_screen *pscreen,
       goto fail;
 
    if (!iris_resource_alloc_aux(screen, res))
-      goto fail;
+      iris_resource_disable_aux(res);
 
    return &res->base;
 

commit 0c9aa5ff48617496ef5e8efc18ec691d45a5f805

iris: Disallow incomplete resource creation
If a modifier specifies an aux, it must be created.

Fixes: 9141bc81814 ("iris/resource: Fall back to no aux if creation fails")
Reviewed-by: Sagar Ghuge <sagar.ghuge@intel.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

src/gallium/drivers/iris/iris_resource.c
@@ -888,8 +888,12 @@ iris_resource_create_with_modifiers(struct pipe_screen *pscreen,
       }
    }
 
-   if (!aux_enabled)
-      iris_resource_disable_aux(res);
+   if (!aux_enabled) {
+      if (res->mod_info && res->mod_info->aux_usage != ISL_AUX_USAGE_NONE)
+         goto fail;
+      else
+         iris_resource_disable_aux(res);
+   }
 
    return &res->base;
 