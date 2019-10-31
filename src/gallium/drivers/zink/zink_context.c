openharmony/third_party_mesa3d

commit bc671bf0b4cab3d8b362ffc2d7845b2935130983

zink: use dynamic state for line-width
This will lead to fewer pipelines in the cache, which is assumed to
become our most unavoidable performance bottle-neck down the line.

Reviewed-by: Dave Airlie <airlied@redhat.com>

src/gallium/drivers/zink/zink_context.c
@@ -1020,8 +1020,10 @@ zink_draw_vbo(struct pipe_context *pctx,
                                                &ctx->gfx_pipeline_state,
                                                dinfo->mode);
 
+   enum pipe_prim_type reduced_prim = u_reduced_prim(dinfo->mode);
+
    bool depth_bias = false;
-   switch (u_reduced_prim(dinfo->mode)) {
+   switch (reduced_prim) {
    case PIPE_PRIM_POINTS:
       depth_bias = rast_state->offset_point;
       break;
@@ -1152,6 +1154,13 @@ zink_draw_vbo(struct pipe_context *pctx,
       fb_scissor.extent.width = ctx->fb_state.width;
       fb_scissor.extent.height = ctx->fb_state.height;
       vkCmdSetScissor(batch->cmdbuf, 0, 1, &fb_scissor);
+   }
+
+   if (reduced_prim == PIPE_PRIM_LINES) {
+      if (screen->feats.wideLines || ctx->line_width == 1.0f)
+         vkCmdSetLineWidth(batch->cmdbuf, ctx->line_width);
+      else
+         debug_printf("BUG: wide lines not supported, needs fallback!");
    }
 
    vkCmdSetStencilReference(batch->cmdbuf, VK_STENCIL_FACE_FRONT_BIT, ctx->stencil_ref.ref_value[0]);

src/gallium/drivers/zink/zink_context.h
@@ -103,6 +103,7 @@ struct zink_context {
    struct pipe_sampler_view *image_views[PIPE_SHADER_TYPES][PIPE_MAX_SHADER_SAMPLER_VIEWS];
    unsigned num_image_views[PIPE_SHADER_TYPES];
 
+   float line_width;
    float blend_constants[4];
 
    struct pipe_stencil_ref stencil_ref;

src/gallium/drivers/zink/zink_pipeline.c
@@ -85,12 +85,7 @@ zink_create_gfx_pipeline(struct zink_screen *screen,
    rast_state.depthBiasConstantFactor = 0.0;
    rast_state.depthBiasClamp = 0.0;
    rast_state.depthBiasSlopeFactor = 0.0;
-   if (screen->feats.wideLines)
-      rast_state.lineWidth = state->line_width;
-   else {
-      debug_printf("BUG: wide lines not supported, needs fallback!");
-      rast_state.lineWidth = 1.0f;
-   }
+   rast_state.lineWidth = 1.0f;
 
    VkPipelineDepthStencilStateCreateInfo depth_stencil_state = {};
    depth_stencil_state.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
@@ -105,11 +100,12 @@ zink_create_gfx_pipeline(struct zink_screen *screen,
    depth_stencil_state.depthWriteEnable = state->depth_stencil_alpha_state->depth_write;
 
    VkDynamicState dynamicStateEnables[] = {
-      VK_DYNAMIC_STATE_DEPTH_BIAS,
-      VK_DYNAMIC_STATE_SCISSOR,
-      VK_DYNAMIC_STATE_STENCIL_REFERENCE,
       VK_DYNAMIC_STATE_VIEWPORT,
+      VK_DYNAMIC_STATE_SCISSOR,
+      VK_DYNAMIC_STATE_LINE_WIDTH,
+      VK_DYNAMIC_STATE_DEPTH_BIAS,
       VK_DYNAMIC_STATE_BLEND_CONSTANTS,
+      VK_DYNAMIC_STATE_STENCIL_REFERENCE,
    };
 
    VkPipelineDynamicStateCreateInfo pipelineDynamicStateCreateInfo = {};

src/gallium/drivers/zink/zink_pipeline.h
@@ -49,8 +49,6 @@ struct zink_gfx_pipeline_state {
 
    struct zink_depth_stencil_alpha_state *depth_stencil_alpha_state;
 
-   float line_width;
-
    VkSampleMask sample_mask;
    uint8_t rast_samples;
 };

src/gallium/drivers/zink/zink_state.c
@@ -424,7 +424,7 @@ zink_bind_rasterizer_state(struct pipe_context *pctx, void *cso)
 
    if (ctx->rast_state) {
       ctx->gfx_pipeline_state.rast_state = &ctx->rast_state->hw_state;
-      ctx->gfx_pipeline_state.line_width = ctx->rast_state->line_width;
+      ctx->line_width = ctx->rast_state->line_width;
    }
 }
 

commit 90d51a94bb84a4dd898aa86d780076110beb4a32

zink: emit line-width when using polygon line-mode
When switching this to dynamic state, I forgot that this also needs to
be emitted when we use a polygon-mode set to lines.

Signed-off-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Fixes: bc671bf0b4c ("zink: use dynamic state for line-width")

src/gallium/drivers/zink/zink_context.c
@@ -1000,6 +1000,25 @@ get_gfx_program(struct zink_context *ctx)
    return ctx->curr_program;
 }
 
+static bool
+line_width_needed(enum pipe_prim_type reduced_prim,
+                  VkPolygonMode polygon_mode)
+{
+   switch (reduced_prim) {
+   case PIPE_PRIM_POINTS:
+      return false;
+
+   case PIPE_PRIM_LINES:
+      return true;
+
+   case PIPE_PRIM_TRIANGLES:
+      return polygon_mode == VK_POLYGON_MODE_LINE;
+
+   default:
+      unreachable("unexpected reduced prim");
+   }
+}
+
 static void
 zink_draw_vbo(struct pipe_context *pctx,
               const struct pipe_draw_info *dinfo)
@@ -1163,7 +1182,7 @@ zink_draw_vbo(struct pipe_context *pctx,
       vkCmdSetScissor(batch->cmdbuf, 0, 1, &fb_scissor);
    }
 
-   if (reduced_prim == PIPE_PRIM_LINES) {
+   if (line_width_needed(reduced_prim, rast_state->hw_state.polygon_mode)) {
       if (screen->feats.wideLines || ctx->line_width == 1.0f)
          vkCmdSetLineWidth(batch->cmdbuf, ctx->line_width);
       else