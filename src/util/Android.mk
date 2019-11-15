openharmony/third_party_mesa3d

commit bc5dec36ec2f45ffd012fe4b5aca725a8dd832aa

util: Move gallium's PIPE_FORMAT utils to /util/format/
To make PIPE_FORMATs usable from non-gallium parts of Mesa, I want to
move their helpers out of gallium.  Since u_format used
util_copy_rect(), I moved that in there, too.

I've put it in a separate directory in util/ because it's a big chunk
of related code, and it's not clear to me whether we might want it as
a separate library from libmesa_util at some point.

Closes: #1905
Acked-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Kristian H. Kristensen <hoegsberg@google.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>

src/util/Android.mk
@@ -68,6 +68,10 @@ LOCAL_GENERATED_SOURCES := $(UTIL_GENERATED_SOURCES)
 MESA_DRI_OPTIONS_H := $(intermediates)/xmlpool/options.h
 LOCAL_GENERATED_SOURCES += $(MESA_DRI_OPTIONS_H)
 
+
+$(intermediates)/format/u_format_table.c: $(intermediates)/%.c: $(LOCAL_PATH)/%.py $(LOCAL_PATH)/format/u_format.csv
+	$(transform-generated-source)
+
 #
 # Generate options.h from gettext translations.
 #

src/gallium/auxiliary/Android.mk
@@ -70,9 +70,6 @@ $(intermediates)/indices/u_unfilled_gen.c \
 $(intermediates)/util/u_format_srgb.c: $(intermediates)/%.c: $(LOCAL_PATH)/%.py
 	$(transform-generated-source)
 
-$(intermediates)/util/u_format_table.c: $(intermediates)/%.c: $(LOCAL_PATH)/%.py $(LOCAL_PATH)/util/u_format.csv
-	$(transform-generated-source)
-
 LOCAL_GENERATED_SOURCES += $(MESA_GEN_NIR_H)
 
 include $(GALLIUM_COMMON_MK)

src/gallium/drivers/radeonsi/Android.mk
@@ -63,7 +63,7 @@ $(intermediates)/radeonsi/si_driinfo.h: $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS)
 	$(hide) $(MESA_PYTHON2) $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS) > $@ || ($(RM) $@; false)
 
 GEN10_FORMAT_TABLE_INPUTS := \
-	$(MESA_TOP)/src/gallium/auxiliary/util/u_format.csv \
+	$(MESA_TOP)/util/format/u_format.csv \
 	$(MESA_TOP)/src/amd/registers/gfx10-rsrc.json
 
 GEN10_FORMAT_TABLE_DEP := \

commit 6cf40f36dca97cbe231a5be1cd8443b57e2783d2

android: util/format: fix include path list
To avoid following building error:

out/target/product/x86_64/obj_x86/STATIC_LIBRARIES/libmesa_util_intermediates/format/u_format_table.c:30:10:
fatal error: 'u_format.h' file not found
         ^~~~~~~~~~~~
1 error generated.

Fixes: bc5dec3 ("util: Move gallium's PIPE_FORMAT utils to /util/format/")
Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>

src/util/Android.mk
@@ -39,7 +39,8 @@ LOCAL_C_INCLUDES := \
 	$(MESA_TOP)/src/mesa \
 	$(MESA_TOP)/src/mapi \
 	$(MESA_TOP)/src/gallium/include \
-	$(MESA_TOP)/src/gallium/auxiliary
+	$(MESA_TOP)/src/gallium/auxiliary \
+	$(MESA_TOP)/src/util/format
 
 # If Android version >=8 MESA should static link libexpat else should dynamic link
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 27; echo $$?), 0)