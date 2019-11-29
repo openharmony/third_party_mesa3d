openharmony/third_party_mesa3d

commit f881f350b2c2e84abc9c6c8d4862ffa2a4e0f6bb

zink: heap-allocate samplers objects
VkSampler is 64-bit even on 32-bit systems, so casting it to a pointer
is a bad idea there. So let's heap-allocate the sampler-object instead.

Signed-off-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Closes: https://gitlab.freedesktop.org/mesa/mesa/issues/2017
Reviewed-by: Witold Baryluk <witold.baryluk@gmail.com>
Tested-by: Witold Baryluk <witold.baryluk@gmail.com>

src/gallium/drivers/zink/zink_context.c
@@ -162,10 +162,14 @@ zink_create_sampler_state(struct pipe_context *pctx,
       sci.anisotropyEnable = VK_TRUE;
    }
 
-   VkSampler sampler;
-   VkResult err = vkCreateSampler(screen->dev, &sci, NULL, &sampler);
-   if (err != VK_SUCCESS)
+   VkSampler *sampler = CALLOC(1, sizeof(VkSampler));
+   if (!sampler)
       return NULL;
+
+   if (vkCreateSampler(screen->dev, &sci, NULL, sampler) != VK_SUCCESS) {
+      FREE(sampler);
+      return NULL;
+   }
 
    return sampler;
 }
@@ -178,8 +182,10 @@ zink_bind_sampler_states(struct pipe_context *pctx,
                          void **samplers)
 {
    struct zink_context *ctx = zink_context(pctx);
-   for (unsigned i = 0; i < num_samplers; ++i)
-      ctx->samplers[shader][start_slot + i] = (VkSampler)samplers[i];
+   for (unsigned i = 0; i < num_samplers; ++i) {
+      VkSampler *sampler = samplers[i];
+      ctx->samplers[shader][start_slot + i] = sampler ? *sampler : VK_NULL_HANDLE;
+   }
    ctx->num_samplers[shader] = start_slot + num_samplers;
 }
 
@@ -188,8 +194,9 @@ zink_delete_sampler_state(struct pipe_context *pctx,
                           void *sampler_state)
 {
    struct zink_batch *batch = zink_curr_batch(zink_context(pctx));
-   util_dynarray_append(&batch->zombie_samplers,
-                        VkSampler, sampler_state);
+   util_dynarray_append(&batch->zombie_samplers, VkSampler,
+                        *(VkSampler *)sampler_state);
+   FREE(sampler_state);
 }
 
 

commit f29eba65a664a5460f4cbe4aff60d856e607bad7

zink: fix crash when restoring sampler-states
When I changed to heap-allocated sampler-objects, I missed the code-path
that restores sampler-states after the blitter; it needs an array of
pointers, not an array of VkSampler objects to behave.

This fixes spec@arb_texture_cube_map@copyteximage for me.

Signed-off-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Fixes: f881f350b2c ("zink: heap-allocate samplers objects")
Reviewed-by: Dave Airlie <airlied@redhat.com>

src/gallium/drivers/zink/zink_blit.c
@@ -171,7 +171,7 @@ zink_blit(struct pipe_context *pctx,
    util_blitter_save_scissor(ctx->blitter, ctx->scissor_states);
    util_blitter_save_fragment_sampler_states(ctx->blitter,
                                              ctx->num_samplers[PIPE_SHADER_FRAGMENT],
-                                             (void **)ctx->samplers[PIPE_SHADER_FRAGMENT]);
+                                             ctx->sampler_states[PIPE_SHADER_FRAGMENT]);
    util_blitter_save_fragment_sampler_views(ctx->blitter,
                                             ctx->num_image_views[PIPE_SHADER_FRAGMENT],
                                             ctx->image_views[PIPE_SHADER_FRAGMENT]);

src/gallium/drivers/zink/zink_context.c
@@ -173,6 +173,7 @@ zink_bind_sampler_states(struct pipe_context *pctx,
    struct zink_context *ctx = zink_context(pctx);
    for (unsigned i = 0; i < num_samplers; ++i) {
       VkSampler *sampler = samplers[i];
+      ctx->sampler_states[shader][start_slot + i] = sampler;
       ctx->samplers[shader][start_slot + i] = sampler ? *sampler : VK_NULL_HANDLE;
    }
    ctx->num_samplers[shader] = start_slot + num_samplers;

src/gallium/drivers/zink/zink_context.h
@@ -97,6 +97,7 @@ struct zink_context {
    struct pipe_vertex_buffer buffers[PIPE_MAX_ATTRIBS];
    uint32_t buffers_enabled_mask;
 
+   void *sampler_states[PIPE_SHADER_TYPES][PIPE_MAX_SAMPLERS];
    VkSampler samplers[PIPE_SHADER_TYPES][PIPE_MAX_SAMPLERS];
    unsigned num_samplers[PIPE_SHADER_TYPES];
    struct pipe_sampler_view *image_views[PIPE_SHADER_TYPES][PIPE_MAX_SHADER_SAMPLER_VIEWS];