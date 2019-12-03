openharmony/third_party_mesa3d

commit 85be8f2f9889b7a415afb08b29a2b29850328f3f

ac/llvm: fix pointer type for global atomics
Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

src/amd/llvm/ac_nir_to_llvm.c
@@ -3022,6 +3022,12 @@ static LLVMValueRef visit_var_atomic(struct ac_nir_context *ctx,
 
 	const char *sync_scope = LLVM_VERSION_MAJOR >= 9 ? "workgroup-one-as" : "workgroup";
 
+	nir_deref_instr *deref = nir_instr_as_deref(instr->src[0].ssa->parent_instr);
+	if (deref->mode == nir_var_mem_global) {
+		LLVMTypeRef ptr_type = LLVMPointerType(LLVMTypeOf(src), LLVMGetPointerAddressSpace(LLVMTypeOf(ptr)));
+		ptr = LLVMBuildBitCast(ctx->ac.builder, ptr, ptr_type , "");
+	}
+
 	if (instr->intrinsic == nir_intrinsic_shared_atomic_comp_swap ||
 	    instr->intrinsic == nir_intrinsic_deref_atomic_comp_swap) {
 		LLVMValueRef src1 = get_src(ctx, instr->src[src_idx + 1]);

commit 3bd6401ff9839e2bc74204744ec644e926596602

ac/llvm: fix atomic var operations if source isn't a deref
Fixes some CTS regressions.

Fixes: 85be8f2f988 ("ac/llvm: fix pointer type for global atomics")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

src/amd/llvm/ac_nir_to_llvm.c
@@ -3022,13 +3022,15 @@ static LLVMValueRef visit_var_atomic(struct ac_nir_context *ctx,
 
 	const char *sync_scope = LLVM_VERSION_MAJOR >= 9 ? "workgroup-one-as" : "workgroup";
 
-	nir_deref_instr *deref = nir_instr_as_deref(instr->src[0].ssa->parent_instr);
-	if (deref->mode == nir_var_mem_global) {
-		/* use "singlethread" sync scope to implement relaxed ordering */
-		sync_scope = LLVM_VERSION_MAJOR >= 9 ? "singlethread-one-as" : "singlethread";
+	if (instr->src[0].ssa->parent_instr->type == nir_instr_type_deref) {
+		nir_deref_instr *deref = nir_instr_as_deref(instr->src[0].ssa->parent_instr);
+		if (deref->mode == nir_var_mem_global) {
+			/* use "singlethread" sync scope to implement relaxed ordering */
+			sync_scope = LLVM_VERSION_MAJOR >= 9 ? "singlethread-one-as" : "singlethread";
 
-		LLVMTypeRef ptr_type = LLVMPointerType(LLVMTypeOf(src), LLVMGetPointerAddressSpace(LLVMTypeOf(ptr)));
-		ptr = LLVMBuildBitCast(ctx->ac.builder, ptr, ptr_type , "");
+			LLVMTypeRef ptr_type = LLVMPointerType(LLVMTypeOf(src), LLVMGetPointerAddressSpace(LLVMTypeOf(ptr)));
+			ptr = LLVMBuildBitCast(ctx->ac.builder, ptr, ptr_type , "");
+		}
 	}
 
 	if (instr->intrinsic == nir_intrinsic_shared_atomic_comp_swap ||