openharmony/third_party_mesa3d

commit 8184dd21ce3862112064c19fbff2d7395b1a78ba

gallivm: add 64-bit const int creator.
Acked-by: Roland Scheidegger <sroland@vmware.com>

src/gallium/auxiliary/gallivm/lp_bld_const.h
@@ -126,6 +126,11 @@ lp_build_const_int32(struct gallivm_state *gallivm, int i)
    return LLVMConstInt(LLVMInt32TypeInContext(gallivm->context), i, 0);
 }
 
+static inline LLVMValueRef
+lp_build_const_int64(struct gallivm_state *gallivm, int i)
+{
+   return LLVMConstInt(LLVMInt64TypeInContext(gallivm->context), i, 0);
+}
 
 static inline LLVMValueRef
 lp_build_const_float(struct gallivm_state *gallivm, float x)

commit 23034e5796cb9494270bb4f326cf54c562262292

gallivm: fixup const int64 builder.
Pointed out by Ilia.

Fixes: 8184dd21ce38 (gallivm: add 64-bit const int creator.)
Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>
Acked-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>

src/gallium/auxiliary/gallivm/lp_bld_const.h
@@ -127,7 +127,7 @@ lp_build_const_int32(struct gallivm_state *gallivm, int i)
 }
 
 static inline LLVMValueRef
-lp_build_const_int64(struct gallivm_state *gallivm, int i)
+lp_build_const_int64(struct gallivm_state *gallivm, int64_t i)
 {
    return LLVMConstInt(LLVMInt64TypeInContext(gallivm->context), i, 0);
 }