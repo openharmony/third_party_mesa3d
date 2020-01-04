openharmony/third_party_mesa3d

commit 59118731a61a6025936fbffb07f12a24463b0d1d

radv: return the correct pitch for linear mipmaps on GFX10
On GFX9, the pitch of a level is always the pitch of the entire image
but not on GFX10.

This fixes graphics glithes with Halo - The Master Chief Collection.

Closes: https://gitlab.freedesktop.org/mesa/mesa/issues/2188
CC: <mesa-stable@lists.freedesktop.org>
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

src/amd/vulkan/radv_image.c
@@ -1889,7 +1889,7 @@ void radv_GetImageSubresourceLayout(
 			pLayout->rowPitch = surface->u.gfx9.surf_pitch * surface->bpe / 3;
 		} else {
 			assert(util_is_power_of_two_nonzero(surface->bpe));
-			pLayout->rowPitch = surface->u.gfx9.surf_pitch * surface->bpe;
+			pLayout->rowPitch = surface->u.gfx9.pitch[level] * surface->bpe;
 		}
 
 		pLayout->arrayPitch = surface->u.gfx9.surf_slice_size;

src/amd/common/ac_surface.c
@@ -1062,8 +1062,10 @@ static int gfx9_compute_miptree(ADDR_HANDLE addrlib,
 	surf->surf_alignment = out.baseAlign;
 
 	if (in->swizzleMode == ADDR_SW_LINEAR) {
-		for (unsigned i = 0; i < in->numMipLevels; i++)
+		for (unsigned i = 0; i < in->numMipLevels; i++) {
 			surf->u.gfx9.offset[i] = mip_info[i].offset;
+			surf->u.gfx9.pitch[i] = mip_info[i].pitch;
+		}
 	}
 
 	if (in->flags.depth) {

src/amd/common/ac_surface.h
@@ -154,6 +154,8 @@ struct gfx9_surf_layout {
     uint64_t                    surf_slice_size;
     /* Mipmap level offset within the slice in bytes. Only valid for LINEAR. */
     uint32_t                    offset[RADEON_SURF_MAX_LEVELS];
+    /* Mipmap level pitch in elements. Only valid for LINEAR. */
+    uint32_t                    pitch[RADEON_SURF_MAX_LEVELS];
 
     uint64_t                    stencil_offset; /* separate stencil */
 

commit a70d64e8752371b36bdf96458e31e68c3a12893a

radv: Only use the gfx mipmap level offset/pitch for linear textures.
The tiled-case is non-sensical for non-base mips, but Vulkan requires
that this function handles it but at the same time does not require
returning anything useful. So we can basically return anything.

Correct tiled pitch and offset are still required for our own WSI and
in the future getting the layouts of images with DRM format modifiers.
Both don't have to deal with images with more than 1 level though.

Fixes: 59118731a61 "radv: return the correct pitch for linear mipmaps on GFX10"
Closes: https://gitlab.freedesktop.org/mesa/mesa/issues/2301
Closes: https://gitlab.freedesktop.org/mesa/mesa/issues/2304
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>

src/amd/vulkan/radv_image.c
@@ -1878,7 +1878,9 @@ void radv_GetImageSubresourceLayout(
 	struct radeon_surf *surface = &plane->surface;
 
 	if (device->physical_device->rad_info.chip_class >= GFX9) {
-		pLayout->offset = plane->offset + surface->u.gfx9.offset[level] + surface->u.gfx9.surf_slice_size * layer;
+		uint64_t level_offset = surface->is_linear ? surface->u.gfx9.offset[level] : 0;
+		
+		pLayout->offset = plane->offset + level_offset + surface->u.gfx9.surf_slice_size * layer;
 		if (image->vk_format == VK_FORMAT_R32G32B32_UINT ||
 		    image->vk_format == VK_FORMAT_R32G32B32_SINT ||
 		    image->vk_format == VK_FORMAT_R32G32B32_SFLOAT) {
@@ -1888,8 +1890,10 @@ void radv_GetImageSubresourceLayout(
 			 */
 			pLayout->rowPitch = surface->u.gfx9.surf_pitch * surface->bpe / 3;
 		} else {
+			uint32_t pitch = surface->is_linear ? surface->u.gfx9.pitch[level] : surface->u.gfx9.surf_pitch;
+
 			assert(util_is_power_of_two_nonzero(surface->bpe));
-			pLayout->rowPitch = surface->u.gfx9.pitch[level] * surface->bpe;
+			pLayout->rowPitch = pitch * surface->bpe;
 		}
 
 		pLayout->arrayPitch = surface->u.gfx9.surf_slice_size;