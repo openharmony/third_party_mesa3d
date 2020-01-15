openharmony/third_party_mesa3d

commit c02bb8f54e889a5f55848d7eab3aa796b636003b

gitlab-ci: Switch LAVA jobs to use shared dEQP runner
Take one step towards sharing code between the LAVA and non-LAVA jobs,
with the goals of reducing maintenance burden and use of computational
resources.

The env var DEQP_NO_SAVE_RESULTS allows us to skip the procesing of the
XML result files, which can take a long time and is not useful in the
LAVA case as we are not uploading artifacts anywhere at the moment.

Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Reviewed-by: Eric Anholt <eric@anholt.net>

.gitlab-ci/deqp-runner.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
 set -ex
 
-DEQP_OPTIONS=(--deqp-surface-width=256 --deqp-surface-height=256)
-DEQP_OPTIONS+=(--deqp-surface-type=pbuffer)
-DEQP_OPTIONS+=(--deqp-gl-config-name=rgba8888d24s8ms0)
-DEQP_OPTIONS+=(--deqp-visibility=hidden)
+DEQP_OPTIONS="--deqp-surface-width=256 --deqp-surface-height=256"
+DEQP_OPTIONS="$DEQP_OPTIONS --deqp-surface-type=pbuffer"
+DEQP_OPTIONS="$DEQP_OPTIONS --deqp-gl-config-name=rgba8888d24s8ms0"
+DEQP_OPTIONS="$DEQP_OPTIONS --deqp-visibility=hidden"
 
 # It would be nice to be able to enable the watchdog, so that hangs in a test
 # don't need to wait the full hour for the run to time out.  However, some
 # shaders end up taking long enough to compile
 # (dEQP-GLES31.functional.ubo.random.all_per_block_buffers.20 for example)
 # that they'll sporadically trigger the watchdog.
-#DEQP_OPTIONS+=(--deqp-watchdog=enable)
+#DEQP_OPTIONS="$DEQP_OPTIONS --deqp-watchdog=enable"
 
 if [ -z "$DEQP_VER" ]; then
    echo 'DEQP_VER must be set to something like "gles2", "gles31" or "vk" for the test run'
    exit 1
 fi
 
-if [ "$DEQP_VER" == "vk" ]; then
+if [ "$DEQP_VER" = "vk" ]; then
    if [ -z "$VK_DRIVER" ]; then
       echo 'VK_DRIVER must be to something like "radeon" or "intel" for the test run'
       exit 1
@@ -46,7 +46,7 @@ RESULTS=`pwd`/results
 mkdir -p $RESULTS
 
 # Generate test case list file.
-if [ "$DEQP_VER" == "vk" ]; then
+if [ "$DEQP_VER" = "vk" ]; then
    cp /deqp/mustpass/vk-master.txt /tmp/case-list.txt
    DEQP=/deqp/external/vulkancts/modules/vulkan/deqp-vk
 else
@@ -83,8 +83,9 @@ run_cts() {
         $XFAIL \
         --job ${DEQP_PARALLEL:-1} \
 	--allow-flakes true \
+	$DEQP_RUNNER_OPTIONS \
         -- \
-        "${DEQP_OPTIONS[@]}"
+        $DEQP_OPTIONS
 }
 
 report_flakes() {
@@ -199,8 +200,10 @@ if [ $DEQP_EXITCODE -ne 0 ]; then
         $RESULTS/cts-runner-unexpected-results.txt
     head -n 50 $RESULTS/cts-runner-unexpected-results.txt
 
-    # Save the logs for up to the first 50 unexpected results:
-    head -n 50 $RESULTS/cts-runner-unexpected-results.txt | quiet extract_xml_results /tmp/*.qpa
+    if [ -z "$DEQP_NO_SAVE_RESULTS" ]; then
+        # Save the logs for up to the first 50 unexpected results:
+        head -n 50 $RESULTS/cts-runner-unexpected-results.txt | quiet extract_xml_results /tmp/*.qpa
+    fi
 
     count=`cat $RESULTS/cts-runner-unexpected-results.txt | wc -l`
 
@@ -217,8 +220,10 @@ else
         echo "Some flakes found (see cts-runner-flakes.txt in artifacts for full results):"
         head -n 50 $RESULTS/cts-runner-flakes.txt
 
-        # Save the logs for up to the first 50 flakes:
-        head -n 50 $RESULTS/cts-runner-flakes.txt | quiet extract_xml_results /tmp/*.qpa
+        if [ -z "$DEQP_NO_SAVE_RESULTS" ]; then
+            # Save the logs for up to the first 50 flakes:
+            head -n 50 $RESULTS/cts-runner-flakes.txt | quiet extract_xml_results /tmp/*.qpa
+        fi
 
         # Report the flakes to IRC channel for monitoring (if configured):
         quiet report_flakes $RESULTS/cts-runner-flakes.txt

.gitlab-ci.yml
@@ -109,13 +109,13 @@ x86_build:
 x86_test-gl:
   extends: x86_build
   variables:
-    DEBIAN_TAG: &x86_test-gl "2019-12-05"
+    DEBIAN_TAG: &x86_test-gl "2019-12-18"
 
 # Debian 10 based x86 test image for VK
 x86_test-vk:
   extends: x86_build
   variables:
-    DEBIAN_TAG: &x86_test-vk "2019-11-22"
+    DEBIAN_TAG: &x86_test-vk "2019-12-18"
   # Can only be triggered manually on personal branches because RADV is the only
   # driver that does Vulkan testing at the moment.
   rules:
@@ -162,7 +162,7 @@ arm_build:
 arm_test:
   extends: arm_build
   variables:
-    DEBIAN_TAG: &arm_test "2019-11-22"
+    DEBIAN_TAG: &arm_test "2019-12-18"
 
 .use-arm_test:
   variables:

.gitlab-ci/build-cts-runner.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-git clone https://gitlab.freedesktop.org/mesa/parallel-deqp-runner.git --depth 1 -b mesa-ci-2019-11-22
+git clone https://gitlab.freedesktop.org/mesa/parallel-deqp-runner.git --depth 1 -b mesa-ci-2019-12-17
 cd parallel-deqp-runner
-meson build/
+meson build/ $EXTRA_MESON_ARGS
 ninja -C build -j4 install
 cd ..
 rm -rf parallel-deqp-runner

.gitlab-ci/build-deqp-gl.sh
@@ -29,6 +29,7 @@ cd /deqp
 cmake -G Ninja \
       -DDEQP_TARGET=surfaceless               \
       -DCMAKE_BUILD_TYPE=Release              \
+      $EXTRA_CMAKE_ARGS                       \
       /VK-GL-CTS
 ninja
 
@@ -53,6 +54,6 @@ rm -rf /deqp/execserver
 rm -rf /deqp/modules/egl
 rm -rf /deqp/framework
 find -iname '*cmake*' -o -name '*ninja*' -o -name '*.o' -o -name '*.a' | xargs rm -rf
-strip modules/*/deqp-*
+${STRIP_CMD:-strip} modules/*/deqp-*
 du -sh *
 rm -rf /VK-GL-CTS

.gitlab-ci/container/lava_arm.sh
@@ -33,13 +33,6 @@ apt-get -y install --no-install-recommends \
 	procps \
 	qemu-user-static \
 	cpio \
-	clang-8 \
-	llvm-8 \
-	libclang-8-dev \
-	llvm-8-dev \
-	gdc-9 \
-	lld-8 \
-	nasm \
 	libegl1-mesa-dev \
 	\
 	libdrm-dev:${DEBIAN_ARCH} \
@@ -66,77 +59,25 @@ apt-get -y install --no-install-recommends \
 	libglvnd-core-dev:${DEBIAN_ARCH} \
 	libgles2-mesa-dev:${DEBIAN_ARCH} \
 	libegl1-mesa-dev:${DEBIAN_ARCH} \
-	libpng-dev:${DEBIAN_ARCH}
-
-############### Cross-build dEQP
-mkdir -p /artifacts/rootfs/deqp
-
-git config --global user.email "mesa@example.com"
-git config --global user.name "Mesa CI"
-# XXX: Use --depth 1 once we can drop the cherry-picks.
-git clone \
-    https://github.com/KhronosGroup/VK-GL-CTS.git \
-    -b opengl-es-cts-3.2.5.1 \
-    /VK-GL-CTS
-cd /VK-GL-CTS
-# Fix surfaceless build
-git cherry-pick -x 22f41e5e321c6dcd8569c4dad91bce89f06b3670
-git cherry-pick -x 1daa8dff73161ea60ead965bd6c9f2a0a2165648
-
-# surfaceless links against libkms and such despite not using it.
-sed -i '/gbm/d' targets/surfaceless/surfaceless.cmake
-sed -i '/libkms/d' targets/surfaceless/surfaceless.cmake
-sed -i '/libgbm/d' targets/surfaceless/surfaceless.cmake
-
-python3 external/fetch_sources.py
-
-cd /artifacts/rootfs/deqp
-cmake -G Ninja                                \
-      -DDEQP_TARGET=surfaceless               \
-      -DCMAKE_BUILD_TYPE=Release              \
-      -DCMAKE_C_COMPILER=${GCC_ARCH}-gcc      \
-      -DCMAKE_CXX_COMPILER=${GCC_ARCH}-g++    \
-      /VK-GL-CTS
-ninja
-rm -rf /artifacts/rootfs/deqp/external
-rm -rf /artifacts/rootfs/deqp/modules/gles31
-rm -rf /artifacts/rootfs/deqp/modules/internal
-rm -rf /artifacts/rootfs/deqp/executor
-rm -rf /artifacts/rootfs/deqp/execserver
-rm -rf /artifacts/rootfs/deqp/modules/egl
-rm -rf /artifacts/rootfs/deqp/framework
-find . -name CMakeFiles | xargs rm -rf
-find . -name lib\*.a | xargs rm -rf
-du -sh *
-rm -rf /VK-GL-CTS
+	libpng-dev:${DEBIAN_ARCH} \
+	libvulkan-dev:${DEBIAN_ARCH} \
+	libvulkan1:${DEBIAN_ARCH} \
+	libclang-7-dev:${DEBIAN_ARCH}
 
 
-############### Cross-build Volt dEQP runner
-mkdir -p /battery
-cd /battery
-wget https://github.com/VoltLang/Battery/releases/download/v0.1.23/battery-0.1.23-x86_64-linux.tar.gz
-tar xzvf battery-0.1.23-x86_64-linux.tar.gz
-rm battery-0.1.23-x86_64-linux.tar.gz
-mv battery /usr/local/bin
-rm -rf /battery
-
-mkdir -p /volt
-cd /volt
-mkdir -p Watt Volta dEQP
-wget -qO- https://github.com/VoltLang/Watt/archive/v0.1.3.tar.gz | tar -xz --strip-components=1 -C ./Watt
-wget -qO- https://github.com/VoltLang/Volta/archive/v0.1.3.tar.gz | tar -xz --strip-components=1 -C ./Volta
-wget -qO- https://github.com/Wallbraker/dEQP/archive/v0.1.4.tar.gz | tar -xz --strip-components=1 -C ./dEQP
-battery config --release --lto Volta Watt
-battery build
-battery config --arch ${VOLT_ARCH} --cmd-volta Volta/volta Volta/rt Watt dEQP
-battery build
-rm /usr/local/bin/battery
-cp dEQP/deqp /artifacts/rootfs/deqp/deqp-volt
-rm -rf /volt
+############### Build dEQP runner
+/usr/share/meson/debcrossgen --arch ${DEBIAN_ARCH} -o /tmp/cross_file.txt
+EXTRA_MESON_ARGS="--cross-file /tmp/cross_file.txt"
+. .gitlab-ci/build-cts-runner.sh
+mkdir -p /artifacts/rootfs/usr/bin
+mv /usr/local/bin/deqp-runner /artifacts/rootfs/usr/bin/.
 
 
-############### Remove LLVM now, so the container image is smaller
-apt-get -y remove \*llvm\*
+############### Build dEQP
+EXTRA_CMAKE_ARGS="-DCMAKE_C_COMPILER=${GCC_ARCH}-gcc -DCMAKE_CXX_COMPILER=${GCC_ARCH}-g++"
+STRIP_CMD="${GCC_ARCH}-strip"
+. .gitlab-ci/build-deqp-gl.sh
+mv /deqp /artifacts/rootfs/.
 
 
 ############### Cross-build kernel

.gitlab-ci/generate_lava.py
@@ -70,6 +70,9 @@ for device_type in args.device_types:
     values['boot_method'] = device_types[device_type]['boot_method']
     values['kernel_image_type'] = device_types[device_type]['kernel_image_type']
     values['tags'] = device_types[device_type]['tags']
+    values['fails_file'] = 'deqp-%s-fails.txt' % device_types[device_type]['gpu_version']
+    values['skips_file'] = 'deqp-%s-skips.txt' % device_types[device_type]['gpu_version']
+    values['deqp_version'] = 'gles2'
 
     f = open('results/lava-deqp-%s.yml' % device_type, "w")
     f.write(template.render(values))

.gitlab-ci/lava-deqp-runner.sh
@@ -1,51 +1,0 @@
-#!/bin/sh
-
-GPU_VERSION="$1"
-
-DEQP_OPTIONS="--deqp-surface-width=256 --deqp-surface-height=256"
-DEQP_OPTIONS="$DEQP_OPTIONS --deqp-visibility=hidden"
-DEQP_OPTIONS="$DEQP_OPTIONS --deqp-log-images=disable"
-DEQP_OPTIONS="$DEQP_OPTIONS --deqp-watchdog=enable"
-DEQP_OPTIONS="$DEQP_OPTIONS --deqp-crashhandler=enable"
-DEQP_OPTIONS="$DEQP_OPTIONS --deqp-surface-type=pbuffer"
-
-export LIBGL_DRIVERS_PATH=/mesa/lib/dri/
-export LD_LIBRARY_PATH=/mesa/lib/
-export MESA_GLES_VERSION_OVERRIDE=3.0
-
-DEVFREQ_GOVERNOR=`echo /sys/devices/platform/*.gpu/devfreq/devfreq0/governor`
-echo performance > $DEVFREQ_GOVERNOR
-
-cd /deqp/modules/gles2
-
-# Generate test case list file
-./deqp-gles2 $DEQP_OPTIONS --deqp-runmode=stdout-caselist | grep "TEST: dEQP-GLES2" | cut -d ' ' -f 2 > /tmp/case-list.txt
-
-# Note: not using sorted input and comm, becuase I want to run the tests in
-# the same order that dEQP would.
-while read -r line; do
-   if echo "$line" | grep -q '^[^#]'; then
-       sed -i "/$line/d" /tmp/case-list.txt
-   fi
-done < /deqp/deqp-$GPU_VERSION-skips.txt
-
-/deqp/deqp-volt --cts-build-dir=/deqp \
-                --threads=8 \
-                --test-names-file=/tmp/case-list.txt \
-                --results-file=/tmp/results.txt \
-                --no-passed-results \
-                --regression-file=/deqp/deqp-$GPU_VERSION-fails.txt \
-                --no-rerun-tests \
-                --print-regression \
-                --no-print-fail \
-                --no-print-quality \
-                --no-colour-term \
-                 $DEQP_OPTIONS
-
-if [ $? -ne 0 ]; then
-    echo "Regressions detected"
-    echo "deqp: fail"
-else
-    echo "No regressions detected"
-    echo "deqp: pass"
-fi

.gitlab-ci/lava-deqp.yml.jinja2
@@ -57,8 +57,29 @@ actions:
           - mount -t devtmpfs none /dev
           - mkdir -p /dev/pts
           - mount -t devpts devpts /dev/pts
-          - sh /deqp/lava-deqp-runner.sh {{ gpu_version }}
-          - cat /proc/loadavg
+
+          - export MESA_GLES_VERSION_OVERRIDE=3.0
+          - export DEQP_PARALLEL=6
+          - export DEQP_NO_SAVE_RESULTS=1
+          - 'export DEQP_RUNNER_OPTIONS="--compact-display false --shuffle false"'
+          - export DEQP_EXPECTED_FAILS={{ fails_file }}
+          - export DEQP_SKIPS={{ skips_file }}
+          - export DEQP_VER={{ deqp_version }}
+          - export LIBGL_DRIVERS_PATH=`pwd`/install/lib/dri
+
+          # Put stuff where the runner script expects it
+          - mkdir artifacts
+          - mkdir results
+          - mkdir -p install/lib
+          - cp /deqp/$DEQP_EXPECTED_FAILS artifacts/.
+          - cp /deqp/$DEQP_SKIPS artifacts/.
+          - mv /mesa/lib/* install/lib/.
+
+          - "if sh /deqp/deqp-runner.sh; then
+                  echo 'deqp: pass';
+             else
+                  echo 'deqp: fail';
+             fi"
         parse:
           pattern: '(?P<test_case_id>\S*):\s+(?P<result>(pass|fail))'
       from: inline

commit 5cf2e0a7a4f6eb8646f0734681a9b72bf56ad8de

gitlab-ci: fix missing caselist.css/xsl
My best guess is that this was broken by c02bb8f5

Signed-off-by: Rob Clark <robdclark@chromium.org>
Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3413>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3413>

.gitlab-ci/deqp-runner.sh
@@ -133,7 +133,8 @@ extract_xml_result() {
                         /deqp/executor/testlog-to-xml $dst "$RESULTS/$testcase.xml"
                         # copy the stylesheets here so they only end up in artifacts
                         # if we have one or more result xml in artifacts
-                        cp /deqp/testlog.{css,xsl} "$RESULTS/"
+                        cp /deqp/testlog.css "$RESULTS/"
+                        cp /deqp/testlog.xsl "$RESULTS/"
                         return 0
                     fi
                     echo $line >> $dst