openharmony/third_party_mesa3d

commit fd81ae73300c0769525efc5bb20f9c92e24cc8f8

panfrost: Add more info to some assertions
Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3625>

src/gallium/drivers/panfrost/pan_format.c
@@ -98,8 +98,10 @@ panfrost_translate_channel_width(unsigned size)
                 return MALI_CHANNEL_16;
         case 32:
                 return MALI_CHANNEL_32;
-        default:
-                unreachable("Invalid width");
+        default: {
+                printf ("Invalid width: %d\n", size);
+                assert(0);
+        }
         }
 }
 

src/panfrost/midgard/midgard_compile.c
@@ -1672,7 +1672,7 @@ emit_intrinsic(compiler_context *ctx, nir_intrinsic_instr *instr)
                 break;
 
         default:
-                printf ("Unhandled intrinsic\n");
+                printf ("Unhandled intrinsic: %d\n", instr->intrinsic);
                 assert(0);
                 break;
         }
@@ -1925,8 +1925,10 @@ emit_texop_native(compiler_context *ctx, nir_tex_instr *instr,
                         break;
                 }
 
-                default:
-                        unreachable("Unknown texture source type\n");
+                default: {
+                        printf ("Unknown texture source type: %d\n", instr->src[i].src_type);
+                        assert(0);
+                }
                 }
         }
 
@@ -1953,8 +1955,10 @@ emit_tex(compiler_context *ctx, nir_tex_instr *instr)
         case nir_texop_txs:
                 emit_sysval_read(ctx, &instr->instr, ~0, 4);
                 break;
-        default:
-                unreachable("Unhanlded texture op");
+        default: {
+                printf ("Unhandled texture op: %d\n", instr->op);
+                assert(0);
+        }
         }
 }
 

commit e09aa32ccfdf868cd406c305f2c262b31b5ba8db

panfrost: Fix non-debug builds
For non-debug builds, where assertions are compiled out, GCC complains
about the end of the non-void function panfrost_translate_channel_width
being reached.

Fixes: fd81ae73300 ("panfrost: Add more info to some assertions")
Reported-by: Piotr Oniszczuk
Suggested-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3665>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3665>

src/gallium/drivers/panfrost/pan_format.c
@@ -101,6 +101,7 @@ panfrost_translate_channel_width(unsigned size)
         default: {
                 fprintf(stderr, "Invalid width: %d\n", size);
                 assert(0);
+                return 0;
         }
         }
 }