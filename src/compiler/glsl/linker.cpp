openharmony/third_party_mesa3d

commit 5185eff342e9631fcad89f33897f43f8b00797fe

st/glsl_to_nir: use nir based program resource list builder
Here we use the NIR based builder to add everything to the resource
list execpt for SSO packed varyings. Since the details of those
varyings get lost during packing we leave the special handing to
the GLSL IR pass for now. In order to do this we add some bools
to the build resource list functions.

Using the NIR based resource list builder gets us a step closer to
using a native NIR based linker. It should also be faster than the
GLSL IR builder, one because the NIR optimisations should mean we
add less entries due to better optimisations, and two because nir
gives us better lists to work with and we don't need to walk the
entire IR to find the resources.

Ack-by: Alejandro Piñeiro <apinheiro@igalia.com>

src/compiler/glsl/linker.cpp
@@ -4124,7 +4124,8 @@ add_fragdata_arrays(const struct gl_context *ctx,
  */
 void
 build_program_resource_list(struct gl_context *ctx,
-                            struct gl_shader_program *shProg)
+                            struct gl_shader_program *shProg,
+                            bool add_packed_varyings_only)
 {
    /* Rebuild resource list. */
    if (shProg->data->ProgramResourceList) {
@@ -4163,6 +4164,9 @@ build_program_resource_list(struct gl_context *ctx,
                                output_stage, GL_PROGRAM_OUTPUT))
          return;
    }
+
+   if (add_packed_varyings_only)
+      return;
 
    if (!add_fragdata_arrays(ctx, shProg, resource_set))
       return;

src/compiler/glsl/gl_nir_linker.c
@@ -397,10 +397,11 @@ add_interface_variables(const struct gl_context *ctx,
  */
 void
 nir_build_program_resource_list(struct gl_context *ctx,
-                                struct gl_shader_program *prog)
+                                struct gl_shader_program *prog,
+                                bool rebuild_resourse_list)
 {
    /* Rebuild resource list. */
-   if (prog->data->ProgramResourceList) {
+   if (prog->data->ProgramResourceList && rebuild_resourse_list) {
       ralloc_free(prog->data->ProgramResourceList);
       prog->data->ProgramResourceList = NULL;
       prog->data->NumProgramResourceList = 0;

src/compiler/glsl/gl_nir_linker.h
@@ -47,7 +47,8 @@ void gl_nir_set_uniform_initializers(struct gl_context *ctx,
                                      struct gl_shader_program *prog);
 
 void nir_build_program_resource_list(struct gl_context *ctx,
-                                     struct gl_shader_program *prog);
+                                     struct gl_shader_program *prog,
+                                     bool rebuild_resourse_list);
 
 void gl_nir_link_assign_atomic_counter_resources(struct gl_context *ctx,
                                                  struct gl_shader_program *prog);

src/compiler/glsl/program.h
@@ -46,7 +46,8 @@ link_shaders(struct gl_context *ctx, struct gl_shader_program *prog);
 
 extern void
 build_program_resource_list(struct gl_context *ctx,
-                            struct gl_shader_program *shProg);
+                            struct gl_shader_program *shProg,
+                            bool add_packed_varyings_only);
 
 extern long
 parse_program_resource_name(const GLchar *name,

src/mesa/drivers/dri/i965/brw_link.cpp
@@ -384,9 +384,9 @@ brw_link_shader(struct gl_context *ctx, struct gl_shader_program *shProg)
 
    /* SPIR-V programs build its resource list from linked NIR shaders. */
    if (!shProg->data->spirv)
-      build_program_resource_list(ctx, shProg);
+      build_program_resource_list(ctx, shProg, false);
    else
-      nir_build_program_resource_list(ctx, shProg);
+      nir_build_program_resource_list(ctx, shProg, true);
 
    for (stage = 0; stage < ARRAY_SIZE(shProg->_LinkedShaders); stage++) {
       struct gl_linked_shader *shader = shProg->_LinkedShaders[stage];

src/mesa/program/ir_to_mesa.cpp
@@ -3127,7 +3127,7 @@ _mesa_ir_link_shader(struct gl_context *ctx, struct gl_shader_program *prog)
       }
    }
 
-   build_program_resource_list(ctx, prog);
+   build_program_resource_list(ctx, prog, false);
    return prog->data->LinkStatus;
 }
 

src/mesa/state_tracker/st_glsl_to_ir.cpp
@@ -167,7 +167,7 @@ st_link_shader(struct gl_context *ctx, struct gl_shader_program *prog)
       validate_ir_tree(ir);
    }
 
-   build_program_resource_list(ctx, prog);
+   build_program_resource_list(ctx, prog, use_nir);
 
    if (use_nir)
       return st_link_nir(ctx, prog);

src/mesa/state_tracker/st_glsl_to_nir.cpp
@@ -699,7 +699,7 @@ st_link_nir(struct gl_context *ctx,
       if (!gl_nir_link(ctx, shader_program, &opts))
          return GL_FALSE;
 
-      nir_build_program_resource_list(ctx, shader_program);
+      nir_build_program_resource_list(ctx, shader_program, true);
 
       for (unsigned i = 0; i < num_shaders; i++) {
          struct gl_linked_shader *shader = linked_shader[i];
@@ -720,6 +720,9 @@ st_link_nir(struct gl_context *ctx,
       st_nir_link_shaders(linked_shader[i]->Program->nir,
                           linked_shader[i + 1]->Program->nir);
    }
+
+   if (!shader_program->data->spirv)
+      nir_build_program_resource_list(ctx, shader_program, false);
 
    for (unsigned i = 0; i < num_shaders; i++) {
       struct gl_linked_shader *shader = linked_shader[i];

commit 6e9b032b44b0d77b13e537b9b91a8dc2525f8535

glsl: fix a memory leak with resource_set
   ==7265== 248 (120 direct, 128 indirect) bytes in 1 blocks are definitely lost in loss record 1,438 of 1,465
   ==7265==    at 0x483980B: malloc (vg_replace_malloc.c:309)
   ==7265==    by 0x598A2AB: ralloc_size (ralloc.c:119)
   ==7265==    by 0x598F861: _mesa_set_create (set.c:127)
   ==7265==    by 0x599079D: _mesa_pointer_set_create (set.c:570)
   ==7265==    by 0x58BD7D1: build_program_resource_list(gl_context*, gl_shader_program*, bool) (linker.cpp:4026)
   ==7265==    by 0x548231B: st_link_shader (st_glsl_to_ir.cpp:170)
   ==7265==    by 0x54DA269: _mesa_glsl_link_shader (ir_to_mesa.cpp:3119)

Fixes: 5185eff3 ("st/glsl_to_nir: use nir based program resource list builder")
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3574>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3574>

src/compiler/glsl/linker.cpp
@@ -4040,8 +4040,10 @@ build_program_resource_list(struct gl_context *ctx,
          return;
    }
 
-   if (add_packed_varyings_only)
+   if (add_packed_varyings_only) {
+      _mesa_set_destroy(resource_set, NULL);
       return;
+   }
 
    if (!add_fragdata_arrays(ctx, shProg, resource_set))
       return;