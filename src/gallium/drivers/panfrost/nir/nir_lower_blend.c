openharmony/third_party_mesa3d

commit 9211dcbd85022d7b24d0f2e411b54c85827ec07e

panfrost: LogicOp support
The generated shaders are definitely not optimal, but for a feature
hardly anyone uses, it's probably good enough.

The XScreensaver demos quasicrystal, blitspin, bouboule, crystal and
munch now seem to work, with no obvious problems.

Currently this only works for 8-bit textures.

Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3887>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3887>

src/gallium/drivers/panfrost/nir/nir_lower_blend.c
@@ -156,6 +156,70 @@ nir_color_mask(
    return nir_vec(b, masked, 4);
 }
 
+static nir_ssa_def *
+nir_logicop_func(
+   nir_builder *b,
+   unsigned func,
+   nir_ssa_def *src, nir_ssa_def *dst)
+{
+   switch (func) {
+   case PIPE_LOGICOP_CLEAR:
+      return nir_imm_ivec4(b, 0, 0, 0, 0);
+   case PIPE_LOGICOP_NOR:
+      return nir_inot(b, nir_ior(b, src, dst));
+   case PIPE_LOGICOP_AND_INVERTED:
+      return nir_iand(b, nir_inot(b, src), dst);
+   case PIPE_LOGICOP_COPY_INVERTED:
+      return nir_inot(b, src);
+   case PIPE_LOGICOP_AND_REVERSE:
+      return nir_iand(b, src, nir_inot(b, dst));
+   case PIPE_LOGICOP_INVERT:
+      return nir_inot(b, dst);
+   case PIPE_LOGICOP_XOR:
+      return nir_ixor(b, src, dst);
+   case PIPE_LOGICOP_NAND:
+      return nir_inot(b, nir_iand(b, src, dst));
+   case PIPE_LOGICOP_AND:
+      return nir_iand(b, src, dst);
+   case PIPE_LOGICOP_EQUIV:
+      return nir_inot(b, nir_ixor(b, src, dst));
+   case PIPE_LOGICOP_NOOP:
+      return dst;
+   case PIPE_LOGICOP_OR_INVERTED:
+      return nir_ior(b, nir_inot(b, src), dst);
+   case PIPE_LOGICOP_COPY:
+      return src;
+   case PIPE_LOGICOP_OR_REVERSE:
+      return nir_ior(b, src, nir_inot(b, dst));
+   case PIPE_LOGICOP_OR:
+      return nir_ior(b, src, dst);
+   case PIPE_LOGICOP_SET:
+      return nir_imm_ivec4(b, ~0, ~0, ~0, ~0);
+   }
+
+   unreachable("Invalid logciop function");
+}
+
+static nir_ssa_def *
+nir_blend_logicop(
+   nir_builder *b,
+   nir_lower_blend_options options,
+   nir_ssa_def *src, nir_ssa_def *dst)
+{
+   /* TODO: Support other sizes */
+   nir_ssa_def *factor = nir_imm_float(b, 255);
+
+   src = nir_fmin(b, nir_fmax(b, src, nir_imm_float(b, -1)), nir_imm_float(b, 1));
+   src = nir_f2u32(b, nir_fround_even(b, nir_fmul(b, src, factor)));
+
+   dst = nir_fmin(b, nir_fmax(b, dst, nir_imm_float(b, -1)), nir_imm_float(b, 1));
+   dst = nir_f2u32(b, nir_fround_even(b, nir_fmul(b, dst, factor)));
+
+   nir_ssa_def *out = nir_logicop_func(b, options.logicop_func, src, dst);
+
+   return nir_fdiv(b, nir_u2f32(b, out), factor);
+}
+
 /* Given a blend state, the source color, and the destination color,
  * return the blended color
  */
@@ -166,6 +230,9 @@ nir_blend(
    nir_lower_blend_options options,
    nir_ssa_def *src, nir_ssa_def *dst)
 {
+   if (options.logicop_enable)
+      return nir_blend_logicop(b, options, src, dst);
+
    /* Grab the blend constant ahead of time */
    nir_ssa_def *bconst = nir_load_blend_const_color_rgba(b);
 

src/gallium/drivers/panfrost/nir/nir_lower_blend.h
@@ -48,6 +48,9 @@ typedef struct {
 
    /* 4-bit colormask. 0x0 for none, 0xF for RGBA, 0x1 for R */
    unsigned colormask;
+
+   bool logicop_enable;
+   unsigned logicop_func;
 } nir_lower_blend_options;
 
 void nir_lower_blend(nir_shader *shader, nir_lower_blend_options options);

src/gallium/drivers/panfrost/pan_blend_shaders.c
@@ -89,6 +89,14 @@ nir_make_options(const struct pipe_blend_state *blend, unsigned i)
 {
         nir_lower_blend_options options;
 
+        if (blend->logicop_enable) {
+            options.logicop_enable = true;
+            options.logicop_func = blend->logicop_func;
+            return options;
+        }
+
+        options.logicop_enable = false;
+
         /* If blend is disabled, we just use replace mode */
 
         nir_lower_blend_channel rgb = {

src/gallium/drivers/panfrost/pan_blend_cso.c
@@ -107,7 +107,6 @@ panfrost_create_blend_state(struct pipe_context *pipe,
         so->base = *blend;
 
         /* TODO: The following features are not yet implemented */
-        assert(!blend->logicop_enable);
         assert(!blend->alpha_to_coverage);
         assert(!blend->alpha_to_one);
 
@@ -119,15 +118,17 @@ panfrost_create_blend_state(struct pipe_context *pipe,
 
                 /* Without indep blending, the first RT settings replicate */
 
-                unsigned g =
-                        blend->independent_blend_enable ? c : 0;
+                if (!blend->logicop_enable) {
+                        unsigned g =
+                                blend->independent_blend_enable ? c : 0;
 
-                rt->has_fixed_function =
-                        panfrost_make_fixed_blend_mode(
-                                &blend->rt[g],
-                                &rt->equation,
-                                &rt->constant_mask,
-                                blend->rt[g].colormask);
+                        rt->has_fixed_function =
+                                panfrost_make_fixed_blend_mode(
+                                        &blend->rt[g],
+                                        &rt->equation,
+                                        &rt->constant_mask,
+                                        blend->rt[g].colormask);
+                }
 
                 /* Regardless if that works, we also need to initialize
                  * the blend shaders */

commit 3a6d98d34a7d5865b086c43fd04ea62c88a17b87

panfrost: LogicOp fixes and non 8-bit format support
With the previous LogicOp commit almost half of the blend modes were
broken because the surplus bits were not cleared after an inot.

v2:
 - Remove u8 "fast path" as 8-bit is not well optimised yet
 - Don't mask for 32-bit formats as that triggers an assert

Fixes: 9211dcbd850 ("panfrost: LogicOp support")
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/3943>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/3943>

src/gallium/drivers/panfrost/nir/nir_lower_blend.c
@@ -32,6 +32,7 @@
 
 #include "compiler/nir/nir.h"
 #include "compiler/nir/nir_builder.h"
+#include "compiler/nir/nir_format_convert.h"
 #include "nir_lower_blend.h"
 
 /* Given processed factors, combine them per a blend function */
@@ -206,18 +207,30 @@ nir_blend_logicop(
    nir_lower_blend_options options,
    nir_ssa_def *src, nir_ssa_def *dst)
 {
-   /* TODO: Support other sizes */
-   nir_ssa_def *factor = nir_imm_float(b, 255);
+   const struct util_format_description *format_desc =
+      util_format_description(options.format);
 
-   src = nir_fmin(b, nir_fmax(b, src, nir_imm_float(b, -1)), nir_imm_float(b, 1));
-   src = nir_f2u32(b, nir_fround_even(b, nir_fmul(b, src, factor)));
+   assert(src->num_components <= 4);
+   assert(dst->num_components <= 4);
 
-   dst = nir_fmin(b, nir_fmax(b, dst, nir_imm_float(b, -1)), nir_imm_float(b, 1));
-   dst = nir_f2u32(b, nir_fround_even(b, nir_fmul(b, dst, factor)));
+   unsigned bits[4];
+   for (int i = 0; i < 4; ++i)
+       bits[i] = format_desc->channel[i].size;
+
+   src = nir_format_float_to_unorm(b, src, bits);
+   dst = nir_format_float_to_unorm(b, dst, bits);
 
    nir_ssa_def *out = nir_logicop_func(b, options.logicop_func, src, dst);
 
-   return nir_fdiv(b, nir_u2f32(b, out), factor);
+   if (bits[0] < 32) {
+       nir_const_value mask[4];
+       for (int i = 0; i < 4; ++i)
+           mask[i] = nir_const_value_for_int((1u << bits[i]) - 1, 32);
+
+       out = nir_iand(b, out, nir_build_imm(b, 4, 32, mask));
+   }
+
+   return nir_format_unorm_to_float(b, out, bits);
 }
 
 /* Given a blend state, the source color, and the destination color,

src/gallium/drivers/panfrost/nir/nir_lower_blend.h
@@ -51,6 +51,7 @@ typedef struct {
 
    bool logicop_enable;
    unsigned logicop_func;
+   enum pipe_format format;
 } nir_lower_blend_options;
 
 void nir_lower_blend(nir_shader *shader, nir_lower_blend_options options);

src/gallium/drivers/panfrost/pan_blend_shaders.c
@@ -173,6 +173,8 @@ panfrost_compile_blend_shader(
 
         nir_lower_blend_options options =
                 nir_make_options(cso, rt);
+        options.format = format;
+
         NIR_PASS_V(shader, nir_lower_blend, options);
 
         NIR_PASS_V(shader, nir_lower_framebuffer, format, screen->gpu_id);