openharmony/third_party_mesa3d

commit 94ca534baabc6402e608cd31098761d6dd22d397

radv: handle missing implicit subpass dependencies
When a subpass doesn't declare an explicit dependency from/to
VK_SUBPASS_EXTERNAL, Vulkan says there is an implicit dependency.

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3330>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3330>

src/amd/vulkan/radv_pass.c
@@ -60,6 +60,90 @@ radv_render_pass_add_subpass_dep(struct radv_render_pass *pass,
 }
 
 static void
+radv_render_pass_add_implicit_deps(struct radv_render_pass *pass,
+				   bool has_ingoing_dep, bool has_outgoing_dep)
+{
+	/* From the Vulkan 1.0.39 spec:
+	*
+	*    If there is no subpass dependency from VK_SUBPASS_EXTERNAL to the
+	*    first subpass that uses an attachment, then an implicit subpass
+	*    dependency exists from VK_SUBPASS_EXTERNAL to the first subpass it is
+	*    used in. The subpass dependency operates as if defined with the
+	*    following parameters:
+	*
+	*    VkSubpassDependency implicitDependency = {
+	*        .srcSubpass = VK_SUBPASS_EXTERNAL;
+	*        .dstSubpass = firstSubpass; // First subpass attachment is used in
+	*        .srcStageMask = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+	*        .dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
+	*        .srcAccessMask = 0;
+	*        .dstAccessMask = VK_ACCESS_INPUT_ATTACHMENT_READ_BIT |
+	*                         VK_ACCESS_COLOR_ATTACHMENT_READ_BIT |
+	*                         VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT |
+	*                         VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT |
+	*                         VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+	*        .dependencyFlags = 0;
+	*    };
+	*
+	*    Similarly, if there is no subpass dependency from the last subpass
+	*    that uses an attachment to VK_SUBPASS_EXTERNAL, then an implicit
+	*    subpass dependency exists from the last subpass it is used in to
+	*    VK_SUBPASS_EXTERNAL. The subpass dependency operates as if defined
+	*    with the following parameters:
+	*
+	*    VkSubpassDependency implicitDependency = {
+	*        .srcSubpass = lastSubpass; // Last subpass attachment is used in
+	*        .dstSubpass = VK_SUBPASS_EXTERNAL;
+	*        .srcStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
+	*        .dstStageMask = VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT;
+	*        .srcAccessMask = VK_ACCESS_INPUT_ATTACHMENT_READ_BIT |
+	*                         VK_ACCESS_COLOR_ATTACHMENT_READ_BIT |
+	*                         VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT |
+	*                         VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT |
+	*                         VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+	*        .dstAccessMask = 0;
+	*        .dependencyFlags = 0;
+	*    };
+	*/
+
+	if (!has_ingoing_dep) {
+		const VkSubpassDependency2KHR implicit_ingoing_dep = {
+			.srcSubpass = VK_SUBPASS_EXTERNAL,
+			.dstSubpass = 0,
+			.srcStageMask = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
+			.dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+			.srcAccessMask = 0,
+			.dstAccessMask = VK_ACCESS_INPUT_ATTACHMENT_READ_BIT |
+					 VK_ACCESS_COLOR_ATTACHMENT_READ_BIT |
+					 VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT |
+					 VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT |
+					 VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT,
+			.dependencyFlags = 0,
+		};
+
+		radv_render_pass_add_subpass_dep(pass, &implicit_ingoing_dep);
+	}
+
+	if (!has_outgoing_dep) {
+		const VkSubpassDependency2KHR implicit_outgoing_dep = {
+			.srcSubpass = 0,
+			.dstSubpass = VK_SUBPASS_EXTERNAL,
+			.srcStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+			.dstStageMask = VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
+			.srcAccessMask = VK_ACCESS_INPUT_ATTACHMENT_READ_BIT |
+					 VK_ACCESS_COLOR_ATTACHMENT_READ_BIT |
+					 VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT |
+					 VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT |
+					 VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT,
+			.dstAccessMask = 0,
+			.dependencyFlags = 0,
+		};
+
+		radv_render_pass_add_subpass_dep(pass, &implicit_outgoing_dep);
+	}
+}
+
+static void
 radv_render_pass_compile(struct radv_render_pass *pass)
 {
 	for (uint32_t i = 0; i < pass->subpass_count; i++) {
@@ -314,6 +398,9 @@ VkResult radv_CreateRenderPass(
 		}
 	}
 
+	bool has_ingoing_dep = false;
+	bool has_outgoing_dep = false;
+
 	for (unsigned i = 0; i < pCreateInfo->dependencyCount; ++i) {
 		/* Convert to a Dependency2 */
 		struct VkSubpassDependency2 dep2 = {
@@ -326,7 +413,18 @@ VkResult radv_CreateRenderPass(
 			.dependencyFlags  = pCreateInfo->pDependencies[i].dependencyFlags,
 		};
 		radv_render_pass_add_subpass_dep(pass, &dep2);
+
+		/* Determine if the subpass has explicit dependencies from/to
+		 * VK_SUBPASS_EXTERNAL.
+		 */
+		if (pCreateInfo->pDependencies[i].srcSubpass == VK_SUBPASS_EXTERNAL)
+			has_ingoing_dep = true;
+		if (pCreateInfo->pDependencies[i].dstSubpass == VK_SUBPASS_EXTERNAL)
+			has_outgoing_dep = true;
 	}
+
+	radv_render_pass_add_implicit_deps(pass,
+					   has_ingoing_dep, has_outgoing_dep);
 
 	radv_render_pass_compile(pass);
 
@@ -511,10 +609,24 @@ VkResult radv_CreateRenderPass2(
 		}
 	}
 
+	bool has_ingoing_dep = false;
+	bool has_outgoing_dep = false;
+
 	for (unsigned i = 0; i < pCreateInfo->dependencyCount; ++i) {
 		radv_render_pass_add_subpass_dep(pass,
 						 &pCreateInfo->pDependencies[i]);
+
+		/* Determine if the subpass has explicit dependencies from/to
+		 * VK_SUBPASS_EXTERNAL.
+		 */
+		if (pCreateInfo->pDependencies[i].srcSubpass == VK_SUBPASS_EXTERNAL)
+			has_ingoing_dep = true;
+		if (pCreateInfo->pDependencies[i].dstSubpass == VK_SUBPASS_EXTERNAL)
+			has_outgoing_dep = true;
 	}
+
+	radv_render_pass_add_implicit_deps(pass,
+					   has_ingoing_dep, has_outgoing_dep);
 
 	radv_render_pass_compile(pass);
 

commit 8523826c2feb2fc515e05feaa83c66c391092723

radv: only inject implicit subpass dependencies if necessary
The Vulkan 1.2.134 spec update clarified when implicit subpass
dependencies should be injected by the driver. They only make
sense if automatic layout transitions are performed.

This should fix a performance regression with RPCS3 (although
they added a workaround for RADV since the regression has been found).

Closes: https://gitlab.freedesktop.org/mesa/mesa/issues/2502
Fixes: 94ca534baab ("radv: handle missing implicit subpass dependencies")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4210>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4210>

src/amd/vulkan/radv_pass.c
@@ -59,6 +59,32 @@ radv_render_pass_add_subpass_dep(struct radv_render_pass *pass,
 	}
 }
 
+static bool
+radv_pass_has_layout_transitions(const struct radv_render_pass *pass)
+{
+	for (unsigned i = 0; i < pass->subpass_count; i++) {
+		const struct radv_subpass *subpass = &pass->subpasses[i];
+		for (unsigned j = 0; j < subpass->attachment_count; j++) {
+			const uint32_t a = subpass->attachments[j].attachment;
+			if (a == VK_ATTACHMENT_UNUSED)
+				continue;
+
+			uint32_t initial_layout = pass->attachments[a].initial_layout;
+			uint32_t stencil_initial_layout = pass->attachments[a].stencil_initial_layout;
+			uint32_t final_layout = pass->attachments[a].final_layout;
+			uint32_t stencil_final_layout = pass->attachments[a].stencil_final_layout;
+
+			if (subpass->attachments[j].layout != initial_layout ||
+			    subpass->attachments[j].layout != stencil_initial_layout ||
+			    subpass->attachments[j].layout != final_layout ||
+			    subpass->attachments[j].layout != stencil_final_layout)
+				return true;
+		}
+	}
+
+	return false;
+}
+
 static void
 radv_render_pass_add_implicit_deps(struct radv_render_pass *pass,
 				   bool has_ingoing_dep, bool has_outgoing_dep)
@@ -68,7 +94,9 @@ radv_render_pass_add_implicit_deps(struct radv_render_pass *pass,
 	*    If there is no subpass dependency from VK_SUBPASS_EXTERNAL to the
 	*    first subpass that uses an attachment, then an implicit subpass
 	*    dependency exists from VK_SUBPASS_EXTERNAL to the first subpass it is
-	*    used in. The subpass dependency operates as if defined with the
+	*    used in. The implicit subpass dependency only exists if there
+	*    exists an automatic layout transition away from initialLayout.
+	*    The subpass dependency operates as if defined with the
 	*    following parameters:
 	*
 	*    VkSubpassDependency implicitDependency = {
@@ -88,8 +116,10 @@ radv_render_pass_add_implicit_deps(struct radv_render_pass *pass,
 	*    Similarly, if there is no subpass dependency from the last subpass
 	*    that uses an attachment to VK_SUBPASS_EXTERNAL, then an implicit
 	*    subpass dependency exists from the last subpass it is used in to
-	*    VK_SUBPASS_EXTERNAL. The subpass dependency operates as if defined
-	*    with the following parameters:
+	*    VK_SUBPASS_EXTERNAL. The implicit subpass dependency only exists
+	*    if there exists an automatic layout transition into finalLayout.
+	*    The subpass dependency operates as if defined with the following
+	*    parameters:
 	*
 	*    VkSubpassDependency implicitDependency = {
 	*        .srcSubpass = lastSubpass; // Last subpass attachment is used in
@@ -105,6 +135,12 @@ radv_render_pass_add_implicit_deps(struct radv_render_pass *pass,
 	*        .dependencyFlags = 0;
 	*    };
 	*/
+
+	/* Implicit subpass dependencies only make sense if automatic layout
+	 * transitions are performed.
+	 */
+	if (!radv_pass_has_layout_transitions(pass))
+		return;
 
 	if (!has_ingoing_dep) {
 		const VkSubpassDependency2KHR implicit_ingoing_dep = {