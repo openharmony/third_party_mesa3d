openharmony/third_party_mesa3d

commit 9e7c11db439e0f95fabaff736d7e020f111adb16

util/u_process: add util_get_process_exec_path
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4181>

src/util/u_process.c
@@ -26,11 +26,18 @@
  */
 
 #include "u_process.h"
+#include "detect_os.h"
+#include "macros.h"
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #undef GET_PROGRAM_NAME
+
+#if DETECT_OS_WINDOWS
+#include <windows.h>
+#endif
 
 #if defined(__linux__) && defined(HAVE_PROGRAM_INVOCATION_NAME)
 
@@ -151,4 +158,28 @@ const char *
 util_get_process_name(void)
 {
    return GET_PROGRAM_NAME();
+}
+
+size_t
+util_get_process_exec_path(char* process_path, size_t len)
+{
+#if DETECT_OS_WINDOWS
+   return GetModuleFileNameA(NULL, process_path, len);
+#elif DETECT_OS_UNIX
+   ssize_t r;
+
+   if ((r = readlink("/proc/self/exe", process_path, len)) > 0)
+      goto success;
+   if ((r = readlink("/proc/curproc/exe", process_path, len)) > 0)
+      goto success;
+   if ((r = readlink("/proc/curproc/file", process_path, len)) > 0)
+      goto success;
+
+    return 0;
+success:
+    process_path[r] = '\0';
+    return r;
+
+#endif
+   return 0;
 }

src/util/meson.build
@@ -278,6 +278,28 @@ if with_tests
      suite : ['util'],
   )
 
+  process_test_exe = executable(
+    'process_test',
+    files('process_test.c'),
+    include_directories : inc_common,
+    dependencies : idep_mesautil,
+    c_args : [c_msvc_compat_args],
+  )
+  if not (host_machine.system() == 'windows')
+    process_test_exe_full_path = process_test_exe.full_path()
+  else
+    process_test_exe_full_path = run_command(
+      'winepath', '-w', process_test_exe.full_path()
+    ).stdout().strip()
+  endif
+
+  test(
+    'process',
+    process_test_exe,
+    suite : ['util'],
+    env: ['BUILD_FULL_PATH='+process_test_exe_full_path]
+  )
+
   subdir('tests/fast_idiv_by_const')
   subdir('tests/fast_urem_by_const')
   subdir('tests/hash_table')

src/util/process_test.c
@@ -1,0 +1,83 @@
+/*
+ * Copyright © 2020 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/* A collection of unit tests for u_process.c */
+
+#include "util/detect_os.h"
+#include "util/u_process.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h>
+
+bool error = false;
+
+static void
+expect_equal_str(const char *expected, const char *actual, const char *test)
+{
+   if (strcmp(expected, actual)) {
+      fprintf (stderr, "Error: Test '%s' failed:\n\t"
+               "Expected=\"%s\", Actual=\"%s\"\n",
+               test, expected, actual);
+      error = true;
+   }
+}
+
+static void
+test_util_get_process_name (void)
+{
+   #if !DETECT_OS_WINDOWS
+   const char* name = util_get_process_name();
+   expect_equal_str("process_test", name, "util_get_process_name");
+   #endif
+}
+
+/* This test gets the real path from Meson (BUILD_FULL_PATH env var),
+ * and compares it to the output of util_get_process_exec_path.
+ */
+static void
+test_util_get_process_exec_path (void)
+{
+   char path[PATH_MAX];
+   if (util_get_process_exec_path(path, PATH_MAX) == 0) {
+      error = true;
+      return;
+   }
+   char* build_path = getenv("BUILD_FULL_PATH");
+   if (!build_path) {
+      fprintf(stderr, "BUILD_FULL_PATH environment variable should be set\n");
+      error = true;
+      return;
+   }
+   expect_equal_str(build_path, path, "util_get_process_name");
+}
+
+int
+main (void)
+{
+   test_util_get_process_name();
+   test_util_get_process_exec_path();
+
+   return error ? 1 : 0;
+}

src/util/u_process.h
@@ -28,7 +28,12 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 
+#include <stddef.h>
+
 const char *
 util_get_process_name(void);
+
+size_t
+util_get_process_exec_path(char* process_path, size_t len);
 
 #endif

commit 308538d53458e053c592b4690cc5d9cc843ae949

util/u_process: Add util_get_process_exec_path for macOS.
Fixes: 9e7c11db439e ("util/u_process: add util_get_process_exec_path")
Closes: https://gitlab.freedesktop.org/mesa/mesa/issues/2682
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4313>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4313>

src/util/u_process.c
@@ -40,6 +40,10 @@
 #include <unistd.h>
 #endif
 
+#if DETECT_OS_APPLE
+#include <mach-o/dyld.h>
+#endif
+
 #if defined(__linux__) && defined(HAVE_PROGRAM_INVOCATION_NAME)
 
 static char *path = NULL;
@@ -166,6 +170,11 @@ util_get_process_exec_path(char* process_path, size_t len)
 {
 #if DETECT_OS_WINDOWS
    return GetModuleFileNameA(NULL, process_path, len);
+#elif DETECT_OS_APPLE
+   uint32_t bufSize = len;
+   int result = _NSGetExecutablePath(process_path, &bufSize);
+
+   return (result == 0) ? strlen(process_path) : 0;
 #elif DETECT_OS_UNIX
    ssize_t r;
 