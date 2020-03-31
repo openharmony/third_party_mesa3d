openharmony/third_party_mesa3d

commit a0e1f890ef94ef7054b8e3bea6774d4bc11affd5

anv/allocator: Add support for large stream allocations
Reviewed-by: Juan A. Suarez Romero <jasuarez@igalia.com>

src/intel/vulkan/anv_allocator.c
@@ -916,10 +916,13 @@ anv_state_stream_alloc(struct anv_state_stream *stream,
    assert(alignment <= PAGE_SIZE);
 
    uint32_t offset = align_u32(stream->next, alignment);
-   if (offset + size > stream->block_size) {
+   if (offset + size > stream->block.alloc_size) {
+      uint32_t block_size = stream->block_size;
+      if (block_size < size)
+         block_size = round_to_power_of_two(size);
+
       stream->block = anv_state_pool_alloc_no_vg(stream->state_pool,
-                                                 stream->block_size,
-                                                 PAGE_SIZE);
+                                                 block_size, PAGE_SIZE);
 
       struct anv_state_stream_block *sb = stream->block.map;
       VG_NOACCESS_WRITE(&sb->block, stream->block);
@@ -933,7 +936,7 @@ anv_state_stream_alloc(struct anv_state_stream *stream,
       stream->next = sizeof(*sb);
 
       offset = align_u32(stream->next, alignment);
-      assert(offset + size <= stream->block_size);
+      assert(offset + size <= stream->block.alloc_size);
    }
 
    struct anv_state state = stream->block;

commit 1533570c20c612c3bbd36fbbf8a0ef50d1f9009b

anv: Account for the header in anv_state_stream_alloc
If we have an allocation that's exactly the block size, we end up
computing a new block size to allocate that's exactly the block size,
add in the header, and then assert fail.  When computing the block size,
we need to account for the header.

Fixes: a0e1f890ef9 "anv/allocator: Add support for large stream..."
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4336>

src/intel/vulkan/anv_allocator.c
@@ -1226,9 +1226,10 @@ anv_state_stream_alloc(struct anv_state_stream *stream,
 
    uint32_t offset = align_u32(stream->next, alignment);
    if (offset + size > stream->block.alloc_size) {
+      uint32_t min_block_size = size + sizeof(struct anv_state_stream_block);
       uint32_t block_size = stream->block_size;
-      if (block_size < size)
-         block_size = round_to_power_of_two(size);
+      if (block_size < min_block_size)
+         block_size = round_to_power_of_two(min_block_size);
 
       stream->block = anv_state_pool_alloc_no_vg(stream->state_pool,
                                                  block_size, PAGE_SIZE);