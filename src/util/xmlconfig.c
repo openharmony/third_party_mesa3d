openharmony/third_party_mesa3d

commit 7cfd9b7c30397d581fc0d4bd10b15482c63804bb

util/xmlconfig: add new sha1 application attribute
This is useful to enable workarounds for applications with a generic name.

For instance all games made with the YoYo game engine have the same executable
name "runner".

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4181>

src/util/xmlconfig.c
@@ -46,6 +46,7 @@
 #include "strndup.h"
 #include "xmlconfig.h"
 #include "u_process.h"
+#include "os_file.h"
 
 /* For systems like Hurd */
 #ifndef PATH_MAX
@@ -780,13 +781,39 @@ parseAppAttr(struct OptConfData *data, const XML_Char **attr)
 {
     uint32_t i;
     const XML_Char *exec = NULL;
+    const XML_Char *sha1 = NULL;
     for (i = 0; attr[i]; i += 2) {
         if (!strcmp (attr[i], "name")) /* not needed here */;
         else if (!strcmp (attr[i], "executable")) exec = attr[i+1];
+        else if (!strcmp (attr[i], "sha1")) sha1 = attr[i+1];
         else XML_WARNING("unknown application attribute: %s.", attr[i]);
     }
-    if (exec && strcmp (exec, data->execName))
+    if (exec && strcmp (exec, data->execName)) {
         data->ignoringApp = data->inApp;
+    } else if (sha1) {
+        if (strlen(sha1) != 40) {
+            XML_WARNING("Incorrect sha1 application attribute");
+            data->ignoringApp = data->inApp;
+        } else {
+            size_t len;
+            char* content;
+            char path[PATH_MAX];
+            if (util_get_process_exec_path(path, ARRAY_SIZE(path)) > 0 &&
+                (content = os_read_file(path, &len))) {
+                uint8_t sha1x[20];
+                char sha1s[40];
+                _mesa_sha1_compute(content, len, sha1x);
+                _mesa_sha1_format((char*) sha1s, sha1x);
+                free(content);
+
+                if (memcmp(sha1, sha1s, SHA1_DIGEST_LENGTH)) {
+                    data->ignoringApp = data->inApp;
+                }
+            } else {
+                data->ignoringApp = data->inApp;
+            }
+        }
+    }
 }
 
 /** \brief Parse attributes of an application element. */

src/util/00-mesa-defaults.conf
@@ -34,7 +34,8 @@ TODO: document the other workarounds.
    <!ATTLIST device       driver CDATA #IMPLIED>
    <!ELEMENT application  (option+)>
    <!ATTLIST application  name CDATA #REQUIRED
-                          executable CDATA #REQUIRED>
+                          executable CDATA #REQUIRED
+                          sha1 CDATA #IMPLIED>
    <!ELEMENT engine       (option+)>
 
    <!-- engine_name_match: A regexp matching the engine name -->

commit 7084a59b9b09652537df7f63edb03879154ad64d

util/xmlconfig: fix sha1 comparison code
Fixes: 7cfd9b7c303 ("util/xmlconfig: add new sha1 application attribute")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/2730
Reviewed-by: Dave Airlie <airlied@redhat.com>
Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4426>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4426>

src/util/xmlconfig.c
@@ -791,7 +791,8 @@ parseAppAttr(struct OptConfData *data, const XML_Char **attr)
     if (exec && strcmp (exec, data->execName)) {
         data->ignoringApp = data->inApp;
     } else if (sha1) {
-        if (strlen(sha1) != 40) {
+        /* SHA1_DIGEST_STRING_LENGTH includes terminating null byte */
+        if (strlen(sha1) != (SHA1_DIGEST_STRING_LENGTH - 1)) {
             XML_WARNING("Incorrect sha1 application attribute");
             data->ignoringApp = data->inApp;
         } else {
@@ -800,13 +801,13 @@ parseAppAttr(struct OptConfData *data, const XML_Char **attr)
             char path[PATH_MAX];
             if (util_get_process_exec_path(path, ARRAY_SIZE(path)) > 0 &&
                 (content = os_read_file(path, &len))) {
-                uint8_t sha1x[20];
-                char sha1s[40];
+                uint8_t sha1x[SHA1_DIGEST_LENGTH];
+                char sha1s[SHA1_DIGEST_STRING_LENGTH];
                 _mesa_sha1_compute(content, len, sha1x);
                 _mesa_sha1_format((char*) sha1s, sha1x);
                 free(content);
 
-                if (memcmp(sha1, sha1s, SHA1_DIGEST_LENGTH)) {
+                if (strcmp(sha1, sha1s)) {
                     data->ignoringApp = data->inApp;
                 }
             } else {