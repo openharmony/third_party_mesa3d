openharmony/third_party_mesa3d

commit a419e16e0816aabee805bf4a1dbdc5d83ee62a1c

ci: Clean up some excessive use of pipes in dEQP results processing.
Given that we use set -x in the script, this actually makes the user
experience of viewing logs nicer.

Reviewed-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4621>

.gitlab-ci/deqp-runner.sh
@@ -242,11 +242,7 @@ DEQP_EXITCODE=$?
 if [ $DEQP_EXITCODE -ne 0 ]; then
     # preserve caselist files in case of failures:
     cp /tmp/deqp_runner.*.txt $RESULTS/
-    cat $RESULTSFILE | \
-        grep -v ",Pass" | \
-        grep -v ",Skip" | \
-        grep -v ",ExpectedFail" > \
-        $UNEXPECTED_RESULTSFILE.txt
+    egrep -v ",Pass|,Skip|,ExpectedFail" $RESULTSFILE > $UNEXPECTED_RESULTSFILE.txt
 
     if [ -z "$DEQP_NO_SAVE_RESULTS" ]; then
         echo "Some unexpected results found (see cts-runner-results.txt in artifacts for full results):"
@@ -259,17 +255,15 @@ if [ $DEQP_EXITCODE -ne 0 ]; then
         cat $UNEXPECTED_RESULTSFILE.txt
     fi
 
-    count=`cat $UNEXPECTED_RESULTSFILE.txt | wc -l`
+    count=`wc -l $UNEXPECTED_RESULTSFILE.txt`
 
     # Re-run fails to detect flakes.  But use a small threshold, if
     # something was fundamentally broken, we don't want to re-run
     # the entire caselist
 else
-    cat $RESULTSFILE | \
-        grep ",Flake" > \
-        $FLAKESFILE
+    grep ",Flake" $RESULTSFILE > $FLAKESFILE
 
-    count=`cat $FLAKESFILE | wc -l`
+    count=`wc -l $FLAKESFILE`
     if [ $count -gt 0 ]; then
         echo "Some flakes found (see cts-runner-flakes.txt in artifacts for full results):"
         head -n 50 $FLAKESFILE

commit fdae8f05d2aaa8d5fc73cec0ac2c5e927ff2d071

ci: fix reporting the number of unexpected/flakes
`wc -l $file` returns the number of lines and the filename.

Fixes: a419e16e081 ("ci: Clean up some excessive use of pipes in dEQP results processing.")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Andres Gomez <agomez@igalia.com>
Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4829>

.gitlab-ci/deqp-runner.sh
@@ -259,7 +259,7 @@ if [ $DEQP_EXITCODE -ne 0 ]; then
         cat $UNEXPECTED_RESULTSFILE.txt
     fi
 
-    count=`wc -l $UNEXPECTED_RESULTSFILE.txt`
+    count=`cat $UNEXPECTED_RESULTSFILE.txt | wc -l`
 
     # Re-run fails to detect flakes.  But use a small threshold, if
     # something was fundamentally broken, we don't want to re-run
@@ -267,7 +267,7 @@ if [ $DEQP_EXITCODE -ne 0 ]; then
 else
     grep ",Flake" $RESULTSFILE > $FLAKESFILE
 
-    count=`wc -l $FLAKESFILE`
+    count=`cat $FLAKESFILE | wc -l`
     if [ $count -gt 0 ]; then
         echo "Some flakes found (see cts-runner-flakes.txt in artifacts for full results):"
         head -n 50 $FLAKESFILE