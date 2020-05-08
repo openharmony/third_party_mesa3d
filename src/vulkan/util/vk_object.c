openharmony/third_party_mesa3d

commit 60c36f2835413a41288f1b2a2b5ee8ba4f4fd870

vulkan: Add run-time object type asserts in handle casts
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Acked-by: Kristian H. Kristensen <hoegsberg@google.com>
Acked-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4690>

src/vulkan/util/vk_object.c
@@ -29,6 +29,9 @@ vk_object_base_init(UNUSED struct vk_device *device,
                     UNUSED VkObjectType obj_type)
 {
    base->_loader_data.loaderMagic = ICD_LOADER_MAGIC;
+#ifndef NDEBUG
+   base->type = obj_type;
+#endif
 }
 
 void

src/vulkan/util/vk_object.h
@@ -36,12 +36,22 @@ struct vk_device;
 
 struct vk_object_base {
    VK_LOADER_DATA _loader_data;
+#ifndef NDEBUG
+   VkObjectType type;
+#endif
 };
 
 void vk_object_base_init(UNUSED struct vk_device *device,
                          struct vk_object_base *base,
                          UNUSED VkObjectType obj_type);
 void vk_object_base_finish(UNUSED struct vk_object_base *base);
+
+static inline void
+vk_object_base_assert_valid(ASSERTED struct vk_object_base *base,
+                            ASSERTED VkObjectType obj_type)
+{
+   assert(base == NULL || base->type == obj_type);
+}
 
 
 struct vk_device {
@@ -59,13 +69,16 @@ void vk_device_finish(struct vk_device *device);
    static inline struct __driver_type *                                    \
    __driver_type ## _from_handle(__VkType _handle)                         \
    {                                                                       \
+      struct vk_object_base *base = (struct vk_object_base *)_handle;      \
+      vk_object_base_assert_valid(base, __VK_TYPE);                        \
       STATIC_ASSERT(offsetof(struct __driver_type, __base) == 0);          \
-      return (struct __driver_type *) _handle;                             \
+      return (struct __driver_type *) base;                                \
    }                                                                       \
                                                                            \
    static inline __VkType                                                  \
    __driver_type ## _to_handle(struct __driver_type *_obj)                 \
    {                                                                       \
+      vk_object_base_assert_valid(&_obj->__base, __VK_TYPE);               \
       return (__VkType) _obj;                                              \
    }
 
@@ -73,13 +86,17 @@ void vk_device_finish(struct vk_device *device);
    static inline struct __driver_type *                                    \
    __driver_type ## _from_handle(__VkType _handle)                         \
    {                                                                       \
+      struct vk_object_base *base =                                        \
+         (struct vk_object_base *)(uintptr_t)_handle;                      \
+      vk_object_base_assert_valid(base, __VK_TYPE);                        \
       STATIC_ASSERT(offsetof(struct __driver_type, __base) == 0);          \
-      return (struct __driver_type *)(uintptr_t) _handle;                  \
+      return (struct __driver_type *)base;                                 \
    }                                                                       \
                                                                            \
    static inline __VkType                                                  \
    __driver_type ## _to_handle(struct __driver_type *_obj)                 \
    {                                                                       \
+      vk_object_base_assert_valid(&_obj->__base, __VK_TYPE);               \
       return (__VkType)(uintptr_t) _obj;                                   \
    }
 

commit accde6bf8a0a82f00b4899ad29fadf1403e65fe4

vulkan/object: Always include the type
This was causing problems for some of the ANV unit tests when run in
release mode.  Having a public struct whose layout depends on NDEBUG
seems kind-of sketchy anyway.

Fixes: 60c36f28354 "vulkan: Add run-time object type asserts in..."
Closes: #2903
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4959>

src/vulkan/util/vk_object.c
@@ -29,9 +29,7 @@ vk_object_base_init(UNUSED struct vk_device *device,
                     UNUSED VkObjectType obj_type)
 {
    base->_loader_data.loaderMagic = ICD_LOADER_MAGIC;
-#ifndef NDEBUG
    base->type = obj_type;
-#endif
 }
 
 void

src/vulkan/util/vk_object.h
@@ -36,9 +36,7 @@ struct vk_device;
 
 struct vk_object_base {
    VK_LOADER_DATA _loader_data;
-#ifndef NDEBUG
    VkObjectType type;
-#endif
 };
 
 void vk_object_base_init(UNUSED struct vk_device *device,