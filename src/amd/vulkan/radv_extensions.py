openharmony/third_party_mesa3d

commit 53c937e4db8c5c18bf23529849123a5a20ff42de

radv: enable Vulkan 1.2
This bumps the Vulkan version to 1.2.128.

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

src/amd/vulkan/radv_extensions.py
@@ -31,7 +31,7 @@ import xml.etree.cElementTree as et
 
 from mako.template import Template
 
-MAX_API_VERSION = '1.1.107'
+MAX_API_VERSION = '1.2.128'
 
 class Extension:
     def __init__(self, name, ext_version, enable):

commit 2a03c2b03d77a4ea19db33f7121cae9e40275937

radv: limit the Vulkan version to 1.1 for Android
Vulkan 1.2 seems rejected. This hardcodes the Android version to
1.1.107.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/2936
Fixes: 53c937e4db8 ("radv: enable Vulkan 1.2")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Acked-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4985>

src/amd/vulkan/radv_extensions.py
@@ -452,8 +452,13 @@ radv_physical_device_api_version(struct radv_physical_device *dev)
 {
     uint32_t override = vk_get_version_override();
     uint32_t version = VK_MAKE_VERSION(1, 0, 68);
-    if (dev->rad_info.has_syncobj_wait_for_submit)
-        version = ${MAX_API_VERSION.c_vk_version()};
+    if (dev->rad_info.has_syncobj_wait_for_submit) {
+        if (ANDROID) {
+            version = VK_MAKE_VERSION(1, 1, 107);
+        } else {
+            version = ${MAX_API_VERSION.c_vk_version()};
+        }
+    }
 
     return override ? MIN2(override, version) : version;
 }