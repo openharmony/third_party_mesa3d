openharmony/third_party_mesa3d

commit 3dcca2c18f16643ec78fb6a7a2d51bd688446a43

dri_util: add driImageFormatToSizedInternalGLFormat function
This is needed to implement the EXT_EGL_image_storage spec:

"If <target> is GL_TEXTURE_2D, then the resultant texture must have a
sized internal format which is colorspace and size compatible with the
dma-buf.  If the GL is unable to determine such a format, the error
INVALID_OPERATION is generated."

Reviewed-by: Tapani Pälli <tapani.palli@intel.com>

src/mesa/drivers/dri/common/dri_util.c
@@ -875,101 +875,125 @@ driUpdateFramebufferSize(struct gl_context *ctx, const __DRIdrawable *dPriv)
 static const struct {
    uint32_t    image_format;
    mesa_format mesa_format;
+   GLenum internal_format;
 } format_mapping[] = {
    {
-      .image_format = __DRI_IMAGE_FORMAT_RGB565,
-      .mesa_format  =        MESA_FORMAT_B5G6R5_UNORM,
+      .image_format    = __DRI_IMAGE_FORMAT_RGB565,
+      .mesa_format     =        MESA_FORMAT_B5G6R5_UNORM,
+      .internal_format =        GL_RGB565,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_ARGB1555,
-      .mesa_format  =        MESA_FORMAT_B5G5R5A1_UNORM,
+      .image_format    = __DRI_IMAGE_FORMAT_ARGB1555,
+      .mesa_format     =        MESA_FORMAT_B5G5R5A1_UNORM,
+      .internal_format =        GL_RGB5_A1,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_XRGB8888,
-      .mesa_format  =        MESA_FORMAT_B8G8R8X8_UNORM,
+      .image_format    = __DRI_IMAGE_FORMAT_XRGB8888,
+      .mesa_format     =        MESA_FORMAT_B8G8R8X8_UNORM,
+      .internal_format =        GL_RGBA8,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_ABGR16161616F,
-      .mesa_format  =        MESA_FORMAT_RGBA_FLOAT16,
+      .image_format    = __DRI_IMAGE_FORMAT_ABGR16161616F,
+      .mesa_format     =        MESA_FORMAT_RGBA_FLOAT16,
+      .internal_format =        GL_RGBA16F,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_XBGR16161616F,
-      .mesa_format  =        MESA_FORMAT_RGBX_FLOAT16,
+      .image_format    = __DRI_IMAGE_FORMAT_XBGR16161616F,
+      .mesa_format     =        MESA_FORMAT_RGBX_FLOAT16,
+      .internal_format =        GL_RGBA16F,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_ARGB2101010,
-      .mesa_format  =        MESA_FORMAT_B10G10R10A2_UNORM,
+      .image_format    = __DRI_IMAGE_FORMAT_ARGB2101010,
+      .mesa_format     =        MESA_FORMAT_B10G10R10A2_UNORM,
+      .internal_format =        GL_RGB10_A2,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_XRGB2101010,
-      .mesa_format  =        MESA_FORMAT_B10G10R10X2_UNORM,
+      .image_format    = __DRI_IMAGE_FORMAT_XRGB2101010,
+      .mesa_format     =        MESA_FORMAT_B10G10R10X2_UNORM,
+      .internal_format =        GL_RGB10_A2,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_ABGR2101010,
-      .mesa_format  =        MESA_FORMAT_R10G10B10A2_UNORM,
+      .image_format    = __DRI_IMAGE_FORMAT_ABGR2101010,
+      .mesa_format     =        MESA_FORMAT_R10G10B10A2_UNORM,
+      .internal_format =        GL_RGB10_A2,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_XBGR2101010,
-      .mesa_format  =        MESA_FORMAT_R10G10B10X2_UNORM,
+      .image_format    = __DRI_IMAGE_FORMAT_XBGR2101010,
+      .mesa_format     =        MESA_FORMAT_R10G10B10X2_UNORM,
+      .internal_format =        GL_RGB10_A2,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_ARGB8888,
-      .mesa_format  =        MESA_FORMAT_B8G8R8A8_UNORM,
+      .image_format    = __DRI_IMAGE_FORMAT_ARGB8888,
+      .mesa_format     =        MESA_FORMAT_B8G8R8A8_UNORM,
+      .internal_format =        GL_RGBA8,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_ABGR8888,
-      .mesa_format  =        MESA_FORMAT_R8G8B8A8_UNORM,
+      .image_format    = __DRI_IMAGE_FORMAT_ABGR8888,
+      .mesa_format     =        MESA_FORMAT_R8G8B8A8_UNORM,
+      .internal_format =        GL_RGBA8,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_XBGR8888,
-      .mesa_format  =        MESA_FORMAT_R8G8B8X8_UNORM,
+      .image_format    = __DRI_IMAGE_FORMAT_XBGR8888,
+      .mesa_format     =        MESA_FORMAT_R8G8B8X8_UNORM,
+      .internal_format =        GL_RGBA8,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_R8,
-      .mesa_format  =        MESA_FORMAT_R_UNORM8,
+      .image_format    = __DRI_IMAGE_FORMAT_R8,
+      .mesa_format     =        MESA_FORMAT_R_UNORM8,
+      .internal_format =        GL_R8,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_R8,
-      .mesa_format  =        MESA_FORMAT_L_UNORM8,
+      .image_format    = __DRI_IMAGE_FORMAT_R8,
+      .mesa_format     =        MESA_FORMAT_L_UNORM8,
+      .internal_format =        GL_R8,
    },
 #if UTIL_ARCH_LITTLE_ENDIAN
    {
-      .image_format = __DRI_IMAGE_FORMAT_GR88,
-      .mesa_format  =        MESA_FORMAT_RG_UNORM8,
+      .image_format    = __DRI_IMAGE_FORMAT_GR88,
+      .mesa_format     =        MESA_FORMAT_RG_UNORM8,
+      .internal_format =        GL_RG8,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_GR88,
-      .mesa_format  =        MESA_FORMAT_LA_UNORM8,
+      .image_format    = __DRI_IMAGE_FORMAT_GR88,
+      .mesa_format     =        MESA_FORMAT_LA_UNORM8,
+      .internal_format =        GL_RG8,
    },
 #endif
    {
-      .image_format = __DRI_IMAGE_FORMAT_SABGR8,
-      .mesa_format  =        MESA_FORMAT_R8G8B8A8_SRGB,
+      .image_format    = __DRI_IMAGE_FORMAT_SABGR8,
+      .mesa_format     =        MESA_FORMAT_R8G8B8A8_SRGB,
+      .internal_format =        GL_SRGB8_ALPHA8,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_SARGB8,
-      .mesa_format  =        MESA_FORMAT_B8G8R8A8_SRGB,
+      .image_format    = __DRI_IMAGE_FORMAT_SARGB8,
+      .mesa_format     =        MESA_FORMAT_B8G8R8A8_SRGB,
+      .internal_format =        GL_SRGB8_ALPHA8,
    },
    {
       .image_format = __DRI_IMAGE_FORMAT_SXRGB8,
-      .mesa_format  =        MESA_FORMAT_B8G8R8X8_SRGB,
+      .mesa_format  =           MESA_FORMAT_B8G8R8X8_SRGB,
+      .internal_format =        GL_SRGB8_ALPHA8,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_R16,
-      .mesa_format  =        MESA_FORMAT_R_UNORM16,
+      .image_format    = __DRI_IMAGE_FORMAT_R16,
+      .mesa_format     =        MESA_FORMAT_R_UNORM16,
+      .internal_format =        GL_R16,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_R16,
-      .mesa_format  =        MESA_FORMAT_L_UNORM16,
+      .image_format    = __DRI_IMAGE_FORMAT_R16,
+      .mesa_format     =        MESA_FORMAT_L_UNORM16,
+      .internal_format =        GL_R16,
    },
 #if UTIL_ARCH_LITTLE_ENDIAN
    {
-      .image_format = __DRI_IMAGE_FORMAT_GR1616,
-      .mesa_format  =        MESA_FORMAT_RG_UNORM16,
+      .image_format    = __DRI_IMAGE_FORMAT_GR1616,
+      .mesa_format     =        MESA_FORMAT_RG_UNORM16,
+      .internal_format =        GL_RG16,
    },
    {
-      .image_format = __DRI_IMAGE_FORMAT_GR1616,
-      .mesa_format  =        MESA_FORMAT_LA_UNORM16,
+      .image_format    = __DRI_IMAGE_FORMAT_GR1616,
+      .mesa_format     =        MESA_FORMAT_LA_UNORM16,
+      .internal_format =        GL_RG16,
    },
 #endif
 };
@@ -982,6 +1006,16 @@ driGLFormatToImageFormat(mesa_format format)
          return format_mapping[i].image_format;
 
    return __DRI_IMAGE_FORMAT_NONE;
+}
+
+uint32_t
+driGLFormatToSizedInternalGLFormat(mesa_format format)
+{
+   for (size_t i = 0; i < ARRAY_SIZE(format_mapping); i++)
+      if (format_mapping[i].mesa_format == format)
+         return format_mapping[i].internal_format;
+
+   return GL_NONE;
 }
 
 mesa_format

src/mesa/drivers/dri/common/dri_util.h
@@ -320,6 +320,9 @@ struct __DRIdrawableRec {
 extern uint32_t
 driGLFormatToImageFormat(mesa_format format);
 
+extern uint32_t
+driGLFormatToSizedInternalGLFormat(mesa_format format);
+
 extern mesa_format
 driImageFormatToGLFormat(uint32_t image_format);
 

commit 8c8b2337db3f6989b5040491864851fe495a2d7d

dri_util: Update internal_format to GL_RGB8 for MESA_FORMAT_R8G8B8X8_UNORM
The commit helps to resolve GL_INVALID_OPERATION error returned
during CTS test when Android format RGBX8888 fallback to RGBA8888
and then set color with glTexSubImage2D(format=GL_RGB).

Fixes android.hardware.nativehardware.cts.AHardwareBufferNativeTests:
 #SingleLayer_ColorTest_GpuSampledImageCanBeSampled_R8G8B8X8_UNORM

Cc: <mesa-stable@lists.freedesktop.org>
Fixes: 3dcca2c18f16 ("dri_util: add driImageFormatToSizedInternalGLFormat function")
Signed-off-by: Nataraj Deshpande <nataraj.deshpande@intel.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5034>

src/mesa/drivers/dri/common/dri_util.c
@@ -935,7 +935,7 @@ static const struct {
    {
       .image_format    = __DRI_IMAGE_FORMAT_XBGR8888,
       .mesa_format     =        MESA_FORMAT_R8G8B8X8_UNORM,
-      .internal_format =        GL_RGBA8,
+      .internal_format =        GL_RGB8,
    },
    {
       .image_format    = __DRI_IMAGE_FORMAT_R8,