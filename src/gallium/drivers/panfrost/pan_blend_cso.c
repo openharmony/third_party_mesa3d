openharmony/third_party_mesa3d

commit db5de004fbc4f0ae31a8a40350bf577defc79dfa

panfrost: Disable tib read/write when colourmask = 0x0
There might still be Z/S updates so we can't drop the whole shader but
we can shortcircuit the colour pipeline.

Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5124>

src/gallium/drivers/panfrost/pan_blend_cso.c
@@ -255,6 +255,8 @@ panfrost_get_blend_for_context(struct panfrost_context *ctx, unsigned rti, struc
                                 (rt->equation.alpha_mode == 0x122) &&
                                 (rt->equation.color_mask == 0xf);
 
+                        final.no_colour = (rt->equation.color_mask == 0x0);
+
                         return final;
                 }
         }

src/gallium/drivers/panfrost/pan_blend.h
@@ -99,9 +99,11 @@ struct panfrost_blend_final {
         /* Set for a shader, clear for an equation */
         bool is_shader;
 
-        /* Set if the destination needs to be loaded from the tilebuffer,
-         * basically (for an equation) or if a shader is present */
+        /* Clear if the destination needs to be loaded from the tilebuffer */
         bool no_blending;
+
+        /* Set if the colour mask is 0x0 (nothing is written) */
+        bool no_colour;
 
         union {
                 struct panfrost_blend_shader_final shader;

src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -725,16 +725,18 @@ panfrost_frag_meta_blend_update(struct panfrost_context *ctx,
                 } else {
                         struct midgard_blend_rt *mrts = rts;
 
-                        mrts[i].flags = 0x200;
+                        if (!blend[i].no_colour) {
+                                mrts[i].flags = 0x200;
 
-                        bool is_srgb = (ctx->pipe_framebuffer.nr_cbufs > i) &&
-                                       (ctx->pipe_framebuffer.cbufs[i]) &&
-                                       util_format_is_srgb(ctx->pipe_framebuffer.cbufs[i]->format);
+                                bool is_srgb = (ctx->pipe_framebuffer.nr_cbufs > i) &&
+                                               (ctx->pipe_framebuffer.cbufs[i]) &&
+                                               util_format_is_srgb(ctx->pipe_framebuffer.cbufs[i]->format);
 
-                        SET_BIT(mrts[i].flags, MALI_BLEND_MRT_SHADER, blend[i].is_shader);
-                        SET_BIT(mrts[i].flags, MALI_BLEND_LOAD_TIB, !blend[i].no_blending);
-                        SET_BIT(mrts[i].flags, MALI_BLEND_SRGB, is_srgb);
-                        SET_BIT(mrts[i].flags, MALI_BLEND_NO_DITHER, !ctx->blend->base.dither);
+                                SET_BIT(mrts[i].flags, MALI_BLEND_MRT_SHADER, blend[i].is_shader);
+                                SET_BIT(mrts[i].flags, MALI_BLEND_LOAD_TIB, !blend[i].no_blending);
+                                SET_BIT(mrts[i].flags, MALI_BLEND_SRGB, is_srgb);
+                                SET_BIT(mrts[i].flags, MALI_BLEND_NO_DITHER, !ctx->blend->base.dither);
+                        }
 
                         if (blend[i].is_shader) {
                                 mrts[i].blend.shader = blend[i].shader.gpu | blend[i].shader.first_tag;

commit 735855f2b9940c5f29d1811a6118a5bb6e624656

panfrost: Ensure final.no_colour is initialized.
Fix warning reported by Coverity Scan.

Uninitialized scalar variable (UNINIT)
uninit_use: Using uninitialized value final. Field final.no_colour is
uninitialized.

Fixes: db5de004fbc4 ("panfrost: Disable tib read/write when colourmask = 0x0")
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5183>

src/gallium/drivers/panfrost/pan_blend_cso.c
@@ -265,6 +265,7 @@ panfrost_get_blend_for_context(struct panfrost_context *ctx, unsigned rti, struc
         struct panfrost_blend_shader *shader = panfrost_get_blend_shader(ctx, blend, fmt, rti);
         final.is_shader = true;
         final.no_blending = false;
+        final.no_colour = false;
         final.shader.work_count = shader->work_count;
         final.shader.first_tag = shader->first_tag;
 