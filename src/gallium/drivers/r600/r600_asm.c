openharmony/third_party_mesa3d

commit 35362203ae32a6dceb2163f5a55be5afc14839ff

r600: force new CF with TEX only if any texture value is written
This works aound splitting the CF when the gradient is set.

Signed-off-by: Gert Wollny <gw.fossdev@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3225>

src/gallium/drivers/r600/r600_asm.c
@@ -1450,7 +1450,9 @@ int r600_bytecode_add_tex(struct r600_bytecode *bc, const struct r600_bytecode_t
 		bc->cf_last->op == CF_OP_TEX) {
 		struct r600_bytecode_tex *ttex;
 		LIST_FOR_EACH_ENTRY(ttex, &bc->cf_last->tex, list) {
-			if (ttex->dst_gpr == ntex->src_gpr) {
+			if (ttex->dst_gpr == ntex->src_gpr &&
+                            (ttex->dst_sel_x < 4 || ttex->dst_sel_y < 4 ||
+                             ttex->dst_sel_z < 4 || ttex->dst_sel_z < 4)) {
 				bc->force_add_cf = 1;
 				break;
 			}

commit 8c072c1840ec35d77423c3a6405f22e1737b7e17

r600: Fix duplicated subexpression in r600_asm.c
Fixes: 35362203ae32a6dceb2163f5a55be5afc14839ff
    r600: force new CF with TEX only if any texture value is written

Closes #3012

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5187>

src/gallium/drivers/r600/r600_asm.c
@@ -1452,7 +1452,7 @@ int r600_bytecode_add_tex(struct r600_bytecode *bc, const struct r600_bytecode_t
 		LIST_FOR_EACH_ENTRY(ttex, &bc->cf_last->tex, list) {
 			if (ttex->dst_gpr == ntex->src_gpr &&
                             (ttex->dst_sel_x < 4 || ttex->dst_sel_y < 4 ||
-                             ttex->dst_sel_z < 4 || ttex->dst_sel_z < 4)) {
+                             ttex->dst_sel_z < 4 || ttex->dst_sel_w < 4)) {
 				bc->force_add_cf = 1;
 				break;
 			}