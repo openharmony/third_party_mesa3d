openharmony/third_party_mesa3d

commit d51c606f3ff7ff4d79e861049c74a630a33f91e8

radv: Handle failing to create .cache dir.
Fixes: 77b891d07cd "radv: add initial non-conformant radv vulkan driver"
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5181>

src/amd/vulkan/radv_meta.c
@@ -262,7 +262,8 @@ radv_builtin_cache_path(char *path)
 
 	strcpy(path, pwd.pw_dir);
 	strcat(path, "/.cache");
-	mkdir(path, 0755);
+	if (mkdir(path, 0755))
+		return false;
 
 	ret = snprintf(path, PATH_MAX + 1, "%s%s%zd",
 		       pwd.pw_dir, suffix2, sizeof(void *) * 8);

commit f2d03bfbce6ee8ab73cc151b56fc54d4b6f48842

radv: fix regression with builtin cache
If the ~/.cache dir already exists continue on without failing.

Fixes: d51c606f3ff7 ("radv: Handle failing to create .cache dir.")

Tested-by: Dieter Nützel <Dieter@nuetzel-hh.de>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5249>

src/amd/vulkan/radv_meta.c
@@ -262,7 +262,7 @@ radv_builtin_cache_path(char *path)
 
 	strcpy(path, pwd.pw_dir);
 	strcat(path, "/.cache");
-	if (mkdir(path, 0755))
+	if (mkdir(path, 0755) && errno != EEXIST)
 		return false;
 
 	ret = snprintf(path, PATH_MAX + 1, "%s%s%zd",