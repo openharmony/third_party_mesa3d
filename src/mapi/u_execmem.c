aras-p/glsl-optimizer

commit a63b90712aad81d544eb8931493a6c4a7805f7fb

mesa: also check for __NetBSD__

src/mesa/main/execmem.c
@@ -36,7 +36,7 @@
 
 
 
-#if defined(__linux__) || defined(__OpenBSD__)
+#if defined(__linux__) || defined(__OpenBSD__) || defined(_NetBSD__)
 
 /*
  * Allocate a large block of memory which can hold code then dole it out

openharmony/third_party_mesa3d

commit 6c3ff6a0bbb2bcd48ad39bbe569eb1ac7cd0fd4e

mesa: Fix NetBSD compiler macro.
Reported-by: Rafał Mikrut <mikrutrafal54@gmail.com>
Fixes: a63b90712aad ("mesa: also check for __NetBSD__")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3015
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Eric Engestrom <eric@engestrom.ch>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5191>

src/mapi/u_execmem.c
@@ -46,7 +46,7 @@ static unsigned int head = 0;
 static unsigned char *exec_mem = (unsigned char *)0;
 
 
-#if defined(__linux__) || defined(__OpenBSD__) || defined(_NetBSD__) || defined(__sun) || defined(__HAIKU__)
+#if defined(__linux__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__sun) || defined(__HAIKU__)
 
 #include <unistd.h>
 #include <sys/mman.h>

src/mesa/main/execmem.c
@@ -37,7 +37,7 @@
 #include "c11/threads.h"
 
 
-#if defined(__linux__) || defined(__OpenBSD__) || defined(_NetBSD__) || defined(__sun) || defined(__HAIKU__)
+#if defined(__linux__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__sun) || defined(__HAIKU__)
 
 /*
  * Allocate a large block of memory which can hold code then dole it out