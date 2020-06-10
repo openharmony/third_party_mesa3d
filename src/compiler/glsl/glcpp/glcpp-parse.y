openharmony/third_party_mesa3d

commit 8592977f22b37583640b83ec2861ed9a46215d76

glsl: ignore trailing whitespace when define redefined
The Nvidia/AMD binary drivers allow this, as does GCC.

This fixes shader compilation issues in the latest update of
No Mans Sky.

Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>

src/compiler/glsl/glcpp/glcpp-parse.y
@@ -1077,6 +1077,20 @@ _token_list_equal_ignoring_space(token_list_t *a, token_list_t *b)
       if (node_a == NULL && node_b == NULL)
          break;
 
+      /* Ignore trailing whitespace */
+      if (node_a == NULL && node_b->token->type == SPACE) {
+         while (node_b && node_b->token->type == SPACE)
+            node_b = node_b->next;
+      }
+
+      if (node_b == NULL && node_a->token->type == SPACE) {
+         while (node_a && node_a->token->type == SPACE)
+            node_a = node_a->next;
+      }
+
+      if (node_a == NULL && node_b == NULL)
+         break;
+
       if (node_a == NULL || node_b == NULL)
          return 0;
       /* Make sure whitespace appears in the same places in both.

src/compiler/glsl/glcpp/tests/122-redefine-whitespace.c
@@ -2,12 +2,16 @@
 #define TWO  ( 1+1 )
 #define FOUR (2 + 2)
 #define SIX  (3 + 3)
+#define EIGHT (8 + 8)
 
 /* Redefinitions with whitespace in same places, but different amounts, (so no
  * error). */
 #define TWO	(	1+1   )
 #define FOUR    (2	+  2)
 #define SIX	(3/*comment is whitespace*/+   /* collapsed */ /* to */ /* one */ /* space */  3)
+
+/* Trailing whitespace (no error) */
+#define EIGHT (8 + 8)       
 
 /* Redefinitions with whitespace in different places. Each of these should
  * trigger an error. */

src/compiler/glsl/glcpp/tests/122-redefine-whitespace.c.expected
@@ -1,18 +1,22 @@
-0:14(9): preprocessor error: Redefinition of macro TWO
+0:18(9): preprocessor error: Redefinition of macro TWO
 
-0:15(9): preprocessor error: Redefinition of macro FOUR
+0:19(9): preprocessor error: Redefinition of macro FOUR
 
-0:16(9): preprocessor error: Redefinition of macro SIX
+0:20(9): preprocessor error: Redefinition of macro SIX
+
+ 
+
+
+
+
 
  
 
 
 
 
+
  
-
-
-
 
 
  

commit b900d753b830676a1b063ff88c850b7e541420d7

glsl: fix crash on glsl macro redefinition
In case shader contains two equal macro defines, first one with trailing spaces
and the second one without.
`#define A 1   `
`#define A 1`
The parser crashes

Fixes: 8592977f22b37583640b83ec2861ed9a46215d76 ("glsl: ignore trailing whitespace when define redefined")
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Signed-off-by: Andrii Simiklit <andrii.simiklit@globallogic.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5312>

src/compiler/glsl/glcpp/glcpp-parse.y
@@ -1187,6 +1187,9 @@ _token_list_equal_ignoring_space(token_list_t *a, token_list_t *b)
             node_b = node_b->next;
       }
 
+      if (node_a == NULL && node_b == NULL)
+         break;
+
       if (node_b == NULL && node_a->token->type == SPACE) {
          while (node_a && node_a->token->type == SPACE)
             node_a = node_a->next;