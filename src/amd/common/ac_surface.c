openharmony/third_party_mesa3d

commit 6ef83981a6a1b661a0b1eb67efacfcc76069f504

radeonsi: fix surf_pitch for subsampled surface
gfx9.surf_pitch is supposed to be in blocks (or elements) but addrlib
returns a pitch in pixels.
This cause a mismatch between surface->bpe and surface.u.gfx9.surf_pitch.

For subsampled formats like uyvy (bpe is 2) this breaks in various places:
 - sdma copy
 - video rendering (see issue https://gitlab.freedesktop.org/mesa/mesa/issues/2363)
   when the vl_compositor_gfx_render method is used

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/3738>

src/amd/common/ac_surface.c
@@ -1071,6 +1071,11 @@ static int gfx9_compute_miptree(ADDR_HANDLE addrlib,
 
 	surf->u.gfx9.surf_slice_size = out.sliceSize;
 	surf->u.gfx9.surf_pitch = out.pitch;
+	if (!compressed && surf->blk_w > 1 && out.pitch == out.pixelPitch) {
+		/* Adjust surf_pitch to be in elements units,
+		 * not in pixels */
+		surf->u.gfx9.surf_pitch /= surf->blk_w;
+	}
 	surf->u.gfx9.surf_height = out.height;
 	surf->surf_size = out.surfSize;
 	surf->surf_alignment = out.baseAlign;

src/gallium/drivers/radeonsi/si_compute_blit.c
@@ -398,17 +398,17 @@ void si_compute_copy_image(struct si_context *sctx,
 	assert(util_format_is_subsampled_422(src_format) ==
 	       util_format_is_subsampled_422(dst_format));
 
-	if (util_format_is_subsampled_422(src_format))
+	if (util_format_is_subsampled_422(src_format)) {
 		src_format = dst_format = PIPE_FORMAT_R32_UINT;
-
-	unsigned x_div = util_format_get_blockwidth(src->format) /
-	                 util_format_get_blockwidth(src_format);
-	assert(src_box->x % x_div == 0);
-	assert(width % x_div == 0);
-
-	unsigned data[] = {src_box->x / x_div, src_box->y, src_box->z, 0,
-	                   dstx / x_div, dsty, dstz, 0};
-	width /= x_div;
+		/* Interpreting 422 subsampled format (16 bpp) as 32 bpp
+		 * should force us to divide src_box->x, dstx and width by 2.
+		 * But given that ac_surface allocates this format as 32 bpp
+		 * and that surf_size is then modified to pack the values
+		 * we must keep the original values to get the correct results.
+		 */
+	}
+	unsigned data[] = {src_box->x, src_box->y, src_box->z, 0,
+	                   dstx, dsty, dstz, 0};
 
 	if (width == 0 || height == 0)
 		return;

commit 7303ff484eb0bf9adb83d207172e667b75fe8a63

ac/surface: fix epitch when modifying surf_pitch
This is needed otherwise it can cause bad rendering of UYVY files.
The align(..., 256 / surf->bpe) constraint comes from addrlib.

Fixes: 6ef83981a6a ("radeonsi: fix surf_pitch for subsampled surface")
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5314>

src/amd/common/ac_surface.c
@@ -1163,10 +1163,14 @@ static int gfx9_compute_miptree(ADDR_HANDLE addrlib,
 
 	surf->u.gfx9.surf_slice_size = out.sliceSize;
 	surf->u.gfx9.surf_pitch = out.pitch;
-	if (!compressed && surf->blk_w > 1 && out.pitch == out.pixelPitch) {
+	if (!compressed && surf->blk_w > 1 && out.pitch == out.pixelPitch &&
+	    surf->u.gfx9.surf.swizzle_mode == ADDR_SW_LINEAR) {
 		/* Adjust surf_pitch to be in elements units,
 		 * not in pixels */
-		surf->u.gfx9.surf_pitch /= surf->blk_w;
+		surf->u.gfx9.surf_pitch =
+			align(surf->u.gfx9.surf_pitch / surf->blk_w, 256 / surf->bpe);
+		surf->u.gfx9.surf.epitch = MAX2(surf->u.gfx9.surf.epitch,
+						surf->u.gfx9.surf_pitch * surf->blk_w - 1);
 	}
 	surf->u.gfx9.surf_height = out.height;
 	surf->surf_size = out.surfSize;