openharmony/third_party_mesa3d

commit fc17b1c25c0018e07dbf602459e4ed102dc17357

turnip: use the common code for generating extensions and dispatch tables
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Acked-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Eric Engestrom <eric@engestrom.ch>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4987>

src/freedreno/vulkan/tu_extensions.py
@@ -25,24 +25,17 @@ COPYRIGHT = """\
 """
 
 import argparse
-import copy
+import os.path
 import re
-import xml.etree.cElementTree as et
+import sys
 
-from mako.template import Template
+VULKAN_UTIL = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../vulkan/util'))
+sys.path.append(VULKAN_UTIL)
+
+from vk_extensions import *
+from vk_extensions_gen import *
 
 MAX_API_VERSION = '1.2.131'
-
-class Extension:
-    def __init__(self, name, ext_version, enable):
-        self.name = name
-        self.ext_version = int(ext_version)
-        if enable is True:
-            self.enable = 'true';
-        elif enable is False:
-            self.enable = 'false';
-        else:
-            self.enable = enable;
 
 # On Android, we disable all surface and swapchain extensions. Android's Vulkan
 # loader implements VK_KHR_surface and VK_KHR_swapchain, and applications
@@ -87,178 +80,8 @@ EXTENSIONS = [
     Extension('VK_EXT_filter_cubic',                      1, 'device->gpu_id == 650'),
 ]
 
-class VkVersion:
-    def __init__(self, string):
-        split = string.split('.')
-        self.major = int(split[0])
-        self.minor = int(split[1])
-        if len(split) > 2:
-            assert len(split) == 3
-            self.patch = int(split[2])
-        else:
-            self.patch = None
-
-        # Sanity check.  The range bits are required by the definition of the
-        # VK_MAKE_VERSION macro
-        assert self.major < 1024 and self.minor < 1024
-        assert self.patch is None or self.patch < 4096
-        assert(str(self) == string)
-
-    def __str__(self):
-        ver_list = [str(self.major), str(self.minor)]
-        if self.patch is not None:
-            ver_list.append(str(self.patch))
-        return '.'.join(ver_list)
-
-    def c_vk_version(self):
-        patch = self.patch if self.patch is not None else 0
-        ver_list = [str(self.major), str(self.minor), str(patch)]
-        return 'VK_MAKE_VERSION(' + ', '.join(ver_list) + ')'
-
-    def __int_ver(self):
-        # This is just an expansion of VK_VERSION
-        patch = self.patch if self.patch is not None else 0
-        return (self.major << 22) | (self.minor << 12) | patch
-
-    def __gt__(self, other):
-        # If only one of them has a patch version, "ignore" it by making
-        # other's patch version match self.
-        if (self.patch is None) != (other.patch is None):
-            other = copy.copy(other)
-            other.patch = self.patch
-
-        return self.__int_ver() > other.__int_ver()
-
 MAX_API_VERSION = VkVersion(MAX_API_VERSION)
-
-def _init_exts_from_xml(xml):
-    """ Walk the Vulkan XML and fill out extra extension information. """
-
-    xml = et.parse(xml)
-
-    ext_name_map = {}
-    for ext in EXTENSIONS:
-        ext_name_map[ext.name] = ext
-
-    for ext_elem in xml.findall('.extensions/extension'):
-        ext_name = ext_elem.attrib['name']
-        if ext_name not in ext_name_map:
-            continue
-
-        ext = ext_name_map[ext_name]
-        ext.type = ext_elem.attrib['type']
-
-_TEMPLATE_H = Template(COPYRIGHT + """
-#ifndef TU_EXTENSIONS_H
-#define TU_EXTENSIONS_H
-
-enum {
-   TU_INSTANCE_EXTENSION_COUNT = ${len(instance_extensions)},
-   TU_DEVICE_EXTENSION_COUNT = ${len(device_extensions)},
-};
-
-struct tu_instance_extension_table {
-   union {
-      bool extensions[TU_INSTANCE_EXTENSION_COUNT];
-      struct {
-%for ext in instance_extensions:
-        bool ${ext.name[3:]};
-%endfor
-      };
-   };
-};
-
-struct tu_device_extension_table {
-   union {
-      bool extensions[TU_DEVICE_EXTENSION_COUNT];
-      struct {
-%for ext in device_extensions:
-        bool ${ext.name[3:]};
-%endfor
-      };
-   };
-};
-
-extern const VkExtensionProperties tu_instance_extensions[TU_INSTANCE_EXTENSION_COUNT];
-extern const VkExtensionProperties tu_device_extensions[TU_DEVICE_EXTENSION_COUNT];
-extern const struct tu_instance_extension_table tu_supported_instance_extensions;
-
-
-struct tu_physical_device;
-
-void tu_fill_device_extension_table(const struct tu_physical_device *device,
-                                      struct tu_device_extension_table* table);
-#endif
-""")
-
-_TEMPLATE_C = Template(COPYRIGHT + """
-#include "tu_private.h"
-
-#include "vk_util.h"
-
-/* Convert the VK_USE_PLATFORM_* defines to booleans */
-%for platform in ['ANDROID_KHR', 'WAYLAND_KHR', 'XCB_KHR', 'XLIB_KHR', 'DISPLAY_KHR', 'XLIB_XRANDR_EXT']:
-#ifdef VK_USE_PLATFORM_${platform}
-#   undef VK_USE_PLATFORM_${platform}
-#   define VK_USE_PLATFORM_${platform} true
-#else
-#   define VK_USE_PLATFORM_${platform} false
-#endif
-%endfor
-
-/* And ANDROID too */
-#ifdef ANDROID
-#   undef ANDROID
-#   define ANDROID true
-#else
-#   define ANDROID false
-#endif
-
-#define TU_HAS_SURFACE (VK_USE_PLATFORM_WAYLAND_KHR || \\
-                         VK_USE_PLATFORM_XCB_KHR || \\
-                         VK_USE_PLATFORM_XLIB_KHR || \\
-                         VK_USE_PLATFORM_DISPLAY_KHR)
-
-
-const VkExtensionProperties tu_instance_extensions[TU_INSTANCE_EXTENSION_COUNT] = {
-%for ext in instance_extensions:
-   {"${ext.name}", ${ext.ext_version}},
-%endfor
-};
-
-const VkExtensionProperties tu_device_extensions[TU_DEVICE_EXTENSION_COUNT] = {
-%for ext in device_extensions:
-   {"${ext.name}", ${ext.ext_version}},
-%endfor
-};
-
-const struct tu_instance_extension_table tu_supported_instance_extensions = {
-%for ext in instance_extensions:
-   .${ext.name[3:]} = ${ext.enable},
-%endfor
-};
-
-void tu_fill_device_extension_table(const struct tu_physical_device *device,
-                                      struct tu_device_extension_table* table)
-{
-%for ext in device_extensions:
-   table->${ext.name[3:]} = ${ext.enable};
-%endfor
-}
-
-VkResult tu_EnumerateInstanceVersion(
-    uint32_t*                                   pApiVersion)
-{
-    *pApiVersion = ${MAX_API_VERSION.c_vk_version()};
-    return VK_SUCCESS;
-}
-
-uint32_t
-tu_physical_device_api_version(struct tu_physical_device *dev)
-{
-    return VK_MAKE_VERSION(1, 1, 82);
-}
-""")
+API_VERSIONS = []
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -271,19 +94,4 @@ if __name__ == '__main__':
                         dest='xml_files')
     args = parser.parse_args()
 
-    for filename in args.xml_files:
-        _init_exts_from_xml(filename)
-
-    for ext in EXTENSIONS:
-        assert ext.type == 'instance' or ext.type == 'device'
-
-    template_env = {
-        'MAX_API_VERSION': MAX_API_VERSION,
-        'instance_extensions': [e for e in EXTENSIONS if e.type == 'instance'],
-        'device_extensions': [e for e in EXTENSIONS if e.type == 'device'],
-    }
-
-    with open(args.out_c, 'w') as f:
-        f.write(_TEMPLATE_C.render(**template_env))
-    with open(args.out_h, 'w') as f:
-        f.write(_TEMPLATE_H.render(**template_env))
+    gen_extensions('tu', args.xml_files, API_VERSIONS, MAX_API_VERSION, EXTENSIONS, args.out_c, args.out_h)

src/freedreno/vulkan/tu_device.c
@@ -310,7 +310,7 @@ tu_physical_device_init(struct tu_physical_device *device,
    tu_get_driver_uuid(&device->device_uuid);
    tu_get_device_uuid(&device->device_uuid);
 
-   tu_fill_device_extension_table(device, &device->supported_extensions);
+   tu_physical_device_get_supported_extensions(device, &device->supported_extensions);
 
    if (result != VK_SUCCESS) {
       vk_error(instance, result);
@@ -445,7 +445,7 @@ tu_CreateInstance(const VkInstanceCreateInfo *pCreateInfo,
       const char *ext_name = pCreateInfo->ppEnabledExtensionNames[i];
       int index = tu_get_instance_extension_index(ext_name);
 
-      if (index < 0 || !tu_supported_instance_extensions.extensions[index]) {
+      if (index < 0 || !tu_instance_extensions_supported.extensions[index]) {
          vk_free2(&default_alloc, pAllocator, instance);
          return vk_error(instance, VK_ERROR_EXTENSION_NOT_PRESENT);
       }
@@ -1519,7 +1519,7 @@ tu_EnumerateInstanceExtensionProperties(const char *pLayerName,
       return vk_error(NULL, VK_ERROR_LAYER_NOT_PRESENT);
 
    for (int i = 0; i < TU_INSTANCE_EXTENSION_COUNT; i++) {
-      if (tu_supported_instance_extensions.extensions[i]) {
+      if (tu_instance_extensions_supported.extensions[i]) {
          vk_outarray_append(&out, prop) { *prop = tu_instance_extensions[i]; }
       }
    }

commit 14b13ad757f08759f2edd684e4f6fb652434bf0d

turnip: set the API version
Some CTS tests don't run because of this.

Fixes: fc17b1c25c0018e ("turnip: use the common code for generating extensions and dispatch tables")

Signed-off-by: Jonathan Marek <jonathan@marek.ca>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5522>

src/freedreno/vulkan/tu_extensions.py
@@ -82,7 +82,7 @@ EXTENSIONS = [
 ]
 
 MAX_API_VERSION = VkVersion(MAX_API_VERSION)
-API_VERSIONS = []
+API_VERSIONS = [ ApiVersion(MAX_API_VERSION,  True) ]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()