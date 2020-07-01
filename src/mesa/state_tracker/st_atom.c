openharmony/third_party_mesa3d

commit de5807a10b0e6043a6410e0c9923bef56d15a456

st/mesa: don't update atomic, SSBO, UBO and TBO states that have no effect
Tested-by: Edmondo Tommasina <edmondo.tommasina@gmail.com>
Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>

src/mesa/state_tracker/st_atom.c
@@ -157,9 +157,12 @@ void st_validate_state( struct st_context *st, enum st_pipeline pipeline )
    uint64_t dirty, pipeline_mask;
    uint32_t dirty_lo, dirty_hi;
 
-   /* Get Mesa driver state. */
-   st->dirty |= st->ctx->NewDriverState & ST_ALL_STATES_MASK;
-   st->ctx->NewDriverState = 0;
+   /* Get Mesa driver state.
+    *
+    * Inactive states are shader states not used by shaders at the moment.
+    */
+   st->dirty |= ctx->NewDriverState & st->active_states & ST_ALL_STATES_MASK;
+   ctx->NewDriverState = 0;
 
    /* Get pipeline state. */
    switch (pipeline) {

commit 560a4a0310b2dacc6f978e16c7bffbcd86ec5e17

st/mesa: do not clear NewDriverState for inactive states
Fixes: de5807a10b0 ("st/mesa: don't update atomic, SSBO, UBO and TBO states that have no effect")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/2951
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5660>

src/mesa/state_tracker/st_atom.c
@@ -181,7 +181,7 @@ void st_validate_state( struct st_context *st, enum st_pipeline pipeline )
     * Inactive states are shader states not used by shaders at the moment.
     */
    st->dirty |= ctx->NewDriverState & st->active_states & ST_ALL_STATES_MASK;
-   ctx->NewDriverState = 0;
+   ctx->NewDriverState &= ~st->dirty;
 
    /* Get pipeline state. */
    switch (pipeline) {