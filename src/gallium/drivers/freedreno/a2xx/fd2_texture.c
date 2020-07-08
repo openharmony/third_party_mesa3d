openharmony/third_party_mesa3d

commit 4bea70f13e6f1c03502982908121c80b3b4515cf

freedreno: Make the slice pitch be bytes, not pixels.
Back in a2xx, HW pitches were in pixels, so storing that was reasonable.
Ever since then, the HW wants pitches in bytes, and we have only one
instance of using pitch in pixels in the code (a3xx sysmem path).

Flip things around so that only a2xx has to worry about the cpp for
looking at pitches.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4558>

src/gallium/drivers/freedreno/a2xx/fd2_texture.c
@@ -187,7 +187,8 @@ fd2_sampler_view_create(struct pipe_context *pctx, struct pipe_resource *prsc,
 		A2XX_SQ_TEX_0_SIGN_Y(fmt.sign) |
 		A2XX_SQ_TEX_0_SIGN_Z(fmt.sign) |
 		A2XX_SQ_TEX_0_SIGN_W(fmt.sign) |
-		A2XX_SQ_TEX_0_PITCH(slice0->pitch) |
+		A2XX_SQ_TEX_0_PITCH(slice0->pitch >> fdl_cpp_shift(&rsc->layout) *
+				util_format_get_blockwidth(rsc->layout.format)) |
 		COND(rsc->layout.tile_mode, A2XX_SQ_TEX_0_TILED);
 	so->tex1 =
 		A2XX_SQ_TEX_1_FORMAT(fmt.format) |

src/freedreno/fdl/fd6_layout.c
@@ -155,12 +155,15 @@ fdl6_layout(struct fdl_layout *layout,
 		if (level == mip_levels - 1)
 			height = align(height, 32);
 
-		slice->pitch = util_align_npot(u_minify(pitch0, level),
+		uint32_t pitch_pixels = util_align_npot(u_minify(pitch0, level),
 				fdl6_pitchalign(layout, ta, level));
 
 		slice->offset = layout->size;
 		uint32_t blocks = util_format_get_nblocks(format,
-				slice->pitch, height);
+				pitch_pixels, height);
+
+		slice->pitch = util_format_get_nblocksx(format, pitch_pixels) *
+			layout->cpp;
 
 		/* 1d array and 2d array textures must all have the same layer size
 		 * for each miplevel on a6xx. 3d textures can have different layer

src/freedreno/fdl/fd6_layout_test.c
@@ -400,12 +400,6 @@ static bool test_layout(const struct testcase *testcase)
 			MAX2(testcase->array_size, 1),
 			testcase->is_3d);
 
-	/* Our pitch values in the testcases[] layouts are in bytes straight out
-	 * of the traces, while fdl is in pixels.  Rescale now.
-	 */
-	for (int l = 0; l < mip_levels; l++)
-		layout.slices[l].pitch *= layout.cpp;
-
 	/* fdl lays out UBWC data before the color data, while all we have
 	 * recorded in this testcase are the color offsets.  Shift the fdl layout
 	 * down so we can compare color offsets.

src/freedreno/fdl/freedreno_layout.c
@@ -56,9 +56,9 @@ fdl_dump_layout(struct fdl_layout *layout)
 				u_minify(layout->depth0, level),
 				layout->cpp, layout->nr_samples,
 				level,
-				slice->pitch * layout->cpp,
+				slice->pitch,
 				slice->size0, ubwc_slice->size0,
-				slice->size0 / (slice->pitch * layout->cpp),
+				slice->size0 / slice->pitch,
 				slice->offset, ubwc_slice->offset,
 				layout->layer_size, layout->ubwc_layer_size,
 				fdl_tile_mode(layout, level));

src/freedreno/fdl/freedreno_layout.h
@@ -79,7 +79,7 @@
 
 struct fdl_slice {
 	uint32_t offset;         /* offset of first layer in slice */
-	uint32_t pitch;
+	uint32_t pitch;          /* pitch in bytes between rows. */
 	uint32_t size0;          /* size of first layer in slice */
 };
 

src/freedreno/vulkan/tu_image.c
@@ -282,8 +282,7 @@ tu_image_view_init(struct tu_image_view *iview,
    uint64_t ubwc_addr = image->bo->iova + image->bo_offset +
       fdl_ubwc_offset(layout, range->baseMipLevel, range->baseArrayLayer);
 
-   uint32_t pitch = layout->slices[range->baseMipLevel].pitch * layout->cpp /
-                     util_format_get_blockwidth(layout->format);
+   uint32_t pitch = layout->slices[range->baseMipLevel].pitch;
    uint32_t ubwc_pitch = layout->ubwc_slices[range->baseMipLevel].pitch;
    uint32_t layer_size = fdl_layer_stride(layout, range->baseMipLevel);
 
@@ -513,7 +512,7 @@ tu_GetImageSubresourceLayout(VkDevice _device,
                                         pSubresource->arrayLayer);
    pLayout->size = slice->size0;
    pLayout->rowPitch =
-      slice->pitch * vk_format_get_blocksize(image->vk_format);
+      slice->pitch * vk_format_get_blockheight(image->vk_format);
    pLayout->arrayPitch = image->layout.layer_size;
    pLayout->depthPitch = slice->size0;
 

src/gallium/drivers/freedreno/a2xx/fd2_gmem.c
@@ -93,8 +93,9 @@ emit_gmem2mem_surf(struct fd_batch *batch, uint32_t base,
 	uint32_t offset =
 		fd_resource_offset(rsc, psurf->u.tex.level, psurf->u.tex.first_layer);
 	enum pipe_format format = fd_gmem_restore_format(psurf->format);
+	uint32_t pitch = slice->pitch >> fdl_cpp_shift(&rsc->layout);
 
-	assert((slice->pitch & 31) == 0);
+	assert((pitch & 31) == 0);
 	assert((offset & 0xfff) == 0);
 
 	if (!rsc->valid)
@@ -109,7 +110,7 @@ emit_gmem2mem_surf(struct fd_batch *batch, uint32_t base,
 	OUT_RING(ring, CP_REG(REG_A2XX_RB_COPY_CONTROL));
 	OUT_RING(ring, 0x00000000);             /* RB_COPY_CONTROL */
 	OUT_RELOCW(ring, rsc->bo, offset, 0, 0);     /* RB_COPY_DEST_BASE */
-	OUT_RING(ring, slice->pitch >> 5); /* RB_COPY_DEST_PITCH */
+	OUT_RING(ring, pitch >> 5); /* RB_COPY_DEST_PITCH */
 	OUT_RING(ring,                          /* RB_COPY_DEST_INFO */
 			A2XX_RB_COPY_DEST_INFO_FORMAT(fd2_pipe2color(format)) |
 			COND(!rsc->layout.tile_mode, A2XX_RB_COPY_DEST_INFO_LINEAR) |
@@ -245,7 +246,7 @@ emit_mem2gmem_surf(struct fd_batch *batch, uint32_t base,
 	OUT_RING(ring, A2XX_SQ_TEX_0_CLAMP_X(SQ_TEX_WRAP) |
 			A2XX_SQ_TEX_0_CLAMP_Y(SQ_TEX_WRAP) |
 			A2XX_SQ_TEX_0_CLAMP_Z(SQ_TEX_WRAP) |
-			A2XX_SQ_TEX_0_PITCH(slice->pitch));
+			A2XX_SQ_TEX_0_PITCH(slice->pitch >> fdl_cpp_shift(&rsc->layout)));
 	OUT_RELOC(ring, rsc->bo, offset,
 			A2XX_SQ_TEX_1_FORMAT(fd2_pipe2surface(format).format) |
 			A2XX_SQ_TEX_1_CLAMP_POLICY(SQ_TEX_CLAMP_POLICY_OGL), 0);
@@ -438,15 +439,16 @@ fd2_emit_sysmem_prep(struct fd_batch *batch)
 	struct fdl_slice *slice = fd_resource_slice(rsc, psurf->u.tex.level);
 	uint32_t offset =
 		fd_resource_offset(rsc, psurf->u.tex.level, psurf->u.tex.first_layer);
+	uint32_t pitch = slice->pitch >> fdl_cpp_shift(&rsc->layout);
 
-	assert((slice->pitch & 31) == 0);
+	assert((pitch & 31) == 0);
 	assert((offset & 0xfff) == 0);
 
 	fd2_emit_restore(ctx, ring);
 
 	OUT_PKT3(ring, CP_SET_CONSTANT, 2);
 	OUT_RING(ring, CP_REG(REG_A2XX_RB_SURFACE_INFO));
-	OUT_RING(ring, A2XX_RB_SURFACE_INFO_SURFACE_PITCH(slice->pitch));
+	OUT_RING(ring, A2XX_RB_SURFACE_INFO_SURFACE_PITCH(pitch));
 
 	OUT_PKT3(ring, CP_SET_CONSTANT, 2);
 	OUT_RING(ring, CP_REG(REG_A2XX_RB_COLOR_INFO));

src/gallium/drivers/freedreno/a2xx/fd2_resource.c
@@ -61,7 +61,7 @@ fd2_setup_slices(struct fd_resource *rsc)
 			height = util_next_power_of_two(height);
 		}
 
-		slice->pitch = width;
+		slice->pitch = util_format_get_nblocksx(format, width) * rsc->layout.cpp;
 		slice->offset = size;
 
 		blocks = util_format_get_nblocks(format, width, height);

commit ef27cebf462e8b66e6bbcb124f342143f6d6bacc

freedreno/a2xx: fix compressed textures
Two problems:
* Multiply has higher priority than shift
* rsc->layout.format isn't initialized for a2xx

Fixes: 4bea70f13e6f1 ("freedreno: Make the slice pitch be bytes, not pixels.")

Signed-off-by: Jonathan Marek <jonathan@marek.ca>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5796>

src/gallium/drivers/freedreno/a2xx/fd2_texture.c
@@ -187,8 +187,8 @@ fd2_sampler_view_create(struct pipe_context *pctx, struct pipe_resource *prsc,
 		A2XX_SQ_TEX_0_SIGN_Y(fmt.sign) |
 		A2XX_SQ_TEX_0_SIGN_Z(fmt.sign) |
 		A2XX_SQ_TEX_0_SIGN_W(fmt.sign) |
-		A2XX_SQ_TEX_0_PITCH(slice0->pitch >> fdl_cpp_shift(&rsc->layout) *
-				util_format_get_blockwidth(rsc->layout.format)) |
+		A2XX_SQ_TEX_0_PITCH((slice0->pitch >> fdl_cpp_shift(&rsc->layout)) *
+				util_format_get_blockwidth(prsc->format)) |
 		COND(rsc->layout.tile_mode, A2XX_SQ_TEX_0_TILED);
 	so->tex1 =
 		A2XX_SQ_TEX_1_FORMAT(fmt.format) |