openharmony/third_party_mesa3d

commit aa71fe814c17b995302efe3d9f6e965b09408cb6

glsl,nir: Switch the enum representing shader image formats to PIPE_FORMAT.
This means you can directly use format utils on it without having to have
your own GL enum to number-of-components switch statement (or whatever) in
your vulkan backend.

Thanks to imirkin for fixing up the nouveau driver (and a couple of core
details).

This fixes the computed qualifiers for EXT_shader_image_load_store's
non-integer sizeNxM qualifiers, which we don't have tests for.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Iago Toral Quiroga <itoral@igalia.com> (v3d)
Tested-by: Marge Bot <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3355>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3355>

src/compiler/glsl/ast_to_hir.cpp
@@ -3497,7 +3497,7 @@ apply_image_qualifier_to_variable(const struct ast_type_qualifier *qual,
                              "`writeonly' must have a format layout qualifier");
          }
       }
-      var->data.image_format = GL_NONE;
+      var->data.image_format = PIPE_FORMAT_NONE;
    }
 
    /* From page 70 of the GLSL ES 3.1 specification:
@@ -3507,9 +3507,9 @@ apply_image_qualifier_to_variable(const struct ast_type_qualifier *qual,
     *  readonly or the memory qualifier writeonly."
     */
    if (state->es_shader &&
-       var->data.image_format != GL_R32F &&
-       var->data.image_format != GL_R32I &&
-       var->data.image_format != GL_R32UI &&
+       var->data.image_format != PIPE_FORMAT_R32_FLOAT &&
+       var->data.image_format != PIPE_FORMAT_R32_SINT &&
+       var->data.image_format != PIPE_FORMAT_R32_UINT &&
        !var->data.memory_read_only &&
        !var->data.memory_write_only) {
       _mesa_glsl_error(loc, state, "image variables of format other than r32f, "
@@ -4947,12 +4947,50 @@ ast_declarator_list::hir(exec_list *instructions,
        *       size4x32      rgba32f   rgba32i   rgba32ui"
        */
       if (strncmp(this->type->specifier->type_name, "image", strlen("image")) == 0) {
-         this->type->qualifier.image_format = GL_R8 +
-                                         this->type->qualifier.image_format - GL_R8I;
+         switch (this->type->qualifier.image_format) {
+         case PIPE_FORMAT_R8_SINT:
+            /* No valid qualifier in this case, driver will need to look at
+             * the underlying image's format (just like no qualifier being
+             * present).
+             */
+            this->type->qualifier.image_format = PIPE_FORMAT_NONE;
+            break;
+         case PIPE_FORMAT_R16_SINT:
+            this->type->qualifier.image_format = PIPE_FORMAT_R16_FLOAT;
+            break;
+         case PIPE_FORMAT_R32_SINT:
+            this->type->qualifier.image_format = PIPE_FORMAT_R32_FLOAT;
+            break;
+         case PIPE_FORMAT_R32G32_SINT:
+            this->type->qualifier.image_format = PIPE_FORMAT_R32G32_FLOAT;
+            break;
+         case PIPE_FORMAT_R32G32B32A32_SINT:
+            this->type->qualifier.image_format = PIPE_FORMAT_R32G32B32A32_FLOAT;
+            break;
+         default:
+            unreachable("Unknown image format");
+         }
          this->type->qualifier.image_base_type = GLSL_TYPE_FLOAT;
       } else if (strncmp(this->type->specifier->type_name, "uimage", strlen("uimage")) == 0) {
-         this->type->qualifier.image_format = GL_R8UI +
-                                         this->type->qualifier.image_format - GL_R8I;
+         switch (this->type->qualifier.image_format) {
+         case PIPE_FORMAT_R8_SINT:
+            this->type->qualifier.image_format = PIPE_FORMAT_R8_UINT;
+            break;
+         case PIPE_FORMAT_R16_SINT:
+            this->type->qualifier.image_format = PIPE_FORMAT_R16_UINT;
+            break;
+         case PIPE_FORMAT_R32_SINT:
+            this->type->qualifier.image_format = PIPE_FORMAT_R32_UINT;
+            break;
+         case PIPE_FORMAT_R32G32_SINT:
+            this->type->qualifier.image_format = PIPE_FORMAT_R32G32_UINT;
+            break;
+         case PIPE_FORMAT_R32G32B32A32_SINT:
+            this->type->qualifier.image_format = PIPE_FORMAT_R32G32B32A32_UINT;
+            break;
+         default:
+            unreachable("Unknown image format");
+         }
          this->type->qualifier.image_base_type = GLSL_TYPE_UINT;
       } else if (strncmp(this->type->specifier->type_name, "iimage", strlen("iimage")) == 0) {
          this->type->qualifier.image_base_type = GLSL_TYPE_INT;
@@ -7630,7 +7668,7 @@ ast_process_struct_or_iface_block_members(exec_list *instructions,
                                       "qualifier");
                   }
 
-                  fields[i].image_format = GL_NONE;
+                  fields[i].image_format = PIPE_FORMAT_NONE;
                }
             }
          }

src/broadcom/compiler/v3d_nir_lower_image_load_store.c
@@ -41,53 +41,13 @@
  */
 
 bool
-v3d_gl_format_is_return_32(GLenum format)
+v3d_gl_format_is_return_32(enum pipe_format format)
 {
-        switch (format) {
-        case GL_R8:
-        case GL_R8_SNORM:
-        case GL_R8UI:
-        case GL_R8I:
-        case GL_RG8:
-        case GL_RG8_SNORM:
-        case GL_RG8UI:
-        case GL_RG8I:
-        case GL_RGBA8:
-        case GL_RGBA8_SNORM:
-        case GL_RGBA8UI:
-        case GL_RGBA8I:
-        case GL_R11F_G11F_B10F:
-        case GL_RGB10_A2:
-        case GL_RGB10_A2UI:
-        case GL_R16F:
-        case GL_R16UI:
-        case GL_R16I:
-        case GL_RG16F:
-        case GL_RG16UI:
-        case GL_RG16I:
-        case GL_RGBA16F:
-        case GL_RGBA16UI:
-        case GL_RGBA16I:
-                return false;
-        case GL_R16:
-        case GL_R16_SNORM:
-        case GL_RG16:
-        case GL_RG16_SNORM:
-        case GL_RGBA16:
-        case GL_RGBA16_SNORM:
-        case GL_R32F:
-        case GL_R32UI:
-        case GL_R32I:
-        case GL_RG32F:
-        case GL_RG32UI:
-        case GL_RG32I:
-        case GL_RGBA32F:
-        case GL_RGBA32UI:
-        case GL_RGBA32I:
-                return true;
-        default:
-                unreachable("Invalid image format");
-        }
+        const struct util_format_description *desc =
+                util_format_description(format);
+        const struct util_format_channel_description *chan = &desc->channel[0];
+
+        return chan->size > 16 || (chan->size == 16 && chan->normalized);
 }
 
 /* Packs a 32-bit vector of colors in the range [0, (1 << bits[i]) - 1] to a
@@ -124,192 +84,75 @@ pack_bits(nir_builder *b, nir_ssa_def *color, const unsigned *bits,
         return nir_vec(b, results, DIV_ROUND_UP(offset, 32));
 }
 
-static nir_ssa_def *
-pack_unorm(nir_builder *b, nir_ssa_def *color, const unsigned *bits,
-           int num_components)
-{
-        color = nir_channels(b, color, (1 << num_components) - 1);
-        color = nir_format_float_to_unorm(b, color, bits);
-        return pack_bits(b, color, bits, color->num_components, false);
-}
-
-static nir_ssa_def *
-pack_snorm(nir_builder *b, nir_ssa_def *color, const unsigned *bits,
-           int num_components)
-{
-        color = nir_channels(b, color, (1 << num_components) - 1);
-        color = nir_format_float_to_snorm(b, color, bits);
-        return pack_bits(b, color, bits, color->num_components, true);
-}
-
-static nir_ssa_def *
-pack_uint(nir_builder *b, nir_ssa_def *color, const unsigned *bits,
-          int num_components)
-{
-        color = nir_channels(b, color, (1 << num_components) - 1);
-        color = nir_format_clamp_uint(b, color, bits);
-        return pack_bits(b, color, bits, num_components, false);
-}
-
-static nir_ssa_def *
-pack_sint(nir_builder *b, nir_ssa_def *color, const unsigned *bits,
-          int num_components)
-{
-        color = nir_channels(b, color, (1 << num_components) - 1);
-        color = nir_format_clamp_sint(b, color, bits);
-        return pack_bits(b, color, bits, num_components, true);
-}
-
-static nir_ssa_def *
-pack_half(nir_builder *b, nir_ssa_def *color, const unsigned *bits,
-          int num_components)
-{
-        color = nir_channels(b, color, (1 << num_components) - 1);
-        color = nir_format_float_to_half(b, color);
-        return pack_bits(b, color, bits, color->num_components, false);
-}
-
 static void
 v3d_nir_lower_image_store(nir_builder *b, nir_intrinsic_instr *instr)
 {
         nir_variable *var = nir_intrinsic_get_var(instr, 0);
-        GLenum format = var->data.image.format;
-        static const unsigned bits_8[4] = {8, 8, 8, 8};
-        static const unsigned bits_16[4] = {16, 16, 16, 16};
-        static const unsigned bits_1010102[4] = {10, 10, 10, 2};
+        enum pipe_format format = var->data.image.format;
+        const struct util_format_description *desc =
+                util_format_description(format);
+        const struct util_format_channel_description *r_chan = &desc->channel[0];
+        unsigned num_components = util_format_get_nr_components(format);
 
         b->cursor = nir_before_instr(&instr->instr);
 
-        nir_ssa_def *unformatted = nir_ssa_for_src(b, instr->src[3], 4);
+        nir_ssa_def *color = nir_channels(b,
+                                          nir_ssa_for_src(b, instr->src[3], 4),
+                                          (1 << num_components) - 1);
         nir_ssa_def *formatted = NULL;
-        switch (format) {
-        case GL_RGBA32F:
-        case GL_RGBA32UI:
-        case GL_RGBA32I:
-                /* For 4-component 32-bit components, there's no packing to be
-                 * done.
+
+        if (format == PIPE_FORMAT_R11G11B10_FLOAT) {
+                formatted = nir_format_pack_11f11f10f(b, color);
+        } else if (format == PIPE_FORMAT_R9G9B9E5_FLOAT) {
+                formatted = nir_format_pack_r9g9b9e5(b, color);
+        } else if (r_chan->size == 32) {
+                /* For 32-bit formats, we just have to move the vector
+                 * across (possibly reducing the number of channels).
                  */
-                return;
+                formatted = color;
+        } else {
+                static const unsigned bits_8[4] = {8, 8, 8, 8};
+                static const unsigned bits_16[4] = {16, 16, 16, 16};
+                static const unsigned bits_1010102[4] = {10, 10, 10, 2};
+                const unsigned *bits;
 
-        case GL_R32F:
-        case GL_R32UI:
-        case GL_R32I:
-                /* For other 32-bit components, just reduce the size of
-                 * the input vector.
-                 */
-                formatted = nir_channels(b, unformatted, 1);
-                break;
-        case GL_RG32F:
-        case GL_RG32UI:
-        case GL_RG32I:
-                formatted = nir_channels(b, unformatted, 2);
-                break;
+                switch (r_chan->size) {
+                case 8:
+                        bits = bits_8;
+                        break;
+                case 10:
+                        bits = bits_1010102;
+                        break;
+                case 16:
+                        bits = bits_16;
+                        break;
+                default:
+                        unreachable("unrecognized bits");
+                }
 
-        case GL_R8:
-                formatted = pack_unorm(b, unformatted, bits_8, 1);
-                break;
-        case GL_RG8:
-                formatted = pack_unorm(b, unformatted, bits_8, 2);
-                break;
-        case GL_RGBA8:
-                formatted = pack_unorm(b, unformatted, bits_8, 4);
-                break;
+                bool pack_mask = false;
+                if (r_chan->pure_integer &&
+                    r_chan->type == UTIL_FORMAT_TYPE_SIGNED) {
+                        formatted = nir_format_clamp_sint(b, color, bits);
+                        pack_mask = true;
+                } else if (r_chan->pure_integer &&
+                           r_chan->type == UTIL_FORMAT_TYPE_UNSIGNED) {
+                        formatted = nir_format_clamp_uint(b, color, bits);
+                } else if (r_chan->normalized &&
+                           r_chan->type == UTIL_FORMAT_TYPE_SIGNED) {
+                        formatted = nir_format_float_to_snorm(b, color, bits);
+                        pack_mask = true;
+                } else if (r_chan->normalized &&
+                           r_chan->type == UTIL_FORMAT_TYPE_UNSIGNED) {
+                        formatted = nir_format_float_to_unorm(b, color, bits);
+                } else {
+                        assert(r_chan->size == 16);
+                        assert(r_chan->type == UTIL_FORMAT_TYPE_FLOAT);
+                        formatted = nir_format_float_to_half(b, color);
+                }
 
-        case GL_R8_SNORM:
-                formatted = pack_snorm(b, unformatted, bits_8, 1);
-                break;
-        case GL_RG8_SNORM:
-                formatted = pack_snorm(b, unformatted, bits_8, 2);
-                break;
-        case GL_RGBA8_SNORM:
-                formatted = pack_snorm(b, unformatted, bits_8, 4);
-                break;
-
-        case GL_R16:
-                formatted = pack_unorm(b, unformatted, bits_16, 1);
-                break;
-        case GL_RG16:
-                formatted = pack_unorm(b, unformatted, bits_16, 2);
-                break;
-        case GL_RGBA16:
-                formatted = pack_unorm(b, unformatted, bits_16, 4);
-                break;
-
-        case GL_R16_SNORM:
-                formatted = pack_snorm(b, unformatted, bits_16, 1);
-                break;
-        case GL_RG16_SNORM:
-                formatted = pack_snorm(b, unformatted, bits_16, 2);
-                break;
-        case GL_RGBA16_SNORM:
-                formatted = pack_snorm(b, unformatted, bits_16, 4);
-                break;
-
-        case GL_R16F:
-                formatted = pack_half(b, unformatted, bits_16, 1);
-                break;
-        case GL_RG16F:
-                formatted = pack_half(b, unformatted, bits_16, 2);
-                break;
-        case GL_RGBA16F:
-                formatted = pack_half(b, unformatted, bits_16, 4);
-                break;
-
-        case GL_R8UI:
-                formatted = pack_uint(b, unformatted, bits_8, 1);
-                break;
-        case GL_R8I:
-                formatted = pack_sint(b, unformatted, bits_8, 1);
-                break;
-        case GL_RG8UI:
-                formatted = pack_uint(b, unformatted, bits_8, 2);
-                break;
-        case GL_RG8I:
-                formatted = pack_sint(b, unformatted, bits_8, 2);
-                break;
-        case GL_RGBA8UI:
-                formatted = pack_uint(b, unformatted, bits_8, 4);
-                break;
-        case GL_RGBA8I:
-                formatted = pack_sint(b, unformatted, bits_8, 4);
-                break;
-
-        case GL_R16UI:
-                formatted = pack_uint(b, unformatted, bits_16, 1);
-                break;
-        case GL_R16I:
-                formatted = pack_sint(b, unformatted, bits_16, 1);
-                break;
-        case GL_RG16UI:
-                formatted = pack_uint(b, unformatted, bits_16, 2);
-                break;
-        case GL_RG16I:
-                formatted = pack_sint(b, unformatted, bits_16, 2);
-                break;
-        case GL_RGBA16UI:
-                formatted = pack_uint(b, unformatted, bits_16, 4);
-                break;
-        case GL_RGBA16I:
-                formatted = pack_sint(b, unformatted, bits_16, 4);
-                break;
-
-        case GL_R11F_G11F_B10F:
-                formatted = nir_format_pack_11f11f10f(b, unformatted);
-                break;
-        case GL_RGB9_E5:
-                formatted = nir_format_pack_r9g9b9e5(b, unformatted);
-                break;
-
-        case GL_RGB10_A2:
-                formatted = pack_unorm(b, unformatted, bits_1010102, 4);
-                break;
-
-        case GL_RGB10_A2UI:
-                formatted = pack_uint(b, unformatted, bits_1010102, 4);
-                break;
-
-        default:
-                unreachable("bad format");
+                formatted = pack_bits(b, formatted, bits, num_components,
+                                      pack_mask);
         }
 
         nir_instr_rewrite_src(&instr->instr, &instr->src[3],

src/compiler/glsl/ast.h
@@ -773,7 +773,7 @@ struct ast_type_qualifier {
     * \note
     * This field is only valid if \c explicit_image_format is set.
     */
-   GLenum image_format;
+   enum pipe_format image_format;
 
    /**
     * Arrangement of invocations used to calculate derivatives in a compute

src/compiler/glsl/builtin_variables.cpp
@@ -351,7 +351,7 @@ per_vertex_accumulator::add_field(int slot, const glsl_type *type,
    this->fields[this->num_fields].memory_coherent = 0;
    this->fields[this->num_fields].memory_volatile = 0;
    this->fields[this->num_fields].memory_restrict = 0;
-   this->fields[this->num_fields].image_format = 0;
+   this->fields[this->num_fields].image_format = PIPE_FORMAT_NONE;
    this->fields[this->num_fields].explicit_xfb_buffer = 0;
    this->fields[this->num_fields].xfb_buffer = -1;
    this->fields[this->num_fields].xfb_stride = -1;

src/compiler/glsl/glsl_parser.yy
@@ -34,6 +34,7 @@
 #include "compiler/glsl_types.h"
 #include "main/context.h"
 #include "util/u_string.h"
+#include "util/format/u_format.h"
 
 #ifdef _MSC_VER
 #pragma warning( disable : 4065 ) // switch statement contains 'default' but no 'case' labels
@@ -1336,7 +1337,7 @@ layout_qualifier_id:
          if (!$$.flags.i) {
             static const struct {
                const char *name;
-               GLenum format;
+               enum pipe_format format;
                glsl_base_type base_type;
                /** Minimum desktop GLSL version required for the image
                 * format.  Use 130 if already present in the original
@@ -1349,54 +1350,54 @@ layout_qualifier_id:
                bool nv_image_formats;
                bool ext_qualifiers;
             } map[] = {
-               { "rgba32f", GL_RGBA32F, GLSL_TYPE_FLOAT, 130, 310, false, false },
-               { "rgba16f", GL_RGBA16F, GLSL_TYPE_FLOAT, 130, 310, false, false },
-               { "rg32f", GL_RG32F, GLSL_TYPE_FLOAT, 130, 0, true, false },
-               { "rg16f", GL_RG16F, GLSL_TYPE_FLOAT, 130, 0, true, false },
-               { "r11f_g11f_b10f", GL_R11F_G11F_B10F, GLSL_TYPE_FLOAT, 130, 0, true, false },
-               { "r32f", GL_R32F, GLSL_TYPE_FLOAT, 130, 310, false, false },
-               { "r16f", GL_R16F, GLSL_TYPE_FLOAT, 130, 0, true, false },
-               { "rgba32ui", GL_RGBA32UI, GLSL_TYPE_UINT, 130, 310, false, false },
-               { "rgba16ui", GL_RGBA16UI, GLSL_TYPE_UINT, 130, 310, false, false },
-               { "rgb10_a2ui", GL_RGB10_A2UI, GLSL_TYPE_UINT, 130, 0, true, false },
-               { "rgba8ui", GL_RGBA8UI, GLSL_TYPE_UINT, 130, 310, false, false },
-               { "rg32ui", GL_RG32UI, GLSL_TYPE_UINT, 130, 0, true, false },
-               { "rg16ui", GL_RG16UI, GLSL_TYPE_UINT, 130, 0, true, false },
-               { "rg8ui", GL_RG8UI, GLSL_TYPE_UINT, 130, 0, true, false },
-               { "r32ui", GL_R32UI, GLSL_TYPE_UINT, 130, 310, false, false },
-               { "r16ui", GL_R16UI, GLSL_TYPE_UINT, 130, 0, true, false },
-               { "r8ui", GL_R8UI, GLSL_TYPE_UINT, 130, 0, true, false },
-               { "rgba32i", GL_RGBA32I, GLSL_TYPE_INT, 130, 310, false, false },
-               { "rgba16i", GL_RGBA16I, GLSL_TYPE_INT, 130, 310, false, false },
-               { "rgba8i", GL_RGBA8I, GLSL_TYPE_INT, 130, 310, false, false },
-               { "rg32i", GL_RG32I, GLSL_TYPE_INT, 130, 0, true, false },
-               { "rg16i", GL_RG16I, GLSL_TYPE_INT, 130, 0, true, false },
-               { "rg8i", GL_RG8I, GLSL_TYPE_INT, 130, 0, true, false },
-               { "r32i", GL_R32I, GLSL_TYPE_INT, 130, 310, false, false },
-               { "r16i", GL_R16I, GLSL_TYPE_INT, 130, 0, true, false },
-               { "r8i", GL_R8I, GLSL_TYPE_INT, 130, 0, true, false },
-               { "rgba16", GL_RGBA16, GLSL_TYPE_FLOAT, 130, 0, true, false },
-               { "rgb10_a2", GL_RGB10_A2, GLSL_TYPE_FLOAT, 130, 0, true, false },
-               { "rgba8", GL_RGBA8, GLSL_TYPE_FLOAT, 130, 310, false, false },
-               { "rg16", GL_RG16, GLSL_TYPE_FLOAT, 130, 0, true, false },
-               { "rg8", GL_RG8, GLSL_TYPE_FLOAT, 130, 0, true, false },
-               { "r16", GL_R16, GLSL_TYPE_FLOAT, 130, 0, true, false },
-               { "r8", GL_R8, GLSL_TYPE_FLOAT, 130, 0, true, false },
-               { "rgba16_snorm", GL_RGBA16_SNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
-               { "rgba8_snorm", GL_RGBA8_SNORM, GLSL_TYPE_FLOAT, 130, 310, false, false },
-               { "rg16_snorm", GL_RG16_SNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
-               { "rg8_snorm", GL_RG8_SNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
-               { "r16_snorm", GL_R16_SNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
-               { "r8_snorm", GL_R8_SNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "rgba32f", PIPE_FORMAT_R32G32B32A32_FLOAT, GLSL_TYPE_FLOAT, 130, 310, false, false },
+               { "rgba16f", PIPE_FORMAT_R16G16B16A16_FLOAT, GLSL_TYPE_FLOAT, 130, 310, false, false },
+               { "rg32f", PIPE_FORMAT_R32G32_FLOAT, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "rg16f", PIPE_FORMAT_R16G16_FLOAT, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "r11f_g11f_b10f", PIPE_FORMAT_R11G11B10_FLOAT, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "r32f", PIPE_FORMAT_R32_FLOAT, GLSL_TYPE_FLOAT, 130, 310, false, false },
+               { "r16f", PIPE_FORMAT_R16_FLOAT, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "rgba32ui", PIPE_FORMAT_R32G32B32A32_UINT, GLSL_TYPE_UINT, 130, 310, false, false },
+               { "rgba16ui", PIPE_FORMAT_R16G16B16A16_UINT, GLSL_TYPE_UINT, 130, 310, false, false },
+               { "rgb10_a2ui", PIPE_FORMAT_R10G10B10A2_UINT, GLSL_TYPE_UINT, 130, 0, true, false },
+               { "rgba8ui", PIPE_FORMAT_R8G8B8A8_UINT, GLSL_TYPE_UINT, 130, 310, false, false },
+               { "rg32ui", PIPE_FORMAT_R32G32_UINT, GLSL_TYPE_UINT, 130, 0, true, false },
+               { "rg16ui", PIPE_FORMAT_R16G16_UINT, GLSL_TYPE_UINT, 130, 0, true, false },
+               { "rg8ui", PIPE_FORMAT_R8G8_UINT, GLSL_TYPE_UINT, 130, 0, true, false },
+               { "r32ui", PIPE_FORMAT_R32_UINT, GLSL_TYPE_UINT, 130, 310, false, false },
+               { "r16ui", PIPE_FORMAT_R16_UINT, GLSL_TYPE_UINT, 130, 0, true, false },
+               { "r8ui", PIPE_FORMAT_R8_UINT, GLSL_TYPE_UINT, 130, 0, true, false },
+               { "rgba32i", PIPE_FORMAT_R32G32B32A32_SINT, GLSL_TYPE_INT, 130, 310, false, false },
+               { "rgba16i", PIPE_FORMAT_R16G16B16A16_SINT, GLSL_TYPE_INT, 130, 310, false, false },
+               { "rgba8i", PIPE_FORMAT_R8G8B8A8_SINT, GLSL_TYPE_INT, 130, 310, false, false },
+               { "rg32i", PIPE_FORMAT_R32G32_SINT, GLSL_TYPE_INT, 130, 0, true, false },
+               { "rg16i", PIPE_FORMAT_R16G16_SINT, GLSL_TYPE_INT, 130, 0, true, false },
+               { "rg8i", PIPE_FORMAT_R8G8_SINT, GLSL_TYPE_INT, 130, 0, true, false },
+               { "r32i", PIPE_FORMAT_R32_SINT, GLSL_TYPE_INT, 130, 310, false, false },
+               { "r16i", PIPE_FORMAT_R16_SINT, GLSL_TYPE_INT, 130, 0, true, false },
+               { "r8i", PIPE_FORMAT_R8_SINT, GLSL_TYPE_INT, 130, 0, true, false },
+               { "rgba16", PIPE_FORMAT_R16G16B16A16_UNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "rgb10_a2", PIPE_FORMAT_R10G10B10A2_UNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "rgba8", PIPE_FORMAT_R8G8B8A8_UNORM, GLSL_TYPE_FLOAT, 130, 310, false, false },
+               { "rg16", PIPE_FORMAT_R16G16_UNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "rg8", PIPE_FORMAT_R8G8_UNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "r16", PIPE_FORMAT_R16_UNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "r8", PIPE_FORMAT_R8_UNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "rgba16_snorm", PIPE_FORMAT_R16G16B16A16_SNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "rgba8_snorm", PIPE_FORMAT_R8G8B8A8_SNORM, GLSL_TYPE_FLOAT, 130, 310, false, false },
+               { "rg16_snorm", PIPE_FORMAT_R16G16_SNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "rg8_snorm", PIPE_FORMAT_R8G8_SNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "r16_snorm", PIPE_FORMAT_R16_SNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
+               { "r8_snorm", PIPE_FORMAT_R8_SNORM, GLSL_TYPE_FLOAT, 130, 0, true, false },
 
                /* From GL_EXT_shader_image_load_store: */
                /* base_type is incorrect but it'll be patched later when we know
                 * the variable type. See ast_to_hir.cpp */
-               { "size1x8", GL_R8I, GLSL_TYPE_VOID, 130, 0, false, true },
-               { "size1x16", GL_R16I, GLSL_TYPE_VOID, 130, 0, false, true },
-               { "size1x32", GL_R32I, GLSL_TYPE_VOID, 130, 0, false, true },
-               { "size2x32", GL_RG32I, GLSL_TYPE_VOID, 130, 0, false, true },
-               { "size4x32", GL_RGBA32I, GLSL_TYPE_VOID, 130, 0, false, true },
+               { "size1x8", PIPE_FORMAT_R8_SINT, GLSL_TYPE_VOID, 130, 0, false, true },
+               { "size1x16", PIPE_FORMAT_R16_SINT, GLSL_TYPE_VOID, 130, 0, false, true },
+               { "size1x32", PIPE_FORMAT_R32_SINT, GLSL_TYPE_VOID, 130, 0, false, true },
+               { "size2x32", PIPE_FORMAT_R32G32_SINT, GLSL_TYPE_VOID, 130, 0, false, true },
+               { "size4x32", PIPE_FORMAT_R32G32B32A32_SINT, GLSL_TYPE_VOID, 130, 0, false, true },
             };
 
             for (unsigned i = 0; i < ARRAY_SIZE(map); i++) {

src/compiler/glsl/ir.cpp
@@ -1806,7 +1806,7 @@ ir_variable::ir_variable(const struct glsl_type *type, const char *name,
    this->data.fb_fetch_output = false;
    this->data.bindless = false;
    this->data.bound = false;
-   this->data.image_format = GL_NONE;
+   this->data.image_format = PIPE_FORMAT_NONE;
    this->data._num_state_slots = 0;
    this->data.param_index = 0;
    this->data.stream = 0;

src/compiler/glsl/ir.h
@@ -29,6 +29,7 @@
 #include <stdlib.h>
 
 #include "util/ralloc.h"
+#include "util/format/u_format.h"
 #include "compiler/glsl_types.h"
 #include "list.h"
 #include "ir_visitor.h"
@@ -885,8 +886,11 @@ public:
       uint8_t warn_extension_index;
 
    public:
-      /** Image internal format if specified explicitly, otherwise GL_NONE. */
-      uint16_t image_format;
+      /**
+       * Image internal format if specified explicitly, otherwise
+       * PIPE_FORMAT_NONE.
+       */
+      enum pipe_format image_format;
 
    private:
       /**

src/compiler/glsl_types.h
@@ -31,6 +31,7 @@
 #include "shader_enums.h"
 #include "c11/threads.h"
 #include "util/blob.h"
+#include "util/format/u_format.h"
 #include "util/macros.h"
 
 #ifdef __cplusplus
@@ -1297,7 +1298,7 @@ struct glsl_struct_field {
    /**
     * Layout format, applicable to image variables only.
     */
-   unsigned image_format:16;
+   enum pipe_format image_format;
 
    /**
     * Any of the xfb_* qualifiers trigger the shader to be in transform
@@ -1314,7 +1315,8 @@ struct glsl_struct_field {
    sample(0), matrix_layout(GLSL_MATRIX_LAYOUT_INHERITED), patch(0),    \
    precision(_precision), memory_read_only(0),                          \
    memory_write_only(0), memory_coherent(0), memory_volatile(0),        \
-   memory_restrict(0), image_format(0), explicit_xfb_buffer(0),         \
+   memory_restrict(0), image_format(PIPE_FORMAT_NONE),                  \
+   explicit_xfb_buffer(0),                                              \
    implicit_sized_array(0)
 
    glsl_struct_field(const struct glsl_type *_type,

commit 75fdfbbb952d4a6718be39c55144fb65bfb58673

glsl: reject size1x8 for image variable with floating-point data types
Fixes: aa71fe814c1 ("glsl,nir: Switch the enum representing shader image formats to PIPE_FORMAT.")
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5688>

src/compiler/glsl/ast_to_hir.cpp
@@ -4983,12 +4983,14 @@ ast_declarator_list::hir(exec_list *instructions,
       if (strncmp(this->type->specifier->type_name, "image", strlen("image")) == 0) {
          switch (this->type->qualifier.image_format) {
          case PIPE_FORMAT_R8_SINT:
-            /* No valid qualifier in this case, driver will need to look at
-             * the underlying image's format (just like no qualifier being
-             * present).
+            /* The GL_EXT_shader_image_load_store spec says:
+             *    A layout of "size1x8" is illegal for image variables associated
+             *    with floating-point data types.
              */
-            this->type->qualifier.image_format = PIPE_FORMAT_NONE;
-            break;
+            _mesa_glsl_error(& loc, state,
+                             "size1x8 is illegal for image variables "
+                             "with floating-point data types.");
+            return NULL;
          case PIPE_FORMAT_R16_SINT:
             this->type->qualifier.image_format = PIPE_FORMAT_R16_FLOAT;
             break;