openharmony/third_party_mesa3d

commit 7303ff484eb0bf9adb83d207172e667b75fe8a63

ac/surface: fix epitch when modifying surf_pitch
This is needed otherwise it can cause bad rendering of UYVY files.
The align(..., 256 / surf->bpe) constraint comes from addrlib.

Fixes: 6ef83981a6a ("radeonsi: fix surf_pitch for subsampled surface")
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5314>

src/amd/common/ac_surface.c
@@ -1163,10 +1163,14 @@ static int gfx9_compute_miptree(ADDR_HANDLE addrlib,
 
 	surf->u.gfx9.surf_slice_size = out.sliceSize;
 	surf->u.gfx9.surf_pitch = out.pitch;
-	if (!compressed && surf->blk_w > 1 && out.pitch == out.pixelPitch) {
+	if (!compressed && surf->blk_w > 1 && out.pitch == out.pixelPitch &&
+	    surf->u.gfx9.surf.swizzle_mode == ADDR_SW_LINEAR) {
 		/* Adjust surf_pitch to be in elements units,
 		 * not in pixels */
-		surf->u.gfx9.surf_pitch /= surf->blk_w;
+		surf->u.gfx9.surf_pitch =
+			align(surf->u.gfx9.surf_pitch / surf->blk_w, 256 / surf->bpe);
+		surf->u.gfx9.surf.epitch = MAX2(surf->u.gfx9.surf.epitch,
+						surf->u.gfx9.surf_pitch * surf->blk_w - 1);
 	}
 	surf->u.gfx9.surf_height = out.height;
 	surf->surf_size = out.surfSize;

commit 28ab44d3590933700b81d4c1e0463bf7d5a4f3a9

ac/surface: adapt surf_size when modifying surf_pitch
Otherwise we might get VM_L2_PROTECTION_FAULT_STATUS errors.

Fixes: 7303ff484eb ("ac/surface: fix epitch when modifying surf_pitch")
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5841>

src/amd/common/ac_surface.c
@@ -1464,18 +1464,27 @@ static int gfx9_compute_miptree(struct ac_addrlib *addrlib,
 
 	surf->u.gfx9.surf_slice_size = out.sliceSize;
 	surf->u.gfx9.surf_pitch = out.pitch;
+	surf->u.gfx9.surf_height = out.height;
+	surf->surf_size = out.surfSize;
+	surf->surf_alignment = out.baseAlign;
+
 	if (!compressed && surf->blk_w > 1 && out.pitch == out.pixelPitch &&
 	    surf->u.gfx9.surf.swizzle_mode == ADDR_SW_LINEAR) {
-		/* Adjust surf_pitch to be in elements units,
-		 * not in pixels */
+		/* Adjust surf_pitch to be in elements units not in pixels */
 		surf->u.gfx9.surf_pitch =
 			align(surf->u.gfx9.surf_pitch / surf->blk_w, 256 / surf->bpe);
 		surf->u.gfx9.surf.epitch = MAX2(surf->u.gfx9.surf.epitch,
 						surf->u.gfx9.surf_pitch * surf->blk_w - 1);
+		/* The surface is really a surf->bpe bytes per pixel surface even if we
+		 * use it as a surf->bpe bytes per element one.
+		 * Adjust surf_slice_size and surf_size to reflect the change
+		 * made to surf_pitch.
+		 */
+		surf->u.gfx9.surf_slice_size = MAX2(
+			surf->u.gfx9.surf_slice_size,
+			surf->u.gfx9.surf_pitch * out.height * surf->bpe * surf->blk_w);
+		surf->surf_size = surf->u.gfx9.surf_slice_size * in->numSlices;
 	}
-	surf->u.gfx9.surf_height = out.height;
-	surf->surf_size = out.surfSize;
-	surf->surf_alignment = out.baseAlign;
 
 	if (in->swizzleMode == ADDR_SW_LINEAR) {
 		for (unsigned i = 0; i < in->numMipLevels; i++) {