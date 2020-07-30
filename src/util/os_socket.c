openharmony/third_party_mesa3d

commit 8b1e22dfb980eb3ad7e1f7f128768c6e9e944f69

util/os_socket: Add socket related functions.
v3:
 - Add os_socket.c/h into Makefile.sources (Lionel)
 - Add empty non-linux implementation to public functions.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

src/util/os_socket.c
@@ -1,0 +1,122 @@
+/*
+ * Copyright 2019 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <errno.h>
+
+#include "os_socket.h"
+
+#if defined(__linux__)
+
+#include <fcntl.h>
+#include <poll.h>
+#include <stddef.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+int
+os_socket_listen_abstract(const char *path, int count)
+{
+   int s = socket(AF_UNIX, SOCK_STREAM, 0);
+   if (s < 0)
+      return -1;
+
+   struct sockaddr_un addr;
+   memset(&addr, 0, sizeof(addr));
+   addr.sun_family = AF_UNIX;
+   strncpy(addr.sun_path + 1, path, sizeof(addr.sun_path) - 2);
+
+   /* Create an abstract socket */
+   int ret = bind(s, (struct sockaddr*)&addr,
+                  offsetof(struct sockaddr_un, sun_path) +
+                  strlen(path) + 1);
+   if (ret < 0)
+      return -1;
+
+   listen(s, count);
+
+   return s;
+}
+
+int
+os_socket_accept(int s)
+{
+   return accept(s, NULL, NULL);
+}
+
+ssize_t
+os_socket_recv(int socket, void *buffer, size_t length, int flags)
+{
+   return recv(socket, buffer, length, flags);
+}
+
+ssize_t
+os_socket_send(int socket, const void *buffer, size_t length, int flags)
+{
+   return send(socket, buffer, length, flags);
+}
+
+void
+os_socket_block(int s, bool block)
+{
+   int old = fcntl(s, F_GETFL, 0);
+   if (old == -1)
+      return;
+
+   /* TODO obey block */
+   if (block)
+      fcntl(s, F_SETFL, old & ~O_NONBLOCK);
+   else
+      fcntl(s, F_SETFL, old | O_NONBLOCK);
+}
+
+void
+os_socket_close(int s)
+{
+   close(s);
+}
+
+#else
+
+int
+os_socket_listen_abstract(const char *path, int count)
+{
+   errno = -ENOSYS;
+   return -1;
+}
+
+int
+os_socket_accept(int s)
+{
+   errno = -ENOSYS;
+   return -1;
+}
+
+ssize_t
+os_socket_recv(int socket, void *buffer, size_t length, int flags)
+{
+   errno = -ENOSYS;
+   return -1;
+}
+
+ssize_t
+os_socket_send(int socket, const void *buffer, size_t length, int flags)
+{
+   errno = -ENOSYS;
+   return -1;
+}
+
+void
+os_socket_block(int s, bool block)
+{
+}
+
+void
+os_socket_close(int s)
+{
+}
+
+#endif

src/util/Makefile.sources
@@ -57,6 +57,8 @@ MESA_UTIL_FILES := \
 	os_time.h \
 	os_file.c \
 	os_file.h \
+	os_socket.c \
+	os_socket.h \
 	os_misc.c \
 	os_misc.h \
 	u_process.c \

src/util/meson.build
@@ -62,6 +62,8 @@ files_mesa_util = files(
   'os_file.c',
   'os_misc.c',
   'os_misc.h',
+  'os_socket.c',
+  'os_socket.h',
   'u_process.c',
   'u_process.h',
   'sha1/sha1.c',

src/util/os_socket.h
@@ -1,0 +1,32 @@
+/*
+ * Copyright 2019 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ *
+ * Socket operations helpers
+ */
+
+#ifndef _OS_SOCKET_H_
+#define _OS_SOCKET_H_
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int os_socket_accept(int s);
+
+int os_socket_listen_abstract(const char *path, int count);
+
+ssize_t os_socket_recv(int socket, void *buffer, size_t length, int flags);
+ssize_t os_socket_send(int socket, const void *buffer, size_t length, int flags);
+
+void os_socket_block(int s, bool block);
+void os_socket_close(int s);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _OS_SOCKET_H_ */

commit 82961722aba8e096dc6928c24aec0f611123a4a4

util: fix possible fd leaks in os_socket_listen_abstract
Found by Coverity.

Signed-off-by: Marcin Ślusarz <marcin.slusarz@intel.com>
Fixes: 8b1e22dfb98 ("util/os_socket: Add socket related functions.")
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6067>

src/util/os_socket.c
@@ -33,10 +33,15 @@ os_socket_listen_abstract(const char *path, int count)
    int ret = bind(s, (struct sockaddr*)&addr,
                   offsetof(struct sockaddr_un, sun_path) +
                   strlen(path) + 1);
-   if (ret < 0)
+   if (ret < 0) {
+      close(s);
       return -1;
+   }
 
-   listen(s, count);
+   if (listen(s, count) < 0) {
+      close(s);
+      return -1;
+   }
 
    return s;
 }