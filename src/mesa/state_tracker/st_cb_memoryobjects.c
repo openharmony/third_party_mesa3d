openharmony/third_party_mesa3d

commit d03ce1dbe2dc4d96da00d6decead4a6e2cbc9e5b

mesa/st: start adding memory object support
v2: pass dedicated flag

v3 (Timothy Arceri):
 - remove unrequired _mesa_init_memory_object_functions()
   call in the state tracker.

Signed-off-by: Andres Rodriguez <andresx7@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com> (v2)
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

src/mesa/state_tracker/st_cb_memoryobjects.c
@@ -1,0 +1,64 @@
+#include "main/imports.h"
+#include "main/mtypes.h"
+
+#include "main/externalobjects.h"
+
+#include "st_context.h"
+#include "st_cb_memoryobjects.h"
+
+#include "state_tracker/drm_driver.h"
+#include "pipe/p_context.h"
+#include "pipe/p_screen.h"
+
+static struct gl_memory_object *
+st_memoryobj_alloc(struct gl_context *ctx, GLuint name)
+{
+   struct st_memory_object *st_obj = ST_CALLOC_STRUCT(st_memory_object);
+   if (!st_obj)
+      return NULL;
+
+   _mesa_initialize_memory_object(ctx, &st_obj->Base, name);
+   return &st_obj->Base;
+}
+
+static void
+st_memoryobj_free(struct gl_context *ctx,
+                  struct gl_memory_object *obj)
+{
+   _mesa_delete_memory_object(ctx, obj);
+}
+
+
+static void
+st_import_memoryobj_fd(struct gl_context *ctx,
+                       struct gl_memory_object *obj,
+                       GLuint64 size,
+                       int fd)
+{
+   struct st_memory_object *st_obj = st_memory_object(obj);
+   struct st_context *st = st_context(ctx);
+   struct pipe_context *pipe = st->pipe;
+   struct pipe_screen *screen = pipe->screen;
+   struct winsys_handle whandle;
+
+   whandle.type = DRM_API_HANDLE_TYPE_FD;
+   whandle.handle = fd;
+   whandle.offset = 0;
+   whandle.layer = 0;
+   whandle.stride = 0;
+
+   st_obj->memory = screen->memobj_create_from_handle(screen,
+                                                      &whandle,
+                                                      obj->Dedicated);
+
+   /* We own fd, but we no longer need it. So get rid of it */
+   close(fd);
+}
+
+void
+st_init_memoryobject_functions(struct dd_function_table *functions)
+{
+   functions->NewMemoryObject = st_memoryobj_alloc;
+   functions->DeleteMemoryObject = st_memoryobj_free;
+   functions->ImportMemoryObjectFd = st_import_memoryobj_fd;
+}

src/mesa/Makefile.sources
@@ -466,6 +466,8 @@ STATETRACKER_FILES = \
 	state_tracker/st_cb_feedback.h \
 	state_tracker/st_cb_flush.c \
 	state_tracker/st_cb_flush.h \
+	state_tracker/st_cb_memoryobjects.c \
+	state_tracker/st_cb_memoryobjects.h \
 	state_tracker/st_cb_msaa.c \
 	state_tracker/st_cb_msaa.h \
 	state_tracker/st_cb_perfmon.c \

src/mesa/state_tracker/st_cb_memoryobjects.h
@@ -1,0 +1,25 @@
+#ifndef ST_CB_MEMORYOBJECTS_H
+#define ST_CB_MEMORYOBJECTS_H
+
+#include "main/compiler.h"
+#include "main/mtypes.h"
+
+struct dd_function_table;
+struct pipe_screen;
+
+struct st_memory_object
+{
+   struct gl_memory_object Base;
+   struct pipe_memory_object *memory;
+};
+
+static inline struct st_memory_object *
+st_memory_object(struct gl_memory_object *obj)
+{
+   return (struct st_memory_object *)obj;
+}
+
+extern void
+st_init_memoryobject_functions(struct dd_function_table *functions);
+
+#endif

src/mesa/state_tracker/st_context.c
@@ -54,6 +54,7 @@
 #include "st_cb_eglimage.h"
 #include "st_cb_fbo.h"
 #include "st_cb_feedback.h"
+#include "st_cb_memoryobjects.h"
 #include "st_cb_msaa.h"
 #include "st_cb_perfmon.h"
 #include "st_cb_program.h"
@@ -684,6 +685,7 @@ void st_init_driver_functions(struct pipe_screen *screen,
 
    st_init_fbo_functions(functions);
    st_init_feedback_functions(functions);
+   st_init_memoryobject_functions(functions);
    st_init_msaa_functions(functions);
    st_init_perfmon_functions(functions);
    st_init_program_functions(functions);

commit 91d19359602830c6714152d5dc6b43f3c3d08012

mesa/st: Actually free the driver part of memory objects on destruction.
_mesa_delete_memory_object(ctx, obj) == free(obj) but doesn't free the
part of the gallium driver.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/1206
Fixes: d03ce1dbe2d "mesa/st: start adding memory object support"
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6148>

src/mesa/state_tracker/st_cb_memoryobjects.c
@@ -54,6 +54,12 @@ static void
 st_memoryobj_free(struct gl_context *ctx,
                   struct gl_memory_object *obj)
 {
+   struct st_memory_object *st_obj = st_memory_object(obj);
+   struct st_context *st = st_context(ctx);
+   struct pipe_context *pipe = st->pipe;
+   struct pipe_screen *screen = pipe->screen;
+
+   screen->memobj_destroy(screen, st_obj->memory);
    _mesa_delete_memory_object(ctx, obj);
 }
 