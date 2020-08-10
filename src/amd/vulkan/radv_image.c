openharmony/third_party_mesa3d

commit d58517db7cb7851056349eef06fb0f15ec845784

radv: Don't use both DCC and CMASK for single sample images.
Fixes: c2bd651ab40 "radv: Use ac_surface to allocate aux surfaces."
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6194>

src/amd/vulkan/radv_image.c
@@ -1233,9 +1233,11 @@ static void
 radv_image_alloc_single_sample_cmask(const struct radv_image *image,
                                      struct radeon_surf *surf)
 {
+	assert(image->info.storage_samples == 1 || surf->cmask_offset);
+
 	if (!surf->cmask_size || surf->cmask_offset || surf->bpe > 8 ||
 	    image->info.levels > 1 || image->info.depth > 1 ||
-	    !radv_image_use_fast_clear_for_image(image))
+	    radv_image_has_dcc(image) || !radv_image_use_fast_clear_for_image(image))
 		return;
 
 	surf->cmask_offset = align64(surf->total_size, surf->cmask_alignment);

commit 7e546d311217ccd0fd8e8577e128c98c207924b4

radv: Fix assert that is too strict.
The added assert fails on MSAA images if we disable FMASK ....

Reordered things.

Fixes: d58517db7cb "radv: Don't use both DCC and CMASK for single sample images."
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3385
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6232>

src/amd/vulkan/radv_image.c
@@ -1233,12 +1233,12 @@ static void
 radv_image_alloc_single_sample_cmask(const struct radv_image *image,
                                      struct radeon_surf *surf)
 {
-	assert(image->info.storage_samples == 1 || surf->cmask_offset);
-
 	if (!surf->cmask_size || surf->cmask_offset || surf->bpe > 8 ||
 	    image->info.levels > 1 || image->info.depth > 1 ||
 	    radv_image_has_dcc(image) || !radv_image_use_fast_clear_for_image(image))
 		return;
+
+	assert(image->info.storage_samples == 1);
 
 	surf->cmask_offset = align64(surf->total_size, surf->cmask_alignment);
 	surf->total_size = surf->cmask_offset + surf->cmask_size;