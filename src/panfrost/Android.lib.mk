openharmony/third_party_mesa3d

commit 3adcf5c17b6e3cb06c82c519276c08134d22e372

android: panfrost: Redirect cmdstream includes through GenXML
Fixes the following building error:

external/mesa/src/panfrost/lib/decode.c:26:10: fatal error: 'midgard_pack.h' file not found
         ^~~~~~~~~~~~~~~~
1 error generated.

Fixes: 9f540e37c ("panfrost: Redirect cmdstream includes through GenXML")
Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6317>

src/panfrost/Android.lib.mk
@@ -21,23 +21,35 @@
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libpanfrost_lib
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+intermediates := $(call local-generated-sources-dir)
 
 LOCAL_SRC_FILES := \
 	$(lib_FILES)
+
+LOCAL_GENERATED_SOURCES := \
+	$(intermediates)/panfrost/lib/midgard_pack.h
 
 LOCAL_C_INCLUDES := \
 	$(MESA_TOP)/src/gallium/auxiliary/ \
 	$(MESA_TOP)/src/gallium/include/ \
 	$(MESA_TOP)/src/panfrost/lib/ \
-	$(MESA_TOP)/src/panfrost/include/
+	$(MESA_TOP)/src/panfrost/include/ \
+	$(intermediates)/panfrost/lib/
 
 LOCAL_STATIC_LIBRARIES := \
 	libmesa_nir
 
-LOCAL_GENERATED_SOURCES := \
+midgard_pack_gen := $(LOCAL_PATH)/lib/gen_pack.py
+midgard_pack_deps := $(LOCAL_PATH)/lib/midgard.xml
+
+$(intermediates)/panfrost/lib/midgard_pack.h: $(midgard_pack_deps)
+	@mkdir -p $(dir $@)
+	$(hide) $(MESA_PYTHON2) $(midgard_pack_gen) $< > $@
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := \
 	$(MESA_TOP)/src/panfrost/lib/ \
+	$(intermediates)
 
 include $(MESA_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)

commit 2f4056b5022caa3c6c578f97b353da9fe60f1d17

android: panfrost: Redirect cmdstream includes through GenXML (v2)
bifrost requires both "pafrost/lib/midgard_pack.h" and "midgard_pack.h" headers

Fixes the following building error:

In file included from external/mesa/src/panfrost/bifrost/cmdline.c:33:
In file included from external/mesa/src/panfrost/bifrost/test/bit.h:31:
external/mesa/src/panfrost/lib/pan_device.h:40:10: fatal error: 'midgard_pack.h' file not found
#include <midgard_pack.h>
         ^~~~~~~~~~~~~~~~
1 error generated.

Fixes: 3adcf5c1 ("android: panfrost: Redirect cmdstream includes through GenXML")
Fixes: 9f540e37 ("panfrost: Redirect cmdstream includes through GenXML")
Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6322>

src/panfrost/Android.lib.mk
@@ -49,6 +49,7 @@ $(intermediates)/panfrost/lib/midgard_pack.h: $(midgard_pack_deps)
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := \
 	$(MESA_TOP)/src/panfrost/lib/ \
+	$(intermediates)/panfrost/lib/ \
 	$(intermediates)
 
 include $(MESA_COMMON_MK)