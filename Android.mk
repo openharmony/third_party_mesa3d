openharmony/third_party_mesa3d

commit 65684e366410a0824ee86193b731936db027d94d

ac/llvm: export ac_init_llvm_once in targets
If a program like mpv uses both radeon_dri.so (because --vo=gpu) and
radeonsi_drv_video.so (because --hwdec=vaapi) then LLVM will be inialized twice.

The commit exports the ac_init_llvm_once so there's only one instance of the
function.

See also aad7160e893 ("targets: export radeon winsys_create functions to silence LLVM warning")
which implemented this workaround initially.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/1377
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Acked-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5648>

src/amd/llvm/ac_llvm_util.c
@@ -78,10 +78,9 @@ static void ac_init_llvm_target()
 	LLVMParseCommandLineOptions(ARRAY_SIZE(argv), argv, NULL);
 }
 
-static once_flag ac_init_llvm_target_once_flag = ONCE_FLAG_INIT;
-
-void ac_init_llvm_once(void)
+PUBLIC void ac_init_llvm_once(void)
 {
+	static once_flag ac_init_llvm_target_once_flag = ONCE_FLAG_INIT;
 	call_once(&ac_init_llvm_target_once_flag, ac_init_llvm_target);
 }
 

src/amd/vulkan/meson.build
@@ -176,6 +176,7 @@ if with_symbols_check
     args : [
       '--lib', libvulkan_radeon,
       '--symbols-file', vulkan_icd_symbols,
+      '--ignore-symbol', 'ac_init_llvm_once',
       symbols_check_args,
     ],
     suite : ['amd'],

src/gallium/targets/dri-vdpau.dyn
@@ -2,4 +2,5 @@
 	nouveau_drm_screen_create;
 	radeon_drm_winsys_create;
 	amdgpu_winsys_create;
+	ac_init_llvm_once;
 };

src/gallium/targets/dri/dri.sym
@@ -6,6 +6,7 @@
 		radeon_drm_winsys_create;
 		amdgpu_winsys_create;
 		fd_drm_screen_create;
+		ac_init_llvm_once;
 	local:
 		*;
 };

src/gallium/targets/omx/omx.sym
@@ -7,6 +7,7 @@
 		# due to LLVM being initialized multiple times.
 		radeon_drm_winsys_create;
 		amdgpu_winsys_create;
+		ac_init_llvm_once;
 	local:
 		*;
 };

src/gallium/targets/pipe-loader/pipe.sym
@@ -7,6 +7,7 @@
 		# due to LLVM being initialized multiple times.
 		radeon_drm_winsys_create;
 		amdgpu_winsys_create;
+		ac_init_llvm_once;
 	local:
 		*;
 };

src/gallium/targets/va/va.sym
@@ -6,6 +6,7 @@
 		# due to LLVM being initialized multiple times.
 		radeon_drm_winsys_create;
 		amdgpu_winsys_create;
+		ac_init_llvm_once;
 	local:
 		*;
 };

src/gallium/targets/vdpau/vdpau.sym
@@ -4,6 +4,7 @@
                nouveau_drm_screen_create;
                radeon_drm_winsys_create;
                amdgpu_winsys_create;
+               ac_init_llvm_once;
        local:
                *;
 };

commit 26ca303bc19d7aa8def503959554c6cd8855e311

ac/llvm: handle static/shared llvm init separately
Having a single init function works as expected for shared llvm, but
when using a static llvm only one llvm will get initialized.

This commit introduces 2 separate init function:
- shared llvm = single public init function
- static llvm = one init function for each module using llvm

Fixes: 65684e36641 ("ac/llvm: export ac_init_llvm_once in targets")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3376
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6253>

Android.mk
@@ -97,7 +97,7 @@ endif
 define mesa-build-with-llvm
   $(if $(filter $(MESA_ANDROID_MAJOR_VERSION), 4 5 6 7), \
     $(warning Unsupported LLVM version in Android $(MESA_ANDROID_MAJOR_VERSION)),) \
-  $(eval LOCAL_CFLAGS += -DLLVM_AVAILABLE -DMESA_LLVM_VERSION_STRING=\"3.9\") \
+  $(eval LOCAL_CFLAGS += -DLLVM_AVAILABLE -DLLVM_IS_SHARED=1 -DMESA_LLVM_VERSION_STRING=\"3.9\") \
   $(eval LOCAL_SHARED_LIBRARIES += libLLVM)
 endef
 

meson.build
@@ -1513,6 +1513,7 @@ endif
 if with_llvm
   pre_args += '-DLLVM_AVAILABLE'
   pre_args += '-DMESA_LLVM_VERSION_STRING="@0@"'.format(dep_llvm.version())
+  pre_args += '-DLLVM_IS_SHARED=@0@'.format(_shared_llvm.to_int())
 
   # LLVM can be built without rtti, turning off rtti changes the ABI of C++
   # programs, so we need to build all C++ code in mesa without rtti as well to

src/amd/llvm/ac_llvm_util.c
@@ -78,10 +78,27 @@ static void ac_init_llvm_target()
 	LLVMParseCommandLineOptions(ARRAY_SIZE(argv), argv, NULL);
 }
 
-PUBLIC void ac_init_llvm_once(void)
+PUBLIC void ac_init_shared_llvm_once(void)
 {
 	static once_flag ac_init_llvm_target_once_flag = ONCE_FLAG_INIT;
 	call_once(&ac_init_llvm_target_once_flag, ac_init_llvm_target);
+}
+
+#if !LLVM_IS_SHARED
+static once_flag ac_init_static_llvm_target_once_flag = ONCE_FLAG_INIT;
+static void ac_init_static_llvm_once(void)
+{
+	call_once(&ac_init_static_llvm_target_once_flag, ac_init_llvm_target);
+}
+#endif
+
+void ac_init_llvm_once(void)
+{
+#if LLVM_IS_SHARED
+	ac_init_shared_llvm_once();
+#else
+	ac_init_static_llvm_once();
+#endif
 }
 
 static LLVMTargetRef ac_get_llvm_target(const char *triple)

src/amd/llvm/ac_llvm_util.h
@@ -131,6 +131,7 @@ ac_count_scratch_private_memory(LLVMValueRef function);
 
 LLVMTargetLibraryInfoRef ac_create_target_library_info(const char *triple);
 void ac_dispose_target_library_info(LLVMTargetLibraryInfoRef library_info);
+void ac_init_shared_llvm_once(void); /* Do not use directly, use ac_init_llvm_once */
 void ac_init_llvm_once(void);
 
 

src/amd/vulkan/meson.build
@@ -176,7 +176,7 @@ if with_symbols_check
     args : [
       '--lib', libvulkan_radeon,
       '--symbols-file', vulkan_icd_symbols,
-      '--ignore-symbol', 'ac_init_llvm_once',
+      '--ignore-symbol', 'ac_init_shared_llvm_once',
       symbols_check_args,
     ],
     suite : ['amd'],

src/gallium/targets/dri-vdpau.dyn
@@ -2,5 +2,5 @@
 	nouveau_drm_screen_create;
 	radeon_drm_winsys_create;
 	amdgpu_winsys_create;
-	ac_init_llvm_once;
+	ac_init_shared_llvm_once;
 };

src/gallium/targets/dri/dri.sym
@@ -6,7 +6,7 @@
 		radeon_drm_winsys_create;
 		amdgpu_winsys_create;
 		fd_drm_screen_create;
-		ac_init_llvm_once;
+		ac_init_shared_llvm_once;
 	local:
 		*;
 };

src/gallium/targets/omx/omx.sym
@@ -7,7 +7,7 @@
 		# due to LLVM being initialized multiple times.
 		radeon_drm_winsys_create;
 		amdgpu_winsys_create;
-		ac_init_llvm_once;
+		ac_init_shared_llvm_once;
 	local:
 		*;
 };

src/gallium/targets/pipe-loader/pipe.sym
@@ -7,7 +7,7 @@
 		# due to LLVM being initialized multiple times.
 		radeon_drm_winsys_create;
 		amdgpu_winsys_create;
-		ac_init_llvm_once;
+		ac_init_shared_llvm_once;
 	local:
 		*;
 };

src/gallium/targets/va/va.sym
@@ -6,7 +6,7 @@
 		# due to LLVM being initialized multiple times.
 		radeon_drm_winsys_create;
 		amdgpu_winsys_create;
-		ac_init_llvm_once;
+		ac_init_shared_llvm_once;
 	local:
 		*;
 };

src/gallium/targets/vdpau/vdpau.sym
@@ -4,7 +4,7 @@
                nouveau_drm_screen_create;
                radeon_drm_winsys_create;
                amdgpu_winsys_create;
-               ac_init_llvm_once;
+               ac_init_shared_llvm_once;
        local:
                *;
 };