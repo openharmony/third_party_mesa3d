openharmony/third_party_mesa3d

commit 5a7fd8050b7abca151af6348b54c0e8c5e13ca84

gallium/swr: Fix compilation with LLVM 12
Update to reflect changes in VectorType and ElementCount APIs

Closes: #3457
Reviewed-by: Krzysztof Raszkowski <krzysztof.raszkowski@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6554>

src/gallium/drivers/swr/rasterizer/jitter/builder_misc.cpp
@@ -134,7 +134,7 @@ namespace SwrJit
     Value* Builder::VIMMED1(uint64_t i)
     {
 #if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount(mVWidth, false), cast<ConstantInt>(C(i)));
+        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantInt>(C(i)));
 #else
         return ConstantVector::getSplat(mVWidth, cast<ConstantInt>(C(i)));
 #endif
@@ -143,7 +143,7 @@ namespace SwrJit
     Value* Builder::VIMMED1_16(uint64_t i)
     {
 #if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount(mVWidth16, false), cast<ConstantInt>(C(i)));
+        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantInt>(C(i)));
 #else
         return ConstantVector::getSplat(mVWidth16, cast<ConstantInt>(C(i)));
 #endif
@@ -152,7 +152,7 @@ namespace SwrJit
     Value* Builder::VIMMED1(int i)
     {
 #if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount(mVWidth, false), cast<ConstantInt>(C(i)));
+        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantInt>(C(i)));
 #else
         return ConstantVector::getSplat(mVWidth, cast<ConstantInt>(C(i)));
 #endif
@@ -161,7 +161,7 @@ namespace SwrJit
     Value* Builder::VIMMED1_16(int i)
     {
 #if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount(mVWidth16, false), cast<ConstantInt>(C(i)));
+        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantInt>(C(i)));
 #else
         return ConstantVector::getSplat(mVWidth16, cast<ConstantInt>(C(i)));
 #endif
@@ -170,7 +170,7 @@ namespace SwrJit
     Value* Builder::VIMMED1(uint32_t i)
     {
 #if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount(mVWidth, false), cast<ConstantInt>(C(i)));
+        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantInt>(C(i)));
 #else
         return ConstantVector::getSplat(mVWidth, cast<ConstantInt>(C(i)));
 #endif
@@ -179,7 +179,7 @@ namespace SwrJit
     Value* Builder::VIMMED1_16(uint32_t i)
     {
 #if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount(mVWidth16, false), cast<ConstantInt>(C(i)));
+        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantInt>(C(i)));
 #else
         return ConstantVector::getSplat(mVWidth16, cast<ConstantInt>(C(i)));
 #endif
@@ -188,7 +188,7 @@ namespace SwrJit
     Value* Builder::VIMMED1(float i)
     {
 #if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount(mVWidth, false), cast<ConstantFP>(C(i)));
+        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantFP>(C(i)));
 #else
         return ConstantVector::getSplat(mVWidth, cast<ConstantFP>(C(i)));
 #endif
@@ -197,7 +197,7 @@ namespace SwrJit
     Value* Builder::VIMMED1_16(float i)
     {
 #if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount(mVWidth16, false), cast<ConstantFP>(C(i)));
+        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantFP>(C(i)));
 #else
         return ConstantVector::getSplat(mVWidth16, cast<ConstantFP>(C(i)));
 #endif
@@ -206,7 +206,7 @@ namespace SwrJit
     Value* Builder::VIMMED1(bool i)
     {
 #if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount(mVWidth, false), cast<ConstantInt>(C(i)));
+        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantInt>(C(i)));
 #else
         return ConstantVector::getSplat(mVWidth, cast<ConstantInt>(C(i)));
 #endif
@@ -215,27 +215,27 @@ namespace SwrJit
     Value* Builder::VIMMED1_16(bool i)
     {
 #if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount(mVWidth16, false), cast<ConstantInt>(C(i)));
+        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantInt>(C(i)));
 #else
         return ConstantVector::getSplat(mVWidth16, cast<ConstantInt>(C(i)));
 #endif
     }
 
-    Value* Builder::VUNDEF_IPTR() { return UndefValue::get(VectorType::get(mInt32PtrTy, mVWidth)); }
+    Value* Builder::VUNDEF_IPTR() { return UndefValue::get(getVectorType(mInt32PtrTy, mVWidth)); }
 
-    Value* Builder::VUNDEF(Type* t) { return UndefValue::get(VectorType::get(t, mVWidth)); }
+    Value* Builder::VUNDEF(Type* t) { return UndefValue::get(getVectorType(t, mVWidth)); }
 
-    Value* Builder::VUNDEF_I() { return UndefValue::get(VectorType::get(mInt32Ty, mVWidth)); }
+    Value* Builder::VUNDEF_I() { return UndefValue::get(getVectorType(mInt32Ty, mVWidth)); }
 
-    Value* Builder::VUNDEF_I_16() { return UndefValue::get(VectorType::get(mInt32Ty, mVWidth16)); }
+    Value* Builder::VUNDEF_I_16() { return UndefValue::get(getVectorType(mInt32Ty, mVWidth16)); }
 
-    Value* Builder::VUNDEF_F() { return UndefValue::get(VectorType::get(mFP32Ty, mVWidth)); }
+    Value* Builder::VUNDEF_F() { return UndefValue::get(getVectorType(mFP32Ty, mVWidth)); }
 
-    Value* Builder::VUNDEF_F_16() { return UndefValue::get(VectorType::get(mFP32Ty, mVWidth16)); }
+    Value* Builder::VUNDEF_F_16() { return UndefValue::get(getVectorType(mFP32Ty, mVWidth16)); }
 
     Value* Builder::VUNDEF(Type* ty, uint32_t size)
     {
-        return UndefValue::get(VectorType::get(ty, size));
+        return UndefValue::get(getVectorType(ty, size));
     }
 
     Value* Builder::VBROADCAST(Value* src, const llvm::Twine& name)
@@ -602,7 +602,7 @@ namespace SwrJit
             // number of 8 bit elements in b
             uint32_t numElms = cast<VectorType>(cB->getType())->getNumElements();
             // output vector
-            Value* vShuf = UndefValue::get(VectorType::get(mInt8Ty, numElms));
+            Value* vShuf = UndefValue::get(getVectorType(mInt8Ty, numElms));
 
             // insert an 8 bit value from the high and low lanes of a per loop iteration
             numElms /= 2;
@@ -643,7 +643,7 @@ namespace SwrJit
     Value* Builder::PMOVSXBD(Value* a)
     {
         // VPMOVSXBD output type
-        Type* v8x32Ty = VectorType::get(mInt32Ty, 8);
+        Type* v8x32Ty = getVectorType(mInt32Ty, 8);
         // Extract 8 values from 128bit lane and sign extend
         return S_EXT(VSHUFFLE(a, a, C<int>({0, 1, 2, 3, 4, 5, 6, 7})), v8x32Ty);
     }
@@ -655,7 +655,7 @@ namespace SwrJit
     Value* Builder::PMOVSXWD(Value* a)
     {
         // VPMOVSXWD output type
-        Type* v8x32Ty = VectorType::get(mInt32Ty, 8);
+        Type* v8x32Ty = getVectorType(mInt32Ty, 8);
         // Extract 8 values from 128bit lane and sign extend
         return S_EXT(VSHUFFLE(a, a, C<int>({0, 1, 2, 3, 4, 5, 6, 7})), v8x32Ty);
     }
@@ -672,9 +672,9 @@ namespace SwrJit
 #else
         uint32_t numElems = a->getType()->getVectorNumElements();
 #endif
-        Value*   input    = BITCAST(a, VectorType::get(mFP16Ty, numElems));
+        Value*   input    = BITCAST(a, getVectorType(mFP16Ty, numElems));
 
-        return FP_EXT(input, VectorType::get(mFP32Ty, numElems), name);
+        return FP_EXT(input, getVectorType(mFP32Ty, numElems), name);
     }
 
     //////////////////////////////////////////////////////////////////////////

src/gallium/drivers/swr/rasterizer/jitter/functionpasses/lower_x86.cpp
@@ -287,7 +287,7 @@ namespace SwrJit
                 SWR_ASSERT(false, "Unhandled vector width type %d\n", width);
             }
 
-            return ConstantVector::getNullValue(VectorType::get(pTy, numElem));
+            return ConstantVector::getNullValue(getVectorType(pTy, numElem));
         }
 
         Value* GetMask(TargetWidth width)
@@ -315,7 +315,7 @@ namespace SwrJit
 #else
             uint32_t numElem = vi1Mask->getType()->getVectorNumElements();
 #endif
-            return B->S_EXT(vi1Mask, VectorType::get(B->mInt32Ty, numElem));
+            return B->S_EXT(vi1Mask, getVectorType(B->mInt32Ty, numElem));
         }
 
         Instruction* ProcessIntrinsicAdvanced(CallInst* pCallInst)
@@ -546,7 +546,7 @@ namespace SwrJit
 
             v32Gather        = UndefValue::get(vSrc->getType());
 #if LLVM_VERSION_MAJOR > 10
-            auto vi32Scale   = ConstantVector::getSplat(ElementCount(numElem, false), cast<ConstantInt>(i32Scale));
+            auto vi32Scale   = ConstantVector::getSplat(ElementCount::get(numElem, false), cast<ConstantInt>(i32Scale));
 #else
             auto vi32Scale   = ConstantVector::getSplat(numElem, cast<ConstantInt>(i32Scale));
 #endif
@@ -609,7 +609,7 @@ namespace SwrJit
 #else
                     uint32_t numElem = v64Mask->getType()->getVectorNumElements();
 #endif
-                    v64Mask = B->S_EXT(v64Mask, VectorType::get(B->mInt64Ty, numElem));
+                    v64Mask = B->S_EXT(v64Mask, getVectorType(B->mInt64Ty, numElem));
                     v64Mask = B->BITCAST(v64Mask, vSrc->getType());
 
                     Value* src0 = B->VSHUFFLE(vSrc, vSrc, B->C({0, 1, 2, 3}));
@@ -632,12 +632,12 @@ namespace SwrJit
                     uint32_t numElemSrc1  = src1->getType()->getVectorNumElements();
                     uint32_t numElemMask1 = mask1->getType()->getVectorNumElements();
 #endif
-                    src0 = B->BITCAST(src0, VectorType::get(B->mInt64Ty, numElemSrc0));
-                    mask0 = B->BITCAST(mask0, VectorType::get(B->mInt64Ty, numElemMask0));
+                    src0 = B->BITCAST(src0, getVectorType(B->mInt64Ty, numElemSrc0));
+                    mask0 = B->BITCAST(mask0, getVectorType(B->mInt64Ty, numElemMask0));
                     Value* gather0 =
                         B->CALL(pX86IntrinFunc, {src0, pBase, indices0, mask0, i8Scale});
-                    src1 = B->BITCAST(src1, VectorType::get(B->mInt64Ty, numElemSrc1));
-                    mask1 = B->BITCAST(mask1, VectorType::get(B->mInt64Ty, numElemMask1));
+                    src1 = B->BITCAST(src1, getVectorType(B->mInt64Ty, numElemSrc1));
+                    mask1 = B->BITCAST(mask1, getVectorType(B->mInt64Ty, numElemMask1));
                     Value* gather1 =
                         B->CALL(pX86IntrinFunc, {src1, pBase, indices1, mask1, i8Scale});
                     v32Gather = B->VSHUFFLE(gather0, gather1, B->C({0, 1, 2, 3, 4, 5, 6, 7}));

src/gallium/drivers/swr/rasterizer/codegen/gen_llvm_ir_macros.py
@@ -52,7 +52,7 @@ intrinsics = [
     ['VPSHUFB',     ['a', 'b'], 'a'],
     ['VPERMD',      ['a', 'idx'], 'a'],
     ['VPERMPS',     ['idx', 'a'], 'a'],
-    ['VCVTPD2PS',   ['a'], 'VectorType::get(mFP32Ty, VEC_GET_NUM_ELEMS)'],
+    ['VCVTPD2PS',   ['a'], 'getVectorType(mFP32Ty, VEC_GET_NUM_ELEMS)'],
     ['VCVTPS2PH',   ['a', 'round'], 'mSimdInt16Ty'],
     ['VHSUBPS',     ['a', 'b'], 'a'],
     ['VPTESTC',     ['a', 'b'], 'mInt32Ty'],

src/gallium/drivers/swr/rasterizer/codegen/gen_llvm_types.py
@@ -57,31 +57,31 @@ def gen_llvm_type(type, name, idx, is_pointer, is_pointer_pointer, is_array, is_
         elif type == 'HANDLE':
             llvm_type = 'PointerType::get(Type::getInt32Ty(ctx), 0)'
         elif type == 'simdscalar':
-            llvm_type = 'VectorType::get(Type::getFloatTy(ctx), pJitMgr->mVWidth)'
+            llvm_type = 'getVectorType(Type::getFloatTy(ctx), pJitMgr->mVWidth)'
         elif type == 'simdscalari':
-            llvm_type = 'VectorType::get(Type::getInt32Ty(ctx), pJitMgr->mVWidth)'
+            llvm_type = 'getVectorType(Type::getInt32Ty(ctx), pJitMgr->mVWidth)'
         elif type == 'simd16scalar':
-            llvm_type = 'VectorType::get(Type::getFloatTy(ctx), 16)'
+            llvm_type = 'getVectorType(Type::getFloatTy(ctx), 16)'
         elif type == 'simd16scalari':
-            llvm_type = 'VectorType::get(Type::getInt32Ty(ctx), 16)'
+            llvm_type = 'getVectorType(Type::getInt32Ty(ctx), 16)'
         elif type == '__m128i':
-            llvm_type = 'VectorType::get(Type::getInt32Ty(ctx), 4)'
+            llvm_type = 'getVectorType(Type::getInt32Ty(ctx), 4)'
         elif type == 'SIMD256::Float':
-            llvm_type = 'VectorType::get(Type::getFloatTy(ctx), 8)'
+            llvm_type = 'getVectorType(Type::getFloatTy(ctx), 8)'
         elif type == 'SIMD256::Integer':
-            llvm_type = 'VectorType::get(Type::getInt32Ty(ctx), 8)'
+            llvm_type = 'getVectorType(Type::getInt32Ty(ctx), 8)'
         elif type == 'SIMD512::Float':
-            llvm_type = 'VectorType::get(Type::getFloatTy(ctx), 16)'
+            llvm_type = 'getVectorType(Type::getFloatTy(ctx), 16)'
         elif type == 'SIMD512::Integer':
-            llvm_type = 'VectorType::get(Type::getInt32Ty(ctx), 16)'
+            llvm_type = 'getVectorType(Type::getInt32Ty(ctx), 16)'
         elif type == 'simdvector':
-            llvm_type = 'ArrayType::get(VectorType::get(Type::getFloatTy(ctx), 8), 4)'
+            llvm_type = 'ArrayType::get(getVectorType(Type::getFloatTy(ctx), 8), 4)'
         elif type == 'simd16vector':
-            llvm_type = 'ArrayType::get(VectorType::get(Type::getFloatTy(ctx), 16), 4)'
+            llvm_type = 'ArrayType::get(getVectorType(Type::getFloatTy(ctx), 16), 4)'
         elif type == 'SIMD256::Vec4':
-            llvm_type = 'ArrayType::get(VectorType::get(Type::getFloatTy(ctx), 8), 4)'
+            llvm_type = 'ArrayType::get(getVectorType(Type::getFloatTy(ctx), 8), 4)'
         elif type == 'SIMD512::Vec4':
-            llvm_type = 'ArrayType::get(VectorType::get(Type::getFloatTy(ctx), 16), 4)'
+            llvm_type = 'ArrayType::get(getVectorType(Type::getFloatTy(ctx), 16), 4)'
         else:
             llvm_type = 'Gen_%s(pJitMgr)' % type
 

src/gallium/drivers/swr/rasterizer/jitter/blend_jit.cpp
@@ -870,7 +870,9 @@ struct BlendJit : public Builder
         passes.add(createCFGSimplificationPass());
         passes.add(createEarlyCSEPass());
         passes.add(createInstructionCombiningPass());
+#if LLVM_VERSION_MAJOR <= 11
         passes.add(createConstantPropagationPass());
+#endif
         passes.add(createSCCPPass());
         passes.add(createAggressiveDCEPass());
 

src/gallium/drivers/swr/rasterizer/jitter/builder.cpp
@@ -63,23 +63,23 @@ namespace SwrJit
         mInt64PtrTy = PointerType::get(mInt64Ty, 0);
         mHandleTy   = mInt8PtrTy;
 
-        mSimd4FP64Ty = VectorType::get(mDoubleTy, 4);
+        mSimd4FP64Ty = getVectorType(mDoubleTy, 4);
 
         // Built in types: target simd
         SetTargetWidth(pJitMgr->mVWidth);
 
         // Built in types: simd16
 
-        mSimd16Int1Ty     = VectorType::get(mInt1Ty, mVWidth16);
-        mSimd16Int16Ty    = VectorType::get(mInt16Ty, mVWidth16);
-        mSimd16Int32Ty    = VectorType::get(mInt32Ty, mVWidth16);
-        mSimd16Int64Ty    = VectorType::get(mInt64Ty, mVWidth16);
-        mSimd16FP16Ty     = VectorType::get(mFP16Ty, mVWidth16);
-        mSimd16FP32Ty     = VectorType::get(mFP32Ty, mVWidth16);
+        mSimd16Int1Ty     = getVectorType(mInt1Ty, mVWidth16);
+        mSimd16Int16Ty    = getVectorType(mInt16Ty, mVWidth16);
+        mSimd16Int32Ty    = getVectorType(mInt32Ty, mVWidth16);
+        mSimd16Int64Ty    = getVectorType(mInt64Ty, mVWidth16);
+        mSimd16FP16Ty     = getVectorType(mFP16Ty, mVWidth16);
+        mSimd16FP32Ty     = getVectorType(mFP32Ty, mVWidth16);
         mSimd16VectorTy   = ArrayType::get(mSimd16FP32Ty, 4);
         mSimd16VectorTRTy = ArrayType::get(mSimd16FP32Ty, 5);
 
-        mSimd32Int8Ty = VectorType::get(mInt8Ty, 32);
+        mSimd32Int8Ty = getVectorType(mInt8Ty, 32);
 
         if (sizeof(uint32_t*) == 4)
         {
@@ -101,12 +101,12 @@ namespace SwrJit
     {
         mVWidth = width;
 
-        mSimdInt1Ty      = VectorType::get(mInt1Ty, mVWidth);
-        mSimdInt16Ty     = VectorType::get(mInt16Ty, mVWidth);
-        mSimdInt32Ty     = VectorType::get(mInt32Ty, mVWidth);
-        mSimdInt64Ty     = VectorType::get(mInt64Ty, mVWidth);
-        mSimdFP16Ty      = VectorType::get(mFP16Ty, mVWidth);
-        mSimdFP32Ty      = VectorType::get(mFP32Ty, mVWidth);
+        mSimdInt1Ty      = getVectorType(mInt1Ty, mVWidth);
+        mSimdInt16Ty     = getVectorType(mInt16Ty, mVWidth);
+        mSimdInt32Ty     = getVectorType(mInt32Ty, mVWidth);
+        mSimdInt64Ty     = getVectorType(mInt64Ty, mVWidth);
+        mSimdFP16Ty      = getVectorType(mFP16Ty, mVWidth);
+        mSimdFP32Ty      = getVectorType(mFP32Ty, mVWidth);
         mSimdVectorTy    = ArrayType::get(mSimdFP32Ty, 4);
         mSimdVectorIntTy = ArrayType::get(mSimdInt32Ty, 4);
         mSimdVectorTRTy  = ArrayType::get(mSimdFP32Ty, 5);
@@ -213,7 +213,7 @@ namespace SwrJit
         }
 
         // <ty> should packetize to <8 x <ty>>
-        Type* vecType = VectorType::get(pType, JM()->mVWidth);
+        Type* vecType = getVectorType(pType, JM()->mVWidth);
         return vecType;
     }
 } // namespace SwrJit

src/gallium/drivers/swr/rasterizer/jitter/builder_mem.cpp
@@ -427,8 +427,8 @@ namespace SwrJit
                                       bool                   bPackedOutput)
     {
         // cast types
-        Type* vGatherTy = VectorType::get(IntegerType::getInt32Ty(JM()->mContext), mVWidth);
-        Type* v32x8Ty   = VectorType::get(mInt8Ty, mVWidth * 4); // vwidth is units of 32 bits
+        Type* vGatherTy = getVectorType(IntegerType::getInt32Ty(JM()->mContext), mVWidth);
+        Type* v32x8Ty   = getVectorType(mInt8Ty, mVWidth * 4); // vwidth is units of 32 bits
 
         // input could either be float or int vector; do shuffle work in int
         vGatherInput[0] = BITCAST(vGatherInput[0], mSimdInt32Ty);
@@ -436,7 +436,7 @@ namespace SwrJit
 
         if (bPackedOutput)
         {
-            Type* v128bitTy = VectorType::get(IntegerType::getIntNTy(JM()->mContext, 128),
+            Type* v128bitTy = getVectorType(IntegerType::getIntNTy(JM()->mContext, 128),
                                               mVWidth / 4); // vwidth is units of 32 bits
 
             // shuffle mask
@@ -532,12 +532,12 @@ namespace SwrJit
                                      bool                   bPackedOutput)
     {
         // cast types
-        Type* vGatherTy = VectorType::get(IntegerType::getInt32Ty(JM()->mContext), mVWidth);
-        Type* v32x8Ty   = VectorType::get(mInt8Ty, mVWidth * 4); // vwidth is units of 32 bits
+        Type* vGatherTy = getVectorType(IntegerType::getInt32Ty(JM()->mContext), mVWidth);
+        Type* v32x8Ty   = getVectorType(mInt8Ty, mVWidth * 4); // vwidth is units of 32 bits
 
         if (bPackedOutput)
         {
-            Type* v128Ty = VectorType::get(IntegerType::getIntNTy(JM()->mContext, 128),
+            Type* v128Ty = getVectorType(IntegerType::getIntNTy(JM()->mContext, 128),
                                            mVWidth / 4); // vwidth is units of 32 bits
                                                          // shuffle mask
             Value* vConstMask = C<char>({0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15,

commit 72873a48be68b18d2b64bd4b64e433afd4d77c12

swr: Use ElemenCount constructor for LLVM 11
In LLVM 12 ElementCount constructor is private
and instead of using it explicitly, ::get function
should be used, but in LLVM 11, the constructor
is still the way to go.

Reviewed-by: Krzysztof Raszkowski <krzysztof.raszkowski@intel.com>
Closes: #3490
Fixes: 5a7fd8050b7abca151af6348b54c0e8c5e13ca84
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6648>

src/gallium/drivers/swr/rasterizer/jitter/builder_misc.cpp
@@ -133,91 +133,111 @@ namespace SwrJit
 
     Value* Builder::VIMMED1(uint64_t i)
     {
-#if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantInt>(C(i)));
-#else
+#if LLVM_VERSION_MAJOR <= 10
         return ConstantVector::getSplat(mVWidth, cast<ConstantInt>(C(i)));
+#elif LLVM_VERSION_MAJOR == 11
+        return ConstantVector::getSplat(ElementCount(mVWidth, false), cast<ConstantInt>(C(i)));
+#else
+        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantInt>(C(i)));
 #endif
     }
 
     Value* Builder::VIMMED1_16(uint64_t i)
     {
-#if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantInt>(C(i)));
-#else
+#if LLVM_VERSION_MAJOR <= 10
         return ConstantVector::getSplat(mVWidth16, cast<ConstantInt>(C(i)));
+#elif LLVM_VERSION_MAJOR == 11
+        return ConstantVector::getSplat(ElementCount(mVWidth16, false), cast<ConstantInt>(C(i)));
+#else
+        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantInt>(C(i)));
 #endif
     }
 
     Value* Builder::VIMMED1(int i)
     {
-#if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantInt>(C(i)));
-#else
+#if LLVM_VERSION_MAJOR <= 10
         return ConstantVector::getSplat(mVWidth, cast<ConstantInt>(C(i)));
+#elif LLVM_VERSION_MAJOR == 11
+        return ConstantVector::getSplat(ElementCount(mVWidth, false), cast<ConstantInt>(C(i)));
+#else
+        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantInt>(C(i)));
 #endif
     }
 
     Value* Builder::VIMMED1_16(int i)
     {
-#if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantInt>(C(i)));
-#else
+#if LLVM_VERSION_MAJOR <= 10
         return ConstantVector::getSplat(mVWidth16, cast<ConstantInt>(C(i)));
+#elif LLVM_VERSION_MAJOR == 11
+        return ConstantVector::getSplat(ElementCount(mVWidth16, false), cast<ConstantInt>(C(i)));
+#else
+        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantInt>(C(i)));
 #endif
     }
 
     Value* Builder::VIMMED1(uint32_t i)
     {
-#if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantInt>(C(i)));
-#else
+#if LLVM_VERSION_MAJOR <= 10
         return ConstantVector::getSplat(mVWidth, cast<ConstantInt>(C(i)));
+#elif LLVM_VERSION_MAJOR == 11
+        return ConstantVector::getSplat(ElementCount(mVWidth, false), cast<ConstantInt>(C(i)));
+#else
+        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantInt>(C(i)));
 #endif
     }
 
     Value* Builder::VIMMED1_16(uint32_t i)
     {
-#if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantInt>(C(i)));
-#else
+#if LLVM_VERSION_MAJOR <= 10
         return ConstantVector::getSplat(mVWidth16, cast<ConstantInt>(C(i)));
+#elif LLVM_VERSION_MAJOR == 11
+        return ConstantVector::getSplat(ElementCount(mVWidth16, false), cast<ConstantInt>(C(i)));
+#else
+        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantInt>(C(i)));
 #endif
     }
 
     Value* Builder::VIMMED1(float i)
     {
-#if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantFP>(C(i)));
-#else
+#if LLVM_VERSION_MAJOR <= 10
         return ConstantVector::getSplat(mVWidth, cast<ConstantFP>(C(i)));
+#elif LLVM_VERSION_MAJOR == 11
+        return ConstantVector::getSplat(ElementCount(mVWidth, false), cast<ConstantFP>(C(i)));
+#else
+        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantFP>(C(i)));
 #endif
     }
 
     Value* Builder::VIMMED1_16(float i)
     {
-#if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantFP>(C(i)));
-#else
+#if LLVM_VERSION_MAJOR <= 10
         return ConstantVector::getSplat(mVWidth16, cast<ConstantFP>(C(i)));
+#elif LLVM_VERSION_MAJOR == 11
+        return ConstantVector::getSplat(ElementCount(mVWidth16, false), cast<ConstantFP>(C(i)));
+#else
+        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantFP>(C(i)));
 #endif
     }
 
     Value* Builder::VIMMED1(bool i)
     {
-#if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantInt>(C(i)));
-#else
+#if LLVM_VERSION_MAJOR <= 10
         return ConstantVector::getSplat(mVWidth, cast<ConstantInt>(C(i)));
+#elif LLVM_VERSION_MAJOR == 11
+        return ConstantVector::getSplat(ElementCount(mVWidth, false), cast<ConstantInt>(C(i)));
+#else
+        return ConstantVector::getSplat(ElementCount::get(mVWidth, false), cast<ConstantInt>(C(i)));
 #endif
     }
 
     Value* Builder::VIMMED1_16(bool i)
     {
-#if LLVM_VERSION_MAJOR > 10
-        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantInt>(C(i)));
-#else
+#if LLVM_VERSION_MAJOR <= 10
         return ConstantVector::getSplat(mVWidth16, cast<ConstantInt>(C(i)));
+#elif LLVM_VERSION_MAJOR == 11
+        return ConstantVector::getSplat(ElementCount(mVWidth16, false), cast<ConstantInt>(C(i)));
+#else
+        return ConstantVector::getSplat(ElementCount::get(mVWidth16, false), cast<ConstantInt>(C(i)));
 #endif
     }
 

src/gallium/drivers/swr/rasterizer/jitter/functionpasses/lower_x86.cpp
@@ -555,10 +555,12 @@ namespace SwrJit
             B->STORE(vSrc, pTmp);
 
             v32Gather        = UndefValue::get(vSrc->getType());
-#if LLVM_VERSION_MAJOR > 10
-            auto vi32Scale   = ConstantVector::getSplat(ElementCount::get(numElem, false), cast<ConstantInt>(i32Scale));
-#else
+#if LLVM_VERSION_MAJOR <= 10
             auto vi32Scale   = ConstantVector::getSplat(numElem, cast<ConstantInt>(i32Scale));
+#elif LLVM_VERSION_MAJOR == 11
+            auto vi32Scale   = ConstantVector::getSplat(ElementCount(numElem, false), cast<ConstantInt>(i32Scale));
+#else
+            auto vi32Scale   = ConstantVector::getSplat(ElementCount::get(numElem, false), cast<ConstantInt>(i32Scale));
 #endif
             auto vi32Offsets = B->MUL(vi32Indices, vi32Scale);
 