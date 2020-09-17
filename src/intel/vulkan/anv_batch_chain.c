openharmony/third_party_mesa3d

commit 5296b01472ec4eb41631b5eaca50ca3e77a87eb1

anv: assert that the target bo is valid when adding a reloc list
this catches some undefined behavior like e.g., using a stale descriptorset
that references deleted bos, which I would absolutely never do

Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6747>

src/intel/vulkan/anv_batch_chain.c
@@ -200,6 +200,8 @@ anv_reloc_list_add(struct anv_reloc_list *list,
    if (result != VK_SUCCESS)
       return result;
 
+   assert(target_bo->gem_handle > 0 && target_bo->refcount > 0);
+
    /* XXX: Can we use I915_EXEC_HANDLE_LUT? */
    index = list->num_relocs++;
    list->reloc_bos[index] = target_bo;

commit e66a308736eaf48381f7b2d4271c95673e513257

anv: Fix the target_bo assertion in anv_reloc_list_add
Fixes: 5296b01472ec "anv: assert that the target bo is valid..."
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3528
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6759>

src/intel/vulkan/anv_batch_chain.c
@@ -188,6 +188,9 @@ anv_reloc_list_add(struct anv_reloc_list *list,
    if (address_u64_out)
       *address_u64_out = target_bo_offset + delta;
 
+   assert(unwrapped_target_bo->gem_handle > 0);
+   assert(unwrapped_target_bo->refcount > 0);
+
    if (unwrapped_target_bo->flags & EXEC_OBJECT_PINNED) {
       assert(!target_bo->is_wrapper);
       uint32_t idx = unwrapped_target_bo->gem_handle;
@@ -199,8 +202,6 @@ anv_reloc_list_add(struct anv_reloc_list *list,
    VkResult result = anv_reloc_list_grow(list, alloc, 1);
    if (result != VK_SUCCESS)
       return result;
-
-   assert(target_bo->gem_handle > 0 && target_bo->refcount > 0);
 
    /* XXX: Can we use I915_EXEC_HANDLE_LUT? */
    index = list->num_relocs++;