openharmony/third_party_mesa3d

commit f1ca514f484a10abbbe19a5a4fbf4290575ddf7e

panfrost: gen_pack: Allow empty structs
This is useful if we want to declare padding sections which can be
packed (filled with zeros) and unpacked (checked for non zero entries).

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6797>

src/panfrost/lib/gen_pack.py
@@ -345,7 +345,7 @@ class Group(object):
 
     def get_length(self):
         # Determine number of bytes in this group.
-        calculated = max(field.end // 8 for field in self.fields) + 1
+        calculated = max(field.end // 8 for field in self.fields) + 1 if len(self.fields) > 0 else 0
         if self.length > 0:
             assert(self.length >= calculated)
         else:
@@ -662,8 +662,10 @@ class Parser(object):
         print('#define %-40s\\' % (name + '_header'))
         if default_fields:
             print(",  \\\n".join(default_fields))
-        else:
+        elif len(self.group.fields) > 0:
             print('   0')
+        else:
+            print('   ')
         print('')
 
     def emit_template_struct(self, name, group, opaque_structs):

commit 4d508f865079c456dbe86df01907142188971543

panfrost: gen_pack: Fix gnu-empty-initializer errors
Fixes: f1ca514f484a ("panfrost: gen_pack: Allow empty structs")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Tested-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Reviewed-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6811>

src/panfrost/lib/gen_pack.py
@@ -378,6 +378,9 @@ class Group(object):
             if self.count > 1:
                 dim = "%s[%d]" % (dim, self.count)
 
+            if len(self.fields) == 0:
+                print("   int dummy;")
+
             for field in self.fields:
                 if field.exact is not None:
                     continue
@@ -692,10 +695,8 @@ class Parser(object):
         print('#define %-40s\\' % (name + '_header'))
         if default_fields:
             print(",  \\\n".join(default_fields))
-        elif len(self.group.fields) > 0:
-            print('   0')
         else:
-            print('   ')
+            print('   0')
         print('')
 
     def emit_template_struct(self, name, group, opaque_structs):