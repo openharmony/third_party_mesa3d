openharmony/third_party_mesa3d

commit 29fcc7f53752bacc1edb96e893619020420716e8

r600/sfn: Fix indirect const buffer access
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6879>

src/gallium/drivers/r600/sfn/sfn_ir_to_assembly.cpp
@@ -77,7 +77,7 @@ private:
    bool copy_dst(r600_bytecode_alu_dst& dst, const Value& src);
    bool copy_src(r600_bytecode_alu_src& src, const Value& s);
 
-
+   EBufferIndexMode emit_index_reg(const Value& reg, unsigned idx);
 
    ConditionalJumpTracker m_jump_tracker;
    CallStack m_callstack;
@@ -510,7 +510,7 @@ bool AssemblyFromShaderLegacyImpl::emit_if_start(const IfInstruction & if_instr)
 
    if (needs_workaround) {
 		r600_bytecode_add_cfinst(m_bc, CF_OP_PUSH);
-      m_bc->cf_last->cf_addr = m_bc->cf_last->id + 2;
+                m_bc->cf_last->cf_addr = m_bc->cf_last->id + 2;
 		op = cf_alu;
 	}
    emit_alu(pred, op);
@@ -726,37 +726,7 @@ bool AssemblyFromShaderLegacyImpl::emit_vtx(const FetchInstruction& fetch_instr)
          const auto& boffs = static_cast<const LiteralValue&>(*addr);
          buffer_offset = boffs.value();
       } else {
-         index_mode = bim_zero;
-         if ((!m_bc->index_loaded[0] || m_loop_nesting  ||
-              m_bc->index_reg[0] != addr->sel() ||
-              m_bc->index_reg_chan[0] != addr->chan())) {
-            struct r600_bytecode_alu alu;
-            memset(&alu, 0, sizeof(alu));
-            alu.op = opcode_map.at(op1_mova_int);
-            alu.dst.chan = 0;
-            alu.src[0].sel = addr->sel();
-            alu.src[0].chan = addr->chan();
-            alu.last = 1;
-            int r = r600_bytecode_add_alu(m_bc, &alu);
-            if (r)
-               return false;
-
-            m_bc->ar_loaded = 0;
-
-            alu.op = opcode_map.at(op1_set_cf_idx0);
-            alu.dst.chan = 0;
-            alu.src[0].sel = 0;
-            alu.src[0].chan = 0;
-            alu.last = 1;
-
-            r = r600_bytecode_add_alu(m_bc, &alu);
-            if (r)
-               return false;
-
-            m_bc->index_reg[0] = addr->sel();
-            m_bc->index_reg_chan[0] = addr->chan();
-            m_bc->index_loaded[0] = true;
-         }
+         index_mode = emit_index_reg(*addr, 0);
       }
    }
 
@@ -887,37 +857,7 @@ bool AssemblyFromShaderLegacyImpl::emit_gds(const GDSInstr& instr)
    int uav_idx = -1;
    auto addr = instr.uav_id();
    if (addr->type() != Value::literal) {
-      if (!m_bc->index_loaded[1] || m_loop_nesting ||
-          m_bc->index_reg[1] != addr->sel()
-          || m_bc->index_reg_chan[1] != addr->chan()) {
-         struct r600_bytecode_alu alu;
-
-         memset(&alu, 0, sizeof(alu));
-         alu.op = opcode_map.at(op1_mova_int);
-         alu.dst.chan = 0;
-         alu.src[0].sel = addr->sel();
-         alu.src[0].chan = addr->chan();
-         alu.last = 1;
-         int r = r600_bytecode_add_alu(m_bc, &alu);
-         if (r)
-            return false;
-
-         m_bc->ar_loaded = 0;
-
-         alu.op = opcode_map.at(op1_set_cf_idx1);
-         alu.dst.chan = 0;
-         alu.src[0].sel = 0;
-         alu.src[0].chan = 0;
-         alu.last = 1;
-
-         r = r600_bytecode_add_alu(m_bc, &alu);
-         if (r)
-            return false;
-
-         m_bc->index_reg[1] = addr->sel();
-         m_bc->index_reg_chan[1] = addr->chan();
-         m_bc->index_loaded[1] = true;
-      }
+      emit_index_reg(*addr, 1);
    } else {
       const LiteralValue& addr_reg = static_cast<const LiteralValue&>(*addr);
       uav_idx = addr_reg.value();
@@ -1102,39 +1042,7 @@ bool AssemblyFromShaderLegacyImpl::emit_rat(const RatInstruction& instr)
 
    if (addr) {
       if (addr->type() != Value::literal) {
-         rat_index_mode = bim_one;
-         if (!m_bc->index_loaded[1] || m_loop_nesting ||
-             m_bc->index_reg[1] != addr->sel()
-             ||  m_bc->index_reg_chan[1] != addr->chan()) {
-            struct r600_bytecode_alu alu;
-
-            memset(&alu, 0, sizeof(alu));
-            alu.op = opcode_map.at(op1_mova_int);
-            alu.dst.chan = 0;
-            alu.src[0].sel = addr->sel();
-            alu.src[0].chan = addr->chan();
-            alu.last = 1;
-            int r = r600_bytecode_add_alu(m_bc, &alu);
-            if (r)
-               return false;
-
-            m_bc->ar_loaded = 0;
-
-            alu.op = opcode_map.at(op1_set_cf_idx1);
-            alu.dst.chan = 0;
-            alu.src[0].sel = 0;
-            alu.src[0].chan = 0;
-            alu.last = 1;
-
-            r = r600_bytecode_add_alu(m_bc, &alu);
-            if (r)
-               return false;
-
-            m_bc->index_reg[1] = addr->sel();
-            m_bc->index_reg_chan[1] = addr->chan();
-            m_bc->index_loaded[1] = true;
-
-         }
+         rat_index_mode = emit_index_reg(*addr, 1);
       } else {
          const LiteralValue& addr_reg = static_cast<const LiteralValue&>(*addr);
          rat_idx += addr_reg.value();
@@ -1165,6 +1073,53 @@ bool AssemblyFromShaderLegacyImpl::emit_rat(const RatInstruction& instr)
    cf->mark = instr.need_ack();
    cf->output.elem_size = instr.elm_size();
    return true;
+}
+
+EBufferIndexMode
+AssemblyFromShaderLegacyImpl::emit_index_reg(const Value& addr, unsigned idx)
+{
+   assert(idx < 2);
+
+   EAluOp idxop = idx ? op1_set_cf_idx1 : op1_set_cf_idx0;
+
+   if (!m_bc->index_loaded[idx] || m_loop_nesting ||
+       m_bc->index_reg[idx] != addr.sel()
+       ||  m_bc->index_reg_chan[idx] != addr.chan()) {
+      struct r600_bytecode_alu alu;
+
+      // Make sure MOVA is not last instr in clause
+      if ((m_bc->cf_last->ndw>>1) >= 110)
+              m_bc->force_add_cf = 1;
+
+      memset(&alu, 0, sizeof(alu));
+      alu.op = opcode_map.at(op1_mova_int);
+      alu.dst.chan = 0;
+      alu.src[0].sel = addr.sel();
+      alu.src[0].chan = addr.chan();
+      alu.last = 1;
+      sfn_log << SfnLog::assembly << "   mova_int, ";
+      int r = r600_bytecode_add_alu(m_bc, &alu);
+      if (r)
+         return bim_invalid;
+
+      m_bc->ar_loaded = 0;
+
+      alu.op = opcode_map.at(idxop);
+      alu.dst.chan = 0;
+      alu.src[0].sel = 0;
+      alu.src[0].chan = 0;
+      alu.last = 1;
+      sfn_log << SfnLog::assembly << "op1_set_cf_idx" << idx;
+      r = r600_bytecode_add_alu(m_bc, &alu);
+      if (r)
+         return bim_invalid;
+
+      m_bc->index_reg[idx] = addr.sel();
+      m_bc->index_reg_chan[idx] = addr.chan();
+      m_bc->index_loaded[idx] = true;
+      sfn_log << SfnLog::assembly << "\n";
+   }
+   return idx == 1 ? bim_zero : bim_one;
 }
 
 bool AssemblyFromShaderLegacyImpl::copy_dst(r600_bytecode_alu_dst& dst,
@@ -1249,6 +1204,16 @@ bool AssemblyFromShaderLegacyImpl::copy_src(r600_bytecode_alu_src& src, const Va
    if (s.type() == Value::kconst) {
       const UniformValue& cv = static_cast<const UniformValue&>(s);
       src.kc_bank = cv.kcache_bank();
+      auto addr = cv.addr();
+      if (addr) {
+         src.kc_rel = 1;
+         emit_index_reg(*addr, 0);
+         auto type = m_bc->cf_last->op;
+         if (r600_bytecode_add_cf(m_bc)) {
+                 return false;
+         }
+         m_bc->cf_last->op = type;
+      }
    }
 
    return true;

src/gallium/drivers/r600/r600_asm.c
@@ -362,7 +362,7 @@ static int assign_alu_units(struct r600_bytecode *bc, struct r600_bytecode_alu *
 			}
 			assignment[4] = alu;
 		} else {
-			if (assignment[chan]) {
+			if (assignment[chan]) {                           
 				assert(0); /* ALU.chan has already been allocated. */
 				return -1;
 			}
@@ -1232,7 +1232,7 @@ int r600_bytecode_add_alu_type(struct r600_bytecode *bc,
 	/* Load index register if required */
 	if (bc->chip_class >= EVERGREEN) {
 		for (i = 0; i < 3; i++)
-			if (nalu->src[i].kc_bank && nalu->src[i].kc_rel)
+			if (nalu->src[i].kc_bank &&  nalu->src[i].kc_rel)
 				egcm_load_index_reg(bc, 0, true);
 	}
 

src/gallium/drivers/r600/sfn/sfn_emitaluinstruction.cpp
@@ -196,6 +196,7 @@ void EmitAluInstruction::preload_src(const nir_alu_instr& instr)
       for (unsigned c = 0; c < nsrc_comp; ++c) {
          m_src[i][c] = from_nir(instr.src[i], c);
          sfn_log << SfnLog::reg << " " << *m_src[i][c];
+
       }
       sfn_log << SfnLog::reg << "\n";
    }
@@ -262,7 +263,7 @@ void EmitAluInstruction::split_constants(const nir_alu_instr& instr, unsigned ns
        if (src->type() == Value::kconst) {
           c[nconst] = static_cast<const UniformValue *>(src.get());
           idx[nconst++] = i;
-          sfn_log << SfnLog::reg << "is constant " << i;
+          sfn_log << SfnLog::reg << " is constant " << i;
        }
        sfn_log << SfnLog::reg << "\n";
     }

src/gallium/drivers/r600/sfn/sfn_emitinstruction.cpp
@@ -80,6 +80,11 @@ void EmitInstruction::emit_instruction(Instruction *ir)
    return m_proc.emit_instruction(ir);
 }
 
+void EmitInstruction::emit_instruction(AluInstruction *ir)
+{
+   return m_proc.emit_instruction(ir);
+}
+
 bool EmitInstruction::emit_instruction(EAluOp opcode, PValue dest,
                                        std::vector<PValue> src0,
                                        const std::set<AluModifiers>& m_flags)
@@ -178,7 +183,6 @@ int EmitInstruction::remap_atomic_base(int base)
 {
 	return m_proc.remap_atomic_base(base);
 }
-
 
 const std::set<AluModifiers> EmitInstruction::empty = {};
 const std::set<AluModifiers> EmitInstruction::write = {alu_write};

src/gallium/drivers/r600/sfn/sfn_emitinstruction.h
@@ -72,6 +72,7 @@ protected:
 
    // forwards from ShaderFromNirProcessor
    void emit_instruction(Instruction *ir);
+   void emit_instruction(AluInstruction *ir);
    bool emit_instruction(EAluOp opcode, PValue dest,
                          std::vector<PValue> src0,
                          const std::set<AluModifiers>& m_flags);
@@ -94,7 +95,6 @@ protected:
                         const PValue& reg, bool map);
 
    int remap_atomic_base(int base);
-
 private:
 
    ShaderFromNirProcessor& m_proc;

src/gallium/drivers/r600/sfn/sfn_instruction_base.cpp
@@ -60,7 +60,13 @@ void ValueRemapper::remap(PValue& v)
       size_t range_end = range_start + val.array_size();
       while (range_start < range_end)
          m_map[range_start++].used = true;
+   } else if (v->type() == Value::kconst) {
+      auto& val = static_cast<UniformValue&>(*v);
+      auto addr = val.addr();
+      if (addr && addr->type() == Value::gpr)
+            val.reset_addr(remap_one_registers(addr));
    }
+
 }
 
 void ValueRemapper::remap(GPRVector& v)

src/gallium/drivers/r600/sfn/sfn_instruction_block.cpp
@@ -42,6 +42,11 @@ bool InstructionBlock::is_equal_to(const Instruction& lhs) const
                      [](PInstruction ri, PInstruction li) {return *ri == *li;});
 }
 
+PInstruction InstructionBlock::last_instruction()
+{
+   return m_block.size() ? *m_block.rbegin() : nullptr;
+}
+
 void InstructionBlock::do_print(std::ostream& os) const
 {
    std::string space(" ", 2 * m_nesting_depth);

src/gallium/drivers/r600/sfn/sfn_instruction_block.h
@@ -61,6 +61,8 @@ public:
            return m_block_number;
         }
 
+        PInstruction last_instruction();
+
 private:
         void do_evalue_liveness(LiverangeEvaluator& eval) const override;
         bool is_equal_to(const Instruction& lhs) const override;

commit d0ad77aec9501d1bc6be67f182b4596dbe4550cb

r600/sfn: Fix IDX register ID
Fixes: 29fcc7f5375
   r600/sfn: Fix indirect const buffer access

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6925>

src/gallium/drivers/r600/sfn/sfn_ir_to_assembly.cpp
@@ -1119,7 +1119,7 @@ AssemblyFromShaderLegacyImpl::emit_index_reg(const Value& addr, unsigned idx)
       m_bc->index_loaded[idx] = true;
       sfn_log << SfnLog::assembly << "\n";
    }
-   return idx == 1 ? bim_zero : bim_one;
+   return idx == 0 ? bim_zero : bim_one;
 }
 
 bool AssemblyFromShaderLegacyImpl::copy_dst(r600_bytecode_alu_dst& dst,