openharmony/third_party_mesa3d

commit b6689183c332d91c86a8ff9420bd29ccf1da8471

gallium/drm: Make the pipe loader handle the driconf merging.
We can pretty easily handle merging the driver's driconf with the common
driverconf right there, rather than pushing that to each driver.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6751>

src/gallium/drivers/radeonsi/Android.mk
@@ -55,17 +55,6 @@ intermediates := $(call local-generated-sources-dir)
 LOCAL_GENERATED_SOURCES := $(MESA_GEN_NIR_H)
 LOCAL_GENERATED_SOURCES += $(addprefix $(intermediates)/radeonsi/,$(GENERATED_SOURCES))
 
-GEN_DRIINFO_INPUTS := \
-	$(MESA_TOP)/src/gallium/auxiliary/pipe-loader/driinfo_gallium.h \
-	$(LOCAL_PATH)/driinfo_radeonsi.h
-
-MERGE_DRIINFO := $(MESA_TOP)/src/util/merge_driinfo.py
-
-$(intermediates)/radeonsi/si_driinfo.h: $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS)
-	@mkdir -p $(dir $@)
-	@echo "Gen Header: $(PRIVATE_MODULE) <= $(notdir $(@))"
-	$(hide) $(MESA_PYTHON2) $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS) > $@ || ($(RM) $@; false)
-
 LOCAL_C_INCLUDES += $(intermediates)/radeonsi
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(intermediates)

src/gallium/drivers/iris/Android.mk
@@ -151,17 +151,6 @@ intermediates := $(call local-generated-sources-dir)
 
 LOCAL_GENERATED_SOURCES := $(addprefix $(intermediates)/iris/,$(GENERATED_SOURCES))
 
-GEN_DRIINFO_INPUTS := \
-        $(MESA_TOP)/src/gallium/auxiliary/pipe-loader/driinfo_gallium.h \
-        $(LOCAL_PATH)/driinfo_iris.h
-
-MERGE_DRIINFO := $(MESA_TOP)/src/util/merge_driinfo.py
-
-$(intermediates)/iris/iris_driinfo.h: $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS)
-	@mkdir -p $(dir $@)
-	@echo "Gen Header: $(PRIVATE_MODULE) <= $(notdir $(@))"
-	$(hide) $(MESA_PYTHON2) $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS) > $@ || ($(RM) $@; false)
-
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(intermediates)
 
 LOCAL_SRC_FILES := \

src/gallium/drivers/virgl/Android.mk
@@ -41,13 +41,6 @@ GEN_DRIINFO_INPUTS := \
 	$(MESA_TOP)/src/gallium/auxiliary/pipe-loader/driinfo_gallium.h \
 	$(LOCAL_PATH)/virgl_driinfo.h.in
 
-MERGE_DRIINFO := $(MESA_TOP)/src/util/merge_driinfo.py
-
-$(intermediates)/virgl/virgl_driinfo.h: $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS)
-	@mkdir -p $(dir $@)
-	@echo "Gen Header: $(PRIVATE_MODULE) <= $(notdir $(@))"
-	$(hide) $(MESA_PYTHON2) $(MERGE_DRIINFO) $(GEN_DRIINFO_INPUTS) > $@ || ($(RM) $@; false)
-
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(intermediates)
 
 include $(GALLIUM_COMMON_MK)

src/gallium/auxiliary/pipe-loader/pipe_loader.c
@@ -52,9 +52,7 @@ static int (*backends[])(struct pipe_loader_device **, int) = {
 };
 
 const char gallium_driinfo_xml[] =
-   DRI_CONF_BEGIN
 #include "driinfo_gallium.h"
-   DRI_CONF_END
 ;
 
 int
@@ -93,26 +91,39 @@ pipe_loader_load_options(struct pipe_loader_device *dev)
    if (dev->option_info.info)
       return;
 
-   const char *xml_options = dev->ops->get_driconf_xml(dev);
-   if (!xml_options)
-      xml_options = gallium_driinfo_xml;
+   const char *driver_xml = dev->ops->get_driconf_xml(dev);
 
-   driParseOptionInfo(&dev->option_info, xml_options);
-   driParseConfigFiles(&dev->option_cache, &dev->option_info, 0,
-                       dev->driver_name, NULL, NULL, 0, NULL, 0);
+   char *xml_options;
+   int ret = asprintf(&xml_options, "%s%s%s%s",
+                      DRI_CONF_BEGIN,
+                      gallium_driinfo_xml,
+                      driver_xml ? driver_xml : "",
+                      DRI_CONF_END);
+   if (ret >= 0) {
+      driParseOptionInfo(&dev->option_info, xml_options);
+      driParseConfigFiles(&dev->option_cache, &dev->option_info, 0,
+                          dev->driver_name, NULL, NULL, 0, NULL, 0);
+      free(xml_options);
+   }
 }
 
 char *
 pipe_loader_get_driinfo_xml(const char *driver_name)
 {
 #ifdef HAVE_LIBDRM
-   char *xml = pipe_loader_drm_get_driinfo_xml(driver_name);
+   char *driver_xml = pipe_loader_drm_get_driinfo_xml(driver_name);
 #else
-   char *xml = NULL;
+   char *driver_xml = NULL;
 #endif
 
-   if (!xml)
-      xml = strdup(gallium_driinfo_xml);
+   char *xml;
+   int ret = asprintf(&xml, "%s%s%s%s",
+                      DRI_CONF_BEGIN,
+                      gallium_driinfo_xml,
+                      driver_xml ? driver_xml : "",
+                      DRI_CONF_END);
+   if (ret < 0)
+      xml = NULL;
 
    return xml;
 }

src/gallium/auxiliary/target-helpers/drm_helper.h
@@ -93,7 +93,7 @@ pipe_iris_create_screen(int fd, const struct pipe_screen_config *config)
 }
 
 const char *iris_driconf_xml =
-      #include "iris/iris_driinfo.h"
+      #include "iris/driinfo_iris.h"
       ;
 DRM_DRIVER_DESCRIPTOR(iris, &iris_driconf_xml)
 
@@ -120,7 +120,7 @@ DRM_DRIVER_DESCRIPTOR_STUB(nouveau)
 
 #if defined(GALLIUM_VC4) || defined(GALLIUM_V3D)
    const char *v3d_driconf_xml =
-      #include "v3d/v3d_driinfo.h"
+      #include "v3d/driinfo_v3d.h"
       ;
 #endif
 
@@ -195,7 +195,7 @@ pipe_radeonsi_create_screen(int fd, const struct pipe_screen_config *config)
 }
 
 const char *radeonsi_driconf_xml =
-      #include "radeonsi/si_driinfo.h"
+      #include "radeonsi/driinfo_radeonsi.h"
       ;
 DRM_DRIVER_DESCRIPTOR(radeonsi, &radeonsi_driconf_xml)
 
@@ -257,7 +257,7 @@ pipe_virtio_gpu_create_screen(int fd, const struct pipe_screen_config *config)
 }
 
 const char *virgl_driconf_xml =
-      #include "virgl/virgl_driinfo.h"
+      #include "virgl/virgl_driinfo.h.in"
       ;
 DRM_DRIVER_DESCRIPTOR(virtio_gpu, &virgl_driconf_xml)
 

src/gallium/drivers/iris/driinfo_iris.h
@@ -7,11 +7,7 @@ DRI_CONF_SECTION_DEBUG
 DRI_CONF_SECTION_END
 
 DRI_CONF_SECTION_PERFORMANCE
-
-//= BEGIN VERBATIM
    DRI_CONF_OPT_BEGIN_V(bo_reuse, enum, 1, "0:1")
       DRI_CONF_DESC("Buffer object reuse")
    DRI_CONF_OPT_END
-//= END VERBATIM
-
 DRI_CONF_SECTION_END

src/gallium/drivers/iris/meson.build
@@ -55,17 +55,6 @@ files_libiris = files(
   'iris_disk_cache.c',
 )
 
-iris_driinfo_h = custom_target(
-  'iris_driinfo.h',
-  input : files(
-    '../../../util/merge_driinfo.py',
-    '../../auxiliary/pipe-loader/driinfo_gallium.h', 'driinfo_iris.h'
-  ),
-  output : 'iris_driinfo.h',
-  command : [prog_python, '@INPUT@'],
-  capture : true,
-)
-
 iris_gen_libs = []
 foreach v : ['80', '90', '100', '110', '120']
   iris_gen_libs += static_library(
@@ -83,7 +72,7 @@ endforeach
 
 libiris = static_library(
   'iris',
-  [files_libiris, gen_xml_pack, iris_driinfo_h],
+  [files_libiris, gen_xml_pack],
   include_directories : [
     inc_src, inc_include, inc_gallium, inc_gallium_aux, inc_intel,
     inc_gallium_drivers,
@@ -102,6 +91,5 @@ libiris = static_library(
 
 driver_iris = declare_dependency(
   compile_args : '-DGALLIUM_IRIS',
-  sources : iris_driinfo_h,
   link_with : [libiris, libiriswinsys],
 )

src/gallium/drivers/radeonsi/driinfo_radeonsi.h
@@ -7,14 +7,10 @@ DRI_CONF_RADEONSI_ZERO_ALL_VRAM_ALLOCS("false")
 DRI_CONF_SECTION_END
 
 DRI_CONF_SECTION_DEBUG
-
-//= BEGIN VERBATIM
 #define OPT_BOOL(name, dflt, description)                                                          \
    DRI_CONF_OPT_BEGIN_B(radeonsi_##name, #dflt)                                                    \
    DRI_CONF_DESC(description)                                                                  \
    DRI_CONF_OPT_END
 
 #include "radeonsi/si_debug_options.h"
-//= END VERBATIM
-
 DRI_CONF_SECTION_END

commit b9c1ab78fdd406be4ee414e90303158ceb54c80c

android: gallium/radeonsi: cleanup si_driinfo.h gen rules
Android.mk and Makefile.sources are still defining si_driinfo.h target
This patch removes the remaining gen rules

Fixes the following building error:

FAILED: out/target/product/x86_64/obj/STATIC_LIBRARIES/libmesa_pipe_radeonsi_intermediates/radeonsi/si_driinfo.h
...
cp: bad 'out/target/product/x86_64/gen/STATIC_LIBRARIES/libmesa_pipe_radeonsi_intermediates/radeonsi/si_driinfo.h': No such file or directory

Fixes: b6689183c332 ("gallium/drm: Make the pipe loader handle the driconf merging.")
Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>
Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6880>

src/gallium/drivers/radeonsi/Android.mk
@@ -49,15 +49,8 @@ LOCAL_STATIC_LIBRARIES := \
 LOCAL_SHARED_LIBRARIES := libdrm_radeon
 LOCAL_MODULE := libmesa_pipe_radeonsi
 
-intermediates := $(call local-generated-sources-dir)
-
 # We need to get NIR's generated headers.
 LOCAL_GENERATED_SOURCES := $(MESA_GEN_NIR_H)
-LOCAL_GENERATED_SOURCES += $(addprefix $(intermediates)/radeonsi/,$(GENERATED_SOURCES))
-
-LOCAL_C_INCLUDES += $(intermediates)/radeonsi
-
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(intermediates)
 
 $(call mesa-build-with-llvm)
 

src/gallium/drivers/radeonsi/Makefile.sources
@@ -1,8 +1,4 @@
-GENERATED_SOURCES := \
-	si_driinfo.h
-
 C_SOURCES := \
-	$(GENERATED_SOURCES) \
 	cik_sdma.c \
 	driinfo_radeonsi.h \
 	gfx10_query.c \