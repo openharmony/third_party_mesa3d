openharmony/third_party_mesa3d

commit 6843968ba42643f70a637a8a9984b0dc340f7495

nir: Add lowering from regular ALU conversions to the intrinsic
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6945>

src/compiler/nir/nir.h
@@ -4388,6 +4388,7 @@ bool nir_opt_simplify_convert_alu_types(nir_shader *shader);
 bool nir_lower_convert_alu_types(nir_shader *shader,
                                  bool (*should_lower)(nir_intrinsic_instr *));
 bool nir_lower_constant_convert_alu_types(nir_shader *shader);
+bool nir_lower_alu_covnersion_to_intrinsic(nir_shader *shader);
 bool nir_lower_int_to_float(nir_shader *shader);
 bool nir_lower_load_const_to_scalar(nir_shader *shader);
 bool nir_lower_read_invocation_to_scalar(nir_shader *shader);

src/compiler/nir/nir_lower_convert_alu_types.c
@@ -180,3 +180,27 @@ nir_lower_constant_convert_alu_types(nir_shader *shader)
 {
    return nir_lower_convert_alu_types(shader, is_constant);
 }
+
+static bool
+is_alu_conversion(const nir_instr *instr, UNUSED const void *_data)
+{
+   return instr->type == nir_instr_type_alu &&
+          nir_op_infos[nir_instr_as_alu(instr)->op].is_conversion;
+}
+
+static nir_ssa_def *
+lower_alu_conversion(nir_builder *b, nir_instr *instr, UNUSED void *_data)
+{
+   nir_alu_instr *alu = nir_instr_as_alu(instr);
+   return nir_convert_alu_types(b, nir_ssa_for_alu_src(b, alu, 0),
+                                nir_op_infos[alu->op].input_types[0],
+                                nir_op_infos[alu->op].output_type,
+                                nir_rounding_mode_undef, false);
+}
+
+bool
+nir_lower_alu_covnersion_to_intrinsic(nir_shader *shader)
+{
+   return nir_shader_lower_instructions(shader, is_alu_conversion,
+                                        lower_alu_conversion, NULL);
+}

commit f7eaa381b83b9aa4b59695d3f9833443773a28a3

nir: Fix a misspelling
Fixes: 6843968ba4264 "nir: Add lowering from regular ALU conversions..."
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Reviewed-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6975>

src/compiler/nir/nir.h
@@ -4388,7 +4388,7 @@ bool nir_opt_simplify_convert_alu_types(nir_shader *shader);
 bool nir_lower_convert_alu_types(nir_shader *shader,
                                  bool (*should_lower)(nir_intrinsic_instr *));
 bool nir_lower_constant_convert_alu_types(nir_shader *shader);
-bool nir_lower_alu_covnersion_to_intrinsic(nir_shader *shader);
+bool nir_lower_alu_conversion_to_intrinsic(nir_shader *shader);
 bool nir_lower_int_to_float(nir_shader *shader);
 bool nir_lower_load_const_to_scalar(nir_shader *shader);
 bool nir_lower_read_invocation_to_scalar(nir_shader *shader);

src/compiler/nir/nir_lower_convert_alu_types.c
@@ -199,7 +199,7 @@ lower_alu_conversion(nir_builder *b, nir_instr *instr, UNUSED void *_data)
 }
 
 bool
-nir_lower_alu_covnersion_to_intrinsic(nir_shader *shader)
+nir_lower_alu_conversion_to_intrinsic(nir_shader *shader)
 {
    return nir_shader_lower_instructions(shader, is_alu_conversion,
                                         lower_alu_conversion, NULL);