openharmony/third_party_mesa3d

commit 23793b343fe9373ef2e651a49356f60a948e7d62

android: pan/bi: Use new disassembler
Fixes the following building error:

external/mesa/src/panfrost/bifrost/bi_pack.c:409:24: error: implicit declaration of function 'pan_pack_fma_nop_i32' is invalid in C99 [-Werror,-Wimplicit-function-declaration]
                return pan_pack_fma_nop_i32(clause, NULL, regs);
                       ^
external/mesa/src/panfrost/bifrost/bi_pack.c:433:32: error: implicit declaration of function 'pan_pack_fma_fadd_f32' is invalid in C99 [-Werror,-Wimplicit-function-declaration]
                        return pan_pack_fma_fadd_f32(clause, bundle.fma, regs);
                               ^
external/mesa/src/panfrost/bifrost/bi_pack.c:433:32: note: did you mean 'pan_pack_fma_nop_i32'?
external/mesa/src/panfrost/bifrost/bi_pack.c:409:24: note: 'pan_pack_fma_nop_i32' declared here
                return pan_pack_fma_nop_i32(clause, NULL, regs);
                       ^
external/mesa/src/panfrost/bifrost/bi_pack.c:435:32: error: implicit declaration of function 'pan_pack_fma_fadd_v2f16' is invalid in C99 [-Werror,-Wimplicit-function-declaration]
                        return pan_pack_fma_fadd_v2f16(clause, bundle.fma, regs);
                               ^
external/mesa/src/panfrost/bifrost/bi_pack.c:435:32: note: did you mean 'pan_pack_fma_fadd_f32'?
external/mesa/src/panfrost/bifrost/bi_pack.c:433:32: note: 'pan_pack_fma_fadd_f32' declared here
                        return pan_pack_fma_fadd_f32(clause, bundle.fma, regs);
                               ^
external/mesa/src/panfrost/bifrost/bi_pack.c:442:32: error: implicit declaration of function 'pan_pack_fma_fcmp_f32' is invalid in C99 [-Werror,-Wimplicit-function-declaration]
                        return pan_pack_fma_fcmp_f32(clause, bundle.fma, regs);
                               ^
external/mesa/src/panfrost/bifrost/bi_pack.c:442:32: note: did you mean 'pan_pack_fma_fadd_f32'?
external/mesa/src/panfrost/bifrost/bi_pack.c:433:32: note: 'pan_pack_fma_fadd_f32' declared here

src/panfrost/Android.bifrost.mk
@@ -70,6 +70,7 @@ LOCAL_STATIC_LIBRARIES := \
 
 LOCAL_GENERATED_SOURCES := \
 	$(intermediates)/bifrost_nir_algebraic.c \
+	$(intermediates)/bifrost_gen_disasms.c \
 	$(MESA_GEN_GLSL_H)
 
 bifrost_nir_algebraic_gen := $(LOCAL_PATH)/bifrost/bifrost_nir_algebraic.py
@@ -79,6 +80,13 @@ bifrost_nir_algebraic_deps := \
 $(intermediates)/bifrost_nir_algebraic.c: $(bifrost_nir_algebraic_deps)
 	@mkdir -p $(dir $@)
 	$(hide) $(MESA_PYTHON2) $(bifrost_nir_algebraic_gen) -p $< > $@
+
+bifrost_gen_disasms_gen := $(LOCAL_PATH)/bifrost/gen_disasm.py
+bifrost_gen_disasms_deps := $(LOCAL_PATH)/bifrost/ISA.xml
+
+$(intermediates)/bifrost_gen_disasms.c: $(bifrost_gen_disasms_deps)
+	@mkdir -p $(dir $@)
+	$(hide) $(MESA_PYTHON2) $(bifrost_gen_disasms_gen) $< > $@
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := \
 	$(MESA_TOP)/src/panfrost/bifrost/ \

commit 8957390ad7290afabc4a554b6f4967b1eafeddde

android: pan/bi: Use new disassembler (v2)
(v2) bifrost_gen_disasm.c generated source belongs to libpanfrost_bifrost_disasm

Fixes the following build errors, which happen with Android P, but not with Android Q

FAILED: out/target/product/x86_64/obj_x86/SHARED_LIBRARIES/gallium_dri_intermediates/LINKED/gallium_dri.so
...
external/mesa/src/panfrost/bifrost/disassemble.c:678: error: undefined reference to 'bi_disasm_fma'
external/mesa/src/panfrost/bifrost/disassemble.c:679: error: undefined reference to 'bi_disasm_add'

Fixes: 23793b343 ("android: pan/bi: Use new disassembler")
Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6996>

src/panfrost/Android.bifrost.mk
@@ -39,6 +39,19 @@ LOCAL_C_INCLUDES := \
 LOCAL_EXPORT_C_INCLUDE_DIRS := \
 	$(MESA_TOP)/src/panfrost/bifrost/ \
 
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+intermediates := $(call local-generated-sources-dir)
+
+LOCAL_GENERATED_SOURCES := \
+	$(intermediates)/bifrost_gen_disasm.c
+
+bifrost_gen_disasm_gen := $(LOCAL_PATH)/bifrost/gen_disasm.py
+bifrost_gen_disasm_deps := $(LOCAL_PATH)/bifrost/ISA.xml
+
+$(intermediates)/bifrost_gen_disasm.c: $(bifrost_gen_disasm_deps)
+	@mkdir -p $(dir $@)
+	$(hide) $(MESA_PYTHON2) $(bifrost_gen_disasm_gen) $< > $@
+
 include $(MESA_COMMON_MK)
 include $(BUILD_STATIC_LIBRARY)
 
@@ -70,7 +83,6 @@ LOCAL_STATIC_LIBRARIES := \
 
 LOCAL_GENERATED_SOURCES := \
 	$(intermediates)/bifrost_nir_algebraic.c \
-	$(intermediates)/bifrost_gen_disasm.c \
 	$(intermediates)/bi_generated_pack.h \
 	$(MESA_GEN_GLSL_H)
 
@@ -81,13 +93,6 @@ bifrost_nir_algebraic_deps := \
 $(intermediates)/bifrost_nir_algebraic.c: $(bifrost_nir_algebraic_deps)
 	@mkdir -p $(dir $@)
 	$(hide) $(MESA_PYTHON2) $(bifrost_nir_algebraic_gen) -p $< > $@
-
-bifrost_gen_disasm_gen := $(LOCAL_PATH)/bifrost/gen_disasm.py
-bifrost_gen_disasm_deps := $(LOCAL_PATH)/bifrost/ISA.xml
-
-$(intermediates)/bifrost_gen_disasm.c: $(bifrost_gen_disasm_deps)
-	@mkdir -p $(dir $@)
-	$(hide) $(MESA_PYTHON2) $(bifrost_gen_disasm_gen) $< > $@
 
 bi_generated_pack_gen := $(LOCAL_PATH)/bifrost/gen_pack.py
 bi_generated_pack_deps := $(LOCAL_PATH)/bifrost/ISA.xml