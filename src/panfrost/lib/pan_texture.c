openharmony/third_party_mesa3d

commit 64144468a3ecb370dd65efd4d0edd1826c758e38

panfrost: Add panfrost_block_dim helper
So we can calculate strides of block-based formats correctly. Will help
us down the road for Bifrost AFBC.

Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7081>

src/panfrost/lib/pan_texture.c
@@ -207,6 +207,28 @@ panfrost_estimate_texture_payload_size(
  */
 
 static unsigned
+panfrost_block_dim(uint64_t modifier, bool width, unsigned plane)
+{
+        if (!drm_is_afbc(modifier)) {
+                assert(modifier == DRM_FORMAT_MOD_ARM_16X16_BLOCK_U_INTERLEAVED);
+                return 16;
+        }
+
+        switch (modifier & AFBC_FORMAT_MOD_BLOCK_SIZE_MASK) {
+        case AFBC_FORMAT_MOD_BLOCK_SIZE_16x16:
+                return 16;
+        case AFBC_FORMAT_MOD_BLOCK_SIZE_32x8:
+                return width ? 32 : 32;
+        case AFBC_FORMAT_MOD_BLOCK_SIZE_64x4:
+                return width ? 64 : 4;
+        case AFBC_FORMAT_MOD_BLOCK_SIZE_32x8_64x4:
+                return plane ? (width ? 64 : 4) : (width ? 32 : 8);
+        default:
+                unreachable("Invalid AFBC block size");
+        }
+}
+
+static unsigned
 panfrost_nonlinear_stride(uint64_t modifier,
                 unsigned bytes_per_pixel,
                 unsigned width,

commit 686bcbd423702a789d3f62f780337a298804f830

panfrost: Fix stride for AFBC_FORMAT_MOD_BLOCK_SIZE_32x8.
Fix defect reported by Coverity Scan.

Identical code for different branches (IDENTICAL_BRANCHES)
identical_branches: Ternary expression on condition width has
identical then and else expressions: 32. Should one of the
expressions be modified, or the entire ternary expression
replaced?

Fixes: 64144468a3ec ("panfrost: Add panfrost_block_dim helper")
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Reviewed-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7177>

src/panfrost/lib/pan_texture.c
@@ -219,7 +219,7 @@ panfrost_block_dim(uint64_t modifier, bool width, unsigned plane)
         case AFBC_FORMAT_MOD_BLOCK_SIZE_16x16:
                 return 16;
         case AFBC_FORMAT_MOD_BLOCK_SIZE_32x8:
-                return width ? 32 : 32;
+                return width ? 32 : 8;
         case AFBC_FORMAT_MOD_BLOCK_SIZE_64x4:
                 return width ? 64 : 4;
         case AFBC_FORMAT_MOD_BLOCK_SIZE_32x8_64x4: