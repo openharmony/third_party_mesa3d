openharmony/third_party_mesa3d

commit 4713e60dd7b95232a673d9f9e824a04bd67283a7

util: Fix/silence variable shadowing warnings
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7552>

src/util/format/u_format.h
@@ -382,6 +382,9 @@ struct util_format_unpack_description {
 typedef void (*util_format_fetch_rgba_func_ptr)(void *dst, const uint8_t *src,
                                                 unsigned i, unsigned j);
 
+/* Silence warnings triggered by sharing function/struct names */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshadow"
 const struct util_format_description *
 util_format_description(enum pipe_format format) ATTRIBUTE_CONST;
 
@@ -390,6 +393,7 @@ util_format_pack_description(enum pipe_format format) ATTRIBUTE_CONST;
 
 const struct util_format_unpack_description *
 util_format_unpack_description(enum pipe_format format) ATTRIBUTE_CONST;
+#pragma GCC diagnostic pop
 
 /**
  * Returns a function to fetch a single pixel (i, j) from a block.

src/util/half_float.h
@@ -123,7 +123,7 @@ struct float16_t {
    uint16_t bits;
    float16_t(float f) : bits(_mesa_float_to_half(f)) {}
    float16_t(double d) : bits(_mesa_float_to_half(d)) {}
-   float16_t(uint16_t bits) : bits(bits) {}
+   float16_t(uint16_t raw_bits) : bits(raw_bits) {}
    static float16_t one() { return float16_t(FP16_ONE); }
    static float16_t zero() { return float16_t(FP16_ZERO); }
 };

commit 3f7bc05ae09b368ee34b1f3ab39ac6fbd10dc4e0

util: fix unknown pragma warning on msvc
MSVC has no idea about these pragmas, and spews warnings about them,
making it hard to spot real problems. So let's only use these macros on
GCC.

Fixes: 4713e60dd7b ("util: Fix/silence variable shadowing warnings")
Reviewed-by: Tony Wasserka <tony.wasserka@gmx.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7633>

src/util/format/u_format.h
@@ -383,8 +383,10 @@ typedef void (*util_format_fetch_rgba_func_ptr)(void *dst, const uint8_t *src,
                                                 unsigned i, unsigned j);
 
 /* Silence warnings triggered by sharing function/struct names */
+#ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wshadow"
+#endif
 const struct util_format_description *
 util_format_description(enum pipe_format format) ATTRIBUTE_CONST;
 
@@ -393,7 +395,9 @@ util_format_pack_description(enum pipe_format format) ATTRIBUTE_CONST;
 
 const struct util_format_unpack_description *
 util_format_unpack_description(enum pipe_format format) ATTRIBUTE_CONST;
+#ifdef __GNUC__
 #pragma GCC diagnostic pop
+#endif
 
 /**
  * Returns a function to fetch a single pixel (i, j) from a block.