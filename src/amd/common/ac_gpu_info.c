openharmony/third_party_mesa3d

commit 851aa36187986bdd0c1ab2966f3969aab794df40

ac: add radeon_info::is_pro_graphics
Tested-by: Dieter Nützel <Dieter@nuetzel-hh.de>
Acked-by: Nicolai Hähnle <nicolai.haehnle@amd.com>

src/amd/common/ac_gpu_info.c
@@ -334,6 +334,10 @@ bool ac_query_gpu_info(int fd, amdgpu_device_handle dev,
 	}
 
 	info->marketing_name = amdgpu_get_marketing_name(dev);
+	info->is_pro_graphics = info->marketing_name &&
+				(!strcmp(info->marketing_name, "Pro") ||
+				 !strcmp(info->marketing_name, "PRO") ||
+				 !strcmp(info->marketing_name, "Frontier"));
 
 	/* Set which chips have dedicated VRAM. */
 	info->has_dedicated_vram =

src/amd/common/ac_gpu_info.h
@@ -49,6 +49,7 @@ struct radeon_info {
 	/* Device info. */
 	const char                  *name;
 	const char                  *marketing_name;
+	bool                        is_pro_graphics;
 	uint32_t                    pci_id;
 	enum radeon_family          family;
 	enum chip_class             chip_class;

commit b482ac80ebbef9d44f5ef1985c32610ca8b4b15c

ac: fix detection of Pro graphics
Fixes: 851aa3618 "ac: add radeon_info::is_pro_graphics"

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7542>

src/amd/common/ac_gpu_info.c
@@ -468,9 +468,9 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
    info->family_id = amdinfo->family_id;
    info->chip_external_rev = amdinfo->chip_external_rev;
    info->marketing_name = amdgpu_get_marketing_name(dev);
-   info->is_pro_graphics = info->marketing_name && (!strcmp(info->marketing_name, "Pro") ||
-                                                    !strcmp(info->marketing_name, "PRO") ||
-                                                    !strcmp(info->marketing_name, "Frontier"));
+   info->is_pro_graphics = info->marketing_name && (!strstr(info->marketing_name, "Pro") ||
+                                                    !strstr(info->marketing_name, "PRO") ||
+                                                    !strstr(info->marketing_name, "Frontier"));
 
    /* Set which chips have dedicated VRAM. */
    info->has_dedicated_vram = !(amdinfo->ids_flags & AMDGPU_IDS_FLAGS_FUSION);