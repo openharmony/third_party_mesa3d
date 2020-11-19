openharmony/third_party_mesa3d

commit b482ac80ebbef9d44f5ef1985c32610ca8b4b15c

ac: fix detection of Pro graphics
Fixes: 851aa3618 "ac: add radeon_info::is_pro_graphics"

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7542>

src/amd/common/ac_gpu_info.c
@@ -468,9 +468,9 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
    info->family_id = amdinfo->family_id;
    info->chip_external_rev = amdinfo->chip_external_rev;
    info->marketing_name = amdgpu_get_marketing_name(dev);
-   info->is_pro_graphics = info->marketing_name && (!strcmp(info->marketing_name, "Pro") ||
-                                                    !strcmp(info->marketing_name, "PRO") ||
-                                                    !strcmp(info->marketing_name, "Frontier"));
+   info->is_pro_graphics = info->marketing_name && (!strstr(info->marketing_name, "Pro") ||
+                                                    !strstr(info->marketing_name, "PRO") ||
+                                                    !strstr(info->marketing_name, "Frontier"));
 
    /* Set which chips have dedicated VRAM. */
    info->has_dedicated_vram = !(amdinfo->ids_flags & AMDGPU_IDS_FLAGS_FUSION);

commit cc167e895d3dea14ed12a8aa53f6a3b9e874feec

ac: Don't negate strstr return values in ac_query_gpu_info
strstr returns a pointer to the needle sub-string within the haystack
string if the latter contains the former, or NULL otherwise. So this
essentially always set info->is_pro_graphics = true, since probably no
marketing name ever contains all of these sub-strings.

Fixes: b482ac80ebbe "ac: fix detection of Pro graphics"
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7675>

src/amd/common/ac_gpu_info.c
@@ -468,9 +468,9 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
    info->family_id = amdinfo->family_id;
    info->chip_external_rev = amdinfo->chip_external_rev;
    info->marketing_name = amdgpu_get_marketing_name(dev);
-   info->is_pro_graphics = info->marketing_name && (!strstr(info->marketing_name, "Pro") ||
-                                                    !strstr(info->marketing_name, "PRO") ||
-                                                    !strstr(info->marketing_name, "Frontier"));
+   info->is_pro_graphics = info->marketing_name && (strstr(info->marketing_name, "Pro") ||
+                                                    strstr(info->marketing_name, "PRO") ||
+                                                    strstr(info->marketing_name, "Frontier"));
 
    /* Set which chips have dedicated VRAM. */
    info->has_dedicated_vram = !(amdinfo->ids_flags & AMDGPU_IDS_FLAGS_FUSION);