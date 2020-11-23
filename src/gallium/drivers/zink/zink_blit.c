openharmony/third_party_mesa3d

commit 933206c78555185ac37dc9a7a47ff11205bec132

zink: more accurately track supported blits
We don't care if blits need to respect render-conditions if there's no
active one. So let's hit the potentially faster native blit-paths
instead.

Fixes: 4945daf995a ("zink: enable conditional rendering if available")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3792
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7606>

src/gallium/drivers/zink/zink_blit.c
@@ -14,8 +14,11 @@ blit_resolve(struct zink_context *ctx, const struct pipe_blit_info *info)
        util_format_get_mask(info->src.format) != info->mask ||
        util_format_is_depth_or_stencil(info->dst.format) ||
        info->scissor_enable ||
-       info->alpha_blend ||
-       info->render_condition_enable)
+       info->alpha_blend)
+      return false;
+
+   if (info->render_condition_enable &&
+       ctx->render_condition_active)
       return false;
 
    struct zink_resource *src = zink_resource(info->src.resource);
@@ -67,8 +70,11 @@ blit_native(struct zink_context *ctx, const struct pipe_blit_info *info)
    if (util_format_get_mask(info->dst.format) != info->mask ||
        util_format_get_mask(info->src.format) != info->mask ||
        info->scissor_enable ||
-       info->alpha_blend ||
-       info->render_condition_enable)
+       info->alpha_blend)
+      return false;
+
+   if (info->render_condition_enable &&
+       ctx->render_condition_active)
       return false;
 
    if (util_format_is_depth_or_stencil(info->dst.format) &&

src/gallium/drivers/zink/zink_context.h
@@ -130,7 +130,7 @@ struct zink_context {
 
    struct list_head suspended_queries;
    struct list_head primitives_generated_queries;
-   bool queries_disabled;
+   bool queries_disabled, render_condition_active;
 
    struct pipe_resource *dummy_buffer;
    struct pipe_resource *null_buffers[5]; /* used to create zink_framebuffer->null_surface, one buffer per samplecount */

src/gallium/drivers/zink/zink_query.c
@@ -492,6 +492,7 @@ zink_render_condition(struct pipe_context *pctx,
 
    if (query == NULL) {
       screen->vk_CmdEndConditionalRenderingEXT(batch->cmdbuf);
+      ctx->render_condition_active = false;
       return;
    }
 
@@ -528,6 +529,7 @@ zink_render_condition(struct pipe_context *pctx,
    begin_info.buffer = res->buffer;
    begin_info.flags = begin_flags;
    screen->vk_CmdBeginConditionalRenderingEXT(batch->cmdbuf, &begin_info);
+   ctx->render_condition_active = true;
 
    zink_batch_reference_resource_rw(batch, res, true);
 

commit 5622f3e25fa6183130f4649cb043daed01df9474

zink: fix layered resolves
Until recently, we ended up using u_blitter here, because
info->render_condition_enable was always true here. But when we recently
fixed that overly broad check, this broke.

So let's fix layered-resolves, by actually checking if the resource has
layers respect them in that case, similar to what we do in blit_native.

Fixes: 933206c7855 ("zink: more accurately track supported blits")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3843
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7737>

src/gallium/drivers/zink/zink_blit.c
@@ -40,19 +40,35 @@ blit_resolve(struct zink_context *ctx, const struct pipe_blit_info *info)
 
    region.srcSubresource.aspectMask = src->aspect;
    region.srcSubresource.mipLevel = info->src.level;
-   region.srcSubresource.baseArrayLayer = 0; // no clue
-   region.srcSubresource.layerCount = 1; // no clue
    region.srcOffset.x = info->src.box.x;
    region.srcOffset.y = info->src.box.y;
-   region.srcOffset.z = info->src.box.z;
+
+   if (src->base.array_size > 1) {
+      region.srcOffset.z = 0;
+      region.srcSubresource.baseArrayLayer = info->src.box.z;
+      region.srcSubresource.layerCount = info->src.box.depth;
+   } else {
+      assert(info->src.box.depth == 1);
+      region.srcOffset.z = info->src.box.z;
+      region.srcSubresource.baseArrayLayer = 0;
+      region.srcSubresource.layerCount = 1;
+   }
 
    region.dstSubresource.aspectMask = dst->aspect;
    region.dstSubresource.mipLevel = info->dst.level;
-   region.dstSubresource.baseArrayLayer = 0; // no clue
-   region.dstSubresource.layerCount = 1; // no clue
    region.dstOffset.x = info->dst.box.x;
    region.dstOffset.y = info->dst.box.y;
-   region.dstOffset.z = info->dst.box.z;
+
+   if (dst->base.array_size > 1) {
+      region.dstOffset.z = 0;
+      region.dstSubresource.baseArrayLayer = info->dst.box.z;
+      region.dstSubresource.layerCount = info->dst.box.depth;
+   } else {
+      assert(info->dst.box.depth == 1);
+      region.dstOffset.z = info->dst.box.z;
+      region.dstSubresource.baseArrayLayer = 0;
+      region.dstSubresource.layerCount = 1;
+   }
 
    region.extent.width = info->dst.box.width;
    region.extent.height = info->dst.box.height;