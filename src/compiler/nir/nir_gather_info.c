openharmony/third_party_mesa3d

commit 79becdbcacf0a971006875bf4cf8040b888252c4

nir: Use src_is_invocation_id in get_deref_info.
Signed-off-by: Timur Kristof <timur.kristof@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7622>

src/compiler/nir/nir_gather_info.c
@@ -53,11 +53,7 @@ get_deref_info(nir_shader *shader, nir_variable *var, nir_deref_instr *deref,
    /* Vertex index is the outermost array index. */
    if (per_vertex) {
       assert((*p)->deref_type == nir_deref_type_array);
-      nir_instr *vertex_index_instr = (*p)->arr.index.ssa->parent_instr;
-      *cross_invocation =
-         vertex_index_instr->type != nir_instr_type_intrinsic ||
-         nir_instr_as_intrinsic(vertex_index_instr)->intrinsic !=
-            nir_intrinsic_load_invocation_id;
+      *cross_invocation = src_is_invocation_id(&(*p)->arr.index);
       p++;
    }
 

commit 5025b4307eacf276880d46179b645263b848adbe

nir: fix gathering cross invocation info
Fixes: 79becdbcacf ("nir: Use src_is_invocation_id in get_deref_info.")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7730>

src/compiler/nir/nir_gather_info.c
@@ -53,7 +53,7 @@ get_deref_info(nir_shader *shader, nir_variable *var, nir_deref_instr *deref,
    /* Vertex index is the outermost array index. */
    if (per_vertex) {
       assert((*p)->deref_type == nir_deref_type_array);
-      *cross_invocation = src_is_invocation_id(&(*p)->arr.index);
+      *cross_invocation = !src_is_invocation_id(&(*p)->arr.index);
       p++;
    }
 