openharmony/third_party_mesa3d

commit a9a8d0adaba828f65ea564a4ee418b9ef82360e0

glsl: fix up location setting for variables pointing to a UBO's base
while linking uniforms, we might get a variable which is the only reference
to the ubo (i.e., offset 0), as determined by its type being the UBO's
interface_type, at which point we can assign the previously-gotten
block index to this variable's location

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5831>

src/compiler/glsl/gl_nir_link_uniforms.c
@@ -1727,54 +1727,57 @@ gl_nir_link_uniforms(struct gl_context *ctx,
             }
          }
 
-         if (!prog->data->spirv && state.var_is_in_block &&
-             glsl_without_array(state.current_var->type) != state.current_var->interface_type) {
+         if (!prog->data->spirv && state.var_is_in_block) {
+            if (glsl_without_array(state.current_var->type) != state.current_var->interface_type) {
+               /* this is nested at some offset inside the block */
+               bool found = false;
+               char sentinel = '\0';
 
-            bool found = false;
-            char sentinel = '\0';
-
-            if (glsl_type_is_struct(state.current_var->type)) {
-               sentinel = '.';
-            } else if (glsl_type_is_array(state.current_var->type) &&
-                       (glsl_type_is_array(glsl_get_array_element(state.current_var->type))
-                        || glsl_type_is_struct(glsl_without_array(state.current_var->type)))) {
-              sentinel = '[';
-            }
-
-            const unsigned l = strlen(state.current_var->name);
-            for (unsigned i = 0; i < num_blocks; i++) {
-               for (unsigned j = 0; j < blocks[i].NumUniforms; j++) {
-                 if (sentinel) {
-                     const char *begin = blocks[i].Uniforms[j].Name;
-                     const char *end = strchr(begin, sentinel);
-
-                     if (end == NULL)
-                        continue;
-
-                     if ((ptrdiff_t) l != (end - begin))
-                        continue;
-                     found = strncmp(state.current_var->name, begin, l) == 0;
-                  } else {
-                     found = strcmp(state.current_var->name, blocks[i].Uniforms[j].Name) == 0;
-                  }
-
-                  if (found) {
-                     location = j;
-
-                     struct hash_entry *entry =
-                        _mesa_hash_table_search(state.referenced_uniforms[shader_type], var->name);
-                     if (entry)
-                        blocks[i].stageref |= 1U << shader_type;
-
-                     break;
-                  }
+               if (glsl_type_is_struct(state.current_var->type)) {
+                  sentinel = '.';
+               } else if (glsl_type_is_array(state.current_var->type) &&
+                          (glsl_type_is_array(glsl_get_array_element(state.current_var->type))
+                           || glsl_type_is_struct(glsl_without_array(state.current_var->type)))) {
+                 sentinel = '[';
                }
 
-               if (found)
-                  break;
-            }
-            assert(found);
+               const unsigned l = strlen(state.current_var->name);
+               for (unsigned i = 0; i < num_blocks; i++) {
+                  for (unsigned j = 0; j < blocks[i].NumUniforms; j++) {
+                    if (sentinel) {
+                        const char *begin = blocks[i].Uniforms[j].Name;
+                        const char *end = strchr(begin, sentinel);
 
+                        if (end == NULL)
+                           continue;
+
+                        if ((ptrdiff_t) l != (end - begin))
+                           continue;
+                        found = strncmp(state.current_var->name, begin, l) == 0;
+                     } else {
+                        found = strcmp(state.current_var->name, blocks[i].Uniforms[j].Name) == 0;
+                     }
+
+                     if (found) {
+                        location = j;
+
+                        struct hash_entry *entry =
+                           _mesa_hash_table_search(state.referenced_uniforms[shader_type], var->name);
+                        if (entry)
+                           blocks[i].stageref |= 1U << shader_type;
+
+                        break;
+                     }
+                  }
+
+                  if (found)
+                     break;
+               }
+               assert(found);
+            } else
+               /* this is the base block offset */
+               location = buffer_block_index;
+            assert(buffer_block_index >= 0);
             const struct gl_uniform_block *const block =
                &blocks[buffer_block_index];
             assert(location != -1);

commit eaf874585ad1f67acb75a7ab14994c55ae2eac8a

glsl: avoid an out-of-bound access while setting up a location for variable
It fixes the following valgrind issue:
==141996== Invalid read of size 4
==141996==    at 0x61F8806: gl_nir_link_uniforms (gl_nir_link_uniforms.c:1788)
==141996==    by 0x60F17AA: gl_nir_link_glsl (gl_nir_linker.c:672)
==141996==    by 0x5C1AEDF: st_link_nir (st_glsl_to_nir.cpp:739)
==141996==    by 0x5C15574: st_link_shader (st_glsl_to_ir.cpp:172)
==141996==    by 0x5C673B0: _mesa_glsl_link_shader (ir_to_mesa.cpp:3117)
==141996==    by 0x5E7B61C: link_program (shaderapi.c:1311)
==141996==    by 0x5E7B61C: link_program_error (shaderapi.c:1419)
==141996==    by 0x5E7CF8A: _mesa_LinkProgram (shaderapi.c:1911)
==141996==    by 0x4923D13: stub_glLinkProgram (piglit-dispatch-gen.c:33956)
==141996==    by 0x1142C0: link_and_use_shaders (shader_runner.c:1636)
==141996==    by 0x1205A6: init_test (shader_runner.c:5347)
==141996==    by 0x121555: piglit_init (shader_runner.c:5725)
==141996==    by 0x4991C84: run_test (piglit_fbo_framework.c:50)

It can be reproduced on `iris` using the following piglit test:
instance-matching-shader-storage-blocks-align-qualifier-mismatch.shader_test

Closes: #3818
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Fixes: a9a8d0ad ("glsl: fix up location setting for variables pointing to a UBO's base")
Signed-off-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Signed-off-by: Andrii Simiklit <andrii.simiklit@globallogic.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7376>

src/compiler/glsl/gl_nir_link_uniforms.c
@@ -1776,19 +1776,21 @@ gl_nir_link_uniforms(struct gl_context *ctx,
                      break;
                }
                assert(found);
-            } else
+               var->data.location = location;
+            } else {
                /* this is the base block offset */
-               location = buffer_block_index;
+               var->data.location = buffer_block_index;
+               location = 0;
+            }
             assert(buffer_block_index >= 0);
             const struct gl_uniform_block *const block =
                &blocks[buffer_block_index];
-            assert(location != -1);
+            assert(location >= 0 && location < block->NumUniforms);
 
             const struct gl_uniform_buffer_variable *const ubo_var =
                &block->Uniforms[location];
 
             state.offset = ubo_var->Offset;
-            var->data.location = location;
          }
 
          /* Check if the uniform has been processed already for