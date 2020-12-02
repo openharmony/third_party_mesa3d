openharmony/third_party_mesa3d

commit 4401c814838df9ab44a9ef527a3f7cac83899f5f

gitlab-ci: run piglit on windows
This tests the D3D12 driver on Windows during CI, to prevent accidental
breakage.

Reviewed-by: Daniel Stone <daniels@collabora.com>
Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7857>

.gitlab-ci.yml
@@ -31,6 +31,7 @@ stages:
   - lima
   - virgl
   - radeonsi
+  - d3d12
   - success
 
 # Generic rule to not run the job during scheduled pipelines
@@ -809,6 +810,26 @@ meson-windows-vs2019:
   artifacts:
     paths:
       - _install/
+
+test-d3d12-windows:
+  extends:
+    - .build-windows
+    - .use-windows_build_vs2019
+  stage: d3d12
+  dependencies:
+    - meson-windows-vs2019
+  needs:
+    - meson-windows-vs2019
+  variables:
+    GALLIUM_DRIVER: d3d12
+    PIGLIT_PROFILE: quick_gl
+  script:
+    - . .\.gitlab-ci\windows\piglit_run.ps1
+  artifacts:
+    when: on_failure
+    name: "mesa_${CI_JOB_NAME}"
+    paths:
+      - summary/
 
 scons-win64:
   extends: .scons-build

.gitlab-ci/windows/piglit_run.ps1
@@ -1,0 +1,21 @@
+$env:PIGLIT_NO_FAST_SKIP = 1
+
+Copy-Item -Path _install\bin\opengl32.dll -Destination C:\Piglit\lib\piglit\bin\opengl32.dll
+
+# Run this using VsDevCmd.bat to ensure DXIL.dll is in %PATH%
+cmd.exe /C "C:\BuildTools\Common7\Tools\VsDevCmd.bat -host_arch=amd64 -arch=amd64 && py -3 C:\Piglit\bin\piglit.py run `"$env:PIGLIT_PROFILE`" $env:PIGLIT_OPTIONS .\results"
+
+py -3 C:\Piglit\bin\piglit.py summary console .\results | Select -SkipLast 1 | Select-String -NotMatch -Pattern ': pass' | Set-Content -Path .\result.txt
+
+$diff = Compare-Object -ReferenceObject $(Get-Content ".gitlab-ci\windows\$env:PIGLIT_PROFILE.txt") `
+                       -DifferenceObject $(Get-Content .\result.txt)
+if (-Not $diff) {
+  Exit 0
+}
+
+py -3 C:\Piglit\bin\piglit.py summary html --exclude-details=pass .\summary .\results
+
+Write-Host "Unexpected change in results:"
+Write-Output $diff | Format-Table -Property SideIndicator,InputObject -Wrap
+
+Exit 1

commit cc23178cd7d2dba0310f2195de57a2fb5325c8f1

gitlab-ci: do not clone git-repo for test-job
The only thing we really need from the git-repo is the piglit_run.ps1
script, so let's upload that into our artifacts instead.

Fixes: 4401c814838 ("gitlab-ci: run piglit on windows")
Acked-by: Michel Dänzer <mdaenzer@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7867>

.gitlab-ci.yml
@@ -821,11 +821,12 @@ test-d3d12-windows:
   needs:
     - meson-windows-vs2019
   variables:
+    GIT_STRATEGY: none # testing doesn't build anything from source
     GALLIUM_DRIVER: d3d12
     PIGLIT_PROFILE: quick_gl
     PIGLIT_OPTIONS: -x nv_copy_depth_to_color
   script:
-    - . .\.gitlab-ci\windows\piglit_run.ps1
+    - . _install/piglit_run.ps1
   artifacts:
     when: on_failure
     name: "mesa_${CI_JOB_NAME}"

.gitlab-ci/windows/mesa_build.ps1
@@ -17,3 +17,5 @@ if (!$buildstatus) {
   Write-Host "Mesa build or test failed"
   Exit 1
 }
+
+Copy-Item ".\.gitlab-ci\windows\piglit_run.ps1" -Destination $installdir