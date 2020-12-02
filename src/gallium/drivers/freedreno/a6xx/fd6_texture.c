openharmony/third_party_mesa3d

commit 2cd7584f18012f88dc3eab48bb700751a807625b

freedreno/a6xx: invalidate tex state cache entries on rebind
When a resource's backing bo changes, its seqno will be incremented.
Which would result in a new tex state cache key, and nothing to clean
up the old tex state until the sampler view/state is destroyed.  But
in some games, that may never happen, or at least not happen before
we run out of memory.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/2830
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4744>

src/gallium/drivers/freedreno/a6xx/fd6_texture.c
@@ -441,10 +441,31 @@ fd6_texture_state_destroy(struct fd6_texture_state *state)
 	free(state);
 }
 
+static void
+fd6_rebind_resource(struct fd_context *ctx, struct fd_resource *rsc)
+{
+	if (!(rsc->dirty & FD_DIRTY_TEX))
+		return;
+
+	struct fd6_context *fd6_ctx = fd6_context(ctx);
+
+	hash_table_foreach (fd6_ctx->tex_cache, entry) {
+		struct fd6_texture_state *state = entry->data;
+
+		for (unsigned i = 0; i < ARRAY_SIZE(state->key.view); i++) {
+			if (rsc->seqno == state->key.view[i].rsc_seqno) {
+				fd6_texture_state_destroy(entry->data);
+				_mesa_hash_table_remove(fd6_ctx->tex_cache, entry);
+			}
+		}
+	}
+}
+
 void
 fd6_texture_init(struct pipe_context *pctx)
 {
-	struct fd6_context *fd6_ctx = fd6_context(fd_context(pctx));
+	struct fd_context *ctx = fd_context(pctx);
+	struct fd6_context *fd6_ctx = fd6_context(ctx);
 
 	pctx->create_sampler_state = fd6_sampler_state_create;
 	pctx->delete_sampler_state = fd6_sampler_state_delete;
@@ -453,6 +474,8 @@ fd6_texture_init(struct pipe_context *pctx)
 	pctx->create_sampler_view = fd6_sampler_view_create;
 	pctx->sampler_view_destroy = fd6_sampler_view_destroy;
 	pctx->set_sampler_views = fd_set_sampler_views;
+
+	ctx->rebind_resource = fd6_rebind_resource;
 
 	fd6_ctx->tex_cache = _mesa_hash_table_create(NULL, key_hash, key_equals);
 }

src/gallium/drivers/freedreno/freedreno_context.h
@@ -324,6 +324,9 @@ struct fd_context {
 
 	struct pipe_debug_callback debug;
 
+	/* Called on rebind_resource() for any per-gen cleanup required: */
+	void (*rebind_resource)(struct fd_context *ctx, struct fd_resource *rsc);
+
 	/* GMEM/tile handling fxns: */
 	void (*emit_tile_init)(struct fd_batch *batch);
 	void (*emit_tile_prep)(struct fd_batch *batch, const struct fd_tile *tile);

src/gallium/drivers/freedreno/freedreno_resource.c
@@ -69,6 +69,9 @@ rebind_resource_in_ctx(struct fd_context *ctx, struct fd_resource *rsc)
 {
 	struct pipe_resource *prsc = &rsc->base;
 
+	if (ctx->rebind_resource)
+		ctx->rebind_resource(ctx, rsc);
+
 	/* VBOs */
 	if (rsc->dirty & FD_DIRTY_VTXBUF) {
 		struct fd_vertexbuf_stateobj *vb = &ctx->vtx.vertexbuf;

commit de40e6b4d468efdd3df41d00b0ae1c671e48ac53

freedreno: Break out of "should we free the entry" loop once we've freed.
Fixes a use-after-free of the state on the next iteration when it was
probably just destroyed.

Fixes: 2cd7584f1801 ("freedreno/a6xx: invalidate tex state cache entries on rebind")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7695>

src/gallium/drivers/freedreno/a6xx/fd6_texture.c
@@ -512,6 +512,7 @@ fd6_rebind_resource(struct fd_context *ctx, struct fd_resource *rsc)
 		for (unsigned i = 0; i < ARRAY_SIZE(state->key.view); i++) {
 			if (rsc->seqno == state->key.view[i].rsc_seqno) {
 				remove_tex_entry(fd6_ctx, entry);
+				break;
 			}
 		}
 	}