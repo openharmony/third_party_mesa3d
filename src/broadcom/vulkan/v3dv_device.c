openharmony/third_party_mesa3d

commit 15bc6fed37c73ce5ad6a379bb11721e1f4a790d7

v3dv: check return value of drmGetMagic
Fixes coverty issue:

** CID 1470345: Error handling issues (CHECKED_RETURN)
/src/broadcom/vulkan/v3dv_device.c: 407 in v3dv_drm_handle_device()
Calling "drmGetMagic" without checking return value (as is done
elsewhere 5 out of 6 times).

Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7828>

src/broadcom/vulkan/v3dv_device.c
@@ -404,7 +404,10 @@ v3dv_drm_handle_device(void *data, struct wl_drm *drm, const char *device)
    info->is_set = info->fd != -1;
 
    drm_magic_t magic;
-   drmGetMagic(info->fd, &magic);
+   if (drmGetMagic(info->fd, &magic)) {
+      fprintf(stderr, "drmGetMagic failed.");
+      return;
+   }
    wl_drm_authenticate(info->wl_drm, magic);
 }
 

commit 72a756128e6ccd29f98a5610d25a270d636e6c7a

v3dv: fix early return from failed drmGetMagic
v2: Log more detail to stderr upon failure (Chema)

Fixes: 15bc6fed37c ('v3dv: check return value of drmGetMagic')
Reviewed-by: Jose Maria Casanova Crespo <jmcasanova@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7865>

src/broadcom/vulkan/v3dv_device.c
@@ -402,10 +402,18 @@ v3dv_drm_handle_device(void *data, struct wl_drm *drm, const char *device)
    struct v3dv_wayland_info *info = data;
    info->fd = open(device, O_RDWR | O_CLOEXEC);
    info->is_set = info->fd != -1;
+   if (!info->is_set) {
+      fprintf(stderr, "v3dv_drm_handle_device: could not open %s (%s)\n",
+              device, strerror(errno));
+      return;
+   }
 
    drm_magic_t magic;
    if (drmGetMagic(info->fd, &magic)) {
-      fprintf(stderr, "drmGetMagic failed.");
+      fprintf(stderr, "v3dv_drm_handle_device: drmGetMagic failed\n");
+      close(info->fd);
+      info->fd = -1;
+      info->is_set = false;
       return;
    }
    wl_drm_authenticate(info->wl_drm, magic);