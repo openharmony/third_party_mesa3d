openharmony/third_party_mesa3d

commit f25def8fe393d4c7d2b81bcbe0a25145cc26ae41

driconf: Make the driver's declarations be structs instead of XML.
We can generate the XML if anybody actually queries it, but this reduces
the amount of work in driver setup and means that we'll be able to support
driconf option queries on Android without libexpat.

This updates the driconf interface struct version for i965, i915, and
radeon to use the new getXml entrypoint to call the on-demand xml
generation.  Note that our loaders (egl, glx) implement the v2 function
interface and don't use .xml when that's set, and the X server doesn't use
this interface at all.

XML generation tested on iris and i965 using adriconf

Acked-by: Eric Engestrom <eric@engestrom.ch>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6916>

src/gallium/auxiliary/pipe-loader/pipe_loader.c
@@ -51,9 +51,9 @@ static int (*backends[])(struct pipe_loader_device **, int) = {
    &pipe_loader_sw_probe
 };
 
-const char gallium_driinfo_xml[] =
+const driOptionDescription gallium_driconf[] = {
 #include "driinfo_gallium.h"
-;
+};
 
 int
 pipe_loader_probe(struct pipe_loader_device **devs, int ndev)
@@ -85,45 +85,62 @@ pipe_loader_base_release(struct pipe_loader_device **dev)
    *dev = NULL;
 }
 
+static driOptionDescription *
+merge_driconf(const driOptionDescription *driver_driconf, unsigned driver_count,
+              unsigned *merged_count)
+{
+   unsigned gallium_count = ARRAY_SIZE(gallium_driconf);
+   driOptionDescription *merged = malloc((driver_count + gallium_count) *
+                                         sizeof(*merged));
+   if (!merged) {
+      *merged_count = 0;
+      return NULL;
+   }
+
+   memcpy(merged, gallium_driconf, sizeof(*merged) * gallium_count);
+   memcpy(&merged[gallium_count], driver_driconf, sizeof(*merged) * driver_count);
+
+   *merged_count = driver_count + gallium_count;
+   return merged;
+}
+
 void
 pipe_loader_load_options(struct pipe_loader_device *dev)
 {
    if (dev->option_info.info)
       return;
 
-   const char *driver_xml = dev->ops->get_driconf_xml(dev);
+   unsigned driver_count, merged_count;
+   const driOptionDescription *driver_driconf =
+      dev->ops->get_driconf(dev, &driver_count);
 
-   char *xml_options;
-   int ret = asprintf(&xml_options, "%s%s%s%s",
-                      DRI_CONF_BEGIN,
-                      gallium_driinfo_xml,
-                      driver_xml ? driver_xml : "",
-                      DRI_CONF_END);
-   if (ret >= 0) {
-      driParseOptionInfo(&dev->option_info, xml_options);
-      driParseConfigFiles(&dev->option_cache, &dev->option_info, 0,
-                          dev->driver_name, NULL, NULL, 0, NULL, 0);
-      free(xml_options);
-   }
+   const driOptionDescription *merged_driconf =
+      merge_driconf(driver_driconf, driver_count, &merged_count);
+
+   driParseOptionInfo(&dev->option_info, merged_driconf, merged_count);
+   driParseConfigFiles(&dev->option_cache, &dev->option_info, 0,
+                       dev->driver_name, NULL, NULL, 0, NULL, 0);
 }
 
 char *
 pipe_loader_get_driinfo_xml(const char *driver_name)
 {
+   unsigned driver_count = 0;
+   const driOptionDescription *driver_driconf = NULL;
+
 #ifdef HAVE_LIBDRM
-   char *driver_xml = pipe_loader_drm_get_driinfo_xml(driver_name);
-#else
-   char *driver_xml = NULL;
+   driver_driconf = pipe_loader_drm_get_driconf_by_name(driver_name,
+                                                        &driver_count);
 #endif
 
-   char *xml;
-   int ret = asprintf(&xml, "%s%s%s%s",
-                      DRI_CONF_BEGIN,
-                      gallium_driinfo_xml,
-                      driver_xml ? driver_xml : "",
-                      DRI_CONF_END);
-   if (ret < 0)
-      xml = NULL;
+   unsigned merged_count;
+   const driOptionDescription *merged_driconf =
+      merge_driconf(driver_driconf, driver_count, &merged_count);
+   free((void *)driver_driconf);
+
+   char *xml = driGetOptionsXml(merged_driconf, merged_count);
+
+   free((void *)merged_driconf);
 
    return xml;
 }

src/amd/vulkan/radv_device.c
@@ -606,8 +606,7 @@ radv_handle_per_app_options(struct radv_instance *instance,
 		instance->debug_flags |= RADV_DEBUG_NO_DYNAMIC_BOUNDS;
 }
 
-static const char radv_dri_options_xml[] =
-DRI_CONF_BEGIN
+static const driOptionDescription radv_dri_options[] = {
 	DRI_CONF_SECTION_PERFORMANCE
 		DRI_CONF_ADAPTIVE_SYNC("true")
 		DRI_CONF_VK_X11_OVERRIDE_MIN_IMAGE_COUNT(0)
@@ -623,11 +622,11 @@ DRI_CONF_BEGIN
 		DRI_CONF_OVERRIDE_VRAM_SIZE()
 		DRI_CONF_VK_WSI_FORCE_BGRA8_UNORM_FIRST("false")
 	DRI_CONF_SECTION_END
-DRI_CONF_END;
+};
 
 static void  radv_init_dri_options(struct radv_instance *instance)
 {
-	driParseOptionInfo(&instance->available_dri_options, radv_dri_options_xml);
+	driParseOptionInfo(&instance->available_dri_options, radv_dri_options, ARRAY_SIZE(radv_dri_options));
 	driParseConfigFiles(&instance->dri_options,
 	                    &instance->available_dri_options,
 	                    0, "radv", NULL,

src/gallium/auxiliary/pipe-loader/pipe_loader.h
@@ -199,14 +199,12 @@ bool
 pipe_loader_drm_probe_fd(struct pipe_loader_device **dev, int fd);
 
 /**
- * Get the driinfo XML used for the DRM driver of the given name, if any.
+ * Get the dri options used for the DRM driver of the given name, if any.
  *
- * The returned string is heap-allocated.
+ * The returned array is heap-allocated.
  */
-char *
-pipe_loader_drm_get_driinfo_xml(const char *driver_name);
-
-extern const char gallium_driinfo_xml[];
+const struct driOptionDescription *
+pipe_loader_drm_get_driconf_by_name(const char *driver_name, unsigned *count);
 
 #ifdef __cplusplus
 }

src/gallium/auxiliary/pipe-loader/pipe_loader_drm.c
@@ -46,6 +46,7 @@
 #include "util/u_memory.h"
 #include "util/u_dl.h"
 #include "util/u_debug.h"
+#include "util/xmlconfig.h"
 
 #define DRM_RENDER_NODE_DEV_NAME_FORMAT "%s/renderD%d"
 #define DRM_RENDER_NODE_MAX_NODES 63
@@ -241,15 +242,13 @@ pipe_loader_drm_release(struct pipe_loader_device **dev)
    pipe_loader_base_release(dev);
 }
 
-static const char *
-pipe_loader_drm_get_driconf_xml(struct pipe_loader_device *dev)
+static const struct driOptionDescription *
+pipe_loader_drm_get_driconf(struct pipe_loader_device *dev, unsigned *count)
 {
    struct pipe_loader_drm_device *ddev = pipe_loader_drm_device(dev);
 
-   if (!ddev->dd->driconf_xml)
-      return NULL;
-
-   return *ddev->dd->driconf_xml;
+   *count = ddev->dd->driconf_count;
+   return ddev->dd->driconf;
 }
 
 static struct pipe_screen *
@@ -261,24 +260,30 @@ pipe_loader_drm_create_screen(struct pipe_loader_device *dev,
    return ddev->dd->create_screen(ddev->fd, config);
 }
 
-char *
-pipe_loader_drm_get_driinfo_xml(const char *driver_name)
+const struct driOptionDescription *
+pipe_loader_drm_get_driconf_by_name(const char *driver_name, unsigned *count)
 {
-   char *xml = NULL;
+   driOptionDescription *driconf = NULL;
    struct util_dl_library *lib = NULL;
    const struct drm_driver_descriptor *dd =
       get_driver_descriptor(driver_name, &lib);
 
-   if (dd && dd->driconf_xml && *dd->driconf_xml)
-      xml = strdup(*dd->driconf_xml);
-
+   if (!dd) {
+      *count = 0;
+   } else {
+      *count = dd->driconf_count;
+      size_t size = sizeof(*driconf) * *count;
+      driconf = malloc(size);
+      memcpy(driconf, dd->driconf, size);
+   }
    if (lib)
       util_dl_close(lib);
-   return xml;
+
+   return driconf;
 }
 
 static const struct pipe_loader_ops pipe_loader_drm_ops = {
    .create_screen = pipe_loader_drm_create_screen,
-   .get_driconf_xml = pipe_loader_drm_get_driconf_xml,
+   .get_driconf = pipe_loader_drm_get_driconf,
    .release = pipe_loader_drm_release
 };

src/gallium/auxiliary/pipe-loader/pipe_loader_priv.h
@@ -34,7 +34,8 @@ struct pipe_loader_ops {
    struct pipe_screen *(*create_screen)(struct pipe_loader_device *dev,
                                         const struct pipe_screen_config *config);
 
-   const char *(*get_driconf_xml)(struct pipe_loader_device *dev);
+   const struct driOptionDescription *(*get_driconf)(struct pipe_loader_device *dev,
+                                                     unsigned *count);
 
    void (*release)(struct pipe_loader_device **dev);
 };

src/gallium/auxiliary/pipe-loader/pipe_loader_sw.c
@@ -294,9 +294,10 @@ pipe_loader_sw_release(struct pipe_loader_device **dev)
    pipe_loader_base_release(dev);
 }
 
-static const char *
-pipe_loader_sw_get_driconf_xml(struct pipe_loader_device *dev)
+static const struct driOptionDescription *
+pipe_loader_sw_get_driconf(struct pipe_loader_device *dev, unsigned *count)
 {
+   *count = 0;
    return NULL;
 }
 
@@ -316,6 +317,6 @@ pipe_loader_sw_create_screen(struct pipe_loader_device *dev,
 
 static const struct pipe_loader_ops pipe_loader_sw_ops = {
    .create_screen = pipe_loader_sw_create_screen,
-   .get_driconf_xml = pipe_loader_sw_get_driconf_xml,
+   .get_driconf = pipe_loader_sw_get_driconf,
    .release = pipe_loader_sw_release
 };

src/gallium/auxiliary/target-helpers/drm_helper.h
@@ -10,10 +10,11 @@
 /**
  * Instantiate a drm_driver_descriptor struct.
  */
-#define DEFINE_DRM_DRIVER_DESCRIPTOR(descriptor_name, driver, driconf, func) \
+#define DEFINE_DRM_DRIVER_DESCRIPTOR(descriptor_name, driver, _driconf, _driconf_count, func) \
 const struct drm_driver_descriptor descriptor_name = {         \
    .driver_name = #driver,                                     \
-   .driconf_xml = driconf,                                     \
+   .driconf = _driconf,                                        \
+   .driconf_count = _driconf_count,                            \
    .create_screen = func,                                      \
 };
 
@@ -32,17 +33,17 @@ const struct drm_driver_descriptor descriptor_name = {         \
 
 #ifdef PIPE_LOADER_DYNAMIC
 
-#define DRM_DRIVER_DESCRIPTOR(driver, driconf)                          \
-   PUBLIC DEFINE_DRM_DRIVER_DESCRIPTOR(driver_descriptor, driver, driconf, pipe_##driver##_create_screen)
+#define DRM_DRIVER_DESCRIPTOR(driver, driconf, driconf_count)           \
+   PUBLIC DEFINE_DRM_DRIVER_DESCRIPTOR(driver_descriptor, driver, driconf, driconf_count, pipe_##driver##_create_screen)
 
 #define DRM_DRIVER_DESCRIPTOR_STUB(driver)
 
-#define DRM_DRIVER_DESCRIPTOR_ALIAS(driver, alias, driconf)
+#define DRM_DRIVER_DESCRIPTOR_ALIAS(driver, alias, driconf, driconf_count)
 
 #else
 
-#define DRM_DRIVER_DESCRIPTOR(driver, driconf)                          \
-   DEFINE_DRM_DRIVER_DESCRIPTOR(driver##_driver_descriptor, driver, driconf, pipe_##driver##_create_screen)
+#define DRM_DRIVER_DESCRIPTOR(driver, driconf, driconf_count)                          \
+   DEFINE_DRM_DRIVER_DESCRIPTOR(driver##_driver_descriptor, driver, driconf, driconf_count, pipe_##driver##_create_screen)
 
 #define DRM_DRIVER_DESCRIPTOR_STUB(driver)                              \
    static struct pipe_screen *                                          \
@@ -51,10 +52,11 @@ const struct drm_driver_descriptor descriptor_name = {         \
       fprintf(stderr, #driver ": driver missing\n");                    \
       return NULL;                                                      \
    }                                                                    \
-   DRM_DRIVER_DESCRIPTOR(driver, NULL)
+   DRM_DRIVER_DESCRIPTOR(driver, NULL, 0)
 
-#define DRM_DRIVER_DESCRIPTOR_ALIAS(driver, alias, driconf) \
-   DEFINE_DRM_DRIVER_DESCRIPTOR(alias##_driver_descriptor, alias, driconf, pipe_##driver##_create_screen)
+#define DRM_DRIVER_DESCRIPTOR_ALIAS(driver, alias, driconf, driconf_count) \
+   DEFINE_DRM_DRIVER_DESCRIPTOR(alias##_driver_descriptor, alias, driconf, \
+                                driconf_count, pipe_##driver##_create_screen)
 
 #endif
 
@@ -75,7 +77,7 @@ pipe_i915_create_screen(int fd, const struct pipe_screen_config *config)
    screen = i915_screen_create(iws);
    return screen ? debug_screen_wrap(screen) : NULL;
 }
-DRM_DRIVER_DESCRIPTOR(i915, NULL)
+DRM_DRIVER_DESCRIPTOR(i915, NULL, 0)
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(i915)
 #endif
@@ -92,10 +94,10 @@ pipe_iris_create_screen(int fd, const struct pipe_screen_config *config)
    return screen ? debug_screen_wrap(screen) : NULL;
 }
 
-const char *iris_driconf_xml =
+const driOptionDescription iris_driconf[] = {
       #include "iris/driinfo_iris.h"
-      ;
-DRM_DRIVER_DESCRIPTOR(iris, &iris_driconf_xml)
+};
+DRM_DRIVER_DESCRIPTOR(iris, iris_driconf, ARRAY_SIZE(iris_driconf))
 
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(iris)
@@ -112,16 +114,16 @@ pipe_nouveau_create_screen(int fd, const struct pipe_screen_config *config)
    screen = nouveau_drm_screen_create(fd);
    return screen ? debug_screen_wrap(screen) : NULL;
 }
-DRM_DRIVER_DESCRIPTOR(nouveau, NULL)
+DRM_DRIVER_DESCRIPTOR(nouveau, NULL, 0)
 
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(nouveau)
 #endif
 
 #if defined(GALLIUM_VC4) || defined(GALLIUM_V3D)
-   const char *v3d_driconf_xml =
+const driOptionDescription v3d_driconf[] = {
       #include "v3d/driinfo_v3d.h"
-      ;
+};
 #endif
 
 #ifdef GALLIUM_KMSRO
@@ -136,9 +138,9 @@ pipe_kmsro_create_screen(int fd, const struct pipe_screen_config *config)
    return screen ? debug_screen_wrap(screen) : NULL;
 }
 #if defined(GALLIUM_VC4) || defined(GALLIUM_V3D)
-DRM_DRIVER_DESCRIPTOR(kmsro, &v3d_driconf_xml)
+DRM_DRIVER_DESCRIPTOR(kmsro, v3d_driconf, ARRAY_SIZE(v3d_driconf))
 #else
-DRM_DRIVER_DESCRIPTOR(kmsro, NULL)
+DRM_DRIVER_DESCRIPTOR(kmsro, NULL, 0)
 #endif
 
 #else
@@ -158,7 +160,7 @@ pipe_r300_create_screen(int fd, const struct pipe_screen_config *config)
    rw = radeon_drm_winsys_create(fd, config, r300_screen_create);
    return rw ? debug_screen_wrap(rw->screen) : NULL;
 }
-DRM_DRIVER_DESCRIPTOR(r300, NULL)
+DRM_DRIVER_DESCRIPTOR(r300, NULL, 0)
 
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(r300)
@@ -177,7 +179,7 @@ pipe_r600_create_screen(int fd, const struct pipe_screen_config *config)
    rw = radeon_drm_winsys_create(fd, config, r600_screen_create);
    return rw ? debug_screen_wrap(rw->screen) : NULL;
 }
-DRM_DRIVER_DESCRIPTOR(r600, NULL)
+DRM_DRIVER_DESCRIPTOR(r600, NULL, 0)
 
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(r600)
@@ -194,10 +196,10 @@ pipe_radeonsi_create_screen(int fd, const struct pipe_screen_config *config)
    return screen ? debug_screen_wrap(screen) : NULL;
 }
 
-const char *radeonsi_driconf_xml =
+const driOptionDescription radeonsi_driconf[] = {
       #include "radeonsi/driinfo_radeonsi.h"
-      ;
-DRM_DRIVER_DESCRIPTOR(radeonsi, &radeonsi_driconf_xml)
+};
+DRM_DRIVER_DESCRIPTOR(radeonsi, radeonsi_driconf, ARRAY_SIZE(radeonsi_driconf))
 
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(radeonsi)
@@ -220,7 +222,7 @@ pipe_vmwgfx_create_screen(int fd, const struct pipe_screen_config *config)
    screen = svga_screen_create(sws);
    return screen ? debug_screen_wrap(screen) : NULL;
 }
-DRM_DRIVER_DESCRIPTOR(vmwgfx, NULL)
+DRM_DRIVER_DESCRIPTOR(vmwgfx, NULL, 0)
 
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(vmwgfx)
@@ -237,11 +239,11 @@ pipe_msm_create_screen(int fd, const struct pipe_screen_config *config)
    screen = fd_drm_screen_create(fd, NULL);
    return screen ? debug_screen_wrap(screen) : NULL;
 }
-DRM_DRIVER_DESCRIPTOR(msm, NULL)
+DRM_DRIVER_DESCRIPTOR(msm, NULL, 0)
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(msm)
 #endif
-DRM_DRIVER_DESCRIPTOR_ALIAS(msm, kgsl, NULL)
+DRM_DRIVER_DESCRIPTOR_ALIAS(msm, kgsl, NULL, 0)
 
 #ifdef GALLIUM_VIRGL
 #include "virgl/drm/virgl_drm_public.h"
@@ -256,10 +258,10 @@ pipe_virtio_gpu_create_screen(int fd, const struct pipe_screen_config *config)
    return screen ? debug_screen_wrap(screen) : NULL;
 }
 
-const char *virgl_driconf_xml =
+const driOptionDescription virgl_driconf[] = {
       #include "virgl/virgl_driinfo.h.in"
-      ;
-DRM_DRIVER_DESCRIPTOR(virtio_gpu, &virgl_driconf_xml)
+};
+DRM_DRIVER_DESCRIPTOR(virtio_gpu, virgl_driconf, ARRAY_SIZE(virgl_driconf))
 
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(virtio_gpu)
@@ -276,7 +278,7 @@ pipe_vc4_create_screen(int fd, const struct pipe_screen_config *config)
    screen = vc4_drm_screen_create(fd, config);
    return screen ? debug_screen_wrap(screen) : NULL;
 }
-DRM_DRIVER_DESCRIPTOR(vc4, &v3d_driconf_xml)
+DRM_DRIVER_DESCRIPTOR(vc4, v3d_driconf, ARRAY_SIZE(v3d_driconf))
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(vc4)
 #endif
@@ -293,7 +295,7 @@ pipe_v3d_create_screen(int fd, const struct pipe_screen_config *config)
    return screen ? debug_screen_wrap(screen) : NULL;
 }
 
-DRM_DRIVER_DESCRIPTOR(v3d, &v3d_driconf_xml)
+DRM_DRIVER_DESCRIPTOR(v3d, v3d_driconf, ARRAY_SIZE(v3d_driconf))
 
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(v3d)
@@ -310,7 +312,7 @@ pipe_panfrost_create_screen(int fd, const struct pipe_screen_config *config)
    screen = panfrost_drm_screen_create(fd);
    return screen ? debug_screen_wrap(screen) : NULL;
 }
-DRM_DRIVER_DESCRIPTOR(panfrost, NULL)
+DRM_DRIVER_DESCRIPTOR(panfrost, NULL, 0)
 
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(panfrost)
@@ -327,7 +329,7 @@ pipe_etnaviv_create_screen(int fd, const struct pipe_screen_config *config)
    screen = etna_drm_screen_create(fd);
    return screen ? debug_screen_wrap(screen) : NULL;
 }
-DRM_DRIVER_DESCRIPTOR(etnaviv, NULL)
+DRM_DRIVER_DESCRIPTOR(etnaviv, NULL, 0)
 
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(etnaviv)
@@ -345,7 +347,7 @@ pipe_tegra_create_screen(int fd, const struct pipe_screen_config *config)
 
    return screen ? debug_screen_wrap(screen) : NULL;
 }
-DRM_DRIVER_DESCRIPTOR(tegra, NULL)
+DRM_DRIVER_DESCRIPTOR(tegra, NULL, 0)
 
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(tegra)
@@ -362,7 +364,7 @@ pipe_lima_create_screen(int fd, const struct pipe_screen_config *config)
    screen = lima_drm_screen_create(fd);
    return screen ? debug_screen_wrap(screen) : NULL;
 }
-DRM_DRIVER_DESCRIPTOR(lima, NULL)
+DRM_DRIVER_DESCRIPTOR(lima, NULL, 0)
 
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(lima)
@@ -378,7 +380,7 @@ pipe_zink_create_screen(int fd, const struct pipe_screen_config *config)
    screen = zink_drm_create_screen(fd);
    return screen ? debug_screen_wrap(screen) : NULL;
 }
-DRM_DRIVER_DESCRIPTOR(zink, NULL)
+DRM_DRIVER_DESCRIPTOR(zink, NULL, 0)
 
 #else
 DRM_DRIVER_DESCRIPTOR_STUB(zink)

src/gallium/drivers/iris/driinfo_iris.h
@@ -7,5 +7,5 @@ DRI_CONF_SECTION_DEBUG
 DRI_CONF_SECTION_END
 
 DRI_CONF_SECTION_PERFORMANCE
-   DRI_CONF_OPT_E(bo_reuse, 1, 0, 1, "Buffer object reuse", "")
+   DRI_CONF_OPT_E(bo_reuse, 1, 0, 1, "Buffer object reuse",)
 DRI_CONF_SECTION_END

commit 516fc209e4fa56b3ee97a6ae0192bb7630b58cc2

gallium: Fix leak of the merged driconf options.
Fixes: f25def8fe393 ("driconf: Make the driver's declarations be structs instead of XML.")
Reviewed-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7695>

src/gallium/auxiliary/pipe-loader/pipe_loader.c
@@ -120,6 +120,7 @@ pipe_loader_load_options(struct pipe_loader_device *dev)
    driParseOptionInfo(&dev->option_info, merged_driconf, merged_count);
    driParseConfigFiles(&dev->option_cache, &dev->option_info, 0,
                        dev->driver_name, NULL, NULL, 0, NULL, 0);
+   free((void *)merged_driconf);
 }
 
 char *