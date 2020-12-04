openharmony/third_party_mesa3d

commit 7f851fdd5808156ef44d266a59fcb0539c87a625

util: Add property_get() fallback for android
Environment variables aren't the easiest thing to use on android.  So
add a fallback to android's property mechanism for os_get_option().

This is slightly complicated by the fact that the assumption that the
return value of os_get_option() need not be freed.

Signed-off-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7763>

src/util/os_misc.c
@@ -56,6 +56,7 @@
 #  define LOG_TAG "MESA"
 #  include <unistd.h>
 #  include <log/log.h>
+#  include <cutils/properties.h>
 #elif DETECT_OS_LINUX || DETECT_OS_CYGWIN || DETECT_OS_SOLARIS || DETECT_OS_HURD
 #  include <unistd.h>
 #elif DETECT_OS_OPENBSD || DETECT_OS_FREEBSD
@@ -121,15 +122,99 @@ os_log_message(const char *message)
 #endif
 }
 
+#if DETECT_OS_ANDROID
+#  include <ctype.h>
+#  include "hash_table.h"
+#  include "ralloc.h"
+#  include "simple_mtx.h"
+
+static struct hash_table *options_tbl;
+static simple_mtx_t options_tbl_lock = _SIMPLE_MTX_INITIALIZER_NP;
+
+static void
+options_tbl_fini(void)
+{
+   _mesa_hash_table_destroy(options_tbl, NULL);
+}
+
+/**
+ * Get an option value from android's property system, as a fallback to
+ * getenv() (which is generally less useful on android due to processes
+ * typically being forked from the zygote.
+ *
+ * The option name used for getenv is translated into a property name
+ * by:
+ *
+ *  1) convert to lowercase
+ *  2) replace '_' with '.'
+ *  3) if necessary, prepend "mesa."
+ *
+ * For example:
+ *  - MESA_EXTENSION_OVERRIDE -> mesa.extension.override
+ *  - GALLIUM_HUD -> mesa.gallium.hud
+ *
+ * Note that we use a hashtable for two purposes:
+ *  1) Avoid re-translating the option name on subsequent lookups
+ *  2) Avoid leaking memory.  Because property_get() returns the
+ *     property value into a user allocated buffer, we cannot return
+ *     that directly to the caller, so we need to strdup().  With the
+ *     hashtable, subsquent lookups can return the existing string.
+ */
+static const char *
+os_get_android_option(const char *name)
+{
+   if (!options_tbl) {
+      options_tbl = _mesa_hash_table_create(NULL, _mesa_hash_string,
+            _mesa_key_string_equal);
+      atexit(options_tbl_fini);
+   }
+
+   struct hash_entry *entry = _mesa_hash_table_search(options_tbl, name);
+   if (entry) {
+      return entry->data;
+   }
+
+   char value[PROPERTY_VALUE_MAX];
+   char key[PROPERTY_KEY_MAX];
+   char *p = key, *end = key + PROPERTY_KEY_MAX;
+   /* add "mesa." prefix if necessary: */
+   if (strstr(name, "MESA_") != name)
+      p += strlcpy(p, "mesa.", end - p);
+   p += strlcpy(p, name, end - p);
+   for (int i = 0; key[i]; i++) {
+      if (key[i] == '_') {
+         key[i] = '.';
+      } else {
+         key[i] = tolower(key[i]);
+      }
+   }
+
+   const char *opt = NULL;
+   int len = property_get(key, value, NULL);
+   if (len > 1) {
+      opt = ralloc_strdup(options_tbl, value);
+   }
+
+   _mesa_hash_table_insert(options_tbl, name, value);
+
+   return opt;
+}
+#endif
+
 
 #if !defined(EMBEDDED_DEVICE)
 const char *
 os_get_option(const char *name)
 {
-   return getenv(name);
+   const char *opt = getenv(name);
+#if DETECT_OS_ANDROID
+   if (!opt) {
+      opt = os_get_android_option(name);
+   }
+#endif
+   return opt;
 }
 #endif /* !EMBEDDED_DEVICE */
-
 
 /**
  * Return the size of the total physical memory.

commit 0ce7b400a59420737aa9b40d82d0c974b2826e75

util: Do not insert uninitialized data if Android property is not set
This prevents funky segfaults in seemingly unrelated codepaths like:

    pid: 20058, tid: 20086, name: Binder:20058_2  >>> /system/bin/surfaceflinger <<<
    uid: 1000
    signal 11 (SIGSEGV), code 1 (SEGV_MAPERR), fault addr 0x184
    Cause: null pointer dereference
    [...]
    #00 pc 000000000020b078  /vendor/lib64/dri/gallium_dri.so (ir3_shader_variant+56)
    #01 pc 00000000001dc168  /vendor/lib64/dri/gallium_dri.so (fd5_draw_vbo+920)
    #02 pc 00000000001a30f8  /vendor/lib64/dri/gallium_dri.so (fd_draw_vbo+1264)
    #03 pc 000000000077e714  /vendor/lib64/dri/gallium_dri.so (util_primconvert_draw_vbo+596)
    #04 pc 00000000001a2d60  /vendor/lib64/dri/gallium_dri.so (fd_draw_vbo+344)
    #05 pc 00000000007671e4  /vendor/lib64/dri/gallium_dri.so (dd_context_draw_vbo+300)
    #06 pc 00000000007b6c9c  /vendor/lib64/dri/gallium_dri.so (u_vbuf_draw_vbo+2268)
    #07 pc 000000000074d230  /vendor/lib64/dri/gallium_dri.so (cso_draw_arrays+96)
    #08 pc 00000000007a4258  /vendor/lib64/dri/gallium_dri.so (util_draw_user_vertex_buffer+96)
    #09 pc 00000000007b0968  /vendor/lib64/dri/gallium_dri.so (util_run_tests+480)
    #10 pc 000000000019e24c  /vendor/lib64/dri/gallium_dri.so (pipe_msm_create_screen+68)
    #11 pc 00000000006ada8c  /vendor/lib64/dri/gallium_dri.so (pipe_loader_create_screen+60)
    #12 pc 000000000020e958  /vendor/lib64/dri/gallium_dri.so (dri2_init_screen+120)
    #13 pc 00000000006ad088  /vendor/lib64/dri/gallium_dri.so (driCreateNewScreen2+544)

Fixes: 7f851fdd580 ("util: Add property_get() fallback for android")
Reviewed-by: Rob Clark <robclark@freedesktop.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7928>

src/util/os_misc.c
@@ -195,7 +195,7 @@ os_get_android_option(const char *name)
       opt = ralloc_strdup(options_tbl, value);
    }
 
-   _mesa_hash_table_insert(options_tbl, name, value);
+   _mesa_hash_table_insert(options_tbl, name, (void *)opt);
 
    return opt;
 }