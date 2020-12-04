openharmony/third_party_mesa3d

commit 0b2759781c24039c96b7752486e764672846e153

aco: split self-intersecting copies instead of swapping
Example situation:
v1 = {v0.hi, v1.lo}
v0.hi = v1.hi

The 4-byte copy's definition is completely used, but swapping it makes no
sense. We have to split it to generate correct code:
swap(v0.hi, v1.lo)
swap(v0.hi, v1.hi)

Found in dEQP-VK.spirv_assembly.type.vec3.i16.constant_composite_vert

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4772>

src/amd/compiler/aco_lower_to_hw_instr.cpp
@@ -1091,6 +1091,39 @@ void handle_operands(std::map<PhysReg, copy_operation>& copy_map, lower_context*
 
       /* to resolve the cycle, we have to swap the src reg with the dst reg */
       copy_operation swap = it->second;
+
+      /* if this is self-intersecting, we have to split it because
+       * self-intersecting swaps don't make sense */
+      PhysReg lower = swap.def.physReg();
+      PhysReg higher = swap.op.physReg();
+      if (lower.reg_b > higher.reg_b)
+         std::swap(lower, higher);
+      if (higher.reg_b - lower.reg_b < (int)swap.bytes) {
+         unsigned offset = higher.reg_b - lower.reg_b;
+         RegType type = swap.def.regClass().type();
+
+         copy_operation middle;
+         lower.reg_b += offset;
+         higher.reg_b += offset;
+         middle.bytes = swap.bytes - offset * 2;
+         memcpy(middle.uses, swap.uses + offset, middle.bytes);
+         middle.op = Operand(lower, RegClass::get(type, middle.bytes));
+         middle.def = Definition(higher, RegClass::get(type, middle.bytes));
+         copy_map[higher] = middle;
+
+         copy_operation end;
+         lower.reg_b += middle.bytes;
+         higher.reg_b += middle.bytes;
+         end.bytes = swap.bytes - (offset + middle.bytes);
+         memcpy(end.uses, swap.uses + offset + middle.bytes, end.bytes);
+         end.op = Operand(lower, RegClass::get(type, end.bytes));
+         end.def = Definition(higher, RegClass::get(type, end.bytes));
+         copy_map[higher] = end;
+
+         memset(swap.uses + offset, 0, swap.bytes - offset);
+         swap.bytes = offset;
+      }
+
       do_swap(ctx, bld, swap, preserve_scc, pi);
 
       /* remove from map */

commit abaff3077ac208aad9ce2943eb27a344272bbd67

aco: don't assume src=lower when splitting self-intersecting copies
Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Fixes: 0b2759781c2 ("aco: split self-intersecting copies instead of swapping")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7798>

src/amd/compiler/aco_lower_to_hw_instr.cpp
@@ -1613,31 +1613,28 @@ void handle_operands(std::map<PhysReg, copy_operation>& copy_map, lower_context*
 
       /* if this is self-intersecting, we have to split it because
        * self-intersecting swaps don't make sense */
-      PhysReg lower = swap.def.physReg();
-      PhysReg higher = swap.op.physReg();
-      if (lower.reg_b > higher.reg_b)
-         std::swap(lower, higher);
-      if (higher.reg_b - lower.reg_b < (int)swap.bytes) {
-         unsigned offset = higher.reg_b - lower.reg_b;
+      PhysReg src = swap.op.physReg(), dst = swap.def.physReg();
+      if (abs((int)src.reg_b - (int)dst.reg_b) < (int)swap.bytes) {
+         unsigned offset = abs((int)src.reg_b - (int)dst.reg_b);
          RegType type = swap.def.regClass().type();
 
          copy_operation middle;
-         lower.reg_b += offset;
-         higher.reg_b += offset;
+         src.reg_b += offset;
+         dst.reg_b += offset;
          middle.bytes = swap.bytes - offset * 2;
          memcpy(middle.uses, swap.uses + offset, middle.bytes);
-         middle.op = Operand(lower, RegClass::get(type, middle.bytes));
-         middle.def = Definition(higher, RegClass::get(type, middle.bytes));
-         copy_map[higher] = middle;
+         middle.op = Operand(src, RegClass::get(type, middle.bytes));
+         middle.def = Definition(dst, RegClass::get(type, middle.bytes));
+         copy_map[dst] = middle;
 
          copy_operation end;
-         lower.reg_b += middle.bytes;
-         higher.reg_b += middle.bytes;
+         src.reg_b += middle.bytes;
+         dst.reg_b += middle.bytes;
          end.bytes = swap.bytes - (offset + middle.bytes);
          memcpy(end.uses, swap.uses + offset + middle.bytes, end.bytes);
-         end.op = Operand(lower, RegClass::get(type, end.bytes));
-         end.def = Definition(higher, RegClass::get(type, end.bytes));
-         copy_map[higher] = end;
+         end.op = Operand(src, RegClass::get(type, end.bytes));
+         end.def = Definition(dst, RegClass::get(type, end.bytes));
+         copy_map[dst] = end;
 
          memset(swap.uses + offset, 0, swap.bytes - offset);
          swap.bytes = offset;