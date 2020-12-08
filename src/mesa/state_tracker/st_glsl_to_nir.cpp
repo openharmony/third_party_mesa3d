openharmony/third_party_mesa3d

commit a283dc1c763aafa00d3851b9f4033289af04fed9

mesa: properly disallow param list reallocation
This can more easily detect shader variants adding too many state
parameters and causing a reallocation.

Reviewed-by: Zoltán Böszörményi <zboszor@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6946>

src/mesa/state_tracker/st_glsl_to_nir.cpp
@@ -468,6 +468,7 @@ st_glsl_to_nir_post_opts(struct st_context *st, struct gl_program *prog,
     * This should be enough for Bitmap and DrawPixels constants.
     */
    _mesa_reserve_parameter_storage(prog->Parameters, 8, 8);
+   _mesa_disallow_parameter_storage_realloc(prog->Parameters);
 
    /* This has to be done last.  Any operation the can cause
     * prog->ParameterValues to get reallocated (e.g., anything that adds a

src/mesa/program/prog_parameter.c
@@ -30,6 +30,7 @@
 
 #include "main/glheader.h"
 #include "main/macros.h"
+#include "main/errors.h"
 #include "util/u_memory.h"
 #include "prog_instruction.h"
 #include "prog_parameter.h"
@@ -192,6 +193,14 @@ _mesa_reserve_parameter_storage(struct gl_program_parameter_list *paramList,
    const GLuint oldNum = paramList->NumParameters;
    const unsigned oldValNum = paramList->NumParameterValues;
 
+   if (paramList->DisallowRealloc &&
+       (oldNum + reserve_params > paramList->Size ||
+        oldValNum + reserve_values > paramList->SizeValues)) {
+      _mesa_problem(NULL, "Parameter storage reallocation disallowed. This "
+              "is a Mesa bug. Increase the reservation size in the code.");
+      abort();
+   }
+
    if (oldNum + reserve_params > paramList->Size) {
       /* Need to grow the parameter list array (alloc some extra) */
       paramList->Size += 4 * reserve_params;
@@ -215,6 +224,17 @@ _mesa_reserve_parameter_storage(struct gl_program_parameter_list *paramList,
                        paramList->SizeValues * 4 * sizeof(gl_constant_value),/*new*/
                        16);
    }
+}
+
+
+/**
+ * Disallow reallocating the parameter storage, so that uniform storage
+ * can have pointers pointing to it.
+ */
+void
+_mesa_disallow_parameter_storage_realloc(struct gl_program_parameter_list *paramList)
+{
+   paramList->DisallowRealloc = true;
 }
 
 

src/mesa/program/prog_parameter.h
@@ -142,6 +142,7 @@ struct gl_program_parameter_list
    gl_constant_value *ParameterValues; /**< Array [Size] of gl_constant_value */
    GLbitfield StateFlags; /**< _NEW_* flags indicating which state changes
                                might invalidate ParameterValues[] */
+   bool DisallowRealloc;
 };
 
 
@@ -158,6 +159,9 @@ extern void
 _mesa_reserve_parameter_storage(struct gl_program_parameter_list *paramList,
                                 unsigned reserve_params,
                                 unsigned reserve_values);
+
+extern void
+_mesa_disallow_parameter_storage_realloc(struct gl_program_parameter_list *paramList);
 
 extern GLint
 _mesa_add_parameter(struct gl_program_parameter_list *paramList,

src/mesa/state_tracker/st_glsl_to_tgsi.cpp
@@ -7167,6 +7167,7 @@ get_mesa_program_tgsi(struct gl_context *ctx,
     * This should be enough for Bitmap and DrawPixels constants.
     */
    _mesa_reserve_parameter_storage(prog->Parameters, 8, 8);
+   _mesa_disallow_parameter_storage_realloc(prog->Parameters);
 
    /* This has to be done last.  Any operation the can cause
     * prog->ParameterValues to get reallocated (e.g., anything that adds a

commit d8f58774cb2238ab3fababd00451aa52af088e7e

mesa/st: set reserved storage for params+values to 16
zink apparently has a lot of uniform data, and 8x8 isn't enough to avoid realloc

fixes mesa/mesa#3930

Fixes: a283dc1c ("mesa: properly disallow param list reallocation")

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7990>

src/mesa/state_tracker/st_glsl_to_nir.cpp
@@ -504,7 +504,7 @@ st_glsl_to_nir_post_opts(struct st_context *st, struct gl_program *prog,
     * storage is only associated with the original parameter list.
     * This should be enough for Bitmap and DrawPixels constants.
     */
-   _mesa_reserve_parameter_storage(prog->Parameters, 8, 8);
+   _mesa_reserve_parameter_storage(prog->Parameters, 16, 16);
    _mesa_disallow_parameter_storage_realloc(prog->Parameters);
 
    /* This has to be done last.  Any operation the can cause