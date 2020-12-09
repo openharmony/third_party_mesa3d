openharmony/third_party_mesa3d

commit 99a7862af64080ad0d0a9c7c8634d204f183868a

radeonsi: remove SDMA support
There are many issues with SDMA across many generations of hardware.
A recent example is that gfx10.3 suffers from random GPU hangs if
userspace uses SDMA.

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7908>

src/gallium/drivers/radeonsi/si_fence.c
@@ -36,10 +36,9 @@ struct si_fine_fence {
    unsigned offset;
 };
 
-struct si_multi_fence {
+struct si_fence {
    struct pipe_reference reference;
    struct pipe_fence_handle *gfx;
-   struct pipe_fence_handle *sdma;
    struct tc_unflushed_batch_token *tc_token;
    struct util_queue_fence ready;
 
@@ -168,8 +167,6 @@ static void si_add_fence_dependency(struct si_context *sctx, struct pipe_fence_h
 {
    struct radeon_winsys *ws = sctx->ws;
 
-   if (sctx->sdma_cs.priv)
-      ws->cs_add_fence_dependency(&sctx->sdma_cs, fence, 0);
    ws->cs_add_fence_dependency(&sctx->gfx_cs, fence, 0);
 }
 
@@ -182,12 +179,11 @@ static void si_fence_reference(struct pipe_screen *screen, struct pipe_fence_han
                                struct pipe_fence_handle *src)
 {
    struct radeon_winsys *ws = ((struct si_screen *)screen)->ws;
-   struct si_multi_fence **sdst = (struct si_multi_fence **)dst;
-   struct si_multi_fence *ssrc = (struct si_multi_fence *)src;
+   struct si_fence **sdst = (struct si_fence **)dst;
+   struct si_fence *ssrc = (struct si_fence *)src;
 
    if (pipe_reference(&(*sdst)->reference, &ssrc->reference)) {
       ws->fence_reference(&(*sdst)->gfx, NULL);
-      ws->fence_reference(&(*sdst)->sdma, NULL);
       tc_unflushed_batch_token_reference(&(*sdst)->tc_token, NULL);
       si_resource_reference(&(*sdst)->fine.buf, NULL);
       FREE(*sdst);
@@ -195,9 +191,9 @@ static void si_fence_reference(struct pipe_screen *screen, struct pipe_fence_han
    *sdst = ssrc;
 }
 
-static struct si_multi_fence *si_create_multi_fence()
+static struct si_fence *si_create_multi_fence()
 {
-   struct si_multi_fence *fence = CALLOC_STRUCT(si_multi_fence);
+   struct si_fence *fence = CALLOC_STRUCT(si_fence);
    if (!fence)
       return NULL;
 
@@ -210,7 +206,7 @@ static struct si_multi_fence *si_create_multi_fence()
 struct pipe_fence_handle *si_create_fence(struct pipe_context *ctx,
                                           struct tc_unflushed_batch_token *tc_token)
 {
-   struct si_multi_fence *fence = si_create_multi_fence();
+   struct si_fence *fence = si_create_multi_fence();
    if (!fence)
       return NULL;
 
@@ -265,7 +261,7 @@ static bool si_fence_finish(struct pipe_screen *screen, struct pipe_context *ctx
                             struct pipe_fence_handle *fence, uint64_t timeout)
 {
    struct radeon_winsys *rws = ((struct si_screen *)screen)->ws;
-   struct si_multi_fence *sfence = (struct si_multi_fence *)fence;
+   struct si_fence *sfence = (struct si_fence *)fence;
    struct si_context *sctx;
    int64_t abs_timeout = os_time_get_absolute_timeout(timeout);
 
@@ -295,17 +291,6 @@ static bool si_fence_finish(struct pipe_screen *screen, struct pipe_context *ctx
             return false;
       }
 
-      if (timeout && timeout != PIPE_TIMEOUT_INFINITE) {
-         int64_t time = os_time_get_nano();
-         timeout = abs_timeout > time ? abs_timeout - time : 0;
-      }
-   }
-
-   if (sfence->sdma) {
-      if (!rws->fence_wait(rws, sfence->sdma, timeout))
-         return false;
-
-      /* Recompute the timeout after waiting. */
       if (timeout && timeout != PIPE_TIMEOUT_INFINITE) {
          int64_t time = os_time_get_nano();
          timeout = abs_timeout > time ? abs_timeout - time : 0;
@@ -376,7 +361,7 @@ static void si_create_fence_fd(struct pipe_context *ctx, struct pipe_fence_handl
 {
    struct si_screen *sscreen = (struct si_screen *)ctx->screen;
    struct radeon_winsys *ws = sscreen->ws;
-   struct si_multi_fence *sfence;
+   struct si_fence *sfence;
 
    *pfence = NULL;
 
@@ -416,8 +401,8 @@ static int si_fence_get_fd(struct pipe_screen *screen, struct pipe_fence_handle 
 {
    struct si_screen *sscreen = (struct si_screen *)screen;
    struct radeon_winsys *ws = sscreen->ws;
-   struct si_multi_fence *sfence = (struct si_multi_fence *)fence;
-   int gfx_fd = -1, sdma_fd = -1;
+   struct si_fence *sfence = (struct si_fence *)fence;
+   int gfx_fd = -1;
 
    if (!sscreen->info.has_fence_to_handle)
       return -1;
@@ -429,32 +414,18 @@ static int si_fence_get_fd(struct pipe_screen *screen, struct pipe_fence_handle 
    if (sfence->gfx_unflushed.ctx)
       return -1;
 
-   if (sfence->sdma) {
-      sdma_fd = ws->fence_export_sync_file(ws, sfence->sdma);
-      if (sdma_fd == -1)
-         return -1;
-   }
    if (sfence->gfx) {
       gfx_fd = ws->fence_export_sync_file(ws, sfence->gfx);
       if (gfx_fd == -1) {
-         if (sdma_fd != -1)
-            close(sdma_fd);
          return -1;
       }
    }
 
    /* If we don't have FDs at this point, it means we don't have fences
     * either. */
-   if (sdma_fd == -1 && gfx_fd == -1)
-      return ws->export_signalled_sync_file(ws);
-   if (sdma_fd == -1)
-      return gfx_fd;
    if (gfx_fd == -1)
-      return sdma_fd;
+      return ws->export_signalled_sync_file(ws);
 
-   /* Get a fence that will be a combination of both fences. */
-   sync_accumulate("radeonsi", &gfx_fd, sdma_fd);
-   close(sdma_fd);
    return gfx_fd;
 }
 
@@ -466,7 +437,6 @@ static void si_flush_all_queues(struct pipe_context *ctx,
    struct si_context *sctx = (struct si_context *)ctx;
    struct radeon_winsys *ws = sctx->ws;
    struct pipe_fence_handle *gfx_fence = NULL;
-   struct pipe_fence_handle *sdma_fence = NULL;
    bool deferred_fence = false;
    struct si_fine_fence fine = {};
    unsigned rflags = PIPE_FLUSH_ASYNC;
@@ -484,10 +454,6 @@ static void si_flush_all_queues(struct pipe_context *ctx,
 
       si_fine_fence_set(sctx, &fine, flags);
    }
-
-   /* DMA IBs are preambles to gfx IBs, therefore must be flushed first. */
-   if (sctx->sdma_cs.priv)
-      si_flush_dma_cs(sctx, rflags, fence ? &sdma_fence : NULL);
 
    if (force_flush) {
       sctx->initial_gfx_cs_size = 0;
@@ -515,45 +481,41 @@ static void si_flush_all_queues(struct pipe_context *ctx,
 
    /* Both engines can signal out of order, so we need to keep both fences. */
    if (fence) {
-      struct si_multi_fence *multi_fence;
+      struct si_fence *new_fence;
 
       if (flags & TC_FLUSH_ASYNC) {
-         multi_fence = (struct si_multi_fence *)*fence;
-         assert(multi_fence);
+         new_fence = (struct si_fence *)*fence;
+         assert(new_fence);
       } else {
-         multi_fence = si_create_multi_fence();
-         if (!multi_fence) {
-            ws->fence_reference(&sdma_fence, NULL);
+         new_fence = si_create_multi_fence();
+         if (!new_fence) {
             ws->fence_reference(&gfx_fence, NULL);
             goto finish;
          }
 
          screen->fence_reference(screen, fence, NULL);
-         *fence = (struct pipe_fence_handle *)multi_fence;
+         *fence = (struct pipe_fence_handle *)new_fence;
       }
 
       /* If both fences are NULL, fence_finish will always return true. */
-      multi_fence->gfx = gfx_fence;
-      multi_fence->sdma = sdma_fence;
+      new_fence->gfx = gfx_fence;
 
       if (deferred_fence) {
-         multi_fence->gfx_unflushed.ctx = sctx;
-         multi_fence->gfx_unflushed.ib_index = sctx->num_gfx_cs_flushes;
+         new_fence->gfx_unflushed.ctx = sctx;
+         new_fence->gfx_unflushed.ib_index = sctx->num_gfx_cs_flushes;
       }
 
-      multi_fence->fine = fine;
+      new_fence->fine = fine;
       fine.buf = NULL;
 
       if (flags & TC_FLUSH_ASYNC) {
-         util_queue_fence_signal(&multi_fence->ready);
-         tc_unflushed_batch_token_reference(&multi_fence->tc_token, NULL);
+         util_queue_fence_signal(&new_fence->ready);
+         tc_unflushed_batch_token_reference(&new_fence->tc_token, NULL);
       }
    }
    assert(!fine.buf);
 finish:
    if (!(flags & (PIPE_FLUSH_DEFERRED | PIPE_FLUSH_ASYNC))) {
-      if (sctx->sdma_cs.priv)
-         ws->cs_sync_flush(&sctx->sdma_cs);
       ws->cs_sync_flush(&sctx->gfx_cs);
    }
 }
@@ -567,13 +529,10 @@ static void si_flush_from_st(struct pipe_context *ctx, struct pipe_fence_handle 
 static void si_fence_server_signal(struct pipe_context *ctx, struct pipe_fence_handle *fence)
 {
    struct si_context *sctx = (struct si_context *)ctx;
-   struct si_multi_fence *sfence = (struct si_multi_fence *)fence;
+   struct si_fence *sfence = (struct si_fence *)fence;
 
-   /* We should have at least one syncobj to signal */
-   assert(sfence->sdma || sfence->gfx);
+   assert(sfence->gfx);
 
-   if (sfence->sdma)
-      si_add_syncobj_signal(sctx, sfence->sdma);
    if (sfence->gfx)
       si_add_syncobj_signal(sctx, sfence->gfx);
 
@@ -595,7 +554,7 @@ static void si_fence_server_signal(struct pipe_context *ctx, struct pipe_fence_h
 static void si_fence_server_sync(struct pipe_context *ctx, struct pipe_fence_handle *fence)
 {
    struct si_context *sctx = (struct si_context *)ctx;
-   struct si_multi_fence *sfence = (struct si_multi_fence *)fence;
+   struct si_fence *sfence = (struct si_fence *)fence;
 
    util_queue_fence_wait(&sfence->ready);
 
@@ -613,10 +572,7 @@ static void si_fence_server_sync(struct pipe_context *ctx, struct pipe_fence_han
     * the time it takes to create and submit that IB, flushing decreases
     * performance. Therefore, DO NOT FLUSH.
     */
-   if (sfence->sdma)
-      si_add_fence_dependency(sctx, sfence->sdma);
-   if (sfence->gfx)
-      si_add_fence_dependency(sctx, sfence->gfx);
+   si_add_fence_dependency(sctx, sfence->gfx);
 }
 
 void si_init_fence_functions(struct si_context *ctx)

src/gallium/drivers/radeon/radeon_video.c
@@ -129,7 +129,8 @@ error:
 void si_vid_clear_buffer(struct pipe_context *context, struct rvid_buffer *buffer)
 {
    struct si_context *sctx = (struct si_context *)context;
+   uint32_t zero = 0;
 
-   si_sdma_clear_buffer(sctx, &buffer->res->b.b, 0, buffer->res->b.b.width0, 0);
+   sctx->b.clear_buffer(&sctx->b, &buffer->res->b.b, 0, buffer->res->b.b.width0, &zero, 4);
    context->flush(context, NULL, 0);
 }

src/gallium/drivers/radeonsi/Makefile.sources
@@ -1,5 +1,4 @@
 C_SOURCES := \
-	cik_sdma.c \
 	driinfo_radeonsi.h \
 	gfx10_query.c \
 	gfx10_shader_ngg.c \
@@ -15,7 +14,6 @@ C_SOURCES := \
 	si_cp_reg_shadowing.c \
 	si_debug.c \
 	si_descriptors.c \
-	si_dma_cs.c \
 	si_fence.c \
 	si_get.c \
 	si_gfx_cs.c \

src/gallium/drivers/radeonsi/meson.build
@@ -19,7 +19,6 @@
 # SOFTWARE.
 
 files_libradeonsi = files(
-  'cik_sdma.c',
   'driinfo_radeonsi.h',
   'gfx10_query.c',
   'gfx10_shader_ngg.c',
@@ -35,7 +34,6 @@ files_libradeonsi = files(
   'si_cp_reg_shadowing.c',
   'si_debug.c',
   'si_descriptors.c',
-  'si_dma_cs.c',
   'si_fence.c',
   'si_get.c',
   'si_gfx_cs.c',

src/gallium/drivers/radeonsi/si_blit.c
@@ -972,7 +972,6 @@ void si_resource_copy_region(struct pipe_context *ctx, struct pipe_resource *dst
 
    /* SNORM8 blitting has precision issues on some chips. Use the SINT
     * equivalent instead, which doesn't force DCC decompression.
-    * Note that some chips avoid this issue by using SDMA.
     */
    if (util_format_is_snorm8(dst_templ.format)) {
       dst_templ.format = src_templ.format = util_format_snorm8_to_sint8(dst_templ.format);
@@ -1137,21 +1136,18 @@ resolve_to_temp:
 static void si_blit(struct pipe_context *ctx, const struct pipe_blit_info *info)
 {
    struct si_context *sctx = (struct si_context *)ctx;
-   struct si_texture *dst = (struct si_texture *)info->dst.resource;
 
    if (do_hardware_msaa_resolve(ctx, info)) {
       return;
    }
 
-   /* Using SDMA for copying to a linear texture in GTT is much faster.
-    * This improves DRI PRIME performance.
-    *
-    * resource_copy_region can't do this yet, because dma_copy calls it
-    * on failure (recursion).
+   /* Using compute for copying to a linear texture in GTT is much faster than
+    * going through RBs (render backends). This improves DRI PRIME performance.
     */
-   if (dst->surface.is_linear && util_can_blit_via_copy_region(info, false)) {
-      sctx->dma_copy(ctx, info->dst.resource, info->dst.level, info->dst.box.x, info->dst.box.y,
-                     info->dst.box.z, info->src.resource, info->src.level, &info->src.box);
+   if (util_can_blit_via_copy_region(info, false)) {
+      si_resource_copy_region(ctx, info->dst.resource, info->dst.level,
+                              info->dst.box.x, info->dst.box.y, info->dst.box.z,
+                              info->src.resource, info->src.level, &info->src.box);
       return;
    }
 
@@ -1165,9 +1161,6 @@ static void si_blit(struct pipe_context *ctx, const struct pipe_blit_info *info)
                                          info->dst.format);
    si_decompress_subresource(ctx, info->src.resource, PIPE_MASK_RGBAZS, info->src.level,
                              info->src.box.z, info->src.box.z + info->src.box.depth - 1);
-
-   if (sctx->screen->debug_flags & DBG(FORCE_SDMA) && util_try_blit_via_copy_region(ctx, info))
-      return;
 
    si_blitter_begin(sctx, SI_BLIT | (info->render_condition_enable ? 0 : SI_DISABLE_RENDER_COND));
    util_blitter_blit(sctx->blitter, info);

src/gallium/drivers/radeonsi/si_buffer.c
@@ -33,68 +33,13 @@
 bool si_cs_is_buffer_referenced(struct si_context *sctx, struct pb_buffer *buf,
                                 enum radeon_bo_usage usage)
 {
-   if (sctx->ws->cs_is_buffer_referenced(&sctx->gfx_cs, buf, usage)) {
-      return true;
-   }
-   if (radeon_emitted(&sctx->sdma_cs, 0) &&
-       sctx->ws->cs_is_buffer_referenced(&sctx->sdma_cs, buf, usage)) {
-      return true;
-   }
-   return false;
+   return sctx->ws->cs_is_buffer_referenced(&sctx->gfx_cs, buf, usage);
 }
 
 void *si_buffer_map(struct si_context *sctx, struct si_resource *resource,
                     unsigned usage)
 {
-   enum radeon_bo_usage rusage = RADEON_USAGE_READWRITE;
-   bool busy = false;
-
-   assert(!(resource->flags & RADEON_FLAG_SPARSE));
-
-   if (usage & PIPE_MAP_UNSYNCHRONIZED) {
-      return sctx->ws->buffer_map(resource->buf, NULL, usage);
-   }
-
-   if (!(usage & PIPE_MAP_WRITE)) {
-      /* have to wait for the last write */
-      rusage = RADEON_USAGE_WRITE;
-   }
-
-   if (radeon_emitted(&sctx->gfx_cs, sctx->initial_gfx_cs_size) &&
-       sctx->ws->cs_is_buffer_referenced(&sctx->gfx_cs, resource->buf, rusage)) {
-      if (usage & PIPE_MAP_DONTBLOCK) {
-         si_flush_gfx_cs(sctx, RADEON_FLUSH_ASYNC_START_NEXT_GFX_IB_NOW, NULL);
-         return NULL;
-      } else {
-         si_flush_gfx_cs(sctx, RADEON_FLUSH_ASYNC_START_NEXT_GFX_IB_NOW, NULL);
-         busy = true;
-      }
-   }
-   if (radeon_emitted(&sctx->sdma_cs, 0) &&
-       sctx->ws->cs_is_buffer_referenced(&sctx->sdma_cs, resource->buf, rusage)) {
-      if (usage & PIPE_MAP_DONTBLOCK) {
-         si_flush_dma_cs(sctx, PIPE_FLUSH_ASYNC, NULL);
-         return NULL;
-      } else {
-         si_flush_dma_cs(sctx, 0, NULL);
-         busy = true;
-      }
-   }
-
-   if (busy || !sctx->ws->buffer_wait(resource->buf, 0, rusage)) {
-      if (usage & PIPE_MAP_DONTBLOCK) {
-         return NULL;
-      } else {
-         /* We will be wait for the GPU. Wait for any offloaded
-          * CS flush to complete to avoid busy-waiting in the winsys. */
-         sctx->ws->cs_sync_flush(&sctx->gfx_cs);
-         if (sctx->sdma_cs.priv)
-            sctx->ws->cs_sync_flush(&sctx->sdma_cs);
-      }
-   }
-
-   /* Setting the CS to NULL will prevent doing checks we have done already. */
-   return sctx->ws->buffer_map(resource->buf, NULL, usage);
+   return sctx->ws->buffer_map(resource->buf, &sctx->gfx_cs, usage);
 }
 
 void si_init_resource_fields(struct si_screen *sscreen, struct si_resource *res, uint64_t size,
@@ -188,7 +133,7 @@ void si_init_resource_fields(struct si_screen *sscreen, struct si_resource *res,
       res->flags |= RADEON_FLAG_DRIVER_INTERNAL;
 
    /* For higher throughput and lower latency over PCIe assuming sequential access.
-    * Only CP DMA, SDMA, and optimized compute benefit from this.
+    * Only CP DMA and optimized compute benefit from this.
     * GFX8 and older don't support RADEON_FLAG_UNCACHED.
     */
    if (sscreen->info.chip_class >= GFX9 &&
@@ -442,13 +387,6 @@ static void *si_buffer_transfer_map(struct pipe_context *ctx, struct pipe_resour
       }
    }
 
-   if (usage & PIPE_MAP_FLUSH_EXPLICIT &&
-       buf->b.b.flags & SI_RESOURCE_FLAG_UPLOAD_FLUSH_EXPLICIT_VIA_SDMA) {
-      usage &= ~(PIPE_MAP_UNSYNCHRONIZED | PIPE_MAP_PERSISTENT);
-      usage |= PIPE_MAP_DISCARD_RANGE;
-      force_discard_range = true;
-   }
-
    if (usage & PIPE_MAP_DISCARD_RANGE &&
        ((!(usage & (PIPE_MAP_UNSYNCHRONIZED | PIPE_MAP_PERSISTENT))) ||
         (buf->flags & RADEON_FLAG_SPARSE))) {
@@ -502,8 +440,8 @@ static void *si_buffer_transfer_map(struct pipe_context *ctx, struct pipe_resour
                                          box->width + (box->x % SI_MAP_BUFFER_ALIGNMENT), 256);
       if (staging) {
          /* Copy the VRAM buffer to the staging buffer. */
-         si_sdma_copy_buffer(sctx, &staging->b.b, resource, box->x % SI_MAP_BUFFER_ALIGNMENT,
-                             box->x, box->width);
+         si_copy_buffer(sctx, &staging->b.b, resource, box->x % SI_MAP_BUFFER_ALIGNMENT,
+                        box->x, box->width);
 
          data = si_buffer_map(sctx, staging, usage & ~PIPE_MAP_UNSYNCHRONIZED);
          if (!data) {
@@ -537,45 +475,6 @@ static void si_buffer_do_flush_region(struct pipe_context *ctx, struct pipe_tran
    if (stransfer->staging) {
       unsigned src_offset =
          stransfer->offset + transfer->box.x % SI_MAP_BUFFER_ALIGNMENT + (box->x - transfer->box.x);
-
-      if (buf->b.b.flags & SI_RESOURCE_FLAG_UPLOAD_FLUSH_EXPLICIT_VIA_SDMA) {
-         /* This should be true for all uploaders. */
-         assert(transfer->box.x == 0);
-
-         /* Find a previous upload and extend its range. The last
-          * upload is likely to be at the end of the list.
-          */
-         for (int i = sctx->num_sdma_uploads - 1; i >= 0; i--) {
-            struct si_sdma_upload *up = &sctx->sdma_uploads[i];
-
-            if (up->dst != buf)
-               continue;
-
-            assert(up->src == stransfer->staging);
-            assert(box->x > up->dst_offset);
-            up->size = box->x + box->width - up->dst_offset;
-            return;
-         }
-
-         /* Enlarge the array if it's full. */
-         if (sctx->num_sdma_uploads == sctx->max_sdma_uploads) {
-            unsigned size;
-
-            sctx->max_sdma_uploads += 4;
-            size = sctx->max_sdma_uploads * sizeof(sctx->sdma_uploads[0]);
-            sctx->sdma_uploads = realloc(sctx->sdma_uploads, size);
-         }
-
-         /* Add a new upload. */
-         struct si_sdma_upload *up = &sctx->sdma_uploads[sctx->num_sdma_uploads++];
-         up->dst = up->src = NULL;
-         si_resource_reference(&up->dst, buf);
-         si_resource_reference(&up->src, stransfer->staging);
-         up->dst_offset = box->x;
-         up->src_offset = src_offset;
-         up->size = box->width;
-         return;
-      }
 
       /* Copy the staging buffer into the original one. */
       si_copy_buffer(sctx, transfer->resource, &stransfer->staging->b.b, box->x, src_offset,
@@ -808,13 +707,6 @@ static bool si_resource_commit(struct pipe_context *pctx, struct pipe_resource *
        ctx->ws->cs_is_buffer_referenced(&ctx->gfx_cs, res->buf, RADEON_USAGE_READWRITE)) {
       si_flush_gfx_cs(ctx, RADEON_FLUSH_ASYNC_START_NEXT_GFX_IB_NOW, NULL);
    }
-   if (radeon_emitted(&ctx->sdma_cs, 0) &&
-       ctx->ws->cs_is_buffer_referenced(&ctx->sdma_cs, res->buf, RADEON_USAGE_READWRITE)) {
-      si_flush_dma_cs(ctx, PIPE_FLUSH_ASYNC, NULL);
-   }
-
-   if (ctx->sdma_cs.priv)
-      ctx->ws->cs_sync_flush(&ctx->sdma_cs);
    ctx->ws->cs_sync_flush(&ctx->gfx_cs);
 
    assert(resource->target == PIPE_BUFFER);

src/gallium/drivers/radeonsi/si_compute_blit.c
@@ -529,7 +529,6 @@ void si_compute_copy_image(struct si_context *sctx, struct pipe_resource *dst, u
 
    /* SNORM8 blitting has precision issues on some chips. Use the SINT
     * equivalent instead, which doesn't force DCC decompression.
-    * Note that some chips avoid this issue by using SDMA.
     */
    if (util_format_is_snorm8(dst->format)) {
       image[0].format = image[1].format = util_format_snorm8_to_sint8(dst->format);

commit 7bf1a513965bd51ec84666f8a199817e859c61d4

radeonsi: fix a crash in si_fence_server_sync
Fixes: 99a7862af64 - radeonsi: remove SDMA support

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8010>

src/gallium/drivers/radeonsi/si_fence.c
@@ -572,7 +572,8 @@ static void si_fence_server_sync(struct pipe_context *ctx, struct pipe_fence_han
     * the time it takes to create and submit that IB, flushing decreases
     * performance. Therefore, DO NOT FLUSH.
     */
-   si_add_fence_dependency(sctx, sfence->gfx);
+   if (sfence->gfx)
+      si_add_fence_dependency(sctx, sfence->gfx);
 }
 
 void si_init_fence_functions(struct si_context *ctx)