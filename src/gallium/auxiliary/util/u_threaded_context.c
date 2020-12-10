openharmony/third_party_mesa3d

commit abca0e37ed7756d9409c1254aa417d4557ce9f8f

gallium/u_threaded: set has_user_indices = false in the driver thread
to remove some overhead from the main thread.

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7719>

src/gallium/auxiliary/util/u_threaded_context.c
@@ -2297,6 +2297,7 @@ tc_call_draw_single(struct pipe_context *pipe, union tc_payload *payload)
    draw.start = info->info.min_index;
    draw.count = info->info.max_index;
    info->info.index_bounds_valid = false;
+   info->info.has_user_indices = false;
 
    pipe->draw_vbo(pipe, &info->info, NULL, &draw, 1);
    if (info->info.index_size)
@@ -2336,6 +2337,7 @@ tc_call_draw_multi(struct pipe_context *pipe, union tc_payload *payload)
 {
    struct tc_draw_multi *info = (struct tc_draw_multi*)payload;
 
+   info->info.has_user_indices = false;
    info->info.index_bounds_valid = false;
 
    pipe->draw_vbo(pipe, &info->info, NULL, info->slot, info->num_draws);
@@ -2396,7 +2398,6 @@ tc_draw_vbo(struct pipe_context *_pipe, const struct pipe_draw_info *info,
          struct tc_draw_single *p =
             tc_add_struct_typed_call(tc, TC_CALL_draw_single, tc_draw_single);
          memcpy(&p->info, info, DRAW_INFO_SIZE_WITHOUT_MIN_MAX_INDEX);
-         p->info.has_user_indices = false;
          p->info.index.resource = buffer;
          /* u_threaded_context stores start/count in min/max_index for single draws. */
          p->info.min_index = offset >> util_logbase2(index_size);
@@ -2444,7 +2445,6 @@ tc_draw_vbo(struct pipe_context *_pipe, const struct pipe_draw_info *info,
          tc_add_slot_based_call(tc, TC_CALL_draw_multi, tc_draw_multi,
                                 num_draws);
       memcpy(&p->info, info, DRAW_INFO_SIZE_WITHOUT_MIN_MAX_INDEX);
-      p->info.has_user_indices = false;
       p->info.index.resource = buffer;
       p->num_draws = num_draws;
 

commit 5bc46f2b1d7df60701ef81b584aea3b08076f6bc

gallium/u_threaded: set has_user_indices = false for merged draws
Fixes an assert when running this piglit test:
   arb_shader_draw_parameters-drawid-single-draw

Fixes: abca0e37ed7 ("gallium/u_threaded: set has_user_indices = false in the driver thread")
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8004>

src/gallium/auxiliary/util/u_threaded_context.c
@@ -98,6 +98,7 @@ simplify_draw_info(struct pipe_draw_info *info)
    /* Clear these fields to facilitate draw merging.
     * Drivers shouldn't use them.
     */
+   info->has_user_indices = false;
    info->index_bounds_valid = false;
    info->_pad = 0;
 