openharmony/third_party_mesa3d

commit cc23178cd7d2dba0310f2195de57a2fb5325c8f1

gitlab-ci: do not clone git-repo for test-job
The only thing we really need from the git-repo is the piglit_run.ps1
script, so let's upload that into our artifacts instead.

Fixes: 4401c814838 ("gitlab-ci: run piglit on windows")
Acked-by: Michel Dänzer <mdaenzer@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7867>

.gitlab-ci/windows/mesa_build.ps1
@@ -17,3 +17,5 @@ if (!$buildstatus) {
   Write-Host "Mesa build or test failed"
   Exit 1
 }
+
+Copy-Item ".\.gitlab-ci\windows\piglit_run.ps1" -Destination $installdir

.gitlab-ci.yml
@@ -821,11 +821,12 @@ test-d3d12-windows:
   needs:
     - meson-windows-vs2019
   variables:
+    GIT_STRATEGY: none # testing doesn't build anything from source
     GALLIUM_DRIVER: d3d12
     PIGLIT_PROFILE: quick_gl
     PIGLIT_OPTIONS: -x nv_copy_depth_to_color
   script:
-    - . .\.gitlab-ci\windows\piglit_run.ps1
+    - . _install/piglit_run.ps1
   artifacts:
     when: on_failure
     name: "mesa_${CI_JOB_NAME}"

commit 3e8f7f484181e8b38d60256118db2eebfb3b59cc

gitlab-ci: copy piglit expected results to artifacts
I somehow missed this while reworking the CI bits to reduce bandwidth
usage. This emits an error on stdout, but doesn't actually fail the
tests.

Fixes: cc23178cd7d ("gitlab-ci: do not clone git-repo for test-job")
Reviewed-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8052>

.gitlab-ci/windows/mesa_build.ps1
@@ -19,3 +19,4 @@ if (!$buildstatus) {
 }
 
 Copy-Item ".\.gitlab-ci\windows\piglit_run.ps1" -Destination $installdir
+Copy-Item ".\.gitlab-ci\windows\quick_gl.txt" -Destination $installdir

.gitlab-ci/windows/piglit_run.ps1
@@ -7,7 +7,7 @@ cmd.exe /C "C:\BuildTools\Common7\Tools\VsDevCmd.bat -host_arch=amd64 -arch=amd6
 
 py -3 C:\Piglit\bin\piglit.py summary console .\results | Select -SkipLast 1 | Select-String -NotMatch -Pattern ': pass' | Set-Content -Path .\result.txt
 
-$diff = Compare-Object -ReferenceObject $(Get-Content ".gitlab-ci\windows\$env:PIGLIT_PROFILE.txt") `
+$diff = Compare-Object -ReferenceObject $(Get-Content ".\_install\$env:PIGLIT_PROFILE.txt") `
                        -DifferenceObject $(Get-Content .\result.txt)
 if (-Not $diff) {
   Exit 0