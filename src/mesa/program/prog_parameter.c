openharmony/third_party_mesa3d

commit 6287e8beb6a44b09873413ae0f6733ebbedc6ff6

mesa: take advantage of sorted parameters in _mesa_load_state_parameters
Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6946>

src/mesa/program/prog_parameter.c
@@ -135,7 +135,14 @@ lookup_parameter_constant(const struct gl_program_parameter_list *list,
 struct gl_program_parameter_list *
 _mesa_new_parameter_list(void)
 {
-   return CALLOC_STRUCT(gl_program_parameter_list);
+   struct gl_program_parameter_list *list =
+      CALLOC_STRUCT(gl_program_parameter_list);
+   if (!list)
+      return NULL;
+
+   list->LastUniformIndex = -1;
+   list->FirstStateVarIndex = INT_MAX;
+   return list;
 }
 
 
@@ -262,7 +269,7 @@ _mesa_add_parameter(struct gl_program_parameter_list *paramList,
                     bool pad_and_align)
 {
    assert(0 < size);
-   const GLuint oldNum = paramList->NumParameters;
+   const int oldNum = paramList->NumParameters;
    unsigned oldValNum = paramList->NumParameterValues;
    const unsigned padded_size = pad_and_align ? align(size, 4) : size;
 
@@ -323,6 +330,16 @@ _mesa_add_parameter(struct gl_program_parameter_list *paramList,
    if (state) {
       for (unsigned i = 0; i < STATE_LENGTH; i++)
          paramList->Parameters[oldNum].StateIndexes[i] = state[i];
+   }
+
+   if (type == PROGRAM_UNIFORM || type == PROGRAM_CONSTANT) {
+      paramList->LastUniformIndex =
+         MAX2(paramList->LastUniformIndex, oldNum);
+   } else if (type == PROGRAM_STATE_VAR) {
+      paramList->FirstStateVarIndex =
+         MIN2(paramList->FirstStateVarIndex, oldNum);
+   } else {
+      unreachable("invalid parameter type");
    }
 
    return (GLint) oldNum;
@@ -431,4 +448,20 @@ _mesa_add_state_reference(struct gl_program_parameter_list *paramList,
                           const gl_state_index16 stateTokens[STATE_LENGTH])
 {
    return _mesa_add_sized_state_reference(paramList, stateTokens, 4, true);
+}
+
+void
+_mesa_recompute_parameter_bounds(struct gl_program_parameter_list *list)
+{
+   list->FirstStateVarIndex = INT_MAX;
+   list->LastUniformIndex = -1;
+
+   for (int i = 0; i < (int)list->NumParameters; i++) {
+      if (list->Parameters[i].Type == PROGRAM_STATE_VAR) {
+         list->FirstStateVarIndex = MIN2(list->FirstStateVarIndex, i);
+      } else {
+         list->LastUniformIndex = MAX2(list->LastUniformIndex, i);
+      }
+   }
+   assert(list->LastUniformIndex < list->FirstStateVarIndex);
 }

src/mesa/program/prog_parameter.h
@@ -142,6 +142,12 @@ struct gl_program_parameter_list
    GLbitfield StateFlags; /**< _NEW_* flags indicating which state changes
                                might invalidate ParameterValues[] */
    bool DisallowRealloc;
+
+   /* Parameters are optionally sorted as follows. Uniforms and constants
+    * are first, then state vars.
+    */
+   int LastUniformIndex;
+   int FirstStateVarIndex;
 };
 
 
@@ -241,6 +247,9 @@ _mesa_gl_datatype_is_64bit(GLenum datatype)
       return false;
    }
 }
+
+void
+_mesa_recompute_parameter_bounds(struct gl_program_parameter_list *list);
 
 #ifdef __cplusplus
 }

src/mesa/program/prog_statevars.c
@@ -1208,16 +1208,15 @@ void
 _mesa_load_state_parameters(struct gl_context *ctx,
                             struct gl_program_parameter_list *paramList)
 {
-   GLuint i;
-
    if (!paramList)
       return;
 
-   for (i = 0; i < paramList->NumParameters; i++) {
-      if (paramList->Parameters[i].Type == PROGRAM_STATE_VAR) {
-         unsigned pvo = paramList->ParameterValueOffset[i];
-         fetch_state(ctx, paramList->Parameters[i].StateIndexes,
-                     paramList->ParameterValues + pvo);
-      }
+   assert(paramList->LastUniformIndex < paramList->FirstStateVarIndex);
+   int num = paramList->NumParameters;
+
+   for (int i = paramList->FirstStateVarIndex; i < num; i++) {
+      unsigned pvo = paramList->ParameterValueOffset[i];
+      fetch_state(ctx, paramList->Parameters[i].StateIndexes,
+                  paramList->ParameterValues + pvo);
    }
 }

src/compiler/glsl/serialize.cpp
@@ -1044,6 +1044,8 @@ write_shader_parameters(struct blob *metadata,
                     sizeof(gl_constant_value) * params->NumParameterValues);
 
    blob_write_uint32(metadata, params->StateFlags);
+   blob_write_uint32(metadata, params->LastUniformIndex);
+   blob_write_uint32(metadata, params->FirstStateVarIndex);
 }
 
 static void
@@ -1078,6 +1080,8 @@ read_shader_parameters(struct blob_reader *metadata,
                    sizeof(gl_constant_value) * params->NumParameterValues);
 
    params->StateFlags = blob_read_uint32(metadata);
+   params->LastUniformIndex = blob_read_uint32(metadata);
+   params->FirstStateVarIndex = blob_read_uint32(metadata);
 }
 
 static void

src/mesa/program/prog_parameter_layout.c
@@ -231,6 +231,8 @@ _mesa_layout_parameters(struct asm_parser_state *state)
       }
    }
 
+   _mesa_recompute_parameter_bounds(layout);
+
    layout->StateFlags = state->prog->Parameters->StateFlags;
    _mesa_free_parameter_list(state->prog->Parameters);
    state->prog->Parameters = layout;

commit d1a26a81a237504bd8429acf87014bf9a7325d05

mesa: fix assertion paramList->LastUniformIndex < paramList->FirstStateVarIndex
This changes the code so that program parameters no longer have to be
sorted (meaning uniforms and constants are before state variables).

Instead of checking if the parameter is a state variable for every element,
teach all functions to handle non-state parameters safely. This is better
for the most common case where parameters are sorted or semi-sorted.

The new enum STATE_NOT_STATE_VAR identifes that a parameter is not
a state variable.

Fixes: 6287e8be - mesa: take advantage of sorted parameters in _mesa_load_state_parameters
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3914

Reviewed-by: Zoltán Böszörményi <zboszor@gmail.com>
Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8046>

src/mesa/program/prog_parameter.c
@@ -326,6 +326,8 @@ _mesa_add_parameter(struct gl_program_parameter_list *paramList,
    if (state) {
       for (unsigned i = 0; i < STATE_LENGTH; i++)
          paramList->Parameters[oldNum].StateIndexes[i] = state[i];
+   } else {
+      paramList->Parameters[oldNum].StateIndexes[0] = STATE_NOT_STATE_VAR;
    }
 
    if (type == PROGRAM_UNIFORM || type == PROGRAM_CONSTANT) {
@@ -462,5 +464,4 @@ _mesa_recompute_parameter_bounds(struct gl_program_parameter_list *list)
          list->UniformBytes = MAX2(list->UniformBytes, list->NumParameterValues * 4);
       }
    }
-   assert(list->LastUniformIndex < list->FirstStateVarIndex);
 }

src/mesa/program/prog_parameter.h
@@ -148,7 +148,9 @@ struct gl_program_parameter_list
    bool DisallowRealloc;
 
    /* Parameters are optionally sorted as follows. Uniforms and constants
-    * are first, then state vars.
+    * are first, then state vars. This should be true in all cases except
+    * ir_to_mesa, which adds constants at the end, and ARB_vp with ARL,
+    * which can't sort parameters.
     */
    int UniformBytes;
    int LastUniformIndex;

src/mesa/program/prog_statevars.c
@@ -674,6 +674,12 @@ fetch_state(struct gl_context *ctx, const gl_state_index16 state[],
       }
       return;
 
+   case STATE_NOT_STATE_VAR:
+      /* Most likely PROGRAM_CONSTANT. This only happens in rare cases, e.g.
+       * ARB_vp with ARL, which can't sort parameters by type.
+       */
+      return;
+
    default:
       unreachable("Invalid state in _mesa_fetch_state");
       return;
@@ -811,6 +817,9 @@ _mesa_program_state_flags(const gl_state_index16 state[STATE_LENGTH])
          */
 	 return 0;
       }
+
+   case STATE_NOT_STATE_VAR:
+      return 0;
 
    default:
       _mesa_problem(NULL, "unexpected state[0] in make_state_flags()");
@@ -1201,6 +1210,9 @@ _mesa_program_state_string(const gl_state_index16 state[STATE_LENGTH])
           state[1] == STATE_CURRENT_ATTRIB_MAYBE_VP_CLAMPED)
          append_index(str, state[2], false);
        break;
+   case STATE_NOT_STATE_VAR:
+      append(str, "not_state");
+      break;
    default:
       _mesa_problem(NULL, "Invalid state in _mesa_program_state_string");
       break;
@@ -1225,7 +1237,6 @@ _mesa_load_state_parameters(struct gl_context *ctx,
    if (!paramList)
       return;
 
-   assert(paramList->LastUniformIndex < paramList->FirstStateVarIndex);
    int num = paramList->NumParameters;
 
    for (int i = paramList->FirstStateVarIndex; i < num; i++) {
@@ -1240,7 +1251,6 @@ _mesa_upload_state_parameters(struct gl_context *ctx,
                               struct gl_program_parameter_list *paramList,
                               uint32_t *dst)
 {
-   assert(paramList->LastUniformIndex < paramList->FirstStateVarIndex);
    int num = paramList->NumParameters;
 
    for (int i = paramList->FirstStateVarIndex; i < num; i++) {
@@ -1260,14 +1270,10 @@ _mesa_upload_state_parameters(struct gl_context *ctx,
 void
 _mesa_optimize_state_parameters(struct gl_program_parameter_list *list)
 {
-   assert(list->LastUniformIndex < list->FirstStateVarIndex);
-
    for (int first_param = list->FirstStateVarIndex;
         first_param < (int)list->NumParameters; first_param++) {
       int last_param = first_param;
       int param_diff = 0;
-
-      assert(list->Parameters[first_param].Type == PROGRAM_STATE_VAR);
 
       switch (list->Parameters[first_param].StateIndexes[0]) {
       case STATE_MODELVIEW_MATRIX:

src/mesa/program/prog_statevars.h
@@ -50,6 +50,8 @@ struct gl_program_parameter_list;
  * always be the array index.
  */
 typedef enum gl_state_index_ {
+   STATE_NOT_STATE_VAR = 0,
+
    STATE_MATERIAL = 100,  /* start at 100 so small ints are seen as ints */
 
    STATE_LIGHT,         /* One gl_light attribute. */