openharmony/third_party_mesa3d

commit 0543da73edf7dd2add9b1882d9cf4a1daaf89cd2

draw: add NIR support to draw_create_vertex_shader
Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7826>

src/gallium/auxiliary/draw/draw_vs_exec.c
@@ -33,7 +33,10 @@
 
 #include "util/u_math.h"
 #include "util/u_memory.h"
+#include "util/ralloc.h"
 #include "pipe/p_shader_tokens.h"
+#include "pipe/p_context.h"
+#include "nir/nir_to_tgsi.h"
 
 #include "draw_private.h"
 #include "draw_context.h"
@@ -221,16 +224,24 @@ draw_create_vs_exec(struct draw_context *draw,
    if (!vs)
       return NULL;
 
-   /* we make a private copy of the tokens */
-   vs->base.state.tokens = tgsi_dup_tokens(state->tokens);
-   if (!vs->base.state.tokens) {
-      FREE(vs);
-      return NULL;
+   if (state->type == PIPE_SHADER_IR_NIR) {
+      vs->base.state.type = PIPE_SHADER_IR_TGSI;
+      vs->base.state.tokens = nir_to_tgsi(state->ir.nir, draw->pipe->screen);
+      ralloc_free(state->ir.nir);
+   } else {
+      assert(state->type == PIPE_SHADER_IR_TGSI);
+      vs->base.state.type = state->type;
+
+      /* we need to keep a local copy of the tokens */
+      vs->base.state.tokens = tgsi_dup_tokens(state->tokens);
+      if (!vs->base.state.tokens) {
+         FREE(vs);
+         return NULL;
+      }
    }
 
    tgsi_scan_shader(state->tokens, &vs->base.info);
 
-   vs->base.state.type = state->type;
    vs->base.state.stream_output = state->stream_output;
    vs->base.draw = draw;
    vs->base.prepare = vs_exec_prepare;

commit f784a733a64bfb3e219ee7f30f0453f5a92b94fc

draw: fix incorrect NIR support code
"state" contains NIR, while "vs->base.state" contains TGSI generated
from NIR. It was a typo.

This fixes the arb_vp subtest of: DRAW_USE_LLVM=1 piglit/bin/rasterpos

Fixes: 0543da73edf - draw: add NIR support to draw_create_vertex_shader

Reviewed-by: Zoltán Böszörményi <zboszor@gmail.com>
Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8046>

src/gallium/auxiliary/draw/draw_vs_exec.c
@@ -240,7 +240,7 @@ draw_create_vs_exec(struct draw_context *draw,
       }
    }
 
-   tgsi_scan_shader(state->tokens, &vs->base.info);
+   tgsi_scan_shader(vs->base.state.tokens, &vs->base.info);
 
    vs->base.state.stream_output = state->stream_output;
    vs->base.draw = draw;