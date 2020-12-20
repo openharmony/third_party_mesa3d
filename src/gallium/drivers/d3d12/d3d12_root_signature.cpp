openharmony/third_party_mesa3d

commit 299ef2093a01bb2aba8bf59e179b5c1d6400b326

d3d12: introduce d3d12 gallium driver
This driver will allow running OpenGL and OpenCL on top of Gallium
for any hardware supporting Microsoft's Direct3D 12 on Windows 10.

This is the combination of a lot of commits from our development branch,
containing code from several authors.

Co-authored-by: Bill Kristiansen <billkris@microsoft.com>
Co-authored-by: Gert Wollny <gert.wollny@collabora.com>
Co-authored-by: Jesse Natalie <jenatali@microsoft.com>
Co-authored-by: Louis-Francis Ratté-Boulianne <lfrb@collabora.com>
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Acked-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7477>

src/gallium/drivers/d3d12/d3d12_root_signature.cpp
@@ -1,0 +1,255 @@
+/*
+ * Copyright © Microsoft Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "d3d12_root_signature.h"
+#include "d3d12_compiler.h"
+#include "d3d12_screen.h"
+
+#include "util/u_memory.h"
+
+#include <wrl.h>
+using Microsoft::WRL::ComPtr;
+
+struct d3d12_root_signature {
+   struct d3d12_root_signature_key key;
+   ID3D12RootSignature *sig;
+};
+
+static D3D12_SHADER_VISIBILITY
+get_shader_visibility(enum pipe_shader_type stage)
+{
+   switch (stage) {
+   case PIPE_SHADER_VERTEX:
+      return D3D12_SHADER_VISIBILITY_VERTEX;
+   case PIPE_SHADER_FRAGMENT:
+      return D3D12_SHADER_VISIBILITY_PIXEL;
+   case PIPE_SHADER_GEOMETRY:
+      return D3D12_SHADER_VISIBILITY_GEOMETRY;
+   case PIPE_SHADER_TESS_CTRL:
+      return D3D12_SHADER_VISIBILITY_HULL;
+   case PIPE_SHADER_TESS_EVAL:
+      return D3D12_SHADER_VISIBILITY_DOMAIN;
+   default:
+      unreachable("unknown shader stage");
+   }
+}
+
+static inline void
+init_constant_root_param(D3D12_ROOT_PARAMETER1 *param,
+                         unsigned reg,
+                         unsigned size,
+                         D3D12_SHADER_VISIBILITY visibility)
+{
+   param->ParameterType = D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS;
+   param->ShaderVisibility = visibility;
+   param->Constants.RegisterSpace = 0;
+   param->Constants.ShaderRegister = reg;
+   param->Constants.Num32BitValues = size;
+}
+
+static inline void
+init_range_root_param(D3D12_ROOT_PARAMETER1 *param,
+                      D3D12_DESCRIPTOR_RANGE1 *range,
+                      D3D12_DESCRIPTOR_RANGE_TYPE type,
+                      uint32_t num_descs,
+                      D3D12_SHADER_VISIBILITY visibility,
+                      uint32_t base_shader_register)
+{
+   range->RangeType = type;
+   range->NumDescriptors = num_descs;
+   range->BaseShaderRegister = base_shader_register;
+   range->RegisterSpace = 0;
+   if (type == D3D12_DESCRIPTOR_RANGE_TYPE_SAMPLER)
+      range->Flags = D3D12_DESCRIPTOR_RANGE_FLAG_NONE;
+   else
+      range->Flags = D3D12_DESCRIPTOR_RANGE_FLAG_DATA_VOLATILE;
+   range->OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+
+   param->ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+   param->DescriptorTable.NumDescriptorRanges = 1;
+   param->DescriptorTable.pDescriptorRanges = range;
+   param->ShaderVisibility = visibility;
+}
+
+static ID3D12RootSignature *
+create_root_signature(struct d3d12_context *ctx, struct d3d12_root_signature_key *key)
+{
+   struct d3d12_screen *screen = d3d12_screen(ctx->base.screen);
+   D3D12_ROOT_PARAMETER1 root_params[D3D12_GFX_SHADER_STAGES * D3D12_NUM_BINDING_TYPES];
+   D3D12_DESCRIPTOR_RANGE1 desc_ranges[D3D12_GFX_SHADER_STAGES * D3D12_NUM_BINDING_TYPES];
+   unsigned num_params = 0;
+
+   for (unsigned i = 0; i < D3D12_GFX_SHADER_STAGES; ++i) {
+      D3D12_SHADER_VISIBILITY visibility = get_shader_visibility((enum pipe_shader_type)i);
+
+      if (key->stages[i].num_cb_bindings > 0) {
+         assert(num_params < PIPE_SHADER_TYPES * D3D12_NUM_BINDING_TYPES);
+         init_range_root_param(&root_params[num_params],
+                               &desc_ranges[num_params],
+                               D3D12_DESCRIPTOR_RANGE_TYPE_CBV,
+                               key->stages[i].num_cb_bindings,
+                               visibility,
+                               key->stages[i].has_default_ubo0 ? 0 : 1);
+         num_params++;
+      }
+
+      if (key->stages[i].num_srv_bindings > 0) {
+         init_range_root_param(&root_params[num_params],
+                               &desc_ranges[num_params],
+                               D3D12_DESCRIPTOR_RANGE_TYPE_SRV,
+                               key->stages[i].num_srv_bindings,
+                               visibility,
+                               0);
+         num_params++;
+      }
+
+      if (key->stages[i].num_srv_bindings > 0) {
+         init_range_root_param(&root_params[num_params],
+                               &desc_ranges[num_params],
+                               D3D12_DESCRIPTOR_RANGE_TYPE_SAMPLER,
+                               key->stages[i].num_srv_bindings,
+                               visibility,
+                               0);
+         num_params++;
+      }
+
+      if (key->stages[i].state_vars_size > 0) {
+         init_constant_root_param(&root_params[num_params],
+                                  key->stages[i].num_cb_bindings + (key->stages[i].has_default_ubo0 ? 0 : 1),
+                                  key->stages[i].state_vars_size,
+                                  visibility);
+         num_params++;
+      }
+   }
+
+   D3D12_VERSIONED_ROOT_SIGNATURE_DESC root_sig_desc;
+   root_sig_desc.Version = D3D_ROOT_SIGNATURE_VERSION_1_1;
+   root_sig_desc.Desc_1_1.NumParameters = num_params;
+   root_sig_desc.Desc_1_1.pParameters = (num_params > 0) ? root_params : NULL;
+   root_sig_desc.Desc_1_1.NumStaticSamplers = 0;
+   root_sig_desc.Desc_1_1.pStaticSamplers = NULL;
+   root_sig_desc.Desc_1_1.Flags = D3D12_ROOT_SIGNATURE_FLAG_NONE;
+
+   /* TODO Only enable this flag when needed (optimization) */
+   root_sig_desc.Desc_1_1.Flags |= D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+   if (key->has_stream_output)
+      root_sig_desc.Desc_1_1.Flags |= D3D12_ROOT_SIGNATURE_FLAG_ALLOW_STREAM_OUTPUT;
+
+   ComPtr<ID3DBlob> sig, error;
+   if (FAILED(ctx->D3D12SerializeVersionedRootSignature(&root_sig_desc,
+                                                        &sig, &error))) {
+      debug_printf("D3D12SerializeRootSignature failed\n");
+      return NULL;
+   }
+
+   ID3D12RootSignature *ret;
+   if (FAILED(screen->dev->CreateRootSignature(0,
+                                               sig->GetBufferPointer(),
+                                               sig->GetBufferSize(),
+                                               __uuidof(ret),
+                                               (void **)&ret))) {
+      debug_printf("CreateRootSignature failed\n");
+      return NULL;
+   }
+   return ret;
+}
+
+static void
+fill_key(struct d3d12_context *ctx, struct d3d12_root_signature_key *key)
+{
+   memset(key, 0, sizeof(struct d3d12_root_signature_key));
+
+   for (unsigned i = 0; i < D3D12_GFX_SHADER_STAGES; ++i) {
+      struct d3d12_shader *shader = ctx->gfx_pipeline_state.stages[i];
+
+      if (shader) {
+         key->stages[i].num_cb_bindings = shader->num_cb_bindings;
+         key->stages[i].num_srv_bindings = shader->num_srv_bindings;
+         key->stages[i].state_vars_size = shader->state_vars_size;
+         key->stages[i].has_default_ubo0 = shader->has_default_ubo0;
+
+         if (ctx->gfx_stages[i]->so_info.num_outputs > 0)
+            key->has_stream_output = true;
+      }
+   }
+}
+
+ID3D12RootSignature *
+d3d12_get_root_signature(struct d3d12_context *ctx)
+{
+   struct d3d12_root_signature_key key;
+
+   fill_key(ctx, &key);
+   struct hash_entry *entry = _mesa_hash_table_search(ctx->root_signature_cache, &key);
+   if (!entry) {
+      struct d3d12_root_signature *data =
+         (struct d3d12_root_signature *)MALLOC(sizeof(struct d3d12_root_signature));
+      if (!data)
+         return NULL;
+
+      data->key = key;
+      data->sig = create_root_signature(ctx, &key);
+      if (!data->sig)
+         return NULL;
+
+      entry = _mesa_hash_table_insert(ctx->root_signature_cache, &data->key, data);
+      assert(entry);
+   }
+
+   return ((struct d3d12_root_signature *)entry->data)->sig;
+}
+
+static uint32_t
+hash_root_signature_key(const void *key)
+{
+   return _mesa_hash_data(key, sizeof(struct d3d12_root_signature_key));
+}
+
+static bool
+equals_root_signature_key(const void *a, const void *b)
+{
+   return memcmp(a, b, sizeof(struct d3d12_root_signature_key)) == 0;
+}
+
+void
+d3d12_root_signature_cache_init(struct d3d12_context *ctx)
+{
+   ctx->root_signature_cache = _mesa_hash_table_create(NULL,
+                                                       hash_root_signature_key,
+                                                       equals_root_signature_key);
+}
+
+static void
+delete_entry(struct hash_entry *entry)
+{
+   struct d3d12_root_signature *data = (struct d3d12_root_signature *)entry->data;
+   data->sig->Release();
+   FREE(data);
+}
+
+void
+d3d12_root_signature_cache_destroy(struct d3d12_context *ctx)
+{
+   _mesa_hash_table_destroy(ctx->root_signature_cache, delete_entry);
+}

meson.build
@@ -235,6 +235,7 @@ with_gallium_virgl = gallium_drivers.contains('virgl')
 with_gallium_swr = gallium_drivers.contains('swr')
 with_gallium_lima = gallium_drivers.contains('lima')
 with_gallium_zink = gallium_drivers.contains('zink')
+with_gallium_d3d12 = gallium_drivers.contains('d3d12')
 
 with_gallium = gallium_drivers.length() != 0
 

src/gallium/drivers/d3d12/d3d12_batch.cpp
@@ -1,0 +1,256 @@
+/*
+ * Copyright © Microsoft Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "d3d12_batch.h"
+#include "d3d12_context.h"
+#include "d3d12_fence.h"
+#include "d3d12_query.h"
+#include "d3d12_resource.h"
+#include "d3d12_screen.h"
+#include "d3d12_surface.h"
+
+#include "util/hash_table.h"
+#include "util/set.h"
+#include "util/u_inlines.h"
+
+bool
+d3d12_init_batch(struct d3d12_context *ctx, struct d3d12_batch *batch)
+{
+   struct d3d12_screen *screen = d3d12_screen(ctx->base.screen);
+
+   batch->bos = _mesa_set_create(NULL, _mesa_hash_pointer,
+                                 _mesa_key_pointer_equal);
+   batch->sampler_views = _mesa_set_create(NULL, _mesa_hash_pointer,
+                                           _mesa_key_pointer_equal);
+   batch->surfaces = _mesa_set_create(NULL, _mesa_hash_pointer,
+                                      _mesa_key_pointer_equal);
+   batch->objects = _mesa_set_create(NULL,
+                                     _mesa_hash_pointer,
+                                     _mesa_key_pointer_equal);
+
+   if (!batch->bos || !batch->sampler_views || !batch->surfaces || !batch->objects)
+      return false;
+
+   util_dynarray_init(&batch->zombie_samplers, NULL);
+
+   if (FAILED(screen->dev->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT,
+                                                  __uuidof(batch->cmdalloc),
+                                                  (void **)&batch->cmdalloc)))
+      return false;
+
+
+   batch->sampler_heap =
+      d3d12_descriptor_heap_new(screen->dev,
+                                D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER,
+                                D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
+                                128);
+
+   batch->view_heap =
+      d3d12_descriptor_heap_new(screen->dev,
+                                D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
+                                D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
+                                1024);
+
+   if (!batch->sampler_heap && !batch->view_heap)
+      return false;
+
+   return true;
+}
+
+static void
+delete_bo(set_entry *entry)
+{
+   struct d3d12_bo *bo = (struct d3d12_bo *)entry->key;
+   d3d12_bo_unreference(bo);
+}
+
+static void
+delete_sampler_view(set_entry *entry)
+{
+   struct pipe_sampler_view *pres = (struct pipe_sampler_view *)entry->key;
+   pipe_sampler_view_reference(&pres, NULL);
+}
+
+static void
+delete_surface(set_entry *entry)
+{
+   struct pipe_surface *surf = (struct pipe_surface *)entry->key;
+   pipe_surface_reference(&surf, NULL);
+}
+
+static void
+delete_object(set_entry *entry)
+{
+   ID3D12Object *object = (ID3D12Object *)entry->key;
+   object->Release();
+}
+
+bool
+d3d12_reset_batch(struct d3d12_context *ctx, struct d3d12_batch *batch, uint64_t timeout_ns)
+{
+   struct d3d12_screen *screen = d3d12_screen(ctx->base.screen);
+
+   // batch hasn't been submitted before
+   if (!batch->fence && !batch->has_errors)
+      return true;
+
+   if (batch->fence) {
+      if (!d3d12_fence_finish(batch->fence, timeout_ns))
+         return false;
+      d3d12_fence_reference(&batch->fence, NULL);
+   }
+
+   _mesa_set_clear(batch->bos, delete_bo);
+   _mesa_set_clear(batch->sampler_views, delete_sampler_view);
+   _mesa_set_clear(batch->surfaces, delete_surface);
+   _mesa_set_clear(batch->objects, delete_object);
+
+   util_dynarray_foreach(&batch->zombie_samplers, d3d12_descriptor_handle, handle)
+      d3d12_descriptor_handle_free(handle);
+   util_dynarray_clear(&batch->zombie_samplers);
+
+   d3d12_descriptor_heap_clear(batch->view_heap);
+   d3d12_descriptor_heap_clear(batch->sampler_heap);
+
+   if (FAILED(batch->cmdalloc->Reset())) {
+      debug_printf("D3D12: resetting ID3D12CommandAllocator failed\n");
+      return false;
+   }
+   batch->has_errors = false;
+   return true;
+}
+
+void
+d3d12_destroy_batch(struct d3d12_context *ctx, struct d3d12_batch *batch)
+{
+   d3d12_reset_batch(ctx, batch, PIPE_TIMEOUT_INFINITE);
+   batch->cmdalloc->Release();
+   d3d12_descriptor_heap_free(batch->sampler_heap);
+   d3d12_descriptor_heap_free(batch->view_heap);
+   _mesa_set_destroy(batch->bos, NULL);
+   _mesa_set_destroy(batch->sampler_views, NULL);
+   _mesa_set_destroy(batch->surfaces, NULL);
+   _mesa_set_destroy(batch->objects, NULL);
+   util_dynarray_fini(&batch->zombie_samplers);
+}
+
+void
+d3d12_start_batch(struct d3d12_context *ctx, struct d3d12_batch *batch)
+{
+   struct d3d12_screen *screen = d3d12_screen(ctx->base.screen);
+   ID3D12DescriptorHeap* heaps[2] = { d3d12_descriptor_heap_get(batch->view_heap),
+                                      d3d12_descriptor_heap_get(batch->sampler_heap) };
+
+   d3d12_reset_batch(ctx, batch, PIPE_TIMEOUT_INFINITE);
+
+   /* Create or reset global command list */
+   if (ctx->cmdlist) {
+      if (FAILED(ctx->cmdlist->Reset(batch->cmdalloc, NULL))) {
+         debug_printf("D3D12: resetting ID3D12GraphicsCommandList failed\n");
+         batch->has_errors = true;
+         return;
+      }
+   } else {
+      if (FAILED(screen->dev->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT,
+                                                batch->cmdalloc, NULL,
+                                                __uuidof(ctx->cmdlist),
+                                                (void **)&ctx->cmdlist))) {
+         debug_printf("D3D12: creating ID3D12GraphicsCommandList failed\n");
+         batch->has_errors = true;
+         return;
+      }
+   }
+
+   ctx->cmdlist->SetDescriptorHeaps(2, heaps);
+   ctx->cmdlist_dirty = ~0;
+   for (int i = 0; i < D3D12_GFX_SHADER_STAGES; ++i)
+      ctx->shader_dirty[i] = ~0;
+
+   if (!ctx->queries_disabled)
+      d3d12_resume_queries(ctx);
+}
+
+void
+d3d12_end_batch(struct d3d12_context *ctx, struct d3d12_batch *batch)
+{
+   struct d3d12_screen *screen = d3d12_screen(ctx->base.screen);
+
+   if (!ctx->queries_disabled)
+      d3d12_suspend_queries(ctx);
+
+   if (FAILED(ctx->cmdlist->Close())) {
+      debug_printf("D3D12: closing ID3D12GraphicsCommandList failed\n");
+      batch->has_errors = true;
+      return;
+   }
+
+   ID3D12CommandList* cmdlists[] = { ctx->cmdlist };
+   screen->cmdqueue->ExecuteCommandLists(1, cmdlists);
+   batch->fence = d3d12_create_fence(screen, ctx);
+}
+
+bool
+d3d12_batch_has_references(struct d3d12_batch *batch,
+                           struct d3d12_bo *bo)
+{
+   return (_mesa_set_search(batch->bos, bo) != NULL);
+}
+
+void
+d3d12_batch_reference_resource(struct d3d12_batch *batch,
+                               struct d3d12_resource *res)
+{
+   if (!d3d12_batch_has_references(batch, res->bo)) {
+      _mesa_set_add(batch->bos, res->bo);
+      d3d12_bo_reference(res->bo);
+   }
+}
+
+void
+d3d12_batch_reference_sampler_view(struct d3d12_batch *batch,
+                                   struct d3d12_sampler_view *sv)
+{
+   struct set_entry *entry = _mesa_set_search(batch->sampler_views, sv);
+   if (!entry) {
+      entry = _mesa_set_add(batch->sampler_views, sv);
+      pipe_reference(NULL, &sv->base.reference);
+   }
+}
+
+void
+d3d12_batch_reference_surface_texture(struct d3d12_batch *batch,
+                                      struct d3d12_surface *surf)
+{
+   d3d12_batch_reference_resource(batch, d3d12_resource(surf->base.texture));
+}
+
+void
+d3d12_batch_reference_object(struct d3d12_batch *batch,
+                             ID3D12Object *object)
+{
+   struct set_entry *entry = _mesa_set_search(batch->objects, object);
+   if (!entry) {
+      entry = _mesa_set_add(batch->objects, object);
+      object->AddRef();
+   }
+}

src/gallium/drivers/d3d12/d3d12_batch.h
@@ -1,0 +1,88 @@
+/*
+ * Copyright © Microsoft Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef D3D12_BATCH_H
+#define D3D12_BATCH_H
+
+#include "util/u_dynarray.h"
+#include <stdint.h>
+
+#define D3D12_IGNORE_SDK_LAYERS
+#include <d3d12.h>
+
+struct d3d12_bo;
+struct d3d12_descriptor_heap;
+struct d3d12_fence;
+
+struct d3d12_batch {
+   struct d3d12_fence *fence;
+
+   struct set *bos;
+   struct set *sampler_views;
+   struct set *surfaces;
+   struct set *objects;
+
+   struct util_dynarray zombie_samplers;
+
+   ID3D12CommandAllocator *cmdalloc;
+   struct d3d12_descriptor_heap *sampler_heap;
+   struct d3d12_descriptor_heap *view_heap;
+   bool has_errors;
+};
+
+bool
+d3d12_init_batch(struct d3d12_context *ctx, struct d3d12_batch *batch);
+
+void
+d3d12_destroy_batch(struct d3d12_context *ctx, struct d3d12_batch *batch);
+
+void
+d3d12_start_batch(struct d3d12_context *ctx, struct d3d12_batch *batch);
+
+void
+d3d12_end_batch(struct d3d12_context *ctx, struct d3d12_batch *batch);
+
+bool
+d3d12_reset_batch(struct d3d12_context *ctx, struct d3d12_batch *batch, uint64_t timeout_ns);
+
+bool
+d3d12_batch_has_references(struct d3d12_batch *batch,
+                           struct d3d12_bo *bo);
+
+void
+d3d12_batch_reference_resource(struct d3d12_batch *batch,
+                               struct d3d12_resource *res);
+
+void
+d3d12_batch_reference_sampler_view(struct d3d12_batch *batch,
+                                   struct d3d12_sampler_view *sv);
+
+void
+d3d12_batch_reference_surface_texture(struct d3d12_batch *batch,
+                              struct d3d12_surface *surf);
+
+void
+d3d12_batch_reference_object(struct d3d12_batch *batch,
+                             ID3D12Object *object);
+
+#endif

src/gallium/drivers/d3d12/d3d12_blit.h
@@ -1,0 +1,43 @@
+/*
+ * Copyright © Microsoft Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef D3D12_BLIT_H
+#define D3D12_BLIT_H
+
+struct d3d12_context;
+struct d3d12_resource;
+struct pipe_box;
+
+void
+d3d12_context_blit_init(struct pipe_context *ctx);
+
+void
+d3d12_direct_copy(struct d3d12_context *ctx,
+                  struct d3d12_resource *dst,
+                  unsigned dst_level,
+                  const struct pipe_box *pdst_box,
+                  struct d3d12_resource *src,
+                  unsigned src_level,
+                  const struct pipe_box *psrc_box, unsigned mask);
+
+#endif // D3D12_BLIT_H

commit 6c40b817c086058f0edc082c6e361f837a4547f2

d3d12: Fix memory leak if create_root_signature failed.
Fix defect reported by Coverity Scan.

Resource leak (RESOURCE_LEAK)
leaked_storage: Variable data going out of scope leaks the storage it points to.

Fixes: 299ef2093a0 ("d3d12: introduce d3d12 gallium driver")
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Reviewed-by: Erik Faye-Lund <kusmabite@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8170>

src/gallium/drivers/d3d12/d3d12_root_signature.cpp
@@ -211,8 +211,10 @@ d3d12_get_root_signature(struct d3d12_context *ctx)
 
       data->key = key;
       data->sig = create_root_signature(ctx, &key);
-      if (!data->sig)
+      if (!data->sig) {
+         FREE(data);
          return NULL;
+      }
 
       entry = _mesa_hash_table_insert(ctx->root_signature_cache, &data->key, data);
       assert(entry);