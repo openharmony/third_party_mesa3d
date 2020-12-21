openharmony/third_party_mesa3d

commit 6cac25e3d09b83c23a1eaca7b0cd06dd3accecae

freedreno/a6xx: Program state for tessellation stages
Signed-off-by: Kristian H. Kristensen <hoegsberg@google.com>
Acked-by: Eric Anholt <eric@anholt.net>
Reviewed-by: Rob Clark <robdclark@gmail.com>

src/gallium/drivers/freedreno/a6xx/fd6_program.c
@@ -277,6 +277,8 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_screen *screen,
 	uint32_t face_regid, coord_regid, zwcoord_regid, samp_id_regid;
 	uint32_t smask_in_regid, smask_regid;
 	uint32_t vertex_regid, instance_regid, layer_regid, primitive_regid;
+	uint32_t hs_invocation_regid;
+	uint32_t tess_coord_x_regid, tess_coord_y_regid, hs_patch_regid, ds_patch_regid;
 	uint32_t ij_pix_regid, ij_samp_regid, ij_cent_regid, ij_size_regid;
 	uint32_t gs_header_regid;
 	enum a3xx_threadsize fssz;
@@ -304,8 +306,25 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_screen *screen,
 	vertex_regid = ir3_find_sysval_regid(vs, SYSTEM_VALUE_VERTEX_ID);
 	instance_regid = ir3_find_sysval_regid(vs, SYSTEM_VALUE_INSTANCE_ID);
 
+	if (hs) {
+		tess_coord_x_regid = ir3_find_sysval_regid(ds, SYSTEM_VALUE_TESS_COORD);
+		tess_coord_y_regid = next_regid(tess_coord_x_regid, 1);
+		hs_patch_regid = ir3_find_sysval_regid(hs, SYSTEM_VALUE_PRIMITIVE_ID);
+		ds_patch_regid = ir3_find_sysval_regid(ds, SYSTEM_VALUE_PRIMITIVE_ID);
+		hs_invocation_regid = ir3_find_sysval_regid(hs, SYSTEM_VALUE_TCS_HEADER_IR3);
+
+		pos_regid = ir3_find_output_regid(ds, VARYING_SLOT_POS);
+		psize_regid = ir3_find_output_regid(ds, VARYING_SLOT_PSIZ);
+	} else {
+		tess_coord_x_regid = regid(63, 0);
+		tess_coord_y_regid = regid(63, 0);
+		hs_patch_regid = regid(63, 0);
+		ds_patch_regid = regid(63, 0);
+		hs_invocation_regid = regid(63, 0);
+	}
+
 	if (gs) {
-		gs_header_regid = ir3_find_sysval_regid(vs, SYSTEM_VALUE_GS_HEADER_IR3);
+		gs_header_regid = ir3_find_sysval_regid(gs, SYSTEM_VALUE_GS_HEADER_IR3);
 		primitive_regid = ir3_find_sysval_regid(gs, SYSTEM_VALUE_PRIMITIVE_ID);
 		pos_regid = ir3_find_output_regid(gs, VARYING_SLOT_POS);
 		psize_regid = ir3_find_output_regid(gs, VARYING_SLOT_PSIZ);
@@ -399,14 +418,8 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_screen *screen,
 			COND(vs->need_pixlod, A6XX_SP_VS_CTRL_REG0_PIXLODENABLE));
 
 	struct ir3_shader_linkage l = {0};
-	if (gs)
-		ir3_link_shaders(&l, gs, fs);
-	else
-		ir3_link_shaders(&l, vs, fs);
-
-	const struct ir3_shader_variant *so_shader = fd6_last_shader(state);
-	if (so_shader->shader->stream_output.num_outputs > 0)
-		link_stream_out(&l, so_shader);
+	const struct ir3_shader_variant *last_shader = fd6_last_shader(state);
+	ir3_link_shaders(&l, last_shader, fs);
 
 	BITSET_DECLARE(varbs, 128) = {0};
 	uint32_t *varmask = (uint32_t *)varbs;
@@ -420,6 +433,10 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_screen *screen,
 	OUT_RING(ring, ~varmask[1]);  /* VPC_VAR[1].DISABLE */
 	OUT_RING(ring, ~varmask[2]);  /* VPC_VAR[2].DISABLE */
 	OUT_RING(ring, ~varmask[3]);  /* VPC_VAR[3].DISABLE */
+
+	/* Add stream out outputs after computing the VPC_VAR_DISABLE bitmask. */
+	if (last_shader->shader->stream_output.num_outputs > 0)
+		link_stream_out(&l, last_shader);
 
 	if (VALIDREG(layer_regid)) {
 		layer_loc = l.max_loc;
@@ -436,13 +453,15 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_screen *screen,
 		ir3_link_add(&l, psize_regid, 0x1, l.max_loc);
 	}
 
-	if (so_shader->shader->stream_output.num_outputs > 0) {
-		setup_stream_out(state, so_shader, &l);
+	if (last_shader->shader->stream_output.num_outputs > 0) {
+		setup_stream_out(state, last_shader, &l);
 	}
 
 	debug_assert(l.cnt < 32);
 	if (gs)
 		OUT_PKT4(ring, REG_A6XX_SP_GS_OUT_REG(0), DIV_ROUND_UP(l.cnt, 2));
+	else if (ds)
+		OUT_PKT4(ring, REG_A6XX_SP_DS_OUT_REG(0), DIV_ROUND_UP(l.cnt, 2));
 	else
 		OUT_PKT4(ring, REG_A6XX_SP_VS_OUT_REG(0), DIV_ROUND_UP(l.cnt, 2));
 
@@ -462,6 +481,8 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_screen *screen,
 
 	if (gs)
 		OUT_PKT4(ring, REG_A6XX_SP_GS_VPC_DST_REG(0), DIV_ROUND_UP(l.cnt, 4));
+	else if (ds)
+		OUT_PKT4(ring, REG_A6XX_SP_DS_VPC_DST_REG(0), DIV_ROUND_UP(l.cnt, 4));
 	else
 		OUT_PKT4(ring, REG_A6XX_SP_VS_VPC_DST_REG(0), DIV_ROUND_UP(l.cnt, 4));
 
@@ -477,9 +498,89 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_screen *screen,
 	}
 
 	fd6_emit_shader(ring, vs);
+	ir3_emit_immediates(screen, vs, ring);
 
-	OUT_PKT4(ring, REG_A6XX_SP_HS_UNKNOWN_A831, 1);
-	OUT_RING(ring, 0);
+	if (hs) {
+		OUT_PKT4(ring, REG_A6XX_SP_HS_CTRL_REG0, 1);
+		OUT_RING(ring, A6XX_SP_HS_CTRL_REG0_THREADSIZE(TWO_QUADS) |
+			A6XX_SP_HS_CTRL_REG0_FULLREGFOOTPRINT(hs->info.max_reg + 1) |
+			A6XX_SP_HS_CTRL_REG0_BRANCHSTACK(hs->branchstack) |
+			COND(hs->need_pixlod, A6XX_SP_HS_CTRL_REG0_PIXLODENABLE));
+
+		fd6_emit_shader(ring, hs);
+		ir3_emit_immediates(screen, hs, ring);
+		ir3_emit_link_map(screen, vs, hs, ring);
+
+		OUT_PKT4(ring, REG_A6XX_SP_DS_CTRL_REG0, 1);
+		OUT_RING(ring, A6XX_SP_DS_CTRL_REG0_THREADSIZE(TWO_QUADS) |
+			A6XX_SP_DS_CTRL_REG0_FULLREGFOOTPRINT(ds->info.max_reg + 1) |
+			A6XX_SP_DS_CTRL_REG0_BRANCHSTACK(ds->branchstack) |
+			COND(ds->need_pixlod, A6XX_SP_DS_CTRL_REG0_PIXLODENABLE));
+
+		fd6_emit_shader(ring, ds);
+		ir3_emit_immediates(screen, ds, ring);
+		ir3_emit_link_map(screen, hs, ds, ring);
+
+		shader_info *hs_info = &hs->shader->nir->info;
+		OUT_PKT4(ring, REG_A6XX_PC_TESS_NUM_VERTEX, 1);
+		OUT_RING(ring, hs_info->tess.tcs_vertices_out);
+
+		/* Total attribute slots in HS incoming patch. */
+		OUT_PKT4(ring, REG_A6XX_PC_UNKNOWN_9801, 1);
+		OUT_RING(ring, hs_info->tess.tcs_vertices_out * vs->shader->output_size / 4);
+
+		OUT_PKT4(ring, REG_A6XX_SP_HS_UNKNOWN_A831, 1);
+		OUT_RING(ring, vs->shader->output_size);
+
+		shader_info *ds_info = &ds->shader->nir->info;
+		OUT_PKT4(ring, REG_A6XX_PC_TESS_CNTL, 1);
+		uint32_t output;
+		if (ds_info->tess.point_mode)
+			output = TESS_POINTS;
+		else if (ds_info->tess.primitive_mode == GL_ISOLINES)
+			output = TESS_LINES;
+		else if (ds_info->tess.ccw)
+			output = TESS_CCW_TRIS;
+		else
+			output = TESS_CW_TRIS;
+
+		OUT_RING(ring, A6XX_PC_TESS_CNTL_SPACING(fd6_gl2spacing(ds_info->tess.spacing)) |
+				A6XX_PC_TESS_CNTL_OUTPUT(output));
+
+		/* xxx: Misc tess unknowns: */
+		OUT_PKT4(ring, REG_A6XX_VPC_UNKNOWN_9103, 1);
+		OUT_RING(ring, 0x00ffff00);
+
+		OUT_PKT4(ring, REG_A6XX_VPC_UNKNOWN_9106, 1);
+		OUT_RING(ring, 0x0000ffff);
+
+		OUT_PKT4(ring, REG_A6XX_GRAS_UNKNOWN_809D, 1);
+		OUT_RING(ring, 0x0);
+
+		OUT_PKT4(ring, REG_A6XX_GRAS_UNKNOWN_8002, 1);
+		OUT_RING(ring, 0x0);
+
+		OUT_PKT4(ring, REG_A6XX_VPC_PACK, 1);
+		OUT_RING(ring, A6XX_VPC_PACK_POSITIONLOC(pos_loc) |
+				 A6XX_VPC_PACK_PSIZELOC(255) |
+				 A6XX_VPC_PACK_STRIDE_IN_VPC(l.max_loc));
+
+		OUT_PKT4(ring, REG_A6XX_VPC_PACK_3, 1);
+		OUT_RING(ring, A6XX_VPC_PACK_3_POSITIONLOC(pos_loc) |
+				 A6XX_VPC_PACK_3_PSIZELOC(psize_loc) |
+				 A6XX_VPC_PACK_3_STRIDE_IN_VPC(l.max_loc));
+
+		OUT_PKT4(ring, REG_A6XX_SP_DS_PRIMITIVE_CNTL, 1);
+		OUT_RING(ring, A6XX_SP_DS_PRIMITIVE_CNTL_DSOUT(l.cnt));
+
+		OUT_PKT4(ring, REG_A6XX_PC_PRIMITIVE_CNTL_4, 1);
+		OUT_RING(ring, A6XX_PC_PRIMITIVE_CNTL_4_STRIDE_IN_VPC(l.max_loc) |
+				CONDREG(psize_regid, 0x100));
+
+	} else {
+		OUT_PKT4(ring, REG_A6XX_SP_HS_UNKNOWN_A831, 1);
+		OUT_RING(ring, 0);
+	}
 
 	OUT_PKT4(ring, REG_A6XX_SP_PRIMITIVE_CNTL, 1);
 	OUT_RING(ring, A6XX_SP_PRIMITIVE_CNTL_VSOUT(l.cnt));
@@ -494,6 +595,9 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_screen *screen,
 	OUT_PKT4(ring, REG_A6XX_PC_PRIMITIVE_CNTL_1, 1);
 	OUT_RING(ring, A6XX_PC_PRIMITIVE_CNTL_1_STRIDE_IN_VPC(l.max_loc) |
 			CONDREG(psize_regid, A6XX_PC_PRIMITIVE_CNTL_1_PSIZE));
+
+	OUT_PKT4(ring, REG_A6XX_PC_PRIMITIVE_CNTL_3, 1);
+	OUT_RING(ring, 0);
 
 	OUT_PKT4(ring, REG_A6XX_HLSQ_CONTROL_1_REG, 5);
 	OUT_RING(ring, 0x7);                /* XXX */
@@ -595,7 +699,10 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_screen *screen,
 
 		fd6_emit_shader(ring, gs);
 		ir3_emit_immediates(screen, gs, ring);
-		ir3_emit_link_map(screen, vs, gs, ring);
+		if (ds)
+			ir3_emit_link_map(screen, ds, gs, ring);
+		else
+			ir3_emit_link_map(screen, vs, gs, ring);
 
 		OUT_PKT4(ring, REG_A6XX_VPC_PACK_GS, 1);
 		OUT_RING(ring, A6XX_VPC_PACK_GS_POSITIONLOC(pos_loc) |
@@ -646,19 +753,15 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_screen *screen,
 		OUT_PKT4(ring, REG_A6XX_VPC_UNKNOWN_9100, 1);
 		OUT_RING(ring, 0xff);
 
-		OUT_PKT4(ring, REG_A6XX_VPC_UNKNOWN_9101, 1);
-		OUT_RING(ring, 0xffff00);
-
 		OUT_PKT4(ring, REG_A6XX_VPC_UNKNOWN_9102, 1);
 		OUT_RING(ring, 0xffff00);
 
-		OUT_PKT4(ring, REG_A6XX_VPC_UNKNOWN_9107, 1);
-		OUT_RING(ring, 0);
+		const struct ir3_shader_variant *prev = state->ds ? state->ds : state->vs;
 
 		/* Size of per-primitive alloction in ldlw memory in vec4s. */
 		uint32_t vec4_size =
 			gs->shader->nir->info.gs.vertices_in *
-			DIV_ROUND_UP(vs->shader->output_size, 4);
+			DIV_ROUND_UP(prev->shader->output_size, 4);
 		OUT_PKT4(ring, REG_A6XX_PC_PRIMITIVE_CNTL_6, 1);
 		OUT_RING(ring, A6XX_PC_PRIMITIVE_CNTL_6_STRIDE_IN_VPC(vec4_size));
 
@@ -666,8 +769,20 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_screen *screen,
 		OUT_RING(ring, 0);
 
 		OUT_PKT4(ring, REG_A6XX_SP_GS_UNKNOWN_A871, 1);
-		OUT_RING(ring, 3);
+		OUT_RING(ring, prev->shader->output_size);
+	} else {
+		OUT_PKT4(ring, REG_A6XX_PC_PRIMITIVE_CNTL_6, 1);
+		OUT_RING(ring, 0);
+		OUT_PKT4(ring, REG_A6XX_SP_GS_UNKNOWN_A871, 1);
+		OUT_RING(ring, 0);
 	}
+
+	OUT_PKT4(ring, REG_A6XX_VPC_UNKNOWN_9101, 1);
+	OUT_RING(ring, 0xffff00);
+
+	OUT_PKT4(ring, REG_A6XX_VPC_UNKNOWN_9107, 1);
+	OUT_RING(ring, 0);
+
 
 	if (!binning_pass) {
 		/* figure out VARYING_INTERP / VARYING_PS_REPL register values: */
@@ -701,11 +816,11 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_screen *screen,
 			A6XX_VFD_CONTROL_1_REGID4INST(instance_regid) |
 			A6XX_VFD_CONTROL_1_REGID4PRIMID(primitive_regid) |
 			0xfc000000);
-	OUT_RING(ring, A6XX_VFD_CONTROL_2_REGID_HSPATCHID(regid(63,0)) |
-			A6XX_VFD_CONTROL_2_REGID_INVOCATIONID(regid(63,0)));
-	OUT_RING(ring, A6XX_VFD_CONTROL_3_REGID_DSPATCHID(regid(63,0)) |
-			A6XX_VFD_CONTROL_3_REGID_TESSX(regid(63,0)) |
-			A6XX_VFD_CONTROL_3_REGID_TESSY(regid(63,0)) |
+	OUT_RING(ring, A6XX_VFD_CONTROL_2_REGID_HSPATCHID(hs_patch_regid) |
+			A6XX_VFD_CONTROL_2_REGID_INVOCATIONID(hs_invocation_regid));
+	OUT_RING(ring, A6XX_VFD_CONTROL_3_REGID_DSPATCHID(ds_patch_regid) |
+			A6XX_VFD_CONTROL_3_REGID_TESSX(tess_coord_x_regid) |
+			A6XX_VFD_CONTROL_3_REGID_TESSY(tess_coord_y_regid) |
 			0xfc);
 	OUT_RING(ring, 0x000000fc);   /* VFD_CONTROL_4 */
 	OUT_RING(ring, A6XX_VFD_CONTROL_5_REGID_GSHEADER(gs_header_regid) |
@@ -719,13 +834,6 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_screen *screen,
 
 	OUT_PKT4(ring, REG_A6XX_GRAS_SU_DEPTH_PLANE_CNTL, 1);
 	OUT_RING(ring, COND(fragz, A6XX_GRAS_SU_DEPTH_PLANE_CNTL_FRAG_WRITES_Z));
-
-	ir3_emit_immediates(screen, vs, ring);
-
-	if (hs) {
-		ir3_emit_immediates(screen, hs, ring);
-		ir3_emit_immediates(screen, ds, ring);
-	}
 
 	if (!binning_pass)
 		ir3_emit_immediates(screen, fs, ring);

src/freedreno/registers/a6xx.xml
@@ -1856,6 +1856,7 @@ to upconvert to 32b float internally?
 
 	<reg32 offset="0x8000" name="GRAS_UNKNOWN_8000"/>
 	<reg32 offset="0x8001" name="GRAS_UNKNOWN_8001"/>
+	<reg32 offset="0x8002" name="GRAS_UNKNOWN_8002"/>
 	<reg32 offset="0x8003" name="GRAS_UNKNOWN_8003"/>
 
 	<enum name="a6xx_layer_type">
@@ -1935,6 +1936,8 @@ to upconvert to 32b float internally?
 	<reg32 offset="0x809c" name="GRAS_UNKNOWN_809C">
 		<bitfield name="GS_WRITES_LAYER" pos="0" type="boolean"/>
 	</reg32>
+
+	<reg32 offset="0x809d" name="GRAS_UNKNOWN_809D"/>
 
 	<reg32 offset="0x80a0" name="GRAS_UNKNOWN_80A0"/>
 
@@ -2421,6 +2424,7 @@ to upconvert to 32b float internally?
 	<!-- always 0x00ffff00 ? */ -->
 	<reg32 offset="0x9101" name="VPC_UNKNOWN_9101"/>
 	<reg32 offset="0x9102" name="VPC_UNKNOWN_9102"/>
+	<reg32 offset="0x9103" name="VPC_UNKNOWN_9103"/>
 
 	<reg32 offset="0x9104" name="VPC_GS_SIV_CNTL"/>
 
@@ -2428,6 +2432,7 @@ to upconvert to 32b float internally?
 		<bitfield name="LAYERLOC" low="0" high="7" type="uint"/>
 	</reg32>
 
+	<reg32 offset="0x9106" name="VPC_UNKNOWN_9106"/>
 	<reg32 offset="0x9107" name="VPC_UNKNOWN_9107"/>
 	<reg32 offset="0x9108" name="VPC_UNKNOWN_9108"/>
 

src/gallium/drivers/freedreno/a6xx/fd6_emit.c
@@ -1338,7 +1338,6 @@ fd6_emit_restore(struct fd_batch *batch, struct fd_ringbuffer *ring)
 
 	WRITE(REG_A6XX_VPC_SO_OVERRIDE, A6XX_VPC_SO_OVERRIDE_SO_DISABLE);
 
-	WRITE(REG_A6XX_PC_UNKNOWN_9801, 0);
 	WRITE(REG_A6XX_PC_UNKNOWN_9806, 0);
 	WRITE(REG_A6XX_PC_UNKNOWN_9980, 0);
 

src/gallium/drivers/freedreno/a6xx/fd6_emit.h
@@ -245,6 +245,22 @@ fd6_stage2shadersb(gl_shader_stage type)
 	}
 }
 
+static inline enum a6xx_tess_spacing
+fd6_gl2spacing(enum gl_tess_spacing spacing)
+{
+	switch (spacing) {
+	case TESS_SPACING_EQUAL:
+		return TESS_EQUAL;
+	case TESS_SPACING_FRACTIONAL_ODD:
+		return TESS_FRACTIONAL_ODD;
+	case TESS_SPACING_FRACTIONAL_EVEN:
+		return TESS_FRACTIONAL_EVEN;
+	case TESS_SPACING_UNSPECIFIED:
+	default:
+		unreachable("spacing must be specified");
+	}
+}
+
 bool fd6_emit_textures(struct fd_pipe *pipe, struct fd_ringbuffer *ring,
 		enum pipe_shader_type type, struct fd_texture_stateobj *tex,
 		unsigned bcolor_offset,

commit 1625f8d50317007bfc467bff6be33dcdcc7090f8

freedreno/a6xx: Fix SP_HS_UNKNOWN_A831 value and document it
It appears that storage for varyings in a wave has an upper
limit of wavesize * max_a831 where max_a831 is 64.
Exceeding the limit seam to force gpu to reduce primitives
processed per wave, at least calculations make sense with
such interpretation.

With blob SP_HS_UNKNOWN_A831 never exceeds 64 and setting
it to 65 in freedreno leads to a hang.

On A630 tests (patch_size=3 + gl_Position + array of vec4)
have shown such relation:

| Num of vec4 | A831 | PC_HS_INPUT_SIZE |
|-------------|------|------------------|
| 1           | 0x10 | 0xc              |
| 2           | 0x14 | 0xf              |
| 3           | 0x18 | 0x12             |
| 4           | 0x1c | 0x15             |
| 5           | 0x20 | 0x18             |
| 6           | 0x24 | 0x1b             |
| 7           | 0x28 | 0x1e             |
| 8           | 0x2c | 0x21             |
| 9           | 0x30 | 0x24             |
| 10          | 0x34 | 0x27             |
| 11          | 0x38 | 0x2a             |
| 12          | 0x3c | 0x2d             |
| 13          | 0x3f | 0x30             |
| 14          | 0x40 | 0x33             |
| 15          | 0x3d | 0x36             |
| 16          | 0x3d | 0x39             |
| 17          | 0x40 | 0x3c             |
| 18          | 0x3f | 0x3f             |
| 19          | 0x3e | 0x42             |
| 20          | 0x3d | 0x45             |
| 21          | 0x3f | 0x48             |
| 22          | 0x3d | 0x4b             |
| 23          | 0x40 | 0x4e             |
| 24          | 0x3d | 0x51             |
| 25          | 0x3f | 0x54             |
| 26          | 0x3c | 0x57             |
| 27          | 0x3e | 0x5a             |
| 28          | 0x40 | 0x5d             |
| 29          | 0x3c | 0x60             |
| 30          | 0x3e | 0x63             |
| 31          | 0x40 | 0x66             |
|-------------|------|------------------|

Brief tests with high patch sizes also confirm that formula
matches blob behaviour.

A831 is not a limit for storage available for one thread, so
naming it as SP_HS_WAVE_INPUT_SIZE would make more sense.

Fixes: 6cac25e3 "freedreno/a6xx: Program state for tessellation stages"

src/freedreno/.gitlab-ci/reference/crash.log
@@ -7973,7 +7973,7 @@ clusters:
 	00000100	SP_VS_CONFIG: { ENABLED | NTEX = 0 | NSAMP = 0 | NIBO = 0 }
 	00000000	SP_VS_INSTRLEN: 0
 	00000000	SP_HS_CTRL_REG0: { HALFREGFOOTPRINT = 0 | FULLREGFOOTPRINT = 0 | BRANCHSTACK = 0 | THREADSIZE = TWO_QUADS }
-	00000000	SP_HS_UNKNOWN_A831: 0
+	00000000	SP_HS_WAVE_INPUT_SIZE: 0
 	00000000	0xa832: 00000000
 	00000000	SP_HS_OBJ_FIRST_EXEC_OFFSET: 0
 	780a8ca5	SP_HS_OBJ_START_LO: 0x780a8ca5
@@ -8119,7 +8119,7 @@ clusters:
 	00000100	SP_VS_CONFIG: { ENABLED | NTEX = 0 | NSAMP = 0 | NIBO = 0 }
 	00000000	SP_VS_INSTRLEN: 0
 	00000000	SP_HS_CTRL_REG0: { HALFREGFOOTPRINT = 0 | FULLREGFOOTPRINT = 0 | BRANCHSTACK = 0 | THREADSIZE = TWO_QUADS }
-	00000000	SP_HS_UNKNOWN_A831: 0
+	00000000	SP_HS_WAVE_INPUT_SIZE: 0
 	00000000	0xa832: 00000000
 	00000000	SP_HS_OBJ_FIRST_EXEC_OFFSET: 0
 	780a8ca5	SP_HS_OBJ_START_LO: 0x780a8ca5

src/freedreno/.gitlab-ci/reference/dEQP-VK.draw.indirect_draw.indexed.indirect_draw_count.triangle_list.log
@@ -948,8 +948,8 @@ t4					write SP_CS_CONFIG (a9bb)
 t4					write HLSQ_CS_CNTL (b987)
 						HLSQ_CS_CNTL: { CONSTLEN = 0 }
 0000000001054250:					0000: 40b98701 00000000
-t4					write SP_HS_UNKNOWN_A831 (a831)
-						SP_HS_UNKNOWN_A831: 0
+t4					write SP_HS_WAVE_INPUT_SIZE (a831)
+						SP_HS_WAVE_INPUT_SIZE: 0
 0000000001054258:					0000: 48a83101 00000000
 t4					write VFD_CONTROL_1 (a001)
 						VFD_CONTROL_1: { REGID4VTX = r2.y | REGID4INST = r63.x | REGID4PRIMID = r63.x | REGID4VIEWID = r63.x }
@@ -1511,7 +1511,7 @@ t7			opcode: CP_DRAW_INDIRECT_MULTI (2a) (12 dwords)
 				- shaderdb: 0 (ss), 0 (sy)
 !+	00000100			SP_VS_CONFIG: { ENABLED | NTEX = 0 | NSAMP = 0 | NIBO = 0 }
 !+	00000001			SP_VS_INSTRLEN: 1
- +	00000000			SP_HS_UNKNOWN_A831: 0
+ +	00000000			SP_HS_WAVE_INPUT_SIZE: 0
  +	00000000			SP_HS_CONFIG: { NTEX = 0 | NSAMP = 0 | NIBO = 0 }
  +	00000000			SP_DS_CONFIG: { NTEX = 0 | NSAMP = 0 | NIBO = 0 }
  +	00000000			SP_GS_CONFIG: { NTEX = 0 | NSAMP = 0 | NIBO = 0 }

src/freedreno/.gitlab-ci/reference/fd-clouds.log
@@ -681,8 +681,8 @@ t4					write SP_VS_OUT[0].REG (a803)
 t4					write SP_VS_VPC_DST[0].REG (a813)
 						SP_VS_VPC_DST[0].REG: { OUTLOC0 = 0 | OUTLOC1 = 0 | OUTLOC2 = 0 | OUTLOC3 = 0 }
 0000000001121070:					0000: 48a81301 00000000
-t4					write SP_HS_UNKNOWN_A831 (a831)
-						SP_HS_UNKNOWN_A831: 0
+t4					write SP_HS_WAVE_INPUT_SIZE (a831)
+						SP_HS_WAVE_INPUT_SIZE: 0
 0000000001121078:					0000: 48a83101 00000000
 t4					write SP_VS_PRIMITIVE_CNTL (a802)
 						SP_VS_PRIMITIVE_CNTL: { OUT = 1 }
@@ -1118,7 +1118,7 @@ t7			opcode: CP_DRAW_INDX_OFFSET (38) (4 dwords)
 				- shaderdb: 0 (ss), 0 (sy)
 !+	00000100			SP_VS_CONFIG: { ENABLED | NTEX = 0 | NSAMP = 0 | NIBO = 0 }
 !+	00000001			SP_VS_INSTRLEN: 1
- +	00000000			SP_HS_UNKNOWN_A831: 0
+ +	00000000			SP_HS_WAVE_INPUT_SIZE: 0
  +	00000000			SP_HS_OBJ_FIRST_EXEC_OFFSET: 0
  +	00000000			SP_HS_CONFIG: { NTEX = 0 | NSAMP = 0 | NIBO = 0 }
  +	00000000			SP_DS_CONFIG: { NTEX = 0 | NSAMP = 0 | NIBO = 0 }
@@ -1996,8 +1996,8 @@ t4					write SP_VS_OUT[0].REG (a803)
 t4					write SP_VS_VPC_DST[0].REG (a813)
 						SP_VS_VPC_DST[0].REG: { OUTLOC0 = 0 | OUTLOC1 = 0 | OUTLOC2 = 0 | OUTLOC3 = 0 }
 0000000001120070:					0000: 48a81301 00000000
-t4					write SP_HS_UNKNOWN_A831 (a831)
-						SP_HS_UNKNOWN_A831: 0
+t4					write SP_HS_WAVE_INPUT_SIZE (a831)
+						SP_HS_WAVE_INPUT_SIZE: 0
 0000000001120078:					0000: 48a83101 00000000
 t4					write SP_VS_PRIMITIVE_CNTL (a802)
 						SP_VS_PRIMITIVE_CNTL: { OUT = 1 }
@@ -5343,7 +5343,7 @@ t7			opcode: CP_DRAW_INDX_OFFSET (38) (4 dwords)
 				- shaderdb: 0 (ss), 0 (sy)
  +	00000100			SP_VS_CONFIG: { ENABLED | NTEX = 0 | NSAMP = 0 | NIBO = 0 }
  +	00000001			SP_VS_INSTRLEN: 1
- +	00000000			SP_HS_UNKNOWN_A831: 0
+ +	00000000			SP_HS_WAVE_INPUT_SIZE: 0
  +	00000000			SP_HS_OBJ_FIRST_EXEC_OFFSET: 0
  +	00000000			SP_HS_CONFIG: { NTEX = 0 | NSAMP = 0 | NIBO = 0 }
  +	00000000			SP_DS_CONFIG: { NTEX = 0 | NSAMP = 0 | NIBO = 0 }

src/freedreno/registers/adreno/a6xx.xml
@@ -3220,7 +3220,13 @@ to upconvert to 32b float internally?
 	<reg32 offset="0xa825" name="SP_VS_PVT_MEM_HW_STACK_OFFSET" low="0" high="18" shr="11"/>
 
 	<reg32 offset="0xa830" name="SP_HS_CTRL_REG0" type="a6xx_sp_xs_ctrl_reg0"/>
-	<reg32 offset="0xa831" name="SP_HS_UNKNOWN_A831"/>
+	<!--
+	Total size of local storage in dwords divided by the wave size.
+	The maximum value is 64. With the wave size being always 64 for HS,
+	the maximum size of local storage should be:
+	 64 (wavesize) * 64 (SP_HS_WAVE_INPUT_SIZE) * 4 = 16k
+	-->
+	<reg32 offset="0xa831" name="SP_HS_WAVE_INPUT_SIZE" low="0" high="7" type="uint"/>
 
 	<reg32 offset="0xa833" name="SP_HS_OBJ_FIRST_EXEC_OFFSET" type="uint"/>
 	<reg32 offset="0xa834" name="SP_HS_OBJ_START_LO"/>

src/freedreno/vulkan/tu_pipeline.c
@@ -1058,7 +1058,7 @@ tu6_emit_vpc(struct tu_cs *cs,
          unknown_a831 = DIV_ROUND_UP(total_size, wavesize);
       }
 
-      tu_cs_emit_pkt4(cs, REG_A6XX_SP_HS_UNKNOWN_A831, 1);
+      tu_cs_emit_pkt4(cs, REG_A6XX_SP_HS_WAVE_INPUT_SIZE, 1);
       tu_cs_emit(cs, unknown_a831);
 
       /* In SPIR-V generated from GLSL, the tessellation primitive params are
@@ -1563,7 +1563,7 @@ tu6_emit_program(struct tu_cs *cs,
       tu_cs_emit(cs, builder->multiview_mask);
    }
 
-   tu_cs_emit_pkt4(cs, REG_A6XX_SP_HS_UNKNOWN_A831, 1);
+   tu_cs_emit_pkt4(cs, REG_A6XX_SP_HS_WAVE_INPUT_SIZE, 1);
    tu_cs_emit(cs, 0);
 
    tu6_emit_vpc(cs, vs, hs, ds, gs, fs, cps_per_patch,

src/gallium/drivers/freedreno/a6xx/fd6_program.c
@@ -654,8 +654,24 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_context *ctx,
 		OUT_PKT4(ring, REG_A6XX_PC_HS_INPUT_SIZE, 1);
 		OUT_RING(ring, hs_info->tess.tcs_vertices_out * vs->output_size / 4);
 
-		OUT_PKT4(ring, REG_A6XX_SP_HS_UNKNOWN_A831, 1);
-		OUT_RING(ring, vs->output_size);
+		const uint32_t wavesize = 64;
+		const uint32_t max_wave_input_size = 64;
+		const uint32_t patch_control_points = hs_info->tess.tcs_vertices_out;
+
+		/* note: if HS is really just the VS extended, then this
+		 * should be by MAX2(patch_control_points, hs_info->tess.tcs_vertices_out)
+		 * however that doesn't match the blob, and fails some dEQP tests.
+		 */
+		uint32_t prims_per_wave = wavesize / hs_info->tess.tcs_vertices_out;
+		uint32_t max_prims_per_wave =
+			max_wave_input_size * wavesize / (vs->output_size * patch_control_points);
+		prims_per_wave = MIN2(prims_per_wave, max_prims_per_wave);
+
+		uint32_t total_size = vs->output_size * patch_control_points * prims_per_wave;
+		uint32_t wave_input_size = DIV_ROUND_UP(total_size, wavesize);
+
+		OUT_PKT4(ring, REG_A6XX_SP_HS_WAVE_INPUT_SIZE, 1);
+		OUT_RING(ring, wave_input_size);
 
 		shader_info *ds_info = &ds->shader->nir->info;
 		OUT_PKT4(ring, REG_A6XX_PC_TESS_CNTL, 1);
@@ -706,7 +722,7 @@ setup_stateobj(struct fd_ringbuffer *ring, struct fd_context *ctx,
 				A6XX_PC_DS_OUT_CNTL_CLIP_MASK(clip_cull_mask));
 
 	} else {
-		OUT_PKT4(ring, REG_A6XX_SP_HS_UNKNOWN_A831, 1);
+		OUT_PKT4(ring, REG_A6XX_SP_HS_WAVE_INPUT_SIZE, 1);
 		OUT_RING(ring, 0);
 	}
 