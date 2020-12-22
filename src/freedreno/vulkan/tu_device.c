openharmony/third_party_mesa3d

commit 5123372be51ba19c54010cd3631f86d2dbd6fa83

freedreno/ir3: add ir3_compiler_destroy()
Use ir3_compiler_destroy() rather than open-coding ralloc_free().  This
will give us a place to add more compiler related cleanup code in the
following patches.

Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5372>

src/freedreno/vulkan/tu_device.c
@@ -1346,8 +1346,7 @@ tu_DestroyDevice(VkDevice _device, const VkAllocationCallbacks *pAllocator)
          tu_bo_finish(device, &device->scratch_bos[i].bo);
    }
 
-   /* the compiler does not use pAllocator */
-   ralloc_free(device->compiler);
+   ir3_compiler_destroy(device->compiler);
 
    VkPipelineCache pc = tu_pipeline_cache_to_handle(device->mem_cache);
    tu_DestroyPipelineCache(tu_device_to_handle(device), pc, NULL);

src/freedreno/ir3/ir3_compiler.c
@@ -52,7 +52,14 @@ DEBUG_GET_ONCE_FLAGS_OPTION(ir3_shader_debug, "IR3_SHADER_DEBUG", shader_debug_o
 
 enum ir3_shader_debug ir3_shader_debug = 0;
 
-struct ir3_compiler * ir3_compiler_create(struct fd_device *dev, uint32_t gpu_id)
+void
+ir3_compiler_destroy(struct ir3_compiler *compiler)
+{
+	ralloc_free(compiler);
+}
+
+struct ir3_compiler *
+ir3_compiler_create(struct fd_device *dev, uint32_t gpu_id)
 {
 	struct ir3_compiler *compiler = rzalloc(NULL, struct ir3_compiler);
 

src/freedreno/ir3/ir3_compiler.h
@@ -95,6 +95,7 @@ struct ir3_compiler {
 	uint32_t const_upload_unit;
 };
 
+void ir3_compiler_destroy(struct ir3_compiler *compiler);
 struct ir3_compiler * ir3_compiler_create(struct fd_device *dev, uint32_t gpu_id);
 
 int ir3_compile_shader_nir(struct ir3_compiler *compiler,

src/gallium/drivers/freedreno/freedreno_screen.c
@@ -59,6 +59,7 @@
 
 
 #include "ir3/ir3_nir.h"
+#include "ir3/ir3_compiler.h"
 #include "a2xx/ir2.h"
 
 static const struct debug_named_value debug_options[] = {
@@ -159,7 +160,9 @@ fd_screen_destroy(struct pipe_screen *pscreen)
 
 	simple_mtx_destroy(&screen->lock);
 
-	ralloc_free(screen->compiler);
+	if (screen->compiler)
+		ir3_compiler_destroy(screen->compiler);
+
 	ralloc_free(screen->live_batches);
 
 	free(screen->perfcntr_queries);

commit 33416e8369d208087ec6cc0a315d9df7e622cf01

turnip: use ir3_compiler_destroy instead of ralloc_free
Fixes: 5123372be51ba19 "freedreno/ir3: add ir3_compiler_destroy()"

Signed-off-by: Hyunjun Ko <zzoon@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6808>

src/freedreno/vulkan/tu_device.c
@@ -1217,7 +1217,7 @@ fail_global_bo_map:
    tu_bo_finish(device, &device->global_bo);
 
 fail_global_bo:
-   ralloc_free(device->compiler);
+   ir3_compiler_destroy(device->compiler);
 
 fail_queues:
    for (unsigned i = 0; i < TU_MAX_QUEUE_FAMILIES; i++) {