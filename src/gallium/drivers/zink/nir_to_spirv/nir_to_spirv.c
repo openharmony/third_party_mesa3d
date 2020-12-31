openharmony/third_party_mesa3d

commit 1a2a4027b4b0c7c02a2e6cd4ec4d70f1b6c83ff2

zink: add ntv handling for tess shader i/o variables
Reviewed-by: Erik Faye-Lund <kusmabite@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8152>

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -304,6 +304,19 @@ handle_slot(struct ntv_context *ctx, unsigned slot)
          break
 
 
+static inline unsigned
+handle_handle_slot(struct ntv_context *ctx, struct nir_variable *var)
+{
+   if (var->data.patch) {
+      assert(var->data.location >= VARYING_SLOT_PATCH0);
+      return var->data.location - VARYING_SLOT_PATCH0;
+   } else if (ctx->stage == MESA_SHADER_TESS_CTRL) {
+      assert(var->data.location >= VARYING_SLOT_VAR0);
+      return var->data.location - VARYING_SLOT_VAR0;
+   }
+   return handle_slot(ctx, var->data.location);
+}
+
 static void
 emit_input(struct ntv_context *ctx, struct nir_variable *var)
 {
@@ -357,7 +370,7 @@ emit_input(struct ntv_context *ctx, struct nir_variable *var)
          break;
 
       default:
-         slot = handle_slot(ctx, slot);
+         slot = handle_handle_slot(ctx, var);
          spirv_builder_emit_location(&ctx->builder, var_id, slot);
       }
    }
@@ -365,6 +378,9 @@ emit_input(struct ntv_context *ctx, struct nir_variable *var)
    if (var->data.location_frac)
       spirv_builder_emit_component(&ctx->builder, var_id,
                                    var->data.location_frac);
+
+   if (var->data.patch)
+      spirv_builder_emit_decoration(&ctx->builder, var_id, SpvDecorationPatch);
 
    if (var->data.interpolation == INTERP_MODE_FLAT)
       spirv_builder_emit_decoration(&ctx->builder, var_id, SpvDecorationFlat);
@@ -413,12 +429,15 @@ emit_output(struct ntv_context *ctx, struct nir_variable *var)
          break;
 
       default:
-         slot = handle_slot(ctx, slot);
+         slot = handle_handle_slot(ctx, var);
          spirv_builder_emit_location(&ctx->builder, var_id, slot);
       }
-      ctx->outputs[var->data.location] = var_id;
-      ctx->so_output_gl_types[var->data.location] = var->type;
-      ctx->so_output_types[var->data.location] = var_type;
+      /* tcs can't do xfb */
+      if (ctx->stage != MESA_SHADER_TESS_CTRL) {
+         ctx->outputs[var->data.location] = var_id;
+         ctx->so_output_gl_types[var->data.location] = var->type;
+         ctx->so_output_types[var->data.location] = var_type;
+      }
    } else {
       if (var->data.location >= FRAG_RESULT_DATA0) {
          spirv_builder_emit_location(&ctx->builder, var_id,
@@ -467,6 +486,9 @@ emit_output(struct ntv_context *ctx, struct nir_variable *var)
    default:
       unreachable("unknown interpolation value");
    }
+
+   if (var->data.patch)
+      spirv_builder_emit_decoration(&ctx->builder, var_id, SpvDecorationPatch);
 
    _mesa_hash_table_insert(ctx->vars, var, (void *)(intptr_t)var_id);
 

commit 945ad28521aae118d3ef00d666499e6b49d02844

zink: fix tess shader i/o variables
this was a mismerge during refactoring for review

Fixes: 1a2a4027b4b ("zink: add ntv handling for tess shader i/o variables")

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8232>

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -309,12 +309,13 @@ handle_slot(struct ntv_context *ctx, unsigned slot)
 
 
 static inline unsigned
-handle_handle_slot(struct ntv_context *ctx, struct nir_variable *var)
+handle_handle_slot(struct ntv_context *ctx, struct nir_variable *var, bool output)
 {
    if (var->data.patch) {
       assert(var->data.location >= VARYING_SLOT_PATCH0);
       return var->data.location - VARYING_SLOT_PATCH0;
-   } else if (ctx->stage == MESA_SHADER_TESS_CTRL) {
+   } else if ((output && ctx->stage == MESA_SHADER_TESS_CTRL) ||
+              (!output && ctx->stage == MESA_SHADER_TESS_EVAL)) {
       assert(var->data.location >= VARYING_SLOT_VAR0);
       return var->data.location - VARYING_SLOT_VAR0;
    }
@@ -383,7 +384,7 @@ emit_input(struct ntv_context *ctx, struct nir_variable *var)
          break;
 
       default:
-         slot = handle_handle_slot(ctx, var);
+         slot = handle_handle_slot(ctx, var, false);
          spirv_builder_emit_location(&ctx->builder, var_id, slot);
       }
    }
@@ -442,7 +443,7 @@ emit_output(struct ntv_context *ctx, struct nir_variable *var)
          break;
 
       default:
-         slot = handle_handle_slot(ctx, var);
+         slot = handle_handle_slot(ctx, var, true);
          spirv_builder_emit_location(&ctx->builder, var_id, slot);
       }
       /* tcs can't do xfb */