openharmony/third_party_mesa3d

commit 9bbd6c4f6f009b30a4206de6143134297bb15d3f

pan/mdg: Explicitly type 64-bit uniform moves
Instead of relying on reg_mode.

Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5443>

src/panfrost/midgard/mir_promote_uniforms.c
@@ -194,6 +194,8 @@ midgard_promote_uniforms(compiler_context *ctx)
                 if (needs_move) {
                         unsigned type_size = nir_alu_type_get_type_size(ins->dest_type);
                         midgard_instruction mov = v_mov(promoted, ins->dest);
+                        mov.dest_type = nir_type_uint | type_size;
+                        mov.src_types[0] = mov.dest_type;
 
                         if (type_size == 64)
                                 mov.alu.reg_mode = midgard_reg_mode_64;

commit 74e87266777acf9d96e8deb37b277bb78264d603

pan/mdg: Fix promoted uniform moves with 64-bit types
The move source is the second src, not the first one.

Fixes: 9bbd6c4f6f0 ("pan/mdg: Explicitly type 64-bit uniform moves")
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8264>

src/panfrost/midgard/mir_promote_uniforms.c
@@ -195,7 +195,7 @@ midgard_promote_uniforms(compiler_context *ctx)
                         unsigned type_size = nir_alu_type_get_type_size(ins->dest_type);
                         midgard_instruction mov = v_mov(promoted, ins->dest);
                         mov.dest_type = nir_type_uint | type_size;
-                        mov.src_types[0] = mov.dest_type;
+                        mov.src_types[1] = mov.dest_type;
 
                         uint16_t rounded = mir_round_bytemask_up(mir_bytemask(ins), type_size);
                         mir_set_bytemask(&mov, rounded);