openharmony/third_party_mesa3d

commit 3f8eec87dc849c9082447c28bf6882614137dfe1

radv: avoid oob read during clear
when clearing a depth/stencil image the passed colorvalue pointer is
smaller than the VkClearValue struct size

Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8288>

src/amd/vulkan/radv_meta_clear.c
@@ -2204,7 +2204,12 @@ radv_cmd_clear_image(struct radv_cmd_buffer *cmd_buffer,
 		     bool cs)
 {
 	VkFormat format = image->vk_format;
-	VkClearValue internal_clear_value = *clear_value;
+	VkClearValue internal_clear_value;
+
+        if (image->aspects & VK_IMAGE_ASPECT_COLOR_BIT)
+                internal_clear_value.color = clear_value->color;
+        else
+                internal_clear_value.depthStencil = clear_value->depthStencil;
 
 	if (format == VK_FORMAT_E5B9G9R9_UFLOAT_PACK32) {
 		uint32_t value;

commit 93ddf69386f8bbfc2154d8fdbaf6d1c07c434b6e

radv: fix clearing images with vkCmdClear{Color,DepthStencil}Image()
The image aspects field is actually never set and we should use the
range aspect anyways.

Fixes: 3f8eec87dc8 ("radv: avoid oob read during clear")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8324>

src/amd/vulkan/radv_meta_clear.c
@@ -2206,7 +2206,7 @@ radv_cmd_clear_image(struct radv_cmd_buffer *cmd_buffer,
 	VkFormat format = image->vk_format;
 	VkClearValue internal_clear_value;
 
-        if (image->aspects & VK_IMAGE_ASPECT_COLOR_BIT)
+        if (ranges->aspectMask & VK_IMAGE_ASPECT_COLOR_BIT)
                 internal_clear_value.color = clear_value->color;
         else
                 internal_clear_value.depthStencil = clear_value->depthStencil;