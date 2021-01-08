openharmony/third_party_mesa3d

commit 7a987ed2f406cab49b8b2b6e48fdd0115b9da709

radv: fix TC-compat HTILE images with DST_OPTIMAL on the compute queue
This is probably rare but can happen if someone performs a depth-stencil
copy on the compute queue. This might work (untested by CTS) but it
looks more conservative to decompress before perfoming the operation.

Found by inspection.

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8039>

src/amd/vulkan/radv_image.c
@@ -1764,6 +1764,11 @@ bool radv_layout_is_htile_compressed(const struct radv_image *image,
 			return true;
 		}
 
+		if ((layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL ||
+		     layout == VK_IMAGE_LAYOUT_GENERAL) &&
+		    (queue_mask & (1u << RADV_QUEUE_COMPUTE)))
+			return false;
+
 		return layout != VK_IMAGE_LAYOUT_GENERAL;
 	}
 

commit 633c86a424c81565b370121a3feff8e7619dfe99

radv: fix performance regression by restoring TC-compat HTILE in GENERAL
This fixes a performance regression for games (eg. Youngblood) that
declare all images as concurrent. This is likely buggy for compute
queues but this just restores the previous behaviour for now.

Fixes: 7a987ed2f40 ("radv: fix TC-compat HTILE images with DST_OPTIMAL on the compute queue")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8351>

src/amd/vulkan/radv_image.c
@@ -1774,8 +1774,11 @@ bool radv_layout_is_htile_compressed(const struct radv_device *device,
 		 * depth pass because this allows compression and this reduces
 		 * the number of decompressions from/to GENERAL.
 		 */
+		/* FIXME: Enabling TC-compat HTILE in GENERAL on the compute
+		 * queue is likely broken for eg. depth/stencil copies.
+		 */
 		if (radv_image_is_tc_compat_htile(image) &&
-		    queue_mask == (1u << RADV_QUEUE_GENERAL) &&
+		    queue_mask & (1u << RADV_QUEUE_GENERAL) &&
 		    !in_render_loop) {
 			/* GFX10+ supports compressed writes to HTILE. */
 			return device->physical_device->rad_info.chip_class >= GFX10 ||