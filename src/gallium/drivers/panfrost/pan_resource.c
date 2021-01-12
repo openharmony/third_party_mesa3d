openharmony/third_party_mesa3d

commit 920b8f7f065bfbac3da8dacca7c1d811727d0b1d

panfrost: Use panfrost_get_layer_stride() instead of open-coding it
If we don't do that we don't account for CRC buffers.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8125>

src/gallium/drivers/panfrost/pan_resource.c
@@ -882,7 +882,8 @@ panfrost_ptr_map(struct pipe_context *pctx,
         if (drm_is_afbc(rsrc->layout.modifier)) {
                 struct panfrost_resource *staging = pan_alloc_staging(ctx, rsrc, level, box);
                 transfer->base.stride = staging->layout.slices[0].line_stride;
-                transfer->base.layer_stride = transfer->base.stride * box->height;
+                transfer->base.layer_stride =
+                        panfrost_get_layer_stride(&staging->layout, level);
 
                 transfer->staging.rsrc = &staging->base;
 

commit 5f0ee4b1fe8c687213cce8c6c326470148807a07

panfrost: Get layer stride of level 0 on staging resources
Staging resources have one level, we shouldn't query the stride of
level > 0.

Fixes: 920b8f7f065b ("panfrost: Use panfrost_get_layer_stride() instead of open-coding it")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8449>

src/gallium/drivers/panfrost/pan_resource.c
@@ -929,9 +929,13 @@ panfrost_ptr_map(struct pipe_context *pctx,
         /* We don't have s/w routines for AFBC, so use a staging texture */
         if (drm_is_afbc(rsrc->layout.modifier)) {
                 struct panfrost_resource *staging = pan_alloc_staging(ctx, rsrc, level, box);
+
+                /* Staging resources have one LOD: level 0. Query the strides
+                 * on this LOD.
+                 */
                 transfer->base.stride = staging->layout.slices[0].line_stride;
                 transfer->base.layer_stride =
-                        panfrost_get_layer_stride(&staging->layout, level);
+                        panfrost_get_layer_stride(&staging->layout, 0);
 
                 transfer->staging.rsrc = &staging->base;
 