openharmony/third_party_mesa3d

commit 5e758a216656f85e0cfa38220e4effbc1793deac

frontends/va: Derive image from interlaced buffers
Allow vaDriveImage to derive a vaImage from interlaced buffers
by creating a new progressive buffer.

v2: Keeps the surface used by DeriveImage untouched (Pierre-Eric)
v3: Fixed a segfault reported by Roman Elshin

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/1428
Signed-off-by: Thong Thai <thong.thai@amd.com>
Reviewed-by: Leo Liu <leo.liu@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5942>

src/gallium/frontends/va/image.c
@@ -201,6 +201,7 @@ vlVaDeriveImage(VADriverContextP ctx, VASurfaceID surface, VAImage *image)
    VAImage *img;
    struct pipe_screen *screen;
    struct pipe_surface **surfaces;
+   struct pipe_video_buffer *new_buffer = NULL;
    int w;
    int h;
    int i;
@@ -224,9 +225,6 @@ vlVaDeriveImage(VADriverContextP ctx, VASurfaceID surface, VAImage *image)
 
    if (!surf || !surf->buffer)
       return VA_STATUS_ERROR_INVALID_SURFACE;
-
-   if (surf->buffer->interlaced)
-     return VA_STATUS_ERROR_OPERATION_FAILED;
 
    surfaces = surf->buffer->get_surfaces(surf->buffer);
    if (!surfaces || !surfaces[0]->texture)
@@ -285,6 +283,38 @@ vlVaDeriveImage(VADriverContextP ctx, VASurfaceID surface, VAImage *image)
    case VA_FOURCC('N','V','1','2'):
    case VA_FOURCC('P','0','1','0'):
    case VA_FOURCC('P','0','1','6'):
+      if (surf->buffer->interlaced) {
+         struct u_rect src_rect, dst_rect;
+         struct pipe_video_buffer new_template;
+
+         new_template = surf->templat;
+         new_template.interlaced = false;
+         new_buffer = drv->pipe->create_video_buffer(drv->pipe, &new_template);
+
+         /* convert the interlaced to the progressive */
+         src_rect.x0 = dst_rect.x0 = 0;
+         src_rect.x1 = dst_rect.x1 = surf->templat.width;
+         src_rect.y0 = dst_rect.y0 = 0;
+         src_rect.y1 = dst_rect.y1 = surf->templat.height;
+
+         vl_compositor_yuv_deint_full(&drv->cstate, &drv->compositor,
+                           surf->buffer, new_buffer,
+                           &src_rect, &dst_rect,
+                           VL_COMPOSITOR_WEAVE);
+
+         /* recalculate the values now that we have a new surface */
+         surfaces = surf->buffer->get_surfaces(new_buffer);
+         if (screen->resource_get_info) {
+            screen->resource_get_info(screen, surfaces[0]->texture, &stride,
+                                    &offset);
+            if (!stride)
+               offset = 0;
+         }
+
+         w = align(new_buffer->width, 2);
+         h = align(new_buffer->height, 2);
+      }
+
       img->num_planes = 2;
       img->pitches[0] = stride > 0 ? stride : w;
       img->pitches[1] = stride > 0 ? stride : w;
@@ -314,6 +344,7 @@ vlVaDeriveImage(VADriverContextP ctx, VASurfaceID surface, VAImage *image)
    img_buf->num_elements = 1;
 
    pipe_resource_reference(&img_buf->derived_surface.resource, surfaces[0]->texture);
+   img_buf->derived_image_buffer = new_buffer;
 
    img->buf = handle_table_add(VL_VA_DRIVER(ctx)->htab, img_buf);
    mtx_unlock(&drv->mutex);

src/gallium/frontends/va/buffer.c
@@ -204,8 +204,12 @@ vlVaDestroyBuffer(VADriverContextP ctx, VABufferID buf_id)
       return VA_STATUS_ERROR_INVALID_BUFFER;
    }
 
-   if (buf->derived_surface.resource)
+   if (buf->derived_surface.resource) {
       pipe_resource_reference(&buf->derived_surface.resource, NULL);
+
+      if (buf->derived_image_buffer)
+         buf->derived_image_buffer->destroy(buf->derived_image_buffer);
+   }
 
    FREE(buf->data);
    FREE(buf);

src/gallium/frontends/va/va_private.h
@@ -267,6 +267,7 @@ typedef struct {
    unsigned int export_refcount;
    VABufferInfo export_state;
    unsigned int coded_size;
+   struct pipe_video_buffer *derived_image_buffer;
 } vlVaBuffer;
 
 typedef struct {

commit be81e4ca19d55c7462b8a1115bd2d024bd33d0fe

frontends/va: Return an error if non-interlaced buffer is not supported
Add a check to vaDeriveImage to see if a non-interlaced buffer was
created successfully. Otherwise, return an error, since we won't be able
to derive an image from the interlaced buffer.

Prevents a null pointer dereference from occuring on some nVidia cards,
reported by Alexander Kapshuk.

v2: Check for PIPE_VIDEO_CAP_SUPPORTS_PROGRESSIVE support (Ilia)

Fixes: 5e758a21665 ("frontends/va: Derive image from interlaced buffers")
Signed-off-by: Thong Thai <thong.thai@amd.com>
Tested-by: Alexander Kapshuk <alexander.kapshuk@gmail.com>
Reviewed-by: Leo Liu <leo.liu@amd.com>
Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8320>

src/gallium/frontends/va/image.c
@@ -212,8 +212,8 @@ vlVaDeriveImage(VADriverContextP ctx, VASurfaceID surface, VAImage *image)
    /* This function is used by some programs to test for hardware decoding, but on
     * AMD devices, the buffers default to interlaced, which causes this function to fail.
     * Some programs expect this function to fail, while others, assume this means
-    * hardware acceleration is not available and give up without trying the fall-back 
-    * vaCreateImage + vaPutImage 
+    * hardware acceleration is not available and give up without trying the fall-back
+    * vaCreateImage + vaPutImage
     */
    const char *proc = util_get_process_name();
    const char *derive_interlaced_allowlist[] = {
@@ -244,6 +244,10 @@ vlVaDeriveImage(VADriverContextP ctx, VASurfaceID surface, VAImage *image)
             break;
 
       if (i >= ARRAY_SIZE(derive_interlaced_allowlist))
+         return VA_STATUS_ERROR_OPERATION_FAILED;
+
+      if (!screen->get_video_param(screen, PIPE_VIDEO_PROFILE_UNKNOWN, PIPE_VIDEO_ENTRYPOINT_BITSTREAM,
+                                   PIPE_VIDEO_CAP_SUPPORTS_PROGRESSIVE))
          return VA_STATUS_ERROR_OPERATION_FAILED;
    }
 
@@ -311,6 +315,13 @@ vlVaDeriveImage(VADriverContextP ctx, VASurfaceID surface, VAImage *image)
          new_template = surf->templat;
          new_template.interlaced = false;
          new_buffer = drv->pipe->create_video_buffer(drv->pipe, &new_template);
+
+         /* not all devices support non-interlaced buffers */
+         if (!new_buffer) {
+            FREE(img);
+            mtx_unlock(&drv->mutex);
+            return VA_STATUS_ERROR_OPERATION_FAILED;
+         }
 
          /* convert the interlaced to the progressive */
          src_rect.x0 = dst_rect.x0 = 0;