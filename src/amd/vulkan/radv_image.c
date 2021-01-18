openharmony/third_party_mesa3d

commit 9d2eff2272cc52f65c1b65dd78309717e50876ac

radv: Create sparse images.
Disable all metadata for now.

Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7953>

src/amd/vulkan/radv_image.c
@@ -476,6 +476,13 @@ radv_get_surface_flags(struct radv_device *device,
 	if (!radv_use_fmask_for_image(device, image))
 		flags |= RADEON_SURF_NO_FMASK;
 
+	if (pCreateInfo->flags & VK_IMAGE_CREATE_SPARSE_RESIDENCY_BIT) {
+		flags |= RADEON_SURF_PRT |
+		         RADEON_SURF_NO_FMASK |
+		         RADEON_SURF_NO_HTILE |
+		         RADEON_SURF_DISABLE_DCC;
+	}
+
 	return flags;
 }
 
@@ -1246,7 +1253,8 @@ radv_image_alloc_single_sample_cmask(const struct radv_device *device,
 	if (!surf->cmask_size || surf->cmask_offset || surf->bpe > 8 ||
 	    image->info.levels > 1 || image->info.depth > 1 ||
 	    radv_image_has_dcc(image) ||
-	    !radv_image_use_fast_clear_for_image(device, image))
+	    !radv_image_use_fast_clear_for_image(device, image) ||
+	    (image->flags & VK_IMAGE_CREATE_SPARSE_BINDING_BIT))
 		return;
 
 	assert(image->info.storage_samples == 1);

commit ab1229cd607d1f2ecee5b62ef7396d14486f7343

radv: Do not use a pipe offset for aliased sparse images.
Otherwise the offset might not match between the images that are
aliased.

Fixes: 9d2eff2272c ("radv: Create sparse images.")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4072
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8535>

src/amd/vulkan/radv_image.c
@@ -1520,7 +1520,8 @@ radv_image_create(VkDevice _device,
 		                     EXTERNAL_MEMORY_IMAGE_CREATE_INFO) ;
 
 	image->shareable = external_info;
-	if (!vk_format_is_depth_or_stencil(format) && !image->shareable) {
+	if (!vk_format_is_depth_or_stencil(format) && !image->shareable &&
+	    !(image->flags & VK_IMAGE_CREATE_SPARSE_ALIASED_BIT)) {
 		image->info.surf_index = &device->image_mrt_offset_counter;
 	}
 