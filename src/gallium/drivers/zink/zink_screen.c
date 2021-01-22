openharmony/third_party_mesa3d

commit d4a2c4059b795d55cc533992e035c90a4153f9a8

zink: enable PIPE_CAP_MULTI_DRAW_INDIRECT(_PARAMS) caps
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8233>

src/gallium/drivers/zink/zink_screen.c
@@ -105,6 +105,11 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_GLSL_TESS_LEVELS_AS_INPUTS:
       return 1;
 
+   case PIPE_CAP_MULTI_DRAW_INDIRECT:
+   case PIPE_CAP_MULTI_DRAW_INDIRECT_PARAMS:
+      return screen->vk_CmdDrawIndirectCount &&
+             screen->vk_CmdDrawIndexedIndirectCount;
+
    case PIPE_CAP_VERTEX_ELEMENT_INSTANCE_DIVISOR:
       return screen->info.have_EXT_vertex_attribute_divisor;
 
@@ -839,7 +844,7 @@ load_instance_extensions(struct zink_screen *screen)
       GET_PROC_ADDR_INSTANCE_LOCAL(screen->instance, CmdDrawIndexedIndirectCountKHR);
       screen->vk_CmdDrawIndirectCount = vk_CmdDrawIndirectCountKHR;
       screen->vk_CmdDrawIndexedIndirectCount = vk_CmdDrawIndexedIndirectCountKHR;
-   } else if (VK_MAKE_VERSION(1,1,0) <= screen->loader_version) {
+   } else if (VK_MAKE_VERSION(1,2,0) <= screen->loader_version) {
       // Get Vk 1.1+ Instance functions
       GET_PROC_ADDR_INSTANCE(CmdDrawIndirectCount);
       GET_PROC_ADDR_INSTANCE(CmdDrawIndexedIndirectCount);

commit 6f394e0df6382ad93b1346d8235967c02202fcab

zink: respect feature-cap for multi-draw indirect
Even in the presence of VK_KHR_draw_indirect_count, we still technically
need to respect the feature-cap when using a Vk 1.0 core-function.

Fixes: d4a2c4059b7 ("zink: enable PIPE_CAP_MULTI_DRAW_INDIRECT(_PARAMS) caps")
Reviewed-by: Hoe Hao Cheng <haochengho12907@gmail.com>
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8640>

src/gallium/drivers/zink/zink_screen.c
@@ -117,6 +117,8 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
       return screen->info.feats.features.robustBufferAccess;
 
    case PIPE_CAP_MULTI_DRAW_INDIRECT:
+      return screen->info.feats.features.multiDrawIndirect;
+
    case PIPE_CAP_MULTI_DRAW_INDIRECT_PARAMS:
       return screen->vk_CmdDrawIndirectCount &&
              screen->vk_CmdDrawIndexedIndirectCount;