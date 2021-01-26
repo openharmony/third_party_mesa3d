openharmony/third_party_mesa3d

commit 2fda4e0b693397071f8f1d43bb68f77f367fef53

spirv: Remove more dead variables
SPIR-V modules can have multiple shaders (including of the same
stage), but the global variables are all declared for the whole
module.  This can result in variables with same Binding but
incompatible types, so those need to be removed before we use.

Previously, a similar issue but with a narrower scope was fixed by
e3bf7334f23 ("spirv: Eliminate dead input/output variables after
translation.").

This patch depends on the previous patch that prevents variables used
only in pointer initializers to be considered dead.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3686
Fixes: 2ccc278f ("nir/spirv: Add support for declaring variables")
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8133>

src/compiler/spirv/spirv_to_nir.c
@@ -5842,19 +5842,23 @@ spirv_to_nir(const uint32_t *words, size_t word_count,
    /* structurize the CFG */
    nir_lower_goto_ifs(b->shader);
 
-   /* When multiple shader stages exist in the same SPIR-V module, we
-    * generate input and output variables for every stage, in the same
-    * NIR program.  These dead variables can be invalid NIR.  For example,
-    * TCS outputs must be per-vertex arrays (or decorated 'patch'), while
-    * VS output variables wouldn't be.
+   /* A SPIR-V module can have multiple shaders stages and also multiple
+    * shaders of the same stage.  Global variables are declared per-module, so
+    * they are all collected when parsing a single shader.  These dead
+    * variables can result in invalid NIR, e.g.
     *
-    * To ensure we have valid NIR, we eliminate any dead inputs and outputs
-    * right away.  In order to do so, we must lower any constant initializers
-    * on outputs so nir_remove_dead_variables sees that they're written to.
+    * - TCS outputs must be per-vertex arrays (or decorated 'patch'), while VS
+    *   output variables wouldn't be;
+    * - Two vertex shaders have two different typed blocks associated to the
+    *   same Binding.
+    *
+    * Before cleaning the dead variables, we must lower any constant
+    * initializers on outputs so nir_remove_dead_variables sees that they're
+    * written to.
     */
-   nir_lower_variable_initializers(b->shader, nir_var_shader_out);
-   nir_remove_dead_variables(b->shader,
-                             nir_var_shader_in | nir_var_shader_out, NULL);
+   nir_lower_variable_initializers(b->shader, nir_var_shader_out |
+                                              nir_var_system_value);
+   nir_remove_dead_variables(b->shader, ~nir_var_function_temp, NULL);
 
    /* We sometimes generate bogus derefs that, while never used, give the
     * validator a bit of heartburn.  Run dead code to get rid of them.

commit bf24e16bcaeb1a70c434413ab118d90bad992405

spirv: Don't remove variables used by resource indexing intrinsics
In Vulkan, for some variable modes, the generated NIR will have derefs
pointing to resource index intrinsics instead of the variable.  This
was letting nir_remove_dead_variables pass remove those variables,
which would lose information relevant for later passes after
spirv2nir.

Add a set to keep track of such variables and prevent them to be
removed when producing the NIR output.

Issue reported by Rhys.

Fixes: 2fda4e0b693 ("spirv: Remove more dead variables")
Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8706>

src/compiler/spirv/spirv_to_nir.c
@@ -5721,6 +5721,9 @@ vtn_create_builder(const uint32_t *words, size_t word_count,
    b->value_id_bound = value_id_bound;
    b->values = rzalloc_array(b, struct vtn_value, value_id_bound);
 
+   if (b->options->environment == NIR_SPIRV_VULKAN)
+      b->vars_used_indirectly = _mesa_pointer_set_create(b);
+
    return b;
  fail:
    ralloc_free(b);
@@ -5796,6 +5799,13 @@ vtn_emit_kernel_entry_point_wrapper(struct vtn_builder *b,
    nir_builder_instr_insert(&b->nb, &call->instr);
 
    return main_entry_point;
+}
+
+static bool
+can_remove(nir_variable *var, void *data)
+{
+   const struct set *vars_used_indirectly = data;
+   return !_mesa_set_search(vars_used_indirectly, var);
 }
 
 nir_shader *
@@ -5946,7 +5956,12 @@ spirv_to_nir(const uint32_t *words, size_t word_count,
     */
    nir_lower_variable_initializers(b->shader, nir_var_shader_out |
                                               nir_var_system_value);
-   nir_remove_dead_variables(b->shader, ~nir_var_function_temp, NULL);
+   const nir_remove_dead_variables_options dead_opts = {
+      .can_remove_var = can_remove,
+      .can_remove_var_data = b->vars_used_indirectly,
+   };
+   nir_remove_dead_variables(b->shader, ~nir_var_function_temp,
+                             b->vars_used_indirectly ? &dead_opts : NULL);
 
    /* We sometimes generate bogus derefs that, while never used, give the
     * validator a bit of heartburn.  Run dead code to get rid of them.

src/compiler/spirv/vtn_private.h
@@ -674,6 +674,13 @@ struct vtn_builder {
     */
    struct hash_table *phi_table;
 
+   /* In Vulkan, when lowering some modes variable access, the derefs of the
+    * variables are replaced with a resource index intrinsics, leaving the
+    * variable hanging.  This set keeps track of them so they can be filtered
+    * (and not removed) in nir_remove_dead_variables.
+    */
+   struct set *vars_used_indirectly;
+
    unsigned num_specializations;
    struct nir_spirv_specialization *specializations;
 

src/compiler/spirv/vtn_variables.c
@@ -237,6 +237,11 @@ vtn_variable_resource_index(struct vtn_builder *b, struct vtn_variable *var,
       desc_array_index = nir_imm_int(&b->nb, 0);
    }
 
+   if (b->vars_used_indirectly) {
+      vtn_assert(var->var);
+      _mesa_set_add(b->vars_used_indirectly, var->var);
+   }
+
    nir_intrinsic_instr *instr =
       nir_intrinsic_instr_create(b->nb.shader,
                                  nir_intrinsic_vulkan_resource_index);