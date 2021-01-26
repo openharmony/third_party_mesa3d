openharmony/third_party_mesa3d

commit 1080ff5570bfb895e13de62856bb44674d2ab79d

radv: Enable sparse buffer and image support.
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7953>

src/amd/vulkan/radv_device.c
@@ -1011,8 +1011,12 @@ void radv_GetPhysicalDeviceFeatures(
 		.shaderInt64                              = true,
 		.shaderInt16                              = true,
 		.sparseBinding                            = true,
+		.sparseResidencyBuffer                    = pdevice->rad_info.chip_class >= GFX8,
+		.sparseResidencyImage2D                   = pdevice->rad_info.chip_class >= GFX8,
+		.sparseResidencyAliased                   = pdevice->rad_info.chip_class >= GFX8,
 		.variableMultisampleRate                  = true,
 		.shaderResourceMinLod                     = true,
+		.shaderResourceResidency                  = true,
 		.inheritedQueries                         = true,
 	};
 }
@@ -1655,7 +1659,10 @@ void radv_GetPhysicalDeviceProperties(
 		.deviceID = pdevice->rad_info.pci_id,
 		.deviceType = pdevice->rad_info.has_dedicated_vram ? VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU : VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU,
 		.limits = limits,
-		.sparseProperties = {0},
+		.sparseProperties = {
+			.residencyNonResidentStrict = pdevice->rad_info.chip_class >= GFX8,
+			.residencyStandard2DBlockShape = pdevice->rad_info.chip_class >= GFX8,
+		},
 	};
 
 	strcpy(pProperties->deviceName, pdevice->name);

commit 3cf17cc9a53aeb70de3fea916b5859341a752a57

radv: Only enable sparse features on Polaris and newer.
On Fiji, the CTS image can cause a hang when these are enabled.
Let's enable them for Polaris and newer only, for now.

Gitlab: #4136
Fixes: 1080ff5570bfb895e13de62856bb44674d2ab79d
Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8646>

src/amd/vulkan/radv_device.c
@@ -1030,9 +1030,9 @@ void radv_GetPhysicalDeviceFeatures(
 		.shaderInt64                              = true,
 		.shaderInt16                              = true,
 		.sparseBinding                            = true,
-		.sparseResidencyBuffer                    = pdevice->rad_info.chip_class >= GFX8,
-		.sparseResidencyImage2D                   = pdevice->rad_info.chip_class >= GFX8,
-		.sparseResidencyAliased                   = pdevice->rad_info.chip_class >= GFX8,
+		.sparseResidencyBuffer                    = pdevice->rad_info.family >= CHIP_POLARIS10,
+		.sparseResidencyImage2D                   = pdevice->rad_info.family >= CHIP_POLARIS10,
+		.sparseResidencyAliased                   = pdevice->rad_info.family >= CHIP_POLARIS10,
 		.variableMultisampleRate                  = true,
 		.shaderResourceMinLod                     = true,
 		.shaderResourceResidency                  = true,
@@ -1679,8 +1679,8 @@ void radv_GetPhysicalDeviceProperties(
 		.deviceType = pdevice->rad_info.has_dedicated_vram ? VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU : VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU,
 		.limits = limits,
 		.sparseProperties = {
-			.residencyNonResidentStrict = pdevice->rad_info.chip_class >= GFX8,
-			.residencyStandard2DBlockShape = pdevice->rad_info.chip_class >= GFX8,
+			.residencyNonResidentStrict = pdevice->rad_info.family >= CHIP_POLARIS10,
+			.residencyStandard2DBlockShape = pdevice->rad_info.family >= CHIP_POLARIS10,
 		},
 	};
 