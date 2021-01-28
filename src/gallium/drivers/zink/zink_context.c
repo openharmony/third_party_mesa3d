openharmony/third_party_mesa3d

commit 3503502fdbbd6b95265ae4a9ff48a69ec3471365

zink: flag ssbo buffer resources as having pending writes on batch
ssbos are the only descriptor type we support (so far) that allows writes
during the draw, so we have to ensure that we set the right flag on the batch
reference to handle sync if the buffer is later read from

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8330>

src/gallium/drivers/zink/zink_context.c
@@ -583,6 +583,10 @@ zink_set_shader_buffers(struct pipe_context *pctx,
 {
    struct zink_context *ctx = zink_context(pctx);
 
+   unsigned modified_bits = u_bit_consecutive(start_slot, count);
+   ctx->writable_ssbos &= ~modified_bits;
+   ctx->writable_ssbos |= writable_bitmask << start_slot;
+
    for (unsigned i = 0; i < count; i++) {
       struct pipe_shader_buffer *ssbo = &ctx->ssbos[p_stage][start_slot + i];
       if (buffers && buffers[i].buffer) {

src/gallium/drivers/zink/zink_context.h
@@ -92,6 +92,7 @@ struct zink_context {
 
    struct pipe_constant_buffer ubos[PIPE_SHADER_TYPES][PIPE_MAX_CONSTANT_BUFFERS];
    struct pipe_shader_buffer ssbos[PIPE_SHADER_TYPES][PIPE_MAX_SHADER_BUFFERS];
+   uint32_t writable_ssbos;
    struct pipe_framebuffer_state fb_state;
 
    struct zink_vertex_elements_state *element_state;

src/gallium/drivers/zink/zink_draw.c
@@ -356,7 +356,10 @@ zink_draw_vbo(struct pipe_context *pctx,
             assert(ctx->ssbos[i][index].buffer_size <= screen->info.props.limits.maxStorageBufferRange);
             assert(ctx->ssbos[i][index].buffer);
             struct zink_resource *res = zink_resource(ctx->ssbos[i][index].buffer);
-            write_desc_resources[num_wds] = res;
+            if (ctx->writable_ssbos & (1 << index))
+               write_desc_resources[num_wds] = res;
+            else
+               read_desc_resources[num_wds] = res;
             buffer_infos[num_buffer_info].buffer = res->buffer;
             buffer_infos[num_buffer_info].offset = ctx->ssbos[i][index].buffer_offset;
             buffer_infos[num_buffer_info].range  = ctx->ssbos[i][index].buffer_size;

commit 003f96cfaa2780e76f69ae684cd4231c397b76da

zink: flag ssbo buffer resources as having pending writes per stage
I meant to squash this down but didn't get around to it

Fixes: 3503502fdbb ("zink: flag ssbo buffer resources as having pending writes on batch")

Reviewed-by: Adam Jackson <ajax@redhat.com>
Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8628>

src/gallium/drivers/zink/zink_context.c
@@ -600,8 +600,8 @@ zink_set_shader_buffers(struct pipe_context *pctx,
    struct zink_context *ctx = zink_context(pctx);
 
    unsigned modified_bits = u_bit_consecutive(start_slot, count);
-   ctx->writable_ssbos &= ~modified_bits;
-   ctx->writable_ssbos |= writable_bitmask << start_slot;
+   ctx->writable_ssbos[p_stage] &= ~modified_bits;
+   ctx->writable_ssbos[p_stage] |= writable_bitmask << start_slot;
 
    for (unsigned i = 0; i < count; i++) {
       struct pipe_shader_buffer *ssbo = &ctx->ssbos[p_stage][start_slot + i];

src/gallium/drivers/zink/zink_context.h
@@ -107,7 +107,7 @@ struct zink_context {
 
    struct pipe_constant_buffer ubos[PIPE_SHADER_TYPES][PIPE_MAX_CONSTANT_BUFFERS];
    struct pipe_shader_buffer ssbos[PIPE_SHADER_TYPES][PIPE_MAX_SHADER_BUFFERS];
-   uint32_t writable_ssbos;
+   uint32_t writable_ssbos[PIPE_SHADER_TYPES];
    struct zink_image_view image_views[PIPE_SHADER_TYPES][PIPE_MAX_SHADER_IMAGES];
    struct pipe_framebuffer_state fb_state;
 

src/gallium/drivers/zink/zink_draw.c
@@ -370,7 +370,7 @@ zink_draw_vbo(struct pipe_context *pctx,
             assert(ctx->ssbos[i][index].buffer_size <= screen->info.props.limits.maxStorageBufferRange);
             assert(ctx->ssbos[i][index].buffer);
             struct zink_resource *res = zink_resource(ctx->ssbos[i][index].buffer);
-            if (ctx->writable_ssbos & (1 << index))
+            if (ctx->writable_ssbos[i] & (1 << index))
                write_desc_resources[num_wds] = res;
             else
                read_desc_resources[num_wds] = res;