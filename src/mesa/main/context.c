openharmony/third_party_mesa3d

commit e431d8b26796fc8582b0792b93eb03e84602c115

mesa/st: Allocate the gl_context with 16-byte alignment.
The _ModelProjectMatrix matrix embedded inside has members inside of it
marked as 16-byte aligned, and so the context also has to be 16-byte
aligned or access to those members would be invalid.  I believe the
compiler used this to use better 16-byte-aligned load/stores to other
members of the context, breaking when the context's alignment was only 8
(as normal mallocs guarantee).

Fixes: ff01c8944e70 ("mesa: don't allocate matrices with malloc")
Tested-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8783>

src/mesa/state_tracker/st_context.c
@@ -1026,12 +1026,14 @@ st_create_context(gl_api api, struct pipe_context *pipe,
    if (pipe->set_context_param)
       funcs.PinDriverToL3Cache = st_pin_driver_to_l3_cache;
 
-   ctx = calloc(1, sizeof(struct gl_context));
+   /* gl_context must be 16-byte aligned due to the alignment on GLmatrix. */
+   ctx = align_malloc(sizeof(struct gl_context), 16);
    if (!ctx)
       return NULL;
+   memset(ctx, 0, sizeof(*ctx));
 
    if (!_mesa_initialize_context(ctx, api, visual, shareCtx, &funcs)) {
-      free(ctx);
+      align_free(ctx);
       return NULL;
    }
 
@@ -1164,7 +1166,7 @@ st_destroy_context(struct st_context *st)
 
    _mesa_destroy_debug_output(ctx);
 
-   free(ctx);
+   align_free(ctx);
 
    if (save_ctx == ctx) {
       /* unbind the context we just deleted */

commit 61c16145f92e2e199da8f3c50ecd2b118afe1920

mesa: Remove _mesa_destroy_context()
There was only one caller, an error path in mesa/st.  But this is now
incorrect as we need align_free().  Just remove it.

Fixes: e431d8b2679 ("mesa/st: Allocate the gl_context with 16-byte alignment.")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8801>

src/mesa/main/context.c
@@ -1409,23 +1409,6 @@ _mesa_free_context_data(struct gl_context *ctx, bool destroy_debug_output)
 
 
 /**
- * Destroy a struct gl_context structure.
- *
- * \param ctx GL context.
- *
- * Calls _mesa_free_context_data() and frees the gl_context object itself.
- */
-void
-_mesa_destroy_context( struct gl_context *ctx )
-{
-   if (ctx) {
-      _mesa_free_context_data(ctx, true);
-      free( (void *) ctx );
-   }
-}
-
-
-/**
  * Copy attribute groups from one context to another.
  *
  * \param src source context

src/mesa/main/context.h
@@ -121,10 +121,6 @@ extern void
 _mesa_free_context_data(struct gl_context *ctx, bool destroy_debug_output);
 
 extern void
-_mesa_destroy_context( struct gl_context *ctx );
-
-
-extern void
 _mesa_copy_context(const struct gl_context *src, struct gl_context *dst, GLuint mask);
 
 extern GLboolean

src/mesa/state_tracker/st_context.c
@@ -1050,7 +1050,8 @@ st_create_context(gl_api api, struct pipe_context *pipe,
 
    st = st_create_context_priv(ctx, pipe, options, no_error);
    if (!st) {
-      _mesa_destroy_context(ctx);
+      _mesa_free_context_data(ctx, true);
+      align_free(ctx);
    }
 
    return st;