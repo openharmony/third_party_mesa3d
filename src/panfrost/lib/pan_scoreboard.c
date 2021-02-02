openharmony/third_party_mesa3d

commit f3088dd24689602ee37111a4d5167b7c85bed6f1

panfrost: Fix tiler job injection
When injecting a tiler job, we shouln't make it depend on the last tiler
job, but instead make the first tiler job depend on it.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7206>

src/panfrost/lib/pan_scoreboard.c
@@ -115,6 +115,7 @@ panfrost_add_job(
                 const struct panfrost_transfer *job,
                 bool inject)
 {
+        bool is_bifrost = !!(pool->dev->quirks & IS_BIFROST);
         unsigned global_dep = 0;
 
         if (type == MALI_JOB_TYPE_TILER) {
@@ -122,12 +123,13 @@ panfrost_add_job(
                  * job must depend on the write value job, whose index we
                  * reserve now */
 
-                if (scoreboard->tiler_dep)
-                        global_dep = scoreboard->tiler_dep;
-                else if (!(pool->dev->quirks & IS_BIFROST)) {
+                if (is_bifrost && !scoreboard->write_value_index)
                         scoreboard->write_value_index = ++scoreboard->job_index;
+
+                if (scoreboard->tiler_dep && !inject)
+                        global_dep = scoreboard->tiler_dep;
+                else if (is_bifrost)
                         global_dep = scoreboard->write_value_index;
-                }
         }
 
         /* Assign the index */
@@ -145,13 +147,30 @@ panfrost_add_job(
         }
 
         if (inject) {
+                if (type == MALI_JOB_TYPE_TILER) {
+                        if (scoreboard->first_tiler) {
+                                /* Manual update of the dep2 field. This is bad,
+                                 * don't copy this pattern.
+                                 */
+                                scoreboard->first_tiler->opaque[5] =
+                                        scoreboard->first_tiler_dep1 | (index << 16);
+                        }
+
+                        scoreboard->first_tiler = (void *)job->cpu;
+                        scoreboard->first_tiler_dep1 = local_dep;
+                }
                 scoreboard->first_job = job->gpu;
                 return index;
         }
 
         /* Form a chain */
-        if (type == MALI_JOB_TYPE_TILER)
+        if (type == MALI_JOB_TYPE_TILER) {
+                if (!scoreboard->first_tiler) {
+                        scoreboard->first_tiler = (void *)job->cpu;
+                        scoreboard->first_tiler_dep1 = local_dep;
+                }
                 scoreboard->tiler_dep = index;
+        }
 
         if (scoreboard->prev_job) {
                 /* Manual update of the next pointer. This is bad, don't copy

src/panfrost/lib/pan_scoreboard.h
@@ -40,6 +40,12 @@ struct pan_scoreboard {
         /* A CPU-side pointer to the previous job for next_job linking */
         struct mali_job_header_packed *prev_job;
 
+        /* A CPU-side pointer to the first tiler job for dep updates when
+         * injecting a reload tiler job.
+         */
+        struct mali_job_header_packed *first_tiler;
+        uint32_t first_tiler_dep1;
+
         /* The dependency for tiler jobs (i.e. the index of the last emitted
          * tiler job, or zero if none have been emitted) */
         unsigned tiler_dep;

commit 711307e8f731f0a6c34d25a0069a07b058b13e15

panfrost: Fix tiler job injection (again)
f3088dd24689 ("panfrost: Fix tiler job injection") had the tests
inverted: WRITE_VALUE jobs are only needed on Midgard, not Bifrost.

Cc: mesa-stable
Fixes: f3088dd24689 ("panfrost: Fix tiler job injection")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8808>

src/panfrost/lib/pan_scoreboard.c
@@ -123,12 +123,12 @@ panfrost_add_job(
                  * job must depend on the write value job, whose index we
                  * reserve now */
 
-                if (is_bifrost && !scoreboard->write_value_index)
+                if (!is_bifrost && !scoreboard->write_value_index)
                         scoreboard->write_value_index = ++scoreboard->job_index;
 
                 if (scoreboard->tiler_dep && !inject)
                         global_dep = scoreboard->tiler_dep;
-                else if (is_bifrost)
+                else if (!is_bifrost)
                         global_dep = scoreboard->write_value_index;
         }
 