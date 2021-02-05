openharmony/third_party_mesa3d

commit bb6548ffbee25bbada9dcf9c825419fe86b1cc4a

pan/bi: Pass through wait_{6, 7} flags
Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8723>

src/panfrost/bifrost/bifrost_compile.c
@@ -2384,6 +2384,15 @@ bifrost_compile_shader_nir(void *mem_ctx, nir_shader *nir,
         util_dynarray_init(&program->compiled, NULL);
         bi_pack(ctx, &program->compiled);
 
+        /* If we need to wait for ATEST or BLEND in the first clause, pass the
+         * corresponding bits through to the renderer state descriptor */
+        pan_block *first_block = list_first_entry(&ctx->blocks, pan_block, link);
+        bi_clause *first_clause = bi_next_clause(ctx, first_block, NULL);
+
+        unsigned first_deps = first_clause->dependencies;
+        program->wait_6 = (first_deps & (1 << 6));
+        program->wait_7 = (first_deps & (1 << 7));
+
         memcpy(program->blend_ret_offsets, ctx->blend_ret_offsets, sizeof(program->blend_ret_offsets));
 
         if (bifrost_debug & BIFROST_DBG_SHADERS && !skip_internal) {

commit be82624aab616e93bb2030b4f4a7cb34f7094625

pan/bi: Fix NULL deref with empty shader
Fixes regression in dEQP-GLES31.functional.compute.basic.empty

Fixes: bb6548ffbee ("pan/bi: Pass through wait_{6, 7} flags")
Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8848>

src/panfrost/bifrost/bifrost_compile.c
@@ -2406,7 +2406,7 @@ bifrost_compile_shader_nir(void *mem_ctx, nir_shader *nir,
         pan_block *first_block = list_first_entry(&ctx->blocks, pan_block, link);
         bi_clause *first_clause = bi_next_clause(ctx, first_block, NULL);
 
-        unsigned first_deps = first_clause->dependencies;
+        unsigned first_deps = first_clause ? first_clause->dependencies : 0;
         program->wait_6 = (first_deps & (1 << 6));
         program->wait_7 = (first_deps & (1 << 7));
 