openharmony/third_party_mesa3d

commit 30aec13fbe305eaf38d03c514617fe69da55a165

util/disk_cache: add thread queue to disk cache
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Grazvydas Ignotas <notasas@gmail.com>

src/util/disk_cache.c
@@ -41,6 +41,7 @@
 
 #include "util/crc32.h"
 #include "util/u_atomic.h"
+#include "util/u_queue.h"
 #include "util/mesa-sha1.h"
 #include "util/ralloc.h"
 #include "main/errors.h"
@@ -59,6 +60,9 @@
 struct disk_cache {
    /* The path to the cache directory. */
    char *path;
+
+   /* Thread queue for compressing and writing cache entries to disk */
+   struct util_queue cache_queue;
 
    /* A pointer to the mmapped index file within the cache directory. */
    uint8_t *index_mmap;
@@ -377,6 +381,14 @@ disk_cache_create(const char *gpu_name, const char *timestamp)
 
    cache->max_size = max_size;
 
+   /* A limit of 32 jobs was choosen as observations of Deus Ex start-up times
+    * showed that we reached at most 11 jobs on an Intel i5-6400 CPU@2.70GHz
+    * (a fairly modest desktop CPU). 1 thread was chosen because we don't
+    * really care about getting things to disk quickly just that it's not
+    * blocking other tasks.
+    */
+   util_queue_init(&cache->cache_queue, "disk_cache", 32, 1);
+
    ralloc_free(local);
 
    return cache;
@@ -394,8 +406,10 @@ disk_cache_create(const char *gpu_name, const char *timestamp)
 void
 disk_cache_destroy(struct disk_cache *cache)
 {
-   if (cache)
+   if (cache) {
+      util_queue_destroy(&cache->cache_queue);
       munmap(cache->index_mmap, cache->index_mmap_size);
+   }
 
    ralloc_free(cache);
 }

commit 5d02602b6c098af827a1b26543a3158fcbd35bd6

disk_cache: Fail creation when cannot inizialize queue.
Signed-off-by: Giovanni Mascellani <gmascellani@codeweavers.com>
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Fixes: 30aec13fbe30 ("util/disk_cache: add thread queue to disk cache")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8983>

src/util/disk_cache.c
@@ -162,10 +162,11 @@ disk_cache_create(const char *gpu_name, const char *driver_id,
     * The queue will resize automatically when it's full, so adding new jobs
     * doesn't stall.
     */
-   util_queue_init(&cache->cache_queue, "disk$", 32, 4,
-                   UTIL_QUEUE_INIT_RESIZE_IF_FULL |
-                   UTIL_QUEUE_INIT_USE_MINIMUM_PRIORITY |
-                   UTIL_QUEUE_INIT_SET_FULL_THREAD_AFFINITY);
+   if (!util_queue_init(&cache->cache_queue, "disk$", 32, 4,
+                        UTIL_QUEUE_INIT_RESIZE_IF_FULL |
+                        UTIL_QUEUE_INIT_USE_MINIMUM_PRIORITY |
+                        UTIL_QUEUE_INIT_SET_FULL_THREAD_AFFINITY))
+      goto fail;
 
    cache->path_init_failed = false;
 