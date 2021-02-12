openharmony/third_party_mesa3d

commit aef503ea1d993a3b7b25c17605659a5a3efc8faf

gallium/targets/libgl-gdi: prefer d3d12 driver
Unlike the other drivers, the D3D12 driver is hardware accelerated, so
it's going to be a more reasonable choice. So let's prefer it.

This only matters for people who build with the D3D12 driver. And they
can set the GALLIUM_DRIVER environment variable as appropriate to
override it.

Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Acked-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7534>

src/gallium/targets/libgl-gdi/libgl_gdi.c
@@ -89,7 +89,9 @@ gdi_screen_create(HDC hDC)
    if(!winsys)
       goto no_winsys;
 
-#ifdef GALLIUM_LLVMPIPE
+#ifdef GALLIUM_D3D12
+   default_driver = "d3d12";
+#elif defined(GALLIUM_LLVMPIPE)
    default_driver = "llvmpipe";
 #elif GALLIUM_SWR
    default_driver = "swr";

commit b88a4d6faa49351b22dc2f56e47e6b83abbd185f

wgl: Add a loop for screen creation with an ordered list of fallbacks
Fixes: aef503ea ("gallium/targets/libgl-gdi: prefer d3d12 driver")
Reviewed-by: Jose Fonseca <jfonseca@vmware.com>
Tested-by: Prodea Alexandru-Liviu <liviuprodea@yahoo.com>
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4022
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8865>

src/gallium/targets/libgl-gdi/libgl_gdi.c
@@ -123,33 +123,41 @@ gdi_screen_create_by_name(HDC hDC, const char* driver, struct sw_winsys *winsys)
 static struct pipe_screen *
 gdi_screen_create(HDC hDC)
 {
-   const char *default_driver;
-   const char *driver;
-   struct pipe_screen *screen = NULL;
    struct sw_winsys *winsys;
 
    winsys = gdi_create_sw_winsys();
    if (!winsys)
-      return screen;
+      return NULL;
 
+   const char *const drivers[] = {
+      debug_get_option("GALLIUM_DRIVER", ""),
 #ifdef GALLIUM_D3D12
-   default_driver = "d3d12";
-#elif defined(GALLIUM_LLVMPIPE)
-   default_driver = "llvmpipe";
-#elif GALLIUM_SWR
-   default_driver = "swr";
-#elif defined(GALLIUM_SOFTPIPE)
-   default_driver = "softpipe";
-#else
-#error "no suitable default-driver"
+      "d3d12",
 #endif
+#if defined(GALLIUM_LLVMPIPE)
+      "llvmpipe",
+#endif
+#if GALLIUM_SWR
+      "swr",
+#endif
+#if defined(GALLIUM_SOFTPIPE)
+      "softpipe",
+#endif
+   };
+   
+   /* If the default driver screen creation fails, fall back to the next option in the
+    * sorted list. Don't do this if GALLIUM_DRIVER is specified.
+    */
+   for (unsigned i = 0; i < ARRAY_SIZE(drivers); ++i) {
+      struct pipe_screen* screen = gdi_screen_create_by_name(hDC, drivers[i], winsys);
+      if (screen)
+         return screen;
+      if (i == 0 && drivers[i][0] != '\0')
+         break;
+   }
 
-   driver = debug_get_option("GALLIUM_DRIVER", default_driver);
-   screen = gdi_screen_create_by_name(hDC, driver, winsys);
-   if (!screen)
-      winsys->destroy(winsys);
-
-   return screen;
+   winsys->destroy(winsys);
+   return NULL;
 }
 
 