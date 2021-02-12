openharmony/third_party_mesa3d

commit aef503ea1d993a3b7b25c17605659a5a3efc8faf

gallium/targets/libgl-gdi: prefer d3d12 driver
Unlike the other drivers, the D3D12 driver is hardware accelerated, so
it's going to be a more reasonable choice. So let's prefer it.

This only matters for people who build with the D3D12 driver. And they
can set the GALLIUM_DRIVER environment variable as appropriate to
override it.

Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Acked-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7534>

src/gallium/targets/libgl-gdi/libgl_gdi.c
@@ -89,7 +89,9 @@ gdi_screen_create(HDC hDC)
    if(!winsys)
       goto no_winsys;
 
-#ifdef GALLIUM_LLVMPIPE
+#ifdef GALLIUM_D3D12
+   default_driver = "d3d12";
+#elif defined(GALLIUM_LLVMPIPE)
    default_driver = "llvmpipe";
 #elif GALLIUM_SWR
    default_driver = "swr";

commit d9c5a3948c4d42e2f5ec4b4ec0673e8ec2b051ed

wgl: Refactor screen creation to a function
Fixes: aef503ea ("gallium/targets/libgl-gdi: prefer d3d12 driver")
Reviewed-by: Jose Fonseca <jfonseca@vmware.com>
Tested-by: Prodea Alexandru-Liviu <liviuprodea@yahoo.com>
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4022
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8865>

src/gallium/targets/libgl-gdi/libgl_gdi.c
@@ -79,6 +79,48 @@ static boolean use_zink = FALSE;
 #endif
 
 static struct pipe_screen *
+gdi_screen_create_by_name(HDC hDC, const char* driver, struct sw_winsys *winsys)
+{
+   struct pipe_screen* screen = NULL;
+
+#ifdef GALLIUM_LLVMPIPE
+   if (strcmp(driver, "llvmpipe") == 0) {
+      screen = llvmpipe_create_screen(winsys);
+      if (screen)
+         use_llvmpipe = TRUE;
+   }
+#endif
+#ifdef GALLIUM_SWR
+   if (strcmp(driver, "swr") == 0) {
+      screen = swr_create_screen(winsys);
+      if (screen)
+         use_swr = TRUE;
+   }
+#endif
+#ifdef GALLIUM_D3D12
+   if (strcmp(driver, "d3d12") == 0) {
+      screen = d3d12_wgl_create_screen(winsys, hDC);
+      if (screen)
+         use_d3d12 = TRUE;
+   }
+#endif
+#ifdef GALLIUM_ZINK
+   if (strcmp(driver, "zink") == 0) {
+      screen = zink_create_screen(winsys);
+      if (screen)
+         use_zink = TRUE;
+   }
+#endif
+#ifdef GALLIUM_SOFTPIPE
+   if (strcmp(driver, "softpipe") == 0) {
+      screen = softpipe_create_screen(winsys);
+   }
+#endif
+
+   return screen;
+}
+
+static struct pipe_screen *
 gdi_screen_create(HDC hDC)
 {
    const char *default_driver;
@@ -87,8 +129,8 @@ gdi_screen_create(HDC hDC)
    struct sw_winsys *winsys;
 
    winsys = gdi_create_sw_winsys();
-   if(!winsys)
-      goto no_winsys;
+   if (!winsys)
+      return screen;
 
 #ifdef GALLIUM_D3D12
    default_driver = "d3d12";
@@ -103,50 +145,11 @@ gdi_screen_create(HDC hDC)
 #endif
 
    driver = debug_get_option("GALLIUM_DRIVER", default_driver);
-
-#ifdef GALLIUM_LLVMPIPE
-   if (strcmp(driver, "llvmpipe") == 0) {
-      screen = llvmpipe_create_screen( winsys );
-      if (screen)
-         use_llvmpipe = TRUE;
-   }
-#endif
-#ifdef GALLIUM_SWR
-   if (strcmp(driver, "swr") == 0) {
-      screen = swr_create_screen( winsys );
-      if (screen)
-         use_swr = TRUE;
-   }
-#endif
-#ifdef GALLIUM_D3D12
-   if (strcmp(driver, "d3d12") == 0) {
-      screen = d3d12_wgl_create_screen( winsys, hDC );
-      if (screen)
-         use_d3d12 = TRUE;
-   }
-#endif
-#ifdef GALLIUM_ZINK
-   if (strcmp(driver, "zink") == 0) {
-       screen = zink_create_screen( winsys );
-       if (screen)
-           use_zink = TRUE;
-   }
-#endif
-   (void) driver;
-
-#ifdef GALLIUM_SOFTPIPE
-   if (screen == NULL)
-      screen = softpipe_create_screen( winsys );
-#endif
+   screen = gdi_screen_create_by_name(hDC, driver, winsys);
    if (!screen)
-      goto no_screen;
+      winsys->destroy(winsys);
 
    return screen;
-   
-no_screen:
-   winsys->destroy(winsys);
-no_winsys:
-   return NULL;
 }
 
 