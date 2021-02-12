openharmony/third_party_mesa3d

commit aef503ea1d993a3b7b25c17605659a5a3efc8faf

gallium/targets/libgl-gdi: prefer d3d12 driver
Unlike the other drivers, the D3D12 driver is hardware accelerated, so
it's going to be a more reasonable choice. So let's prefer it.

This only matters for people who build with the D3D12 driver. And they
can set the GALLIUM_DRIVER environment variable as appropriate to
override it.

Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Acked-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7534>

src/gallium/targets/libgl-gdi/libgl_gdi.c
@@ -89,7 +89,9 @@ gdi_screen_create(HDC hDC)
    if(!winsys)
       goto no_winsys;
 
-#ifdef GALLIUM_LLVMPIPE
+#ifdef GALLIUM_D3D12
+   default_driver = "d3d12";
+#elif defined(GALLIUM_LLVMPIPE)
    default_driver = "llvmpipe";
 #elif GALLIUM_SWR
    default_driver = "swr";

commit 11baf25a6bb98bcf5ae9e0647e02bb8d72285afb

wgl: Disable automatic use of layered drivers with LIBGL_ALWAYS_SOFTWARE
Fixes: aef503ea ("gallium/targets/libgl-gdi: prefer d3d12 driver")
Reviewed-by: Jose Fonseca <jfonseca@vmware.com>
Tested-by: Prodea Alexandru-Liviu <liviuprodea@yahoo.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8865>

src/gallium/targets/libgl-gdi/libgl_gdi.c
@@ -37,6 +37,7 @@
 #include <windows.h>
 
 #include "util/u_debug.h"
+#include "util/debug.h"
 #include "stw_winsys.h"
 #include "stw_device.h"
 #include "gdi/gdi_sw_winsys.h"
@@ -124,6 +125,7 @@ static struct pipe_screen *
 gdi_screen_create(HDC hDC)
 {
    struct sw_winsys *winsys;
+   UNUSED bool sw_only = env_var_as_boolean("LIBGL_ALWAYS_SOFTWARE", false);
 
    winsys = gdi_create_sw_winsys();
    if (!winsys)
@@ -132,7 +134,7 @@ gdi_screen_create(HDC hDC)
    const char *const drivers[] = {
       debug_get_option("GALLIUM_DRIVER", ""),
 #ifdef GALLIUM_D3D12
-      "d3d12",
+      sw_only ? "" : "d3d12",
 #endif
 #if defined(GALLIUM_LLVMPIPE)
       "llvmpipe",