openharmony/third_party_mesa3d

commit 945ad28521aae118d3ef00d666499e6b49d02844

zink: fix tess shader i/o variables
this was a mismerge during refactoring for review

Fixes: 1a2a4027b4b ("zink: add ntv handling for tess shader i/o variables")

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8232>

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -309,12 +309,13 @@ handle_slot(struct ntv_context *ctx, unsigned slot)
 
 
 static inline unsigned
-handle_handle_slot(struct ntv_context *ctx, struct nir_variable *var)
+handle_handle_slot(struct ntv_context *ctx, struct nir_variable *var, bool output)
 {
    if (var->data.patch) {
       assert(var->data.location >= VARYING_SLOT_PATCH0);
       return var->data.location - VARYING_SLOT_PATCH0;
-   } else if (ctx->stage == MESA_SHADER_TESS_CTRL) {
+   } else if ((output && ctx->stage == MESA_SHADER_TESS_CTRL) ||
+              (!output && ctx->stage == MESA_SHADER_TESS_EVAL)) {
       assert(var->data.location >= VARYING_SLOT_VAR0);
       return var->data.location - VARYING_SLOT_VAR0;
    }
@@ -383,7 +384,7 @@ emit_input(struct ntv_context *ctx, struct nir_variable *var)
          break;
 
       default:
-         slot = handle_handle_slot(ctx, var);
+         slot = handle_handle_slot(ctx, var, false);
          spirv_builder_emit_location(&ctx->builder, var_id, slot);
       }
    }
@@ -442,7 +443,7 @@ emit_output(struct ntv_context *ctx, struct nir_variable *var)
          break;
 
       default:
-         slot = handle_handle_slot(ctx, var);
+         slot = handle_handle_slot(ctx, var, true);
          spirv_builder_emit_location(&ctx->builder, var_id, slot);
       }
       /* tcs can't do xfb */

commit dad729991ba215870b535de3c03b0d5da95ebcc0

zink: fix slot mapping for legacy gl io with tess stages
e.g., gl_BackColor is a thing

Fixes: 945ad28521a ("zink: fix tess shader i/o variables")

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9019>

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -413,9 +413,9 @@ handle_handle_slot(struct ntv_context *ctx, struct nir_variable *var, bool outpu
    if (var->data.patch) {
       assert(var->data.location >= VARYING_SLOT_PATCH0);
       return var->data.location - VARYING_SLOT_PATCH0;
-   } else if ((output && ctx->stage == MESA_SHADER_TESS_CTRL) ||
-              (!output && ctx->stage == MESA_SHADER_TESS_EVAL)) {
-      assert(var->data.location >= VARYING_SLOT_VAR0);
+   } else if (var->data.location >= VARYING_SLOT_VAR0 &&
+              ((output && ctx->stage == MESA_SHADER_TESS_CTRL) ||
+              (!output && ctx->stage == MESA_SHADER_TESS_EVAL))) {
       return var->data.location - VARYING_SLOT_VAR0;
    }
    return handle_slot(ctx, var->data.location);