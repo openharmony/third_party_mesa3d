openharmony/third_party_mesa3d

commit 6f10a3f6baeb04fbc3eaceda5e38e9ee8319095e

zink: use uvec for undefs
Acked-by: Jordan Justen <jordan.l.justen@intel.com>

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -1063,7 +1063,7 @@ emit_intrinsic(struct ntv_context *ctx, nir_intrinsic_instr *intr)
 static void
 emit_undef(struct ntv_context *ctx, nir_ssa_undef_instr *undef)
 {
-   SpvId type = get_fvec_type(ctx, undef->def.bit_size,
+   SpvId type = get_uvec_type(ctx, undef->def.bit_size,
                               undef->def.num_components);
 
    store_ssa_def_uint(ctx, &undef->def,

commit 59445c5031e6b11cc839b3e4da9c432d4a8d6574

zink: handle 1bit undef values in ntv
spirv requires that 1bit values be bool types, not uints

Fixes: 6f10a3f6bae ("zink: use uvec for undefs")

Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9059>

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -2860,8 +2860,9 @@ emit_intrinsic(struct ntv_context *ctx, nir_intrinsic_instr *intr)
 static void
 emit_undef(struct ntv_context *ctx, nir_ssa_undef_instr *undef)
 {
-   SpvId type = get_uvec_type(ctx, undef->def.bit_size,
-                              undef->def.num_components);
+   SpvId type = undef->def.bit_size == 1 ? get_bvec_type(ctx, undef->def.num_components) :
+                                           get_uvec_type(ctx, undef->def.bit_size,
+                                                         undef->def.num_components);
 
    store_ssa_def(ctx, &undef->def,
                  spirv_builder_emit_undef(&ctx->builder, type));