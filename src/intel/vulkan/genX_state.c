openharmony/third_party_mesa3d

commit a25a0303bf0dac4fa025798d4d4275b7ec1cb2d7

intel/anv: Fix condition to set MipModeFilter for YUV surface
Mip Mode Filter must be set to MIPFILTER_NONE for Planar YUV surfaces.
Add the missing condition to check for planar format.

Fixes: 103d862c43c "anv: enable VK_KHR_sampler_ycbcr_conversion"
Signed-off-by: Anuj Phogat <anuj.phogat@gmail.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

src/intel/vulkan/genX_state.c
@@ -634,9 +634,12 @@ VkResult genX(CreateSampler)(
       /* From Broadwell PRM, SAMPLER_STATE:
        *   "Mip Mode Filter must be set to MIPFILTER_NONE for Planar YUV surfaces."
        */
+      const bool isl_format_is_planar_yuv =
+         isl_format_is_yuv(sampler->conversion->format->planes[0].isl_format) &&
+         isl_format_is_planar(sampler->conversion->format->planes[0].isl_format);
+
       const uint32_t mip_filter_mode =
-         (sampler->conversion &&
-          isl_format_is_yuv(sampler->conversion->format->planes[0].isl_format)) ?
+         (sampler->conversion && isl_format_is_planar_yuv) ?
          MIPFILTER_NONE : vk_to_gen_mipmap_mode[pCreateInfo->mipmapMode];
 
       struct GENX(SAMPLER_STATE) sampler_state = {

commit 061567b8ce86c050128f237f86f88cd06a602f59

intel/anv: Fix condition for planar yuv surface
Test the sampler->conversion for NULL pointer before dereferencing it.

Fixes: Regressions in VulkanCTS.
Fixes: a25a0303bf0 "intel/anv: Fix condition to set MipModeFilter for YUV surface"
Signed-off-by: Anuj Phogat <anuj.phogat@gmail.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

src/intel/vulkan/genX_state.c
@@ -634,12 +634,12 @@ VkResult genX(CreateSampler)(
       /* From Broadwell PRM, SAMPLER_STATE:
        *   "Mip Mode Filter must be set to MIPFILTER_NONE for Planar YUV surfaces."
        */
-      const bool isl_format_is_planar_yuv =
+      const bool isl_format_is_planar_yuv = sampler->conversion &&
          isl_format_is_yuv(sampler->conversion->format->planes[0].isl_format) &&
          isl_format_is_planar(sampler->conversion->format->planes[0].isl_format);
 
       const uint32_t mip_filter_mode =
-         (sampler->conversion && isl_format_is_planar_yuv) ?
+         isl_format_is_planar_yuv ?
          MIPFILTER_NONE : vk_to_gen_mipmap_mode[pCreateInfo->mipmapMode];
 
       struct GENX(SAMPLER_STATE) sampler_state = {