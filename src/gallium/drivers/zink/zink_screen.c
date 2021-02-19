openharmony/third_party_mesa3d

commit a2344f113b634036eb943a483121d5f78bb1e3d3

zink: handle dual blending override from driconf
when this is enabled, we need to push gl_FragData[1] to location 0 and
index 1 so that it gets blended like the application expects

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9095>

src/gallium/drivers/zink/zink_screen.c
@@ -40,6 +40,7 @@
 #include "util/u_screen.h"
 #include "util/u_string.h"
 #include "util/u_transfer_helper.h"
+#include "util/xmlconfig.h"
 
 #include "frontend/sw_winsys.h"
 
@@ -1282,6 +1283,9 @@ zink_internal_create_screen(const struct pipe_screen_config *config)
    free((void*)pcci.pInitialData);
 
    slab_create_parent(&screen->transfer_pool, sizeof(struct zink_transfer), 16);
+
+   if (config)
+      screen->driconf.dual_color_blend_by_location = driQueryOptionb(config->options, "dual_color_blend_by_location");
 
    return screen;
 

src/gallium/drivers/zink/zink_compiler.c
@@ -296,6 +296,20 @@ lower_drawid(nir_shader *shader)
    return nir_shader_instructions_pass(shader, lower_drawid_instr, nir_metadata_dominance, NULL);
 }
 
+static bool
+lower_dual_blend(nir_shader *shader)
+{
+   bool progress = false;
+   nir_variable *var = nir_find_variable_with_location(shader, nir_var_shader_out, FRAG_RESULT_DATA1);
+   if (var) {
+      var->data.location = FRAG_RESULT_DATA0;
+      var->data.index = 1;
+      progress = true;
+   }
+   nir_shader_preserve_all_metadata(shader);
+   return progress;
+}
+
 void
 zink_screen_init_compiler(struct zink_screen *screen)
 {
@@ -449,6 +463,11 @@ zink_shader_compile(struct zink_screen *screen, struct zink_shader *zs, struct z
          nir_fixup_deref_modes(nir);
          NIR_PASS_V(nir, nir_remove_dead_variables, nir_var_shader_temp, NULL);
          optimize_nir(nir);
+      }
+      if (zink_fs_key(key)->force_dual_color_blend && nir->info.outputs_written & BITFIELD64_BIT(FRAG_RESULT_DATA1)) {
+         if (nir == zs->nir)
+            nir = nir_shader_clone(NULL, zs->nir);
+         NIR_PASS_V(nir, lower_dual_blend);
       }
    }
    struct spirv_shader *spirv = nir_to_spirv(nir, streamout, shader_slot_map, shader_slots_reserved);

src/gallium/drivers/zink/zink_program.c
@@ -231,6 +231,7 @@ static void
 shader_key_fs_gen(struct zink_context *ctx, struct zink_shader *zs,
                   struct zink_shader *shaders[ZINK_SHADER_COUNT], struct zink_shader_key *key)
 {
+   struct zink_screen *screen = zink_screen(ctx->base.screen);
    struct zink_fs_key *fs_key = &key->key.fs;
    key->size = sizeof(struct zink_fs_key);
 
@@ -243,6 +244,9 @@ shader_key_fs_gen(struct zink_context *ctx, struct zink_shader *zs,
     */
    if (zs->nir->info.outputs_written & (1 << FRAG_RESULT_SAMPLE_MASK))
       fs_key->samples = !!ctx->fb_state.samples;
+   fs_key->force_dual_color_blend = screen->driconf.dual_color_blend_by_location &&
+                                    ctx->gfx_pipeline_state.blend_state->dual_src_blend &&
+                                    ctx->gfx_pipeline_state.blend_state->attachments[1].blendEnable;
 }
 
 static void

src/gallium/drivers/zink/zink_screen.h
@@ -115,6 +115,10 @@ struct zink_screen {
    PFN_vkUseIOSurfaceMVK vk_UseIOSurfaceMVK;
    PFN_vkGetIOSurfaceMVK vk_GetIOSurfaceMVK;
 #endif
+
+   struct {
+      bool dual_color_blend_by_location;
+   } driconf;
 };
 
 static inline struct zink_screen *

src/gallium/drivers/zink/zink_shader_keys.h
@@ -37,6 +37,7 @@ struct zink_fs_key {
    unsigned shader_id;
    //bool flat_shade;
    bool samples;
+   bool force_dual_color_blend;
 };
 
 struct zink_tcs_key {

src/gallium/drivers/zink/zink_state.c
@@ -26,6 +26,8 @@
 #include "zink_context.h"
 #include "zink_screen.h"
 
+#include "compiler/shader_enums.h"
+#include "util/u_dual_blend.h"
 #include "util/u_memory.h"
 
 #include <math.h>
@@ -275,6 +277,7 @@ zink_create_blend_state(struct pipe_context *pctx,
 
       cso->attachments[i] = att;
    }
+   cso->dual_src_blend = util_blend_state_is_dual(blend_state, 0);
 
    return cso;
 }

src/gallium/drivers/zink/zink_state.h
@@ -70,6 +70,7 @@ struct zink_blend_state {
    VkBool32 alpha_to_one;
 
    bool need_blend_constants;
+   bool dual_src_blend;
 };
 
 struct zink_depth_stencil_alpha_hw_state {

commit e4fcf7d9d77b76f23ffe18e01438137466a8481b

zink: fix win32 build
Fixes: a2344f11, 36983fa1

Tested-by: Prodea Alexandru-Liviu <liviuprodea@yahoo.com>
Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9126>

src/gallium/drivers/zink/zink_screen.c
@@ -808,8 +808,10 @@ zink_destroy_screen(struct pipe_screen *pscreen)
 
    u_transfer_helper_destroy(pscreen->transfer_helper);
    zink_screen_update_pipeline_cache(screen);
+#ifdef ENABLE_SHADER_CACHE
    if (screen->disk_cache)
       disk_cache_wait_for_idle(screen->disk_cache);
+#endif
    disk_cache_destroy(screen->disk_cache);
    vkDestroyPipelineCache(screen->dev, screen->pipeline_cache, NULL);
 
@@ -1287,8 +1289,10 @@ zink_internal_create_screen(const struct pipe_screen_config *config)
 
    slab_create_parent(&screen->transfer_pool, sizeof(struct zink_transfer), 16);
 
+#if WITH_XMLCONFIG
    if (config)
       screen->driconf.dual_color_blend_by_location = driQueryOptionb(config->options, "dual_color_blend_by_location");
+#endif
 
    return screen;
 