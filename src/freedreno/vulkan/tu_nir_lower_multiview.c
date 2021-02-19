openharmony/third_party_mesa3d

commit a64fb6af4d4dcc2c7cce5ef3c054023773370af6

tu: Enable multi-position output
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6515>

src/freedreno/vulkan/tu_nir_lower_multiview.c
@@ -15,10 +15,20 @@
  * one).
  */
 static bool
-lower_multiview_mask(nir_function_impl *impl, uint32_t mask)
+lower_multiview_mask(nir_shader *nir, uint32_t *mask)
 {
+   nir_function_impl *impl = nir_shader_get_entrypoint(nir);
+
+   if (util_is_power_of_two_or_zero(*mask + 1)) {
+      nir_metadata_preserve(impl, nir_metadata_all);
+      return false;
+   }
+
    nir_builder b;
    nir_builder_init(&b, impl);
+
+   uint32_t old_mask = *mask;
+   *mask = BIT(util_logbase2(old_mask) + 1) - 1;
 
    nir_foreach_block_reverse(block, impl) {
       nir_foreach_instr_reverse(instr, block) {
@@ -43,7 +53,7 @@ lower_multiview_mask(nir_function_impl *impl, uint32_t mask)
 
          /* ((1ull << gl_ViewIndex) & mask) != 0 */
          nir_ssa_def *cmp =
-            nir_i2b(&b, nir_iand(&b, nir_imm_int(&b, mask),
+            nir_i2b(&b, nir_iand(&b, nir_imm_int(&b, old_mask),
                                   nir_ishl(&b, nir_imm_int(&b, 1),
                                            nir_load_view_index(&b))));
 
@@ -61,16 +71,40 @@ lower_multiview_mask(nir_function_impl *impl, uint32_t mask)
 }
 
 bool
-tu_nir_lower_multiview(nir_shader *nir, uint32_t mask, struct tu_device *dev)
+tu_nir_lower_multiview(nir_shader *nir, uint32_t mask, bool *multi_pos_output,
+                       struct tu_device *dev)
 {
-   nir_function_impl *entrypoint = nir_shader_get_entrypoint(nir);
+   *multi_pos_output = false;
 
-   if (!dev->physical_device->supports_multiview_mask &&
-       !util_is_power_of_two_or_zero(mask + 1)) {
-      return lower_multiview_mask(entrypoint, mask);
+   bool progress = false;
+
+   if (!dev->physical_device->supports_multiview_mask)
+      NIR_PASS(progress, nir, lower_multiview_mask, &mask);
+
+   unsigned num_views = util_logbase2(mask) + 1;
+
+   /* Speculatively assign output locations so that we know num_outputs. We
+    * will assign output locations for real after this pass.
+    */
+   unsigned num_outputs;
+   nir_assign_io_var_locations(nir, nir_var_shader_out, &num_outputs, MESA_SHADER_VERTEX);
+
+   /* In addition to the generic checks done by NIR, check that we don't
+    * overflow VPC with the extra copies of gl_Position.
+    */
+   if (likely(!(dev->physical_device->instance->debug_flags & TU_DEBUG_NOMULTIPOS)) &&
+       num_outputs + (num_views - 1) <= 32 && nir_can_lower_multiview(nir)) {
+      *multi_pos_output = true;
+
+      /* It appears that the multiview mask is ignored when multi-position
+       * output is enabled, so we have to write 0 to inactive views ourselves.
+       */
+      NIR_PASS(progress, nir, lower_multiview_mask, &mask);
+
+      NIR_PASS_V(nir, nir_lower_multiview, mask);
+      progress = true;
    }
 
-   nir_metadata_preserve(entrypoint, nir_metadata_all);
-   return false;
+   return progress;
 }
 

src/freedreno/vulkan/tu_device.c
@@ -197,6 +197,7 @@ static const struct debug_control tu_debug_options[] = {
    { "sysmem", TU_DEBUG_SYSMEM },
    { "forcebin", TU_DEBUG_FORCEBIN },
    { "noubwc", TU_DEBUG_NOUBWC },
+   { "nomultipos", TU_DEBUG_NOMULTIPOS },
    { NULL, 0 }
 };
 

src/freedreno/vulkan/tu_private.h
@@ -227,6 +227,7 @@ enum tu_debug_flags
    TU_DEBUG_SYSMEM = 1 << 4,
    TU_DEBUG_FORCEBIN = 1 << 5,
    TU_DEBUG_NOUBWC = 1 << 6,
+   TU_DEBUG_NOMULTIPOS = 1 << 7,
 };
 
 struct tu_instance
@@ -1031,7 +1032,8 @@ struct tu_shader
 };
 
 bool
-tu_nir_lower_multiview(nir_shader *nir, uint32_t mask, struct tu_device *dev);
+tu_nir_lower_multiview(nir_shader *nir, uint32_t mask, bool *multi_pos_output,
+                       struct tu_device *dev);
 
 nir_shader *
 tu_spirv_to_nir(struct tu_device *dev,

src/freedreno/vulkan/tu_shader.c
@@ -761,7 +761,8 @@ tu_shader_create(struct tu_device *dev,
    }
 
    if (nir->info.stage == MESA_SHADER_VERTEX && multiview_mask) {
-      NIR_PASS_V(nir, tu_nir_lower_multiview, multiview_mask, dev);
+      tu_nir_lower_multiview(nir, multiview_mask,
+                             &shader->multi_pos_output, dev);
    }
 
    NIR_PASS_V(nir, nir_lower_explicit_io,

commit f746f57b5b0c2ef2d10689c9e9bedae39766402e

turnip: consider HW limit on number of views when apply multipos opt
Blob doesn't apply multipos optimization starting from 11 views
even on a650, however in practice, with the limit of 16 views,
tests pass on a640/a650 and fail on a630.

Fixes tests:
 dEQP-VK.multiview.draw_indexed.max_multi_view_view_count
 dEQP-VK.multiview.input_attachments.max_multi_view_view_count
 dEQP-VK.multiview.masks.max_multi_view_view_count
 dEQP-VK.multiview.multisample.max_multi_view_view_count
 dEQP-VK.multiview.queries.max_multi_view_view_count
 dEQP-VK.multiview.renderpass2.index.fragment_shader.max_multi_view_view_count
 dEQP-VK.multiview.secondary_cmd_buffer.max_multi_view_view_count

Fixes: a64fb6af ("tu: Enable multi-position output")

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9135>

src/freedreno/vulkan/tu_nir_lower_multiview.c
@@ -83,6 +83,13 @@ tu_nir_lower_multiview(nir_shader *nir, uint32_t mask, bool *multi_pos_output,
 
    unsigned num_views = util_logbase2(mask) + 1;
 
+   /* Blob doesn't apply multipos optimization starting from 11 views
+    * even on a650, however in practice, with the limit of 16 views,
+    * tests pass on a640/a650 and fail on a630.
+    */
+   unsigned max_views_for_multipos =
+      dev->physical_device->info.a6xx.supports_multiview_mask ? 16 : 10;
+
    /* Speculatively assign output locations so that we know num_outputs. We
     * will assign output locations for real after this pass.
     */
@@ -93,7 +100,8 @@ tu_nir_lower_multiview(nir_shader *nir, uint32_t mask, bool *multi_pos_output,
     * overflow VPC with the extra copies of gl_Position.
     */
    if (likely(!(dev->physical_device->instance->debug_flags & TU_DEBUG_NOMULTIPOS)) &&
-       num_outputs + (num_views - 1) <= 32 && nir_can_lower_multiview(nir)) {
+       num_views <= max_views_for_multipos && num_outputs + (num_views - 1) <= 32 &&
+       nir_can_lower_multiview(nir)) {
       *multi_pos_output = true;
 
       /* It appears that the multiview mask is ignored when multi-position