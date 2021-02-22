openharmony/third_party_mesa3d

commit 2457f8c94c2f7030a8231281fff55db43f21df7d

pan/bi: Fix jumps to terminal block again
New scheduler broke this. We need to shuffle some code around so we do
the lower pre-schedule instead of post-schedule (no clauses to work
with).

Fixes: f058cc8fcc9 ("pan/bi: Switch to new scheduler")
Reported-by: Icecream95 <ixn@disroot.org>
Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9011>

src/panfrost/bifrost/bifrost_compile.c
@@ -2424,10 +2424,12 @@ bifrost_nir_lower_i8_fragout(nir_shader *shader)
 }
 
 /* Dead code elimination for branches at the end of a block - only one branch
- * per block is legal semantically, but unreachable jumps can be generated */
+ * per block is legal semantically, but unreachable jumps can be generated.
+ * Likewise we can generate jumps to the terminal block which need to be
+ * lowered away to a jump to #0x0, which induces successful termination. */
 
 static void
-bi_cull_dead_branch(bi_block *block)
+bi_lower_branch(bi_block *block)
 {
         bool branched = false;
         ASSERTED bool was_jump = false;
@@ -2436,12 +2438,16 @@ bi_cull_dead_branch(bi_block *block)
                 if (!ins->branch_target) continue;
 
                 if (branched) {
-                        assert(was_jump);
+                        assert(was_jump && (ins->op == BI_OPCODE_JUMP));
                         bi_remove_instruction(ins);
+                        break;
                 }
 
                 branched = true;
                 was_jump = ins->op == BI_OPCODE_JUMP;
+
+                if (bi_is_terminal_block(ins->branch_target))
+                        ins->branch_target = NULL;
         }
 }
 
@@ -2525,7 +2531,7 @@ bifrost_compile_shader_nir(void *mem_ctx, nir_shader *nir,
                  * consistent */
                 block->base.name = block_source_count++;
 
-                bi_cull_dead_branch(block);
+                bi_lower_branch(block);
         }
 
         /* Runs before copy prop */

src/panfrost/bifrost/compiler.h
@@ -784,7 +784,7 @@ bi_is_terminal_block(bi_block *block)
 {
         return block->base.successors[0] == NULL &&
                block->base.successors[1] == NULL &&
-               list_is_empty(&block->clauses);
+               list_is_empty(&block->base.instructions);
 }
 
 /* Code emit */

commit 8a0665ccab1947146121f79919145e50d2126802

pan/bi: Fix more jumps to terminal blocks
Here's another edge case: there could be instructions in the last block
after NIR->BIR but they could be optimized out by backend DCE, causing
the block to become a terminal block.

Noticed while toying with geometry shaders.

Fixes: 2457f8c94c2 ("pan/bi: Fix jumps to terminal block again")
Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9137>

src/panfrost/bifrost/bifrost_compile.c
@@ -2973,8 +2973,6 @@ bifrost_compile_shader_nir(nir_shader *nir,
                 /* Name blocks now that we're done emitting so the order is
                  * consistent */
                 block->base.name = block_source_count++;
-
-                bi_lower_branch(block);
         }
 
         /* Runs before copy prop */
@@ -2992,6 +2990,11 @@ bifrost_compile_shader_nir(nir_shader *nir,
                         progress |= bi_opt_dead_code_eliminate(ctx, block, false);
                 }
         } while(progress);
+
+        bi_foreach_block(ctx, _block) {
+                bi_block *block = (bi_block *) _block;
+                bi_lower_branch(block);
+        }
 
         if (bifrost_debug & BIFROST_DBG_SHADERS && !skip_internal)
                 bi_print_shader(ctx, stdout);

src/panfrost/bifrost/compiler.h
@@ -777,12 +777,16 @@ unsigned bi_clause_quadwords(bi_clause *clause);
 signed bi_block_offset(bi_context *ctx, bi_clause *start, bi_block *target);
 bool bi_ec0_packed(unsigned tuple_count);
 
+/* Check if there are no more instructions starting with a given block, this
+ * needs to recurse in case a shader ends with multiple empty blocks */
+
 static inline bool
 bi_is_terminal_block(bi_block *block)
 {
-        return block->base.successors[0] == NULL &&
-               block->base.successors[1] == NULL &&
-               list_is_empty(&block->base.instructions);
+        return (block == NULL) ||
+                (list_is_empty(&block->base.instructions) &&
+                 bi_is_terminal_block((bi_block *) block->base.successors[0]) &&
+                 bi_is_terminal_block((bi_block *) block->base.successors[1]));
 }
 
 /* Code emit */