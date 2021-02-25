openharmony/third_party_mesa3d

commit 4921f927e324d247a28b2597f511e1a2303950b5

freedreno: Add perf_debug logging for bo stalls
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9223>

src/gallium/drivers/freedreno/freedreno_resource.c
@@ -177,6 +177,25 @@ fd_resource_set_bo(struct fd_resource *rsc, struct fd_bo *bo)
 	rsc->seqno = p_atomic_inc_return(&screen->rsc_seqno);
 }
 
+int
+__fd_resource_wait(struct fd_context *ctx, struct fd_resource *rsc,
+		unsigned op, const char *func)
+{
+	if (op & DRM_FREEDRENO_PREP_NOSYNC)
+		return fd_bo_cpu_prep(rsc->bo, ctx->pipe, op);
+
+	int64_t elapsed = -os_time_get_nano();
+	int ret = fd_bo_cpu_prep(rsc->bo, ctx->pipe, op);
+
+	elapsed += os_time_get_nano();
+	if (elapsed > 10000) /* 0.01ms */ {
+		perf_debug_ctx(ctx, "%s: a busy \"%"PRSC_FMT"\" BO stalled and took %.03f ms.\n",
+				func, PRSC_ARGS(&rsc->base), 1000000 * (double)elapsed);
+	}
+
+	return ret;
+}
+
 static void
 realloc_bo(struct fd_resource *rsc, uint32_t size)
 {
@@ -695,7 +714,7 @@ fd_resource_transfer_map(struct pipe_context *pctx,
 			if (usage & PIPE_MAP_READ) {
 				fd_blit_to_staging(ctx, trans);
 
-				fd_bo_cpu_prep(staging_rsc->bo, ctx->pipe,
+				fd_resource_wait(ctx, staging_rsc,
 						DRM_FREEDRENO_PREP_READ);
 			}
 
@@ -809,7 +828,7 @@ fd_resource_transfer_map(struct pipe_context *pctx,
 		 * completed.
 		 */
 		if (busy) {
-			ret = fd_bo_cpu_prep(rsc->bo, ctx->pipe, op);
+			ret = fd_resource_wait(ctx, rsc, op);
 			if (ret)
 				goto fail;
 		}

src/gallium/drivers/freedreno/freedreno_query_acc.c
@@ -171,7 +171,7 @@ fd_acc_get_query_result(struct fd_context *ctx, struct fd_query *q,
 			return false;
 		}
 
-		ret = fd_bo_cpu_prep(rsc->bo, ctx->pipe,
+		ret = fd_resource_wait(ctx, rsc,
 				DRM_FREEDRENO_PREP_READ | DRM_FREEDRENO_PREP_NOSYNC);
 		if (ret)
 			return false;
@@ -186,7 +186,7 @@ fd_acc_get_query_result(struct fd_context *ctx, struct fd_query *q,
 	}
 
 	/* get the result: */
-	fd_bo_cpu_prep(rsc->bo, ctx->pipe, DRM_FREEDRENO_PREP_READ);
+	fd_resource_wait(ctx, rsc, DRM_FREEDRENO_PREP_READ);
 
 	void *ptr = fd_bo_map(rsc->bo);
 	p->result(aq, ptr, result);

src/gallium/drivers/freedreno/freedreno_query_hw.c
@@ -230,7 +230,7 @@ fd_hw_get_query_result(struct fd_context *ctx, struct fd_query *q,
 		if (!rsc->bo)
 			return false;
 
-		ret = fd_bo_cpu_prep(rsc->bo, ctx->pipe,
+		ret = fd_resource_wait(ctx, rsc,
 				DRM_FREEDRENO_PREP_READ | DRM_FREEDRENO_PREP_NOSYNC);
 		if (ret)
 			return false;
@@ -260,7 +260,7 @@ fd_hw_get_query_result(struct fd_context *ctx, struct fd_query *q,
 		if (!rsc->bo)
 			continue;
 
-		fd_bo_cpu_prep(rsc->bo, ctx->pipe, DRM_FREEDRENO_PREP_READ);
+		fd_resource_wait(ctx, rsc, DRM_FREEDRENO_PREP_READ);
 
 		void *ptr = fd_bo_map(rsc->bo);
 

src/gallium/drivers/freedreno/freedreno_resource.h
@@ -165,6 +165,9 @@ fd_resource_busy(struct fd_resource *rsc, unsigned op)
 	return fd_bo_cpu_prep(rsc->bo, NULL, op | DRM_FREEDRENO_PREP_NOSYNC) != 0;
 }
 
+int __fd_resource_wait(struct fd_context *ctx, struct fd_resource *rsc, unsigned op, const char *func);
+#define fd_resource_wait(ctx, rsc, op) __fd_resource_wait(ctx, rsc, op, __func__)
+
 static inline void
 fd_resource_lock(struct fd_resource *rsc)
 {

commit e292576b9ac4446812b83406587719f46dacb5b4

freedreno: Fix think-o in fd_resource_wait()
Fixes: 4921f927e32 ("freedreno: Add perf_debug logging for bo stalls")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9264>

src/gallium/drivers/freedreno/freedreno_resource.c
@@ -190,7 +190,7 @@ __fd_resource_wait(struct fd_context *ctx, struct fd_resource *rsc,
 	elapsed += os_time_get_nano();
 	if (elapsed > 10000) /* 0.01ms */ {
 		perf_debug_ctx(ctx, "%s: a busy \"%"PRSC_FMT"\" BO stalled and took %.03f ms.\n",
-				func, PRSC_ARGS(&rsc->base), 1000000 * (double)elapsed);
+				func, PRSC_ARGS(&rsc->base), (double)elapsed / 1000000.0);
 	}
 
 	return ret;