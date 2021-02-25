openharmony/third_party_mesa3d

commit 11ad3743e2b5d43874072e63fd9e9ef2319b9ce8

android: pan/bi: Separate disasm/compiler targets
Fixes the following building errors:

ld.lld: error: undefined symbol: bi_interp_mode_name
>>> referenced by bi_print.c:207 (external/mesa/src/panfrost/bifrost/bi_print.c:207)
...
ld.lld: error: undefined symbol: bi_round_mode_name
>>> referenced by bi_print.c:285 (external/mesa/src/panfrost/bifrost/bi_print.c:285)

ld.lld: error: undefined symbol: bi_clause_type_name
>>> referenced by disassemble.c:142 (external/mesa/src/panfrost/bifrost/disassemble.c:142)
...
ld.lld: error: undefined symbol: bi_ldst_type_name
>>> referenced by disassemble.c:0 (external/mesa/src/panfrost/bifrost/disassemble.c:0)

Fixes: 7703fa509 ("pan/bi: Separate disasm/compiler targets")
Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6261>

src/gallium/targets/dri/Android.mk
@@ -62,6 +62,7 @@ LOCAL_STATIC_LIBRARIES += \
 	libfreedreno_perfcntrs \
 	libmesa_gallium \
 	libpanfrost_bifrost \
+	libpanfrost_bifrost_disasm \
 	libpanfrost_lib \
 	libpanfrost_midgard \
 	libpanfrost_midgard_disasm \

src/panfrost/Android.bifrost.mk
@@ -18,6 +18,31 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+# build libpanfrost_bifrost_disasm
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libpanfrost_bifrost_disasm
+
+LOCAL_SRC_FILES := \
+	$(bifrost_disasm_FILES)
+
+LOCAL_C_INCLUDES := \
+	$(MESA_TOP)/include \
+	$(MESA_TOP)/src/compiler/nir/ \
+	$(MESA_TOP)/src/gallium/auxiliary/ \
+	$(MESA_TOP)/src/gallium/include/ \
+	$(MESA_TOP)/src/mapi/ \
+	$(MESA_TOP)/src/mesa/ \
+	$(MESA_TOP)/src/panfrost/bifrost/ \
+	$(MESA_TOP)/src/panfrost/include/
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := \
+	$(MESA_TOP)/src/panfrost/bifrost/ \
+
+include $(MESA_COMMON_MK)
+include $(BUILD_STATIC_LIBRARY)
+
+# build libpanfrost_bifrost
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libpanfrost_bifrost

src/panfrost/Makefile.sources
@@ -16,9 +16,13 @@ bifrost_FILES := \
         bifrost/bi_quirks.h \
         bifrost/bir.c \
         bifrost/compiler.h \
-        bifrost/cmdline.c \
+        bifrost/cmdline.c
+
+bifrost_disasm_FILES := \
         bifrost/disassemble.c \
-        bifrost/disassemble.h
+        bifrost/disassemble.h \
+        bifrost/bi_print_common.c \
+        bifrost/bi_print_common.h
 
 lib_FILES := \
         lib/decode_common.c \

commit cde0c32ede0a7750db2bf495a20d60032f6d14d3

android: pan/bi: reorder static dependencies in gallium/dri
libpanfrost_lib depends on libpanfrost_bifrost for 'bifrost_compile_shader_nir' symbol
libpanfrost_lib depends on libpanfrost_bifrost_disasm for 'disassemble_bifrost' symbol
LOCAL_STATIC_LIBRARIES requires proper ordering to make the symbols available

Fixes the following building error happening with Android P:

FAILED: out/target/product/x86_64/obj/SHARED_LIBRARIES/gallium_dri_intermediates/LINKED/gallium_dri.so
external/mesa/src/panfrost/lib/decode.c:534: error: undefined reference to 'disassemble_bifrost'
external/mesa/src/panfrost/lib/pan_shader.c:145: error: undefined reference to 'bifrost_compile_shader_nir'

Cc: 20.3 21.0 <mesa-stable@lists.freedesktop.org>
Fixes: 11ad374 ("android: pan/bi: Separate disasm/compiler targets")
Acked-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9265>

src/gallium/targets/dri/Android.mk
@@ -63,9 +63,9 @@ LOCAL_STATIC_LIBRARIES += \
 	libfreedreno_ir3 \
 	libfreedreno_perfcntrs \
 	libmesa_gallium \
+	libpanfrost_lib \
 	libpanfrost_bifrost \
 	libpanfrost_bifrost_disasm \
-	libpanfrost_lib \
 	libpanfrost_midgard \
 	libpanfrost_midgard_disasm \
 	libpanfrost_shared \