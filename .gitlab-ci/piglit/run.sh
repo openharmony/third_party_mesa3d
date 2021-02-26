openharmony/third_party_mesa3d

commit 422eee8819039a5e63dcecb4df125b8308fbf8a8

ci/piglit: Stop including the test counts at the end of expectations.
It's just a ton of fuss for driver developers fixing piglit tests.  This
makes the trace expectation files pretty silly (empty expectation, but
you'll get a diff to a non-empty result when something fails)

Acked-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Acked-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Acked-by: Adam Jackson <ajax@redhat.com>
Acked-by: Andres Gomez <agomez@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9226>

.gitlab-ci/piglit/run.sh
@@ -230,7 +230,9 @@ RESULTSFILE="$RESULTS/$PIGLIT_RESULTS.txt"
 mkdir -p .gitlab-ci/piglit
 ./piglit summary console "$RESULTS"/results.json.bz2 \
     | tee ".gitlab-ci/piglit/$PIGLIT_RESULTS.txt.orig" \
-    | head -n -1 | grep -v ": pass" > $RESULTSFILE
+    | head -n -1 | grep -v ": pass" \
+    | sed '/^summary:/Q' \
+    > $RESULTSFILE
 
 if [ "x$PIGLIT_PROFILES" = "xreplay" ] \
        && [ ${PIGLIT_REPLAY_UPLOAD_TO_MINIO:-0} -eq 1 ]; then

.gitlab-ci/piglit/run_cl.sh
@@ -43,7 +43,11 @@ set -e
 
 PIGLIT_RESULTS=${PIGLIT_RESULTS:-$PIGLIT_PROFILES}
 mkdir -p .gitlab-ci/piglit
-./piglit summary console $OLDPWD/results | head -n -1 | grep -v ": pass" > .gitlab-ci/piglit/$PIGLIT_RESULTS.txt
+./piglit summary console $OLDPWD/results \
+  | head -n -1 \
+  | grep -v ": pass" \
+  | sed '/^summary:/Q' \
+  > .gitlab-ci/piglit/$PIGLIT_RESULTS.txt
 
 if [ -n "$USE_CASELIST" ]; then
     # Just filter the expected results based on the tests that were actually

commit 063e6bc007ad1ebe23e3b474335f338ece4fb745

ci/piglit: fix parallel piglit results
Commit 422eee88190 removed the summary from the expected piglit
results, but handling of results when using parallel jobs was also doing
the same, which ends up on removing too many lines from results.

Fixes: 422eee88190 ("ci/piglit: Stop including the test counts at the
end of expectations.")

Acked-by: Eric Anholt <eric@anholt.net>
Signed-off-by: Juan A. Suarez Romero <jasuarez@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9286>

.gitlab-ci/piglit/run.sh
@@ -255,9 +255,7 @@ fi
 if [ -n "$USE_CASELIST" ]; then
     # Just filter the expected results based on the tests that were actually
     # executed, and switch to the version with no summary
-    cat $RESULTSFILE | head -n -16 > ".gitlab-ci/piglit/$PIGLIT_RESULTS.txt.new"
-    RESULTSFILE=".gitlab-ci/piglit/$PIGLIT_RESULTS.txt.new"
-    cat ".gitlab-ci/piglit/$PIGLIT_RESULTS.txt.orig" | head -n -17 | rev \
+    cat ".gitlab-ci/piglit/$PIGLIT_RESULTS.txt.orig" | sed '/^summary:/Q' | rev \
         | cut -f2- -d: | rev | sed "s/$/:/g" > /tmp/executed.txt
 
     grep -F -f /tmp/executed.txt "$INSTALL/$PIGLIT_RESULTS.txt" \

.gitlab-ci/piglit/run_cl.sh
@@ -44,6 +44,7 @@ set -e
 PIGLIT_RESULTS=${PIGLIT_RESULTS:-$PIGLIT_PROFILES}
 mkdir -p .gitlab-ci/piglit
 ./piglit summary console $OLDPWD/results \
+  | tee ".gitlab-ci/piglit/$PIGLIT_RESULTS.txt.orig" \
   | head -n -1 \
   | grep -v ": pass" \
   | sed '/^summary:/Q' \
@@ -52,21 +53,20 @@ mkdir -p .gitlab-ci/piglit
 if [ -n "$USE_CASELIST" ]; then
     # Just filter the expected results based on the tests that were actually
     # executed, and switch to the version with no summary
-    cat .gitlab-ci/piglit/$PIGLIT_RESULTS.txt | head -n -16 | tee .gitlab-ci/piglit/$PIGLIT_RESULTS.txt.new \
-         | rev | cut -f2- -d: | rev | sed "s/$/:/g" > /tmp/executed.txt
+    cat .gitlab-ci/piglit/$PIGLIT_RESULTS.txt.orig | sed '/^summary:/Q' | rev \
+         | cut -f2- -d: | rev | sed "s/$/:/g" > /tmp/executed.txt
     grep -F -f /tmp/executed.txt $OLDPWD/install/$PIGLIT_RESULTS.txt \
          > .gitlab-ci/piglit/$PIGLIT_RESULTS.txt.baseline || true
 else
     cp $OLDPWD/install/$PIGLIT_RESULTS.txt .gitlab-ci/piglit/$PIGLIT_RESULTS.txt.baseline
-    cp .gitlab-ci/piglit/$PIGLIT_RESULTS.txt .gitlab-ci/piglit/$PIGLIT_RESULTS.txt.new
 fi
 
-if diff -q .gitlab-ci/piglit/$PIGLIT_RESULTS.txt.{baseline,new}; then
+if diff -q .gitlab-ci/piglit/$PIGLIT_RESULTS.txt{.baseline,}; then
     exit 0
 fi
 
 ./piglit summary html --exclude-details=pass $OLDPWD/summary $OLDPWD/results
 
 echo Unexpected change in results:
-diff -u .gitlab-ci/piglit/$PIGLIT_RESULTS.txt.{baseline,new}
+diff -u .gitlab-ci/piglit/$PIGLIT_RESULTS.txt{.baseline,}
 exit 1