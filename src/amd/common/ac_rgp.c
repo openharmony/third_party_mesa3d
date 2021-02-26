openharmony/third_party_mesa3d

commit 678e32faeb8bb3ff287aa47b2a7b7f24ad33b434

ac/rgp: report the number of primitives per clock
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9303>

src/amd/common/ac_rgp.c
@@ -486,7 +486,9 @@ static void ac_fill_sqtt_asic_info(struct radeon_info *rad_info,
 
    chunk->alu_per_clock = 0.0;
    chunk->texture_per_clock = 0.0;
-   chunk->prims_per_clock = 0.0;
+   chunk->prims_per_clock = rad_info->max_se;
+   if (rad_info->chip_class >= GFX10)
+      chunk->prims_per_clock *= 2;
    chunk->pixels_per_clock = 0.0;
 
    chunk->gpu_timestamp_frequency = rad_info->clock_crystal_freq * 1000;

commit 38ba0df488b95798f99cff9495b40a0b1f4ee05f

ac/rgp: Only report double the prims per clock on GFX10.
Misinterpreted review comment.

Fixes: 678e32faeb8 ("ac/rgp: report the number of primitives per clock")
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9312>

src/amd/common/ac_rgp.c
@@ -487,7 +487,7 @@ static void ac_fill_sqtt_asic_info(struct radeon_info *rad_info,
    chunk->alu_per_clock = 0.0;
    chunk->texture_per_clock = 0.0;
    chunk->prims_per_clock = rad_info->max_se;
-   if (rad_info->chip_class >= GFX10)
+   if (rad_info->chip_class == GFX10)
       chunk->prims_per_clock *= 2;
    chunk->pixels_per_clock = 0.0;
 