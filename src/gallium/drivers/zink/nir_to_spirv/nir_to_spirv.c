openharmony/third_party_mesa3d

commit ae1430bd261d7a9af67ad6312366bf2eb890b88d

zink: fix streamout for clipdistance
the assert added here during review broke this since clipdistance is 8 components

Fixes: 0280c52374a ("zink: tweak xfb slot mapping in ntv")

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8673>

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -1249,7 +1249,7 @@ emit_so_outputs(struct ntv_context *ctx,
       while (!output)
          output = ctx->outputs[location--];
       location++;
-      assert(orig_location - location < 4);
+      assert(orig_location - location < 8);
       SpvId output_type = ctx->so_output_types[location];
       const struct glsl_type *out_type = ctx->so_output_gl_types[location];
 

commit b3ecad823f937faa2ddb23bc37582794c6e294c8

zink: remove ntv streamout assert
this was added during review, but it was never correct and just crashes
valid cases like streamout from a mat3x4 type

Fixes: ae1430bd261 ("zink: fix streamout for clipdistance")

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9271>

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -1309,7 +1309,6 @@ emit_so_outputs(struct ntv_context *ctx,
       while (!output)
          output = ctx->outputs[location--];
       location++;
-      assert(orig_location - location < 8);
       SpvId output_type = ctx->so_output_types[location];
       const struct glsl_type *out_type = ctx->so_output_gl_types[location];
 