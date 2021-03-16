openharmony/third_party_mesa3d

commit 86e11e791353807ee6c4a6c2e7ee43023838b429

lavapipe: implement VK_EXT_index_type_uint8
Since Gallium supports 8 bit indices, this extension is a simple matter
of plumbing a value through, exposing a feature and flipping the switch
for the extension. This lets zink avoid up-converting the index-buffer
before drawing.

Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8082>

src/gallium/frontends/lavapipe/lvp_execute.c
@@ -1917,6 +1917,9 @@ static void handle_index_buffer(struct lvp_cmd_buffer_entry *cmd,
 {
    struct lvp_cmd_bind_index_buffer *ib = &cmd->u.index_buffer;
    switch (ib->index_type) {
+   case VK_INDEX_TYPE_UINT8_EXT:
+      state->index_size = 1;
+      break;
    case VK_INDEX_TYPE_UINT16:
       state->index_size = 2;
       break;

src/gallium/frontends/lavapipe/lvp_device.c
@@ -386,6 +386,13 @@ void lvp_GetPhysicalDeviceFeatures2(
          break;
       }
 
+      case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_INDEX_TYPE_UINT8_FEATURES_EXT: {
+         VkPhysicalDeviceIndexTypeUint8FeaturesEXT *features =
+            (VkPhysicalDeviceIndexTypeUint8FeaturesEXT *)ext;
+         features->indexTypeUint8 = true;
+         break;
+      }
+
       default:
          break;
       }

src/gallium/frontends/lavapipe/lvp_extensions.py
@@ -123,7 +123,7 @@ EXTENSIONS = [
     Extension('VK_EXT_external_memory_host',              1, False),
     Extension('VK_EXT_global_priority',                   1, False),
     Extension('VK_EXT_host_query_reset',                  1, False),
-    Extension('VK_EXT_index_type_uint8',                  1, False),
+    Extension('VK_EXT_index_type_uint8',                  1, True),
     Extension('VK_EXT_inline_uniform_block',              1, False),
     Extension('VK_EXT_memory_budget',                     1, False),
     Extension('VK_EXT_memory_priority',                   1, False),

commit e3163a8bc2408f9c8688eb964089b17d70c0a003

lavapipe: fix primitive-restart for uint8 indices
I forgot to add an implicit primitive-restart index for uint8 while
adding support for this initially. But we also have a nice little
helper to simplify this, so let's use that instead of open-coding.

Fixes: 86e11e79135 ("lavapipe: implement VK_EXT_index_type_uint8")
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9622>

src/gallium/frontends/lavapipe/lvp_execute.c
@@ -38,6 +38,7 @@
 #include "util/u_sampler.h"
 #include "util/u_box.h"
 #include "util/u_inlines.h"
+#include "util/u_prim_restart.h"
 #include "util/format/u_format_zs.h"
 
 #include "vk_util.h"
@@ -2114,12 +2115,9 @@ static void handle_draw_indexed(struct lvp_cmd_buffer_entry *cmd,
    state->info.index_bias = cmd->u.draw_indexed.vertex_offset;
    state->info.view_mask = subpass->view_mask;
 
-   if (state->info.primitive_restart) {
-      if (state->info.index_size == 4)
-         state->info.restart_index = 0xffffffff;
-      else
-         state->info.restart_index = 0xffff;
-   }
+   if (state->info.primitive_restart)
+      state->info.restart_index = util_prim_restart_index_from_size(state->info.index_size);
+
    state->pctx->draw_vbo(state->pctx, &state->info, NULL, &state->draw, 1);
 }
 