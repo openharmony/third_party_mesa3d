openharmony/third_party_mesa3d

commit 19928e782dca5d57a99a97daa39f2d6bb09547ee

zink: ralloc screen objects
Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9541>

src/gallium/drivers/zink/zink_screen.c
@@ -865,7 +865,7 @@ zink_destroy_screen(struct pipe_screen *pscreen)
    vkDestroyInstance(screen->instance, NULL);
 
    slab_destroy_parent(&screen->transfer_pool);
-   FREE(screen);
+   ralloc_free(screen);
 }
 
 static void
@@ -1275,7 +1275,7 @@ pre_hash_descriptor_states(struct zink_screen *screen)
 static struct zink_screen *
 zink_internal_create_screen(const struct pipe_screen_config *config)
 {
-   struct zink_screen *screen = CALLOC_STRUCT(zink_screen);
+   struct zink_screen *screen = rzalloc(NULL, struct zink_screen);
    if (!screen)
       return NULL;
 
@@ -1371,7 +1371,7 @@ zink_internal_create_screen(const struct pipe_screen_config *config)
    return screen;
 
 fail:
-   FREE(screen);
+   ralloc_free(screen);
    return NULL;
 }
 

commit 600f6dbf35708310164eb2d369afa948fa97165d

zink: fix free of ralloced pointer
When we alloc with ralloc, we also need to free with it.

But let's take a step back here; we don't just need to use ralloc, we
also need to destroy all other screen-resources. So let's call the
destructor here instead.

Fixes: 19928e782dc ("zink: ralloc screen objects")
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9647>

src/gallium/drivers/zink/zink_screen.c
@@ -1461,7 +1461,7 @@ zink_drm_create_screen(int fd, const struct pipe_screen_config *config)
 
    if (ret && !ret->info.have_KHR_external_memory_fd) {
       debug_printf("ZINK: KHR_external_memory_fd required!\n");
-      free(ret);
+      zink_destroy_screen(&ret->base);
       return NULL;
    }
 