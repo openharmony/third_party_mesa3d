openharmony/third_party_mesa3d

commit 2ae85ee75d603abc4395e1b0b2a830172ddeea71

mesa/st: handle running nir lower passes for ucp and psiz in tess stage
if tess is the last shader program to run then we need to perform these
lowering passes at that time rather than during the vs

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5885>

src/mesa/state_tracker/st_context.c
@@ -260,7 +260,7 @@ st_invalidate_state(struct gl_context *ctx)
 
    /* Update the vertex shader if ctx->Point was changed. */
    if (st->lower_point_size && new_state & _NEW_POINT)
-      st->dirty |= ST_NEW_VS_STATE | ST_NEW_GS_STATE;
+      st->dirty |= ST_NEW_VS_STATE | ST_NEW_TES_STATE | ST_NEW_GS_STATE;
 
    /* Which shaders are dirty will be determined manually. */
    if (new_state & _NEW_PROGRAM) {
@@ -723,15 +723,6 @@ st_create_context_priv(struct gl_context *ctx, struct pipe_context *pipe,
    st_init_limits(screen, &ctx->Const, &ctx->Extensions);
    st_init_extensions(screen, &ctx->Const,
                       &ctx->Extensions, &st->options, ctx->API);
-
-   /* FIXME: add support for geometry and tessellation shaders for
-    * lower_point_size
-    */
-   assert(!ctx->Extensions.OES_geometry_shader || !st->lower_point_size);
-   assert(!ctx->Extensions.ARB_tessellation_shader || !st->lower_point_size);
-
-   /* FIXME: add support for tessellation shaders for lower_ucp */
-   assert(!ctx->Extensions.ARB_tessellation_shader || !st->lower_ucp);
 
    if (st_have_perfmon(st)) {
       ctx->Extensions.AMD_performance_monitor = GL_TRUE;

src/mesa/state_tracker/st_atom_shader.c
@@ -225,16 +225,16 @@ st_update_vp( struct st_context *st )
          key.clip_negative_one_to_one =
                st->ctx->Transform.ClipDepthMode == GL_NEGATIVE_ONE_TO_ONE;
 
-      if (!st->ctx->GeometryProgram._Current) {
+      if (!st->ctx->GeometryProgram._Current &&
+          !st->ctx->TessEvalProgram._Current) {
          /* _NEW_POINT */
          key.lower_point_size = st->lower_point_size &&
                                 !st_point_size_per_vertex(st->ctx);
-      }
 
-      /* _NEW_TRANSFORM */
-      if (st->lower_ucp && st_user_clip_planes_enabled(st->ctx) &&
-          !st->ctx->GeometryProgram._Current)
-         key.lower_ucp = st->ctx->Transform.ClipPlanesEnabled;
+         /* _NEW_TRANSFORM */
+         if (st->lower_ucp && st_user_clip_planes_enabled(st->ctx))
+            key.lower_ucp = st->ctx->Transform.ClipPlanesEnabled;
+      }
 
       simple_mtx_lock(&st->ctx->Shared->Mutex);
       st->vp_variant = st_get_vp_variant(st, stvp, &key);

commit 3965a5c0ad201a5e68f8c46ac28e8970d0cc63ad

gallium/st: fix shader_has_one_variant
I think we need to care about this here as well, otherwise the variant
logic might be short-circuited.

Fixes: 2ae85ee75d6 ("mesa/st: handle running nir lower passes for ucp and psiz in tess stage")
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9631>

src/mesa/state_tracker/st_context.c
@@ -832,7 +832,8 @@ st_create_context_priv(struct gl_context *ctx, struct pipe_context *pipe,
          st->has_shareable_shaders &&
          !st->clamp_frag_depth_in_shader &&
          !st->clamp_vert_color_in_shader &&
-         !st->lower_point_size;
+         !st->lower_point_size &&
+         !st->lower_ucp;
 
    st->shader_has_one_variant[MESA_SHADER_GEOMETRY] =
          st->has_shareable_shaders &&