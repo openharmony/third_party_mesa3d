openharmony/third_party_mesa3d

commit eb46532fec41674052a0be60b506d4c83762ce5d

zink: force stencil format for stencil-only samplers and swizzle the right component
packed buffers will still return the full format when we're using only the
stencil aspect, so we need to explicitly set the format here

also due to 72ad30c064786048d0e260347bdfe7a6392869b9 we can't use the provided
swizzle for this case and have to force y -> x component swizzle

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7491>

src/gallium/drivers/zink/zink_context.c
@@ -330,14 +330,20 @@ zink_create_sampler_view(struct pipe_context *pctx, struct pipe_resource *pres,
       ivci.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
       ivci.image = res->image;
       ivci.viewType = image_view_type(state->target);
-      ivci.format = zink_get_format(screen, state->format);
-      assert(ivci.format);
+
       ivci.components.r = component_mapping(state->swizzle_r);
       ivci.components.g = component_mapping(state->swizzle_g);
       ivci.components.b = component_mapping(state->swizzle_b);
       ivci.components.a = component_mapping(state->swizzle_a);
-
       ivci.subresourceRange.aspectMask = sampler_aspect_from_format(state->format);
+      /* samplers for stencil aspects of packed formats need to always use stencil type */
+      if (ivci.subresourceRange.aspectMask == VK_IMAGE_ASPECT_STENCIL_BIT) {
+         ivci.format = VK_FORMAT_S8_UINT;
+         ivci.components.g = VK_COMPONENT_SWIZZLE_R;
+      } else
+         ivci.format = zink_get_format(screen, state->format);
+      assert(ivci.format);
+
       ivci.subresourceRange.baseMipLevel = state->u.tex.first_level;
       ivci.subresourceRange.baseArrayLayer = state->u.tex.first_layer;
       ivci.subresourceRange.levelCount = state->u.tex.last_level - state->u.tex.first_level + 1;

commit 93ffe22038effc96ceff499692c44f3ad4e71c06

zink: always use requested format for sampler view creation
this should be fine and good

Fixes: eb46532fec4 ("zink: force stencil format for stencil-only samplers and swizzle the right component")

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9673>

src/gallium/drivers/zink/zink_context.c
@@ -617,12 +617,11 @@ zink_create_sampler_view(struct pipe_context *pctx, struct pipe_resource *pres,
       ivci.components.b = component_mapping(state->swizzle_b);
       ivci.components.a = component_mapping(state->swizzle_a);
       ivci.subresourceRange.aspectMask = sampler_aspect_from_format(state->format);
-      /* samplers for stencil aspects of packed formats need to always use stencil type */
+      /* samplers for stencil aspects of packed formats need to always use stencil swizzle */
       if (ivci.subresourceRange.aspectMask == VK_IMAGE_ASPECT_STENCIL_BIT) {
-         ivci.format = VK_FORMAT_S8_UINT;
          ivci.components.g = VK_COMPONENT_SWIZZLE_R;
-      } else
-         ivci.format = zink_get_format(screen, state->format);
+      }
+      ivci.format = zink_get_format(screen, state->format);
       assert(ivci.format);
 
       ivci.subresourceRange.baseMipLevel = state->u.tex.first_level;