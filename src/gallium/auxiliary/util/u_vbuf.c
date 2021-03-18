openharmony/third_party_mesa3d

commit 6a25287e0d37c27ca5c24aca994eb62af78763c0

gallium: extend draw_vbo to support multi draws
Essentially rename multi_draw to draw_vbo and remove start and count
from pipe_draw_info.

This is only an interface change. It doesn't add multi draw support
anywhere.

Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7441>

src/gallium/auxiliary/util/u_vbuf.c
@@ -409,6 +409,7 @@ void u_vbuf_destroy(struct u_vbuf *mgr)
 static enum pipe_error
 u_vbuf_translate_buffers(struct u_vbuf *mgr, struct translate_key *key,
                          const struct pipe_draw_info *info,
+                         const struct pipe_draw_start_count *draw,
                          unsigned vb_mask, unsigned out_vb,
                          int start_vertex, unsigned num_vertices,
                          int min_index, boolean unroll_indices)
@@ -475,12 +476,12 @@ u_vbuf_translate_buffers(struct u_vbuf *mgr, struct translate_key *key,
    /* Translate. */
    if (unroll_indices) {
       struct pipe_transfer *transfer = NULL;
-      const unsigned offset = info->start * info->index_size;
+      const unsigned offset = draw->start * info->index_size;
       uint8_t *map;
 
       /* Create and map the output buffer. */
       u_upload_alloc(mgr->pipe->stream_uploader, 0,
-                     key->output_stride * info->count, 4,
+                     key->output_stride * draw->count, 4,
                      &out_offset, &out_buffer,
                      (void**)&out_map);
       if (!out_buffer)
@@ -490,19 +491,19 @@ u_vbuf_translate_buffers(struct u_vbuf *mgr, struct translate_key *key,
          map = (uint8_t*)info->index.user + offset;
       } else {
          map = pipe_buffer_map_range(mgr->pipe, info->index.resource, offset,
-                                     info->count * info->index_size,
+                                     draw->count * info->index_size,
                                      PIPE_MAP_READ, &transfer);
       }
 
       switch (info->index_size) {
       case 4:
-         tr->run_elts(tr, (unsigned*)map, info->count, 0, 0, out_map);
+         tr->run_elts(tr, (unsigned*)map, draw->count, 0, 0, out_map);
          break;
       case 2:
-         tr->run_elts16(tr, (uint16_t*)map, info->count, 0, 0, out_map);
+         tr->run_elts16(tr, (uint16_t*)map, draw->count, 0, 0, out_map);
          break;
       case 1:
-         tr->run_elts8(tr, map, info->count, 0, 0, out_map);
+         tr->run_elts8(tr, map, draw->count, 0, 0, out_map);
          break;
       }
 
@@ -611,6 +612,7 @@ u_vbuf_translate_find_free_vb_slots(struct u_vbuf *mgr,
 static boolean
 u_vbuf_translate_begin(struct u_vbuf *mgr,
                        const struct pipe_draw_info *info,
+                       const struct pipe_draw_start_count *draw,
                        int start_vertex, unsigned num_vertices,
                        int min_index, boolean unroll_indices)
 {
@@ -717,8 +719,8 @@ u_vbuf_translate_begin(struct u_vbuf *mgr,
    for (type = 0; type < VB_NUM; type++) {
       if (key[type].nr_elements) {
          enum pipe_error err;
-         err = u_vbuf_translate_buffers(mgr, &key[type], info, mask[type],
-                                        mgr->fallback_vbs[type],
+         err = u_vbuf_translate_buffers(mgr, &key[type], info, draw,
+                                        mask[type], mgr->fallback_vbs[type],
                                         start[type], num[type], min_index,
                                         unroll_indices && type == VB_VERTEX);
          if (err != PIPE_OK)
@@ -1133,10 +1135,11 @@ static boolean u_vbuf_mapping_vertex_buffer_blocks(const struct u_vbuf *mgr)
 
 static void
 u_vbuf_get_minmax_index_mapped(const struct pipe_draw_info *info,
+                               unsigned count,
                                const void *indices, unsigned *out_min_index,
                                unsigned *out_max_index)
 {
-   if (!info->count) {
+   if (!count) {
       *out_min_index = 0;
       *out_max_index = 0;
       return;
@@ -1148,7 +1151,7 @@ u_vbuf_get_minmax_index_mapped(const struct pipe_draw_info *info,
       unsigned max = 0;
       unsigned min = ~0u;
       if (info->primitive_restart) {
-         for (unsigned i = 0; i < info->count; i++) {
+         for (unsigned i = 0; i < count; i++) {
             if (ui_indices[i] != info->restart_index) {
                if (ui_indices[i] > max) max = ui_indices[i];
                if (ui_indices[i] < min) min = ui_indices[i];
@@ -1156,7 +1159,7 @@ u_vbuf_get_minmax_index_mapped(const struct pipe_draw_info *info,
          }
       }
       else {
-         for (unsigned i = 0; i < info->count; i++) {
+         for (unsigned i = 0; i < count; i++) {
             if (ui_indices[i] > max) max = ui_indices[i];
             if (ui_indices[i] < min) min = ui_indices[i];
          }
@@ -1170,7 +1173,7 @@ u_vbuf_get_minmax_index_mapped(const struct pipe_draw_info *info,
       unsigned short max = 0;
       unsigned short min = ~((unsigned short)0);
       if (info->primitive_restart) {
-         for (unsigned i = 0; i < info->count; i++) {
+         for (unsigned i = 0; i < count; i++) {
             if (us_indices[i] != info->restart_index) {
                if (us_indices[i] > max) max = us_indices[i];
                if (us_indices[i] < min) min = us_indices[i];
@@ -1178,7 +1181,7 @@ u_vbuf_get_minmax_index_mapped(const struct pipe_draw_info *info,
          }
       }
       else {
-         for (unsigned i = 0; i < info->count; i++) {
+         for (unsigned i = 0; i < count; i++) {
             if (us_indices[i] > max) max = us_indices[i];
             if (us_indices[i] < min) min = us_indices[i];
          }
@@ -1192,7 +1195,7 @@ u_vbuf_get_minmax_index_mapped(const struct pipe_draw_info *info,
       unsigned char max = 0;
       unsigned char min = ~((unsigned char)0);
       if (info->primitive_restart) {
-         for (unsigned i = 0; i < info->count; i++) {
+         for (unsigned i = 0; i < count; i++) {
             if (ub_indices[i] != info->restart_index) {
                if (ub_indices[i] > max) max = ub_indices[i];
                if (ub_indices[i] < min) min = ub_indices[i];
@@ -1200,7 +1203,7 @@ u_vbuf_get_minmax_index_mapped(const struct pipe_draw_info *info,
          }
       }
       else {
-         for (unsigned i = 0; i < info->count; i++) {
+         for (unsigned i = 0; i < count; i++) {
             if (ub_indices[i] > max) max = ub_indices[i];
             if (ub_indices[i] < min) min = ub_indices[i];
          }
@@ -1216,6 +1219,7 @@ u_vbuf_get_minmax_index_mapped(const struct pipe_draw_info *info,
 
 void u_vbuf_get_minmax_index(struct pipe_context *pipe,
                              const struct pipe_draw_info *info,
+                             const struct pipe_draw_start_count *draw,
                              unsigned *out_min_index, unsigned *out_max_index)
 {
    struct pipe_transfer *transfer = NULL;
@@ -1223,15 +1227,16 @@ void u_vbuf_get_minmax_index(struct pipe_context *pipe,
 
    if (info->has_user_indices) {
       indices = (uint8_t*)info->index.user +
-                info->start * info->index_size;
+                draw->start * info->index_size;
    } else {
       indices = pipe_buffer_map_range(pipe, info->index.resource,
-                                      info->start * info->index_size,
-                                      info->count * info->index_size,
+                                      draw->start * info->index_size,
+                                      draw->count * info->index_size,
                                       PIPE_MAP_READ, &transfer);
    }
 
-   u_vbuf_get_minmax_index_mapped(info, indices, out_min_index, out_max_index);
+   u_vbuf_get_minmax_index_mapped(info, draw->count, indices,
+                                  out_min_index, out_max_index);
 
    if (transfer) {
       pipe_buffer_unmap(pipe, transfer);
@@ -1259,24 +1264,26 @@ u_vbuf_split_indexed_multidraw(struct u_vbuf *mgr, struct pipe_draw_info *info,
    assert(info->index_size);
 
    for (unsigned i = 0; i < draw_count; i++) {
+      struct pipe_draw_start_count draw;
       unsigned offset = i * stride / 4;
 
-      info->count = indirect_data[offset + 0];
+      draw.count = indirect_data[offset + 0];
       info->instance_count = indirect_data[offset + 1];
 
-      if (!info->count || !info->instance_count)
+      if (!draw.count || !info->instance_count)
          continue;
 
-      info->start = indirect_data[offset + 2];
+      draw.start = indirect_data[offset + 2];
       info->index_bias = indirect_data[offset + 3];
       info->start_instance = indirect_data[offset + 4];
 
-      u_vbuf_draw_vbo(mgr, info, NULL);
+      u_vbuf_draw_vbo(mgr, info, NULL, draw);
    }
 }
 
 void u_vbuf_draw_vbo(struct u_vbuf *mgr, const struct pipe_draw_info *info,
-                     const struct pipe_draw_indirect_info *indirect)
+                     const struct pipe_draw_indirect_info *indirect,
+                     const struct pipe_draw_start_count draw)
 {
    struct pipe_context *pipe = mgr->pipe;
    int start_vertex;
@@ -1288,6 +1295,7 @@ void u_vbuf_draw_vbo(struct u_vbuf *mgr, const struct pipe_draw_info *info,
    const uint32_t incompatible_vb_mask =
       mgr->incompatible_vb_mask & used_vb_mask;
    struct pipe_draw_info new_info;
+   struct pipe_draw_start_count new_draw;
 
    /* Normal draw. No fallback and no user buffers. */
    if (!incompatible_vb_mask &&
@@ -1299,11 +1307,12 @@ void u_vbuf_draw_vbo(struct u_vbuf *mgr, const struct pipe_draw_info *info,
          u_vbuf_set_driver_vertex_buffers(mgr);
       }
 
-      pipe->draw_vbo(pipe, info, indirect);
+      pipe->draw_vbo(pipe, info, indirect, &draw, 1);
       return;
    }
 
    new_info = *info;
+   new_draw = draw;
 
    /* Handle indirect (multi)draws. */
    if (indirect && indirect->buffer) {
@@ -1406,8 +1415,7 @@ void u_vbuf_draw_vbo(struct u_vbuf *mgr, const struct pipe_draw_info *info,
 
             /* Update the index range. */
             unsigned min, max;
-            new_info.count = count; /* only used by get_minmax_index */
-            u_vbuf_get_minmax_index_mapped(&new_info,
+            u_vbuf_get_minmax_index_mapped(&new_info, count,
                                            indices +
                                            new_info.index_size * start,
                                            &min, &max);
@@ -1435,7 +1443,7 @@ void u_vbuf_draw_vbo(struct u_vbuf *mgr, const struct pipe_draw_info *info,
           * This efficiently processes the multidraw with the time complexity
           * equal to 1 draw call.
           */
-         new_info.start = ~0u;
+         new_draw.start = ~0u;
          new_info.start_instance = ~0u;
          unsigned end_vertex = 0;
          unsigned end_instance = 0;
@@ -1447,7 +1455,7 @@ void u_vbuf_draw_vbo(struct u_vbuf *mgr, const struct pipe_draw_info *info,
             unsigned start_instance = data[offset + 3];
             unsigned instance_count = data[offset + 1];
 
-            new_info.start = MIN2(new_info.start, start);
+            new_draw.start = MIN2(new_draw.start, start);
             new_info.start_instance = MIN2(new_info.start_instance,
                                            start_instance);
 
@@ -1457,10 +1465,10 @@ void u_vbuf_draw_vbo(struct u_vbuf *mgr, const struct pipe_draw_info *info,
          free(data);
 
          /* Set the final counts. */
-         new_info.count = end_vertex - new_info.start;
+         new_draw.count = end_vertex - new_draw.start;
          new_info.instance_count = end_instance - new_info.start_instance;
 
-         if (new_info.start == ~0u || !new_info.count || !new_info.instance_count)
+         if (new_draw.start == ~0u || !new_draw.count || !new_info.instance_count)
             return;
       }
    }
@@ -1474,7 +1482,7 @@ void u_vbuf_draw_vbo(struct u_vbuf *mgr, const struct pipe_draw_info *info,
             min_index = new_info.min_index;
             max_index = new_info.max_index;
          } else {
-            u_vbuf_get_minmax_index(mgr->pipe, &new_info,
+            u_vbuf_get_minmax_index(mgr->pipe, &new_info, &new_draw,
                                     &min_index, &max_index);
          }
 
@@ -1489,7 +1497,7 @@ void u_vbuf_draw_vbo(struct u_vbuf *mgr, const struct pipe_draw_info *info,
           * performance. */
          if (!indirect &&
              !new_info.primitive_restart &&
-             util_is_vbo_upload_ratio_too_large(new_info.count, num_vertices) &&
+             util_is_vbo_upload_ratio_too_large(new_draw.count, num_vertices) &&
              !u_vbuf_mapping_vertex_buffer_blocks(mgr)) {
             unroll_indices = TRUE;
             user_vb_mask &= ~(mgr->nonzero_stride_vb_mask &
@@ -1502,8 +1510,8 @@ void u_vbuf_draw_vbo(struct u_vbuf *mgr, const struct pipe_draw_info *info,
          min_index = 0;
       }
    } else {
-      start_vertex = new_info.start;
-      num_vertices = new_info.count;
+      start_vertex = new_draw.start;
+      num_vertices = new_draw.count;
       min_index = 0;
    }
 
@@ -1511,7 +1519,8 @@ void u_vbuf_draw_vbo(struct u_vbuf *mgr, const struct pipe_draw_info *info,
    if (unroll_indices ||
        incompatible_vb_mask ||
        mgr->ve->incompatible_elem_mask) {
-      if (!u_vbuf_translate_begin(mgr, &new_info, start_vertex, num_vertices,
+      if (!u_vbuf_translate_begin(mgr, &new_info, &new_draw,
+                                  start_vertex, num_vertices,
                                   min_index, unroll_indices)) {
          debug_warn_once("u_vbuf_translate_begin() failed");
          return;
@@ -1521,8 +1530,8 @@ void u_vbuf_draw_vbo(struct u_vbuf *mgr, const struct pipe_draw_info *info,
          new_info.index_size = 0;
          new_info.index_bias = 0;
          new_info.min_index = 0;
-         new_info.max_index = new_info.count - 1;
-         new_info.start = 0;
+         new_info.max_index = new_draw.count - 1;
+         new_draw.start = 0;
       }
 
       user_vb_mask &= ~(incompatible_vb_mask |
@@ -1565,7 +1574,7 @@ void u_vbuf_draw_vbo(struct u_vbuf *mgr, const struct pipe_draw_info *info,
    u_upload_unmap(pipe->stream_uploader);
    u_vbuf_set_driver_vertex_buffers(mgr);
 
-   pipe->draw_vbo(pipe, &new_info, indirect);
+   pipe->draw_vbo(pipe, &new_info, indirect, &draw, 1);
 
    if (mgr->using_translate) {
       u_vbuf_translate_end(mgr);

commit 8dad19adfd03eaa43e8845f41998fdc8eb415f0c

gallium/u_vbuf: use updated pipe_draw_start_count while using draw_vbo
new_draw has updated draw count. Stale info in draw caused regression
with piglit gl-3.0-multidrawarrays-vertexid -indirect

fixes piglit test ./gl-3.0-multidrawarrays-vertexid -indirect

Fixes: 6a25287e0d37 ("gallium: extend draw_vbo to support multi draws")

Reviewed-by: Brian Paul <brianp@vmware.com>
Reviewed-by: Charmaine Lee <charmainel@vmware.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9656>

src/gallium/auxiliary/util/u_vbuf.c
@@ -1597,7 +1597,7 @@ void u_vbuf_draw_vbo(struct u_vbuf *mgr, const struct pipe_draw_info *info,
    u_upload_unmap(pipe->stream_uploader);
    u_vbuf_set_driver_vertex_buffers(mgr);
 
-   pipe->draw_vbo(pipe, &new_info, indirect, &draw, 1);
+   pipe->draw_vbo(pipe, &new_info, indirect, &new_draw, 1);
 
    if (mgr->using_translate) {
       u_vbuf_translate_end(mgr);