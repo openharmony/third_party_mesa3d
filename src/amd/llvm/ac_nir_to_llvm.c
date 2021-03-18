openharmony/third_party_mesa3d

commit decac320d2b54b96280937db81ffc7d5478f7921

ac/llvm: Emit more efficient code for load_shared.
Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9201>

src/amd/llvm/ac_nir_to_llvm.c
@@ -2960,18 +2960,16 @@ static LLVMValueRef visit_first_invocation(struct ac_nir_context *ctx)
 
 static LLVMValueRef visit_load_shared(struct ac_nir_context *ctx, const nir_intrinsic_instr *instr)
 {
-   LLVMValueRef values[4], derived_ptr, index, ret;
-
+   unsigned alignment = nir_intrinsic_align(instr);
    unsigned const_off = nir_intrinsic_base(instr);
+
    LLVMValueRef ptr = get_memory_ptr(ctx, instr->src[0], instr->dest.ssa.bit_size, const_off);
+   LLVMTypeRef result_type = get_def_type(ctx, &instr->dest.ssa);
+   int addr_space = LLVMGetPointerAddressSpace(LLVMTypeOf(ptr));
+   LLVMValueRef derived_ptr = LLVMBuildBitCast(ctx->ac.builder, ptr, LLVMPointerType(result_type, addr_space), "");
+   LLVMSetAlignment(derived_ptr, alignment);
 
-   for (int chan = 0; chan < instr->num_components; chan++) {
-      index = LLVMConstInt(ctx->ac.i32, chan, 0);
-      derived_ptr = LLVMBuildGEP(ctx->ac.builder, ptr, &index, 1, "");
-      values[chan] = LLVMBuildLoad(ctx->ac.builder, derived_ptr, "");
-   }
-
-   ret = ac_build_gather_values(&ctx->ac, values, instr->num_components);
+   LLVMValueRef ret = LLVMBuildLoad(ctx->ac.builder, derived_ptr, "");
    return LLVMBuildBitCast(ctx->ac.builder, ret, get_def_type(ctx, &instr->dest.ssa), "");
 }
 

commit ea396d7ab0003debe93856a3f3a91eecc2b74f78

ac/llvm: Fix alignment of shared load intrinsics.
After some research, I found out that the alignment needs to be set
on the return value of the load, rather than on the pointer.

Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Tested-by: Michel Dänzer <mdaenzer@redhat.com>
Fixes: decac320d2b54b96280937db81ffc7d5478f7921 "ac/llvm: Emit more efficient code for load_shared."
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9688>

src/amd/llvm/ac_nir_to_llvm.c
@@ -2967,9 +2967,9 @@ static LLVMValueRef visit_load_shared(struct ac_nir_context *ctx, const nir_intr
    LLVMTypeRef result_type = get_def_type(ctx, &instr->dest.ssa);
    int addr_space = LLVMGetPointerAddressSpace(LLVMTypeOf(ptr));
    LLVMValueRef derived_ptr = LLVMBuildBitCast(ctx->ac.builder, ptr, LLVMPointerType(result_type, addr_space), "");
-   LLVMSetAlignment(derived_ptr, alignment);
-
    LLVMValueRef ret = LLVMBuildLoad(ctx->ac.builder, derived_ptr, "");
+   LLVMSetAlignment(ret, alignment);
+
    return LLVMBuildBitCast(ctx->ac.builder, ret, get_def_type(ctx, &instr->dest.ssa), "");
 }
 