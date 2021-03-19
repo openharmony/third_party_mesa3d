openharmony/third_party_mesa3d

commit c1197f23cfca4bddf9a47aa508fbcf2eefc7ca68

ci/bare-metal: Restart a run on intermittent kernel lockups.
Since enabling SMP on db820c and cranking up how many tests we run, we've
been seeing lockups like this a couple of times a week.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9655>

.gitlab-ci/bare-metal/cros_servo_run.py
@@ -139,6 +139,12 @@ class CrosServoRun:
                 self.print_error("Detected cheza power management bus error, restarting run...")
                 return 2
 
+            # db820c sometimes wedges around iommu fault recovery
+            if re.search("watchdog: BUG: soft lockup - CPU.* stuck", line):
+                self.print_error(
+                    "Detected kernel soft lockup, restarting run...")
+                return 2
+
             result = re.search("bare-metal result: (\S*)", line)
             if result:
                 if result.group(1) == "pass":

commit 743e79876b77fc0eef44984acfcd3432f86ffa82

ci/bare-metal: Move the db820c lockup detect to the right boot script.
Fixes: c1197f23cfca ("ci/bare-metal: Restart a run on intermittent kernel lockups.")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9715>

.gitlab-ci/bare-metal/cros_servo_run.py
@@ -139,12 +139,6 @@ class CrosServoRun:
                 self.print_error("Detected cheza power management bus error, restarting run...")
                 return 2
 
-            # db820c sometimes wedges around iommu fault recovery
-            if re.search("watchdog: BUG: soft lockup - CPU.* stuck", line):
-                self.print_error(
-                    "Detected kernel soft lockup, restarting run...")
-                return 2
-
             result = re.search("bare-metal result: (\S*)", line)
             if result:
                 if result.group(1) == "pass":

.gitlab-ci/bare-metal/fastboot_run.py
@@ -74,6 +74,12 @@ class FastbootRun:
                 self.print_error("Detected spontaneous reboot, restarting run...")
                 return 2
 
+            # db820c sometimes wedges around iommu fault recovery
+            if re.search("watchdog: BUG: soft lockup - CPU.* stuck", line):
+                self.print_error(
+                    "Detected kernel soft lockup, restarting run...")
+                return 2
+
             result = re.search("bare-metal result: (\S*)", line)
             if result:
                 if result.group(1) == "pass":