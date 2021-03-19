openharmony/third_party_mesa3d

commit c71b2ad7eaaebc3b36bea86541bb1d9d02efb48b

util: create some standalone compression helpers
This will be used by the following patch. It allows us to detangle
compression from the disk cache code, and abstract the underlying
compression libraries we use.

Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9593>

src/util/compress.c
@@ -1,0 +1,148 @@
+/*
+ * Copyright © 2021 Valve Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <assert.h>
+
+#include "zlib.h"
+
+#ifdef HAVE_ZSTD
+#include "zstd.h"
+#endif
+
+#include "util/compress.h"
+
+/* 3 is the recomended level, with 22 as the absolute maximum */
+#define ZSTD_COMPRESSION_LEVEL 3
+
+size_t
+util_compress_max_compressed_len(size_t in_data_size)
+{
+#ifdef HAVE_ZSTD
+   /* from the zstd docs (https://facebook.github.io/zstd/zstd_manual.html):
+    * compression runs faster if `dstCapacity` >= `ZSTD_compressBound(srcSize)`.
+    */
+   return ZSTD_compressBound(in_data_size);
+#else
+   /* From https://zlib.net/zlib_tech.html:
+    *
+    *    "In the worst possible case, where the other block types would expand
+    *    the data, deflation falls back to stored (uncompressed) blocks. Thus
+    *    for the default settings used by deflateInit(), compress(), and
+    *    compress2(), the only expansion is an overhead of five bytes per 16 KB
+    *    block (about 0.03%), plus a one-time overhead of six bytes for the
+    *    entire stream."
+    */
+   size_t num_blocks = (in_data_size + 16383) / 16384; /* round up blocks */
+   return in_data_size + 6 + (num_blocks * 5);        
+#endif
+}
+
+/* Compress data and return the size of the compressed data */
+size_t
+util_compress_deflate(uint8_t *in_data, size_t in_data_size,
+                      uint8_t *out_data, size_t out_buff_size)
+{
+#ifdef HAVE_ZSTD
+   size_t ret = ZSTD_compress(out_data, out_buff_size, in_data, in_data_size,
+                              ZSTD_COMPRESSION_LEVEL);
+   if (ZSTD_isError(ret))
+      return 0;
+
+   return ret;
+#else
+   size_t compressed_size = 0;
+
+   /* allocate deflate state */
+   z_stream strm;
+   strm.zalloc = Z_NULL;
+   strm.zfree = Z_NULL;
+   strm.opaque = Z_NULL;
+   strm.next_in = in_data;
+   strm.next_out = out_data;
+   strm.avail_in = in_data_size;
+   strm.avail_out = out_buff_size;
+
+   int ret = deflateInit(&strm, Z_BEST_COMPRESSION);
+   if (ret != Z_OK) {
+       (void) deflateEnd(&strm);
+       return 0;
+   }
+
+   /* compress until end of in_data */
+   ret = deflate(&strm, Z_FINISH);
+
+   /* stream should be complete */
+   assert(ret == Z_STREAM_END);
+   if (ret == Z_STREAM_END) {
+       compressed_size = strm.total_out;
+   }
+
+   /* clean up and return */
+   (void) deflateEnd(&strm);
+   return compressed_size;
+# endif
+}
+
+/**
+ * Decompresses data, returns true if successful.
+ */
+bool
+util_compress_inflate(uint8_t *in_data, size_t in_data_size,
+                      uint8_t *out_data, size_t out_data_size)
+{
+#ifdef HAVE_ZSTD
+   size_t ret = ZSTD_decompress(out_data, out_data_size, in_data, in_data_size);
+   return !ZSTD_isError(ret);
+#else
+   z_stream strm;
+
+   /* allocate inflate state */
+   strm.zalloc = Z_NULL;
+   strm.zfree = Z_NULL;
+   strm.opaque = Z_NULL;
+   strm.next_in = in_data;
+   strm.avail_in = in_data_size;
+   strm.next_out = out_data;
+   strm.avail_out = out_data_size;
+
+   int ret = inflateInit(&strm);
+   if (ret != Z_OK)
+      return false;
+
+   ret = inflate(&strm, Z_NO_FLUSH);
+   assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+
+   /* Unless there was an error we should have decompressed everything in one
+    * go as we know the uncompressed file size.
+    */
+   if (ret != Z_STREAM_END) {
+      (void)inflateEnd(&strm);
+      return false;
+   }
+   assert(strm.avail_out == 0);
+
+   /* clean up and return */
+   (void)inflateEnd(&strm);
+   return true;
+#endif
+}

src/util/compress.h
@@ -1,0 +1,36 @@
+/*
+ * Copyright © 2021 Valve Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <stdbool.h>
+#include <inttypes.h>
+
+size_t
+util_compress_max_compressed_len(size_t in_data_size);
+
+bool
+util_compress_inflate(uint8_t *in_data, size_t in_data_size,
+                      uint8_t *out_data, size_t out_data_size);
+
+size_t
+util_compress_deflate(uint8_t *in_data, size_t in_data_size,
+                      uint8_t *out_data, size_t out_buff_size);

src/util/meson.build
@@ -35,6 +35,8 @@ files_mesa_util = files(
   'build_id.h',
   'cnd_monotonic.h',
   'compiler.h',
+  'compress.c',
+  'compress.h',
   'crc32.c',
   'crc32.h',
   'dag.c',

commit e3ca179564dcb4c6764c69a4f9f8a6278de8daaf

meson, util: Make zlib optional again
This adds a HAVE_COMPRESSION macro, which is undefined if neither zlib
nor zstd are present, and is used to no-op compress.h/c. This also has
a side effect of fixing SCons, since it won't define this macro.

Fixes: c71b2ad7eaae ("util: create some standalone compression helpers")
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9689>

Android.common.mk
@@ -73,6 +73,7 @@ LOCAL_CFLAGS += \
 	-DHAVE_LINUX_FUTEX_H \
 	-DHAVE_ENDIAN_H \
 	-DHAVE_ZLIB \
+	-DHAVE_COMPRESSION \
 	-DMAJOR_IN_SYSMACROS \
 	-DVK_USE_PLATFORM_ANDROID_KHR \
 	-fvisibility=hidden \

meson.build
@@ -1400,6 +1400,13 @@ else
   dep_zstd = null_dep
 endif
 
+with_compression = dep_zlib.found() or dep_zstd.found()
+if with_compression
+  pre_args += '-DHAVE_COMPRESSION'
+elif with_shader_cache
+  error('Shader Cache requires compression')
+endif
+
 dep_thread = dependency('threads')
 if dep_thread.found() and host_machine.system() != 'windows'
   pre_args += '-DHAVE_PTHREAD'

src/util/compress.c
@@ -21,6 +21,8 @@
  * IN THE SOFTWARE.
  */
 
+#ifdef HAVE_COMPRESSION
+
 #include <assert.h>
 
 /* Ensure that zlib uses 'const' in 'z_const' declarations. */
@@ -28,13 +30,16 @@
 #define ZLIB_CONST
 #endif
 
+#ifdef HAVE_ZLIB
 #include "zlib.h"
+#endif
 
 #ifdef HAVE_ZSTD
 #include "zstd.h"
 #endif
 
 #include "util/compress.h"
+#include "macros.h"
 
 /* 3 is the recomended level, with 22 as the absolute maximum */
 #define ZSTD_COMPRESSION_LEVEL 3
@@ -47,7 +52,7 @@ util_compress_max_compressed_len(size_t in_data_size)
     * compression runs faster if `dstCapacity` >= `ZSTD_compressBound(srcSize)`.
     */
    return ZSTD_compressBound(in_data_size);
-#else
+#elif defined(HAVE_ZLIB)
    /* From https://zlib.net/zlib_tech.html:
     *
     *    "In the worst possible case, where the other block types would expand
@@ -58,7 +63,9 @@ util_compress_max_compressed_len(size_t in_data_size)
     *    entire stream."
     */
    size_t num_blocks = (in_data_size + 16383) / 16384; /* round up blocks */
-   return in_data_size + 6 + (num_blocks * 5);        
+   return in_data_size + 6 + (num_blocks * 5);
+#else
+   STATIC_ASSERT(false);
 #endif
 }
 
@@ -74,7 +81,7 @@ util_compress_deflate(const uint8_t *in_data, size_t in_data_size,
       return 0;
 
    return ret;
-#else
+#elif defined(HAVE_ZLIB)
    size_t compressed_size = 0;
 
    /* allocate deflate state */
@@ -105,6 +112,8 @@ util_compress_deflate(const uint8_t *in_data, size_t in_data_size,
    /* clean up and return */
    (void) deflateEnd(&strm);
    return compressed_size;
+#else
+   STATIC_ASSERT(false);
 # endif
 }
 
@@ -118,7 +127,7 @@ util_compress_inflate(const uint8_t *in_data, size_t in_data_size,
 #ifdef HAVE_ZSTD
    size_t ret = ZSTD_decompress(out_data, out_data_size, in_data, in_data_size);
    return !ZSTD_isError(ret);
-#else
+#elif defined(HAVE_ZLIB)
    z_stream strm;
 
    /* allocate inflate state */
@@ -149,5 +158,9 @@ util_compress_inflate(const uint8_t *in_data, size_t in_data_size,
    /* clean up and return */
    (void)inflateEnd(&strm);
    return true;
+#else
+   STATIC_ASSERT(false);
 #endif
 }
+
+#endif

src/util/compress.h
@@ -21,6 +21,8 @@
  * IN THE SOFTWARE.
  */
 
+#ifdef HAVE_COMPRESSION
+
 #include <stdbool.h>
 #include <inttypes.h>
 
@@ -34,3 +36,5 @@ util_compress_inflate(const uint8_t *in_data, size_t in_data_size,
 size_t
 util_compress_deflate(const uint8_t *in_data, size_t in_data_size,
                       uint8_t *out_data, size_t out_buff_size);
+
+#endif