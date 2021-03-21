openharmony/third_party_mesa3d

commit a596580373aa6c0a3e38ece52a3e86d320c0cba3

zink: check if multisample support exists for shader image formats
avoid issues later by checking this now while we're here

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8504>

src/gallium/drivers/zink/zink_screen.c
@@ -627,6 +627,10 @@ zink_is_format_supported(struct pipe_screen *pscreen,
              !(screen->info.props.limits.sampledImageColorSampleCounts & sample_mask))
             return false;
       }
+      if (bind & PIPE_BIND_SHADER_IMAGE) {
+          if (!screen->info.feats.features.shaderStorageImageMultisample)
+             return false;
+      }
    }
 
    VkFormatProperties props;

commit 947aeb39ac9aeb02c213c149edbfb73de26bde78

zink: more accurately check samplecount caps for shader images
lavapipe can only do up to 4bits, so this isn't just a binary check

Fixes: a596580373a ("zink: check if multisample support exists for shader image formats")

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9713>

src/gallium/drivers/zink/zink_screen.c
@@ -796,7 +796,7 @@ zink_is_format_supported(struct pipe_screen *pscreen,
             return false;
       }
       if (bind & PIPE_BIND_SHADER_IMAGE) {
-          if (!screen->info.feats.features.shaderStorageImageMultisample)
+          if (!(screen->info.props.limits.storageImageSampleCounts & sample_mask))
              return false;
       }
    }