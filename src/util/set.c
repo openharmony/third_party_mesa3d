openharmony/third_party_mesa3d

commit 893dcc8d866baca585d5ce6afbc7288eb920835e

util: add _mesa_set_create_u32_keys where keys are not pointers
the only limitation is that key=0 is not allowed

Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6955>

src/util/set.c
@@ -148,6 +148,26 @@ _mesa_set_create(void *mem_ctx,
    return ht;
 }
 
+static uint32_t
+key_u32_hash(const void *key)
+{
+   uint32_t u = (uint32_t)(uintptr_t)key;
+   return _mesa_hash_uint(&u);
+}
+
+static bool
+key_u32_equals(const void *a, const void *b)
+{
+   return (uint32_t)(uintptr_t)a == (uint32_t)(uintptr_t)b;
+}
+
+/* key == 0 and key == deleted_key are not allowed */
+struct set *
+_mesa_set_create_u32_keys(void *mem_ctx)
+{
+   return _mesa_set_create(NULL, key_u32_hash, key_u32_equals);
+}
+
 struct set *
 _mesa_set_clone(struct set *set, void *dst_mem_ctx)
 {

src/util/set.h
@@ -61,6 +61,9 @@ _mesa_set_create(void *mem_ctx,
                  bool (*key_equals_function)(const void *a,
                                              const void *b));
 struct set *
+_mesa_set_create_u32_keys(void *mem_ctx);
+
+struct set *
 _mesa_set_clone(struct set *set, void *dst_mem_ctx);
 
 void

commit f2c583b67209152f0d0cbf4c4c9141b64571828a

util/set: stop leaking u32 key sets which pass a mem ctx
Fixes: 893dcc8d866 ("util: add _mesa_set_create_u32_keys where keys are not pointers")

Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9810>

src/util/set.c
@@ -174,7 +174,7 @@ key_u32_equals(const void *a, const void *b)
 struct set *
 _mesa_set_create_u32_keys(void *mem_ctx)
 {
-   return _mesa_set_create(NULL, key_u32_hash, key_u32_equals);
+   return _mesa_set_create(mem_ctx, key_u32_hash, key_u32_equals);
 }
 
 struct set *