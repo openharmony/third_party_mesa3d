openharmony/third_party_mesa3d

commit 0e4318b58eacbc4c7e8e17942ea2d0e0b214c17f

zink: enable PIPE_CAP_POLYGON_OFFSET_CLAMP
this is already handled

Acked-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9072>

src/gallium/drivers/zink/zink_screen.c
@@ -182,6 +182,7 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_TEXTURE_BARRIER:
    case PIPE_CAP_TGSI_VOTE:
    case PIPE_CAP_DRAW_PARAMETERS:
+   case PIPE_CAP_POLYGON_OFFSET_CLAMP:
       return 1;
 
    case PIPE_CAP_ROBUST_BUFFER_ACCESS_BEHAVIOR:

commit 3147af6efb9a8788d11704a515ced4781adbf33a

zink: check for depth-bias-clamp feature
This cap depends on a feature-bit.

Fixes: 0e4318b58ea ("zink: enable PIPE_CAP_POLYGON_OFFSET_CLAMP")
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9803>

src/gallium/drivers/zink/zink_screen.c
@@ -245,12 +245,14 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_TEXTURE_BARRIER:
    case PIPE_CAP_TGSI_VOTE:
    case PIPE_CAP_DRAW_PARAMETERS:
-   case PIPE_CAP_POLYGON_OFFSET_CLAMP:
    case PIPE_CAP_QUERY_SO_OVERFLOW:
    case PIPE_CAP_GL_SPIRV:
    case PIPE_CAP_CLEAR_SCISSORED:
    case PIPE_CAP_INVALIDATE_BUFFER:
       return 1;
+
+   case PIPE_CAP_POLYGON_OFFSET_CLAMP:
+      return screen->info.feats.features.depthBiasClamp;
 
    case PIPE_CAP_QUERY_PIPELINE_STATISTICS_SINGLE:
       return screen->info.feats.features.pipelineStatisticsQuery;