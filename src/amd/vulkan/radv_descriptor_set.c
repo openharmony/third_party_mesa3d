openharmony/third_party_mesa3d

commit ff0b5bb9ed4045455a92e0dbfdc08324f8b200e7

vk: consolidate dynamic descriptor binding sorting
this code was duplicated across several drivers

Reviewed-by: Adam Jackson <ajax@redhat.com>
turnip changes Reviewed-by: Hyunjun Ko <zzoon@igalia.com>

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9480>

src/amd/vulkan/radv_descriptor_set.c
@@ -29,6 +29,7 @@
 #include "util/mesa-sha1.h"
 #include "radv_private.h"
 #include "sid.h"
+#include "vk_descriptors.h"
 #include "vk_format.h"
 #include "vk_util.h"
 
@@ -44,28 +45,6 @@ static bool has_equal_immutable_samplers(const VkSampler *samplers, uint32_t cou
 		}
 	}
 	return true;
-}
-
-static int binding_compare(const void* av, const void *bv)
-{
-	const VkDescriptorSetLayoutBinding *a = (const VkDescriptorSetLayoutBinding*)av;
-	const VkDescriptorSetLayoutBinding *b = (const VkDescriptorSetLayoutBinding*)bv;
-
-	return (a->binding < b->binding) ? -1 : (a->binding > b->binding) ? 1 : 0;
-}
-
-static VkDescriptorSetLayoutBinding *
-create_sorted_bindings(const VkDescriptorSetLayoutBinding *bindings, unsigned count) {
-	VkDescriptorSetLayoutBinding *sorted_bindings = malloc(MAX2(count * sizeof(VkDescriptorSetLayoutBinding), 1));
-	if (!sorted_bindings)
-		return NULL;
-
-	if (count) {
-		memcpy(sorted_bindings, bindings, count * sizeof(VkDescriptorSetLayoutBinding));
-		qsort(sorted_bindings, count, sizeof(VkDescriptorSetLayoutBinding), binding_compare);
-	}
-
-	return sorted_bindings;
 }
 
 static bool radv_mutable_descriptor_type_size_alignment(const VkMutableDescriptorTypeListVALVE *list,
@@ -176,8 +155,8 @@ VkResult radv_CreateDescriptorSetLayout(
 	} else
 		set_layout->ycbcr_sampler_offsets_offset = 0;
 
-	VkDescriptorSetLayoutBinding *bindings = create_sorted_bindings(pCreateInfo->pBindings,
-	                                                                pCreateInfo->bindingCount);
+	VkDescriptorSetLayoutBinding *bindings = vk_create_sorted_bindings(pCreateInfo->pBindings,
+	                                                                   pCreateInfo->bindingCount);
 	if (!bindings) {
 		vk_object_base_finish(&set_layout->base);
 		vk_free2(&device->vk.alloc, pAllocator, set_layout);
@@ -358,8 +337,8 @@ void radv_GetDescriptorSetLayoutSupport(VkDevice device,
                                         const VkDescriptorSetLayoutCreateInfo* pCreateInfo,
                                         VkDescriptorSetLayoutSupport* pSupport)
 {
-	VkDescriptorSetLayoutBinding *bindings = create_sorted_bindings(pCreateInfo->pBindings,
-	                                                                pCreateInfo->bindingCount);
+	VkDescriptorSetLayoutBinding *bindings = vk_create_sorted_bindings(pCreateInfo->pBindings,
+	                                                                   pCreateInfo->bindingCount);
 	if (!bindings) {
 		pSupport->supported = false;
 		return;

src/broadcom/vulkan/v3dv_descriptor_set.c
@@ -21,6 +21,7 @@
  * IN THE SOFTWARE.
  */
 
+#include "vk_descriptors.h"
 #include "vk_util.h"
 
 #include "v3dv_private.h"
@@ -557,40 +558,6 @@ v3dv_ResetDescriptorPool(VkDevice _device,
    return VK_SUCCESS;
 }
 
-static int
-binding_compare(const void *av, const void *bv)
-{
-   const VkDescriptorSetLayoutBinding *a =
-      (const VkDescriptorSetLayoutBinding *) av;
-   const VkDescriptorSetLayoutBinding *b =
-      (const VkDescriptorSetLayoutBinding *) bv;
-
-   return (a->binding < b->binding) ? -1 : (a->binding > b->binding) ? 1 : 0;
-}
-
-static VkDescriptorSetLayoutBinding *
-create_sorted_bindings(const VkDescriptorSetLayoutBinding *bindings,
-                       unsigned count,
-                       struct v3dv_device *device,
-                       const VkAllocationCallbacks *pAllocator)
-{
-   VkDescriptorSetLayoutBinding *sorted_bindings =
-      vk_alloc2(&device->vk.alloc, pAllocator,
-                count * sizeof(VkDescriptorSetLayoutBinding),
-                8, VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
-
-   if (!sorted_bindings)
-      return NULL;
-
-   memcpy(sorted_bindings, bindings,
-          count * sizeof(VkDescriptorSetLayoutBinding));
-
-   qsort(sorted_bindings, count, sizeof(VkDescriptorSetLayoutBinding),
-         binding_compare);
-
-   return sorted_bindings;
-}
-
 VkResult
 v3dv_CreateDescriptorSetLayout(VkDevice _device,
                                const VkDescriptorSetLayoutCreateInfo *pCreateInfo,
@@ -643,9 +610,8 @@ v3dv_CreateDescriptorSetLayout(VkDevice _device,
    VkDescriptorSetLayoutBinding *bindings = NULL;
    if (pCreateInfo->bindingCount > 0) {
       assert(max_binding >= 0);
-      bindings = create_sorted_bindings(pCreateInfo->pBindings,
-                                        pCreateInfo->bindingCount,
-                                        device, pAllocator);
+      bindings = vk_create_sorted_bindings(pCreateInfo->pBindings,
+                                           pCreateInfo->bindingCount);
       if (!bindings) {
          vk_object_free(&device->vk, pAllocator, set_layout);
          return vk_error(device->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
@@ -719,8 +685,7 @@ v3dv_CreateDescriptorSetLayout(VkDevice _device,
          binding->descriptorCount;
    }
 
-   if (bindings)
-      vk_free2(&device->vk.alloc, pAllocator, bindings);
+   free(bindings);
 
    set_layout->descriptor_count = descriptor_count;
    set_layout->dynamic_offset_count = dynamic_offset_count;

src/freedreno/vulkan/tu_descriptor_set.c
@@ -44,36 +44,8 @@
 #include <unistd.h>
 
 #include "util/mesa-sha1.h"
+#include "vk_descriptors.h"
 #include "vk_util.h"
-
-static int
-binding_compare(const void *av, const void *bv)
-{
-   const VkDescriptorSetLayoutBinding *a =
-      (const VkDescriptorSetLayoutBinding *) av;
-   const VkDescriptorSetLayoutBinding *b =
-      (const VkDescriptorSetLayoutBinding *) bv;
-
-   return (a->binding < b->binding) ? -1 : (a->binding > b->binding) ? 1 : 0;
-}
-
-static VkDescriptorSetLayoutBinding *
-create_sorted_bindings(const VkDescriptorSetLayoutBinding *bindings,
-                       unsigned count)
-{
-   VkDescriptorSetLayoutBinding *sorted_bindings =
-      malloc(count * sizeof(VkDescriptorSetLayoutBinding));
-   if (!sorted_bindings)
-      return NULL;
-
-   memcpy(sorted_bindings, bindings,
-          count * sizeof(VkDescriptorSetLayoutBinding));
-
-   qsort(sorted_bindings, count, sizeof(VkDescriptorSetLayoutBinding),
-         binding_compare);
-
-   return sorted_bindings;
-}
 
 static uint32_t
 descriptor_size(VkDescriptorType type)
@@ -160,7 +132,7 @@ tu_CreateDescriptorSetLayout(
    struct tu_sampler_ycbcr_conversion *ycbcr_samplers =
       (void*) &samplers[immutable_sampler_count];
 
-   VkDescriptorSetLayoutBinding *bindings = create_sorted_bindings(
+   VkDescriptorSetLayoutBinding *bindings = vk_create_sorted_bindings(
       pCreateInfo->pBindings, pCreateInfo->bindingCount);
    if (!bindings) {
       vk_object_free(&device->vk, pAllocator, set_layout);
@@ -275,7 +247,7 @@ tu_GetDescriptorSetLayoutSupport(
    const VkDescriptorSetLayoutCreateInfo *pCreateInfo,
    VkDescriptorSetLayoutSupport *pSupport)
 {
-   VkDescriptorSetLayoutBinding *bindings = create_sorted_bindings(
+   VkDescriptorSetLayoutBinding *bindings = vk_create_sorted_bindings(
       pCreateInfo->pBindings, pCreateInfo->bindingCount);
    if (!bindings) {
       pSupport->supported = false;

src/gallium/frontends/lavapipe/lvp_descriptor_set.c
@@ -22,30 +22,9 @@
  */
 
 #include "lvp_private.h"
+#include "vk_descriptors.h"
 #include "vk_util.h"
 #include "u_math.h"
-
-static int binding_compare(const void* av, const void *bv)
-{
-   const VkDescriptorSetLayoutBinding *a = (const VkDescriptorSetLayoutBinding*)av;
-   const VkDescriptorSetLayoutBinding *b = (const VkDescriptorSetLayoutBinding*)bv;
-
-   return (a->binding < b->binding) ? -1 : (a->binding > b->binding) ? 1 : 0;
-}
-
-static VkDescriptorSetLayoutBinding *
-create_sorted_bindings(const VkDescriptorSetLayoutBinding *bindings, unsigned count) {
-   VkDescriptorSetLayoutBinding *sorted_bindings = malloc(MAX2(count * sizeof(VkDescriptorSetLayoutBinding), 1));
-   if (!sorted_bindings)
-      return NULL;
-
-   if (count) {
-      memcpy(sorted_bindings, bindings, count * sizeof(VkDescriptorSetLayoutBinding));
-      qsort(sorted_bindings, count, sizeof(VkDescriptorSetLayoutBinding), binding_compare);
-   }
-
-   return sorted_bindings;
-}
 
 VKAPI_ATTR VkResult VKAPI_CALL lvp_CreateDescriptorSetLayout(
     VkDevice                                    _device,
@@ -100,8 +79,8 @@ VKAPI_ATTR VkResult VKAPI_CALL lvp_CreateDescriptorSetLayout(
    set_layout->shader_stages = 0;
    set_layout->size = 0;
 
-   VkDescriptorSetLayoutBinding *bindings = create_sorted_bindings(pCreateInfo->pBindings,
-								   pCreateInfo->bindingCount);
+   VkDescriptorSetLayoutBinding *bindings = vk_create_sorted_bindings(pCreateInfo->pBindings,
+                                                                      pCreateInfo->bindingCount);
    if (!bindings) {
       vk_object_base_finish(&set_layout->base);
       vk_free2(&device->vk.alloc, pAllocator, set_layout);

src/vulkan/util/vk_descriptors.c
@@ -1,0 +1,55 @@
+/*
+ * Copyright © 2016 Red Hat.
+ * Copyright © 2016 Bas Nieuwenhuizen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include "vk_descriptors.h"
+#include "util/macros.h"
+
+static int
+binding_compare(const void* av, const void *bv)
+{
+   const VkDescriptorSetLayoutBinding *a = (const VkDescriptorSetLayoutBinding*)av;
+   const VkDescriptorSetLayoutBinding *b = (const VkDescriptorSetLayoutBinding*)bv;
+ 
+   return (a->binding < b->binding) ? -1 : (a->binding > b->binding) ? 1 : 0;
+}
+ 
+VkDescriptorSetLayoutBinding *
+vk_create_sorted_bindings(const VkDescriptorSetLayoutBinding *bindings, unsigned count)
+{
+   VkDescriptorSetLayoutBinding *sorted_bindings = malloc(MAX2(count * sizeof(VkDescriptorSetLayoutBinding), 1));
+   if (!sorted_bindings)
+      return NULL;
+ 
+   if (count) {
+      memcpy(sorted_bindings, bindings, count * sizeof(VkDescriptorSetLayoutBinding));
+      qsort(sorted_bindings, count, sizeof(VkDescriptorSetLayoutBinding), binding_compare);
+   } else {
+      /* just an empty struct */
+      memset(sorted_bindings, 0, sizeof(VkDescriptorSetLayoutBinding));
+   }
+ 
+   return sorted_bindings;
+}

src/vulkan/util/vk_descriptors.h
@@ -1,0 +1,43 @@
+/*
+ * Copyright © 2016 Red Hat.
+ * Copyright © 2016 Bas Nieuwenhuizen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef VK_DESCRIPTORS_H
+#define VK_DESCRIPTORS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <vulkan/vulkan.h>
+
+VkDescriptorSetLayoutBinding *
+vk_create_sorted_bindings(const VkDescriptorSetLayoutBinding *bindings, unsigned count);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif

src/vulkan/util/meson.build
@@ -29,6 +29,8 @@ files_vulkan_util = files(
   'vk_debug_report.h',
   'vk_deferred_operation.c',
   'vk_deferred_operation.h',
+  'vk_descriptors.c',
+  'vk_descriptors.h',
   'vk_device.c',
   'vk_device.h',
   'vk_format.c',

commit 7483c2136e002bbfc17564eb41555aa2dab2b974

vulkan: Fix descriptor set creation with zero bindings.
MAX2(count * struct size, 1) results in 1 for count=0, not the size of a struct.

Since this MAX only seems to exist so we can keep using NULL for error reporting,
just refactor to return a VkResult.

Fixes: ff0b5bb9ed4 ("vk: consolidate dynamic descriptor binding sorting")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4522
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Acked-by: Dave Airlie <airlied@redhat.com>
Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9880>

src/amd/vulkan/radv_descriptor_set.c
@@ -155,12 +155,14 @@ VkResult radv_CreateDescriptorSetLayout(
 	} else
 		set_layout->ycbcr_sampler_offsets_offset = 0;
 
-	VkDescriptorSetLayoutBinding *bindings = vk_create_sorted_bindings(pCreateInfo->pBindings,
-	                                                                   pCreateInfo->bindingCount);
-	if (!bindings) {
+	VkDescriptorSetLayoutBinding *bindings = NULL;
+	VkResult result = vk_create_sorted_bindings(pCreateInfo->pBindings,
+	                                            pCreateInfo->bindingCount,
+	                                            &bindings);
+	if (result != VK_SUCCESS) {
 		vk_object_base_finish(&set_layout->base);
 		vk_free2(&device->vk.alloc, pAllocator, set_layout);
-		return vk_error(device->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
+		return vk_error(device->instance, result);
 	}
 
 	set_layout->binding_count = num_bindings;
@@ -337,9 +339,11 @@ void radv_GetDescriptorSetLayoutSupport(VkDevice device,
                                         const VkDescriptorSetLayoutCreateInfo* pCreateInfo,
                                         VkDescriptorSetLayoutSupport* pSupport)
 {
-	VkDescriptorSetLayoutBinding *bindings = vk_create_sorted_bindings(pCreateInfo->pBindings,
-	                                                                   pCreateInfo->bindingCount);
-	if (!bindings) {
+	VkDescriptorSetLayoutBinding *bindings = NULL;
+	VkResult result = vk_create_sorted_bindings(pCreateInfo->pBindings,
+	                                            pCreateInfo->bindingCount,
+	                                            &bindings);
+	if (result != VK_SUCCESS) {
 		pSupport->supported = false;
 		return;
 	}

src/broadcom/vulkan/v3dv_descriptor_set.c
@@ -607,15 +607,14 @@ v3dv_CreateDescriptorSetLayout(VkDevice _device,
    /* We just allocate all the immutable samplers at the end of the struct */
    struct v3dv_sampler *samplers = (void*) &set_layout->binding[max_binding + 1];
 
+   assert(pCreateInfo->bindingCount == 0 || max_binding >= 0);
+
    VkDescriptorSetLayoutBinding *bindings = NULL;
-   if (pCreateInfo->bindingCount > 0) {
-      assert(max_binding >= 0);
-      bindings = vk_create_sorted_bindings(pCreateInfo->pBindings,
-                                           pCreateInfo->bindingCount);
-      if (!bindings) {
-         vk_object_free(&device->vk, pAllocator, set_layout);
-         return vk_error(device->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
-      }
+   VkResult result = vk_create_sorted_bindings(pCreateInfo->pBindings,
+                                               pCreateInfo->bindingCount, &bindings);
+   if (result != VK_SUCCESS) {
+      vk_object_free(&device->vk, pAllocator, set_layout);
+      return vk_error(device->instance, result);
    }
 
    memset(set_layout->binding, 0,

src/freedreno/vulkan/tu_descriptor_set.c
@@ -132,11 +132,12 @@ tu_CreateDescriptorSetLayout(
    struct tu_sampler_ycbcr_conversion *ycbcr_samplers =
       (void*) &samplers[immutable_sampler_count];
 
-   VkDescriptorSetLayoutBinding *bindings = vk_create_sorted_bindings(
-      pCreateInfo->pBindings, pCreateInfo->bindingCount);
-   if (!bindings) {
+   VkDescriptorSetLayoutBinding *bindings = NULL;
+   VkResult result = vk_create_sorted_bindings(
+      pCreateInfo->pBindings, pCreateInfo->bindingCount, &bindings);
+   if (result != VK_SUCCESS) {
       vk_object_free(&device->vk, pAllocator, set_layout);
-      return vk_error(device->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
+      return vk_error(device->instance, result);
    }
 
    set_layout->binding_count = max_binding + 1;
@@ -247,9 +248,10 @@ tu_GetDescriptorSetLayoutSupport(
    const VkDescriptorSetLayoutCreateInfo *pCreateInfo,
    VkDescriptorSetLayoutSupport *pSupport)
 {
-   VkDescriptorSetLayoutBinding *bindings = vk_create_sorted_bindings(
-      pCreateInfo->pBindings, pCreateInfo->bindingCount);
-   if (!bindings) {
+   VkDescriptorSetLayoutBinding *bindings = NULL;
+   VkResult result = vk_create_sorted_bindings(
+      pCreateInfo->pBindings, pCreateInfo->bindingCount, &bindings);
+   if (result != VK_SUCCESS) {
       pSupport->supported = false;
       return;
    }

src/gallium/frontends/lavapipe/lvp_descriptor_set.c
@@ -79,12 +79,14 @@ VKAPI_ATTR VkResult VKAPI_CALL lvp_CreateDescriptorSetLayout(
    set_layout->shader_stages = 0;
    set_layout->size = 0;
 
-   VkDescriptorSetLayoutBinding *bindings = vk_create_sorted_bindings(pCreateInfo->pBindings,
-                                                                      pCreateInfo->bindingCount);
-   if (!bindings) {
+   VkDescriptorSetLayoutBinding *bindings = NULL;
+   VkResult result = vk_create_sorted_bindings(pCreateInfo->pBindings,
+                                               pCreateInfo->bindingCount,
+                                               &bindings);
+   if (result != VK_SUCCESS) {
       vk_object_base_finish(&set_layout->base);
       vk_free2(&device->vk.alloc, pAllocator, set_layout);
-      return vk_error(device->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
+      return vk_error(device->instance, result);
    }
 
    uint32_t dynamic_offset_count = 0;

src/vulkan/util/vk_descriptors.c
@@ -36,20 +36,21 @@ binding_compare(const void* av, const void *bv)
    return (a->binding < b->binding) ? -1 : (a->binding > b->binding) ? 1 : 0;
 }
  
-VkDescriptorSetLayoutBinding *
-vk_create_sorted_bindings(const VkDescriptorSetLayoutBinding *bindings, unsigned count)
+VkResult
+vk_create_sorted_bindings(const VkDescriptorSetLayoutBinding *bindings, unsigned count,
+                          VkDescriptorSetLayoutBinding **sorted_bindings)
 {
-   VkDescriptorSetLayoutBinding *sorted_bindings = malloc(MAX2(count * sizeof(VkDescriptorSetLayoutBinding), 1));
-   if (!sorted_bindings)
-      return NULL;
- 
-   if (count) {
-      memcpy(sorted_bindings, bindings, count * sizeof(VkDescriptorSetLayoutBinding));
-      qsort(sorted_bindings, count, sizeof(VkDescriptorSetLayoutBinding), binding_compare);
-   } else {
-      /* just an empty struct */
-      memset(sorted_bindings, 0, sizeof(VkDescriptorSetLayoutBinding));
+   if (!count) {
+      *sorted_bindings = NULL;
+      return VK_SUCCESS;
    }
+
+   *sorted_bindings = malloc(count * sizeof(VkDescriptorSetLayoutBinding));
+   if (!*sorted_bindings)
+      return VK_ERROR_OUT_OF_HOST_MEMORY;
+
+   memcpy(*sorted_bindings, bindings, count * sizeof(VkDescriptorSetLayoutBinding));
+   qsort(*sorted_bindings, count, sizeof(VkDescriptorSetLayoutBinding), binding_compare);
  
-   return sorted_bindings;
+   return VK_SUCCESS;
 }

src/vulkan/util/vk_descriptors.h
@@ -31,8 +31,9 @@ extern "C" {
 
 #include <vulkan/vulkan.h>
 
-VkDescriptorSetLayoutBinding *
-vk_create_sorted_bindings(const VkDescriptorSetLayoutBinding *bindings, unsigned count);
+VkResult
+vk_create_sorted_bindings(const VkDescriptorSetLayoutBinding *bindings, unsigned count,
+                          VkDescriptorSetLayoutBinding **sorted_bindings);
 
 #ifdef __cplusplus
 }