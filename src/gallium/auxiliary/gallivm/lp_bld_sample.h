openharmony/third_party_mesa3d

commit cca4c9d1bd611c510ea35a7a5d777aed7e39ed09

gallivm: Use the proper enum for the texture target bitfield.
This gets you nice output in gdb when trying to figure out why your 2D
sample shader is hitting the cube paths.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8118>

src/gallium/auxiliary/gallivm/lp_bld_sample.h
@@ -169,7 +169,7 @@ struct lp_static_texture_state
    unsigned swizzle_a:3;
 
    /* pipe_texture's state */
-   unsigned target:4;        /**< PIPE_TEXTURE_* */
+   enum pipe_texture_target target:4;        /**< PIPE_TEXTURE_* */
    unsigned pot_width:1;     /**< is the width a power of two? */
    unsigned pot_height:1;
    unsigned pot_depth:1;

commit 2ced1c76e0eaeb272e58673ef465088fbafecda5

gallivm: increase size of texture target enum bitfield
Need to bump up the size of texture target bitfield for MSVC.

Fixes: cca4c9d1bd61 ("gallivm: Use the proper enum for the texture target bitfield.")

Reviewed-by: Neha Bhende <bhenden@vmware.com>
Reviewed-by: Roland Scheidegger <sroland@vmware.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9928>

src/gallium/auxiliary/gallivm/lp_bld_sample.h
@@ -169,7 +169,7 @@ struct lp_static_texture_state
    unsigned swizzle_a:3;
 
    /* pipe_texture's state */
-   enum pipe_texture_target target:4;        /**< PIPE_TEXTURE_* */
+   enum pipe_texture_target target:5;        /**< PIPE_TEXTURE_* */
    unsigned pot_width:1;     /**< is the width a power of two? */
    unsigned pot_height:1;
    unsigned pot_depth:1;