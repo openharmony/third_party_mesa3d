openharmony/third_party_mesa3d

commit ff1f96271a0e420d3ef7a42a4835e3aab9197dd6

vulkan: Deduplicate mesa stage conversion
Across every driver...

v2: Add casts to appease -fpermissive used on CI.

Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9477>

src/vulkan/util/vk_util.h
@@ -24,6 +24,7 @@
 #define VK_UTIL_H
 
 #include "util/macros.h"
+#include "compiler/shader_enums.h"
 #include <string.h>
 
 #ifdef __cplusplus
@@ -239,6 +240,19 @@ struct vk_pipeline_cache_header {
    STATIC_ASSERT(sizeof(*(src)) == sizeof(*(dest))); \
    memcpy((dest), (src), (count) * sizeof(*(src))); \
 } while (0)
+
+static inline gl_shader_stage
+vk_to_mesa_shader_stage(VkShaderStageFlagBits vk_stage)
+{
+   assert(__builtin_popcount((uint32_t) vk_stage) == 1);
+   return (gl_shader_stage) (ffs((uint32_t) vk_stage) - 1);
+}
+
+static inline VkShaderStageFlagBits
+mesa_to_vk_shader_stage(gl_shader_stage mesa_stage)
+{
+   return (VkShaderStageFlagBits) (1 << ((uint32_t) mesa_stage));
+}
 
 #ifdef __cplusplus
 }

src/amd/vulkan/radv_private.h
@@ -61,6 +61,7 @@
 #include "vk_format.h"
 #include "vk_physical_device.h"
 #include "vk_shader_module.h"
+#include "vk_util.h"
 
 #include "radv_radeon_winsys.h"
 #include "ac_binary.h"
@@ -1647,19 +1648,6 @@ radv_hash_shaders(unsigned char *hash,
 		  const struct radv_pipeline_layout *layout,
 		  const struct radv_pipeline_key *key,
 		  uint32_t flags);
-
-static inline gl_shader_stage
-vk_to_mesa_shader_stage(VkShaderStageFlagBits vk_stage)
-{
-	assert(util_bitcount(vk_stage) == 1);
-	return ffs(vk_stage) - 1;
-}
-
-static inline VkShaderStageFlagBits
-mesa_to_vk_shader_stage(gl_shader_stage mesa_stage)
-{
-	return (1 << mesa_stage);
-}
 
 #define RADV_STAGE_MASK ((1 << MESA_SHADER_STAGES) - 1)
 

src/broadcom/vulkan/v3dv_private.h
@@ -40,6 +40,7 @@
 #include "vk_instance.h"
 #include "vk_physical_device.h"
 #include "vk_shader_module.h"
+#include "vk_util.h"
 
 #include <xf86drm.h>
 
@@ -1355,16 +1356,6 @@ struct v3dv_event {
    struct vk_object_base base;
    int state;
 };
-
-/* FIXME: the same function at anv, radv and tu, perhaps create common
- * place?
- */
-static inline gl_shader_stage
-vk_to_mesa_shader_stage(VkShaderStageFlagBits vk_stage)
-{
-   assert(__builtin_popcount(vk_stage) == 1);
-   return ffs(vk_stage) - 1;
-}
 
 struct v3dv_shader_variant {
    broadcom_shader_stage stage;

src/freedreno/vulkan/tu_util.h
@@ -23,19 +23,7 @@
 #include "a6xx.xml.h"
 
 #include <vulkan/vulkan.h>
-
-static inline gl_shader_stage
-vk_to_mesa_shader_stage(VkShaderStageFlagBits vk_stage)
-{
-   assert(__builtin_popcount(vk_stage) == 1);
-   return util_logbase2(vk_stage);
-}
-
-static inline VkShaderStageFlagBits
-mesa_to_vk_shader_stage(gl_shader_stage mesa_stage)
-{
-   return 1 << mesa_stage;
-}
+#include "vk_util.h"
 
 #define TU_STAGE_MASK ((1 << MESA_SHADER_STAGES) - 1)
 

src/gallium/frontends/lavapipe/lvp_private.h
@@ -53,6 +53,7 @@ typedef uint32_t xcb_window_t;
 #include "vk_instance.h"
 #include "vk_physical_device.h"
 #include "vk_shader_module.h"
+#include "vk_util.h"
 
 #include "wsi_common.h"
 
@@ -118,19 +119,6 @@ void __lvp_finishme(const char *file, int line, const char *format, ...)
       lvp_finishme("stub %s", __func__); \
       return; \
    } while (0)
-
-static inline gl_shader_stage
-vk_to_mesa_shader_stage(VkShaderStageFlagBits vk_stage)
-{
-   assert(__builtin_popcount(vk_stage) == 1);
-   return ffs(vk_stage) - 1;
-}
-
-static inline VkShaderStageFlagBits
-mesa_to_vk_shader_stage(gl_shader_stage mesa_stage)
-{
-   return (1 << mesa_stage);
-}
 
 #define LVP_STAGE_MASK ((1 << MESA_SHADER_STAGES) - 1)
 

src/intel/vulkan/anv_private.h
@@ -68,6 +68,7 @@
 #include "vk_instance.h"
 #include "vk_physical_device.h"
 #include "vk_shader_module.h"
+#include "vk_util.h"
 
 /* Pre-declarations needed for WSI entrypoints */
 struct wl_surface;
@@ -3219,19 +3220,6 @@ struct anv_semaphore {
 
 void anv_semaphore_reset_temporary(struct anv_device *device,
                                    struct anv_semaphore *semaphore);
-
-static inline gl_shader_stage
-vk_to_mesa_shader_stage(VkShaderStageFlagBits vk_stage)
-{
-   assert(__builtin_popcount(vk_stage) == 1);
-   return ffs(vk_stage) - 1;
-}
-
-static inline VkShaderStageFlagBits
-mesa_to_vk_shader_stage(gl_shader_stage mesa_stage)
-{
-   return (1 << mesa_stage);
-}
 
 #define ANV_STAGE_MASK ((1 << MESA_SHADER_STAGES) - 1)
 

commit f9acd87d8927908242322de165207277a1a34bb3

vulkan/util: Fix implicit declaration of ffs for Android build
Similar to 16784f562c6a and bf50cb5fad55 to avoid Android build errors

Fixes the following building error:

In file included from external/mesa/src/vulkan/util/vk_util.c:28:
external/mesa/src/vulkan/util/vk_util.h:248:30: error: implicit declaration of function 'ffs' is invalid in C99 [-Werror,-Wimplicit-function-declaration]
   return (gl_shader_stage) (ffs((uint32_t) vk_stage) - 1);
                             ^
1 error generated.

Fixes: ff1f96271a0e ("vulkan: Deduplicate mesa stage conversion")
Acked-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10023>

src/vulkan/util/vk_util.h
@@ -23,6 +23,7 @@
 #ifndef VK_UTIL_H
 #define VK_UTIL_H
 
+#include "util/bitscan.h"
 #include "util/macros.h"
 #include "compiler/shader_enums.h"
 #include <string.h>