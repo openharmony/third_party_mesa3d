openharmony/third_party_mesa3d

commit 5d2a13d4893f60c92816fd91255a2f526c03098c

glsl: Handle 16-bit types in loop analysis
Fixes crash with mediump lowering in:

dEQP-GLES2.functional.shaders.loops.do_while_constant_iterations.basic_mediump_float_fragment

Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5746>

src/compiler/glsl/loop_analysis.cpp
@@ -161,11 +161,20 @@ calculate_iterations(ir_rvalue *from, ir_rvalue *to, ir_rvalue *increment,
       case GLSL_TYPE_INT:
          iter = new(mem_ctx) ir_constant(iter_value + bias[i]);
          break;
+      case GLSL_TYPE_INT16:
+         iter = new(mem_ctx) ir_constant(uint16_t(iter_value + bias[i]));
+         break;
       case GLSL_TYPE_UINT:
          iter = new(mem_ctx) ir_constant(unsigned(iter_value + bias[i]));
          break;
+      case GLSL_TYPE_UINT16:
+         iter = new(mem_ctx) ir_constant(uint16_t(iter_value + bias[i]));
+         break;
       case GLSL_TYPE_FLOAT:
          iter = new(mem_ctx) ir_constant(float(iter_value + bias[i]));
+         break;
+      case GLSL_TYPE_FLOAT16:
+         iter = new(mem_ctx) ir_constant(float16_t(float(iter_value + bias[i])));
          break;
       case GLSL_TYPE_DOUBLE:
          iter = new(mem_ctx) ir_constant(double(iter_value + bias[i]));

commit dd70bc2f66bc4b354c9e9ff635b818577628c057

glsl: fix int16 type
This shouldn't be unsigned, or else we'll trigger asserts.

Fixes: 5d2a13d4893 ("glsl: Handle 16-bit types in loop analysis")
Reviewed-by: Alyssa Rosenzweig <alyssa@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10125>

src/compiler/glsl/loop_analysis.cpp
@@ -162,7 +162,7 @@ calculate_iterations(ir_rvalue *from, ir_rvalue *to, ir_rvalue *increment,
          iter = new(mem_ctx) ir_constant(int32_t(iter_value + bias[i]));
          break;
       case GLSL_TYPE_INT16:
-         iter = new(mem_ctx) ir_constant(uint16_t(iter_value + bias[i]));
+         iter = new(mem_ctx) ir_constant(int16_t(iter_value + bias[i]));
          break;
       case GLSL_TYPE_INT64:
          iter = new(mem_ctx) ir_constant(int64_t(iter_value + bias[i]));