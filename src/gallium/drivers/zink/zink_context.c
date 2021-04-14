openharmony/third_party_mesa3d

commit 997ed5387bd7927b32d7c8f0b049294777742189

zink: force explicit fence only on first frame flush
we have implicit sync hooked up for drivers now so we don't need to worry
about drawing over our frontbuffer unexpectedly

still a weird issue remaining where we miss the first frame without an explicit
fence, but I expect that will get resolved eventually by wsi

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9290>

src/gallium/drivers/zink/zink_context.c
@@ -1638,18 +1638,18 @@ zink_flush(struct pipe_context *pctx,
    zink_fence_reference(zink_screen(pctx->screen),
                         (struct zink_fence **)pfence,
                         batch->fence);
-   /* HACK:
-    * For some strange reason, we need to finish before presenting, or else
-    * we start rendering on top of the back-buffer for the next frame. This
-    * seems like a bug in the DRI-driver to me, because we really should
-    * be properly protected by fences here, and the back-buffer should
-    * either be swapped with the front-buffer, or blitted from. But for
-    * some strange reason, neither of these things happen.
-    */
-   if (flags & PIPE_FLUSH_END_OF_FRAME)
-      pctx->screen->fence_finish(pctx->screen, pctx,
-                                 (struct pipe_fence_handle *)batch->fence,
-                                 PIPE_TIMEOUT_INFINITE);
+
+   if (flags & PIPE_FLUSH_END_OF_FRAME) {
+      /* if the first frame has not yet occurred, we need an explicit fence here
+       * in some cases in order to correctly draw the first frame, though it's
+       * unknown at this time why this is the case
+       */
+      if (!ctx->first_frame_done)
+         pctx->screen->fence_finish(pctx->screen, pctx,
+                                    (struct pipe_fence_handle *)batch->fence,
+                                    PIPE_TIMEOUT_INFINITE);
+      ctx->first_frame_done = true;
+   }
 }
 
 void

src/gallium/drivers/zink/zink_context.h
@@ -214,6 +214,7 @@ struct zink_context {
    struct pipe_stream_output_target *so_targets[PIPE_MAX_SO_OUTPUTS];
    bool dirty_so_targets;
    bool xfb_barrier;
+   bool first_frame_done;
 };
 
 static inline struct zink_context *

commit 12bbe8e20c55e90747dcc1245273af6052b41dca

zink: do not dereference NULL pointer
If first_frame_done isn't set, but fence is NULL, we end up dereferncing
that NULL-pointer.

This can happen in the case where the first submitted batch has no work,
and pfence was passed as a NULL-pointer.

While we're at it, simplify the check with the surrounding code, which
also checks for a NULL-pointer here.

Fixes: 997ed5387bd ("zink: force explicit fence only on first frame flush")
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10235>

src/gallium/drivers/zink/zink_context.c
@@ -1885,17 +1885,19 @@ zink_flush(struct pipe_context *pctx,
             util_queue_fence_signal(&mfence->ready);
       }
    }
-   if (fence && !(flags & (PIPE_FLUSH_DEFERRED | PIPE_FLUSH_ASYNC)))
-      sync_flush(ctx, zink_batch_state(fence));
+   if (fence) {
+      if (!(flags & (PIPE_FLUSH_DEFERRED | PIPE_FLUSH_ASYNC)))
+         sync_flush(ctx, zink_batch_state(fence));
 
-   if (flags & PIPE_FLUSH_END_OF_FRAME && !(flags & TC_FLUSH_ASYNC) && !deferred) {
-      /* if the first frame has not yet occurred, we need an explicit fence here
-       * in some cases in order to correctly draw the first frame, though it's
-       * unknown at this time why this is the case
-       */
-      if (!ctx->first_frame_done)
-         zink_vkfence_wait(screen, fence, PIPE_TIMEOUT_INFINITE);
-      ctx->first_frame_done = true;
+      if (flags & PIPE_FLUSH_END_OF_FRAME && !(flags & TC_FLUSH_ASYNC) && !deferred) {
+         /* if the first frame has not yet occurred, we need an explicit fence here
+         * in some cases in order to correctly draw the first frame, though it's
+         * unknown at this time why this is the case
+         */
+         if (!ctx->first_frame_done)
+            zink_vkfence_wait(screen, fence, PIPE_TIMEOUT_INFINITE);
+         ctx->first_frame_done = true;
+      }
    }
 }
 