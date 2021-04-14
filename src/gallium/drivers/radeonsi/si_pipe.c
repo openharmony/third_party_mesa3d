openharmony/third_party_mesa3d

commit a74128a98cee389b7b2bc2e94a731db5495b8c14

radeonsi: update pipe_screen::num_contexts
This allows skipping mutex locking. Don't take the aux context into account.

Reviewed-by: Zoltán Böszörményi <zboszor@gmail.com>
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9356>

src/gallium/drivers/radeonsi/si_pipe.c
@@ -330,6 +330,10 @@ static void si_destroy_context(struct pipe_context *context)
    util_dynarray_fini(&sctx->resident_tex_needs_color_decompress);
    util_dynarray_fini(&sctx->resident_img_needs_color_decompress);
    util_dynarray_fini(&sctx->resident_tex_needs_depth_decompress);
+
+   if (!(sctx->context_flags & SI_CONTEXT_FLAG_AUX))
+      p_atomic_dec(&context->screen->num_contexts);
+
    FREE(sctx);
 }
 
@@ -466,6 +470,7 @@ static struct pipe_context *si_create_context(struct pipe_screen *screen, unsign
    sctx->b.destroy = si_destroy_context;
    sctx->screen = sscreen; /* Easy accessing of screen/winsys. */
    sctx->is_debug = (flags & PIPE_CONTEXT_DEBUG) != 0;
+   sctx->context_flags = flags;
 
    slab_create_child(&sctx->pool_transfers, &sscreen->pool_transfers);
    slab_create_child(&sctx->pool_transfers_unsync, &sscreen->pool_transfers);
@@ -603,9 +608,7 @@ static struct pipe_context *si_create_context(struct pipe_screen *screen, unsign
 
       si_init_draw_functions(sctx);
 
-      /* If aux_context == NULL, we are initializing aux_context right now. */
-      bool is_aux_context = !sscreen->aux_context;
-      si_initialize_prim_discard_tunables(sscreen, is_aux_context,
+      si_initialize_prim_discard_tunables(sscreen, flags & SI_CONTEXT_FLAG_AUX,
                                           &sctx->prim_discard_vertex_count_threshold,
                                           &sctx->index_ring_size_per_ib);
    } else {
@@ -735,6 +738,9 @@ static struct pipe_context *si_create_context(struct pipe_screen *screen, unsign
       si_clear_buffer(sctx, sctx->null_const_buf.buffer, 0, sctx->null_const_buf.buffer->width0,
                       &clear_value, 4, SI_COHERENCY_SHADER, SI_CP_DMA_CLEAR_METHOD);
    }
+
+   if (!(flags & SI_CONTEXT_FLAG_AUX))
+      p_atomic_inc(&screen->num_contexts);
 
    sctx->initial_gfx_cs_size = sctx->gfx_cs.current.cdw;
    return &sctx->b;
@@ -1320,8 +1326,11 @@ static struct pipe_screen *radeonsi_screen_create_impl(struct radeon_winsys *ws,
 
    /* Create the auxiliary context. This must be done last. */
    sscreen->aux_context = si_create_context(
-      &sscreen->b, (sscreen->options.aux_debug ? PIPE_CONTEXT_DEBUG : 0) |
-                      (sscreen->info.has_graphics ? 0 : PIPE_CONTEXT_COMPUTE_ONLY));
+      &sscreen->b,
+      SI_CONTEXT_FLAG_AUX |
+      (sscreen->options.aux_debug ? PIPE_CONTEXT_DEBUG : 0) |
+      (sscreen->info.has_graphics ? 0 : PIPE_CONTEXT_COMPUTE_ONLY));
+
    if (sscreen->options.aux_debug) {
       struct u_log_context *log = CALLOC_STRUCT(u_log_context);
       u_log_context_init(log);

src/gallium/drivers/radeonsi/si_pipe.h
@@ -112,6 +112,8 @@ extern "C" {
 #define SI_MAP_BUFFER_ALIGNMENT           64
 #define SI_MAX_VARIABLE_THREADS_PER_BLOCK 1024
 
+#define SI_CONTEXT_FLAG_AUX               (1u << 31)
+
 #define SI_RESOURCE_FLAG_FORCE_LINEAR      (PIPE_RESOURCE_FLAG_DRV_PRIV << 0)
 #define SI_RESOURCE_FLAG_FLUSHED_DEPTH     (PIPE_RESOURCE_FLAG_DRV_PRIV << 1)
 #define SI_RESOURCE_FLAG_FORCE_MSAA_TILING (PIPE_RESOURCE_FLAG_DRV_PRIV << 2)
@@ -1304,6 +1306,8 @@ struct si_context {
    struct pipe_fence_handle *last_sqtt_fence;
    enum rgp_sqtt_marker_event_type sqtt_next_event;
    bool thread_trace_enabled;
+
+   unsigned context_flags;
 };
 
 /* si_blit.c */

commit 9445e08c6fc08f55d284452562f0d3ab93ce2edf

radeonsi: use SI_CONTEXT_FLAG_AUX when recreating the aux context
Fixes: a74128a98ce ("radeonsi: update pipe_screen::num_contexts")
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10179>

src/gallium/drivers/radeonsi/si_pipe.c
@@ -371,8 +371,9 @@ static enum pipe_reset_status si_get_reset_status(struct pipe_context *ctx)
       sscreen->aux_context->destroy(sscreen->aux_context);
 
       sscreen->aux_context = si_create_context(
-         &sscreen->b, (sscreen->options.aux_debug ? PIPE_CONTEXT_DEBUG : 0) |
-                         (sscreen->info.has_graphics ? 0 : PIPE_CONTEXT_COMPUTE_ONLY));
+         &sscreen->b, SI_CONTEXT_FLAG_AUX |
+                      (sscreen->options.aux_debug ? PIPE_CONTEXT_DEBUG : 0) |
+                      (sscreen->info.has_graphics ? 0 : PIPE_CONTEXT_COMPUTE_ONLY));
       sscreen->aux_context->set_log_context(sscreen->aux_context, aux_log);
       simple_mtx_unlock(&sscreen->aux_context_lock);
    }