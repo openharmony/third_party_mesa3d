openharmony/third_party_mesa3d

commit 9ef0fc401cc78938d11d2b3212986c63fd699bff

freedreno: Remove uniform variables after finalizing NIR.
mesa/st optimizes the uniform storage if you have the finalize hook in
place, causing the uniforms declared to potentially not have storage in
the ParameterValues list any more.  If you leave your uniforms around in
the NIR, then a later finalization after variant creation will re-add the
uniforms to parameters, defeating the optimization and likely reallocating
the uniform storage (causing use-after-free).  So, we have to do this
before we can start using variants in mesa/st.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8997>

src/freedreno/ir3/ir3_nir.c
@@ -332,6 +332,11 @@ ir3_finalize_nir(struct ir3_compiler *compiler, nir_shader *s)
 		debug_printf("----------------------\n");
 	}
 
+	nir_foreach_uniform_variable_safe(var, s) {
+		exec_node_remove(&var->node);
+	}
+	nir_validate_shader(s, "after uniform var removal");
+
 	nir_sweep(s);
 }
 

commit ba605836c008ee87cec70f6927178c906d67179d

freedreno: Fix YUV sampler regression.
We have to keep sampler uniforms around for later YUV lowering, and we
only need to remove uniforms that take up storage space.  Code comes from
radeonsi.

Closes: #4644.
Fixes: 9ef0fc401cc7 ("freedreno: Remove uniform variables after finalizing NIR.")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10246>

src/freedreno/ir3/ir3_nir.c
@@ -337,7 +337,17 @@ ir3_finalize_nir(struct ir3_compiler *compiler, nir_shader *s)
 		debug_printf("----------------------\n");
 	}
 
+	/* st_program.c's parameter list optimization requires that future nir
+	 * variants don't reallocate the uniform storage, so we have to remove
+	 * uniforms that occupy storage.  But we don't want to remove samplers,
+	 * because they're needed for YUV variant lowering.
+	 */
 	nir_foreach_uniform_variable_safe(var, s) {
+      if (var->data.mode == nir_var_uniform &&
+          (glsl_type_get_image_count(var->type) ||
+           glsl_type_get_sampler_count(var->type)))
+         continue;
+
 		exec_node_remove(&var->node);
 	}
 	nir_validate_shader(s, "after uniform var removal");