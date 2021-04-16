openharmony/third_party_mesa3d

commit 8e81b0fb1f6dc2cc72c08cf251408c5f0bc0470c

st/nine: Disable fpu exceptions during init
Prevents a crash in wine tests. The crash
seems to happen in llvmpipe.

Signed-off-by: Axel Davy <davyaxel0@gmail.com>
Acked-by: Timur Kristóf <timur.kristof@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10160>

src/gallium/frontends/nine/device9.c
@@ -75,11 +75,35 @@ static void nine_setup_fpu()
     __asm__ __volatile__ ("fldcw %0" : : "m" (*&c));
 }
 
+static void nine_setup_set_fpu(uint16_t val)
+{
+    __asm__ __volatile__ ("fldcw %0" : : "m" (*&val));
+}
+
+static uint16_t nine_setup_get_fpu()
+{
+    uint16_t c;
+
+    __asm__ __volatile__ ("fnstcw %0" : "=m" (*&c));
+    return c;
+}
+
 #else
 
 static void nine_setup_fpu(void)
 {
     WARN_ONCE("FPU setup not supported on non-x86 platforms\n");
+}
+
+static void nine_setup_set_fpu(uint16_t)
+{
+    WARN_ONCE("FPU setup not supported on non-x86 platforms\n");
+}
+
+static uint16_t nine_setup_get_fpu()
+{
+    WARN_ONCE("FPU setup not supported on non-x86 platforms\n");
+    return 0;
 }
 
 #endif
@@ -159,6 +183,7 @@ NineDevice9_ctor( struct NineDevice9 *This,
                   int minorVersionNum )
 {
     unsigned i;
+    uint16_t fpu_cw = 0;
     HRESULT hr = NineUnknown_ctor(&This->base, pParams);
 
     DBG("This=%p pParams=%p pScreen=%p pCreationParameters=%p pCaps=%p pPresentationParameters=%p "
@@ -188,8 +213,13 @@ NineDevice9_ctor( struct NineDevice9 *This,
     IDirect3D9_AddRef(This->d3d9);
     ID3DPresentGroup_AddRef(This->present);
 
-    if (!(This->params.BehaviorFlags & D3DCREATE_FPU_PRESERVE))
+    if (!(This->params.BehaviorFlags & D3DCREATE_FPU_PRESERVE)) {
         nine_setup_fpu();
+    } else {
+        /* Software renderer initialization needs exceptions masked */
+        fpu_cw = nine_setup_get_fpu();
+        nine_setup_set_fpu(fpu_cw | 0x007f);
+    }
 
     if (This->params.BehaviorFlags & D3DCREATE_SOFTWARE_VERTEXPROCESSING) {
         DBG("Application asked full Software Vertex Processing.\n");
@@ -543,6 +573,9 @@ NineDevice9_ctor( struct NineDevice9 *This,
     ID3DPresentGroup_Release(This->present);
     nine_context_update_state(This); /* Some drivers needs states to be initialized */
     nine_csmt_process(This);
+
+    if (This->params.BehaviorFlags & D3DCREATE_FPU_PRESERVE)
+        nine_setup_set_fpu(fpu_cw);
 
     return D3D_OK;
 }

commit 35c6c3b9f39a4389ba707ae92023c32461fa4bb6

st/nine: Fix compilation error on non-x86 platforms
The unused variable was improperly declared.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4647
Fixes: 8e81b0fb1f ("st/nine: Disable fpu exceptions during init")

Signed-off-by: Axel Davy <davyaxel0@gmail.com>
Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10263>

src/gallium/frontends/nine/device9.c
@@ -45,6 +45,7 @@
 #include "pipe/p_screen.h"
 #include "pipe/p_context.h"
 #include "pipe/p_config.h"
+#include "util/macros.h"
 #include "util/u_math.h"
 #include "util/u_inlines.h"
 #include "util/u_hash_table.h"
@@ -95,7 +96,7 @@ static void nine_setup_fpu(void)
     WARN_ONCE("FPU setup not supported on non-x86 platforms\n");
 }
 
-static void nine_setup_set_fpu(uint16_t)
+static void nine_setup_set_fpu(UNUSED uint16_t val)
 {
     WARN_ONCE("FPU setup not supported on non-x86 platforms\n");
 }