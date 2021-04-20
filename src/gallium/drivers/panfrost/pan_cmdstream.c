openharmony/third_party_mesa3d

commit 6cd7cb34fce111aa06521adb836fbdc277a20aeb

panfrost: Use the generic preload and FB helpers in the gallium driver
We also rework the code to have the FB descriptor shared by the tiler
and fragment jobs, instead of allocating a separate FB descriptor for
tiler jobs.

The new implementation seems to fix a few dEQP failures on T720.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10033>

src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -2251,3 +2251,117 @@ panfrost_emit_vertex_tiler_jobs(struct panfrost_batch *batch,
                          MALI_JOB_TYPE_TILER, false, false,
                          vertex, 0, tiler_job, false);
 }
+
+void
+panfrost_emit_tls(struct panfrost_batch *batch)
+{
+        struct panfrost_device *dev = pan_device(batch->ctx->base.screen);
+
+        /* Emitted with the FB descriptor on Midgard. */
+        if (!pan_is_bifrost(dev))
+                return;
+
+        struct panfrost_bo *tls_bo =
+                batch->stack_size ?
+                panfrost_batch_get_scratchpad(batch,
+                                              batch->stack_size,
+                                              dev->thread_tls_alloc,
+                                              dev->core_count):
+                NULL;
+        struct pan_tls_info tls = {
+                .tls = {
+                        .ptr = tls_bo ? tls_bo->ptr.gpu : 0,
+                        .size = batch->stack_size,
+                },
+        };
+
+        assert(batch->tls.cpu);
+        pan_emit_tls(dev, &tls, batch->tls.cpu);
+}
+
+void
+panfrost_emit_fbd(struct panfrost_batch *batch,
+                  const struct pan_fb_info *fb)
+{
+        struct panfrost_device *dev = pan_device(batch->ctx->base.screen);
+        struct panfrost_bo *tls_bo =
+                batch->stack_size ?
+                panfrost_batch_get_scratchpad(batch,
+                                              batch->stack_size,
+                                              dev->thread_tls_alloc,
+                                              dev->core_count):
+                NULL;
+        struct pan_tls_info tls = {
+                .tls = {
+                        .ptr = tls_bo ? tls_bo->ptr.gpu : 0,
+                        .size = batch->stack_size,
+                },
+        };
+
+        batch->framebuffer.gpu |=
+                pan_emit_fbd(dev, fb, &tls, &batch->tiler_ctx,
+                             batch->framebuffer.cpu);
+}
+
+/* Mark a surface as written */
+
+static void
+panfrost_initialize_surface(struct panfrost_batch *batch,
+                            struct pipe_surface *surf)
+{
+        if (!surf)
+                return;
+
+        unsigned level = surf->u.tex.level;
+        struct panfrost_resource *rsrc = pan_resource(surf->texture);
+
+        rsrc->state.slices[level].data_valid = true;
+}
+
+/* Generate a fragment job. This should be called once per frame. (According to
+ * presentations, this is supposed to correspond to eglSwapBuffers) */
+
+mali_ptr
+panfrost_emit_fragment_job(struct panfrost_batch *batch,
+                           const struct pan_fb_info *pfb)
+{
+        struct panfrost_device *dev = pan_device(batch->ctx->base.screen);
+
+        /* Mark the affected buffers as initialized, since we're writing to it.
+         * Also, add the surfaces we're writing to to the batch */
+
+        struct pipe_framebuffer_state *fb = &batch->key;
+
+        for (unsigned i = 0; i < fb->nr_cbufs; ++i)
+                panfrost_initialize_surface(batch, fb->cbufs[i]);
+
+        panfrost_initialize_surface(batch, fb->zsbuf);
+
+        /* The passed tile coords can be out of range in some cases, so we need
+         * to clamp them to the framebuffer size to avoid a TILE_RANGE_FAULT.
+         * Theoretically we also need to clamp the coordinates positive, but we
+         * avoid that edge case as all four values are unsigned. Also,
+         * theoretically we could clamp the minima, but if that has to happen
+         * the asserts would fail anyway (since the maxima would get clamped
+         * and then be smaller than the minima). An edge case of sorts occurs
+         * when no scissors are added to draw, so by default min=~0 and max=0.
+         * But that can't happen if any actual drawing occurs (beyond a
+         * wallpaper reload), so this is again irrelevant in practice. */
+
+        batch->maxx = MIN2(batch->maxx, fb->width);
+        batch->maxy = MIN2(batch->maxy, fb->height);
+
+        /* Rendering region must be at least 1x1; otherwise, there is nothing
+         * to do and the whole job chain should have been discarded. */
+
+        assert(batch->maxx > batch->minx);
+        assert(batch->maxy > batch->miny);
+
+        struct panfrost_ptr transfer =
+                panfrost_pool_alloc_desc(&batch->pool, FRAGMENT_JOB);
+
+        pan_emit_fragment_job(dev, pfb, batch->framebuffer.gpu,
+                              transfer.cpu);
+
+        return transfer.gpu;
+}

src/gallium/drivers/panfrost/pan_context.c
@@ -57,69 +57,6 @@
 #include "decode.h"
 #include "util/pan_lower_framebuffer.h"
 
-void
-panfrost_emit_midg_tiler(struct panfrost_batch *batch,
-                         struct mali_midgard_tiler_packed *tp,
-                         unsigned vertex_count)
-{
-        struct panfrost_device *device = pan_device(batch->ctx->base.screen);
-        bool hierarchy = !(device->quirks & MIDGARD_NO_HIER_TILING);
-        unsigned height = batch->key.height;
-        unsigned width = batch->key.width;
-
-        pan_pack(tp, MIDGARD_TILER, t) {
-                t.hierarchy_mask =
-                        panfrost_choose_hierarchy_mask(width, height,
-                                                       vertex_count, hierarchy);
-
-                /* Compute the polygon header size and use that to offset the body */
-
-                unsigned header_size =
-                        panfrost_tiler_header_size(width, height,
-                                                   t.hierarchy_mask, hierarchy);
-
-                t.polygon_list_size =
-                        panfrost_tiler_full_size(width, height, t.hierarchy_mask,
-                                                 hierarchy);
-
-                if (vertex_count) {
-                        t.polygon_list =
-                                panfrost_batch_get_polygon_list(batch,
-                                                                header_size +
-                                                                t.polygon_list_size);
-
-                        t.heap_start = device->tiler_heap->ptr.gpu;
-                        t.heap_end = device->tiler_heap->ptr.gpu +
-                                     device->tiler_heap->size;
-                } else {
-                        struct panfrost_bo *tiler_dummy;
-
-                        tiler_dummy = panfrost_batch_get_tiler_dummy(batch);
-                        header_size = MALI_MIDGARD_TILER_MINIMUM_HEADER_SIZE;
-
-                        /* The tiler is disabled, so don't allow the tiler heap */
-                        t.heap_start = tiler_dummy->ptr.gpu;
-                        t.heap_end = t.heap_start;
-
-                        /* Use a dummy polygon list */
-                        t.polygon_list = tiler_dummy->ptr.gpu;
-
-                        /* Disable the tiler */
-                        if (hierarchy)
-                                t.hierarchy_mask |= MALI_MIDGARD_TILER_DISABLED;
-                        else {
-                                t.hierarchy_mask = MALI_MIDGARD_TILER_USER;
-                                t.polygon_list_size = MALI_MIDGARD_TILER_MINIMUM_HEADER_SIZE + 4;
-
-                                /* We don't have a WRITE_VALUE job, so write the polygon list manually */
-                                uint32_t *polygon_list_body = (uint32_t *) (tiler_dummy->ptr.cpu + header_size);
-                                polygon_list_body[0] = 0xa0000000; /* TODO: Just that? */
-                        }
-                }
-                t.polygon_list_body = t.polygon_list + header_size;
-        }
-}
-
 static void
 panfrost_clear(
         struct pipe_context *pipe,
@@ -523,7 +460,7 @@ panfrost_direct_draw(struct panfrost_context *ctx,
 
         unsigned vertex_count = ctx->vertex_count;
 
-        mali_ptr shared_mem = panfrost_batch_reserve_framebuffer(batch);
+        mali_ptr shared_mem = panfrost_batch_reserve_tls(batch);
 
         unsigned min_index = 0, max_index = 0;
         mali_ptr indices = 0;

src/gallium/drivers/panfrost/pan_job.h
@@ -28,6 +28,7 @@
 
 #include "util/u_dynarray.h"
 #include "pipe/p_state.h"
+#include "pan_cs.h"
 #include "pan_pool.h"
 #include "pan_resource.h"
 #include "pan_scoreboard.h"
@@ -108,17 +109,14 @@ struct panfrost_batch {
         /* Shared memory BO bound to the batch, or NULL if none bound yet */
         struct panfrost_bo *shared_memory;
 
-        /* Tiler heap BO bound to the batch, or NULL if none bound yet */
-        struct panfrost_bo *tiler_heap;
-
-        /* Dummy tiler BO bound to the batch, or NULL if none bound yet */
-        struct panfrost_bo *tiler_dummy;
-
         /* Framebuffer descriptor. */
         struct panfrost_ptr framebuffer;
 
-        /* Bifrost tiler meta descriptor. */
-        mali_ptr tiler_meta;
+        /* Thread local storage descriptor. */
+        struct panfrost_ptr tls;
+
+        /* Tiler context */
+        struct pan_tiler_context tiler_ctx;
 
         /* Output sync object. Only valid when submitted is true. */
         struct panfrost_batch_fence *out_sync;
@@ -179,12 +177,6 @@ panfrost_batch_get_scratchpad(struct panfrost_batch *batch, unsigned size, unsig
 struct panfrost_bo *
 panfrost_batch_get_shared_memory(struct panfrost_batch *batch, unsigned size, unsigned workgroup_count);
 
-mali_ptr
-panfrost_batch_get_polygon_list(struct panfrost_batch *batch, unsigned size);
-
-struct panfrost_bo *
-panfrost_batch_get_tiler_dummy(struct panfrost_batch *batch);
-
 void
 panfrost_batch_clear(struct panfrost_batch *batch,
                      unsigned buffers,
@@ -206,5 +198,8 @@ panfrost_batch_get_bifrost_tiler(struct panfrost_batch *batch, unsigned vertex_c
 
 mali_ptr
 panfrost_batch_reserve_framebuffer(struct panfrost_batch *batch);
+
+mali_ptr
+panfrost_batch_reserve_tls(struct panfrost_batch *batch);
 
 #endif

src/gallium/drivers/panfrost/Makefile.sources
@@ -8,13 +8,10 @@ C_SOURCES := \
 	pan_compute.c \
 	pan_context.c \
 	pan_context.h \
-	pan_fragment.c \
 	pan_job.c \
 	pan_job.h \
-	pan_mfbd.c \
 	pan_public.h \
 	pan_resource.c \
 	pan_resource.h \
 	pan_screen.c \
-	pan_screen.h \
-	pan_sfbd.c \
+	pan_screen.h

src/gallium/drivers/panfrost/meson.build
@@ -32,9 +32,6 @@ files_panfrost = files(
   'pan_blend_cso.c',
   'pan_cmdstream.c',
   'pan_compute.c',
-  'pan_fragment.c',
-  'pan_sfbd.c',
-  'pan_mfbd.c',
 )
 
 panfrost_includes = [

src/gallium/drivers/panfrost/pan_cmdstream.h
@@ -92,6 +92,17 @@ panfrost_emit_vertex_tiler_jobs(struct panfrost_batch *batch,
                                 const struct panfrost_ptr *vertex_job,
                                 const struct panfrost_ptr *tiler_job);
 
+mali_ptr
+panfrost_emit_fragment_job(struct panfrost_batch *batch,
+                           const struct pan_fb_info *fb);
+
+void
+panfrost_emit_tls(struct panfrost_batch *batch);
+
+void
+panfrost_emit_fbd(struct panfrost_batch *batch,
+                  const struct pan_fb_info *fb);
+
 static inline unsigned
 panfrost_translate_compare_func(enum pipe_compare_func in)
 {

src/gallium/drivers/panfrost/pan_context.h
@@ -315,23 +315,6 @@ panfrost_flush(
 bool
 panfrost_render_condition_check(struct panfrost_context *ctx);
 
-mali_ptr panfrost_sfbd_fragment(struct panfrost_batch *batch, bool has_draws);
-mali_ptr panfrost_mfbd_fragment(struct panfrost_batch *batch, bool has_draws);
-
-void
-panfrost_attach_mfbd(struct panfrost_batch *batch, unsigned vertex_count);
-
-void
-panfrost_attach_sfbd(struct panfrost_batch *batch, unsigned vertex_count);
-
-void
-panfrost_emit_midg_tiler(struct panfrost_batch *batch,
-                         struct mali_midgard_tiler_packed *tp,
-                         unsigned vertex_count);
-
-mali_ptr
-panfrost_fragment_job(struct panfrost_batch *batch, bool has_draws);
-
 void
 panfrost_shader_compile(struct panfrost_context *ctx,
                         enum pipe_shader_ir ir_type,

commit 87fae5366895007d41c33ba8920c0d406e177530

panfrost: Reserve thread storage descriptor in panfrost_launch_grid()
If we don't do that the compute batch is left with an empty thread
storage descriptor, and panfrost_batch_submit() tries to emit an FB
descriptors using invalid FB information.

Reported-by: Italo Nicola <italonicola@collabora.com>
Fixes: 6cd7cb34fce1 ("panfrost: Use the generic preload and FB helpers in the gallium driver")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Alyssa Rosenzweig <alyssa@collabora.com>
Tested-by: Italo Nicola <italonicola@collabora.com>
Reviewed-by: Italo Nicola <italonicola@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10312>

src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -2267,7 +2267,7 @@ panfrost_emit_tls(struct panfrost_batch *batch)
         struct panfrost_device *dev = pan_device(batch->ctx->base.screen);
 
         /* Emitted with the FB descriptor on Midgard. */
-        if (!pan_is_bifrost(dev))
+        if (!pan_is_bifrost(dev) && batch->framebuffer.gpu)
                 return;
 
         struct panfrost_bo *tls_bo =

src/gallium/drivers/panfrost/pan_compute.c
@@ -101,6 +101,11 @@ panfrost_launch_grid(struct pipe_context *pipe,
         struct panfrost_device *dev = pan_device(pipe->screen);
         struct panfrost_batch *batch = panfrost_get_batch_for_fbo(ctx);
 
+        /* Reserve a thread storage descriptor now (will be emitted at submit
+         * time).
+         */
+        panfrost_batch_reserve_tls(batch, true);
+
         /* TODO: Indirect compute dispatch */
         assert(!info->indirect);
 

src/gallium/drivers/panfrost/pan_context.c
@@ -460,7 +460,7 @@ panfrost_direct_draw(struct panfrost_context *ctx,
 
         unsigned vertex_count = ctx->vertex_count;
 
-        mali_ptr shared_mem = panfrost_batch_reserve_tls(batch);
+        mali_ptr shared_mem = panfrost_batch_reserve_tls(batch, false);
 
         unsigned min_index = 0, max_index = 0;
         mali_ptr indices = 0;

src/gallium/drivers/panfrost/pan_job.c
@@ -910,7 +910,7 @@ panfrost_batch_reserve_framebuffer(struct panfrost_batch *batch)
 }
 
 mali_ptr
-panfrost_batch_reserve_tls(struct panfrost_batch *batch)
+panfrost_batch_reserve_tls(struct panfrost_batch *batch, bool compute)
 {
         struct panfrost_device *dev = pan_device(batch->ctx->base.screen);
 
@@ -919,7 +919,7 @@ panfrost_batch_reserve_tls(struct panfrost_batch *batch)
         if (batch->tls.gpu)
                 return batch->tls.gpu;
 
-        if (pan_is_bifrost(dev)) {
+        if (pan_is_bifrost(dev) || compute) {
                 batch->tls = panfrost_pool_alloc_desc(&batch->pool, LOCAL_STORAGE);
         } else {
                 /* On Midgard, the FB descriptor contains a thread storage
@@ -1122,7 +1122,7 @@ panfrost_batch_submit(struct panfrost_batch *batch,
 
         panfrost_batch_to_fb_info(batch, &fb, rts, &zs, &s, false);
 
-        panfrost_batch_reserve_tls(batch);
+        panfrost_batch_reserve_tls(batch, false);
         panfrost_batch_draw_wallpaper(batch, &fb);
 
 

src/gallium/drivers/panfrost/pan_job.h
@@ -200,6 +200,6 @@ mali_ptr
 panfrost_batch_reserve_framebuffer(struct panfrost_batch *batch);
 
 mali_ptr
-panfrost_batch_reserve_tls(struct panfrost_batch *batch);
+panfrost_batch_reserve_tls(struct panfrost_batch *batch, bool compute);
 
 #endif