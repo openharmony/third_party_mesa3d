openharmony/third_party_mesa3d

commit 6cd7cb34fce111aa06521adb836fbdc277a20aeb

panfrost: Use the generic preload and FB helpers in the gallium driver
We also rework the code to have the FB descriptor shared by the tiler
and fragment jobs, instead of allocating a separate FB descriptor for
tiler jobs.

The new implementation seems to fix a few dEQP failures on T720.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10033>

src/gallium/drivers/panfrost/pan_context.c
@@ -57,69 +57,6 @@
 #include "decode.h"
 #include "util/pan_lower_framebuffer.h"
 
-void
-panfrost_emit_midg_tiler(struct panfrost_batch *batch,
-                         struct mali_midgard_tiler_packed *tp,
-                         unsigned vertex_count)
-{
-        struct panfrost_device *device = pan_device(batch->ctx->base.screen);
-        bool hierarchy = !(device->quirks & MIDGARD_NO_HIER_TILING);
-        unsigned height = batch->key.height;
-        unsigned width = batch->key.width;
-
-        pan_pack(tp, MIDGARD_TILER, t) {
-                t.hierarchy_mask =
-                        panfrost_choose_hierarchy_mask(width, height,
-                                                       vertex_count, hierarchy);
-
-                /* Compute the polygon header size and use that to offset the body */
-
-                unsigned header_size =
-                        panfrost_tiler_header_size(width, height,
-                                                   t.hierarchy_mask, hierarchy);
-
-                t.polygon_list_size =
-                        panfrost_tiler_full_size(width, height, t.hierarchy_mask,
-                                                 hierarchy);
-
-                if (vertex_count) {
-                        t.polygon_list =
-                                panfrost_batch_get_polygon_list(batch,
-                                                                header_size +
-                                                                t.polygon_list_size);
-
-                        t.heap_start = device->tiler_heap->ptr.gpu;
-                        t.heap_end = device->tiler_heap->ptr.gpu +
-                                     device->tiler_heap->size;
-                } else {
-                        struct panfrost_bo *tiler_dummy;
-
-                        tiler_dummy = panfrost_batch_get_tiler_dummy(batch);
-                        header_size = MALI_MIDGARD_TILER_MINIMUM_HEADER_SIZE;
-
-                        /* The tiler is disabled, so don't allow the tiler heap */
-                        t.heap_start = tiler_dummy->ptr.gpu;
-                        t.heap_end = t.heap_start;
-
-                        /* Use a dummy polygon list */
-                        t.polygon_list = tiler_dummy->ptr.gpu;
-
-                        /* Disable the tiler */
-                        if (hierarchy)
-                                t.hierarchy_mask |= MALI_MIDGARD_TILER_DISABLED;
-                        else {
-                                t.hierarchy_mask = MALI_MIDGARD_TILER_USER;
-                                t.polygon_list_size = MALI_MIDGARD_TILER_MINIMUM_HEADER_SIZE + 4;
-
-                                /* We don't have a WRITE_VALUE job, so write the polygon list manually */
-                                uint32_t *polygon_list_body = (uint32_t *) (tiler_dummy->ptr.cpu + header_size);
-                                polygon_list_body[0] = 0xa0000000; /* TODO: Just that? */
-                        }
-                }
-                t.polygon_list_body = t.polygon_list + header_size;
-        }
-}
-
 static void
 panfrost_clear(
         struct pipe_context *pipe,
@@ -523,7 +460,7 @@ panfrost_direct_draw(struct panfrost_context *ctx,
 
         unsigned vertex_count = ctx->vertex_count;
 
-        mali_ptr shared_mem = panfrost_batch_reserve_framebuffer(batch);
+        mali_ptr shared_mem = panfrost_batch_reserve_tls(batch);
 
         unsigned min_index = 0, max_index = 0;
         mali_ptr indices = 0;

src/gallium/drivers/panfrost/Makefile.sources
@@ -8,13 +8,10 @@ C_SOURCES := \
 	pan_compute.c \
 	pan_context.c \
 	pan_context.h \
-	pan_fragment.c \
 	pan_job.c \
 	pan_job.h \
-	pan_mfbd.c \
 	pan_public.h \
 	pan_resource.c \
 	pan_resource.h \
 	pan_screen.c \
-	pan_screen.h \
-	pan_sfbd.c \
+	pan_screen.h

src/gallium/drivers/panfrost/meson.build
@@ -32,9 +32,6 @@ files_panfrost = files(
   'pan_blend_cso.c',
   'pan_cmdstream.c',
   'pan_compute.c',
-  'pan_fragment.c',
-  'pan_sfbd.c',
-  'pan_mfbd.c',
 )
 
 panfrost_includes = [

src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -2251,3 +2251,117 @@ panfrost_emit_vertex_tiler_jobs(struct panfrost_batch *batch,
                          MALI_JOB_TYPE_TILER, false, false,
                          vertex, 0, tiler_job, false);
 }
+
+void
+panfrost_emit_tls(struct panfrost_batch *batch)
+{
+        struct panfrost_device *dev = pan_device(batch->ctx->base.screen);
+
+        /* Emitted with the FB descriptor on Midgard. */
+        if (!pan_is_bifrost(dev))
+                return;
+
+        struct panfrost_bo *tls_bo =
+                batch->stack_size ?
+                panfrost_batch_get_scratchpad(batch,
+                                              batch->stack_size,
+                                              dev->thread_tls_alloc,
+                                              dev->core_count):
+                NULL;
+        struct pan_tls_info tls = {
+                .tls = {
+                        .ptr = tls_bo ? tls_bo->ptr.gpu : 0,
+                        .size = batch->stack_size,
+                },
+        };
+
+        assert(batch->tls.cpu);
+        pan_emit_tls(dev, &tls, batch->tls.cpu);
+}
+
+void
+panfrost_emit_fbd(struct panfrost_batch *batch,
+                  const struct pan_fb_info *fb)
+{
+        struct panfrost_device *dev = pan_device(batch->ctx->base.screen);
+        struct panfrost_bo *tls_bo =
+                batch->stack_size ?
+                panfrost_batch_get_scratchpad(batch,
+                                              batch->stack_size,
+                                              dev->thread_tls_alloc,
+                                              dev->core_count):
+                NULL;
+        struct pan_tls_info tls = {
+                .tls = {
+                        .ptr = tls_bo ? tls_bo->ptr.gpu : 0,
+                        .size = batch->stack_size,
+                },
+        };
+
+        batch->framebuffer.gpu |=
+                pan_emit_fbd(dev, fb, &tls, &batch->tiler_ctx,
+                             batch->framebuffer.cpu);
+}
+
+/* Mark a surface as written */
+
+static void
+panfrost_initialize_surface(struct panfrost_batch *batch,
+                            struct pipe_surface *surf)
+{
+        if (!surf)
+                return;
+
+        unsigned level = surf->u.tex.level;
+        struct panfrost_resource *rsrc = pan_resource(surf->texture);
+
+        rsrc->state.slices[level].data_valid = true;
+}
+
+/* Generate a fragment job. This should be called once per frame. (According to
+ * presentations, this is supposed to correspond to eglSwapBuffers) */
+
+mali_ptr
+panfrost_emit_fragment_job(struct panfrost_batch *batch,
+                           const struct pan_fb_info *pfb)
+{
+        struct panfrost_device *dev = pan_device(batch->ctx->base.screen);
+
+        /* Mark the affected buffers as initialized, since we're writing to it.
+         * Also, add the surfaces we're writing to to the batch */
+
+        struct pipe_framebuffer_state *fb = &batch->key;
+
+        for (unsigned i = 0; i < fb->nr_cbufs; ++i)
+                panfrost_initialize_surface(batch, fb->cbufs[i]);
+
+        panfrost_initialize_surface(batch, fb->zsbuf);
+
+        /* The passed tile coords can be out of range in some cases, so we need
+         * to clamp them to the framebuffer size to avoid a TILE_RANGE_FAULT.
+         * Theoretically we also need to clamp the coordinates positive, but we
+         * avoid that edge case as all four values are unsigned. Also,
+         * theoretically we could clamp the minima, but if that has to happen
+         * the asserts would fail anyway (since the maxima would get clamped
+         * and then be smaller than the minima). An edge case of sorts occurs
+         * when no scissors are added to draw, so by default min=~0 and max=0.
+         * But that can't happen if any actual drawing occurs (beyond a
+         * wallpaper reload), so this is again irrelevant in practice. */
+
+        batch->maxx = MIN2(batch->maxx, fb->width);
+        batch->maxy = MIN2(batch->maxy, fb->height);
+
+        /* Rendering region must be at least 1x1; otherwise, there is nothing
+         * to do and the whole job chain should have been discarded. */
+
+        assert(batch->maxx > batch->minx);
+        assert(batch->maxy > batch->miny);
+
+        struct panfrost_ptr transfer =
+                panfrost_pool_alloc_desc(&batch->pool, FRAGMENT_JOB);
+
+        pan_emit_fragment_job(dev, pfb, batch->framebuffer.gpu,
+                              transfer.cpu);
+
+        return transfer.gpu;
+}

src/gallium/drivers/panfrost/pan_cmdstream.h
@@ -92,6 +92,17 @@ panfrost_emit_vertex_tiler_jobs(struct panfrost_batch *batch,
                                 const struct panfrost_ptr *vertex_job,
                                 const struct panfrost_ptr *tiler_job);
 
+mali_ptr
+panfrost_emit_fragment_job(struct panfrost_batch *batch,
+                           const struct pan_fb_info *fb);
+
+void
+panfrost_emit_tls(struct panfrost_batch *batch);
+
+void
+panfrost_emit_fbd(struct panfrost_batch *batch,
+                  const struct pan_fb_info *fb);
+
 static inline unsigned
 panfrost_translate_compare_func(enum pipe_compare_func in)
 {

src/gallium/drivers/panfrost/pan_context.h
@@ -315,23 +315,6 @@ panfrost_flush(
 bool
 panfrost_render_condition_check(struct panfrost_context *ctx);
 
-mali_ptr panfrost_sfbd_fragment(struct panfrost_batch *batch, bool has_draws);
-mali_ptr panfrost_mfbd_fragment(struct panfrost_batch *batch, bool has_draws);
-
-void
-panfrost_attach_mfbd(struct panfrost_batch *batch, unsigned vertex_count);
-
-void
-panfrost_attach_sfbd(struct panfrost_batch *batch, unsigned vertex_count);
-
-void
-panfrost_emit_midg_tiler(struct panfrost_batch *batch,
-                         struct mali_midgard_tiler_packed *tp,
-                         unsigned vertex_count);
-
-mali_ptr
-panfrost_fragment_job(struct panfrost_batch *batch, bool has_draws);
-
 void
 panfrost_shader_compile(struct panfrost_context *ctx,
                         enum pipe_shader_ir ir_type,

src/gallium/drivers/panfrost/pan_fragment.c
@@ -1,109 +1,0 @@
-/*
- * Copyright (C) 2019-2020 Collabora, Ltd.
- * Copyright 2018-2019 Alyssa Rosenzweig
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice (including the next
- * paragraph) shall be included in all copies or substantial portions of the
- * Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- */
-
-#include "pan_context.h"
-#include "pan_util.h"
-#include "panfrost-quirks.h"
-
-#include "util/format/u_format.h"
-
-/* Mark a surface as written */
-
-static void
-panfrost_initialize_surface(
-                struct panfrost_batch *batch,
-                struct pipe_surface *surf)
-{
-        if (!surf)
-                return;
-
-        unsigned level = surf->u.tex.level;
-        struct panfrost_resource *rsrc = pan_resource(surf->texture);
-
-        rsrc->state.slices[level].data_valid = true;
-}
-
-/* Generate a fragment job. This should be called once per frame. (According to
- * presentations, this is supposed to correspond to eglSwapBuffers) */
-
-mali_ptr
-panfrost_fragment_job(struct panfrost_batch *batch, bool has_draws)
-{
-        struct panfrost_device *dev = pan_device(batch->ctx->base.screen);
-
-        mali_ptr framebuffer = (dev->quirks & MIDGARD_SFBD) ?
-                               panfrost_sfbd_fragment(batch, has_draws) :
-                               panfrost_mfbd_fragment(batch, has_draws);
-
-        /* Mark the affected buffers as initialized, since we're writing to it.
-         * Also, add the surfaces we're writing to to the batch */
-
-        struct pipe_framebuffer_state *fb = &batch->key;
-
-        for (unsigned i = 0; i < fb->nr_cbufs; ++i)
-                panfrost_initialize_surface(batch, fb->cbufs[i]);
-
-        panfrost_initialize_surface(batch, fb->zsbuf);
-
-        /* The passed tile coords can be out of range in some cases, so we need
-         * to clamp them to the framebuffer size to avoid a TILE_RANGE_FAULT.
-         * Theoretically we also need to clamp the coordinates positive, but we
-         * avoid that edge case as all four values are unsigned. Also,
-         * theoretically we could clamp the minima, but if that has to happen
-         * the asserts would fail anyway (since the maxima would get clamped
-         * and then be smaller than the minima). An edge case of sorts occurs
-         * when no scissors are added to draw, so by default min=~0 and max=0.
-         * But that can't happen if any actual drawing occurs (beyond a
-         * wallpaper reload), so this is again irrelevant in practice. */
-
-        batch->maxx = MIN2(batch->maxx, fb->width);
-        batch->maxy = MIN2(batch->maxy, fb->height);
-
-        /* Rendering region must be at least 1x1; otherwise, there is nothing
-         * to do and the whole job chain should have been discarded. */
-
-        assert(batch->maxx > batch->minx);
-        assert(batch->maxy > batch->miny);
-
-        struct panfrost_ptr transfer =
-                panfrost_pool_alloc_desc(&batch->pool, FRAGMENT_JOB);
-
-        pan_section_pack(transfer.cpu, FRAGMENT_JOB, HEADER, header) {
-                header.type = MALI_JOB_TYPE_FRAGMENT;
-                header.index = 1;
-        }
-
-        pan_section_pack(transfer.cpu, FRAGMENT_JOB, PAYLOAD, payload) {
-                payload.bound_min_x = batch->minx >> MALI_TILE_SHIFT;
-                payload.bound_min_y = batch->miny >> MALI_TILE_SHIFT;
-
-                /* Batch max values are inclusive, we need to subtract 1. */
-                payload.bound_max_x = (batch->maxx - 1) >> MALI_TILE_SHIFT;
-                payload.bound_max_y = (batch->maxy - 1) >> MALI_TILE_SHIFT;
-                payload.framebuffer = framebuffer;
-        }
-
-        return transfer.gpu;
-}

commit a75113b025a46ec6d6ba3dc97bbbec3ce061a89a

panfrost: Fix indirect draws
Use panfrost_batch_reserve_tls() to reserve the TSD.

Cc: mesa-stable
Fixes: 6cd7cb34fce1 ("panfrost: Use the generic preload and FB helpers in the gallium driver")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Alyssa Rosenzweig <alyssa@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10369>

src/gallium/drivers/panfrost/pan_context.c
@@ -570,7 +570,7 @@ panfrost_indirect_draw(struct panfrost_context *ctx,
 
         panfrost_batch_set_requirements(batch);
 
-        mali_ptr shared_mem = panfrost_batch_reserve_framebuffer(batch);
+        mali_ptr shared_mem = panfrost_batch_reserve_tls(batch, false);
 
         struct panfrost_ptr tiler =
                 panfrost_pool_alloc_aligned(&batch->pool,