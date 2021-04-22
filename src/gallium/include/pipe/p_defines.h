openharmony/third_party_mesa3d

commit d2301e6038193d7052942667041fc8b8fcee21bc

gallium: add PIPE_BIND_SAMPLER_REDUCTION_MINMAX
for querying format support

Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10030>

src/gallium/include/pipe/p_defines.h
@@ -490,6 +490,7 @@ enum pipe_flush_flags
 #define PIPE_BIND_COMPUTE_RESOURCE     (1 << 16) /* set_compute_resources */
 #define PIPE_BIND_COMMAND_ARGS_BUFFER  (1 << 17) /* pipe_draw_info.indirect */
 #define PIPE_BIND_QUERY_BUFFER         (1 << 18) /* get_query_result_resource */
+#define PIPE_BIND_SAMPLER_REDUCTION_MINMAX (1 << 19) /* PIPE_CAP_SAMPLER_REDUCTION_MINMAX */
 
 /**
  * The first two flags above were previously part of the amorphous

commit 2c07d6baa251c1486176f1faa406249df3770ee1

gallium: Fix PIPE_BIND_SAMPLER_REDUCTION_MINMAX definition to be unique
This accidentally collided with PIPE_BIND_SCANOUT.

Fixes: d2301e60381 gallium: add PIPE_BIND_SAMPLER_REDUCTION_MINMAX
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10397>

src/gallium/include/pipe/p_defines.h
@@ -490,7 +490,6 @@ enum pipe_flush_flags
 #define PIPE_BIND_COMPUTE_RESOURCE     (1 << 16) /* set_compute_resources */
 #define PIPE_BIND_COMMAND_ARGS_BUFFER  (1 << 17) /* pipe_draw_info.indirect */
 #define PIPE_BIND_QUERY_BUFFER         (1 << 18) /* get_query_result_resource */
-#define PIPE_BIND_SAMPLER_REDUCTION_MINMAX (1 << 19) /* PIPE_CAP_SAMPLER_REDUCTION_MINMAX */
 
 /**
  * The first two flags above were previously part of the amorphous
@@ -514,6 +513,7 @@ enum pipe_flush_flags
 #define PIPE_BIND_SHARED      (1 << 20) /* get_texture_handle ??? */
 #define PIPE_BIND_LINEAR      (1 << 21)
 #define PIPE_BIND_PROTECTED   (1 << 22) /* Resource will be protected/encrypted */
+#define PIPE_BIND_SAMPLER_REDUCTION_MINMAX (1 << 23) /* PIPE_CAP_SAMPLER_REDUCTION_MINMAX */
 
 
 /**