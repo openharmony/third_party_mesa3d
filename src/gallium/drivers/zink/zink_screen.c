openharmony/third_party_mesa3d

commit 2938c709591ea6d5ca8beebee290f9954812775d

zink: support format queries for VK_EXT_sampler_filter_minmax
Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10248>

src/gallium/drivers/zink/zink_screen.c
@@ -876,6 +876,9 @@ zink_is_format_supported(struct pipe_screen *pscreen,
          !(props.optimalTilingFeatures & VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT))
             return false;
 
+      if (bind & PIPE_BIND_SAMPLER_REDUCTION_MINMAX)
+         return props.optimalTilingFeatures & VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_MINMAX_BIT;
+
       if ((bind & PIPE_BIND_SAMPLER_VIEW) || (bind & PIPE_BIND_RENDER_TARGET)) {
          /* if this is a 3-component texture, force gallium to give us 4 components by rejecting this one */
          const struct util_format_description *desc = util_format_description(format);

commit f9308128d47e67412172a2166b1ca470885a94e2

zink: Fix format query for minmax reduction support
All the rest of the filters here are written as "if asking for this
feature and it's not there, return false", with an implicit fallthrough
to the next test. There's no good reason to treat minmax differently.

Fixes: 2938c709591 zink: support format queries for VK_EXT_sampler_filter_minmax
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10397>

src/gallium/drivers/zink/zink_screen.c
@@ -879,8 +879,9 @@ zink_is_format_supported(struct pipe_screen *pscreen,
          !(props.optimalTilingFeatures & VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT))
             return false;
 
-      if (bind & PIPE_BIND_SAMPLER_REDUCTION_MINMAX)
-         return props.optimalTilingFeatures & VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_MINMAX_BIT;
+      if (bind & PIPE_BIND_SAMPLER_REDUCTION_MINMAX &&
+          !(props.optimalTilingFeatures & VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_MINMAX_BIT))
+         return false;
 
       if ((bind & PIPE_BIND_SAMPLER_VIEW) || (bind & PIPE_BIND_RENDER_TARGET)) {
          /* if this is a 3-component texture, force gallium to give us 4 components by rejecting this one */