openharmony/third_party_mesa3d

commit 54b2a8c78bda35a46167f0784db53c9aa5a1f61a

zink: reset queries when suspending if >50% of total pool is used
this is a great time for resets since there's never a renderpass active,
so an early reset here may mean avoiding a renderpass split later

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10049>

src/gallium/drivers/zink/zink_query.c
@@ -780,6 +780,8 @@ zink_suspend_queries(struct zink_context *ctx, struct zink_batch *batch)
       }
       if (query->needs_update)
          update_qbo(ctx, query);
+      if (query->curr_query > NUM_QUERIES / 2)
+         reset_pool(ctx, batch, query);
    }
 }
 

commit f586c254e3f234458486587660c5ec36db9162c6

zink: only reset query on suspend if the query has previously been stopped
if the query has never been stopped, then doing an implicit reset here does
nothing except give the gpu more work

Fixes: 54b2a8c78bd ("zink: reset queries when suspending if >50% of total pool is used")

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10268>

src/gallium/drivers/zink/zink_query.c
@@ -780,7 +780,7 @@ zink_suspend_queries(struct zink_context *ctx, struct zink_batch *batch)
       }
       if (query->needs_update)
          update_qbo(ctx, query);
-      if (query->curr_query > NUM_QUERIES / 2)
+      if (query->last_start && query->curr_query > NUM_QUERIES / 2)
          reset_pool(ctx, batch, query);
    }
 }