openharmony/third_party_mesa3d

commit d036df46685bb058448c5646f9685bec988047f1

zink: don't use cached mem for staging resources
Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9983>

src/gallium/drivers/zink/zink_resource.c
@@ -437,7 +437,8 @@ resource_object_create(struct zink_screen *screen, const struct pipe_resource *t
 
    if (templ->flags & PIPE_RESOURCE_FLAG_MAP_COHERENT)
       flags |= VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
-   else if (!(flags & VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT))
+   else if (!(flags & VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT) &&
+            templ->usage != PIPE_USAGE_STAGING)
       flags |= VK_MEMORY_PROPERTY_HOST_CACHED_BIT;
 
    VkMemoryAllocateInfo mai = {};

commit 8cf71fea37445ed33ac2b79a457bd139be583925

zink: use cached memory for staging resources
I think at one point before staging resource flagging was less reliable
this method made sense, but now it's worse

Fixes: d036df46685 ("zink: don't use cached mem for staging resources")

Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10363>

src/gallium/drivers/zink/zink_resource.c
@@ -454,7 +454,7 @@ resource_object_create(struct zink_screen *screen, const struct pipe_resource *t
    if (templ->flags & PIPE_RESOURCE_FLAG_MAP_COHERENT || templ->usage == PIPE_USAGE_DYNAMIC)
       flags |= VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
    else if (!(flags & VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT) &&
-            templ->usage != PIPE_USAGE_STAGING)
+            templ->usage == PIPE_USAGE_STAGING)
       flags |= VK_MEMORY_PROPERTY_HOST_CACHED_BIT;
 
    VkMemoryAllocateInfo mai = {};