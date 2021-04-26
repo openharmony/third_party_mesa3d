openharmony/third_party_mesa3d

commit 2f6fd883a28ebc985f1839d6055ec25ef0b33383

src/mesa: add GL_NV_half_float extension support (v2)
This patch adds support for GL_NV_half_float
extension.

v2: fix main_test failure

Signed-off-by: Yogesh Mohan Marimuthu <yogesh.mohanmarimuthu@amd.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6219>

src/mapi/glapi/gen/NV_half_float.xml
@@ -1,0 +1,173 @@
+<?xml version="1.0"?>
+<!DOCTYPE OpenGLAPI SYSTEM "gl_API.dtd">
+
+<OpenGLAPI>
+
+<category name="GL_NV_half_float" number="283">
+
+    <function name="Vertex2hNV" exec="dynamic" deprecated="3.1">
+        <param name="x" type="GLhalfNV"/>
+        <param name="y" type="GLhalfNV"/>
+    </function>
+
+    <function name="Vertex2hvNV" exec="dynamic" deprecated="3.1">
+        <param name="v" type="const GLhalfNV *"/>
+    </function>
+
+    <function name="Vertex3hNV" exec="dynamic" deprecated="3.1">
+        <param name="x" type="GLhalfNV"/>
+        <param name="y" type="GLhalfNV"/>
+        <param name="z" type="GLhalfNV"/>
+    </function>
+
+    <function name="Vertex3hvNV" exec="dynamic" deprecated="3.1">
+        <param name="v" type="const GLhalfNV *"/>
+    </function>
+
+    <function name="Vertex4hNV" exec="dynamic" deprecated="3.1">
+        <param name="x" type="GLhalfNV"/>
+        <param name="y" type="GLhalfNV"/>
+        <param name="z" type="GLhalfNV"/>
+        <param name="w" type="GLhalfNV"/>
+    </function>
+
+    <function name="Vertex4hvNV" exec="dynamic" deprecated="3.1">
+        <param name="v" type="const GLhalfNV *"/>
+    </function>
+
+    <function name="Normal3hNV" exec="dynamic" deprecated="3.1">
+        <param name="nx" type="GLhalfNV"/>
+        <param name="ny" type="GLhalfNV"/>
+        <param name="nz" type="GLhalfNV"/>
+    </function>
+
+    <function name="Normal3hvNV" exec="dynamic" deprecated="3.1">
+        <param name="v" type="const GLhalfNV *" count="3"/>
+    </function>
+
+    <function name="Color3hNV" exec="dynamic" deprecated="3.1">
+        <param name="red" type="GLhalfNV"/>
+        <param name="green" type="GLhalfNV"/>
+        <param name="blue" type="GLhalfNV"/>
+    </function>
+
+    <function name="Color3hvNV" exec="dynamic" deprecated="3.1">
+        <param name="v" type="const GLhalfNV *" count="3"/>
+    </function>
+
+    <function name="Color4hNV" exec="dynamic" deprecated="3.1">
+        <param name="red" type="GLhalfNV"/>
+        <param name="green" type="GLhalfNV"/>
+        <param name="blue" type="GLhalfNV"/>
+        <param name="alpha" type="GLhalfNV"/>
+    </function>
+
+    <function name="Color4hvNV" exec="dynamic" deprecated="3.1">
+        <param name="v" type="const GLhalfNV *" count="4"/>
+    </function>
+
+    <function name="TexCoord1hNV" exec="dynamic" deprecated="3.1">
+        <param name="s" type="GLhalfNV"/>
+    </function>
+
+    <function name="TexCoord1hvNV" exec="dynamic" deprecated="3.1">
+        <param name="v" type="const GLhalfNV *"/>
+    </function>
+
+    <function name="TexCoord2hNV" exec="dynamic" deprecated="3.1">
+        <param name="s" type="GLhalfNV"/>
+        <param name="t" type="GLhalfNV"/>
+    </function>
+
+    <function name="TexCoord2hvNV" exec="dynamic" deprecated="3.1">
+        <param name="v" type="const GLhalfNV *"/>
+    </function>
+
+    <function name="TexCoord3hNV" exec="dynamic" deprecated="3.1">
+        <param name="s" type="GLhalfNV"/>
+        <param name="t" type="GLhalfNV"/>
+        <param name="r" type="GLhalfNV"/>
+    </function>
+
+    <function name="TexCoord3hvNV" exec="dynamic" deprecated="3.1">
+        <param name="v" type="const GLhalfNV *"/>
+    </function>
+
+    <function name="TexCoord4hNV" exec="dynamic" deprecated="3.1">
+        <param name="s" type="GLhalfNV"/>
+        <param name="t" type="GLhalfNV"/>
+        <param name="r" type="GLhalfNV"/>
+        <param name="q" type="GLhalfNV"/>
+    </function>
+
+    <function name="TexCoord4hvNV" exec="dynamic" deprecated="3.1">
+        <param name="v" type="const GLhalfNV *"/>
+    </function>
+
+    <function name="MultiTexCoord1hNV" exec="dynamic" deprecated="3.1">
+        <param name="target" type="GLenum"/>
+        <param name="s" type="GLhalfNV"/>
+    </function>
+
+    <function name="MultiTexCoord1hvNV" exec="dynamic" deprecated="3.1">
+        <param name="target" type="GLenum"/>
+        <param name="v" type="const GLhalfNV *"/>
+    </function>
+
+    <function name="MultiTexCoord2hNV" exec="dynamic" deprecated="3.1">
+        <param name="target" type="GLenum"/>
+        <param name="s" type="GLhalfNV"/>
+        <param name="t" type="GLhalfNV"/>
+    </function>
+
+    <function name="MultiTexCoord2hvNV" exec="dynamic" deprecated="3.1">
+        <param name="target" type="GLenum"/>
+        <param name="v" type="const GLhalfNV *"/>
+    </function>
+
+    <function name="MultiTexCoord3hNV" exec="dynamic" deprecated="3.1">
+        <param name="target" type="GLenum"/>
+        <param name="s" type="GLhalfNV"/>
+        <param name="t" type="GLhalfNV"/>
+        <param name="r" type="GLhalfNV"/>
+    </function>
+
+    <function name="MultiTexCoord3hvNV" exec="dynamic" deprecated="3.1">
+        <param name="target" type="GLenum"/>
+        <param name="v" type="const GLhalfNV *"/>
+    </function>
+
+    <function name="MultiTexCoord4hNV" exec="dynamic" deprecated="3.1">
+        <param name="target" type="GLenum"/>
+        <param name="s" type="GLhalfNV"/>
+        <param name="t" type="GLhalfNV"/>
+        <param name="r" type="GLhalfNV"/>
+        <param name="q" type="GLhalfNV"/>
+    </function>
+
+    <function name="MultiTexCoord4hvNV" exec="dynamic" deprecated="3.1">
+        <param name="target" type="GLenum"/>
+        <param name="v" type="const GLhalfNV *"/>
+    </function>
+
+    <function name="FogCoordhNV" exec="dynamic" deprecated="3.1">
+        <param name="x" type="GLhalfNV"/>
+    </function>
+
+    <function name="FogCoordhvNV" exec="dynamic" deprecated="3.1">
+        <param name="v" type="const GLhalfNV *"/>
+    </function>
+
+    <function name="SecondaryColor3hNV" exec="dynamic" deprecated="3.1">
+        <param name="red" type="GLhalfNV"/>
+        <param name="green" type="GLhalfNV"/>
+        <param name="blue" type="GLhalfNV"/>
+    </function>
+
+    <function name="SecondaryColor3hvNV" exec="dynamic" deprecated="3.1">
+        <param name="v" type="const GLhalfNV *"/>
+    </function>
+
+</category>
+
+</OpenGLAPI>

src/mapi/glapi/gen/static_data.py
@@ -1644,6 +1644,38 @@ offsets = {
     "ViewportSwizzleNV": 1608,
     "AlphaToCoverageDitherControlNV": 1609,
     "InternalBufferSubDataCopyMESA": 1610,
+    "Vertex2hNV": 1611,
+    "Vertex2hvNV": 1612,
+    "Vertex3hNV": 1613,
+    "Vertex3hvNV": 1614,
+    "Vertex4hNV": 1615,
+    "Vertex4hvNV": 1616,
+    "Normal3hNV": 1617,
+    "Normal3hvNV": 1618,
+    "Color3hNV": 1619,
+    "Color3hvNV": 1620,
+    "Color4hNV": 1621,
+    "Color4hvNV": 1622,
+    "TexCoord1hNV": 1623,
+    "TexCoord1hvNV": 1624,
+    "TexCoord2hNV": 1625,
+    "TexCoord2hvNV": 1626,
+    "TexCoord3hNV": 1627,
+    "TexCoord3hvNV": 1628,
+    "TexCoord4hNV": 1629,
+    "TexCoord4hvNV": 1630,
+    "MultiTexCoord1hNV": 1631,
+    "MultiTexCoord1hvNV": 1632,
+    "MultiTexCoord2hNV": 1633,
+    "MultiTexCoord2hvNV": 1634,
+    "MultiTexCoord3hNV": 1635,
+    "MultiTexCoord3hvNV": 1636,
+    "MultiTexCoord4hNV": 1637,
+    "MultiTexCoord4hvNV": 1638,
+    "FogCoordhNV": 1639,
+    "FogCoordhvNV": 1640,
+    "SecondaryColor3hNV": 1641,
+    "SecondaryColor3hvNV": 1642,
 }
 
 functions = [

src/mesa/main/dd.h
@@ -1503,6 +1503,41 @@ typedef struct {
 
    void (GLAPIENTRYP VertexAttribL1ui64ARB)( GLuint index, GLuint64EXT x);
    void (GLAPIENTRYP VertexAttribL1ui64vARB)( GLuint index, const GLuint64EXT *v);
+
+   /* GL_NV_half_float */
+   void (GLAPIENTRYP Vertex2hNV)( GLhalfNV, GLhalfNV );
+   void (GLAPIENTRYP Vertex2hvNV)( const GLhalfNV * );
+   void (GLAPIENTRYP Vertex3hNV)( GLhalfNV, GLhalfNV, GLhalfNV );
+   void (GLAPIENTRYP Vertex3hvNV)( const GLhalfNV * );
+   void (GLAPIENTRYP Vertex4hNV)( GLhalfNV, GLhalfNV, GLhalfNV, GLhalfNV );
+   void (GLAPIENTRYP Vertex4hvNV)( const GLhalfNV * );
+   void (GLAPIENTRYP Normal3hNV)( GLhalfNV, GLhalfNV, GLhalfNV );
+   void (GLAPIENTRYP Normal3hvNV)( const GLhalfNV * );
+   void (GLAPIENTRYP Color3hNV)( GLhalfNV, GLhalfNV, GLhalfNV );
+   void (GLAPIENTRYP Color3hvNV)( const GLhalfNV * );
+   void (GLAPIENTRYP Color4hNV)( GLhalfNV, GLhalfNV, GLhalfNV, GLhalfNV );
+   void (GLAPIENTRYP Color4hvNV)( const GLhalfNV * );
+   void (GLAPIENTRYP TexCoord1hNV)( GLhalfNV );
+   void (GLAPIENTRYP TexCoord1hvNV)( const GLhalfNV * );
+   void (GLAPIENTRYP TexCoord2hNV)( GLhalfNV, GLhalfNV );
+   void (GLAPIENTRYP TexCoord2hvNV)( const GLhalfNV * );
+   void (GLAPIENTRYP TexCoord3hNV)( GLhalfNV, GLhalfNV, GLhalfNV );
+   void (GLAPIENTRYP TexCoord3hvNV)( const GLhalfNV * );
+   void (GLAPIENTRYP TexCoord4hNV)( GLhalfNV, GLhalfNV, GLhalfNV, GLhalfNV );
+   void (GLAPIENTRYP TexCoord4hvNV)( const GLhalfNV * );
+   void (GLAPIENTRYP MultiTexCoord1hNV)( GLenum, GLhalfNV );
+   void (GLAPIENTRYP MultiTexCoord1hvNV)( GLenum, const GLhalfNV * );
+   void (GLAPIENTRYP MultiTexCoord2hNV)( GLenum, GLhalfNV, GLhalfNV );
+   void (GLAPIENTRYP MultiTexCoord2hvNV)( GLenum, const GLhalfNV * );
+   void (GLAPIENTRYP MultiTexCoord3hNV)( GLenum, GLhalfNV, GLhalfNV, GLhalfNV );
+   void (GLAPIENTRYP MultiTexCoord3hvNV)( GLenum, const GLhalfNV * );
+   void (GLAPIENTRYP MultiTexCoord4hNV)( GLenum, GLhalfNV, GLhalfNV, GLhalfNV, GLhalfNV );
+   void (GLAPIENTRYP MultiTexCoord4hvNV)( GLenum, const GLhalfNV * );
+   void (GLAPIENTRYP FogCoordhNV)( GLhalfNV );
+   void (GLAPIENTRYP FogCoordhvNV)( const GLhalfNV * );
+   void (GLAPIENTRYP SecondaryColor3hNV)( GLhalfNV, GLhalfNV, GLhalfNV );
+   void (GLAPIENTRYP SecondaryColor3hvNV)( const GLhalfNV * );
+
 } GLvertexformat;
 
 

src/mesa/main/vtxfmt.c
@@ -193,6 +193,40 @@ install_vtxfmt(struct gl_context *ctx, struct _glapi_table *tab,
 
       SET_SecondaryColorP3ui(tab, vfmt->SecondaryColorP3ui);
       SET_SecondaryColorP3uiv(tab, vfmt->SecondaryColorP3uiv);
+
+      /* GL_NV_half_float */
+      SET_Vertex2hNV(tab, vfmt->Vertex2hNV);
+      SET_Vertex2hvNV(tab, vfmt->Vertex2hvNV);
+      SET_Vertex3hNV(tab, vfmt->Vertex3hNV);
+      SET_Vertex3hvNV(tab, vfmt->Vertex3hvNV);
+      SET_Vertex4hNV(tab, vfmt->Vertex4hNV);
+      SET_Vertex4hvNV(tab, vfmt->Vertex4hvNV);
+      SET_Normal3hNV(tab, vfmt->Normal3hNV);
+      SET_Normal3hvNV(tab, vfmt->Normal3hvNV);
+      SET_Color3hNV(tab, vfmt->Color3hNV);
+      SET_Color3hvNV(tab, vfmt->Color4hvNV);
+      SET_Color4hNV(tab, vfmt->Color4hNV);
+      SET_Color4hvNV(tab, vfmt->Color3hvNV);
+      SET_TexCoord1hNV(tab, vfmt->TexCoord1hNV);
+      SET_TexCoord1hvNV(tab, vfmt->TexCoord1hvNV);
+      SET_TexCoord2hNV(tab, vfmt->TexCoord2hNV);
+      SET_TexCoord2hvNV(tab, vfmt->TexCoord2hvNV);
+      SET_TexCoord3hNV(tab, vfmt->TexCoord3hNV);
+      SET_TexCoord3hvNV(tab, vfmt->TexCoord3hvNV);
+      SET_TexCoord4hNV(tab, vfmt->TexCoord4hNV);
+      SET_TexCoord4hvNV(tab, vfmt->TexCoord4hvNV);
+      SET_MultiTexCoord1hNV(tab, vfmt->MultiTexCoord1hNV);
+      SET_MultiTexCoord1hvNV(tab, vfmt->MultiTexCoord1hvNV);
+      SET_MultiTexCoord2hNV(tab, vfmt->MultiTexCoord2hNV);
+      SET_MultiTexCoord2hvNV(tab, vfmt->MultiTexCoord2hvNV);
+      SET_MultiTexCoord3hNV(tab, vfmt->MultiTexCoord3hNV);
+      SET_MultiTexCoord3hvNV(tab, vfmt->MultiTexCoord3hvNV);
+      SET_MultiTexCoord4hNV(tab, vfmt->MultiTexCoord4hNV);
+      SET_MultiTexCoord4hvNV(tab, vfmt->MultiTexCoord4hvNV);
+      SET_FogCoordhNV(tab, vfmt->FogCoordhNV);
+      SET_FogCoordhvNV(tab, vfmt->FogCoordhvNV);
+      SET_SecondaryColor3hNV(tab, vfmt->SecondaryColor3hNV);
+      SET_SecondaryColor3hvNV(tab, vfmt->SecondaryColor3hvNV);
    }
 
    if (_mesa_is_desktop_gl(ctx)) {

commit cf54c1cd4bfb9c7f1b2b58c4b7deaefd24b2c049

mesa: fix incomplete GL_NV_half_float implementation
All of the VertexAttrib* functions were missing.

Fixes: 2f6fd883a28e ("src/mesa: add GL_NV_half_float extension support (v2)")

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10431>

src/mapi/glapi/gen/NV_half_float.xml
@@ -150,6 +150,76 @@
         <param name="v" type="const GLhalfNV *" count="4"/>
     </function>
 
+    <function name="VertexAttrib1hNV" exec="dynamic" deprecated="3.1">
+        <param name="index" type="GLuint"/>
+        <param name="x" type="GLhalfNV"/>
+    </function>
+
+    <function name="VertexAttrib1hvNV" exec="dynamic" deprecated="3.1">
+        <param name="index" type="GLuint"/>
+        <param name="v" type="const GLhalfNV *" count="1"/>
+    </function>
+
+    <function name="VertexAttrib2hNV" exec="dynamic" deprecated="3.1">
+        <param name="index" type="GLuint"/>
+        <param name="x" type="GLhalfNV"/>
+        <param name="y" type="GLhalfNV"/>
+    </function>
+
+    <function name="VertexAttrib2hvNV" exec="dynamic" deprecated="3.1">
+        <param name="index" type="GLuint"/>
+        <param name="v" type="const GLhalfNV *" count="2"/>
+    </function>
+
+    <function name="VertexAttrib3hNV" exec="dynamic" deprecated="3.1">
+        <param name="index" type="GLuint"/>
+        <param name="x" type="GLhalfNV"/>
+        <param name="y" type="GLhalfNV"/>
+        <param name="z" type="GLhalfNV"/>
+    </function>
+
+    <function name="VertexAttrib3hvNV" exec="dynamic" deprecated="3.1">
+        <param name="index" type="GLuint"/>
+        <param name="v" type="const GLhalfNV *" count="3"/>
+    </function>
+
+    <function name="VertexAttrib4hNV" exec="dynamic" deprecated="3.1">
+        <param name="index" type="GLuint"/>
+        <param name="x" type="GLhalfNV"/>
+        <param name="y" type="GLhalfNV"/>
+        <param name="z" type="GLhalfNV"/>
+        <param name="w" type="GLhalfNV"/>
+    </function>
+
+    <function name="VertexAttrib4hvNV" exec="dynamic" deprecated="3.1">
+        <param name="index" type="GLuint"/>
+        <param name="v" type="const GLhalfNV *" count="4"/>
+    </function>
+
+    <function name="VertexAttribs1hvNV" exec="dynamic" deprecated="3.1">
+        <param name="index" type="GLuint"/>
+        <param name="n" type="GLsizei"/>
+        <param name="v" type="const GLhalfNV *" count="n"/>
+    </function>
+
+    <function name="VertexAttribs2hvNV" exec="dynamic" deprecated="3.1">
+        <param name="index" type="GLuint"/>
+        <param name="n" type="GLsizei"/>
+        <param name="v" type="const GLhalfNV *" count="n" count_scale="2"/>
+    </function>
+
+    <function name="VertexAttribs3hvNV" exec="dynamic" deprecated="3.1">
+        <param name="index" type="GLuint"/>
+        <param name="n" type="GLsizei"/>
+        <param name="v" type="const GLhalfNV *" count="n" count_scale="3"/>
+    </function>
+
+    <function name="VertexAttribs4hvNV" exec="dynamic" deprecated="3.1">
+        <param name="index" type="GLuint"/>
+        <param name="n" type="GLsizei"/>
+        <param name="v" type="const GLhalfNV *" count="n" count_scale="4"/>
+    </function>
+
     <function name="FogCoordhNV" exec="dynamic" deprecated="3.1">
         <param name="x" type="GLhalfNV"/>
     </function>

src/mapi/glapi/gen/static_data.py
@@ -1677,6 +1677,18 @@ offsets = {
     "SecondaryColor3hNV": 1641,
     "SecondaryColor3hvNV": 1642,
     "InternalSetError": 1643,
+    "VertexAttrib1hNV": 1644,
+    "VertexAttrib1hvNV": 1645,
+    "VertexAttrib2hNV": 1646,
+    "VertexAttrib2hvNV": 1647,
+    "VertexAttrib3hNV": 1648,
+    "VertexAttrib3hvNV": 1649,
+    "VertexAttrib4hNV": 1650,
+    "VertexAttrib4hvNV": 1651,
+    "VertexAttribs1hvNV": 1652,
+    "VertexAttribs2hvNV": 1653,
+    "VertexAttribs3hvNV": 1654,
+    "VertexAttribs4hvNV": 1655,
 }
 
 functions = [

src/mesa/main/dd.h
@@ -1597,6 +1597,18 @@ typedef struct {
    void (GLAPIENTRYP MultiTexCoord3hvNV)( GLenum, const GLhalfNV * );
    void (GLAPIENTRYP MultiTexCoord4hNV)( GLenum, GLhalfNV, GLhalfNV, GLhalfNV, GLhalfNV );
    void (GLAPIENTRYP MultiTexCoord4hvNV)( GLenum, const GLhalfNV * );
+   void (GLAPIENTRYP VertexAttrib1hNV)( GLuint index, GLhalfNV x );
+   void (GLAPIENTRYP VertexAttrib1hvNV)( GLuint index, const GLhalfNV *v );
+   void (GLAPIENTRYP VertexAttrib2hNV)( GLuint index, GLhalfNV x, GLhalfNV y );
+   void (GLAPIENTRYP VertexAttrib2hvNV)( GLuint index, const GLhalfNV *v );
+   void (GLAPIENTRYP VertexAttrib3hNV)( GLuint index, GLhalfNV x, GLhalfNV y, GLhalfNV z );
+   void (GLAPIENTRYP VertexAttrib3hvNV)( GLuint index, const GLhalfNV *v );
+   void (GLAPIENTRYP VertexAttrib4hNV)( GLuint index, GLhalfNV x, GLhalfNV y, GLhalfNV z, GLhalfNV w );
+   void (GLAPIENTRYP VertexAttrib4hvNV)( GLuint index, const GLhalfNV *v );
+   void (GLAPIENTRYP VertexAttribs1hvNV)(GLuint index, GLsizei n, const GLhalfNV *v);
+   void (GLAPIENTRYP VertexAttribs2hvNV)(GLuint index, GLsizei n, const GLhalfNV *v);
+   void (GLAPIENTRYP VertexAttribs3hvNV)(GLuint index, GLsizei n, const GLhalfNV *v);
+   void (GLAPIENTRYP VertexAttribs4hvNV)(GLuint index, GLsizei n, const GLhalfNV *v);
    void (GLAPIENTRYP FogCoordhNV)( GLhalfNV );
    void (GLAPIENTRYP FogCoordhvNV)( const GLhalfNV * );
    void (GLAPIENTRYP SecondaryColor3hNV)( GLhalfNV, GLhalfNV, GLhalfNV );

src/mesa/main/vtxfmt.c
@@ -223,6 +223,18 @@ install_vtxfmt(struct gl_context *ctx, struct _glapi_table *tab,
       SET_MultiTexCoord3hvNV(tab, vfmt->MultiTexCoord3hvNV);
       SET_MultiTexCoord4hNV(tab, vfmt->MultiTexCoord4hNV);
       SET_MultiTexCoord4hvNV(tab, vfmt->MultiTexCoord4hvNV);
+      SET_VertexAttrib1hNV(tab, vfmt->VertexAttrib1hNV);
+      SET_VertexAttrib2hNV(tab, vfmt->VertexAttrib2hNV);
+      SET_VertexAttrib3hNV(tab, vfmt->VertexAttrib3hNV);
+      SET_VertexAttrib4hNV(tab, vfmt->VertexAttrib4hNV);
+      SET_VertexAttrib1hvNV(tab, vfmt->VertexAttrib1hvNV);
+      SET_VertexAttrib2hvNV(tab, vfmt->VertexAttrib2hvNV);
+      SET_VertexAttrib3hvNV(tab, vfmt->VertexAttrib3hvNV);
+      SET_VertexAttrib4hvNV(tab, vfmt->VertexAttrib4hvNV);
+      SET_VertexAttribs1hvNV(tab, vfmt->VertexAttribs1hvNV);
+      SET_VertexAttribs2hvNV(tab, vfmt->VertexAttribs2hvNV);
+      SET_VertexAttribs3hvNV(tab, vfmt->VertexAttribs3hvNV);
+      SET_VertexAttribs4hvNV(tab, vfmt->VertexAttribs4hvNV);
       SET_FogCoordhNV(tab, vfmt->FogCoordhNV);
       SET_FogCoordhvNV(tab, vfmt->FogCoordhvNV);
       SET_SecondaryColor3hNV(tab, vfmt->SecondaryColor3hNV);

src/mesa/vbo/vbo_attrib_tmp.h
@@ -1477,6 +1477,141 @@ TAG(MultiTexCoord4hvNV)(GLenum target, const GLhalfNV * v)
 
 
 static void GLAPIENTRY
+TAG(VertexAttrib1hNV)(GLuint index, GLhalfNV x)
+{
+   GET_CURRENT_CONTEXT(ctx);
+   if (is_vertex_position(ctx, index))
+      ATTR1H(0, x);
+   else if (index < MAX_VERTEX_GENERIC_ATTRIBS)
+      ATTR1H(VBO_ATTRIB_GENERIC0 + index, x);
+   else
+      ERROR(GL_INVALID_VALUE);
+}
+
+static void GLAPIENTRY
+TAG(VertexAttrib2hNV)(GLuint index, GLhalfNV x, GLhalfNV y)
+{
+   GET_CURRENT_CONTEXT(ctx);
+   if (is_vertex_position(ctx, index))
+      ATTR2H(0, x, y);
+   else if (index < MAX_VERTEX_GENERIC_ATTRIBS)
+      ATTR2H(VBO_ATTRIB_GENERIC0 + index, x, y);
+   else
+      ERROR(GL_INVALID_VALUE);
+}
+
+static void GLAPIENTRY
+TAG(VertexAttrib3hNV)(GLuint index, GLhalfNV x, GLhalfNV y, GLhalfNV z)
+{
+   GET_CURRENT_CONTEXT(ctx);
+   if (is_vertex_position(ctx, index))
+      ATTR3H(0, x, y, z);
+   else if (index < MAX_VERTEX_GENERIC_ATTRIBS)
+      ATTR3H(VBO_ATTRIB_GENERIC0 + index, x, y, z);
+   else
+      ERROR(GL_INVALID_VALUE);
+}
+
+static void GLAPIENTRY
+TAG(VertexAttrib4hNV)(GLuint index, GLhalfNV x, GLhalfNV y, GLhalfNV z, GLhalfNV w)
+{
+   GET_CURRENT_CONTEXT(ctx);
+   if (is_vertex_position(ctx, index))
+      ATTR4H(0, x, y, z, w);
+   else if (index < MAX_VERTEX_GENERIC_ATTRIBS)
+      ATTR4H(VBO_ATTRIB_GENERIC0 + index, x, y, z, w);
+   else
+      ERROR(GL_INVALID_VALUE);
+}
+
+static void GLAPIENTRY
+TAG(VertexAttrib1hvNV)(GLuint index, const GLhalfNV * v)
+{
+   GET_CURRENT_CONTEXT(ctx);
+   if (is_vertex_position(ctx, index))
+      ATTR1HV(0, v);
+   else if (index < MAX_VERTEX_GENERIC_ATTRIBS)
+      ATTR1HV(VBO_ATTRIB_GENERIC0 + index, v);
+   else
+      ERROR(GL_INVALID_VALUE);
+}
+
+static void GLAPIENTRY
+TAG(VertexAttrib2hvNV)(GLuint index, const GLhalfNV * v)
+{
+   GET_CURRENT_CONTEXT(ctx);
+   if (is_vertex_position(ctx, index))
+      ATTR2HV(0, v);
+   else if (index < MAX_VERTEX_GENERIC_ATTRIBS)
+      ATTR2HV(VBO_ATTRIB_GENERIC0 + index, v);
+   else
+      ERROR(GL_INVALID_VALUE);
+}
+
+static void GLAPIENTRY
+TAG(VertexAttrib3hvNV)(GLuint index, const GLhalfNV * v)
+{
+   GET_CURRENT_CONTEXT(ctx);
+   if (is_vertex_position(ctx, index))
+      ATTR3HV(0, v);
+   else if (index < MAX_VERTEX_GENERIC_ATTRIBS)
+      ATTR3HV(VBO_ATTRIB_GENERIC0 + index, v);
+   else
+      ERROR(GL_INVALID_VALUE);
+}
+
+static void GLAPIENTRY
+TAG(VertexAttrib4hvNV)(GLuint index, const GLhalfNV * v)
+{
+   GET_CURRENT_CONTEXT(ctx);
+   if (is_vertex_position(ctx, index))
+      ATTR4HV(0, v);
+   else if (index < MAX_VERTEX_GENERIC_ATTRIBS)
+      ATTR4HV(VBO_ATTRIB_GENERIC0 + index, v);
+   else
+      ERROR(GL_INVALID_VALUE);
+}
+
+static void GLAPIENTRY
+TAG(VertexAttribs1hvNV)(GLuint index, GLsizei n, const GLhalfNV *v)
+{
+   GET_CURRENT_CONTEXT(ctx);
+   n = MIN2(n, VBO_ATTRIB_MAX - index);
+   for (GLint i = n - 1; i >= 0; i--)
+      ATTR1H(index + i, v[i]);
+}
+
+static void GLAPIENTRY
+TAG(VertexAttribs2hvNV)(GLuint index, GLsizei n, const GLhalfNV *v)
+{
+   GET_CURRENT_CONTEXT(ctx);
+   n = MIN2(n, VBO_ATTRIB_MAX - index);
+   for (GLint i = n - 1; i >= 0; i--)
+      ATTR2H(index + i, v[2 * i], v[2 * i + 1]);
+}
+
+static void GLAPIENTRY
+TAG(VertexAttribs3hvNV)(GLuint index, GLsizei n, const GLhalfNV *v)
+{
+   GET_CURRENT_CONTEXT(ctx);
+   n = MIN2(n, VBO_ATTRIB_MAX - index);
+   for (GLint i = n - 1; i >= 0; i--)
+      ATTR3H(index + i, v[3 * i], v[3 * i + 1], v[3 * i + 2]);
+}
+
+
+static void GLAPIENTRY
+TAG(VertexAttribs4hvNV)(GLuint index, GLsizei n, const GLhalfNV *v)
+{
+   GET_CURRENT_CONTEXT(ctx);
+   n = MIN2(n, VBO_ATTRIB_MAX - index);
+   for (GLint i = n - 1; i >= 0; i--)
+      ATTR4H(index + i, v[4 * i], v[4 * i + 1], v[4 * i + 2], v[4 * i + 3]);
+}
+
+
+
+static void GLAPIENTRY
 TAG(FogCoordhNV)(GLhalf x)
 {
    GET_CURRENT_CONTEXT(ctx);

src/mesa/vbo/vbo_init_tmp.h
@@ -123,6 +123,18 @@ vfmt->MultiTexCoord3hNV = NAME(MultiTexCoord3hNV);
 vfmt->MultiTexCoord3hvNV = NAME(MultiTexCoord3hvNV);
 vfmt->MultiTexCoord4hNV = NAME(MultiTexCoord4hNV);
 vfmt->MultiTexCoord4hvNV = NAME(MultiTexCoord4hvNV);
+vfmt->VertexAttrib1hNV = NAME(VertexAttrib1hNV);
+vfmt->VertexAttrib2hNV = NAME(VertexAttrib2hNV);
+vfmt->VertexAttrib3hNV = NAME(VertexAttrib3hNV);
+vfmt->VertexAttrib4hNV = NAME(VertexAttrib4hNV);
+vfmt->VertexAttrib1hvNV = NAME(VertexAttrib1hvNV);
+vfmt->VertexAttrib2hvNV = NAME(VertexAttrib2hvNV);
+vfmt->VertexAttrib3hvNV = NAME(VertexAttrib3hvNV);
+vfmt->VertexAttrib4hvNV = NAME(VertexAttrib4hvNV);
+vfmt->VertexAttribs1hvNV = NAME(VertexAttribs1hvNV);
+vfmt->VertexAttribs2hvNV = NAME(VertexAttribs2hvNV);
+vfmt->VertexAttribs3hvNV = NAME(VertexAttribs3hvNV);
+vfmt->VertexAttribs4hvNV = NAME(VertexAttribs4hvNV);
 vfmt->FogCoordhNV = NAME(FogCoordhNV);
 vfmt->FogCoordhvNV = NAME(FogCoordhvNV);
 vfmt->SecondaryColor3hNV = NAME(SecondaryColor3hNV);

src/mesa/main/tests/dispatch_sanity.cpp
@@ -1978,6 +1978,18 @@ const struct function gl_compatibility_functions_possible[] = {
    { "glFogCoordhvNV", 13, -1 },
    { "glSecondaryColor3hNV", 13, -1 },
    { "glSecondaryColor3hvNV", 13, -1 },
+   { "glVertexAttrib1hNV", 13, -1 },
+   { "glVertexAttrib1hvNV", 13, -1 },
+   { "glVertexAttrib2hNV", 13, -1 },
+   { "glVertexAttrib2hvNV", 13, -1 },
+   { "glVertexAttrib3hNV", 13, -1 },
+   { "glVertexAttrib3hvNV", 13, -1 },
+   { "glVertexAttrib4hNV", 13, -1 },
+   { "glVertexAttrib4hvNV", 13, -1 },
+   { "glVertexAttribs1hvNV", 13, -1 },
+   { "glVertexAttribs2hvNV", 13, -1 },
+   { "glVertexAttribs3hvNV", 13, -1 },
+   { "glVertexAttribs4hvNV", 13, -1 },
 
    { NULL, 0, -1 }
 };