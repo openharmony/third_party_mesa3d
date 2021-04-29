openharmony/third_party_mesa3d

commit c33e7283b18eb37e7f30b0f61e41bd6205b6a8b7

vulkan: Make vk_debug_report_callback derive from vk_object_base
Fixes: 0e7ce142a71b "anv,vulkan: Implement VK_EXT_private_data"
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8676>

src/vulkan/util/vk_debug_report.c
@@ -60,6 +60,9 @@ vk_create_debug_report_callback(struct vk_debug_report_instance *instance,
    if (!cb)
       return VK_ERROR_OUT_OF_HOST_MEMORY;
 
+   vk_object_base_init(NULL, &cb->base,
+                       VK_OBJECT_TYPE_DEBUG_REPORT_CALLBACK_EXT);
+
    cb->flags = pCreateInfo->flags;
    cb->callback = pCreateInfo->pfnCallback;
    cb->data = pCreateInfo->pUserData;
@@ -68,7 +71,7 @@ vk_create_debug_report_callback(struct vk_debug_report_instance *instance,
    list_addtail(&cb->link, &instance->callbacks);
    mtx_unlock(&instance->callbacks_mutex);
 
-   *pCallback = (VkDebugReportCallbackEXT)(uintptr_t)cb;
+   *pCallback = vk_debug_report_callback_to_handle(cb);
 
    return VK_SUCCESS;
 }
@@ -79,19 +82,19 @@ vk_destroy_debug_report_callback(struct vk_debug_report_instance *instance,
                                  const VkAllocationCallbacks* pAllocator,
                                  const VkAllocationCallbacks* instance_allocator)
 {
-   if (_callback == VK_NULL_HANDLE)
-      return;
+   VK_FROM_HANDLE(vk_debug_report_callback, callback, _callback);
 
-   struct vk_debug_report_callback *callback =
-            (struct vk_debug_report_callback *)(uintptr_t)_callback;
+   if (callback == NULL)
+      return;
 
    /* Remove from list and destroy given callback. */
    mtx_lock(&instance->callbacks_mutex);
    list_del(&callback->link);
    vk_free2(instance_allocator, pAllocator, callback);
    mtx_unlock(&instance->callbacks_mutex);
-}
 
+   vk_object_base_finish(&callback->base);
+}
 
 void
 vk_debug_report(struct vk_debug_report_instance *instance,

src/vulkan/util/vk_debug_report.h
@@ -26,17 +26,24 @@
 #ifndef VK_DEBUG_REPORT_H
 #define VK_DEBUG_REPORT_H
 
+#include "vk_object.h"
+
 #include "c11/threads.h"
 #include "util/list.h"
 #include <vulkan/vulkan.h>
 
 struct vk_debug_report_callback {
+   struct vk_object_base                        base;
+
    /* Link in the 'callbacks' list in anv_instance struct. */
    struct list_head                             link;
    VkDebugReportFlagsEXT                        flags;
    PFN_vkDebugReportCallbackEXT                 callback;
    void *                                       data;
 };
+
+VK_DEFINE_HANDLE_CASTS(vk_debug_report_callback, base, VkDebugReportCallbackEXT,
+                       VK_OBJECT_TYPE_DEBUG_REPORT_CALLBACK_EXT)
 
 struct vk_debug_report_instance {
    /* VK_EXT_debug_report debug callbacks */

commit 618f33fce0f22be94132cbc8ede01e0b004f6069

vulkan: fix use-after-free in vk_common_DestroyDebugReportCallbackEXT
Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Fixes: c33e7283b18 ("vulkan: Make vk_debug_report_callback derive from vk_object_base")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10433>

src/vulkan/util/vk_debug_report.c
@@ -88,10 +88,9 @@ vk_common_DestroyDebugReportCallbackEXT(VkInstance _instance,
    /* Remove from list and destroy given callback. */
    mtx_lock(&instance->debug_report.callbacks_mutex);
    list_del(&callback->link);
+   vk_object_base_finish(&callback->base);
    vk_free2(&instance->alloc, pAllocator, callback);
    mtx_unlock(&instance->debug_report.callbacks_mutex);
-
-   vk_object_base_finish(&callback->base);
 }
 
 static void