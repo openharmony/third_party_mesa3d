openharmony/third_party_mesa3d

commit f6a0e7d0c4a363d016d4fa057ef28d643f93fee9

venus: handle VK_IMAGE_LAYOUT_PRESENT_SRC_KHR transfer
Whenver VK_IMAGE_LAYOUT_PRESENT_SRC_KHR is used, replace it with
VK_IMAGE_LAYOUT_GENERAL as required for proper layout and ownership
transfer for external memory backed swapchain images.

This will be Android only until common WSI is fixed.

Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Reviewed-by: Chia-I Wu <olvaffe@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10335>

src/virtio/vulkan/vn_render_pass.c
@@ -17,6 +17,43 @@
 
 /* render pass commands */
 
+static const VkAttachmentDescription *
+vn_get_intercepted_attachments(const VkAttachmentDescription *attachments,
+                               uint32_t count,
+                               const VkAllocationCallbacks *alloc)
+{
+   /* XXX drop the #ifdef after fixing common wsi */
+#ifdef ANDROID
+   bool has_present_src = false;
+   for (uint32_t i = 0; i < count; i++) {
+      if (attachments[i].initialLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR ||
+          attachments[i].finalLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR) {
+         has_present_src = true;
+         break;
+      }
+   }
+   if (!has_present_src)
+      return attachments;
+
+   size_t size = sizeof(VkAttachmentDescription) * count;
+   VkAttachmentDescription *out_attachments = vk_alloc(
+      alloc, size, VN_DEFAULT_ALIGN, VK_SYSTEM_ALLOCATION_SCOPE_COMMAND);
+   if (!out_attachments)
+      return NULL;
+
+   memcpy(out_attachments, attachments, size);
+   for (uint32_t i = 0; i < count; i++) {
+      if (out_attachments[i].initialLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR)
+         out_attachments[i].initialLayout = VK_IMAGE_LAYOUT_GENERAL;
+      if (out_attachments[i].finalLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR)
+         out_attachments[i].finalLayout = VK_IMAGE_LAYOUT_GENERAL;
+   }
+   return out_attachments;
+#else
+   return attachments;
+#endif
+}
+
 VkResult
 vn_CreateRenderPass(VkDevice device,
                     const VkRenderPassCreateInfo *pCreateInfo,
@@ -35,15 +72,61 @@ vn_CreateRenderPass(VkDevice device,
 
    vn_object_base_init(&pass->base, VK_OBJECT_TYPE_RENDER_PASS, &dev->base);
 
-   /* XXX VK_IMAGE_LAYOUT_PRESENT_SRC_KHR */
+   VkRenderPassCreateInfo local_pass_info = *pCreateInfo;
+   local_pass_info.pAttachments = vn_get_intercepted_attachments(
+      pCreateInfo->pAttachments, pCreateInfo->attachmentCount, alloc);
+   if (!local_pass_info.pAttachments) {
+      vk_free(alloc, pass);
+      return vn_error(dev->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
+   }
 
    VkRenderPass pass_handle = vn_render_pass_to_handle(pass);
-   vn_async_vkCreateRenderPass(dev->instance, device, pCreateInfo, NULL,
+   vn_async_vkCreateRenderPass(dev->instance, device, &local_pass_info, NULL,
                                &pass_handle);
+
+   if (local_pass_info.pAttachments != pCreateInfo->pAttachments)
+      vk_free(alloc, (void *)local_pass_info.pAttachments);
 
    *pRenderPass = pass_handle;
 
    return VK_SUCCESS;
+}
+
+static const VkAttachmentDescription2 *
+vn_get_intercepted_attachments2(const VkAttachmentDescription2 *attachments,
+                                uint32_t count,
+                                const VkAllocationCallbacks *alloc)
+{
+   /* XXX drop the #ifdef after fixing common wsi */
+#ifdef ANDROID
+   bool has_present_src = false;
+   for (uint32_t i = 0; i < count; i++) {
+      if (attachments[i].initialLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR ||
+          attachments[i].finalLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR) {
+         has_present_src = true;
+         break;
+      }
+   }
+   if (!has_present_src)
+      return attachments;
+
+   size_t size = sizeof(VkAttachmentDescription2) * count;
+   VkAttachmentDescription2 *out_attachments = vk_alloc(
+      alloc, size, VN_DEFAULT_ALIGN, VK_SYSTEM_ALLOCATION_SCOPE_COMMAND);
+   if (!out_attachments)
+      return NULL;
+
+   memcpy(out_attachments, attachments, size);
+   for (uint32_t i = 0; i < count; i++) {
+      if (out_attachments[i].initialLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR)
+         out_attachments[i].initialLayout = VK_IMAGE_LAYOUT_GENERAL;
+      if (out_attachments[i].finalLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR)
+         out_attachments[i].finalLayout = VK_IMAGE_LAYOUT_GENERAL;
+   }
+   return out_attachments;
+#else
+   return attachments;
+#endif
 }
 
 VkResult
@@ -64,11 +147,20 @@ vn_CreateRenderPass2(VkDevice device,
 
    vn_object_base_init(&pass->base, VK_OBJECT_TYPE_RENDER_PASS, &dev->base);
 
-   /* XXX VK_IMAGE_LAYOUT_PRESENT_SRC_KHR */
+   VkRenderPassCreateInfo2 local_pass_info = *pCreateInfo;
+   local_pass_info.pAttachments = vn_get_intercepted_attachments2(
+      pCreateInfo->pAttachments, pCreateInfo->attachmentCount, alloc);
+   if (!local_pass_info.pAttachments) {
+      vk_free(alloc, pass);
+      return vn_error(dev->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
+   }
 
    VkRenderPass pass_handle = vn_render_pass_to_handle(pass);
-   vn_async_vkCreateRenderPass2(dev->instance, device, pCreateInfo, NULL,
+   vn_async_vkCreateRenderPass2(dev->instance, device, &local_pass_info, NULL,
                                 &pass_handle);
+
+   if (local_pass_info.pAttachments != pCreateInfo->pAttachments)
+      vk_free(alloc, (void *)local_pass_info.pAttachments);
 
    *pRenderPass = pass_handle;
 

src/virtio/vulkan/vn_command_buffer.c
@@ -134,6 +134,7 @@ vn_AllocateCommandBuffers(VkDevice device,
       vn_object_base_init(&cmd->base, VK_OBJECT_TYPE_COMMAND_BUFFER,
                           &dev->base);
       cmd->device = dev;
+      cmd->allocator = pool->allocator;
 
       list_addtail(&cmd->head, &pool->command_buffers);
 
@@ -169,6 +170,9 @@ vn_FreeCommandBuffers(VkDevice device,
 
       if (!cmd)
          continue;
+
+      if (cmd->image_barriers)
+         vk_free(alloc, cmd->image_barriers);
 
       vn_cs_encoder_fini(&cmd->cs);
       list_del(&cmd->head);
@@ -939,6 +943,51 @@ vn_CmdResetEvent(VkCommandBuffer commandBuffer,
    vn_encode_vkCmdResetEvent(&cmd->cs, 0, commandBuffer, event, stageMask);
 }
 
+static const VkImageMemoryBarrier *
+vn_get_intercepted_barriers(struct vn_command_buffer *cmd,
+                            const VkImageMemoryBarrier *img_barriers,
+                            uint32_t count)
+{
+   /* XXX drop the #ifdef after fixing common wsi */
+#ifdef ANDROID
+   bool has_present_src = false;
+   for (uint32_t i = 0; i < count; i++) {
+      if (img_barriers[i].oldLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR ||
+          img_barriers[i].newLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR) {
+         has_present_src = false;
+         break;
+      }
+   }
+   if (!has_present_src)
+      return img_barriers;
+
+   size_t size = sizeof(VkImageMemoryBarrier) * count;
+   /* avoid shrinking in case of non efficient reallocation implementation */
+   VkImageMemoryBarrier *barriers = cmd->image_barriers;
+   if (count > cmd->image_barrier_count) {
+      barriers =
+         vk_realloc(&cmd->allocator, cmd->image_barriers, size,
+                    VN_DEFAULT_ALIGN, VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
+      if (!barriers)
+         return img_barriers;
+
+      /* update upon successful reallocation */
+      cmd->image_barrier_count = count;
+      cmd->image_barriers = barriers;
+   }
+   memcpy(barriers, img_barriers, size);
+   for (uint32_t i = 0; i < count; i++) {
+      if (barriers[i].oldLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR)
+         barriers[i].oldLayout = VK_IMAGE_LAYOUT_GENERAL;
+      if (barriers[i].newLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR)
+         barriers[i].newLayout = VK_IMAGE_LAYOUT_GENERAL;
+   }
+   return barriers;
+#else
+   return img_barriers;
+#endif
+}
+
 void
 vn_CmdWaitEvents(VkCommandBuffer commandBuffer,
                  uint32_t eventCount,
@@ -963,13 +1012,14 @@ vn_CmdWaitEvents(VkCommandBuffer commandBuffer,
    if (!vn_cs_encoder_reserve(&cmd->cs, cmd_size))
       return;
 
-   /* XXX VK_IMAGE_LAYOUT_PRESENT_SRC_KHR */
+   const VkImageMemoryBarrier *img_barriers = vn_get_intercepted_barriers(
+      cmd, pImageMemoryBarriers, imageMemoryBarrierCount);
 
    vn_encode_vkCmdWaitEvents(&cmd->cs, 0, commandBuffer, eventCount, pEvents,
                              srcStageMask, dstStageMask, memoryBarrierCount,
                              pMemoryBarriers, bufferMemoryBarrierCount,
                              pBufferMemoryBarriers, imageMemoryBarrierCount,
-                             pImageMemoryBarriers);
+                             img_barriers);
 }
 
 void
@@ -995,12 +1045,13 @@ vn_CmdPipelineBarrier(VkCommandBuffer commandBuffer,
    if (!vn_cs_encoder_reserve(&cmd->cs, cmd_size))
       return;
 
-   /* XXX VK_IMAGE_LAYOUT_PRESENT_SRC_KHR */
+   const VkImageMemoryBarrier *img_barriers = vn_get_intercepted_barriers(
+      cmd, pImageMemoryBarriers, imageMemoryBarrierCount);
 
    vn_encode_vkCmdPipelineBarrier(
       &cmd->cs, 0, commandBuffer, srcStageMask, dstStageMask, dependencyFlags,
       memoryBarrierCount, pMemoryBarriers, bufferMemoryBarrierCount,
-      pBufferMemoryBarriers, imageMemoryBarrierCount, pImageMemoryBarriers);
+      pBufferMemoryBarriers, imageMemoryBarrierCount, img_barriers);
 }
 
 void

src/virtio/vulkan/vn_command_buffer.h
@@ -38,6 +38,11 @@ struct vn_command_buffer {
 
    struct vn_device *device;
 
+   /* for scrubbing VK_IMAGE_LAYOUT_PRESENT_SRC_KHR */
+   VkAllocationCallbacks allocator;
+   uint32_t image_barrier_count;
+   VkImageMemoryBarrier *image_barriers;
+
    struct list_head head;
 
    enum vn_command_buffer_state state;

commit 145ee434a171c22aa31e26249c5666eb2c3e4899

venus: fix render pass without attachments
It was treated as VK_ERROR_OUT_OF_HOST_MEMORY because
vn_get_intercepted_attachments would return NULL.  This fixes various
dEQP tests.

Fixes: f6a0e7d0c4a ("venus: handle VK_IMAGE_LAYOUT_PRESENT_SRC_KHR transfer")
Signed-off-by: Chia-I Wu <olvaffe@gmail.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10592>

src/virtio/vulkan/vn_render_pass.c
@@ -15,6 +15,38 @@
 
 #include "vn_device.h"
 
+static bool
+vn_render_pass_has_present_src(const VkRenderPassCreateInfo *create_info)
+{
+   /* XXX drop the #ifdef after fixing common wsi */
+#ifdef ANDROID
+   for (uint32_t i = 0; i < create_info->attachmentCount; i++) {
+      const VkAttachmentDescription *att = &create_info->pAttachments[i];
+      if (att->initialLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR ||
+          att->finalLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR)
+         return true;
+   }
+#endif
+
+   return false;
+}
+
+static bool
+vn_render_pass_has_present_src2(const VkRenderPassCreateInfo2 *create_info)
+{
+   /* XXX drop the #ifdef after fixing common wsi */
+#ifdef ANDROID
+   for (uint32_t i = 0; i < create_info->attachmentCount; i++) {
+      const VkAttachmentDescription2 *att = &create_info->pAttachments[i];
+      if (att->initialLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR ||
+          att->finalLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR)
+         return true;
+   }
+#endif
+
+   return false;
+}
+
 /* render pass commands */
 
 static const VkAttachmentDescription *
@@ -22,19 +54,6 @@ vn_get_intercepted_attachments(const VkAttachmentDescription *attachments,
                                uint32_t count,
                                const VkAllocationCallbacks *alloc)
 {
-   /* XXX drop the #ifdef after fixing common wsi */
-#ifdef ANDROID
-   bool has_present_src = false;
-   for (uint32_t i = 0; i < count; i++) {
-      if (attachments[i].initialLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR ||
-          attachments[i].finalLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR) {
-         has_present_src = true;
-         break;
-      }
-   }
-   if (!has_present_src)
-      return attachments;
-
    size_t size = sizeof(VkAttachmentDescription) * count;
    VkAttachmentDescription *out_attachments = vk_alloc(
       alloc, size, VN_DEFAULT_ALIGN, VK_SYSTEM_ALLOCATION_SCOPE_COMMAND);
@@ -49,9 +68,6 @@ vn_get_intercepted_attachments(const VkAttachmentDescription *attachments,
          out_attachments[i].finalLayout = VK_IMAGE_LAYOUT_GENERAL;
    }
    return out_attachments;
-#else
-   return attachments;
-#endif
 }
 
 VkResult
@@ -72,19 +88,23 @@ vn_CreateRenderPass(VkDevice device,
 
    vn_object_base_init(&pass->base, VK_OBJECT_TYPE_RENDER_PASS, &dev->base);
 
-   VkRenderPassCreateInfo local_pass_info = *pCreateInfo;
-   local_pass_info.pAttachments = vn_get_intercepted_attachments(
-      pCreateInfo->pAttachments, pCreateInfo->attachmentCount, alloc);
-   if (!local_pass_info.pAttachments) {
-      vk_free(alloc, pass);
-      return vn_error(dev->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
+   VkRenderPassCreateInfo local_pass_info;
+   if (vn_render_pass_has_present_src(pCreateInfo)) {
+      local_pass_info = *pCreateInfo;
+      local_pass_info.pAttachments = vn_get_intercepted_attachments(
+         pCreateInfo->pAttachments, pCreateInfo->attachmentCount, alloc);
+      if (!local_pass_info.pAttachments) {
+         vk_free(alloc, pass);
+         return vn_error(dev->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
+      }
+      pCreateInfo = &local_pass_info;
    }
 
    VkRenderPass pass_handle = vn_render_pass_to_handle(pass);
-   vn_async_vkCreateRenderPass(dev->instance, device, &local_pass_info, NULL,
+   vn_async_vkCreateRenderPass(dev->instance, device, pCreateInfo, NULL,
                                &pass_handle);
 
-   if (local_pass_info.pAttachments != pCreateInfo->pAttachments)
+   if (pCreateInfo == &local_pass_info)
       vk_free(alloc, (void *)local_pass_info.pAttachments);
 
    *pRenderPass = pass_handle;
@@ -97,19 +117,6 @@ vn_get_intercepted_attachments2(const VkAttachmentDescription2 *attachments,
                                 uint32_t count,
                                 const VkAllocationCallbacks *alloc)
 {
-   /* XXX drop the #ifdef after fixing common wsi */
-#ifdef ANDROID
-   bool has_present_src = false;
-   for (uint32_t i = 0; i < count; i++) {
-      if (attachments[i].initialLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR ||
-          attachments[i].finalLayout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR) {
-         has_present_src = true;
-         break;
-      }
-   }
-   if (!has_present_src)
-      return attachments;
-
    size_t size = sizeof(VkAttachmentDescription2) * count;
    VkAttachmentDescription2 *out_attachments = vk_alloc(
       alloc, size, VN_DEFAULT_ALIGN, VK_SYSTEM_ALLOCATION_SCOPE_COMMAND);
@@ -124,9 +131,6 @@ vn_get_intercepted_attachments2(const VkAttachmentDescription2 *attachments,
          out_attachments[i].finalLayout = VK_IMAGE_LAYOUT_GENERAL;
    }
    return out_attachments;
-#else
-   return attachments;
-#endif
 }
 
 VkResult
@@ -147,19 +151,23 @@ vn_CreateRenderPass2(VkDevice device,
 
    vn_object_base_init(&pass->base, VK_OBJECT_TYPE_RENDER_PASS, &dev->base);
 
-   VkRenderPassCreateInfo2 local_pass_info = *pCreateInfo;
-   local_pass_info.pAttachments = vn_get_intercepted_attachments2(
-      pCreateInfo->pAttachments, pCreateInfo->attachmentCount, alloc);
-   if (!local_pass_info.pAttachments) {
-      vk_free(alloc, pass);
-      return vn_error(dev->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
+   VkRenderPassCreateInfo2 local_pass_info;
+   if (vn_render_pass_has_present_src2(pCreateInfo)) {
+      local_pass_info = *pCreateInfo;
+      local_pass_info.pAttachments = vn_get_intercepted_attachments2(
+         pCreateInfo->pAttachments, pCreateInfo->attachmentCount, alloc);
+      if (!local_pass_info.pAttachments) {
+         vk_free(alloc, pass);
+         return vn_error(dev->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
+      }
+      pCreateInfo = &local_pass_info;
    }
 
    VkRenderPass pass_handle = vn_render_pass_to_handle(pass);
-   vn_async_vkCreateRenderPass2(dev->instance, device, &local_pass_info, NULL,
+   vn_async_vkCreateRenderPass2(dev->instance, device, pCreateInfo, NULL,
                                 &pass_handle);
 
-   if (local_pass_info.pAttachments != pCreateInfo->pAttachments)
+   if (pCreateInfo == &local_pass_info)
       vk_free(alloc, (void *)local_pass_info.pAttachments);
 
    *pRenderPass = pass_handle;