openharmony/third_party_mesa3d

commit 2c4b6d30617103232a8b9f3caac12318845c2455

radv: require DRM 3.35+
Linux Kernel 4.15+ is now required for RADV, this kernel has been
released 3 years ago and should be in most modern distros.

This allows us to remove a lot of legacy code for fence/semaphore.

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9800>

src/amd/vulkan/winsys/amdgpu/radv_amdgpu_cs.c
@@ -127,10 +127,6 @@ struct radv_amdgpu_cs_request {
 };
 
 
-static int radv_amdgpu_signal_sems(struct radv_amdgpu_ctx *ctx,
-				   uint32_t ip_type,
-				   uint32_t ring,
-				   struct radv_winsys_sem_info *sem_info);
 static int radv_amdgpu_cs_submit(struct radv_amdgpu_ctx *ctx,
 				 struct radv_amdgpu_cs_request *request,
 				 struct radv_winsys_sem_info *sem_info);
@@ -145,114 +141,6 @@ static void radv_amdgpu_request_to_fence(struct radv_amdgpu_ctx *ctx,
 	fence->fence.ring = req->ring;
 	fence->fence.fence = req->seq_no;
 	fence->user_ptr = (volatile uint64_t*)(ctx->fence_map + req->ip_type * MAX_RINGS_PER_TYPE + req->ring);
-}
-
-static struct radeon_winsys_fence *radv_amdgpu_create_fence()
-{
-	struct radv_amdgpu_fence *fence = calloc(1, sizeof(struct radv_amdgpu_fence));
-	if (!fence)
-		return NULL;
-
-	fence->fence.fence = UINT64_MAX;
-	return (struct radeon_winsys_fence*)fence;
-}
-
-static void radv_amdgpu_destroy_fence(struct radeon_winsys_fence *_fence)
-{
-	struct radv_amdgpu_fence *fence = (struct radv_amdgpu_fence *)_fence;
-	free(fence);
-}
-
-static void radv_amdgpu_reset_fence(struct radeon_winsys_fence *_fence)
-{
-	struct radv_amdgpu_fence *fence = (struct radv_amdgpu_fence *)_fence;
-	fence->fence.fence = UINT64_MAX;
-}
-
-static void radv_amdgpu_signal_fence(struct radeon_winsys_fence *_fence)
-{
-	struct radv_amdgpu_fence *fence = (struct radv_amdgpu_fence *)_fence;
-	fence->fence.fence = 0;
-}
-
-static bool radv_amdgpu_is_fence_waitable(struct radeon_winsys_fence *_fence)
-{
-	struct radv_amdgpu_fence *fence = (struct radv_amdgpu_fence *)_fence;
-	return fence->fence.fence < UINT64_MAX;
-}
-
-static bool radv_amdgpu_fence_wait(struct radeon_winsys *_ws,
-			      struct radeon_winsys_fence *_fence,
-			      bool absolute,
-			      uint64_t timeout)
-{
-	struct radv_amdgpu_fence *fence = (struct radv_amdgpu_fence *)_fence;
-	unsigned flags = absolute ? AMDGPU_QUERY_FENCE_TIMEOUT_IS_ABSOLUTE : 0;
-	int r;
-	uint32_t expired = 0;
-
-	/* Special casing 0 and UINT64_MAX so that they work without user_ptr/fence.ctx */
-	if (fence->fence.fence == UINT64_MAX)
-		return false;
-
-	if (fence->fence.fence == 0)
-		return true;
-
-	if (fence->user_ptr) {
-		if (*fence->user_ptr >= fence->fence.fence)
-			return true;
-		if (!absolute && !timeout)
-			return false;
-	}
-
-	/* Now use the libdrm query. */
-	r = amdgpu_cs_query_fence_status(&fence->fence,
-	                                 timeout,
-	                                 flags,
-	                                 &expired);
-
-	if (r) {
-		fprintf(stderr, "amdgpu: radv_amdgpu_cs_query_fence_status failed.\n");
-		return false;
-	}
-
-	if (expired)
-		return true;
-
-	return false;
-}
-
-
-static bool radv_amdgpu_fences_wait(struct radeon_winsys *_ws,
-			      struct radeon_winsys_fence *const *_fences,
-			      uint32_t fence_count,
-			      bool wait_all,
-			      uint64_t timeout)
-{
-	struct amdgpu_cs_fence *fences = malloc(sizeof(struct amdgpu_cs_fence) * fence_count);
-	int r;
-	uint32_t expired = 0, first = 0;
-
-	if (!fences)
-		return false;
-
-	for (uint32_t i = 0; i < fence_count; ++i)
-		fences[i] = ((struct radv_amdgpu_fence *)_fences[i])->fence;
-
-	/* Now use the libdrm query. */
-	r = amdgpu_cs_wait_fences(fences, fence_count, wait_all,
-	                          timeout, &expired, &first);
-
-	free(fences);
-	if (r) {
-		fprintf(stderr, "amdgpu: amdgpu_cs_wait_fences failed.\n");
-		return false;
-	}
-
-	if (expired)
-		return true;
-
-	return false;
 }
 
 static void radv_amdgpu_cs_destroy(struct radeon_cmdbuf *rcs)
@@ -840,11 +728,9 @@ radv_amdgpu_winsys_cs_submit_chained(struct radeon_winsys_ctx *_ctx,
 				     struct radv_winsys_sem_info *sem_info,
 				     struct radeon_cmdbuf **cs_array,
 				     unsigned cs_count,
-				     struct radeon_cmdbuf *initial_preamble_cs,
-				     struct radeon_winsys_fence *_fence)
+				     struct radeon_cmdbuf *initial_preamble_cs)
 {
 	struct radv_amdgpu_ctx *ctx = radv_amdgpu_ctx(_ctx);
-	struct radv_amdgpu_fence *fence = (struct radv_amdgpu_fence *)_fence;
 	struct radv_amdgpu_cs *cs0 = radv_amdgpu_cs(cs_array[0]);
 	struct radv_amdgpu_winsys *aws = cs0->ws;
 	struct drm_amdgpu_bo_list_entry *handles = NULL;
@@ -910,9 +796,6 @@ radv_amdgpu_winsys_cs_submit_chained(struct radeon_winsys_ctx *_ctx,
 	if (result != VK_SUCCESS)
 		goto fail;
 
-	if (fence)
-		radv_amdgpu_request_to_fence(ctx, fence, &request);
-
 	radv_assign_last_submit(ctx, &request);
 
 fail:
@@ -926,11 +809,9 @@ radv_amdgpu_winsys_cs_submit_fallback(struct radeon_winsys_ctx *_ctx,
 				      struct radv_winsys_sem_info *sem_info,
 				      struct radeon_cmdbuf **cs_array,
 				      unsigned cs_count,
-				      struct radeon_cmdbuf *initial_preamble_cs,
-				      struct radeon_winsys_fence *_fence)
+				      struct radeon_cmdbuf *initial_preamble_cs)
 {
 	struct radv_amdgpu_ctx *ctx = radv_amdgpu_ctx(_ctx);
-	struct radv_amdgpu_fence *fence = (struct radv_amdgpu_fence *)_fence;
 	struct drm_amdgpu_bo_list_entry *handles = NULL;
 	struct radv_amdgpu_cs_request request;
 	struct amdgpu_cs_ib_info *ibs;
@@ -996,9 +877,6 @@ radv_amdgpu_winsys_cs_submit_fallback(struct radeon_winsys_ctx *_ctx,
 	if (result != VK_SUCCESS)
 		goto fail;
 
-	if (fence)
-		radv_amdgpu_request_to_fence(ctx, fence, &request);
-
 	radv_assign_last_submit(ctx, &request);
 
 fail:
@@ -1013,11 +891,9 @@ radv_amdgpu_winsys_cs_submit_sysmem(struct radeon_winsys_ctx *_ctx,
 				    struct radeon_cmdbuf **cs_array,
 				    unsigned cs_count,
 				    struct radeon_cmdbuf *initial_preamble_cs,
-				    struct radeon_cmdbuf *continue_preamble_cs,
-				    struct radeon_winsys_fence *_fence)
+				    struct radeon_cmdbuf *continue_preamble_cs)
 {
 	struct radv_amdgpu_ctx *ctx = radv_amdgpu_ctx(_ctx);
-	struct radv_amdgpu_fence *fence = (struct radv_amdgpu_fence *)_fence;
 	struct radv_amdgpu_cs *cs0 = radv_amdgpu_cs(cs_array[0]);
 	struct radeon_winsys *ws = (struct radeon_winsys*)cs0->ws;
 	struct radv_amdgpu_winsys *aws = cs0->ws;
@@ -1198,8 +1074,6 @@ radv_amdgpu_winsys_cs_submit_sysmem(struct radeon_winsys_ctx *_ctx,
 
 		i += cnt;
 	}
-	if (fence)
-		radv_amdgpu_request_to_fence(ctx, fence, &request);
 
 	radv_assign_last_submit(ctx, &request);
 
@@ -1213,26 +1087,23 @@ static VkResult radv_amdgpu_winsys_cs_submit(struct radeon_winsys_ctx *_ctx,
 					     struct radeon_cmdbuf *initial_preamble_cs,
 					     struct radeon_cmdbuf *continue_preamble_cs,
 					     struct radv_winsys_sem_info *sem_info,
-					     bool can_patch,
-					     struct radeon_winsys_fence *_fence)
+					     bool can_patch)
 {
 	struct radv_amdgpu_cs *cs = radv_amdgpu_cs(cs_array[0]);
-	struct radv_amdgpu_ctx *ctx = radv_amdgpu_ctx(_ctx);
 	VkResult result;
 
 	assert(sem_info);
 	if (!cs->ws->use_ib_bos) {
 		result = radv_amdgpu_winsys_cs_submit_sysmem(_ctx, queue_idx, sem_info, cs_array,
-							     cs_count, initial_preamble_cs, continue_preamble_cs, _fence);
+							     cs_count, initial_preamble_cs, continue_preamble_cs);
 	} else if (can_patch) {
 		result = radv_amdgpu_winsys_cs_submit_chained(_ctx, queue_idx, sem_info, cs_array,
-							      cs_count, initial_preamble_cs, _fence);
+							      cs_count, initial_preamble_cs);
 	} else {
 		result = radv_amdgpu_winsys_cs_submit_fallback(_ctx, queue_idx, sem_info, cs_array,
-							       cs_count, initial_preamble_cs, _fence);
+							       cs_count, initial_preamble_cs);
 	}
 
-	radv_amdgpu_signal_sems(ctx, cs->hw_ip, queue_idx, sem_info);
 	return result;
 }
 
@@ -1380,37 +1251,6 @@ static bool radv_amdgpu_ctx_wait_idle(struct radeon_winsys_ctx *rwctx,
 	}
 
 	return true;
-}
-
-static struct radeon_winsys_sem *radv_amdgpu_create_sem(struct radeon_winsys *_ws)
-{
-	struct amdgpu_cs_fence *sem = CALLOC_STRUCT(amdgpu_cs_fence);
-	if (!sem)
-		return NULL;
-
-	return (struct radeon_winsys_sem *)sem;
-}
-
-static void radv_amdgpu_destroy_sem(struct radeon_winsys_sem *_sem)
-{
-	struct amdgpu_cs_fence *sem = (struct amdgpu_cs_fence *)_sem;
-	FREE(sem);
-}
-
-static int radv_amdgpu_signal_sems(struct radv_amdgpu_ctx *ctx,
-				   uint32_t ip_type,
-				   uint32_t ring,
-				   struct radv_winsys_sem_info *sem_info)
-{
-	for (unsigned i = 0; i < sem_info->signal.sem_count; i++) {
-		struct amdgpu_cs_fence *sem = (struct amdgpu_cs_fence *)(sem_info->signal.sem)[i];
-
-		if (sem->context)
-			return -EINVAL;
-
-		*sem = ctx->last_submission[ip_type][ring].fence;
-	}
-	return 0;
 }
 
 static void *radv_amdgpu_cs_alloc_syncobj_chunk(struct radv_winsys_sem_counts *counts,
@@ -1570,17 +1410,14 @@ radv_amdgpu_cs_submit(struct radv_amdgpu_ctx *ctx,
 	int size;
 	struct drm_amdgpu_cs_chunk *chunks;
 	struct drm_amdgpu_cs_chunk_data *chunk_data;
-	struct drm_amdgpu_cs_chunk_dep *sem_dependencies = NULL;
-	bool use_bo_list_create = ctx->ws->info.drm_minor < 27;
 	struct drm_amdgpu_bo_list_in bo_list_in;
 	void *wait_syncobj = NULL, *signal_syncobj = NULL;
 	uint32_t *in_syncobjs = NULL;
 	int i;
-	struct amdgpu_cs_fence *sem;
 	uint32_t bo_list = 0;
 	VkResult result = VK_SUCCESS;
 
-	size = request->number_of_ibs + 2 /* user fence */ + (!use_bo_list_create ? 1 : 0) + 3;
+	size = request->number_of_ibs + 2 /* user fence */ + 4;
 
 	chunks = malloc(sizeof(chunks[0]) * size);
 	if (!chunks)
@@ -1644,37 +1481,6 @@ radv_amdgpu_cs_submit(struct radv_amdgpu_ctx *ctx,
 		}
 		num_chunks++;
 
-		if (sem_info->wait.sem_count == 0)
-			sem_info->cs_emit_wait = false;
-
-	}
-
-	if (sem_info->wait.sem_count && sem_info->cs_emit_wait) {
-		sem_dependencies = malloc(sizeof(sem_dependencies[0]) * sem_info->wait.sem_count);
-		if (!sem_dependencies) {
-			result = VK_ERROR_OUT_OF_HOST_MEMORY;
-			goto error_out;
-		}
-
-		int sem_count = 0;
-
-		for (unsigned j = 0; j < sem_info->wait.sem_count; j++) {
-			sem = (struct amdgpu_cs_fence *)sem_info->wait.sem[j];
-			if (!sem->context)
-				continue;
-			struct drm_amdgpu_cs_chunk_dep *dep = &sem_dependencies[sem_count++];
-
-			amdgpu_cs_chunk_fence_to_dep(sem, dep);
-
-			sem->context = NULL;
-		}
-		i = num_chunks++;
-
-		/* dependencies chunk */
-		chunks[i].chunk_id = AMDGPU_CHUNK_ID_DEPENDENCIES;
-		chunks[i].length_dw = sizeof(struct drm_amdgpu_cs_chunk_dep) / 4 * sem_count;
-		chunks[i].chunk_data = (uint64_t)(uintptr_t)sem_dependencies;
-
 		sem_info->cs_emit_wait = false;
 	}
 
@@ -1697,35 +1503,17 @@ radv_amdgpu_cs_submit(struct radv_amdgpu_ctx *ctx,
 		num_chunks++;
 	}
 
-	if (use_bo_list_create) {
-		/* Legacy path creating the buffer list handle and passing it
-		 * to the CS ioctl.
-		 */
-		r = amdgpu_bo_list_create_raw(ctx->ws->dev, request->num_handles,
-					      request->handles, &bo_list);
-		if (r) {
-			if (r == -ENOMEM) {
-				fprintf(stderr, "amdgpu: Not enough memory for buffer list creation.\n");
-				result = VK_ERROR_OUT_OF_HOST_MEMORY;
-			} else {
-				fprintf(stderr, "amdgpu: buffer list creation failed (%d).\n", r);
-				result = VK_ERROR_UNKNOWN;
-			}
-			goto error_out;
-		}
-	} else {
-		/* Standard path passing the buffer list via the CS ioctl. */
-		bo_list_in.operation = ~0;
-		bo_list_in.list_handle = ~0;
-		bo_list_in.bo_number = request->num_handles;
-		bo_list_in.bo_info_size = sizeof(struct drm_amdgpu_bo_list_entry);
-		bo_list_in.bo_info_ptr = (uint64_t)(uintptr_t)request->handles;
+	/* Standard path passing the buffer list via the CS ioctl. */
+	bo_list_in.operation = ~0;
+	bo_list_in.list_handle = ~0;
+	bo_list_in.bo_number = request->num_handles;
+	bo_list_in.bo_info_size = sizeof(struct drm_amdgpu_bo_list_entry);
+	bo_list_in.bo_info_ptr = (uint64_t)(uintptr_t)request->handles;
 
-		chunks[num_chunks].chunk_id = AMDGPU_CHUNK_ID_BO_HANDLES;
-		chunks[num_chunks].length_dw = sizeof(struct drm_amdgpu_bo_list_in) / 4;
-		chunks[num_chunks].chunk_data = (uintptr_t)&bo_list_in;
-		num_chunks++;
-	}
+	chunks[num_chunks].chunk_id = AMDGPU_CHUNK_ID_BO_HANDLES;
+	chunks[num_chunks].length_dw = sizeof(struct drm_amdgpu_bo_list_in) / 4;
+	chunks[num_chunks].chunk_data = (uintptr_t)&bo_list_in;
+	num_chunks++;
 
 	r = amdgpu_cs_submit_raw2(ctx->ws->dev,
 				 ctx->ctx,
@@ -1758,7 +1546,6 @@ error_out:
 	}
 	free(chunks);
 	free(chunk_data);
-	free(sem_dependencies);
 	free(wait_syncobj);
 	free(signal_syncobj);
 	return result;
@@ -1918,13 +1705,6 @@ void radv_amdgpu_cs_init_functions(struct radv_amdgpu_winsys *ws)
 	ws->base.cs_execute_secondary = radv_amdgpu_cs_execute_secondary;
 	ws->base.cs_submit = radv_amdgpu_winsys_cs_submit;
 	ws->base.cs_dump = radv_amdgpu_winsys_cs_dump;
-	ws->base.create_fence = radv_amdgpu_create_fence;
-	ws->base.destroy_fence = radv_amdgpu_destroy_fence;
-	ws->base.reset_fence = radv_amdgpu_reset_fence;
-	ws->base.signal_fence = radv_amdgpu_signal_fence;
-	ws->base.is_fence_waitable = radv_amdgpu_is_fence_waitable;
-	ws->base.create_sem = radv_amdgpu_create_sem;
-	ws->base.destroy_sem = radv_amdgpu_destroy_sem;
 	ws->base.create_syncobj = radv_amdgpu_create_syncobj;
 	ws->base.destroy_syncobj = radv_amdgpu_destroy_syncobj;
 	ws->base.reset_syncobj = radv_amdgpu_reset_syncobj;
@@ -1936,6 +1716,4 @@ void radv_amdgpu_cs_init_functions(struct radv_amdgpu_winsys *ws)
 	ws->base.import_syncobj = radv_amdgpu_import_syncobj;
 	ws->base.export_syncobj_to_sync_file = radv_amdgpu_export_syncobj_to_sync_file;
 	ws->base.import_syncobj_from_sync_file = radv_amdgpu_import_syncobj_from_sync_file;
-	ws->base.fence_wait = radv_amdgpu_fence_wait;
-	ws->base.fences_wait = radv_amdgpu_fences_wait;
 }

src/amd/vulkan/winsys/amdgpu/radv_amdgpu_winsys.c
@@ -45,6 +45,11 @@ do_winsys_init(struct radv_amdgpu_winsys *ws, int fd)
 	if (!ac_query_gpu_info(fd, ws->dev, &ws->info, &ws->amdinfo))
 		return false;
 
+	if (ws->info.drm_minor < 35) {
+		fprintf(stderr, "radv: DRM 3.35+ is required (Linux kernel 4.15+)\n");
+		return false;
+	}
+
 	/* LLVM 11 is required for GFX10.3. */
 	if (ws->info.chip_class == GFX10_3 && ws->use_llvm && LLVM_VERSION_MAJOR < 11) {
 		fprintf(stderr, "radv: GFX 10.3 requires LLVM 11 or higher\n");

src/amd/common/ac_gpu_info.c
@@ -616,9 +616,7 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
    info->has_userptr = true;
    info->has_syncobj = has_syncobj(fd);
    info->has_timeline_syncobj = has_timeline_syncobj(fd);
-   info->has_syncobj_wait_for_submit = info->has_syncobj && info->drm_minor >= 20;
    info->has_fence_to_handle = info->has_syncobj && info->drm_minor >= 21;
-   info->has_ctx_priority = info->drm_minor >= 22;
    info->has_local_buffers = info->drm_minor >= 20;
    info->kernel_flushes_hdp_before_ib = true;
    info->htile_cmask_support_1d_tiling = true;
@@ -1107,10 +1105,8 @@ void ac_print_gpu_info(struct radeon_info *info, FILE *f)
    fprintf(f, "    drm = %i.%i.%i\n", info->drm_major, info->drm_minor, info->drm_patchlevel);
    fprintf(f, "    has_userptr = %i\n", info->has_userptr);
    fprintf(f, "    has_syncobj = %u\n", info->has_syncobj);
-   fprintf(f, "    has_syncobj_wait_for_submit = %u\n", info->has_syncobj_wait_for_submit);
    fprintf(f, "    has_timeline_syncobj = %u\n", info->has_timeline_syncobj);
    fprintf(f, "    has_fence_to_handle = %u\n", info->has_fence_to_handle);
-   fprintf(f, "    has_ctx_priority = %u\n", info->has_ctx_priority);
    fprintf(f, "    has_local_buffers = %u\n", info->has_local_buffers);
    fprintf(f, "    kernel_flushes_hdp_before_ib = %u\n", info->kernel_flushes_hdp_before_ib);
    fprintf(f, "    htile_cmask_support_1d_tiling = %u\n", info->htile_cmask_support_1d_tiling);

src/amd/common/ac_gpu_info.h
@@ -143,10 +143,8 @@ struct radeon_info {
    bool is_amdgpu;
    bool has_userptr;
    bool has_syncobj;
-   bool has_syncobj_wait_for_submit;
    bool has_timeline_syncobj;
    bool has_fence_to_handle;
-   bool has_ctx_priority;
    bool has_local_buffers;
    bool kernel_flushes_hdp_before_ib;
    bool htile_cmask_support_1d_tiling;

src/amd/vulkan/radv_debug.c
@@ -127,12 +127,6 @@ radv_dump_debug_registers(struct radv_device *device, FILE *f)
 	fprintf(f, "Memory-mapped registers:\n");
 	radv_dump_mmapped_reg(device, f, R_008010_GRBM_STATUS);
 
-	/* No other registers can be read on DRM < 3.1.0. */
-	if (info->drm_minor < 1) {
-		fprintf(f, "\n");
-		return;
-	}
-
 	radv_dump_mmapped_reg(device, f, R_008008_GRBM_STATUS2);
 	radv_dump_mmapped_reg(device, f, R_008014_GRBM_STATUS_SE0);
 	radv_dump_mmapped_reg(device, f, R_008018_GRBM_STATUS_SE1);

commit 18bf691ce766f26543d8182eb9cd62cb700e0a18

radv: Only require DRM 3.23.
Turns out kernel 4.15 only goes up to amdgpu 3.23 . 3.35 is way
too new. Too new for e.g. ChromeOS.

Fixes: 2c4b6d30617 ("radv: require DRM 3.35+")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4728
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10576>

src/amd/vulkan/winsys/amdgpu/radv_amdgpu_cs.c
@@ -1448,6 +1448,7 @@ radv_amdgpu_cs_submit(struct radv_amdgpu_ctx *ctx, struct radv_amdgpu_cs_request
    int size;
    struct drm_amdgpu_cs_chunk *chunks;
    struct drm_amdgpu_cs_chunk_data *chunk_data;
+   bool use_bo_list_create = ctx->ws->info.drm_minor < 27;
    struct drm_amdgpu_bo_list_in bo_list_in;
    void *wait_syncobj = NULL, *signal_syncobj = NULL;
    uint32_t *in_syncobjs = NULL;
@@ -1455,7 +1456,7 @@ radv_amdgpu_cs_submit(struct radv_amdgpu_ctx *ctx, struct radv_amdgpu_cs_request
    uint32_t bo_list = 0;
    VkResult result = VK_SUCCESS;
 
-   size = request->number_of_ibs + 2 /* user fence */ + 4;
+   size = request->number_of_ibs + 2 /* user fence */ + (!use_bo_list_create ? 1 : 0) + 3;
 
    chunks = malloc(sizeof(chunks[0]) * size);
    if (!chunks)
@@ -1536,17 +1537,35 @@ radv_amdgpu_cs_submit(struct radv_amdgpu_ctx *ctx, struct radv_amdgpu_cs_request
       num_chunks++;
    }
 
-   /* Standard path passing the buffer list via the CS ioctl. */
-   bo_list_in.operation = ~0;
-   bo_list_in.list_handle = ~0;
-   bo_list_in.bo_number = request->num_handles;
-   bo_list_in.bo_info_size = sizeof(struct drm_amdgpu_bo_list_entry);
-   bo_list_in.bo_info_ptr = (uint64_t)(uintptr_t)request->handles;
+   if (use_bo_list_create) {
+      /* Legacy path creating the buffer list handle and passing it
+       * to the CS ioctl.
+       */
+      r = amdgpu_bo_list_create_raw(ctx->ws->dev, request->num_handles,
+                                    request->handles, &bo_list);
+      if (r) {
+         if (r == -ENOMEM) {
+            fprintf(stderr, "amdgpu: Not enough memory for buffer list creation.\n");
+            result = VK_ERROR_OUT_OF_HOST_MEMORY;
+         } else {
+            fprintf(stderr, "amdgpu: buffer list creation failed (%d).\n", r);
+            result = VK_ERROR_UNKNOWN;
+         }
+         goto error_out;
+      }
+   } else {
+      /* Standard path passing the buffer list via the CS ioctl. */
+      bo_list_in.operation = ~0;
+      bo_list_in.list_handle = ~0;
+      bo_list_in.bo_number = request->num_handles;
+      bo_list_in.bo_info_size = sizeof(struct drm_amdgpu_bo_list_entry);
+      bo_list_in.bo_info_ptr = (uint64_t)(uintptr_t)request->handles;
 
-   chunks[num_chunks].chunk_id = AMDGPU_CHUNK_ID_BO_HANDLES;
-   chunks[num_chunks].length_dw = sizeof(struct drm_amdgpu_bo_list_in) / 4;
-   chunks[num_chunks].chunk_data = (uintptr_t)&bo_list_in;
-   num_chunks++;
+      chunks[num_chunks].chunk_id = AMDGPU_CHUNK_ID_BO_HANDLES;
+      chunks[num_chunks].length_dw = sizeof(struct drm_amdgpu_bo_list_in) / 4;
+      chunks[num_chunks].chunk_data = (uintptr_t)&bo_list_in;
+      num_chunks++;
+   }
 
    r = amdgpu_cs_submit_raw2(ctx->ws->dev, ctx->ctx, bo_list, num_chunks, chunks, &request->seq_no);
 

src/amd/vulkan/winsys/amdgpu/radv_amdgpu_winsys.c
@@ -44,8 +44,8 @@ do_winsys_init(struct radv_amdgpu_winsys *ws, int fd)
    if (!ac_query_gpu_info(fd, ws->dev, &ws->info, &ws->amdinfo))
       return false;
 
-   if (ws->info.drm_minor < 35) {
-      fprintf(stderr, "radv: DRM 3.35+ is required (Linux kernel 4.15+)\n");
+   if (ws->info.drm_minor < 23) {
+      fprintf(stderr, "radv: DRM 3.23+ is required (Linux kernel 4.15+)\n");
       return false;
    }
 