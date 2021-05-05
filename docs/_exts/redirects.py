openharmony/third_party_mesa3d

commit 8f1fb4f14c5c7619c44329c12daa3536edf984a4

docs: add an extension to generate redirects
Reviewed-by: Eric Engestrom <eric@engestrom.ch>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5706>

docs/_exts/redirects.py
@@ -1,0 +1,19 @@
+import os
+
+redirects = []
+
+def create_redirect(dst):
+    tpl = '<html><head><meta http-equiv="refresh" content="0; url={0}"><script>window.location.replace("{0}")</script></head></html>'
+    return tpl.format(dst)
+
+def create_redirects(app, docname):
+    if not app.builder.name == 'html':
+        return
+    for src, dst in redirects:
+        path = os.path.join(app.outdir, '{0}.html'.format(src))
+        url = '{0}.html'.format(dst)
+        with open(path, 'w') as f:
+            f.write(create_redirect(url))
+
+def setup(app):
+    app.connect('build-finished', create_redirects)

docs/conf.py
@@ -38,7 +38,7 @@ sys.path.append(os.path.abspath('_exts'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.graphviz', 'formatting']
+extensions = ['sphinx.ext.graphviz', 'formatting', 'redirects']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

commit f40c62eab973842a99ff2584a38091c5b23bd793

docs: do not generate redirects on error
The build-finished event is also triggered when there's an error. I
somehow got the second argument wrong, and ended up ignoring the case.
This can lead to new exceptions being thrown due to missing files, that
ends up hiding the real problem.

Fixes: 8f1fb4f14c5 ("docs: add an extension to generate redirects")
Reviewed-by: Eric Engestrom <eric@engestrom.ch>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10407>

docs/_exts/redirects.py
@@ -6,8 +6,8 @@ def create_redirect(dst):
     tpl = '<html><head><meta http-equiv="refresh" content="0; url={0}"><script>window.location.replace("{0}")</script></head></html>'
     return tpl.format(dst)
 
-def create_redirects(app, docname):
-    if not app.builder.name == 'html':
+def create_redirects(app, exception):
+    if exception is not None or not app.builder.name == 'html':
         return
     for src, dst in app.config.html_redirects:
         path = os.path.join(app.outdir, '{0}.html'.format(src))