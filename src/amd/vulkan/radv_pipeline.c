openharmony/third_party_mesa3d

commit ad86705685326ce306799a5786aded675d90f410

radv: Reorder some NIR optimizations in preparation for the I/O changes.
Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9201>

src/amd/vulkan/radv_pipeline.c
@@ -3451,6 +3451,9 @@ VkResult radv_create_shaders(struct radv_pipeline *pipeline,
 				nir_opt_idiv_const(nir[i], 8);
 			nir_lower_idiv(nir[i], nir_lower_idiv_precise);
 
+			nir_opt_sink(nir[i], nir_move_load_input | nir_move_const_undef | nir_move_copies);
+			nir_opt_move(nir[i], nir_move_load_input | nir_move_const_undef | nir_move_copies);
+
 			/* optimize the lowered ALU operations */
 			bool more_algebraic = true;
 			while (more_algebraic) {
@@ -3458,8 +3461,12 @@ VkResult radv_create_shaders(struct radv_pipeline *pipeline,
 				NIR_PASS_V(nir[i], nir_copy_prop);
 				NIR_PASS_V(nir[i], nir_opt_dce);
 				NIR_PASS_V(nir[i], nir_opt_constant_folding);
+				NIR_PASS_V(nir[i], nir_opt_cse);
 				NIR_PASS(more_algebraic, nir[i], nir_opt_algebraic);
 			}
+
+			if (i == MESA_SHADER_COMPUTE)
+				NIR_PASS_V(nir[i], nir_opt_offsets);
 
 			/* Do late algebraic optimization to turn add(a,
 			 * neg(b)) back into subs, then the mandatory cleanup

commit 3411a9cc388e363af078eadf24d6574ed6842d5c

radv: call nir_copy_prop() after load-store vectorization
The load-store vectorizer can create a large amount
of unnecessary nir_op_vec and nir_op_mov instructions.
This prevents nir_opt_move from stalling to much and
potentially also helps other passes.

Closes: #4778
Fixes: ad86705685326ce306799a5786aded675d90f410 ('radv: Reorder some NIR optimizations in preparation for the I/O changes.')
Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10804>

src/amd/vulkan/radv_pipeline.c
@@ -3402,6 +3402,7 @@ radv_create_shaders(struct radv_pipeline *pipeline, struct radv_device *device,
          }
 
          if (nir_opt_load_store_vectorize(nir[i], &vectorize_opts)) {
+            NIR_PASS_V(nir[i], nir_copy_prop);
             lower_to_scalar = true;
 
             /* Gather info again, to update whether 8/16-bit are used. */