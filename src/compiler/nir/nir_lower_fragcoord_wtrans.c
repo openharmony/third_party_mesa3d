openharmony/third_party_mesa3d

commit 4729c4915f8216a37bfce62b6af1a34ec36c7715

compiler/nir: extend lower_fragcoord_wtrans to support VARYING_SLOT_POS
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Andreas Baierl <ichgeh@imkreisrum.de>
Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6220>

src/compiler/nir/nir_lower_fragcoord_wtrans.c
@@ -30,8 +30,6 @@
  *    gl_FragCoord.xyz = gl_FragCoord_orig.xyz
  *    gl_FragCoord.w = 1.0 / gl_FragCoord_orig.w
  *
- * To trigger the transformation, gl_FragCoord currently has to be treated
- * as a system value with PIPE_CAP_TGSI_FS_POSITION_IS_SYSVAL enabled.
  */
 
 static bool
@@ -41,7 +39,14 @@ lower_fragcoord_wtrans_filter(const nir_instr *instr, UNUSED const void *_option
       return false;
 
    nir_intrinsic_instr *intr = nir_instr_as_intrinsic(instr);
-   return intr->intrinsic == nir_intrinsic_load_frag_coord;
+   if (intr->intrinsic == nir_intrinsic_load_frag_coord)
+      return true;
+
+   if (intr->intrinsic != nir_intrinsic_load_deref)
+      return false;
+
+   nir_variable *var = nir_intrinsic_get_var(intr, 0);
+   return var->data.location == VARYING_SLOT_POS;
 }
 
 static nir_ssa_def *

commit 022ab3e1ad7dc824408748511b354262cdaae04f

compiler/nir: check whether var is an input in lower_fragcoord_wtrans
Otherwise the lowering pass might try to lower any other load from
a deref if its data.location value happens to be zero.

Fixes: 4729c4915f8216a37bfce62b6af1a34ec36c7715
  compiler/nir: extend lower_fragcoord_wtrans to support VARYING_SLOT_POS

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10577>

src/compiler/nir/nir_lower_fragcoord_wtrans.c
@@ -46,6 +46,9 @@ lower_fragcoord_wtrans_filter(const nir_instr *instr, UNUSED const void *_option
       return false;
 
    nir_variable *var = nir_intrinsic_get_var(intr, 0);
+   if (var->data.mode != nir_var_shader_in)
+      return false;
+
    return var->data.location == VARYING_SLOT_POS;
 }
 