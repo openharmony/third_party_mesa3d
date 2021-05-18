openharmony/third_party_mesa3d

commit a17e2a033089ce99fd932500bf222aa482f7a85e

pan/mdg: properly encode/decode ldst instructions
Signed-off-by: Italo Nicola <italonicola@collabora.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9461>

src/panfrost/midgard/compiler.h
@@ -516,9 +516,9 @@ void mir_insert_instruction_after_scheduled(compiler_context *ctx, midgard_block
 void mir_flip(midgard_instruction *ins);
 void mir_compute_temp_count(compiler_context *ctx);
 
-#define LDST_GLOBAL 0x3E
-#define LDST_SHARED 0x2E
-#define LDST_SCRATCH 0x2A
+#define LDST_GLOBAL (REGISTER_LDST_ZERO << 2)
+#define LDST_SHARED ((REGISTER_LDST_LOCAL_STORAGE_PTR << 2) | COMPONENT_Z)
+#define LDST_SCRATCH ((REGISTER_LDST_PC_SP << 2) | COMPONENT_Z)
 
 void mir_set_offset(compiler_context *ctx, midgard_instruction *ins, nir_src *offset, unsigned seg);
 void mir_set_ubo_offset(midgard_instruction *ins, nir_src *src, unsigned bias);
@@ -573,8 +573,11 @@ v_load_store_scratch(
                 .op = is_store ? midgard_op_st_128 : midgard_op_ld_128,
                 .load_store = {
                         /* For register spilling - to thread local storage */
-                        .arg_1 = 0xEA,
-                        .arg_2 = 0x1E,
+                        .arg_reg = REGISTER_LDST_LOCAL_STORAGE_PTR,
+                        .arg_comp = COMPONENT_Z,
+                        .bitsize_toggle = true,
+                        .index_format = midgard_index_address_u32,
+                        .index_reg = REGISTER_LDST_ZERO,
                 },
 
                 /* If we spill an unspill, RA goes into an infinite loop */

src/panfrost/midgard/disassemble.c
@@ -304,6 +304,13 @@ static char *argmod_names[3] = {
         ".x2",
 };
 
+static char *index_format_names[4] = {
+        "",
+        ".u64",
+        ".u32",
+        ".s32"
+};
+
 static void
 print_outmod(FILE *fp, unsigned outmod, bool is_int)
 {
@@ -758,6 +765,21 @@ print_alu_mask(FILE *fp, uint8_t mask, unsigned bits, midgard_shrink_mode shrink
 
         if (tripped)
                 fprintf(fp, " /* %X */", mask);
+}
+
+/* TODO: 16-bit mode */
+static void
+print_ldst_mask(FILE *fp, unsigned mask, unsigned swizzle) {
+        fprintf(fp, ".");
+
+        for (unsigned i = 0; i < 4; ++i) {
+                bool write = (mask & (1 << i)) != 0;
+                unsigned c = (swizzle >> (i * 2)) & 3;
+                /* We can't omit the swizzle here since many ldst ops have a
+                 * combined swizzle/writemask, and it would be ambiguous to not
+                 * print the masked-out components. */
+                fprintf(fp, "%c", write ? components[c] : '~');
+        }
 }
 
 /* Prints the 4-bit masks found in texture and load/store ops, as opposed to
@@ -1235,41 +1257,40 @@ print_alu_word(FILE *fp, uint32_t *words, unsigned num_quad_words,
         return branch_forward;
 }
 
-static void
+/* TODO: how can we use this now that we know that these params can't be known
+ * before run time in every single case? Maybe just use it in the cases we can? */
+UNUSED static void
 print_varying_parameters(FILE *fp, midgard_load_store_word *word)
 {
-        midgard_varying_parameter param;
-        unsigned v = word->varying_parameters;
-        memcpy(&param, &v, sizeof(param));
+        midgard_varying_params p = midgard_unpack_varying_params(*word);
 
-        if (param.is_varying) {
-                /* If a varying, there are qualifiers */
-                if (param.flat)
-                        fprintf(fp, ".flat");
+        /* If a varying, there are qualifiers */
+        if (p.flat_shading)
+                fprintf(fp, ".flat");
 
-                if (param.interpolation != midgard_interp_default) {
-                        if (param.interpolation == midgard_interp_centroid)
-                                fprintf(fp, ".centroid");
-                        else if (param.interpolation == midgard_interp_sample)
-                                fprintf(fp, ".sample");
-                        else
-                                fprintf(fp, ".interp%d", param.interpolation);
-                }
+        if (p.perspective_correction)
+                fprintf(fp, ".correction");
 
-                if (param.modifier != midgard_varying_mod_none) {
-                        if (param.modifier == midgard_varying_mod_perspective_w)
-                                fprintf(fp, ".perspectivew");
-                        else if (param.modifier == midgard_varying_mod_perspective_z)
-                                fprintf(fp, ".perspectivez");
-                        else
-                                fprintf(fp, ".mod%d", param.modifier);
-                }
-        } else if (param.flat || param.interpolation || param.modifier) {
-                fprintf(fp, " /* is_varying not set but varying metadata attached */");
+        if (p.centroid_mapping)
+                fprintf(fp, ".centroid");
+
+        if (p.interpolate_sample)
+                fprintf(fp, ".sample");
+
+        switch (p.modifier) {
+                case midgard_varying_mod_perspective_y:
+                        fprintf(fp, ".perspectivey");
+                        break;
+                case midgard_varying_mod_perspective_z:
+                        fprintf(fp, ".perspectivez");
+                        break;
+                case midgard_varying_mod_perspective_w:
+                        fprintf(fp, ".perspectivew");
+                        break;
+                default:
+                        unreachable("invalid varying modifier");
+                        break;
         }
-
-        if (param.zero0 || param.zero1 || param.zero2)
-                fprintf(fp, " /* zero tripped, %u %u %u */ ", param.zero0, param.zero1, param.zero2);
 }
 
 static bool
@@ -1304,33 +1325,14 @@ is_op_attribute(unsigned op)
         return false;
 }
 
+/* Helper to print integer well-formatted, but only when non-zero. */
 static void
-print_load_store_arg(FILE *fp, uint8_t arg, unsigned index)
+midgard_print_sint(FILE *fp, int n)
 {
-        /* Try to interpret as a register */
-        midgard_ldst_register_select sel;
-        memcpy(&sel, &arg, sizeof(arg));
-
-        /* If unknown is set, we're not sure what this is or how to
-         * interpret it. But if it's zero, we get it. */
-
-        if (sel.unknown) {
-                fprintf(fp, "0x%02X", arg);
-                return;
-        }
-
-        print_ldst_read_reg(fp, sel.select);
-        fprintf(fp, ".%c", components[sel.component]);
-
-        /* Only print a shift if it's non-zero. Shifts only make sense for the
-         * second index. For the first, we're not sure what it means yet */
-
-        if (index == 1) {
-                if (sel.shift)
-                        fprintf(fp, " << %u", sel.shift);
-        } else {
-                fprintf(fp, " /* %X */", sel.shift);
-        }
+        if (n > 0)
+                fprintf(fp, " + 0x%X", n);
+        else if (n < 0)
+                fprintf(fp, " - 0x%X", -n);
 }
 
 static void
@@ -1347,63 +1349,160 @@ print_load_store_instr(FILE *fp, uint64_t data)
 
         print_ld_st_opcode(fp, word->op);
 
-        unsigned address = word->address;
+        if (word->op == midgard_op_trap) {
+                fprintf(fp, " 0x%X\n", word->signed_offset);
+                return;
+        }
+
+        /* Print opcode modifiers */
+
+        if (OP_USES_ATTRIB(word->op)) /* which attrib table? */
+                fprintf(fp, ".%s", (word->index_format >> 1) ? "secondary" : "primary");
+        else if (word->op == midgard_op_ld_cubemap_coords || OP_IS_PROJECTION(word->op))
+                fprintf(fp, ".%s", word->bitsize_toggle ? "f32" : "f16");
+
+        fprintf(fp, " ");
+
+        /* src/dest register */
+
+        if (!OP_IS_STORE(word->op)) {
+                print_ldst_write_reg(fp, word->reg);
+
+                /* Some opcodes don't have a swizzable src register, and
+                 * instead the swizzle is applied before the result is written
+                 * to the dest reg. For these ops, we combine the writemask
+                 * with the swizzle to display them in the disasm compactly. */
+                unsigned swizzle = word->swizzle;
+                if ((OP_IS_REG2REG_LDST(word->op) &&
+                        word->op != midgard_op_lea &&
+                        word->op != midgard_op_lea_image) || OP_IS_ATOMIC(word->op))
+                        swizzle = 0xE4;
+                print_ldst_mask(fp, word->mask, swizzle);
+        } else {
+                print_ldst_read_reg(fp, word->reg);
+                print_vec_swizzle(fp, word->swizzle, midgard_src_passthrough,
+                                  midgard_reg_mode_32, 0xFF);
+        }
+
+        /* ld_ubo args */
+        if (OP_IS_UBO_READ(word->op)) {
+                if (word->signed_offset & 1) { /* buffer index imm */
+                        unsigned imm = midgard_unpack_ubo_index_imm(*word);
+                        fprintf(fp, ", %u", imm);
+                } else { /* buffer index from reg */
+                        fprintf(fp, ", ");
+                        print_ldst_read_reg(fp, word->arg_reg);
+                        fprintf(fp, ".%c", components[word->arg_comp]);
+                }
+
+                fprintf(fp, ", ");
+                print_ldst_read_reg(fp, word->index_reg);
+                fprintf(fp, ".%c << %u", components[word->index_comp], word->index_shift);
+                midgard_print_sint(fp, UNPACK_LDST_UBO_OFS(word->signed_offset));
+        }
+
+        /* mem addr expression */
+        if (OP_HAS_ADDRESS(word->op)) {
+                fprintf(fp, ", [ ");
+                print_ldst_read_reg(fp, word->arg_reg);
+                fprintf(fp, ".u%d.%c",
+                        word->bitsize_toggle ? 64 : 32, components[word->arg_comp]);
+
+                if ((word->op < midgard_op_atomic_cmpxchg ||
+                     word->op > midgard_op_atomic_cmpxchg64_be) &&
+                     word->index_reg != 0x7) {
+                        fprintf(fp, " + (");
+                        print_ldst_read_reg(fp, word->index_reg);
+                        fprintf(fp, "%s.%c << %u)",
+                                index_format_names[word->index_format],
+                                components[word->index_comp], word->index_shift);
+                }
+
+                midgard_print_sint(fp, word->signed_offset);
+
+                fprintf(fp, " ]");
+        }
+
+        /* src reg for reg2reg ldst opcodes */
+        if (OP_IS_REG2REG_LDST(word->op)) {
+                fprintf(fp, ", ");
+                print_ldst_read_reg(fp, word->arg_reg);
+                print_vec_swizzle(fp, word->swizzle, midgard_src_passthrough,
+                                  midgard_reg_mode_32, 0xFF);
+        }
+
+        /* atomic ops encode the source arg where the ldst swizzle would be. */
+        if (OP_IS_ATOMIC(word->op)) {
+                unsigned src = (word->swizzle >> 2) & 0x7;
+                unsigned src_comp = word->swizzle & 0x3;
+                fprintf(fp, ", ");
+                print_ldst_read_reg(fp, src);
+                fprintf(fp, ".%c", components[src_comp]);
+        }
+
+        /* CMPXCHG encodes the extra comparison arg where the index reg would be. */
+        if (word->op >= midgard_op_atomic_cmpxchg &&
+            word->op <= midgard_op_atomic_cmpxchg64_be) {
+                fprintf(fp, ", ");
+                print_ldst_read_reg(fp, word->index_reg);
+                fprintf(fp, ".%c", components[word->index_comp]);
+        }
+
+        /* index reg for attr/vary/images, selector for ld/st_special */
+        if (OP_IS_SPECIAL(word->op) || OP_USES_ATTRIB(word->op)) {
+                fprintf(fp, ", ");
+                print_ldst_read_reg(fp, word->index_reg);
+                fprintf(fp, ".%c << %u", components[word->index_comp], word->index_shift);
+                midgard_print_sint(fp, UNPACK_LDST_ATTRIB_OFS(word->signed_offset));
+        }
+
+        /* vertex reg for attrib/varying ops, coord reg for image ops */
+        if (OP_USES_ATTRIB(word->op)) {
+                fprintf(fp, ", ");
+                print_ldst_read_reg(fp, word->arg_reg);
+
+                if (OP_IS_IMAGE(word->op))
+                        fprintf(fp, ".u%d", word->bitsize_toggle ? 64 : 32);
+
+                fprintf(fp, ".%c", components[word->arg_comp]);
+
+                if (word->bitsize_toggle && !OP_IS_IMAGE(word->op))
+                        midgard_print_sint(fp, UNPACK_LDST_VERTEX_OFS(word->signed_offset));
+        }
+
+        /* TODO: properly decode format specifier for PACK/UNPACK ops */
+        if (OP_IS_PACK_COLOUR(word->op) || OP_IS_UNPACK_COLOUR(word->op)) {
+                fprintf(fp, ", ");
+                unsigned format_specifier = (word->signed_offset << 4) | word->index_shift;
+                fprintf(fp, "0x%X", format_specifier);
+        }
+
+        fprintf(fp, "\n");
+
+        /* Debugging stuff */
 
         if (is_op_varying(word->op)) {
-                print_varying_parameters(fp, word);
+                /* Do some analysis: check if direct access */
 
-                /* Do some analysis: check if direct cacess */
-
-                if ((word->arg_2 == 0x1E) && midg_stats.varying_count >= 0)
-                        update_stats(&midg_stats.varying_count, address);
+                if (word->index_reg == 0x7 && midg_stats.varying_count >= 0)
+                        update_stats(&midg_stats.varying_count,
+                                     UNPACK_LDST_ATTRIB_OFS(word->signed_offset));
                 else
                         midg_stats.varying_count = -16;
         } else if (is_op_attribute(word->op)) {
-                if ((word->arg_2 == 0x1E) && midg_stats.attribute_count >= 0)
-                        update_stats(&midg_stats.attribute_count, address);
+                if (word->index_reg == 0x7 && midg_stats.attribute_count >= 0)
+                        update_stats(&midg_stats.attribute_count,
+                                     UNPACK_LDST_ATTRIB_OFS(word->signed_offset));
                 else
                         midg_stats.attribute_count = -16;
         }
 
-        fprintf(fp, " ");
-
-        if (!OP_IS_STORE(word->op))
-                print_ldst_write_reg(fp, word->reg);
-        else
-                print_ldst_read_reg(fp, word->reg);
-
-        print_mask_4(fp, word->mask, false);
-
         if (!OP_IS_STORE(word->op))
                 update_dest(word->reg);
 
-        bool is_ubo = OP_IS_UBO_READ(word->op);
-
-        if (is_ubo) {
-                /* UBOs use their own addressing scheme */
-
-                int lo = word->varying_parameters >> 7;
-                int hi = word->address;
-
-                /* TODO: Combine fields logically */
-                address = (hi << 3) | lo;
-        }
-
-        fprintf(fp, ", %u", address);
-
-        print_vec_swizzle(fp, word->swizzle, midgard_src_passthrough, midgard_reg_mode_32, 0xFF);
-
-        fprintf(fp, ", ");
-
-        if (is_ubo) {
-                fprintf(fp, "ubo%u", word->arg_1);
-                update_stats(&midg_stats.uniform_buffer_count, word->arg_1);
-        } else
-                print_load_store_arg(fp, word->arg_1, 0);
-
-        fprintf(fp, ", ");
-        print_load_store_arg(fp, word->arg_2, 1);
-        fprintf(fp, " /* %X */\n", word->varying_parameters);
+        if (OP_IS_UBO_READ(word->op))
+                update_stats(&midg_stats.uniform_buffer_count,
+                             UNPACK_LDST_UBO_OFS(word->signed_offset));
 
         midg_stats.instruction_count++;
 }

src/panfrost/midgard/helpers.h
@@ -355,9 +355,9 @@ mir_is_simple_swizzle(unsigned *swizzle, unsigned mask)
 /* Packs a load/store argument */
 
 static inline uint8_t
-midgard_ldst_reg(unsigned reg, unsigned component, unsigned size)
+midgard_ldst_comp(unsigned reg, unsigned component, unsigned size)
 {
-        assert((reg == REGISTER_LDST_BASE) || (reg == REGISTER_LDST_BASE + 1));
+        assert((reg & ~1) == 0);
         assert(size == 16 || size == 32 || size == 64);
 
         /* Shift so everything is in terms of 32-bit units */
@@ -369,16 +369,37 @@ midgard_ldst_reg(unsigned reg, unsigned component, unsigned size)
                 component >>= 1;
         }
 
-        midgard_ldst_register_select sel = {
-                .component = component,
-                .select = reg - 26
-        };
-
-        uint8_t packed;
-        memcpy(&packed, &sel, sizeof(packed));
-
-        return packed;
+        return component;
 }
+
+/* Packs/unpacks a ubo index immediate */
+
+void midgard_pack_ubo_index_imm(midgard_load_store_word *word, unsigned index);
+unsigned midgard_unpack_ubo_index_imm(midgard_load_store_word word);
+
+/* Packs/unpacks varying parameters.
+ * FIXME: IMPORTANT: We currently handle varying mode weirdly, by passing all
+ * parameters via an offset and using REGISTER_LDST_ZERO as base. This works
+ * for most parameters, but does not allow us to encode/decode direct sample
+ * position. */
+void midgard_pack_varying_params(midgard_load_store_word *word, midgard_varying_params p);
+midgard_varying_params midgard_unpack_varying_params(midgard_load_store_word word);
+
+/* Load/store ops' displacement helpers.
+ * This is useful because different types of load/store ops have different
+ * displacement bitsize. */
+
+#define UNPACK_LDST_ATTRIB_OFS(a) ((a) >> 9)
+#define UNPACK_LDST_VERTEX_OFS(a) util_sign_extend((a) & 0x1FF, 9)
+#define UNPACK_LDST_SELECTOR_OFS(a) ((a) >> 9)
+#define UNPACK_LDST_UBO_OFS(a) ((a) >> 2)
+#define UNPACK_LDST_MEM_OFS(a) ((a))
+
+#define PACK_LDST_ATTRIB_OFS(a) ((a) << 9)
+#define PACK_LDST_VERTEX_OFS(a) ((a) & 0x1FF)
+#define PACK_LDST_SELECTOR_OFS(a) ((a) << 9)
+#define PACK_LDST_UBO_OFS(a) ((a) << 2)
+#define PACK_LDST_MEM_OFS(a) ((a))
 
 static inline bool
 midgard_is_branch_unit(unsigned unit)

src/panfrost/midgard/midgard.h
@@ -664,31 +664,37 @@ typedef enum {
 typedef enum {
         midgard_varying_mod_none = 0,
 
-        /* Other values unknown */
-
-        /* Take the would-be result and divide all components by its z/w
+        /* Take the would-be result and divide all components by its y/z/w
          * (perspective division baked in with the load)  */
+        midgard_varying_mod_perspective_y = 1,
         midgard_varying_mod_perspective_z = 2,
         midgard_varying_mod_perspective_w = 3,
+
+        /* The result is a 64-bit cubemap descriptor to use with
+         * midgard_tex_op_normal or midgard_tex_op_gradient */
+        midgard_varying_mod_cubemap = 4,
 } midgard_varying_modifier;
 
 typedef struct
 __attribute__((__packed__))
 {
-        unsigned zero0 : 1; /* Always zero */
+        midgard_varying_modifier modifier : 3;
 
-        midgard_varying_modifier modifier : 2;
+        bool flat_shading : 1;
 
-        unsigned zero1: 1; /* Always zero */
+        /* These are ignored if flat_shading is enabled. */
+        bool perspective_correction : 1;
+        bool centroid_mapping : 1;
 
-        /* Varying qualifiers, zero if not a varying */
-        unsigned flat    : 1;
-        unsigned is_varying : 1; /* Always one for varying, but maybe something else? */
-        midgard_interpolation interpolation : 2;
+        /* This is ignored if the shader only runs once per pixel. */
+        bool interpolate_sample : 1;
 
-        unsigned zero2 : 2; /* Always zero */
+        bool zero0 : 1; /* Always zero */
+
+        unsigned direct_sample_pos_x : 4;
+        unsigned direct_sample_pos_y : 4;
 }
-midgard_varying_parameter;
+midgard_varying_params;
 
 /* 8-bit register/etc selector for load/store ops */
 typedef struct
@@ -711,26 +717,56 @@ __attribute__((__packed__))
 }
 midgard_ldst_register_select;
 
+typedef enum {
+        /* 0 is reserved */
+        midgard_index_address_u64 = 1,
+        midgard_index_address_u32 = 2,
+        midgard_index_address_s32 = 3,
+} midgard_index_address_format;
+
 typedef struct
 __attribute__((__packed__))
 {
         midgard_load_store_op op : 8;
-        unsigned reg     : 5;
-        unsigned mask    : 4;
+
+        /* Source/dest reg */
+        unsigned reg  : 5;
+
+        /* Generally is a writemask.
+         * For ST_ATTR and ST_TEX, unused.
+         * For other stores, each bit masks 1/4th of the output. */
+        unsigned mask : 4;
+
+        /* Swizzle for stores, but for atomics it encodes also the source
+         * register. This fits because atomics dont need a swizzle since they
+         * are not vectorized instructions. */
         unsigned swizzle : 8;
 
-        /* Load/store ops can take two additional registers as arguments, but
-         * these are limited to load/store registers with only a few supported
-         * mask/swizzle combinations. The tradeoff is these are much more
-         * compact, requiring 8-bits each rather than 17-bits for a full
-         * reg/mask/swizzle. Usually (?) encoded as
-         * midgard_ldst_register_select. */
-        unsigned arg_1   : 8;
-        unsigned arg_2   : 8;
+        /* Arg reg, meaning changes according to each opcode */
+        unsigned arg_comp : 2;
+        unsigned arg_reg  : 3;
 
-        unsigned varying_parameters : 10;
+        /* 64-bit address enable
+         * 32-bit data type enable for CUBEMAP and perspective div.
+         * Explicit indexing enable for LD_ATTR.
+         * 64-bit coordinate enable for LD_IMAGE. */
+        bool bitsize_toggle : 1;
 
-        unsigned address : 9;
+        /* These are mainly used for opcodes that have addresses.
+         * For cmpxchg, index_reg is used for the comparison value.
+         * For ops that access the attrib table, bit 1 encodes which table.
+         * For LD_VAR and LD/ST_ATTR, bit 0 enables dest/src type inferral. */
+        midgard_index_address_format index_format : 2;
+        unsigned index_comp  : 2;
+        unsigned index_reg   : 3;
+        unsigned index_shift : 4;
+
+        /* Generaly is a signed offset, but has different bitsize and starts at
+         * different bits depending on the opcode, LDST_*_DISPLACEMENT helpers
+         * are recommended when packing/unpacking this attribute.
+         * For LD_UBO, bit 0 enables ubo index immediate.
+         * For LD_TILEBUFFER_RAW, bit 0 disables sample index immediate. */
+        int signed_offset : 18;
 }
 midgard_load_store_word;
 

src/panfrost/midgard/midgard_address.c
@@ -36,17 +36,11 @@
  * This allows for fast indexing into arrays. This file tries to pattern match the offset in NIR with this form to reduce pressure on the ALU pipe.
  */
 
-enum index_type {
-        ITYPE_U64 = 1 << 6,
-        ITYPE_U32 = 2 << 6, // zero-extend
-        ITYPE_I32 = 3 << 6, // sign-extend
-};
-
 struct mir_address {
         nir_ssa_scalar A;
         nir_ssa_scalar B;
 
-        enum index_type type;
+        midgard_index_address_format type;
         unsigned shift;
         unsigned bias;
 };
@@ -136,7 +130,7 @@ mir_match_u2u64(struct mir_address *address)
         nir_ssa_scalar arg = nir_ssa_scalar_chase_alu_src(address->B, 0);
 
         address->B = arg;
-        address->type = ITYPE_U32;
+        address->type = midgard_index_address_u32;
 }
 
 /* Matches i2i64 and sets type */
@@ -155,7 +149,7 @@ mir_match_i2i64(struct mir_address *address)
         nir_ssa_scalar arg = nir_ssa_scalar_chase_alu_src(address->B, 0);
 
         address->B = arg;
-        address->type = ITYPE_I32;
+        address->type = midgard_index_address_s32;
 }
 
 /* Matches ishl to shift */
@@ -210,7 +204,7 @@ mir_match_offset(nir_ssa_def *offset, bool first_free, bool extend)
 {
         struct mir_address address = {
                 .B = { .def = offset },
-                .type = extend ? ITYPE_U64 : ITYPE_U32,
+                .type = extend ? midgard_index_address_u64 : midgard_index_address_u32,
         };
 
         mir_match_mov(&address);
@@ -243,14 +237,16 @@ mir_set_offset(compiler_context *ctx, midgard_instruction *ins, nir_src *offset,
         bool force_sext = (nir_src_bit_size(*offset) < 64);
 
         if (!offset->is_ssa) {
-                ins->load_store.arg_1 |= seg;
+                ins->load_store.bitsize_toggle = true;
+                ins->load_store.arg_comp = seg & 0x3;
+                ins->load_store.arg_reg = (seg >> 2) & 0x7;
                 ins->src[2] = nir_src_index(ctx, offset);
                 ins->src_types[2] = nir_type_uint | nir_src_bit_size(*offset);
 
                 if (force_sext)
-                        ins->load_store.arg_1 |= ITYPE_I32;
+                        ins->load_store.index_format = midgard_index_address_s32;
                 else
-                        ins->load_store.arg_1 |= ITYPE_U64;
+                        ins->load_store.index_format = midgard_index_address_u64;
 
                 return;
         }
@@ -263,23 +259,26 @@ mir_set_offset(compiler_context *ctx, midgard_instruction *ins, nir_src *offset,
                 ins->src[1] = nir_ssa_index(match.A.def);
                 ins->swizzle[1][0] = match.A.comp;
                 ins->src_types[1] = nir_type_uint | match.A.def->bit_size;
-        } else
-                ins->load_store.arg_1 |= seg;
+        } else {
+                ins->load_store.bitsize_toggle = true;
+                ins->load_store.arg_comp = seg & 0x3;
+                ins->load_store.arg_reg = (seg >> 2) & 0x7;
+        }
 
         if (match.B.def) {
                 ins->src[2] = nir_ssa_index(match.B.def);
                 ins->swizzle[2][0] = match.B.comp;
                 ins->src_types[2] = nir_type_uint | match.B.def->bit_size;
         } else
-                ins->load_store.arg_2 = 0x1E;
+                ins->load_store.index_reg = REGISTER_LDST_ZERO;
 
         if (force_sext)
-                match.type = ITYPE_I32;
+                match.type = midgard_index_address_s32;
 
-        ins->load_store.arg_1 |= match.type;
+        ins->load_store.index_format = match.type;
 
         assert(match.shift <= 7);
-        ins->load_store.arg_2 |= (match.shift) << 5;
+        ins->load_store.index_shift = match.shift;
 
         ins->constants.u32[0] = match.bias;
 }
@@ -298,6 +297,6 @@ mir_set_ubo_offset(midgard_instruction *ins, nir_src *src, unsigned bias)
                         ins->swizzle[2][i] = match.B.comp;
         }
 
-        ins->load_store.arg_2 |= (match.shift) << 5;
+        ins->load_store.index_shift = match.shift;
         ins->constants.u32[0] = match.bias + bias;
 }

commit 07c007417b6d05dd076f49456fe2ff1ff854af41

pan/mdg: Fix spills to TLS
LOCAL_STORAGE.zw is workgroup local memory, whereas LOCAL_STORAGE.xy is
thread local memory. Likewise PC_SP.zw is the stack pointer, which is
initialized to (LOCAL_STORAGE.zw + offset) but is modifiable by the
shader. Panfrost doesn't modify the s tack pointer, and the register
allocation logic assumes a zero offset, so let's always spill to thread
local memory = LOCAL_STORAGE.xy, as was intended by Italo's cleanup.

This is visible on any shader that spills. Compute shaders aren't
advertised yet, so WLS will be null, causing a fault like the following
(reproduced on Mali T860 with the glyphy trace):

[15634.148873] panfrost ff9a0000.gpu: Unhandled Page fault in AS0 at VA 0x0000000000000000
          Reason: TODO
          raw fault status: 0x70003C2
          decoded fault status: SLAVE FAULT
          exception type 0xC2: TRANSLATION_FAULT_LEVEL2
          access type 0x3: WRITE
          source id 0x700
[15634.658170] panfrost ff9a0000.gpu: gpu sched timeout, js=0, config=0x3300, status=0x8, head=0x31d4540,
tail=0x31d4540, sched_job=00000000e8101b2e

Fixes: a17e2a03308 ("pan/mdg: properly encode/decode ldst instructions")
Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10866>

src/panfrost/midgard/compiler.h
@@ -574,7 +574,7 @@ v_load_store_scratch(
                 .load_store = {
                         /* For register spilling - to thread local storage */
                         .arg_reg = REGISTER_LDST_LOCAL_STORAGE_PTR,
-                        .arg_comp = COMPONENT_Z,
+                        .arg_comp = COMPONENT_X,
                         .bitsize_toggle = true,
                         .index_format = midgard_index_address_u32,
                         .index_reg = REGISTER_LDST_ZERO,