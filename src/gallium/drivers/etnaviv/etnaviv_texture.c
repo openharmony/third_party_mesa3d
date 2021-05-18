openharmony/third_party_mesa3d

commit 0ae4f52aa1589ea8bd7baa79a0686ff6b244a152

etnaviv: handle NULL views in set_sampler_views
Passing NULL for the views parameter should be the same as passing an
array of NULL, according to the documentation. So let's respect that
detail.

This fixes a crash when using GALLIUM_HUD. The wrong handling of views
parameter was causing problems starting with
b30acabab4f ("gallium/hud: don't use cso_context to restore VBs, constbuf 0 and sampler views").

Cc: <mesa-stable@lists.freedesktop.org>
Fixes: b03a8c83a4e ("etnaviv: gallium driver for Vivante GPUs")
Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Reviewed-by: Lucas Stach <l.stach@pengutronix.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8670>

src/gallium/drivers/etnaviv/etnaviv_texture.c
@@ -247,8 +247,10 @@ set_sampler_views(struct etna_context *ctx, unsigned start, unsigned end,
    uint32_t prev_active_sampler_views = ctx->active_sampler_views;
 
    for (i = start, j = 0; j < nr; i++, j++, mask <<= 1) {
-      pipe_sampler_view_reference(&ctx->sampler_view[i], views[j]);
-      if (views[j]) {
+      struct pipe_sampler_view *view = views ? views[i] : NULL;
+
+      pipe_sampler_view_reference(&ctx->sampler_view[i], view);
+      if (view) {
          ctx->active_sampler_views |= mask;
          ctx->dirty_sampler_views |= mask;
       } else

commit 12ec228d337b5ddaf13d5562bfb5163cae597dac

etnaviv: fix vertex sampler setup
The start offset of the vertex samplers isn't zero, but the indexing of
the passed in views array is still zero based. Use the correct indexing
variable to fix vertex sampler setup.

Cc: <mesa-stable@lists.freedesktop.org>
Fixes: 0ae4f52aa158 ("etnaviv: handle NULL views in set_sampler_views")
Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
Reviewed-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10724>

src/gallium/drivers/etnaviv/etnaviv_texture.c
@@ -247,7 +247,7 @@ set_sampler_views(struct etna_context *ctx, unsigned start, unsigned end,
    uint32_t prev_active_sampler_views = ctx->active_sampler_views;
 
    for (i = start, j = 0; j < nr; i++, j++, mask <<= 1) {
-      struct pipe_sampler_view *view = views ? views[i] : NULL;
+      struct pipe_sampler_view *view = views ? views[j] : NULL;
 
       pipe_sampler_view_reference(&ctx->sampler_view[i], view);
       if (view) {