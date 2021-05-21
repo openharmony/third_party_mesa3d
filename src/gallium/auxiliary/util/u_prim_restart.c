openharmony/third_party_mesa3d

commit bf109569ff6fbf36dcc3fa8207853682ad150033

gallium/util: factor out primitive-restart rewriting logic
Reviewed-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5976>

src/gallium/auxiliary/util/u_prim_restart.c
@@ -57,6 +57,38 @@ read_indirect_elements(struct pipe_context *context, struct pipe_draw_indirect_i
    return ret;
 }
 
+void
+util_translate_prim_restart_data(unsigned index_size,
+                                 void *src_map, void *dst_map,
+                                 unsigned count, unsigned restart_index)
+{
+   if (index_size == 1) {
+      uint8_t *src = (uint8_t *) src_map;
+      uint16_t *dst = (uint16_t *) dst_map;
+      unsigned i;
+      for (i = 0; i < count; i++) {
+         dst[i] = (src[i] == restart_index) ? 0xffff : src[i];
+      }
+   }
+   else if (index_size == 2) {
+      uint16_t *src = (uint16_t *) src_map;
+      uint16_t *dst = (uint16_t *) dst_map;
+      unsigned i;
+      for (i = 0; i < count; i++) {
+         dst[i] = (src[i] == restart_index) ? 0xffff : src[i];
+      }
+   }
+   else {
+      uint32_t *src = (uint32_t *) src_map;
+      uint32_t *dst = (uint32_t *) dst_map;
+      unsigned i;
+      assert(index_size == 4);
+      for (i = 0; i < count; i++) {
+         dst[i] = (src[i] == restart_index) ? 0xffffffff : src[i];
+      }
+   }
+}
+
 /**
  * Translate an index buffer for primitive restart.
  * Create a new index buffer which is a copy of the original index buffer
@@ -113,32 +145,8 @@ util_translate_prim_restart_ib(struct pipe_context *context,
    if (!src_map)
       goto error;
 
-   if (src_index_size == 1 && dst_index_size == 2) {
-      uint8_t *src = (uint8_t *) src_map;
-      uint16_t *dst = (uint16_t *) dst_map;
-      unsigned i;
-      for (i = 0; i < count; i++) {
-         dst[i] = (src[i] == info->restart_index) ? 0xffff : src[i];
-      }
-   }
-   else if (src_index_size == 2 && dst_index_size == 2) {
-      uint16_t *src = (uint16_t *) src_map;
-      uint16_t *dst = (uint16_t *) dst_map;
-      unsigned i;
-      for (i = 0; i < count; i++) {
-         dst[i] = (src[i] == info->restart_index) ? 0xffff : src[i];
-      }
-   }
-   else {
-      uint32_t *src = (uint32_t *) src_map;
-      uint32_t *dst = (uint32_t *) dst_map;
-      unsigned i;
-      assert(src_index_size == 4);
-      assert(dst_index_size == 4);
-      for (i = 0; i < count; i++) {
-         dst[i] = (src[i] == info->restart_index) ? 0xffffffff : src[i];
-      }
-   }
+   util_translate_prim_restart_data(src_index_size, src_map, dst_map,
+                                    info->count, info->restart_index);
 
    if (src_transfer)
       pipe_buffer_unmap(context, src_transfer);

src/gallium/auxiliary/util/u_prim_restart.h
@@ -41,6 +41,10 @@ struct pipe_draw_info;
 union pipe_index_binding;
 struct pipe_resource;
 
+void
+util_translate_prim_restart_data(unsigned index_size,
+                                 void *src_map, void *dst_map,
+                                 unsigned count, unsigned restart_index);
 
 enum pipe_error
 util_translate_prim_restart_ib(struct pipe_context *context,

commit 4e5c892ac76ce6050307e63d4e616eb1c18476ba

util/prim_restart: fix util_translate_prim_restart_ib
this was broken for the indirect case if the indirect draw count or
firstIndex was nonzero and also would rewrite the index buffer onto the
wrong offset of the dst buffer

Fixes: bf109569ff6 ("gallium/util: factor out primitive-restart rewriting logic")
Fixes: b594759e8f8 ("gallium: remove pipe_index_buffer and set_index_buffer")

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10909>

src/gallium/auxiliary/util/u_prim_restart.c
@@ -125,13 +125,14 @@ util_translate_prim_restart_ib(struct pipe_context *context,
    /* Create new index buffer */
    *dst_buffer = pipe_buffer_create(screen, PIPE_BIND_INDEX_BUFFER,
                                     PIPE_USAGE_STREAM,
-                                    count * dst_index_size);
+                                    (start + count) * dst_index_size);
    if (!*dst_buffer)
       goto error;
 
    /* Map new / dest index buffer */
-   dst_map = pipe_buffer_map(context, *dst_buffer,
-                             PIPE_MAP_WRITE, &dst_transfer);
+   dst_map = pipe_buffer_map_range(context, *dst_buffer,
+                                   start * dst_index_size, count * dst_index_size,
+                                   PIPE_MAP_WRITE, &dst_transfer);
    if (!dst_map)
       goto error;
 
@@ -148,7 +149,7 @@ util_translate_prim_restart_ib(struct pipe_context *context,
       goto error;
 
    util_translate_prim_restart_data(src_index_size, src_map, dst_map,
-                                    draw->count, info->restart_index);
+                                    count, info->restart_index);
 
    if (src_transfer)
       pipe_buffer_unmap(context, src_transfer);