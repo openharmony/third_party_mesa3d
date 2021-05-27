openharmony/third_party_mesa3d

commit 914be9516b4b71a9509577d81017165fae1f7683

mesa/st: Fix iris regression with clip distances.
In general gallium shaders are all SSO and it's up to the driver to handle
lining up varying storage between stages at draw time.  However, there's a
NIR option "unify_interfaces" that iris uses which applies to non-SSO (as
indicated by nir->info.separate_shader) shaders and makes the inputs_read
and outputs_written match up at GLSL-to-NIR link time, and then iris then
avoids any lowering passes that would add new varyings.

By introducing info gathering after variant creation (because all I knew
was "gallium is always SSO"), I broke the unify_interfaces link-time setup
on iris.  Just skip that when the unify_interfaces flag is set, and add
some asserts to catch anyone trying to mix unify_interfaces with known
varying-adjusting lowering passes.

Closes: #4450
Reviewed-by: Dave Airlie <airlied@redhat.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10876>

src/mesa/state_tracker/st_program.c
@@ -774,11 +774,14 @@ st_create_common_variant(struct st_context *st,
 
       state.type = PIPE_SHADER_IR_NIR;
       state.ir.nir = get_nir_shader(st, stp);
+      const nir_shader_compiler_options *options = ((nir_shader *)state.ir.nir)->options;
+
       if (key->clamp_color) {
          NIR_PASS_V(state.ir.nir, nir_lower_clamp_color_outputs);
          finalize = true;
       }
       if (key->passthrough_edgeflags) {
+         assert(!options->unify_interfaces);
          NIR_PASS_V(state.ir.nir, nir_lower_passthrough_edgeflags);
          finalize = true;
       }
@@ -806,6 +809,7 @@ st_create_common_variant(struct st_context *st,
       }
 
       if (key->lower_ucp) {
+         assert(!options->unify_interfaces);
          lower_ucp(st, state.ir.nir, key->lower_ucp, params);
          finalize = true;
       }
@@ -823,9 +827,18 @@ st_create_common_variant(struct st_context *st,
          st_finalize_nir(st, &stp->Base, stp->shader_program, state.ir.nir,
                          true, false);
 
-         /* Some of the lowering above may have introduced new varyings */
-         nir_shader_gather_info(state.ir.nir,
-                                nir_shader_get_entrypoint(state.ir.nir));
+         /* Clip lowering and edgeflags may have introduced new varyings, so
+          * update the inputs_read/outputs_written. However, with
+          * unify_interfaces set (aka iris) the non-SSO varyings layout is
+          * decided at link time with outputs_written updated so the two line
+          * up.  A driver with this flag set may not use any of the lowering
+          * passes that would change the varyings, so skip to make sure we don't
+          * break its linkage.
+          */
+         if (!options->unify_interfaces) {
+            nir_shader_gather_info(state.ir.nir,
+                                   nir_shader_get_entrypoint(state.ir.nir));
+         }
       }
 
       if (key->is_draw_shader)

commit 6caf4e6c176a99d4aa83348ddc7f10bc931243c1

mesa/st: Don't assert !unify_interfaces in the passthrough edge flags case
Technically, this is correct, and I think it's the right long-term
solution.  However, Iris has code that undoes the damage caused by
nir_lower_passthrough_edgeflags (see iris_fix_edge_flags in
iris_program.c), so it should be safe to do the lowering here.

I'm not marking this as closing mesa#4838 because I think we should move
the Iris code up to here, make a different version of the NIR pass, or
something different... to properly fix this problem.  In the mean time,
this gets a bunch of tests to stop crashing. :)

Fixes: 914be9516b4 ("mesa/st: Fix iris regression with clip distances.")
Acked-by: Emma Anholt <emma@anholt.net>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11013>

src/mesa/state_tracker/st_program.c
@@ -781,7 +781,6 @@ st_create_common_variant(struct st_context *st,
          finalize = true;
       }
       if (key->passthrough_edgeflags) {
-         assert(!options->unify_interfaces);
          NIR_PASS_V(state.ir.nir, nir_lower_passthrough_edgeflags);
          finalize = true;
       }