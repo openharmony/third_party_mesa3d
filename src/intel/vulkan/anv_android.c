openharmony/third_party_mesa3d

commit ff631f262e871f4a89bb3406b5c91371ca18b7a6

anv: fix AHB leak upon exportable allocation
A successful AHardwareBuffer_allocate itself will increase a refcount on
the newly allocated AHB. For the import case, the implementation must
acquire a reference on the AHB. So if we layer the exportable allocation
on top of AHB allocation and AHB import, we must release an AHB
reference to avoid leak.

Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10940>

src/intel/vulkan/anv_android.c
@@ -437,8 +437,15 @@ anv_create_ahw_memory(VkDevice device_h,
    if (AHardwareBuffer_allocate(&desc, &ahw) != 0)
       return VK_ERROR_OUT_OF_HOST_MEMORY;
 
-   mem->ahw = ahw;
-   return VK_SUCCESS;
+   const VkImportAndroidHardwareBufferInfoANDROID import_info = {
+      .buffer = ahw,
+   };
+   result = anv_import_ahw_memory(device_h, mem, &import_info);
+
+   /* Release a reference to avoid leak for AHB allocation. */
+   AHardwareBuffer_release(ahw);
+
+   return result;
 #else
    return VK_ERROR_EXTENSION_NOT_PRESENT;
 #endif

src/intel/vulkan/anv_device.c
@@ -3828,13 +3828,6 @@ VkResult anv_AllocateMemory(
       if (result != VK_SUCCESS)
          goto fail;
 
-      const VkImportAndroidHardwareBufferInfoANDROID import_info = {
-         .buffer = mem->ahw,
-      };
-      result = anv_import_ahw_memory(_device, mem, &import_info);
-      if (result != VK_SUCCESS)
-         goto fail;
-
       goto success;
    }
 

commit a3c8b6d99c99bfb82c3344a6d5e5e0a5c9ba29ca

anv_android: Add missing type
Fixes: ff631f262e87 ("anv: fix AHB leak upon exportable allocation")
Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11068>

src/intel/vulkan/anv_android.c
@@ -440,7 +440,7 @@ anv_create_ahw_memory(VkDevice device_h,
    const VkImportAndroidHardwareBufferInfoANDROID import_info = {
       .buffer = ahw,
    };
-   result = anv_import_ahw_memory(device_h, mem, &import_info);
+   VkResult result = anv_import_ahw_memory(device_h, mem, &import_info);
 
    /* Release a reference to avoid leak for AHB allocation. */
    AHardwareBuffer_release(ahw);