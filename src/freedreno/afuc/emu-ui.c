openharmony/third_party_mesa3d

commit 9fe23328a25a457d623fd07c7b4766f519f95289

freedreno/afuc: Add emulator mode to afuc-disasm
This is an (at least somewhat complete) logical emulator of the a6xx SQE
that lets us step through firmware execution (bootstrap, cmdstream pkt
handling, etc).  It lets us poke at various fw visible state and run
through pm4 packet(s) to better understand what the fw is doing when it
handles various packets.

Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10944>

src/freedreno/afuc/emu-ui.c
@@ -1,0 +1,531 @@
+/*
+ * Copyright © 2021 Google, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <assert.h>
+#include <ctype.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include "freedreno_pm4.h"
+
+#include "emu.h"
+#include "util.h"
+
+/*
+ * Emulator User Interface:
+ *
+ * Handles the user prompts and input parsing.
+ */
+
+static void
+clear_line(void)
+{
+   if (!isatty(STDOUT_FILENO))
+      return;
+   printf("\r                                                           \r");
+}
+
+static int
+readchar(void)
+{
+   static struct termios saved_termios, unbuffered_termios;
+   int c;
+
+   fflush(stdout);
+
+   tcgetattr(STDIN_FILENO, &saved_termios);
+   unbuffered_termios = saved_termios;
+   cfmakeraw(&unbuffered_termios);
+
+   tcsetattr(STDIN_FILENO, TCSANOW, &unbuffered_termios);
+   do {
+      c = getchar();
+   } while (isspace(c));
+   tcsetattr(STDIN_FILENO, TCSANOW, &saved_termios);
+
+   /* TODO, read from script until EOF and then read from stdin: */
+   if (c == -1)
+      exit(0);
+
+   return c;
+}
+
+static const char *
+extract_string(char **buf)
+{
+   char *p = *buf;
+
+   /* eat any leading whitespace: */
+   while (*p && isspace(*p))
+      p++;
+
+   if (!*p)
+      return NULL;
+
+   char *ret = p;
+
+   /* skip to next whitespace: */
+   while (*p && !isspace(*p))
+      p++;
+
+   if (*p)
+      *p = '\0';
+
+   *buf = ++p;
+
+   return ret;
+}
+
+static size_t
+readline(char **p)
+{
+   static char *buf;
+   static size_t n;
+
+   ssize_t ret = getline(&buf, &n, stdin);
+   if (ret < 0)
+      return ret;
+
+   *p = buf;
+   return 0;
+}
+
+static ssize_t
+read_two_values(const char **val1, const char **val2)
+{
+   char *p;
+
+   ssize_t ret = readline(&p);
+   if (ret < 0)
+      return ret;
+
+   *val1 = extract_string(&p);
+   *val2 = extract_string(&p);
+
+   return 0;
+}
+
+static ssize_t
+read_one_value(const char **val)
+{
+   char *p;
+
+   ssize_t ret = readline(&p);
+   if (ret < 0)
+      return ret;
+
+   *val = extract_string(&p);
+
+   return 0;
+}
+
+static void
+dump_gpr_register(struct emu *emu, unsigned n)
+{
+   printf("              GPR:  ");
+   print_dst(n);
+   printf(": ");
+   if (BITSET_TEST(emu->gpr_regs.written, n)) {
+      printdelta("%08x\n", emu->gpr_regs.val[n]);
+   } else {
+      printf("%08x\n", emu->gpr_regs.val[n]);
+   }
+}
+
+static void
+dump_gpr_registers(struct emu *emu)
+{
+   for (unsigned i = 0; i < ARRAY_SIZE(emu->gpr_regs.val); i++) {
+      dump_gpr_register(emu, i);
+   }
+}
+
+static void
+dump_gpu_register(struct emu *emu, unsigned n)
+{
+   printf("              GPU:  ");
+   char *name = afuc_gpu_reg_name(n);
+   if (name) {
+      printf("%s", name);
+      free(name);
+   } else {
+      printf("0x%04x", n);
+   }
+   printf(": ");
+   if (BITSET_TEST(emu->gpu_regs.written, n)) {
+      printdelta("%08x\n", emu->gpu_regs.val[n]);
+   } else {
+      printf("%08x\n", emu->gpu_regs.val[n]);
+   }
+}
+
+static void
+dump_pipe_register(struct emu *emu, unsigned n)
+{
+   printf("              PIPE: ");
+   print_pipe_reg(n);
+   printf(": ");
+   if (BITSET_TEST(emu->pipe_regs.written, n)) {
+      printdelta("%08x\n", emu->pipe_regs.val[n]);
+   } else {
+      printf("%08x\n", emu->pipe_regs.val[n]);
+   }
+}
+
+static void
+dump_control_register(struct emu *emu, unsigned n)
+{
+   printf("              CTRL: ");
+   print_control_reg(n);
+   printf(": ");
+   if (BITSET_TEST(emu->control_regs.written, n)) {
+      printdelta("%08x\n", emu->control_regs.val[n]);
+   } else {
+      printf("%08x\n", emu->control_regs.val[n]);
+   }
+}
+
+static void
+dump_gpumem(struct emu *emu, uintptr_t addr)
+{
+   uint32_t val = emu_mem_read_dword(emu, addr);
+
+   printf("              MEM:  0x%016"PRIx64": ", addr);
+   if (addr == emu->gpumem_written) {
+      printdelta("0x%08x\n", val);
+   } else {
+      printf("0x%08x\n", val);
+   }
+}
+
+static void
+emu_write_gpr_prompt(struct emu *emu)
+{
+   clear_line();
+   printf("    GPR register (name or offset) and value: ");
+
+   const char *name;
+   const char *value;
+
+   if (read_two_values(&name, &value))
+      return;
+
+   unsigned offset = afuc_gpr_reg(name);
+   uint32_t val = strtoul(value, NULL, 0);
+
+   emu_set_gpr_reg(emu, offset, val);
+}
+
+static void
+emu_write_control_prompt(struct emu *emu)
+{
+   clear_line();
+   printf("    Control register (name or offset) and value: ");
+
+   const char *name;
+   const char *value;
+
+   if (read_two_values(&name, &value))
+      return;
+
+   unsigned offset = afuc_control_reg(name);
+   uint32_t val = strtoul(value, NULL, 0);
+
+   emu_set_control_reg(emu, offset, val);
+}
+
+static void
+emu_dump_control_prompt(struct emu *emu)
+{
+   clear_line();
+   printf("    Control register (name or offset): ");
+
+   const char *name;
+
+   if (read_one_value(&name))
+      return;
+
+   printf("\n");
+
+   unsigned offset = afuc_control_reg(name);
+   dump_control_register(emu, offset);
+}
+
+static void
+emu_write_gpu_prompt(struct emu *emu)
+{
+   clear_line();
+   printf("    GPU register (name or offset) and value: ");
+
+   const char *name;
+   const char *value;
+
+   if (read_two_values(&name, &value))
+      return;
+
+   unsigned offset = afuc_gpu_reg(name);
+   uint32_t val = strtoul(value, NULL, 0);
+
+   emu_set_gpu_reg(emu, offset, val);
+}
+
+static void
+emu_dump_gpu_prompt(struct emu *emu)
+{
+   clear_line();
+   printf("    GPU register (name or offset): ");
+
+   const char *name;
+
+   if (read_one_value(&name))
+      return;
+
+   printf("\n");
+
+   unsigned offset = afuc_gpu_reg(name);
+   dump_gpu_register(emu, offset);
+}
+
+static void
+emu_write_mem_prompt(struct emu *emu)
+{
+   clear_line();
+   printf("    GPU memory offset and value: ");
+
+   const char *offset;
+   const char *value;
+
+   if (read_two_values(&offset, &value))
+      return;
+
+   uintptr_t addr = strtoull(offset, NULL, 0);
+   uint32_t val = strtoul(value, NULL, 0);
+
+   emu_mem_write_dword(emu, addr, val);
+}
+
+static void
+emu_dump_mem_prompt(struct emu *emu)
+{
+   clear_line();
+   printf("    GPU memory offset: ");
+
+   const char *offset;
+
+   if (read_one_value(&offset))
+      return;
+
+   printf("\n");
+
+   uintptr_t addr = strtoull(offset, NULL, 0);
+   dump_gpumem(emu, addr);
+}
+
+static void
+emu_dump_prompt(struct emu *emu)
+{
+   do {
+      clear_line();
+      printf("  dump: GPR (r)egisters, (c)ontrol register, (g)pu register, (m)emory: ");
+
+      int c = readchar();
+      printf("%c\n", c);
+
+      if (c == 'r') {
+         /* Since there aren't too many GPR registers, just dump
+          * them all:
+          */
+         dump_gpr_registers(emu);
+         break;
+      } else if (c == 'c') {
+         emu_dump_control_prompt(emu);
+         break;
+      } else if (c == 'g') {
+         emu_dump_gpu_prompt(emu);
+         break;
+      } else if (c == 'm') {
+         emu_dump_mem_prompt(emu);
+         break;
+      } else {
+         printf("invalid option: '%c'\n", c);
+         break;
+      }
+   } while (true);
+}
+
+static void
+emu_write_prompt(struct emu *emu)
+{
+   do {
+      clear_line();
+      printf("  write: GPR (r)egister, (c)ontrol register, (g)pu register, (m)emory: ");
+
+      int c = readchar();
+      printf("%c\n", c);
+
+      if (c == 'r') {
+         emu_write_gpr_prompt(emu);
+         break;
+      } else if (c == 'c') {
+         emu_write_control_prompt(emu);
+         break;
+      } else if (c == 'g') {
+         emu_write_gpu_prompt(emu);
+         break;
+      } else if (c == 'm') {
+         emu_write_mem_prompt(emu);
+         break;
+      } else {
+         printf("invalid option: '%c'\n", c);
+         break;
+      }
+   } while (true);
+}
+
+static void
+emu_packet_prompt(struct emu *emu)
+{
+   clear_line();
+   printf("  Enter packet (opc or register name), followed by payload: ");
+   fflush(stdout);
+
+   char *p;
+   if (readline(&p) < 0)
+      return;
+
+   printf("\n");
+
+   const char *name = extract_string(&p);
+
+   /* Read the payload, so we can know the size to generate correct header: */
+   uint32_t payload[0x7f];
+   unsigned cnt = 0;
+
+   do {
+      const char *val = extract_string(&p);
+      if (!val)
+         break;
+
+      assert(cnt < ARRAY_SIZE(payload));
+      payload[cnt++] = strtoul(val, NULL, 0);
+   } while (true);
+
+   uint32_t hdr;
+   if (afuc_pm4_id(name) >= 0) {
+      unsigned opcode = afuc_pm4_id(name);
+      hdr = pm4_pkt7_hdr(opcode, cnt);
+   } else {
+      unsigned regindx = afuc_gpu_reg(name);
+      hdr = pm4_pkt4_hdr(regindx, cnt);
+   }
+
+   ASSERTED bool ret = emu_queue_push(&emu->roq, hdr);
+   assert(ret);
+
+   for (unsigned i = 0; i < cnt; i++) {
+      ASSERTED bool ret = emu_queue_push(&emu->roq, payload[i]);
+      assert(ret);
+   }
+}
+
+void
+emu_main_prompt(struct emu *emu)
+{
+   if (emu->run_mode)
+      return;
+
+   do {
+      clear_line();
+      printf("(s)tep, (r)un, (d)ump, (w)rite, (p)acket, (h)elp, (q)uit: ");
+
+      int c = readchar();
+
+      printf("%c\n", c);
+
+      if (c == 's') {
+         break;
+      } else if (c == 'r') {
+         emu->run_mode = true;
+         break;
+      } else if (c == 'd') {
+         emu_dump_prompt(emu);
+      } else if (c == 'w') {
+         emu_write_prompt(emu);
+      } else if (c == 'p') {
+         emu_packet_prompt(emu);
+      } else if (c == 'h') {
+         printf("  (s)tep   - single step to next instruction\n");
+         printf("  (r)un    - run until next waitin\n");
+         printf("  (d)ump   - dump memory/register menu\n");
+         printf("  (w)rite  - write memory/register menu\n");
+         printf("  (p)acket - inject a pm4 packet\n");
+         printf("  (h)elp   - show this usage message\n");
+         printf("  (q)uit   - exit emulator\n");
+      } else if (c == 'q') {
+         printf("\n");
+         exit(0);
+      } else {
+         printf("invalid option: '%c'\n", c);
+      }
+   } while (true);
+}
+
+void
+emu_clear_state_change(struct emu *emu)
+{
+   memset(emu->control_regs.written, 0, sizeof(emu->control_regs.written));
+   memset(emu->pipe_regs.written,    0, sizeof(emu->pipe_regs.written));
+   memset(emu->gpu_regs.written,     0, sizeof(emu->gpu_regs.written));
+   memset(emu->gpr_regs.written,     0, sizeof(emu->gpr_regs.written));
+   emu->gpumem_written = ~0;
+}
+
+void
+emu_dump_state_change(struct emu *emu)
+{
+   unsigned i;
+
+   /* Print the GPRs that changed: */
+   BITSET_FOREACH_SET (i, emu->gpr_regs.written, EMU_NUM_GPR_REGS) {
+      dump_gpr_register(emu, i);
+   }
+
+   BITSET_FOREACH_SET (i, emu->gpu_regs.written, EMU_NUM_GPU_REGS) {
+      dump_gpu_register(emu, i);
+   }
+
+   BITSET_FOREACH_SET (i, emu->pipe_regs.written, EMU_NUM_PIPE_REGS) {
+      dump_pipe_register(emu, i);
+   }
+
+   BITSET_FOREACH_SET (i, emu->control_regs.written, EMU_NUM_CONTROL_REGS) {
+      dump_control_register(emu, i);
+   }
+
+   if (emu->gpumem_written != ~0) {
+      dump_gpumem(emu, emu->gpumem_written);
+   }
+}

src/freedreno/afuc/afuc.h
@@ -24,6 +24,8 @@
 #ifndef _AFUC_H_
 #define _AFUC_H_
 
+#include <stdbool.h>
+
 #include "util/macros.h"
 
 /*
@@ -164,6 +166,16 @@ typedef union PACKED {
    } alu;
    struct PACKED {
       uint32_t uimm : 12;
+      /* TODO this needs to be confirmed:
+       *
+       * flags:
+       *   0x4 - post-increment src2 by uimm (need to confirm this is also
+       *         true for load/cread).  TBD whether, when used in conjunction
+       *         with @LOAD_STORE_HI, 32b rollover works properly.
+       *
+       * other values tbd, also need to confirm if different bits can be
+       * set together (I don't see examples of this in existing fw)
+       */
       uint32_t flags : 4;
       uint32_t src1 : 5; /* dst (cread) or src (cwrite) register */
       uint32_t src2 : 5; /* read or write address is src2+uimm */
@@ -217,5 +229,10 @@ afuc_set_opc(afuc_instr *ai, afuc_opc opc, bool rep)
       ai->opc_r = opc;
    }
 }
+
+void print_src(unsigned reg);
+void print_dst(unsigned reg);
+void print_control_reg(uint32_t id);
+void print_pipe_reg(uint32_t id);
 
 #endif /* _AFUC_H_ */

src/freedreno/afuc/disasm.c
@@ -39,6 +39,7 @@
 
 #include "afuc.h"
 #include "util.h"
+#include "emu.h"
 
 static int gpuver;
 
@@ -47,6 +48,9 @@ static int gpuver;
  * (like unexpected bits that are set)
  */
 static bool verbose = false;
+
+/* emulator mode: */
+static bool emulator = false;
 
 static void
 print_gpu_reg(uint32_t regbase)
@@ -64,7 +68,7 @@ print_gpu_reg(uint32_t regbase)
 #define printerr(fmt, ...) afuc_printc(AFUC_ERR, fmt, ##__VA_ARGS__)
 #define printlbl(fmt, ...) afuc_printc(AFUC_LBL, fmt, ##__VA_ARGS__)
 
-static void
+void
 print_src(unsigned reg)
 {
    if (reg == REG_REM)
@@ -79,7 +83,7 @@ print_src(unsigned reg)
       printf("$%02x", reg);
 }
 
-static void
+void
 print_dst(unsigned reg)
 {
    if (reg == REG_REM)
@@ -257,7 +261,7 @@ fxn_name(uint32_t offset)
    return name;
 }
 
-static void
+void
 print_control_reg(uint32_t id)
 {
    char *name = afuc_control_reg_name(id);
@@ -269,7 +273,7 @@ print_control_reg(uint32_t id)
    }
 }
 
-static void
+void
 print_pipe_reg(uint32_t id)
 {
    char *name = afuc_pipe_reg_name(id);
@@ -752,6 +756,20 @@ disasm(uint32_t *buf, int sizedwords)
       }
    }
 
+   if (emulator) {
+      struct emu state = {
+            .instrs = instrs,
+            .sizedwords = sizedwords,
+      };
+
+      emu_init(&state);
+
+      while (true) {
+         disasm_instr(instrs, state.gpr_regs.pc);
+         emu_step(&state);
+      }
+   }
+
    /* print instructions: */
    for (i = 0; i < jmptbl_start; i++) {
       disasm_instr(instrs, i);
@@ -784,7 +802,8 @@ usage(void)
                    "\tdisasm [-g GPUVER] [-v] [-c] filename.asm\n"
                    "\t\t-g - specify GPU version (5, etc)\n"
                    "\t\t-c - use colors\n"
-                   "\t\t-v - verbose output\n");
+                   "\t\t-v - verbose output\n"
+                   "\t\t-e - emulator mode\n");
    exit(2);
 }
 
@@ -798,7 +817,7 @@ main(int argc, char **argv)
    int c, ret;
 
    /* Argument parsing: */
-   while ((c = getopt(argc, argv, "g:vc")) != -1) {
+   while ((c = getopt(argc, argv, "g:vce")) != -1) {
       switch (c) {
       case 'g':
          gpuver = atoi(optarg);
@@ -808,6 +827,10 @@ main(int argc, char **argv)
          break;
       case 'c':
          colors = true;
+         break;
+      case 'e':
+         emulator = true;
+         verbose  = true;
          break;
       default:
          usage();
@@ -828,6 +851,15 @@ main(int argc, char **argv)
       } else if (strstr(file, "a6")) {
          gpuver = 6;
       }
+   }
+
+   /* a6xx is *mostly* a superset of a5xx, but some opcodes shuffle
+    * around, and behavior of special regs is a bit different.  Right
+    * now we only bother to support the a6xx variant.
+    */
+   if (emulator && (gpuver != 6)) {
+      fprintf(stderr, "Emulator only supported on a6xx!\n");
+      return 1;
    }
 
    ret = afuc_util_init(gpuver, colors);

src/freedreno/afuc/emu-ds.c
@@ -1,0 +1,91 @@
+/*
+ * Copyright © 2021 Google, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <assert.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "emu.h"
+#include "util.h"
+
+/*
+ * Emulation for draw-state (ie. CP_SET_DRAW_STATE) related control registers:
+ */
+
+EMU_CONTROL_REG(DRAW_STATE_SET);
+EMU_CONTROL_REG(DRAW_STATE_SEL);
+EMU_CONTROL_REG(DRAW_STATE_ACTIVE_BITMASK);
+EMU_CONTROL_REG(DRAW_STATE_HDR);
+EMU_CONTROL_REG(DRAW_STATE_BASE);
+EMU_CONTROL_REG(SDS_BASE);
+EMU_CONTROL_REG(SDS_DWORDS);
+
+uint32_t
+emu_get_draw_state_reg(struct emu *emu, unsigned n)
+{
+   // TODO maybe we don't need to do anything here
+   return emu->control_regs.val[n];
+}
+
+void
+emu_set_draw_state_reg(struct emu *emu, unsigned n, uint32_t val)
+{
+   struct emu_draw_state *ds = &emu->draw_state;
+   unsigned cur_idx = emu_get_reg32(emu, &DRAW_STATE_SEL);
+
+   if (n == emu_reg_offset(&DRAW_STATE_SET)) {
+      if (ds->write_idx == 0) {
+         cur_idx = (val >> 24) & 0x1f;
+         ds->state[cur_idx].count = val & 0xffff;
+         ds->state[cur_idx].mode_mask = (val >> 20) & 0x7;
+
+         unsigned active_mask = emu_get_reg32(emu, &DRAW_STATE_ACTIVE_BITMASK);
+         active_mask |= (1 << cur_idx);
+
+         emu_set_reg32(emu, &DRAW_STATE_ACTIVE_BITMASK, active_mask);
+         emu_set_reg32(emu, &DRAW_STATE_SEL, cur_idx);
+      } else {
+         ds->state[cur_idx].base_lohi[ds->write_idx - 1] = val;
+      }
+
+      ds->write_idx = (ds->write_idx + 1) % 3;
+   } else if (n == emu_reg_offset(&DRAW_STATE_SEL)) {
+      emu_set_reg32(emu, &DRAW_STATE_HDR, ds->state[val].hdr);
+      emu_set_reg64(emu, &DRAW_STATE_BASE, ds->state[val].base);
+
+      /* It seems that SDS_BASE/SDS_DWORDS is also per draw-state group,
+       * and that when a new state-group is selected, SQE compares to
+       * the previous values to new DRAW_STATE_BASE & count to detect
+       * that new state has been appended to existing draw-state group:
+       */
+      unsigned prev_idx = ds->prev_draw_state_sel;
+      ds->state[prev_idx].sds_base = emu_get_reg64(emu, &SDS_BASE);
+      ds->state[prev_idx].sds_dwords = emu_get_reg32(emu, &SDS_DWORDS);
+
+      emu_set_reg64(emu, &SDS_BASE, ds->state[val].sds_base);
+      emu_set_reg32(emu, &SDS_DWORDS, ds->state[val].sds_dwords);
+
+      ds->prev_draw_state_sel = val;
+   }
+}

commit b945af1c99eefe7f747031d877850de1f7949058

freedreno/afuc: Print uintptr_t with PRIxPTR
Fixes a compilation error on 32-bit.

Fixes: 9fe23328a25 ("freedreno/afuc: Add emulator mode to afuc-disasm")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11118>

src/freedreno/afuc/emu-ui.c
@@ -213,7 +213,7 @@ dump_gpumem(struct emu *emu, uintptr_t addr)
 {
    uint32_t val = emu_mem_read_dword(emu, addr);
 
-   printf("              MEM:  0x%016"PRIx64": ", addr);
+   printf("              MEM:  0x%016"PRIxPTR": ", addr);
    if (addr == emu->gpumem_written) {
       printdelta("0x%08x\n", val);
    } else {