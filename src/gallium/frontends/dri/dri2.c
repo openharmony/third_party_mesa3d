openharmony/third_party_mesa3d

commit 0a20b7b281fc4dcca4ab4d461a6f3e6e2ded4d13

driconf: add disable_protected_content_check option
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5096>

src/gallium/frontends/dri/dri2.c
@@ -833,7 +833,9 @@ dri2_create_image_from_winsys(__DRIscreen *_screen,
       /* Reject image creation if there's an inconsistency between
        * content protection status of tex and img.
        */
-      if ((tex->bind & PIPE_BIND_PROTECTED) != is_protected_content) {
+      const struct driOptionCache *optionCache = &screen->dev->option_cache;
+      if (!driQueryOptionb(optionCache, "disable_protected_content_check") &&
+          (tex->bind & PIPE_BIND_PROTECTED) != is_protected_content) {
          pipe_resource_reference(&img->texture, NULL);
          pipe_resource_reference(&tex, NULL);
          FREE(img);

src/gallium/auxiliary/pipe-loader/driinfo_gallium.h
@@ -39,6 +39,7 @@ DRI_CONF_SECTION_DEBUG
    DRI_CONF_OVERRIDE_VRAM_SIZE()
    DRI_CONF_GLX_EXTENSION_OVERRIDE()
    DRI_CONF_INDIRECT_GL_EXTENSION_OVERRIDE()
+   DRI_CONF_DISABLE_PROTECTED_CONTENT_CHECK(false)
 DRI_CONF_SECTION_END
 
 DRI_CONF_SECTION_MISCELLANEOUS

src/util/driconf.h
@@ -225,6 +225,9 @@
    DRI_CONF_OPT_S(indirect_gl_extension_override, def, \
                   "Allow enabling/disabling a list of indirect-GL extensions")
 
+#define DRI_CONF_DISABLE_PROTECTED_CONTENT_CHECK(def) \
+   DRI_CONF_OPT_B(disable_protected_content_check, def, \
+                  "Don't reject image import if protected_content attribute doesn't match")
 
 /**
  * \brief Image quality-related options

commit cf4e5eddd81f96830976d874a7c5251182235f48

frontend/dri: fix bool/int comparison
Cast tex->bind & PIPE_BIND_PROTECTED to a bool before doing the
comparison, otherwise it'll incorrectly fail.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Fixes: 0a20b7b281f ("driconf: add disable_protected_content_check option")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11107>

src/gallium/frontends/dri/dri2.c
@@ -908,7 +908,7 @@ dri2_create_image_from_winsys(__DRIscreen *_screen,
        */
       const struct driOptionCache *optionCache = &screen->dev->option_cache;
       if (!driQueryOptionb(optionCache, "disable_protected_content_check") &&
-          (tex->bind & PIPE_BIND_PROTECTED) != is_protected_content) {
+          (bool)(tex->bind & PIPE_BIND_PROTECTED) != is_protected_content) {
          pipe_resource_reference(&img->texture, NULL);
          pipe_resource_reference(&tex, NULL);
          FREE(img);