openharmony/third_party_mesa3d

commit 9c230581ed0c27343474954f26eedc24af9e305a

freedreno: Re-work fd_submit fence interface
Move everything into a struct assocated with the pipe_fence_handle, so
that the drm layer can fill in the seqn/fd fences directly.

This will give us a comvenient place to insert a util_queue_fence in the
next commit.

While we're at it, extract the uint32_t fence (previously called
'timestamp' in place, a kgsl legacy) into a struct that encapsulates
both the kernel fence and the userspace fence.

Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10444>

src/freedreno/perfcntrs/fdperf.c
@@ -173,7 +173,7 @@ flush_ring(void)
    if (!dev.submit)
       return;
 
-   ret = fd_submit_flush(dev.submit, -1, NULL, NULL);
+   ret = fd_submit_flush(dev.submit, -1, NULL);
    if (ret)
       errx(1, "submit failed: %d", ret);
    fd_ringbuffer_del(dev.ring);

src/freedreno/computerator/main.c
@@ -284,7 +284,7 @@ main(int argc, char **argv)
 
    backend->emit_grid(kernel, grid, submit);
 
-   fd_submit_flush(submit, -1, NULL, NULL);
+   fd_submit_flush(submit, -1, NULL);
 
    for (int i = 0; i < kernel->num_bufs; i++) {
       fd_bo_cpu_prep(kernel->bufs[i], pipe, FD_BO_PREP_READ);

src/freedreno/drm/freedreno_drmif.h
@@ -79,6 +79,22 @@ fd_fence_after(uint32_t a, uint32_t b)
    return (int32_t)(a - b) > 0;
 }
 
+/**
+ * Per submit, there are actually two fences:
+ *  1) The userspace maintained fence, which is used to optimistically
+ *     avoid kernel ioctls to query if specific rendering is completed
+ *  2) The kernel maintained fence, which we cannot directly do anything
+ *     with, other than pass it back to the kernel
+ *
+ * The userspace fence is mostly internal to the drm layer, but we want
+ * the gallium layer to be able to pass it back to us for things like
+ * fd_pipe_wait().  So this struct encapsulates the two.
+ */
+struct fd_fence {
+   uint32_t kfence;     /* kernel fence */
+   uint32_t ufence;     /* userspace fence */
+};
+
 /* bo flags: */
 #define FD_BO_GPUREADONLY  BITSET_BIT(1)
 #define FD_BO_SCANOUT      BITSET_BIT(2)
@@ -126,9 +142,9 @@ struct fd_pipe *fd_pipe_ref_locked(struct fd_pipe *pipe);
 void fd_pipe_del(struct fd_pipe *pipe);
 int fd_pipe_get_param(struct fd_pipe *pipe, enum fd_param_id param,
                       uint64_t *value);
-int fd_pipe_wait(struct fd_pipe *pipe, uint32_t timestamp);
+int fd_pipe_wait(struct fd_pipe *pipe, const struct fd_fence *fence);
 /* timeout in nanosec */
-int fd_pipe_wait_timeout(struct fd_pipe *pipe, uint32_t timestamp,
+int fd_pipe_wait_timeout(struct fd_pipe *pipe, const struct fd_fence *fence,
                          uint64_t timeout);
 
 /* buffer-object functions:

src/freedreno/drm/freedreno_pipe.c
@@ -124,16 +124,19 @@ fd_pipe_get_param(struct fd_pipe *pipe, enum fd_param_id param, uint64_t *value)
 }
 
 int
-fd_pipe_wait(struct fd_pipe *pipe, uint32_t timestamp)
+fd_pipe_wait(struct fd_pipe *pipe, const struct fd_fence *fence)
 {
-   return fd_pipe_wait_timeout(pipe, timestamp, ~0);
+   return fd_pipe_wait_timeout(pipe, fence, ~0);
 }
 
 int
-fd_pipe_wait_timeout(struct fd_pipe *pipe, uint32_t timestamp, uint64_t timeout)
+fd_pipe_wait_timeout(struct fd_pipe *pipe, const struct fd_fence *fence,
+                     uint64_t timeout)
 {
+   if (!fd_fence_after(fence->ufence, pipe->control->fence))
+      return 0;
 
-   return pipe->funcs->wait(pipe, timestamp, timeout);
+   return pipe->funcs->wait(pipe, fence, timeout);
 }
 
 uint32_t

src/freedreno/drm/freedreno_priv.h
@@ -147,7 +147,8 @@ struct fd_pipe_funcs {
    struct fd_submit *(*submit_new)(struct fd_pipe *pipe);
    int (*get_param)(struct fd_pipe *pipe, enum fd_param_id param,
                     uint64_t *value);
-   int (*wait)(struct fd_pipe *pipe, uint32_t timestamp, uint64_t timeout);
+   int (*wait)(struct fd_pipe *pipe, const struct fd_fence *fence,
+               uint64_t timeout);
    void (*destroy)(struct fd_pipe *pipe);
 };
 
@@ -188,8 +189,8 @@ struct fd_submit_funcs {
    struct fd_ringbuffer *(*new_ringbuffer)(struct fd_submit *submit,
                                            uint32_t size,
                                            enum fd_ringbuffer_flags flags);
-   int (*flush)(struct fd_submit *submit, int in_fence_fd, int *out_fence_fd,
-                uint32_t *out_fence);
+   int (*flush)(struct fd_submit *submit, int in_fence_fd,
+                struct fd_submit_fence *out_fence);
    void (*destroy)(struct fd_submit *submit);
 };
 

src/freedreno/drm/freedreno_ringbuffer.c
@@ -58,11 +58,11 @@ fd_submit_ref(struct fd_submit *submit)
 }
 
 int
-fd_submit_flush(struct fd_submit *submit, int in_fence_fd, int *out_fence_fd,
-                uint32_t *out_fence)
+fd_submit_flush(struct fd_submit *submit, int in_fence_fd,
+                struct fd_submit_fence *out_fence)
 {
    submit->fence = fd_pipe_emit_fence(submit->pipe, submit->primary);
-   return submit->funcs->flush(submit, in_fence_fd, out_fence_fd, out_fence);
+   return submit->funcs->flush(submit, in_fence_fd, out_fence);
 }
 
 struct fd_ringbuffer *

src/freedreno/drm/freedreno_ringbuffer.h
@@ -92,11 +92,26 @@ struct fd_ringbuffer *fd_submit_new_ringbuffer(struct fd_submit *submit,
                                                uint32_t size,
                                                enum fd_ringbuffer_flags flags);
 
+/**
+ * Encapsulates submit out-fence(s), which consist of a 'timestamp' (per-
+ * pipe (submitqueue) sequence number) and optionally, if requested, an
+ * out-fence-fd
+ */
+struct fd_submit_fence {
+   struct fd_fence fence;
+
+   /**
+    * Optional dma_fence fd, returned by submit if use_fence_fd is true
+    */
+   int fence_fd;
+   bool use_fence_fd;
+};
+
 /* in_fence_fd: -1 for no in-fence, else fence fd
- * out_fence_fd: NULL for no output-fence requested, else ptr to return out-fence
+ * out_fence can be NULL if no output fence is required
  */
 int fd_submit_flush(struct fd_submit *submit, int in_fence_fd,
-                    int *out_fence_fd, uint32_t *out_fence);
+                    struct fd_submit_fence *out_fence);
 
 struct fd_ringbuffer;
 struct fd_reloc;

src/freedreno/drm/msm_pipe.c
@@ -106,11 +106,11 @@ msm_pipe_get_param(struct fd_pipe *pipe, enum fd_param_id param,
 }
 
 static int
-msm_pipe_wait(struct fd_pipe *pipe, uint32_t timestamp, uint64_t timeout)
+msm_pipe_wait(struct fd_pipe *pipe, const struct fd_fence *fence, uint64_t timeout)
 {
    struct fd_device *dev = pipe->dev;
    struct drm_msm_wait_fence req = {
-      .fence = timestamp,
+      .fence = fence->kfence,
       .queueid = to_msm_pipe(pipe)->queue_id,
    };
    int ret;

commit 04c5f18c4cdc65297e348243855d9a6f43e3d3de

freedreno: Fix fdperf flush
We created and initialized the fence, but forgot to pass it to
fd_submit_flush().

Fixes: 9c230581ed0 ("freedreno: Re-work fd_submit fence interface")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11200>

src/freedreno/perfcntrs/fdperf.c
@@ -176,7 +176,7 @@ flush_ring(void)
    struct fd_submit_fence fence = {};
    util_queue_fence_init(&fence.ready);
 
-   ret = fd_submit_flush(dev.submit, -1, NULL);
+   ret = fd_submit_flush(dev.submit, -1, &fence);
 
    if (ret)
       errx(1, "submit failed: %d", ret);