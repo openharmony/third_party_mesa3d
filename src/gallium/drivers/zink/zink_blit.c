openharmony/third_party_mesa3d

commit d57f1d5f9bbe09bfdfb31736ef6211d5884a3a63

zink: discard pending clears during blit/copy if we'll overwrite the data
these operations are redundant, so there's no point in forcing them through
since they'll never be visible

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9206>

src/gallium/drivers/zink/zink_blit.c
@@ -38,7 +38,7 @@ blit_resolve(struct zink_context *ctx, const struct pipe_blit_info *info)
       util_range_add(info->dst.resource, &dst->valid_buffer_range,
                      info->dst.box.x, info->dst.box.x + info->dst.box.width);
 
-   zink_fb_clears_apply(ctx, info->dst.resource);
+   zink_fb_clears_apply_or_discard(ctx, info->dst.resource, zink_rect_from_box(&info->dst.box), false);
    zink_fb_clears_apply(ctx, info->src.resource);
    struct zink_batch *batch = zink_batch_no_rp(ctx);
 
@@ -120,7 +120,7 @@ blit_native(struct zink_context *ctx, const struct pipe_blit_info *info)
        dst->format != zink_get_format(screen, info->dst.format))
       return false;
 
-   zink_fb_clears_apply(ctx, info->dst.resource);
+   zink_fb_clears_apply_or_discard(ctx, info->dst.resource, zink_rect_from_box(&info->dst.box), false);
    zink_fb_clears_apply(ctx, info->src.resource);
    struct zink_batch *batch = zink_batch_no_rp(ctx);
    zink_batch_reference_resource_rw(batch, src, false);
@@ -212,6 +212,8 @@ zink_blit(struct pipe_context *pctx,
               util_format_short_name(info->dst.resource->format));
       return;
    }
+
+   zink_fb_clears_apply_or_discard(ctx, info->dst.resource, zink_rect_from_box(&info->dst.box), true);
 
    if (info->dst.resource->target == PIPE_BUFFER)
       util_range_add(info->dst.resource, &dst->valid_buffer_range,

src/gallium/drivers/zink/zink_context.c
@@ -1591,7 +1591,7 @@ zink_resource_copy_region(struct pipe_context *pctx,
       } else
          unreachable("planar formats not yet handled");
 
-      zink_fb_clears_apply(ctx, pdst);
+      zink_fb_clears_apply_or_discard(ctx, pdst, (struct u_rect){dstx, dstx + src_box->width, dsty, dsty + src_box->height}, false);
       zink_fb_clears_apply(ctx, psrc);
 
       region.srcSubresource.aspectMask = src->aspect;

commit 4c3e95ab21c60e48b4efc15fad6e63226bcdcc68

zink: use scissor region for discarding clears during blit
ensure that clears aren't being mistakenly discarded or applied due to
scissor region being ignored and full surface geometry being used

Fixes: d57f1d5f9bb ("zink: discard pending clears during blit/copy if we'll overwrite the data")

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11307>

src/gallium/drivers/zink/zink_blit.c
@@ -8,6 +8,17 @@
 #include "util/u_surface.h"
 #include "util/format/u_format.h"
 
+static void
+apply_dst_clears(struct zink_context *ctx, const struct pipe_blit_info *info, bool discard_only)
+{
+   if (info->scissor_enable) {
+      struct u_rect rect = { info->scissor.minx, info->scissor.maxx,
+                             info->scissor.miny, info->scissor.maxy };
+      zink_fb_clears_apply_or_discard(ctx, info->dst.resource, rect, discard_only);
+   } else
+      zink_fb_clears_apply_or_discard(ctx, info->dst.resource, zink_rect_from_box(&info->dst.box), discard_only);
+}
+
 static bool
 blit_resolve(struct zink_context *ctx, const struct pipe_blit_info *info)
 {
@@ -38,7 +49,7 @@ blit_resolve(struct zink_context *ctx, const struct pipe_blit_info *info)
       util_range_add(info->dst.resource, &dst->valid_buffer_range,
                      info->dst.box.x, info->dst.box.x + info->dst.box.width);
 
-   zink_fb_clears_apply_or_discard(ctx, info->dst.resource, zink_rect_from_box(&info->dst.box), false);
+   apply_dst_clears(ctx, info, false);
    zink_fb_clears_apply_region(ctx, info->src.resource, zink_rect_from_box(&info->src.box));
    struct zink_batch *batch = zink_batch_no_rp(ctx);
 
@@ -143,7 +154,7 @@ blit_native(struct zink_context *ctx, const struct pipe_blit_info *info)
           VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT))
       return false;
 
-   zink_fb_clears_apply_or_discard(ctx, info->dst.resource, zink_rect_from_box(&info->dst.box), false);
+   apply_dst_clears(ctx, info, false);
    zink_fb_clears_apply_region(ctx, info->src.resource, zink_rect_from_box(&info->src.box));
    struct zink_batch *batch = zink_batch_no_rp(ctx);
    zink_batch_reference_resource_rw(batch, src, false);
@@ -276,7 +287,10 @@ zink_blit(struct pipe_context *pctx,
       return;
    }
 
-   zink_fb_clears_apply_or_discard(ctx, info->dst.resource, zink_rect_from_box(&info->dst.box), true);
+   /* this is discard_only because we're about to start a renderpass that will
+    * flush all pending clears anyway
+    */
+   apply_dst_clears(ctx, info, true);
 
    if (info->dst.resource->target == PIPE_BUFFER)
       util_range_add(info->dst.resource, &dst->valid_buffer_range,