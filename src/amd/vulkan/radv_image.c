openharmony/third_party_mesa3d

commit 2bf20eacc61dd0e4254a540f99cf3e1a5c5892f1

radv: Support DCC modifiers fully.
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9998>

src/amd/vulkan/radv_image.c
@@ -1360,6 +1360,10 @@ radv_image_alloc_single_sample_cmask(const struct radv_device *device,
 static void
 radv_image_alloc_values(const struct radv_device *device, struct radv_image *image)
 {
+	/* images with modifiers can be potentially imported */
+	if (image->tiling == VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT)
+		return;
+
 	if (radv_image_has_cmask(image) || radv_image_has_dcc(image)) {
 		image->fce_pred_offset = image->size;
 		image->size += 8 * image->info.levels;

src/amd/vulkan/radv_formats.c
@@ -1156,10 +1156,7 @@ radv_get_modifier_flags(struct radv_physical_device *dev,
 		return 0;
 
 	if (ac_modifier_has_dcc(modifier)) {
-		features &= ~(VK_FORMAT_FEATURE_TRANSFER_DST_BIT |
-	                      VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BIT |
-	                      VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT |
-	                      VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT);
+		features &= ~VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT;
 
 		if (dev->instance->debug_flags & (RADV_DEBUG_NO_DCC | RADV_DEBUG_NO_DISPLAY_DCC))
 			return 0;

commit f671847153d9be0966ef60ecdd9b102c83762169

radv: Allow DCC images to be compressed with foreign queues.
Otherwise we would always decompress when transitioning to the
foreign queue.

Fixes: 2bf20eacc61 ("radv: Support DCC modifiers fully.")
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10802>

src/amd/vulkan/radv_image.c
@@ -1974,6 +1974,9 @@ radv_layout_dcc_compressed(const struct radv_device *device, const struct radv_i
    if (!radv_dcc_enabled(image, level))
       return false;
 
+   if (image->tiling == VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT && queue_mask & (1u << RADV_QUEUE_FOREIGN))
+      return true;
+
    /* If the image is read-only, we can always just keep it compressed */
    if (!(image->usage & RADV_IMAGE_USAGE_WRITE_BITS))
       return true;