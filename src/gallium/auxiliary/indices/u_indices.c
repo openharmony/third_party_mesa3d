openharmony/third_party_mesa3d

commit 6a60613295f0a24951afaa74e7b90ad04f43de17

aux/indices: break out primitive type conversion to separate function
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10965>

src/gallium/auxiliary/indices/u_indices.c
@@ -59,6 +59,39 @@ static void translate_byte_to_ushort( const void *in,
    }
 }
 
+enum pipe_prim_type
+u_index_prim_type_convert(unsigned hw_mask, enum pipe_prim_type prim)
+{
+   if (hw_mask & (1<<prim))
+      return prim;
+
+   switch (prim) {
+   case PIPE_PRIM_POINTS:
+      return PIPE_PRIM_POINTS;
+   case PIPE_PRIM_LINES:
+   case PIPE_PRIM_LINE_STRIP:
+   case PIPE_PRIM_LINE_LOOP:
+      return PIPE_PRIM_LINES;
+   case PIPE_PRIM_TRIANGLES:
+   case PIPE_PRIM_TRIANGLE_STRIP:
+   case PIPE_PRIM_TRIANGLE_FAN:
+   case PIPE_PRIM_QUADS:
+   case PIPE_PRIM_QUAD_STRIP:
+   case PIPE_PRIM_POLYGON:
+      return PIPE_PRIM_TRIANGLES;
+   case PIPE_PRIM_LINES_ADJACENCY:
+   case PIPE_PRIM_LINE_STRIP_ADJACENCY:
+      return PIPE_PRIM_LINES_ADJACENCY;
+   case PIPE_PRIM_TRIANGLES_ADJACENCY:
+   case PIPE_PRIM_TRIANGLE_STRIP_ADJACENCY:
+      return PIPE_PRIM_TRIANGLES_ADJACENCY;
+   default:
+      assert(0);
+      break;
+   }
+   return PIPE_PRIM_POINTS;
+}
+
 /**
  * Translate indexes when a driver can't support certain types
  * of drawing.  Example include:
@@ -127,81 +160,67 @@ u_index_translator(unsigned hw_mask,
    }
    else {
       *out_translate = translate[in_idx][out_idx][in_pv][out_pv][prim_restart][prim];
+      *out_prim = u_index_prim_type_convert(hw_mask, prim);
 
       switch (prim) {
       case PIPE_PRIM_POINTS:
-         *out_prim = PIPE_PRIM_POINTS;
          *out_nr = nr;
          break;
 
       case PIPE_PRIM_LINES:
-         *out_prim = PIPE_PRIM_LINES;
          *out_nr = nr;
          break;
 
       case PIPE_PRIM_LINE_STRIP:
-         *out_prim = PIPE_PRIM_LINES;
          *out_nr = (nr - 1) * 2;
          break;
 
       case PIPE_PRIM_LINE_LOOP:
-         *out_prim = PIPE_PRIM_LINES;
          *out_nr = nr * 2;
          break;
 
       case PIPE_PRIM_TRIANGLES:
-         *out_prim = PIPE_PRIM_TRIANGLES;
          *out_nr = nr;
          break;
 
       case PIPE_PRIM_TRIANGLE_STRIP:
-         *out_prim = PIPE_PRIM_TRIANGLES;
          *out_nr = (nr - 2) * 3;
          break;
 
       case PIPE_PRIM_TRIANGLE_FAN:
-         *out_prim = PIPE_PRIM_TRIANGLES;
          *out_nr = (nr - 2) * 3;
          break;
 
       case PIPE_PRIM_QUADS:
-         *out_prim = PIPE_PRIM_TRIANGLES;
          *out_nr = (nr / 4) * 6;
          break;
 
       case PIPE_PRIM_QUAD_STRIP:
-         *out_prim = PIPE_PRIM_TRIANGLES;
          *out_nr = (nr - 2) * 3;
          break;
 
       case PIPE_PRIM_POLYGON:
-         *out_prim = PIPE_PRIM_TRIANGLES;
          *out_nr = (nr - 2) * 3;
          break;
 
       case PIPE_PRIM_LINES_ADJACENCY:
-         *out_prim = PIPE_PRIM_LINES_ADJACENCY;
          *out_nr = nr;
          break;
 
       case PIPE_PRIM_LINE_STRIP_ADJACENCY:
-         *out_prim = PIPE_PRIM_LINES_ADJACENCY;
          *out_nr = (nr - 3) * 4;
          break;
 
       case PIPE_PRIM_TRIANGLES_ADJACENCY:
-         *out_prim = PIPE_PRIM_TRIANGLES_ADJACENCY;
          *out_nr = nr;
          break;
 
       case PIPE_PRIM_TRIANGLE_STRIP_ADJACENCY:
-         *out_prim = PIPE_PRIM_TRIANGLES_ADJACENCY;
          *out_nr = ((nr - 4) / 2) * 6;
          break;
 
       default:
          assert(0);
-         *out_prim = PIPE_PRIM_POINTS;
          *out_nr = nr;
          return U_TRANSLATE_ERROR;
       }
@@ -251,12 +270,12 @@ u_index_generator(unsigned hw_mask,
 
    *out_index_size = ((start + nr) > 0xfffe) ? 4 : 2;
    out_idx = out_size_idx(*out_index_size);
+   *out_prim = u_index_prim_type_convert(hw_mask, prim);
 
    if ((hw_mask & (1<<prim)) && 
        (in_pv == out_pv)) {
        
       *out_generate = generate[out_idx][in_pv][out_pv][PIPE_PRIM_POINTS];
-      *out_prim = prim;
       *out_nr = nr;
       return U_GENERATE_LINEAR;
    }
@@ -265,79 +284,64 @@ u_index_generator(unsigned hw_mask,
 
       switch (prim) {
       case PIPE_PRIM_POINTS:
-         *out_prim = PIPE_PRIM_POINTS;
          *out_nr = nr;
          return U_GENERATE_REUSABLE;
 
       case PIPE_PRIM_LINES:
-         *out_prim = PIPE_PRIM_LINES;
          *out_nr = nr;
          return U_GENERATE_REUSABLE;
 
       case PIPE_PRIM_LINE_STRIP:
-         *out_prim = PIPE_PRIM_LINES;
          *out_nr = (nr - 1) * 2;
          return U_GENERATE_REUSABLE;
 
       case PIPE_PRIM_LINE_LOOP:
-         *out_prim = PIPE_PRIM_LINES;
          *out_nr = nr * 2;
          return U_GENERATE_ONE_OFF;
 
       case PIPE_PRIM_TRIANGLES:
-         *out_prim = PIPE_PRIM_TRIANGLES;
          *out_nr = nr;
          return U_GENERATE_REUSABLE;
 
       case PIPE_PRIM_TRIANGLE_STRIP:
-         *out_prim = PIPE_PRIM_TRIANGLES;
          *out_nr = (nr - 2) * 3;
          return U_GENERATE_REUSABLE;
 
       case PIPE_PRIM_TRIANGLE_FAN:
-         *out_prim = PIPE_PRIM_TRIANGLES;
          *out_nr = (nr - 2) * 3;
          return U_GENERATE_REUSABLE;
 
       case PIPE_PRIM_QUADS:
-         *out_prim = PIPE_PRIM_TRIANGLES;
          *out_nr = (nr / 4) * 6;
          return U_GENERATE_REUSABLE;
 
       case PIPE_PRIM_QUAD_STRIP:
-         *out_prim = PIPE_PRIM_TRIANGLES;
          *out_nr = (nr - 2) * 3;
          return U_GENERATE_REUSABLE;
 
       case PIPE_PRIM_POLYGON:
-         *out_prim = PIPE_PRIM_TRIANGLES;
          *out_nr = (nr - 2) * 3;
          return U_GENERATE_REUSABLE;
 
       case PIPE_PRIM_LINES_ADJACENCY:
-         *out_prim = PIPE_PRIM_LINES_ADJACENCY;
          *out_nr = nr;
          return U_GENERATE_REUSABLE;
 
       case PIPE_PRIM_LINE_STRIP_ADJACENCY:
-         *out_prim = PIPE_PRIM_LINES_ADJACENCY;
          *out_nr = (nr - 3) * 4;
          return U_GENERATE_REUSABLE;
 
       case PIPE_PRIM_TRIANGLES_ADJACENCY:
-         *out_prim = PIPE_PRIM_TRIANGLES_ADJACENCY;
          *out_nr = nr;
          return U_GENERATE_REUSABLE;
 
       case PIPE_PRIM_TRIANGLE_STRIP_ADJACENCY:
-         *out_prim = PIPE_PRIM_TRIANGLES_ADJACENCY;
          *out_nr = ((nr - 4) / 2) * 6;
          return U_GENERATE_REUSABLE;
 
       default:
          assert(0);
          *out_generate = generate[out_idx][in_pv][out_pv][PIPE_PRIM_POINTS];
-         *out_prim = PIPE_PRIM_POINTS;
          *out_nr = nr;
          return U_TRANSLATE_ERROR;
       }

src/gallium/auxiliary/indices/u_indices.h
@@ -82,6 +82,10 @@ enum indices_mode {
 
 void u_index_init( void );
 
+/* returns the primitive type resulting from index translation */
+enum pipe_prim_type
+u_index_prim_type_convert(unsigned hw_mask, enum pipe_prim_type prim);
+
 
 /**
  * For indexed drawing, this function determines what kind of primitive

commit f6bc71485aad49c8e297827201b1d7424b4f705c

aux/indices: include provoking vertex check in prim type conversion
Include provoking vertex checking in u_index_prim_type_convert() to make sure
the right primitive type is returned from the generator function.

Fixes google earth running on VMware svga device on hw version 11.

Fixes: 6a60613295f0 ("aux/indices: break out primitive type conversion to separate function")

Reviewed-by: Charmaine Lee <charmainel@vmware.com>
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11367>

src/gallium/auxiliary/indices/u_indices.c
@@ -60,9 +60,9 @@ static void translate_byte_to_ushort( const void *in,
 }
 
 enum pipe_prim_type
-u_index_prim_type_convert(unsigned hw_mask, enum pipe_prim_type prim)
+u_index_prim_type_convert(unsigned hw_mask, enum pipe_prim_type prim, bool pv_matches)
 {
-   if (hw_mask & (1<<prim))
+   if ((hw_mask & (1<<prim)) && pv_matches)
       return prim;
 
    switch (prim) {
@@ -159,7 +159,7 @@ u_index_translator(unsigned hw_mask,
       return U_TRANSLATE_MEMCPY;
    }
    *out_translate = translate[in_idx][out_idx][in_pv][out_pv][prim_restart][prim];
-   *out_prim = u_index_prim_type_convert(hw_mask, prim);
+   *out_prim = u_index_prim_type_convert(hw_mask, prim, in_pv == out_pv);
    *out_nr = u_index_count_converted_indices(hw_mask, in_pv == out_pv, prim, nr);
 
    return ret;
@@ -248,7 +248,7 @@ u_index_generator(unsigned hw_mask,
 
    *out_index_size = ((start + nr) > 0xfffe) ? 4 : 2;
    out_idx = out_size_idx(*out_index_size);
-   *out_prim = u_index_prim_type_convert(hw_mask, prim);
+   *out_prim = u_index_prim_type_convert(hw_mask, prim, in_pv == out_pv);
    *out_nr = u_index_count_converted_indices(hw_mask, in_pv == out_pv, prim, nr);
 
    if ((hw_mask & (1<<prim)) && 

src/gallium/auxiliary/indices/u_indices.h
@@ -84,7 +84,7 @@ void u_index_init( void );
 
 /* returns the primitive type resulting from index translation */
 enum pipe_prim_type
-u_index_prim_type_convert(unsigned hw_mask, enum pipe_prim_type prim);
+u_index_prim_type_convert(unsigned hw_mask, enum pipe_prim_type prim, bool pv_matches);
 
 static inline unsigned
 u_index_size_convert(unsigned index_size)