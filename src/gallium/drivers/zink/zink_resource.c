openharmony/third_party_mesa3d

commit 3369c28b2cf20d77efa343fef302cf4241be57be

zink: flatten out buffer creation usage flags codepath
GL doesn't know what it's doing, so we just need all the flags all the time

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9788>

src/gallium/drivers/zink/zink_resource.c
@@ -193,48 +193,22 @@ create_bci(struct zink_screen *screen, const struct pipe_resource *templ, unsign
                VK_BUFFER_USAGE_TRANSFER_DST_BIT |
                VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
 
-   if (templ->usage != PIPE_USAGE_STAGING)
+   VkFormatProperties props = screen->format_props[templ->format];
+
+   bci.usage |= VK_BUFFER_USAGE_UNIFORM_TEXEL_BUFFER_BIT |
+                VK_BUFFER_USAGE_INDIRECT_BUFFER_BIT |
+                VK_BUFFER_USAGE_INDEX_BUFFER_BIT |
+                VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT |
+                VK_BUFFER_USAGE_TRANSFORM_FEEDBACK_BUFFER_BIT_EXT |
+                VK_BUFFER_USAGE_TRANSFORM_FEEDBACK_COUNTER_BUFFER_BIT_EXT;
+   if (props.bufferFeatures & VK_FORMAT_FEATURE_VERTEX_BUFFER_BIT)
+      bci.usage |= VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+   if (props.bufferFeatures & VK_FORMAT_FEATURE_UNIFORM_TEXEL_BUFFER_BIT)
       bci.usage |= VK_BUFFER_USAGE_UNIFORM_TEXEL_BUFFER_BIT;
 
-   /* apparently gallium thinks these are the jack-of-all-trades bind types */
-   if (bind & (PIPE_BIND_SAMPLER_VIEW | PIPE_BIND_QUERY_BUFFER)) {
-      bci.usage |= VK_BUFFER_USAGE_UNIFORM_TEXEL_BUFFER_BIT |
-                   VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT |
-                   VK_BUFFER_USAGE_INDIRECT_BUFFER_BIT |
-                   VK_BUFFER_USAGE_INDEX_BUFFER_BIT |
-                   VK_BUFFER_USAGE_VERTEX_BUFFER_BIT |
-                   VK_BUFFER_USAGE_TRANSFORM_FEEDBACK_BUFFER_BIT_EXT;
-   }
-
-   if (bind & PIPE_BIND_VERTEX_BUFFER)
-      bci.usage |= VK_BUFFER_USAGE_VERTEX_BUFFER_BIT |
-                   VK_BUFFER_USAGE_INDEX_BUFFER_BIT |
-                   VK_BUFFER_USAGE_UNIFORM_TEXEL_BUFFER_BIT |
-                   VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT |
-                   VK_BUFFER_USAGE_TRANSFORM_FEEDBACK_BUFFER_BIT_EXT;
-
-   if (bind & PIPE_BIND_INDEX_BUFFER)
-      bci.usage |= VK_BUFFER_USAGE_INDEX_BUFFER_BIT;
-
-   if (bind & PIPE_BIND_CONSTANT_BUFFER)
-      bci.usage |= VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
-
-   if (bind & PIPE_BIND_SHADER_BUFFER)
-      bci.usage |= VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
-
-   if (bind & PIPE_BIND_SHADER_IMAGE)
+   if (bind & PIPE_BIND_SHADER_IMAGE) {
+      assert(props.bufferFeatures & VK_FORMAT_FEATURE_STORAGE_TEXEL_BUFFER_BIT);
       bci.usage |= VK_BUFFER_USAGE_STORAGE_TEXEL_BUFFER_BIT;
-
-   if (bind & PIPE_BIND_COMMAND_ARGS_BUFFER)
-      bci.usage |= VK_BUFFER_USAGE_INDIRECT_BUFFER_BIT;
-
-   if (bind == (PIPE_BIND_STREAM_OUTPUT | PIPE_BIND_CUSTOM)) {
-      bci.usage |= VK_BUFFER_USAGE_TRANSFORM_FEEDBACK_COUNTER_BUFFER_BIT_EXT;
-   } else if (bind & PIPE_BIND_STREAM_OUTPUT) {
-      bci.usage |= VK_BUFFER_USAGE_VERTEX_BUFFER_BIT |
-                   VK_BUFFER_USAGE_INDEX_BUFFER_BIT |
-                   VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT |
-                   VK_BUFFER_USAGE_TRANSFORM_FEEDBACK_BUFFER_BIT_EXT;
    }
    return bci;
 }

commit a6a5f5a683b42e9e1463c7428def5b1fefee74db

zink: drop repeated usage-bit
We already set this bit unconditionally right before, no point in
repeating it.

Fixes: 3369c28b2 ("zink: flatten out buffer creation usage flags codepath")
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11416>

src/gallium/drivers/zink/zink_resource.c
@@ -234,8 +234,6 @@ create_bci(struct zink_screen *screen, const struct pipe_resource *templ, unsign
                 VK_BUFFER_USAGE_TRANSFORM_FEEDBACK_COUNTER_BUFFER_BIT_EXT;
    if (props.bufferFeatures & VK_FORMAT_FEATURE_VERTEX_BUFFER_BIT)
       bci.usage |= VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
-   if (props.bufferFeatures & VK_FORMAT_FEATURE_UNIFORM_TEXEL_BUFFER_BIT)
-      bci.usage |= VK_BUFFER_USAGE_UNIFORM_TEXEL_BUFFER_BIT;
 
    if (bind & PIPE_BIND_SHADER_IMAGE) {
       assert(props.bufferFeatures & VK_FORMAT_FEATURE_STORAGE_TEXEL_BUFFER_BIT);