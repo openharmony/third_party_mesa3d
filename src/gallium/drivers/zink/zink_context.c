openharmony/third_party_mesa3d

commit 9db0b3c171448f9da5369f8ec6d9881e37ed1669

zink: switch to memory barriers instead of actual buffer barriers
drivers don't seem to actually use the buffer part of the info, so this is
just wasting cycles initializing the values

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10996>

src/gallium/drivers/zink/zink_context.c
@@ -2053,14 +2053,16 @@ zink_resource_buffer_barrier_init(VkBufferMemoryBarrier *bmb, struct zink_resour
 void
 zink_resource_buffer_barrier(struct zink_context *ctx, struct zink_batch *batch, struct zink_resource *res, VkAccessFlags flags, VkPipelineStageFlags pipeline)
 {
-   VkBufferMemoryBarrier bmb;
-   if (!zink_resource_buffer_barrier_init(&bmb, res, flags, pipeline))
-      return;
+   VkMemoryBarrier bmb;
    if (!pipeline)
       pipeline = pipeline_access_stage(flags);
    if (!zink_resource_buffer_needs_barrier(res, flags, pipeline))
       return;
 
+   bmb.sType = VK_STRUCTURE_TYPE_MEMORY_BARRIER;
+   bmb.pNext = NULL;
+   bmb.srcAccessMask = res->access;
+   bmb.dstAccessMask = flags;
    VkCommandBuffer cmdbuf = get_cmdbuf(ctx, res);
    /* only barrier if we're changing layout or doing something besides read -> read */
    vkCmdPipelineBarrier(
@@ -2068,8 +2070,8 @@ zink_resource_buffer_barrier(struct zink_context *ctx, struct zink_batch *batch,
       res->access_stage ? res->access_stage : pipeline_access_stage(res->access),
       pipeline,
       0,
-      0, NULL,
       1, &bmb,
+      0, NULL,
       0, NULL
    );
 

commit 7d4bb600cc4b80534c3770b22ba16d1906a65aaf

zink: remove unused function
There's no call-sites to this function, so let's drop it.

Fixes: 9db0b3c1714 ("zink: switch to memory barriers instead of actual buffer barriers")
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11421>

src/gallium/drivers/zink/zink_context.c
@@ -2275,25 +2275,6 @@ zink_resource_buffer_needs_barrier(struct zink_resource *res, VkAccessFlags flag
           (res->access & flags) != flags;
 }
 
-ALWAYS_INLINE static bool
-zink_resource_buffer_barrier_init(VkBufferMemoryBarrier *bmb, struct zink_resource *res, VkAccessFlags flags, VkPipelineStageFlags pipeline)
-{
-   if (!pipeline)
-      pipeline = pipeline_access_stage(flags);
-   *bmb = (VkBufferMemoryBarrier){
-      VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER,
-      NULL,
-      res->access,
-      flags,
-      VK_QUEUE_FAMILY_IGNORED,
-      VK_QUEUE_FAMILY_IGNORED,
-      res->obj->buffer,
-      res->obj->offset,
-      res->base.b.width0
-   };
-   return zink_resource_buffer_needs_barrier(res, flags, pipeline);
-}
-
 void
 zink_resource_buffer_barrier(struct zink_context *ctx, struct zink_batch *batch, struct zink_resource *res, VkAccessFlags flags, VkPipelineStageFlags pipeline)
 {