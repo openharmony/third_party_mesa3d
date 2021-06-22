openharmony/third_party_mesa3d

commit 56764fbdaa198340e2108cf414c98a09aa8f6214

intel: Simplify few version checks involving G4X
Signed-off-by: Anuj Phogat <anuj.phogat@gmail.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9608>

src/mesa/drivers/dri/i965/genX_pipe_control.c
@@ -492,7 +492,7 @@ genX(emit_raw_pipe_control)(struct brw_context *brw, uint32_t flags,
       pc.InstructionCacheInvalidateEnable =
          flags & PIPE_CONTROL_INSTRUCTION_INVALIDATE;
       pc.NotifyEnable = flags & PIPE_CONTROL_NOTIFY_ENABLE;
-   #if GEN_GEN >= 5 || GEN_VERSIONx10 == 45
+   #if GEN_GEN >= 45
       pc.IndirectStatePointersDisable =
          flags & PIPE_CONTROL_INDIRECT_STATE_POINTERS_DISABLE;
    #endif

src/intel/isl/isl_surface_state.c
@@ -547,7 +547,7 @@ isl_genX(surf_fill_state_s)(const struct isl_device *dev, void *state,
    s.MOCS = info->mocs;
 #endif
 
-#if GEN_GEN > 4 || GEN_VERSIONx10 == 45
+#if GEN_VERSIONx10 >= 45
    if (info->x_offset_sa != 0 || info->y_offset_sa != 0) {
       /* There are fairly strict rules about when the offsets can be used.
        * These are mostly taken from the Sky Lake PRM documentation for

src/mesa/drivers/dri/i965/genX_state_upload.c
@@ -1679,7 +1679,7 @@ genX(upload_sf)(struct brw_context *brw)
          sf.SmoothPointEnable = false;
 #endif
 
-#if GEN_VERSIONx10 == 45 || GEN_GEN >= 5
+#if GEN_VERSIONx10 >= 45
       sf.AALineDistanceMode = AALINEDISTANCE_TRUE;
 #endif
 

commit fa88d991cd5a0c33f04404a84e91d877ab6f68f3

i965: fix regression in pipe control on g45
The cleanups got the wrong value here, just noticed in drive by.

Fixes: 56764fbdaa19 ("intel: Simplify few version checks involving G4X")
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11519>

src/mesa/drivers/dri/i965/genX_pipe_control.c
@@ -495,7 +495,7 @@ genX(emit_raw_pipe_control)(struct brw_context *brw, uint32_t flags,
       pc.InstructionCacheInvalidateEnable =
          flags & PIPE_CONTROL_INSTRUCTION_INVALIDATE;
       pc.NotifyEnable = flags & PIPE_CONTROL_NOTIFY_ENABLE;
-   #if GFX_VER >= 45
+   #if GFX_VERx10 >= 45
       pc.IndirectStatePointersDisable =
          flags & PIPE_CONTROL_INDIRECT_STATE_POINTERS_DISABLE;
    #endif