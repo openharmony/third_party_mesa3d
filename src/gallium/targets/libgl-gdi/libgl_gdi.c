openharmony/third_party_mesa3d

commit 9a6d0710982dcdaa9620dde1d8c9140945bf1414

libgl-gdi: add zink support
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Acked-by: Jose Fonseca <jfonseca@vmware.com>

Closes #3802

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7643>

src/gallium/targets/libgl-gdi/libgl_gdi.c
@@ -60,6 +60,10 @@
 #include "d3d12/d3d12_public.h"
 #endif
 
+#ifdef GALLIUM_ZINK
+#include "zink/zink_public.h"
+#endif
+
 #ifdef GALLIUM_LLVMPIPE
 static boolean use_llvmpipe = FALSE;
 #endif
@@ -68,6 +72,9 @@ static boolean use_swr = FALSE;
 #endif
 #ifdef GALLIUM_D3D12
 static boolean use_d3d12 = FALSE;
+#endif
+#ifdef GALLIUM_ZINK
+static boolean use_zink = FALSE;
 #endif
 
 static struct pipe_screen *
@@ -113,6 +120,13 @@ gdi_screen_create(void)
       screen = d3d12_create_screen( winsys, NULL );
       if (screen)
          use_d3d12 = TRUE;
+   }
+#endif
+#ifdef GALLIUM_ZINK
+   if (strcmp(driver, "zink") == 0) {
+       screen = zink_create_screen( winsys );
+       if (screen)
+           use_zink = TRUE;
    }
 #endif
    (void) driver;
@@ -169,6 +183,13 @@ gdi_present(struct pipe_screen *screen,
 
 #ifdef GALLIUM_D3D12
    if (use_d3d12) {
+      screen->flush_frontbuffer(screen, res, 0, 0, hDC, NULL);
+      return;
+   }
+#endif
+
+#ifdef GALLIUM_ZINK
+   if (use_zink) {
       screen->flush_frontbuffer(screen, res, 0, 0, hDC, NULL);
       return;
    }

src/gallium/targets/libgl-gdi/meson.build
@@ -39,7 +39,7 @@ libopengl32 = shared_library(
   ],
   dependencies : [
     dep_ws2_32, idep_nir, idep_mesautil, driver_swrast, driver_swr,
-    driver_d3d12
+    driver_d3d12, driver_zink
   ],
   name_prefix : '',  # otherwise mingw will create libopengl32.dll
   install : true,

commit c73cd9f33dc14cf4e2b3ca9569592a42f7ef1930

libgl-gdi: add missing include
Without this, I get the following error if I try to compile Zink without
any other drivers:

src/gallium/targets/libgl-gdi/libgl_gdi.c(210): error C2037: left of
'flush_frontbuffer' specifies undefined struct/union 'pipe_screen'

Fixes: 9a6d0710982 ("libgl-gdi: add zink support")
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Acked-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11536>

src/gallium/targets/libgl-gdi/libgl_gdi.c
@@ -41,6 +41,7 @@
 #include "stw_winsys.h"
 #include "stw_device.h"
 #include "gdi/gdi_sw_winsys.h"
+#include "pipe/p_screen.h"
 #include "pipe/p_context.h"
 
 #ifdef GALLIUM_SOFTPIPE