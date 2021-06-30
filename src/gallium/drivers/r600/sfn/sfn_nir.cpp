openharmony/third_party_mesa3d

commit 6b33368adbab0690f58c0a6df87a718a34a5a0a4

r600/sfn: Lower offset in TXF instructions
Closes: #4960
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11639>

src/gallium/drivers/r600/sfn/sfn_nir.cpp
@@ -851,9 +851,10 @@ int r600_shader_from_nir(struct r600_context *rctx,
    NIR_PASS_V(sel->nir, r600_lower_shared_io);
    NIR_PASS_V(sel->nir, r600_nir_lower_atomics);
 
-   static const struct nir_lower_tex_options lower_tex_options = {
-      .lower_txp = ~0u,
-   };
+   struct nir_lower_tex_options lower_tex_options;
+   lower_tex_options.lower_txp = ~0u;
+   lower_tex_options.lower_txf_offset = true;
+
    NIR_PASS_V(sel->nir, nir_lower_tex, &lower_tex_options);
    NIR_PASS_V(sel->nir, r600::r600_nir_lower_txl_txf_array_or_cube);
    NIR_PASS_V(sel->nir, r600::r600_nir_lower_cube_to_2darray);

commit 5640308555947d3fa5fb7a68c4a3d975f7c8a69b

r600/sfn: initialize all texture lower options
Fixes: 6b33368adbab0690f58c0a6df87a718a34a5a0a4
    r600/sfn: Lower offset in TXF instructions

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11650>

src/gallium/drivers/r600/sfn/sfn_nir.cpp
@@ -851,7 +851,7 @@ int r600_shader_from_nir(struct r600_context *rctx,
    NIR_PASS_V(sel->nir, r600_lower_shared_io);
    NIR_PASS_V(sel->nir, r600_nir_lower_atomics);
 
-   struct nir_lower_tex_options lower_tex_options;
+   struct nir_lower_tex_options lower_tex_options = {0};
    lower_tex_options.lower_txp = ~0u;
    lower_tex_options.lower_txf_offset = true;
 