openharmony/third_party_mesa3d

commit 18636b88dbb7570dce61b903232b736f3de16499

panfrost: Implement alpha testing natively
On Midgard, we still have to lower on v6+. Passes Piglit
./fbo-mrt-alphatest (saving a cycle in the fragment shader to
compare/discard).

Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8447>

src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -542,7 +542,7 @@ panfrost_prepare_fs_state(struct panfrost_context *ctx,
         state->stencil_mask_misc.stencil_mask_back = zsa->stencil_mask_back;
         state->stencil_mask_misc.stencil_enable = zsa->base.stencil[0].enabled;
         state->stencil_mask_misc.alpha_to_coverage = alpha_to_coverage;
-        state->stencil_mask_misc.alpha_test_compare_function = MALI_FUNC_ALWAYS;
+        state->stencil_mask_misc.alpha_test_compare_function = zsa->alpha_func;
         state->stencil_mask_misc.depth_range_1 = rast->offset_tri;
         state->stencil_mask_misc.depth_range_2 = rast->offset_tri;
         state->stencil_mask_misc.single_sampled_lines = !rast->multisample;
@@ -554,6 +554,10 @@ panfrost_prepare_fs_state(struct panfrost_context *ctx,
         state->stencil_back = zsa->stencil_back;
         state->stencil_front.reference_value = ctx->stencil_ref.ref_value[0];
         state->stencil_back.reference_value = ctx->stencil_ref.ref_value[back_enab ? 1 : 0];
+
+        /* v6+ fits register preload here, no alpha testing */
+        if (dev->arch <= 5)
+                state->alpha_reference = zsa->base.alpha_ref_value;
 }
 
 

src/gallium/drivers/panfrost/pan_context.c
@@ -1240,8 +1240,9 @@ panfrost_create_depth_stencil_state(struct pipe_context *pipe,
                 so->stencil_mask_back = so->stencil_mask_front;
         }
 
-        /* Alpha lowered by frontend */
-        assert(!zsa->alpha_enabled);
+        so->alpha_func = zsa->alpha_enabled ?
+                panfrost_translate_compare_func(zsa->alpha_func) :
+                MALI_FUNC_ALWAYS;
 
         /* TODO: Bounds test should be easy */
         assert(!zsa->depth_bounds_test);

src/gallium/drivers/panfrost/pan_context.h
@@ -282,6 +282,7 @@ struct panfrost_vertex_state {
 
 struct panfrost_zsa_state {
         struct pipe_depth_stencil_alpha_state base;
+        enum mali_func alpha_func;
 
         /* Precomputed stencil state */
         struct MALI_STENCIL stencil_front;

src/gallium/drivers/panfrost/pan_screen.c
@@ -267,7 +267,10 @@ panfrost_get_param(struct pipe_screen *screen, enum pipe_cap param)
         case PIPE_CAP_MAX_VARYINGS:
                 return 16;
 
+        /* Removed in v6 (Bifrost) */
         case PIPE_CAP_ALPHA_TEST:
+                return dev->arch <= 5;
+
         case PIPE_CAP_FLATSHADE:
         case PIPE_CAP_TWO_SIDED_COLOR:
         case PIPE_CAP_CLIP_PLANES:

commit 2d4ef7514339d166f172ba3278766ebc7f9c936f

panfrost: Always use a fragment shader when alpha test is enabled
Fixes incorrect rendering with OpenSCAD.

Fixes: 18636b88dbb ("panfrost: Implement alpha testing natively")
Reported-by: Urja Rannikko <urjaman@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11812>

src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -284,11 +284,17 @@ static bool
 panfrost_fs_required(
                 struct panfrost_shader_state *fs,
                 struct panfrost_blend_state *blend,
-                struct pipe_framebuffer_state *state)
+                struct pipe_framebuffer_state *state,
+                const struct panfrost_zsa_state *zsa)
 {
         /* If we generally have side effects. This inclues use of discard,
          * which can affect the results of an occlusion query. */
         if (fs->info.fs.sidefx)
+                return true;
+
+        /* Using an empty FS requires early-z to be enabled, but alpha test
+         * needs it disabled */
+        if ((enum mali_func) zsa->base.alpha_func != MALI_FUNC_ALWAYS)
                 return true;
 
         /* If colour is written we need to execute */
@@ -518,7 +524,7 @@ panfrost_prepare_fs_state(struct panfrost_context *ctx,
         bool msaa = rast->multisample;
 
         pan_pack(rsd, RENDERER_STATE, cfg) {
-                if (pan_is_bifrost(dev) && panfrost_fs_required(fs, so, &ctx->pipe_framebuffer)) {
+                if (pan_is_bifrost(dev) && panfrost_fs_required(fs, so, &ctx->pipe_framebuffer, zsa)) {
                         /* Track if any colour buffer is reused across draws, either
                          * from reading it directly, or from failing to write it */
                         unsigned rt_mask = ctx->fb_rt_mask;
@@ -532,7 +538,7 @@ panfrost_prepare_fs_state(struct panfrost_context *ctx,
                 } else if (!pan_is_bifrost(dev)) {
                         unsigned rt_count = ctx->pipe_framebuffer.nr_cbufs;
 
-                        if (panfrost_fs_required(fs, ctx->blend, &ctx->pipe_framebuffer)) {
+                        if (panfrost_fs_required(fs, ctx->blend, &ctx->pipe_framebuffer, zsa)) {
                                 cfg.properties.midgard.force_early_z =
                                         fs->info.fs.can_early_z && !alpha_to_coverage &&
                                         ((enum mali_func) zsa->base.alpha_func == MALI_FUNC_ALWAYS);
@@ -637,7 +643,7 @@ panfrost_emit_frag_shader(struct panfrost_context *ctx,
         }
 
         /* Merge with CSO state and upload */
-        if (panfrost_fs_required(fs, ctx->blend, &ctx->pipe_framebuffer))
+        if (panfrost_fs_required(fs, ctx->blend, &ctx->pipe_framebuffer, zsa))
                 pan_merge(rsd, fs->partial_rsd, RENDERER_STATE);
         else
                 pan_merge_empty_fs(&rsd, pan_is_bifrost(dev));