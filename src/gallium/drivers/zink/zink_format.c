openharmony/third_party_mesa3d

commit 33d32432769166574a52e9b4ee362daa2d60d0a3

zink: Fix VK_FORMAT_A8B8G8R8_SRGB_PACK32 mapping on big-endian
The PACK32 Vulkan formats need to map to the corresponding XYZWxxxx pipe
formats, since the latter take endianness into account.

Reviewed-by: Eric Anholt <eric@anholt.net>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8394>

src/gallium/drivers/zink/zink_format.c
@@ -82,7 +82,7 @@ static const VkFormat formats[PIPE_FORMAT_COUNT] = {
    MAP_FORMAT_SCALED(B8G8R8A8)
    MAP_FORMAT_INT(B8G8R8A8)
    MAP_FORMAT_SRGB(B8G8R8A8)
-   [PIPE_FORMAT_R8G8B8A8_SRGB] = VK_FORMAT_A8B8G8R8_SRGB_PACK32,
+   [PIPE_FORMAT_RGBA8888_SRGB] = VK_FORMAT_A8B8G8R8_SRGB_PACK32,
    // 16-bits
    MAP_FORMAT_NORM(R16G16B16A16)
    MAP_FORMAT_SCALED(R16G16B16A16)

commit 942c3f39caaff4b3e2a1e4002f4c60f38af41860

zink: remove duplicate format-mapping on little-endian
Doing *both* of thse ends up rewriting the previous mapping. Since this
doesn't seem to have lead to issues, it seems like the new mapping works
just as well.

Fixes: 33d32432769 ("zink: Fix VK_FORMAT_A8B8G8R8_SRGB_PACK32 mapping on big-endian")
Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11417>

src/gallium/drivers/zink/zink_format.c
@@ -77,7 +77,6 @@ static const VkFormat formats[PIPE_FORMAT_COUNT] = {
    MAP_FORMAT_NORM(R8G8B8A8)
    MAP_FORMAT_SCALED(R8G8B8A8)
    MAP_FORMAT_INT(R8G8B8A8)
-   MAP_FORMAT_SRGB(R8G8B8A8)
    MAP_FORMAT_NORM(B8G8R8A8)
    MAP_FORMAT_SCALED(B8G8R8A8)
    MAP_FORMAT_INT(B8G8R8A8)