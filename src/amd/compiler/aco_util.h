openharmony/third_party_mesa3d

commit a6502c3b0174110f5cafcbb86acae3ea09952165

aco: reorder and cleanup #includes
Reviewed-by: Tony Wasserka <tony.wasserka@gmx.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11271>

src/amd/compiler/aco_util.h
@@ -25,6 +25,8 @@
 #ifndef ACO_UTIL_H
 #define ACO_UTIL_H
 
+#include "util/bitscan.h"
+
 #include <cassert>
 #include <iterator>
 #include <vector>

src/amd/compiler/aco_assembler.cpp
@@ -1,13 +1,14 @@
-#include <vector>
+
+
+#include "aco_builder.h"
+#include "aco_ir.h"
+#include "sid.h"
+
+#include "util/memstream.h"
+
 #include <algorithm>
 #include <map>
-
-#include "aco_ir.h"
-#include "aco_builder.h"
-#include "common/sid.h"
-#include "ac_shader_util.h"
-#include "util/memstream.h"
-#include "util/u_math.h"
+#include <vector>
 
 namespace aco {
 

src/amd/compiler/aco_builder_h.py
@@ -29,8 +29,6 @@ template = """\
 #define _ACO_BUILDER_
 
 #include "aco_ir.h"
-#include "util/u_math.h"
-#include "util/bitscan.h"
 
 namespace aco {
 enum dpp_ctrl {
@@ -601,7 +599,8 @@ formats = [(f if len(f) == 5 else f + ('',)) for f in formats]
 % endfor
 };
 
-}
+} // namespace aco
+
 #endif /* _ACO_BUILDER_ */"""
 
 from aco_opcodes import opcodes, Format

src/amd/compiler/aco_dead_code_analysis.cpp
@@ -25,6 +25,7 @@
 #include "aco_ir.h"
 
 #include <algorithm>
+#include <vector>
 
 /*
  * Implements an analysis pass to determine the number of uses

src/amd/compiler/aco_form_hard_clauses.cpp
@@ -21,8 +21,10 @@
  * IN THE SOFTWARE.
  */
 
-#include "aco_ir.h"
 #include "aco_builder.h"
+#include "aco_ir.h"
+
+#include <vector>
 
 namespace aco {
 namespace {

src/amd/compiler/aco_insert_NOPs.cpp
@@ -22,14 +22,13 @@
  *
  */
 
-#include <algorithm>
-
-#include "aco_ir.h"
 #include "aco_builder.h"
-#include <stack>
-#include <functional>
+#include "aco_ir.h"
 
-#include <time.h>
+#include <algorithm>
+#include <bitset>
+#include <stack>
+#include <vector>
 
 namespace aco {
 namespace {

src/amd/compiler/aco_insert_exec_mask.cpp
@@ -22,9 +22,12 @@
  *
  */
 
-#include "aco_ir.h"
 #include "aco_builder.h"
+#include "aco_ir.h"
 #include "util/u_math.h"
+
+#include <set>
+#include <vector>
 
 namespace aco {
 

src/amd/compiler/aco_insert_waitcnt.cpp
@@ -22,13 +22,12 @@
  *
  */
 
-#include <algorithm>
-#include <map>
-#include <stack>
-#include <math.h>
-
 #include "aco_ir.h"
 #include "sid.h"
+
+#include <map>
+#include <stack>
+#include <vector>
 
 namespace aco {
 

commit 25dddbeb93e1f5cc41de9cf70ecb0678907a2a06

aco: include <cstddef> in aco_util.h
It's needed for ptrdiff.

Fixes: a6502c3b0174110f5cafcbb86acae3ea09952165 ('aco: reorder and cleanup #includes')
Closes: #5076
Reviewed-by: Tony Wasserka <tony.wasserka@gmx.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11947>

src/amd/compiler/aco_util.h
@@ -29,6 +29,7 @@
 #include "util/bitscan.h"
 
 #include <cassert>
+#include <cstddef>
 #include <iterator>
 #include <vector>
 
@@ -51,7 +52,7 @@ public:
    using reverse_iterator = std::reverse_iterator<iterator>;
    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
    using size_type = uint16_t;
-   using difference_type = ptrdiff_t;
+   using difference_type = std::ptrdiff_t;
 
    /*! \brief                  Compiler generated default constructor
     */