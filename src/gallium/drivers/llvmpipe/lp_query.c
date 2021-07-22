openharmony/third_party_mesa3d

commit bb2261a905cae87606ddbcb1b6bd87b6666f02a7

llvmpipe: initial query buffer object support. (v2)
This fails a couple of piglits due to other bugs in llvmpipe,
but it adds support for the feature properly.

v2: don't reset pipestats, just recalc, fix CI expectation

src/gallium/drivers/llvmpipe/lp_query.c
@@ -185,6 +185,156 @@ llvmpipe_get_query_result(struct pipe_context *pipe,
    return true;
 }
 
+static void
+llvmpipe_get_query_result_resource(struct pipe_context *pipe,
+                                   struct pipe_query *q,
+                                   bool wait,
+                                   enum pipe_query_value_type result_type,
+                                   int index,
+                                   struct pipe_resource *resource,
+                                   unsigned offset)
+{
+   struct llvmpipe_screen *screen = llvmpipe_screen(pipe->screen);
+   unsigned num_threads = MAX2(1, screen->num_threads);
+   struct llvmpipe_query *pq = llvmpipe_query(q);
+   struct llvmpipe_resource *lpr = llvmpipe_resource(resource);
+   bool unflushed = false;
+   bool unsignalled = false;
+   if (pq->fence) {
+      /* only have a fence if there was a scene */
+      if (!lp_fence_signalled(pq->fence)) {
+         unsignalled = true;
+         if (!lp_fence_issued(pq->fence))
+            unflushed = true;
+      }
+   }
+
+
+   uint64_t value = 0;
+   if (index == -1)
+      if (unsignalled)
+         value = 0;
+      else
+         value = 1;
+   else {
+      unsigned i;
+
+      if (unflushed) {
+         llvmpipe_flush(pipe, NULL, __FUNCTION__);
+
+         if (!wait)
+            return;
+
+         lp_fence_wait(pq->fence);
+      }
+
+      switch (pq->type) {
+      case PIPE_QUERY_OCCLUSION_COUNTER:
+         for (i = 0; i < num_threads; i++) {
+            value += pq->end[i];
+         }
+         break;
+      case PIPE_QUERY_OCCLUSION_PREDICATE:
+      case PIPE_QUERY_OCCLUSION_PREDICATE_CONSERVATIVE:
+         for (i = 0; i < num_threads; i++) {
+            /* safer (still not guaranteed) when there's an overflow */
+            value = value || pq->end[i];
+         }
+         break;
+      case PIPE_QUERY_PRIMITIVES_GENERATED:
+         value = pq->num_primitives_generated;
+         break;
+      case PIPE_QUERY_PRIMITIVES_EMITTED:
+         value = pq->num_primitives_written;
+         break;
+      case PIPE_QUERY_TIMESTAMP:
+         for (i = 0; i < num_threads; i++) {
+            if (pq->end[i] > value) {
+               value = pq->end[i];
+            }
+         }
+         break;
+      case PIPE_QUERY_SO_OVERFLOW_PREDICATE:
+      case PIPE_QUERY_SO_OVERFLOW_ANY_PREDICATE:
+         value = !!(pq->num_primitives_generated > pq->num_primitives_written);
+         break;
+      case PIPE_QUERY_PIPELINE_STATISTICS:
+         switch ((enum pipe_statistics_query_index)index) {
+         case PIPE_STAT_QUERY_IA_VERTICES:
+            value = pq->stats.ia_vertices;
+            break;
+         case PIPE_STAT_QUERY_IA_PRIMITIVES:
+            value = pq->stats.ia_primitives;
+            break;
+         case PIPE_STAT_QUERY_VS_INVOCATIONS:
+            value = pq->stats.vs_invocations;
+            break;
+         case PIPE_STAT_QUERY_GS_INVOCATIONS:
+            value = pq->stats.gs_invocations;
+            break;
+         case PIPE_STAT_QUERY_GS_PRIMITIVES:
+            value = pq->stats.gs_primitives;
+            break;
+         case PIPE_STAT_QUERY_C_INVOCATIONS:
+            value = pq->stats.c_invocations;
+            break;
+         case PIPE_STAT_QUERY_C_PRIMITIVES:
+            value = pq->stats.c_primitives;
+            break;
+         case PIPE_STAT_QUERY_PS_INVOCATIONS:
+            value = 0;
+            for (i = 0; i < num_threads; i++) {
+               value += pq->end[i];
+            }
+            value *= LP_RASTER_BLOCK_SIZE * LP_RASTER_BLOCK_SIZE;
+            break;
+         case PIPE_STAT_QUERY_HS_INVOCATIONS:
+            value = pq->stats.hs_invocations;
+            break;
+         case PIPE_STAT_QUERY_DS_INVOCATIONS:
+            value = pq->stats.ds_invocations;
+            break;
+         case PIPE_STAT_QUERY_CS_INVOCATIONS:
+            value = pq->stats.cs_invocations;
+            break;
+         }
+         break;
+      default:
+         fprintf(stderr, "Unknown query type %d\n", pq->type);
+         break;
+      }
+   }
+
+   void *dst = (uint8_t *)lpr->data + offset;
+   switch (result_type) {
+   case PIPE_QUERY_TYPE_I32: {
+      int32_t *iptr = (int32_t *)dst;
+      if (value > 0x7fffffff)
+         *iptr = 0x7fffffff;
+      else
+         *iptr = (int32_t)value;
+      break;
+   }
+   case PIPE_QUERY_TYPE_U32: {
+      uint32_t *uptr = (uint32_t *)dst;
+      if (value > 0xffffffff)
+         *uptr = 0xffffffff;
+      else
+         *uptr = (uint32_t)value;
+      break;
+   }
+   case PIPE_QUERY_TYPE_I64: {
+      int64_t *iptr = (int64_t *)dst;
+      *iptr = (int64_t)value;
+      break;
+   }
+   case PIPE_QUERY_TYPE_U64: {
+      uint64_t *uptr = (uint64_t *)dst;
+      *uptr = (uint64_t)value;
+      break;
+   }
+   }
+}
 
 static bool
 llvmpipe_begin_query(struct pipe_context *pipe, struct pipe_query *q)
@@ -341,6 +491,7 @@ void llvmpipe_init_query_funcs(struct llvmpipe_context *llvmpipe )
    llvmpipe->pipe.begin_query = llvmpipe_begin_query;
    llvmpipe->pipe.end_query = llvmpipe_end_query;
    llvmpipe->pipe.get_query_result = llvmpipe_get_query_result;
+   llvmpipe->pipe.get_query_result_resource = llvmpipe_get_query_result_resource;
    llvmpipe->pipe.set_active_query_state = llvmpipe_set_active_query_state;
 }
 

src/gallium/drivers/llvmpipe/lp_screen.c
@@ -318,6 +318,8 @@ llvmpipe_get_param(struct pipe_screen *screen, enum pipe_cap param)
       return 32;
    case PIPE_CAP_SHADER_BUFFER_OFFSET_ALIGNMENT:
       return 1;
+   case PIPE_CAP_QUERY_BUFFER_OBJECT:
+      return 1;
    case PIPE_CAP_MULTISAMPLE_Z_RESOLVE:
    case PIPE_CAP_RESOURCE_FROM_USER_MEMORY:
    case PIPE_CAP_DEVICE_RESET_STATUS_QUERY:
@@ -337,7 +339,6 @@ llvmpipe_get_param(struct pipe_screen *screen, enum pipe_cap param)
    case PIPE_CAP_STRING_MARKER:
    case PIPE_CAP_BUFFER_SAMPLER_VIEW_RGBA_ONLY:
    case PIPE_CAP_SURFACE_REINTERPRET_BLOCKS:
-   case PIPE_CAP_QUERY_BUFFER_OBJECT:
    case PIPE_CAP_QUERY_MEMORY_INFO:
    case PIPE_CAP_PCI_GROUP:
    case PIPE_CAP_PCI_BUS:

commit 770517bf1d37d6e6890b348c8770a26953f7c168

llvmpipe: add support for time elapsed queries.
It turns out for QBO you really need to explicitly support time
elapsed queries to avoid wierd interactions with the non-qbo
query paths.

Fixes: bb2261a905ca ("llvmpipe: initial query buffer object support. (v2)")
Reviewed-by: Michel Dänzer <mdaenzer@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11946>

src/gallium/drivers/llvmpipe/lp_query.c
@@ -139,6 +139,17 @@ llvmpipe_get_query_result(struct pipe_context *pipe,
          }
       }
       break;
+   case PIPE_QUERY_TIME_ELAPSED: {
+      uint64_t start = (uint64_t)-1, end = 0;
+      for (i = 0; i < num_threads; i++) {
+         if (pq->start[i] && pq->start[i] < start)
+            start = pq->start[i];
+         if (pq->end[i] && pq->end[i] > end)
+            end = pq->end[i];
+      }
+      *result = end - start;
+      break;
+   }
    case PIPE_QUERY_TIMESTAMP_DISJOINT: {
       struct pipe_query_data_timestamp_disjoint *td =
          (struct pipe_query_data_timestamp_disjoint *)vresult;
@@ -260,6 +271,17 @@ llvmpipe_get_query_result_resource(struct pipe_context *pipe,
             }
          }
          break;
+      case PIPE_QUERY_TIME_ELAPSED: {
+         uint64_t start = (uint64_t)-1, end = 0;
+         for (i = 0; i < num_threads; i++) {
+            if (pq->start[i] && pq->start[i] < start)
+               start = pq->start[i];
+            if (pq->end[i] && pq->end[i] > end)
+               end = pq->end[i];
+         }
+         value = end - start;
+         break;
+      }
       case PIPE_QUERY_SO_STATISTICS:
          value = pq->num_primitives_written[0];
          value2 = pq->num_primitives_generated[0];

src/gallium/drivers/llvmpipe/lp_rast.c
@@ -648,6 +648,9 @@ lp_rast_begin_query(struct lp_rasterizer_task *task,
    case PIPE_QUERY_PIPELINE_STATISTICS:
       pq->start[task->thread_index] = task->thread_data.ps_invocations;
       break;
+   case PIPE_QUERY_TIME_ELAPSED:
+      pq->start[task->thread_index] = os_time_get_nano();
+      break;
    default:
       assert(0);
       break;
@@ -675,6 +678,7 @@ lp_rast_end_query(struct lp_rasterizer_task *task,
       pq->start[task->thread_index] = 0;
       break;
    case PIPE_QUERY_TIMESTAMP:
+   case PIPE_QUERY_TIME_ELAPSED:
       pq->end[task->thread_index] = os_time_get_nano();
       break;
    case PIPE_QUERY_PIPELINE_STATISTICS:

src/gallium/drivers/llvmpipe/lp_screen.c
@@ -139,6 +139,7 @@ llvmpipe_get_param(struct pipe_screen *screen, enum pipe_cap param)
       return PIPE_MAX_COLOR_BUFS;
    case PIPE_CAP_OCCLUSION_QUERY:
    case PIPE_CAP_QUERY_TIMESTAMP:
+   case PIPE_CAP_QUERY_TIME_ELAPSED:
       return 1;
    case PIPE_CAP_QUERY_PIPELINE_STATISTICS:
       return 1;

src/gallium/drivers/llvmpipe/lp_setup.c
@@ -1627,7 +1627,8 @@ lp_setup_begin_query(struct lp_setup_context *setup,
    if (!(pq->type == PIPE_QUERY_OCCLUSION_COUNTER ||
          pq->type == PIPE_QUERY_OCCLUSION_PREDICATE ||
          pq->type == PIPE_QUERY_OCCLUSION_PREDICATE_CONSERVATIVE ||
-         pq->type == PIPE_QUERY_PIPELINE_STATISTICS))
+         pq->type == PIPE_QUERY_PIPELINE_STATISTICS ||
+         pq->type == PIPE_QUERY_TIME_ELAPSED))
       return;
 
    /* init the query to its beginning state */
@@ -1679,7 +1680,8 @@ lp_setup_end_query(struct lp_setup_context *setup, struct llvmpipe_query *pq)
           pq->type == PIPE_QUERY_OCCLUSION_PREDICATE ||
           pq->type == PIPE_QUERY_OCCLUSION_PREDICATE_CONSERVATIVE ||
           pq->type == PIPE_QUERY_PIPELINE_STATISTICS ||
-          pq->type == PIPE_QUERY_TIMESTAMP) {
+          pq->type == PIPE_QUERY_TIMESTAMP ||
+          pq->type == PIPE_QUERY_TIME_ELAPSED) {
          if (pq->type == PIPE_QUERY_TIMESTAMP &&
                !(setup->scene->tiles_x | setup->scene->tiles_y)) {
             /*
@@ -1715,7 +1717,8 @@ fail:
    if (pq->type == PIPE_QUERY_OCCLUSION_COUNTER ||
       pq->type == PIPE_QUERY_OCCLUSION_PREDICATE ||
       pq->type == PIPE_QUERY_OCCLUSION_PREDICATE_CONSERVATIVE ||
-      pq->type == PIPE_QUERY_PIPELINE_STATISTICS) {
+      pq->type == PIPE_QUERY_PIPELINE_STATISTICS ||
+      pq->type == PIPE_QUERY_TIME_ELAPSED) {
       unsigned i;
 
       /* remove from active binned query list */