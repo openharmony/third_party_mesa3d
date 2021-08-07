openharmony/third_party_mesa3d

commit 093f0b34fba6918dd6b76d912ff31b5aee98e6f6

spirv_to_dxil: expose version number
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12227>

src/microsoft/spirv_to_dxil/spirv_to_dxil.h
@@ -95,6 +95,9 @@ spirv_to_dxil(const uint32_t* words,
 void
 spirv_to_dxil_free(void* buffer);
 
+uint64_t
+spirv_to_dxil_get_version();
+
 #ifdef __cplusplus
 }
 #endif

src/microsoft/spirv_to_dxil/meson.build
@@ -21,10 +21,13 @@
 
 libspirv_to_dxil = both_libraries(
   'spirv_to_dxil',
-  files(
-    'spirv_to_dxil.c',
-    'spirv_to_dxil.h',
-  ),
+  [
+    files(
+      'spirv_to_dxil.c',
+      'spirv_to_dxil.h',
+    ),
+    sha1_h,
+  ],
   vs_module_defs : 'spirv_to_dxil.def',
   dependencies : [idep_nir, idep_libdxil_compiler],
   include_directories : [inc_include, inc_src, inc_compiler, inc_gallium],

src/microsoft/spirv_to_dxil/spirv_to_dxil.c
@@ -28,6 +28,8 @@
 #include "spirv/nir_spirv.h"
 #include "util/blob.h"
 
+#include "git_sha1.h"
+
 bool
 spirv_to_dxil(const uint32_t *words, size_t word_count,
               struct dxil_spirv_specialization *specializations,
@@ -172,4 +174,15 @@ void
 spirv_to_dxil_free(void* buffer)
 {
    free(buffer);
+}
+
+uint64_t
+spirv_to_dxil_get_version()
+{
+   const char sha1[] = MESA_GIT_SHA1;
+   const char* dash = strchr(sha1, '-');
+   if (dash) {
+      return strtoull(dash + 1, NULL, 16);
+   }
+   return 0;
 }

src/microsoft/spirv_to_dxil/spirv_to_dxil.def
@@ -1,3 +1,4 @@
 EXPORTS
     spirv_to_dxil
     spirv_to_dxil_free
+    spirv_to_dxil_get_version

commit cef5118cb94ed11a6f0341c85882b32bca1fce93

spirv_to_dxil: Fix missing-prototypes build error.
../src/microsoft/spirv_to_dxil/spirv_to_dxil.c: At top level:
../src/microsoft/spirv_to_dxil/spirv_to_dxil.c:200:1: error: no previous prototype for ‘spirv_to_dxil_get_version’ [-Werror=missing-prototypes]
  200 | spirv_to_dxil_get_version()
      | ^~~~~~~~~~~~~~~~~~~~~~~~~

Fixes: 093f0b34fba ("spirv_to_dxil: expose version number")
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12264>

src/microsoft/spirv_to_dxil/spirv_to_dxil.h
@@ -96,7 +96,7 @@ void
 spirv_to_dxil_free(void* buffer);
 
 uint64_t
-spirv_to_dxil_get_version();
+spirv_to_dxil_get_version(void);
 
 #ifdef __cplusplus
 }