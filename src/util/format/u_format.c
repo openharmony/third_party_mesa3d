openharmony/third_party_mesa3d

commit 81b5b5654596e4acdb6e6adc6775d8efb25d6c9a

util: Switch the non-block formats to unpacking rgba rows instead of rects.
We have only a few callers of unpack that do rects, so add a helper that
iterates over y adding the strides.  This saves us 36kb of generated code
and means that adding cpu-specific variants for RGBA format unpack will be
much simpler.

Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10014>

src/util/format/u_format.c
@@ -359,6 +359,47 @@ util_get_depth_format_mrd(const struct util_format_description *desc)
    return mrd;
 }
 
+void
+util_format_unpack_rgba_rect(enum pipe_format format,
+                   void *dst, unsigned dst_stride,
+                   const void *src, unsigned src_stride,
+                   unsigned w, unsigned h)
+{
+   const struct util_format_unpack_description *unpack =
+      util_format_unpack_description(format);
+
+   /* Optimized function for block-compressed formats */
+   if (unpack->unpack_rgba_rect) {
+      unpack->unpack_rgba_rect(dst, dst_stride, src, src_stride, w, h);
+   } else {
+     for (unsigned y = 0; y < h; y++) {
+        unpack->unpack_rgba(dst, src, w);
+        src = (const char *)src + src_stride;
+        dst = (char *)dst + dst_stride;
+     }
+  }
+}
+
+void
+util_format_unpack_rgba_8unorm_rect(enum pipe_format format,
+                   void *dst, unsigned dst_stride,
+                   const void *src, unsigned src_stride,
+                   unsigned w, unsigned h)
+{
+   const struct util_format_unpack_description *unpack =
+      util_format_unpack_description(format);
+
+   /* Optimized function for block-compressed formats */
+   if (unpack->unpack_rgba_8unorm_rect) {
+      unpack->unpack_rgba_8unorm_rect(dst, dst_stride, src, src_stride, w, h);
+   } else {
+     for (unsigned y = 0; y < h; y++) {
+        unpack->unpack_rgba_8unorm(dst, src, w);
+        src = (const char *)src + src_stride;
+        dst = (char *)dst + dst_stride;
+     }
+  }
+}
 
 void
 util_format_read_4(enum pipe_format format,
@@ -367,8 +408,6 @@ util_format_read_4(enum pipe_format format,
                    unsigned x, unsigned y, unsigned w, unsigned h)
 {
    const struct util_format_description *format_desc;
-   const struct util_format_unpack_description *unpack =
-      util_format_unpack_description(format);
    const uint8_t *src_row;
 
    format_desc = util_format_description(format);
@@ -378,7 +417,7 @@ util_format_read_4(enum pipe_format format,
 
    src_row = (const uint8_t *)src + y*src_stride + x*(format_desc->block.bits/8);
 
-   unpack->unpack_rgba(dst, dst_stride, src_row, src_stride, w, h);
+   util_format_unpack_rgba_rect(format, dst, dst_stride, src_row, src_stride, w, h);
 }
 
 
@@ -413,10 +452,7 @@ void
 util_format_read_4ub(enum pipe_format format, uint8_t *dst, unsigned dst_stride, const void *src, unsigned src_stride, unsigned x, unsigned y, unsigned w, unsigned h)
 {
    const struct util_format_description *format_desc;
-   const struct util_format_unpack_description *unpack =
-      util_format_unpack_description(format);
    const uint8_t *src_row;
-   uint8_t *dst_row;
 
    format_desc = util_format_description(format);
 
@@ -424,9 +460,8 @@ util_format_read_4ub(enum pipe_format format, uint8_t *dst, unsigned dst_stride,
    assert(y % format_desc->block.height == 0);
 
    src_row = (const uint8_t *)src + y*src_stride + x*(format_desc->block.bits/8);
-   dst_row = dst;
 
-   unpack->unpack_rgba_8unorm(dst_row, dst_stride, src_row, src_stride, w, h);
+   util_format_unpack_rgba_8unorm_rect(format, dst, dst_stride, src_row, src_stride, w, h);
 }
 
 
@@ -715,7 +750,7 @@ util_format_translate(enum pipe_format dst_format,
          return FALSE;
 
       while (height >= y_step) {
-         unpack->unpack_rgba_8unorm(tmp_row, tmp_stride, src_row, src_stride, width, y_step);
+         util_format_unpack_rgba_8unorm_rect(src_format, tmp_row, tmp_stride, src_row, src_stride, width, y_step);
          pack->pack_rgba_8unorm(dst_row, dst_stride, tmp_row, tmp_stride, width, y_step);
 
          dst_row += dst_step;
@@ -724,7 +759,7 @@ util_format_translate(enum pipe_format dst_format,
       }
 
       if (height) {
-         unpack->unpack_rgba_8unorm(tmp_row, tmp_stride, src_row, src_stride, width, height);
+         util_format_unpack_rgba_8unorm_rect(src_format, tmp_row, tmp_stride, src_row, src_stride, width, height);
          pack->pack_rgba_8unorm(dst_row, dst_stride, tmp_row, tmp_stride, width, height);
       }
 
@@ -746,7 +781,7 @@ util_format_translate(enum pipe_format dst_format,
          return FALSE;
 
       while (height >= y_step) {
-         unpack->unpack_rgba(tmp_row, tmp_stride, src_row, src_stride, width, y_step);
+         util_format_unpack_rgba_rect(src_format, tmp_row, tmp_stride, src_row, src_stride, width, y_step);
          pack->pack_rgba_sint(dst_row, dst_stride, tmp_row, tmp_stride, width, y_step);
 
          dst_row += dst_step;
@@ -755,7 +790,7 @@ util_format_translate(enum pipe_format dst_format,
       }
 
       if (height) {
-         unpack->unpack_rgba(tmp_row, tmp_stride, src_row, src_stride, width, height);
+         util_format_unpack_rgba_rect(src_format, tmp_row, tmp_stride, src_row, src_stride, width, height);
          pack->pack_rgba_sint(dst_row, dst_stride, tmp_row, tmp_stride, width, height);
       }
 
@@ -777,7 +812,7 @@ util_format_translate(enum pipe_format dst_format,
          return FALSE;
 
       while (height >= y_step) {
-         unpack->unpack_rgba(tmp_row, tmp_stride, src_row, src_stride, width, y_step);
+         util_format_unpack_rgba_rect(src_format, tmp_row, tmp_stride, src_row, src_stride, width, y_step);
          pack->pack_rgba_uint(dst_row, dst_stride, tmp_row, tmp_stride, width, y_step);
 
          dst_row += dst_step;
@@ -786,7 +821,7 @@ util_format_translate(enum pipe_format dst_format,
       }
 
       if (height) {
-         unpack->unpack_rgba(tmp_row, tmp_stride, src_row, src_stride, width, height);
+         util_format_unpack_rgba_rect(src_format, tmp_row, tmp_stride, src_row, src_stride, width, height);
          pack->pack_rgba_uint(dst_row, dst_stride, tmp_row, tmp_stride, width, height);
       }
 
@@ -807,7 +842,7 @@ util_format_translate(enum pipe_format dst_format,
          return FALSE;
 
       while (height >= y_step) {
-         unpack->unpack_rgba(tmp_row, tmp_stride, src_row, src_stride, width, y_step);
+         util_format_unpack_rgba_rect(src_format, tmp_row, tmp_stride, src_row, src_stride, width, y_step);
          pack->pack_rgba_float(dst_row, dst_stride, tmp_row, tmp_stride, width, y_step);
 
          dst_row += dst_step;
@@ -816,7 +851,7 @@ util_format_translate(enum pipe_format dst_format,
       }
 
       if (height) {
-         unpack->unpack_rgba(tmp_row, tmp_stride, src_row, src_stride, width, height);
+         util_format_unpack_rgba_rect(src_format, tmp_row, tmp_stride, src_row, src_stride, width, height);
          pack->pack_rgba_float(dst_row, dst_stride, tmp_row, tmp_stride, width, height);
       }
 

src/gallium/auxiliary/translate/translate_generic.c
@@ -50,9 +50,8 @@ struct translate_generic {
    struct {
       enum translate_element_type type;
 
-      void (*fetch)(void *restrict dst, unsigned dst_stride,
-                    const uint8_t *restrict src, unsigned src_stride,
-                    unsigned width, unsigned height);
+      void (*fetch)(void *restrict dst, const uint8_t *restrict src,
+                    unsigned width);
       unsigned buffer;
       unsigned input_offset;
       unsigned instance_divisor;
@@ -625,7 +624,7 @@ generic_run_one(struct translate_generic *tg,
          if (likely(copy_size >= 0)) {
             memcpy(dst, src, copy_size);
          } else {
-            tg->attrib[attr].fetch(data, 0, src, 0, 1, 1);
+            tg->attrib[attr].fetch(data, src, 1);
 
             if (0)
                debug_printf("Fetch linear attr %d  from %p  stride %d  index %d: "

src/gallium/frontends/nine/device9.c
@@ -848,16 +848,13 @@ NineDevice9_SetCursorProperties( struct NineDevice9 *This,
     {
         D3DLOCKED_RECT lock;
         HRESULT hr;
-        const struct util_format_unpack_description *unpack =
-            util_format_unpack_description(surf->base.info.format);
-        assert(unpack);
 
         hr = NineSurface9_LockRect(surf, &lock, NULL, D3DLOCK_READONLY);
         if (FAILED(hr))
             ret_err("Failed to map cursor source image.\n",
                     D3DERR_DRIVERINTERNALERROR);
 
-        unpack->unpack_rgba_8unorm(ptr, transfer->stride,
+        util_format_unpack_rgba_8unorm_rect(surf->base.info.format, ptr, transfer->stride,
                                    lock.pBits, lock.Pitch,
                                    This->cursor.w, This->cursor.h);
 
@@ -865,7 +862,8 @@ NineDevice9_SetCursorProperties( struct NineDevice9 *This,
             void *data = lock.pBits;
             /* SetCursor assumes 32x32 argb with pitch 128 */
             if (lock.Pitch != 128) {
-                unpack->unpack_rgba_8unorm(This->cursor.hw_upload_temp, 128,
+                util_format_unpack_rgba_8unorm_rect(surf->base.info.format,
+                                           This->cursor.hw_upload_temp, 128,
                                            lock.pBits, lock.Pitch,
                                            32, 32);
                 data = This->cursor.hw_upload_temp;

src/mesa/main/pack.c
@@ -1641,7 +1641,7 @@ _mesa_unpack_ubyte_rgba_row(mesa_format format, uint32_t n,
       util_format_unpack_description((enum pipe_format)format);
 
    if (unpack->unpack_rgba_8unorm) {
-      unpack->unpack_rgba_8unorm((uint8_t *)dst, 0, src, 0, n, 1);
+      unpack->unpack_rgba_8unorm((uint8_t *)dst, src, n);
    } else {
       /* get float values, convert to ubyte */
       {

src/util/format/u_format.h
@@ -315,10 +315,20 @@ struct util_format_unpack_description {
     * Unpack pixel blocks to R8G8B8A8_UNORM.
     * Note: strides are in bytes.
     *
-    * Only defined for non-depth-stencil formats.
+    * Only defined for non-block non-depth-stencil formats.
     */
    void
-   (*unpack_rgba_8unorm)(uint8_t *restrict dst, unsigned dst_stride,
+   (*unpack_rgba_8unorm)(uint8_t *restrict dst, const uint8_t *restrict src,
+                         unsigned width);
+
+   /**
+    * Unpack pixel blocks to R8G8B8A8_UNORM.
+    * Note: strides are in bytes.
+    *
+    * Only defined for block non-depth-stencil formats.
+    */
+   void
+   (*unpack_rgba_8unorm_rect)(uint8_t *restrict dst, unsigned dst_stride,
                          const uint8_t *restrict src, unsigned src_stride,
                          unsigned width, unsigned height);
 
@@ -338,10 +348,22 @@ struct util_format_unpack_description {
     *
     * Note: strides are in bytes.
     *
-    * Only defined for non-depth-stencil formats.
+    * Only defined for non-block non-depth-stencil formats.
     */
    void
-   (*unpack_rgba)(void *restrict dst, unsigned dst_stride,
+   (*unpack_rgba)(void *restrict dst, const uint8_t *restrict src,
+                  unsigned width);
+
+   /**
+    * Unpack pixel blocks to R32G32B32A32_UINT/_INT_FLOAT based on whether the
+    * type is pure uint, int, or other.
+    *
+    * Note: strides are in bytes.
+    *
+    * Only defined for block non-depth-stencil formats.
+    */
+   void
+   (*unpack_rgba_rect)(void *restrict dst, unsigned dst_stride,
                   const uint8_t *restrict src, unsigned src_stride,
                   unsigned width, unsigned height);
 
@@ -1477,7 +1499,7 @@ util_format_unpack_rgba(enum pipe_format format, void *dst,
    const struct util_format_unpack_description *desc =
       util_format_unpack_description(format);
 
-   desc->unpack_rgba(dst, 0, (const uint8_t *)src, 0, w, 1);
+   desc->unpack_rgba(dst, (const uint8_t *)src, w);
 }
 
 static inline void
@@ -1557,6 +1579,18 @@ util_format_write_4ub(enum pipe_format format,
                       const uint8_t *src, unsigned src_stride, 
                       void *dst, unsigned dst_stride, 
                       unsigned x, unsigned y, unsigned w, unsigned h);
+
+void
+util_format_unpack_rgba_rect(enum pipe_format format,
+                             void *dst, unsigned dst_stride,
+                             const void *src, unsigned src_stride,
+                             unsigned w, unsigned h);
+
+void
+util_format_unpack_rgba_8unorm_rect(enum pipe_format format,
+                                    void *dst, unsigned dst_stride,
+                                    const void *src, unsigned src_stride,
+                                    unsigned w, unsigned h);
 
 /*
  * Generic format conversion;

src/util/format/u_format_bptc.c
@@ -62,10 +62,14 @@ util_format_bptc_rgba_unorm_unpack_rgba_float(void *restrict dst_row, unsigned d
    decompress_rgba_unorm(width, height,
                          src_row, src_stride,
                          temp_block, width * 4 * sizeof(uint8_t));
-   util_format_r8g8b8a8_unorm_unpack_rgba_float(
-                      dst_row, dst_stride,
-                      temp_block, width * 4 * sizeof(uint8_t),
-                      width, height);
+   /* Direct call to row unpack instead of util_format_rgba_unpack_rect()
+    * to avoid table lookup that would pull in all unpack symbols.
+    */
+   for (int y = 0; y < height; y++) {
+      util_format_r8g8b8a8_unorm_unpack_rgba_float((char *)dst_row + dst_stride * y,
+                                                    temp_block + 4 * width * y,
+                                                    width);
+   }
    free((void *) temp_block);
 }
 
@@ -76,10 +80,15 @@ util_format_bptc_rgba_unorm_pack_rgba_float(uint8_t *restrict dst_row, unsigned 
 {
    uint8_t *temp_block;
    temp_block = malloc(width * height * 4 * sizeof(uint8_t));
-   util_format_r32g32b32a32_float_unpack_rgba_8unorm(
-                        temp_block, width * 4 * sizeof(uint8_t),
-                        (uint8_t *)src_row, src_stride,
-                        width, height);
+   /* Direct call to row unpack instead of util_format_rgba_unpack_rect()
+    * to avoid table lookup that would pull in all unpack symbols.
+    */
+   for (int y = 0; y < height; y++) {
+      util_format_r32g32b32a32_float_unpack_rgba_8unorm(
+                        temp_block + 4 * width * y,
+                        (uint8_t *)src_row + src_stride * y,
+                        width);
+   }
    compress_rgba_unorm(width, height,
                        temp_block, width * 4 * sizeof(uint8_t),
                        dst_row, dst_stride);
@@ -131,9 +140,15 @@ util_format_bptc_srgba_unpack_rgba_float(void *restrict dst_row, unsigned dst_st
    decompress_rgba_unorm(width, height,
                          src_row, src_stride,
                          temp_block, width * 4 * sizeof(uint8_t));
-   util_format_r8g8b8a8_srgb_unpack_rgba_float(dst_row, dst_stride,
-                                               temp_block, width * 4 * sizeof(uint8_t),
-                                               width, height);
+
+   /* Direct call to row unpack instead of util_format_rgba_unpack_rect()
+    * to avoid table lookup that would pull in all unpack symbols.
+    */
+   for (int y = 0; y < height; y++) {
+      util_format_r8g8b8a8_srgb_unpack_rgba_float((char *)dst_row + dst_stride * y,
+                                                  temp_block + width * 4 * y,
+                                                  width);
+   }
 
    free((void *) temp_block);
 }
@@ -171,10 +186,15 @@ util_format_bptc_rgb_float_unpack_rgba_8unorm(uint8_t *restrict dst_row, unsigne
                         src_row, src_stride,
                         temp_block, width * 4 * sizeof(float),
                         true);
-   util_format_r32g32b32a32_float_unpack_rgba_8unorm(
-                        dst_row, dst_stride,
-                        (const uint8_t *)temp_block, width * 4 * sizeof(float),
-                        width, height);
+   /* Direct call to row unpack instead of util_format_rgba_unpack_rect()
+    * to avoid table lookup that would pull in all unpack symbols.
+    */
+   for (int y = 0; y < height; y++) {
+      util_format_r32g32b32a32_float_unpack_rgba_8unorm(
+          dst_row + dst_stride * y,
+          (const uint8_t *)temp_block + width * 4 * sizeof(float) * y,
+          width);
+   }
    free((void *) temp_block);
 }
 
@@ -229,10 +249,14 @@ util_format_bptc_rgb_ufloat_unpack_rgba_8unorm(uint8_t *restrict dst_row, unsign
                         src_row, src_stride,
                         temp_block, width * 4 * sizeof(float),
                         false);
-   util_format_r32g32b32a32_float_unpack_rgba_8unorm(
-                        dst_row, dst_stride,
-                        (const uint8_t *)temp_block, width * 4 * sizeof(float),
-                        width, height);
+   /* Direct call to row unpack instead of util_format_rgba_unpack_8unorm()
+    * to avoid table lookup that would pull in all unpack symbols.
+    */
+   for (int y = 0; y < height; y++) {
+      util_format_r32g32b32a32_float_unpack_rgba_8unorm(dst_row + dst_stride * y,
+                                                        (void *)(temp_block + 4 * width * y),
+                                                        width);
+   }
    free((void *) temp_block);
 }
 

src/util/format/u_format_other.c
@@ -33,23 +33,19 @@
 
 
 void
-util_format_r9g9b9e5_float_unpack_rgba_float(void *restrict dst_row, unsigned dst_stride,
-                                        const uint8_t *restrict src_row, unsigned src_stride,
-                                        unsigned width, unsigned height)
+util_format_r9g9b9e5_float_unpack_rgba_float(void *restrict dst_row,
+                                        const uint8_t *restrict src_row,
+                                        unsigned width)
 {
-   unsigned x, y;
-   for(y = 0; y < height; y += 1) {
-      float *dst = dst_row;
-      const uint8_t *src = src_row;
-      for(x = 0; x < width; x += 1) {
-         uint32_t value = util_cpu_to_le32(*(const uint32_t *)src);
-         rgb9e5_to_float3(value, dst);
-         dst[3] = 1; /* a */
-         src += 4;
-         dst += 4;
-      }
-      src_row += src_stride;
-      dst_row = (uint8_t *)dst_row + dst_stride;
+   unsigned x;
+   float *dst = dst_row;
+   const uint8_t *src = src_row;
+   for(x = 0; x < width; x += 1) {
+      uint32_t value = util_cpu_to_le32(*(const uint32_t *)src);
+      rgb9e5_to_float3(value, dst);
+      dst[3] = 1; /* a */
+      src += 4;
+      dst += 4;
    }
 }
 
@@ -85,27 +81,23 @@ util_format_r9g9b9e5_float_fetch_rgba(void *restrict in_dst, const uint8_t *rest
 
 
 void
-util_format_r9g9b9e5_float_unpack_rgba_8unorm(uint8_t *restrict dst_row, unsigned dst_stride,
-                                         const uint8_t *restrict src_row, unsigned src_stride,
-                                         unsigned width, unsigned height)
+util_format_r9g9b9e5_float_unpack_rgba_8unorm(uint8_t *restrict dst_row,
+                                         const uint8_t *restrict src_row,
+                                         unsigned width)
 {
-   unsigned x, y;
+   unsigned x;
    float p[3];
-   for(y = 0; y < height; y += 1) {
-      uint8_t *dst = dst_row;
-      const uint8_t *src = src_row;
-      for(x = 0; x < width; x += 1) {
-         uint32_t value = util_cpu_to_le32(*(const uint32_t *)src);
-         rgb9e5_to_float3(value, p);
-         dst[0] = float_to_ubyte(p[0]); /* r */
-         dst[1] = float_to_ubyte(p[1]); /* g */
-         dst[2] = float_to_ubyte(p[2]); /* b */
-         dst[3] = 255; /* a */
-         src += 4;
-         dst += 4;
-      }
-      src_row += src_stride;
-      dst_row += dst_stride/sizeof(*dst_row);
+   uint8_t *dst = dst_row;
+   const uint8_t *src = src_row;
+   for(x = 0; x < width; x += 1) {
+      uint32_t value = util_cpu_to_le32(*(const uint32_t *)src);
+      rgb9e5_to_float3(value, p);
+      dst[0] = float_to_ubyte(p[0]); /* r */
+      dst[1] = float_to_ubyte(p[1]); /* g */
+      dst[2] = float_to_ubyte(p[2]); /* b */
+      dst[3] = 255; /* a */
+      src += 4;
+      dst += 4;
    }
 }
 
@@ -137,23 +129,19 @@ util_format_r9g9b9e5_float_pack_rgba_8unorm(uint8_t *restrict dst_row, unsigned 
 
 
 void
-util_format_r11g11b10_float_unpack_rgba_float(void *restrict dst_row, unsigned dst_stride,
-                                        const uint8_t *restrict src_row, unsigned src_stride,
-                                        unsigned width, unsigned height)
+util_format_r11g11b10_float_unpack_rgba_float(void *restrict dst_row,
+                                        const uint8_t *restrict src_row,
+                                        unsigned width)
 {
-   unsigned x, y;
-   for(y = 0; y < height; y += 1) {
-      float *dst = dst_row;
-      const uint8_t *src = src_row;
-      for(x = 0; x < width; x += 1) {
-         uint32_t value = util_cpu_to_le32(*(const uint32_t *)src);
-         r11g11b10f_to_float3(value, dst);
-         dst[3] = 1; /* a */
-         src += 4;
-         dst += 4;
-      }
-      src_row += src_stride;
-      dst_row = (uint8_t *)dst_row + dst_stride;
+   unsigned x;
+   float *dst = dst_row;
+   const uint8_t *src = src_row;
+   for(x = 0; x < width; x += 1) {
+      uint32_t value = util_cpu_to_le32(*(const uint32_t *)src);
+      r11g11b10f_to_float3(value, dst);
+      dst[3] = 1; /* a */
+      src += 4;
+      dst += 4;
    }
 }
 
@@ -189,27 +177,23 @@ util_format_r11g11b10_float_fetch_rgba(void *restrict in_dst, const uint8_t *res
 
 
 void
-util_format_r11g11b10_float_unpack_rgba_8unorm(uint8_t *restrict dst_row, unsigned dst_stride,
-                                         const uint8_t *restrict src_row, unsigned src_stride,
-                                         unsigned width, unsigned height)
+util_format_r11g11b10_float_unpack_rgba_8unorm(uint8_t *restrict dst_row,
+                                         const uint8_t *restrict src_row,
+                                         unsigned width)
 {
-   unsigned x, y;
+   unsigned x;
    float p[3];
-   for(y = 0; y < height; y += 1) {
-      uint8_t *dst = dst_row;
-      const uint8_t *src = src_row;
-      for(x = 0; x < width; x += 1) {
-         uint32_t value = util_cpu_to_le32(*(const uint32_t *)src);
-         r11g11b10f_to_float3(value, p);
-         dst[0] = float_to_ubyte(p[0]); /* r */
-         dst[1] = float_to_ubyte(p[1]); /* g */
-         dst[2] = float_to_ubyte(p[2]); /* b */
-         dst[3] = 255; /* a */
-         src += 4;
-         dst += 4;
-      }
-      src_row += src_stride;
-      dst_row += dst_stride/sizeof(*dst_row);
+   uint8_t *dst = dst_row;
+   const uint8_t *src = src_row;
+   for(x = 0; x < width; x += 1) {
+      uint32_t value = util_cpu_to_le32(*(const uint32_t *)src);
+      r11g11b10f_to_float3(value, p);
+      dst[0] = float_to_ubyte(p[0]); /* r */
+      dst[1] = float_to_ubyte(p[1]); /* g */
+      dst[2] = float_to_ubyte(p[2]); /* b */
+      dst[3] = 255; /* a */
+      src += 4;
+      dst += 4;
    }
 }
 
@@ -256,58 +240,47 @@ r8g8bx_derive(int16_t r, int16_t g)
 }
 
 void
-util_format_r8g8bx_snorm_unpack_rgba_float(void *restrict dst_row, unsigned dst_stride,
-                                      const uint8_t *restrict src_row, unsigned src_stride,
-                                      unsigned width, unsigned height)
+util_format_r8g8bx_snorm_unpack_rgba_float(void *restrict dst_row,
+                                      const uint8_t *restrict src_row, unsigned width)
 {
-   unsigned x, y;
+   unsigned x;
+   float *dst = dst_row;
+   const uint16_t *src = (const uint16_t *)src_row;
+   for(x = 0; x < width; x += 1) {
+      uint16_t value = util_cpu_to_le16(*src++);
+      int16_t r, g;
 
-   for(y = 0; y < height; y += 1) {
-      float *dst = dst_row;
-      const uint16_t *src = (const uint16_t *)src_row;
-      for(x = 0; x < width; x += 1) {
-         uint16_t value = util_cpu_to_le16(*src++);
-         int16_t r, g;
+      r = ((int16_t)(value << 8)) >> 8;
+      g = ((int16_t)(value << 0)) >> 8;
 
-         r = ((int16_t)(value << 8)) >> 8;
-         g = ((int16_t)(value << 0)) >> 8;
-
-         dst[0] = (float)(r * (1.0f/0x7f)); /* r */
-         dst[1] = (float)(g * (1.0f/0x7f)); /* g */
-         dst[2] = r8g8bx_derive(r, g) * (1.0f/0xff); /* b */
-         dst[3] = 1.0f; /* a */
-         dst += 4;
-      }
-      src_row += src_stride;
-      dst_row = (uint8_t *)dst_row + dst_stride;
+      dst[0] = (float)(r * (1.0f/0x7f)); /* r */
+      dst[1] = (float)(g * (1.0f/0x7f)); /* g */
+      dst[2] = r8g8bx_derive(r, g) * (1.0f/0xff); /* b */
+      dst[3] = 1.0f; /* a */
+      dst += 4;
    }
 }
 
 
 void
-util_format_r8g8bx_snorm_unpack_rgba_8unorm(uint8_t *restrict dst_row, unsigned dst_stride,
-                                       const uint8_t *restrict src_row, unsigned src_stride,
-                                       unsigned width, unsigned height)
+util_format_r8g8bx_snorm_unpack_rgba_8unorm(uint8_t *restrict dst,
+                                       const uint8_t *restrict src_row,
+                                       unsigned width)
 {
-   unsigned x, y;
-   for(y = 0; y < height; y += 1) {
-      uint8_t *dst = dst_row;
-      const uint16_t *src = (const uint16_t *)src_row;
-      for(x = 0; x < width; x += 1) {
-         uint16_t value = util_cpu_to_le16(*src++);
-         int16_t r, g;
+   unsigned x;
+   const uint16_t *src = (const uint16_t *)src_row;
+   for(x = 0; x < width; x += 1) {
+      uint16_t value = util_cpu_to_le16(*src++);
+      int16_t r, g;
 
-         r = ((int16_t)(value << 8)) >> 8;
-         g = ((int16_t)(value << 0)) >> 8;
+      r = ((int16_t)(value << 8)) >> 8;
+      g = ((int16_t)(value << 0)) >> 8;
 
-         dst[0] = (uint8_t)(((uint16_t)MAX2(r, 0)) * 0xff / 0x7f); /* r */
-         dst[1] = (uint8_t)(((uint16_t)MAX2(g, 0)) * 0xff / 0x7f); /* g */
-         dst[2] = r8g8bx_derive(r, g); /* b */
-         dst[3] = 255; /* a */
-         dst += 4;
-      }
-      src_row += src_stride;
-      dst_row += dst_stride/sizeof(*dst_row);
+      dst[0] = (uint8_t)(((uint16_t)MAX2(r, 0)) * 0xff / 0x7f); /* r */
+      dst[1] = (uint8_t)(((uint16_t)MAX2(g, 0)) * 0xff / 0x7f); /* g */
+      dst[2] = r8g8bx_derive(r, g); /* b */
+      dst[3] = 255; /* a */
+      dst += 4;
    }
 }
 

commit a6594444ba05a0d75633325385b64a432ac27a81

util: Fix translate from block compressed to rgba
Since
81b5b565 util: Switch the non-block formats to unpacking rgba rows instead of rects,
compressed formats define unpack_rgba_8unorm_rect instead
of unpack_rgba_8unorm.

Fixes the u_format_translate check to take this into account.
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/5201
Fixes: 81b5b565 ("util: Switch the non-block formats to unpacking rgba rows instead of rects")

Signed-off-by: Axel Davy <davyaxel0@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12315>

src/util/format/u_format.c
@@ -740,7 +740,7 @@ util_format_translate(enum pipe_format dst_format,
       unsigned tmp_stride;
       uint8_t *tmp_row;
 
-      if (!unpack->unpack_rgba_8unorm ||
+      if ((!unpack->unpack_rgba_8unorm && !unpack->unpack_rgba_8unorm_rect) ||
           !pack->pack_rgba_8unorm) {
          return FALSE;
       }
@@ -802,7 +802,7 @@ util_format_translate(enum pipe_format dst_format,
       unsigned tmp_stride;
       unsigned int *tmp_row;
 
-      if (!unpack->unpack_rgba ||
+      if ((!unpack->unpack_rgba && !unpack->unpack_rgba_rect) ||
           !pack->pack_rgba_uint) {
          return FALSE;
       }
@@ -832,7 +832,7 @@ util_format_translate(enum pipe_format dst_format,
       unsigned tmp_stride;
       float *tmp_row;
 
-      if (!unpack->unpack_rgba ||
+      if ((!unpack->unpack_rgba && !unpack->unpack_rgba_rect) ||
           !pack->pack_rgba_float) {
          return FALSE;
       }