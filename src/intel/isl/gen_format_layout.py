openharmony/third_party_mesa3d

commit ec6a1660cfc382dda819de5e5e9edad456bae340

isl: Bring back isl_format_layout::bpb
A while ago we got rid of the bits-per-block because we thought we didn't
need it.  We're about to introduce some very useful 1 and 2-bit formats so
we really should be able to handle them again.

Reviewed-by: Chad Versace <chad.versace@intel.com>

src/intel/isl/gen_format_layout.py
@@ -67,7 +67,8 @@ TEMPLATE = template.Template(
           [ISL_FORMAT_${format.name}] = {
             .format = ISL_FORMAT_${format.name},
             .name = "ISL_FORMAT_${format.name}",
-            .bs = ${format.bs},
+            .bpb = ${format.bpb},
+            .bs = ${format.bpb // 8},
             .bw = ${format.bw},
             .bh = ${format.bh},
             .bd = ${format.bd},
@@ -137,7 +138,7 @@ class Format(object):
         self.name = line[0].strip()
 
         # Future division makes this work in python 2.
-        self.bs = int(line[1]) // 8
+        self.bpb = int(line[1])
         self.bw = line[2].strip()
         self.bh = line[3].strip()
         self.bd = line[4].strip()

src/intel/isl/isl.h
@@ -639,6 +639,7 @@ struct isl_format_layout {
    enum isl_format format;
    const char *name;
 
+   uint16_t bpb; /**< Bits per block */
    uint8_t bs; /**< Block size, in bytes, rounded towards 0 */
    uint8_t bw; /**< Block width, in pixels */
    uint8_t bh; /**< Block height, in pixels */

commit 00e799b4ed7ff91884b9dff8d034b1c84ca0c21b

isl: drop left-over comment
Fixes: ec6a1660cfc382dda819 ("isl: Bring back isl_format_layout::bpb")
Signed-off-by: Eric Engestrom <eric@engestrom.ch>
Reviewed-by: Alyssa Rosenzweig <alyssa@collabora.com>
Reviewed-by: Dylan Baker <dylan@pnwbakers.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/3674>

src/intel/isl/gen_format_layout.py
@@ -171,7 +171,6 @@ class Format(object):
         # pylint: disable=invalid-name
         self.name = line[0].strip()
 
-        # Future division makes this work in python 2.
         self.bpb = int(line[1])
         self.bw = line[2].strip()
         self.bh = line[3].strip()