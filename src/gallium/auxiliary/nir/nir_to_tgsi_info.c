openharmony/third_party_mesa3d

commit d77faaa2e95547d73104ef538b98e43ba28f02ca

gallivm/nir: fix vulkan vertex inputs
the tgsi file max is used to determine the number of input slots,
the old code was pretty bogus for the lavapipe cases (it worked
by accident).

Reviewed-by: Roland Scheidegger <sroland@vmware.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7416>

src/gallium/auxiliary/nir/nir_to_tgsi_info.c
@@ -577,9 +577,17 @@ void nir_tgsi_scan_shader(const struct nir_shader *nir,
       info->num_inputs = util_bitcount64(nir->info.inputs_read);
       if (nir->info.inputs_read_indirectly)
          info->indirect_files |= 1 << TGSI_FILE_INPUT;
+      info->file_max[TGSI_FILE_INPUT] = info->num_inputs - 1;
+   } else {
+      int max = -1;
+      nir_foreach_shader_in_variable(var, nir) {
+	 int slots = glsl_count_attribute_slots(var->type, false);
+	 int tmax = var->data.driver_location + slots - 1;
+	 if (tmax > max)
+	    max = tmax;
+	 info->file_max[TGSI_FILE_INPUT] = max;
+      }
    }
-
-   info->file_max[TGSI_FILE_INPUT] = info->num_inputs - 1;
 
    i = 0;
    uint64_t processed_outputs = 0;

commit 1ccb3ac6a80ad4be6b0424cedc41b07a50312095

gallium/nir/tgsi: initialize file_max for inputs
When this was rewritten to support Vulkan, we stopped initializing
file_max to -1 in the case of no inputs. This causes the draw module
to go down a needlessly pessimistic case, printing an error while we're
at it.

Fixes: d77faaa2e95 ("gallivm/nir: fix vulkan vertex inputs")
Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12440>

src/gallium/auxiliary/nir/nir_to_tgsi_info.c
@@ -579,7 +579,7 @@ void nir_tgsi_scan_shader(const struct nir_shader *nir,
          info->indirect_files |= 1 << TGSI_FILE_INPUT;
       info->file_max[TGSI_FILE_INPUT] = info->num_inputs - 1;
    } else {
-      int max = -1;
+      int max = info->file_max[TGSI_FILE_INPUT] = -1;
       nir_foreach_shader_in_variable(var, nir) {
          int slots = glsl_count_attribute_slots(var->type, false);
          int tmax = var->data.driver_location + slots - 1;