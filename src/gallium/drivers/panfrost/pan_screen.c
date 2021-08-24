openharmony/third_party_mesa3d

commit 9519d6881bd5b3f9a33e0d9d8305cb349acfeb79

panfrost: Init/destroy blitter from per-gen file
Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11785>

src/gallium/drivers/panfrost/pan_screen.c
@@ -43,7 +43,6 @@
 #include "drm-uapi/drm_fourcc.h"
 #include "drm-uapi/panfrost_drm.h"
 
-#include "pan_blitter.h"
 #include "pan_bo.h"
 #include "pan_shader.h"
 #include "pan_screen.h"
@@ -696,10 +695,11 @@ panfrost_destroy_screen(struct pipe_screen *pscreen)
         pan_indirect_dispatch_cleanup(dev);
         panfrost_cleanup_indirect_draw_shaders(dev);
         panfrost_pool_cleanup(&screen->indirect_draw.bin_pool);
-        pan_blitter_cleanup(dev);
         panfrost_pool_cleanup(&screen->blitter.bin_pool);
         panfrost_pool_cleanup(&screen->blitter.desc_pool);
         pan_blend_shaders_cleanup(dev);
+
+        screen->vtbl.screen_destroy(pscreen);
 
         if (dev->ro)
                 dev->ro->destroy(dev->ro);
@@ -892,9 +892,6 @@ panfrost_create_screen(int fd, struct renderonly *ro)
                            4096, "Blitter shaders", false, true);
         panfrost_pool_init(&screen->blitter.desc_pool, NULL, dev, 0, 65536,
                            "Blitter RSDs", false, true);
-        pan_blitter_init(dev, &screen->blitter.bin_pool.base,
-                         &screen->blitter.desc_pool.base);
-
         panfrost_cmdstream_screen_init(screen);
 
         return &screen->base;

src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -45,6 +45,7 @@
 #include "pan_util.h"
 #include "pan_indirect_draw.h"
 #include "pan_indirect_dispatch.h"
+#include "pan_blitter.h"
 
 #include "midgard_pack.h"
 
@@ -3682,13 +3683,26 @@ panfrost_get_sample_position(struct pipe_context *context,
                         out_value);
 }
 
+static void
+screen_destroy(struct pipe_screen *pscreen)
+{
+        struct panfrost_device *dev = pan_device(pscreen);
+        pan_blitter_cleanup(dev);
+}
+
 void
 panfrost_cmdstream_screen_init(struct panfrost_screen *screen)
 {
+        struct panfrost_device *dev = &screen->dev;
+
         screen->vtbl.prepare_rsd = prepare_rsd;
         screen->vtbl.emit_tls    = emit_tls;
         screen->vtbl.emit_fbd    = emit_fbd;
         screen->vtbl.emit_fragment_job = emit_fragment_job;
+        screen->vtbl.screen_destroy = screen_destroy;
+
+        pan_blitter_init(dev, &screen->blitter.bin_pool.base,
+                         &screen->blitter.desc_pool.base);
 }
 
 void

src/gallium/drivers/panfrost/pan_screen.h
@@ -63,6 +63,9 @@ struct panfrost_vtable {
 
         /* Emits a fragment job */
         mali_ptr (*emit_fragment_job)(struct panfrost_batch *, const struct pan_fb_info *);
+
+        /* General destructor */
+        void (*screen_destroy)(struct pipe_screen *);
 };
 
 struct panfrost_screen {

commit 29dd2f41aefc312749ebe3415208ca4cfec92c7f

panfrost: Fix NULL dereference in allowlist code
If a user attempts to run Panfrost on an unsupported GPU (e.g. Mali
T604), Panfrost will refuse to load and will destroy the screen
immediately, allowing for a graceful fallback to a software rasterizer.
However, the screen destroy code calls a screen_destroy function in the
GenXML vtbl -- and this function is still NULL when the allowlist is
checked. This manifests as crashes on unsuported GPUs.

Issue tracked down with Icecream95's mad Ghidra skills.

Closes: #5269
Fixes: 9519d6881bd ("panfrost: Init/destroy blitter from per-gen file")
Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Reported-by: Icecream95 <ixn@disroot.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12512>

src/gallium/drivers/panfrost/pan_screen.c
@@ -703,7 +703,8 @@ panfrost_destroy_screen(struct pipe_screen *pscreen)
         panfrost_pool_cleanup(&screen->blitter.desc_pool);
         pan_blend_shaders_cleanup(dev);
 
-        screen->vtbl.screen_destroy(pscreen);
+        if (screen->vtbl.screen_destroy)
+                screen->vtbl.screen_destroy(pscreen);
 
         if (dev->ro)
                 dev->ro->destroy(dev->ro);