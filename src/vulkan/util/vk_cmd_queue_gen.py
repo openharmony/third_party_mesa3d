openharmony/third_party_mesa3d

commit 65d6e9985a96b5d2345b6a4e400b826a220c2c96

vulkan: Generate entrypoints that enqueue commands
For drivers such as Lavapipe that record the commands at the execution
stage.

Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Reviewed-by: Antonio Caggiano <antonio.caggiano@collabora.com>
Acked-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12369>

src/vulkan/util/vk_commands_gen.py
@@ -1,0 +1,138 @@
+# coding=utf-8
+COPYRIGHT=u"""
+/* Copyright © 2015-2021 Intel Corporation
+ * Copyright © 2021 Collabora, Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+"""
+
+import argparse
+import os
+import re
+import xml.etree.ElementTree as et
+
+from mako.template import Template
+
+# Mesa-local imports must be declared in meson variable
+# '{file_without_suffix}_depend_files'.
+from vk_dispatch_table_gen import get_entrypoints_from_xml, EntrypointParam
+
+MANUAL_COMMANDS = ['CmdPushDescriptorSetKHR',             # This script doesn't know how to copy arrays in structs in arrays
+                   'CmdPushDescriptorSetWithTemplateKHR', # pData's size cannot be calculated from the xml
+                   'CmdDrawMultiEXT',                     # The size of the elements is specified in a stride param
+                   'CmdDrawMultiIndexedEXT',              # The size of the elements is specified in a stride param
+                   'CmdBindDescriptorSets',               # The VkPipelineLayout object could be released before the command is executed
+                   'CmdCopyImageToBuffer',                # There are wrappers that implement these in terms of the newer variants
+                   'CmdCopyImage',
+                   'CmdCopyBuffer',
+                   'CmdCopyImage',
+                   'CmdCopyBufferToImage',
+                   'CmdCopyImageToBuffer',
+                   'CmdBlitImage',
+                   'CmdResolveImage',
+                  ]
+
+TEMPLATE_C = Template(COPYRIGHT + """
+/* This file generated from ${filename}, don't edit directly. */
+
+#define VK_PROTOTYPES
+#include <vulkan/vulkan.h>
+
+#include "lvp_private.h"
+#include "pipe/p_context.h"
+#include "vk_util.h"
+
+% for c in commands:
+% if c.name in manual_commands:
+<% continue %>
+% endif
+% if c.guard is not None:
+#ifdef ${c.guard}
+% endif
+VKAPI_ATTR ${c.return_type} VKAPI_CALL lvp_${c.name} (VkCommandBuffer commandBuffer
+% for p in c.params[1:]:
+, ${p.decl}
+% endfor
+)
+{
+   LVP_FROM_HANDLE(lvp_cmd_buffer, cmd_buffer, commandBuffer);
+
+   vk_enqueue_${to_underscore(c.name)}(&cmd_buffer->queue
+% for p in c.params[1:]:
+, ${p.name}
+% endfor
+   );
+
+% if c.return_type == 'VkResult':
+   return VK_SUCCESS;
+% endif
+}
+% if c.guard is not None:
+#endif // ${c.guard}
+% endif
+% endfor
+
+""", output_encoding='utf-8')
+
+def to_underscore(name):
+    return re.sub('([A-Z]+)', r'_\1', name).lower().removeprefix('_')
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--out-c', required=True, help='Output C file.')
+    parser.add_argument('--xml',
+                        help='Vulkan API XML file.',
+                        required=True, action='append', dest='xml_files')
+    parser.add_argument('--prefix',
+                        help='Prefix to use for all dispatch tables.',
+                        action='append', default=[], dest='prefixes')
+    args = parser.parse_args()
+
+    commands = []
+    for e in get_entrypoints_from_xml(args.xml_files):
+        if e.name.startswith('Cmd') and \
+           not e.alias:
+            commands.append(e)
+
+    environment = {
+        'commands': commands,
+        'filename': os.path.basename(__file__),
+        'to_underscore': to_underscore,
+        'manual_commands': MANUAL_COMMANDS,
+    }
+
+    try:
+        with open(args.out_c, 'wb') as f:
+            f.write(TEMPLATE_C.render(**environment))
+    except Exception:
+        # In the event there's an error, this imports some helpers from mako
+        # to print a useful stack trace and prints it, then exits with
+        # status 1, if python is run with debug; otherwise it just raises
+        # the exception
+        if __debug__:
+            import sys
+            from mako import exceptions
+            sys.stderr.write(exceptions.text_error_template().render() + '\n')
+            sys.exit(1)
+        raise
+
+if __name__ == '__main__':
+    main()

src/vulkan/util/meson.build
@@ -38,10 +38,15 @@ vk_cmd_queue_gen_depend_files = [
   files('vk_dispatch_table_gen.py'),
   vk_dispatch_table_gen_depend_files,
 ]
+vk_commands_gen_depend_files = [
+  files('vk_dispatch_table_gen.py'),
+  vk_dispatch_table_gen_depend_files,
+]
 
 vk_entrypoints_gen = files('vk_entrypoints_gen.py')
 vk_extensions_gen = files('vk_extensions_gen.py')
 vk_icd_gen = files('vk_icd_gen.py')
+vk_commands_gen = files('vk_commands_gen.py')
 
 files_vulkan_util = files(
   'vk_alloc.c',

commit 2ce3ed2eb4f09a8f10aaa9bc1fd2bac81544a354

vulkan: Remove dependency on Python 3.9+
Closes #5311.

Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Fixes: 65d6e9985a96 ("vulkan: Generate entrypoints that enqueue commands")
Fixes: 2218f027417e ("vulkan: Generate code to place commands in a queue")
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12672>

src/vulkan/util/vk_cmd_queue_gen.py
@@ -215,7 +215,7 @@ vk_free_queue(struct vk_cmd_queue *queue)
       case ${to_enum_name(c.name)}:
 % for p in c.params[1:]:
 % if p.len:
-   vk_free(queue->alloc, (${p.decl.replace("const", "").removesuffix(p.name)})cmd->u.${to_struct_field_name(c.name)}.${to_field_name(p.name)});
+   vk_free(queue->alloc, (${remove_suffix(p.decl.replace("const", ""), p.name)})cmd->u.${to_struct_field_name(c.name)}.${to_field_name(p.name)});
 % elif '*' in p.decl:
    ${get_struct_free(c, p, types)}
 % endif
@@ -232,14 +232,24 @@ vk_free_queue(struct vk_cmd_queue *queue)
 
 """, output_encoding='utf-8')
 
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+
+def remove_suffix(text, suffix):
+    if text.endswith(suffix):
+        return text[:-len(suffix)]
+    return text
+
 def to_underscore(name):
-    return re.sub('([A-Z]+)', r'_\1', name).lower().removeprefix('_')
+    return remove_prefix(re.sub('([A-Z]+)', r'_\1', name).lower(), '_')
 
 def to_struct_field_name(name):
     return to_underscore(name).replace('cmd_', '')
 
 def to_field_name(name):
-    return to_underscore(name).replace('cmd_', '').removeprefix('p_')
+    return remove_prefix(to_underscore(name).replace('cmd_', ''), 'p_')
 
 def to_field_decl(decl):
     decl = decl.replace('const ', '')
@@ -262,7 +272,7 @@ def get_array_copy(command, param):
     else:
         field_size = "sizeof(*%s)" % field_name
     allocation = "%s = vk_zalloc(queue->alloc, %s * %s, 8, VK_SYSTEM_ALLOCATION_SCOPE_COMMAND);" % (field_name, field_size, param.len)
-    const_cast = param.decl.replace("const", "").removesuffix(param.name)
+    const_cast = remove_suffix(param.decl.replace("const", ""), param.name)
     copy = "memcpy((%s)%s, %s, %s * %s);" % (const_cast, field_name, param.name, field_size, param.len)
     return "%s\n   %s" % (allocation, copy)
 
@@ -270,7 +280,7 @@ def get_array_member_copy(command, param, member):
     field_name = "cmd->u.%s.%s->%s" % (to_struct_field_name(command.name), to_field_name(param.name), member.name)
     len_field_name = "cmd->u.%s.%s->%s" % (to_struct_field_name(command.name), to_field_name(param.name), member.len)
     allocation = "%s = vk_zalloc(queue->alloc, sizeof(*%s) * %s, 8, VK_SYSTEM_ALLOCATION_SCOPE_COMMAND);" % (field_name, field_name, len_field_name)
-    const_cast = member.decl.replace("const", "").removesuffix(member.name)
+    const_cast = remove_suffix(member.decl.replace("const", ""), member.name)
     copy = "memcpy((%s)%s, %s->%s, sizeof(*%s) * %s);" % (const_cast, field_name, param.name, member.name, field_name, len_field_name)
     return "%s\n   %s\n" % (allocation, copy)
     
@@ -278,7 +288,7 @@ def get_struct_copy(command, param, types):
     field_name = "cmd->u.%s.%s" % (to_struct_field_name(command.name), to_field_name(param.name))
 
     if param.type == "void":
-        const_cast = param.decl.replace("const", "").removesuffix(param.name)
+        const_cast = remove_suffix(param.decl.replace("const", ""), param.name)
         return "%s = (%s) %s;" % (field_name, const_cast, param.name)
 
     allocation = "%s = vk_zalloc(queue->alloc, sizeof(*%s), 8, VK_SYSTEM_ALLOCATION_SCOPE_COMMAND);" % (field_name, field_name)
@@ -296,7 +306,7 @@ def get_struct_copy(command, param, types):
 
 def get_struct_free(command, param, types):
     field_name = "cmd->u.%s.%s" % (to_struct_field_name(command.name), to_field_name(param.name))
-    const_cast = param.decl.replace("const", "").removesuffix(param.name)
+    const_cast = remove_suffix(param.decl.replace("const", ""), param.name)
     driver_data_free = "vk_free(queue->alloc, cmd->driver_data);\n"
     struct_free = "vk_free(queue->alloc, (%s)%s);" % (const_cast, field_name)
     member_frees = ""
@@ -304,7 +314,7 @@ def get_struct_free(command, param, types):
         for member in types[param.type]:
             if member.len and member.len != 'null-terminated':
                 member_name = "cmd->u.%s.%s->%s" % (to_struct_field_name(command.name), to_field_name(param.name), member.name)
-                const_cast = member.decl.replace("const", "").removesuffix(member.name)
+                const_cast = remove_suffix(member.decl.replace("const", ""), member.name)
                 member_frees += "vk_free(queue->alloc, (%s)%s);\n" % (const_cast, member_name)
     return "%s      %s      %s\n" % (member_frees, driver_data_free, struct_free)
 
@@ -369,6 +379,7 @@ def main():
         'get_struct_free': get_struct_free,
         'types': types,
         'manual_commands': MANUAL_COMMANDS,
+        'remove_suffix': remove_suffix,
     }
 
     try:

src/vulkan/util/vk_commands_gen.py
@@ -92,8 +92,13 @@ VKAPI_ATTR ${c.return_type} VKAPI_CALL lvp_${c.name} (VkCommandBuffer commandBuf
 
 """, output_encoding='utf-8')
 
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+
 def to_underscore(name):
-    return re.sub('([A-Z]+)', r'_\1', name).lower().removeprefix('_')
+    return remove_prefix(re.sub('([A-Z]+)', r'_\1', name).lower(), '_')
 
 def main():
     parser = argparse.ArgumentParser()