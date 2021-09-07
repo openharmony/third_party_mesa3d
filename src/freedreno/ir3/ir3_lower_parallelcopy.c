openharmony/third_party_mesa3d

commit 79f172694019734dad464951eec36c585ce1ce6c

ir3: Rewrite register allocation
Switch to the new SSA-based register allocator.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9842>

src/freedreno/ir3/ir3_lower_parallelcopy.c
@@ -1,0 +1,514 @@
+/*
+ * Copyright (C) 2021 Valve Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "ir3_ra.h"
+#include "ir3_shader.h"
+
+struct copy_src {
+	unsigned flags;
+	union {
+		uint32_t imm;
+		physreg_t reg;
+		unsigned const_num;
+	};
+};
+
+struct copy_entry {
+	physreg_t dst;
+	unsigned flags;
+	bool done;
+
+	struct copy_src src;
+};
+
+static unsigned
+copy_entry_size(const struct copy_entry *entry)
+{
+	return (entry->flags & IR3_REG_HALF) ? 1 : 2;
+}
+
+static struct copy_src
+get_copy_src(const struct ir3_register *reg, unsigned offset)
+{
+	if (reg->flags & IR3_REG_IMMED) {
+		return (struct copy_src) {
+			.flags = IR3_REG_IMMED,
+			.imm = reg->uim_val,
+		};
+	} else if (reg->flags & IR3_REG_CONST) {
+		return (struct copy_src) {
+			.flags = IR3_REG_CONST,
+			.const_num = reg->num,
+		};
+	} else {
+		return (struct copy_src) {
+			.flags = 0,
+			.reg = ra_reg_get_physreg(reg) + offset,
+		};
+	}
+}
+
+static void
+do_xor(struct ir3_instruction *instr, unsigned dst_num, unsigned src1_num, unsigned src2_num, unsigned flags)
+{
+	struct ir3_instruction *xor = ir3_instr_create(instr->block, OPC_XOR_B, 3);
+	struct ir3_register *dst = ir3_reg_create(xor, dst_num, flags | IR3_REG_DEST);
+	dst->wrmask = 1;
+	struct ir3_register *src1 = ir3_reg_create(xor, src1_num, flags);
+	src1->wrmask = 1;
+	struct ir3_register *src2 = ir3_reg_create(xor, src2_num, flags);
+	src2->wrmask = 1;
+
+	ir3_instr_move_before(xor, instr);
+}
+
+static void
+do_swap(struct ir3_instruction *instr, const struct copy_entry *entry)
+{
+	assert(!entry->src.flags);
+	/* TODO implement shared swaps */
+	assert(!(entry->flags & IR3_REG_SHARED));
+
+	if (entry->flags & IR3_REG_HALF) {
+		/* We currently make sure to never emit parallel copies where the
+		 * source/destination is a half-reg above the range accessable to half
+		 * registers. However, when a full-reg source overlaps a half-reg
+		 * destination or vice versa, it can be very, very complicated to come
+		 * up with a series of "legal" swaps and copies to resolve the
+		 * parallel copy. So here we provide a fallback to implement the
+		 * "illegal" swap instead. This may also be useful for implementing
+		 * "spilling" half-regs to the inaccessable space.
+		 */
+		if (entry->src.reg >= RA_HALF_SIZE) {
+			/* Choose a temporary that doesn't overlap src or dst */
+			physreg_t tmp = entry->dst < 2 ? 2 : 0;
+
+			/* Swap src and the temporary */
+			do_swap(instr, &(struct copy_entry) {
+				.src = { .reg = entry->src.reg & ~1u },
+				.dst = tmp,
+				.flags = entry->flags & ~IR3_REG_HALF,
+			});
+
+			/* Do the original swap with src replaced with tmp */
+			do_swap(instr, &(struct copy_entry) {
+				.src = { .reg = tmp + (entry->src.reg & 1) },
+				.dst = entry->dst,
+				.flags = entry->flags,
+			});
+
+			/* Swap src and the temporary back */
+			do_swap(instr, &(struct copy_entry) {
+				.src = { .reg = entry->src.reg & ~1u },
+				.dst = tmp,
+				.flags = entry->flags & ~IR3_REG_HALF,
+			});
+			return;
+		}
+
+		/* If dst is not addressable, we only need to swap the arguments and
+		 * let the case above handle it.
+		 */
+		if (entry->dst >= RA_HALF_SIZE) {
+			do_swap(instr, &(struct copy_entry) {
+				.src = { .reg = entry->dst },
+				.dst = entry->src.reg,
+				.flags = entry->flags,
+			});
+			return;
+		}
+	}
+
+	unsigned src_num = ra_physreg_to_num(entry->src.reg, entry->flags);
+	unsigned dst_num = ra_physreg_to_num(entry->dst, entry->flags);
+
+	do_xor(instr, dst_num, dst_num, src_num, entry->flags);
+	do_xor(instr, src_num, src_num, dst_num, entry->flags);
+	do_xor(instr, dst_num, dst_num, src_num, entry->flags);
+}
+
+static void
+do_copy(struct ir3_instruction *instr, const struct copy_entry *entry)
+{
+	/* TODO implement shared copies */
+	assert(!(entry->flags & IR3_REG_SHARED));
+
+	if (entry->flags & IR3_REG_HALF) {
+		/* See do_swap() for why this is here. */
+		if (entry->dst >= RA_HALF_SIZE) {
+			/* TODO: is there a hw instruction we can use for this case? */
+			physreg_t tmp = !entry->src.flags && entry->src.reg < 2 ? 2 : 0;
+
+			do_swap(instr, &(struct copy_entry) {
+				.src = { .reg = entry->dst & ~1u },
+				.dst = tmp,
+				.flags = entry->flags & ~IR3_REG_HALF,
+			});
+
+			do_copy(instr, &(struct copy_entry) {
+				.src = entry->src,
+				.dst = tmp + (entry->dst & 1),
+				.flags = entry->flags,
+			});
+
+			do_swap(instr, &(struct copy_entry) {
+				.src = { .reg = entry->dst & ~1u },
+				.dst = tmp,
+				.flags = entry->flags & ~IR3_REG_HALF,
+			});
+			return;
+		}
+
+		if (!entry->src.flags && entry->src.reg >= RA_HALF_SIZE) {
+			unsigned src_num =
+				ra_physreg_to_num(entry->src.reg & ~1u, entry->flags & ~IR3_REG_HALF);
+			unsigned dst_num = ra_physreg_to_num(entry->dst, entry->flags);
+			
+			if (entry->src.reg % 2 == 0) {
+				/* cov.u32u16 dst, src */
+				struct ir3_instruction *cov = ir3_instr_create(instr->block, OPC_MOV, 2);
+				ir3_reg_create(cov, dst_num, entry->flags | IR3_REG_DEST)->wrmask = 1;
+				ir3_reg_create(cov, src_num, entry->flags & ~IR3_REG_HALF)->wrmask = 1;
+				cov->cat1.dst_type = TYPE_U16;
+				cov->cat1.src_type = TYPE_U32;
+				ir3_instr_move_before(cov, instr);
+			} else {
+				/* shr.b dst, src, h(16) */
+				struct ir3_instruction *shr = ir3_instr_create(instr->block, OPC_SHR_B, 3);
+				ir3_reg_create(shr, dst_num, entry->flags | IR3_REG_DEST)->wrmask = 1;
+				ir3_reg_create(shr, src_num, entry->flags & ~IR3_REG_HALF)->wrmask = 1;
+				ir3_reg_create(shr, 0, entry->flags | IR3_REG_IMMED)->uim_val = 16;
+				ir3_instr_move_before(shr, instr);
+			}
+			return;
+		}
+	}
+
+	unsigned src_num = ra_physreg_to_num(entry->src.reg, entry->flags);
+	unsigned dst_num = ra_physreg_to_num(entry->dst, entry->flags);
+
+	struct ir3_instruction *mov = ir3_instr_create(instr->block, OPC_MOV, 2);
+	ir3_reg_create(mov, dst_num, entry->flags | IR3_REG_DEST)->wrmask = 1;
+	ir3_reg_create(mov, src_num, entry->flags | entry->src.flags)->wrmask = 1;
+	mov->cat1.dst_type = (entry->flags & IR3_REG_HALF) ? TYPE_U16 : TYPE_U32;
+	mov->cat1.src_type = (entry->flags & IR3_REG_HALF) ? TYPE_U16 : TYPE_U32;
+	if (entry->src.flags & IR3_REG_IMMED)
+		mov->regs[1]->uim_val = entry->src.imm;
+	else if (entry->src.flags & IR3_REG_CONST)
+		mov->regs[1]->num = entry->src.const_num;
+	ir3_instr_move_before(mov, instr);
+}
+
+struct copy_ctx {
+	/* For each physreg, the number of pending copy entries that use it as a
+	 * source. Once this drops to zero, then the physreg is unblocked and can
+	 * be moved to.
+	 */
+	unsigned physreg_use_count[RA_MAX_FILE_SIZE];
+
+	/* For each physreg, the pending copy_entry that uses it as a dest. */
+	struct copy_entry *physreg_dst[RA_MAX_FILE_SIZE];
+
+	struct copy_entry entries[RA_MAX_FILE_SIZE];
+	unsigned entry_count;
+};
+
+static bool
+entry_blocked(struct copy_entry *entry, struct copy_ctx *ctx)
+{
+	for (unsigned i = 0; i < copy_entry_size(entry); i++) {
+		if (ctx->physreg_use_count[entry->dst + i] != 0)
+			return true;
+	}
+
+	return false;
+}
+
+static void
+split_32bit_copy(struct copy_ctx *ctx, struct copy_entry *entry)
+{
+	assert(!entry->done);
+	assert(!(entry->flags & (IR3_REG_IMMED | IR3_REG_CONST)));
+	assert(copy_entry_size(entry) == 2);
+	struct copy_entry *new_entry = &ctx->entries[ctx->entry_count++];
+
+	new_entry->dst = entry->dst + 1;
+	new_entry->src.flags = entry->src.flags;
+	new_entry->src.reg = entry->src.reg + 1;
+	new_entry->done = false;
+	entry->flags |= IR3_REG_HALF;
+	new_entry->flags = entry->flags;
+	ctx->physreg_dst[entry->dst + 1] = new_entry;
+}
+
+static void
+_handle_copies(struct ir3_instruction *instr, struct copy_ctx *ctx)
+{
+	/* Set up the bookkeeping */
+	memset(ctx->physreg_dst, 0, sizeof(ctx->physreg_dst));
+	memset(ctx->physreg_use_count, 0, sizeof(ctx->physreg_use_count));
+
+	for (unsigned i = 0; i < ctx->entry_count; i++) {
+		struct copy_entry *entry = &ctx->entries[i];
+		for (unsigned j = 0; j < copy_entry_size(entry); j++) {
+			if (!entry->src.flags)
+				ctx->physreg_use_count[entry->src.reg + j]++;
+
+			/* Copies should not have overlapping destinations. */
+			assert(!ctx->physreg_dst[entry->dst + j]);
+			ctx->physreg_dst[entry->dst + j] = entry;
+		}
+	}
+
+	bool progress = true;
+	while (progress) {
+		progress = false;
+
+		/* Step 1: resolve paths in the transfer graph. This means finding
+		 * copies whose destination aren't blocked by something else and then
+		 * emitting them, continuing this process until every copy is blocked
+		 * and there are only cycles left.
+		 *
+		 * TODO: We should note that src is also available in dst to unblock
+		 * cycles that src is involved in.
+		 */
+
+		for (unsigned i = 0; i < ctx->entry_count; i++) {
+			struct copy_entry *entry = &ctx->entries[i];
+			if (!entry->done && !entry_blocked(entry, ctx)) {
+				entry->done = true;
+				progress = true;
+				do_copy(instr, entry);
+				for (unsigned j = 0; j < copy_entry_size(entry); j++) {
+					if (!entry->src.flags)
+						ctx->physreg_use_count[entry->src.reg + j]--;
+					ctx->physreg_dst[entry->dst + j] = NULL;
+				}
+			}
+		}
+
+		if (progress)
+			continue;
+
+		/* Step 2: Find partially blocked copies and split them. In the
+		 * mergedregs case, we can 32-bit copies which are only blocked on one
+		 * 16-bit half, and splitting them helps get things moving.
+		 *
+		 * We can skip splitting copies if the source isn't a register,
+		 * however, because it does not unblock anything and therefore doesn't
+		 * contribute to making forward progress with step 1. These copies
+		 * should still be resolved eventually in step 1 because they can't be
+		 * part of a cycle.
+		 */
+		for (unsigned i = 0; i < ctx->entry_count; i++) {
+			struct copy_entry *entry = &ctx->entries[i];
+			if (entry->done || entry->flags & IR3_REG_HALF)
+				continue;
+
+			if (((ctx->physreg_use_count[entry->dst] == 0 ||
+				  ctx->physreg_use_count[entry->dst + 1] == 0)) &&
+				 !(entry->flags & (IR3_REG_IMMED | IR3_REG_CONST))) {
+				split_32bit_copy(ctx, entry);
+				progress = true;
+			}
+		}
+	}
+
+	/* Step 3: resolve cycles through swapping.
+	 *
+	 * At this point, the transfer graph should consist of only cycles.
+	 * The reason is that, given any physreg n_1 that's the source of a
+	 * remaining entry, it has a destination n_2, which (because every
+	 * copy is blocked) is the source of some other copy whose destination
+	 * is n_3, and so we can follow the chain until we get a cycle. If we
+	 * reached some other node than n_1:
+	 *
+	 *  n_1 -> n_2 -> ... -> n_i
+	 *          ^             |
+	 *          |-------------|
+	 *
+	 *  then n_2 would be the destination of 2 copies, which is illegal
+	 *  (checked above in an assert). So n_1 must be part of a cycle:
+	 *
+	 *  n_1 -> n_2 -> ... -> n_i
+	 *  ^                     |
+	 *  |---------------------|
+	 *
+	 *  and this must be only cycle n_1 is involved in, because any other
+	 *  path starting from n_1 would also have to end in n_1, resulting in
+	 *  a node somewhere along the way being the destination of 2 copies
+	 *  when the 2 paths merge.
+	 *
+	 *  The way we resolve the cycle is through picking a copy (n_1, n_2)
+	 *  and swapping n_1 and n_2. This moves n_1 to n_2, so n_2 is taken
+	 *  out of the cycle:
+	 *
+	 *  n_1 -> ... -> n_i
+	 *  ^              |
+	 *  |--------------|
+	 *
+	 *  and we can keep repeating this until the cycle is empty.
+	 */
+
+	for (unsigned i = 0; i < ctx->entry_count; i++) {
+		struct copy_entry *entry = &ctx->entries[i];
+		if (entry->done)
+			continue;
+
+		assert(!entry->src.flags);
+
+		/* catch trivial copies */
+		if (entry->dst == entry->src.reg) {
+			entry->done = true;
+			continue;
+		}
+
+		do_swap(instr, entry);
+
+		/* Split any blocking copies whose sources are only partially
+		 * contained within our destination.
+		 */
+		if (entry->flags & IR3_REG_HALF) {
+			for (unsigned j = 0; j < ctx->entry_count; j++) {
+				struct copy_entry *blocking = &ctx->entries[j];
+
+				if (blocking->done)
+					continue;
+
+				if (blocking->src.reg <= entry->dst &&
+					blocking->src.reg + 1 >= entry->dst &&
+					!(blocking->flags & IR3_REG_HALF)) {
+					split_32bit_copy(ctx, blocking);
+				}
+			}
+		}
+
+		/* Update sources of blocking copies.
+		 *
+		 * Note: at this point, every blocking copy's source should be
+		 * contained within our destination.
+		 */
+		for (unsigned j = 0; j < ctx->entry_count; j++) {
+			struct copy_entry *blocking = &ctx->entries[j];
+			if (blocking->src.reg >= entry->dst &&
+				blocking->src.reg < entry->dst + copy_entry_size(entry)) {
+				blocking->src.reg = entry->src.reg + (blocking->src.reg - entry->dst);
+			}
+		}
+	}
+}
+
+static void
+handle_copies(struct ir3_instruction *instr, struct copy_entry *entries,
+			  unsigned entry_count, bool mergedregs)
+{
+	struct copy_ctx ctx;	
+
+	if (mergedregs) {
+		/* Half regs and full regs are in the same file, so handle everything
+		 * at once.
+		 */
+		memcpy(ctx.entries, entries, sizeof(struct copy_entry) * entry_count);
+		ctx.entry_count = entry_count;
+		_handle_copies(instr, &ctx);
+	} else {
+		/* There may be both half copies and full copies, so we have to split
+		 * them up since they don't interfere.
+		 */
+		ctx.entry_count = 0;
+		for (unsigned i = 0; i < entry_count; i++) {
+			if (entries[i].flags & IR3_REG_HALF)
+				ctx.entries[ctx.entry_count++] = entries[i];
+		}
+		_handle_copies(instr, &ctx);
+
+		ctx.entry_count = 0;
+		for (unsigned i = 0; i < entry_count; i++) {
+			if (!(entries[i].flags & IR3_REG_HALF))
+				ctx.entries[ctx.entry_count++] = entries[i];
+		}
+		_handle_copies(instr, &ctx);
+	}
+}
+
+void
+ir3_lower_copies(struct ir3_shader_variant *v)
+{
+	DECLARE_ARRAY(struct copy_entry, copies);
+	copies_count = copies_sz = 0;
+	copies = NULL;
+
+	foreach_block (block, &v->ir->block_list) {
+		foreach_instr_safe (instr, &block->instr_list) {
+			if (instr->opc == OPC_META_PARALLEL_COPY) {
+				copies_count = 0;
+				for (unsigned i = 0; i < instr->regs_count / 2; i++) {
+					struct ir3_register *dst = instr->regs[i];
+					struct ir3_register *src = instr->regs[i + instr->regs_count / 2];
+					unsigned flags = src->flags & (IR3_REG_HALF | IR3_REG_SHARED);
+					for (unsigned j = 0; j < reg_elems(dst); j++) {
+						array_insert(NULL, copies, (struct copy_entry) {
+							.dst = ra_num_to_physreg(dst->num + j, flags),
+							.src = get_copy_src(src, j * reg_elem_size(dst)),
+							.flags = flags,
+						});
+					}
+				}
+				handle_copies(instr, copies, copies_count, v->mergedregs);
+				list_del(&instr->node);
+			} else if (instr->opc == OPC_META_COLLECT) {
+				copies_count = 0;
+				struct ir3_register *dst = instr->regs[0];
+				unsigned flags = dst->flags & (IR3_REG_HALF | IR3_REG_SHARED);
+				for (unsigned i = 1; i < instr->regs_count; i++) {
+					struct ir3_register *src = instr->regs[i];
+					array_insert(NULL, copies, (struct copy_entry) {
+						.dst = ra_num_to_physreg(dst->num + i - 1, flags),
+						.src = get_copy_src(src, 0),
+						.flags = flags,
+					});
+				}
+				handle_copies(instr, copies, copies_count, v->mergedregs);
+				list_del(&instr->node);
+			} else if (instr->opc == OPC_META_SPLIT) {
+				copies_count = 0;
+				struct ir3_register *dst = instr->regs[0];
+				struct ir3_register *src = instr->regs[1];
+				unsigned flags = src->flags & (IR3_REG_HALF | IR3_REG_SHARED);
+				array_insert(NULL, copies, (struct copy_entry) {
+					.dst = ra_reg_get_physreg(dst),
+					.src = get_copy_src(src, instr->split.off * reg_elem_size(dst)),
+					.flags = flags,
+				});
+				handle_copies(instr, copies, copies_count, v->mergedregs);
+				list_del(&instr->node);
+			} else if (instr->opc == OPC_META_PHI) {
+				list_del(&instr->node);
+			}
+		}
+	}
+
+	if (copies)
+		ralloc_free(copies);
+}
+

src/freedreno/ci/traces-freedreno.yml
@@ -435,11 +435,11 @@ traces:
   - path: minetest/minetest.trace
     expectations:
       - device: freedreno-a306
-        checksum: 9227cc8d4e6445f2323438340f2a5d9b
+        checksum: daedbc987cc1b1f934364ce6b633bc54
       - device: freedreno-a530
-        checksum: d0d655d81fabeb4087bf7c4837301f2a
+        checksum: 0054f0ba67ace5d2defe17b74b5364e9
       - device: freedreno-a630
-        checksum: c7349124612a8760ddd825b903561ec4
+        checksum: b47f8151d4310d87070deea9059d001b
   - path: neverball/neverball.trace
     expectations:
       # Skipped since it's long on a530.

src/freedreno/ir3/ir3.h
@@ -83,6 +83,17 @@ struct ir3_info {
 	uint16_t instrs_per_cat[8];
 };
 
+struct ir3_merge_set {
+	uint16_t preferred_reg;
+	uint16_t size;
+	uint16_t alignment;
+
+	unsigned interval_start;
+
+	unsigned regs_count;
+	struct ir3_register **regs;
+};
+
 struct ir3_register {
 	enum {
 		IR3_REG_CONST  = 0x001,
@@ -119,6 +130,9 @@ struct ir3_register {
 		IR3_REG_ARRAY  = 0x8000,
 
 		IR3_REG_DEST   = 0x10000,
+		IR3_REG_KILL = 0x20000,
+		IR3_REG_FIRST_KILL = 0x40000,
+		IR3_REG_UNUSED = 0x80000,
 	} flags;
 
 	/* used for cat5 instructions, but also for internal/IR level
@@ -142,6 +156,7 @@ struct ir3_register {
 	 * rN.x becomes: (N << 2) | x
 	 */
 	uint16_t num;
+	uint16_t name;
 	union {
 		/* immediate: */
 		int32_t  iim_val;
@@ -169,6 +184,10 @@ struct ir3_register {
 	 * back to a previous instruction that we depend on).
 	 */
 	struct ir3_register *def;
+
+	unsigned merge_set_offset;
+	struct ir3_merge_set *merge_set;
+	unsigned interval_start, interval_end;
 };
 
 /*
@@ -178,12 +197,12 @@ struct ir3_register {
 	unsigned name ## _count, name ## _sz; \
 	type * name;
 
-#define array_insert(ctx, arr, val) do { \
+#define array_insert(ctx, arr, ...) do { \
 		if (arr ## _count == arr ## _sz) { \
 			arr ## _sz = MAX2(2 * arr ## _sz, 16); \
 			arr = reralloc_size(ctx, arr, arr ## _sz * sizeof(arr[0])); \
 		} \
-		arr[arr ##_count++] = val; \
+		arr[arr ##_count++] = __VA_ARGS__; \
 	} while (0)
 
 struct ir3_instruction {
@@ -696,12 +715,12 @@ bool ir3_valid_flags(struct ir3_instruction *instr, unsigned n, unsigned flags);
 		set_foreach ((__instr)->uses, __entry) \
 			if ((__use = (void *)__entry->key))
 
-static inline uint32_t reg_num(struct ir3_register *reg)
+static inline uint32_t reg_num(const struct ir3_register *reg)
 {
 	return reg->num >> 2;
 }
 
-static inline uint32_t reg_comp(struct ir3_register *reg)
+static inline uint32_t reg_comp(const struct ir3_register *reg)
 {
 	return reg->num & 0x3;
 }
@@ -1479,9 +1498,6 @@ bool ir3_cp_postsched(struct ir3 *ir);
 /* Make arrays SSA */
 bool ir3_array_to_ssa(struct ir3 *ir);
 
-/* group neighbors and insert mov's to resolve conflicts: */
-bool ir3_group(struct ir3 *ir);
-
 /* scheduling: */
 bool ir3_sched_add_deps(struct ir3 *ir);
 int ir3_sched(struct ir3 *ir);
@@ -1489,11 +1505,8 @@ int ir3_sched(struct ir3 *ir);
 struct ir3_context;
 bool ir3_postsched(struct ir3 *ir, struct ir3_shader_variant *v);
 
-bool ir3_a6xx_fixup_atomic_dests(struct ir3 *ir, struct ir3_shader_variant *so);
-
 /* register assignment: */
-struct ir3_ra_reg_set * ir3_ra_alloc_reg_set(struct ir3_compiler *compiler, bool mergedregs);
-int ir3_ra(struct ir3_shader_variant *v, struct ir3_instruction **precolor, unsigned nprecolor);
+int ir3_ra(struct ir3_shader_variant *v);
 
 /* legalize: */
 bool ir3_legalize(struct ir3 *ir, struct ir3_shader_variant *so, int *max_bary);

src/freedreno/ir3/ir3_a6xx.c
@@ -125,9 +125,9 @@ emit_intrinsic_atomic_ssbo(struct ir3_context *ctx, nir_intrinsic_instr *intr)
 	 *    src1.z  - is 'data' for cmpxchg
 	 *
 	 * The combining src and dest kinda doesn't work out so well with how
-	 * scheduling and RA work.  So for now we create a dummy src2.x, and
-	 * then in a later fixup path, insert an extra MOV out of src1.x.
-	 * See ir3_a6xx_fixup_atomic_dests().
+	 * scheduling and RA work. So we create a dummy src2 which is tied to the
+	 * destination in RA (i.e. must be allocated to the same vec2/vec3
+	 * register) and then immediately extract the first component.
 	 *
 	 * Note that nir already multiplies the offset by four
 	 */
@@ -193,7 +193,10 @@ emit_intrinsic_atomic_ssbo(struct ir3_context *ctx, nir_intrinsic_instr *intr)
 	/* even if nothing consume the result, we can't DCE the instruction: */
 	array_insert(b, b->keeps, atomic);
 
-	return atomic;
+	atomic->regs[0]->wrmask = src1->regs[0]->wrmask;
+	struct ir3_instruction *split;
+	ir3_split_dest(b, &split, atomic, 0, 1);
+	return split;
 }
 
 /* src[] = { deref, coord, sample_index }. const_index[] = {} */
@@ -270,9 +273,9 @@ emit_intrinsic_atomic_image(struct ir3_context *ctx, nir_intrinsic_instr *intr)
 	 *    src1.z  - is 'value' for cmpxchg
 	 *
 	 * The combining src and dest kinda doesn't work out so well with how
-	 * scheduling and RA work.  So for now we create a dummy src2.x, and
-	 * then in a later fixup path, insert an extra MOV out of src1.x.
-	 * See ir3_a6xx_fixup_atomic_dests().
+	 * scheduling and RA work. So we create a dummy src2 which is tied to the
+	 * destination in RA (i.e. must be allocated to the same vec2/vec3
+	 * register) and then immediately extract the first component.
 	 */
 	dummy = create_immed(b, 0);
 	src0 = ir3_create_collect(ctx, coords, ncoords);
@@ -341,7 +344,10 @@ emit_intrinsic_atomic_image(struct ir3_context *ctx, nir_intrinsic_instr *intr)
 	/* even if nothing consume the result, we can't DCE the instruction: */
 	array_insert(b, b->keeps, atomic);
 
-	return atomic;
+	atomic->regs[0]->wrmask = src1->regs[0]->wrmask;
+	struct ir3_instruction *split;
+	ir3_split_dest(b, &split, atomic, 0, 1);
+	return split;
 }
 
 static void
@@ -373,77 +379,3 @@ const struct ir3_context_funcs ir3_a6xx_funcs = {
 		.emit_intrinsic_image_size = emit_intrinsic_image_size,
 };
 
-/*
- * Special pass to run after instruction scheduling to insert an
- * extra mov from src1.x to dst.  This way the other compiler passes
- * can ignore this quirk of the new instruction encoding.
- *
- * This should run after RA.
- */
-
-static struct ir3_instruction *
-get_atomic_dest_mov(struct ir3_instruction *atomic)
-{
-	struct ir3_instruction *mov;
-
-	/* if we've already created the mov-out, then re-use it: */
-	if (atomic->data)
-		return atomic->data;
-
-	/* We are already out of SSA here, so we can't use the nice builders: */
-	mov = ir3_instr_create(atomic->block, OPC_MOV, 2);
-	ir3_reg_create(mov, 0, 0);    /* dst */
-	ir3_reg_create(mov, 0, 0);    /* src */
-
-	mov->cat1.src_type = TYPE_U32;
-	mov->cat1.dst_type = TYPE_U32;
-
-	/* extract back out the 'dummy' which serves as stand-in for dest: */
-	struct ir3_instruction *src = atomic->regs[3]->instr;
-	debug_assert(src->opc == OPC_META_COLLECT);
-
-	*mov->regs[0] = *atomic->regs[0];
-	*mov->regs[1] = *src->regs[1]->instr->regs[0];
-
-	mov->flags |= IR3_INSTR_SY;
-
-	/* it will have already been appended to the end of the block, which
-	 * isn't where we want it, so fix-up the location:
-	 */
-	ir3_instr_move_after(mov, atomic);
-
-	return atomic->data = mov;
-}
-
-bool
-ir3_a6xx_fixup_atomic_dests(struct ir3 *ir, struct ir3_shader_variant *so)
-{
-	bool progress = false;
-
-	if (ir3_shader_nibo(so) == 0 && !so->bindless_ibo)
-		return false;
-
-	foreach_block (block, &ir->block_list) {
-		foreach_instr (instr, &block->instr_list) {
-			instr->data = NULL;
-		}
-	}
-
-	foreach_block (block, &ir->block_list) {
-		foreach_instr_safe (instr, &block->instr_list) {
-			foreach_src (reg, instr) {
-				struct ir3_instruction *src = reg->instr;
-
-				if (!src)
-					continue;
-
-				if (is_atomic(src->opc) && (src->flags & IR3_INSTR_G)) {
-					reg->instr = get_atomic_dest_mov(src);
-					progress = true;
-				}
-			}
-		}
-	}
-
-	return progress;
-}

src/freedreno/ir3/ir3_compiler.c
@@ -78,7 +78,6 @@ ir3_compiler_create(struct fd_device *dev, uint32_t gpu_id, bool robust_ubo_acce
 	compiler->dev = dev;
 	compiler->gpu_id = gpu_id;
 	compiler->robust_ubo_access = robust_ubo_access;
-	compiler->set = ir3_ra_alloc_reg_set(compiler, false);
 
 	/* All known GPU's have 32k local memory (aka shared) */
 	compiler->local_mem_size = 32 * 1024;
@@ -88,7 +87,6 @@ ir3_compiler_create(struct fd_device *dev, uint32_t gpu_id, bool robust_ubo_acce
 	compiler->max_waves = 16;
 
 	if (compiler->gpu_id >= 600) {
-		compiler->mergedregs_set = ir3_ra_alloc_reg_set(compiler, true);
 		compiler->samgq_workaround = true;
 		/* a6xx split the pipeline state into geometry and fragment state, in
 		 * order to let the VS run ahead of the FS. As a result there are now

src/freedreno/ir3/ir3_compiler.h
@@ -38,8 +38,6 @@ struct ir3_shader;
 struct ir3_compiler {
 	struct fd_device *dev;
 	uint32_t gpu_id;
-	struct ir3_ra_reg_set *set;
-	struct ir3_ra_reg_set *mergedregs_set;
 	uint32_t shader_count;
 
 	struct disk_cache *disk_cache;

commit 3e4a4cba898215e43378c7e3227af69975b34b4f

ir3/lower_pcopy: Set entry->done in the swap loop
We process all the remaining copy entries one-at-a-time when emitting
swaps, so normally we wouldn't need to care about whether the entry is
done, except that we need to look at the other entries to split them up
and we need to skip this when the copy to be split has already been
processed.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/5307
Fixes: 79f17269401 ("ir3: Rewrite register allocation")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12747>

src/freedreno/ir3/ir3_lower_parallelcopy.c
@@ -451,6 +451,8 @@ _handle_copies(struct ir3_compiler *compiler, struct ir3_instruction *instr,
                entry->src.reg + (blocking->src.reg - entry->dst);
          }
       }
+
+      entry->done = true;
    }
 }
 