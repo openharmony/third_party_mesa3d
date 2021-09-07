openharmony/third_party_mesa3d

commit 97409da2a882280505803a35c81e2830ce748c84

llvmpipe: handle compute shader launch with 0 threads
If you set LP_NUM_THREADS=0 compute shaders would hang,
just execute the workloads in sequence if we have no threads
in the pool.

Fixes: ebc08e4e21 ("llvmpipe: add compute threadpool + mutex")
Reviewed-by: Roland Scheidegger <sroland@vmware.com>

src/gallium/drivers/llvmpipe/lp_cs_tpool.c
@@ -114,6 +114,15 @@ lp_cs_tpool_queue_task(struct lp_cs_tpool *pool,
 {
    struct lp_cs_tpool_task *task;
 
+   if (pool->num_threads == 0) {
+      struct lp_cs_local_mem lmem;
+
+      memset(&lmem, 0, sizeof(lmem));
+      for (unsigned t = 0; t < num_iters; t++) {
+         work(data, t, &lmem);
+      }
+      return NULL;
+   }
    task = CALLOC_STRUCT(lp_cs_tpool_task);
    if (!task) {
       return NULL;

commit 79c91ac09ea9524444565ba5200d957dcce82292

llvmpipe: Fix leak of CS local memory with 0 threads.
Fixes: 97409da2a882 ("llvmpipe: handle compute shader launch with 0 threads")
Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12724>

src/gallium/drivers/llvmpipe/lp_cs_tpool.c
@@ -121,6 +121,7 @@ lp_cs_tpool_queue_task(struct lp_cs_tpool *pool,
       for (unsigned t = 0; t < num_iters; t++) {
          work(data, t, &lmem);
       }
+      FREE(lmem.local_mem_ptr);
       return NULL;
    }
    task = CALLOC_STRUCT(lp_cs_tpool_task);