openharmony/third_party_mesa3d

commit 7289493428be3627a47c60edc8d4ee1c7b09772b

tu, freedreno/a6xx, ir3: Rewrite tess PrimID handling
The previous handling conflated RelPatchID and PrimID, which would
result in incorrect gl_PrimitiveID when doing draw splitting and didn't
work with PrimID passthrough which fills the VPC slot with the "correct"
PrimID value from the tess factor BO which we left 0. Replace PrimID in
the tess lowering pass with a new RelPatchID sysval, and relace PrimID
with RelPatchID in the VS input code in turnip/freedreno at the same
time so that there is no net change in the tess lowering code. However,
now we have to add new mechanisms for getting the user-level PrimID:

- In the TCS it comes from the VS, just like gl_PrimitiveIDIn in the GS.
  This means we have to add another register to our VS->TCS ABI. I
  decided to put PrimID in r0.z, after the TCS header and RelPatchID,
  because it might not be read in the TCS.
- If any stage after the TCS uses PrimID, the TCS stores it in the first
  dword of the tess factor BO, and it is read by the fixed-function
  tessellator and accessed in the TES via the newly-uncovered DSPRIMID
  field. If we have tess and GS, the TES passes this value through to
  the GS in the same way as the VS does. PrimID passthrough for reading
  it in the FS when there's tess but no GS also "just works" once we
  start storing it in the TCS. In particular this fixes
  dEQP-VK.pipeline.misc.primitive_id_from_tess which tests exactly that.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12166>

src/freedreno/vulkan/tu_pipeline.c
@@ -646,16 +646,24 @@ tu6_emit_vs_system_values(struct tu_cs *cs,
          tess_coord_x_regid + 1 :
          regid(63, 0);
    const uint32_t hs_rel_patch_regid = hs ?
-         ir3_find_sysval_regid(hs, SYSTEM_VALUE_PRIMITIVE_ID) :
+         ir3_find_sysval_regid(hs, SYSTEM_VALUE_REL_PATCH_ID_IR3) :
          regid(63, 0);
    const uint32_t ds_rel_patch_regid = hs ?
-         ir3_find_sysval_regid(ds, SYSTEM_VALUE_PRIMITIVE_ID) :
+         ir3_find_sysval_regid(ds, SYSTEM_VALUE_REL_PATCH_ID_IR3) :
          regid(63, 0);
    const uint32_t hs_invocation_regid = hs ?
          ir3_find_sysval_regid(hs, SYSTEM_VALUE_TCS_HEADER_IR3) :
          regid(63, 0);
-   const uint32_t primitiveid_regid = gs ?
+   const uint32_t gs_primitiveid_regid = gs ?
          ir3_find_sysval_regid(gs, SYSTEM_VALUE_PRIMITIVE_ID) :
+         regid(63, 0);
+   const uint32_t hs_primitiveid_regid = hs ?
+         ir3_find_sysval_regid(hs, SYSTEM_VALUE_PRIMITIVE_ID) :
+         regid(63, 0);
+   const uint32_t vs_primitiveid_regid = gs ? gs_primitiveid_regid :
+      hs_primitiveid_regid;
+   const uint32_t ds_primitiveid_regid = ds ?
+         ir3_find_sysval_regid(ds, SYSTEM_VALUE_PRIMITIVE_ID) :
          regid(63, 0);
    const uint32_t gsheader_regid = gs ?
          ir3_find_sysval_regid(gs, SYSTEM_VALUE_GS_HEADER_IR3) :
@@ -671,14 +679,14 @@ tu6_emit_vs_system_values(struct tu_cs *cs,
    tu_cs_emit_pkt4(cs, REG_A6XX_VFD_CONTROL_1, 6);
    tu_cs_emit(cs, A6XX_VFD_CONTROL_1_REGID4VTX(vertexid_regid) |
                   A6XX_VFD_CONTROL_1_REGID4INST(instanceid_regid) |
-                  A6XX_VFD_CONTROL_1_REGID4PRIMID(primitiveid_regid) |
+                  A6XX_VFD_CONTROL_1_REGID4PRIMID(vs_primitiveid_regid) |
                   A6XX_VFD_CONTROL_1_REGID4VIEWID(viewid_regid));
    tu_cs_emit(cs, A6XX_VFD_CONTROL_2_REGID_HSRELPATCHID(hs_rel_patch_regid) |
                   A6XX_VFD_CONTROL_2_REGID_INVOCATIONID(hs_invocation_regid));
    tu_cs_emit(cs, A6XX_VFD_CONTROL_3_REGID_DSRELPATCHID(ds_rel_patch_regid) |
                   A6XX_VFD_CONTROL_3_REGID_TESSX(tess_coord_x_regid) |
                   A6XX_VFD_CONTROL_3_REGID_TESSY(tess_coord_y_regid) |
-                  0xfc);
+                  A6XX_VFD_CONTROL_3_REGID_DSPRIMID(ds_primitiveid_regid));
    tu_cs_emit(cs, 0x000000fc); /* VFD_CONTROL_4 */
    tu_cs_emit(cs, A6XX_VFD_CONTROL_5_REGID_GSHEADER(gsheader_regid) |
                   0xfc00); /* VFD_CONTROL_5 */
@@ -2303,6 +2311,16 @@ tu_pipeline_builder_compile_shaders(struct tu_pipeline_builder *builder,
       if ((stage == MESA_SHADER_TESS_EVAL || stage == MESA_SHADER_TESS_CTRL) &&
           key.tessellation == IR3_TESS_NONE) {
          key.tessellation = tu6_get_tessmode(shader);
+      }
+
+      if (stage > MESA_SHADER_TESS_CTRL) {
+         if (stage == MESA_SHADER_FRAGMENT) {
+            key.tcs_store_primid = key.tcs_store_primid ||
+               (nir[stage]->info.inputs_read & (1ull << VARYING_SLOT_PRIMITIVE_ID));
+         } else {
+            key.tcs_store_primid = key.tcs_store_primid ||
+               BITSET_TEST(nir[stage]->info.system_values_read, SYSTEM_VALUE_PRIMITIVE_ID);
+         }
       }
 
       /* Keep track of the status of each shader's active descriptor sets,

src/compiler/nir/nir_intrinsics.py
@@ -1021,6 +1021,7 @@ system_value("hs_patch_stride_ir3", 1)
 system_value("tess_factor_base_ir3", 2)
 system_value("tess_param_base_ir3", 2)
 system_value("tcs_header_ir3", 1)
+system_value("rel_patch_id_ir3", 1)
 
 # System values for freedreno compute shaders.
 system_value("subgroup_id_shift_ir3", 1)

src/compiler/shader_enums.c
@@ -307,6 +307,7 @@ gl_system_value_name(gl_system_value sysval)
      ENUM(SYSTEM_VALUE_RAY_GEOMETRY_INDEX),
      ENUM(SYSTEM_VALUE_GS_HEADER_IR3),
      ENUM(SYSTEM_VALUE_TCS_HEADER_IR3),
+     ENUM(SYSTEM_VALUE_REL_PATCH_ID_IR3),
      ENUM(SYSTEM_VALUE_FRAG_SHADING_RATE),
    };
    STATIC_ASSERT(ARRAY_SIZE(names) == SYSTEM_VALUE_MAX);

src/compiler/shader_enums.h
@@ -779,6 +779,11 @@ typedef enum
    SYSTEM_VALUE_GS_HEADER_IR3,
    SYSTEM_VALUE_TCS_HEADER_IR3,
 
+   /* IR3 specific system value that contains the patch id for the current
+    * subdraw.
+    */
+   SYSTEM_VALUE_REL_PATCH_ID_IR3,
+
    /**
     * Fragment shading rate used for KHR_fragment_shading_rate (Vulkan).
     */

src/freedreno/ir3/ir3_compiler_nir.c
@@ -1703,7 +1703,15 @@ emit_intrinsic(struct ir3_context *ctx, nir_intrinsic_instr *intr)
       dst[0] = ctx->tcs_header;
       break;
 
+   case nir_intrinsic_load_rel_patch_id_ir3:
+      dst[0] = ctx->rel_patch_id;
+      break;
+
    case nir_intrinsic_load_primitive_id:
+      if (!ctx->primitive_id) {
+         ctx->primitive_id =
+            create_sysval_input(ctx, SYSTEM_VALUE_PRIMITIVE_ID, 0x1);
+      }
       dst[0] = ctx->primitive_id;
       break;
 
@@ -3734,6 +3742,8 @@ emit_instructions(struct ir3_context *ctx)
       if (has_tess) {
          ctx->tcs_header =
             create_sysval_input(ctx, SYSTEM_VALUE_TCS_HEADER_IR3, 0x1);
+         ctx->rel_patch_id =
+            create_sysval_input(ctx, SYSTEM_VALUE_REL_PATCH_ID_IR3, 0x1);
          ctx->primitive_id =
             create_sysval_input(ctx, SYSTEM_VALUE_PRIMITIVE_ID, 0x1);
       } else if (has_gs) {
@@ -3746,21 +3756,22 @@ emit_instructions(struct ir3_context *ctx)
    case MESA_SHADER_TESS_CTRL:
       ctx->tcs_header =
          create_sysval_input(ctx, SYSTEM_VALUE_TCS_HEADER_IR3, 0x1);
-      ctx->primitive_id =
-         create_sysval_input(ctx, SYSTEM_VALUE_PRIMITIVE_ID, 0x1);
+      ctx->rel_patch_id =
+         create_sysval_input(ctx, SYSTEM_VALUE_REL_PATCH_ID_IR3, 0x1);
       break;
    case MESA_SHADER_TESS_EVAL:
-      if (has_gs)
+      if (has_gs) {
          ctx->gs_header =
             create_sysval_input(ctx, SYSTEM_VALUE_GS_HEADER_IR3, 0x1);
-      ctx->primitive_id =
-         create_sysval_input(ctx, SYSTEM_VALUE_PRIMITIVE_ID, 0x1);
+         ctx->primitive_id =
+            create_sysval_input(ctx, SYSTEM_VALUE_PRIMITIVE_ID, 0x1);
+      }
+      ctx->rel_patch_id =
+         create_sysval_input(ctx, SYSTEM_VALUE_REL_PATCH_ID_IR3, 0x1);
       break;
    case MESA_SHADER_GEOMETRY:
       ctx->gs_header =
          create_sysval_input(ctx, SYSTEM_VALUE_GS_HEADER_IR3, 0x1);
-      ctx->primitive_id =
-         create_sysval_input(ctx, SYSTEM_VALUE_PRIMITIVE_ID, 0x1);
       break;
    default:
       break;
@@ -3986,6 +3997,19 @@ ir3_compile_shader_nir(struct ir3_compiler *compiler,
          struct ir3_instruction *out = ir3_collect(ctx, ctx->primitive_id);
          outputs[outputs_count] = out;
          outidxs[outputs_count] = n;
+         if (so->type == MESA_SHADER_VERTEX && ctx->rel_patch_id)
+            regids[outputs_count] = regid(0, 2);
+         else
+            regids[outputs_count] = regid(0, 1);
+         outputs_count++;
+      }
+
+      if (so->type == MESA_SHADER_VERTEX && ctx->rel_patch_id) {
+         unsigned n = so->outputs_count++;
+         so->outputs[n].slot = VARYING_SLOT_REL_PATCH_ID_IR3;
+         struct ir3_instruction *out = ir3_collect(ctx, ctx->rel_patch_id);
+         outputs[outputs_count] = out;
+         outidxs[outputs_count] = n;
          regids[outputs_count] = regid(0, 1);
          outputs_count++;
       }
@@ -4188,7 +4212,9 @@ ir3_compile_shader_nir(struct ir3_compiler *compiler,
        */
 
       ctx->tcs_header->dsts[0]->num = regid(0, 0);
-      ctx->primitive_id->dsts[0]->num = regid(0, 1);
+      ctx->rel_patch_id->dsts[0]->num = regid(0, 1);
+      if (ctx->primitive_id)
+         ctx->primitive_id->dsts[0]->num = regid(0, 2);
    } else if (ctx->gs_header) {
       /* We need to have these values in the same registers between producer
        * (VS or DS) and GS since the producer chains to GS and doesn't get
@@ -4196,7 +4222,8 @@ ir3_compile_shader_nir(struct ir3_compiler *compiler,
        */
 
       ctx->gs_header->dsts[0]->num = regid(0, 0);
-      ctx->primitive_id->dsts[0]->num = regid(0, 1);
+      if (ctx->primitive_id)
+         ctx->primitive_id->dsts[0]->num = regid(0, 1);
    } else if (so->num_sampler_prefetch) {
       assert(so->type == MESA_SHADER_FRAGMENT);
       int idx = 0;

src/freedreno/ir3/ir3_context.h
@@ -98,6 +98,7 @@ struct ir3_context {
    struct ir3_instruction *patch_vertices_in;
    struct ir3_instruction *tcs_header;
    struct ir3_instruction *tess_coord;
+   struct ir3_instruction *rel_patch_id;
 
    /* Compute shader inputs: */
    struct ir3_instruction *local_invocation_id, *work_group_id;

src/freedreno/ir3/ir3_nir_lower_tess.c
@@ -76,7 +76,8 @@ build_local_primitive_id(nir_builder *b, struct state *state)
 static bool
 is_tess_levels(gl_varying_slot slot)
 {
-   return (slot == VARYING_SLOT_TESS_LEVEL_OUTER ||
+   return (slot == VARYING_SLOT_PRIMITIVE_ID ||
+           slot == VARYING_SLOT_TESS_LEVEL_OUTER ||
            slot == VARYING_SLOT_TESS_LEVEL_INNER);
 }
 
@@ -391,9 +392,9 @@ build_per_vertex_offset(nir_builder *b, struct state *state,
                         nir_ssa_def *vertex, uint32_t location, uint32_t comp,
                         nir_ssa_def *offset)
 {
-   nir_ssa_def *primitive_id = nir_load_primitive_id(b);
+   nir_ssa_def *patch_id = nir_load_rel_patch_id_ir3(b);
    nir_ssa_def *patch_stride = nir_load_hs_patch_stride_ir3(b);
-   nir_ssa_def *patch_offset = nir_imul24(b, primitive_id, patch_stride);
+   nir_ssa_def *patch_offset = nir_imul24(b, patch_id, patch_stride);
    nir_ssa_def *attr_offset;
 
    if (nir_src_is_const(nir_src_for_ssa(offset))) {
@@ -471,15 +472,17 @@ build_tessfactor_base(nir_builder *b, gl_varying_slot slot, struct state *state)
 
    const uint32_t patch_stride = 1 + inner_levels + outer_levels;
 
-   nir_ssa_def *primitive_id = nir_load_primitive_id(b);
+   nir_ssa_def *patch_id = nir_load_rel_patch_id_ir3(b);
 
    nir_ssa_def *patch_offset =
-      nir_imul24(b, primitive_id, nir_imm_int(b, patch_stride));
+      nir_imul24(b, patch_id, nir_imm_int(b, patch_stride));
 
    uint32_t offset;
    switch (slot) {
+   case VARYING_SLOT_PRIMITIVE_ID:
+      offset = 0;
+      break;
    case VARYING_SLOT_TESS_LEVEL_OUTER:
-      /* There's some kind of header dword, tess levels start at index 1. */
       offset = 1;
       break;
    case VARYING_SLOT_TESS_LEVEL_INNER:
@@ -582,32 +585,37 @@ lower_tess_ctrl_block(nir_block *block, nir_builder *b, struct state *state)
 
          gl_varying_slot location = nir_intrinsic_io_semantics(intr).location;
          if (is_tess_levels(location)) {
-            /* with tess levels are defined as float[4] and float[2],
-             * but tess factor BO has smaller sizes for tris/isolines,
-             * so we have to discard any writes beyond the number of
-             * components for inner/outer levels */
             uint32_t inner_levels, outer_levels, levels;
             tess_level_components(state, &inner_levels, &outer_levels);
-
-            if (location == VARYING_SLOT_TESS_LEVEL_OUTER)
-               levels = outer_levels;
-            else
-               levels = inner_levels;
 
             assert(intr->src[0].ssa->num_components == 1);
 
             nir_ssa_def *offset =
                nir_iadd_imm(b, intr->src[1].ssa, nir_intrinsic_component(intr));
 
-            nir_if *nif =
-               nir_push_if(b, nir_ult(b, offset, nir_imm_int(b, levels)));
+            nir_if *nif = NULL;
+            if (location != VARYING_SLOT_PRIMITIVE_ID) {
+               /* with tess levels are defined as float[4] and float[2],
+                * but tess factor BO has smaller sizes for tris/isolines,
+                * so we have to discard any writes beyond the number of
+                * components for inner/outer levels
+                */
+               if (location == VARYING_SLOT_TESS_LEVEL_OUTER)
+                  levels = outer_levels;
+               else
+                  levels = inner_levels;
+
+               nif = nir_push_if(b, nir_ult(b, offset, nir_imm_int(b, levels)));
+            }
 
             replace_intrinsic(
                b, intr, nir_intrinsic_store_global_ir3, intr->src[0].ssa,
                nir_load_tess_factor_base_ir3(b),
                nir_iadd(b, offset, build_tessfactor_base(b, location, state)));
 
-            nir_pop_if(b, nif);
+            if (location != VARYING_SLOT_PRIMITIVE_ID) {
+               nir_pop_if(b, nif);
+            }
          } else {
             nir_ssa_def *address = nir_load_tess_param_base_ir3(b);
             nir_ssa_def *offset = build_patch_offset(
@@ -663,6 +671,19 @@ ir3_nir_lower_tess_ctrl(nir_shader *shader, struct ir3_shader_variant *v,
    b.cursor = nir_before_cf_list(&impl->body);
 
    state.header = nir_load_tcs_header_ir3(&b);
+
+   /* If required, store gl_PrimitiveID. */
+   if (v->key.tcs_store_primid) {
+      b.cursor = nir_after_cf_list(&impl->body);
+
+      nir_store_output(&b, nir_load_primitive_id(&b), nir_imm_int(&b, 0),
+                       .io_semantics = {
+                           .location = VARYING_SLOT_PRIMITIVE_ID,
+                           .num_slots = 1
+                        });
+
+      b.cursor = nir_before_cf_list(&impl->body);
+   }
 
    nir_foreach_block_safe (block, impl)
       lower_tess_ctrl_block(block, &b, &state);

src/freedreno/ir3/ir3_shader.c
@@ -507,6 +507,9 @@ ir3_setup_used_key(struct ir3_shader *shader)
          key->vastc_srgb = ~0;
          key->vsamples = ~0;
       }
+
+      if (info->stage == MESA_SHADER_TESS_CTRL)
+         key->tcs_store_primid = true;
    }
 }
 

commit 8d7f0a3ee2a5e316f67d994a6f6bd989f055cbfb

tu: Fix VS primid with tess + GS
If tess is enabled, then VS chains to the HS and we have to use the HS
value for the PrimID sysval. This is still true if GS is also enabled,
so we need to use the HS instead of the GS if both are enabled.  In
particular if the HS or DS uses gl_PrimitiveID but the GS doesn't then
we still need to setup the register.

This fixes a bunch of dEQP-VK.tessellation.invariance.primitive_set.*
cases on a650, although it seems they managed to still pass on a630 with
the broken PrimID handling.

Fixes: 7289493428b ("tu, freedreno/a6xx, ir3: Rewrite tess PrimID handling")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12768>

src/freedreno/vulkan/tu_pipeline.c
@@ -680,11 +680,9 @@ tu6_emit_vs_system_values(struct tu_cs *cs,
    const uint32_t gs_primitiveid_regid = gs ?
          ir3_find_sysval_regid(gs, SYSTEM_VALUE_PRIMITIVE_ID) :
          regid(63, 0);
-   const uint32_t hs_primitiveid_regid = hs ?
+   const uint32_t vs_primitiveid_regid = hs ?
          ir3_find_sysval_regid(hs, SYSTEM_VALUE_PRIMITIVE_ID) :
-         regid(63, 0);
-   const uint32_t vs_primitiveid_regid = gs ? gs_primitiveid_regid :
-      hs_primitiveid_regid;
+         gs_primitiveid_regid;
    const uint32_t ds_primitiveid_regid = ds ?
          ir3_find_sysval_regid(ds, SYSTEM_VALUE_PRIMITIVE_ID) :
          regid(63, 0);