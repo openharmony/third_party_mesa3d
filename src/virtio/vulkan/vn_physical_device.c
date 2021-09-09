openharmony/third_party_mesa3d

commit 32acfad6736e0b23cd25afbd4cfb8e452ec5ce75

venus: no supported device is not an error
Signed-off-by: Chia-I Wu <olvaffe@gmail.com>
Reviewed-by: Ryan Neph <ryanneph@google.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12653>

src/virtio/vulkan/vn_physical_device.c
@@ -1336,16 +1336,20 @@ vn_instance_enumerate_physical_devices(struct vn_instance *instance)
    }
 
    count = supported_count;
-   if (!count)
-      goto out;
-
-   result = vn_instance_enumerate_physical_device_groups_locked(
-      instance, physical_devs, count);
-   if (result != VK_SUCCESS) {
-      for (uint32_t i = 0; i < count; i++)
-         vn_physical_device_fini(&physical_devs[i]);
-      count = 0;
-      goto out;
+   if (count) {
+      result = vn_instance_enumerate_physical_device_groups_locked(
+         instance, physical_devs, count);
+      if (result != VK_SUCCESS) {
+         for (uint32_t i = 0; i < count; i++)
+            vn_physical_device_fini(&physical_devs[i]);
+         count = 0;
+         goto out;
+      }
+   } else {
+      /* no supported physical device is not an error */
+      result = VK_SUCCESS;
+      vk_free(alloc, physical_devs);
+      physical_devs = NULL;
    }
 
    instance->physical_device.devices = physical_devs;

commit 267eb5ad0b0ff67cbebf6ea9be99e500fb623c16

venus: separate physical device init and filter
It is not an error when a physical device is filtered out.  But it is an
error when a physical device fails to initialize.

Fixed
dEQP-VK.api.device_init.create_instance_device_intentional_alloc_fail.

v2: separate success and fail paths

Fixes: 32acfad6 ("venus: no supported device is not an error")
Signed-off-by: Chia-I Wu <olvaffe@gmail.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org> (v1)
Reviewed-by: Ryan Neph <ryanneph@google.com> (v1)
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12723>

src/virtio/vulkan/vn_physical_device.c
@@ -1101,10 +1101,7 @@ vn_physical_device_init(struct vn_physical_device *physical_dev)
 {
    struct vn_instance *instance = physical_dev->instance;
    const VkAllocationCallbacks *alloc = &instance->base.base.alloc;
-
-   VkResult result = vn_physical_device_init_renderer_version(physical_dev);
-   if (result != VK_SUCCESS)
-      return result;
+   VkResult result;
 
    result = vn_physical_device_init_renderer_extensions(physical_dev);
    if (result != VK_SUCCESS)
@@ -1324,6 +1321,30 @@ fail:
    return result;
 }
 
+static uint32_t
+filter_physical_devices(struct vn_physical_device *physical_devs,
+                        uint32_t count)
+{
+   uint32_t supported_count = 0;
+   for (uint32_t i = 0; i < count; i++) {
+      struct vn_physical_device *physical_dev = &physical_devs[i];
+
+      /* init renderer version and discard unsupported devices */
+      VkResult result =
+         vn_physical_device_init_renderer_version(physical_dev);
+      if (result != VK_SUCCESS) {
+         vn_physical_device_base_fini(&physical_dev->base);
+         continue;
+      }
+
+      if (supported_count < i)
+         physical_devs[supported_count] = *physical_dev;
+      supported_count++;
+   }
+
+   return supported_count;
+}
+
 static VkResult
 vn_instance_enumerate_physical_devices_and_groups(struct vn_instance *instance)
 {
@@ -1335,52 +1356,47 @@ vn_instance_enumerate_physical_devices_and_groups(struct vn_instance *instance)
    mtx_lock(&instance->physical_device.mutex);
 
    if (instance->physical_device.initialized)
-      goto out;
+      goto unlock;
    instance->physical_device.initialized = true;
 
    result = enumerate_physical_devices(instance, &physical_devs, &count);
    if (result != VK_SUCCESS)
-      goto out;
+      goto unlock;
 
-   /* fully initialize physical devices and discard unsupported ones */
-   uint32_t supported_count = 0;
+   count = filter_physical_devices(physical_devs, count);
+   if (!count) {
+      vk_free(alloc, physical_devs);
+      goto unlock;
+   }
+
+   /* fully initialize physical devices */
    for (uint32_t i = 0; i < count; i++) {
       struct vn_physical_device *physical_dev = &physical_devs[i];
 
       result = vn_physical_device_init(physical_dev);
       if (result != VK_SUCCESS) {
-         vn_physical_device_base_fini(&physical_dev->base);
-         continue;
+         for (uint32_t j = 0; j < i; j++)
+            vn_physical_device_fini(&physical_devs[j]);
+         for (uint32_t j = i; j < count; j++)
+            vn_physical_device_base_fini(&physical_devs[j].base);
+         vk_free(alloc, physical_devs);
+         goto unlock;
       }
-
-      if (supported_count < i)
-         physical_devs[supported_count] = *physical_dev;
-      supported_count++;
    }
 
-   count = supported_count;
-   if (count) {
-      result = vn_instance_enumerate_physical_device_groups_locked(
-         instance, physical_devs, count);
-      if (result != VK_SUCCESS) {
-         for (uint32_t i = 0; i < count; i++)
-            vn_physical_device_fini(&physical_devs[i]);
-         goto out;
-      }
-   } else {
-      /* no supported physical device is not an error */
-      result = VK_SUCCESS;
+   result = vn_instance_enumerate_physical_device_groups_locked(
+      instance, physical_devs, count);
+   if (result != VK_SUCCESS) {
+      for (uint32_t i = 0; i < count; i++)
+         vn_physical_device_fini(&physical_devs[i]);
       vk_free(alloc, physical_devs);
-      physical_devs = NULL;
+      goto unlock;
    }
 
    instance->physical_device.devices = physical_devs;
    instance->physical_device.device_count = count;
 
-out:
-   if (result != VK_SUCCESS && physical_devs)
-      vk_free(alloc, physical_devs);
-
+unlock:
    mtx_unlock(&instance->physical_device.mutex);
    return result;
 }