openharmony/third_party_mesa3d

commit 0593b61a58af6d330b42c24f004b42ff8374f394

vk/util: add a util macro for initializing stack vk_shader_module structs
radv does a lot of this, so having a central dispatch point will be useful in
case changes are made to this struct in the future

Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9508>

src/vulkan/util/vk_shader_module.h
@@ -43,6 +43,19 @@ struct vk_shader_module {
 
 VK_DEFINE_NONDISP_HANDLE_CASTS(vk_shader_module, base, VkShaderModule,
                                VK_OBJECT_TYPE_SHADER_MODULE)
+
+/* this should only be used for stack-allocated, temporary objects */
+#define vk_shader_module_handle_from_nir(_nir) \
+   vk_shader_module_to_handle(&(struct vk_shader_module) { \
+      .base.type = VK_OBJECT_TYPE_SHADER_MODULE, \
+      .nir = _nir, \
+   })
+#define vk_shader_module_from_nir(_nir) \
+   (struct vk_shader_module) { \
+      .base.type = VK_OBJECT_TYPE_SHADER_MODULE, \
+      .nir = _nir, \
+   }
+
 #ifdef __cplusplus
 }
 #endif

commit d6579682d5465a300c381cf22a22c2f66bb04582

vulkan/shader_module: Fix the lifetime of temporary shader modules
The vk_shader_module_handle_from_nir() macro was constructing a
temporary vk_shader_module and passing it through
vk_shader_module_to_handle().  Since this is a function and not a macro,
it means that the lifetime of the temporary vk_shader_module will end
once the to_handle() function is called.  Technically, this is a
use-after-free.  I really don't know why no one has been bitten by this
yet....

Fixes: 0593b61a58af "vk/util: add a util macro for initializing stack..."
Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13101>

src/vulkan/util/vk_shader_module.h
@@ -46,7 +46,7 @@ VK_DEFINE_NONDISP_HANDLE_CASTS(vk_shader_module, base, VkShaderModule,
 
 /* this should only be used for stack-allocated, temporary objects */
 #define vk_shader_module_handle_from_nir(_nir) \
-   vk_shader_module_to_handle(&(struct vk_shader_module) { \
+   ((VkShaderModule)(uintptr_t)&(struct vk_shader_module) { \
       .base.type = VK_OBJECT_TYPE_SHADER_MODULE, \
       .nir = _nir, \
    })