openharmony/third_party_mesa3d

commit f5429e99c86c8ccfa697202271a4458525061cc0

mesa: Throw an error for compressed glGenerateMipmap on GLES2 contexts.
This error is gone from GLES3.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Andreas Baierl <ichgeh@imkreisrum.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12924>

src/mesa/main/genmipmap.c
@@ -150,6 +150,19 @@ generate_texture_mipmap(struct gl_context *ctx,
                      _mesa_enum_to_string(srcImage->InternalFormat));
          return;
       }
+
+      /* The GLES 2.0 spec says:
+       *
+       *    "If the level zero array is stored in a compressed internal format,
+       *     the error INVALID_OPERATION is generated."
+       *
+       * and this text is gone from the GLES 3.0 spec.
+       */
+      if (ctx->API == API_OPENGLES2 && ctx->Version < 30 &&
+          _mesa_is_format_compressed(srcImage->TexFormat)) {
+         _mesa_error(ctx, GL_INVALID_OPERATION, "generate mipmaps on compressed texture");
+         return;
+      }
    }
 
    if (srcImage->Width == 0 || srcImage->Height == 0) {

commit e6cfc0ad6776a85b4fe74140c4af995002894b92

mesa: add missing unlock_texture into generate_texture_mipmap
Fixes: f5429e99c86c8ccfa697 "mesa: Throw an error for compressed glGenerateMipmap on GLES2 contexts."

Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13152>

src/mesa/main/genmipmap.c
@@ -160,6 +160,7 @@ generate_texture_mipmap(struct gl_context *ctx,
        */
       if (ctx->API == API_OPENGLES2 && ctx->Version < 30 &&
           _mesa_is_format_compressed(srcImage->TexFormat)) {
+         _mesa_unlock_texture(ctx, texObj);
          _mesa_error(ctx, GL_INVALID_OPERATION, "generate mipmaps on compressed texture");
          return;
       }