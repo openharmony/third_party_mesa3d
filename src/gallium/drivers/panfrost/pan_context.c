openharmony/third_party_mesa3d

commit 5b3931cd7e7e60d45817e753f3099766563f556f

panfrost: Call nir_lower_fragcolor based on key
We only want it to trigger if MRT is actually in use. This is a cheap
key (only require multiple variants for an obscure edge case) and avoids
the perf regression of using this pass which is needed for conformance.

Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Cc: mesa-stable
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10393>

src/gallium/drivers/panfrost/pan_context.c
@@ -959,6 +959,10 @@ panfrost_variant_matches(
                 }
         }
 
+        if (variant->info.stage == MESA_SHADER_FRAGMENT &&
+            variant->nr_cbufs != ctx->pipe_framebuffer.nr_cbufs)
+                return false;
+
         /* Otherwise, we're good to go */
         return true;
 }
@@ -1054,6 +1058,8 @@ panfrost_bind_shader_state(
 
                 if (type == PIPE_SHADER_FRAGMENT) {
                         struct pipe_framebuffer_state *fb = &ctx->pipe_framebuffer;
+                        v->nr_cbufs = fb->nr_cbufs;
+
                         for (unsigned i = 0; i < fb->nr_cbufs; ++i) {
                                 enum pipe_format fmt = PIPE_FORMAT_R8G8B8A8_UNORM;
 

src/gallium/drivers/panfrost/pan_assemble.c
@@ -76,6 +76,10 @@ panfrost_shader_compile(struct panfrost_context *ctx,
                 s = tgsi_to_nir(ir, ctx->base.screen, false);
         }
 
+        /* Lower this early so the backends don't have to worry about it */
+        if (stage == MESA_SHADER_FRAGMENT)
+                NIR_PASS_V(s, nir_lower_fragcolor, state->nr_cbufs);
+
         s->info.stage = stage;
 
         /* Call out to Midgard compiler given the above NIR */

src/gallium/drivers/panfrost/pan_context.h
@@ -216,7 +216,9 @@ struct panfrost_shader_state {
         /* GPU-executable memory */
         struct panfrost_bo *bo;
 
+        /* Variants */
         enum pipe_format rt_formats[8];
+        unsigned nr_cbufs;
 };
 
 /* A collection of varyings (the CSO) */

src/panfrost/util/pan_ir.h
@@ -130,6 +130,7 @@ struct panfrost_compile_inputs {
         bool no_ubo_to_push;
 
         enum pipe_format rt_formats[8];
+        unsigned nr_cbufs;
 };
 
 struct pan_shader_varying {

commit 901a65da1ceb67c9d447df30a38eb64da78b436b

panfrost: Fix gl_FragColor lowering
The gl_FragColor lowering in the fragment shader depends on the number
of render targets, which can change every set_framebuffer_state.
set_framebuffer_state thus needs to force a rebind of the fragment
shader.

Fixes a regression in Piglit fbo-drawbuffers-none gl_FragColor -auto
-fbo from enabling AFBC on Mali G52.

Fixes: 5b3931cd7e7 ("panfrost: Call nir_lower_fragcolor based on key")
Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13498>

src/gallium/drivers/panfrost/pan_context.c
@@ -688,11 +688,10 @@ panfrost_set_framebuffer_state(struct pipe_context *pctx,
         }
 
         /* We may need to generate a new variant if the fragment shader is
-         * keyed to the framebuffer format (due to EXT_framebuffer_fetch) */
+         * keyed to the framebuffer format or render target count */
         struct panfrost_shader_variants *fs = ctx->shader[PIPE_SHADER_FRAGMENT];
 
-        if (fs && fs->variant_count &&
-            fs->variants[fs->active_variant].info.fs.outputs_read)
+        if (fs && fs->variant_count)
                 ctx->base.bind_fs_state(&ctx->base, fs);
 }
 