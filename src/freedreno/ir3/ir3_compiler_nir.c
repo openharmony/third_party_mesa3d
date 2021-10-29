openharmony/third_party_mesa3d

commit 57a6de288893819e12467a0621f76441ad95683d

ir3: Check for nir_var_mem_image in shared_barrier handling
Reviewed-by: Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4743>

src/freedreno/ir3/ir3_compiler_nir.c
@@ -1384,7 +1384,7 @@ emit_intrinsic_barrier(struct ir3_context *ctx, nir_intrinsic_instr *intr)
          }
 
          /* TODO: check for image mode when it has a separate one */
-         if (modes & nir_var_mem_ssbo) {
+         if (modes & (nir_var_mem_ssbo | nir_var_mem_image)) {
             barrier->barrier_class |= IR3_BARRIER_IMAGE_W;
             barrier->barrier_conflict |=
                IR3_BARRIER_IMAGE_W | IR3_BARRIER_IMAGE_R;

commit 8606886fa8dce5750a109cd40453f4003ad1a4bc

ir3: Emit barriers for images again
This was accidentally broken with the nir_var_image work.

Fixes: 57a6de28 ("ir3: Check for nir_var_mem_image in shared_barrier handling")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13593>

src/freedreno/ir3/ir3_compiler_nir.c
@@ -1428,21 +1428,22 @@ emit_intrinsic_barrier(struct ir3_context *ctx, nir_intrinsic_instr *intr)
       assert(!(modes & nir_var_shader_out));
 
       if ((modes &
-           (nir_var_mem_shared | nir_var_mem_ssbo | nir_var_mem_global))) {
+           (nir_var_mem_shared | nir_var_mem_ssbo | nir_var_mem_global |
+            nir_var_image))) {
          barrier = ir3_FENCE(b);
          barrier->cat7.r = true;
          barrier->cat7.w = true;
 
-         if (modes & (nir_var_mem_ssbo | nir_var_mem_global)) {
+         if (modes & (nir_var_mem_ssbo | nir_var_image | nir_var_mem_global)) {
             barrier->cat7.g = true;
          }
 
          if (ctx->compiler->gen >= 6) {
-            if (modes & nir_var_mem_ssbo) {
+            if (modes & (nir_var_mem_ssbo | nir_var_image)) {
                barrier->cat7.l = true;
             }
          } else {
-            if (modes & (nir_var_mem_shared | nir_var_mem_ssbo)) {
+            if (modes & (nir_var_mem_shared | nir_var_mem_ssbo | nir_var_image)) {
                barrier->cat7.l = true;
             }
          }