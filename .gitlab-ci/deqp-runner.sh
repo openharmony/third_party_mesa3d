upcliujie/mesa

ci/deqp-runner: Move more non-suite logic under the non-suite 'if'.
Changing these variables won't do anything for you otherwise.

.gitlab-ci/deqp-runner.sh
@@ -2,28 +2,6 @@
 
 set -ex
 
-DEQP_WIDTH=${DEQP_WIDTH:-256}
-DEQP_HEIGHT=${DEQP_HEIGHT:-256}
-DEQP_CONFIG=${DEQP_CONFIG:-rgba8888d24s8ms0}
-DEQP_VARIANT=${DEQP_VARIANT:-master}
-
-DEQP_OPTIONS="$DEQP_OPTIONS --deqp-surface-width=$DEQP_WIDTH --deqp-surface-height=$DEQP_HEIGHT"
-DEQP_OPTIONS="$DEQP_OPTIONS --deqp-surface-type=${DEQP_SURFACE_TYPE:-pbuffer}"
-DEQP_OPTIONS="$DEQP_OPTIONS --deqp-gl-config-name=$DEQP_CONFIG"
-DEQP_OPTIONS="$DEQP_OPTIONS --deqp-visibility=hidden"
-
-if [ -z "$DEQP_VER" -a -z "$DEQP_SUITE" ]; then
-   echo 'DEQP_SUITE muist be set to the name of your deqp-gpu_version.toml, or DEQP_VER must be set to something like "gles2", "gles31-khr" or "vk" for the test run'
-   exit 1
-fi
-
-if [ "$DEQP_VER" = "vk" ]; then
-   if [ -z "$VK_DRIVER" ]; then
-      echo 'VK_DRIVER must be to something like "radeon" or "intel" for the test run'
-      exit 1
-   fi
-fi
-
 if [ -z "$GPU_VERSION" ]; then
    echo 'GPU_VERSION must be set to something like "llvmpipe" or "freedreno-a630" (the name used in .gitlab-ci/deqp-gpu-version-*.txt)'
    exit 1
@@ -42,6 +20,26 @@ mkdir -p $RESULTS
 HANG_DETECTION_CMD=""
 
 if [ -z "$DEQP_SUITE" ]; then
+    if [ -z "$DEQP_VER" ]; then
+        echo 'DEQP_SUITE muist be set to the name of your deqp-gpu_version.toml, or DEQP_VER must be set to something like "gles2", "gles31-khr" or "vk" for the test run'
+        exit 1
+    fi
+
+    DEQP_WIDTH=${DEQP_WIDTH:-256}
+    DEQP_HEIGHT=${DEQP_HEIGHT:-256}
+    DEQP_CONFIG=${DEQP_CONFIG:-rgba8888d24s8ms0}
+    DEQP_VARIANT=${DEQP_VARIANT:-master}
+
+    DEQP_OPTIONS="$DEQP_OPTIONS --deqp-surface-width=$DEQP_WIDTH --deqp-surface-height=$DEQP_HEIGHT"
+    DEQP_OPTIONS="$DEQP_OPTIONS --deqp-surface-type=${DEQP_SURFACE_TYPE:-pbuffer}"
+    DEQP_OPTIONS="$DEQP_OPTIONS --deqp-gl-config-name=$DEQP_CONFIG"
+    DEQP_OPTIONS="$DEQP_OPTIONS --deqp-visibility=hidden"
+
+    if [ "$DEQP_VER" = "vk" -a -z "$VK_DRIVER" ]; then
+        echo 'VK_DRIVER must be to something like "radeon" or "intel" for the test run'
+        exit 1
+    fi
+
     # Generate test case list file.
     if [ "$DEQP_VER" = "vk" ]; then
        MUSTPASS=/deqp/mustpass/vk-$DEQP_VARIANT.txt

openharmony/third_party_mesa3d

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>.
Reviewed-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13372>
(cherry picked from commit 440f207a1f21b5c14833e555f5c6a8cdcac6897d)

.gitlab-ci/deqp-runner.sh
@@ -2,28 +2,6 @@
 
 set -ex
 
-DEQP_WIDTH=${DEQP_WIDTH:-256}
-DEQP_HEIGHT=${DEQP_HEIGHT:-256}
-DEQP_CONFIG=${DEQP_CONFIG:-rgba8888d24s8ms0}
-DEQP_VARIANT=${DEQP_VARIANT:-master}
-
-DEQP_OPTIONS="$DEQP_OPTIONS --deqp-surface-width=$DEQP_WIDTH --deqp-surface-height=$DEQP_HEIGHT"
-DEQP_OPTIONS="$DEQP_OPTIONS --deqp-surface-type=${DEQP_SURFACE_TYPE:-pbuffer}"
-DEQP_OPTIONS="$DEQP_OPTIONS --deqp-gl-config-name=$DEQP_CONFIG"
-DEQP_OPTIONS="$DEQP_OPTIONS --deqp-visibility=hidden"
-
-if [ -z "$DEQP_VER" -a -z "$DEQP_SUITE" ]; then
-   echo 'DEQP_SUITE must be set to the name of your deqp-gpu_version.toml, or DEQP_VER must be set to something like "gles2", "gles31-khr" or "vk" for the test run'
-   exit 1
-fi
-
-if [ "$DEQP_VER" = "vk" ]; then
-   if [ -z "$VK_DRIVER" ]; then
-      echo 'VK_DRIVER must be to something like "radeon" or "intel" for the test run'
-      exit 1
-   fi
-fi
-
 if [ -z "$GPU_VERSION" ]; then
    echo 'GPU_VERSION must be set to something like "llvmpipe" or "freedreno-a630" (the name used in .gitlab-ci/deqp-gpu-version-*.txt)'
    exit 1
@@ -42,6 +20,26 @@ mkdir -p $RESULTS
 HANG_DETECTION_CMD=""
 
 if [ -z "$DEQP_SUITE" ]; then
+    if [ -z "$DEQP_VER" ]; then
+        echo 'DEQP_SUITE must be set to the name of your deqp-gpu_version.toml, or DEQP_VER must be set to something like "gles2", "gles31-khr" or "vk" for the test run'
+        exit 1
+    fi
+
+    DEQP_WIDTH=${DEQP_WIDTH:-256}
+    DEQP_HEIGHT=${DEQP_HEIGHT:-256}
+    DEQP_CONFIG=${DEQP_CONFIG:-rgba8888d24s8ms0}
+    DEQP_VARIANT=${DEQP_VARIANT:-master}
+
+    DEQP_OPTIONS="$DEQP_OPTIONS --deqp-surface-width=$DEQP_WIDTH --deqp-surface-height=$DEQP_HEIGHT"
+    DEQP_OPTIONS="$DEQP_OPTIONS --deqp-surface-type=${DEQP_SURFACE_TYPE:-pbuffer}"
+    DEQP_OPTIONS="$DEQP_OPTIONS --deqp-gl-config-name=$DEQP_CONFIG"
+    DEQP_OPTIONS="$DEQP_OPTIONS --deqp-visibility=hidden"
+
+    if [ "$DEQP_VER" = "vk" -a -z "$VK_DRIVER" ]; then
+        echo 'VK_DRIVER must be to something like "radeon" or "intel" for the test run'
+        exit 1
+    fi
+
     # Generate test case list file.
     if [ "$DEQP_VER" = "vk" ]; then
        MUSTPASS=/deqp/mustpass/vk-$DEQP_VARIANT.txt