openharmony/third_party_mesa3d

commit b23155afbd294f9520e314521fa58286ae80ca84

mesa: use alloca in search_resource_hash
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13507>

src/mesa/main/shader_query.cpp
@@ -568,7 +568,7 @@ search_resource_hash(struct gl_shader_program *shProg,
 
    /* If dealing with array, we need to get the basename. */
    if (index >= 0) {
-      name_copy = (char *) malloc(base_name_end - name + 1);
+      name_copy = (char *) alloca(base_name_end - name + 1);
       memcpy(name_copy, name, base_name_end - name);
       name_copy[base_name_end - name] = '\0';
       len = base_name_end - name;
@@ -579,9 +579,6 @@ search_resource_hash(struct gl_shader_program *shProg,
    uint32_t key = compute_resource_key(programInterface, name_copy, len);
    struct gl_program_resource *res = (struct gl_program_resource *)
       _mesa_hash_table_u64_search(shProg->data->ProgramResourceHash, key);
-
-   if (name_copy != name)
-      free(name_copy);
 
    if (res && array_index)
       *array_index = index >= 0 ? index : 0;

commit ee17e57f9812e66ed63cde04e9f40344c1b66080

mesa/main: Fix use of alloca() without #include "c99_alloca.h"
Fixes: b23155af ("mesa: use alloca in search_resource_hash")
Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13735>

src/mesa/main/shader_query.cpp
@@ -38,6 +38,7 @@
 #include "compiler/glsl/program.h"
 #include "compiler/glsl/string_to_uint_map.h"
 #include "util/mesa-sha1.h"
+#include "c99_alloca.h"
 
 static GLint
 program_resource_location(struct gl_program_resource *res,