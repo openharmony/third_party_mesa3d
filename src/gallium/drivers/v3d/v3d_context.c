openharmony/third_party_mesa3d

commit 0932addf11fbecc2f552856ee5d4075174bfae30

v3d: fix primitive queries for geometry shaders
With geometry shaders the number of emitted primitived is decided
at run time, so we cannot precompute it in the CPU and we need to
use the PRIMITIVE_COUNTS_FEEDBACK commands to have the GPU provide
the number like we do for the number of primitives written to
transform feedback. This may have a performance impact though, since
it requires a sync wait for the draw to complete, so we only do
it when geometry shaders are present.

v2: remove '> 0' comparison for ponter type (Alejandro)

Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>

src/gallium/drivers/v3d/v3d_context.c
@@ -123,7 +123,7 @@ v3d_invalidate_resource(struct pipe_context *pctx, struct pipe_resource *prsc)
  * output targets.
  */
 void
-v3d_tf_update_counters(struct v3d_context *v3d)
+v3d_update_primitive_counters(struct v3d_context *v3d)
 {
         struct v3d_job *job = v3d_get_job_for_fbo(v3d);
         if (job->draw_calls_queued == 0)

src/gallium/drivers/v3d/v3d_context.h
@@ -693,7 +693,7 @@ bool v3d_generate_mipmap(struct pipe_context *pctx,
 
 struct v3d_fence *v3d_fence_create(struct v3d_context *v3d);
 
-void v3d_tf_update_counters(struct v3d_context *v3d);
+void v3d_update_primitive_counters(struct v3d_context *v3d);
 
 #ifdef v3dX
 #  include "v3dx_context.h"

src/gallium/drivers/v3d/v3d_job.c
@@ -459,6 +459,11 @@ v3d_read_and_accumulate_primitive_counters(struct v3d_context *v3d)
         if (v3d_bo_wait(rsc->bo, PIPE_TIMEOUT_INFINITE, "prim-counts")) {
                 uint32_t *map = v3d_bo_map(rsc->bo) + v3d->prim_counts_offset;
                 v3d->tf_prims_generated += map[V3D_PRIM_COUNTS_TF_WRITTEN];
+                /* When we only have a vertex shader we determine the primitive
+                 * count in the CPU so don't update it here again.
+                 */
+                if (v3d->prog.gs)
+                        v3d->prims_generated += map[V3D_PRIM_COUNTS_WRITTEN];
         }
 }
 

src/gallium/drivers/v3d/v3dx_state.c
@@ -1234,7 +1234,7 @@ v3d_set_stream_output_targets(struct pipe_context *pctx,
          * draw we need to do it here as well.
          */
         if (num_targets == 0 && so->num_targets > 0)
-                v3d_tf_update_counters(ctx);
+                v3d_update_primitive_counters(ctx);
 
         for (i = 0; i < num_targets; i++) {
                 if (offsets[i] != -1)

src/gallium/drivers/v3d/v3d_query.c
@@ -72,6 +72,13 @@ v3d_begin_query(struct pipe_context *pctx, struct pipe_query *query)
 
         switch (q->type) {
         case PIPE_QUERY_PRIMITIVES_GENERATED:
+                /* If we are using PRIMITIVE_COUNTS_FEEDBACK to retrieve
+                 * primitive counts from the GPU (which we need when a GS
+                 * is present), then we need to update our counters now
+                 * to discard any primitives generated before this.
+                 */
+                if (v3d->prog.gs)
+                        v3d_update_primitive_counters(v3d);
                 q->start = v3d->prims_generated;
                 break;
         case PIPE_QUERY_PRIMITIVES_EMITTED:
@@ -79,7 +86,7 @@ v3d_begin_query(struct pipe_context *pctx, struct pipe_query *query)
                  * primitive counts to skip primtives recorded before this.
                  */
                 if (v3d->streamout.num_targets > 0)
-                        v3d_tf_update_counters(v3d);
+                        v3d_update_primitive_counters(v3d);
                 q->start = v3d->tf_prims_generated;
                 break;
         case PIPE_QUERY_OCCLUSION_COUNTER:
@@ -107,6 +114,12 @@ v3d_end_query(struct pipe_context *pctx, struct pipe_query *query)
 
         switch (q->type) {
         case PIPE_QUERY_PRIMITIVES_GENERATED:
+                /* If we are using PRIMITIVE_COUNTS_FEEDBACK to retrieve
+                 * primitive counts from the GPU (which we need when a GS
+                 * is present), then we need to update our counters now.
+                 */
+                if (v3d->prog.gs)
+                        v3d_update_primitive_counters(v3d);
                 q->end = v3d->prims_generated;
                 break;
         case PIPE_QUERY_PRIMITIVES_EMITTED:
@@ -115,7 +128,7 @@ v3d_end_query(struct pipe_context *pctx, struct pipe_query *query)
                  * time. Otherwise, we have to do it now.
                  */
                 if (v3d->streamout.num_targets > 0)
-                        v3d_tf_update_counters(v3d);
+                        v3d_update_primitive_counters(v3d);
                 q->end = v3d->tf_prims_generated;
                 break;
         case PIPE_QUERY_OCCLUSION_COUNTER:

src/gallium/drivers/v3d/v3dx_draw.c
@@ -961,15 +961,18 @@ v3d_emit_gl_shader_state(struct v3d_context *v3d,
 }
 
 /**
- * Updates the number of primitvies generated from the number of vertices
- * to draw. We do this here instead of using PRIMITIVE_COUNTS_FEEDBACK because
- * using the GPU packet for this might require sync waits and this is trivial
- * to handle in the CPU instead.
+ * Updates the number of primitives generated from the number of vertices
+ * to draw. This only works when no GS is present, since otherwise the number
+ * of primitives generated cannot be determined in advance and we need to
+ * use the PRIMITIVE_COUNTS_FEEDBACK command instead, however, that requires
+ * a sync wait for the draw to complete, so we only use that when GS is present.
  */
 static void
 v3d_update_primitives_generated_counter(struct v3d_context *v3d,
                                         const struct pipe_draw_info *info)
 {
+        assert(!v3d->prog.gs);
+
         if (!v3d->active_queries)
                 return;
 
@@ -1131,7 +1134,7 @@ v3d_draw_vbo(struct pipe_context *pctx, const struct pipe_draw_info *info)
          */
         if (v3d->streamout.num_targets > 0 &&
             u_base_prim_type(info->mode) != u_base_prim_type(v3d->prim_mode)) {
-                v3d_tf_update_counters(v3d);
+                v3d_update_primitive_counters(v3d);
         }
 
         struct v3d_job *job = v3d_get_job_for_fbo(v3d);
@@ -1239,7 +1242,8 @@ v3d_draw_vbo(struct pipe_context *pctx, const struct pipe_draw_info *info)
                 prim_tf_enable = (V3D_PRIM_POINTS_TF - V3D_PRIM_POINTS);
 #endif
 
-        v3d_update_primitives_generated_counter(v3d, info);
+        if (!v3d->prog.gs)
+                v3d_update_primitives_generated_counter(v3d, info);
 
         uint32_t hw_prim_type = v3d_hw_prim_type(info->mode);
         if (info->index_size) {

commit 1d74acad0930c59e7fbe71f3f1865c3aab0c95d0

v3d: Update prim_counts when prims generated query in flight without TF
In order to implement GL_PRIMITIVES_GENERATED, v3d allocates a small
resource and adds a command to the job to store the prim counts to it.
However it was only doing this when TF was enabled which meant that if
the query was used with a geometry shader but no TF then the query would
always be zero. This patch makes the driver keep track of how many
PRIMITIVES_GENERATED queries are in flight and then enable writing the
prim count if its more than zero.

Fix dEQP-GLES31.functional.geometry_shading.query.primitives_generated_*

v2: Update CI expectations and references to fixed tests in commit log.
v3: - Add comment that GL_PRIMITIVES_GENERATED query is included because
      OES_geometry_shader, but it is not part of OpenGL ES 3.1. (Iago)
    - Update Fixes to commit introducing geometry shaders. (Iago)

Fixes: 0932addf ("v3d: fix primitive queries for geometry shaders")
Signed-off-by: Neil Roberts <nroberts@igalia.com>
Signed-off-by: Jose Maria Casanova Crespo <jmcasanova@igalia.com>
Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13712>

src/gallium/drivers/v3d/v3d_context.c
@@ -194,6 +194,20 @@ v3d_get_real_line_width(struct v3d_context *v3d)
 }
 
 void
+v3d_ensure_prim_counts_allocated(struct v3d_context *ctx)
+{
+        if (ctx->prim_counts)
+                return;
+
+        /* Init all 7 counters and 1 padding to 0 */
+        uint32_t zeroes[8] = { 0 };
+        u_upload_data(ctx->uploader,
+                      0, sizeof(zeroes), 32, zeroes,
+                      &ctx->prim_counts_offset,
+                      &ctx->prim_counts);
+}
+
+void
 v3d_flag_dirty_sampler_state(struct v3d_context *v3d,
                              enum pipe_shader_type shader)
 {

src/gallium/drivers/v3d/v3d_context.h
@@ -447,6 +447,8 @@ struct v3d_job {
          */
         bool tf_enabled;
 
+        bool needs_primitives_generated;
+
         /**
          * Current EZ state for drawing. Updated at the start of draw after
          * we've decided on the shader being rendered.
@@ -573,6 +575,8 @@ struct v3d_context {
 
         uint32_t tf_prims_generated;
         uint32_t prims_generated;
+
+        uint32_t n_primitives_generated_queries_in_flight;
 
         struct pipe_poly_stipple stipple;
         struct pipe_clip_state clip;
@@ -763,6 +767,8 @@ void v3d_update_primitive_counters(struct v3d_context *v3d);
 bool v3d_line_smoothing_enabled(struct v3d_context *v3d);
 
 float v3d_get_real_line_width(struct v3d_context *v3d);
+
+void v3d_ensure_prim_counts_allocated(struct v3d_context *ctx);
 
 void v3d_flag_dirty_sampler_state(struct v3d_context *v3d,
                                   enum pipe_shader_type shader);

src/gallium/drivers/v3d/v3d_job.c
@@ -482,6 +482,16 @@ v3d_job_submit(struct v3d_context *v3d, struct v3d_job *job)
         if (!job->needs_flush)
                 goto done;
 
+        /* The GL_PRIMITIVES_GENERATED query is included with
+         * OES_geometry_shader.
+         */
+        job->needs_primitives_generated =
+                v3d->n_primitives_generated_queries_in_flight > 0 &&
+                v3d->prog.gs;
+
+        if (job->needs_primitives_generated)
+                v3d_ensure_prim_counts_allocated(v3d);
+
         if (screen->devinfo.ver >= 41)
                 v3d41_emit_rcl(job);
         else
@@ -553,9 +563,11 @@ v3d_job_submit(struct v3d_context *v3d, struct v3d_job *job)
                 }
 
                 /* If we are submitting a job in the middle of transform
-                 * feedback we need to read the primitive counts and accumulate
-                 * them, otherwise they will be reset at the start of the next
-                 * draw when we emit the Tile Binning Mode Configuration packet.
+                 * feedback or there is a primitives generated query with a
+                 * geometry shader then we need to read the primitive counts
+                 * and accumulate them, otherwise they will be reset at the
+                 * start of the next draw when we emit the Tile Binning Mode
+                 * Configuration packet.
                  *
                  * If the job doesn't have any TF draw calls, then we know
                  * the primitive count must be zero and we can skip stalling
@@ -565,7 +577,9 @@ v3d_job_submit(struct v3d_context *v3d, struct v3d_job *job)
                  * to us reading an obsolete (possibly non-zero) value from
                  * the GPU counters.
                  */
-                if (v3d->streamout.num_targets && job->tf_draw_calls_queued > 0)
+                if (job->needs_primitives_generated ||
+                    (v3d->streamout.num_targets &&
+                     job->tf_draw_calls_queued > 0))
                         v3d_read_and_accumulate_primitive_counters(v3d);
         }
 

src/gallium/drivers/v3d/v3d_query_pipe.c
@@ -69,6 +69,7 @@ v3d_begin_query_pipe(struct v3d_context *v3d, struct v3d_query *query)
                 if (v3d->prog.gs)
                         v3d_update_primitive_counters(v3d);
                 pquery->start = v3d->prims_generated;
+                v3d->n_primitives_generated_queries_in_flight++;
                 break;
         case PIPE_QUERY_PRIMITIVES_EMITTED:
                 /* If we are inside transform feedback we need to update the
@@ -109,6 +110,7 @@ v3d_end_query_pipe(struct v3d_context *v3d, struct v3d_query *query)
                 if (v3d->prog.gs)
                         v3d_update_primitive_counters(v3d);
                 pquery->end = v3d->prims_generated;
+                v3d->n_primitives_generated_queries_in_flight--;
                 break;
         case PIPE_QUERY_PRIMITIVES_EMITTED:
                 /* If transform feedback has ended, then we have already

src/gallium/drivers/v3d/v3dx_job.c
@@ -39,7 +39,7 @@ void v3dX(bcl_epilogue)(struct v3d_context *v3d, struct v3d_job *job)
 #endif
                                                 cl_packet_length(FLUSH));
 
-                if (job->tf_enabled) {
+                if (job->tf_enabled || job->needs_primitives_generated) {
                         /* Write primitive counts to memory. */
                         assert(v3d->prim_counts);
                         struct v3d_resource *rsc =

src/gallium/drivers/v3d/v3dx_state.c
@@ -1243,14 +1243,8 @@ v3d_set_stream_output_targets(struct pipe_context *pctx,
         so->num_targets = num_targets;
 
         /* Create primitive counters BO if needed */
-        if (num_targets > 0 && !ctx->prim_counts) {
-                /* Init all 7 counters and 1 padding to 0 */
-                uint32_t zeroes[8] = { 0 };
-                u_upload_data(ctx->uploader,
-                              0, sizeof(zeroes), 32, zeroes,
-                              &ctx->prim_counts_offset,
-                              &ctx->prim_counts);
-        }
+        if (num_targets > 0)
+                v3d_ensure_prim_counts_allocated(ctx);
 
         ctx->dirty |= V3D_DIRTY_STREAMOUT;
 }