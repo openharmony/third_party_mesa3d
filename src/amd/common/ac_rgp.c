openharmony/third_party_mesa3d

commit fe27ba2b9f5c00f0765764859768f7e36198bc88

ac/rgp: add support for clock calibration
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13709>

src/amd/common/ac_rgp.c
@@ -841,6 +841,30 @@ ac_sqtt_fill_queue_event_timings(struct rgp_queue_info *rgp_queue_info,
    chunk->queue_event_table_size = queue_event_size;
 }
 
+/**
+ * SQTT clock calibration info.
+ */
+struct sqtt_file_chunk_clock_calibration {
+   struct sqtt_file_chunk_header header;
+   uint64_t cpu_timestamp;
+   uint64_t gpu_timestamp;
+   uint64_t reserved;
+};
+
+static_assert(sizeof(struct sqtt_file_chunk_clock_calibration) == 40,
+	      "sqtt_file_chunk_clock_calibration doesn't match RGP spec");
+
+static void
+ac_sqtt_fill_clock_calibration(struct sqtt_file_chunk_clock_calibration *chunk,
+                               int32_t chunk_index)
+{
+   chunk->header.chunk_id.type = SQTT_FILE_CHUNK_TYPE_CLOCK_CALIBRATION;
+   chunk->header.chunk_id.index = chunk_index;
+   chunk->header.major_version = 0;
+   chunk->header.minor_version = 0;
+   chunk->header.size_in_bytes = sizeof(*chunk);
+}
+
 /* Below values are from from llvm project
  * llvm/include/llvm/BinaryFormat/ELF.h
  */
@@ -886,6 +910,7 @@ static void ac_sqtt_dump_data(struct radeon_info *rad_info,
                                       &thread_trace_data->rgp_pso_correlation;
    struct rgp_queue_info *rgp_queue_info = &thread_trace_data->rgp_queue_info;
    struct rgp_queue_event *rgp_queue_event = &thread_trace_data->rgp_queue_event;
+   struct rgp_clock_calibration *rgp_clock_calibration = &thread_trace_data->rgp_clock_calibration;
 
    /* SQTT header file. */
    ac_sqtt_fill_header(&header);
@@ -1000,6 +1025,27 @@ static void ac_sqtt_dump_data(struct radeon_info *rad_info,
       }
       file_offset += (rgp_queue_event->record_count *
                       sizeof(struct sqtt_queue_event_record));
+   }
+
+   /* SQTT clock calibration. */
+   if (rgp_clock_calibration->record_count) {
+      uint32_t num_records = 0;
+
+      list_for_each_entry_safe(struct rgp_clock_calibration_record, record,
+                               &rgp_clock_calibration->record, list) {
+         struct sqtt_file_chunk_clock_calibration clock_calibration;
+
+         ac_sqtt_fill_clock_calibration(&clock_calibration, num_records);
+
+         clock_calibration.cpu_timestamp = record->cpu_timestamp;
+         clock_calibration.gpu_timestamp = record->gpu_timestamp;
+
+         fwrite(&clock_calibration, sizeof(struct sqtt_file_chunk_clock_calibration), 1,
+             output);
+         file_offset += sizeof(struct sqtt_file_chunk_clock_calibration);
+
+         num_records++;
+      }
    }
 
    if (thread_trace) {

src/amd/common/ac_rgp.h
@@ -175,6 +175,18 @@ struct rgp_queue_event {
    simple_mtx_t lock;
 };
 
+struct rgp_clock_calibration_record {
+   uint64_t cpu_timestamp;
+   uint64_t gpu_timestamp;
+   struct list_head list;
+};
+
+struct rgp_clock_calibration {
+   uint32_t record_count;
+   struct list_head record;
+   simple_mtx_t lock;
+};
+
 int
 ac_dump_rgp_capture(struct radeon_info *info,
                     struct ac_thread_trace *thread_trace);

src/amd/common/ac_sqtt.h
@@ -52,6 +52,8 @@ struct ac_thread_trace_data {
 
    struct rgp_queue_info rgp_queue_info;
    struct rgp_queue_event rgp_queue_event;
+
+   struct rgp_clock_calibration rgp_clock_calibration;
 };
 
 #define SQTT_BUFFER_ALIGN_SHIFT 12

commit bcd4b4c7d33d3aa45f83d42aed74924f1c8016d4

ac/rgp: Initialize clock_calibration with memset.
Fix defect reported by Coverity Scan.

Uninitialized scalar variable (UNINIT)
uninit_use_in_call: Using uninitialized value clock_calibration.
Field clock_calibration.reserved is uninitialized when calling
fwrite.

Fixes: fe27ba2b9f5 ("ac/rgp: add support for clock calibration")
Suggested-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13783>

src/amd/common/ac_rgp.c
@@ -1145,6 +1145,7 @@ static void ac_sqtt_dump_data(struct radeon_info *rad_info,
       list_for_each_entry_safe(struct rgp_clock_calibration_record, record,
                                &rgp_clock_calibration->record, list) {
          struct sqtt_file_chunk_clock_calibration clock_calibration;
+         memset(&clock_calibration, 0, sizeof(clock_calibration));
 
          ac_sqtt_fill_clock_calibration(&clock_calibration, num_records);
 