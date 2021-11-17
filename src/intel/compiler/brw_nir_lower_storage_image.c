openharmony/third_party_mesa3d

commit 76f9ff93df8d169eb9e13b19efe950167854d59f

intel/nir: allow unknown format in lowering of storage images
We're about to allow unknown format for specific formats in Anv.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13198>

src/intel/compiler/brw_nir_lower_storage_image.c
@@ -367,6 +367,10 @@ lower_image_load_instr(nir_builder *b,
 {
    nir_deref_instr *deref = nir_src_as_deref(intrin->src[0]);
    nir_variable *var = nir_deref_instr_get_variable(deref);
+
+   if (var->data.image.format == PIPE_FORMAT_NONE)
+      return false;
+
    const enum isl_format image_fmt =
       isl_format_for_pipe_format(var->data.image.format);
 
@@ -524,6 +528,9 @@ lower_image_store_instr(nir_builder *b,
     * conversion for us.
     */
    if (var->data.access & ACCESS_NON_READABLE)
+      return false;
+
+   if (var->data.image.format == PIPE_FORMAT_NONE)
       return false;
 
    const enum isl_format image_fmt =

commit f2c6f6186cd52fc706b83d779fa159b1473d8de6

intel/nir: also allow unknown format for getting the size of a storage image
Fixes: 76f9ff93df8d ("intel/nir: allow unknown format in lowering of storage images")
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13847>

src/intel/compiler/brw_nir_lower_storage_image.c
@@ -646,6 +646,9 @@ lower_image_size_instr(nir_builder *b,
    if (var->data.access & ACCESS_NON_READABLE)
       return false;
 
+   if (var->data.image.format == PIPE_FORMAT_NONE)
+      return false;
+
    /* If we have a matching typed format, then we have an actual image surface
     * so we fall back and let the back-end emit a TXS for this.
     */