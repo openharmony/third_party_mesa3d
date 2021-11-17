openharmony/third_party_mesa3d

commit f1c7215939d4e01f4906ca874fb8129a772c5bf1

iris: Combine iris_use_pinned_bo and add_exec_bo

src/gallium/drivers/iris/iris_batch.c
@@ -196,18 +196,45 @@ iris_init_batch(struct iris_batch *batch,
 
 #define READ_ONCE(x) (*(volatile __typeof__(x) *)&(x))
 
-static unsigned
-add_exec_bo(struct iris_batch *batch, struct iris_bo *bo)
+static struct drm_i915_gem_exec_object2 *
+find_validation_entry(struct iris_batch *batch, struct iris_bo *bo)
 {
    unsigned index = READ_ONCE(bo->index);
 
    if (index < batch->exec_count && batch->exec_bos[index] == bo)
-      return index;
+      return &batch->validation_list[index];
 
    /* May have been shared between multiple active batches */
    for (index = 0; index < batch->exec_count; index++) {
       if (batch->exec_bos[index] == bo)
-         return index;
+         return &batch->validation_list[index];
+   }
+
+   return NULL;
+}
+
+/**
+ * Add a buffer to the current batch's validation list.
+ *
+ * You must call this on any BO you wish to use in this batch, to ensure
+ * that it's resident when the GPU commands execute.
+ */
+void
+iris_use_pinned_bo(struct iris_batch *batch,
+                   struct iris_bo *bo,
+                   bool writable)
+{
+   assert(bo->kflags & EXEC_OBJECT_PINNED);
+
+   struct drm_i915_gem_exec_object2 *existing_entry =
+      find_validation_entry(batch, bo);
+
+   if (existing_entry) {
+      /* The BO is already in the validation list; mark it writable */
+      if (writable)
+         existing_entry->flags |= EXEC_OBJECT_WRITE;
+
+      return;
    }
 
    /* This is the first time our batch has seen this BO.  Before we use it,
@@ -218,9 +245,6 @@ add_exec_bo(struct iris_batch *batch, struct iris_bo *bo)
       // XXX: this is bad, we use the same state / instruction buffers for
       // both batches, and if both of them are reading some dynamic state,
       // we flush all the time.  check for writes vs. reads?
-      //
-      // XXX: need to combine add_exec_bo and iris_use_pinned_bo so that
-      // we know whether we're writing the buffer or not.
       if (iris_batch_references(batch->other_batches[b], bo))
          iris_batch_flush(batch->other_batches[b]);
    }
@@ -242,14 +266,14 @@ add_exec_bo(struct iris_batch *batch, struct iris_bo *bo)
       (struct drm_i915_gem_exec_object2) {
          .handle = bo->gem_handle,
          .offset = bo->gtt_offset,
-         .flags = bo->kflags,
+         .flags = bo->kflags | (writable ? EXEC_OBJECT_WRITE : 0),
       };
 
    bo->index = batch->exec_count;
    batch->exec_bos[batch->exec_count] = bo;
    batch->aperture_space += bo->size;
 
-   return batch->exec_count++;
+   batch->exec_count++;
 }
 
 static void
@@ -521,30 +545,5 @@ _iris_batch_flush_fence(struct iris_batch *batch,
 bool
 iris_batch_references(struct iris_batch *batch, struct iris_bo *bo)
 {
-   unsigned index = READ_ONCE(bo->index);
-   if (index < batch->exec_count && batch->exec_bos[index] == bo)
-      return true;
-
-   for (int i = 0; i < batch->exec_count; i++) {
-      if (batch->exec_bos[i] == bo)
-         return true;
-   }
-   return false;
-}
-
-/**
- * Add a buffer to the current batch's validation list.
- *
- * You must call this on any BO you wish to use in this batch, to ensure
- * that it's resident when the GPU commands execute.
- */
-void
-iris_use_pinned_bo(struct iris_batch *batch,
-                   struct iris_bo *bo,
-                   bool writable)
-{
-   assert(bo->kflags & EXEC_OBJECT_PINNED);
-   unsigned index = add_exec_bo(batch, bo);
-   if (writable)
-      batch->validation_list[index].flags |= EXEC_OBJECT_WRITE;
+   return find_validation_entry(batch, bo) != NULL;
 }

commit 755642c756dfa93ec143197ff49c7debfeaff1ca

iris: Tidy code in iris_use_pinned_bo a bit
Now that we aren't short-circuiting most of the code, we should probably
reorganize it a little bit.  Tagged with fixes just so we pull all the
refactors together as one group.

Fixes: f1c7215939d ("iris: Combine iris_use_pinned_bo and add_exec_bo")
Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13808>

src/gallium/drivers/iris/iris_batch.c
@@ -360,20 +360,17 @@ iris_use_pinned_bo(struct iris_batch *batch,
 
    int existing_index = find_exec_index(batch, bo);
 
-   if (existing_index != -1) {
+   if (existing_index == -1) {
+      flush_for_cross_batch_dependencies(batch, bo, writable);
+
+      ensure_exec_obj_space(batch, 1);
+      add_bo_to_batch(batch, bo, writable);
+   } else if (writable && !BITSET_TEST(batch->bos_written, existing_index)) {
+      flush_for_cross_batch_dependencies(batch, bo, writable);
+
       /* The BO is already in the list; mark it writable */
-      if (writable && !BITSET_TEST(batch->bos_written, existing_index)) {
-         BITSET_SET(batch->bos_written, existing_index);
-         flush_for_cross_batch_dependencies(batch, bo, writable);
-      }
-
-      return;
+      BITSET_SET(batch->bos_written, existing_index);
    }
-
-   flush_for_cross_batch_dependencies(batch, bo, writable);
-
-   ensure_exec_obj_space(batch, 1);
-   add_bo_to_batch(batch, bo, writable);
 }
 
 static void