openharmony/third_party_mesa3d

commit 6427718bbc4462fe10b53e7e4a2727054148e166

lima: Print usage if --help is any of the arguments.
Fix defect reported by Coverity Scan.

Structurally dead code (UNREACHABLE)
unreachable: Since the loop increment n++; is unreachable, the loop body
will never execute more than once.

Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Vasily Khoruzhick <anarsoul@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6906>

src/gallium/drivers/lima/standalone/lima_disasm.c
@@ -163,8 +163,6 @@ main(int argc, char **argv)
          print_usage();
          return 1;
       }
-
-      break;
    }
 
    char *filename = NULL;

commit b9c5abf43cba668547805bb4f31ad987483abf9d

lima: disasm: use last argument as a filename
Otherwise it fails to open a file.

Fixes: 6427718bbc44 ("lima: Print usage if --help is any of the arguments.")
Reviewed-by: Andreas Baierl <ichgeh@imkreisrum.de>
Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13968>

src/gallium/drivers/lima/standalone/lima_disasm.c
@@ -166,7 +166,7 @@ main(int argc, char **argv)
    }
 
    char *filename = NULL;
-   filename = argv[n];
+   filename = argv[argc - 1];
 
    uint32_t size = 0;
    uint32_t *prog = extract_shader_binary(filename, &size, &is_frag);