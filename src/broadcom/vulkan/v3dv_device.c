openharmony/third_party_mesa3d

commit 8291029126dc7d693e58a8f88a02ceb0bdba18e8

v3dv: implement VK_KHR_get_display_properties2
This is entirely implemented in the common WSI code, we just need to
implement the API entry points.

Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11287>

src/broadcom/vulkan/v3dv_device.c
@@ -84,6 +84,7 @@ static const struct vk_instance_extension_table instance_extensions = {
    .KHR_external_fence_capabilities     = true,
    .KHR_external_memory_capabilities    = true,
    .KHR_external_semaphore_capabilities = true,
+   .KHR_get_display_properties2         = true,
    .KHR_get_physical_device_properties2 = true,
 #ifdef V3DV_HAS_SURFACE
    .KHR_get_surface_capabilities2       = true,

src/broadcom/vulkan/v3dv_wsi_display.c
@@ -38,6 +38,21 @@ v3dv_GetPhysicalDeviceDisplayPropertiesKHR(VkPhysicalDevice physical_device,
       properties);
 }
 
+VkResult
+v3dv_GetPhysicalDeviceDisplayProperties2KHR(
+    VkPhysicalDevice physical_device,
+    uint32_t *pPropertyCount,
+    VkDisplayProperties2KHR *pProperties)
+{
+   V3DV_FROM_HANDLE(v3dv_physical_device, pdevice, physical_device);
+
+   return wsi_display_get_physical_device_display_properties2(
+      physical_device,
+      &pdevice->wsi_device,
+      pPropertyCount,
+      pProperties);
+}
+
 VKAPI_ATTR VkResult VKAPI_CALL
 v3dv_GetPhysicalDeviceDisplayPlanePropertiesKHR(
    VkPhysicalDevice physical_device,
@@ -51,6 +66,21 @@ v3dv_GetPhysicalDeviceDisplayPlanePropertiesKHR(
       &pdevice->wsi_device,
       property_count,
       properties);
+}
+
+VkResult
+v3dv_GetPhysicalDeviceDisplayPlaneProperties2KHR(
+    VkPhysicalDevice physical_device,
+    uint32_t *pPropertyCount,
+    VkDisplayPlaneProperties2KHR *pProperties)
+{
+   V3DV_FROM_HANDLE(v3dv_physical_device, pdevice, physical_device);
+
+   return wsi_display_get_physical_device_display_plane_properties2(
+      physical_device,
+      &pdevice->wsi_device,
+      pPropertyCount,
+      pProperties);
 }
 
 VKAPI_ATTR VkResult VKAPI_CALL
@@ -84,6 +114,21 @@ v3dv_GetDisplayModePropertiesKHR(VkPhysicalDevice physical_device,
                                                   properties);
 }
 
+VkResult
+v3dv_GetDisplayModeProperties2KHR(VkPhysicalDevice physical_device,
+                                  VkDisplayKHR display,
+                                  uint32_t *pPropertyCount,
+                                  VkDisplayModeProperties2KHR *pProperties)
+{
+   V3DV_FROM_HANDLE(v3dv_physical_device, pdevice, physical_device);
+
+   return wsi_display_get_display_mode_properties2(physical_device,
+                                                   &pdevice->wsi_device,
+                                                   display,
+                                                   pPropertyCount,
+                                                   pProperties);
+}
+
 VKAPI_ATTR VkResult VKAPI_CALL
 v3dv_CreateDisplayModeKHR(VkPhysicalDevice physical_device,
                           VkDisplayKHR display,
@@ -114,6 +159,20 @@ v3dv_GetDisplayPlaneCapabilitiesKHR(VkPhysicalDevice physical_device,
                                              mode_khr,
                                              plane_index,
                                              capabilities);
+}
+
+VkResult
+v3dv_GetDisplayPlaneCapabilities2KHR(
+   VkPhysicalDevice physical_device,
+   const VkDisplayPlaneInfo2KHR *pDisplayPlaneInfo,
+   VkDisplayPlaneCapabilities2KHR *pCapabilities)
+{
+   V3DV_FROM_HANDLE(v3dv_physical_device, pdevice, physical_device);
+
+   return wsi_get_display_plane_capabilities2(physical_device,
+                                              &pdevice->wsi_device,
+                                              pDisplayPlaneInfo,
+                                              pCapabilities);
 }
 
 VKAPI_ATTR VkResult VKAPI_CALL

commit 9339a570d645396e669af7dd93b524fcfb5f5548

v3dv: Fix dEQP-VK.info#instance_extensions test
When mesa3d is built without VK_USE_PLATFORM_DISPLAY_KHR definition,
dEQP test fails:

    dEQP    : Test case 'dEQP-VK.info.instance_extensions'..
    dEQP    :   Fail (Extension VK_KHR_get_display_properties2 is missing
                                                 dependency: VK_KHR_display)
    dEQP    : DONE!

Enable KHR_get_display_properties2 only if VK_USE_PLATFORM_DISPLAY_KHR
is enabled.

Fixes: 8291029126dc ("v3dv: implement VK_KHR_get_display_properties2")
Signed-off-by: Roman Stratiienko <roman.o.stratiienko@globallogic.com>
Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14047>

src/broadcom/vulkan/v3dv_device.c
@@ -85,11 +85,11 @@ static const struct vk_instance_extension_table instance_extensions = {
    .KHR_device_group_creation           = true,
 #ifdef VK_USE_PLATFORM_DISPLAY_KHR
    .KHR_display                         = true,
+   .KHR_get_display_properties2         = true,
 #endif
    .KHR_external_fence_capabilities     = true,
    .KHR_external_memory_capabilities    = true,
    .KHR_external_semaphore_capabilities = true,
-   .KHR_get_display_properties2         = true,
    .KHR_get_physical_device_properties2 = true,
 #ifdef V3DV_HAS_SURFACE
    .KHR_get_surface_capabilities2       = true,