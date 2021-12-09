openharmony/third_party_mesa3d

commit 615ce37105e429a20ca47027ea5f9909b912a3c7

glx: check drawable type before create drawble
If glxDrawable is not a X window ID, we can only support
GLXPbuffer now.

Reviewed-by: Adam Jackson <ajax@redhat.com>
Signed-off-by: Qiang Yu <yuq825@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13750>

src/glx/dri_common.c
@@ -339,10 +339,12 @@ driInferDrawableConfig(struct glx_screen *psc, GLXDrawable draw)
 _X_HIDDEN __GLXDRIdrawable *
 driFetchDrawable(struct glx_context *gc, GLXDrawable glxDrawable)
 {
-   struct glx_display *const priv = __glXInitialize(gc->psc->dpy);
+   Display *dpy = gc->psc->dpy;
+   struct glx_display *const priv = __glXInitialize(dpy);
    __GLXDRIdrawable *pdraw;
    struct glx_screen *psc;
    struct glx_config *config = gc->config;
+   unsigned int type;
 
    if (priv == NULL)
       return NULL;
@@ -364,6 +366,43 @@ driFetchDrawable(struct glx_context *gc, GLXDrawable glxDrawable)
       config = driInferDrawableConfig(gc->psc, glxDrawable);
    if (config == NULL)
       return NULL;
+
+   /* We can't find this GLX drawable above because it's either:
+    *
+    * 1. An X window ID instead of a GLX window ID. This could happend when
+    *    glXMakeCurrent() is passed an X window directly instead of creating
+    *    GLXWindow with glXCreateWindow() first.
+    *
+    * 2. A GLXPbuffer created on other display:
+    *
+    *    From the GLX spec:
+    *
+    *      Like other drawable types, GLXPbuffers are shared; any client which
+    *      knows the associated XID can use a GLXPbuffer.
+    *
+    *    So client other than the creator of this GLXPbuffer could use its
+    *    XID to do something like glXMakeCurrent(). I can't find explicite
+    *    statement in GLX spec that also allow GLXWindow and GLXPixmap.
+    *
+    *    But even GLXWindow and GLXPixmap is allowed, currently client other
+    *    than the GLX drawable creator has no way to find which X drawable
+    *    (window or pixmap) this GLX drawable uses, except the GLXPbuffer
+    *    case which use the same XID for both X pixmap and GLX drawable.
+    */
+
+   /* Infer the GLX drawable type. */
+   if (__glXGetDrawableAttribute(dpy, glxDrawable, GLX_DRAWABLE_TYPE, &type)) {
+      if (type != GLX_PBUFFER_BIT) {
+         ErrorMessageF("GLX drawable type is not supported\n");
+         return NULL;
+      }
+   } else {
+      /* Xserver may not implement GLX_DRAWABLE_TYPE query yet, or glxDrawable
+       * is a X window. Assume it's a GLXPbuffer in former case, because we don't
+       * know GLXPixmap and GLXWindow's X drawable ID anyway.
+       */
+      type = GLX_PBUFFER_BIT | GLX_WINDOW_BIT;
+   }
 
    pdraw = psc->driScreen->createDrawable(psc, glxDrawable, glxDrawable,
                                           config);

commit 0104608d0ef2c438c1ef01d7f2815c1155845134

glx: fix regression for drawable type detection
Newer version of XServer supporting GLX_DRAWABLE_TYPE query also
support query with raw X11 window ID besides GLXWindow ID. So we
should not limit the suppported type to GLXPbuffer when query
success.

Otherwise can't start GLX application on newer XServer with:

  libGL error: GLX drawable type is not supported
  libGL error: GLX drawable type is not supported
  X Error of failed request:  GLXBadContext
    Major opcode of failed request:  149 (GLX)
    Minor opcode of failed request:  5 (X_GLXMakeCurrent)
    Serial number of failed request:  35
    Current serial number in output stream:  35

Fixes: 615ce37105e ("glx: check drawable type before create drawble")

Tested-by: Mike Lothian <mike@fireburn.co.uk>
Reviewed-by: Adam Jackson <ajax@redhat.com>
Signed-off-by: Qiang Yu <yuq825@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14120>

src/glx/dri_common.c
@@ -392,15 +392,13 @@ driFetchDrawable(struct glx_context *gc, GLXDrawable glxDrawable)
 
    /* Infer the GLX drawable type. */
    if (__glXGetDrawableAttribute(dpy, glxDrawable, GLX_DRAWABLE_TYPE, &type)) {
-      if (type != GLX_PBUFFER_BIT) {
-         ErrorMessageF("GLX drawable type is not supported\n");
+      /* Xserver may support query with raw X11 window. */
+      if (type == GLX_PIXMAP_BIT) {
+         ErrorMessageF("GLXPixmap drawable type is not supported\n");
          return NULL;
       }
    } else {
-      /* Xserver may not implement GLX_DRAWABLE_TYPE query yet, or glxDrawable
-       * is a X window. Assume it's a GLXPbuffer in former case, because we don't
-       * know GLXPixmap and GLXWindow's X drawable ID anyway.
-       */
+      /* Xserver may not implement GLX_DRAWABLE_TYPE query yet. */
       type = GLX_PBUFFER_BIT | GLX_WINDOW_BIT;
    }
 