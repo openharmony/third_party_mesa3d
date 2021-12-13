openharmony/third_party_mesa3d

commit b2a89c19cdca72a44d3d66d8a74ee5dd883ca3f2

win32: Fixes thread local on win32 with clang/mingw (!14062)
The mingw compiling error:
```
../../src/mapi/glapi/glapi.h:86:66: error: '_glapi_tls_Dispatch' cannot be thread local when declared 'dllimport'
_GLAPI_EXPORT extern __THREAD_INITIAL_EXEC struct _glapi_table * _glapi_tls_Dispatch;
                                                                 ^
../../src/mapi/glapi/glapi.h:88:51: error: '_glapi_tls_Context' cannot be thread local when declared 'dllimport'
_GLAPI_EXPORT extern __THREAD_INITIAL_EXEC void * _glapi_tls_Context;
```

Fixes: 28641050 ("windows: Use TLS context/dispatch with shared-glapi")

Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14062>

src/mapi/glapi/glapi.h
@@ -79,12 +79,11 @@ struct _glapi_table;
 
 #if defined (USE_ELF_TLS)
 
-#ifdef _MSC_VER
-extern __declspec(thread) struct _glapi_table * _glapi_tls_Dispatch;
-extern __declspec(thread) void * _glapi_tls_Context;
+#if defined(USE_TLS_BEHIND_FUNCTIONS)
+extern __THREAD_INITIAL_EXEC struct _glapi_table * _glapi_tls_Dispatch;
+extern __THREAD_INITIAL_EXEC void * _glapi_tls_Context;
 #else
 _GLAPI_EXPORT extern __THREAD_INITIAL_EXEC struct _glapi_table * _glapi_tls_Dispatch;
-
 _GLAPI_EXPORT extern __THREAD_INITIAL_EXEC void * _glapi_tls_Context;
 #endif
 

commit e1e97ed392c881ce91abf06d4ab9cca0c00e9013

glapi: Never use dllimport/dllexport for TLS vars on Windows
Fixes: b2a89c19 ("win32: Fixes thread local on win32 with clang/mingw")
Reviewed-by: Yonggang Luo <luoyonggang@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14162>

src/mapi/glapi/glapi.h
@@ -46,6 +46,7 @@
 
 #include "util/macros.h"
 #include "util/u_thread.h"
+#include "util/detect_os.h"
 
 
 #ifdef __cplusplus
@@ -79,7 +80,7 @@ struct _glapi_table;
 
 #if defined (USE_ELF_TLS)
 
-#if defined(USE_TLS_BEHIND_FUNCTIONS)
+#if DETECT_OS_WINDOWS
 extern __THREAD_INITIAL_EXEC struct _glapi_table * _glapi_tls_Dispatch;
 extern __THREAD_INITIAL_EXEC void * _glapi_tls_Context;
 #else