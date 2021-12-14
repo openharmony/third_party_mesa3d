openharmony/third_party_mesa3d

commit 4d9b1341e16211e137390f1ebe701f401260c02c

venus: implement vn_buffer_get_max_buffer_size
This change estimates the max_buffer_size with quick sort. Try to
avoid some traffic upon device creation time, but not worth adding a
buffer simple create api to avoid the extra requirement query traffic
since this is temporary.

Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Reviewed-by: Chia-I Wu <olvaffe@gmail.com>
Reviewed-by: Ryan Neph <ryanneph@google.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13428>

src/virtio/vulkan/vn_buffer.c
@@ -43,7 +43,33 @@ static VkResult
 vn_buffer_get_max_buffer_size(struct vn_device *dev,
                               uint64_t *out_max_buffer_size)
 {
-   *out_max_buffer_size = 0;
+   /* XXX use VK_KHR_maintenance4 when available */
+   const VkAllocationCallbacks *alloc = &dev->base.base.alloc;
+   VkDevice dev_handle = vn_device_to_handle(dev);
+   VkBuffer buf_handle;
+   VkBufferCreateInfo create_info = {
+      .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+      .usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+      .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+   };
+   uint64_t max_buffer_size = 0;
+   uint8_t begin = 0;
+   uint8_t end = 64;
+
+   while (begin <= end) {
+      uint8_t mid = (begin + end) >> 1;
+      create_info.size = 1 << mid;
+      if (vn_CreateBuffer(dev_handle, &create_info, alloc, &buf_handle) ==
+          VK_SUCCESS) {
+         vn_DestroyBuffer(dev_handle, buf_handle, alloc);
+         max_buffer_size = create_info.size;
+         begin = mid + 1;
+      } else {
+         end = mid - 1;
+      }
+   }
+
+   *out_max_buffer_size = max_buffer_size;
    return VK_SUCCESS;
 }
 

commit 51c5b3f5dfd97af615b4690ef8955ea7462e1764

venus: fix vn_buffer_get_max_buffer_size
The binary search can lead to infinite loop.  Fixes
dEQP-VK.api.object_management.alloc_callback_fail.device where
vn_CreateBuffer can always fail.

Fixes: 4d9b1341e16 ("venus: implement vn_buffer_get_max_buffer_size")
Signed-off-by: Chia-I Wu <olvaffe@gmail.com>
Reviewed-by: Ryan Neph <ryanneph@google.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14184>

src/virtio/vulkan/vn_buffer.c
@@ -136,16 +136,16 @@ vn_buffer_get_max_buffer_size(struct vn_device *dev,
    uint8_t begin = 0;
    uint8_t end = 64;
 
-   while (begin <= end) {
+   while (begin < end) {
       uint8_t mid = (begin + end) >> 1;
-      create_info.size = 1 << mid;
+      create_info.size = 1ull << mid;
       if (vn_CreateBuffer(dev_handle, &create_info, alloc, &buf_handle) ==
           VK_SUCCESS) {
          vn_DestroyBuffer(dev_handle, buf_handle, alloc);
          max_buffer_size = create_info.size;
          begin = mid + 1;
       } else {
-         end = mid - 1;
+         end = mid;
       }
    }
 