openharmony/third_party_mesa3d

commit 24cccfb54a633599139f1c0aad90944eab366632

anv: Convert to the common sync and submit framework
This is, unfortunately, a large flag-day mega-commit.  However, any
other approach would likely be fragile and involve a lot more churn as
we try to plumb the new vk_fence and vk_semaphore primitives into ANV's
submit code before we delete it all.  Instead, we do it all in one go
and accept the consequences.

While this should be mostly functionally equivalent to the previous
code, there is one potential perf-affecting change.  The command buffer
chaining optimization no longer works across VkSubmitInfo structs.
Within a single VkSubmitInfo, we will attempt to chain all the command
buffers together but we no longer try to chain across a VkSubmitInfo
boundary.  Hopefully, this isn't a significant perf problem.  If it ever
is, we'll have to teach the core runtime code how to combine two or more
VkSubmitInfos into a single vk_queue_submit.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13427>

src/intel/vulkan/anv_bo_sync.c
@@ -211,7 +211,6 @@ const struct vk_sync_type anv_bo_sync_type = {
    .reset = anv_bo_sync_reset,
    .wait_many = anv_bo_sync_wait,
 };
-VK_DECL_TIMELINE_TYPE(anv_bo_timeline_type, &anv_bo_sync_type);
 
 VkResult
 anv_sync_create_for_bo(struct anv_device *device,

src/intel/vulkan/anv_device.c
@@ -53,6 +53,7 @@
 #include "git_sha1.h"
 #include "vk_util.h"
 #include "vk_deferred_operation.h"
+#include "vk_drm_syncobj.h"
 #include "common/intel_aux_map.h"
 #include "common/intel_defines.h"
 #include "common/intel_uuid.h"
@@ -906,8 +907,23 @@ anv_physical_device_try_create(struct anv_instance *instance,
    if (env_var_as_boolean("ANV_QUEUE_THREAD_DISABLE", false))
       device->has_exec_timeline = false;
 
-   device->has_thread_submit =
-      device->has_syncobj_wait_available && device->has_exec_timeline;
+   unsigned st_idx = 0;
+   if (device->has_syncobj_wait) {
+      device->sync_types[st_idx++] = &vk_drm_binary_syncobj_type;
+   } else {
+      device->sync_types[st_idx++] = &vk_drm_binary_syncobj_no_wait_type;
+      device->sync_types[st_idx++] = &anv_bo_sync_type;
+   }
+
+   if (device->has_syncobj_wait_available && device->has_exec_timeline) {
+      device->sync_types[st_idx++] = &vk_drm_timeline_syncobj_type;
+   } else {
+      device->sync_timeline_type = vk_sync_timeline_get_type(&anv_bo_sync_type);
+      device->sync_types[st_idx++] = &device->sync_timeline_type.sync;
+   }
+   device->sync_types[st_idx++] = NULL;
+   assert(st_idx <= ARRAY_SIZE(device->sync_types));
+   device->vk.supported_sync_types = device->sync_types;
 
    device->always_use_bindless =
       env_var_as_boolean("ANV_ALWAYS_BINDLESS", false);
@@ -3021,6 +3037,7 @@ VkResult anv_CreateDevice(
    }
 
    device->vk.check_status = anv_device_check_status;
+   vk_device_set_drm_fd(&device->vk, device->fd);
 
    uint32_t num_queues = 0;
    for (uint32_t i = 0; i < pCreateInfo->queueCreateInfoCount; i++)
@@ -3064,8 +3081,6 @@ VkResult anv_CreateDevice(
     */
    anv_gem_set_context_param(device->fd, device->context_id,
                              I915_CONTEXT_PARAM_RECOVERABLE, false);
-
-   device->has_thread_submit = physical_device->has_thread_submit;
 
    device->queues =
       vk_zalloc(&device->vk.alloc, num_queues * sizeof(*device->queues), 8,
@@ -3462,30 +3477,6 @@ anv_device_check_status(struct vk_device *vk_device)
    }
 
    return VK_SUCCESS;
-}
-
-VkResult
-anv_device_bo_busy(struct anv_device *device, struct anv_bo *bo)
-{
-   /* Note:  This only returns whether or not the BO is in use by an i915 GPU.
-    * Other usages of the BO (such as on different hardware) will not be
-    * flagged as "busy" by this ioctl.  Use with care.
-    */
-   int ret = anv_gem_busy(device, bo->gem_handle);
-   if (ret == 1) {
-      return VK_NOT_READY;
-   } else if (ret == -1) {
-      /* We don't know the real error. */
-      return vk_device_set_lost(&device->vk, "gem wait failed: %m");
-   }
-
-   /* Query for device status after the busy call.  If the BO we're checking
-    * got caught in a GPU hang we don't want to return VK_SUCCESS to the
-    * client because it clearly doesn't have valid data.  Yes, this most
-    * likely means an ioctl, but we just did an ioctl to query the busy status
-    * so it's no great loss.
-    */
-   return vk_device_check_status(&device->vk);
 }
 
 VkResult

src/intel/vulkan/anv_private.h
@@ -66,17 +66,17 @@
 #include "vk_alloc.h"
 #include "vk_debug_report.h"
 #include "vk_device.h"
+#include "vk_drm_syncobj.h"
 #include "vk_enum_defines.h"
 #include "vk_image.h"
 #include "vk_instance.h"
 #include "vk_physical_device.h"
 #include "vk_shader_module.h"
 #include "vk_sync.h"
-#include "vk_timeline.h"
+#include "vk_sync_timeline.h"
 #include "vk_util.h"
 #include "vk_command_buffer.h"
 #include "vk_queue.h"
-#include "vk_sync.h"
 #include "vk_log.h"
 
 /* Pre-declarations needed for WSI entrypoints */
@@ -922,7 +922,6 @@ struct anv_physical_device {
     bool                                        has_syncobj_wait_available;
     int                                         max_context_priority;
     bool                                        has_context_isolation;
-    bool                                        has_thread_submit;
     bool                                        has_mmap_offset;
     bool                                        has_userptr_probe;
     uint64_t                                    gtt_size;
@@ -977,6 +976,9 @@ struct anv_physical_device {
     uint8_t                                     driver_uuid[VK_UUID_SIZE];
     uint8_t                                     device_uuid[VK_UUID_SIZE];
 
+    struct vk_sync_timeline_type                sync_timeline_type;
+    const struct vk_sync_type *                 sync_types[4];
+
     struct disk_cache *                         disk_cache;
 
     struct wsi_device                       wsi_device;
@@ -1017,53 +1019,6 @@ struct anv_instance {
 VkResult anv_init_wsi(struct anv_physical_device *physical_device);
 void anv_finish_wsi(struct anv_physical_device *physical_device);
 
-struct anv_queue_submit {
-   struct anv_cmd_buffer **                  cmd_buffers;
-   uint32_t                                  cmd_buffer_count;
-   uint32_t                                  cmd_buffer_array_length;
-
-   uint32_t                                  fence_count;
-   uint32_t                                  fence_array_length;
-   struct drm_i915_gem_exec_fence *          fences;
-   uint64_t *                                fence_values;
-
-   uint32_t                                  temporary_semaphore_count;
-   uint32_t                                  temporary_semaphore_array_length;
-   struct anv_semaphore_impl *               temporary_semaphores;
-
-   /* Allocated only with non shareable timelines. */
-   union {
-      struct anv_timeline **                 wait_timelines;
-      uint32_t *                             wait_timeline_syncobjs;
-   };
-   uint32_t                                  wait_timeline_count;
-   uint32_t                                  wait_timeline_array_length;
-   uint64_t *                                wait_timeline_values;
-
-   struct anv_timeline **                    signal_timelines;
-   uint32_t                                  signal_timeline_count;
-   uint32_t                                  signal_timeline_array_length;
-   uint64_t *                                signal_timeline_values;
-
-   uint32_t                                  fence_bo_count;
-   uint32_t                                  fence_bo_array_length;
-   /* An array of struct anv_bo pointers with lower bit used as a flag to
-    * signal we will wait on that BO (see anv_(un)pack_ptr).
-    */
-   uintptr_t *                               fence_bos;
-
-   int                                       perf_query_pass;
-   struct anv_query_pool *                   perf_query_pool;
-
-   const VkAllocationCallbacks *             alloc;
-   VkSystemAllocationScope                   alloc_scope;
-
-   struct anv_bo *                           simple_bo;
-   uint32_t                                  simple_bo_size;
-
-   struct list_head                          link;
-};
-
 struct anv_queue {
    struct vk_queue                           vk;
 
@@ -1072,22 +1027,6 @@ struct anv_queue {
    const struct anv_queue_family *           family;
 
    uint32_t                                  exec_flags;
-
-   /*
-    * This mutext protects the variables below.
-    */
-   pthread_mutex_t                           mutex;
-
-   pthread_t                                 thread;
-   pthread_cond_t                            cond;
-
-   /*
-    * A list of struct anv_queue_submit to be submitted to i915.
-    */
-   struct list_head                          queued_submits;
-
-   /* Set to true to stop the submission thread */
-   bool                                      quit;
 };
 
 struct anv_pipeline_cache {
@@ -1176,7 +1115,6 @@ struct anv_device {
     int                                         fd;
     bool                                        can_chain_batches;
     bool                                        robust_buffer_access;
-    bool                                        has_thread_submit;
 
     pthread_mutex_t                             vma_mutex;
     struct util_vma_heap                        vma_lo;
@@ -1398,7 +1336,6 @@ anv_device_lookup_bo(struct anv_device *device, uint32_t gem_handle)
    return util_sparse_array_get(&device->bo_cache.bo_map, gem_handle);
 }
 
-VkResult anv_device_bo_busy(struct anv_device *device, struct anv_bo *bo);
 VkResult anv_device_wait(struct anv_device *device, struct anv_bo *bo,
                          int64_t timeout);
 
@@ -1408,12 +1345,10 @@ VkResult anv_queue_init(struct anv_device *device, struct anv_queue *queue,
                         uint32_t index_in_family);
 void anv_queue_finish(struct anv_queue *queue);
 
-VkResult anv_queue_execbuf_locked(struct anv_queue *queue, struct anv_queue_submit *submit);
+VkResult anv_queue_submit(struct vk_queue *queue,
+                          struct vk_queue_submit *submit);
 VkResult anv_queue_submit_simple_batch(struct anv_queue *queue,
                                        struct anv_batch *batch);
-
-uint64_t anv_gettime_ns(void);
-uint64_t anv_get_absolute_timeout(uint64_t timeout);
 
 void* anv_gem_mmap(struct anv_device *device,
                    uint32_t gem_handle, uint64_t offset, uint64_t size, uint32_t flags);
@@ -3264,7 +3199,6 @@ struct anv_bo_sync {
 };
 
 extern const struct vk_sync_type anv_bo_sync_type;
-extern const struct vk_timeline_type anv_bo_timeline_type;
 
 static inline bool
 vk_sync_is_anv_bo_sync(const struct vk_sync *sync)
@@ -3276,161 +3210,11 @@ VkResult anv_sync_create_for_bo(struct anv_device *device,
                                 struct anv_bo *bo,
                                 struct vk_sync **sync_out);
 
-enum anv_fence_type {
-   ANV_FENCE_TYPE_NONE = 0,
-   ANV_FENCE_TYPE_BO,
-   ANV_FENCE_TYPE_WSI_BO,
-   ANV_FENCE_TYPE_SYNCOBJ,
-   ANV_FENCE_TYPE_WSI,
-};
-
-enum anv_bo_fence_state {
-   /** Indicates that this is a new (or newly reset fence) */
-   ANV_BO_FENCE_STATE_RESET,
-
-   /** Indicates that this fence has been submitted to the GPU but is still
-    * (as far as we know) in use by the GPU.
-    */
-   ANV_BO_FENCE_STATE_SUBMITTED,
-
-   ANV_BO_FENCE_STATE_SIGNALED,
-};
-
-struct anv_fence_impl {
-   enum anv_fence_type type;
-
-   union {
-      /** Fence implementation for BO fences
-       *
-       * These fences use a BO and a set of CPU-tracked state flags.  The BO
-       * is added to the object list of the last execbuf call in a QueueSubmit
-       * and is marked EXEC_WRITE.  The state flags track when the BO has been
-       * submitted to the kernel.  We need to do this because Vulkan lets you
-       * wait on a fence that has not yet been submitted and I915_GEM_BUSY
-       * will say it's idle in this case.
-       */
-      struct {
-         struct anv_bo *bo;
-         enum anv_bo_fence_state state;
-      } bo;
-
-      /** DRM syncobj handle for syncobj-based fences */
-      uint32_t syncobj;
-
-      /** WSI fence */
-      struct vk_sync *sync_wsi;
-   };
-};
-
-struct anv_fence {
-   struct vk_object_base base;
-
-   /* Permanent fence state.  Every fence has some form of permanent state
-    * (type != ANV_SEMAPHORE_TYPE_NONE).  This may be a BO to fence on (for
-    * cross-process fences) or it could just be a dummy for use internally.
-    */
-   struct anv_fence_impl permanent;
-
-   /* Temporary fence state.  A fence *may* have temporary state.  That state
-    * is added to the fence by an import operation and is reset back to
-    * ANV_SEMAPHORE_TYPE_NONE when the fence is reset.  A fence with temporary
-    * state cannot be signaled because the fence must already be signaled
-    * before the temporary state can be exported from the fence in the other
-    * process and imported here.
-    */
-   struct anv_fence_impl temporary;
-};
-
-void anv_fence_reset_temporary(struct anv_device *device,
-                               struct anv_fence *fence);
-
 struct anv_event {
    struct vk_object_base                        base;
    uint64_t                                     semaphore;
    struct anv_state                             state;
 };
-
-enum anv_semaphore_type {
-   ANV_SEMAPHORE_TYPE_NONE = 0,
-   ANV_SEMAPHORE_TYPE_WSI_BO,
-   ANV_SEMAPHORE_TYPE_DRM_SYNCOBJ,
-   ANV_SEMAPHORE_TYPE_TIMELINE,
-   ANV_SEMAPHORE_TYPE_DRM_SYNCOBJ_TIMELINE,
-};
-
-struct anv_timeline_point {
-   struct list_head link;
-
-   uint64_t serial;
-
-   /* Number of waiter on this point, when > 0 the point should not be garbage
-    * collected.
-    */
-   int waiting;
-
-   /* BO used for synchronization. */
-   struct anv_bo *bo;
-};
-
-struct anv_timeline {
-   pthread_mutex_t mutex;
-   pthread_cond_t  cond;
-
-   uint64_t highest_past;
-   uint64_t highest_pending;
-
-   struct list_head points;
-   struct list_head free_points;
-};
-
-struct anv_semaphore_impl {
-   enum anv_semaphore_type type;
-
-   union {
-      /* A BO representing this semaphore when type == ANV_SEMAPHORE_TYPE_BO
-       * or type == ANV_SEMAPHORE_TYPE_WSI_BO.  This BO will be added to the
-       * object list on any execbuf2 calls for which this semaphore is used as
-       * a wait or signal fence.  When used as a signal fence or when type ==
-       * ANV_SEMAPHORE_TYPE_WSI_BO, the EXEC_OBJECT_WRITE flag will be set.
-       */
-      struct anv_bo *bo;
-
-      /* Sync object handle when type == ANV_SEMAPHORE_TYPE_DRM_SYNCOBJ.
-       * Unlike GEM BOs, DRM sync objects aren't deduplicated by the kernel on
-       * import so we don't need to bother with a userspace cache.
-       */
-      uint32_t syncobj;
-
-      /* Non shareable timeline semaphore
-       *
-       * Used when kernel don't have support for timeline semaphores.
-       */
-      struct anv_timeline timeline;
-   };
-};
-
-struct anv_semaphore {
-   struct vk_object_base base;
-
-   /* Permanent semaphore state.  Every semaphore has some form of permanent
-    * state (type != ANV_SEMAPHORE_TYPE_NONE).  This may be a BO to fence on
-    * (for cross-process semaphores0 or it could just be a dummy for use
-    * internally.
-    */
-   struct anv_semaphore_impl permanent;
-
-   /* Temporary semaphore state.  A semaphore *may* have temporary state.
-    * That state is added to the semaphore by an import operation and is reset
-    * back to ANV_SEMAPHORE_TYPE_NONE when the semaphore is waited on.  A
-    * semaphore with temporary state cannot be signaled because the semaphore
-    * must already be signaled before the temporary state can be exported from
-    * the semaphore in the other process and imported here.
-    */
-   struct anv_semaphore_impl temporary;
-};
-
-void anv_semaphore_reset_temporary(struct anv_device *device,
-                                   struct anv_semaphore *semaphore);
 
 #define ANV_STAGE_MASK ((1 << MESA_VULKAN_SHADER_STAGES) - 1)
 
@@ -4787,7 +4571,6 @@ VK_DEFINE_NONDISP_HANDLE_CASTS(anv_descriptor_update_template, base,
                                VK_OBJECT_TYPE_DESCRIPTOR_UPDATE_TEMPLATE)
 VK_DEFINE_NONDISP_HANDLE_CASTS(anv_device_memory, base, VkDeviceMemory,
                                VK_OBJECT_TYPE_DEVICE_MEMORY)
-VK_DEFINE_NONDISP_HANDLE_CASTS(anv_fence, base, VkFence, VK_OBJECT_TYPE_FENCE)
 VK_DEFINE_NONDISP_HANDLE_CASTS(anv_event, base, VkEvent, VK_OBJECT_TYPE_EVENT)
 VK_DEFINE_NONDISP_HANDLE_CASTS(anv_framebuffer, base, VkFramebuffer,
                                VK_OBJECT_TYPE_FRAMEBUFFER)
@@ -4806,8 +4589,6 @@ VK_DEFINE_NONDISP_HANDLE_CASTS(anv_render_pass, base, VkRenderPass,
                                VK_OBJECT_TYPE_RENDER_PASS)
 VK_DEFINE_NONDISP_HANDLE_CASTS(anv_sampler, base, VkSampler,
                                VK_OBJECT_TYPE_SAMPLER)
-VK_DEFINE_NONDISP_HANDLE_CASTS(anv_semaphore, base, VkSemaphore,
-                               VK_OBJECT_TYPE_SEMAPHORE)
 VK_DEFINE_NONDISP_HANDLE_CASTS(anv_ycbcr_conversion, base,
                                VkSamplerYcbcrConversion,
                                VK_OBJECT_TYPE_SAMPLER_YCBCR_CONVERSION)

src/intel/vulkan/anv_android.c
@@ -845,7 +845,7 @@ anv_AcquireImageANDROID(
          .handleType = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_SYNC_FD_BIT,
          .fd = semaphore_fd,
       };
-      result = anv_ImportSemaphoreFdKHR(device_h, &info);
+      result = vk_common_ImportSemaphoreFdKHR(device_h, &info);
       if (result == VK_SUCCESS)
          semaphore_fd = -1; /* ANV took ownership */
    }
@@ -858,7 +858,7 @@ anv_AcquireImageANDROID(
          .handleType = VK_EXTERNAL_FENCE_HANDLE_TYPE_SYNC_FD_BIT,
          .fd = fence_fd,
       };
-      result = anv_ImportFenceFdKHR(device_h, &info);
+      result = vk_common_ImportFenceFdKHR(device_h, &info);
       if (result == VK_SUCCESS)
          fence_fd = -1; /* ANV took ownership */
    }

src/intel/vulkan/anv_wsi.c
@@ -24,6 +24,9 @@
 #include "anv_private.h"
 #include "anv_measure.h"
 #include "wsi_common.h"
+#include "vk_fence.h"
+#include "vk_queue.h"
+#include "vk_semaphore.h"
 #include "vk_util.h"
 
 static PFN_vkVoidFunction
@@ -39,19 +42,19 @@ anv_wsi_signal_semaphore_for_memory(VkDevice _device,
                                     VkDeviceMemory _memory)
 {
    ANV_FROM_HANDLE(anv_device, device, _device);
-   ANV_FROM_HANDLE(anv_semaphore, semaphore, _semaphore);
+   VK_FROM_HANDLE(vk_semaphore, semaphore, _semaphore);
    ANV_FROM_HANDLE(anv_device_memory, memory, _memory);
+   ASSERTED VkResult result;
 
    /* Put a BO semaphore with the image BO in the temporary.  For BO binary
     * semaphores, we always set EXEC_OBJECT_WRITE so this creates a WaR
     * hazard with the display engine's read to ensure that no one writes to
     * the image before the read is complete.
     */
-   anv_semaphore_reset_temporary(device, semaphore);
+   vk_semaphore_reset_temporary(&device->vk, semaphore);
 
-   struct anv_semaphore_impl *impl = &semaphore->temporary;
-   impl->type = ANV_SEMAPHORE_TYPE_WSI_BO;
-   impl->bo = anv_bo_ref(memory->bo);
+   result = anv_sync_create_for_bo(device, memory->bo, &semaphore->temporary);
+   assert(result == VK_SUCCESS);
 }
 
 static void
@@ -60,19 +63,18 @@ anv_wsi_signal_fence_for_memory(VkDevice _device,
                                 VkDeviceMemory _memory)
 {
    ANV_FROM_HANDLE(anv_device, device, _device);
-   ANV_FROM_HANDLE(anv_fence, fence, _fence);
+   VK_FROM_HANDLE(vk_fence, fence, _fence);
    ANV_FROM_HANDLE(anv_device_memory, memory, _memory);
+   ASSERTED VkResult result;
 
    /* Put a BO fence with the image BO in the temporary.  For BO fences, we
     * always just wait until the BO isn't busy and reads from the BO should
     * count as busy.
     */
-   anv_fence_reset_temporary(device, fence);
+   vk_fence_reset_temporary(&device->vk, fence);
 
-   struct anv_fence_impl *impl = &fence->temporary;
-   impl->type = ANV_FENCE_TYPE_WSI_BO;
-   impl->bo.bo = anv_bo_ref(memory->bo);
-   impl->bo.state = ANV_BO_FENCE_STATE_SUBMITTED;
+   result = anv_sync_create_for_bo(device, memory->bo, &fence->temporary);
+   assert(result == VK_SUCCESS);
 }
 
 VkResult
@@ -118,6 +120,7 @@ VkResult anv_QueuePresentKHR(
 {
    ANV_FROM_HANDLE(anv_queue, queue, _queue);
    struct anv_device *device = queue->device;
+   VkResult result;
 
    if (device->debug_frame_desc) {
       device->debug_frame_desc->frame_id++;
@@ -127,64 +130,24 @@ VkResult anv_QueuePresentKHR(
       }
    }
 
-   if (device->has_thread_submit &&
-       pPresentInfo->waitSemaphoreCount > 0) {
-      /* Make sure all of the dependency semaphores have materialized when
-       * using a threaded submission.
-       */
-      VK_MULTIALLOC(ma);
-      VK_MULTIALLOC_DECL(&ma, uint64_t, values,
-                              pPresentInfo->waitSemaphoreCount);
-      VK_MULTIALLOC_DECL(&ma, uint32_t, syncobjs,
-                              pPresentInfo->waitSemaphoreCount);
+   result = vk_queue_wait_before_present(&queue->vk, pPresentInfo);
+   if (result != VK_SUCCESS)
+      return result;
 
-      if (!vk_multialloc_alloc(&ma, &device->vk.alloc,
-                               VK_SYSTEM_ALLOCATION_SCOPE_COMMAND))
-         return vk_error(queue, VK_ERROR_OUT_OF_HOST_MEMORY);
-
-      uint32_t wait_count = 0;
-      for (uint32_t i = 0; i < pPresentInfo->waitSemaphoreCount; i++) {
-         ANV_FROM_HANDLE(anv_semaphore, semaphore, pPresentInfo->pWaitSemaphores[i]);
-         struct anv_semaphore_impl *impl =
-            semaphore->temporary.type != ANV_SEMAPHORE_TYPE_NONE ?
-            &semaphore->temporary : &semaphore->permanent;
-
-         assert(impl->type == ANV_SEMAPHORE_TYPE_DRM_SYNCOBJ);
-         syncobjs[wait_count] = impl->syncobj;
-         values[wait_count] = 0;
-         wait_count++;
-      }
-
-      int ret = 0;
-      if (wait_count > 0) {
-         ret =
-            anv_gem_syncobj_timeline_wait(device,
-                                          syncobjs, values, wait_count,
-                                          anv_get_absolute_timeout(INT64_MAX),
-                                          true /* wait_all */,
-                                          true /* wait_materialize */);
-      }
-
-      vk_free(&device->vk.alloc, values);
-
-      if (ret)
-         return vk_error(queue, VK_ERROR_DEVICE_LOST);
-   }
-
-   VkResult result = wsi_common_queue_present(&device->physical->wsi_device,
-                                              anv_device_to_handle(queue->device),
-                                              _queue, 0,
-                                              pPresentInfo);
+   result = wsi_common_queue_present(&device->physical->wsi_device,
+                                     anv_device_to_handle(queue->device),
+                                     _queue, 0,
+                                     pPresentInfo);
 
    for (uint32_t i = 0; i < pPresentInfo->waitSemaphoreCount; i++) {
-      ANV_FROM_HANDLE(anv_semaphore, semaphore, pPresentInfo->pWaitSemaphores[i]);
+      VK_FROM_HANDLE(vk_semaphore, semaphore, pPresentInfo->pWaitSemaphores[i]);
       /* From the Vulkan 1.0.53 spec:
        *
        *    "If the import is temporary, the implementation must restore the
        *    semaphore to its prior permanent state after submitting the next
        *    semaphore wait operation."
        */
-      anv_semaphore_reset_temporary(queue->device, semaphore);
+      vk_semaphore_reset_temporary(&queue->device->vk, semaphore);
    }
 
    return result;

src/intel/vulkan/anv_wsi_display.c
@@ -22,6 +22,7 @@
 
 #include "anv_private.h"
 #include "wsi_common.h"
+#include "vk_fence.h"
 #include "vk_util.h"
 #include "wsi_common_display.h"
 
@@ -34,26 +35,27 @@ anv_RegisterDeviceEventEXT(VkDevice _device,
                             VkFence *_fence)
 {
    ANV_FROM_HANDLE(anv_device, device, _device);
-   struct anv_fence *fence;
+   struct vk_fence *fence;
    VkResult ret;
 
-   fence = vk_object_zalloc(&device->vk, allocator, sizeof (*fence),
-                            VK_OBJECT_TYPE_FENCE);
-   if (!fence)
-      return vk_error(device, VK_ERROR_OUT_OF_HOST_MEMORY);
-
-   fence->permanent.type = ANV_FENCE_TYPE_WSI;
+   const VkFenceCreateInfo info = {
+      .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+      .flags = 0,
+   };
+   ret = vk_fence_create(&device->vk, &info, allocator, &fence);
+   if (ret != VK_SUCCESS)
+      return ret;
 
    ret = wsi_register_device_event(_device,
                                    &device->physical->wsi_device,
                                    device_event_info,
                                    allocator,
-                                   &fence->permanent.sync_wsi,
+                                   &fence->temporary,
                                    -1);
    if (ret == VK_SUCCESS)
-      *_fence = anv_fence_to_handle(fence);
+      *_fence = vk_fence_to_handle(fence);
    else
-      vk_free2(&device->vk.alloc, allocator, fence);
+      vk_fence_destroy(&device->vk, fence, allocator);
    return ret;
 }
 
@@ -65,23 +67,24 @@ anv_RegisterDisplayEventEXT(VkDevice _device,
                              VkFence *_fence)
 {
    ANV_FROM_HANDLE(anv_device, device, _device);
-   struct anv_fence *fence;
+   struct vk_fence *fence;
    VkResult ret;
 
-   fence = vk_object_zalloc(&device->vk, allocator, sizeof (*fence),
-                            VK_OBJECT_TYPE_FENCE);
-   if (!fence)
-      return VK_ERROR_OUT_OF_HOST_MEMORY;
-
-   fence->permanent.type = ANV_FENCE_TYPE_WSI;
+   const VkFenceCreateInfo info = {
+      .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+      .flags = 0,
+   };
+   ret = vk_fence_create(&device->vk, &info, allocator, &fence);
+   if (ret != VK_SUCCESS)
+      return ret;
 
    ret = wsi_register_display_event(
       _device, &device->physical->wsi_device,
-      display, display_event_info, allocator, &fence->permanent.sync_wsi, -1);
+      display, display_event_info, allocator, &fence->temporary, -1);
 
    if (ret == VK_SUCCESS)
-      *_fence = anv_fence_to_handle(fence);
+      *_fence = vk_fence_to_handle(fence);
    else
-      vk_free2(&device->vk.alloc, allocator, fence);
+      vk_fence_destroy(&device->vk, fence, allocator);
    return ret;
 }

commit f9b78058104cfd1fb8c5634b6f4ca738c6535b04

anv: Implement vk_device::create_sync_for_memory
Fixes: 24cccfb54a63 ("anv: Convert to the common sync and submit framework")
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14237>

src/intel/vulkan/anv_bo_sync.c
@@ -212,10 +212,11 @@ const struct vk_sync_type anv_bo_sync_type = {
    .wait_many = anv_bo_sync_wait,
 };
 
-VkResult
-anv_sync_create_for_bo(struct anv_device *device,
-                       struct anv_bo *bo,
-                       struct vk_sync **sync_out)
+static VkResult
+_anv_sync_create_for_bo(struct anv_device *device,
+                        struct anv_bo *bo,
+                        enum anv_bo_sync_state state,
+                        struct vk_sync **sync_out)
 {
    struct anv_bo_sync *bo_sync;
 
@@ -225,10 +226,35 @@ anv_sync_create_for_bo(struct anv_device *device,
       return vk_error(device, VK_ERROR_OUT_OF_HOST_MEMORY);
 
    bo_sync->sync.type = &anv_bo_sync_type;
-   bo_sync->state = ANV_BO_SYNC_STATE_SUBMITTED;
+   bo_sync->state = state;
    bo_sync->bo = anv_bo_ref(bo);
 
    *sync_out = &bo_sync->sync;
 
    return VK_SUCCESS;
+}
+
+VkResult
+anv_sync_create_for_bo(struct anv_device *device,
+                       struct anv_bo *bo,
+                       struct vk_sync **sync_out)
+{
+   return _anv_sync_create_for_bo(device, bo, ANV_BO_SYNC_STATE_SUBMITTED,
+                                  sync_out);
+}
+
+VkResult
+anv_create_sync_for_memory(struct vk_device *vk_device,
+                           VkDeviceMemory memory,
+                           bool signal_memory,
+                           struct vk_sync **sync_out)
+{
+   ANV_FROM_HANDLE(anv_device_memory, mem, memory);
+   struct anv_device *device =
+      container_of(vk_device, struct anv_device, vk);
+
+   enum anv_bo_sync_state state = signal_memory ?
+      ANV_BO_SYNC_STATE_RESET : ANV_BO_SYNC_STATE_SUBMITTED;
+
+   return _anv_sync_create_for_bo(device, mem->bo, state, sync_out);
 }

src/intel/vulkan/anv_device.c
@@ -3025,6 +3025,7 @@ VkResult anv_CreateDevice(
    }
 
    device->vk.check_status = anv_device_check_status;
+   device->vk.create_sync_for_memory = anv_create_sync_for_memory;
    vk_device_set_drm_fd(&device->vk, device->fd);
 
    uint32_t num_queues = 0;

src/intel/vulkan/anv_private.h
@@ -3176,6 +3176,11 @@ VkResult anv_sync_create_for_bo(struct anv_device *device,
                                 struct anv_bo *bo,
                                 struct vk_sync **sync_out);
 
+VkResult anv_create_sync_for_memory(struct vk_device *device,
+                                    VkDeviceMemory memory,
+                                    bool signal_memory,
+                                    struct vk_sync **sync_out);
+
 struct anv_event {
    struct vk_object_base                        base;
    uint64_t                                     semaphore;