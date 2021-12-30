openharmony/third_party_mesa3d

commit 98e11e02c902ccd0f13baddac470f1a76f432dd6

mesa: Add support for EXT_clear_texture
This extension implements a subset of ARB_clear_texture (i.e.
only the features that are not available in OpenGL ES have been
dropped).

v2: Move call declarations from function to offsets  (Emil)

v3: Update llvmpipe and softpipe expectations

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10325>

src/mapi/glapi/gen/static_data.py
@@ -1689,6 +1689,8 @@ offsets = {
     "VertexAttribs2hvNV": 1653,
     "VertexAttribs3hvNV": 1654,
     "VertexAttribs4hvNV": 1655,
+    "ClearTexImageEXT": 1656,
+    "ClearTexSubImageEXT": 1657,
 }
 
 functions = [

src/mapi/glapi/gen/es_EXT.xml
@@ -1295,6 +1295,32 @@
 
 </category>
 
+<category name="GL_EXT_clear_texture" number="269">
+
+    <function name ="ClearTexImageEXT" es2="3.1" alias="ClearTexImage">
+        <param name="texture" type="GLuint"/>
+        <param name="level" type="GLint"/>
+        <param name="format" type="GLenum"/>
+        <param name="type" type="GLenum"/>
+        <param name="data" type="const GLvoid *"/>
+    </function>
+
+    <function name ="ClearTexSubImageEXT" es2="3.1" alias="ClearTexSubImage">
+        <param name="texture" type="GLuint"/>
+        <param name="level" type="GLint"/>
+        <param name="xoffset" type="GLint"/>
+        <param name="yoffset" type="GLint"/>
+        <param name="zoffset" type="GLint"/>
+        <param name="width" type="GLsizei"/>
+        <param name="height" type="GLsizei"/>
+        <param name="depth" type="GLsizei"/>
+        <param name="format" type="GLenum"/>
+        <param name="type" type="GLenum"/>
+        <param name="data" type="const GLvoid *"/>
+    </function>
+
+</category>
+
 <category name="GL_EXT_clip_control" number="290">
 
     <enum name="LOWER_LEFT_EXT" value = "0x8CA1"/>

src/mesa/main/extensions_table.h
@@ -219,6 +219,7 @@ EXT(EXT_blend_func_separate                 , EXT_blend_func_separate           
 EXT(EXT_blend_minmax                        , EXT_blend_minmax                       , GLL,  x , ES1, ES2, 1995)
 EXT(EXT_blend_subtract                      , dummy_true                             , GLL,  x ,  x ,  x , 1995)
 EXT(EXT_buffer_storage                      , ARB_buffer_storage                     ,  x ,  x ,  x ,  31, 2015)
+EXT(EXT_clear_texture                       , ARB_clear_texture                      ,  x ,  x ,  x ,  31, 2016)
 EXT(EXT_clip_control                        , ARB_clip_control                       ,  x ,  x ,  x , ES2, 2017)
 EXT(EXT_clip_cull_distance                  , ARB_cull_distance                      ,  x ,  x ,  x ,  30, 2016)
 EXT(EXT_color_buffer_float                  , dummy_true                             ,  x ,  x ,  x ,  30, 2013)

src/mesa/main/tests/dispatch_sanity.cpp
@@ -2497,6 +2497,10 @@ const struct function gles2_functions_possible[] = {
    { "glGetQueryObjectivEXT", 20, -1 },
    { "glGetQueryObjectuivEXT", 20, -1 },
 
+   /* GL_EXT_clear_texture */
+   { "glClearTexImageEXT", 31, -1 },
+   { "glClearTexSubImageEXT", 31, -1 },
+
    /* GL_EXT_clip_control */
    { "glClipControlEXT", 20, -1 },
 

commit 80689e184871b255b6c67aa0be6ccf5d3536d8c2

glapi: should not add alias function to static_data.py
Alias function should not be assigned an offset, otherwise new added
function will get error:

  Exception: entries are not ordered by slots

Fixes: 98e11e02c90 ("mesa: Add support for EXT_clear_texture")

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Signed-off-by: Qiang Yu <yuq825@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14223>

src/mapi/glapi/gen/static_data.py
@@ -1689,8 +1689,6 @@ offsets = {
     "VertexAttribs2hvNV": 1653,
     "VertexAttribs3hvNV": 1654,
     "VertexAttribs4hvNV": 1655,
-    "ClearTexImageEXT": 1656,
-    "ClearTexSubImageEXT": 1657,
 }
 
 functions = [