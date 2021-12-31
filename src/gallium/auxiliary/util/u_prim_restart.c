openharmony/third_party_mesa3d

commit 5216e66b6ffbad63b22a02e9ab4ce0b5ea4765e4

u_prim_restart: Fix index scanning with start offset
Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13681>

src/gallium/auxiliary/util/u_prim_restart.c
@@ -239,7 +239,7 @@ util_prim_restart_convert_to_direct(const void *index_map,
 #define SCAN_INDEXES(TYPE) \
    for (i = 0; i <= draw->count; i++) { \
       if (i == draw->count || \
-          ((const TYPE *) index_map)[i] == info->restart_index) { \
+          ((const TYPE *) index_map)[draw->start + i] == info->restart_index) { \
          /* cut / restart */ \
          if (count > 0) { \
             if (!add_range(info->mode, &ranges, draw->start + start, count, draw->index_bias)) { \

commit be74fa5e1b188d6fcac64bbea835237122edfecb

gallium/aux: Move index offsetting from prim restart to primconvert
Fixes: 5216e66b ("u_prim_restart: Fix index scanning with start offset")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/5799
Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14346>

src/gallium/auxiliary/util/u_prim_restart.c
@@ -239,7 +239,7 @@ util_prim_restart_convert_to_direct(const void *index_map,
 #define SCAN_INDEXES(TYPE) \
    for (i = 0; i <= draw->count; i++) { \
       if (i == draw->count || \
-          ((const TYPE *) index_map)[draw->start + i] == info->restart_index) { \
+          ((const TYPE *) index_map)[i] == info->restart_index) { \
          /* cut / restart */ \
          if (count > 0) { \
             if (!add_range(info->mode, &ranges, draw->start + start, count, draw->index_bias)) { \

src/util/indices/u_primconvert.c
@@ -148,7 +148,7 @@ primconvert_init_draw(struct primconvert_context *pc,
          src = pipe_buffer_map(pc->pipe, info->index.resource,
                                PIPE_MAP_READ, &src_transfer);
       }
-      src = (const uint8_t *)src;
+      const void *restart_src = (const uint8_t *)src + draw->start * info->index_size;
 
       /* if the resulting primitive type is not supported by the driver for primitive restart,
        * or if the original primitive type was not supported by the driver,
@@ -160,7 +160,7 @@ primconvert_init_draw(struct primconvert_context *pc,
          /* step 1: rewrite draw to not use primitive primitive restart;
           *         this pre-filters degenerate primitives
           */
-         direct_draws = util_prim_restart_convert_to_direct(src, info, draw, &num_direct_draws,
+         direct_draws = util_prim_restart_convert_to_direct(restart_src, info, draw, &num_direct_draws,
                                                             &new_info->min_index, &new_info->max_index, &total_index_count);
          new_info->primitive_restart = false;
          /* step 2: get a translator function which does nothing but handle any index size conversions