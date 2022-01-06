openharmony/third_party_mesa3d

commit 783a967aa22260764fc2a2a760b1cabf53f7a36c

vbo/dlist: convert LINE_STRIPS to LINES
Less primitive modes allows for better primitive merging.

Lines are always used (instead of picking dynamically lines or line
strips for instance) because:
- they don't need primitive restarts to be merged
- they perform better (at least on radeonsi) - SPECviewperf13 snx subtests
  with lines (like 4 or 10) are 1.5x-2x faster.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7078>

src/mesa/vbo/vbo_save_api.c
@@ -605,7 +605,7 @@ compile_vertex_list(struct gl_context *ctx)
       int end = node->prims[node->prim_count - 1].start +
                 node->prims[node->prim_count - 1].count;
       int total_vert_count = end - node->prims[0].start;
-      int max_indices_count = total_vert_count;
+      int max_indices_count = total_vert_count * 2;
       int size = max_indices_count * sizeof(uint32_t);
       uint32_t* indices = (uint32_t*) malloc(size);
       uint32_t max_index = 0, min_index = 0xFFFFFFFF;
@@ -618,8 +618,28 @@ compile_vertex_list(struct gl_context *ctx)
          int vertex_count = node->prims[i].count;
          int start = idx;
 
-         for (unsigned j = 0; j < vertex_count; j++) {
-            indices[idx++] = node->prims[i].start + j;
+         /* Convert line strips to lines if it'll allow if the previous
+          * prim mode is GL_LINES or if the next primitive mode is
+          * GL_LINES or GL_LINE_LOOP.
+          */
+         if (node->prims[i].mode == GL_LINE_STRIP &&
+             ((i > 0 && node->prims[i - 1].mode == GL_LINES) ||
+              (i < node->prim_count - 1 &&
+               (node->prims[i + 1].mode == GL_LINE_STRIP ||
+                node->prims[i + 1].mode == GL_LINES)))) {
+            for (unsigned j = 0; j < vertex_count; j++) {
+               indices[idx++] = node->prims[i].start + j;
+               /* Repeat all but the first/last indices. */
+               if (j && j != vertex_count - 1) {
+                  indices[idx++] = node->prims[i].start + j;
+                  node->prims[i].count++;
+               }
+            }
+            node->prims[i].mode = GL_LINES;
+         } else {
+            for (unsigned j = 0; j < vertex_count; j++) {
+               indices[idx++] = node->prims[i].start + j;
+            }
          }
 
          min_index = MIN2(min_index, indices[start]);

commit c6db8f5a500fa9b71fc0c90c9629496697153bad

vbo/dlist: add vertices to incomplete primitives
If a primitive is added with missing vertices, this will shift all
the vertices and produce incorrect rendering.

In issue #5786 the app uses GL_LINE_STRIPS with a single vertex.

Adding extra vertices can make the initial estimation for the index
buffer size incorrect, so this buffer can now be growed if needed.

Fixes: 783a967aa22 ("vbo/dlist: convert LINE_STRIPS to LINES")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/5786
Acked-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14243>

src/mesa/vbo/vbo_save_api.c
@@ -117,6 +117,7 @@ USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "util/u_memory.h"
 #include "util/hash_table.h"
 #include "util/indices/u_indices.h"
+#include "util/u_prim.h"
 
 #include "gallium/include/pipe/p_state.h"
 
@@ -599,10 +600,8 @@ compile_vertex_list(struct gl_context *ctx)
    /* converting primitive types may result in many more indices */
    bool all_prims_supported = (ctx->Const.DriverSupportedPrimMask & BITFIELD_MASK(PIPE_PRIM_MAX)) == BITFIELD_MASK(PIPE_PRIM_MAX);
    int max_index_count = total_vert_count * (all_prims_supported ? 2 : 3);
-
-   int size = max_index_count * sizeof(uint32_t);
-   uint32_t* indices = (uint32_t*) malloc(size);
-   void *tmp_indices = all_prims_supported ? NULL : malloc(size);
+   uint32_t* indices = (uint32_t*) malloc(max_index_count * sizeof(uint32_t));
+   void *tmp_indices = all_prims_supported ? NULL : malloc(max_index_count * sizeof(uint32_t));
    struct _mesa_prim *merged_prims = NULL;
 
    int idx = 0;
@@ -630,6 +629,13 @@ compile_vertex_list(struct gl_context *ctx)
       int vertex_count = original_prims[i].count;
       if (!vertex_count) {
          continue;
+      }
+
+      /* Increase indices storage if the original estimation was too small. */
+      if (idx + 3 * vertex_count > max_index_count) {
+         max_index_count = max_index_count + 3 * vertex_count;
+         indices = (uint32_t*) realloc(indices, max_index_count * sizeof(uint32_t));
+         tmp_indices = all_prims_supported ? NULL : realloc(tmp_indices, max_index_count * sizeof(uint32_t));
       }
 
       /* Line strips may get converted to lines */
@@ -719,6 +725,16 @@ compile_vertex_list(struct gl_context *ctx)
                                         temp_vertices_buffer, &max_index);
          }
       }
+
+      /* Duplicate the last vertex for incomplete primitives */
+      unsigned min_vert = u_prim_vertex_count(mode)->min;
+      for (unsigned j = vertex_count; j < min_vert; j++) {
+         indices[idx++] = add_vertex(save, vertex_to_index,
+                                     converted_prim ? CAST_INDEX(tmp_indices, index_size, vertex_count - 1) :
+                                                      original_prims[i].start + vertex_count - 1,
+                                     temp_vertices_buffer, &max_index);
+      }
+
 #undef CAST_INDEX
       if (merge_prims) {
          /* Update vertex count. */