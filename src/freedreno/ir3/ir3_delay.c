openharmony/third_party_mesa3d

commit 545dc052164fcd835f6b25b77913afbcdf7f91d0

ir3/delay: Fix full->half and half->full delay
The current compiler never does this, but the new compiler will start to
in mergeregs mode. There is an extra penalty for this.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9842>

src/freedreno/ir3/ir3_delay.c
@@ -88,12 +88,25 @@ ir3_delayslots(struct ir3_instruction *assigner,
 	if (is_flow(consumer) || is_sfu(consumer) || is_tex(consumer) ||
 			is_mem(consumer)) {
 		return 6;
-	} else if ((is_mad(consumer->opc) || is_madsh(consumer->opc)) &&
-			(n == 3)) {
-		/* special case, 3rd src to cat3 not required on first cycle */
-		return 1;
 	} else {
-		return 3;
+		/* assigner and consumer are both alu */
+		assert(n > 0);
+
+		/* In mergedregs mode, there is an extra 2-cycle penalty when half of
+		 * a full-reg is read as a half-reg or when a half-reg is read as a
+		 * full-reg.
+		 */
+		bool mismatched_half =
+			(assigner->regs[0]->flags & IR3_REG_HALF) !=
+			(consumer->regs[n - 1]->flags & IR3_REG_HALF);
+		unsigned penalty = mismatched_half ? 2 : 0;
+		if ((is_mad(consumer->opc) || is_madsh(consumer->opc)) &&
+			(n == 3)) {
+			/* special case, 3rd src to cat3 not required on first cycle */
+			return 1 + penalty;
+		} else {
+			return 3 + penalty;
+		}
 	}
 }
 

commit c95b7ff1ca3f98ee2b09e05409d614cc559dc085

ir3: Bump type mismatch penalty to 3
After some experimentation with computerator, it seems on a618 that
writing a full register and then reading half of it as a half register
requires a delay of 6, the same as the delay for cat5/cat6 sources. The
other direction only has a delay of 5, but just bump it unconditionally
out of an abundance of caution.

Fixes: 545dc052164 ("ir3/delay: Fix full->half and half->full delay")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14246>

src/freedreno/ir3/ir3_delay.c
@@ -98,7 +98,7 @@ ir3_delayslots(struct ir3_instruction *assigner,
        */
       bool mismatched_half = (assigner->dsts[0]->flags & IR3_REG_HALF) !=
                              (consumer->srcs[n]->flags & IR3_REG_HALF);
-      unsigned penalty = mismatched_half ? 2 : 0;
+      unsigned penalty = mismatched_half ? 3 : 0;
       if ((is_mad(consumer->opc) || is_madsh(consumer->opc)) && (n == 2)) {
          /* special case, 3rd src to cat3 not required on first cycle */
          return 1 + penalty;