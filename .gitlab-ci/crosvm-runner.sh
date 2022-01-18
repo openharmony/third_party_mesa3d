openharmony/third_party_mesa3d

commit 97b8165c4be3fbff1f654af62eb60185266b4173

virgl/ci: Run each dEQP instance in its own VM
Currently we run deqp-runner inside a single VM, which makes very poor
use of the available CPUs because Virgl has a bottleneck in the VMM that
serializes everything.

With this change, we can run several Crosvm instances in a runner and
make full use of the CPUs. Getting the same coverage with 3 runners
instead of 6.

Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Reviewed-by: Emma Anholt <emma@anholt.net>
Reviewed-by: Corentin Noël <corentin.noel@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12828>

.gitlab-ci/crosvm-runner.sh
@@ -1,25 +1,20 @@
 #!/bin/sh
 
-set -x
+set -e
 
-ln -sf $CI_PROJECT_DIR/install /install
+# This script can be called concurrently, pass arguments and env in a per-instance tmp dir
+export DEQP_TEMP_DIR=`mktemp -d /tmp.XXXXXXXXXX`
 
-export LD_LIBRARY_PATH=$CI_PROJECT_DIR/install/lib/
-export EGL_PLATFORM=surfaceless
+# The dEQP binary needs to run from the directory it's in
+if [ -z "${1##*"deqp"*}" ]; then
+  PWD=`dirname $1`
+fi
 
-export -p > /crosvm-env.sh
-export GALLIUM_DRIVER="$CROSVM_GALLIUM_DRIVER"
-export GALLIVM_PERF="nopt"
-export LIBGL_ALWAYS_SOFTWARE="true"
+export -p > $DEQP_TEMP_DIR/crosvm-env.sh
 
-CROSVM_KERNEL_ARGS="root=my_root rw rootfstype=virtiofs loglevel=3 init=$CI_PROJECT_DIR/install/crosvm-init.sh ip=192.168.30.2::192.168.30.1:255.255.255.0:crosvm:eth0"
+CROSVM_KERNEL_ARGS="console=null root=my_root rw rootfstype=virtiofs init=$CI_PROJECT_DIR/install/crosvm-init.sh ip=192.168.30.2::192.168.30.1:255.255.255.0:crosvm:eth0 -- $DEQP_TEMP_DIR"
 
-# Temporary results dir because from the guest we cannot write to /
-mkdir -p /results
-mount -t tmpfs tmpfs /results
-
-mkdir -p /piglit/.gitlab-ci/piglit
-mount -t tmpfs tmpfs /piglit/.gitlab-ci/piglit
+echo $@ > $DEQP_TEMP_DIR/crosvm-script.sh
 
 unset DISPLAY
 unset XDG_RUNTIME_DIR
@@ -27,20 +22,25 @@ unset XDG_RUNTIME_DIR
 /usr/sbin/iptables-legacy  -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-# We aren't testing LLVMPipe here, so we don't need to validate NIR on the host
-export NIR_VALIDATE=0
+# Send output from guest to host
+touch $DEQP_TEMP_DIR/stderr $DEQP_TEMP_DIR/stdout
+tail -f $DEQP_TEMP_DIR/stderr > /dev/stderr &
+ERR_TAIL_PID=$!
+tail -f $DEQP_TEMP_DIR/stdout > /dev/stdout &
+OUT_TAIL_PID=$!
 
-crosvm run \
+trap "exit \$exit_code" INT TERM
+trap "exit_code=\$?; kill $ERR_TAIL_PID $OUT_TAIL_PID" EXIT
+
+# We aren't testing LLVMPipe here, so we don't need to validate NIR on the host
+NIR_VALIDATE=0 LIBGL_ALWAYS_SOFTWARE="true" GALLIUM_DRIVER="$CROSVM_GALLIUM_DRIVER" stdbuf -oL crosvm run \
   --gpu "$CROSVM_GPU_ARGS" \
   -m 4096 \
-  -c $((FDO_CI_CONCURRENT > 1 ? FDO_CI_CONCURRENT - 1 : 1)) \
+  -c 2 \
   --disable-sandbox \
   --shared-dir /:my_root:type=fs:writeback=true:timeout=60:cache=always \
   --host_ip=192.168.30.1 --netmask=255.255.255.0 --mac "AA:BB:CC:00:00:12" \
   -p "$CROSVM_KERNEL_ARGS" \
-  /lava-files/bzImage
+  /lava-files/bzImage >> $DEQP_TEMP_DIR/stderr > /dev/null
 
-mkdir -p $CI_PROJECT_DIR/results
-mv /results/* $CI_PROJECT_DIR/results/.
-
-test -f $CI_PROJECT_DIR/results/success
+exit `cat $DEQP_TEMP_DIR/exit_code`

.gitlab-ci/crosvm-init.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 
-set -ex
+set -e
+
+export DEQP_TEMP_DIR=$1
 
 mount -t proc none /proc
 mount -t sysfs none /sys
-mount -t devtmpfs none /dev || echo possibly already mounted
 mkdir -p /dev/pts
 mount -t devpts devpts /dev/pts
 mount -t tmpfs tmpfs /tmp
 
-. /crosvm-env.sh
+. $DEQP_TEMP_DIR/crosvm-env.sh
 
-# / is ro
-export PIGLIT_REPLAY_EXTRA_ARGS="$PIGLIT_REPLAY_EXTRA_ARGS --db-path /tmp/replayer-db"
+cd $PWD
 
-if sh $CROSVM_TEST_SCRIPT; then
-    touch /results/success
-fi
+dmesg --level crit,err,warn -w >> $DEQP_TEMP_DIR/stderr &
 
-sleep 5   # Leave some time to get the last output flushed out
+set +e
+stdbuf -oL sh $DEQP_TEMP_DIR/crosvm-script.sh 2>> $DEQP_TEMP_DIR/stderr >> $DEQP_TEMP_DIR/stdout
+echo $? > $DEQP_TEMP_DIR/exit_code
+set -e
+
+sync
+sleep 1
 
 poweroff -d -n -f || true
 
-sleep 10   # Just in case init would exit before the kernel shuts down the VM
-
-exit 1
+sleep 1   # Just in case init would exit before the kernel shuts down the VM

src/gallium/drivers/virgl/ci/deqp-virgl-gl.toml
@@ -1,51 +1,57 @@
 [[deqp]]
-deqp = "/deqp/modules/gles2/deqp-gles2"
+deqp = "/install/crosvm-runner.sh"
 caselists = ["/deqp/mustpass/gles2-master.txt"]
 deqp_args = [
+    "/deqp/modules/gles2/deqp-gles2",
     "--deqp-surface-width=256",
     "--deqp-surface-height=256",
     "--deqp-surface-type=pbuffer",
     "--deqp-gl-config-name=rgba8888d24s8ms0",
     "--deqp-visibility=hidden"
 ]
+timeout = 360.0 # Starting 8 Crosvm instances simultaneously can take some time
 version_check = "GL ES 3.2.*git"
-renderer_check = "virgl"
+renderer_check = "virgl.*llvmpipe"
 
 [[deqp]]
-deqp = "/deqp/modules/gles3/deqp-gles3"
+deqp = "/install/crosvm-runner.sh"
 caselists = ["/deqp/mustpass/gles3-master.txt"]
 deqp_args = [
+    "/deqp/modules/gles3/deqp-gles3",
     "--deqp-surface-width=256",
     "--deqp-surface-height=256",
     "--deqp-surface-type=pbuffer",
     "--deqp-gl-config-name=rgba8888d24s8ms0",
     "--deqp-visibility=hidden"
 ]
-timeout = 180.0
+timeout = 360.0 # Starting 8 Crosvm instances simultaneously can take some time
 
 [[deqp]]
-deqp = "/deqp/modules/gles31/deqp-gles31"
+deqp = "/install/crosvm-runner.sh"
 caselists = ["/deqp/mustpass/gles31-master.txt"]
 deqp_args = [
+    "/deqp/modules/gles31/deqp-gles31",
     "--deqp-surface-width=256",
     "--deqp-surface-height=256",
     "--deqp-surface-type=pbuffer",
     "--deqp-gl-config-name=rgba8888d24s8ms0",
     "--deqp-visibility=hidden"
 ]
-timeout = 180.0
+timeout = 360.0 # Starting 8 Crosvm instances simultaneously can take some time
 
 [[deqp]]
-deqp = "/deqp/external/openglcts/modules/glcts"
+deqp = "/install/crosvm-runner.sh"
 caselists = [
     "/deqp/mustpass/gl30-master.txt",
     "/deqp/mustpass/gl31-master.txt",
     "/deqp/mustpass/gl32-master.txt",
 ]
 deqp_args = [
+    "/deqp/external/openglcts/modules/glcts",
     "--deqp-surface-width=256",
     "--deqp-surface-height=256",
     "--deqp-surface-type=pbuffer",
     "--deqp-gl-config-name=rgba8888d24s8ms0",
     "--deqp-visibility=hidden"
 ]
+timeout = 360.0 # Starting 8 Crosvm instances simultaneously can take some time

src/gallium/drivers/virgl/ci/deqp-virgl-gles.toml
@@ -1,36 +1,57 @@
 [[deqp]]
-deqp = "/deqp/modules/gles2/deqp-gles2"
+deqp = "/install/crosvm-runner.sh"
 caselists = ["/deqp/mustpass/gles2-master.txt"]
 deqp_args = [
+    "/deqp/modules/gles2/deqp-gles2",
     "--deqp-surface-width=256",
     "--deqp-surface-height=256",
     "--deqp-surface-type=pbuffer",
     "--deqp-gl-config-name=rgba8888d24s8ms0",
     "--deqp-visibility=hidden"
 ]
-version_check = "GL ES 3.1.*git"
-renderer_check = "virgl"
+timeout = 360.0 # Starting 8 Crosvm instances simultaneously can take some time
+version_check = "GL ES 3.2.*git"
+renderer_check = "virgl.*llvmpipe"
 
 [[deqp]]
-deqp = "/deqp/modules/gles3/deqp-gles3"
+deqp = "/install/crosvm-runner.sh"
 caselists = ["/deqp/mustpass/gles3-master.txt"]
 deqp_args = [
+    "/deqp/modules/gles3/deqp-gles3",
     "--deqp-surface-width=256",
     "--deqp-surface-height=256",
     "--deqp-surface-type=pbuffer",
     "--deqp-gl-config-name=rgba8888d24s8ms0",
     "--deqp-visibility=hidden"
 ]
-timeout = 180.0
+timeout = 360.0 # Starting 8 Crosvm instances simultaneously can take some time
 
 [[deqp]]
-deqp = "/deqp/modules/gles31/deqp-gles31"
+deqp = "/install/crosvm-runner.sh"
 caselists = ["/deqp/mustpass/gles31-master.txt"]
 deqp_args = [
+    "/deqp/modules/gles31/deqp-gles31",
     "--deqp-surface-width=256",
     "--deqp-surface-height=256",
     "--deqp-surface-type=pbuffer",
     "--deqp-gl-config-name=rgba8888d24s8ms0",
     "--deqp-visibility=hidden"
 ]
-timeout = 180.0
+timeout = 360.0 # Starting 8 Crosvm instances simultaneously can take some time
+
+[[deqp]]
+deqp = "/install/crosvm-runner.sh"
+caselists = [
+    "/deqp/mustpass/gl30-master.txt",
+    "/deqp/mustpass/gl31-master.txt",
+    "/deqp/mustpass/gl32-master.txt",
+]
+deqp_args = [
+    "/deqp/external/openglcts/modules/glcts",
+    "--deqp-surface-width=256",
+    "--deqp-surface-height=256",
+    "--deqp-surface-type=pbuffer",
+    "--deqp-gl-config-name=rgba8888d24s8ms0",
+    "--deqp-visibility=hidden"
+]
+timeout = 360.0 # Starting 8 Crosvm instances simultaneously can take some time

src/gallium/drivers/virgl/ci/gitlab-ci.yml
@@ -5,22 +5,17 @@
   variables:
     GALLIUM_DRIVER: "virgl"
     CROSVM_GALLIUM_DRIVER: "llvmpipe"
-    CROSVM_GPU_ARGS: "gles=false,backend=virglrenderer,egl=true,surfaceless=true"
+    CROSVM_GPU_ARGS: "gles=false,backend=virglrenderer,egl=true,surfaceless=true,width=1024,height=768"
     GALLIVM_PERF: "nopt,no_quad_lod"
 
 virgl-on-gl:
   variables:
     DEQP_SUITE: virgl-gl
     GPU_VERSION: virgl-gl
-    # Speed rendering a bit, as crosvm is processing requests serially
-    LP_NUM_THREADS: 8
-    FDO_CI_CONCURRENT: 3
-    CROSVM_TEST_SCRIPT: "/install/deqp-runner.sh"
-  parallel: 6
+    LP_NUM_THREADS: 1 # There will be FDO_CI_CONCURRENT Crosvm processes, so each should use a single thread
+  parallel: 3
   tags:
     - kvm
-  script:
-    - install/crosvm-runner.sh
   extends:
     - .deqp-test
     - .virgl-test
@@ -30,10 +25,9 @@ virgl-on-gles:
     VIRGL_HOST_API: GLES
     DEQP_SUITE: virgl-gles
     GPU_VERSION: virgl-gles
-    CROSVM_GPU_ARGS: "gles=true,backend=virglrenderer,egl=true,surfaceless=true"
+    CROSVM_GPU_ARGS: "gles=true,backend=virglrenderer,egl=true,surfaceless=true,width=1024,height=768"
   extends:
     - virgl-on-gl
-    - .test-manual
 
 virgl-traces:
   extends:
@@ -44,9 +38,10 @@ virgl-traces:
     PIGLIT_REPLAY_DESCRIPTION_FILE: "${CI_PROJECT_DIR}/install/traces-virgl.yml"
     PIGLIT_REPLAY_DEVICE_NAME: "gl-virgl"
     PIGLIT_RESULTS: "virgl-replay"
-    CROSVM_TEST_SCRIPT: "/install/piglit/run.sh"
+    LD_LIBRARY_PATH: "${CI_PROJECT_DIR}/install/lib/"     # For Crosvm
   tags:
     - kvm
   script:
-    - install/crosvm-runner.sh
-
+    # Use all threads for rendering and only run one job at a time
+    # Couldn't get GitLab CI to correctly substitute the variable in the yaml
+    - LP_NUM_THREADS=${FDO_CI_CONCURRENT} FDO_CI_CONCURRENT=1 install/crosvm-runner.sh install/piglit/run.sh

src/gallium/drivers/virgl/ci/traces-virgl.yml
@@ -231,7 +231,7 @@ traces:
   - path: supertuxkart/supertuxkart-mansion-egl-gles.trace
     expectations:
       - device: gl-virgl
-        checksum: 156c26de2cefe1973b1593e6b22f7edb
+        checksum: c8e5d7c4377b8e484ae41270692914f3
   - path: xonotic/xonotic-keybench-high.trace
     expectations:
       - device: gl-virgl

commit 651070f60f8c5f8743ffd9f4bc2d2cbfaf43d398

virgl/ci: Fix identification of dEQP binary paths
In some cases the file paths passed to crosvm for execution do not point
to dEQP binaries, but can be wrapper scripts, like deqp-runner.sh.

Detect such cases and skip changing the working directory.

Additionally, use the POSIX compliant command substitution syntax
instead of the obsolete variant based on backquotes.

Fixes: 97b8165c4be ("virgl/ci: Run each dEQP instance in its own VM")

Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
Reviewed-by: Rohan Garg <rohan.garg@intel.com>
Reviewed-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14413>

.gitlab-ci/crosvm-runner.sh
@@ -3,11 +3,11 @@
 set -ex
 
 # This script can be called concurrently, pass arguments and env in a per-instance tmp dir
-export DEQP_TEMP_DIR=`mktemp -d /tmp.XXXXXXXXXX`
+export DEQP_TEMP_DIR=$(mktemp -d /tmp.XXXXXXXXXX)
 
 # The dEQP binary needs to run from the directory it's in
-if [ -z "${1##*"deqp"*}" ]; then
-  PWD=`dirname $1`
+if [ -n "${1##*.sh}" ] && [ -z "${1##*"deqp"*}" ]; then
+  PWD=$(dirname $1)
 fi
 
 export -p > $DEQP_TEMP_DIR/crosvm-env.sh