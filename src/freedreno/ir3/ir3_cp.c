openharmony/third_party_mesa3d

commit d0d88f9ad79a724267d613a712e2d827fe481a4f

ir3: Prevent propagating shared regs out of loops
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6752>

src/freedreno/ir3/ir3_cp.c
@@ -310,6 +310,17 @@ reg_cp(struct ir3_cp_ctx *ctx, struct ir3_instruction *instr,
 {
 	struct ir3_instruction *src = ssa(reg);
 
+	/* Values that are uniform inside a loop can become divergent outside
+	 * it if the loop has a divergent trip count. This means that we can't
+	 * propagate a copy of a shared to non-shared register if it would
+	 * make the shared reg's live range extend outside of its loop. Users
+	 * outside the loop would see the value for the thread(s) that last
+	 * exited the loop, rather than for their own thread.
+	 */
+	if ((src->dsts[0]->flags & IR3_REG_SHARED) &&
+		src->block->loop_id != instr->block->loop_id)
+		return false;
+
 	if (is_eligible_mov(src, instr, true)) {
 		/* simple case, no immed/const/relativ, only mov's w/ ssa src: */
 		struct ir3_register *src_reg = src->srcs[0];

src/freedreno/ir3/ir3.h
@@ -563,6 +563,8 @@ struct ir3_block {
 	uint32_t dom_pre_index;
 	uint32_t dom_post_index;
 
+	uint32_t loop_id;
+
 #ifdef DEBUG
 	uint32_t serialno;
 #endif

src/freedreno/ir3/ir3_compiler_nir.c
@@ -2861,6 +2861,8 @@ emit_block(struct ir3_context *ctx, nir_block *nblock)
 
 	list_addtail(&ctx->block->node, &ctx->ir->block_list);
 
+	ctx->block->loop_id = ctx->loop_id;
+
 	/* re-emit addr register in each block if needed: */
 	for (int i = 0; i < ARRAY_SIZE(ctx->addr0_ht); i++) {
 		_mesa_hash_table_destroy(ctx->addr0_ht[i], NULL);
@@ -2915,8 +2917,11 @@ emit_if(struct ir3_context *ctx, nir_if *nif)
 static void
 emit_loop(struct ir3_context *ctx, nir_loop *nloop)
 {
+	unsigned old_loop_id = ctx->loop_id;
+	ctx->loop_id = ctx->so->loops + 1;
 	emit_cf_list(ctx, &nloop->body);
 	ctx->so->loops++;
+	ctx->loop_id = old_loop_id;
 }
 
 static void

src/freedreno/ir3/ir3_context.h
@@ -110,6 +110,8 @@ struct ir3_context {
 	 */
 	unsigned stack, max_stack;
 
+	unsigned loop_id;
+
 	/* a common pattern for indirect addressing is to request the
 	 * same address register multiple times.  To avoid generating
 	 * duplicate instruction sequences (which our backend does not

commit c9ed2dbcc3f3cef0eafaf1d6837a2ee1d84fdf2c

ir3/cp: ir3: Prevent propagating shared regs out of loops harder
We need to check the source of the copy, not the destination. That means
this we need to move this check inside the ifs, where we know that the
source is a copy.

Fixes: d0d88f9ad79 ("Prevent propagating shared regs out of loops")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14412>

src/freedreno/ir3/ir3_cp.c
@@ -303,6 +303,22 @@ try_swap_mad_two_srcs(struct ir3_instruction *instr, unsigned new_flags)
    return valid_swap;
 }
 
+/* Values that are uniform inside a loop can become divergent outside
+ * it if the loop has a divergent trip count. This means that we can't
+ * propagate a copy of a shared to non-shared register if it would
+ * make the shared reg's live range extend outside of its loop. Users
+ * outside the loop would see the value for the thread(s) that last
+ * exited the loop, rather than for their own thread.
+ */
+static bool
+is_valid_shared_copy(struct ir3_instruction *dst_instr,
+                     struct ir3_instruction *src_instr,
+                     struct ir3_register *src_reg)
+{
+   return !(src_reg->flags & IR3_REG_SHARED) ||
+      dst_instr->block->loop_id == src_instr->block->loop_id;
+}
+
 /**
  * Handle cp for a given src register.  This additionally handles
  * the cases of collapsing immedate/const (which replace the src
@@ -316,21 +332,13 @@ reg_cp(struct ir3_cp_ctx *ctx, struct ir3_instruction *instr,
 {
    struct ir3_instruction *src = ssa(reg);
 
-   /* Values that are uniform inside a loop can become divergent outside
-    * it if the loop has a divergent trip count. This means that we can't
-    * propagate a copy of a shared to non-shared register if it would
-    * make the shared reg's live range extend outside of its loop. Users
-    * outside the loop would see the value for the thread(s) that last
-    * exited the loop, rather than for their own thread.
-    */
-   if ((src->dsts[0]->flags & IR3_REG_SHARED) &&
-       src->block->loop_id != instr->block->loop_id)
-      return false;
-
    if (is_eligible_mov(src, instr, true)) {
       /* simple case, no immed/const/relativ, only mov's w/ ssa src: */
       struct ir3_register *src_reg = src->srcs[0];
       unsigned new_flags = reg->flags;
+
+      if (!is_valid_shared_copy(instr, src, src_reg))
+         return false;
 
       combine_flags(&new_flags, src);
 
@@ -356,6 +364,9 @@ reg_cp(struct ir3_cp_ctx *ctx, struct ir3_instruction *instr,
       /* immed/const/etc cases, which require some special handling: */
       struct ir3_register *src_reg = src->srcs[0];
       unsigned new_flags = reg->flags;
+
+      if (!is_valid_shared_copy(instr, src, src_reg))
+         return false;
 
       if (src_reg->flags & IR3_REG_ARRAY)
          return false;