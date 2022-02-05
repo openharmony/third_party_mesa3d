openharmony/third_party_mesa3d

commit a29ab67c47a85e3db60fa684deed69a62262df4a

svga: Add support for compute shader, shader buffers and image views
This commit is squash of commits which handles resource creation and management
for compute shader, shader buffers and image views. It creates uavs for shader
buffers and image views.

Reviewed-by: Charmaine Lee <charmainel@vmware.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14270>

src/gallium/drivers/svga/svga_context.h
@@ -43,6 +43,8 @@
 #include "svga_winsys.h"
 #include "svga_hw_reg.h"
 #include "svga3d_shaderdefs.h"
+#include "svga_image_view.h"
+#include "svga_shader_buffer.h"
 #include "svga_debug.h"
 
 /** Non-GPU queries for gallium HUD */
@@ -96,6 +98,19 @@ enum svga_hud {
 #define SVGA_MAX_CONST_BUF_SIZE (4096 * 4 * sizeof(int))
 
 #define CONST0_UPLOAD_ALIGNMENT 256
+
+#define SVGA_MAX_IMAGES         SVGA3D_MAX_UAVIEWS
+#define SVGA_MAX_SHADER_BUFFERS	SVGA3D_MAX_UAVIEWS
+#define SVGA_MAX_ATOMIC_BUFFERS	SVGA3D_MAX_UAVIEWS
+#define SVGA_MAX_UAVIEWS        SVGA3D_DX11_1_MAX_UAVIEWS
+
+enum svga_surface_state
+{
+   SVGA_SURFACE_STATE_CREATED,
+   SVGA_SURFACE_STATE_INVALIDATED,
+   SVGA_SURFACE_STATE_UPDATED,
+   SVGA_SURFACE_STATE_RENDERED,
+};
 
 struct draw_vertex_shader;
 struct draw_fragment_shader;
@@ -322,12 +337,27 @@ struct svga_state
    unsigned sample_mask;
    unsigned vertices_per_patch;
    float default_tesslevels[6]; /* tessellation (outer[4] + inner[2]) levels */
+
+   /* Image views */
+   unsigned num_image_views[PIPE_SHADER_TYPES];
+   struct svga_image_view image_views[PIPE_SHADER_TYPES][SVGA_MAX_IMAGES];
+
+   /* Shader buffers */
+   unsigned num_shader_buffers[PIPE_SHADER_TYPES];
+   struct svga_shader_buffer shader_buffers[PIPE_SHADER_TYPES][SVGA_MAX_SHADER_BUFFERS];
+
+   /* HW atomic buffers */
+   unsigned num_atomic_buffers;
+   struct svga_shader_buffer atomic_buffers[SVGA_MAX_SHADER_BUFFERS];
+
    struct {
       /* Determine the layout of the grid (in block units) to be used. */
       unsigned size[3];
       /* If DispatchIndirect is used, this will has grid size info*/
       struct pipe_resource *indirect;
    } grid_info;
+
+   unsigned shared_mem_size;
 };
 
 struct svga_prescale {
@@ -441,6 +471,35 @@ struct svga_hw_draw_state
 
    boolean rasterizer_discard; /* set if rasterization is disabled */
    boolean has_backed_views;   /* set if any of the rtv/dsv is a backed surface view */
+
+   /* Image Views */
+   int uavSpliceIndex;
+   unsigned num_image_views[PIPE_SHADER_TYPES];
+   struct svga_image_view image_views[PIPE_SHADER_TYPES][SVGA_MAX_IMAGES];
+
+   /* Shader Buffers */
+   unsigned num_shader_buffers[PIPE_SHADER_TYPES];
+   struct svga_shader_buffer shader_buffers[PIPE_SHADER_TYPES][SVGA_MAX_SHADER_BUFFERS];
+
+   /* HW Atomic Buffers */
+   unsigned num_atomic_buffers;
+   struct svga_shader_buffer atomic_buffers[SVGA_MAX_SHADER_BUFFERS];
+
+   /* UAV state */
+   unsigned num_uavs;
+   SVGA3dUAViewId uaViewIds[SVGA_MAX_UAVIEWS];
+   struct svga_winsys_surface *uaViews[SVGA_MAX_UAVIEWS];
+
+   /* Compute UAV state */
+   unsigned num_cs_uavs;
+   SVGA3dUAViewId csUAViewIds[SVGA_MAX_UAVIEWS];
+   struct svga_winsys_surface *csUAViews[SVGA_MAX_UAVIEWS];
+
+   /* starting uav index for each shader */
+   unsigned uav_start_index[PIPE_SHADER_TYPES];
+
+   /* starting uav index for HW atomic buffers */
+   unsigned uav_atomic_buf_index;
 };
 
 
@@ -468,6 +527,30 @@ struct svga_hw_queue;
 struct svga_query;
 struct svga_qmem_alloc_entry;
 
+enum svga_uav_type
+{
+   SVGA_IMAGE_VIEW = 0,
+   SVGA_SHADER_BUFFER
+};
+
+struct svga_uav
+{
+   enum svga_uav_type type;
+   union {
+      struct svga_image_view image_view;
+      struct svga_shader_buffer shader_buffer;
+   } desc;
+   struct pipe_resource *resource;
+   unsigned next_uaView;
+   SVGA3dUAViewId uaViewId;
+   unsigned timestamp[2];
+};
+struct svga_cache_uav
+{
+   unsigned num_uaViews;
+   unsigned next_uaView;
+   struct svga_uav uaViews[SVGA3D_DX11_1_MAX_UAVIEWS];
+};
 struct svga_context
 {
    struct pipe_context pipe;
@@ -529,6 +612,12 @@ struct svga_context
    /* Bitmask of used query IDs */
    struct util_bitmask *query_id_bm;
 
+   /* Bitmask of used uav IDs */
+   struct util_bitmask *uav_id_bm;
+
+   /* Bitmask of to-free uav IDs */
+   struct util_bitmask *uav_to_free_id_bm;
+
    struct {
       uint64_t dirty[SVGA_STATE_MAX];
 
@@ -536,6 +625,7 @@ struct svga_context
       unsigned dirty_constbufs[PIPE_SHADER_TYPES];
 
       unsigned texture_timestamp;
+      unsigned uav_timestamp[2];
 
       struct svga_sw_state          sw;
       struct svga_hw_draw_state     hw_draw;
@@ -557,6 +647,7 @@ struct svga_context
          unsigned tes:1;
          unsigned cs:1;
          unsigned query:1;
+         unsigned uav:1;
       } flags;
       unsigned val;
    } rebind;
@@ -665,6 +756,8 @@ struct svga_context
       boolean passthrough;
    } tcs;
 
+   struct svga_cache_uav cache_uav;
+   struct pipe_resource *dummy_resource;
 };
 
 /* A flag for each frontend state object:
@@ -707,18 +800,37 @@ struct svga_context
 #define SVGA_NEW_TCS_CONST_BUFFER    ((uint64_t) 0x1000000000)
 #define SVGA_NEW_TES_CONST_BUFFER    ((uint64_t) 0x2000000000)
 #define SVGA_NEW_TCS_PARAM           ((uint64_t) 0x4000000000)
-#define SVGA_NEW_FS_CONSTS           ((uint64_t) 0x8000000000)
-#define SVGA_NEW_VS_CONSTS           ((uint64_t) 0x10000000000)
-#define SVGA_NEW_GS_CONSTS           ((uint64_t) 0x20000000000)
-#define SVGA_NEW_TCS_CONSTS          ((uint64_t) 0x40000000000)
-#define SVGA_NEW_TES_CONSTS          ((uint64_t) 0x800000000000)
+#define SVGA_NEW_IMAGE_VIEW          ((uint64_t) 0x8000000000)
+#define SVGA_NEW_SHADER_BUFFER       ((uint64_t) 0x10000000000)
+#define SVGA_NEW_CS                  ((uint64_t) 0x20000000000)
+#define SVGA_NEW_CS_VARIANT          ((uint64_t) 0x40000000000)
+#define SVGA_NEW_CS_CONST_BUFFER     ((uint64_t) 0x80000000000)
+#define SVGA_NEW_FS_CONSTS           ((uint64_t) 0x100000000000)
+#define SVGA_NEW_VS_CONSTS           ((uint64_t) 0x200000000000)
+#define SVGA_NEW_GS_CONSTS           ((uint64_t) 0x400000000000)
+#define SVGA_NEW_TCS_CONSTS          ((uint64_t) 0x800000000000)
+#define SVGA_NEW_TES_CONSTS          ((uint64_t) 0x1000000000000)
+#define SVGA_NEW_CS_CONSTS           ((uint64_t) 0x2000000000000)
+#define SVGA_NEW_FS_RAW_BUFFER       ((uint64_t) 0x4000000000000)
+#define SVGA_NEW_VS_RAW_BUFFER       ((uint64_t) 0x8000000000000)
+#define SVGA_NEW_GS_RAW_BUFFER       ((uint64_t) 0x10000000000000)
+#define SVGA_NEW_TCS_RAW_BUFFER      ((uint64_t) 0x20000000000000)
+#define SVGA_NEW_TES_RAW_BUFFER      ((uint64_t) 0x40000000000000)
+#define SVGA_NEW_CS_RAW_BUFFER       ((uint64_t) 0x80000000000000)
 #define SVGA_NEW_ALL                 ((uint64_t) 0xFFFFFFFFFFFFFFFF)
 
 #define SVGA_NEW_CONST_BUFFER \
    (SVGA_NEW_FS_CONST_BUFFER | SVGA_NEW_VS_CONST_BUFFER | \
-    SVGA_NEW_GS_CONST_BUFFER | \
+    SVGA_NEW_GS_CONST_BUFFER | SVGA_NEW_CS_CONST_BUFFER | \
     SVGA_NEW_TCS_CONST_BUFFER | SVGA_NEW_TES_CONST_BUFFER)
 
+
+/** Program pipelines */
+enum svga_pipe_type
+{
+   SVGA_PIPE_GRAPHICS = 0,
+   SVGA_PIPE_COMPUTE  = 1
+};
 
 void svga_init_state_functions( struct svga_context *svga );
 void svga_init_flush_functions( struct svga_context *svga );
@@ -768,6 +880,28 @@ svga_context_create(struct pipe_screen *screen,
 void svga_toggle_render_condition(struct svga_context *svga,
                                   boolean render_condition_enabled,
                                   boolean on);
+enum pipe_error
+svga_validate_sampler_resources(struct svga_context *svga,
+                                enum svga_pipe_type);
+
+enum pipe_error
+svga_validate_constant_buffers(struct svga_context *svga,
+                               enum svga_pipe_type);
+
+enum pipe_error
+svga_validate_image_views(struct svga_context *svga,
+                          enum svga_pipe_type);
+
+enum pipe_error
+svga_validate_shader_buffers(struct svga_context *svga,
+                             enum svga_pipe_type);
+
+void
+svga_destroy_rawbuf_srv(struct svga_context *svga);
+
+void
+svga_uav_cache_init(struct svga_context *svga);
+
 
 /***********************************************************************
  * Inline conversion functions.  These are better-typed than the

src/gallium/drivers/svga/svga_pipe_cs.c
@@ -1,0 +1,231 @@
+/**********************************************************
+ * Copyright 2022 VMware, Inc.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ **********************************************************/
+
+#include "util/u_inlines.h"
+#include "util/u_memory.h"
+#include "util/u_bitmask.h"
+#include "tgsi/tgsi_parse.h"
+#include "tgsi/tgsi_text.h"
+
+#include "svga_context.h"
+#include "svga_cmd.h"
+#include "svga_debug.h"
+#include "svga_shader.h"
+#include "svga_streamout.h"
+#include "svga_resource_buffer.h"
+
+
+/**
+ * Create the compute program.
+ */
+static void *
+svga_create_compute_state(struct pipe_context *pipe,
+                          const struct pipe_compute_state *templ)
+{
+   struct svga_context *svga = svga_context(pipe);
+
+   struct svga_compute_shader *cs = CALLOC_STRUCT(svga_compute_shader);
+
+   if (!cs)
+      return NULL;
+
+   SVGA_STATS_TIME_PUSH(svga_sws(svga), SVGA_STATS_TIME_CREATECS);
+
+   assert(templ->ir_type == PIPE_SHADER_IR_TGSI);
+   cs->base.tokens = tgsi_dup_tokens(templ->prog);
+
+   /* Collect shader basic info */
+   tgsi_scan_shader(cs->base.tokens, &cs->base.info);
+
+   cs->base.id = svga->debug.shader_id++;
+
+   svga->curr.shared_mem_size = templ->req_local_mem;
+
+   SVGA_STATS_TIME_POP(svga_sws(svga));
+   return cs;
+}
+
+
+/**
+ * Bind the compute program.
+ */
+static void
+svga_bind_compute_state(struct pipe_context *pipe, void *shader)
+{
+   struct svga_context *svga = svga_context(pipe);
+   struct svga_compute_shader *cs = (struct svga_compute_shader *)shader;
+
+   svga->curr.cs = cs;
+   svga->dirty |= SVGA_NEW_CS;
+}
+
+
+/**
+ * Delete the compute program.
+ */
+static void
+svga_delete_compute_state(struct pipe_context *pipe, void *shader)
+{
+   struct svga_context *svga = svga_context(pipe);
+   struct svga_compute_shader *cs = (struct svga_compute_shader *)shader;
+   struct svga_compute_shader *next_cs;
+   struct svga_shader_variant *variant, *tmp;
+
+   svga_hwtnl_flush_retry(svga);
+
+   /* Free the list of compute shaders */
+   while (cs) {
+      next_cs = (struct svga_compute_shader *)cs->base.next;
+
+      for (variant = cs->base.variants; variant; variant = tmp) {
+         tmp = variant->next;
+
+         /* Check if deleting currently bound shader */
+         if (variant == svga->state.hw_draw.cs) {
+            SVGA_RETRY(svga, svga_set_shader(svga, SVGA3D_SHADERTYPE_CS, NULL));
+            svga->state.hw_draw.cs = NULL;
+         }
+
+         svga_destroy_shader_variant(svga, variant);
+      }
+
+      FREE((void *)cs->base.tokens);
+      FREE(cs);
+      cs = next_cs;
+   }
+}
+
+
+/**
+ * Bind an array of shader resources that will be used by the
+ * compute program.  Any resources that were previously bound to
+ * the specified range will be unbound after this call.
+ */
+static void
+svga_set_compute_resources(struct pipe_context *pipe,
+                           unsigned start, unsigned count,
+                           struct pipe_surface **resources)
+{
+   //TODO
+   return;
+}
+
+
+/**
+ * Bind an array of buffers to be mapped into the address space of
+ * the GLOBAL resource.  Any buffers that were previously bound
+ * between [first, first + count - 1] are unbound after this call.
+ */
+static void
+svga_set_global_binding(struct pipe_context *pipe,
+                        unsigned first, unsigned count,
+                        struct pipe_resource **resources,
+                        uint32_t **handles)
+{
+   //TODO
+   return;
+}
+
+
+/**
+ */
+static void
+svga_validate_compute_resources(struct svga_context *svga)
+{
+   /* validate sampler view resources */
+   SVGA_RETRY(svga,
+              svga_validate_sampler_resources(svga, SVGA_PIPE_COMPUTE));
+
+   /* validate constant buffer resources */
+   SVGA_RETRY(svga,
+              svga_validate_constant_buffers(svga, SVGA_PIPE_COMPUTE));
+
+   /* validate image view resources */
+   SVGA_RETRY(svga,
+              svga_validate_image_views(svga, SVGA_PIPE_COMPUTE));
+
+   /* validate shader buffer resources */
+   SVGA_RETRY(svga,
+              svga_validate_shader_buffers(svga, SVGA_PIPE_COMPUTE));
+}
+
+
+/**
+ * Launch the compute kernel starting from instruction pc of the
+ * currently bound compute program.
+ */
+static void
+svga_launch_grid(struct pipe_context *pipe,
+                 const struct pipe_grid_info *info)
+{
+   struct svga_context *svga = svga_context(pipe);
+   struct svga_winsys_context *swc = svga->swc;
+
+   assert(svga_have_gl43(svga));
+
+   SVGA_STATS_TIME_PUSH(svga_sws(svga), SVGA_STATS_TIME_LAUNCHGRID);
+
+   if (info->indirect) {
+      svga->curr.grid_info.indirect= info->indirect;
+   }
+
+   svga_update_compute_state(svga);
+
+   /* validate compute resources */
+   svga_validate_compute_resources(svga);
+
+   if (info->indirect) {
+      struct svga_winsys_surface *indirect_surf;
+      indirect_surf = svga_buffer_handle(svga, info->indirect,
+                                         PIPE_BIND_COMMAND_ARGS_BUFFER);
+      SVGA_RETRY(svga, SVGA3D_sm5_DispatchIndirect(swc, indirect_surf,
+                                                   info->indirect_offset));
+   }
+   else {
+      svga->curr.grid_info.size[0] = info->grid[0];
+      svga->curr.grid_info.size[1] = info->grid[1];
+      svga->curr.grid_info.size[2] = info->grid[2];
+
+      SVGA_RETRY(svga, SVGA3D_sm5_Dispatch(swc, info->grid));
+   }
+
+   SVGA_STATS_TIME_POP(svga_sws(svga));
+   return;
+}
+
+
+/**
+ * Initialize the compute interface function pointers.
+ */
+void
+svga_init_cs_functions(struct svga_context *svga)
+{
+   svga->pipe.create_compute_state = svga_create_compute_state;
+   svga->pipe.bind_compute_state = svga_bind_compute_state;
+   svga->pipe.delete_compute_state = svga_delete_compute_state;
+   svga->pipe.set_compute_resources = svga_set_compute_resources;
+   svga->pipe.set_global_binding = svga_set_global_binding;
+   svga->pipe.launch_grid = svga_launch_grid;
+}

src/gallium/drivers/svga/svga_shader.h
@@ -115,6 +115,7 @@ struct svga_compile_key
    /* compute shader */
    struct {
       unsigned grid_size[3];
+      unsigned mem_size;
    } cs;
 
    /* any shader type */

src/gallium/drivers/svga/svga_state_cs.c
@@ -1,0 +1,166 @@
+/**********************************************************
+ * Copyright 2022 VMware, Inc.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ **********************************************************/
+
+#include "util/u_inlines.h"
+#include "util/u_memory.h"
+#include "util/u_bitmask.h"
+#include "translate/translate.h"
+#include "tgsi/tgsi_ureg.h"
+
+#include "svga_context.h"
+#include "svga_cmd.h"
+#include "svga_shader.h"
+#include "svga_tgsi.h"
+
+
+/**
+ * Translate TGSI shader into an svga shader variant.
+ */
+static enum pipe_error
+compile_cs(struct svga_context *svga,
+           struct svga_compute_shader *cs,
+           const struct svga_compile_key *key,
+           struct svga_shader_variant **out_variant)
+{
+   struct svga_shader_variant *variant;
+   enum pipe_error ret = PIPE_ERROR;
+
+   variant = svga_tgsi_vgpu10_translate(svga, &cs->base, key,
+                                        PIPE_SHADER_COMPUTE);
+   if (!variant)
+      return PIPE_ERROR;
+
+   ret = svga_define_shader(svga, variant);
+   if (ret != PIPE_OK) {
+      svga_destroy_shader_variant(svga, variant);
+      return ret;
+   }
+
+   *out_variant = variant;
+
+   return PIPE_OK;
+}
+
+
+/**
+ * Create compute shader compile key.
+ */
+static void
+make_cs_key(struct svga_context *svga,
+            struct svga_compile_key *key)
+{
+   struct svga_compute_shader *cs = svga->curr.cs;
+
+   memset(key, 0, sizeof *key);
+
+   svga_init_shader_key_common(svga, PIPE_SHADER_COMPUTE, &cs->base, key);
+
+   key->cs.grid_size[0] = svga->curr.grid_info.size[0];
+   key->cs.grid_size[1] = svga->curr.grid_info.size[1];
+   key->cs.grid_size[2] = svga->curr.grid_info.size[2];
+   key->cs.mem_size = svga->curr.shared_mem_size;
+
+   if (svga->curr.grid_info.indirect && cs->base.info.uses_grid_size) {
+      struct pipe_transfer *transfer = NULL;
+      const void *map = NULL;
+      map = pipe_buffer_map(&svga->pipe, svga->curr.grid_info.indirect,
+                            PIPE_MAP_READ, &transfer);
+      memcpy(key->cs.grid_size, map, 3 * sizeof(uint));
+      pipe_buffer_unmap(&svga->pipe, transfer);
+   }
+
+}
+
+
+/**
+ * Emit current compute shader to device.
+ */
+static enum pipe_error
+emit_hw_cs(struct svga_context *svga, uint64_t dirty)
+{
+   struct svga_shader_variant *variant;
+   struct svga_compute_shader *cs = svga->curr.cs;
+   enum pipe_error ret = PIPE_OK;
+   struct svga_compile_key key;
+
+   assert(svga_have_sm5(svga));
+
+   SVGA_STATS_TIME_PUSH(svga_sws(svga), SVGA_STATS_TIME_EMITCS);
+
+   if (!cs) {
+      if (svga->state.hw_draw.cs != NULL) {
+
+         /** The previous compute shader is made inactive.
+          *  Needs to unbind the compute shader.
+          */
+         ret = svga_set_shader(svga, SVGA3D_SHADERTYPE_CS, NULL);
+         if (ret != PIPE_OK)
+            goto done;
+         svga->state.hw_draw.cs = NULL;
+      }
+      goto done;
+   }
+
+   make_cs_key(svga, &key);
+
+   /* See if we already have a CS variant that matches the key */
+   variant = svga_search_shader_key(&cs->base, &key);
+
+   if (!variant) {
+      ret = compile_cs(svga, cs, &key, &variant);
+      if (ret != PIPE_OK)
+         goto done;
+
+      /* insert the new variant at head of linked list */
+      assert(variant);
+      variant->next = cs->base.variants;
+      cs->base.variants = variant;
+   }
+
+   if (variant != svga->state.hw_draw.cs) {
+      /* Bind the new variant */
+      ret = svga_set_shader(svga, SVGA3D_SHADERTYPE_CS, variant);
+      if (ret != PIPE_OK)
+         goto done;
+
+      svga->rebind.flags.cs = FALSE;
+      svga->dirty |= SVGA_NEW_CS_VARIANT;
+      svga->state.hw_draw.cs = variant;
+   }
+
+done:
+   SVGA_STATS_TIME_POP(svga_sws(svga));
+   return ret;
+}
+
+struct svga_tracked_state svga_hw_cs =
+{
+   "compute shader",
+   (SVGA_NEW_CS |
+    SVGA_NEW_TEXTURE_BINDING |
+    SVGA_NEW_SAMPLER |
+    SVGA_NEW_CS_RAW_BUFFER),
+   emit_hw_cs
+};

src/gallium/drivers/svga/meson.build
@@ -26,11 +26,13 @@ files_svga = files(
   'svga_draw.c',
   'svga_draw_elements.c',
   'svga_format.c',
+  'svga_image_view.c',
   'svga_link.c',
   'svga_pipe_blend.c',
   'svga_pipe_blit.c',
   'svga_pipe_clear.c',
   'svga_pipe_constants.c',
+  'svga_pipe_cs.c',
   'svga_pipe_depthstencil.c',
   'svga_pipe_draw.c',
   'svga_pipe_flush.c',
@@ -52,8 +54,10 @@ files_svga = files(
   'svga_screen.c',
   'svga_screen_cache.c',
   'svga_shader.c',
+  'svga_shader_buffer.c',
   'svga_state.c',
   'svga_state_constants.c',
+  'svga_state_cs.c',
   'svga_state_framebuffer.c',
   'svga_state_fs.c',
   'svga_state_gs.c',
@@ -63,6 +67,7 @@ files_svga = files(
   'svga_state_sampler.c',
   'svga_state_tgsi_transform.c',
   'svga_state_tss.c',
+  'svga_state_uav.c',
   'svga_state_vdecl.c',
   'svga_state_vs.c',
   'svga_surface.c',

src/gallium/drivers/svga/svga_debug.h
@@ -46,6 +46,8 @@
 #define DEBUG_CACHE        0x8000
 #define DEBUG_STREAMOUT    0x10000
 #define DEBUG_SAMPLERS     0x20000
+#define DEBUG_IMAGE        0x40000
+#define DEBUG_UAV          0x80000
 #define DEBUG_RETRY        0x100000
 
 #ifdef DEBUG

commit 6763e47d283857a150c3fc061bd276cd0c9be1da

svga: store shared_mem_size in svga_compute_shader instead of svga_context
When new context was created, shared_mem_size was getting overwritten.
This fixes glretrace failure seen with manhattan, aztec and BASS2_intro
apitraces

Fixes: a29ab67c47a85e ('svga: Add support for compute shader, shader buffers and image views')

Tested with glretrace, piglit

Reviewed-by: Charmaine Lee <charmainel@vmware.com>
(cherry picked from commit dd6793ec9218782b1b716a87582d7219bae4e75f)

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14870>

src/gallium/drivers/svga/svga_context.h
@@ -374,7 +374,6 @@ struct svga_state
       struct pipe_resource *indirect;
    } grid_info;
 
-   unsigned shared_mem_size;
 };
 
 struct svga_prescale {

src/gallium/drivers/svga/svga_pipe_cs.c
@@ -61,7 +61,7 @@ svga_create_compute_state(struct pipe_context *pipe,
 
    cs->base.id = svga->debug.shader_id++;
 
-   svga->curr.shared_mem_size = templ->req_local_mem;
+   cs->shared_mem_size = templ->req_local_mem;
 
    SVGA_STATS_TIME_POP(svga_sws(svga));
    return cs;

src/gallium/drivers/svga/svga_shader.h
@@ -380,6 +380,7 @@ struct svga_tes_shader
 struct svga_compute_shader
 {
    struct svga_shader base;
+   unsigned shared_mem_size;
 };
 
 

src/gallium/drivers/svga/svga_state_cs.c
@@ -80,7 +80,7 @@ make_cs_key(struct svga_context *svga,
    key->cs.grid_size[0] = svga->curr.grid_info.size[0];
    key->cs.grid_size[1] = svga->curr.grid_info.size[1];
    key->cs.grid_size[2] = svga->curr.grid_info.size[2];
-   key->cs.mem_size = svga->curr.shared_mem_size;
+   key->cs.mem_size = cs->shared_mem_size;
 
    if (svga->curr.grid_info.indirect && cs->base.info.uses_grid_size) {
       struct pipe_transfer *transfer = NULL;