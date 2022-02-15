openharmony/third_party_mesa3d

commit 528774c3d8d471a0474c5bf955fddb4751477948

i915g: Switch to using nir-to-tgsi.
This fixes ~10% of the GLES2 failures thanks to having a better compiler,
though in some cases we get some new compile fails due to instr count or
uniform count.  We still have to do NIR-to-TGSI because the NIR gallivm
draw path isn't ready for non-native-integer NIR code, and st/mesa treats
native-integer as a screen property instead of a stage property.

Other than the noted regressions in the xfails, for
dEQP-GLES2.functional.uniform_api.random.74: compile fail changes reasons
triggering an aassertion instead of drawing magenta.

Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11329>

src/gallium/drivers/i915/i915_state.c
@@ -30,6 +30,8 @@
 
 
 #include "draw/draw_context.h"
+#include "nir.h"
+#include "nir/nir_to_tgsi.h"
 #include "util/u_helpers.h"
 #include "util/u_inlines.h"
 #include "util/u_math.h"
@@ -476,9 +478,18 @@ i915_create_fs_state(struct pipe_context *pipe,
       return NULL;
 
    ifs->draw_data = draw_create_fragment_shader(i915->draw, templ);
-   ifs->state.tokens = tgsi_dup_tokens(templ->tokens);
 
-   tgsi_scan_shader(templ->tokens, &ifs->info);
+   if (templ->type == PIPE_SHADER_IR_NIR) {
+      ifs->state.tokens = nir_to_tgsi(templ->ir.nir, pipe->screen);
+   } else {
+      assert(templ->type == PIPE_SHADER_IR_TGSI);
+      /* we need to keep a local copy of the tokens */
+      ifs->state.tokens = tgsi_dup_tokens(templ->tokens);
+   }
+
+   ifs->state.type = PIPE_SHADER_IR_TGSI;
+
+   tgsi_scan_shader(ifs->state.tokens, &ifs->info);
 
    /* The shader's compiled to i915 instructions here */
    i915_translate_fragment_program(i915, ifs);
@@ -527,7 +538,18 @@ i915_create_vs_state(struct pipe_context *pipe,
 {
    struct i915_context *i915 = i915_context(pipe);
 
-   /* just pass-through to draw module */
+   struct pipe_shader_state from_nir;
+   if (templ->type == PIPE_SHADER_IR_NIR) {
+      /* The gallivm draw path doesn't support non-native-integers NIR shaders,
+       * st/mesa does native-integers for the screen as a whole rather than
+       * per-stage, and i915 FS can't do native integers.  So, convert to TGSI,
+       * where the draw path *does* support non-native-integers.
+       */
+      from_nir.type = PIPE_SHADER_IR_TGSI;
+      from_nir.tokens = nir_to_tgsi(templ->ir.nir, pipe->screen);
+      templ = &from_nir;
+   }
+
    return draw_create_vertex_shader(i915->draw, templ);
 }
 

src/gallium/drivers/i915/ci/traces-i915.yml
@@ -12,7 +12,7 @@ traces:
   - path: gputest/plot3d.trace
     expectations:
       - device: i915-g33
-        checksum: c85a570e1d2a45555b9e92298522f7d8
+        checksum: dc187485584bc45b9050e88163e72c34
   - path: gputest/triangle.trace
     expectations:
       # Weird white bar behind Tux's head.
@@ -23,11 +23,14 @@ traces:
   #  expectations:
   #    - device: i915-g33
   #      checksum: 8d4c52f0af9c09710d358f24c73fae3c
-  - path: humus/CelShading.trace
-    # The cel shading is pretty chunky, but maybe that's just precision stuff.
-    expectations:
-      - device: i915-g33
-        checksum: 4605ceed2b5c3a03f9262c546d75a8d8
+  #- path: humus/CelShading.trace
+  #  # The cel shading is pretty chunky, but maybe that's just precision stuff.
+  #  # Since the NIR change, 1D textures are getting undefined .y channels that
+  #  # could trigger the known bad 1D texture wrap behavior, and the rendering looks
+  #  # like undefined data.
+  #  expectations:
+  #    - device: i915-g33
+  #      checksum: 4605ceed2b5c3a03f9262c546d75a8d8
   # Requires GLSL 1.30
   #- path: humus/DynamicBranching3.trace
   #  expectations:
@@ -41,7 +44,7 @@ traces:
   - path: humus/Portals.trace
     expectations:
       - device: i915-g33
-        checksum: 9306a8d9f37d83a327759319c46f7cae
+        checksum: 46a5f3c04ef40f275e1d330c2ac965c8
   # The shadow raytracing shader fails to compile
   #- path: humus/RaytracedShadows.trace
   #  expectations:
@@ -55,9 +58,9 @@ traces:
   - path: neverball/neverball.trace
     expectations:
       - device: i915-g33
-        checksum: 344479e929ad4bc7f7316b5e574b0131
+        checksum: e621fd97320f24ea4451565390f9b1ac
   - path: valve/counterstrike-v2.trace
     # Tree foliage rendering is extra aliased.
     expectations:
       - device: i915-g33
-        checksum: 66a18e91b95674a3a1d227ed5681257d
+        checksum: 4a693fcd3b2fbe388ba847ca3a93f079

src/gallium/drivers/i915/i915_screen.c
@@ -26,6 +26,7 @@
  **************************************************************************/
 
 
+#include "compiler/nir/nir.h"
 #include "draw/draw_context.h"
 #include "util/os_misc.h"
 #include "util/format/u_format.h"
@@ -107,11 +108,107 @@ i915_get_name(struct pipe_screen *screen)
    return buffer;
 }
 
+static const nir_shader_compiler_options i915_compiler_options = {
+   .fuse_ffma32 = true,
+   .lower_bitops = true, /* required for !CAP_INTEGERS nir_to_tgsi */
+   .lower_extract_byte = true,
+   .lower_extract_word = true,
+   .lower_fdiv = true,
+   .lower_fdph = true,
+   .lower_flrp32 = true,
+   .lower_fmod = true,
+   .lower_rotate = true,
+   .lower_uniforms_to_ubo = true,
+   .lower_vector_cmp = true,
+   .use_interpolated_input_intrinsics = true,
+};
+
+static const struct nir_shader_compiler_options gallivm_nir_options = {
+   .lower_bitops = true, /* required for !CAP_INTEGERS nir_to_tgsi */
+   .lower_scmp = true,
+   .lower_flrp32 = true,
+   .lower_flrp64 = true,
+   .lower_fsat = true,
+   .lower_bitfield_insert_to_shifts = true,
+   .lower_bitfield_extract_to_shifts = true,
+   .lower_fdph = true,
+   .lower_ffma16 = true,
+   .lower_ffma32 = true,
+   .lower_ffma64 = true,
+   .lower_fmod = true,
+   .lower_hadd = true,
+   .lower_add_sat = true,
+   .lower_ldexp = true,
+   .lower_pack_snorm_2x16 = true,
+   .lower_pack_snorm_4x8 = true,
+   .lower_pack_unorm_2x16 = true,
+   .lower_pack_unorm_4x8 = true,
+   .lower_pack_half_2x16 = true,
+   .lower_pack_split = true,
+   .lower_unpack_snorm_2x16 = true,
+   .lower_unpack_snorm_4x8 = true,
+   .lower_unpack_unorm_2x16 = true,
+   .lower_unpack_unorm_4x8 = true,
+   .lower_unpack_half_2x16 = true,
+   .lower_extract_byte = true,
+   .lower_extract_word = true,
+   .lower_rotate = true,
+   .lower_uadd_carry = true,
+   .lower_usub_borrow = true,
+   .lower_mul_2x32_64 = true,
+   .lower_ifind_msb = true,
+   .max_unroll_iterations = 32,
+   .use_interpolated_input_intrinsics = true,
+   .lower_cs_local_index_from_id = true,
+   .lower_uniforms_to_ubo = true,
+   .lower_vector_cmp = true,
+   .lower_device_index_to_zero = true,
+   /* .support_16bit_alu = true, */
+};
+
+static const void *
+i915_get_compiler_options(struct pipe_screen *pscreen,
+                              enum pipe_shader_ir ir,
+                              enum pipe_shader_type shader)
+{
+   assert(ir == PIPE_SHADER_IR_NIR);
+   if (shader == PIPE_SHADER_FRAGMENT)
+      return &i915_compiler_options;
+   else
+      return &gallivm_nir_options;
+}
+
 static int
 i915_get_shader_param(struct pipe_screen *screen,
                       enum pipe_shader_type shader,
                       enum pipe_shader_cap cap)
 {
+   switch (cap) {
+      case PIPE_SHADER_CAP_PREFERRED_IR:
+         return PIPE_SHADER_IR_NIR;
+      case PIPE_SHADER_CAP_SUPPORTED_IRS:
+         return (1 << PIPE_SHADER_IR_NIR) | (1 << PIPE_SHADER_IR_TGSI);
+
+      case PIPE_SHADER_CAP_INTEGERS:
+         /* mesa/st requires that this cap is the same across stages, and the FS
+          * can't do ints.
+          */
+         return 0;
+
+      case PIPE_SHADER_CAP_INT16:
+         return 0;
+
+      case PIPE_SHADER_CAP_INDIRECT_TEMP_ADDR:
+         /* While draw could normally handle this for the VS, the NIR lowering
+          * to regs can't handle our non-native-integers, so we have to lower to
+          * if ladders.
+          */
+         return 0;
+
+      default:
+         break;
+   }
+
    switch(shader) {
    case PIPE_SHADER_VERTEX:
       switch (cap) {
@@ -156,7 +253,6 @@ i915_get_shader_param(struct pipe_screen *screen,
       case PIPE_SHADER_CAP_INDIRECT_CONST_ADDR:
       case PIPE_SHADER_CAP_SUBROUTINES:
          return 0;
-      case PIPE_SHADER_CAP_INTEGERS:
       case PIPE_SHADER_CAP_INT64_ATOMICS:
       case PIPE_SHADER_CAP_FP16:
       case PIPE_SHADER_CAP_FP16_DERIVATIVES:
@@ -175,11 +271,11 @@ i915_get_shader_param(struct pipe_screen *screen,
       case PIPE_SHADER_CAP_MAX_SHADER_BUFFERS:
       case PIPE_SHADER_CAP_MAX_SHADER_IMAGES:
       case PIPE_SHADER_CAP_LOWER_IF_THRESHOLD:
-      case PIPE_SHADER_CAP_PREFERRED_IR:
       case PIPE_SHADER_CAP_TGSI_SKIP_MERGE_REGISTERS:
       case PIPE_SHADER_CAP_MAX_HW_ATOMIC_COUNTERS:
       case PIPE_SHADER_CAP_MAX_HW_ATOMIC_COUNTER_BUFFERS:
          return 0;
+
       case PIPE_SHADER_CAP_MAX_UNROLL_ITERATIONS_HINT:
          return 32;
       default:
@@ -277,6 +373,9 @@ i915_get_param(struct pipe_screen *screen, enum pipe_cap cap)
       return PIPE_ENDIAN_LITTLE;
    case PIPE_CAP_MAX_VARYINGS:
       return 10;
+
+   case PIPE_CAP_NIR_IMAGES_AS_DEREF:
+      return 0;
 
    case PIPE_CAP_VENDOR_ID:
       return 0x8086;
@@ -530,6 +629,7 @@ i915_screen_create(struct i915_winsys *iws)
    is->base.get_param = i915_get_param;
    is->base.get_shader_param = i915_get_shader_param;
    is->base.get_paramf = i915_get_paramf;
+   is->base.get_compiler_options = i915_get_compiler_options;
    is->base.is_format_supported = i915_is_format_supported;
 
    is->base.context_create = i915_create_context;

src/gallium/drivers/i915/meson.build
@@ -67,6 +67,7 @@ libi915 = static_library(
   files_i915,
   gnu_symbol_visibility : 'hidden',
   include_directories : [inc_include, inc_src, inc_gallium, inc_gallium_aux],
+  dependencies : idep_nir,
 )
 
 driver_i915 = declare_dependency(

commit 7ce3240deed1140b16f388086eb639110ec80af4

i915g: Initialize the rest of the "from_nir" temporary VS struct.
draw looked at the uninitialized XFB state, which should just be zeroed
out since i915 doesn't have XFB.

Fixes: 528774c3d8d4 ("i915g: Switch to using nir-to-tgsi.")
Reviewed-by: Zoltán Böszörményi <zboszor@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14896>

src/gallium/drivers/i915/i915_state.c
@@ -611,7 +611,7 @@ i915_create_vs_state(struct pipe_context *pipe,
 {
    struct i915_context *i915 = i915_context(pipe);
 
-   struct pipe_shader_state from_nir;
+   struct pipe_shader_state from_nir = { PIPE_SHADER_IR_TGSI };
    if (templ->type == PIPE_SHADER_IR_NIR) {
       nir_shader *s = templ->ir.nir;
 
@@ -622,7 +622,6 @@ i915_create_vs_state(struct pipe_context *pipe,
        * per-stage, and i915 FS can't do native integers.  So, convert to TGSI,
        * where the draw path *does* support non-native-integers.
        */
-      from_nir.type = PIPE_SHADER_IR_TGSI;
       from_nir.tokens = nir_to_tgsi(s, pipe->screen);
       templ = &from_nir;
    }