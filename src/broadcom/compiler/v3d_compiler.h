openharmony/third_party_mesa3d

commit 9dc89ca8e816e06494cce313aa02703cb813aa8d

broadcom/compiler: do not rebuild the interference graph after each spill
Instead, we only recompute liveness and we add new nodes and
interferences to the graph manually (we also need to patch
register classes in some cases).

To assist in this process, we also add an ip counter to our
instructions that we also recompute after each spill, which we use
to identify registers that cross thrsw boundries introduced with
TMU spills and fills and adjust their register classes accordingly
(removing their capacity to use accumulators).

This significantly reduces the CPU cost of spills. Using
shaders/closed/gputest/piano/7.shader_test as reference:

Compile time up to the first successful compile strategy in main is
~24s and with this change it is ~11s. With this speed up, we can now
try all 2-thread compile strategies (including the fallback scheduler)
in only ~15s.

A full shader-db run results in:
Total CPU time (seconds): 9904.67 -> 9087.98 (-8.25%)

Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15041>

src/broadcom/compiler/v3d_compiler.h
@@ -172,6 +172,11 @@ struct qinst {
 
         /* If this is a a TLB Z write */
         bool is_tlb_z_write;
+
+        /* Position of this instruction in the program. Filled in during
+         * register allocation.
+         */
+        int32_t ip;
 };
 
 enum quniform_contents {
@@ -587,6 +592,23 @@ struct v3d_interp_input {
    unsigned mode; /* interpolation mode */
 };
 
+/* Data types used during register allocation to map nodes and temps */
+struct node_to_temp_map {
+        uint32_t temp;
+        uint32_t priority;
+};
+
+struct temp_to_node_map {
+        uint32_t node;
+        uint8_t  class_bits;
+};
+
+struct v3d_ra_temp_node_info {
+        struct node_to_temp_map *node;
+        struct temp_to_node_map *temp;
+        uint32_t alloc_count;
+};
+
 struct v3d_compile {
         const struct v3d_device_info *devinfo;
         nir_shader *s;
@@ -774,6 +796,11 @@ struct v3d_compile {
         struct qreg spill_base;
         /* Bit vector of which temps may be spilled */
         BITSET_WORD *spillable;
+
+        /* Used during register allocation */
+        int thread_index;
+        struct v3d_ra_temp_node_info ra_map;
+        struct ra_graph *g;
 
         /**
          * Array of the VARYING_SLOT_* of all FS QFILE_VARY reads.
@@ -1122,7 +1149,7 @@ void v3d40_vir_emit_image_load_store(struct v3d_compile *c,
 void v3d_vir_to_qpu(struct v3d_compile *c, struct qpu_reg *temp_registers);
 uint32_t v3d_qpu_schedule_instructions(struct v3d_compile *c);
 void qpu_validate(struct v3d_compile *c);
-struct qpu_reg *v3d_register_allocate(struct v3d_compile *c, bool *spilled);
+struct qpu_reg *v3d_register_allocate(struct v3d_compile *c);
 bool vir_init_reg_sets(struct v3d_compiler *compiler);
 
 int v3d_shaderdb_dump(struct v3d_compile *c, char **shaderdb_str);

src/broadcom/compiler/nir_to_vir.c
@@ -4471,13 +4471,11 @@ v3d_nir_to_vir(struct v3d_compile *c)
         int min_threads = (c->devinfo->ver >= 41) ? 2 : 1;
         struct qpu_reg *temp_registers;
         while (true) {
-                bool spilled;
-                temp_registers = v3d_register_allocate(c, &spilled);
-                if (spilled && c->spills + c->fills <= c->max_tmu_spills)
-                        continue;
-
-                if (temp_registers)
+                temp_registers = v3d_register_allocate(c);
+                if (temp_registers) {
+                        assert(c->spills + c->fills <= c->max_tmu_spills);
                         break;
+                }
 
                 if (c->threads == min_threads &&
                     (V3D_DEBUG & V3D_DEBUG_RA)) {

src/broadcom/compiler/vir.c
@@ -369,6 +369,8 @@ vir_add_inst(enum v3d_qpu_add_op op, struct qreg dst, struct qreg src0, struct q
         inst->src[1] = src1;
         inst->uniform = ~0;
 
+        inst->ip = -1;
+
         return inst;
 }
 
@@ -384,6 +386,8 @@ vir_mul_inst(enum v3d_qpu_mul_op op, struct qreg dst, struct qreg src0, struct q
         inst->src[0] = src0;
         inst->src[1] = src1;
         inst->uniform = ~0;
+
+        inst->ip = -1;
 
         return inst;
 }
@@ -404,12 +408,16 @@ vir_branch_inst(struct v3d_compile *c, enum v3d_qpu_branch_cond cond)
         inst->dst = vir_nop_reg();
         inst->uniform = vir_get_uniform_index(c, QUNIFORM_CONSTANT, 0);
 
+        inst->ip = -1;
+
         return inst;
 }
 
 static void
 vir_emit(struct v3d_compile *c, struct qinst *inst)
 {
+        inst->ip = -1;
+
         switch (c->cursor.mode) {
         case vir_cursor_add:
                 list_add(&inst->link, c->cursor.link);

commit 15cceb7c2f64980507b62bcd85b6703f448d8ada

broadcom/compiler: only patch temps that existed before the current spill
When we spill we add new temps. We should be careful not to access
liveness for these until we have re-computed it after all spills and
fill for that the spilled temp have been processed so as to avoid
out-of-bounds accesses to the c->temp_start and c->temp_end arrays.

This fixes a crash in a Three.js demo when we try to patch register
classes after a TMU spill that was caused because we would incorrectly
try to patch the same temps we had just added for the spill itself,
which is not only unnecessary but also incorrect since we these temps
would not have liveness information available yet and thus would
cause out of bounds accesses.

Fixes: 9dc89ca8e8 ('broadcom/compiler: do not rebuild the interference graph after each spill')
Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15107>

src/broadcom/compiler/v3d_compiler.h
@@ -845,6 +845,11 @@ struct v3d_compile {
 
         struct qreg undef;
         uint32_t num_temps;
+        /* Number of temps in the program right before we spill a new temp. We
+         * use this to know which temps existed before a spill and which were
+         * added with the spill itself.
+         */
+        uint32_t spill_start_num_temps;
 
         struct vir_cursor cursor;
         struct list_head blocks;

src/broadcom/compiler/vir_register_allocate.c
@@ -356,7 +356,7 @@ v3d_emit_spill_tmua(struct v3d_compile *c,
          * is not affected (and only the spilled temp starts at ip). Something
          * that ends at ip won't be affected either.
          */
-        for (int i = 0; i < c->num_temps; i++) {
+        for (int i = 0; i < c->spill_start_num_temps; i++) {
                 bool thrsw_cross = fill_dst ?
                         c->temp_start[i] < ip && c->temp_end[i] >= ip :
                         c->temp_start[i] < ip && c->temp_end[i] > ip;
@@ -414,7 +414,7 @@ interferes(int32_t t0_start, int32_t t0_end, int32_t t1_start, int32_t t1_end)
 static void
 v3d_spill_reg(struct v3d_compile *c, int *acc_nodes, int spill_temp)
 {
-        int start_num_temps = c->num_temps;
+        c->spill_start_num_temps = c->num_temps;
         c->spilling = true;
 
         bool is_uniform = vir_is_mov_uniform(c, spill_temp);
@@ -564,7 +564,7 @@ v3d_spill_reg(struct v3d_compile *c, int *acc_nodes, int spill_temp)
         /* Don't allow spilling of our spilling instructions.  There's no way
          * they can help get things colored.
          */
-        for (int i = start_num_temps; i < c->num_temps; i++)
+        for (int i = c->spill_start_num_temps; i < c->num_temps; i++)
                 BITSET_CLEAR(c->spillable, i);
 
         /* Reset interference for spilled node */
@@ -592,7 +592,8 @@ v3d_spill_reg(struct v3d_compile *c, int *acc_nodes, int spill_temp)
                 uint32_t node = c->ra_map.temp[i].node;
                 c->ra_map.node[node].priority = c->temp_end[i] - c->temp_start[i];
 
-                for (uint32_t j = MAX2(i + 1, start_num_temps); j < c->num_temps; j++) {
+                for (uint32_t j = MAX2(i + 1, c->spill_start_num_temps);
+                     j < c->num_temps; j++) {
                         if (interferes(c->temp_start[i], c->temp_end[i],
                                        c->temp_start[j], c->temp_end[j])) {
                                 ra_add_node_interference(c->g,