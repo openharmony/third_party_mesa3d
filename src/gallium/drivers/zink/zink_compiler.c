openharmony/third_party_mesa3d

commit aacc211f9301af65f87f179839f7dd2170271ce6

zink: fix 64bit float shader ops
this was being set from back before zink actually supported 64bit
natively and only 32bit was functional, but it breaks 64bit support

cc: mesa-stable

fixes (lavapipe):
KHR-GL46.gpu_shader_fp64.builtin.mod_dvec2
KHR-GL46.gpu_shader_fp64.builtin.mod_dvec3
KHR-GL46.gpu_shader_fp64.builtin.mod_dvec4

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15274>

src/gallium/drivers/zink/zink_compiler.c
@@ -335,7 +335,7 @@ zink_screen_init_compiler(struct zink_screen *screen)
       .lower_unpack_32_2x16_split = true,
       .lower_vector_cmp = true,
       .lower_int64_options = 0,
-      .lower_doubles_options = ~nir_lower_fp64_full_software,
+      .lower_doubles_options = 0,
       .lower_uniforms_to_ubo = true,
       .has_fsub = true,
       .has_isub = true,

commit dbd4cd9e82e35f01024ad5d130a2cfe6b0ab7ef3

zink: lower dmod on AMD hardware
this hardware won't return the correct value from dmod instructions,
so lower it to ensure that cts passes

nobody else will ever hit this, so perf isn't an issue and regular fmod
can be left alone

fixes (amd):
KHR-GL46.gpu_shader_fp64.builtin.mod_d*

Fixes: aacc211f9301af65f87f179839f7dd2170271ce6 ('zink: fix 64bit float shader ops ')

Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15306>

src/gallium/drivers/zink/zink_compiler.c
@@ -353,6 +353,21 @@ zink_screen_init_compiler(struct zink_screen *screen)
       screen->nir_options.lower_flrp64 = true;
       screen->nir_options.lower_ffma64 = true;
    }
+
+   /*
+       The OpFRem and OpFMod instructions use cheap approximations of remainder,
+       and the error can be large due to the discontinuity in trunc() and floor().
+       This can produce mathematically unexpected results in some cases, such as
+       FMod(x,x) computing x rather than 0, and can also cause the result to have
+       a different sign than the infinitely precise result.
+
+       -Table 84. Precision of core SPIR-V Instructions
+       * for drivers that are known to have imprecise fmod for doubles, lower dmod
+    */
+   if (screen->info.driver_props.driverID == VK_DRIVER_ID_MESA_RADV ||
+       screen->info.driver_props.driverID == VK_DRIVER_ID_AMD_OPEN_SOURCE ||
+       screen->info.driver_props.driverID == VK_DRIVER_ID_AMD_PROPRIETARY)
+      screen->nir_options.lower_doubles_options = nir_lower_dmod;
 }
 
 const void *