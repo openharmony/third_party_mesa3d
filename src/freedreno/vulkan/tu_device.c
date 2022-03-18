openharmony/third_party_mesa3d

commit 192a4b84c8c4a2443bcfd0464f63d0f65d938ff6

tu: Trivially implement VK_EXT_texel_buffer_alignment
The previous alignment of 64 bytes, which we got from the blob,
indicates that single-texel alignment isn't supported. So just do a
trivial no-op implementation that returns the same alignment as before.
This matches what newer blobs that expose this extension do.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15427>

src/freedreno/vulkan/tu_device.c
@@ -825,6 +825,12 @@ tu_GetPhysicalDeviceFeatures2(VkPhysicalDevice physicalDevice,
          features->depthClipControl = true;
          break;
       }
+      case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_TEXEL_BUFFER_ALIGNMENT_FEATURES_EXT: {
+         VkPhysicalDeviceTexelBufferAlignmentFeaturesEXT *features =
+            (VkPhysicalDeviceTexelBufferAlignmentFeaturesEXT *)ext;
+         features->texelBufferAlignment = true;
+         break;
+      }
 
       default:
          break;
@@ -1020,10 +1026,9 @@ tu_get_physical_device_properties_1_3(struct tu_physical_device *pdevice,
    p->integerDotProductAccumulatingSaturating64BitSignedAccelerated = false;
    p->integerDotProductAccumulatingSaturating64BitMixedSignednessAccelerated = false;
 
-   /* VK_EXT_texel_buffer_alignment is not implemented */
-   p->storageTexelBufferOffsetAlignmentBytes = 0;
+   p->storageTexelBufferOffsetAlignmentBytes = 64;
    p->storageTexelBufferOffsetSingleTexelAlignment = false;
-   p->uniformTexelBufferOffsetAlignmentBytes = 0;
+   p->uniformTexelBufferOffsetAlignmentBytes = 64;
    p->uniformTexelBufferOffsetSingleTexelAlignment = false;
 
    /* TODO: find out the limit */

commit 5c38228e0a6c83339c3b8bd9656662fceffcf585

tu: Actually expose VK_EXT_texel_buffer_alignment
Oops...

Fixes: 192a4b84 ("tu: Trivially implement VK_EXT_texel_buffer_alignment")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15451>

src/freedreno/vulkan/tu_device.c
@@ -194,6 +194,7 @@ get_device_extensions(const struct tu_physical_device *device,
       .EXT_shader_demote_to_helper_invocation = true,
       .EXT_shader_stencil_export = true,
       .EXT_shader_viewport_index_layer = true,
+      .EXT_texel_buffer_alignment = true,
       .EXT_vertex_attribute_divisor = true,
       .EXT_provoking_vertex = true,
       .EXT_line_rasterization = true,