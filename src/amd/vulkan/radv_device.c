openharmony/third_party_mesa3d

commit 42d90adf9387839a1a9c67d008395da260785e25

radv: Use common GetPhysicalDeviceMemoryProperties
Signed-off-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15613>

src/amd/vulkan/radv_device.c
@@ -2500,15 +2500,6 @@ radv_GetPhysicalDeviceQueueFamilyProperties2(VkPhysicalDevice physicalDevice, ui
    }
 }
 
-VKAPI_ATTR void VKAPI_CALL
-radv_GetPhysicalDeviceMemoryProperties(VkPhysicalDevice physicalDevice,
-                                       VkPhysicalDeviceMemoryProperties *pMemoryProperties)
-{
-   RADV_FROM_HANDLE(radv_physical_device, physical_device, physicalDevice);
-
-   *pMemoryProperties = physical_device->memory_properties;
-}
-
 static void
 radv_get_memory_budget_properties(VkPhysicalDevice physicalDevice,
                                   VkPhysicalDeviceMemoryBudgetPropertiesEXT *memoryBudget)
@@ -2618,7 +2609,9 @@ VKAPI_ATTR void VKAPI_CALL
 radv_GetPhysicalDeviceMemoryProperties2(VkPhysicalDevice physicalDevice,
                                         VkPhysicalDeviceMemoryProperties2 *pMemoryProperties)
 {
-   radv_GetPhysicalDeviceMemoryProperties(physicalDevice, &pMemoryProperties->memoryProperties);
+   RADV_FROM_HANDLE(radv_physical_device, pdevice, physicalDevice);
+   
+   pMemoryProperties->memoryProperties = pdevice->memory_properties;
 
    VkPhysicalDeviceMemoryBudgetPropertiesEXT *memory_budget =
       vk_find_struct(pMemoryProperties->pNext, PHYSICAL_DEVICE_MEMORY_BUDGET_PROPERTIES_EXT);

commit f955ac21617b80cf4a5a2933a62a3042aa188951

radv: fix mismatch between radv_GetPhysicalDeviceMemoryProperties*()
This fixes a regression with
dEQP-VK.api.info.get_physical_device_properties2.memory_properties.
This test expects the unused array elements to be untouched.

Fixes: 42d90adf938 ("radv: Use common GetPhysicalDeviceMemoryProperties")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15629>

src/amd/vulkan/radv_device.c
@@ -2610,8 +2610,22 @@ radv_GetPhysicalDeviceMemoryProperties2(VkPhysicalDevice physicalDevice,
                                         VkPhysicalDeviceMemoryProperties2 *pMemoryProperties)
 {
    RADV_FROM_HANDLE(radv_physical_device, pdevice, physicalDevice);
-   
-   pMemoryProperties->memoryProperties = pdevice->memory_properties;
+
+   pMemoryProperties->memoryProperties.memoryTypeCount = pdevice->memory_properties.memoryTypeCount;
+   for (uint32_t i = 0; i < pdevice->memory_properties.memoryTypeCount; i++) {
+      pMemoryProperties->memoryProperties.memoryTypes[i] = (VkMemoryType) {
+         .propertyFlags = pdevice->memory_properties.memoryTypes[i].propertyFlags,
+         .heapIndex     = pdevice->memory_properties.memoryTypes[i].heapIndex,
+      };
+   }
+
+   pMemoryProperties->memoryProperties.memoryHeapCount = pdevice->memory_properties.memoryHeapCount;
+   for (uint32_t i = 0; i < pdevice->memory_properties.memoryHeapCount; i++) {
+      pMemoryProperties->memoryProperties.memoryHeaps[i] = (VkMemoryHeap) {
+         .size    = pdevice->memory_properties.memoryHeaps[i].size,
+         .flags   = pdevice->memory_properties.memoryHeaps[i].flags,
+      };
+   }
 
    VkPhysicalDeviceMemoryBudgetPropertiesEXT *memory_budget =
       vk_find_struct(pMemoryProperties->pNext, PHYSICAL_DEVICE_MEMORY_BUDGET_PROPERTIES_EXT);