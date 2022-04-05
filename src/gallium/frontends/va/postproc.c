openharmony/third_party_mesa3d

commit 6ecbb28c2b2a2cf8cce3581ea20b5799a4bc9291

frontends/va: add encoder format conversion (EFC) support
Signed-off-by: Thong Thai <thong.thai@amd.com>
Reviewed-by: Leo Liu <leo.liu@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15196>

src/gallium/frontends/va/postproc.c
@@ -32,6 +32,7 @@
 #include "vl/vl_defines.h"
 #include "vl/vl_video_buffer.h"
 #include "vl/vl_deint_filter.h"
+#include "vl/vl_winsys.h"
 
 #include "va_private.h"
 
@@ -124,9 +125,12 @@ static VAStatus vlVaPostProcBlit(vlVaDriver *drv, vlVaContext *context,
    bool scale = false;
    bool grab = false;
    unsigned i;
+   struct pipe_screen *pscreen = drv->vscreen->pscreen;
 
-   if ((src->buffer_format == PIPE_FORMAT_B8G8R8A8_UNORM ||
-        src->buffer_format == PIPE_FORMAT_B8G8R8X8_UNORM) &&
+   if ((src->buffer_format == PIPE_FORMAT_B8G8R8X8_UNORM ||
+        src->buffer_format == PIPE_FORMAT_B8G8R8A8_UNORM ||
+        src->buffer_format == PIPE_FORMAT_R8G8B8X8_UNORM ||
+        src->buffer_format == PIPE_FORMAT_R8G8B8A8_UNORM) &&
        !src->interlaced)
       grab = true;
 
@@ -165,7 +169,9 @@ static VAStatus vlVaPostProcBlit(vlVaDriver *drv, vlVaContext *context,
    dst_rect.x1 = dst_region->x + dst_region->width;
    dst_rect.y1 = dst_region->y + dst_region->height;
 
-   if (grab) {
+   if (grab && !pscreen->get_video_param(pscreen, PIPE_VIDEO_PROFILE_UNKNOWN,
+                                         PIPE_VIDEO_ENTRYPOINT_ENCODE,
+                                         PIPE_VIDEO_CAP_EFC_SUPPORTED)) {
       vl_compositor_convert_rgb_to_yuv(&drv->cstate, &drv->compositor, 0,
                                        ((struct vl_video_buffer *)src)->resources[0],
                                        dst, &src_rect, &dst_rect);
@@ -220,7 +226,7 @@ static VAStatus vlVaPostProcBlit(vlVaDriver *drv, vlVaContext *context,
       blit.mask = PIPE_MASK_RGBA;
       blit.filter = PIPE_TEX_MIPFILTER_LINEAR;
 
-      if (drv->pipe->screen->get_param(drv->pipe->screen,
+      if (!grab && drv->pipe->screen->get_param(drv->pipe->screen,
                                        PIPE_CAP_PREFER_COMPUTE_FOR_MULTIMEDIA))
          util_compute_blit(drv->pipe, &blit, &context->blit_cs, !drv->compositor.deinterlace);
       else
@@ -288,6 +294,7 @@ vlVaHandleVAProcPipelineParameterBufferType(vlVaDriver *drv, vlVaContext *contex
    struct pipe_video_buffer *src, *dst;
    vlVaSurface *src_surface, *dst_surface;
    unsigned i;
+   struct pipe_screen *pscreen;
 
    if (!drv || !context)
       return VA_STATUS_ERROR_INVALID_CONTEXT;
@@ -302,6 +309,27 @@ vlVaHandleVAProcPipelineParameterBufferType(vlVaDriver *drv, vlVaContext *contex
 
    src_surface = handle_table_get(drv->htab, param->surface);
    dst_surface = handle_table_get(drv->htab, context->target_id);
+
+   pscreen = drv->vscreen->pscreen;
+
+   if (src_surface->buffer->buffer_format != dst_surface->buffer->buffer_format &&
+       pscreen->get_video_param(pscreen, PIPE_VIDEO_PROFILE_UNKNOWN, PIPE_VIDEO_ENTRYPOINT_ENCODE, PIPE_VIDEO_CAP_EFC_SUPPORTED)) {
+
+      // EFC will convert the buffer to a format the encoder accepts
+      dst_surface->encoder_format = dst_surface->buffer->buffer_format;
+
+      vlVaSurface *surf;
+
+      surf = handle_table_get(drv->htab, context->target_id);
+      surf->templat.interlaced = src_surface->templat.interlaced;
+      surf->templat.buffer_format = src_surface->templat.buffer_format;
+      surf->buffer->destroy(surf->buffer);
+
+      if (vlVaHandleSurfaceAllocate(drv, surf, &surf->templat, NULL, 0) != VA_STATUS_SUCCESS)
+         return VA_STATUS_ERROR_ALLOCATION_FAILED;
+
+      context->target = surf->buffer;
+   }
 
    if (!src_surface || !src_surface->buffer)
       return VA_STATUS_ERROR_INVALID_SURFACE;
@@ -369,7 +397,11 @@ vlVaHandleVAProcPipelineParameterBufferType(vlVaDriver *drv, vlVaContext *contex
 
    if (context->target->buffer_format != PIPE_FORMAT_NV12 &&
        context->target->buffer_format != PIPE_FORMAT_P010 &&
-       context->target->buffer_format != PIPE_FORMAT_P016)
+       context->target->buffer_format != PIPE_FORMAT_P016 &&
+       context->target->buffer_format != PIPE_FORMAT_B8G8R8X8_UNORM &&
+       context->target->buffer_format != PIPE_FORMAT_B8G8R8A8_UNORM &&
+       context->target->buffer_format != PIPE_FORMAT_R8G8B8X8_UNORM &&
+       context->target->buffer_format != PIPE_FORMAT_R8G8B8A8_UNORM)
       return vlVaPostProcCompositor(drv, context, src_region, dst_region,
                                     src, context->target, deinterlace);
    else

src/gallium/frontends/va/picture.c
@@ -785,6 +785,10 @@ vlVaEndPicture(VADriverContextP ctx, VAContextID context_id)
          context->desc.h264enc.frame_num_cnt++;
       } else if (u_reduce_video_profile(context->templat.profile) == PIPE_VIDEO_FORMAT_HEVC)
          getEncParamPresetH265(context);
+
+      context->desc.base.input_format = surf->buffer->buffer_format;
+      context->desc.base.output_format = surf->encoder_format;
+
       context->decoder->begin_frame(context->decoder, context->target, &context->desc.base);
       context->decoder->encode_bitstream(context->decoder, context->target,
                                          coded_buf->derived_surface.resource, &feedback);

src/gallium/frontends/va/va_private.h
@@ -337,6 +337,7 @@ typedef struct {
    unsigned int frame_num_cnt;
    bool force_flushed;
    struct pipe_video_buffer *obsolete_buf;
+   enum pipe_format encoder_format;
 } vlVaSurface;
 
 // Public functions:

commit f25574fb4d16f6c0c1a3bd5d2877688e1b835778

frontends/va: fix decode issues introduced by efc change
Fixes: 6ecbb28c2b2 ("frontends/va: add encoder format conversion (EFC) support")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6153
Signed-off-by: Thong Thai <thong.thai@amd.com>
Tested-by: Andrew Falcon <bluestang2006@gmail.com>
Reviewed-by: Leo Liu <leo.liu@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15717>

src/gallium/frontends/va/postproc.c
@@ -125,7 +125,6 @@ static VAStatus vlVaPostProcBlit(vlVaDriver *drv, vlVaContext *context,
    bool scale = false;
    bool grab = false;
    unsigned i;
-   struct pipe_screen *pscreen = drv->vscreen->pscreen;
 
    if ((src->buffer_format == PIPE_FORMAT_B8G8R8X8_UNORM ||
         src->buffer_format == PIPE_FORMAT_B8G8R8A8_UNORM ||
@@ -169,9 +168,7 @@ static VAStatus vlVaPostProcBlit(vlVaDriver *drv, vlVaContext *context,
    dst_rect.x1 = dst_region->x + dst_region->width;
    dst_rect.y1 = dst_region->y + dst_region->height;
 
-   if (grab && !pscreen->get_video_param(pscreen, PIPE_VIDEO_PROFILE_UNKNOWN,
-                                         PIPE_VIDEO_ENTRYPOINT_ENCODE,
-                                         PIPE_VIDEO_CAP_EFC_SUPPORTED)) {
+   if (grab) {
       vl_compositor_convert_rgb_to_yuv(&drv->cstate, &drv->compositor, 0,
                                        ((struct vl_video_buffer *)src)->resources[0],
                                        dst, &src_rect, &dst_rect);
@@ -226,7 +223,7 @@ static VAStatus vlVaPostProcBlit(vlVaDriver *drv, vlVaContext *context,
       blit.mask = PIPE_MASK_RGBA;
       blit.filter = PIPE_TEX_MIPFILTER_LINEAR;
 
-      if (!grab && drv->pipe->screen->get_param(drv->pipe->screen,
+      if (drv->pipe->screen->get_param(drv->pipe->screen,
                                        PIPE_CAP_PREFER_COMPUTE_FOR_MULTIMEDIA))
          util_compute_blit(drv->pipe, &blit, &context->blit_cs, !drv->compositor.deinterlace);
       else
@@ -313,7 +310,14 @@ vlVaHandleVAProcPipelineParameterBufferType(vlVaDriver *drv, vlVaContext *contex
    pscreen = drv->vscreen->pscreen;
 
    if (src_surface->buffer->buffer_format != dst_surface->buffer->buffer_format &&
-       pscreen->get_video_param(pscreen, PIPE_VIDEO_PROFILE_UNKNOWN, PIPE_VIDEO_ENTRYPOINT_ENCODE, PIPE_VIDEO_CAP_EFC_SUPPORTED)) {
+       !src_surface->buffer->interlaced &&
+       (dst_surface->buffer->buffer_format == PIPE_FORMAT_NV12 ||
+        dst_surface->buffer->buffer_format == PIPE_FORMAT_P010 ||
+        dst_surface->buffer->buffer_format == PIPE_FORMAT_P016) &&
+       pscreen->get_video_param(pscreen,
+                                PIPE_VIDEO_PROFILE_UNKNOWN,
+                                PIPE_VIDEO_ENTRYPOINT_ENCODE,
+                                PIPE_VIDEO_CAP_EFC_SUPPORTED)) {
 
       // EFC will convert the buffer to a format the encoder accepts
       dst_surface->encoder_format = dst_surface->buffer->buffer_format;
@@ -400,11 +404,7 @@ vlVaHandleVAProcPipelineParameterBufferType(vlVaDriver *drv, vlVaContext *contex
 
    if (context->target->buffer_format != PIPE_FORMAT_NV12 &&
        context->target->buffer_format != PIPE_FORMAT_P010 &&
-       context->target->buffer_format != PIPE_FORMAT_P016 &&
-       context->target->buffer_format != PIPE_FORMAT_B8G8R8X8_UNORM &&
-       context->target->buffer_format != PIPE_FORMAT_B8G8R8A8_UNORM &&
-       context->target->buffer_format != PIPE_FORMAT_R8G8B8X8_UNORM &&
-       context->target->buffer_format != PIPE_FORMAT_R8G8B8A8_UNORM)
+       context->target->buffer_format != PIPE_FORMAT_P016)
       return vlVaPostProcCompositor(drv, context, src_region, dst_region,
                                     src, context->target, deinterlace);
    else