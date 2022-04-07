openharmony/third_party_mesa3d

commit 9ef41d1f490c72a1e43a5b4e892e40a6ec6618c9

radv: implement VK_EXT_depth_clip_control
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15192>

src/amd/vulkan/radv_pipeline.c
@@ -1859,7 +1859,7 @@ radv_pipeline_init_raster_state(struct radv_pipeline *pipeline,
    }
 
    pipeline->graphics.pa_cl_clip_cntl =
-      S_028810_DX_CLIP_SPACE_DEF(1) | // vulkan uses DX conventions.
+      S_028810_DX_CLIP_SPACE_DEF(!pipeline->graphics.negative_one_to_one) |
       S_028810_ZCLIP_NEAR_DISABLE(depth_clip_disable ? 1 : 0) |
       S_028810_ZCLIP_FAR_DISABLE(depth_clip_disable ? 1 : 0) |
       S_028810_DX_RASTERIZATION_KILL(raster_info->rasterizerDiscardEnable ? 1 : 0) |
@@ -1868,6 +1868,21 @@ radv_pipeline_init_raster_state(struct radv_pipeline *pipeline,
    pipeline->graphics.uses_conservative_overestimate =
       radv_get_conservative_raster_mode(pCreateInfo->pRasterizationState) ==
          VK_CONSERVATIVE_RASTERIZATION_MODE_OVERESTIMATE_EXT;
+}
+
+static void
+radv_pipeline_init_viewport_state(struct radv_pipeline *pipeline,
+                                  const VkGraphicsPipelineCreateInfo *pCreateInfo)
+{
+   if (pCreateInfo->pRasterizationState->rasterizerDiscardEnable)
+      return;
+
+   const VkPipelineViewportDepthClipControlCreateInfoEXT *depth_clip_control =
+      vk_find_struct_const(pCreateInfo->pViewportState->pNext,
+                           PIPELINE_VIEWPORT_DEPTH_CLIP_CONTROL_CREATE_INFO_EXT);
+   if (depth_clip_control) {
+      pipeline->graphics.negative_one_to_one = !!depth_clip_control->negativeOneToOne;
+   }
 }
 
 static struct radv_depth_stencil_state
@@ -6397,6 +6412,7 @@ radv_pipeline_init(struct radv_pipeline *pipeline, struct radv_device *device,
    if (!radv_pipeline_has_mesh(pipeline))
       radv_pipeline_init_input_assembly_state(pipeline, pCreateInfo);
    radv_pipeline_init_dynamic_state(pipeline, pCreateInfo);
+   radv_pipeline_init_viewport_state(pipeline, pCreateInfo);
    radv_pipeline_init_raster_state(pipeline, pCreateInfo);
 
    struct radv_depth_stencil_state ds_state =

src/amd/vulkan/radv_cmd_buffer.c
@@ -1422,6 +1422,7 @@ radv_emit_graphics_pipeline(struct radv_cmd_buffer *cmd_buffer)
 static void
 radv_emit_viewport(struct radv_cmd_buffer *cmd_buffer)
 {
+   bool negative_one_to_one = cmd_buffer->state.pipeline->graphics.negative_one_to_one;
    const struct radv_viewport_state *viewport = &cmd_buffer->state.dynamic.viewport;
    int i;
    const unsigned count = viewport->count;
@@ -1434,8 +1435,18 @@ radv_emit_viewport(struct radv_cmd_buffer *cmd_buffer)
       radeon_emit(cmd_buffer->cs, fui(viewport->xform[i].translate[0]));
       radeon_emit(cmd_buffer->cs, fui(viewport->xform[i].scale[1]));
       radeon_emit(cmd_buffer->cs, fui(viewport->xform[i].translate[1]));
-      radeon_emit(cmd_buffer->cs, fui(viewport->xform[i].scale[2]));
-      radeon_emit(cmd_buffer->cs, fui(viewport->xform[i].translate[2]));
+
+      double scale_z, translate_z;
+      if (negative_one_to_one) {
+         scale_z = viewport->xform[i].scale[2] * 0.5f;
+         translate_z = (viewport->xform[i].translate[2] + viewport->viewports[i].maxDepth) * 0.5f;
+      } else {
+         scale_z = viewport->xform[i].scale[2];
+         translate_z = viewport->xform[i].translate[2];
+
+      }
+      radeon_emit(cmd_buffer->cs, fui(scale_z));
+      radeon_emit(cmd_buffer->cs, fui(translate_z));
    }
 
    radeon_set_context_reg_seq(cmd_buffer->cs, R_0282D0_PA_SC_VPORT_ZMIN_0, count * 2);

src/amd/vulkan/radv_private.h
@@ -1893,6 +1893,7 @@ struct radv_pipeline {
          unsigned cb_color_control;
          bool uses_dynamic_stride;
          bool uses_conservative_overestimate;
+         bool negative_one_to_one;
 
          /* Used for rbplus */
          uint32_t col_format;

commit 02ed2d6bd8c8966f2a230d1a0e6c0f49610b23f5

radv: fix dynamic raster discard with VK_EXT_depth_clip_control
Fixes: 9ef41d1f490 ("radv: implement VK_EXT_depth_clip_control")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15764>

src/amd/vulkan/radv_pipeline.c
@@ -1874,7 +1874,8 @@ static void
 radv_pipeline_init_viewport_state(struct radv_pipeline *pipeline,
                                   const VkGraphicsPipelineCreateInfo *pCreateInfo)
 {
-   if (pCreateInfo->pRasterizationState->rasterizerDiscardEnable)
+   if (pCreateInfo->pRasterizationState->rasterizerDiscardEnable &&
+       !radv_is_state_dynamic(pCreateInfo, VK_DYNAMIC_STATE_RASTERIZER_DISCARD_ENABLE))
       return;
 
    const VkPipelineViewportDepthClipControlCreateInfoEXT *depth_clip_control =