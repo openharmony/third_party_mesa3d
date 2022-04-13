openharmony/third_party_mesa3d

commit 3e1be64627e7d68c7a861fa033aed979a5ddd836

virgl: Add an extra mov for int outputs from constant and immediate inputs
virglrenderer doesn't properly emit the conversion code when the source
is a integer value and the output is also integer.

Fixes on NTT:
  dEQP-GLES31.functional.shaders.sample_variables.sample_mask.inverse_per_*

v2: fix typo (Emma)

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15836>

src/gallium/drivers/virgl/virgl_tgsi.c
@@ -329,6 +329,27 @@ virgl_tgsi_transform_instruction(struct tgsi_transform_context *ctx,
       inst->Src[0].Register.Index = vtctx->src_temp;
    }
 
+   /* virglrenderer doesn't resolve non-float output write properly,
+    * so we have to first write to a temporary */
+   if ((inst->Src[0].Register.File == TGSI_FILE_CONSTANT ||
+        inst->Src[0].Register.File == TGSI_FILE_IMMEDIATE) &&
+       inst->Dst[0].Register.File == TGSI_FILE_OUTPUT &&
+       tgsi_opcode_infer_dst_type(inst->Instruction.Opcode, 0) != TGSI_TYPE_FLOAT)  {
+      struct tgsi_full_instruction op_to_temp = *inst;
+      op_to_temp.Dst[0].Register.File = TGSI_FILE_TEMPORARY;
+      op_to_temp.Dst[0].Register.Index = vtctx->src_temp;
+      ctx->emit_instruction(ctx, &op_to_temp);
+
+      inst->Instruction.Opcode = TGSI_OPCODE_MOV;
+      inst->Src[0].Register.File = TGSI_FILE_TEMPORARY;
+      inst->Src[0].Register.Index = vtctx->src_temp;
+      inst->Src[0].Register.Indirect = 0;
+      inst->Src[0].Register.SwizzleX = 0;
+      inst->Src[0].Register.SwizzleY = 1;
+      inst->Src[0].Register.SwizzleZ = 2;
+      inst->Src[0].Register.SwizzleW = 3;
+   }
+
    for (unsigned i = 0; i < inst->Instruction.NumDstRegs; i++) {
       /* virglrenderer would fail to compile on clipdist, clipvertex, and some
        * two-sided-related color writes without a full writemask.  So, we write

commit dd0d54d1c382ecc794865162319ecd1f8d3b2a0d

virgl: Apply integer op fix only for ALU ops and clear modifiers
For texture fetches and buffer load the fix is not needed,
and the override creates faulty TGSI.

In addition remove all modifiers from the src in the additional mov
instruction.

Fixes: 3e1be64627e7d68c7a861fa033aed979a5ddd836
  virgl: Add an extra mov for int outputs from constant and immediate inputs

v2: Move workaround after the use of
    virgl_tgsi_rewrite_src_for_input_temp (Emma)

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15896>

src/gallium/drivers/virgl/virgl_tgsi.c
@@ -329,27 +329,6 @@ virgl_tgsi_transform_instruction(struct tgsi_transform_context *ctx,
       inst->Src[0].Register.Index = vtctx->src_temp;
    }
 
-   /* virglrenderer doesn't resolve non-float output write properly,
-    * so we have to first write to a temporary */
-   if ((inst->Src[0].Register.File == TGSI_FILE_CONSTANT ||
-        inst->Src[0].Register.File == TGSI_FILE_IMMEDIATE) &&
-       inst->Dst[0].Register.File == TGSI_FILE_OUTPUT &&
-       tgsi_opcode_infer_dst_type(inst->Instruction.Opcode, 0) != TGSI_TYPE_FLOAT)  {
-      struct tgsi_full_instruction op_to_temp = *inst;
-      op_to_temp.Dst[0].Register.File = TGSI_FILE_TEMPORARY;
-      op_to_temp.Dst[0].Register.Index = vtctx->src_temp;
-      ctx->emit_instruction(ctx, &op_to_temp);
-
-      inst->Instruction.Opcode = TGSI_OPCODE_MOV;
-      inst->Src[0].Register.File = TGSI_FILE_TEMPORARY;
-      inst->Src[0].Register.Index = vtctx->src_temp;
-      inst->Src[0].Register.Indirect = 0;
-      inst->Src[0].Register.SwizzleX = 0;
-      inst->Src[0].Register.SwizzleY = 1;
-      inst->Src[0].Register.SwizzleZ = 2;
-      inst->Src[0].Register.SwizzleW = 3;
-   }
-
    for (unsigned i = 0; i < inst->Instruction.NumDstRegs; i++) {
       /* virglrenderer would fail to compile on clipdist, clipvertex, and some
        * two-sided-related color writes without a full writemask.  So, we write
@@ -404,6 +383,30 @@ virgl_tgsi_transform_instruction(struct tgsi_transform_context *ctx,
       }
    }
    ctx->emit_instruction(ctx, inst);
+
+   /* virglrenderer doesn't resolve non-float output write properly,
+    * so we have to first write to a temporary */
+   if ((inst->Src[0].Register.File == TGSI_FILE_CONSTANT ||
+        inst->Src[0].Register.File == TGSI_FILE_IMMEDIATE) &&
+       !tgsi_get_opcode_info(inst->Instruction.Opcode)->is_tex &&
+       !tgsi_get_opcode_info(inst->Instruction.Opcode)->is_store &&
+       inst->Dst[0].Register.File == TGSI_FILE_OUTPUT &&
+       tgsi_opcode_infer_dst_type(inst->Instruction.Opcode, 0) != TGSI_TYPE_FLOAT)  {
+      struct tgsi_full_instruction op_to_temp = *inst;
+      op_to_temp.Dst[0].Register.File = TGSI_FILE_TEMPORARY;
+      op_to_temp.Dst[0].Register.Index = vtctx->src_temp;
+      ctx->emit_instruction(ctx, &op_to_temp);
+
+      inst->Instruction.Opcode = TGSI_OPCODE_MOV;
+      inst->Instruction.NumSrcRegs = 1;
+
+      memset(&inst->Src[0], 0, sizeof(inst->Src[0]));
+      inst->Src[0].Register.File = TGSI_FILE_TEMPORARY;
+      inst->Src[0].Register.Index = vtctx->src_temp;
+      inst->Src[0].Register.SwizzleY = 1;
+      inst->Src[0].Register.SwizzleZ = 2;
+      inst->Src[0].Register.SwizzleW = 3;
+   }
 
    for (unsigned i = 0; i < inst->Instruction.NumDstRegs; i++) {
       if (vtctx->num_writemask_fixups &&