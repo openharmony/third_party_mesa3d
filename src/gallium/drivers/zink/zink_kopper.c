openharmony/third_party_mesa3d

commit ca0e1bfb9df5e67f580b3f021d4e041f16bb1526

zink: add kopper api
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14541>

src/gallium/drivers/zink/zink_kopper.h
@@ -1,0 +1,109 @@
+/*
+ * Copyright © 2021 Valve Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ * 
+ * Authors:
+ *    Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
+ */
+
+#ifndef ZINK_KOPPER_H
+#define ZINK_KOPPER_H
+
+#include "kopper_interface.h"
+
+struct kopper_swapchain {
+   VkSwapchainKHR swapchain;
+   VkImage *images;
+   bool *inits;
+   unsigned last_present;
+   unsigned num_images;
+   VkSemaphore *acquires;
+   uint32_t last_present_prune;
+   struct hash_table *presents;
+   VkSwapchainCreateInfoKHR scci;
+   unsigned num_acquires;
+   unsigned max_acquires;
+};
+
+enum kopper_type {
+   KOPPER_X11,
+   KOPPER_WAYLAND,
+};
+
+struct kopper_displaytarget
+{
+   unsigned refcount;
+   VkFormat formats[2];
+   unsigned width;
+   unsigned height;
+   unsigned stride;
+   void *loader_private;
+
+   VkSurfaceKHR surface;
+   struct kopper_swapchain *swapchain;
+   struct kopper_swapchain *old_swapchain;
+
+   struct kopper_loader_info info;
+
+   VkSurfaceCapabilitiesKHR caps;
+   VkImageFormatListCreateInfoKHR format_list;
+   enum kopper_type type;
+};
+
+struct zink_screen;
+struct zink_resource;
+
+static inline bool
+zink_kopper_has_srgb(const struct kopper_displaytarget *cdt)
+{
+   return cdt->formats[1] != VK_FORMAT_UNDEFINED;
+}
+
+static inline bool
+zink_kopper_last_present_eq(const struct kopper_displaytarget *cdt, uint32_t idx)
+{
+   return cdt->swapchain->last_present == idx;
+}
+
+struct kopper_displaytarget *
+zink_kopper_displaytarget_create(struct zink_screen *screen, unsigned tex_usage,
+                                 enum pipe_format format, unsigned width,
+                                 unsigned height, unsigned alignment,
+                                 const void *loader_private, unsigned *stride);
+void
+zink_kopper_displaytarget_destroy(struct zink_screen *screen, struct kopper_displaytarget *cdt);
+
+
+bool
+zink_kopper_acquire(struct zink_context *ctx, struct zink_resource *res, uint64_t timeout);
+VkSemaphore
+zink_kopper_acquire_submit(struct zink_screen *screen, struct zink_resource *res);
+VkSemaphore
+zink_kopper_present(struct zink_screen *screen, struct zink_resource *res); 
+void
+zink_kopper_present_queue(struct zink_screen *screen, struct zink_resource *res);
+bool
+zink_kopper_acquire_readback(struct zink_context *ctx, struct zink_resource *res);
+bool
+zink_kopper_present_readback(struct zink_context *ctx, struct zink_resource *res);
+void
+zink_kopper_deinit_displaytarget(struct zink_screen *screen, struct kopper_displaytarget *cdt);
+#endif

commit 87e0a18d1b082c7412ca7ba942a7388e7a57d427

zink: move the kopper present fence to the displaytarget object
this is otherwise broken when front/back buffers share the same dt

Fixes: ca0e1bfb9df ("zink: add kopper api")

Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16070>

src/gallium/drivers/zink/zink_kopper.c
@@ -148,6 +148,7 @@ zink_kopper_deinit_displaytarget(struct zink_screen *screen, struct kopper_displ
    VKSCR(DestroySurfaceKHR)(screen->instance, cdt->surface, NULL);
    cdt->swapchain = cdt->old_swapchain = NULL;
    cdt->surface = VK_NULL_HANDLE;
+   util_queue_fence_destroy(&cdt->present_fence);
 }
 
 static struct kopper_swapchain *
@@ -322,6 +323,7 @@ zink_kopper_displaytarget_create(struct zink_screen *screen, unsigned tex_usage,
    cdt->refcount = 1;
    cdt->loader_private = (void*)loader_private;
    cdt->info = *info;
+   util_queue_fence_init(&cdt->present_fence);
 
    enum pipe_format srgb = PIPE_FORMAT_NONE;
    if (screen->info.have_KHR_swapchain_mutable_format) {
@@ -403,7 +405,7 @@ kopper_acquire(struct zink_screen *screen, struct zink_resource *res, uint64_t t
       }
       if (timeout == UINT64_MAX && util_queue_is_initialized(&screen->flush_queue) &&
           p_atomic_read_relaxed(&cdt->swapchain->num_acquires) > cdt->swapchain->max_acquires) {
-         util_queue_fence_wait(&res->obj->present_fence);
+         util_queue_fence_wait(&cdt->present_fence);
       }
       VkSemaphoreCreateInfo sci = {
          VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
@@ -600,7 +602,7 @@ zink_kopper_present_queue(struct zink_screen *screen, struct zink_resource *res)
    cpi->info.pResults = NULL;
    res->obj->present = VK_NULL_HANDLE;
    if (util_queue_is_initialized(&screen->flush_queue)) {
-      util_queue_add_job(&screen->flush_queue, cpi, &res->obj->present_fence,
+      util_queue_add_job(&screen->flush_queue, cpi, &cdt->present_fence,
                          kopper_present, NULL, 0);
    } else {
       kopper_present(cpi, screen, 0);

src/gallium/drivers/zink/zink_kopper.h
@@ -62,6 +62,7 @@ struct kopper_displaytarget
    struct kopper_swapchain *old_swapchain;
 
    struct kopper_loader_info info;
+   struct util_queue_fence present_fence;
 
    VkSurfaceCapabilitiesKHR caps;
    VkImageFormatListCreateInfoKHR format_list;

src/gallium/drivers/zink/zink_resource.c
@@ -104,7 +104,6 @@ zink_destroy_resource_object(struct zink_screen *screen, struct zink_resource_ob
 
    zink_descriptor_set_refs_clear(&obj->desc_set_refs, obj);
    if (obj->dt) {
-      util_queue_fence_destroy(&obj->present_fence);
       FREE(obj->bo); //this is a dummy struct
    } else
       zink_bo_unref(screen, obj->bo);
@@ -521,7 +520,6 @@ resource_object_create(struct zink_screen *screen, const struct pipe_resource *t
    if (loader_private) {
       obj->bo = CALLOC_STRUCT(zink_bo);
       obj->transfer_dst = true;
-      util_queue_fence_init(&obj->present_fence);
       return obj;
    } else if (templ->target == PIPE_BUFFER) {
       VkBufferCreateInfo bci = create_bci(screen, templ, templ->bind);

src/gallium/drivers/zink/zink_resource.h
@@ -91,7 +91,6 @@ struct zink_resource_object {
    bool new_dt;
    bool dt_has_data;
    bool indefinite_acquire;
-   struct util_queue_fence present_fence;
    // }
 
 