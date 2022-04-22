openharmony/third_party_mesa3d

commit c4de59453d5ca5cd46c659844b3db6eb8332ca90

util/set: add the found param to search_or_add
this brings parity with the internal api

Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8450>

src/util/set.c
@@ -510,15 +510,15 @@ _mesa_set_search_and_add_pre_hashed(struct set *set, uint32_t hash,
 }
 
 struct set_entry *
-_mesa_set_search_or_add(struct set *set, const void *key)
+_mesa_set_search_or_add(struct set *set, const void *key, bool *found)
 {
    assert(set->key_hash_function);
-   return set_search_or_add(set, set->key_hash_function(key), key, NULL);
+   return set_search_or_add(set, set->key_hash_function(key), key, found);
 }
 
 struct set_entry *
 _mesa_set_search_or_add_pre_hashed(struct set *set, uint32_t hash,
-                                   const void *key)
+                                   const void *key, bool *found)
 {
    assert(set->key_hash_function == NULL ||
           hash == set->key_hash_function(key));

src/compiler/nir/nir_instr_set.c
@@ -807,7 +807,7 @@ nir_instr_set_add_or_rewrite(struct set *instr_set, nir_instr *instr)
    if (!instr_can_rewrite(instr))
       return false;
 
-   struct set_entry *e = _mesa_set_search_or_add(instr_set, instr);
+   struct set_entry *e = _mesa_set_search_or_add(instr_set, instr, NULL);
    nir_instr *match = (nir_instr *) e->key;
    if (match != instr) {
       nir_ssa_def *def = nir_instr_get_dest_ssa_def(instr);

src/util/set.h
@@ -81,10 +81,10 @@ struct set_entry *
 _mesa_set_add_pre_hashed(struct set *set, uint32_t hash, const void *key);
 
 struct set_entry *
-_mesa_set_search_or_add(struct set *set, const void *key);
+_mesa_set_search_or_add(struct set *set, const void *key, bool *found);
 struct set_entry *
 _mesa_set_search_or_add_pre_hashed(struct set *set, uint32_t hash,
-                                   const void *key);
+                                   const void *key, bool *found);
 
 struct set_entry *
 _mesa_set_search(const struct set *set, const void *key);

src/util/tests/set/set_test.cpp
@@ -143,12 +143,16 @@ TEST(set, search_or_add)
    _mesa_set_add(s, &b);
    EXPECT_EQ(s->entries, 2);
 
-   struct set_entry *entry = _mesa_set_search_or_add(s, &c);
+   bool found = false;
+   struct set_entry *entry = _mesa_set_search_or_add(s, &c, &found);
    EXPECT_EQ(entry->key, (void *)&b);
+   EXPECT_EQ(found, true);
    EXPECT_EQ(s->entries, 2);
 
-   struct set_entry *entry3 = _mesa_set_search_or_add(s, &d);
+   found = false;
+   struct set_entry *entry3 = _mesa_set_search_or_add(s, &d, &found);
    EXPECT_EQ(entry3->key, &d);
+   EXPECT_EQ(found, false);
    EXPECT_EQ(s->entries, 3);
 
    _mesa_set_destroy(s, NULL);

commit 651df8490e3f8b08241a79c568a0e36514d106a8

util/set: Respect found in search_or_add_pre_hashed
Fixes: c4de59453d5c "util/set: add the found param to search_or_add"
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Reviewed-by: Connor Abbott <cwabbott0@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13184>

src/util/set.c
@@ -531,7 +531,7 @@ _mesa_set_search_or_add_pre_hashed(struct set *set, uint32_t hash,
 {
    assert(set->key_hash_function == NULL ||
           hash == set->key_hash_function(key));
-   return set_search_or_add(set, hash, key, NULL);
+   return set_search_or_add(set, hash, key, found);
 }
 
 /**