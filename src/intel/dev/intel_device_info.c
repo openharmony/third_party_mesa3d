openharmony/third_party_mesa3d

commit c21ab0cca752174f69cb625f42bd2f27630ea019

intel: Add device info for DG2
Reworks:
 * Lionel: simulator_id
 * Rafael: has_llc
 * Merged Lionel's "intel/devinfo: store the different kind of DG2"

Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Acked-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14524>

src/intel/dev/intel_device_info.c
@@ -1028,6 +1028,31 @@ static const struct intel_device_info intel_device_info_sg1 = {
    GFX12_DG1_SG1_FEATURES,
 };
 
+#define XEHP_FEATURES(_gt, _slices, _l3)                        \
+   GFX12_FEATURES(_gt, _slices, _l3),                           \
+   .verx10 = 125,                                               \
+   .has_llc = false,                                            \
+   .has_local_mem = true,                                       \
+   .has_aux_map = false,                                        \
+   .simulator_id = 29,                                          \
+   .cs_prefetch_size = 1024
+
+#define DG2_FEATURES                                            \
+   /* (Sub)slice info comes from the kernel topology info */    \
+   XEHP_FEATURES(0, 1, 0),                                      \
+   .num_subslices = dual_subslices(1),                          \
+   .has_lsc = true
+
+UNUSED static const struct intel_device_info intel_device_info_dg2_g10 = {
+   DG2_FEATURES,
+   .platform = INTEL_PLATFORM_DG2_G10,
+};
+
+UNUSED static const struct intel_device_info intel_device_info_dg2_g11 = {
+   DG2_FEATURES,
+   .platform = INTEL_PLATFORM_DG2_G11,
+};
+
 static void
 reset_masks(struct intel_device_info *devinfo)
 {

commit 65d3884c74cb85d490d09b1ae85a3f74b3cc06f9

intel: fixup number of threads per EU on XeHP
Computations for indexing in-memory data structures for ray queries
depend on this.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: c21ab0cca752 ("intel: Add device info for DG2")
Acked-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15925>

src/intel/dev/intel_device_info.c
@@ -1053,6 +1053,7 @@ static const struct intel_device_info intel_device_info_sg1 = {
 
 #define XEHP_FEATURES(_gt, _slices, _l3)                        \
    GFX12_FEATURES(_gt, _slices, _l3),                           \
+   .num_thread_per_eu = 8 /* BSpec 44472 */,                    \
    .verx10 = 125,                                               \
    .has_llc = false,                                            \
    .has_local_mem = true,                                       \