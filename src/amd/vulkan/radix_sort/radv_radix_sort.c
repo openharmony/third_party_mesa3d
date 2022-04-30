openharmony/third_party_mesa3d

commit 701e24301273fee8762d002e33000585cca6d7b0

radv: Add the fuchsia radix sort
Signed-off-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15648>

src/amd/vulkan/radix_sort/radv_radix_sort.c
@@ -1,0 +1,193 @@
+/*
+ * Copyright © 2022 Konstantin Seurer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "radv_radix_sort.h"
+#include "targets/u64/config.h"
+#include "radv_private.h"
+#include "target.h"
+
+static const uint32_t init_spv[] = {
+#include "radix_sort/shaders/init.comp.spv.h"
+};
+
+static const uint32_t fill_spv[] = {
+#include "radix_sort/shaders/fill.comp.spv.h"
+};
+
+static const uint32_t histogram_spv[] = {
+#include "radix_sort/shaders/histogram.comp.spv.h"
+};
+
+static const uint32_t prefix_spv[] = {
+#include "radix_sort/shaders/prefix.comp.spv.h"
+};
+
+static const uint32_t scatter_0_even_spv[] = {
+#include "radix_sort/shaders/scatter_0_even.comp.spv.h"
+};
+
+static const uint32_t scatter_0_odd_spv[] = {
+#include "radix_sort/shaders/scatter_0_odd.comp.spv.h"
+};
+
+static const uint32_t scatter_1_even_spv[] = {
+#include "radix_sort/shaders/scatter_1_even.comp.spv.h"
+};
+
+static const uint32_t scatter_1_odd_spv[] = {
+#include "radix_sort/shaders/scatter_1_odd.comp.spv.h"
+};
+
+static const struct radix_sort_vk_target_config target_config = {
+   .keyval_dwords = RS_KEYVAL_DWORDS,
+
+   .histogram =
+      {
+         .workgroup_size_log2 = RS_HISTOGRAM_WORKGROUP_SIZE_LOG2,
+         .subgroup_size_log2 = RS_HISTOGRAM_SUBGROUP_SIZE_LOG2,
+         .block_rows = RS_HISTOGRAM_BLOCK_ROWS,
+      },
+
+   .prefix =
+      {
+         .workgroup_size_log2 = RS_PREFIX_WORKGROUP_SIZE_LOG2,
+         .subgroup_size_log2 = RS_PREFIX_SUBGROUP_SIZE_LOG2,
+      },
+
+   .scatter =
+      {
+         .workgroup_size_log2 = RS_SCATTER_WORKGROUP_SIZE_LOG2,
+         .subgroup_size_log2 = RS_SCATTER_SUBGROUP_SIZE_LOG2,
+         .block_rows = RS_SCATTER_BLOCK_ROWS,
+      },
+};
+
+radix_sort_vk_t *
+radv_create_radix_sort_u64(VkDevice device, VkAllocationCallbacks const *ac, VkPipelineCache pc)
+{
+   const uint32_t *spv[8] = {
+      init_spv,           fill_spv,          histogram_spv,      prefix_spv,
+      scatter_0_even_spv, scatter_0_odd_spv, scatter_1_even_spv, scatter_1_odd_spv,
+   };
+   const uint32_t spv_sizes[8] = {
+      sizeof(init_spv),           sizeof(fill_spv),           sizeof(histogram_spv),
+      sizeof(prefix_spv),         sizeof(scatter_0_even_spv), sizeof(scatter_0_odd_spv),
+      sizeof(scatter_1_even_spv), sizeof(scatter_1_odd_spv),
+   };
+   return radix_sort_vk_create(device, ac, pc, spv, spv_sizes, target_config);
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL
+vkCreateShaderModule(VkDevice device, const VkShaderModuleCreateInfo *pCreateInfo,
+                     const VkAllocationCallbacks *pAllocator, VkShaderModule *pShaderModule)
+{
+   RADV_FROM_HANDLE(radv_device, pdevice, device);
+   return pdevice->vk.dispatch_table.CreateShaderModule(device, pCreateInfo, pAllocator,
+                                                        pShaderModule);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vkDestroyShaderModule(VkDevice device, VkShaderModule shaderModule,
+                      const VkAllocationCallbacks *pAllocator)
+{
+   RADV_FROM_HANDLE(radv_device, pdevice, device);
+   pdevice->vk.dispatch_table.DestroyShaderModule(device, shaderModule, pAllocator);
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL
+vkCreatePipelineLayout(VkDevice device, const VkPipelineLayoutCreateInfo *pCreateInfo,
+                       const VkAllocationCallbacks *pAllocator, VkPipelineLayout *pPipelineLayout)
+{
+   RADV_FROM_HANDLE(radv_device, pdevice, device);
+   return pdevice->vk.dispatch_table.CreatePipelineLayout(device, pCreateInfo, pAllocator,
+                                                          pPipelineLayout);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vkDestroyPipelineLayout(VkDevice device, VkPipelineLayout pipelineLayout,
+                        const VkAllocationCallbacks *pAllocator)
+{
+   RADV_FROM_HANDLE(radv_device, pdevice, device);
+   pdevice->vk.dispatch_table.DestroyPipelineLayout(device, pipelineLayout, pAllocator);
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL
+vkCreateComputePipelines(VkDevice device, VkPipelineCache pipelineCache, uint32_t createInfoCount,
+                         const VkComputePipelineCreateInfo *pCreateInfos,
+                         const VkAllocationCallbacks *pAllocator, VkPipeline *pPipelines)
+{
+   RADV_FROM_HANDLE(radv_device, pdevice, device);
+   return pdevice->vk.dispatch_table.CreateComputePipelines(device, pipelineCache, createInfoCount,
+                                                            pCreateInfos, pAllocator, pPipelines);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vkDestroyPipeline(VkDevice device, VkPipeline pipeline, const VkAllocationCallbacks *pAllocator)
+{
+   RADV_FROM_HANDLE(radv_device, pdevice, device);
+   return pdevice->vk.dispatch_table.DestroyPipeline(device, pipeline, pAllocator);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vkCmdPipelineBarrier(VkCommandBuffer commandBuffer, VkPipelineStageFlags srcStageMask,
+                     VkPipelineStageFlags dstStageMask, VkDependencyFlags dependencyFlags,
+                     uint32_t memoryBarrierCount, const VkMemoryBarrier *pMemoryBarriers,
+                     uint32_t bufferMemoryBarrierCount,
+                     const VkBufferMemoryBarrier *pBufferMemoryBarriers,
+                     uint32_t imageMemoryBarrierCount,
+                     const VkImageMemoryBarrier *pImageMemoryBarriers)
+{
+   RADV_FROM_HANDLE(radv_cmd_buffer, cmd_buffer, commandBuffer);
+   cmd_buffer->device->vk.dispatch_table.CmdPipelineBarrier(
+      commandBuffer, srcStageMask, dstStageMask, dependencyFlags, memoryBarrierCount,
+      pMemoryBarriers, bufferMemoryBarrierCount, pBufferMemoryBarriers, imageMemoryBarrierCount,
+      pImageMemoryBarriers);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vkCmdPushConstants(VkCommandBuffer commandBuffer, VkPipelineLayout layout,
+                   VkShaderStageFlags stageFlags, uint32_t offset, uint32_t size,
+                   const void *pValues)
+{
+   RADV_FROM_HANDLE(radv_cmd_buffer, cmd_buffer, commandBuffer);
+   cmd_buffer->device->vk.dispatch_table.CmdPushConstants(commandBuffer, layout, stageFlags, offset,
+                                                          size, pValues);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vkCmdBindPipeline(VkCommandBuffer commandBuffer, VkPipelineBindPoint pipelineBindPoint,
+                  VkPipeline pipeline)
+{
+   RADV_FROM_HANDLE(radv_cmd_buffer, cmd_buffer, commandBuffer);
+   cmd_buffer->device->vk.dispatch_table.CmdBindPipeline(commandBuffer, pipelineBindPoint,
+                                                         pipeline);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vkCmdDispatch(VkCommandBuffer commandBuffer, uint32_t groupCountX, uint32_t groupCountY,
+              uint32_t groupCountZ)
+{
+   RADV_FROM_HANDLE(radv_cmd_buffer, cmd_buffer, commandBuffer);
+   cmd_buffer->device->vk.dispatch_table.CmdDispatch(commandBuffer, groupCountX, groupCountY,
+                                                     groupCountZ);
+}

meson.build
@@ -680,7 +680,7 @@ if with_gallium_d3d12 or with_microsoft_clc or with_microsoft_vk
   endif
 endif
 
-if with_vulkan_overlay_layer or with_aco_tests
+if with_vulkan_overlay_layer or with_aco_tests or with_amd_vk
   prog_glslang = find_program('glslangValidator')
 endif
 

src/amd/vulkan/meson.build
@@ -120,6 +120,9 @@ if with_llvm
   )
 endif
 
+subdir('radix_sort')
+libradv_files += radix_sort_files
+
 radv_deps = []
 radv_flags = cc.get_supported_arguments(['-Wimplicit-fallthrough', '-Wshadow'])
 
@@ -151,7 +154,7 @@ endif
 
 libvulkan_radeon = shared_library(
   'vulkan_radeon',
-  [libradv_files, radv_entrypoints, sha1_h],
+  [libradv_files, radv_entrypoints, sha1_h, radix_sort_spv],
   vs_module_defs : vulkan_api_def,
   include_directories : [
     inc_include, inc_src, inc_mapi, inc_mesa, inc_gallium, inc_gallium_aux, inc_amd, inc_amd_common, inc_amd_common_llvm, inc_compiler, inc_util,

src/amd/vulkan/radix_sort/LICENSE
@@ -1,0 +1,24 @@
+Copyright 2019 The Fuchsia Authors.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+   * Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following disclaimer
+in the documentation and/or other materials provided with the
+distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.

src/amd/vulkan/radix_sort/common/macros.h
@@ -1,0 +1,112 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_GRAPHICS_LIB_COMPUTE_COMMON_MACROS_H_
+#define SRC_GRAPHICS_LIB_COMPUTE_COMMON_MACROS_H_
+
+//
+//
+//
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+
+//
+// clang-format off
+//
+
+#define ARRAY_LENGTH_MACRO(x_)          (sizeof(x_)/sizeof(x_[0]))
+#define OFFSETOF_MACRO(t_,m_)           offsetof(t_,m_)
+#define MEMBER_SIZE_MACRO(t_,m_)        sizeof(((t_*)0)->m_)
+
+//
+// FIXME(allanmac):
+//
+// Consider providing typed min/max() functions:
+//
+//   <type> [min|max]_<type>(a,b) { ; }
+//
+// But note we still need preprocessor-time min/max().
+//
+
+#define MAX_MACRO(t_,a_,b_)             (((a_) > (b_)) ? (a_) : (b_))
+#define MIN_MACRO(t_,a_,b_)             (((a_) < (b_)) ? (a_) : (b_))
+
+//
+//
+//
+
+#define BITS_TO_MASK_MACRO(n_)          (((uint32_t)1<<(n_))-1)
+#define BITS_TO_MASK_64_MACRO(n_)       (((uint64_t)1<<(n_))-1)
+
+#define BITS_TO_MASK_AT_MACRO(n_,b_)    (BITS_TO_MASK_MACRO(n_)   <<(b_))
+#define BITS_TO_MASK_AT_64_MACRO(n_,b_) (BITS_TO_MASK_64_MACRO(n_)<<(b_))
+
+//
+//
+//
+
+#define STRINGIFY_MACRO_2(a_)           #a_
+#define STRINGIFY_MACRO(a_)             STRINGIFY_MACRO_2(a_)
+
+//
+//
+//
+
+#define CONCAT_MACRO_2(a_,b_)           a_ ## b_
+#define CONCAT_MACRO(a_,b_)             CONCAT_MACRO_2(a_,b_)
+
+//
+// Round up/down
+//
+
+#define ROUND_DOWN_MACRO(v_,q_)         (((v_) / (q_)) * (q_))
+#define ROUND_UP_MACRO(v_,q_)           ((((v_) + (q_) - 1) / (q_)) * (q_))
+
+//
+// Round up/down when q is a power-of-two.
+//
+
+#define ROUND_DOWN_POW2_MACRO(v_,q_)    ((v_) & ~((q_) - 1))
+#define ROUND_UP_POW2_MACRO(v_,q_)      ROUND_DOWN_POW2_MACRO((v_) + (q_) - 1, q_)
+
+//
+//
+//
+
+#if defined (_MSC_VER) && !defined (__clang__)
+#define STATIC_ASSERT_MACRO(c_,m_)      static_assert(c_,m_)
+#else
+#define STATIC_ASSERT_MACRO(c_,m_)      _Static_assert(c_,m_)
+#endif
+
+#define STATIC_ASSERT_MACRO_1(c_)       STATIC_ASSERT_MACRO(c_,#c_)
+
+//
+//
+//
+
+#if defined (_MSC_VER) && !defined (__clang__)
+#define POPCOUNT_MACRO(...)             __popcnt(__VA_ARGS__)
+#else
+#define POPCOUNT_MACRO(...)             __builtin_popcount(__VA_ARGS__)
+#endif
+
+//
+//
+//
+
+#if defined (_MSC_VER) && !defined (__clang__)
+#define ALIGN_MACRO(bytes_)             __declspec(align(bytes_)) // only accepts integer as arg
+#else
+#include <stdalign.h>
+#define ALIGN_MACRO(bytes_)             alignas(bytes_)
+#endif
+
+//
+// clang-format on
+//
+
+#endif  // SRC_GRAPHICS_LIB_COMPUTE_COMMON_MACROS_H_

src/amd/vulkan/radix_sort/common/util.c
@@ -1,0 +1,90 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "util.h"
+
+#include <assert.h>
+
+//
+//
+//
+
+#if defined(_MSC_VER) && !defined(__clang__)
+
+#include <intrin.h>
+
+#endif
+
+//
+//
+//
+
+bool
+is_pow2_u32(uint32_t n)
+{
+  return n && !(n & (n - 1));
+}
+
+//
+//
+//
+
+uint32_t
+pow2_ru_u32(uint32_t n)
+{
+  assert(n <= 0x80000000U);
+
+  n--;
+  n |= n >> 1;
+  n |= n >> 2;
+  n |= n >> 4;
+  n |= n >> 8;
+  n |= n >> 16;
+  n++;
+
+  return n;
+}
+
+//
+//
+//
+
+uint32_t
+pow2_rd_u32(uint32_t n)
+{
+  assert(n > 0);
+
+  return 1u << msb_idx_u32(n);
+}
+
+//
+// ASSUMES NON-ZERO
+//
+
+uint32_t
+msb_idx_u32(uint32_t n)
+{
+  assert(n > 0);
+#if defined(_MSC_VER) && !defined(__clang__)
+
+  uint32_t index;
+
+  _BitScanReverse((unsigned long *)&index, n);
+
+  return index;
+
+#elif defined(__GNUC__)
+
+  return __builtin_clz(n) ^ 31;
+
+#else
+
+#error "No msb_index()"
+
+#endif
+}
+
+//
+//
+//

src/amd/vulkan/radix_sort/common/util.h
@@ -1,0 +1,59 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_GRAPHICS_LIB_COMPUTE_COMMON_UTIL_H_
+#define SRC_GRAPHICS_LIB_COMPUTE_COMMON_UTIL_H_
+
+//
+//
+//
+
+#include <stdbool.h>
+#include <stdint.h>
+
+//
+//
+//
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//
+//
+//
+
+// Return true iff |n| is a power of 2.
+bool
+is_pow2_u32(uint32_t n);
+
+// Return |n| rounded-up to the nearest power of 2.
+// If |n| is zero then return 0.
+// REQUIRES: |n <= 0x80000000|.
+uint32_t
+pow2_ru_u32(uint32_t n);
+
+// Return |n| rounded-down to the nearest power of 2.
+// REQUIRES: |n > 0|.
+uint32_t
+pow2_rd_u32(uint32_t n);
+
+// Return the most-significant bit position for |n|.
+// REQUIRES: |n > 0|.
+uint32_t
+msb_idx_u32(uint32_t n);  // 0-based bit position
+
+//
+//
+//
+
+#ifdef __cplusplus
+}
+#endif
+
+//
+//
+//
+
+#endif  // SRC_GRAPHICS_LIB_COMPUTE_COMMON_UTIL_H_

src/amd/vulkan/radix_sort/common/vk/assert.c
@@ -1,0 +1,108 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//
+//
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//
+//
+//
+
+#include "assert.h"
+
+//
+//
+//
+
+#define VK_RESULT_TO_STRING(result)                                                                \
+  case result:                                                                                     \
+    return #result
+
+//
+// FIXME -- results and errors
+//
+
+char const *
+vk_get_result_string(VkResult const result)
+{
+  switch (result)
+    {
+      //
+      // Results
+      //
+      VK_RESULT_TO_STRING(VK_SUCCESS);
+      VK_RESULT_TO_STRING(VK_NOT_READY);
+      VK_RESULT_TO_STRING(VK_TIMEOUT);
+      VK_RESULT_TO_STRING(VK_EVENT_SET);
+      VK_RESULT_TO_STRING(VK_EVENT_RESET);
+      VK_RESULT_TO_STRING(VK_INCOMPLETE);
+      //
+      // Errors
+      //
+      VK_RESULT_TO_STRING(VK_ERROR_OUT_OF_HOST_MEMORY);
+      VK_RESULT_TO_STRING(VK_ERROR_OUT_OF_DEVICE_MEMORY);
+      VK_RESULT_TO_STRING(VK_ERROR_INITIALIZATION_FAILED);
+      VK_RESULT_TO_STRING(VK_ERROR_DEVICE_LOST);
+      VK_RESULT_TO_STRING(VK_ERROR_MEMORY_MAP_FAILED);
+      VK_RESULT_TO_STRING(VK_ERROR_LAYER_NOT_PRESENT);
+      VK_RESULT_TO_STRING(VK_ERROR_EXTENSION_NOT_PRESENT);
+      VK_RESULT_TO_STRING(VK_ERROR_FEATURE_NOT_PRESENT);
+      VK_RESULT_TO_STRING(VK_ERROR_INCOMPATIBLE_DRIVER);
+      VK_RESULT_TO_STRING(VK_ERROR_TOO_MANY_OBJECTS);
+      VK_RESULT_TO_STRING(VK_ERROR_FORMAT_NOT_SUPPORTED);
+      VK_RESULT_TO_STRING(VK_ERROR_FRAGMENTED_POOL);
+      VK_RESULT_TO_STRING(VK_ERROR_OUT_OF_POOL_MEMORY);
+      VK_RESULT_TO_STRING(VK_ERROR_INVALID_EXTERNAL_HANDLE);
+      VK_RESULT_TO_STRING(VK_ERROR_SURFACE_LOST_KHR);
+      VK_RESULT_TO_STRING(VK_ERROR_NATIVE_WINDOW_IN_USE_KHR);
+      VK_RESULT_TO_STRING(VK_SUBOPTIMAL_KHR);
+      VK_RESULT_TO_STRING(VK_ERROR_OUT_OF_DATE_KHR);
+      VK_RESULT_TO_STRING(VK_ERROR_INCOMPATIBLE_DISPLAY_KHR);
+      VK_RESULT_TO_STRING(VK_ERROR_VALIDATION_FAILED_EXT);
+      VK_RESULT_TO_STRING(VK_ERROR_INVALID_SHADER_NV);
+      VK_RESULT_TO_STRING(VK_ERROR_FRAGMENTATION_EXT);
+      VK_RESULT_TO_STRING(VK_ERROR_NOT_PERMITTED_EXT);
+
+        //
+        // Extensions: vk_xyz
+        //
+      default:
+        return "UNKNOWN VULKAN RESULT";
+    }
+}
+
+//
+//
+//
+
+VkResult
+vk_assert(VkResult const result, char const * const file, int const line, bool const is_abort)
+{
+  if (result != VK_SUCCESS)
+    {
+      char const * const vk_result_str = vk_get_result_string(result);
+
+      fprintf(stderr,
+              "\"%s\", line %d: vk_assert( %d ) = \"%s\"\n",
+              file,
+              line,
+              result,
+              vk_result_str);
+
+      if (is_abort)
+        {
+          abort();
+        }
+    }
+
+  return result;
+}
+
+//
+//
+//

commit 61fcb2b78adf7f069b414452fa6f0f191bace7e1

radv/radix_sort: Add missing entry points
Fixes: 701e243 ("radv: Add the fuchsia radix sort")
Signed-off-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Reviewed-by: Yonggang Luo <luoyonggang@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16259>

src/amd/vulkan/radix_sort/radv_radix_sort.c
@@ -191,3 +191,26 @@ vkCmdDispatch(VkCommandBuffer commandBuffer, uint32_t groupCountX, uint32_t grou
    cmd_buffer->device->vk.dispatch_table.CmdDispatch(commandBuffer, groupCountX, groupCountY,
                                                      groupCountZ);
 }
+
+VKAPI_ATTR VkDeviceAddress VKAPI_CALL
+vkGetBufferDeviceAddress(VkDevice _device, const VkBufferDeviceAddressInfo *pInfo)
+{
+   RADV_FROM_HANDLE(radv_device, device, _device);
+   return device->vk.dispatch_table.GetBufferDeviceAddress(_device, pInfo);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vkCmdFillBuffer(VkCommandBuffer commandBuffer, VkBuffer dstBuffer, VkDeviceSize dstOffset,
+                VkDeviceSize size, uint32_t data)
+{
+   RADV_FROM_HANDLE(radv_cmd_buffer, cmd_buffer, commandBuffer);
+   cmd_buffer->device->vk.dispatch_table.CmdFillBuffer(commandBuffer, dstBuffer, dstOffset, size,
+                                                       data);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vkCmdDispatchIndirect(VkCommandBuffer commandBuffer, VkBuffer buffer, VkDeviceSize offset)
+{
+   RADV_FROM_HANDLE(radv_cmd_buffer, cmd_buffer, commandBuffer);
+   cmd_buffer->device->vk.dispatch_table.CmdDispatchIndirect(commandBuffer, buffer, offset);
+}