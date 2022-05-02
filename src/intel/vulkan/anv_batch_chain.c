openharmony/third_party_mesa3d

commit be2c3a8f4eeef71fccbbaf14c6d1ea6fa9c49d40

anv: implement u_trace support
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Rohan Garg <rohan.garg@intel.com>
Acked-by: Antonio Caggiano <antonio.caggiano@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13996>

src/intel/vulkan/anv_batch_chain.c
@@ -37,6 +37,7 @@
 #include "perf/intel_perf.h"
 
 #include "util/debug.h"
+#include "util/perf/u_trace.h"
 
 /** \file anv_batch_chain.c
  *
@@ -1956,6 +1957,94 @@ setup_empty_execbuf(struct anv_execbuf *execbuf, struct anv_queue *queue)
    return VK_SUCCESS;
 }
 
+static VkResult
+setup_utrace_execbuf(struct anv_execbuf *execbuf, struct anv_queue *queue,
+                     struct anv_utrace_flush_copy *flush)
+{
+   struct anv_device *device = queue->device;
+   VkResult result = anv_execbuf_add_bo(device, execbuf,
+                                        flush->batch_bo,
+                                        &flush->relocs, 0);
+   if (result != VK_SUCCESS)
+      return result;
+
+   result = anv_execbuf_add_sync(device, execbuf, flush->sync,
+                                 true /* is_signal */, 0 /* value */);
+   if (result != VK_SUCCESS)
+      return result;
+
+   if (flush->batch_bo->exec_obj_index != execbuf->bo_count - 1) {
+      uint32_t idx = flush->batch_bo->exec_obj_index;
+      uint32_t last_idx = execbuf->bo_count - 1;
+
+      struct drm_i915_gem_exec_object2 tmp_obj = execbuf->objects[idx];
+      assert(execbuf->bos[idx] == flush->batch_bo);
+
+      execbuf->objects[idx] = execbuf->objects[last_idx];
+      execbuf->bos[idx] = execbuf->bos[last_idx];
+      execbuf->bos[idx]->exec_obj_index = idx;
+
+      execbuf->objects[last_idx] = tmp_obj;
+      execbuf->bos[last_idx] = flush->batch_bo;
+      flush->batch_bo->exec_obj_index = last_idx;
+   }
+
+   if (!device->info.has_llc) {
+      __builtin_ia32_mfence();
+      for (uint32_t i = 0; i < flush->batch_bo->size; i += CACHELINE_SIZE)
+         __builtin_ia32_clflush(flush->batch_bo->map);
+   }
+
+   execbuf->execbuf = (struct drm_i915_gem_execbuffer2) {
+      .buffers_ptr = (uintptr_t) execbuf->objects,
+      .buffer_count = execbuf->bo_count,
+      .batch_start_offset = 0,
+      .batch_len = flush->batch.next - flush->batch.start,
+      .flags = I915_EXEC_HANDLE_LUT | I915_EXEC_FENCE_ARRAY | queue->exec_flags |
+               (execbuf->has_relocs ? 0 : I915_EXEC_NO_RELOC),
+      .rsvd1 = device->context_id,
+      .rsvd2 = 0,
+      .num_cliprects = execbuf->syncobj_count,
+      .cliprects_ptr = (uintptr_t)execbuf->syncobjs,
+   };
+
+   return VK_SUCCESS;
+}
+
+static VkResult
+anv_queue_exec_utrace_locked(struct anv_queue *queue,
+                             struct anv_utrace_flush_copy *flush)
+{
+   assert(flush->batch_bo);
+
+   struct anv_device *device = queue->device;
+   struct anv_execbuf execbuf;
+   anv_execbuf_init(&execbuf);
+   execbuf.alloc = &device->vk.alloc;
+   execbuf.alloc_scope = VK_SYSTEM_ALLOCATION_SCOPE_DEVICE;
+
+   VkResult result = setup_utrace_execbuf(&execbuf, queue, flush);
+   if (result != VK_SUCCESS)
+      goto error;
+
+   int ret = queue->device->info.no_hw ? 0 :
+      anv_gem_execbuffer(queue->device, &execbuf.execbuf);
+   if (ret)
+      result = vk_queue_set_lost(&queue->vk, "execbuf2 failed: %m");
+
+   struct drm_i915_gem_exec_object2 *objects = execbuf.objects;
+   for (uint32_t k = 0; k < execbuf.bo_count; k++) {
+      if (anv_bo_is_pinned(execbuf.bos[k]))
+         assert(execbuf.bos[k]->offset == objects[k].offset);
+      execbuf.bos[k]->offset = objects[k].offset;
+   }
+
+ error:
+   anv_execbuf_finish(&execbuf);
+
+   return result;
+}
+
 /* We lock around execbuf for three main reasons:
  *
  *  1) When a block pool is resized, we create a new gem handle with a
@@ -1992,16 +2081,37 @@ anv_queue_exec_locked(struct anv_queue *queue,
                       uint32_t perf_query_pass)
 {
    struct anv_device *device = queue->device;
+   struct anv_utrace_flush_copy *utrace_flush_data = NULL;
    struct anv_execbuf execbuf;
    anv_execbuf_init(&execbuf);
    execbuf.alloc = &queue->device->vk.alloc;
    execbuf.alloc_scope = VK_SYSTEM_ALLOCATION_SCOPE_DEVICE;
    execbuf.perf_query_pass = perf_query_pass;
 
+   /* Flush the trace points first, they need to be moved */
+   VkResult result =
+      anv_device_utrace_flush_cmd_buffers(queue,
+                                          cmd_buffer_count,
+                                          cmd_buffers,
+                                          &utrace_flush_data);
+   if (result != VK_SUCCESS)
+      goto error;
+
+   if (utrace_flush_data && !utrace_flush_data->batch_bo) {
+      result = anv_execbuf_add_sync(device, &execbuf,
+                                    utrace_flush_data->sync,
+                                    true /* is_signal */,
+                                    0);
+      if (result != VK_SUCCESS)
+         goto error;
+
+      utrace_flush_data = NULL;
+   }
+
    /* Always add the workaround BO as it includes a driver identifier for the
     * error_state.
     */
-   VkResult result =
+   result =
       anv_execbuf_add_bo(device, &execbuf, device->workaround_bo, NULL, 0);
    if (result != VK_SUCCESS)
       goto error;
@@ -2147,6 +2257,9 @@ anv_queue_exec_locked(struct anv_queue *queue,
 
  error:
    anv_execbuf_finish(&execbuf);
+
+   if (result == VK_SUCCESS && utrace_flush_data)
+      result = anv_queue_exec_utrace_locked(queue, utrace_flush_data);
 
    return result;
 }

src/intel/vulkan/anv_cmd_buffer.c
@@ -302,6 +302,8 @@ static VkResult anv_create_cmd_buffer(
 
    anv_measure_init(cmd_buffer);
 
+   u_trace_init(&cmd_buffer->trace, &device->trace_context);
+
    *pCommandBuffer = anv_cmd_buffer_to_handle(cmd_buffer);
 
    return VK_SUCCESS;
@@ -343,6 +345,8 @@ VkResult anv_AllocateCommandBuffers(
 static void
 anv_cmd_buffer_destroy(struct anv_cmd_buffer *cmd_buffer)
 {
+   u_trace_fini(&cmd_buffer->trace);
+
    anv_measure_destroy(cmd_buffer);
 
    list_del(&cmd_buffer->pool_link);
@@ -401,6 +405,10 @@ anv_cmd_buffer_reset(struct anv_cmd_buffer *cmd_buffer)
                          &cmd_buffer->device->general_state_pool, 16384);
 
    anv_measure_reset(cmd_buffer);
+
+   u_trace_fini(&cmd_buffer->trace);
+   u_trace_init(&cmd_buffer->trace, &cmd_buffer->device->trace_context);
+
    return VK_SUCCESS;
 }
 

src/intel/vulkan/anv_device.c
@@ -3297,6 +3297,8 @@ VkResult anv_CreateDevice(
 
    anv_device_perf_init(device);
 
+   anv_device_utrace_init(device);
+
    *pDevice = anv_device_to_handle(device);
 
    return VK_SUCCESS;
@@ -3363,6 +3365,8 @@ void anv_DestroyDevice(
 
    if (!device)
       return;
+
+   anv_device_utrace_finish(device);
 
    anv_device_finish_blorp(device);
 

src/intel/vulkan/anv_genX.h
@@ -143,8 +143,9 @@ void genX(blorp_exec)(struct blorp_batch *batch,
                       const struct blorp_params *params);
 
 void genX(cmd_emit_timestamp)(struct anv_batch *batch,
-                              struct anv_bo *bo,
-                              uint32_t offset);
+                              struct anv_device *device,
+                              struct anv_address addr,
+                              bool end_of_pipe);
 
 void
 genX(rasterization_mode)(VkPolygonMode raster_mode,

src/intel/vulkan/anv_measure.c
@@ -158,7 +158,11 @@ anv_measure_start_snapshot(struct anv_cmd_buffer *cmd_buffer,
 
    unsigned index = measure->base.index++;
 
-   (*device->cmd_emit_timestamp)(batch, measure->bo, index * sizeof(uint64_t));
+   (*device->cmd_emit_timestamp)(batch, cmd_buffer->device,
+                                 (struct anv_address) {
+                                    .bo = measure->bo,
+                                    .offset = index * sizeof(uint64_t) },
+                                 true /* end_of_pipe */);
 
    if (event_name == NULL)
       event_name = intel_measure_snapshot_string(type);
@@ -195,7 +199,11 @@ anv_measure_end_snapshot(struct anv_cmd_buffer *cmd_buffer,
    unsigned index = measure->base.index++;
    assert(index % 2 == 1);
 
-   (*device->cmd_emit_timestamp)(batch, measure->bo, index * sizeof(uint64_t));
+   (*device->cmd_emit_timestamp)(batch, cmd_buffer->device,
+                                 (struct anv_address) {
+                                    .bo = measure->bo,
+                                    .offset = index * sizeof(uint64_t) },
+                                 true /* end_of_pipe */);
 
    struct intel_measure_snapshot *snapshot = &(measure->base.snapshots[index]);
    memset(snapshot, 0, sizeof(*snapshot));

src/intel/vulkan/anv_private.h
@@ -57,6 +57,7 @@
 #include "util/macros.h"
 #include "util/hash_table.h"
 #include "util/list.h"
+#include "util/perf/u_trace.h"
 #include "util/sparse_array.h"
 #include "util/u_atomic.h"
 #include "util/u_vector.h"
@@ -552,6 +553,46 @@ anv_bo_is_pinned(struct anv_bo *bo)
 #endif
 }
 
+struct anv_address {
+   struct anv_bo *bo;
+   int64_t offset;
+};
+
+#define ANV_NULL_ADDRESS ((struct anv_address) { NULL, 0 })
+
+static inline struct anv_address
+anv_address_from_u64(uint64_t addr_u64)
+{
+   assert(addr_u64 == intel_canonical_address(addr_u64));
+   return (struct anv_address) {
+      .bo = NULL,
+      .offset = addr_u64,
+   };
+}
+
+static inline bool
+anv_address_is_null(struct anv_address addr)
+{
+   return addr.bo == NULL && addr.offset == 0;
+}
+
+static inline uint64_t
+anv_address_physical(struct anv_address addr)
+{
+   if (addr.bo && anv_bo_is_pinned(addr.bo)) {
+      return intel_canonical_address(addr.bo->offset + addr.offset);
+   } else {
+      return intel_canonical_address(addr.offset);
+   }
+}
+
+static inline struct anv_address
+anv_address_add(struct anv_address addr, uint64_t offset)
+{
+   addr.offset += offset;
+   return addr;
+}
+
 /* Represents a lock-free linked list of "free" things.  This is used by
  * both the block pool and the state pools.  Unfortunately, in order to
  * solve the ABA problem, we can't use a single uint32_t head.
@@ -986,7 +1027,7 @@ struct anv_physical_device {
     int64_t                                     master_minor;
     struct drm_i915_query_engine_info *         engine_info;
 
-    void (*cmd_emit_timestamp)(struct anv_batch *, struct anv_bo *, uint32_t );
+    void (*cmd_emit_timestamp)(struct anv_batch *, struct anv_device *, struct anv_address, bool);
     struct intel_measure_device                 measure_device;
 };
 
@@ -1094,11 +1135,6 @@ anv_device_upload_nir(struct anv_device *device,
                       const struct nir_shader *nir,
                       unsigned char sha1_key[20]);
 
-struct anv_address {
-   struct anv_bo *bo;
-   int64_t offset;
-};
-
 struct anv_device {
     struct vk_device                            vk;
 
@@ -1179,6 +1215,8 @@ struct anv_device {
     const struct intel_l3_config                *l3_config;
 
     struct intel_debug_block_frame              *debug_frame_desc;
+
+    struct u_trace_context                      trace_context;
 };
 
 #if defined(GFX_VERx10) && GFX_VERx10 >= 90
@@ -1504,42 +1542,6 @@ anv_batch_emit_reloc(struct anv_batch *batch,
    }
 
    return address_u64;
-}
-
-
-#define ANV_NULL_ADDRESS ((struct anv_address) { NULL, 0 })
-
-static inline struct anv_address
-anv_address_from_u64(uint64_t addr_u64)
-{
-   assert(addr_u64 == intel_canonical_address(addr_u64));
-   return (struct anv_address) {
-      .bo = NULL,
-      .offset = addr_u64,
-   };
-}
-
-static inline bool
-anv_address_is_null(struct anv_address addr)
-{
-   return addr.bo == NULL && addr.offset == 0;
-}
-
-static inline uint64_t
-anv_address_physical(struct anv_address addr)
-{
-   if (addr.bo && anv_bo_is_pinned(addr.bo)) {
-      return intel_canonical_address(addr.bo->offset + addr.offset);
-   } else {
-      return intel_canonical_address(addr.offset);
-   }
-}
-
-static inline struct anv_address
-anv_address_add(struct anv_address addr, uint64_t offset)
-{
-   addr.offset += offset;
-   return addr;
 }
 
 static inline void
@@ -3088,6 +3090,11 @@ struct anv_cmd_buffer {
     * Used to increase allocation size for long command buffers.
     */
    uint32_t                                     total_batch_size;
+
+   /**
+    *
+    */
+   struct u_trace                               trace;
 };
 
 /* Determine whether we can chain a given cmd_buffer to another one. We need
@@ -4540,6 +4547,29 @@ struct anv_memcpy_state {
    struct anv_vb_cache_range vb_bound;
    struct anv_vb_cache_range vb_dirty;
 };
+
+struct anv_utrace_flush_copy {
+   struct u_trace trace;
+
+   struct anv_reloc_list relocs;
+   struct anv_batch batch;
+   struct anv_bo *batch_bo;
+
+   struct anv_bo *trace_bo;
+
+   struct vk_sync *sync;
+
+   struct anv_memcpy_state memcpy_state;
+};
+
+void anv_device_utrace_init(struct anv_device *device);
+void anv_device_utrace_finish(struct anv_device *device);
+VkResult
+anv_device_utrace_flush_cmd_buffers(struct anv_queue *queue,
+                                    uint32_t cmd_buffer_count,
+                                    struct anv_cmd_buffer **cmd_buffers,
+                                    struct anv_utrace_flush_copy **out_flush_data);
+
 
 #define ANV_FROM_HANDLE(__anv_type, __name, __handle) \
    VK_FROM_HANDLE(__anv_type, __name, __handle)

src/intel/vulkan/anv_tracepoints.py
@@ -1,0 +1,163 @@
+#
+# Copyright © 2021 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice (including the next
+# paragraph) shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+import argparse
+import sys
+
+#
+# Tracepoint definitions:
+#
+def define_tracepoints(args):
+    from u_trace import Header, HeaderScope
+    from u_trace import ForwardDecl
+    from u_trace import Tracepoint
+    from u_trace import TracepointArg as Arg
+    from u_trace import TracepointArgStruct as ArgStruct
+
+    Header('anv_private.h', scope=HeaderScope.SOURCE)
+    Header('blorp/blorp_priv.h', scope=HeaderScope.HEADER)
+
+    def begin_end_tp(name, tp_args=[], tp_struct=None, end_pipelined=True):
+        Tracepoint('begin_{0}'.format(name))
+        Tracepoint('end_{0}'.format(name),
+                   args=tp_args,
+                   tp_struct=tp_struct,
+                   end_of_pipe=end_pipelined)
+
+
+    begin_end_tp('cmd_buffer',
+                 tp_args=[ArgStruct(type='uint8_t', var='level'),],
+                 tp_struct=[Arg(type='uint8_t', name='level', var='level', c_format='%hhu'),],
+                 end_pipelined=False)
+
+    begin_end_tp('render_pass',
+                 tp_args=[ArgStruct(type='uint16_t', var='width'),
+                          ArgStruct(type='uint16_t', var='height'),
+                          ArgStruct(type='uint8_t', var='att_count'),
+                          ArgStruct(type='uint8_t', var='msaa'),
+                          ArgStruct(type='uint32_t', var='subpass_count'),],
+                 tp_struct=[Arg(type='uint16_t', name='width', var='width', c_format='%hu'),
+                            Arg(type='uint16_t', name='height', var='height', c_format='%hu'),
+                            Arg(type='uint8_t', name='att_count', var='att_count', c_format='%hhu'),
+                            Arg(type='uint8_t', name='msaa', var='msaa', c_format='%hhu'),
+                            Arg(type='uint32_t', name='subpass_count', var='subpass_count', c_format='%ou'),])
+
+    begin_end_tp('blorp',
+                 tp_args=[ArgStruct(type='uint32_t', var='width'),
+                          ArgStruct(type='uint32_t', var='height'),
+                          ArgStruct(type='enum isl_aux_op', var='hiz_op'),
+                          ArgStruct(type='enum isl_aux_op', var='fast_clear_op'),
+                          ArgStruct(type='enum blorp_shader_type', var='shader_type'),
+                          ArgStruct(type='enum blorp_shader_pipeline', var='shader_pipe'),],
+                 tp_struct=[Arg(type='uint32_t', name='width', var='width', c_format='%u'),
+                        Arg(type='uint32_t', name='height', var='height', c_format='%u'),
+                            Arg(type='enum isl_aux_op', name='hiz_op', var='hiz_op', c_format='%s', to_prim_type='isl_aux_op_to_name({})'),
+                            Arg(type='enum isl_aux_op', name='fast_clear_op', var='fast_clear_op', c_format='%s', to_prim_type='isl_aux_op_to_name({})'),
+                            Arg(type='enum blorp_shader_type', name='type', var='shader_type', c_format='%s', to_prim_type='blorp_shader_type_to_name({})'),
+                            Arg(type='enum blorp_shader_pipeline', name='pipe', var='shader_pipe', c_format='%s', to_prim_type='blorp_shader_pipeline_to_name({})'),])
+
+    begin_end_tp('draw',
+                 tp_args=[ArgStruct(type='uint32_t', var='count'),],
+                 tp_struct=[Arg(type='uint32_t', name='count', var='count', c_format='%u'),])
+    begin_end_tp('draw_multi',
+                 tp_args=[ArgStruct(type='uint32_t', var='count'),],
+                 tp_struct=[Arg(type='uint32_t', name='count', var='count', c_format='%u'),])
+    begin_end_tp('draw_indexed',
+                 tp_args=[ArgStruct(type='uint32_t', var='count'),],
+                 tp_struct=[Arg(type='uint32_t', name='count', var='count', c_format='%u'),])
+    begin_end_tp('draw_indexed_multi',
+                 tp_args=[ArgStruct(type='uint32_t', var='count'),],
+                 tp_struct=[Arg(type='uint32_t', name='count', var='count', c_format='%u'),])
+    begin_end_tp('draw_indirect_byte_count',
+                 tp_args=[ArgStruct(type='uint32_t', var='instance_count'),],
+                 tp_struct=[Arg(type='uint32_t', name='instance_count', var='instance_count', c_format='%u'),])
+    begin_end_tp('draw_indirect',
+                 tp_args=[ArgStruct(type='uint32_t', var='draw_count'),],
+                 tp_struct=[Arg(type='uint32_t', name='draw_count', var='draw_count', c_format='%u'),])
+    begin_end_tp('draw_indexed_indirect',
+                 tp_args=[ArgStruct(type='uint32_t', var='draw_count'),],
+                 tp_struct=[Arg(type='uint32_t', name='draw_count', var='draw_count', c_format='%u'),])
+    begin_end_tp('draw_indirect_count',
+                 tp_args=[ArgStruct(type='uint32_t', var='max_draw_count'),],
+                 tp_struct=[Arg(type='uint32_t', name='max_draw_count', var='max_draw_count', c_format='%u'),])
+    begin_end_tp('draw_indexed_indirect_count',
+                 tp_args=[ArgStruct(type='uint32_t', var='max_draw_count'),],
+                 tp_struct=[Arg(type='uint32_t', name='max_draw_count', var='max_draw_count', c_format='%u'),])
+
+    begin_end_tp('compute',
+                 tp_args=[ArgStruct(type='uint32_t', var='group_x'),
+                          ArgStruct(type='uint32_t', var='group_y'),
+                          ArgStruct(type='uint32_t', var='group_z'),],
+                 tp_struct=[Arg(type='uint32_t', name='group_x', var='group_x', c_format='%u'),
+                            Arg(type='uint32_t', name='group_y', var='group_y', c_format='%u'),
+                            Arg(type='uint32_t', name='group_z', var='group_z', c_format='%u'),])
+
+    def stall_args(args):
+        fmt = ''
+        exprs = []
+        for a in args:
+            fmt += '%s'
+            exprs.append('(__entry->flags & ANV_PIPE_{0}_BIT) ? "+{1}" : ""'.format(a[0], a[1]))
+        fmt = [fmt]
+        fmt += exprs
+        return fmt
+
+    Tracepoint('stall',
+               args=[ArgStruct(type='uint32_t', var='flags'),],
+               tp_struct=[Arg(type='uint32_t', name='flags', var='flags', c_format='0x%x'),],
+               tp_print=stall_args([['DEPTH_CACHE_FLUSH', 'depth_flush'],
+                                    ['DATA_CACHE_FLUSH', 'dc_flush'],
+                                    ['HDC_PIPELINE_FLUSH', 'hdc_flush'],
+                                    ['RENDER_TARGET_CACHE_FLUSH', 'rt_flush'],
+                                    ['TILE_CACHE_FLUSH', 'tile_flush'],
+                                    ['STATE_CACHE_INVALIDATE', 'state_inval'],
+                                    ['CONSTANT_CACHE_INVALIDATE', 'const_inval'],
+                                    ['VF_CACHE_INVALIDATE', 'vf_inval'],
+                                    ['TEXTURE_CACHE_INVALIDATE', 'tex_inval'],
+                                    ['INSTRUCTION_CACHE_INVALIDATE', 'ic_inval'],
+                                    ['STALL_AT_SCOREBOARD', 'pb_stall'],
+                                    ['DEPTH_STALL', 'depth_stall'],
+                                    ['CS_STALL', 'cs_stall'],
+                                    ]))
+
+
+
+def generate_code(args):
+    from u_trace import utrace_generate
+
+    utrace_generate(cpath=args.utrace_src, hpath=args.utrace_hdr, ctx_param='struct anv_device *dev')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--import-path', required=True)
+    parser.add_argument('--utrace-src', required=True)
+    parser.add_argument('--utrace-hdr', required=True)
+    args = parser.parse_args()
+    sys.path.insert(0, args.import_path)
+    define_tracepoints(args)
+    generate_code(args)
+
+
+if __name__ == '__main__':
+    main()

commit f328882c35afbc7bff2218817f8a2e92bb8b7435

anv: fix clflush usage on utrace copy batch
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: be2c3a8f4eee ("anv: implement u_trace support")
Reviewed-by: Marcin Ślusarz <marcin.slusarz@intel.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15780>

src/intel/vulkan/anv_batch_chain.c
@@ -1998,11 +1998,8 @@ setup_utrace_execbuf(struct anv_execbuf *execbuf, struct anv_queue *queue,
       flush->batch_bo->exec_obj_index = last_idx;
    }
 
-   if (!device->info.has_llc) {
-      __builtin_ia32_mfence();
-      for (uint32_t i = 0; i < flush->batch_bo->size; i += CACHELINE_SIZE)
-         __builtin_ia32_clflush(flush->batch_bo->map);
-   }
+   if (!device->info.has_llc)
+      intel_flush_range(flush->batch_bo->map, flush->batch_bo->size);
 
    execbuf->execbuf = (struct drm_i915_gem_execbuffer2) {
       .buffers_ptr = (uintptr_t) execbuf->objects,