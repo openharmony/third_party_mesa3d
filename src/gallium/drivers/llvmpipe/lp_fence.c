openharmony/third_party_mesa3d

commit 373166c5966dde0830c9e736182ba36860ea5838

llvmpipe/fence: make the fence id counter atomic
Multiple threads can race on the function static variable

Signed-off-by: Karol Herbst <kherbst@redhat.com>
Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16348>

src/gallium/drivers/llvmpipe/lp_fence.c
@@ -44,7 +44,7 @@
 struct lp_fence *
 lp_fence_create(unsigned rank)
 {
-   static int fence_id;
+   static _Atomic int fence_id;
    struct lp_fence *fence = CALLOC_STRUCT(lp_fence);
 
    if (!fence)

commit 95494ab1af6e2eed6e69e90f9b292083bef4c179

llvmpipe: Do not use _Atomic keyword that doesn't support by MSVC
Fixes: 373166c5966 ("llvmpipe/fence: make the fence id counter atomic")

Fixes:
```
../mesa/src/gallium/drivers/llvmpipe/lp_fence.c
../mesa/src/gallium/drivers/llvmpipe/lp_fence.c(47): error C2143: syntax error: missing ';' before 'type'
```

fence_id initialized to 0

Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
Erik Faye-Lund <erik.faye-lund@collabora.com>

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16373>

src/gallium/drivers/llvmpipe/lp_fence.c
@@ -44,7 +44,7 @@
 struct lp_fence *
 lp_fence_create(unsigned rank)
 {
-   static _Atomic int fence_id;
+   static unsigned fence_id = 0;
    struct lp_fence *fence = CALLOC_STRUCT(lp_fence);
 
    if (!fence)
@@ -55,7 +55,7 @@ lp_fence_create(unsigned rank)
    (void) mtx_init(&fence->mutex, mtx_plain);
    cnd_init(&fence->signalled);
 
-   fence->id = fence_id++;
+   fence->id = p_atomic_inc_return(&fence_id) - 1;
    fence->rank = rank;
 
    if (LP_DEBUG & DEBUG_FENCE)