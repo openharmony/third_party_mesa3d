openharmony/third_party_mesa3d

commit a28c75d231adec8c454f91a94d10bc4b26c73044

radv: Implement task shader intrinsics in the ABI.
Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14929>

src/amd/vulkan/radv_nir_lower_abi.c
@@ -161,6 +161,21 @@ lower_abi_instr(nir_builder *b, nir_instr *instr, void *state)
    case nir_intrinsic_load_viewport_y_offset:
       return ac_nir_load_arg(b, &s->args->ac, s->args->ngg_viewport_translate[1]);
 
+   case nir_intrinsic_load_ring_task_draw_amd:
+      return load_ring(b, RING_TS_DRAW, s);
+
+   case nir_intrinsic_load_ring_task_payload_amd:
+      return load_ring(b, RING_TS_PAYLOAD, s);
+
+   case nir_intrinsic_load_task_ring_entry_amd:
+      return ac_nir_load_arg(b, &s->args->ac, s->args->ac.task_ring_entry);
+
+   case nir_intrinsic_load_task_ib_addr:
+      return ac_nir_load_arg(b, &s->args->ac, s->args->task_ib_addr);
+
+   case nir_intrinsic_load_task_ib_stride:
+      return ac_nir_load_arg(b, &s->args->ac, s->args->task_ib_stride);
+
    default:
       unreachable("invalid NIR RADV ABI intrinsic.");
    }
@@ -197,7 +212,12 @@ filter_abi_instr(const nir_instr *instr,
           intrin->intrinsic == nir_intrinsic_load_viewport_x_scale ||
           intrin->intrinsic == nir_intrinsic_load_viewport_x_offset ||
           intrin->intrinsic == nir_intrinsic_load_viewport_y_scale ||
-          intrin->intrinsic == nir_intrinsic_load_viewport_y_offset;
+          intrin->intrinsic == nir_intrinsic_load_viewport_y_offset ||
+          intrin->intrinsic == nir_intrinsic_load_ring_task_draw_amd ||
+          intrin->intrinsic == nir_intrinsic_load_ring_task_payload_amd ||
+          intrin->intrinsic == nir_intrinsic_load_task_ring_entry_amd ||
+          intrin->intrinsic == nir_intrinsic_load_task_ib_addr ||
+          intrin->intrinsic == nir_intrinsic_load_task_ib_stride;
 }
 
 void

commit 7c1c06f9d45085447a14ede1d76da01b83c698d2

radv: Fix loading task shader ring buffer addresses.
I forgot to use task_ring_offsets instead of ring_offsets
when I ported this code to the new ABI.

Fixes: a28c75d231adec8c454f91a94d10bc4b26c73044
Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16500>

src/amd/vulkan/radv_nir_lower_abi.c
@@ -39,7 +39,12 @@ typedef struct {
 static nir_ssa_def *
 load_ring(nir_builder *b, unsigned ring, lower_abi_state *s)
 {
-   nir_ssa_def *ring_offsets = ac_nir_load_arg(b, &s->args->ac, s->args->ring_offsets);
+   struct ac_arg arg =
+      b->shader->info.stage == MESA_SHADER_TASK ?
+      s->args->task_ring_offsets :
+      s->args->ring_offsets;
+
+   nir_ssa_def *ring_offsets = ac_nir_load_arg(b, &s->args->ac, arg);
    ring_offsets = nir_pack_64_2x32_split(b, nir_channel(b, ring_offsets, 0), nir_channel(b, ring_offsets, 1));
    return nir_load_smem_amd(b, 4, ring_offsets, nir_imm_int(b, ring * 16u), .align_mul = 4u);
 }