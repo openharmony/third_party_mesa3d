openharmony/third_party_mesa3d

commit 5cedc0861365b72b298ba567e407dc52404dd2fc

gallium/vl: respect the video codecs configure in meson
Acked-by: Christian König <christian.koenig@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15258>

src/gallium/auxiliary/vl/vl_codec.c
@@ -32,5 +32,25 @@ bool vl_codec_supported(struct pipe_screen *screen,
                         enum pipe_video_profile profile,
                         bool encode)
 {
+   if (profile == PIPE_VIDEO_PROFILE_VC1_SIMPLE ||
+       profile == PIPE_VIDEO_PROFILE_VC1_MAIN ||
+       profile == PIPE_VIDEO_PROFILE_VC1_ADVANCED) {
+      return VIDEO_CODEC_VC1DEC ? true : false;
+   }
+   if (profile >= PIPE_VIDEO_PROFILE_MPEG4_AVC_BASELINE &&
+       profile <= PIPE_VIDEO_PROFILE_MPEG4_AVC_HIGH444) {
+      if (encode)
+         return VIDEO_CODEC_H264ENC ? true : false;
+      else
+         return VIDEO_CODEC_H264DEC ? true : false;
+   }
+   if (profile >= PIPE_VIDEO_PROFILE_HEVC_MAIN &&
+       profile <= PIPE_VIDEO_PROFILE_HEVC_MAIN_444) {
+      if (encode)
+         return VIDEO_CODEC_H265ENC ? true : false;
+      else
+         return VIDEO_CODEC_H265DEC ? true : false;
+   }
+
    return screen->get_video_param(screen, profile, encode ? PIPE_VIDEO_ENTRYPOINT_ENCODE : PIPE_VIDEO_ENTRYPOINT_BITSTREAM, PIPE_VIDEO_CAP_SUPPORTED);
 }

commit 93b0888ce12377dbad0347326b082206c6a61c74

vl: fix codec checks to disable properly
This was wrong and enabled codecs where they shouldn't have been.

Fixes: 5cedc0861365 ("gallium/vl: respect the video codecs configure in meson")
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16542>

src/gallium/auxiliary/vl/vl_codec.c
@@ -35,21 +35,26 @@ bool vl_codec_supported(struct pipe_screen *screen,
    if (profile == PIPE_VIDEO_PROFILE_VC1_SIMPLE ||
        profile == PIPE_VIDEO_PROFILE_VC1_MAIN ||
        profile == PIPE_VIDEO_PROFILE_VC1_ADVANCED) {
-      return VIDEO_CODEC_VC1DEC ? true : false;
+      if (!VIDEO_CODEC_VC1DEC)
+         return false;
    }
    if (profile >= PIPE_VIDEO_PROFILE_MPEG4_AVC_BASELINE &&
        profile <= PIPE_VIDEO_PROFILE_MPEG4_AVC_HIGH444) {
-      if (encode)
-         return VIDEO_CODEC_H264ENC ? true : false;
-      else
-         return VIDEO_CODEC_H264DEC ? true : false;
+      if (encode) {
+         if (!VIDEO_CODEC_H264ENC)
+            return false;
+      } else if (!VIDEO_CODEC_H264DEC) {
+         return false;
+      }
    }
    if (profile >= PIPE_VIDEO_PROFILE_HEVC_MAIN &&
        profile <= PIPE_VIDEO_PROFILE_HEVC_MAIN_444) {
-      if (encode)
-         return VIDEO_CODEC_H265ENC ? true : false;
-      else
-         return VIDEO_CODEC_H265DEC ? true : false;
+      if (encode) {
+         if (!VIDEO_CODEC_H265ENC)
+            return false;
+      } else if (!VIDEO_CODEC_H265DEC) {
+         return false;
+      }
    }
 
    return screen->get_video_param(screen, profile, encode ? PIPE_VIDEO_ENTRYPOINT_ENCODE : PIPE_VIDEO_ENTRYPOINT_BITSTREAM, PIPE_VIDEO_CAP_SUPPORTED);