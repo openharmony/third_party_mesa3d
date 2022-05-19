openharmony/third_party_mesa3d

commit 9424c7598a1c9a9c133244103677577329ff55c5

vulkan/pipeline_cache: Implement deserialize for raw objects
When caching NIR, it's cached as a raw object because we cache the
serialized NIR.  When it's then loaded from the disk cache later, we
fail to deserialize it because raw objects are a special case.  There
are two callers of vk_pipeline_cache_object_deserialize(), one of which
has a special case for raw objects and the other is called only when
we've checked that it isn't a raw object.  The special cases are
pointless; raw objects should deserialize themselves.

Fixes: 4a47619c261e ("vulkan: Add a common VkPipelineCache implementation")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16281>

src/vulkan/runtime/vk_pipeline_cache.c
@@ -44,6 +44,11 @@ struct raw_data_object {
    size_t data_size;
 };
 
+static struct raw_data_object *
+raw_data_object_create(struct vk_device *device,
+                       const void *key_data, size_t key_size,
+                       const void *data, size_t data_size);
+
 static bool
 raw_data_object_serialize(struct vk_pipeline_cache_object *object,
                           struct blob *blob)
@@ -54,6 +59,27 @@ raw_data_object_serialize(struct vk_pipeline_cache_object *object,
    blob_write_bytes(blob, data_obj->data, data_obj->data_size);
 
    return true;
+}
+
+static struct vk_pipeline_cache_object *
+raw_data_object_deserialize(struct vk_device *device,
+                            const void *key_data,
+                            size_t key_size,
+                            struct blob_reader *blob)
+{
+   /* We consume the entire blob_reader.  Each call to ops->deserialize()
+    * happens with a brand new blob reader for error checking anyway so we
+    * can assume the blob consumes the entire reader and we don't need to
+    * serialize the data size separately.
+    */
+   assert(blob->current < blob->end);
+   size_t data_size = blob->end - blob->current;
+   const void *data = blob_read_bytes(blob, data_size);
+
+   struct raw_data_object *data_obj =
+      raw_data_object_create(device, key_data, key_size, data, data_size);
+
+   return data_obj ? &data_obj->base : NULL;
 }
 
 static void
@@ -67,6 +93,7 @@ raw_data_object_destroy(struct vk_pipeline_cache_object *object)
 
 static const struct vk_pipeline_cache_object_ops raw_data_object_ops = {
    .serialize = raw_data_object_serialize,
+   .deserialize = raw_data_object_deserialize,
    .destroy = raw_data_object_destroy,
 };
 
@@ -527,18 +554,10 @@ vk_pipeline_cache_load(struct vk_pipeline_cache *cache,
       const struct vk_pipeline_cache_object_ops *ops =
          find_ops_for_type(cache->base.device->physical, type);
 
-      struct vk_pipeline_cache_object *object;
-      if (ops != NULL) {
-         object = vk_pipeline_cache_object_deserialize(cache,
-                                                       key_data, key_size,
-                                                       data, data_size, ops);
-      } else {
-         struct raw_data_object *data_obj =
-            raw_data_object_create(cache->base.device,
-                                   key_data, key_size,
-                                   data, data_size);
-         object = data_obj != NULL ? &data_obj->base : NULL;
-      }
+      struct vk_pipeline_cache_object *object =
+         vk_pipeline_cache_object_deserialize(cache,
+                                              key_data, key_size,
+                                              data, data_size, ops);
       if (object == NULL)
          continue;
 

commit 9ec7ab2a37f25bc6452db2359c2fab904b19b6eb

vulkan: Fall back to raw data objects when deserializing if ops == NULL
This can happen if an object is serialized whose object type isn't in
the pipeline cache import ops.  In this case, we generate a raw data
object and plan to turn it into the right object type later.

Fixes: 9424c7598a1c ("vulkan/pipeline_cache: Implement deserialize for raw objects")
Reviewed-by: Connor Abbott <cwabbott0@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16584>

src/vulkan/runtime/vk_pipeline_cache.c
@@ -271,6 +271,9 @@ vk_pipeline_cache_object_deserialize(struct vk_pipeline_cache *cache,
                                      const void *data, size_t data_size,
                                      const struct vk_pipeline_cache_object_ops *ops)
 {
+   if (ops == NULL)
+      ops = &raw_data_object_ops;
+
    if (unlikely(ops->deserialize == NULL)) {
       vk_logw(VK_LOG_OBJS(cache),
               "Pipeline cache object cannot be deserialized");