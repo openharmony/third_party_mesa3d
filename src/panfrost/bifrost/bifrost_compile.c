openharmony/third_party_mesa3d

commit b8efdd06fb833c88c164bf09fdcaf33e3189f70d

pan/bi: Don't read base for combined stores
`base` is meaningless for combined stores, so don't read it. This allows
us to remove the base index from the intrinsic and simplify the lowering
code generating the combined store intrinsic.

Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13714>

src/panfrost/bifrost/bifrost_compile.c
@@ -574,12 +574,17 @@ bi_emit_fragment_out(bi_builder *b, nir_intrinsic_instr *instr)
         bool emit_blend = writeout & (PAN_WRITEOUT_C);
         bool emit_zs = writeout & (PAN_WRITEOUT_Z | PAN_WRITEOUT_S);
 
-        const nir_variable *var =
-                nir_find_variable_with_driver_location(b->shader->nir,
-                                nir_var_shader_out, nir_intrinsic_base(instr));
-        assert(var);
+        const nir_variable *var = NULL;
+        unsigned loc = ~0;
 
-        unsigned loc = var->data.location;
+        if (!combined) {
+                var = nir_find_variable_with_driver_location(b->shader->nir,
+                                        nir_var_shader_out, nir_intrinsic_base(instr));
+                assert(var);
+
+                loc = var->data.location;
+        }
+
         bi_index src0 = bi_src_index(&instr->src[0]);
 
         /* By ISA convention, the coverage mask is stored in R60. The store
@@ -595,7 +600,7 @@ bi_emit_fragment_out(bi_builder *b, nir_intrinsic_instr *instr)
 
         /* Dual-source blending is implemented by putting the color in
          * registers r4-r7. */
-        if (var->data.index) {
+        if (var && var->data.index) {
                 unsigned count = nir_src_num_components(instr->src[0]);
 
                 for (unsigned i = 0; i < count; ++i)
@@ -643,9 +648,7 @@ bi_emit_fragment_out(bi_builder *b, nir_intrinsic_instr *instr)
         }
 
         if (emit_blend) {
-                assert(loc >= FRAG_RESULT_DATA0);
-
-                unsigned rt = (loc - FRAG_RESULT_DATA0);
+                unsigned rt = combined ? 0 : (loc - FRAG_RESULT_DATA0);
                 bi_index color = bi_src_index(&instr->src[0]);
 
                 /* Explicit copy since BLEND inputs are precoloured to R0-R3,

commit e35567b794e9651cf6759205a970a942be75098c

pan/bi: Read base for combined stores
Fixes depth/stencil writes with MRT.

Fixes: b8efdd06fb8 ("pan/bi: Don't read base for combined stores")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16685>

src/panfrost/bifrost/bifrost_compile.c
@@ -868,16 +868,11 @@ bi_emit_fragment_out(bi_builder *b, nir_intrinsic_instr *instr)
         bool emit_blend = writeout & (PAN_WRITEOUT_C);
         bool emit_zs = writeout & (PAN_WRITEOUT_Z | PAN_WRITEOUT_S);
 
-        unsigned loc = ~0;
+        const nir_variable *var =
+                nir_find_variable_with_driver_location(b->shader->nir,
+                                                       nir_var_shader_out, nir_intrinsic_base(instr));
 
-        if (!combined) {
-                const nir_variable *var =
-                        nir_find_variable_with_driver_location(b->shader->nir,
-                                        nir_var_shader_out, nir_intrinsic_base(instr));
-                assert(var);
-
-                loc = var->data.location;
-        }
+        unsigned loc = var ? var->data.location : 0;
 
         bi_index src0 = bi_src_index(&instr->src[0]);
 
@@ -929,7 +924,7 @@ bi_emit_fragment_out(bi_builder *b, nir_intrinsic_instr *instr)
         }
 
         if (emit_blend) {
-                unsigned rt = combined ? 0 : (loc - FRAG_RESULT_DATA0);
+                unsigned rt = loc ? (loc - FRAG_RESULT_DATA0) : 0;
                 bool dual = (writeout & PAN_WRITEOUT_2);
                 bi_index color = bi_src_index(&instr->src[0]);
                 bi_index color2 = dual ? bi_src_index(&instr->src[4]) : bi_null();