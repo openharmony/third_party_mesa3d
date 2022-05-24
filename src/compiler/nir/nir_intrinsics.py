openharmony/third_party_mesa3d

commit 1e707ef4e5ecd890db8b0056a6f7a886f0b8cb50

nir: Eliminate store_combined_output_pan BASE
It's meaningless for this intrinsic and is just adding noise to the
lowering pass.

Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13714>

src/compiler/nir/nir_intrinsics.py
@@ -1149,7 +1149,7 @@ intrinsic("shared_atomic_comp_swap_dxil", src_comp=[1, 1, 1], dest_comp=1)
 
 # src[] = { value }
 store("raw_output_pan", [], [])
-store("combined_output_pan", [1, 1, 1], [BASE, COMPONENT, SRC_TYPE])
+store("combined_output_pan", [1, 1, 1], [COMPONENT, SRC_TYPE])
 load("raw_output_pan", [1], [BASE], [CAN_ELIMINATE, CAN_REORDER])
 
 # Loads the sampler paramaters <min_lod, max_lod, lod_bias>

src/panfrost/util/pan_lower_writeout.c
@@ -115,7 +115,6 @@ pan_nir_lower_zs_store(nir_shader *nir)
 
                                 combined_store->num_components = intr->src[0].ssa->num_components;
 
-                                nir_intrinsic_set_base(combined_store, nir_intrinsic_base(intr));
                                 nir_intrinsic_set_src_type(combined_store, nir_intrinsic_src_type(intr));
 
                                 unsigned writeout = PAN_WRITEOUT_C;
@@ -167,12 +166,6 @@ pan_nir_lower_zs_store(nir_shader *nir)
 
                         combined_store->num_components = 4;
 
-                        unsigned base;
-                        if (z_store)
-                                base = nir_intrinsic_base(z_store);
-                        else
-                                base = nir_intrinsic_base(s_store);
-                        nir_intrinsic_set_base(combined_store, base);
                         nir_intrinsic_set_src_type(combined_store, nir_type_float32);
 
                         unsigned writeout = 0;

commit 745af6aea00db3ce89925b52048ded3a7e38d106

nir: Add store_combined_output_pan BASE back
It's meaningful for this intrinsic and so does not add noise to the
lowering pass.

(Although dual-source writes must be to RT 0, depth and stencil
writes, which store_combined_output_pan is also used for, can still be
done with MRT enabled.)

Fixes: 1e707ef4e5e ("nir: Eliminate store_combined_output_pan BASE")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16685>

src/compiler/nir/nir_intrinsics.py
@@ -1222,7 +1222,7 @@ intrinsic("shared_atomic_comp_swap_dxil", src_comp=[1, 1, 1], dest_comp=1)
 
 # src[] = { value }
 store("raw_output_pan", [], [])
-store("combined_output_pan", [1, 1, 1, 4], [COMPONENT, SRC_TYPE, DEST_TYPE])
+store("combined_output_pan", [1, 1, 1, 4], [BASE, COMPONENT, SRC_TYPE, DEST_TYPE])
 load("raw_output_pan", [1], [BASE], [CAN_ELIMINATE, CAN_REORDER])
 
 # Loads the sampler paramaters <min_lod, max_lod, lod_bias>

src/panfrost/util/pan_lower_writeout.c
@@ -55,6 +55,8 @@ pan_nir_emit_combined_store(nir_builder *b,
 
         intr->num_components = rt0_store ? rt0_store->src[0].ssa->num_components : 4;
 
+        if (rt0_store)
+                nir_intrinsic_set_base(intr, nir_intrinsic_base(rt0_store));
         nir_intrinsic_set_src_type(intr, pan_nir_rt_store_type(rt0_store));
         nir_intrinsic_set_dest_type(intr, pan_nir_rt_store_type(stores[2]));
         nir_intrinsic_set_component(intr, writeout);