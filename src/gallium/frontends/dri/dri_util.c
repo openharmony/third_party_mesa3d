openharmony/third_party_mesa3d

commit 096621d321e50cd555ac0a6d7a63fff9a4de66b3

dri2: Require a loader with working buffer invalidation
Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10808>

src/gallium/frontends/dri/dri_util.c
@@ -121,6 +121,9 @@ driCreateNewScreen2(int scrn, int fd,
     }
 
     setupLoaderExtensions(psp, extensions);
+    // dri2 drivers require working invalidate
+    if (fd != -1 && !psp->dri2.useInvalidate)
+       return NULL;
 
     psp->loaderPrivate = data;
 

src/gallium/frontends/dri/dri2.c
@@ -2477,7 +2477,6 @@ dri2_init_screen(__DRIscreen * sPriv)
 
    screen->can_share_buffer = true;
    screen->auto_fake_front = dri_with_format(sPriv);
-   screen->broken_invalidate = !sPriv->dri2.useInvalidate;
    screen->lookup_egl_image = dri2_lookup_egl_image;
 
    const __DRIimageLookupExtension *loader = sPriv->dri2.image;
@@ -2540,7 +2539,6 @@ dri_kms_init_screen(__DRIscreen * sPriv)
 
    screen->can_share_buffer = false;
    screen->auto_fake_front = dri_with_format(sPriv);
-   screen->broken_invalidate = !sPriv->dri2.useInvalidate;
    screen->lookup_egl_image = dri2_lookup_egl_image;
 
    const __DRIimageLookupExtension *loader = sPriv->dri2.image;

src/gallium/frontends/dri/dri_drawable.c
@@ -75,7 +75,7 @@ dri_st_framebuffer_validate(struct st_context_iface *stctx,
       lastStamp = drawable->dPriv->lastStamp;
       new_stamp = (drawable->texture_stamp != lastStamp);
 
-      if (new_stamp || new_mask || screen->broken_invalidate) {
+      if (new_stamp || new_mask) {
          if (new_stamp && drawable->update_drawable_info)
             drawable->update_drawable_info(drawable);
 

src/gallium/frontends/dri/dri_screen.c
@@ -763,14 +763,7 @@ static int
 dri_get_param(struct st_manager *smapi,
               enum st_manager_param param)
 {
-   struct dri_screen *screen = (struct dri_screen *)smapi;
-
-   switch(param) {
-   case ST_MANAGER_BROKEN_INVALIDATE:
-      return screen->broken_invalidate;
-   default:
-      return 0;
-   }
+   return 0;
 }
 
 void

src/gallium/frontends/dri/dri_screen.h
@@ -53,9 +53,6 @@ struct dri_screen
    struct st_manager base;
    struct st_api *st_api;
 
-   /* on old libGL's invalidate doesn't get called as it should */
-   boolean broken_invalidate;
-
    /* dri */
    __DRIscreen *sPriv;
    boolean throttle;

commit 611fa929db8ff5fae2e99e9527e8e1c9475abd56

dri2: Fix memory leak.
Fix defect reported by Coverity Scan.

Resource leak (RESOURCE_LEAK)
leaked_storage: Variable psp going out of scope leaks the storage it points to.

Fixes: 096621d321e ("dri2: Require a loader with working buffer invalidation")
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16680>

src/gallium/frontends/dri/dri_util.c
@@ -122,8 +122,10 @@ driCreateNewScreen2(int scrn, int fd,
 
     setupLoaderExtensions(psp, extensions);
     // dri2 drivers require working invalidate
-    if (fd != -1 && !psp->dri2.useInvalidate)
+    if (fd != -1 && !psp->dri2.useInvalidate) {
+       free(psp);
        return NULL;
+    }
 
     psp->loaderPrivate = data;
 