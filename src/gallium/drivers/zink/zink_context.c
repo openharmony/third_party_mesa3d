openharmony/third_party_mesa3d

commit 970c12a55d9cf9cb5165cca54168662515e2c431

zink: add error logging for SRGB framebuffer without KHR_swapchain_mutable_format
this is going to explode, so at least print an error explaining why

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15808>

src/gallium/drivers/zink/zink_context.c
@@ -2766,6 +2766,12 @@ zink_set_framebuffer_state(struct pipe_context *pctx,
             assert(!ctx->needs_present || ctx->needs_present == res);
             ctx->needs_present = res;
          }
+         if (res->obj->dt) {
+            /* #6274 */
+            if (!zink_screen(ctx->base.screen)->info.have_KHR_swapchain_mutable_format &&
+                surf->format != res->base.b.format)
+               mesa_loge("zink: SRGB framebuffer unsupported without KHR_swapchain_mutable_format");
+         }
          res->fb_binds++;
          ctx->gfx_pipeline_state.void_alpha_attachments |= util_format_has_alpha1(surf->format) ? BITFIELD_BIT(i) : 0;
       }

commit bd4b88fbce172cccc5a0dc0b2b9e7bb1a3a22575

zink: only warn once for SRGB fb without KHR_swapchain_mutable_format
Fixes: 970c12a55d9 ("zink: add error logging for SRGB framebuffer without KHR_swapchain_mutable_format")

Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16642>

src/gallium/drivers/zink/zink_context.c
@@ -2610,8 +2610,13 @@ zink_set_framebuffer_state(struct pipe_context *pctx,
          if (res->obj->dt) {
             /* #6274 */
             if (!zink_screen(ctx->base.screen)->info.have_KHR_swapchain_mutable_format &&
-                psurf->format != res->base.b.format)
-               mesa_loge("zink: SRGB framebuffer unsupported without KHR_swapchain_mutable_format");
+                psurf->format != res->base.b.format) {
+               static bool warned = false;
+               if (!warned) {
+                  mesa_loge("zink: SRGB framebuffer unsupported without KHR_swapchain_mutable_format");
+                  warned = true;
+               }
+            }
          }
          res->fb_binds++;
          ctx->gfx_pipeline_state.void_alpha_attachments |= util_format_has_alpha1(psurf->format) ? BITFIELD_BIT(i) : 0;