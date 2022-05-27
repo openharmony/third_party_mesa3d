openharmony/third_party_mesa3d

commit 14209e304a241130eaadff41dc2a69ce40fd387a

d3d12: Add video encode implementation of pipe_video_codec
Acked-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16286>

src/gallium/drivers/d3d12/d3d12_video_encoder_bitstream.cpp
@@ -1,0 +1,276 @@
+/*
+ * Copyright © Microsoft Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <climits>
+#include "d3d12_video_encoder_bitstream.h"
+
+d3d12_video_encoder_bitstream::d3d12_video_encoder_bitstream()
+{
+   m_pBitsBuffer       = nullptr;
+   m_uiBitsBufferSize  = 0;
+   m_iBitsToGo         = 32;
+   m_uintEncBuffer     = 0;
+   m_bExternalBuffer   = false;
+   m_bBufferOverflow   = false;
+   m_bPreventStartCode = false;
+   m_bAllowReallocate  = false;
+}
+
+d3d12_video_encoder_bitstream::~d3d12_video_encoder_bitstream()
+{
+   if (!m_bExternalBuffer) {
+      if (m_pBitsBuffer) {
+         delete[](m_pBitsBuffer);
+         (m_pBitsBuffer) = NULL;
+      }
+   }
+}
+
+int32_t
+d3d12_video_encoder_bitstream::get_exp_golomb0_code_len(uint32_t uiVal)
+{
+   int32_t iLen = 0;
+   uiVal++;
+
+   if (uiVal >= 0x10000) {
+      uiVal >>= 16;
+      iLen += 16;
+   }
+   if (uiVal >= 0x100) {
+      uiVal >>= 8;
+      iLen += 8;
+   }
+
+   assert(uiVal < 256);
+
+   return iLen + m_iLog_2_N[uiVal];
+}
+
+void
+d3d12_video_encoder_bitstream::exp_Golomb_ue(uint32_t uiVal)
+{
+   if (uiVal != UINT32_MAX) {
+      int32_t iLen = get_exp_golomb0_code_len(uiVal);
+      put_bits((iLen << 1) + 1, uiVal + 1);
+   } else {
+      put_bits(32, 0);
+      put_bits(1, 1);
+      put_bits(32, 1);
+   }
+}
+
+void
+d3d12_video_encoder_bitstream::exp_Golomb_se(int32_t iVal)
+{
+   if (iVal > 0) {
+      exp_Golomb_ue((iVal << 1) - 1);
+   } else {
+      exp_Golomb_ue(((-iVal) << 1) - (iVal == INT_MIN));
+   }
+}
+
+void
+d3d12_video_encoder_bitstream::setup_bitstream(uint32_t uiInitBufferSize, uint8_t *pBuffer)
+{
+   m_pBitsBuffer      = pBuffer;
+   m_uiBitsBufferSize = uiInitBufferSize;
+   m_uiOffset         = 0;
+   memset(m_pBitsBuffer, 0, m_uiBitsBufferSize);
+   m_bExternalBuffer  = true;
+   m_bAllowReallocate = false;
+}
+
+bool
+d3d12_video_encoder_bitstream::create_bitstream(uint32_t uiInitBufferSize)
+{
+   assert((uiInitBufferSize) >= 4 && !(uiInitBufferSize & 3));
+
+   m_pBitsBuffer = (uint8_t *) new uint8_t[uiInitBufferSize];
+
+   if (nullptr == m_pBitsBuffer) {
+      return false;
+   }
+
+   m_uiBitsBufferSize = uiInitBufferSize;
+   m_uiOffset         = 0;
+   memset(m_pBitsBuffer, 0, m_uiBitsBufferSize);
+   m_bExternalBuffer = false;
+
+   return true;
+}
+
+bool
+d3d12_video_encoder_bitstream::reallocate_buffer()
+{
+   uint32_t uiBufferSize = m_uiBitsBufferSize * 3 / 2;
+   uint8_t *pNewBuffer   = (uint8_t *) new uint8_t[uiBufferSize];
+
+   if (nullptr == pNewBuffer) {
+      return false;
+   }
+
+   memcpy(pNewBuffer, m_pBitsBuffer, m_uiOffset * sizeof(uint8_t));
+   if (m_pBitsBuffer) {
+      delete[](m_pBitsBuffer);
+      (m_pBitsBuffer) = NULL;
+   }
+   m_pBitsBuffer      = pNewBuffer;
+   m_uiBitsBufferSize = uiBufferSize;
+   return true;
+}
+
+bool
+d3d12_video_encoder_bitstream::verify_buffer(uint32_t uiBytesToWrite)
+{
+   if (!m_bBufferOverflow) {
+      if (m_uiOffset + uiBytesToWrite > m_uiBitsBufferSize) {
+         if (!m_bAllowReallocate || !reallocate_buffer()) {
+            m_bBufferOverflow = true;
+            return false;
+         }
+      }
+
+      return true;
+   }
+
+   return false;
+}
+
+void
+d3d12_video_encoder_bitstream::inc_current_offset(int32_t dwOffset)
+{
+   assert(32 == m_iBitsToGo && m_uiOffset < m_uiBitsBufferSize);
+   m_uiOffset += dwOffset;
+}
+
+void
+d3d12_video_encoder_bitstream::get_current_buffer_position_and_size(uint8_t **ppCurrBufPos, int32_t *pdwLeftBufSize)
+{
+   assert(32 == m_iBitsToGo && m_uiOffset < m_uiBitsBufferSize);
+   *ppCurrBufPos   = m_pBitsBuffer + m_uiOffset;
+   *pdwLeftBufSize = m_uiBitsBufferSize - m_uiOffset;
+}
+
+void
+d3d12_video_encoder_bitstream::attach(uint8_t *pBitsBuffer, uint32_t uiBufferSize)
+{
+   m_pBitsBuffer      = pBitsBuffer;
+   m_uiBitsBufferSize = uiBufferSize;
+   m_bExternalBuffer  = true;
+   m_bBufferOverflow  = false;
+   m_bAllowReallocate = false;
+
+   clear();
+}
+
+void
+d3d12_video_encoder_bitstream::write_byte_start_code_prevention(uint8_t u8Val)
+{
+   int32_t  iOffset = m_uiOffset;
+   uint8_t *pBuffer = m_pBitsBuffer + iOffset;
+
+   if (m_bPreventStartCode && iOffset > 1) {
+      if (((u8Val & 0xfc) | pBuffer[-2] | pBuffer[-1]) == 0) {
+         *pBuffer++ = 3;
+         iOffset++;
+      }
+   }
+
+   *pBuffer = u8Val;
+   iOffset++;
+
+   m_uiOffset = iOffset;
+}
+
+#define WRITE_BYTE(byte) write_byte_start_code_prevention(byte)
+
+void
+d3d12_video_encoder_bitstream::put_bits(int32_t uiBitsCount, uint32_t iBitsVal)
+{
+   assert(uiBitsCount <= 32);
+
+   if (uiBitsCount < m_iBitsToGo) {
+      m_uintEncBuffer |= (iBitsVal << (m_iBitsToGo - uiBitsCount));
+      m_iBitsToGo -= uiBitsCount;
+   } else if (verify_buffer(4)) {
+      int32_t iLeftOverBits = uiBitsCount - m_iBitsToGo;
+      m_uintEncBuffer |= (iBitsVal >> iLeftOverBits);
+
+      uint8_t *temp = (uint8_t *) (&m_uintEncBuffer);
+      WRITE_BYTE(*(temp + 3));
+      WRITE_BYTE(*(temp + 2));
+      WRITE_BYTE(*(temp + 1));
+      WRITE_BYTE(*temp);
+
+      m_uintEncBuffer = 0;
+      m_iBitsToGo     = 32 - iLeftOverBits;
+
+      if (iLeftOverBits > 0) {
+         m_uintEncBuffer = (iBitsVal << (32 - iLeftOverBits));
+      }
+   }
+}
+
+void
+d3d12_video_encoder_bitstream::flush()
+{
+   bool isAligned = is_byte_aligned();   // causes side-effects in object state, don't put inside assert()
+   assert(isAligned);
+
+   uint32_t temp = (uint32_t)(32 - m_iBitsToGo);
+
+   if (!verify_buffer(temp >> 3)) {
+      return;
+   }
+
+   while (temp > 0) {
+      WRITE_BYTE((uint8_t)(m_uintEncBuffer >> 24));
+      m_uintEncBuffer <<= 8;
+      temp -= 8;
+   }
+
+   m_iBitsToGo     = 32;
+   m_uintEncBuffer = 0;
+}
+
+void
+d3d12_video_encoder_bitstream::append_byte_stream(d3d12_video_encoder_bitstream *pStream)
+{
+   bool isStreamAligned =
+      pStream->is_byte_aligned();   // causes side-effects in object state, don't put inside assert()
+   assert(isStreamAligned);
+   bool isThisAligned = is_byte_aligned();   // causes side-effects in object state, don't put inside assert()
+   assert(isThisAligned);
+   assert(m_iBitsToGo == 32);
+
+   uint8_t *pDst  = m_pBitsBuffer + m_uiOffset;
+   uint8_t *pSrc  = pStream->get_bitstream_buffer();
+   uint32_t uiLen = (uint32_t) pStream->get_byte_count();
+
+   if (!verify_buffer(uiLen)) {
+      return;
+   }
+
+   memcpy(pDst, pSrc, uiLen);
+   m_uiOffset += uiLen;
+}

src/gallium/drivers/d3d12/d3d12_video_enc.h
@@ -1,0 +1,321 @@
+/*
+ * Copyright © Microsoft Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef D3D12_VIDEO_ENC_H
+#define D3D12_VIDEO_ENC_H
+
+#include "d3d12_video_types.h"
+#include "d3d12_video_encoder_references_manager.h"
+#include "d3d12_video_dpb_storage_manager.h"
+#include "d3d12_video_encoder_bitstream_builder_h264.h"
+
+///
+/// Pipe video interface starts
+///
+
+/**
+ * creates a video encoder
+ */
+struct pipe_video_codec *
+d3d12_video_encoder_create_encoder(struct pipe_context *context, const struct pipe_video_codec *templ);
+
+/**
+ * destroy this video encoder
+ */
+void
+d3d12_video_encoder_destroy(struct pipe_video_codec *codec);
+
+/**
+ * start encoding of a new frame
+ */
+void
+d3d12_video_encoder_begin_frame(struct pipe_video_codec * codec,
+                                struct pipe_video_buffer *target,
+                                struct pipe_picture_desc *picture);
+
+/**
+ * encode to a bitstream
+ */
+void
+d3d12_video_encoder_encode_bitstream(struct pipe_video_codec * codec,
+                                     struct pipe_video_buffer *source,
+                                     struct pipe_resource *    destination,
+                                     void **                   feedback);
+
+/**
+ * get encoder feedback
+ */
+void
+d3d12_video_encoder_get_feedback(struct pipe_video_codec *codec, void *feedback, unsigned *size);
+
+/**
+ * end encoding of the current frame
+ */
+void
+d3d12_video_encoder_end_frame(struct pipe_video_codec * codec,
+                              struct pipe_video_buffer *target,
+                              struct pipe_picture_desc *picture);
+
+/**
+ * flush any outstanding command buffers to the hardware
+ * should be called before a video_buffer is acessed by the gallium frontend again
+ */
+void
+d3d12_video_encoder_flush(struct pipe_video_codec *codec);
+
+///
+/// Pipe video interface ends
+///
+
+enum d3d12_video_encoder_config_dirty_flags
+{
+   d3d12_video_encoder_config_dirty_flag_none                   = 0x0,
+   d3d12_video_encoder_config_dirty_flag_codec                  = 0x1,
+   d3d12_video_encoder_config_dirty_flag_profile                = 0x2,
+   d3d12_video_encoder_config_dirty_flag_level                  = 0x4,
+   d3d12_video_encoder_config_dirty_flag_codec_config           = 0x8,
+   d3d12_video_encoder_config_dirty_flag_input_format           = 0x10,
+   d3d12_video_encoder_config_dirty_flag_resolution             = 0x20,
+   d3d12_video_encoder_config_dirty_flag_rate_control           = 0x40,
+   d3d12_video_encoder_config_dirty_flag_slices                 = 0x80,
+   d3d12_video_encoder_config_dirty_flag_gop                    = 0x100,
+   d3d12_video_encoder_config_dirty_flag_motion_precision_limit = 0x200,
+};
+DEFINE_ENUM_FLAG_OPERATORS(d3d12_video_encoder_config_dirty_flags);
+
+///
+/// d3d12_video_encoder functions starts
+///
+
+struct d3d12_video_encoder
+{
+   struct pipe_video_codec base;
+   struct pipe_screen *    m_screen;
+   struct d3d12_screen *   m_pD3D12Screen;
+
+   ///
+   /// D3D12 objects and context info
+   ///
+
+   const uint m_NodeMask  = 0u;
+   const uint m_NodeIndex = 0u;
+
+   ComPtr<ID3D12Fence> m_spFence;
+   uint                m_fenceValue = 1u;
+
+   ComPtr<ID3D12VideoDevice3>            m_spD3D12VideoDevice;
+   ComPtr<ID3D12VideoEncoder>            m_spVideoEncoder;
+   ComPtr<ID3D12VideoEncoderHeap>        m_spVideoEncoderHeap;
+   ComPtr<ID3D12CommandQueue>            m_spEncodeCommandQueue;
+   ComPtr<ID3D12CommandAllocator>        m_spCommandAllocator;
+   ComPtr<ID3D12VideoEncodeCommandList2> m_spEncodeCommandList;
+   ComPtr<ID3D12CommandQueue>            m_spCopyQueue;
+   std::vector<D3D12_RESOURCE_BARRIER>   m_transitionsBeforeCloseCmdList;
+
+   std::unique_ptr<d3d12_video_encoder_references_manager_interface> m_upDPBManager;
+   std::unique_ptr<d3d12_video_dpb_storage_manager_interface>        m_upDPBStorageManager;
+   std::unique_ptr<d3d12_video_bitstream_builder_interface>          m_upBitstreamBuilder;
+
+   bool m_needsGPUFlush = false;
+
+   ComPtr<ID3D12Resource> m_spResolvedMetadataBuffer;
+   ComPtr<ID3D12Resource> m_spMetadataOutputBuffer;
+
+   std::vector<uint8_t> m_BitstreamHeadersBuffer;
+
+   struct
+   {
+      bool m_fArrayOfTexturesDpb;
+
+      D3D12_VIDEO_ENCODER_SUPPORT_FLAGS                          m_SupportFlags;
+      D3D12_VIDEO_ENCODER_VALIDATION_FLAGS                       m_ValidationFlags;
+      D3D12_FEATURE_DATA_VIDEO_ENCODER_RESOLUTION_SUPPORT_LIMITS m_currentResolutionSupportCaps;
+      union
+      {
+         D3D12_VIDEO_ENCODER_PROFILE_H264 m_H264Profile;
+         D3D12_VIDEO_ENCODER_PROFILE_HEVC m_HEVCProfile;
+      } m_encoderSuggestedProfileDesc = {};
+
+      union
+      {
+         D3D12_VIDEO_ENCODER_LEVELS_H264                 m_H264LevelSetting;
+         D3D12_VIDEO_ENCODER_LEVEL_TIER_CONSTRAINTS_HEVC m_HEVCLevelSetting;
+      } m_encoderLevelSuggestedDesc = {};
+
+      // Required size for the layout-resolved metadata buffer of current frame to be encoded
+      size_t m_resolvedLayoutMetadataBufferRequiredSize;
+
+      // The maximum number of slices that the output of the current frame to be encoded will contain
+      uint32_t m_MaxSlicesInOutput;
+
+      D3D12_FEATURE_DATA_VIDEO_ENCODER_RESOURCE_REQUIREMENTS m_ResourceRequirementsCaps;
+
+   } m_currentEncodeCapabilities;
+
+   struct
+   {
+      d3d12_video_encoder_config_dirty_flags m_ConfigDirtyFlags = d3d12_video_encoder_config_dirty_flag_none;
+
+      D3D12_VIDEO_ENCODER_PICTURE_RESOLUTION_DESC m_currentResolution = {};
+      D3D12_BOX m_FrameCroppingCodecConfig = {};
+
+      D3D12_FEATURE_DATA_FORMAT_INFO m_encodeFormatInfo = {};
+
+      D3D12_VIDEO_ENCODER_CODEC m_encoderCodecDesc = {};
+
+      D3D12_VIDEO_ENCODER_SEQUENCE_CONTROL_FLAGS m_seqFlags = D3D12_VIDEO_ENCODER_SEQUENCE_CONTROL_FLAG_NONE;
+
+      /// As the following D3D12 Encode types have pointers in their structures, we need to keep a deep copy of them
+
+      union
+      {
+         D3D12_VIDEO_ENCODER_PROFILE_H264 m_H264Profile;
+         D3D12_VIDEO_ENCODER_PROFILE_HEVC m_HEVCProfile;
+      } m_encoderProfileDesc = {};
+
+      union
+      {
+         D3D12_VIDEO_ENCODER_LEVELS_H264                 m_H264LevelSetting;
+         D3D12_VIDEO_ENCODER_LEVEL_TIER_CONSTRAINTS_HEVC m_HEVCLevelSetting;
+      } m_encoderLevelDesc = {};
+
+      struct
+      {
+         D3D12_VIDEO_ENCODER_RATE_CONTROL_MODE  m_Mode;
+         D3D12_VIDEO_ENCODER_RATE_CONTROL_FLAGS m_Flags;
+         DXGI_RATIONAL                          m_FrameRate;
+         union
+         {
+            D3D12_VIDEO_ENCODER_RATE_CONTROL_CQP  m_Configuration_CQP;
+            D3D12_VIDEO_ENCODER_RATE_CONTROL_CBR  m_Configuration_CBR;
+            D3D12_VIDEO_ENCODER_RATE_CONTROL_VBR  m_Configuration_VBR;
+            D3D12_VIDEO_ENCODER_RATE_CONTROL_QVBR m_Configuration_QVBR;
+         } m_Config;
+      } m_encoderRateControlDesc = {};
+
+      union
+      {
+         D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION_H264 m_H264Config;
+         D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION_HEVC m_HEVCConfig;
+      } m_encoderCodecSpecificConfigDesc = {};
+
+
+      D3D12_VIDEO_ENCODER_FRAME_SUBREGION_LAYOUT_MODE m_encoderSliceConfigMode;
+      union
+      {
+         D3D12_VIDEO_ENCODER_PICTURE_CONTROL_SUBREGIONS_LAYOUT_DATA_SLICES m_SlicesPartition_H264;
+         D3D12_VIDEO_ENCODER_PICTURE_CONTROL_SUBREGIONS_LAYOUT_DATA_SLICES m_SlicesPartition_HEVC;
+      } m_encoderSliceConfigDesc = {};
+
+      union
+      {
+         D3D12_VIDEO_ENCODER_SEQUENCE_GOP_STRUCTURE_H264 m_H264GroupOfPictures;
+         D3D12_VIDEO_ENCODER_SEQUENCE_GOP_STRUCTURE_HEVC m_HEVCGroupOfPictures;
+      } m_encoderGOPConfigDesc = {};
+
+      union
+      {
+         D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA_H264 m_H264PicData;
+         D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA_HEVC m_HEVCPicData;
+      } m_encoderPicParamsDesc = {};
+
+      D3D12_VIDEO_ENCODER_MOTION_ESTIMATION_PRECISION_MODE m_encoderMotionPrecisionLimit =
+         D3D12_VIDEO_ENCODER_MOTION_ESTIMATION_PRECISION_MODE_MAXIMUM;
+
+      D3D12_VIDEO_ENCODER_INTRA_REFRESH m_IntraRefresh = { D3D12_VIDEO_ENCODER_INTRA_REFRESH_MODE_NONE, 0 };
+      uint32_t                          m_IntraRefreshCurrentFrameIndex = 0;
+
+   } m_currentEncodeConfig;
+};
+
+bool
+d3d12_video_encoder_create_command_objects(struct d3d12_video_encoder *pD3D12Enc);
+bool
+d3d12_video_encoder_reconfigure_session(struct d3d12_video_encoder *pD3D12Enc,
+                                        struct pipe_video_buffer *  srcTexture,
+                                        struct pipe_picture_desc *  picture);
+bool
+d3d12_video_encoder_update_current_encoder_config_state(struct d3d12_video_encoder *pD3D12Enc,
+                                                        struct pipe_video_buffer *  srcTexture,
+                                                        struct pipe_picture_desc *  picture);
+bool
+d3d12_video_encoder_reconfigure_encoder_objects(struct d3d12_video_encoder *pD3D12Enc,
+                                                struct pipe_video_buffer *  srcTexture,
+                                                struct pipe_picture_desc *  picture);
+D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA
+d3d12_video_encoder_get_current_picture_param_settings(struct d3d12_video_encoder *pD3D12Enc);
+D3D12_VIDEO_ENCODER_LEVEL_SETTING
+d3d12_video_encoder_get_current_level_desc(struct d3d12_video_encoder *pD3D12Enc);
+D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION
+d3d12_video_encoder_get_current_codec_config_desc(struct d3d12_video_encoder *pD3D12Enc);
+D3D12_VIDEO_ENCODER_PROFILE_DESC
+d3d12_video_encoder_get_current_profile_desc(struct d3d12_video_encoder *pD3D12Enc);
+D3D12_VIDEO_ENCODER_RATE_CONTROL
+d3d12_video_encoder_get_current_rate_control_settings(struct d3d12_video_encoder *pD3D12Enc);
+D3D12_VIDEO_ENCODER_PICTURE_CONTROL_SUBREGIONS_LAYOUT_DATA
+d3d12_video_encoder_get_current_slice_param_settings(struct d3d12_video_encoder *pD3D12Enc);
+D3D12_VIDEO_ENCODER_SEQUENCE_GOP_STRUCTURE
+d3d12_video_encoder_get_current_gop_desc(struct d3d12_video_encoder *pD3D12Enc);
+uint32_t
+d3d12_video_encoder_get_current_max_dpb_capacity(struct d3d12_video_encoder *pD3D12Enc);
+void
+d3d12_video_encoder_create_reference_picture_manager(struct d3d12_video_encoder *pD3D12Enc);
+void
+d3d12_video_encoder_update_picparams_tracking(struct d3d12_video_encoder *pD3D12Enc,
+                                              struct pipe_video_buffer *  srcTexture,
+                                              struct pipe_picture_desc *  picture);
+void
+d3d12_video_encoder_calculate_metadata_resolved_buffer_size(uint32_t maxSliceNumber, size_t &bufferSize);
+uint32_t
+d3d12_video_encoder_calculate_max_slices_count_in_output(
+   D3D12_VIDEO_ENCODER_FRAME_SUBREGION_LAYOUT_MODE                          slicesMode,
+   const D3D12_VIDEO_ENCODER_PICTURE_CONTROL_SUBREGIONS_LAYOUT_DATA_SLICES *slicesConfig,
+   uint32_t                                                                 MaxSubregionsNumberFromCaps,
+   D3D12_VIDEO_ENCODER_PICTURE_RESOLUTION_DESC                              sequenceTargetResolution,
+   uint32_t                                                                 SubregionBlockPixelsSize);
+bool
+d3d12_video_encoder_prepare_output_buffers(struct d3d12_video_encoder *pD3D12Enc,
+                                           struct pipe_video_buffer *  srcTexture,
+                                           struct pipe_picture_desc *  picture);
+uint32_t
+d3d12_video_encoder_build_codec_headers(struct d3d12_video_encoder *pD3D12Enc);
+void
+d3d12_video_encoder_extract_encode_metadata(
+   struct d3d12_video_encoder *                               pD3D12Dec,
+   ID3D12Resource *                                           pResolvedMetadataBuffer,
+   size_t                                                     resourceMetadataSize,
+   D3D12_VIDEO_ENCODER_OUTPUT_METADATA &                      encoderMetadata,
+   std::vector<D3D12_VIDEO_ENCODER_FRAME_SUBREGION_METADATA> &pSubregionsMetadata);
+
+D3D12_VIDEO_ENCODER_CODEC
+d3d12_video_encoder_get_current_codec(struct d3d12_video_encoder *pD3D12Enc);
+
+bool d3d12_video_encoder_negotiate_requested_features_and_d3d12_driver_caps(struct d3d12_video_encoder *pD3D12Enc, D3D12_FEATURE_DATA_VIDEO_ENCODER_SUPPORT &capEncoderSupportData);
+bool d3d12_video_encoder_query_d3d12_driver_caps(struct d3d12_video_encoder *pD3D12Enc, D3D12_FEATURE_DATA_VIDEO_ENCODER_SUPPORT &capEncoderSupportData);
+bool d3d12_video_encoder_check_subregion_mode_support(struct d3d12_video_encoder *pD3D12Enc, D3D12_VIDEO_ENCODER_FRAME_SUBREGION_LAYOUT_MODE requestedSlicesMode);
+
+///
+/// d3d12_video_encoder functions ends
+///
+
+#endif

src/gallium/drivers/d3d12/d3d12_video_enc_h264.h
@@ -1,0 +1,67 @@
+
+/*
+ * Copyright © Microsoft Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef D3D12_VIDEO_ENC_H264_H
+#define D3D12_VIDEO_ENC_H264_H
+#include "d3d12_video_types.h"
+
+bool
+d3d12_video_encoder_update_current_encoder_config_state_h264(struct d3d12_video_encoder *pD3D12Enc,
+                                                             struct pipe_video_buffer *  srcTexture,
+                                                             struct pipe_picture_desc *  picture);
+void
+d3d12_video_encoder_update_current_rate_control_h264(struct d3d12_video_encoder *pD3D12Enc,
+                                                     pipe_h264_enc_picture_desc *picture);
+bool
+d3d12_video_encoder_negotiate_current_h264_slices_configuration(struct d3d12_video_encoder *pD3D12Enc,
+                                                             pipe_h264_enc_picture_desc *picture);
+bool
+d3d12_video_encoder_update_h264_gop_configuration(struct d3d12_video_encoder *pD3D12Enc,
+                                                  pipe_h264_enc_picture_desc *picture);
+D3D12_VIDEO_ENCODER_MOTION_ESTIMATION_PRECISION_MODE
+d3d12_video_encoder_convert_h264_motion_configuration(struct d3d12_video_encoder *pD3D12Enc,
+                                                      pipe_h264_enc_picture_desc *picture);
+D3D12_VIDEO_ENCODER_LEVELS_H264
+d3d12_video_encoder_convert_level_h264(uint32_t h264SpecLevel);
+D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION_H264
+d3d12_video_encoder_convert_h264_codec_configuration(struct d3d12_video_encoder *pD3D12Enc,
+                                                     pipe_h264_enc_picture_desc *picture);
+void
+d3d12_video_encoder_update_current_frame_pic_params_info_h264(struct d3d12_video_encoder *pD3D12Enc,
+                                                              struct pipe_video_buffer *  srcTexture,
+                                                              struct pipe_picture_desc *  picture,
+                                                              D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA &picParams,
+                                                              bool &bUsedAsReference);
+D3D12_VIDEO_ENCODER_FRAME_TYPE_H264
+d3d12_video_encoder_convert_frame_type(enum pipe_h2645_enc_picture_type picType);
+uint32_t
+d3d12_video_encoder_build_codec_headers_h264(struct d3d12_video_encoder *pD3D12Enc);
+bool
+d3d12_video_encoder_compare_slice_config_h264_hevc(
+   D3D12_VIDEO_ENCODER_FRAME_SUBREGION_LAYOUT_MODE                   targetMode,
+   D3D12_VIDEO_ENCODER_PICTURE_CONTROL_SUBREGIONS_LAYOUT_DATA_SLICES targetConfig,
+   D3D12_VIDEO_ENCODER_FRAME_SUBREGION_LAYOUT_MODE                   otherMode,
+   D3D12_VIDEO_ENCODER_PICTURE_CONTROL_SUBREGIONS_LAYOUT_DATA_SLICES otherConfig);
+
+#endif

commit f555e2ba211730a0761c59609aa49b8ff4a1014a

d3d12: Initialize d3d12_video_encoder_bitstream member m_uiOffset.
Fix defect reported by Coverity Scan.

Uninitialized scalar field (UNINIT_CTOR)
uninit_member: Non-static class member m_uiOffset is not initialized in
this constructor nor in any functions that it calls.

Fixes: 14209e304a2 ("d3d12: Add video encode implementation of pipe_video_codec")
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Sil Vilerino <sivileri@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16682>

src/gallium/drivers/d3d12/d3d12_video_encoder_bitstream.cpp
@@ -28,6 +28,7 @@ d3d12_video_encoder_bitstream::d3d12_video_encoder_bitstream()
 {
    m_pBitsBuffer       = nullptr;
    m_uiBitsBufferSize  = 0;
+   m_uiOffset          = 0;
    m_iBitsToGo         = 32;
    m_uintEncBuffer     = 0;
    m_bExternalBuffer   = false;