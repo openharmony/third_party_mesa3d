openharmony/third_party_mesa3d

commit f09d955f330658a94dabbef6480ba50a74e38d69

egl/wgl: On win32, there is no support for EGL_EXT_device and EGL_EXT_platform_device
Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16537>

src/egl/main/eglglobals.c
@@ -67,9 +67,11 @@ struct _egl_global _eglGlobal =
    .ClientExtensionString =
 #endif
    "EGL_EXT_client_extensions"
+#ifndef DETECT_OS_WINDOWS
    " EGL_EXT_device_base"
    " EGL_EXT_device_enumeration"
    " EGL_EXT_device_query"
+#endif
    " EGL_EXT_platform_base"
    " EGL_KHR_client_get_all_proc_addresses"
    " EGL_KHR_debug"
@@ -81,7 +83,9 @@ struct _egl_global _eglGlobal =
    " "
 #endif
 
+#ifndef DETECT_OS_WINDOWS
    "EGL_EXT_platform_device"
+#endif
 #ifdef HAVE_WAYLAND_PLATFORM
    " EGL_EXT_platform_wayland"
    " EGL_KHR_platform_wayland"

commit dcf915092a7a4ef2b31564f667433a5762e2750d

egl: Fix DETECT_OS macro usage
As described in src/util/detect_os.h, the DETECT_OS macros are always
defined to a 0 or 1 value, and they should be used with #if rather
than #ifdef.
Commit f09d955f330 accidentally disabled those extensions on all
platforms, so enable them again.

Fixes: f09d955f330 ("egl/wgl: On win32, there is no support for EGL_EXT_device and EGL_EXT_platform_device")

Signed-off-by: Erico Nunes <nunes.erico@gmail.com>
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Acked-by: Yonggang Luo <luoyonggang@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16751>

src/egl/main/eglglobals.c
@@ -67,7 +67,7 @@ struct _egl_global _eglGlobal =
    .ClientExtensionString =
 #endif
    "EGL_EXT_client_extensions"
-#ifndef DETECT_OS_WINDOWS
+#if !DETECT_OS_WINDOWS
    " EGL_EXT_device_base"
    " EGL_EXT_device_enumeration"
    " EGL_EXT_device_query"
@@ -83,7 +83,7 @@ struct _egl_global _eglGlobal =
    " "
 #endif
 
-#ifndef DETECT_OS_WINDOWS
+#if !DETECT_OS_WINDOWS
    "EGL_EXT_platform_device"
 #endif
 #ifdef HAVE_WAYLAND_PLATFORM