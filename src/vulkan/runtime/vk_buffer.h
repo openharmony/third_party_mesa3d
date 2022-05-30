openharmony/third_party_mesa3d

commit 9548eaaa96a6e355da97aae449fbcccabd84ff6d

vulkan: Add a base struct for buffers
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16607>

src/vulkan/runtime/vk_buffer.h
@@ -1,0 +1,78 @@
+/*
+ * Copyright © 2022 Collabora, Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#ifndef VK_BUFFER_H
+#define VK_BUFFER_H
+
+#include "vk_object.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct vk_buffer {
+   struct vk_object_base base;
+
+   /** VkBufferCreateInfo::flags */
+   VkImageCreateFlags create_flags;
+
+   /** VkBufferCreateInfo::size */
+   VkDeviceSize size;
+
+   /** VkBufferCreateInfo::usage */
+   VkBufferUsageFlags usage;
+};
+VK_DEFINE_NONDISP_HANDLE_CASTS(vk_buffer, base, VkBuffer,
+                               VK_OBJECT_TYPE_BUFFER);
+
+void vk_buffer_init(struct vk_device *device,
+                    struct vk_buffer *buffer,
+                    const VkBufferCreateInfo *pCreateInfo);
+void vk_buffer_finish(struct vk_buffer *buffer);
+
+void *vk_buffer_create(struct vk_device *device,
+                       const VkBufferCreateInfo *pCreateInfo,
+                       const VkAllocationCallbacks *alloc,
+                       size_t size);
+void vk_buffer_destroy(struct vk_device *device,
+                       const VkAllocationCallbacks *alloc,
+                       struct vk_buffer *buffer);
+
+static inline uint64_t
+vk_buffer_range(const struct vk_buffer *buffer,
+                uint64_t offset, uint64_t range)
+{
+   assert(offset <= buffer->size);
+   if (range == VK_WHOLE_SIZE) {
+      return buffer->size - offset;
+   } else {
+      assert(range + offset >= range);
+      assert(range + offset <= buffer->size);
+      return range;
+   }
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VK_BUFFER_H */

src/vulkan/runtime/meson.build
@@ -24,6 +24,8 @@ vk_physical_device_features_gen_depend_files = [
 ]
 
 vulkan_runtime_files = files(
+  'vk_buffer.c',
+  'vk_buffer.h',
   'vk_cmd_copy.c',
   'vk_cmd_enqueue.c',
   'vk_command_buffer.c',

src/vulkan/runtime/vk_buffer.c
@@ -1,0 +1,73 @@
+/*
+ * Copyright © 2022 Collabora, Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "vk_buffer.h"
+
+#include "vk_alloc.h"
+#include "vk_device.h"
+
+void
+vk_buffer_init(struct vk_device *device,
+               struct vk_buffer *buffer,
+               const VkBufferCreateInfo *pCreateInfo)
+{
+   vk_object_base_init(device, &buffer->base, VK_OBJECT_TYPE_BUFFER);
+
+   assert(pCreateInfo->sType == VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO);
+   assert(pCreateInfo->size > 0);
+
+   buffer->create_flags = pCreateInfo->flags;
+   buffer->size = pCreateInfo->size;
+   buffer->usage = pCreateInfo->usage;
+}
+
+void *
+vk_buffer_create(struct vk_device *device,
+                 const VkBufferCreateInfo *pCreateInfo,
+                 const VkAllocationCallbacks *alloc,
+                 size_t size)
+{
+   struct vk_buffer *buffer =
+      vk_zalloc2(&device->alloc, alloc, size, 8,
+                 VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
+   if (buffer == NULL)
+      return NULL;
+
+   vk_buffer_init(device, buffer, pCreateInfo);
+
+   return buffer;
+}
+
+void
+vk_buffer_finish(struct vk_buffer *buffer)
+{
+   vk_object_base_finish(&buffer->base);
+}
+
+void
+vk_buffer_destroy(struct vk_device *device,
+                  const VkAllocationCallbacks *alloc,
+                  struct vk_buffer *buffer)
+{
+   vk_object_free(device, alloc, buffer);
+}

commit 347a79156e9b2fde1350b2014a61a7dc270ecd1d

vulkan: Use the correct enum in vk_buffer
VkBufferCreateFlags is correct.

Fixes: 9548eaa ("vulkan: Add a base struct for buffers")
Signed-off-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Reviewed-by: Jason Ekstrand <jason.ekstrand@collabora.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16764>

src/vulkan/runtime/vk_buffer.h
@@ -33,7 +33,7 @@ struct vk_buffer {
    struct vk_object_base base;
 
    /** VkBufferCreateInfo::flags */
-   VkImageCreateFlags create_flags;
+   VkBufferCreateFlags create_flags;
 
    /** VkBufferCreateInfo::size */
    VkDeviceSize size;