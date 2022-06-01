openharmony/third_party_mesa3d

commit ed72241af03fc066a7734308bf775b509bd6312e

d3d12: Add video decode implementation of pipe_video_codec
Acked-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16286>

src/gallium/drivers/d3d12/d3d12_video_buffer.cpp
@@ -1,0 +1,329 @@
+/*
+ * Copyright © Microsoft Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "d3d12_video_buffer.h"
+#include "d3d12_resource.h"
+#include "d3d12_video_dec.h"
+#include "d3d12_residency.h"
+
+#include "util/format/u_format.h"
+#include "util/u_inlines.h"
+#include "util/u_memory.h"
+#include "util/u_video.h"
+#include "vl/vl_video_buffer.h"
+#include "util/u_sampler.h"
+
+/**
+ * creates a video buffer
+ */
+struct pipe_video_buffer *
+d3d12_video_buffer_create(struct pipe_context *pipe, const struct pipe_video_buffer *tmpl)
+{
+   assert(pipe);
+   assert(tmpl);
+
+   ///
+   /// Initialize d3d12_video_buffer
+   ///
+
+
+   if (!(tmpl->buffer_format == PIPE_FORMAT_NV12)) {
+      debug_printf("[d3d12_video_buffer] buffer_format is only supported as PIPE_FORMAT_NV12.\n");
+      return nullptr;
+   }
+
+   if (!(pipe_format_to_chroma_format(tmpl->buffer_format) == PIPE_VIDEO_CHROMA_FORMAT_420)) {
+      debug_printf(
+         "[d3d12_video_buffer] tmpl->buffer_format only supported as a PIPE_VIDEO_CHROMA_FORMAT_420 format.\n");
+      return nullptr;
+   }
+
+   // Not using new doesn't call ctor and the initializations in the class declaration are lost
+   struct d3d12_video_buffer *pD3D12VideoBuffer = new d3d12_video_buffer;
+
+   // Fill base template
+   pD3D12VideoBuffer->base               = *tmpl;
+   pD3D12VideoBuffer->base.buffer_format = tmpl->buffer_format;
+   pD3D12VideoBuffer->base.context       = pipe;
+   pD3D12VideoBuffer->base.width         = tmpl->width;
+   pD3D12VideoBuffer->base.height        = tmpl->height;
+   pD3D12VideoBuffer->base.interlaced    = tmpl->interlaced;
+   pD3D12VideoBuffer->base.associated_data = nullptr;
+   pD3D12VideoBuffer->base.bind = PIPE_BIND_SAMPLER_VIEW | PIPE_BIND_RENDER_TARGET | PIPE_BIND_DISPLAY_TARGET;
+
+   // Fill vtable
+   pD3D12VideoBuffer->base.destroy                     = d3d12_video_buffer_destroy;
+   pD3D12VideoBuffer->base.get_sampler_view_planes     = d3d12_video_buffer_get_sampler_view_planes;
+   pD3D12VideoBuffer->base.get_sampler_view_components = d3d12_video_buffer_get_sampler_view_components;
+   pD3D12VideoBuffer->base.get_surfaces                = d3d12_video_buffer_get_surfaces;
+   pD3D12VideoBuffer->base.destroy_associated_data     = d3d12_video_buffer_destroy_associated_data;
+
+   struct pipe_resource templ;
+   memset(&templ, 0, sizeof(templ));
+   templ.target     = PIPE_TEXTURE_2D;
+   templ.bind       = pD3D12VideoBuffer->base.bind;
+   templ.format     = pD3D12VideoBuffer->base.buffer_format;
+   // YUV 4:2:0 formats in D3D12 need to have multiple of 2 dimensions
+   templ.width0     = align(pD3D12VideoBuffer->base.width, 2);
+   templ.height0    = align(pD3D12VideoBuffer->base.height, 2);
+   templ.depth0     = 1;
+   templ.array_size = 1;
+   templ.flags      = 0;
+
+   // This calls d3d12_create_resource as the function ptr is set in d3d12_screen.resource_create
+   pD3D12VideoBuffer->texture = (struct d3d12_resource *) pipe->screen->resource_create(pipe->screen, &templ);
+   d3d12_promote_to_permanent_residency((struct d3d12_screen*) pipe->screen, pD3D12VideoBuffer->texture);
+
+   if (pD3D12VideoBuffer->texture == nullptr) {
+      debug_printf("[d3d12_video_buffer] d3d12_video_buffer_create - Call to resource_create() to create "
+                      "d3d12_resource failed\n");
+      goto failed;
+   }
+
+   pD3D12VideoBuffer->num_planes = util_format_get_num_planes(pD3D12VideoBuffer->texture->overall_format);
+   assert(pD3D12VideoBuffer->num_planes == 2);
+   return &pD3D12VideoBuffer->base;
+
+failed:
+   if (pD3D12VideoBuffer != nullptr) {
+      d3d12_video_buffer_destroy((struct pipe_video_buffer *) pD3D12VideoBuffer);
+   }
+
+   return nullptr;
+}
+
+/**
+ * destroy this video buffer
+ */
+void
+d3d12_video_buffer_destroy(struct pipe_video_buffer *buffer)
+{
+   struct d3d12_video_buffer *pD3D12VideoBuffer = (struct d3d12_video_buffer *) buffer;
+
+   // Destroy pD3D12VideoBuffer->texture (if any)
+   if (pD3D12VideoBuffer->texture) {
+      pipe_resource *pBaseResource = &pD3D12VideoBuffer->texture->base.b;
+      pipe_resource_reference(&pBaseResource, NULL);
+   }
+
+   // Destroy associated data (if any)
+   if (pD3D12VideoBuffer->base.associated_data != nullptr) {
+      d3d12_video_buffer_destroy_associated_data(pD3D12VideoBuffer->base.associated_data);
+      // Set to nullptr after cleanup, no dangling pointers
+      pD3D12VideoBuffer->base.associated_data = nullptr;
+   }
+
+   // Destroy (if any) codec where the associated data came from
+   if (pD3D12VideoBuffer->base.codec != nullptr) {
+      d3d12_video_decoder_destroy(pD3D12VideoBuffer->base.codec);
+      // Set to nullptr after cleanup, no dangling pointers
+      pD3D12VideoBuffer->base.codec = nullptr;
+   }
+
+   for (uint i = 0; i < pD3D12VideoBuffer->surfaces.size(); ++i) {
+      if (pD3D12VideoBuffer->surfaces[i] != NULL) {
+         pipe_surface_reference(&pD3D12VideoBuffer->surfaces[i], NULL);
+      }
+   }
+
+   for (uint i = 0; i < pD3D12VideoBuffer->sampler_view_planes.size(); ++i) {
+      if (pD3D12VideoBuffer->sampler_view_planes[i] != NULL) {
+         pipe_sampler_view_reference(&pD3D12VideoBuffer->sampler_view_planes[i], NULL);
+      }
+   }
+
+   for (uint i = 0; i < pD3D12VideoBuffer->sampler_view_components.size(); ++i) {
+      if (pD3D12VideoBuffer->sampler_view_components[i] != NULL) {
+         pipe_sampler_view_reference(&pD3D12VideoBuffer->sampler_view_components[i], NULL);
+      }
+   }
+
+   delete pD3D12VideoBuffer;
+}
+
+/*
+ * destroy the associated data
+ */
+void
+d3d12_video_buffer_destroy_associated_data(void *associated_data)
+{ }
+
+/**
+ * get an individual surfaces for each plane
+ */
+struct pipe_surface **
+d3d12_video_buffer_get_surfaces(struct pipe_video_buffer *buffer)
+{
+   assert(buffer);
+   struct d3d12_video_buffer *pD3D12VideoBuffer = (struct d3d12_video_buffer *) buffer;
+   struct pipe_context *      pipe              = pD3D12VideoBuffer->base.context;
+   struct pipe_surface        surface_template  = {};
+
+   // Some video frameworks iterate over [0..VL_MAX_SURFACES) and ignore the nullptr entries
+   // So we have to null initialize the other surfaces not used from [num_planes..VL_MAX_SURFACES)
+   // Like in src/gallium/frontends/va/surface.c
+   pD3D12VideoBuffer->surfaces.resize(VL_MAX_SURFACES, nullptr);
+
+   // pCurPlaneResource refers to the planar resource, not the overall resource.
+   // in d3d12_resource this is handled by having a linked list of planes with
+   // d3dRes->base.next ptr to next plane resource
+   // starting with the plane 0 being the overall resource
+   struct pipe_resource *pCurPlaneResource = &pD3D12VideoBuffer->texture->base.b;
+
+   for (uint PlaneSlice = 0; PlaneSlice < pD3D12VideoBuffer->num_planes; ++PlaneSlice) {
+      if (!pD3D12VideoBuffer->surfaces[PlaneSlice]) {
+         memset(&surface_template, 0, sizeof(surface_template));
+         surface_template.format =
+            util_format_get_plane_format(pD3D12VideoBuffer->texture->overall_format, PlaneSlice);
+
+         pD3D12VideoBuffer->surfaces[PlaneSlice] =
+            pipe->create_surface(pipe, pCurPlaneResource, &surface_template);
+
+         if (!pD3D12VideoBuffer->surfaces[PlaneSlice]) {
+            goto error;
+         }
+      }
+      pCurPlaneResource = pCurPlaneResource->next;
+   }
+
+   return pD3D12VideoBuffer->surfaces.data();
+
+error:
+   for (uint PlaneSlice = 0; PlaneSlice < pD3D12VideoBuffer->num_planes; ++PlaneSlice) {
+      pipe_surface_reference(&pD3D12VideoBuffer->surfaces[PlaneSlice], NULL);
+   }
+
+   return nullptr;
+}
+
+/**
+ * get an individual sampler view for each plane
+ */
+struct pipe_sampler_view **
+d3d12_video_buffer_get_sampler_view_planes(struct pipe_video_buffer *buffer)
+{
+   assert(buffer);
+   struct d3d12_video_buffer *pD3D12VideoBuffer = (struct d3d12_video_buffer *) buffer;
+   struct pipe_context *      pipe              = pD3D12VideoBuffer->base.context;
+   struct pipe_sampler_view   samplerViewTemplate;
+
+   // Some video frameworks iterate over [0..VL_MAX_SURFACES) and ignore the nullptr entries
+   // So we have to null initialize the other surfaces not used from [num_planes..VL_MAX_SURFACES)
+   // Like in src/gallium/frontends/vdpau/surface.c
+   pD3D12VideoBuffer->sampler_view_planes.resize(VL_MAX_SURFACES, nullptr);
+
+   // pCurPlaneResource refers to the planar resource, not the overall resource.
+   // in d3d12_resource this is handled by having a linked list of planes with
+   // d3dRes->base.next ptr to next plane resource
+   // starting with the plane 0 being the overall resource
+   struct pipe_resource *pCurPlaneResource = &pD3D12VideoBuffer->texture->base.b;
+
+   for (uint i = 0; i < pD3D12VideoBuffer->num_planes; ++i) {
+      if (!pD3D12VideoBuffer->sampler_view_planes[i]) {
+         assert(pCurPlaneResource);   // the d3d12_resource has a linked list with the exact name of number of elements
+                                      // as planes
+
+         memset(&samplerViewTemplate, 0, sizeof(samplerViewTemplate));
+         u_sampler_view_default_template(&samplerViewTemplate, pCurPlaneResource, pCurPlaneResource->format);
+
+         pD3D12VideoBuffer->sampler_view_planes[i] =
+            pipe->create_sampler_view(pipe, pCurPlaneResource, &samplerViewTemplate);
+
+         if (!pD3D12VideoBuffer->sampler_view_planes[i]) {
+            goto error;
+         }
+      }
+
+      pCurPlaneResource = pCurPlaneResource->next;
+   }
+
+   return pD3D12VideoBuffer->sampler_view_planes.data();
+
+error:
+   for (uint i = 0; i < pD3D12VideoBuffer->num_planes; ++i) {
+      pipe_sampler_view_reference(&pD3D12VideoBuffer->sampler_view_planes[i], NULL);
+   }
+
+   return nullptr;
+}
+
+/**
+ * get an individual sampler view for each component
+ */
+struct pipe_sampler_view **
+d3d12_video_buffer_get_sampler_view_components(struct pipe_video_buffer *buffer)
+{
+   assert(buffer);
+   struct d3d12_video_buffer *pD3D12VideoBuffer = (struct d3d12_video_buffer *) buffer;
+   struct pipe_context *      pipe              = pD3D12VideoBuffer->base.context;
+   struct pipe_sampler_view   samplerViewTemplate;
+
+   // pCurPlaneResource refers to the planar resource, not the overall resource.
+   // in d3d12_resource this is handled by having a linked list of planes with
+   // d3dRes->base.next ptr to next plane resource
+   // starting with the plane 0 being the overall resource
+   struct pipe_resource *pCurPlaneResource = &pD3D12VideoBuffer->texture->base.b;
+
+   // At the end of the loop, "component" will have the total number of items valid in sampler_view_components
+   // since component can end up being <= VL_NUM_COMPONENTS, we assume VL_NUM_COMPONENTS first and then resize/adjust to
+   // fit the container size pD3D12VideoBuffer->sampler_view_components to the actual components number
+   pD3D12VideoBuffer->sampler_view_components.resize(VL_NUM_COMPONENTS, nullptr);
+   uint component = 0;
+
+   for (uint i = 0; i < pD3D12VideoBuffer->num_planes; ++i) {
+      // For example num_components would be 1 for the Y plane (R8 in NV12), 2 for the UV plane (R8G8 in NV12)
+      unsigned num_components = util_format_get_nr_components(pCurPlaneResource->format);
+
+      for (uint j = 0; j < num_components; ++j, ++component) {
+         assert(component < VL_NUM_COMPONENTS);
+
+         if (!pD3D12VideoBuffer->sampler_view_components[component]) {
+            memset(&samplerViewTemplate, 0, sizeof(samplerViewTemplate));
+            u_sampler_view_default_template(&samplerViewTemplate, pCurPlaneResource, pCurPlaneResource->format);
+            samplerViewTemplate.swizzle_r = samplerViewTemplate.swizzle_g = samplerViewTemplate.swizzle_b =
+               PIPE_SWIZZLE_X + j;
+            samplerViewTemplate.swizzle_a = PIPE_SWIZZLE_1;
+
+            pD3D12VideoBuffer->sampler_view_components[component] =
+               pipe->create_sampler_view(pipe, pCurPlaneResource, &samplerViewTemplate);
+            if (!pD3D12VideoBuffer->sampler_view_components[component]) {
+               goto error;
+            }
+         }
+      }
+
+      pCurPlaneResource = pCurPlaneResource->next;
+   }
+
+   // Adjust size to fit component <= VL_NUM_COMPONENTS
+   pD3D12VideoBuffer->sampler_view_components.resize(component);
+
+   return pD3D12VideoBuffer->sampler_view_components.data();
+
+error:
+   for (uint i = 0; i < pD3D12VideoBuffer->num_planes; ++i) {
+      pipe_sampler_view_reference(&pD3D12VideoBuffer->sampler_view_components[i], NULL);
+   }
+
+   return nullptr;
+}

src/gallium/drivers/d3d12/d3d12_util.h
@@ -1,0 +1,246 @@
+/*
+ * Copyright © Microsoft Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef D3D12_UTIL_H
+#define D3D12_UTIL_H
+
+//------------------------------------------------------------------------------------------------
+template <typename T, typename U, typename V>
+inline void D3D12DecomposeSubresource( UINT Subresource, UINT MipLevels, UINT ArraySize, _Out_ T& MipSlice, _Out_ U& ArraySlice, _Out_ V& PlaneSlice ) noexcept
+{
+    MipSlice = static_cast<T>(Subresource % MipLevels);
+    ArraySlice = static_cast<U>((Subresource / MipLevels) % ArraySize);
+    PlaneSlice = static_cast<V>(Subresource / (MipLevels * ArraySize));
+}
+
+//------------------------------------------------------------------------------------------------
+constexpr UINT D3D12CalcSubresource( UINT MipSlice, UINT ArraySlice, UINT PlaneSlice, UINT MipLevels, UINT ArraySize ) noexcept
+{
+    return MipSlice + ArraySlice * MipLevels + PlaneSlice * MipLevels * ArraySize;
+}
+
+//------------------------------------------------------------------------------------------------
+struct CD3DX12_RESOURCE_BARRIER : public D3D12_RESOURCE_BARRIER
+{
+    CD3DX12_RESOURCE_BARRIER() = default;
+    explicit CD3DX12_RESOURCE_BARRIER(const D3D12_RESOURCE_BARRIER &o) noexcept :
+        D3D12_RESOURCE_BARRIER(o)
+    {}
+    static inline CD3DX12_RESOURCE_BARRIER Transition(
+        _In_ ID3D12Resource* pResource,
+        D3D12_RESOURCE_STATES stateBefore,
+        D3D12_RESOURCE_STATES stateAfter,
+        UINT subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES,
+        D3D12_RESOURCE_BARRIER_FLAGS flags = D3D12_RESOURCE_BARRIER_FLAG_NONE) noexcept
+    {
+        CD3DX12_RESOURCE_BARRIER result = {};
+        D3D12_RESOURCE_BARRIER &barrier = result;
+        result.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+        result.Flags = flags;
+        barrier.Transition.pResource = pResource;
+        barrier.Transition.StateBefore = stateBefore;
+        barrier.Transition.StateAfter = stateAfter;
+        barrier.Transition.Subresource = subresource;
+        return result;
+    }
+    static inline CD3DX12_RESOURCE_BARRIER Aliasing(
+        _In_ ID3D12Resource* pResourceBefore,
+        _In_ ID3D12Resource* pResourceAfter) noexcept
+    {
+        CD3DX12_RESOURCE_BARRIER result = {};
+        D3D12_RESOURCE_BARRIER &barrier = result;
+        result.Type = D3D12_RESOURCE_BARRIER_TYPE_ALIASING;
+        barrier.Aliasing.pResourceBefore = pResourceBefore;
+        barrier.Aliasing.pResourceAfter = pResourceAfter;
+        return result;
+    }
+    static inline CD3DX12_RESOURCE_BARRIER UAV(
+        _In_ ID3D12Resource* pResource) noexcept
+    {
+        CD3DX12_RESOURCE_BARRIER result = {};
+        D3D12_RESOURCE_BARRIER &barrier = result;
+        result.Type = D3D12_RESOURCE_BARRIER_TYPE_UAV;
+        barrier.UAV.pResource = pResource;
+        return result;
+    }
+};
+
+//------------------------------------------------------------------------------------------------
+struct CD3DX12_RESOURCE_DESC : public D3D12_RESOURCE_DESC
+{
+    CD3DX12_RESOURCE_DESC() = default;
+    explicit CD3DX12_RESOURCE_DESC( const D3D12_RESOURCE_DESC& o ) noexcept :
+        D3D12_RESOURCE_DESC( o )
+    {}
+    CD3DX12_RESOURCE_DESC(
+        D3D12_RESOURCE_DIMENSION dimension,
+        UINT64 alignment,
+        UINT64 width,
+        UINT height,
+        UINT16 depthOrArraySize,
+        UINT16 mipLevels,
+        DXGI_FORMAT format,
+        UINT sampleCount,
+        UINT sampleQuality,
+        D3D12_TEXTURE_LAYOUT layout,
+        D3D12_RESOURCE_FLAGS flags ) noexcept
+    {
+        Dimension = dimension;
+        Alignment = alignment;
+        Width = width;
+        Height = height;
+        DepthOrArraySize = depthOrArraySize;
+        MipLevels = mipLevels;
+        Format = format;
+        SampleDesc.Count = sampleCount;
+        SampleDesc.Quality = sampleQuality;
+        Layout = layout;
+        Flags = flags;
+    }
+    static inline CD3DX12_RESOURCE_DESC Buffer(
+        const D3D12_RESOURCE_ALLOCATION_INFO& resAllocInfo,
+        D3D12_RESOURCE_FLAGS flags = D3D12_RESOURCE_FLAG_NONE ) noexcept
+    {
+        return CD3DX12_RESOURCE_DESC( D3D12_RESOURCE_DIMENSION_BUFFER, resAllocInfo.Alignment, resAllocInfo.SizeInBytes,
+            1, 1, 1, DXGI_FORMAT_UNKNOWN, 1, 0, D3D12_TEXTURE_LAYOUT_ROW_MAJOR, flags );
+    }
+    static inline CD3DX12_RESOURCE_DESC Buffer(
+        UINT64 width,
+        D3D12_RESOURCE_FLAGS flags = D3D12_RESOURCE_FLAG_NONE,
+        UINT64 alignment = 0 ) noexcept
+    {
+        return CD3DX12_RESOURCE_DESC( D3D12_RESOURCE_DIMENSION_BUFFER, alignment, width, 1, 1, 1,
+            DXGI_FORMAT_UNKNOWN, 1, 0, D3D12_TEXTURE_LAYOUT_ROW_MAJOR, flags );
+    }
+    static inline CD3DX12_RESOURCE_DESC Tex1D(
+        DXGI_FORMAT format,
+        UINT64 width,
+        UINT16 arraySize = 1,
+        UINT16 mipLevels = 0,
+        D3D12_RESOURCE_FLAGS flags = D3D12_RESOURCE_FLAG_NONE,
+        D3D12_TEXTURE_LAYOUT layout = D3D12_TEXTURE_LAYOUT_UNKNOWN,
+        UINT64 alignment = 0 ) noexcept
+    {
+        return CD3DX12_RESOURCE_DESC( D3D12_RESOURCE_DIMENSION_TEXTURE1D, alignment, width, 1, arraySize,
+            mipLevels, format, 1, 0, layout, flags );
+    }
+    static inline CD3DX12_RESOURCE_DESC Tex2D(
+        DXGI_FORMAT format,
+        UINT64 width,
+        UINT height,
+        UINT16 arraySize = 1,
+        UINT16 mipLevels = 0,
+        UINT sampleCount = 1,
+        UINT sampleQuality = 0,
+        D3D12_RESOURCE_FLAGS flags = D3D12_RESOURCE_FLAG_NONE,
+        D3D12_TEXTURE_LAYOUT layout = D3D12_TEXTURE_LAYOUT_UNKNOWN,
+        UINT64 alignment = 0 ) noexcept
+    {
+        return CD3DX12_RESOURCE_DESC( D3D12_RESOURCE_DIMENSION_TEXTURE2D, alignment, width, height, arraySize,
+            mipLevels, format, sampleCount, sampleQuality, layout, flags );
+    }
+    static inline CD3DX12_RESOURCE_DESC Tex3D(
+        DXGI_FORMAT format,
+        UINT64 width,
+        UINT height,
+        UINT16 depth,
+        UINT16 mipLevels = 0,
+        D3D12_RESOURCE_FLAGS flags = D3D12_RESOURCE_FLAG_NONE,
+        D3D12_TEXTURE_LAYOUT layout = D3D12_TEXTURE_LAYOUT_UNKNOWN,
+        UINT64 alignment = 0 ) noexcept
+    {
+        return CD3DX12_RESOURCE_DESC( D3D12_RESOURCE_DIMENSION_TEXTURE3D, alignment, width, height, depth,
+            mipLevels, format, 1, 0, layout, flags );
+    }
+    inline UINT16 Depth() const noexcept
+    { return (Dimension == D3D12_RESOURCE_DIMENSION_TEXTURE3D ? DepthOrArraySize : 1u); }
+    inline UINT16 ArraySize() const noexcept
+    { return (Dimension != D3D12_RESOURCE_DIMENSION_TEXTURE3D ? DepthOrArraySize : 1u); }
+    inline UINT CalcSubresource(UINT MipSlice, UINT ArraySlice, UINT PlaneSlice) noexcept
+    { return D3D12CalcSubresource(MipSlice, ArraySlice, PlaneSlice, MipLevels, ArraySize()); }
+};
+inline bool operator==( const D3D12_RESOURCE_DESC& l, const D3D12_RESOURCE_DESC& r ) noexcept
+{
+    return l.Dimension == r.Dimension &&
+        l.Alignment == r.Alignment &&
+        l.Width == r.Width &&
+        l.Height == r.Height &&
+        l.DepthOrArraySize == r.DepthOrArraySize &&
+        l.MipLevels == r.MipLevels &&
+        l.Format == r.Format &&
+        l.SampleDesc.Count == r.SampleDesc.Count &&
+        l.SampleDesc.Quality == r.SampleDesc.Quality &&
+        l.Layout == r.Layout &&
+        l.Flags == r.Flags;
+}
+inline bool operator!=( const D3D12_RESOURCE_DESC& l, const D3D12_RESOURCE_DESC& r ) noexcept
+{ return !( l == r ); }
+
+
+//------------------------------------------------------------------------------------------------
+struct CD3DX12_HEAP_PROPERTIES : public D3D12_HEAP_PROPERTIES
+{
+    CD3DX12_HEAP_PROPERTIES() = default;
+    explicit CD3DX12_HEAP_PROPERTIES(const D3D12_HEAP_PROPERTIES &o) noexcept :
+        D3D12_HEAP_PROPERTIES(o)
+    {}
+    CD3DX12_HEAP_PROPERTIES(
+        D3D12_CPU_PAGE_PROPERTY cpuPageProperty,
+        D3D12_MEMORY_POOL memoryPoolPreference,
+        UINT creationNodeMask = 1,
+        UINT nodeMask = 1 ) noexcept
+    {
+        Type = D3D12_HEAP_TYPE_CUSTOM;
+        CPUPageProperty = cpuPageProperty;
+        MemoryPoolPreference = memoryPoolPreference;
+        CreationNodeMask = creationNodeMask;
+        VisibleNodeMask = nodeMask;
+    }
+    explicit CD3DX12_HEAP_PROPERTIES(
+        D3D12_HEAP_TYPE type,
+        UINT creationNodeMask = 1,
+        UINT nodeMask = 1 ) noexcept
+    {
+        Type = type;
+        CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+        MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+        CreationNodeMask = creationNodeMask;
+        VisibleNodeMask = nodeMask;
+    }
+    bool IsCPUAccessible() const noexcept
+    {
+        return Type == D3D12_HEAP_TYPE_UPLOAD || Type == D3D12_HEAP_TYPE_READBACK || (Type == D3D12_HEAP_TYPE_CUSTOM &&
+            (CPUPageProperty == D3D12_CPU_PAGE_PROPERTY_WRITE_COMBINE || CPUPageProperty == D3D12_CPU_PAGE_PROPERTY_WRITE_BACK));
+    }
+};
+inline bool operator==( const D3D12_HEAP_PROPERTIES& l, const D3D12_HEAP_PROPERTIES& r ) noexcept
+{
+    return l.Type == r.Type && l.CPUPageProperty == r.CPUPageProperty &&
+        l.MemoryPoolPreference == r.MemoryPoolPreference &&
+        l.CreationNodeMask == r.CreationNodeMask &&
+        l.VisibleNodeMask == r.VisibleNodeMask;
+}
+inline bool operator!=( const D3D12_HEAP_PROPERTIES& l, const D3D12_HEAP_PROPERTIES& r ) noexcept
+{ return !( l == r ); }
+
+#endif

commit 06eb72d7863e4eeee34fd67c26dad16bc07f7037

d3d12: Remove unnecessary NULL check.
Fix defect reported by Coverity Scan.

Dereference before null check (REVERSE_INULL)
check_after_deref: Null-checking pD3D12VideoBuffer suggests that it may
be null, but it has already been dereferenced on all paths leading to
the check.

Fixes: ed72241af03 ("d3d12: Add video decode implementation of pipe_video_codec")
Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16714>

src/gallium/drivers/d3d12/d3d12_video_buffer.cpp
@@ -105,9 +105,7 @@ d3d12_video_buffer_create(struct pipe_context *pipe, const struct pipe_video_buf
    return &pD3D12VideoBuffer->base;
 
 failed:
-   if (pD3D12VideoBuffer != nullptr) {
-      d3d12_video_buffer_destroy((struct pipe_video_buffer *) pD3D12VideoBuffer);
-   }
+   d3d12_video_buffer_destroy((struct pipe_video_buffer *) pD3D12VideoBuffer);
 
    return nullptr;
 }