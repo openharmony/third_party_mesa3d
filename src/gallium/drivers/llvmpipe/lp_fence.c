openharmony/third_party_mesa3d

commit 3a492ee9fa114063e67f3eaab6c68612eb91ccbd

llvmpipe: add lp_fence_timedwait() helper
The function is analogous to lp_fence_wait() while taking at timeout
(ns) parameter, as needed for EGL fence/sync.

v2:
 - use absolute UTC time, as per spec (Gustaw)
 - bail out on cnd_timedwait() failure (Gustaw)

v3:
 - check count/rank under mutex (Gustaw)

Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
Reviewed-by: Roland Scheidegger <sroland@vmware.com> (v1)
Reviewed-by: Gustaw Smolarczyk <wielkiegie@gmail.com>

src/gallium/drivers/llvmpipe/lp_fence.c
@@ -125,3 +125,32 @@ lp_fence_wait(struct lp_fence *f)
 }
 
 
+boolean
+lp_fence_timedwait(struct lp_fence *f, uint64_t timeout)
+{
+   struct timespec ts;
+   int ret;
+
+   timespec_get(&ts, TIME_UTC);
+
+   ts.tv_nsec += timeout % 1000000000L;
+   ts.tv_sec += timeout / 1000000000L;
+   if (ts.tv_nsec >= 1000000000L) {
+      ts.tv_sec++;
+      ts.tv_nsec -= 1000000000L;
+   }
+
+   if (LP_DEBUG & DEBUG_FENCE)
+      debug_printf("%s %d\n", __FUNCTION__, f->id);
+
+   mtx_lock(&f->mutex);
+   assert(f->issued);
+   while (f->count < f->rank) {
+      ret = cnd_timedwait(&f->signalled, &f->mutex, &ts);
+      if (ret != thrd_success)
+         break;
+   }
+   const boolean result = (f->count >= f->rank);
+   mtx_unlock(&f->mutex);
+   return result;
+}

src/gallium/drivers/llvmpipe/lp_fence.h
@@ -65,6 +65,9 @@ lp_fence_signalled(struct lp_fence *fence);
 void
 lp_fence_wait(struct lp_fence *fence);
 
+boolean
+lp_fence_timedwait(struct lp_fence *fence, uint64_t timeout);
+
 void
 llvmpipe_init_screen_fence_funcs(struct pipe_screen *screen);
 

commit e1542d61776a72305ce6533222f3ca4479c95005

llvmpipe: handle timespec overflow on fence waits.
on 32-bit systems VK CTS was failing due to an overflow here,
detect the overflow and just do a normal wait.

Fixes: 3a492ee9fa11 ("llvmpipe: add lp_fence_timedwait() helper")
Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16847>

src/gallium/drivers/llvmpipe/lp_fence.c
@@ -32,6 +32,8 @@
 #include "lp_fence.h"
 
 
+#include "util/timespec.h"
+
 /**
  * Create a new fence object.
  *
@@ -128,17 +130,12 @@ lp_fence_wait(struct lp_fence *f)
 boolean
 lp_fence_timedwait(struct lp_fence *f, uint64_t timeout)
 {
-   struct timespec ts;
+   struct timespec ts, abs_ts;
    int ret;
 
    timespec_get(&ts, TIME_UTC);
 
-   ts.tv_nsec += timeout % 1000000000L;
-   ts.tv_sec += timeout / 1000000000L;
-   if (ts.tv_nsec >= 1000000000L) {
-      ts.tv_sec++;
-      ts.tv_nsec -= 1000000000L;
-   }
+   bool ts_overflow = timespec_add_nsec(&abs_ts, &ts, timeout);
 
    if (LP_DEBUG & DEBUG_FENCE)
       debug_printf("%s %d\n", __FUNCTION__, f->id);
@@ -146,7 +143,10 @@ lp_fence_timedwait(struct lp_fence *f, uint64_t timeout)
    mtx_lock(&f->mutex);
    assert(f->issued);
    while (f->count < f->rank) {
-      ret = cnd_timedwait(&f->signalled, &f->mutex, &ts);
+      if (ts_overflow)
+         ret = cnd_wait(&f->signalled, &f->mutex);
+      else
+         ret = cnd_timedwait(&f->signalled, &f->mutex, &abs_ts);
       if (ret != thrd_success)
          break;
    }