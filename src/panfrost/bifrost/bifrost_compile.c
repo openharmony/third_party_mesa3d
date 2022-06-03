openharmony/third_party_mesa3d

commit 1be1bd8534111f6b3ad84c11af7ac9e9ea3e4cba

pan/bi: Optimize abs(derivative)
We implement fine derivatives as:

	broadcast(x, (lane & ~1) + 1) - broadcast(x, lane & ~1)

Most of the complexity is to get the right sign. If we can ignore the
sign, we can generate the simpler code:

	broadcast(x, lane ^ 1) - lane

This is a particular win on v7+ where the broadcast instruction (CLPER)
can do `lane ^ value` for free. However, even on v6 where we lower to an
explicit XOR instruction, it's still a win.

The limiting case is fwidth. The fragment shader

   gl_FragColor = fwidth(vec4_varying);

has the following results on v6, v7, and v9:

G72 (-26% instructions, -43% cycles):
38 inst, 30 tuples, 5 clauses, 1.166667 cycles, 1.166667 arith, 28 quadwords
28 inst, 19 tuples, 4 clauses, 0.666667 cycles, 0.666667 arith, 19 quadwords

G76 (-37% instructions, -54% cycles):
38 inst, 30 tuples, 5 clauses, 1.166667 cycles, 1.166667 arith, 28 quadwords
24 inst, 16 tuples, 4 clauses, 0.541667 cycles, 0.541667 arith, 18 quadwords

G78 (-40% instructions, -56% cycles):
40 inst, 1.125000 cycles, 0.250000 fma, 0.109375 cvt, 1.125000 sfu, 20 quadwords
24 inst, 0.500000 cycles, 0.250000 fma, 0.015625 cvt, 0.500000 sfu, 12 quadwords

shader-db tells a similar story -- most shaders are unaffected, but a
shader that uses fwidth has a 20% reduction in cycle count:

instructions helped:   shaders/tesseract/488.shader_test MESA_SHADER_FRAGMENT: 264 -> 262 (-0.76%)
instructions helped:   shaders/chromeos/109-1.shader_test MESA_SHADER_FRAGMENT: 36 -> 28 (-22.22%)
tuples helped:   shaders/chromeos/109-1.shader_test MESA_SHADER_FRAGMENT: 27 -> 22 (-18.52%)
tuples HURT:   shaders/tesseract/488.shader_test MESA_SHADER_FRAGMENT: 211 -> 212 (0.47%)
clauses HURT:   shaders/tesseract/488.shader_test MESA_SHADER_FRAGMENT: 32 -> 33 (3.12%)
cycles helped:   shaders/chromeos/109-1.shader_test MESA_SHADER_FRAGMENT: 1 -> 0.79 (-20.83%)
arith helped:   shaders/chromeos/109-1.shader_test MESA_SHADER_FRAGMENT: 1 -> 0.79 (-20.83%)

src/panfrost/bifrost/bifrost_compile.c
@@ -1696,6 +1696,23 @@ bi_lower_fsincos_32(bi_builder *b, bi_index dst, bi_index s0, bool cos)
         bi_fadd_f32_to(b, dst, I->dest[0], cos ? cosx : sinx, BI_ROUND_NONE);
 }
 
+/* The XOR lane op is useful for derivative calculation, but was added in v7.
+ * Add a safe helper that will do the appropriate lowering on v6 */
+
+static bi_index
+bi_clper_xor(bi_builder *b, bi_index s0, bi_index s1)
+{
+        if (b->shader->arch >= 7) {
+                return bi_clper_i32(b, s0, s1,
+                                BI_INACTIVE_RESULT_ZERO, BI_LANE_OP_XOR,
+                                BI_SUBGROUP_SUBGROUP4);
+        }
+
+        bi_index lane_id = bi_fau(BIR_FAU_LANE_ID, false);
+        bi_index lane = bi_lshift_xor_i32(b, lane_id, s1, bi_imm_u8(0));
+        return bi_clper_v6_i32(b, s0, lane);
+}
+
 static bi_instr *
 bi_emit_alu_bool(bi_builder *b, unsigned sz, nir_op op,
       bi_index dst, bi_index s0, bi_index s1, bi_index s2)
@@ -2010,6 +2027,14 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
                 bi_csel_to(b, nir_op_infos[instr->op].input_types[0], sz, dst,
                                 s0, s1, s0, s1, BI_CMPF_GT);
                 break;
+
+        case nir_op_fddx_must_abs_mali:
+        case nir_op_fddy_must_abs_mali: {
+                bi_index bit = bi_imm_u32(instr->op == nir_op_fddx_must_abs_mali ? 1 : 2);
+                bi_index adjacent = bi_clper_xor(b, s0, bit);
+                bi_fadd_to(b, sz, dst, adjacent, bi_neg(s0), BI_ROUND_NONE);
+                break;
+        }
 
         case nir_op_fddx:
         case nir_op_fddy: {

src/panfrost/bifrost/bifrost_nir_algebraic.py
@@ -36,6 +36,9 @@ algebraic_late = [
     (('fmin', ('fmax', a, -1.0), 1.0), ('fsat_signed_mali', a)),
     (('fmax', ('fmin', a, 1.0), -1.0), ('fsat_signed_mali', a)),
     (('fmax', a, 0.0), ('fclamp_pos_mali', a)),
+
+    (('fabs', ('fddx', a)), ('fabs', ('fddx_must_abs_mali', a))),
+    (('fabs', ('fddy', b)), ('fabs', ('fddy_must_abs_mali', b))),
 ]
 
 

commit 67cc73cfaa00c3ac973c4de98f28bd081652d694

pan/bi: Fix clper_xor on Mali-G31
Mali-G31 has the old CLPER instruction, not the new one, which means we don't
get to specify a custom lane op. But the clper_xor helper incorrectly checked
the arch, not the implementation quirk.

Fixes: 1be1bd85341 ("pan/bi: Optimize abs(derivative)")
Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Reported-by: Icecream95 <ixn@disroot.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16846>

src/panfrost/bifrost/bifrost_compile.c
@@ -2157,13 +2157,15 @@ bi_lower_fsincos_32(bi_builder *b, bi_index dst, bi_index s0, bool cos)
         bi_fadd_f32_to(b, dst, I->dest[0], cos ? cosx : sinx);
 }
 
-/* The XOR lane op is useful for derivative calculation, but was added in v7.
- * Add a safe helper that will do the appropriate lowering on v6 */
-
+/*
+ * The XOR lane op is useful for derivative calculations, but not all Bifrost
+ * implementations have it. Add a safe helper that uses the hardware
+ * functionality when available and lowers where unavailable.
+ */
 static bi_index
 bi_clper_xor(bi_builder *b, bi_index s0, bi_index s1)
 {
-        if (b->shader->arch >= 7) {
+        if (!(b->shader->quirks & BIFROST_LIMITED_CLPER)) {
                 return bi_clper_i32(b, s0, s1,
                                 BI_INACTIVE_RESULT_ZERO, BI_LANE_OP_XOR,
                                 BI_SUBGROUP_SUBGROUP4);