openharmony/third_party_mesa3d

commit ca0e1bfb9df5e67f580b3f021d4e041f16bb1526

zink: add kopper api
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14541>

src/gallium/drivers/zink/zink_kopper.h
@@ -1,0 +1,109 @@
+/*
+ * Copyright © 2021 Valve Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ * 
+ * Authors:
+ *    Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
+ */
+
+#ifndef ZINK_KOPPER_H
+#define ZINK_KOPPER_H
+
+#include "kopper_interface.h"
+
+struct kopper_swapchain {
+   VkSwapchainKHR swapchain;
+   VkImage *images;
+   bool *inits;
+   unsigned last_present;
+   unsigned num_images;
+   VkSemaphore *acquires;
+   uint32_t last_present_prune;
+   struct hash_table *presents;
+   VkSwapchainCreateInfoKHR scci;
+   unsigned num_acquires;
+   unsigned max_acquires;
+};
+
+enum kopper_type {
+   KOPPER_X11,
+   KOPPER_WAYLAND,
+};
+
+struct kopper_displaytarget
+{
+   unsigned refcount;
+   VkFormat formats[2];
+   unsigned width;
+   unsigned height;
+   unsigned stride;
+   void *loader_private;
+
+   VkSurfaceKHR surface;
+   struct kopper_swapchain *swapchain;
+   struct kopper_swapchain *old_swapchain;
+
+   struct kopper_loader_info info;
+
+   VkSurfaceCapabilitiesKHR caps;
+   VkImageFormatListCreateInfoKHR format_list;
+   enum kopper_type type;
+};
+
+struct zink_screen;
+struct zink_resource;
+
+static inline bool
+zink_kopper_has_srgb(const struct kopper_displaytarget *cdt)
+{
+   return cdt->formats[1] != VK_FORMAT_UNDEFINED;
+}
+
+static inline bool
+zink_kopper_last_present_eq(const struct kopper_displaytarget *cdt, uint32_t idx)
+{
+   return cdt->swapchain->last_present == idx;
+}
+
+struct kopper_displaytarget *
+zink_kopper_displaytarget_create(struct zink_screen *screen, unsigned tex_usage,
+                                 enum pipe_format format, unsigned width,
+                                 unsigned height, unsigned alignment,
+                                 const void *loader_private, unsigned *stride);
+void
+zink_kopper_displaytarget_destroy(struct zink_screen *screen, struct kopper_displaytarget *cdt);
+
+
+bool
+zink_kopper_acquire(struct zink_context *ctx, struct zink_resource *res, uint64_t timeout);
+VkSemaphore
+zink_kopper_acquire_submit(struct zink_screen *screen, struct zink_resource *res);
+VkSemaphore
+zink_kopper_present(struct zink_screen *screen, struct zink_resource *res); 
+void
+zink_kopper_present_queue(struct zink_screen *screen, struct zink_resource *res);
+bool
+zink_kopper_acquire_readback(struct zink_context *ctx, struct zink_resource *res);
+bool
+zink_kopper_present_readback(struct zink_context *ctx, struct zink_resource *res);
+void
+zink_kopper_deinit_displaytarget(struct zink_screen *screen, struct kopper_displaytarget *cdt);
+#endif

commit 7a71c6a78e81b75dc4fa42ac1dd17e7606cd37eb

zink: defer old swapchain destruction
ensure that swapchains that are about to be presented asynchronously
cannot be destroyed

Fixes: ca0e1bfb9df ("zink: add kopper api")

Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16932>

src/gallium/drivers/zink/zink_kopper.c
@@ -143,6 +143,21 @@ destroy_swapchain(struct zink_screen *screen, struct kopper_swapchain *cswap)
    free(cswap);
 }
 
+static void
+prune_old_swapchains(struct zink_screen *screen, struct kopper_displaytarget *cdt, bool wait)
+{
+   while (cdt->old_swapchain) {
+      struct kopper_swapchain *cswap = cdt->old_swapchain;
+      if (cswap->async_presents) {
+         if (wait)
+            continue;
+         return;
+      }
+      cdt->old_swapchain = cswap->next;
+      destroy_swapchain(screen, cswap);
+   }
+}
+
 static struct hash_entry *
 find_dt_entry(struct zink_screen *screen, const struct kopper_displaytarget *cdt)
 {
@@ -182,7 +197,7 @@ zink_kopper_deinit_displaytarget(struct zink_screen *screen, struct kopper_displ
    _mesa_hash_table_remove(&screen->dts, he);
    simple_mtx_unlock(&screen->dt_lock);
    destroy_swapchain(screen, cdt->swapchain);
-   destroy_swapchain(screen, cdt->old_swapchain);
+   prune_old_swapchains(screen, cdt, true);
    VKSCR(DestroySurfaceKHR)(screen->instance, cdt->surface, NULL);
    cdt->swapchain = cdt->old_swapchain = NULL;
    cdt->surface = VK_NULL_HANDLE;
@@ -309,8 +324,11 @@ update_swapchain(struct zink_screen *screen, struct kopper_displaytarget *cdt, u
    struct kopper_swapchain *cswap = kopper_CreateSwapchain(screen, cdt, w, h, &error);
    if (!cswap)
       return error;
-   destroy_swapchain(screen, cdt->old_swapchain);
-   cdt->old_swapchain = cdt->swapchain;
+   prune_old_swapchains(screen, cdt, false);
+   struct kopper_swapchain **pswap = &cdt->old_swapchain;
+   while (*pswap)
+      *pswap = (*pswap)->next;
+   *pswap = cdt->swapchain;
    cdt->swapchain = cswap;
 
    return kopper_GetSwapchainImages(screen, cdt->swapchain);

src/gallium/drivers/zink/zink_kopper.h
@@ -30,6 +30,7 @@
 #include "kopper_interface.h"
 
 struct kopper_swapchain {
+   struct kopper_swapchain *next;
    VkSwapchainKHR swapchain;
    VkImage *images;
    bool *inits;