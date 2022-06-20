openharmony/third_party_mesa3d

commit 02a5097377a689396df617abe5dbe645400326f3

intel/dev: Read hwconfig from i915
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14511>

src/intel/dev/intel_device_info.c
@@ -31,6 +31,7 @@
 #include <xf86drm.h>
 
 #include "intel_device_info.h"
+#include "intel_hwconfig.h"
 #include "intel/common/intel_gem.h"
 #include "util/bitscan.h"
 #include "util/debug.h"
@@ -1877,6 +1878,8 @@ intel_get_device_info_from_fd(int fd, struct intel_device_info *devinfo)
       devinfo->gtt_size = 2ull * 1024 * 1024 * 1024;
       return true;
    }
+
+   intel_get_and_process_hwconfig_table(fd, devinfo);
 
    int timestamp_frequency;
    if (getparam(fd, I915_PARAM_CS_TIMESTAMP_FREQUENCY,

src/intel/dev/intel_hwconfig.c
@@ -24,6 +24,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "drm-uapi/i915_drm.h"
 #include "intel_device_info.h"
 #include "intel_hwconfig.h"
 #include "intel_hwconfig_types.h"
@@ -271,7 +272,7 @@ apply_hwconfig_item(struct intel_device_info *devinfo,
    }
 }
 
-void
+static void
 intel_apply_hwconfig_table(struct intel_device_info *devinfo,
                            const struct hwconfig *hwconfig,
                            int32_t hwconfig_len)
@@ -283,6 +284,20 @@ intel_apply_hwconfig_table(struct intel_device_info *devinfo,
    if (devinfo->apply_hwconfig) {
       devinfo->max_cs_threads =
          devinfo->max_eus_per_subslice * devinfo->num_thread_per_eu;
+   }
+}
+
+void
+intel_get_and_process_hwconfig_table(int fd,
+                                     struct intel_device_info *devinfo)
+{
+   struct hwconfig *hwconfig;
+   int32_t hwconfig_len = 0;
+   hwconfig = intel_i915_query_alloc(fd, DRM_I915_QUERY_HWCONFIG_BLOB,
+                                     &hwconfig_len);
+   if (hwconfig) {
+      intel_apply_hwconfig_table(devinfo, hwconfig, hwconfig_len);
+      free(hwconfig);
    }
 }
 

src/intel/dev/intel_hwconfig.h
@@ -36,9 +36,8 @@ struct hwconfig;
 struct intel_device_info;
 
 void
-intel_apply_hwconfig_table(struct intel_device_info *devinfo,
-                           const struct hwconfig *hwconfig,
-                           int32_t hwconfig_len);
+intel_get_and_process_hwconfig_table(int fd,
+                                     struct intel_device_info *devinfo);
 void
 intel_print_hwconfig_table(const struct hwconfig *hwconfig,
                            int32_t hwconfig_len);

commit 9f31f1fde3fed5b6d7248db084e5b0d35b69db3c

intel/dev: fixup max_cs_workgroup_threads after hwconfig is read
Without it we get max value from gfx12 on gfx>12.

Fixes: 02a5097377a ("intel/dev: Read hwconfig from i915")

Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17063>

src/intel/dev/intel_device_info.c
@@ -1899,7 +1899,13 @@ intel_get_device_info_from_fd(int fd, struct intel_device_info *devinfo)
       return true;
    }
 
-   intel_get_and_process_hwconfig_table(fd, devinfo);
+   if (intel_get_and_process_hwconfig_table(fd, devinfo)) {
+      /* After applying hwconfig values, some items need to be recalculated. */
+      devinfo->max_cs_threads =
+         devinfo->max_eus_per_subslice * devinfo->num_thread_per_eu;
+
+      update_cs_workgroup_threads(devinfo);
+   }
 
    int timestamp_frequency;
    if (getparam(fd, I915_PARAM_CS_TIMESTAMP_FREQUENCY,

src/intel/dev/intel_hwconfig.c
@@ -272,22 +272,7 @@ apply_hwconfig_item(struct intel_device_info *devinfo,
    }
 }
 
-static void
-intel_apply_hwconfig_table(struct intel_device_info *devinfo,
-                           const struct hwconfig *hwconfig,
-                           int32_t hwconfig_len)
-{
-   intel_process_hwconfig_table(devinfo, hwconfig, hwconfig_len,
-                                apply_hwconfig_item);
-
-   /* After applying hwconfig values, some items need to be recalculated. */
-   if (devinfo->apply_hwconfig) {
-      devinfo->max_cs_threads =
-         devinfo->max_eus_per_subslice * devinfo->num_thread_per_eu;
-   }
-}
-
-void
+bool
 intel_get_and_process_hwconfig_table(int fd,
                                      struct intel_device_info *devinfo)
 {
@@ -296,9 +281,14 @@ intel_get_and_process_hwconfig_table(int fd,
    hwconfig = intel_i915_query_alloc(fd, DRM_I915_QUERY_HWCONFIG_BLOB,
                                      &hwconfig_len);
    if (hwconfig) {
-      intel_apply_hwconfig_table(devinfo, hwconfig, hwconfig_len);
+      intel_process_hwconfig_table(devinfo, hwconfig, hwconfig_len,
+                                   apply_hwconfig_item);
       free(hwconfig);
+      if (devinfo->apply_hwconfig)
+         return true;
    }
+
+   return false;
 }
 
 static void

src/intel/dev/intel_hwconfig.h
@@ -34,7 +34,7 @@ extern "C" {
 
 struct intel_device_info;
 
-void
+bool
 intel_get_and_process_hwconfig_table(int fd,
                                      struct intel_device_info *devinfo);
 void