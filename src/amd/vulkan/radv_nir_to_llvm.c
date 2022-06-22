openharmony/third_party_mesa3d

commit b7b716fd420e3a516765048e54ed3f952059d7cc

ac,radeonsi: don't export null from PS if it has no effect on gfx10+
We just need to pass the uses_discard flag to the epilog.

The hw skips the export anyway. This will hang if SPI registers declare
an output format or KILL_ENABLE is set because those cases require
an export with done=1.

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16885>

src/amd/vulkan/radv_nir_to_llvm.c
@@ -1797,7 +1797,7 @@ handle_fs_outputs_post(struct radv_shader_context *ctx)
    if (depth || stencil || samplemask)
       radv_export_mrt_z(ctx, depth, stencil, samplemask);
    else if (!index)
-      ac_build_export_null(&ctx->ac);
+      ac_build_export_null(&ctx->ac, ctx->shader_info->ps.can_discard);
 }
 
 static void

src/amd/llvm/ac_llvm_build.c
@@ -1996,9 +1996,15 @@ void ac_build_export(struct ac_llvm_context *ctx, struct ac_export_args *a)
    }
 }
 
-void ac_build_export_null(struct ac_llvm_context *ctx)
+void ac_build_export_null(struct ac_llvm_context *ctx, bool uses_discard)
 {
    struct ac_export_args args;
+
+   /* Gfx10+ doesn't need to export anything if we don't need to export the EXEC mask
+    * for discard.
+    */
+   if (ctx->gfx_level >= GFX10 && !uses_discard)
+      return;
 
    args.enabled_channels = 0x0; /* enabled channels */
    args.valid_mask = 1;         /* whether the EXEC mask is valid */

src/amd/llvm/ac_llvm_build.h
@@ -342,7 +342,7 @@ struct ac_export_args {
 
 void ac_build_export(struct ac_llvm_context *ctx, struct ac_export_args *a);
 
-void ac_build_export_null(struct ac_llvm_context *ctx);
+void ac_build_export_null(struct ac_llvm_context *ctx, bool uses_discard);
 
 enum ac_image_opcode
 {

src/gallium/drivers/radeonsi/si_shader.c
@@ -2222,6 +2222,7 @@ void si_get_ps_epilog_key(struct si_shader *shader, union si_shader_part_key *ke
    struct si_shader_info *info = &shader->selector->info;
    memset(key, 0, sizeof(*key));
    key->ps_epilog.wave32 = shader->wave_size == 32;
+   key->ps_epilog.uses_discard = si_shader_uses_discard(shader);
    key->ps_epilog.colors_written = info->colors_written;
    key->ps_epilog.color_types = info->output_color_types;
    key->ps_epilog.writes_z = info->writes_z;

src/gallium/drivers/radeonsi/si_shader.h
@@ -642,6 +642,7 @@ union si_shader_part_key {
    struct {
       struct si_ps_epilog_bits states;
       unsigned wave32 : 1;
+      unsigned uses_discard : 1;
       unsigned colors_written : 8;
       unsigned color_types : 16;
       unsigned writes_z : 1;
@@ -1051,6 +1052,14 @@ static inline bool si_shader_uses_streamout(struct si_shader *shader)
    return shader->selector->stage <= MESA_SHADER_GEOMETRY &&
           shader->selector->info.enabled_streamout_buffer_mask &&
           !shader->key.ge.opt.remove_streamout;
+}
+
+static inline bool si_shader_uses_discard(struct si_shader *shader)
+{
+   /* Changes to this should also update ps_modifies_zs. */
+   return shader->selector->info.base.fs.uses_discard ||
+          shader->key.ps.part.prolog.poly_stipple ||
+          shader->key.ps.part.epilog.alpha_func != PIPE_FUNC_ALWAYS;
 }
 
 #ifdef __cplusplus

src/gallium/drivers/radeonsi/si_shader_llvm_ps.c
@@ -946,7 +946,7 @@ void si_llvm_build_ps_epilog(struct si_shader_context *ctx, union si_shader_part
       for (unsigned i = 0; i < exp.num; i++)
          ac_build_export(&ctx->ac, &exp.args[i]);
    } else {
-      ac_build_export_null(&ctx->ac);
+      ac_build_export_null(&ctx->ac, key->ps_epilog.uses_discard);
    }
 
    /* Compile. */

src/gallium/drivers/radeonsi/si_state_shaders.cpp
@@ -1871,10 +1871,7 @@ static void si_shader_ps(struct si_screen *sscreen, struct si_shader *shader)
       S_02880C_Z_EXPORT_ENABLE(info->writes_z) |
       S_02880C_STENCIL_TEST_VAL_EXPORT_ENABLE(info->writes_stencil) |
       S_02880C_MASK_EXPORT_ENABLE(info->writes_samplemask) |
-      /* Changes KILL_ENABLE should also update ps_modifies_zs. */
-      S_02880C_KILL_ENABLE(info->base.fs.uses_discard ||
-                           shader->key.ps.part.prolog.poly_stipple ||
-                           shader->key.ps.part.epilog.alpha_func != PIPE_FUNC_ALWAYS);
+      S_02880C_KILL_ENABLE(si_shader_uses_discard(shader));
 
    switch (info->base.fs.depth_layout) {
    case FRAG_DEPTH_LAYOUT_GREATER:

commit 731b2809a8feeefd90f6bfecfa34c268098999e5

radv/llvm: always emit a null export even if the FS doesn't discard
Even with a noop FS, the color blend state can still be non-zero, and
then SPI color related registers won't be 0 and this would hang.

Fixes: b7b716fd420 ("ac,radeonsi: don't export null from PS if it has no effect on gfx10+")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17169>

src/amd/vulkan/radv_nir_to_llvm.c
@@ -1797,7 +1797,7 @@ handle_fs_outputs_post(struct radv_shader_context *ctx)
    if (depth || stencil || samplemask)
       radv_export_mrt_z(ctx, depth, stencil, samplemask);
    else if (!index)
-      ac_build_export_null(&ctx->ac, ctx->shader_info->ps.can_discard);
+      ac_build_export_null(&ctx->ac, true);
 }
 
 static void