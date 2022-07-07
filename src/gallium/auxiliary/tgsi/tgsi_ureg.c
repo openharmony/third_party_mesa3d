openharmony/third_party_mesa3d

commit 773a10e6974e51b329f8de0331b2fef3e19c9ea3

tgsi: Add SEPARABLE_PROGRAM property
Signed-off-by: Italo Nicola <italonicola@collabora.com>
Reviewed-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16308>

src/gallium/auxiliary/tgsi/tgsi_ureg.c
@@ -2408,6 +2408,9 @@ ureg_setup_shader_info(struct ureg_program *ureg,
    if (info->layer_viewport_relative)
       ureg_property(ureg, TGSI_PROPERTY_LAYER_VIEWPORT_RELATIVE, 1);
 
+   if (info->separate_shader)
+      ureg_property(ureg, TGSI_PROPERTY_SEPARABLE_PROGRAM, 1);
+
    switch (info->stage) {
    case MESA_SHADER_VERTEX:
       ureg_setup_clipdist_info(ureg, info);

src/gallium/auxiliary/tgsi/tgsi_strings.c
@@ -165,6 +165,7 @@ const char *tgsi_property_names[TGSI_PROPERTY_COUNT] =
    "CS_USER_DATA_COMPONENTS_AMD",
    "LAYER_VIEWPORT_RELATIVE",
    "FS_BLEND_EQUATION_ADVANCED",
+   "SEPARABLE_PROGRAM",
 };
 
 const char *tgsi_return_type_names[TGSI_RETURN_TYPE_COUNT] =

src/gallium/include/pipe/p_shader_tokens.h
@@ -303,6 +303,7 @@ enum tgsi_property_name {
    TGSI_PROPERTY_CS_USER_DATA_COMPONENTS_AMD,
    TGSI_PROPERTY_LAYER_VIEWPORT_RELATIVE,
    TGSI_PROPERTY_FS_BLEND_EQUATION_ADVANCED,
+   TGSI_PROPERTY_SEPARABLE_PROGRAM,
    TGSI_PROPERTY_COUNT,
 };
 

commit 49288c15308ea17bf2b49ec23af5f337274f7a6c

tgsi: Don't bother setting the TGSI_PROPERTY_SEPARABLE_PROGRAM
Most drivers don't care about the property, and virgl should only handle
it if the host supports it.

This is a partial revert of 773a10e, i.e. we keep the definition of the
property, but we don't set it only based on the shader info.

Fixes: 773a10e6974e51b329f8de0331b2fef3e19c9ea3
  tgsi: Add SEPARABLE_PROGRAM property

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17344>

src/gallium/auxiliary/tgsi/tgsi_ureg.c
@@ -2408,9 +2408,6 @@ ureg_setup_shader_info(struct ureg_program *ureg,
    if (info->layer_viewport_relative)
       ureg_property(ureg, TGSI_PROPERTY_LAYER_VIEWPORT_RELATIVE, 1);
 
-   if (info->separate_shader)
-      ureg_property(ureg, TGSI_PROPERTY_SEPARABLE_PROGRAM, 1);
-
    switch (info->stage) {
    case MESA_SHADER_VERTEX:
       ureg_setup_clipdist_info(ureg, info);