openharmony/third_party_mesa3d

commit dbaf8def7b7826987394ed97fac481d1619757d2

wgl: Refactor drivers to a libgallium_wgl.dll
I'd like to support EGL on Windows, using the same architecture as Linux.
On Linux, libgallium_dri.so is a "megadriver," containing the Mesa Gallium
state tracker, plus the actual driver implementations.

Now, on Windows, libgallium_wgl.dll is a proper OpenGL ICD, and OpenGL32.dll
is just a stub DLL which implements the wgl* APIs in terms of that ICD.

This is the more "architecturally clean" way to share the state tracker
between EGL and WGL.

Reviewed By: Bill Kristiansen <billkris@microsoft.com>

Reviewed-by: Charmaine Lee >charmainel@vmware.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12677>

src/gallium/targets/libgl-gdi/meson.build
@@ -28,18 +28,16 @@ ogldef = files('opengl32.def')[0]
 
 libopengl32 = shared_library(
   'opengl32',
-  ['libgl_gdi.c', 'stw_wgl.c'],
+  ['stw_wgl.c'],
   vs_module_defs : ogldef,
   include_directories : [
-    inc_include, inc_src, inc_mapi, inc_mesa, inc_gallium, inc_gallium_aux, inc_wgl, inc_gallium_winsys, inc_gallium_winsys_sw, inc_gallium_drivers,
+    inc_include, inc_wgl, inc_src,
   ],
-  link_whole : [libwgl],
   link_with : [
-    libgallium, libglsl, libmesa_gallium, libwsgdi, libglapi_static, libglapi
+    libgallium_wgl, libglapi_static, libglapi
   ],
   dependencies : [
-    dep_ws2_32, idep_nir, idep_mesautil, driver_swrast, driver_swr,
-    driver_d3d12, driver_zink, winsys_d3d12_wgl
+    idep_mesautil
   ],
   c_args : ['-D_GDI32_'],
   link_args : opengl32_link_args,
@@ -69,6 +67,7 @@ if with_tests
   # The CI pipeline for MinGW doesn't support creating a window, so don't run these tests there
   if with_tests and cc.get_id() != 'gcc'
     wgl_test_env = environment()
+    wgl_test_env.append('PATH', libgallium_wgl_build_dir)
     if with_shared_glapi
       wgl_test_env.append('PATH', libglapi_build_dir)
     endif

src/gallium/meson.build
@@ -240,6 +240,7 @@ if with_platform_windows
   else
     winsys_d3d12_wgl = declare_dependency()
   endif
+  subdir('targets/wgl')
   subdir('targets/libgl-gdi')
 endif
 if with_tests

src/gallium/targets/wgl/meson.build
@@ -1,0 +1,48 @@
+# Copyright © Microsoft Corporation
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+gallium_wgl_link_args = []
+if cc.get_id() == 'gcc' and host_machine.cpu_family() != 'x86_64'
+  gallium_wgl_link_args += ['-Wl,--enable-stdcall-fixup']
+endif
+
+wgldef = files('gallium_wgl.def')[0]
+
+libgallium_wgl = shared_library(
+  'gallium_wgl',
+  ['wgl.c'],
+  vs_module_defs : wgldef,
+  include_directories : [
+    inc_include, inc_src, inc_mapi, inc_mesa, inc_gallium, inc_gallium_aux, inc_wgl, inc_gallium_winsys, inc_gallium_winsys_sw, inc_gallium_drivers,
+  ],
+  link_whole : [libwgl],
+  link_with : [
+    libgallium, libglsl, libmesa_gallium, libwsgdi, libglapi_static, libglapi
+  ],
+  dependencies : [
+    dep_ws2_32, idep_nir, idep_mesautil, driver_swrast, driver_swr,
+    driver_d3d12, driver_zink, winsys_d3d12_wgl
+  ],
+  link_args : gallium_wgl_link_args,
+  name_prefix : 'lib',
+  install : true,
+)
+libgallium_wgl_build_dir = meson.current_build_dir()

.gitlab-ci/windows/piglit_run.ps1
@@ -1,6 +1,7 @@
 $env:PIGLIT_NO_FAST_SKIP = 1
 
 Copy-Item -Path _install\bin\opengl32.dll -Destination C:\Piglit\lib\piglit\bin\opengl32.dll
+Copy-Item -Path _install\bin\libgallium_wgl.dll -Destination C:\Piglit\lib\piglit\bin\libgallium_wgl.dll
 
 # Run this using VsDevCmd.bat to ensure DXIL.dll is in %PATH%
 cmd.exe /C "C:\BuildTools\Common7\Tools\VsDevCmd.bat -host_arch=amd64 -arch=amd64 && py -3 C:\Piglit\bin\piglit.py run `"$env:PIGLIT_PROFILE`" $env:PIGLIT_OPTIONS $env:PIGLIT_TESTS .\results"

src/gallium/targets/wgl/gallium_wgl.def
@@ -1,0 +1,26 @@
+EXPORTS
+	DrvCopyContext
+	DrvCreateContext
+	DrvCreateLayerContext
+	DrvDeleteContext
+	DrvDescribeLayerPlane
+	DrvDescribePixelFormat
+	DrvGetLayerPaletteEntries
+	DrvGetProcAddress
+	DrvPresentBuffers
+	DrvRealizeLayerPalette
+	DrvReleaseContext
+	DrvSetCallbackProcs
+	DrvSetContext
+	DrvSetLayerPaletteEntries
+	DrvSetPixelFormat
+	DrvShareLists
+	DrvSwapBuffers
+	DrvSwapLayerBuffers
+	DrvValidateVersion
+
+	stw_get_current_context
+	stw_get_current_dc
+	stw_pixelformat_choose
+	stw_pixelformat_get
+	stw_override_opengl32_entry_points

src/gallium/targets/wgl/wgl.c
@@ -83,7 +83,7 @@ static boolean use_zink = FALSE;
 static const char *created_driver_name = NULL;
 
 static struct pipe_screen *
-gdi_screen_create_by_name(HDC hDC, const char* driver, struct sw_winsys *winsys)
+wgl_screen_create_by_name(HDC hDC, const char* driver, struct sw_winsys *winsys)
 {
    struct pipe_screen* screen = NULL;
 
@@ -125,7 +125,7 @@ gdi_screen_create_by_name(HDC hDC, const char* driver, struct sw_winsys *winsys)
 }
 
 static struct pipe_screen *
-gdi_screen_create(HDC hDC)
+wgl_screen_create(HDC hDC)
 {
    struct sw_winsys *winsys;
    UNUSED bool sw_only = env_var_as_boolean("LIBGL_ALWAYS_SOFTWARE", false);
@@ -154,7 +154,7 @@ gdi_screen_create(HDC hDC)
     * sorted list. Don't do this if GALLIUM_DRIVER is specified.
     */
    for (unsigned i = 0; i < ARRAY_SIZE(drivers); ++i) {
-      struct pipe_screen* screen = gdi_screen_create_by_name(hDC, drivers[i], winsys);
+      struct pipe_screen* screen = wgl_screen_create_by_name(hDC, drivers[i], winsys);
       if (screen) {
          created_driver_name = drivers[i];
          return screen;
@@ -169,7 +169,7 @@ gdi_screen_create(HDC hDC)
 
 
 static void
-gdi_present(struct pipe_screen *screen,
+wgl_present(struct pipe_screen *screen,
             struct pipe_context *ctx,
             struct pipe_resource *res,
             HDC hDC)
@@ -227,7 +227,7 @@ gdi_present(struct pipe_screen *screen,
 
 #if WINVER >= 0xA00
 static boolean
-gdi_get_adapter_luid(struct pipe_screen* screen,
+wgl_get_adapter_luid(struct pipe_screen* screen,
    HDC hDC,
    LUID* adapter_luid)
 {
@@ -241,7 +241,7 @@ gdi_get_adapter_luid(struct pipe_screen* screen,
 
 
 static unsigned
-gdi_get_pfd_flags(struct pipe_screen *screen)
+wgl_get_pfd_flags(struct pipe_screen *screen)
 {
 #ifdef GALLIUM_D3D12
    if (use_d3d12)
@@ -252,7 +252,7 @@ gdi_get_pfd_flags(struct pipe_screen *screen)
 
 
 static struct stw_winsys_framebuffer *
-gdi_create_framebuffer(struct pipe_screen *screen,
+wgl_create_framebuffer(struct pipe_screen *screen,
                        HDC hDC,
                        int iPixelFormat)
 {
@@ -264,26 +264,26 @@ gdi_create_framebuffer(struct pipe_screen *screen,
 }
 
 static const char *
-gdi_get_name(void)
+wgl_get_name(void)
 {
    return created_driver_name;
 }
 
 
 static const struct stw_winsys stw_winsys = {
-   &gdi_screen_create,
-   &gdi_present,
+   &wgl_screen_create,
+   &wgl_present,
 #if WINVER >= 0xA00
-   &gdi_get_adapter_luid,
+   &wgl_get_adapter_luid,
 #else
    NULL, /* get_adapter_luid */
 #endif
    NULL, /* shared_surface_open */
    NULL, /* shared_surface_close */
    NULL, /* compose */
-   &gdi_get_pfd_flags,
-   &gdi_create_framebuffer,
-   &gdi_get_name,
+   &wgl_get_pfd_flags,
+   &wgl_create_framebuffer,
+   &wgl_get_name,
 };
 
 

commit 9de2ff18ddde52015528e6294dd6e590e45cc625

Fix static glapi on Windows
On Linux, the static glapi path sees libGL.so implementing the static
glapi, and the drivers (libgallium_dri.so) updating/reading the TLS
vars.

On Windows, to allow libgallium_wgl.dll to be a full ICD, it's
responsible for implementing the actual static glapi. However, before
this change, OpenGL32.dll was also implementing the static glapi,
meaning that GL API calls from OpenGL32.dll didn't route to the driver
correctly because the TLS vars were never actually set - the driver set
its copy, and OpenGL32.dll read its own copy.

Now, always build a bridge and static version of glapi when not using
shared. The bridge version is linked into OpenGL32.dll, and the static
version is linked into the driver on Windows. GLES only builds with
shared glapi - but after this, shared glapi is not really needed on
Windows for GLES, since the driver has all of the data.

Fixes: dbaf8def ("wgl: Refactor drivers to a libgallium_wgl.dll")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6560
Acked-by: Marek Olšák <marek.olsak@amd.com>
Reviewed-by: Dylan Baker <dylan@pnwbakers.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16713>

meson.build
@@ -550,14 +550,6 @@ if not have_mtls_dialect
   endif
 endif
 
-if with_platform_windows and with_shared_glapi
-  # Windows doesn't support DLL exports/imports being TLS variables. When using shared
-  # glapi, libglapi.dll hosts the TLS, but other DLLs need to use them. Instead of falling
-  # all the way back to using globals and manual per-thread data, keep using TLS but just
-  # put accesses to it behind a function so the variable doesn't need to be exported.
-  pre_args += '-DUSE_TLS_BEHIND_FUNCTIONS'
-endif
-
 if with_glx != 'disabled'
   if not (with_platform_x11 and with_any_opengl)
     error('Cannot build GLX support without X11 platform support and at least one OpenGL API')

src/gallium/targets/libgl-gdi/meson.build
@@ -38,7 +38,7 @@ libopengl32 = shared_library(
     inc_include, inc_wgl, inc_src,
   ],
   link_with : [
-    libgallium_wgl, libglapi_static, libglapi
+    libgallium_wgl, libglapi_bridge, libglapi
   ],
   dependencies : [
     idep_mesautil

src/mapi/glapi/glapi.h
@@ -91,7 +91,7 @@ _GLAPI_EXPORT extern __THREAD_INITIAL_EXEC void * _glapi_tls_Context;
 _GLAPI_EXPORT extern const struct _glapi_table *_glapi_Dispatch;
 _GLAPI_EXPORT extern const void *_glapi_Context;
 
-#if defined (USE_TLS_BEHIND_FUNCTIONS)
+#if DETECT_OS_WINDOWS && !defined(MAPI_MODE_UTIL) && !defined(MAPI_MODE_GLAPI)
 # define GET_DISPATCH() _glapi_get_dispatch()
 # define GET_CURRENT_CONTEXT(C)  struct gl_context *C = (struct gl_context *) _glapi_get_context()
 #else

src/mapi/glapi/meson.build
@@ -29,61 +29,76 @@ if with_dri and ['apple', 'windows'].contains(with_dri_platform)
   static_glapi_files += [glapi_gentable_c, glapitable_h]
 endif
 
-if with_shared_glapi
-  static_glapi_files += files(
-    '../entry.c',
-    '../entry.h',
-    '../entry_x86-64_tls.h',
-    '../entry_x86_tls.h',
-    '../entry_x86_tsd.h',
-    '../entry_ppc64le_tls.h',
-    '../entry_ppc64le_tsd.h',
-    '../mapi_tmp.h',
-  )
-  static_glapi_files += glapi_mapi_tmp_h
-  static_glapi_args += [
-    '-DMAPI_MODE_BRIDGE',
-    '-DMAPI_ABI_HEADER="@0@"'.format(glapi_mapi_tmp_h.full_path()),
-    gcc_lto_quirk,
-  ]
-  if with_platform_windows
-    static_glapi_args += ['-D_GDI32_']
-  endif
-else
-  static_glapi_args += '-DMAPI_MODE_UTIL'
-  if with_platform_windows
-    static_glapi_args += ['-D_GDI32_', '-DKHRONOS_DLL_EXPORTS', '-D_GLAPI_NO_EXPORTS']
-  endif
-  static_glapi_files += files(
-    'glapi_dispatch.c',
-    'glapi_entrypoint.c',
-    'glapi_getproc.c',
-    'glapi_nop.c',
-    'glapi.c',
-    'glapi.h',
-    'glapi_priv.h',
-  )
-  static_glapi_files += files_mapi_util
-  static_glapi_files += [
-    glapitable_h, glapi_mapi_tmp_h, glprocs_h, glapitemp_h,
-  ]
-  if with_asm_arch == 'x86'
-    static_glapi_files += glapi_x86_s
-  elif with_asm_arch == 'x86_64'
-    static_glapi_files += glapi_x86_64_s
-  elif with_asm_arch == 'sparc'
-    static_glapi_files += glapi_sparc_s
-  endif
+bridge_glapi_files = static_glapi_files
+bridge_glapi_args = static_glapi_args
+
+bridge_glapi_files += files(
+  '../entry.c',
+  '../entry.h',
+  '../entry_x86-64_tls.h',
+  '../entry_x86_tls.h',
+  '../entry_x86_tsd.h',
+  '../entry_ppc64le_tls.h',
+  '../entry_ppc64le_tsd.h',
+  '../mapi_tmp.h',
+)
+bridge_glapi_files += glapi_mapi_tmp_h
+bridge_glapi_args += [
+  '-DMAPI_MODE_BRIDGE',
+  '-DMAPI_ABI_HEADER="@0@"'.format(glapi_mapi_tmp_h.full_path()),
+  gcc_lto_quirk,
+]
+if with_platform_windows
+  bridge_glapi_args += ['-D_GDI32_']
 endif
 
-libglapi_static = static_library(
-  'glapi_static',
-  static_glapi_files,
+static_glapi_args += '-DMAPI_MODE_UTIL'
+if with_platform_windows
+  static_glapi_args += ['-D_GDI32_', '-DKHRONOS_DLL_EXPORTS', '-D_GLAPI_DLL_EXPORTS']
+endif
+static_glapi_files += files(
+  'glapi_dispatch.c',
+  'glapi_entrypoint.c',
+  'glapi_getproc.c',
+  'glapi_nop.c',
+  'glapi.c',
+  'glapi.h',
+  'glapi_priv.h',
+)
+static_glapi_files += files_mapi_util
+static_glapi_files += [
+  glapitable_h, glapi_mapi_tmp_h, glprocs_h, glapitemp_h,
+]
+if with_asm_arch == 'x86'
+  static_glapi_files += glapi_x86_s
+elif with_asm_arch == 'x86_64'
+  static_glapi_files += glapi_x86_64_s
+elif with_asm_arch == 'sparc'
+  static_glapi_files += glapi_sparc_s
+endif
+
+
+libglapi_bridge = static_library(
+  'glapi_bridge',
+  bridge_glapi_files,
   include_directories : [inc_mesa, inc_include, inc_src, inc_mapi],
-  c_args : [c_msvc_compat_args, static_glapi_args],
+  c_args : [c_msvc_compat_args, bridge_glapi_args],
   dependencies : [dep_thread, dep_selinux, idep_mesautilc11],
   build_by_default : false,
 )
+
+if with_shared_glapi
+  libglapi_static = libglapi_bridge
+else
+  libglapi_static = static_library(
+    'glapi_static',
+    static_glapi_files,
+    include_directories : [inc_mesa, inc_include, inc_src, inc_mapi],
+    c_args : [c_msvc_compat_args, static_glapi_args],
+    dependencies : [dep_thread, dep_selinux, idep_mesautilc11],
+    build_by_default : false,
+  )
+endif
 
 # TODO: this test doesn't compile on windows with mingw or msvc due to
 # undefined symbols from libglapi_static, but that should be fixable.