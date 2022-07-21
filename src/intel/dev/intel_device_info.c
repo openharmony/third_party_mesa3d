openharmony/third_party_mesa3d

commit e7838cd4670dd27313a83af47b6dfe117c1651e5

intel/dev: Add devinfo::mem to store i915 regions information
Reworks:
 * Lionel: Change check on memory region valid to vram size
 * Jordan: Drop regions.valid (Lionel implemented a fallback)
 * Jordan: Rename devinfo::regions to devinfo::mem.
 * Jordan: Add devinfo::mem::use_class_instance
 * Add mesa_logw for lmem requiring regions. (s-b Lionel)

Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17075>

src/intel/dev/intel_device_info.c
@@ -1587,6 +1587,45 @@ query_topology(struct intel_device_info *devinfo, int fd)
 
 }
 
+/**
+ * Reports memory region info, and allows buffers to target system-memory,
+ * and/or device local memory.
+ */
+static bool
+query_regions(struct intel_device_info *devinfo, int fd)
+{
+   struct drm_i915_query_memory_regions *meminfo =
+      intel_i915_query_alloc(fd, DRM_I915_QUERY_MEMORY_REGIONS, NULL);
+   if (meminfo == NULL)
+      return false;
+
+   for (int i = 0; i < meminfo->num_regions; i++) {
+      const struct drm_i915_memory_region_info *mem = &meminfo->regions[i];
+      switch (mem->region.memory_class) {
+      case I915_MEMORY_CLASS_SYSTEM:
+         devinfo->mem.sram.mem_class = mem->region.memory_class;
+         devinfo->mem.sram.mem_instance = mem->region.memory_instance;
+         devinfo->mem.sram.mappable.size = mem->probed_size;
+         if (mem->unallocated_size != -1)
+            devinfo->mem.sram.mappable.free = mem->unallocated_size;
+         break;
+      case I915_MEMORY_CLASS_DEVICE:
+         devinfo->mem.vram.mem_class = mem->region.memory_class;
+         devinfo->mem.vram.mem_instance = mem->region.memory_instance;
+         devinfo->mem.vram.mappable.size = mem->probed_size;
+         if (mem->unallocated_size != -1)
+            devinfo->mem.vram.mappable.free = mem->unallocated_size;
+         break;
+      default:
+         break;
+      }
+   }
+
+   free(meminfo);
+   devinfo->mem.use_class_instance = true;
+   return true;
+}
+
 static int
 intel_get_aperture_size(int fd, uint64_t *size)
 {
@@ -1929,6 +1968,14 @@ intel_get_device_info_from_fd(int fd, struct intel_device_info *devinfo)
        * will be wrong, affecting GPU metrics. In this case, fail silently.
        */
       getparam_topology(devinfo, fd);
+   }
+
+   query_regions(devinfo, fd);
+
+   /* region info is required for lmem support */
+   if (devinfo->has_local_mem && !devinfo->mem.use_class_instance) {
+      mesa_logw("Could not query local memory size.");
+      return false;
    }
 
    if (devinfo->platform == INTEL_PLATFORM_CHV)

src/intel/dev/intel_device_info.h
@@ -391,6 +391,18 @@ struct intel_device_info
     * apply_hwconfig is true when the platform should apply hwconfig values
     */
    bool apply_hwconfig;
+
+   struct {
+      bool use_class_instance;
+      struct {
+         uint16_t mem_class;
+         uint16_t mem_instance;
+         struct {
+            uint64_t size;
+            uint64_t free;
+         } mappable;
+      } sram, vram;
+   } mem;
    /** @} */
 };
 

commit eef4c2b1ad3933892a91a7d97547f9d0735f0cf9

intel/dev: deal with i915 unallocated_size on smem
We cannot rely on unallocated_size on system memory for
VK_EXT_memory_budget.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: e7838cd4670d ("intel/dev: Add devinfo::mem to store i915 regions information")
Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17349>

src/intel/dev/intel_device_info.c
@@ -1603,7 +1603,7 @@ query_regions(struct intel_device_info *devinfo, int fd, bool update)
    for (int i = 0; i < meminfo->num_regions; i++) {
       const struct drm_i915_memory_region_info *mem = &meminfo->regions[i];
       switch (mem->region.memory_class) {
-      case I915_MEMORY_CLASS_SYSTEM:
+      case I915_MEMORY_CLASS_SYSTEM: {
          if (!update) {
             devinfo->mem.sram.mem_class = mem->region.memory_class;
             devinfo->mem.sram.mem_instance = mem->region.memory_instance;
@@ -1613,9 +1613,14 @@ query_regions(struct intel_device_info *devinfo, int fd, bool update)
             assert(devinfo->mem.sram.mem_instance == mem->region.memory_instance);
             assert(devinfo->mem.sram.mappable.size == mem->probed_size);
          }
-         if (mem->unallocated_size != -1)
-            devinfo->mem.sram.mappable.free = mem->unallocated_size;
+         /* The kernel uAPI only reports an accurate unallocated_size value
+          * for I915_MEMORY_CLASS_DEVICE.
+          */
+         uint64_t available;
+         if (os_get_available_system_memory(&available))
+            devinfo->mem.sram.mappable.free = MIN2(available, mem->probed_size);
          break;
+      }
       case I915_MEMORY_CLASS_DEVICE:
          if (!update) {
             devinfo->mem.vram.mem_class = mem->region.memory_class;