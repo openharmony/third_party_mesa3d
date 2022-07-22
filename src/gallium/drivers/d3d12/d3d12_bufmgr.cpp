openharmony/third_party_mesa3d

commit 8c893937caa236b9020091bc6b99bbe0ca44a2e0

d3d12: Add residency info to d3d12_bo
This is all currently immutable, but will be used to manage the
residency of the underlying D3D objects in a future commit.

Reviewed-by: Bill Kristiansen <billkris@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14959>

src/gallium/drivers/d3d12/d3d12_bufmgr.cpp
@@ -39,7 +39,7 @@
 struct d3d12_bufmgr {
    struct pb_manager base;
 
-   ID3D12Device *dev;
+   struct d3d12_screen *screen;
 };
 
 extern const struct pb_vtbl d3d12_buffer_vtbl;
@@ -72,7 +72,7 @@ create_trans_state(ID3D12Resource *res, enum pipe_format format)
 }
 
 struct d3d12_bo *
-d3d12_bo_wrap_res(ID3D12Resource *res, enum pipe_format format)
+d3d12_bo_wrap_res(struct d3d12_screen *screen, ID3D12Resource *res, enum pipe_format format, enum d3d12_residency_status residency)
 {
    struct d3d12_bo *bo;
 
@@ -84,12 +84,23 @@ d3d12_bo_wrap_res(ID3D12Resource *res, enum pipe_format format)
    bo->res = res;
    bo->trans_state = create_trans_state(res, format);
 
+   bo->residency_status = residency;
+   bo->last_used_timestamp = 0;
+   D3D12_RESOURCE_DESC desc = res->GetDesc();
+   screen->dev->GetCopyableFootprints(&desc, 0, bo->trans_state->NumSubresources(), 0, nullptr, nullptr, nullptr, &bo->estimated_size);
+   if (residency != d3d12_evicted) {
+      mtx_lock(&screen->submit_mutex);
+      list_add(&bo->residency_list_entry, &screen->residency_list);
+      mtx_unlock(&screen->submit_mutex);
+   }
+
    return bo;
 }
 
 struct d3d12_bo *
-d3d12_bo_new(ID3D12Device *dev, uint64_t size, const pb_desc *pb_desc)
+d3d12_bo_new(struct d3d12_screen *screen, uint64_t size, const pb_desc *pb_desc)
 {
+   ID3D12Device *dev = screen->dev;
    ID3D12Resource *res;
 
    D3D12_RESOURCE_DESC res_desc;
@@ -122,7 +133,7 @@ d3d12_bo_new(ID3D12Device *dev, uint64_t size, const pb_desc *pb_desc)
    if (FAILED(hres))
       return NULL;
 
-   return d3d12_bo_wrap_res(res, PIPE_FORMAT_NONE);
+   return d3d12_bo_wrap_res(screen, res, PIPE_FORMAT_NONE, d3d12_resident);
 }
 
 struct d3d12_bo *
@@ -155,6 +166,9 @@ d3d12_bo_unreference(struct d3d12_bo *bo)
       } else {
          delete bo->trans_state;
          bo->res->Release();
+         if (bo->residency_status != d3d12_evicted) {
+            list_del(&bo->residency_list_entry);
+         }
       }
       FREE(bo);
    }
@@ -289,7 +303,7 @@ d3d12_bufmgr_create_buffer(struct pb_manager *pmgr,
    buf->range.Begin = 0;
    buf->range.End = size;
 
-   buf->bo = d3d12_bo_new(mgr->dev, size, pb_desc);
+   buf->bo = d3d12_bo_new(mgr->screen, size, pb_desc);
    if (!buf->bo) {
       FREE(buf);
       return NULL;
@@ -341,7 +355,7 @@ d3d12_bufmgr_create(struct d3d12_screen *screen)
    mgr->base.flush = d3d12_bufmgr_flush;
    mgr->base.is_buffer_busy = d3d12_bufmgr_is_buffer_busy;
 
-   mgr->dev = screen->dev;
+   mgr->screen = screen;
 
    return &mgr->base;
 }

src/gallium/drivers/d3d12/d3d12_bufmgr.h
@@ -26,6 +26,7 @@
 
 #include "pipebuffer/pb_buffer.h"
 #include "util/u_atomic.h"
+#include "util/list.h"
 
 #ifndef _WIN32
 #include <wsl/winadapter.h>
@@ -38,11 +39,23 @@ struct d3d12_screen;
 struct pb_manager;
 struct TransitionableResourceState;
 
+enum d3d12_residency_status {
+   d3d12_evicted,
+   d3d12_resident,
+   d3d12_permanently_resident,
+};
+
 struct d3d12_bo {
    int refcount;
    ID3D12Resource *res;
    struct pb_buffer *buffer;
    struct TransitionableResourceState *trans_state;
+
+   struct list_head residency_list_entry;
+   uint64_t estimated_size;
+   uint64_t last_used_timestamp;
+   uint64_t last_used_fence;
+   enum d3d12_residency_status residency_status;
 };
 
 struct d3d12_buffer {
@@ -96,10 +109,10 @@ d3d12_bo_is_suballocated(struct d3d12_bo *bo)
 }
 
 struct d3d12_bo *
-d3d12_bo_new(ID3D12Device *dev, uint64_t size, uint64_t alignment);
+d3d12_bo_new(struct d3d12_screen *screen, uint64_t size, uint64_t alignment);
 
 struct d3d12_bo *
-d3d12_bo_wrap_res(ID3D12Resource *res, enum pipe_format format);
+d3d12_bo_wrap_res(struct d3d12_screen *screen, ID3D12Resource *res, enum pipe_format format, enum d3d12_residency_status residency);
 
 struct d3d12_bo *
 d3d12_bo_wrap_buffer(struct pb_buffer *buf);

src/gallium/drivers/d3d12/d3d12_resource.cpp
@@ -269,7 +269,7 @@ init_texture(struct d3d12_screen *screen,
                                              &res->dt_stride);
    }
 
-   res->bo = d3d12_bo_wrap_res(d3d12_res, templ->format);
+   res->bo = d3d12_bo_wrap_res(screen, d3d12_res, templ->format, d3d12_resident);
 
    return true;
 }
@@ -547,7 +547,7 @@ d3d12_resource_from_handle(struct pipe_screen *pscreen,
    res->dxgi_format = d3d12_get_format(res->overall_format);
 
    if (!res->bo) {
-      res->bo = d3d12_bo_wrap_res(d3d12_res, res->overall_format);
+      res->bo = d3d12_bo_wrap_res(screen, d3d12_res, res->overall_format, d3d12_permanently_resident);
    }
    init_valid_range(res);
 

src/gallium/drivers/d3d12/d3d12_screen.cpp
@@ -1188,6 +1188,8 @@ d3d12_init_screen(struct d3d12_screen *screen, struct sw_winsys *winsys, IUnknow
    if (FAILED(screen->dev->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&screen->fence))))
       goto failed;
 
+   list_inithead(&screen->residency_list);
+
    UINT64 timestamp_freq;
    if (FAILED(screen->cmdqueue->GetTimestampFrequency(&timestamp_freq)))
        timestamp_freq = 10000000;

src/gallium/drivers/d3d12/d3d12_screen.h
@@ -66,6 +66,7 @@ struct d3d12_screen {
    mtx_t submit_mutex;
    ID3D12Fence *fence;
    uint64_t fence_value;
+   struct list_head residency_list;
 
    struct slab_parent_pool transfer_pool;
    struct pb_manager *bufmgr;

commit f64af689bfbd2af6a3cb5a50b8b79aea39b8ba87

d3d12: Hold lock when removing resources from residency list
Also, remove them from the list before releasing the ID3D12Resource.

Fixes: 8c893937 ("d3d12: Add residency info to d3d12_bo")
Reviewed-by: Bill Kristiansen <billkris@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17688>

src/gallium/drivers/d3d12/d3d12_bufmgr.cpp
@@ -106,6 +106,7 @@ d3d12_bo_wrap_res(struct d3d12_screen *screen, ID3D12Resource *res, enum pipe_fo
       return NULL;
 
    pipe_reference_init(&bo->reference, 1);
+   bo->screen = screen;
    bo->res = res;
    bo->trans_state = create_trans_state(res, format);
    bo->unique_id = p_atomic_inc_return(&screen->resource_id_generator);
@@ -177,6 +178,7 @@ d3d12_bo_wrap_buffer(struct d3d12_screen *screen, struct pb_buffer *buf)
       return NULL;
 
    pipe_reference_init(&bo->reference, 1);
+   bo->screen = screen;
    bo->buffer = buf;
    bo->trans_state = NULL; /* State from base BO will be used */
    bo->unique_id = p_atomic_inc_return(&screen->resource_id_generator);
@@ -198,11 +200,13 @@ d3d12_bo_unreference(struct d3d12_bo *bo)
       if (bo->buffer) {
          pb_reference(&bo->buffer, NULL);
       } else {
-         delete bo->trans_state;
-         bo->res->Release();
+         mtx_lock(&bo->screen->submit_mutex);
          if (bo->residency_status != d3d12_evicted) {
             list_del(&bo->residency_list_entry);
          }
+         mtx_unlock(&bo->screen->submit_mutex);
+         delete bo->trans_state;
+         bo->res->Release();
       }
       FREE(bo);
    }

src/gallium/drivers/d3d12/d3d12_bufmgr.h
@@ -43,6 +43,7 @@ enum d3d12_residency_status {
 
 struct d3d12_bo {
    struct pipe_reference reference;
+   struct d3d12_screen *screen;
    ID3D12Resource *res;
    struct pb_buffer *buffer;
    struct TransitionableResourceState *trans_state;