openharmony/third_party_mesa3d

commit f754901b36a9ab5635a29b19b7006e0ec38c3690

zink: start a unified driver workarounds struct
eventually anything that checks driverID should be moved here for ease of
reference/updating

Acked-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15392>

src/gallium/drivers/zink/zink_screen.c
@@ -2056,6 +2056,12 @@ zink_get_sample_pixel_grid(struct pipe_screen *pscreen, unsigned sample_count,
    *height = screen->maxSampleLocationGridSize[idx].height;
 }
 
+static void
+init_driver_workarounds(struct zink_screen *screen)
+{
+   screen->driver_workarounds.color_write_missing = !screen->info.have_EXT_color_write_enable;
+}
+
 static struct zink_screen *
 zink_internal_create_screen(const struct pipe_screen_config *config)
 {
@@ -2265,6 +2271,8 @@ zink_internal_create_screen(const struct pipe_screen_config *config)
    screen->base.create_vertex_state = zink_cache_create_vertex_state;
    screen->base.vertex_state_destroy = zink_cache_vertex_state_destroy;
    glsl_type_singleton_init_or_ref();
+
+   init_driver_workarounds(screen);
 
    return screen;
 

src/gallium/drivers/zink/zink_screen.h
@@ -177,6 +177,10 @@ struct zink_screen {
    } null_descriptor_hashes;
 
    VkExtent2D maxSampleLocationGridSize[5];
+
+   struct {
+      bool color_write_missing;
+   } driver_workarounds;
 };
 
 

commit 551bea4e8b12ecccc7dbf97e22f03520645f00a3

zink: fix EXT_color_write_enable check
Checking for the extension isn't enough, we also need to check for the
feature-bit.

Fixes: f754901b36a ("zink: start a unified driver workarounds struct")
Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17709>

src/gallium/drivers/zink/zink_screen.c
@@ -2079,7 +2079,11 @@ init_driver_workarounds(struct zink_screen *screen)
    default:
       break;
    }
-   screen->driver_workarounds.color_write_missing = !screen->info.have_EXT_color_write_enable;
+
+   screen->driver_workarounds.color_write_missing =
+      !screen->info.have_EXT_color_write_enable ||
+      !screen->info.cwrite_feats.colorWriteEnable;
+
    screen->driver_workarounds.depth_clip_control_missing = !screen->info.have_EXT_depth_clip_control;
    if (screen->info.driver_props.driverID == VK_DRIVER_ID_AMD_PROPRIETARY)
       /* this completely breaks xfb somehow */