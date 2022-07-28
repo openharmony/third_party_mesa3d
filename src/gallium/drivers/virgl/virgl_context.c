openharmony/third_party_mesa3d

commit 62e426550ac48b9647f3fada46daed822a059ad4

virgl: Switch to nir-to-tgsi by default.
With !8044 all TGSI drivers will end up going through the nir-to-tgsi
path, so make the switch now that CI is happy (which will also make sure
that future NTT work doesn't break virgl).

Reviewed-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13295>

src/gallium/drivers/virgl/ci/traces-virgl.yml
@@ -9,7 +9,7 @@ traces:
   - path: glmark2/jellyfish.trace
     expectations:
       - device: gl-virgl
-        checksum: 1873b70a28539825879b530477cf6d27
+        checksum: 48a2ad1162bf92301cedbe53edf52a6b
   - path: glxgears/glxgears-2.trace
     expectations:
       - device: gl-virgl
@@ -17,11 +17,11 @@ traces:
   - path: gputest/furmark.trace
     expectations:
       - device: gl-virgl
-        checksum: a38d4c123d13c5ccd3a86f0663fe1aab
+        checksum: 6e498eb959f7da847eb6d2cbbfaea5d5
   - path: gputest/pixmark-piano.trace
     expectations:
       - device: gl-virgl
-        checksum: b580ae01560380461a103975cab77393
+        checksum: 33770ade1aed893528c597f63af892f2
   - path: gputest/triangle.trace
     expectations:
       - device: gl-virgl
@@ -33,7 +33,7 @@ traces:
   - path: 0ad/0ad.trace
     expectations:
       - device: gl-virgl
-        checksum: 5e5bd83446d2554bf25761576d9b1af6
+        checksum: 5e8f945ba7316a70a5195c1c23d35809
   - path: glmark2/buffer:update-fraction=0.5:update-dispersion=0.9:columns=200:update-method=map:interleave=false.trace
     expectations:
       - device: gl-virgl
@@ -77,7 +77,7 @@ traces:
   - path: glmark2/effect2d:kernel=0,1,0;1,-4,1;0,1,0;.trace
     expectations:
       - device: gl-virgl
-        checksum: 25d3b5d18a64a86fc812872987f4f5e7
+        checksum: 35584880539813436d87bfcbe22cf59b
   - path: glmark2/effect2d:kernel=1,1,1,1,1;1,1,1,1,1;1,1,1,1,1;.trace
     expectations:
       - device: gl-virgl
@@ -174,11 +174,11 @@ traces:
   - path: gputest/pixmark-volplosion.trace
     expectations:
       - device: gl-virgl
-        checksum: 9bedb84d81528e1b4087522de9f70383
+        checksum: 0c7aab484c251b0f90745ced620bed82
   - path: gputest/plot3d.trace
     expectations:
       - device: gl-virgl
-        checksum: 7e818a6070005056700e5ef8590a3f8e
+        checksum: 2915192067704d738cdc4c2eaa88a2b1
 # Times out
 # - path: gputest/tessmark.trace
 #   expectations:
@@ -246,7 +246,7 @@ traces:
   - path: supertuxkart/supertuxkart-mansion-egl-gles.trace
     expectations:
       - device: gl-virgl
-        checksum: 5e4e0cedd57bedf3eb7f127489a46b12
+        checksum: b93f2d23cc42072eff674829094cbe74
   - path: xonotic/xonotic-keybench-high.trace
     expectations:
       - device: gl-virgl

src/gallium/drivers/virgl/virgl_screen.c
@@ -46,7 +46,7 @@ int virgl_debug = 0;
 static const struct debug_named_value virgl_debug_options[] = {
    { "verbose",   VIRGL_DEBUG_VERBOSE,             NULL },
    { "tgsi",      VIRGL_DEBUG_TGSI,                NULL },
-   { "nir",       VIRGL_DEBUG_NIR,                 NULL },
+   { "use_tgsi",  VIRGL_DEBUG_USE_TGSI,            NULL },
    { "noemubgra", VIRGL_DEBUG_NO_EMULATE_BGRA,     "Disable tweak to emulate BGRA as RGBA on GLES hosts"},
    { "nobgraswz", VIRGL_DEBUG_NO_BGRA_DEST_SWIZZLE,"Disable tweak to swizzle emulated BGRA on GLES hosts" },
    { "sync",      VIRGL_DEBUG_SYNC,                "Sync after every flush" },
@@ -437,9 +437,9 @@ virgl_get_shader_param(struct pipe_screen *screen,
          else
             return vscreen->caps.caps.v2.max_shader_image_other_stages;
       case PIPE_SHADER_CAP_PREFERRED_IR:
-         return (virgl_debug & VIRGL_DEBUG_NIR) ? PIPE_SHADER_IR_NIR : PIPE_SHADER_IR_TGSI;
+         return (virgl_debug & VIRGL_DEBUG_USE_TGSI) ? PIPE_SHADER_IR_TGSI : PIPE_SHADER_IR_NIR;
       case PIPE_SHADER_CAP_SUPPORTED_IRS:
-         return (1 << PIPE_SHADER_IR_TGSI) | ((virgl_debug & VIRGL_DEBUG_NIR) ? (1 << PIPE_SHADER_IR_NIR) : 0);
+         return (1 << PIPE_SHADER_IR_TGSI) | ((virgl_debug & VIRGL_DEBUG_USE_TGSI) ? 0 : (1 << PIPE_SHADER_IR_NIR));
       case PIPE_SHADER_CAP_MAX_HW_ATOMIC_COUNTERS:
          return vscreen->caps.caps.v2.max_atomic_counters[shader];
       case PIPE_SHADER_CAP_MAX_HW_ATOMIC_COUNTER_BUFFERS:
@@ -961,7 +961,7 @@ static void virgl_disk_cache_create(struct virgl_screen *screen)
    _mesa_sha1_init(&sha1_ctx);
    _mesa_sha1_update(&sha1_ctx, id_sha1, build_id_len);
 
-   uint32_t shader_debug_flags = virgl_debug & VIRGL_DEBUG_NIR;
+   uint32_t shader_debug_flags = virgl_debug & VIRGL_DEBUG_USE_TGSI;
    _mesa_sha1_update(&sha1_ctx, &shader_debug_flags, sizeof(shader_debug_flags));
 
    uint8_t sha1[20];

src/gallium/drivers/virgl/virgl_screen.h
@@ -37,7 +37,7 @@ enum virgl_debug_flags {
    VIRGL_DEBUG_SYNC                 = 1 << 4,
    VIRGL_DEBUG_XFER                 = 1 << 5,
    VIRGL_DEBUG_NO_COHERENT          = 1 << 6,
-   VIRGL_DEBUG_NIR                  = 1 << 7,
+   VIRGL_DEBUG_USE_TGSI             = 1 << 7,
    VIRGL_DEBUG_L8_SRGB_ENABLE_READBACK = 1 << 8,
 };
 

commit 64b8bb82c0a296e34efa0e77fc1ad148cc1304a8

virgL: lower fneg and fabs
The host doesn't currently handle source mods on immediate values
correctly, so lower the ops that would create these source mods.

v2: update trace checksum

Fixes:  62e426550ac48b9647f3fada46daed822a059ad4
    virgl: Switch to nir-to-tgsi by default.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6929

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Italo Nicola <italonicola@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17765>

src/gallium/drivers/virgl/ci/traces-virgl.yml
@@ -207,7 +207,7 @@ traces:
   - path: itoral-gl-terrain-demo/demo-v2.trace
     expectations:
       - device: gl-virgl
-        checksum: 716d4fe36a6212b161285fed8a423ee8
+        checksum: 37780a6eaa38a55700e8207e89009f56
   - path: neverball/neverball-v2.trace
     expectations:
       - device: gl-virgl

src/gallium/drivers/virgl/virgl_context.c
@@ -686,7 +686,8 @@ static void *virgl_shader_encoder(struct pipe_context *ctx,
 
    if (shader->type == PIPE_SHADER_IR_NIR) {
       struct nir_to_tgsi_options options = {
-         .unoptimized_ra = true
+         .unoptimized_ra = true,
+         .lower_fabs = true
       };
       nir_shader *s = nir_shader_clone(NULL, shader->ir.nir);
 
@@ -1375,7 +1376,8 @@ static void *virgl_create_compute_state(struct pipe_context *ctx,
 
    if (state->ir_type == PIPE_SHADER_IR_NIR) {
       struct nir_to_tgsi_options options = {
-         .unoptimized_ra = true
+         .unoptimized_ra = true,
+         .lower_fabs = true
       };
       nir_shader *s = nir_shader_clone(NULL, state->prog);
       ntt_tokens = tokens = nir_to_tgsi_options(s, vctx->base.screen, &options); /* takes ownership */

src/gallium/drivers/virgl/virgl_screen.c
@@ -1088,6 +1088,7 @@ virgl_create_screen(struct virgl_winsys *vws, const struct pipe_screen_config *c
        * ffract+fsub back into ffloor.
        */
       screen->compiler_options.lower_ffloor = true;
+      screen->compiler_options.lower_fneg = true;
    }
 
    slab_create_parent(&screen->transfer_pool, sizeof(struct virgl_transfer), 16);