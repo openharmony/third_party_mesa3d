openharmony/third_party_mesa3d

commit 44b126beaf3f88700c97ac57763a1a77d8e66ea8

dri: Fill in the driver extensions for the legacy createNewScreen paths
The inner routine is going to want access to the driver extensions, but
the legacy paths only give you the loader extensions.

Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15649>

src/gallium/frontends/dri/dri_util.c
@@ -42,6 +42,7 @@
 #include <stdbool.h>
 #include "dri_util.h"
 #include "dri_context.h"
+#include "dri_screen.h"
 #include "utils.h"
 #include "util/u_endian.h"
 #include "util/driconf.h"
@@ -197,8 +198,19 @@ dri2CreateNewScreen(int scrn, int fd,
 		    const __DRIextension **extensions,
 		    const __DRIconfig ***driver_configs, void *data)
 {
-   return driCreateNewScreen2(scrn, fd, extensions, NULL,
-                               driver_configs, data);
+   return driCreateNewScreen2(scrn, fd, extensions,
+                              galliumdrm_driver_extensions,
+                              driver_configs, data);
+}
+
+static __DRIscreen *
+swkmsCreateNewScreen(int scrn, int fd,
+		     const __DRIextension **extensions,
+		     const __DRIconfig ***driver_configs, void *data)
+{
+   return driCreateNewScreen2(scrn, fd, extensions,
+                              dri_kms_driver_extensions,
+                              driver_configs, data);
 }
 
 /** swrast driver createNewScreen entrypoint. */
@@ -206,8 +218,9 @@ static __DRIscreen *
 driSWRastCreateNewScreen(int scrn, const __DRIextension **extensions,
                          const __DRIconfig ***driver_configs, void *data)
 {
-   return driCreateNewScreen2(scrn, -1, extensions, NULL,
-                               driver_configs, data);
+   return driCreateNewScreen2(scrn, -1, extensions,
+                              galliumsw_driver_extensions,
+                              driver_configs, data);
 }
 
 static __DRIscreen *
@@ -822,6 +835,20 @@ const __DRIdri2Extension driDRI2Extension = {
     .base = { __DRI_DRI2, 4 },
 
     .createNewScreen            = dri2CreateNewScreen,
+    .createNewDrawable          = driCreateNewDrawable,
+    .createNewContext           = driCreateNewContext,
+    .getAPIMask                 = driGetAPIMask,
+    .createNewContextForAPI     = driCreateNewContextForAPI,
+    .allocateBuffer             = dri2AllocateBuffer,
+    .releaseBuffer              = dri2ReleaseBuffer,
+    .createContextAttribs       = driCreateContextAttribs,
+    .createNewScreen2           = driCreateNewScreen2,
+};
+
+const __DRIdri2Extension swkmsDRI2Extension = {
+    .base = { __DRI_DRI2, 4 },
+
+    .createNewScreen            = swkmsCreateNewScreen,
     .createNewDrawable          = driCreateNewDrawable,
     .createNewContext           = driCreateNewContext,
     .getAPIMask                 = driGetAPIMask,

src/gallium/frontends/dri/dri_util.h
@@ -69,6 +69,7 @@ struct gl_context;
 extern const __DRIcoreExtension driCoreExtension;
 extern const __DRIswrastExtension driSWRastExtension;
 extern const __DRIdri2Extension driDRI2Extension;
+extern const __DRIdri2Extension swkmsDRI2Extension;
 extern const __DRI2configQueryExtension dri2ConfigQueryExtension;
 extern const __DRIcopySubBufferExtension driCopySubBufferExtension;
 extern const __DRI2flushControlExtension dri2FlushControlExtension;

commit be10855ca1a1fe47721aa59c196a706a7d8fb86e

gallium: Disable dri2 interface on OSX
Error message:
Undefined symbols for architecture x86_64:
  "_dri_kms_driver_extensions", referenced from:
      _swkmsCreateNewScreen in libdri.a(dri_util.c.o)
  "_galliumdrm_driver_extensions", referenced from:
      _dri2CreateNewScreen in libdri.a(dri_util.c.o)
ld: symbol(s) not found for architecture x86_64

Fixes: 44b126beaf3 ("dri: Fill in the driver extensions for the legacy createNewScreen paths")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6519

Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
Reviewed-by: Eric Engestrom <eric@igalia.com>
Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17784>

meson.build
@@ -1692,6 +1692,9 @@ endif
 if with_dri
   pre_args += '-DHAVE_DRI'
 endif
+if with_dri2
+  pre_args += '-DHAVE_DRI2'
+endif
 if with_gallium_drisw_kms
   pre_args += '-DHAVE_DRISW_KMS'
 endif

src/gallium/frontends/dri/dri_util.c
@@ -963,6 +963,8 @@ const __DRIcoreExtension driCoreExtension = {
     .unbindContext              = driUnbindContext
 };
 
+#if HAVE_DRI2
+
 /** DRI2 interface */
 const __DRIdri2Extension driDRI2Extension = {
     .base = { __DRI_DRI2, 4 },
@@ -991,6 +993,8 @@ const __DRIdri2Extension swkmsDRI2Extension = {
     .createContextAttribs       = driCreateContextAttribs,
     .createNewScreen2           = driCreateNewScreen2,
 };
+
+#endif
 
 const __DRIswrastExtension driSWRastExtension = {
     .base = { __DRI_SWRAST, 4 },