openharmony/third_party_mesa3d

commit 8f23eacadf5fcbbba1e7ee6b3cb19d1b86782f8c

tu: increase maxPushConstantsSize to 256.
Now there are two paths for push constants.

When it's range is under 128b, we can use shared consts.
When it's over 128b, we can instead do loading data through
regular path, which is same as the previous way.

Now we can satisfy emulations like vkd3d that requires 256b for
its root signatures and we think it fairly maps to push constants
rather than inline uniform blocks that requires one indirection.

Signed-off-by: Hyunjun Ko <zzoon@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15503>

src/freedreno/vulkan/tu_shader.c
@@ -141,18 +141,22 @@ static void
 lower_load_push_constant(struct tu_device *dev,
                          nir_builder *b,
                          nir_intrinsic_instr *instr,
-                         struct tu_shader *shader)
+                         struct tu_shader *shader,
+                         const struct tu_pipeline_layout *layout)
 {
    uint32_t base = nir_intrinsic_base(instr);
    assert(base % 4 == 0);
    assert(base >= shader->push_consts.lo * 4);
    base -= shader->push_consts.lo * 4;
 
+   if (tu6_shared_constants_enable(layout, dev->compiler))
+      base += dev->compiler->shared_consts_base_offset * 4;
+
    nir_ssa_def *load =
       nir_load_uniform(b, instr->num_components,
             instr->dest.ssa.bit_size,
             nir_ushr(b, instr->src[0].ssa, nir_imm_int(b, 2)),
-            .base = base + dev->compiler->shared_consts_base_offset * 4);
+            .base = base);
 
    nir_ssa_def_rewrite_uses(&instr->dest.ssa, load);
 
@@ -401,7 +405,7 @@ lower_intrinsic(nir_builder *b, nir_intrinsic_instr *instr,
 {
    switch (instr->intrinsic) {
    case nir_intrinsic_load_push_constant:
-      lower_load_push_constant(dev, b, instr, shader);
+      lower_load_push_constant(dev, b, instr, shader, layout);
       return true;
 
    case nir_intrinsic_load_vulkan_descriptor:
@@ -827,10 +831,13 @@ tu_shader_create(struct tu_device *dev,
 
    ir3_finalize_nir(dev->compiler, nir);
 
+   uint32_t reserved_consts_vec4 = align(shader->push_consts.dwords, 16) / 4;
+   bool shared_consts_enable = tu6_shared_constants_enable(layout, dev->compiler);
+
    shader->ir3_shader =
       ir3_shader_from_nir(dev->compiler, nir, &(struct ir3_shader_options) {
-                           .reserved_user_consts = 0,
-                           .shared_consts_enable = layout->push_constant_size > 0,
+                           .reserved_user_consts = reserved_consts_vec4,
+                           .shared_consts_enable = shared_consts_enable,
                            .api_wavesize = key->api_wavesize,
                            .real_wavesize = key->real_wavesize,
                           }, &so_info);

src/freedreno/vulkan/tu_pipeline.c
@@ -1692,7 +1692,8 @@ tu6_emit_program_config(struct tu_cs *cs,
 
    STATIC_ASSERT(MESA_SHADER_VERTEX == 0);
 
-   bool shared_consts_enable = builder->layout->push_constant_size > 0;
+   bool shared_consts_enable = tu6_shared_constants_enable(builder->layout,
+         builder->device->compiler);
    tu6_emit_shared_consts_enable(cs, shared_consts_enable);
 
    tu_cs_emit_regs(cs, A6XX_HLSQ_INVALIDATE_CMD(
@@ -2811,7 +2812,7 @@ tu_pipeline_builder_compile_shaders(struct tu_pipeline_builder *builder,
       stage_infos[stage] = &builder->create_info->pStages[i];
    }
 
-   if (builder->layout->push_constant_size > 0) {
+   if (tu6_shared_constants_enable(builder->layout, builder->device->compiler)) {
       pipeline->shared_consts = (struct tu_push_constant_range) {
          .lo = 0,
          .dwords = builder->layout->push_constant_size / 4,
@@ -4064,7 +4065,7 @@ tu_compute_pipeline_create(VkDevice device,
          VK_PIPELINE_CREATION_FEEDBACK_APPLICATION_PIPELINE_CACHE_HIT_BIT;
    }
 
-   if (layout->push_constant_size > 0) {
+   if (tu6_shared_constants_enable(layout, dev->compiler)) {
       pipeline->shared_consts = (struct tu_push_constant_range) {
          .lo = 0,
          .dwords = layout->push_constant_size / 4,

src/freedreno/vulkan/tu_private.h
@@ -114,7 +114,7 @@ typedef uint32_t xcb_window_t;
 #define MAX_VIEWPORT_SIZE (1 << 14)
 #define MAX_SCISSORS 16
 #define MAX_DISCARD_RECTANGLES 4
-#define MAX_PUSH_CONSTANTS_SIZE 128
+#define MAX_PUSH_CONSTANTS_SIZE 256
 #define MAX_PUSH_DESCRIPTORS 32
 #define MAX_DYNAMIC_UNIFORM_BUFFERS 16
 #define MAX_DYNAMIC_STORAGE_BUFFERS 8
@@ -1429,6 +1429,15 @@ void
 tu_shader_destroy(struct tu_device *dev,
                   struct tu_shader *shader,
                   const VkAllocationCallbacks *alloc);
+
+static bool inline
+tu6_shared_constants_enable(const struct tu_pipeline_layout *layout,
+                            const struct ir3_compiler *compiler)
+{
+   return layout->push_constant_size > 0 &&
+          layout->push_constant_size <= (compiler->shared_consts_size * 16);
+}
+
 
 struct tu_program_descriptor_linkage
 {

commit 83073ed7484c102c13d9a63d268f3c26e3775e88

turnip: shared_consts and push_consts are mutually exclusive
Skip gather_push_constants when shared consts are enabled.  This makes
sure push_consts is only zero-initialized, and reserved_user_consts is
0.  This saves some space in the const file.

This change also adds a few asserts and a comment to
lower_load_push_constant.  Because shared consts share the same range
for all stages, we should not apply per-stage offsets in
lower_load_push_constant.  It worked because nir_lower_explicit_io
always sets base to 0 for nir_var_mem_push_const and
shader->push_consts.lo was always 0 for all stages.

Fixes: 8f23eacadf5 ("tu: increase maxPushConstantsSize to 256.")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17777>

src/freedreno/vulkan/tu_cmd_buffer.c
@@ -4213,6 +4213,12 @@ tu6_emit_consts(struct tu_cmd_buffer *cmd,
 
    if (pipeline->shared_consts.dwords > 0) {
       tu6_emit_shared_consts(&cs, pipeline, cmd->push_constants, compute);
+
+      for (uint32_t i = 0; i < ARRAY_SIZE(pipeline->program.link); i++) {
+         const struct tu_program_descriptor_linkage *link =
+            &pipeline->program.link[i];
+         assert(!link->push_consts.dwords);
+      }
    } else {
       if (compute) {
          tu6_emit_user_consts(&cs, pipeline, MESA_SHADER_COMPUTE, cmd->push_constants);

src/freedreno/vulkan/tu_shader.c
@@ -146,11 +146,17 @@ lower_load_push_constant(struct tu_device *dev,
 {
    uint32_t base = nir_intrinsic_base(instr);
    assert(base % 4 == 0);
-   assert(base >= shader->push_consts.lo * 4);
-   base -= shader->push_consts.lo * 4;
 
-   if (tu6_shared_constants_enable(layout, dev->compiler))
+   if (tu6_shared_constants_enable(layout, dev->compiler)) {
+      /* All stages share the same range.  We could potentially add
+       * push_constant_offset to layout and apply it, but this is good for
+       * now.
+       */
       base += dev->compiler->shared_consts_base_offset * 4;
+   } else {
+      assert(base >= shader->push_consts.lo * 4);
+      base -= shader->push_consts.lo * 4;
+   }
 
    nir_ssa_def *load =
       nir_load_uniform(b, instr->num_components,
@@ -641,7 +647,8 @@ tu_lower_io(nir_shader *shader, struct tu_device *dev,
             struct tu_shader *tu_shader,
             const struct tu_pipeline_layout *layout)
 {
-   gather_push_constants(shader, tu_shader);
+   if (!tu6_shared_constants_enable(layout, dev->compiler))
+      gather_push_constants(shader, tu_shader);
 
    struct lower_instr_params params = {
       .dev = dev,
@@ -835,6 +842,8 @@ tu_shader_create(struct tu_device *dev,
 
    uint32_t reserved_consts_vec4 = align(shader->push_consts.dwords, 16) / 4;
    bool shared_consts_enable = tu6_shared_constants_enable(layout, dev->compiler);
+   if (shared_consts_enable)
+      assert(!shader->push_consts.dwords);
 
    shader->ir3_shader =
       ir3_shader_from_nir(dev->compiler, nir, &(struct ir3_shader_options) {