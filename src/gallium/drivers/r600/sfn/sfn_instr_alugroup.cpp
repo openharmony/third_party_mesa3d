openharmony/third_party_mesa3d

commit 9fdd2579338f7804b58abcc41048bc0a4c67a507

r600/sfn: Handle slot differences between pre-EG and EG
Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Filip Gawin <filip@gawin.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17822>

src/gallium/drivers/r600/sfn/sfn_instr_alugroup.cpp
@@ -42,8 +42,12 @@ bool AluGroup::add_instruction(AluInstr *instr)
    if (m_has_lds_op && instr->has_lds_access())
       return false;
 
-   if (instr->has_alu_flag(alu_is_trans) && add_trans_instructions(instr))
-      return true;
+   if (instr->has_alu_flag(alu_is_trans)) {
+      auto opinfo = alu_ops.find(instr->opcode());
+      assert(opinfo->second.can_channel(AluOp::t, s_eg_t_slot_handling));
+      if (add_trans_instructions(instr))
+         return true;
+   }
 
    if (add_vec_instructions(instr)) {
       instr->set_parent_group(this);
@@ -54,7 +58,7 @@ bool AluGroup::add_instruction(AluInstr *instr)
    assert(opinfo != alu_ops.end());
 
    if (s_max_slots > 4 &&
-       opinfo->second.can_channel(AluOp::t) &&
+       opinfo->second.can_channel(AluOp::t, s_eg_t_slot_handling) &&
        add_trans_instructions(instr)) {
       instr->set_parent_group(this);
       return true;
@@ -78,8 +82,10 @@ bool AluGroup::add_trans_instructions(AluInstr *instr)
    auto opinfo = alu_ops.find(instr->opcode());
    assert(opinfo != alu_ops.end());
 
-   if (!opinfo->second.can_channel(AluOp::t))
+   if (!opinfo->second.can_channel(AluOp::t, s_eg_t_slot_handling)) {
+      std::cerr << *instr << ": t-slot not supported ("<< s_eg_t_slot_handling << ")\n";
       return false;
+   }
 
 
    /* if we schedule a non-trans instr into the trans slot, we have to make
@@ -373,15 +379,19 @@ AluInstr::SrcValues AluGroup::get_kconsts() const
 
 void AluGroup::set_chipclass(r600_chip_class chip_class)
 {
+   s_eg_t_slot_handling = false;
    switch (chip_class) {
    case ISA_CC_CAYMAN:
       s_max_slots = 4;
    break;
+   case ISA_CC_EVERGREEN:
+      s_eg_t_slot_handling = true;
+      FALLTHROUGH;
    default:
       s_max_slots = 5;
    }
 }
 
 int AluGroup::s_max_slots = 5;
-
+bool AluGroup::s_eg_t_slot_handling = false;
 }

src/gallium/drivers/r600/sfn/sfn_alu_defines.h
@@ -312,18 +312,19 @@ struct AluOp {
    static constexpr int t = 16;
    static constexpr int a = 31;
 
-   AluOp(int ns, int f, int um, const char *n):
-      nsrc(ns), is_float(f), unit_mask(um), name(n)
+   AluOp(int ns, int f, int um, int um_eg, const char *n):
+      nsrc(ns), is_float(f), unit_mask(um), unit_mask_eg(um_eg), name(n)
    {
    }
 
-   bool can_channel(int flags) const {
-      return flags & unit_mask;
+   bool can_channel(int flags, bool eg) const {
+      return flags & (eg ? unit_mask_eg : unit_mask);
    }
 
    int nsrc: 4;
    int is_float:1;
    int unit_mask: 5;
+   int unit_mask_eg: 5;
    const char *name;
 };
 

src/gallium/drivers/r600/sfn/sfn_instr_alu.cpp
@@ -1227,6 +1227,21 @@ bool AluInstr::from_nir(nir_alu_instr *alu, Shader& shader)
          ;
       }
    } else {
+      if (shader.chip_class() == ISA_CC_EVERGREEN) {
+         switch (alu->op) {
+         case nir_op_f2i32: return emit_alu_f2i32_or_u32_eg(*alu, op1_flt_to_int, shader);
+         case nir_op_f2u32: return emit_alu_f2i32_or_u32_eg(*alu, op1_flt_to_uint, shader);
+         default:
+            ;
+         }
+      } else {
+         switch (alu->op) {
+         case nir_op_f2i32: return emit_alu_trans_op1_eg(*alu, op1_flt_to_int, shader);
+         case nir_op_f2u32: return emit_alu_trans_op1_eg(*alu, op1_flt_to_uint, shader);
+         default:
+            ;
+         }
+      }
       switch (alu->op) {
       case nir_op_fcos_amd: return emit_alu_trans_op1_eg(*alu, op1_cos, shader);
       case nir_op_fexp2: return emit_alu_trans_op1_eg(*alu, op1_exp_ieee, shader);
@@ -1240,8 +1255,6 @@ bool AluInstr::from_nir(nir_alu_instr *alu, Shader& shader)
       case nir_op_imul: return emit_alu_trans_op2_eg(*alu, op2_mullo_int, shader);
       case nir_op_imul_high: return emit_alu_trans_op2_eg(*alu, op2_mulhi_int, shader);
       case nir_op_umul_high: return emit_alu_trans_op2_eg(*alu, op2_mulhi_uint, shader);
-      case nir_op_f2i32: return emit_alu_f2i32_or_u32_eg(*alu, op1_flt_to_int, shader);
-      case nir_op_f2u32: return emit_alu_f2i32_or_u32_eg(*alu, op1_flt_to_uint, shader);
       default:
          ;
       }

src/gallium/drivers/r600/sfn/sfn_instr_alugroup.h
@@ -99,6 +99,7 @@ private:
    AluReadportReservation m_readports_evaluator;
 
    static int s_max_slots;
+   static bool s_eg_t_slot_handling;
 
    PRegister m_addr_used{nullptr};
 

src/gallium/drivers/r600/sfn/sfn_scheduler.cpp
@@ -148,7 +148,7 @@ public:
 
 class BlockSheduler {
 public:
-   BlockSheduler();
+   BlockSheduler( bool eg_t_slot_handling);
    void run(Shader *shader);
 
    void finalize();
@@ -218,6 +218,7 @@ private:
 
    int m_lds_addr_count{0};
    int m_alu_groups_schduled{0};
+   bool m_eg_t_slot_handling;
 
 };
 
@@ -236,7 +237,7 @@ Shader *schedule(Shader *original)
    // to be able to re-start scheduling
 
    auto scheduled_shader = original;
-   BlockSheduler s;
+   BlockSheduler s(original->chip_class() >= ISA_CC_EVERGREEN);
    s.run(scheduled_shader);
    s.finalize();
 
@@ -250,12 +251,13 @@ Shader *schedule(Shader *original)
    return scheduled_shader;
 }
 
-BlockSheduler::BlockSheduler():
+BlockSheduler::BlockSheduler(bool eg_t_slot_handling):
    current_shed(sched_alu),
    m_last_pos(nullptr),
    m_last_pixel(nullptr),
    m_last_param(nullptr),
-   m_current_block(nullptr)
+   m_current_block(nullptr),
+   m_eg_t_slot_handling(eg_t_slot_handling)
 {
 }
 
@@ -820,7 +822,8 @@ bool BlockSheduler::collect_ready_alu_vec(std::list<AluInstr *>& ready, std::lis
          else if (AluGroup::has_t()) {
             auto opinfo = alu_ops.find((*i)->opcode());
             assert(opinfo != alu_ops.end());
-            if (opinfo->second.can_channel(AluOp::t) && !(*i)->indirect_addr().first)
+            if (opinfo->second.can_channel(AluOp::t, m_eg_t_slot_handling) &&
+                !(*i)->indirect_addr().first)
                priority = -1;
          }
 

commit d63b2e54f560a6f299c7992623bf5148a7d883ea

r600/sfn: remove leftover debug output
Fixes: 9fdd2579338f7804b58abcc41048bc0a4c67a507
   r600/sfn: Handle slot differences between pre-EG and EG

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17839>

src/gallium/drivers/r600/sfn/sfn_instr_alugroup.cpp
@@ -82,11 +82,8 @@ bool AluGroup::add_trans_instructions(AluInstr *instr)
    auto opinfo = alu_ops.find(instr->opcode());
    assert(opinfo != alu_ops.end());
 
-   if (!opinfo->second.can_channel(AluOp::t, s_eg_t_slot_handling)) {
-      std::cerr << *instr << ": t-slot not supported ("<< s_eg_t_slot_handling << ")\n";
+   if (!opinfo->second.can_channel(AluOp::t, s_eg_t_slot_handling))
       return false;
-   }
-
 
    /* if we schedule a non-trans instr into the trans slot, we have to make
     * sure that the corresponding vector slot is already occupied, otherwise