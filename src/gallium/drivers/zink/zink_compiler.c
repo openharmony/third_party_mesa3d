openharmony/third_party_mesa3d

commit 3249135c1725afa46250061bf2d9f547ea308617

zink: invoke descriptor_program_deinit for programs on context destroy
this should make multi-context shutdown more stable

affects:
glx@glx-visuals-depth -pixmap
glx@glx-visuals-stencil

cc: mesa-stable

Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17658>

src/gallium/drivers/zink/zink_context.c
@@ -101,6 +101,17 @@ zink_context_destroy(struct pipe_context *pctx)
    if (ctx->batch.state && !screen->device_lost && VKSCR(QueueWaitIdle)(screen->queue) != VK_SUCCESS)
       mesa_loge("ZINK: vkQueueWaitIdle failed");
 
+   for (unsigned i = 0; i < ARRAY_SIZE(ctx->program_cache); i++) {
+      hash_table_foreach(&ctx->program_cache[i], entry) {
+         struct zink_program *pg = entry->data;
+         screen->descriptor_program_deinit(ctx, pg);
+      }
+   }
+   hash_table_foreach(&ctx->compute_program_cache, entry) {
+      struct zink_program *pg = entry->data;
+      screen->descriptor_program_deinit(ctx, pg);
+   }
+
    if (ctx->blitter)
       util_blitter_destroy(ctx->blitter);
    for (unsigned i = 0; i < ctx->fb_state.nr_cbufs; i++)

src/gallium/drivers/zink/zink_descriptors_lazy.c
@@ -354,6 +354,8 @@ void
 zink_descriptor_program_deinit_lazy(struct zink_context *ctx, struct zink_program *pg)
 {
    struct zink_screen *screen = zink_screen(ctx->base.screen);
+   if (!pg->dd)
+      return;
    for (unsigned i = 0; pg->num_dsl && i < ZINK_DESCRIPTOR_TYPES; i++) {
       if (pg->dd->pool_key[i])
          pg->dd->pool_key[i]->use_count--;
@@ -363,6 +365,7 @@ zink_descriptor_program_deinit_lazy(struct zink_context *ctx, struct zink_progra
          VKSCR(DestroyDescriptorUpdateTemplate)(screen->dev, pg->dd->templates[i], NULL);
    }
    ralloc_free(pg->dd);
+   pg->dd = NULL;
 }
 
 static VkDescriptorPool

commit baf5e7e7f631328760279f054664345d36ca2703

zink: fix gfx program cache pruning with generated tcs
if the tcs was generated, then the prgram was added to the non-tcs cache,
which means deleting it from the tcs+tes cache will fail and then
context_destroy will explode

Fixes: 3249135c172 ("zink: invoke descriptor_program_deinit for programs on context destroy")

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17866>

src/gallium/drivers/zink/zink_compiler.c
@@ -3139,7 +3139,13 @@ zink_shader_free(struct zink_context *ctx, struct zink_shader *shader)
          enum pipe_shader_type pstage = pipe_shader_type_from_mesa(shader->nir->info.stage);
          assert(pstage < ZINK_SHADER_COUNT);
          if (!prog->base.removed && (shader->nir->info.stage != MESA_SHADER_TESS_CTRL || !shader->is_generated)) {
-            _mesa_hash_table_remove_key(&ctx->program_cache[prog->stages_present >> 2], prog->shaders);
+            unsigned stages_present = prog->stages_present;
+            if (prog->shaders[PIPE_SHADER_TESS_CTRL] && prog->shaders[PIPE_SHADER_TESS_CTRL]->is_generated)
+               stages_present &= ~BITFIELD_BIT(PIPE_SHADER_TESS_CTRL);
+            struct hash_table *ht = &ctx->program_cache[stages_present >> 2];
+            struct hash_entry *he = _mesa_hash_table_search(ht, prog->shaders);
+            assert(he);
+            _mesa_hash_table_remove(ht, he);
             prog->base.removed = true;
          }
          if (shader->nir->info.stage != MESA_SHADER_TESS_CTRL || !shader->is_generated)

src/gallium/drivers/zink/zink_context.c
@@ -108,11 +108,13 @@ zink_context_destroy(struct pipe_context *pctx)
    for (unsigned i = 0; i < ARRAY_SIZE(ctx->program_cache); i++) {
       hash_table_foreach(&ctx->program_cache[i], entry) {
          struct zink_program *pg = entry->data;
+         pg->removed = true;
          screen->descriptor_program_deinit(ctx, pg);
       }
    }
    hash_table_foreach(&ctx->compute_program_cache, entry) {
       struct zink_program *pg = entry->data;
+      pg->removed = true;
       screen->descriptor_program_deinit(ctx, pg);
    }
 